e04e30076e187c0678321fb796f14a9290f0ad69
Tom Coakes <tomcoakes@gmail.com>
2015-02-13 21:54:53 +0000

    Changed Order instantiation to a let variable

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,16 +2,16 @@ require 'order'
 
 describe Order do
 
+  let(:order) {Order.new}
+
   it "can have a dish added to the current order" do
     dish = double :dish
-    order = Order.new
     order.add_to_order(dish)
     expect(order.current_order).to include(dish)
   end
 
   it "has a quantity specified for each item in the order" do
     dish = double :dish
-    order = Order.new
     order.add_to_order(dish, 2)
     expect(order.current_order).to include(dish => 2)
   end
@@ -19,7 +19,6 @@ describe Order do
   it "knows the total price of the order" do
     dish1 = double :dish, price: 4
     dish2 = double :dish, price: 2
-    order = Order.new
     order.add_to_order(dish1, 2)
     order.add_to_order(dish2, 1)
     expect(order.total_price).to eq(10)
