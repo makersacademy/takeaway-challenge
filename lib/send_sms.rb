239a49e47849549d1a3c11cb2f57a7628de68e05
Costas Sarris <costassarris@gmail.com>
2015-02-13 17:01:03 +0000

    added twilio file

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'twilio-ruby'
 
+module Send_sms
+
+  def confirmation
+
   # Get your Account Sid and Auth Token from twilio.com/user/account
   account_sid = 'AC3e91b2ba4f4e6bc3369586e5d7c1179c'
   auth_token = '89e4130a264f38b29ff740de206cd418'
@@ -10,3 +14,7 @@ require 'twilio-ruby'
       :to => "+447905809168",     # Replace with your phone number
       :from => "+441412802108")   # Replace with your Twilio number
   puts message.sid
+
+  end
+
+end
