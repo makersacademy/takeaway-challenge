3179be18c6aca4e945562927145e41b3c8ebc9ae
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-6.local>
2015-02-13 16:59:45 +0000

    sms functionality added

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,10 @@
 require_relative 'menu'
 
+
+require 'rubygems'
+require 'twilio-ruby'
+require 'sinatra'
+
 class Order
 
 include Menu
@@ -62,6 +67,14 @@ def send_text
   time1hr = time + 3600
   puts "Thank you! Your order was placed and will be delivered before #{time1hr.strftime("%I:%M%p")}."
 
+account_sid = 'AC60c54cda75e5c928759e85292d5cd749'
+auth_token = 'b6f9be75ac109b8aad8db70d3679272e'
+@client = Twilio::REST::Client.new account_sid, auth_token
+ 
+message = @client.account.messages.create(:body => "Thank you! Your order was placed and will be delivered before #{time1hr.strftime("%I:%M%p")}.",
+    :to => "+447982998xxx",     # Replace with your phone number
+    :from => "+441618505718")   # Replace with your Twilio number
+puts message.sid
 end
 
 def get_overall_price
