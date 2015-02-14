ed79dc9b1c016df78d9a0776f097d0563c92a4ea
Kate Beavis <katebeavis84@gmail.com>
2015-02-14 21:30:27 +0000

    Added Order class with working tests for maximum order and add cereal

--- lib/order.rb
+++ lib/order.rb
@@ -1,24 +1,24 @@
 class Order
 
+  DEFAULT_MAXIMUM = 10
 
+  attr_accessor :orders
 
+  def initialize(options = {})
+    maximum_order = options.fetch(:maximum_order, maximum_order)
+    @orders = {}
+  end
 
+  def maximum_order
+    @maximum_order ||= DEFAULT_MAXIMUM
+  end
 
+  def add_cereal(cereal, quantity)
+    @orders[cereal] = 1
+  end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  def order_count
+    @orders.count
+  end
   
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,7 +2,22 @@ require 'Order'
 
 describe Order do
   
-  let(:order){ Order.new }
+  let(:order){ Order.new(maximum_order: 10) }
+  let(:cereal){ double :cereal}
+
+  it 'should have a maximum order value' do
+    expect(order.maximum_order).to eq 10
+  end
+
+  it 'should let you add cereal to your order' do
+    order.add_cereal(cereal, 1)
+    expect(order.orders).to include (cereal)
+  end
+
+  it 'knows how many items are in the order' do
+    order.add_cereal(cereal, 2)
+    expect(order.orders).to include {double :cereal=>2}
+  end
 
 
 
