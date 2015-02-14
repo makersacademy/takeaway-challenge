2142d016049e54e06fefdcf9a18b54ad018d2dd1
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-14 17:06:44 +0000

    passed second spec test in order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,14 @@
 class Order
 
+  attr_accessor :dishes, :total
+
   def initialize
     @dishes = []
+    @total = 0
   end
 
   def dishes
     @dishes = []
   end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,9 +2,14 @@ require 'order'
 
 describe Order do
 
+  let(:order) { order = Order.new }
+
   it 'should start with no dishes' do
-    order = Order.new
     expect(order.dishes).to eq []
+  end
+
+  it 'should start with no total value' do
+    expect(order.total).to eq 0
   end
 
 end
