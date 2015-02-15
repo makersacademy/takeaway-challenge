d23004065ab0584c755b8c742f4d48c9e47c3e60
gabe <GabeMaker@users.noreply.github.com>
2015-02-15 21:37:16 +0000

    added text sending functionality

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class Takeaway
 
   def dish_list
@@ -12,10 +14,26 @@ class Takeaway
   def check_order(order, total)
     bill = order.map { |dish| dish_list[dish] }.inject(:+)
     if bill == total
+      send_message
       true
     else
       raise "sum is incorrect"
     end
+  end
+
+  def send_message
+    time = Time.new + 3600
+    @account_sid  = "private"
+    @auth_token   = "private"
+    @from_number  = 000000000000 #private
+    @to_number    = 111111111111 #private
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    @account = @client.account
+    @message = @account.messages.create({
+      :from => @from_number, 
+      :to => @to_number, 
+      :body => "Thank you! Your order was placed and will be delivered before #{time}"})
+    puts @message
   end
 
 end
