092af55414b2a3ee273bdcbf331b251b1f209899
gabe <GabeMaker@users.noreply.github.com>
2015-02-15 19:11:39 +0000

    can add item to an order

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,15 @@
 class Customer
 
+  def initialize
+    @order = []
+  end
+
   def order
-    []
+    @order
+  end 
+
+  def add_to_order(item)
+    @order << item
   end 
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,4 +5,11 @@ describe Customer do
     customer = Customer.new
     expect(customer.order).to eq []
   end
+
+  it 'should be able to add an item to an order' do
+    customer = Customer.new
+    customer.add_to_order(:sandwich)
+    expect(customer.order).to eq [:sandwich]
+  end
+
 end
