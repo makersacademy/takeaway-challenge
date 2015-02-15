e6cdafebdf7fd6c006b82e18a657a41c7dbf105f
SebastienPires <pires.sebastien@gmail.com>
2015-02-15 13:20:06 +0000

    First commit and menu list created

---
+++ lib/menu.rb
@@ -0,0 +1,5 @@
+class Menu
+
+food_drinks_and_prices = {"Burger supreme" => 15, "Chicken wrap" => 8, "Chorizo and eggs" => 7, "Salmon sushi x6" => 10, "Gin and tonic" => 5, "Homemade beer" => 3}
+
+end
