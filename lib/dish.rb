cd9fe5a6832fc6eca74b4440c0b042d1ab9f9d25
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-15 20:59:38 +0000

    tested for instance variables

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,7 +1,10 @@
-require 'singleton'
-
 class Dish
-  
 
+  attr_accessor :price, :name 
+  
+  def initialize(options={})
+    @name=options.fetch(:name, "")
+    @price=options.fetch(:price, 1)
+  end
     
 end  
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,6 +2,10 @@ require 'dish'
 
 describe 'Dish' do 
 
+  dish = Dish.new
 
-
+  it 'should have name and price as instance variables' do
+    expect(dish).to respond_to(:price, :name)
+  end  
+  
 end  
