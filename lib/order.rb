ff7baf4959581ef4fbef1157284b1267559f24c4
Chris Ward <chriswarddev@gmail.com>
2015-02-15 14:59:03 +0000

    order now can create a total of the items placed into the order

--- lib/order.rb
+++ lib/order.rb
@@ -12,7 +12,14 @@ class Order
 
 	def add_item(menu,item)
 		order_items << menu.dishes.select{|dish| dish == item}.first
+	end
 
+	def order_total
+		total = 0
+		order_items.each do |order_item|
+			total = total + order_item.price
+		end
+		total
 	end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,23 +5,32 @@ describe Order do
 	let(:menu) {double :menu, dishes: [curry,kebab]}
 	let(:curry) {double :dish, name: "curry", price: 500}
 	let(:kebab){double :dish, name: "kebab", price: 450}
+	let(:order) {Order.new}
 
 	it 'should be able to store a list of dishes' do
-		order = Order.new
 		expect(order.item_count).to eq (0)
 	end
 
 	it 'should be able to select items from the menu and add them to the list' do
-		order = Order.new
 		order.add_item(menu,curry)
 		order.add_item(menu,kebab)
 		expect(order.item_count).to eq (2)
 		expect(order.order_items).to eq [curry,kebab]
 	end
 
-	it 'should be able to total the items in the order list' do
-
+	it 'should be able to total the items in the order list if there is one item' do
+		order.add_item(menu,curry)
+		expect(curry).to receive(:price)
+		expect(order.order_total).to eq 500
 	end
 
+	it 'should be able to total the item in the order list if there are several items' do 
+		order.add_item(menu,curry)
+		order.add_item(menu,kebab)
+		expect(curry).to receive(:price)
+		expect(kebab).to receive(:price)
+		expect(order.order_total).to eq 950
+
+	end
 	
 end
