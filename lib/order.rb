dc18ce8790862614f68ed463ec9deda8fedbf3a0
meads58 <meads109@gmail.com>
2015-02-15 11:12:09 +0000

    can update and add more items to an order, customer order accumulates

--- lib/order.rb
+++ lib/order.rb
@@ -4,14 +4,14 @@ require 'twilio-ruby'
 class Order
 
   def initialize
-    @customer_order = []
+    @customer_order = {}
   end
 
   def place_order(item, quantity, menu)
     raise "Cannot have 0 quantity" if quantity == 0
     item_price = menu.menu_items[item]
-    item_total = sub_total(item_price, quantity)
-    @customer_order << [item, quantity, item_total]
+    item_total_qty_price = sub_total(item, quantity, item_price)
+    @customer_order[item] =  item_total_qty_price
   end
 
   def customer_order
@@ -22,8 +22,26 @@ class Order
 
   end
 
-  def sub_total(item_price,quantity)
-    item_price * quantity
+  def sub_total(item, quantity, item_price)
+    qty = quantity + item_quanity_ordered(item)
+    item_total = (item_price * quantity) + item_total_price(item)
+    accum_order = [qty, item_total]
+  end
+
+  def item_quanity_ordered(item)
+    if customer_order[item].nil?
+      return 0
+    else
+      customer_order[item][0]
+    end
+  end
+
+  def item_total_price(item)
+    if customer_order[item].nil?
+      return 0
+    else
+      customer_order[item][1]
+    end
   end
 
 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,24 +5,39 @@ require 'order'
     let(:order) { Order.new }
     let(:menu) { double :menu }
 
+
+
     it "should allow 1 order of beer to placed" do
-      allow(menu).to receive(:menu_items).and_return( {Beer: 2.0} )
-      order.place_order(:Beer, 1, menu)
-      p order.customer_order
-      expect(order.customer_order).to eq [[:Beer, 1, 2.0]]
+      place_order_helper(:Beer, 1, { Beer: 2.0 } )
+      expect(order.customer_order).to eq({ Beer: [1, 2.0] })
     end
 
     it "should allow 2 items of more than 1 quantity to be ordered" do
+      place_order_helper(:Beer, 3, { Beer: 2.0 } )
+      place_order_helper(:"Chicken Nuggets", 3, { :"Chicken Nuggets" => 3.0} )
+      expect(order.customer_order).to eq({ Beer: [3, 6.0], :"Chicken Nuggets" => [3, 9.0] })
 
     end
-
 
     it "should rasie an error if 0 quantity it ordered" do
       expect { order.place_order(:Beer, 0, menu) }.to raise_error(RuntimeError, "Cannot have 0 quantity")
     end
 
+    it "should accumulate orders for the same item" do
+      place_order_helper(:Beer, 2, { Beer: 2.0 } )
+      place_order_helper(:Beer, 1, { Beer: 2.0 } )
+      expect(order.customer_order).to eq({ Beer: [3, 6.0] })
+    end
+
+
     it 'should calcualate the sub_total of an ordered item' do
-      expect(order.sub_total(2, 3)).to eq 6.0
+      expect(order.sub_total(:Beer, 2, 3)).to eq [2, 6.0]
+    end
+
+
+    def place_order_helper(item , quantity, hash_return)
+      allow(menu).to receive(:menu_items).and_return( hash_return)
+      order.place_order(item, quantity, menu)
     end
 
   end
