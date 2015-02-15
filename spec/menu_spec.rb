cf4440f15db455bf4321d72ea8eecd9237f80a7f
SebastienPires <pires.sebastien@gmail.com>
2015-02-15 23:02:09 +0000

    Added another item on the menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,11 +2,12 @@ require 'menu'
 
 
 describe Menu do
-	let(:fish_and_chips) { double :dish, name: "Fish and chips", price: 15 }
-	let(:menu){Menu.new([fish_and_chips])}
+	let(:fish_and_chips){double :dish, name: "Fish and chips", price: 15}
+	let(:rib_eye){double :dish, name: "Rib eye", price: 20}
+	let(:menu){Menu.new([fish_and_chips, rib_eye])}
 	
 
-	it 'should have an item on the menu' do
-		expect(menu.menu_list).to eq([fish_and_chips])
+	it 'should have a list of food on the menu' do
+		expect(menu.menu_list).to eq([fish_and_chips, rib_eye])
 	end
 end
