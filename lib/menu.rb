ba7b85213992a350d79608bbeff0128a9c7e2a8c
SebastienPires <pires.sebastien@gmail.com>
2015-02-15 21:55:10 +0000

    Deleted code and started again

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,21 +1,13 @@
-module Menu
+class Menu
 
 	attr_reader :menu_list
 
-	def initialize
-		@menu_list = {"Burger supreme" => 15, "Chicken wrap" => 8, "Chorizo and eggs" => 7, "Salmon sushi x6" => 10, "Gin and tonic" => 5, "Homemade beer" => 3}
+	def initialize(menu_list = [])
+		@menu_list = menu_list
 	end
 
-	def menu
-		@menu_list
+	def add(dish)
+		@menu_list << dish
 	end
 
-	def food_item
-		menu.keys
-	end
-
-	def item_price
-		menu.keys
-	end
-	
 end
