6fd2dd7c93e56f484ca76ffaa10b79723b374c36
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-15 13:42:16 +0000

    Menu can receive orders

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,13 +1,17 @@
 class Menu
-  attr_reader :dishlist
+  attr_reader :dishlist, :ordered
 
   def initialize
     @dishlist = []
+    @ordered = []
   end
-
 
   def add_dish(dish)
     @dishlist << dish    
+  end
+
+  def custom_order(orders)
+   orders.each {|order| @ordered << order}  
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,13 +3,20 @@ require 'menu'
 describe Menu do
 
 let(:schnitzel){double :dish, name: "Schnitzel", price: 4}
+let(:pie){double :dish, name: "pie", price: 2}
 let(:menu){Menu.new}
+let(:customer){double :customer}
+let(:order){double :order, orders: [schnitzel, pie]}
 
   it "can contain dishes" do
     menu.add_dish(schnitzel)
     expect(menu.dishlist).not_to be_empty
   end
 
-  
+  it "can receive an order" do
+    menu.custom_order(order.orders)
+    expect(menu.ordered).to eq(order.orders)
+  end
+
   
 end
