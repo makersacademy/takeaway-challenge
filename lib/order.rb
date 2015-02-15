e00d68547fe20e51dc839b1d79b78dfbc3efbbc5
Kate Beavis <katebeavis84@gmail.com>
2015-02-15 15:58:32 +0000

    Added total and price calculation to order

--- lib/order.rb
+++ lib/order.rb
@@ -2,23 +2,40 @@ class Order
 
   DEFAULT_MAXIMUM = 10
 
-  attr_accessor :orders
+  attr_accessor :orders, :count, :total
 
   def initialize(options = {})
-    maximum_order = options.fetch(:maximum_order, maximum_order)
+    @maximum_order = options.fetch(:maximum_order, maximum_order)
     @orders = {}
+    @count = 0
+    @total = 0
   end
 
   def maximum_order
     @maximum_order ||= DEFAULT_MAXIMUM
   end
 
-  def add_cereal(cereal, quantity)
-    @orders[cereal] = 1
+  def add_cereal(cereal, amount)
+    # raise 'Maximum order exceeded' if full?
+    @orders[cereal] = amount
   end
 
-  def order_count
-    @orders.count
+  # def full?
+  #   item_count == maximum_order
+  # end
+
+  # def item_count
+  #   @orders.count
+  # end
+
+  def item_count
+    orders.map {|dish, amount| @count += amount}
+    @count
+  end
+
+  def order_total
+    orders.map {|dish, amount| @total += (dish.price * amount)}
+    @total
   end
   
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,21 +3,34 @@ require 'Order'
 describe Order do
   
   let(:order){ Order.new(maximum_order: 10) }
-  let(:cereal){ double :cereal}
+  let(:frosties){ double :dish, price: 2.50 }
+  let(:coco_pops){ double :dish, price:4.20 }
 
   it 'should have a maximum order value' do
     expect(order.maximum_order).to eq 10
   end
 
   it 'should let you add cereal to your order' do
-    order.add_cereal(cereal, 1)
-    expect(order.orders).to include (cereal)
+    order.add_cereal(frosties, 1)
+    expect(order.orders).to include (frosties)
   end
 
-  it 'knows how many items are in the order' do
-    order.add_cereal(cereal, 2)
-    expect(order.orders).to include {double :cereal=>2}
+  it 'knows how many of each item are in the order' do
+    order.add_cereal(frosties, 2)
+    order.add_cereal(coco_pops, 3)
+    expect(order.item_count).to eq (5)
   end
+
+  it 'can calculate the total of an order' do
+    order.add_cereal(frosties, 2)
+    order.add_cereal(coco_pops, 3)
+    expect(order.order_total).to eq (17.60)
+  end
+
+  # it 'raises error if you try to add more than maximum order' do
+  #   20.times{order.add_cereal(frosties, 1)}
+  #   expect{order.add_cereal(frosties, 1)}.to raise_error(RuntimeError, 'Maximum order exceeded')
+  # end
 
 
 
