3f39295a40aaf26ef1bea46f49712cc720376459
SebastienPires <pires.sebastien@gmail.com>
2015-02-15 13:27:05 +0000

    Added total order and selection method

--- lib/order.rb
+++ lib/order.rb
@@ -5,7 +5,13 @@ class Order
 	attr_reader :food_drinks_and_prices
 
 def initialize
-  @food_drinks_and_prices = {}
+  @food_drinks_and_prices = []
+  @total_order = 0
+end
+
+def selection(dish)
+	food_drinks_and_prices << dish
+	
 end
 
 end
