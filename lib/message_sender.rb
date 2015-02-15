07dc200495d28a7540d8e028ad722d2af0751b8f
Chris Ward <chriswarddev@gmail.com>
2015-02-15 23:10:12 +0000

    message sender knows what the current time is and test passes

--- lib/message_sender.rb
+++ lib/message_sender.rb
@@ -1,3 +1,7 @@
 class MessageSender
 
+	def order_time(time)
+		time.to_s.slice(11,5)
+	end
+
 end
--- spec/message_sender_spec.rb
+++ spec/message_sender_spec.rb
@@ -6,8 +6,12 @@ describe MessageSender do
 	let(:time) {double :time, to_s: "2015-02-15 22:48:22 +0000"}
 
 	it 'should know what time it is' do
-	message_sender = MessageSender.new
+		message_sender = MessageSender.new
+		expect(time).to receive(:to_s)
+		expect(message_sender.order_time(time)).to eq "22:48"
+	end
 
+	it 'should be able to add 1 hour to the order time' do
 
 
 	end
