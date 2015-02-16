b9b82573ddd9989ec73220cd195e805e19f9a0c5
tommasobratto <tbratto22@gmail.com>
2015-02-16 12:16:57 +0000

    fixed 'order' and 'total_price_calculator' methods, tests passed

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,7 +2,7 @@ require 'twilio-ruby'
 
 class Takeaway
 
-  attr_accessor :menu, :dishes_selected, :total_price, :dishes_available
+  attr_accessor :menu, :dishes_selected, :total_price, :total_price_array, :dishes_available, :dish_price
 
   def initialize
     @menu = Hash.new
@@ -16,6 +16,7 @@ class Takeaway
   def order
     puts self.menu
     puts "To begin the selection, press enter. To cancel the action, type 'exit'."
+    @total_price_array = []
 
     while gets.chomp != 'exit' 
       puts "Please choose a dish."
@@ -26,20 +27,28 @@ class Takeaway
       quantity = gets.chomp
 
       puts "Please press enter to select another dish or type 'exit'."
+
+      dish_price_calculator(dish, quantity)
     end
 
     puts "Here is your order."
     puts dishes_selected
-    total_price_calculator(dish, quantity)
-    send_reply_message
+    total_price_calculator
   end
 
-  def total_price_calculator(dish, quantity)
+  def dish_price_calculator(dish, quantity)
     price = menu["#{dish}"]
-    @total_price = price.to_i * quantity.to_i
-    raise 'Total price not correct' if @total_price != price.to_i * quantity.to_i
-    puts total_price.to_s + '£'
+    dish_price = price.to_i * quantity.to_i 
+    total_price_array << dish_price
   end
+
+  def total_price_calculator
+    @total_price = total_price_array.inject {|result, i| result + i}
+    p total_price.to_s  + '£'
+    raise 'Total price is wrong' if total_price != total_price_array.inject {|result, i| result + i}
+  end
+
+  # 'send_reply_message' method not implemented, it should have been part of the 'order' method
 
   def send_reply_message
   account_sid = ''
