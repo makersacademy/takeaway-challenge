87592b39cbde2ec50c061afd847a8d06dbbee248
Sebastien Pires <pires.sebastien@gmail.com>
2015-02-16 12:59:12 +0000

    Update sms.rb

--- lib/sms.rb
+++ lib/sms.rb
@@ -19,7 +19,7 @@ auth_token = '13be63ad78d57375f9704d9d9adb0d81'
 @client.account.messages.create({
 	:from => '+441233800288', 
 	:to => '+447759190557', 
-	:body => 'Thanks for you order at Awesome Takeaway! Your oder will be with you by #{time}',  
+	:body => "Thanks for you order at Awesome Takeaway! Your oder will be with you by #{time}",  
 		account_sid = 'ACc36ee9ccdc509580db7161dd88669a8e'
 		auth_token = '861bfe0a7073153f9f735baf1145adf1'
 		@client = Twilio::REST::Client.new account_sid, auth_token
