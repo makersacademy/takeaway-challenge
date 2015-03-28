db00270ac693c69d7acd6b95656393672afe690e
sanjsanj <sanjsanj@hotmail.com>
2015-03-28 17:13:27 +0000

    Customer can view menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,4 +6,13 @@ class Takeaway
   def menu
     { meal_small: 5, meal_medium: 10, meal_large: 15 }
   end
+
+  def show_menu
+    to_return = ''
+    # menu.map { |key, value| to_return << key.to_s + ': £' + value.to_s }.join(', ')
+    # menu.map { |key, value| to_return << "#{key}: £#{value}" }.join(', ')
+    menu.each { |key, value| to_return << key.to_s << ': £' << value.to_s << '\n' }
+    # menu.join(': £', ', ')
+    to_return
+  end
 end
