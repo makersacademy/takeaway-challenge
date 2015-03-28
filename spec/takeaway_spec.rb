7a472fbb48333dc911b9da5c377828d448a760eb
sanjsanj <sanjsanj@hotmail.com>
2015-03-28 15:49:06 +0000

    Takeaway knows what their selections cost

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -10,4 +10,20 @@ context 'Takeaway' do
   it 'knows their menu contains 3 selections' do
     expect(takeaway.menu.count).to eq 3
   end
+
+  it 'knows meal_small costs 5' do
+    expect(takeaway.menu[:meal_small]).to eq 5
+  end
+
+  it 'knows meal_medium costs 10' do
+    expect(takeaway.menu[:meal_medium]).to eq 10
+  end
+
+  it 'knows meal_large costs 15' do
+    expect(takeaway.menu[:meal_large]).to eq 15
+  end
+
+  it 'can let a customer view the menu' do
+
+  end
 end
