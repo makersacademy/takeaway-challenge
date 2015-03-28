169f870535498fada7770163cdcee9a657d9003e
sanjsanj <sanjsanj@hotmail.com>
2015-03-28 15:14:42 +0000

    Takeaway knows their menu contains 3 selections

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,4 +2,8 @@ class Takeaway
   def customer name
     name
   end
+
+  def menu
+    { meal_small: 5, meal_medium: 10, meal_large: 15 }
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,4 +6,8 @@ context 'Takeaway' do
   it 'knows Bob is their customer' do
     expect(takeaway.customer 'Bob').to eq 'Bob'
   end
+
+  it 'knows their menu contains 3 selections' do
+    expect(takeaway.menu.count).to eq 3
+  end
 end
