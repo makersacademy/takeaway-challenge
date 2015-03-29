5f397624aea7be3b22950b0076bcc9461547b43d
Stacca <caronstace@gmail.com>
2015-03-29 13:04:36 +0100

    input issues still

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,27 @@
+require_relative 'menu'
 class Order
+  attr_reader :total
+
   def initialize
-    @input = []
+    @dish_number = input.to_i
   end
 
-   def input
+  def input input = []
     puts "please select the number of your dish and type'checkout'"
     gets.chomp
   end
 
   def valid_range? input
-    input.to_i.between?(1..4)
+    input.to_i.between?(1, 4)
+  end
+
+  def cart input, quantity = 1
+    @items << { price: price, quantity: quantity }
+  end
+
+  def total_price
+    items.inject(0) do |result, item|
+    result + item[:price].price * item[:quantity]
+    end
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,12 +1,41 @@
 require 'order'
 
 describe Order do
-    it 'gets dish input' do
-        order = Order.new
-        allow(order.input).to receive(:gets).and_return(:name)
-    end
-    it 'checks input' do
-        order = Order.new
-        expect(order.input).to eq (1..4)
-    end
+  let(:order) { described_class.new }
+
+  it 'has no items when created' do
+    expect(order.input).to eq []
+  end
+
+  it 'gets dish number as input' do
+    allow_any_instance_of(order).to receive(:input).and_return("1")
+    expect(order.dish_number).to eq(1)
+  end
+
+  it 'checks valid dish' do
+    allow_any_instance_of(order).to receive(:input).and_return("1")
+    expect(order).to be_valid_dish
+  end
+
+  it 'can add to cart' do
+    order.cart(:name)
+    expect(order.cart[:name]).to eq :cart
+  end
+
+  it 'has a quantity for a dish' do
+    order.add(dish1, 4)
+    expect(order.items.first[:quantity]).to eq 4
+  end
+
+  it 'has a total price' do
+    order.add(dish1, 1)
+    order.add(dish2, 1)
+    expect(order.total_price).to eq 25.98
+  end
+
+  it 'has a total quantity' do
+    order.add(dish1, 1)
+    order.add(dish2, 2)
+    expect(order.total_quantity).to eq 3
+  end
 end
