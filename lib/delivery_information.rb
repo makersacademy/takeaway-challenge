a4c42ec3dd3ea54d6554e01ae7484d7599f63cad
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-03-29 18:49:15 +0100

    a SMS can be sent once paid (not Twilio yet)

--- lib/delivery_information.rb
+++ lib/delivery_information.rb
@@ -3,4 +3,13 @@ class DeliveryInformation
   def initialize
     @sent_sms = false
   end
+
+  def client_pays
+    # use twilio gem to send SMS, but I'll do that later
+    if @sent_sms == true
+      fail 'SMS was already sent'
+    else
+      @sent_sms = true
+    end
+  end
 end
--- spec/delivery_information_spec.rb
+++ spec/delivery_information_spec.rb
@@ -6,7 +6,11 @@ describe DeliveryInformation do
     expect(delinfo.sent_sms).to be false
   end
   it 'sends a SMS to the client once he/she paid' do
+    delinfo.client_pays
+    expect(delinfo.sent_sms).to be true
   end
   it 'cannot send a SMS more than once' do
+    delinfo.client_pays
+    expect { delinfo.client_pays }.to raise_error 'SMS was already sent'
   end
 end
