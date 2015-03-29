4239a4faee0e53a4fad22c5df241799597e3c685
sanjsanj <sanjsanj@hotmail.com>
2015-03-29 20:19:53 +0100

    Does not text if no customer number or order

--- spec/send_text_spec.rb
+++ spec/send_text_spec.rb
@@ -1,14 +1,16 @@
 require 'send_text'
 
 context 'Text delivery confirmation' do
-  before do
-    message = MessageKlass.new
-  end
+  let(:messageklass) { MessageKlass.new }
+  let(:twilio) { double :twilio }
 
   it 'fails if there is no customer number given and order to deliver' do
-    expect { message.send_text }.to raise_error
+    expect { messageklass.send_text }.to raise_error
   end
 
   xit 'succeeds otherwise' do
+    twilio.stub_chain("account.messages.create")
+    Twilio::REST::Client.stub(:new).and_return(twilio)
+    expect(messageklass.send_text '1234', "Message").not_to raise_error
   end
 end
