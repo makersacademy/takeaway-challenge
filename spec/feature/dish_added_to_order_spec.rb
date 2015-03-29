ec2d52baefad5f64af92cd75091339c217a539f9
Jade King <dev@jadeking.co.uk>
2015-03-29 19:41:01 +0100

    update method name in test

--- spec/feature/dish_added_to_order_spec.rb
+++ spec/feature/dish_added_to_order_spec.rb
@@ -3,7 +3,7 @@ feature 'Restaurant dish can be added to order and total shown' do
   let(:restaurant) { Restaurant.new }
   let(:dish) { Dish.new("chicken curry", 3.50) }
   before(:each) do
-    restaurant.add_dish(dish)
+    restaurant.load_dishes(dish)
   end
   scenario '#add_dish to ::order' do
     restaurant.list_dishes
