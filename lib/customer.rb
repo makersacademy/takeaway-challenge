fdfb34d61412b6c688dc7988fce9912c018c99f2
Kanishk Walia <kanishk.walia@gmail.com>
2015-03-29 23:06:08 +0100

    Update

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,8 @@
-require 'send_sms'
-
 class Customer
 
-	include Send_sms
-
-	attr_reader :dish_list
+attr_reader :dish_list
 
 	def initialize(dish_list = [])
 		@dish_list = dish_list
-	ends
+	end
+end
