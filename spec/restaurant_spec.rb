053f801141dd3e740a64efca25e7773963cb32b1
Jade King <dev@jadeking.co.uk>
2015-03-29 21:35:53 +0100

    add unit test list dishes

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,7 +1,10 @@
 require 'restaurant'
 describe Restaurant do
   let(:dish) { double :dish }
-  it 'has a ::dish' do
-    expect(subject.load_dishes(dish)).to eq dish
+  it 'accepts a ::dish' do
+    expect(subject.load_dishes(dish)).to eq [dish]
+  end
+  it 'has a list of dishes' do
+    expect(subject.dishes).to eq [dish]
   end
 end
