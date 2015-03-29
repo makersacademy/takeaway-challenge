7038fea3b6ab5d5e2ee6f02d014db42d74fb86ba
Stacca <caronstace@gmail.com>
2015-03-29 14:49:21 +0100

    add item to order

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,14 +2,13 @@ require 'order'
 
 describe Order do
   let(:order) { described_class.new }
-  let(:Burger) { double :dishes, name: 'Burger', price: 10.99 }
 
   it 'has is empty when created' do
     expect(order.new_order).to be_empty
   end
 
   it 'can add a dish to order' do
-    order.add :burger 10
-    expect(order.add).to eq [burger: 10] 
+    order.add
+    allow(order).to receive(:new_order)
   end 
 end
