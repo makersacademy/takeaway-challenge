0f2dcf159b9b4736d388e8f433d2e74995e6a4d9
Jade King <dev@jadeking.co.uk>
2015-03-30 05:22:53 +0100

    add scenario add more than 1 item

--- spec/feature/dish_added_to_order_spec.rb
+++ spec/feature/dish_added_to_order_spec.rb
@@ -10,6 +10,11 @@ feature 'Restaurant dish can be added to order and total shown' do
   end
   scenario '#add_dish to ::order' do
     restaurant.dishes
-    expect(order.add_dish(dish)).to eq dish
+    expect(order.add_dish(dish)).to be_an_instance_of OrderItem
+  end
+  scenario 'add more than 1 of an item' do
+    order.add_dish(dish)
+    item = order.add_dish(dish)
+    expect(item.quantity).to eq 2
   end
 end
