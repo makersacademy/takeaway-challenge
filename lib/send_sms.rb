0e8094dcd838c43bae5b10c89cd9e9041043aac1
Max Weaver <maxlweaver@gmail.com>
2015-03-30 07:01:39 +0100

    Update send_sms.rb
    removed auth info

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -12,14 +12,14 @@ class TwilioTest
   end
 
   def auth
-    account_sid = 'AC56033ac8937f1a9274d6e94cf72a238a'
-    auth_token = '3cd94a6af915302f065d904e34d44cab'
+    account_sid = '***********************'
+    auth_token = '**********************'
   end
 
   def send_sms
     @message = @client.account.messages.create(
       from: '+441315104572',
-      to: '07804862447',
+      to: '0780*******',
       body: 'Thank you for your order. It will arrive at approx "#{time}"'
     )
   end
