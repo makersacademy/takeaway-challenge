1a74304e0f8131f923a2f0db79907857ea0bd6c3
Jade King <dev@jadeking.co.uk>
2015-03-31 03:08:15 +0100

    delete comments

--- lib/order.rb
+++ lib/order.rb
@@ -11,21 +11,3 @@ class Order
     end
   end
 end
-# require_relative 'order_item'
-# class Order
-#   attr_reader :order_list
-#   def initialize
-#     @order_list = []
-#   end
-#
-#   def add_dish(dish)
-#     dish_already_added ? update_item(dish) : add_new_item(dish)
-#     item = OrderItem.new(dish)
-#     @order_list << item
-#     item
-#   end
-#
-#   def dish_already_added?(dish)
-#     order_list.include dish
-#   end
-# end
