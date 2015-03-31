b093b119093b3b21f8648a72cc34e6bdcdb854cc
Jade King <dev@jadeking.co.uk>
2015-03-31 02:03:37 +0100

    fix test expect to match hash

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,9 +2,8 @@ require 'restaurant'
 describe Restaurant do
   it 'displays a list of dishes' do
     subject.load_dishes("./lib/dishes.txt")
-    subject.view_dishes
-    expect(restaurant.dishes).to match [
-      a_hash_including("Chicken curry" => 3.5, "Lamb curry" => 4.0)]
+    expect(subject.dishes).to include(
+      "Chicken curry" => 3.5, "Lamb curry" => 4.0)
   end
 end
 
