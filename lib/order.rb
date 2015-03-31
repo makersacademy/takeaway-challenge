e322563cb7a3d3c17d4d1b4f4740613fbc2c20c0
Jade King <dev@jadeking.co.uk>
2015-03-31 03:07:49 +0100

    add place order method

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,16 @@
+class Order
+  attr_writer :order
+  def initialize(restaurant)
+    @restaurant = restaurant
+    @order_list = {}
+  end
+
+  def place_order(hash)
+    hash.each_pair do |key, value|
+      @order_list = { key => value }
+    end
+  end
+end
 # require_relative 'order_item'
 # class Order
 #   attr_reader :order_list
