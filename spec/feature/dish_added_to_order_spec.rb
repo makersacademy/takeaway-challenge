87110cce3ce30d771a7c5d73f0a847927cb552ab
Jade King <dev@jadeking.co.uk>
2015-03-31 02:04:31 +0100

    fix expect hash match

--- spec/feature/dish_added_to_order_spec.rb
+++ spec/feature/dish_added_to_order_spec.rb
@@ -4,26 +4,7 @@ feature 'User can view dishes' do
   scenario 'view dishes' do
     restaurant = Restaurant.new
     restaurant.load_dishes("./lib/dishes.txt")
-    expect(restaurant.dishes).to match [
-      a_hash_including("Chicken curry" => 3.5, "Lamb curry" => 4.0)]
+    expect(restaurant.dishes).to include(
+      "Chicken curry" => 3.5, "Lamb curry" => 4.0)
   end
 end
-# require 'dish'
-# # require 'restaurant'
-# feature 'Restaurant dish can be added to order and total shown' do
-#   let(:restaurant) { Restaurant.new }
-#   let(:dish) { Dish.new("Chicken curry") }
-#   let(:order) { Order.new }
-#   before(:each) do
-#     restaurant.load_dishes(dish)
-#   end
-#   scenario '#add_dish to ::order' do
-#     restaurant.dishes
-#     expect(order.add_dish(dish)).to be_an_instance_of OrderItem
-#   end
-#   scenario 'add more than 1 of an item' do
-#     order.add_dish(dish)
-#     item = order.add_dish(dish)
-#     expect(item.quantity).to eq 2
-#   end
-# end
