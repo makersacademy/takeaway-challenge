3608345b5fda164513420504b949e6ed859f3b1a
Jade King <dev@jadeking.co.uk>
2015-03-31 23:19:43 +0100

    fox unit test f

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,9 +1,18 @@
 require 'order'
+require 'restaurant'
 describe Order do
   it 'can accept a list of dishes' do
-    restaurant = double { :restaurant }
+    restaurant = Restaurant.new
+    restaurant.load_dishes("./lib/dishes.txt")
     order = described_class.new(restaurant)
-    dishes = {  "Chicken curry" => "1" }
-    expect(order.place_order(dishes)).to include("Chicken curry" => "1")
+    dishes = {  "Chicken curry" => "1", "Lamb curry" => "1"  }
+    expect(order.place_order(dishes, "7.50")).to eq 7.5
+  end
+  it 'raises an error if totals dont match' do
+    restaurant = Restaurant.new
+    restaurant.load_dishes("./lib/dishes.txt")
+    order = described_class.new(restaurant)
+    dishes = {  "Chicken curry" => "1", "Lamb curry" => "1" }
+    expect { order.place_order(dishes, "8.50") }.to raise_error "Wrong total"
   end
 end
