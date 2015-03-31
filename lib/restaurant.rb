77d187b0df08234652e86cbf6822ac441a5ab5d0
Jade King <dev@jadeking.co.uk>
2015-03-31 02:04:02 +0100

    add load dishes method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,10 +1,15 @@
-# class Restaurant
-#   attr_reader :dishes
-#   def initialize
-#     @dishes = []
-#   end
-#
-#   def load_dishes(dish)
-#     @dishes << dish
-#   end
-# end
+class Restaurant
+  attr_reader :dishes
+  def initialize
+    @dishes = {}
+  end
+
+  def load_dishes(filepath)
+    File.open(filepath) do |file|
+      file.each do |line|
+        key, value = line.chomp.split(",")
+        @dishes[key] = value.to_f
+      end
+    end
+  end
+end
