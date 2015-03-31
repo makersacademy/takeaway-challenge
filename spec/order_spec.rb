279b4935f6edda477e6ba38e37add28ce644ee8f
Jade King <dev@jadeking.co.uk>
2015-03-31 02:48:25 +0100

    fix test using described class

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,8 +1,9 @@
 require 'order'
 describe Order do
   it 'can accept a list of dishes' do
+    restaurant = double { :restaurant }
+    order = described_class.new(restaurant)
     dishes = {  "Chicken curry" => "1" }
-    expect(subject.place_order(dishes)).to include(
-      "Chicken curry" => "1")
+    expect(order.place_order(dishes)).to include("Chicken curry" => "1")
   end
 end
