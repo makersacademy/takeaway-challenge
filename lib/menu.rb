49694d7823d80ba5e3e1cdd46e1f39498a854572
Stacca <caronstace@gmail.com>
2015-04-24 16:39:27 +0100

    a bit of refactoring

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,19 +1,15 @@
 class Menu
+  attr_reader :dishes
   def initialize
-    @dishes = []
+    @dishes = [
+        { name: 'Burger', price: 10.95  },
+        { name: 'Pizza', price: 14.00 },
+        { name: 'Salad', price: 7.60 },
+        { name: 'fries', price: 2.90 }
+     ]
   end
 
-  def dishes
-    @dishes = 
-    [
-      { name: 'Burger', price: 10.95  },
-      { name: 'Pizza', price: 14.00 },
-      { name: 'Salad', price: 7.60 },
-      { name: 'fries', price: 2.90 }
-    ]
-  end
-
-   def add_dish(name, price)
-   @dishes << {name: name, price: price}
-  end
+   def add_dish(dish)
+     @dishes << dish
+   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,32 +1,30 @@
 require 'menu'
 
 describe Menu do
-  let(:menu) { Menu.new }
-
-  it 'has a menu' do
-    expect(subject.respond_to?(:dishes)).to be true
-  end
-
-   it 'displays dishes and prices' do
-    expect(subject.dishes).to eq [
+  let :dishes do
+    [
       { name: 'Burger', price: 10.95 },
       { name: 'Pizza', price: 14.00 },
       { name: 'Salad', price: 7.60 },
       { name: 'fries', price: 2.90 }
-      ]
+    ]
   end
 
-  it 'can add dishes to it' do
-    menu.add_dish("Icecream", 4.80)
+  subject { Menu.new }
+  it { is_expected.to respond_to(:dishes) }
+
+  describe '#dishes' do
+    it 'returns the list of dishes' do
+      expect(subject.dishes).to eq dishes
+    end
   end
 
-  # it 'can add dishes to it' do
-  #   menu.add_dish("Icecream", 4.80)
-  #   expect(subject.dishes).to eq 
-  #     { name: 'Burger', price: 10.95 },
-  #     { name: 'Pizza', price: 14.00 },
-  #     { name: 'Salad', price: 7.60 },
-  #     { name: 'fries', price: 2.90 },
-  #     { name: 'Icecream', price: 4.80 }
-  # end
+  describe "#add_dish" do
+    # it 'can add dishes to it' do
+    it "adds the given dish to the list of dishes" do
+      new_dish = {name: 'salad', price: 4.0 }
+      expect { subject.add_dish(new_dish) }.to change(subject.dishes, :count).by(1)
+      expect(subject.dishes).to include new_dish
+    end
+  end
 end
