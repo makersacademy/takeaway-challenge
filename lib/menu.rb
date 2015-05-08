6d70e03234177a0aaec39043837d18e64136cf2f
Makers Laptop 66 <chidumaga@gmail.com>
2015-05-08 18:39:13 +0100

    created a customer.rb and a menu.rb file

---
+++ lib/menu.rb
@@ -0,0 +1,14 @@
+class Menu
+
+  attr_reader :display_menu
+
+  def initialize
+    @display_menu = [["Pounded Yam" , 15],
+      ["Jollof Rice" , 10],
+      ["Fried Rice" , 12],
+      ["Suya" , 6],
+      ["Moi Moi" , 5],
+    ]
+  end
+
+end
