39aeaff5ff6884f1458abc6f5fc4221a65a480cc
Makers Laptop 66 <chidumaga@gmail.com>
2015-05-08 18:40:17 +0100

    Unit tests for the menu class

---
+++ spec/menu_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/menu'
+
+describe Menu do
+
+  it 'should display a menu to the customer' do
+    expect(subject.display_menu).to eq([["Pounded Yam" , 15],
+      ["Jollof Rice" , 10],
+      ["Fried Rice" , 12],
+      ["Suya" , 6],
+      ["Moi Moi" , 5],
+    ])
+  end
+
+end
