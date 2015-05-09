2a284193a0fab001dd50b44d8758b1a47cb82a4b
Makers Laptop 66 <chidumaga@gmail.com>
2015-05-09 16:12:34 +0100

    Updated tests to reflect updated code

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,10 +4,10 @@ describe Menu do
 
   it 'should display a menu to the customer' do
     expect(subject.display_menu).to eq([["Pounded Yam" , 15],
-      ["Jollof Rice" , 10],
-      ["Fried Rice" , 12],
-      ["Suya" , 6],
-      ["Moi Moi" , 5],
+      ["Jollof Rice", 10],
+      ["Fried Rice", 12],
+      ["Suya", 6],
+      ["Moi Moi", 5]
     ])
   end
 
