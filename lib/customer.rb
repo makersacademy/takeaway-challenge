053bf9b14d365ecfbe9df22e88aaa76c7962d1e4
Makers Laptop 66 <chidumaga@gmail.com>
2015-05-09 16:12:04 +0100

    Added text verification to customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -8,12 +8,13 @@ class Customer
     @order_total = 0
   end
 
-  def select_dish menu , dish_number
+  def select_dish menu, dish_number
     @order_total += menu.display_menu[dish_number - 1][1]
     menu.display_menu[dish_number - 1]
   end
 
-  def text
+  def confirm_order text_client
+    text_client.send_message
   end
 
 end
---
+++ lib/twilio_response.rb
@@ -0,0 +1,23 @@
+require 'twilio-ruby'
+
+class TwilioResponse
+
+  def initialize
+    account_sid = 'ACf16002429104e481859dc4625e572723'
+    auth_token = 'd8c8dfaf3fedfe2695563568c8344428'
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  def send_message
+    time = Time.new
+
+    @client.account.messages.create({
+    :from => '+442033223858',
+    :to => '07459747193',
+    :body => 'Thank you. Your order has been placed and will be delivered before #{(time.hour + 1).to_s}: #{time.min}',
+    })
+  end
+
+end
