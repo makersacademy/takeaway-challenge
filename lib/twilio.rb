a6fa284ebf61d9509f48064ecd279ab9f74302e3
Rodney Cullen <rodcul@gmail.com>
2015-05-10 21:47:56 +0100

    Removed credentials from repo (and expired token on twilio)

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -3,15 +3,18 @@ require 'twilio-ruby'
 class TwilioSMS
   def send(message)
     # put your own credentials here
-    account_sid = 'AC84c52b9039676c287afc619813e63e52'
-    auth_token = 'aa482aeb0ffec2853fedc20025fb6c9b'
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    tel_number_to = ENV['TWILIO_TEL_NUMBER_TO']
+    tel_number_from = ENV['TWILIO_TEL_NUMBER_FROM']
+
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     @client.messages.create(
-      from: '+44 1603914052',
-      to: '+44 7481 595622',
+      from: tel_number_from,
+      to: tel_number_to,
       body: message
     )
   end
