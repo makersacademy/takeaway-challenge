e396baf8bf0f39a228828a1c2013382086ab3af0
Daniel Johnston <macademy@danieljohnston.co.uk>
2015-05-10 16:03:52 +0100

    created order code, still making it work

--- lib/order.rb
+++ lib/order.rb
@@ -1,9 +1,23 @@
 class Order
 
   def initialize(customer, takeaway)
-
+    @customer = customer
+    @takeaway = takeaway
+    @order = Array.new
   end
 
-  
+  def add_order_line(item, quantity)
+    # Ruh roh. An array of arrays?! Check that adding items works properly.
+    @order << [item, quantity, @takeaway.item_price(item)]
+  end
+
+  def order_line(index)
+    @order[index]
+  end
+
+  def total
+    #Another messy array reference here
+    @order.inject(0) {|accum, item| accum + item[2]}
+  end
 
 end
