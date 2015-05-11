5e147d43978b3dcc9f893df6c7f8e59503a4489f
Ashleigh <ashleighmaund@gmail.com>
2015-05-11 01:11:22 +0100

    amended print_menu method in menu.rb

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,13 +1,16 @@
 class Menu
-  attr_accessor :list
+  attr_accessor :menu_list
 
   def initialize
-  	@list = {"Bottle of Water" => 3.08, "Disney Themed Napkins (x5)" => 50, "Carbonara" => 0.4, "Ice cream" => 0.48, "Happiness" => 0}
+  	@menu_list = {"Bottle of Water" => 3.08, "Disney Themed Napkins (x5)" => 50, "Carbonara" => 0.4, "Ice cream" => 0.48, "Happiness" => 0}
   end
 
   def print_menu
-  	list
+  	menu_list.each do |dish_key, dish_price|
+      "#{dish_key}: £#{dish_price}"
+  	end
   end
 end
 
 
+#perhaps would need to create module? Check that this is okay...
--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,11 @@
 require 'menu'
 
 class Order
-  attr_accessor :order_list
+  attr_accessor :order_list, :menu_list
 
   def initialize
   	@order_list = []
+  	@menu_list = Menu.new
   end
   def add_dish(quantity, dish_key)
   	quantity.times do
@@ -17,9 +18,21 @@ class Order
   end
 
   def order_price
+  	# OKAY - what do we want?
+  	# 	To make a ORDER_HASH with key names and values taken
+  	# 	from hash provided in menu class maybe...?
+  	#   only way I can think of that will let me get the total price.
+  	#   Want to go back and change print_menu method now
   	order_hash = {}
-    order_list.each do |dish_key,i|
-      order_hash << dish_key if list.include(dish_key)
+
+
+
+
+
+    order_list.map(&:v).inject do |sum, x|
+      sum + x
     end
   end
 end
+
+#use inject method to add up all dish prices?
