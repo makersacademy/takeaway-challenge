377ad7cb3f5c82b141cdd1e7a2d7a182d345b31a
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-19 12:29:35 +0100

    test for show_menu ability

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,4 +6,11 @@ describe Takeaway do
   
   it { is_expected.to respond_to :show_menu }
 
+  describe 'show_menu' do
+    it 'displays a list of dishes and prices' do
+      dish = double :dish , name: 'Pizza', price: 8
+      expect(takeaway.show_menu).to eq ("pizza: 8")
+    end
+  end
+
 end
