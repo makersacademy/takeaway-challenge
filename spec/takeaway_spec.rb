8fe6100337b92cf07017844ebb9e185bd2ef69ba
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-19 12:00:03 +0100

    test for populating the menu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,8 +4,8 @@ describe Takeaway do
 
   it { is_expected.to respond_to :menu }
 
-  # it 'records a list of dishes and prices' do
-  #   expect(subject.show_menu).to eq
-  # end
+  it 'has a way of populating the menu' do
+    expect(subject).to respond_to(:create_menu).with(2).arguments
+  end
   
 end
