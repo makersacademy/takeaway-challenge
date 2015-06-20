7d9edef27a78bd22ac3318c6daaf0571cc09ead4
Michael Arnott <arnottmj@aol.com>
2015-06-20 16:47:41 +0100

    added remove functionality for order

--- lib/order.rb
+++ lib/order.rb
@@ -20,18 +20,56 @@ class Order
 
   end
 
-  def select pizza
-    @selection[pizza] = @menu[pizza]
-    @total += @menu[pizza]
+  def select pizza, number
+
+    if pizza_already_selected? pizza
+      temp_number = number + @selection[pizza][0]
+      @selection.delete(pizza)
+      temp_total = @menu[pizza]*temp_number
+      @selection[pizza] = [temp_number, temp_total]
+      @total += @menu[pizza]*number
+    else
+      temp_total = @menu[pizza]*number
+      @selection[pizza] = [number, temp_total]
+      @total += temp_total
+
+    end
 
   end
 
-  def remove pizza
+  def remove pizza, number
+    temp_number = @selection[pizza][0] - number
     @selection.delete(pizza)
-    @total -= @menu[pizza]
+    temp_total = @menu[pizza]*temp_number
+    @selection[pizza] = [temp_number, temp_total]
+    @total -= @menu[pizza]*number
+
+  end
+
+  private
+
+  def pizza_already_selected? pizza
+    @selection.keys.include?(pizza)
 
   end
 
 end
+
+# menu = Menu.new
+# p menu.view
+# order = Order.new(menu)
+# order.select(:pepperoni,5)
+# p order.view_selection
+# p order.view_total
+# order.select(:margharita,7)
+# p order.view_selection
+# p order.view_total
+# order.select(:pepperoni,3)
+# p order.view_selection
+# p order.view_total
+# order.remove(:margharita, 3)
+# p order.view_selection
+# p order.view_total
+
 
 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -11,7 +11,7 @@ describe Order do
 
   subject {Order.new(menu)}
 
-  it {expect(subject).to respond_to(:select).with(1).argument}
+  it {expect(subject).to respond_to(:select).with(2).argument}
 
   it 'should let customers see their selection' do
     expect(subject.view_selection).to eq ({})
@@ -22,30 +22,48 @@ describe Order do
   end
 
   context 'select should' do
-    it 'add a pizza to the selection' do
-      subject.select(:pepperoni)
-      expect(subject.view_selection).to eq ({pepperoni: 10})
+    it 'add a single pizza to the selection' do
+      subject.select(:pepperoni, 1)
+      expect(subject.view_selection).to eq ({pepperoni: [1, 10 ]})
     end
 
-    it 'update the order total' do
-      subject.select(:pepperoni)
+    it 'update the order total when adding single pizza' do
+      subject.select(:pepperoni, 1)
       expect(subject.view_total).to eq 10
     end
+
+    it 'add more than one of a pizza to the selection' do
+      subject.select(:pepperoni, 2)
+      expect(subject.view_selection).to eq ({pepperoni: [2, 20]})
+    end
+
+    it 'update the order total when adding more than one of a pizza' do
+      subject.select(:pepperoni, 2)
+      expect(subject.view_total).to eq 20
+    end
+
+    it 'only allow 1 type of pizza to be listed in the selection and update its number' do
+      subject.select(:pepperoni, 2)
+      subject.select(:pepperoni, 2)
+      expect(subject.view_selection).to eq ({pepperoni: [4, 40]})
+    end
+
+
 
   end
 
   context 'remove should' do
 
     it 'let customers remove an item and update selection' do
-      subject.select(:pepperoni)
-      subject.remove(:pepperoni)
-      expect(subject.view_selection).to eq ({})
+      subject.select(:pepperoni, 5)
+      subject.remove(:pepperoni,2)
+      expect(subject.view_selection).to eq ({pepperoni: [3, 30]})
     end
 
     it 'let customers remove an item and update total' do
-      subject.select(:pepperoni)
-      subject.remove(:pepperoni)
-      expect(subject.view_total).to eq 0
+      subject.select(:pepperoni, 5)
+      subject.remove(:pepperoni,2)
+      expect(subject.view_total).to eq 30
     end
 
   end
