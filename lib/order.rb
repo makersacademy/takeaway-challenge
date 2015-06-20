413fb1c53ca2524f43aa1ff433e56880931bb2ef
Michael Arnott <arnottmj@aol.com>
2015-06-20 17:19:08 +0100

    refactored code

--- lib/order.rb
+++ lib/order.rb
@@ -23,27 +23,20 @@ class Order
   def select pizza, number
 
     if pizza_already_selected? pizza
-      temp_number = number + @selection[pizza][0]
-      @selection.delete(pizza)
-      temp_total = @menu[pizza]*temp_number
-      @selection[pizza] = [temp_number, temp_total]
-      @total += @menu[pizza]*number
+      @selection[pizza] = updated_values_for_select(pizza, number)
+      @total = calc_total
     else
       temp_total = @menu[pizza]*number
       @selection[pizza] = [number, temp_total]
-      @total += temp_total
+      @total = calc_total
 
     end
 
   end
 
   def remove pizza, number
-    temp_number = @selection[pizza][0] - number
-    @selection.delete(pizza)
-    temp_total = @menu[pizza]*temp_number
-    @selection[pizza] = [temp_number, temp_total]
-    @total -= @menu[pizza]*number
-
+    select(pizza, -1 * number)
+    @total = calc_total
   end
 
   private
@@ -53,23 +46,36 @@ class Order
 
   end
 
+  def updated_values_for_select pizza, number
+    temp_number = number + @selection[pizza][0]
+    temp_total = @menu[pizza]*temp_number
+    [temp_number, temp_total]
+
+  end
+
+  def calc_total
+    @selection.inject(0) {|total,(pizza,values)| total + values[1]}
+
+  end
+
+
 end
 
-# menu = Menu.new
-# p menu.view
-# order = Order.new(menu)
-# order.select(:pepperoni,5)
-# p order.view_selection
-# p order.view_total
-# order.select(:margharita,7)
-# p order.view_selection
-# p order.view_total
-# order.select(:pepperoni,3)
-# p order.view_selection
-# p order.view_total
-# order.remove(:margharita, 3)
-# p order.view_selection
-# p order.view_total
+menu = Menu.new
+p menu.view
+order = Order.new(menu)
+order.select(:pepperoni,5)
+p order.view_selection
+p order.view_total
+order.select(:margharita,7)
+p order.view_selection
+p order.view_total
+order.select(:pepperoni,3)
+p order.view_selection
+p order.view_total
+order.remove(:margharita, 3)
+p order.view_selection
+p order.view_total
 
 
 
