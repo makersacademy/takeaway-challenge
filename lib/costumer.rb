de5351dc05e0f150b5ca5f6fccabd4f537b2daca
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-20 15:39:08 +0100

    added a select method

--- lib/costumer.rb
+++ lib/costumer.rb
@@ -2,8 +2,17 @@ class Costumer
 
   attr_reader :basket
 
-  def initialize
+  def initialize (menu)
     @basket = {}
+    @menu = menu
   end
-  
+
+  def add(item)
+    basket[item] = menu.show[item]
+    basket
+  end
+
+  private
+  attr_reader :menu
+
 end
