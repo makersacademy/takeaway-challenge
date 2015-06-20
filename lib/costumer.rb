129e800cd86654b8e04453ef93c98678fdc07d1c
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-20 16:07:05 +0100

    new method that returns the total of the order

--- lib/costumer.rb
+++ lib/costumer.rb
@@ -5,11 +5,17 @@ class Costumer
   def initialize (menu)
     @basket = {}
     @menu = menu
+    @sum = 0
   end
 
   def add(item, count = 1)
     basket[item] = count * menu.show[item]
     basket
+  end
+
+  def total
+    basket.each_value{|price| @sum += price}
+    @sum
   end
 
   private
