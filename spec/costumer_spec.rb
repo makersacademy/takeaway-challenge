bc59249ec37a12a62576942551fb7a98016cebf6
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-20 15:38:27 +0100

    added a new test for selecting

--- spec/costumer_spec.rb
+++ spec/costumer_spec.rb
@@ -1,6 +1,19 @@
 require 'costumer'
 describe Costumer do
+  let(:menu) {double :menu, show: { cheese_burger: 5,
+                                    hotdog:        4,
+                                    lasagne:       5,
+                                    beer:          2,
+                                    coke:          2,
+                                    cheese_cake:   3}}
+  subject {Costumer.new(menu)}
   it 'has a basket' do
     expect(subject.basket).to eq({})
+  end
+
+  describe 'select' do
+    it 'can select an item from a menu' do
+      expect(subject.add(:cheese_burger)).to eq({cheese_burger: 5})
+    end
   end
 end
