ec10673db8adaf7410cff2687ac8928c5ee6fad1
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-21 17:27:38 +0100

    always refactoring test

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,37 +3,32 @@ require 'customer'
 describe Customer do
 
   let(:shop) { shop = Shop.new }
-  # let(:customer) { Customer.new(shop) }
+  let(:customer) { Customer.new(shop) }
 
 
   it 'can see a list of dishes with prices' do
-    customer = Customer.new shop
     expect(customer.ask_for_menu shop).to eq (shop.show_menu)
   end
 
   it 'can be able to select some number of several available dishes' do
-    customer = Customer.new shop
     customer.ask_for_menu shop
     customer.select_dish('linguine')
     expect(customer.basket).to eq [12]
   end
 
   it 'can ask for the total to pay' do
-    customer = Customer.new shop
     customer.select_dish('linguine')
     customer.select_dish('beer')
     expect(customer.ask_total).to be 16.5
   end
 
   it 'can pay for the exact amount' do
-    customer = Customer.new shop
     customer.select_dish('linguine')
     customer.select_dish('beer')
     expect(customer.pay(16.5)).to eq("Thanks for pay")
   end
 
   it 'get an error if tries to pay a amount different from the total' do
-    customer = Customer.new shop
     customer.select_dish('linguine')
     customer.select_dish('beer')
     expect{customer.pay(3)}.to raise_error 'Sorry, you need to pay the exact total'
