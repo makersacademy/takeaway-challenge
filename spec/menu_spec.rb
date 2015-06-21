2d29aa50d390eab560bc0cbab4613f013db3ea9f
TheoLeanse <theoleanse@gmail.com>
2015-06-21 13:08:39 +0100

    tests for #price_of method

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,7 +5,20 @@ describe Menu do
   subject(:menu) { Menu.new }
 
   it "displays the menu" do
-    expect(menu.show).to eq "1 - Burger - £9.99" # do not use subject?
+    menu.add :burger, 9.99
+    expect(menu.show).to eq "Burger - £9.99" # do not use subject?
+  end
+
+  it "gives the price of an item" do
+    menu.add :burger, 9.99
+    expect(menu.price_of :burger).to eq 9.99
+  end
+
+  it "can have dishes added to it" do
+    menu.add :burger, 9.99
+    menu.add :chips, 1.99
+    menu.add :coke, 0.99
+    expect(menu.show).to eq "Burger - £9.99 // Chips - £1.99 // Coke - £0.99"
   end
 
 end
