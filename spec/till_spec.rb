80f4b11571085f17e83c8740497c2a1cdf68c755
TheoLeanse <theoleanse@gmail.com>
2015-06-21 16:26:02 +0100

    tests message-composition responsibility of Till class

--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -2,19 +2,20 @@ require 'till'
 
 describe Till do
 
-  subject(:till) { Till.new double :order, items: { :burger => 9.99 } }
+  let(:order   ) { double :order, items: { :burger => 9.99 } }
+  let(:notifier) { double :notifier, notify: nil }
+  subject(:till) { Till.new order, notifier }
 
   it "calculates a total" do
     expect(till.total).to eq 9.99
   end
 
-  xit "generates a receipt" do
-    expect(receipt).to recieve(:print)
-    till.print_receipt
+  it "generates a message" do
+    expect(till.generate_message).to eq "Thank you for your order!"
   end
 
-  xit "sends a text" do
-    expect(notifier).to receive(:text)
+  it "sends the message to a messenger" do
+    expect(notifier).to receive(:send_message).with("Thank you for your order!")
     till.notify
   end
 end
