1a05f7a58cd6a3ff2166e5d2311225cbf852414c
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-21 23:40:13 +0100

    Refactoring such that orering throws an error if expected total is not the exact total

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -8,12 +8,13 @@ class Takeaway
   end
 
   def order dishes, quantities, total
-    @preparing = dishes.zip(quantities)
-
+    might_prepare = dishes.zip(quantities)
+    raise "Not totalling to expected" unless (bill_comes_to might_prepare) == total
+    @preparing = might_prepare
   end
 
-  def bill_comes_to
-    bill = preparing.map { |dish,quantity| quantity * @menu.content[dish]}
+  def bill_comes_to order
+    bill = order.map { |dish,quantity| quantity * @menu.content[dish]}
     total = bill.inject { |sum,n| sum + n }
     total
   end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -12,11 +12,10 @@ describe Takeaway do
   end
 
   it 'gives a bill' do
-    subject.order ["Steak and chips", "Fish and chips"],[2, 1], 27
-    expect(subject.bill_comes_to).to eq 27
+    expect(subject.bill_comes_to [["Steak and chips", 2], ["Fish and chips", 1]]).to eq 27
   end
 
-  xit 'will raise an error if the expected total does not equal the actual total' do
+  it 'will raise an error if the expected total does not equal the actual total' do
     expect { subject.order ["Steak and chips", "Fish and chips"],[2, 1], 26 }.to raise_error "Not totalling to expected"
 
   end
