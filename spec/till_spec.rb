bbe053e4a93c7c92db0fd25dcf0ef21df8489735
TheoLeanse <theoleanse@gmail.com>
2015-06-21 13:09:38 +0100

    tests for output to Messenger class

--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -2,13 +2,19 @@ require 'till'
 
 describe Till do
 
-  subject(:till) { Till.new double :order, items: { burger: [1, 9.99]} }
+  subject(:till) { Till.new double :order, items: { :burger => 9.99 } }
 
   it "calculates a total" do
     expect(till.total).to eq 9.99
   end
 
-  xit "shows a reciept" do
+  xit "generates a receipt" do
+    expect(receipt).to recieve(:print)
+    till.print_receipt
+  end
 
+  xit "sends a text" do
+    expect(notifier).to receive(:text)
+    till.notify
   end
 end
