bb23370f7d07545b38f3586c96ebaadd4591556c
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-21 21:08:10 +0100

    nb text is still not sending. need to fix

--- lib/texter.rb
+++ lib/texter.rb
@@ -1,9 +1,26 @@
 require_relative 'takeaway'
 
+require 'twilio-ruby'
+
 class Texter
 
+
+
+  def initialize
+    @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
+  end
+  
   def send
     'message sent'
+    @client.account.messages.create({
+    :from => '441227641157', 
+    :to =>   '+447428121604',
+    :body => 'test message'
+    })
   end
 
 end
+
+
+
+
