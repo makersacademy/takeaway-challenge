c1c89613a5c50519986405cf1eadc19291b3680e
TheoLeanse <theoleanse@gmail.com>
2015-06-21 13:07:47 +0100

    adds items to the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,24 @@
 class Menu
-  MENU = "1 - Burger - £9.99"
+  def initialize
+    @menu = {}
+  end
 
-  def show
-    MENU
+  def add dish, price
+    @menu[dish] = price
+  end
+
+  def show # abstract this to a showable module? takes an item / price hash and lists it
+    list = ""
+    counter = @menu.size
+    @menu.size.times do |dish_no|
+      list << "#{@menu.keys[dish_no].capitalize} - £#{@menu[@menu.keys[dish_no]]}" # new line?
+      counter -= 1
+      list << " // " unless counter == 0
+    end
+    list
+  end
+
+  def price_of dish
+    @menu[dish]
   end
 end
