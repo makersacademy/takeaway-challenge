8cdf7905c0316438c05e7356f98abbe4bae1a548
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-21 17:33:40 +0100

    finished refactoring customer spec file

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,35 +5,30 @@ describe Customer do
   let(:shop) { shop = Shop.new }
   let(:customer) { Customer.new(shop) }
 
+  before(:each) do
+    customer.select_dish('linguine')
+    customer.select_dish('beer')
+  end
+
 
   it 'can see a list of dishes with prices' do
     expect(customer.ask_for_menu shop).to eq (shop.show_menu)
   end
 
   it 'can be able to select some number of several available dishes' do
-    customer.ask_for_menu shop
-    customer.select_dish('linguine')
-    expect(customer.basket).to eq [12]
+    expect(customer.basket).to eq [12, 4.5]
   end
 
   it 'can ask for the total to pay' do
-    customer.select_dish('linguine')
-    customer.select_dish('beer')
     expect(customer.ask_total).to be 16.5
   end
 
   it 'can pay for the exact amount' do
-    customer.select_dish('linguine')
-    customer.select_dish('beer')
     expect(customer.pay(16.5)).to eq("Thanks for pay")
   end
 
   it 'get an error if tries to pay a amount different from the total' do
-    customer.select_dish('linguine')
-    customer.select_dish('beer')
     expect{customer.pay(3)}.to raise_error 'Sorry, you need to pay the exact total'
   end
-
-
 
 end
