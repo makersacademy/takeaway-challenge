f369778e43f7075f23e5c6bc8cd30bbca7c33030
Salman <salman.karim@hotmail.com>
2015-06-21 23:09:40 +0100

    pending twilio test

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,13 @@
 class Customer
 
-  attr_accessor :order
+  attr_accessor :order, :total_price, :total_quantity
+  attr_reader :summary
 
   def initialize
     @order = {}
+    @total_price = 0
+    @total_quantity = 0
+    # @summary = [["Item","Quantity","Price"]]
   end
 
   def check_menu(menu)
@@ -12,6 +16,31 @@ class Customer
 
   def add_to_order(menu,food,quantity)
     self.order.has_key?(food.to_sym) ? self.order[food.to_sym][0] += quantity : order[food.to_sym] = [quantity, menu.items[food.to_sym]]
+    self.calculate_total_price
+    self.calculate_total_quantity
+  end
+
+  def calculate_total_price
+    self.total_price = 0
+    order.each {|k,v| self.total_price += v[0]*v[1] }
+  end
+
+  def calculate_total_quantity
+    self.total_quantity = 0
+    order.each {|k,v| self.total_quantity += v[0] }
+  end
+
+  def view_order
+    puts "Item".ljust(20) + "Quantity".center(20) + "Price".rjust(20)
+    order.each do |k,v|
+      puts "#{k}".ljust(20) + "#{v[0]}".center(20) + "#{v[0]*v[1]}GBP".rjust(20)
+    end
+    puts "Total".ljust(20) + "#{total_quantity}".center(20) + "#{total_price}GBP".rjust(20)
+  end
+
+  def place_order
+    fail "Total price miscalculation" if self.total_price != order.values.inject(0) {|result,element| result + element[1]}
+    load '/Users/khalidkarim/desktop/projects/takeaway/lib/send-sms.rb'
   end
 
 
--- lib/send-sms.rb
+++ lib/send-sms.rb
@@ -1,13 +1,23 @@
 require 'rubygems'
 require 'twilio-ruby'
 
-# Get your Account Sid and Auth Token from twilio.com/user/account
-account_sid = 'ACd15ab842ef09376b39f5347ddc7c6bf1'
-auth_token = 'bd07746d563aa96ff4a2cf25d4c5bd6f'
-@client = Twilio::REST::Client.new account_sid, auth_token
+class Textsender
 
-message = @client.account.messages.create(
-    :body => "Thank you, your order was placed and will be delivered before #{Time.now+3600}",
-    :to => "+447947634555",     # Replace with your phone number
-    :from => "+441252236319")   # Replace wi'h your Twilio number
-puts message.sid
+  def initialize
+    account_sid = 'ACd15ab842ef09376b39f5347ddc7c6bf1'
+    auth_token = 'bd07746d563aa96ff4a2cf25d4c5bd6f'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  def send_message
+    message = @client.account.messages.create(
+      :body => "Thank you, your order was placed and will be delivered before #{Time.now+3600}",
+      :to => "+447947634555",     # Replace with your phone number
+      :from => "+441252236319")   # Replace wi'h your Twilio number
+    puts message.sid
+  end
+
+end
+
+# t = Textsender.new
+# t.send_message
