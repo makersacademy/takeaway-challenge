9a035bfcdc8ad9ca7c9cd2f7e825e65e60bade06
TheoLeanse <theoleanse@gmail.com>
2015-06-21 23:21:00 +0100

    Till class with messaging responsibility

--- lib/till.rb
+++ lib/till.rb
@@ -1,23 +1,32 @@
 class Till # could call it something like Summer, Calculator...?
+  DEFAULT_MESSAGE="Thank you for your order! Your food will arrive by"
+  attr_accessor :message
+  attr_reader :items
   def initialize itemizable, notifier
     @items = itemizable.items
     @notifier = notifier
+    @message = DEFAULT_MESSAGE
   end
 
   def total
     @items.values.inject(:+)
   end
 
-  def generate_message
-    "Thank you for your order!"
+  def process message_centre
+    message_centre.process self.total
+  end
+
+  def time_in_one_hour # to messenger?
+    Time.now + 60*60
+  end
+
+  def generate_message # to messenger?
+    "#{message} #{time_in_one_hour}."
   end
 
   def notify
     @notifier.send_message generate_message # assumes send_message interface on notifier
   end
 
-  def items
-    @items
-  end
 
 end
