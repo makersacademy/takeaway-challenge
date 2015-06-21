b82bc7607897c3bfe2e71b1c15a6a48452efe444
TheoLeanse <theoleanse@gmail.com>
2015-06-21 16:28:30 +0100

    implements message composition in Till class

--- lib/till.rb
+++ lib/till.rb
@@ -1,9 +1,23 @@
 class Till # could call it something like Summer, Calculator...?
-  def initialize itemizable
+  def initialize itemizable, notifier
     @items = itemizable.items
+    @notifier = notifier
   end
 
   def total
     @items.values.inject(:+)
   end
+
+  def generate_message
+    "Thank you for your order!"
+  end
+
+  def notify
+    @notifier.send_message generate_message # assumes send_message interface on notifier
+  end
+
+  def items
+    @items
+  end
+
 end
