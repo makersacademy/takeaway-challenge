8972f25cba8690962a41c601abef798340ac7be8
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-22 20:04:43 +0100

    Creating TextOrder class using Twilio API and testing it can send messages

--- lib/text_order.rb
+++ lib/text_order.rb
@@ -2,4 +2,25 @@ require 'twilio-ruby'
 
 class TextOrder
 
+  attr_reader :client, :message, :sender, :receiver
+
+  def initialize(message:, from:, to:, via:)
+    @message  = message
+    @sender   = from
+    @receiver = to
+    @client   = via
+  end
+
+  def self.call(message:, from:, to:, via:)
+    new(message: message, from: from, to: to, via: via).send_message
+  end
+
+  def send_message
+    client.messages.create(
+    from: sender.number,
+    to: receiver.number,
+    body: "#{message}, #{receiver.name}"
+    )
+  end
+
 end
