b4523f0b8f4330b70363a22417e0d21171228abb
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-01 16:16:42 +0100

    Can add multiple items in order, delete items from order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -16,12 +16,19 @@ class TakeAway
     menu
   end
 
-  def place_order(dish, quantity)
-    order["#{dish} x #{quantity}"] = total_cost_per_item(menu[dish], quantity)
+  def place_order(items={})
+    items.each do |dish, quantity|
+      raise "This item is not on the menu." unless menu.include?(dish)
+      order["#{dish} x #{quantity}"] = total_cost_per_item(menu[dish], quantity)
+    end
   end
 
   def total_cost_per_item(price, quantity)
-    price * quantity
+    (price * quantity).round(2)
+  end
+
+  def delete_from_order(dish, quantity)
+    order.delete("#{dish} x #{quantity}")
   end
 
 end
