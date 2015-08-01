e7b1ed9c7b1b1ac9b0f64f560bde3d65df1f9038
Diego Regules <diegoregules@gmail.com>
2015-08-01 20:13:27 +0100

    Add dish to List

--- lib/List.rb
+++ lib/List.rb
@@ -5,12 +5,18 @@ class List
                     :risotto => 10, :parmigiana => 12.50,
                     :quinoa_salad => 7.50
                   }
-  def initialize
+  attr_accessor :available
 
+  def initialize
+    @available = Hash.new
   end
 
-  def show
-    DEFAULT_DISHES
+  def load_predefined_dishes
+    @available = DEFAULT_DISHES
+  end
+
+  def add_dish name
+    @available[dish.name] = dish.price
   end
 
 end
--- spec/List_spec.rb
+++ spec/List_spec.rb
@@ -1,8 +1,18 @@
 require 'List'
 
 describe List do
-  it 'has a predefined list of dishes with prices' do
-    dish_list = subject.show
-    expect(dish_list).to have_at_least(5).dishes
+  let(:dish) { double(:dish, :name => :paella, :price => 15) }
+
+  describe '#load_predefined_dishes'
+  it 'creates a list of dishes with prices' do
+    subject.load_predefined_dishes
+    expect(subject.available).to have_at_least(5).dishes
+  end
+
+  describe '#add' do
+    it 'new dish to the list' do
+      subject.add_dish dish
+      expect(subject.available).to include dish.name
+    end
   end
 end
