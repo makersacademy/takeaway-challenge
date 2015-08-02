65939058ef7d2936330717ef1155195365f2fcce
giuseppe de santis <giusdesan@gmail.com>
2015-08-02 18:40:36 +0100

    added a class menu and passed tests

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,20 @@
 class Menu
+
+  DISHES = { 'Pizza' => 5,
+             'Pasta' => 4,
+             'Lasagne' => 5,
+             'Steak' => 6,
+             'Cod' => 5 }
+
+  def initialize dishes = DISHES
+    @dishes = dishes
+  end
+
+  def show_menu
+    result = ''
+    @dishes.each do |dish, price|
+      result += ("#{dish}, £ #{price}\n")
+    end
+    result
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/menu'
 
-describe Menu do
-
+describe Menu  do
+  it 'shows the menu properly' do
+    hash_example = { dish: :price }
+    subject = Menu.new hash_example
+    expect(subject.show_menu).to eq("#{:dish}, £ #{:price}\n")
+  end
 end
