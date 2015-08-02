8955bbea8d2e9afa0e2edb27ce48202b204b4dba
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-02 13:01:10 +0100

    customers can choose a number of pizzas if their choice is on the list. The choices are added to the ordered list

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,7 +4,13 @@ class Menu
 
   def initialize
     @list = {margherita: 5, pepperoni: 6, hawaiian: 6, supreme: 7}
+    @ordered = {}
   end
-  
-  attr_reader :list
+
+  def choose(amount, pizza)
+    raise "Your choice is not on the menu" unless list.include?(pizza.to_sym)
+    ordered.store(pizza.to_sym, amount)
+  end
+
+  attr_reader :list, :ordered
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,8 +2,21 @@ require 'menu'
 
 describe Menu do
 
-  it 'contains a list of dishes with prices' do
+  it 'contains a list of pizzas with prices' do
     expect(subject.list).to eq({margherita: 5, pepperoni: 6, hawaiian: 6, supreme: 7})
+  end
+
+  it 'allows to choose one or more of the same pizzas' do
+    expect(subject).to respond_to(:choose).with(2).arguments
+  end
+
+  it 'stores the choice the right number of times' do
+    subject.choose(2, "margherita")
+    expect(subject.ordered).to eq({margherita: 2})
+  end
+
+  it 'raises an error if the selected pizza is not in the list' do
+    expect{subject.choose(1, "banana")}.to raise_error "Your choice is not on the menu"
   end
 
 
