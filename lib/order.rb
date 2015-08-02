02fe7c2d5044c78bb194d859614707e0a266b34d
giuseppe de santis <giusdesan@gmail.com>
2015-08-02 20:14:37 +0100

    refactored and organised the program in one class order, passing tests

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,10 @@
-require_relative 'menu'
-
 class Order
   MENU = { Pizza: 5,
            Pasta: 4,
            Steak: 6,
            Cod: 5 }
 
-  attr_reader :menu
+  attr_reader :menu, :ordered_dishes
 
   def initialize menu = MENU
     @menu = menu
@@ -16,7 +14,7 @@ class Order
 
   def show_menu
     result = ''
-    @dishes.each do |dish, price|
+    @menu.each do |dish, price|
       result += ("#{dish}, £ #{price}\n")
     end
     result
@@ -24,6 +22,22 @@ class Order
 
   def add_order(dish, quantity)
     @ordered_dishes[dish] = quantity
+  end
+
+  def calculate_total
+    total = 0
+    ordered_dishes.each do |dish, quantity|
+      total += menu[dish] * quantity
+    end
+    total
+  end
+
+  def verify_total
+    result = ''
+    @ordered_dishes.each do |dish, quantity|
+      result += ("#{dish}, £ #{menu[dish]} * #{quantity}\n")
+    end
+    result += "Total amount: #{calculate_total}"
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,17 +1,32 @@
 require_relative '../lib/order'
 
 describe Order do
-  let(:menu) { double :menu }
 
   it 'shows the menu properly' do
     hash_example = { dish: :price }
-    subject = Menu.new hash_example
+    subject = Order.new hash_example
     expect(subject.show_menu).to eq("#{:dish}, £ #{:price}\n")
   end
 
-  it 'adds a dish x times when ordered' do
-    hash_example = { dish: :price }
-    subject.add_order(:dish, 2)
-    expect(subject.ordered_dishes).to eq [:dish, :dish]
+  describe '#order' do
+    before(:each) do
+      subject.add_order(:Cod, 2)
+      total = subject.calculate_total
+    end
+
+    it 'adds a dish x times when ordered' do
+      expect(subject.ordered_dishes[:Cod]).to eq 2
+    end
+
+    it 'calculates the total amount for the order' do
+      total = subject.calculate_total
+      expect(subject.calculate_total).to eq total
+    end
+
+    it 'shows the quantity of each dish and its price and the total amount' do
+      total = subject.calculate_total
+      str = "Cod, £ 5 * 2\nTotal amount: #{total}"
+      expect(subject.verify_total).to eq(str)
+    end
   end
 end
