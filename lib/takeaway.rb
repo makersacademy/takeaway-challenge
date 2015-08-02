6d4ee4706aded14d3558c3b5483e5556a4e36382
Josh <Tattsy@hotmail.co.uk>
2015-08-02 21:09:15 +0100

    now compares customer and actual price

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,24 +1,38 @@
 class Takeaway
 
-  attr_reader :menu, :order
+  attr_reader :menu, :order, :price, :cust_price
 
   def initialize
     @menu = { "pizza" => 5.95, "pasta" => 6.45, "curry" => 6.95, "toast" => 0.1, "fajitas" => 6.45, "bacon sandwich" => 3.50, "steak and chips" => 10.95 }
+    @order = []
   end
 
-  def place_order(item1,*items)
-    @order = [item1]
+  def place_order(*items,cust_price)
+    @cust_price = cust_price
+
     items.each do |x|
-      item_checker(item1,x)
+      item_checker(x)
       order << x 
     end
+
+    price_adder
+
+    fail "Your order costs £#{price} not £#{cust_price}" if !same_price?
+  end
+
+  def same_price?
+    cust_price == price
   end
 
   private
 
-  def item_checker(item1,item2)
-    fail "Apologies, we do not have #{item1} on the menu." if !menu.include?(item1)
-    fail "Apologies, we do not have #{item2} on the menu." if !menu.include?(item2)
+  def item_checker(item)
+    fail "Apologies, we do not have #{item} on the menu." if !menu.include?(item)
+  end
+
+  def price_adder
+    @price = 0
+    order.each { |y| @price += menu[y] }
   end
 
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,16 +2,27 @@ require "takeaway"
 describe "Features" do
   describe "takeaway" do
     let (:takeaway) { Takeaway.new }
-    it "displays menu items and prices" do
-      our_menu = { "pizza" => 5.95, "pasta" => 6.45, "curry" => 6.95, "toast" => 0.1, "fajitas" => 6.45, "bacon sandwich" => 3.50, "steak and chips" => 10.95 }
-      expect(takeaway.menu).to eq our_menu 
+    describe "#menu" do
+      it "displays menu items and prices" do
+        our_menu = { "pizza" => 5.95, "pasta" => 6.45, "curry" => 6.95, "toast" => 0.1, "fajitas" => 6.45, "bacon sandwich" => 3.50, "steak and chips" => 10.95 }
+        expect(takeaway.menu).to eq our_menu 
+      end
     end
-    it "places order of dishes" do
-      takeaway.place_order("pizza", "pasta")
-      expect(takeaway.order).to eq ["pizza", "pasta"]
-    end
-    it "doesn't accept orders of items not on the menu" do 
-      expect { takeaway.place_order("steak and chips", "toast", "grilled baby") }.to raise_error "Apologies, we do not have grilled baby on the menu."
+    describe "#place_order" do
+      it "places order of dishes" do
+        takeaway.place_order("pizza", "pasta", 12.40)
+        expect(takeaway.order).to eq ["pizza", "pasta"]
+      end
+      it "doesn't accept orders of items not on the menu" do 
+        expect { takeaway.place_order("steak and chips", "toast", "grilled baby", 15.00) }.to raise_error "Apologies, we do not have grilled baby on the menu."
+      end
+      it "takes a customers price and compares to what it should be" do
+        takeaway.place_order("pizza", "pasta", "curry", 19.35)
+        expect(takeaway).to be_same_price
+      end
+      it "raises error if price entered is not the same as the order total" do
+        expect { takeaway.place_order("pizza", "pasta", "curry", 1) }.to raise_error "Your order costs £19.35 not £1"
+      end
     end
   end
 end
