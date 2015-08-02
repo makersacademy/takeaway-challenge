3fc90acde2b6751c0698da0daa8dff89b64a3f4f
Sivan Patel <sivan.patel@googlemail.com>
2015-08-02 15:27:01 +0100

    Send text working

--- lib/twilio_sender.rb
+++ lib/twilio_sender.rb
@@ -1,5 +1,21 @@
-require 'twilio-ruby'
+require 'twilio-ruby'  # => true
 
 class Twilio_Sender
+
+  attr_reader :from, :client, :account_sid, :auth_token  # => nil
+
+  def initialize
+    @from = "441767512027"                                        # => "441767512027"
+    @account_sid = 'AC60f704b617eb4d0f74dec894d219dcf3'           # => "AC60f704b617eb4d0f74dec894d219dcf3"
+    @auth_token = 'c8757446a92df4de724fccdae07b7299'              # => "c8757446a92df4de724fccdae07b7299"
+    @client = Twilio::REST::Client.new @account_sid, @auth_token  # => <Twilio::REST::Client @account_sid=AC60f704b617eb4d0f74dec894d219dcf3>
+  end
+
+  def send_text to, message
+    message = @client.account.messages.create(:body => message,  # => "test"
+        :to => to,     # Replace with your phone number
+        :from => @from,                                          # => "441767512027"
+        :via => @client)   # Replace with your Twilio number
+  end
 
 end
--- spec/twilio_sender_spec.rb
+++ spec/twilio_sender_spec.rb
@@ -3,5 +3,13 @@ require 'twilio-ruby'
 
 describe Twilio_Sender do
 
+  it 'can send a message' do
+    messages = double :messages, create: nil
+    account = double :account, messages: messages
+    client = double :client, account: account
+    allow(Twilio::REST::Client).to receive(:new) { client }
+    expect(messages).to receive(:create).with(body: "Test", to: "447825786185", from: "441767512027", via: client)
+    subject.send_text("447825786185", "Test")
+  end
 
 end
