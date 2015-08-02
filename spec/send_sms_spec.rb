63cb5dc9fc6f8c91d8296a11e29470b56adda240
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-08-02 17:24:44 +0100

    Added extra test

--- spec/send_sms_spec.rb
+++ spec/send_sms_spec.rb
@@ -4,9 +4,13 @@ describe Send_sms do
 
   let(:phonenumber){double :phonenumber}
 
-  #Unsure if necessary (or possible) given Twilio's own tests/error raising.
-  xit "send_confirmation_sms creates sms using client" do
-    expect(subject.send_confirmation_sms(phonenumber)).to be_a Twilio::REST::Message
+  it "repsonds to send_confirmation_sms" do
+    expect(subject).to respond_to(:send_confirmation_sms)
   end
+
+  #Unsure if necessary (or possible) given Twilio's own tests/error raising.
+  # it "send_confirmation_sms creates sms using client" do
+  #   expect(subject.send_confirmation_sms(phonenumber)).to be_a Twilio::REST::Message
+  # end
 
 end
