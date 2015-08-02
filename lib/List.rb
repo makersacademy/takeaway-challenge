15239d2f6e02f3b094f7a4d73e28da7b5ba242dd
Diego Regules <diegoregules@gmail.com>
2015-08-02 14:19:27 +0100

    food_order_sum implemented

--- lib/List.rb
+++ lib/List.rb
@@ -5,18 +5,33 @@ class List
                     :risotto => 10, :parmigiana => 12.50,
                     :quinoa_salad => 7.50
                   }
-  attr_accessor :available
+  attr_accessor :available, :food_order
 
   def initialize
     @available = Hash.new
+    @food_order = Hash.new
   end
 
   def load_predefined_dishes
     @available = DEFAULT_DISHES
   end
 
-  def add_dish name
-    @available[dish.name] = dish.price
+  def add_dishes *dishes
+    dishes.each_with_index { |name, index| available[dishes[index].name] = dishes[index].price }
   end
 
+  def select_food_order *dish_names
+    fail 'Your food selection is not available.' unless food_available? dish_names
+    dish_names.each { |name| food_order[name] = available[name] }
+  end
+
+  def food_available? selected_food
+    selected_food.any? { |name| available.has_key?(name) }
+  end
+
+  def food_order_sum
+    sum = 0
+    food_order.each_value { |price| sum += price }
+    sum
+  end
 end
--- spec/List_spec.rb
+++ spec/List_spec.rb
@@ -2,6 +2,7 @@ require 'List'
 
 describe List do
   let(:dish) { double(:dish, :name => :paella, :price => 15) }
+#  let(:dish2) { double(:dish, :name => :fish_n_chips, :price => 11) }
 
   describe '#load_predefined_dishes'
   it 'creates a list of dishes with prices' do
@@ -9,10 +10,32 @@ describe List do
     expect(subject.available).to have_at_least(5).dishes
   end
 
-  describe '#add' do
+  describe '#add_dishes' do
     it 'new dish to the list' do
-      subject.add_dish dish
+      subject.add_dishes dish
       expect(subject.available).to include dish.name
+    end
+  end
+
+  describe '#select_order_of_food' do
+    it 'creates a food-order collection' do
+      subject.load_predefined_dishes
+      subject.select_food_order :risotto, :lasagna
+      expect(subject.food_order).to include(:risotto, :lasagna)
+    end
+
+    it 'raises error if food selection not available' do
+      subject.add_dishes dish
+      expect { subject.select_food_order :risotto }.to raise_error 'Your food selection is not available.'
+    end
+
+    describe '#food_order_sum' do
+      it 'verifies the sum of all dishes' do
+        subject.load_predefined_dishes
+        subject.select_food_order :risotto, :lasagna
+        #p subject.food_order[:risotto]
+        expect(subject.food_order_sum).to eq(subject.food_order[:risotto] + subject.food_order[:lasagna])
+      end
     end
   end
 end
