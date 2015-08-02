ffe8140aaa407577bd14b6f05ebe185e1d1dd04b
zlahham <zlahham@gmail.com>
2015-08-02 23:08:32 +0100

    User Story 1 | Completed

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,5 +1,13 @@
 class Menu
-  def list_meals
+  DISHES = {:burger => 5, :hotdog => 4, :fries => 3, :milkshake => 4, :soda => 3}
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = DISHES
+  end
+
+  def list_dishes
+    DISHES.each {|item, price| puts "#{item.to_s.capitalize} £#{price}"}
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,10 +1,15 @@
 require "takeaway"
 
 describe Menu do
-
   it "responds to #list_meals" do
-    expect(subject).to respond_to(:list_meals)
+    expect(subject).to respond_to(:list_dishes)
   end
 
-  
+  it "has a list of food items" do
+    expect(subject.list_dishes).to include(:hotdog, :burger, :fries, :milkshake, :soda)
+  end
+
+  it "shows a list of dishes with corresponding prices" do
+    expect(subject.dishes).to match(:burger => (a_value < 6), :hotdog => (a_value < 5), :fries => (a_value < 4), :milkshake => (a_value < 5),:soda => (a_value < 4))
+  end
 end
