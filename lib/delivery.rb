546ea8bf48aff479deb67594d1d285d9d90b28b3
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-08-03 00:05:50 +0100

    modified travisyml

--- lib/delivery.rb
+++ lib/delivery.rb
@@ -2,9 +2,9 @@ require 'twilio-ruby'
 
 class Delivery
 	def text_customer
-		account_sid = 'AC2f4a152920799e258a9c461663d3239a'
-		auth_token = '3a3cb5780128a54b1133ab029628ad9a'
-		@client = Twilio::REST::Client.new account_sid, auth_token
+		@account_sid = 'AC2f4a152920799e258a9c461663d3239a'
+		@auth_token = '3a3cb5780128a54b1133ab029628ad9a'
+		@client = Twilio::REST::Client.new @account_sid, @auth_token
 
 		@client.account.messages.create({:body => "Thank you! Your order was placed and will be delivered before 18:52",
 		    :to => "+441767512049",  
