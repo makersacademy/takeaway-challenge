72306e0980c498deb36750136f99286cbe6089c8
Sivan Patel <sivan.patel@googlemail.com>
2015-08-02 16:10:50 +0100

    Added display_menu test + method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,34 @@
+require_relative 'twilio_sender'
+
 class Takeaway
 
+  def initialize
+    @menu = {
+      pizza: 9.99,
+      chicken: 2.99,
+      kebab: 4.99,
+      beer: 0.99,
+      cider: 0.99,
+      wine: 7.99
+      }
+      @selection = {}
+  end
+
+  def display_menu
+    @menu.each do |item, price|
+      "#{item.to_s.capitalize} : £#{price}"
+    end
+  end
+
+
+#it has a menu y
+#it can show a menu y
+#it can choose from the menu
+#it can choose more than one item off the menu
+#it can choose different quantitites of items from the menu
+#it can calculate the price of the menu (With one or multiple items)
+#it can then send a text with the price and time for delivery
+
+#
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,6 +2,6 @@ require 'takeaway'
 
 describe Takeaway do
 
-
+  it { is_expected.to respond_to(:display_menu) }
 
 end
