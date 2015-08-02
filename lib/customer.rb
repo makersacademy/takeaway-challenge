6cfbe7dbefc9b16c66f24ff7d216cdc914ed6221
GabyML <gabriela.lagunes.11@uclac.uk>
2015-08-02 22:55:06 +0100

    Costumer can see menu and pick things

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,6 +2,17 @@ require_relative 'menu'
 
 class Customer
 
+	attr_reader :pizzas_menu, :drinks_menu, :ordered_pizzas, :ordered_drinks, :pizzas_bill, :drinks_bill
+
+	def initialize
+		@pizzas_menu = {}
+		@drinks_menu = {}
+		@ordered_pizzas = []
+		@ordered_drinks = []
+		@pizzas_bill = []
+		@drinks_bill = []
+	end
+
 	def show_pizzas(menu_class)
 		@pizzas_menu = menu_class.pizzas
 		@pizzas_menu
@@ -10,5 +21,21 @@ class Customer
 	def show_drinks(menu_class)
 		@drinks_menu = menu_class.drinks
 		@drinks_menu
+	end
+
+	def pick_pizza(quantity, pizza)
+		quantity.times do
+			ordered_pizzas << pizza
+			pizzas_bill << @pizzas_menu[pizza]
+		end
+		ordered_pizzas
+	end
+
+	def pick_drinks(quantity, drink)
+		quantity.times do
+			ordered_drinks << drink
+			drinks_bill << @drinks_menu[drink]
+		end
+		ordered_drinks
 	end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -11,4 +11,33 @@ describe Customer do
 	it 'responds to #show_menu by showing drinksfrom Menu class' do
 		expect(subject.show_drinks(menu_class)).not_to be_empty #Do this with a class method
 	end
+
+	it 'can choose a number of several available pizzas' do
+		expect(subject.pick_pizza(1, 'some pizza')).to eq ['some pizza']
+	end
+
+	it 'registers the prices of the pizzas chosen by the customer' do
+		subject.show_pizzas(menu_class)
+		subject.pick_pizza(1, 'some pizza')
+		expect(subject.pizzas_bill).to eq [4.50]
+	end
+
+	it 'can choose a number of several available drinks' do
+		expect(subject.pick_drinks(1, 'some drink')).to eq ['some drink']
+	end
+
+	it 'registers the prices of the drinks chosen by the customer' do
+		subject.show_drinks(menu_class)
+		subject.pick_drinks(1, 'some drink')
+		expect(subject.drinks_bill).to eq [3.50]
+	end
+
+	it 'gives the total charge for the required pizzas and drinks' do
+		subject.show_pizzas(menu_class)
+		subject.show_drinks(menu_class)
+		subject.pick_pizza(1, 'some pizza')
+		subject.pick_drinks(1, 'some drink')
+		expect(subject.total_charge).to eq 7.50
+	end
+
 end
