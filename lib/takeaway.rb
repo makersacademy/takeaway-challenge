341c55fe432e0195e60829ea9cc75a08606fc20b
zlahham <zlahham@gmail.com>
2015-08-03 00:58:13 +0100

    User Story 2 | Partially completed

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,29 @@
 class Menu
   DISHES = {:burger => 5, :hotdog => 4, :fries => 3, :milkshake => 4, :soda => 3}
 
-  attr_reader :dishes
+  attr_reader :dishes, :basket
 
   def initialize
     @dishes = DISHES
+    @basket = []
   end
 
   def list_dishes
-    DISHES.each {|item, price| puts "#{item.to_s.capitalize} £#{price}"}
+    dishes.each {|item, price| puts "#{item.to_s.capitalize} £#{price}"}
+    "Enjoy our delicious menu"
+  end
+
+  def add_to_basket(item, quantity=1)
+    raise "Please order from menu" unless check_for_food(item)
+    include_items(item, quantity)
+    item.to_s * quantity
+  end
+
+  def check_for_food(item)
+    dishes.has_key?(item)
+  end
+
+  def include_items(item, quantity)
+    dishes.select {|food, price|quantity.times { basket << food } if food == item}
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,10 +6,26 @@ describe Menu do
   end
 
   it "has a list of food items" do
-    expect(subject.list_dishes).to include(:hotdog, :burger, :fries, :milkshake, :soda)
+    expect(subject.dishes).to include(:hotdog, :burger, :fries, :milkshake, :soda)
   end
 
   it "shows a list of dishes with corresponding prices" do
     expect(subject.dishes).to match(:burger => (a_value < 6), :hotdog => (a_value < 5), :fries => (a_value < 4), :milkshake => (a_value < 5),:soda => (a_value < 4))
+  end
+
+  it "responds to #add_to_basket" do
+    expect(subject).to respond_to(:add_to_basket).with(2).argument
+  end
+
+  it "is capable of adding an item to basket" do
+    expect(subject.add_to_basket(:burger)).to eq(subject.basket.join)
+  end
+
+  it "is capable of adding multipes of the same item to basket" do
+    expect(subject.add_to_basket(:burger, 2)).to eq(subject.basket.join)
+  end
+
+  it 'does not accepts orders outside the menu' do
+    expect{subject.add_to_basket(:pizza)}.to raise_error "Please order from menu"
   end
 end
