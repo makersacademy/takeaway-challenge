3b3036df6faa252b99cbe4f8d5ce4203aaedc056
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-08-04 20:58:14 +0100

    Refactored

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -3,15 +3,13 @@ require 'twilio-ruby'
 class Restaurant
 	
 	def send_confirmation_message
-		account_sid = 'AC57bd6a35fc71e021da4ca730b6d1294e' 
-		auth_token = '4487c5be04356a927a6da0562418fa10' 
 		 
 
-		@client = Twilio::REST::Client.new account_sid, auth_token 
+		@client = Twilio::REST::Client.new ENV['TWILIO_AUTH_SID'], ENV['TWILIO_AUTH_TOKEN']
 		 
 		@client.account.messages.create(
-		from: "+441773252056", 
-      to: "+447729835662",   
+		from: ENV['TWILIO_NUMBER'], 
+      to: "+447729835662",  
       body: "Thank you for your order! It will arrive at #{(Time.now + 3600).hour}:#{Time.new.min}"
 		)
 	end
