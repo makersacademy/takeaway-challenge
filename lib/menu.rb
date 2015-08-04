7f90b838e5cc22afa8fca5664e165241af9a6f7c
Toby Clarke <teeohbee@gmail.com>
2015-08-04 18:27:12 +0100

    Refactored and redesigned domain model

---
+++ lib/menu.rb
@@ -0,0 +1,11 @@
+class Menu
+  attr_reader :list
+
+  def initialize
+    @list = { hoisin_crispy_owl: 11,
+              pints_o_cream: 6,
+              pasta_pillows: 5,
+              bonbonbonbons: 7,
+              fluffy_ruffs: 8 }
+  end
+end
--- lib/order.rb
+++ lib/order.rb
@@ -1,23 +1,21 @@
+require_relative 'menu'
+
 class Order
 
-attr_reader :ordered_items, :menu, :total_price
+  attr_reader :ordered_items, :total_price
 
   def initialize
     @ordered_items = []
     @total_price = []
-    @menu = { pizza: 11,
-              salad: 6,
-              hotdog: 5,
-              pasta: 7,
-              chicken: 8 }
   end
 
   def request_menu
-    menu.each { |food, price| puts "#{food} - £#{price}" }
-    end
+    menu = Menu.new
+    menu.list.each { |food, price| puts "#{food} - £#{price}" }
+  end
 
   def add(item)
-    selection = menu.select { |key| key == item }
+    selection = Menu.new.list.select { |key| key == item }
     ordered_items << selection.keys[0]
     total_price << selection.values[0]
   end
@@ -26,18 +24,18 @@ attr_reader :ordered_items, :menu, :total_price
     total_price.inject { |sum, x| sum + x }
   end
 
- def order_confirmation
-  require 'rubygems'
-  require 'twilio-ruby'
+  def order_confirmation
+    require 'rubygems'
+    require 'twilio-ruby'
 
-  account_sid = 'AC3b1ad07e8cd4535d34644bc14d23d56b'
-  auth_token = '0324930a70225d309b4924819b395661'
+    account_sid = 'AC3b1ad07e8cd4535d34644bc14d23d56b'
+    auth_token = '0324930a70225d309b4924819b395661'
 
-  @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new account_sid, auth_token
 
-  @client.account.messages.create({
-    :from => '+441822600013',
-    :to => '+447919376807',
-    :body => 'Thank you for your order! It should be with you by 10:30' })
-end
+    @client.account.messages.create({
+      :from => '+441822600013',
+      :to => '+447919376807',
+      :body => "Thank you for your order! It should be with you by #{Time.new}" })
+  end
 end
