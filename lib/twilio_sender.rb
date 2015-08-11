4c6c626c8512d4ce1f8979554af83ea31335767c
Sivan Patel <sivan.patel@googlemail.com>
2015-08-11 15:14:51 +0100

    taking out twilio info

--- lib/twilio_sender.rb
+++ lib/twilio_sender.rb
@@ -5,18 +5,21 @@ class TwilioSender
   attr_reader :from, :client, :account_sid, :auth_token
 
   def initialize
-    @from = "441767512027"
-    @account_sid = 'AC60f704b617eb4d0f74dec894d219dcf3'
-    @auth_token = 'c8757446a92df4de724fccdae07b7299'
+    @from = "____"
+    @account_sid = '____'
+    @auth_token = '____'
     @client = Twilio::REST::Client.new @account_sid, @auth_token
   end
 
   def send_text to, message
     message = @client.account.messages.create(
-                                                body: message,
+        body: message,
         to: to,
         from: @from,
         via: @client)
   end
 
 end
+
+t = TwilioSender.new
+t.send_text '____', 'test'
