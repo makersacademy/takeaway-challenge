6ca8ba03e599d3c0f9e79def100b7b53050d6832
chn.challenger <chn.challenger@gmail.com>
2015-09-12 19:03:12 +0100

    refactor

--- lib/menu.rb
+++ lib/menu.rb
@@ -9,11 +9,21 @@ class Menu
   end
 
   def add_dish(dish)
+    wrong_name?(dish)
+    wrong_price?(dish)
+    @dishes << dish
+  end
+
+  private
+
+  def wrong_name?(dish)
     fail 'invalid dish' unless dish.respond_to?(:name)
-    fail 'invalid name' unless dish.name.is_a?(String) && dish.name.length <= 30
+    fail 'invalid name' unless dish.name.is_a?(String) && dish.name.length < 31
+  end
+
+  def wrong_price?(dish)
     fail 'invalid dish' unless dish.respond_to?(:price)
     fail 'invalid price' unless dish.price.is_a?(Float) && dish.price >= 0
-    @dishes << dish
   end
 
 end
