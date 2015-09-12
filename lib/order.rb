ad453ebf79b4137cd36d049c93f2e200c5b2cf3d
chn.challenger <chn.challenger@gmail.com>
2015-09-12 23:00:49 +0100

    Added display order and tests

--- lib/order.rb
+++ lib/order.rb
@@ -13,4 +13,30 @@ class Order
     @ordered_dishes << add_to_order
   end
 
+  def display_order
+    total_price = 0
+    display = " "*20 + "Order Details           \n\n"
+    ordered_dishes.each do |dish_hash|
+      display += add_line_to_order_display(dish_hash)
+      total_price += dish_hash[:dish].price * dish_hash[:quantity]
+    end
+    display += "-"*53 + "\n"
+    total_price = total_price.round(2).to_s
+    total_price += "0" if total_price[-2] == "."
+    total_line_space = 44 - total_price.length
+    display += "Total:" + " "*total_line_space + " £ " + total_price
+    display
+  end
+
+  private
+
+  def add_line_to_order_display(dish_hash)
+    line_to_add = ''
+    space_length = 40 - dish_hash[:dish].name.length
+    price = (dish_hash[:dish].price * dish_hash[:quantity]).round(2).to_s
+    price += "0" if price[-2] == "."
+    price_space = 7 - price.length
+    dish_hash[:dish].name + " x " + dish_hash[:quantity].to_s + " "*(space_length+price_space) + "£ " + price + "\n"
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -21,6 +21,25 @@ describe Order do
     end
   end
 
-
+  describe "#display_order" do
+    it 'displays information about the order with total cost' do
+      dish1 = double(:dish)
+      dish2 = double(:dish)
+      allow(dish1).to receive(:name).and_return('Egg Fried Rice')
+      allow(dish1).to receive(:price).and_return(3.95)
+      allow(dish2).to receive(:name).and_return('Singapore Noodle')
+      allow(dish2).to receive(:price).and_return(4.90)
+      order = Order.new
+      order.add_to_order(dish1,2)
+      order.add_to_order(dish2,4)
+      display = order.display_order
+      expected_display = "                    Order Details           \n\nEgg"\
+      " Fried Rice x 2                             £ 7.90\nSingapore Noodle x"\
+      " 4                          £ 19.60\n---------------------------------"\
+      "--------------------\nTotal:                                        "\
+      "£ 27.50"
+      expect(display).to eq(expected_display)
+    end
+  end
 
 end
