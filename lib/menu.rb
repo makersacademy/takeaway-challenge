f3bc559676d9bad352e812c4793d9ebbde725342
chn.challenger <chn.challenger@gmail.com>
2015-09-12 18:52:48 +0100

    Menu can now add dishes with test suits

--- lib/menu.rb
+++ lib/menu.rb
@@ -8,4 +8,12 @@ class Menu
     @dishes = []
   end
 
+  def add_dish(dish)
+    fail 'invalid dish' unless dish.respond_to?(:name)
+    fail 'invalid name' unless dish.name.is_a?(String) && dish.name.length <= 30
+    fail 'invalid dish' unless dish.respond_to?(:price)
+    fail 'invalid price' unless dish.price.is_a?(Float) && dish.price >= 0
+    @dishes << dish
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -9,4 +9,35 @@ describe Menu do
     end
   end
 
+  describe "#add_dish" do
+    it 'raises error when dish passed in does not respond to name' do
+      expect{subject.add_dish('not_a_dish')}.to raise_error 'invalid dish'
+    end
+
+    it 'raises error when dish name is not a valid string' do
+      dish = double(:dish)
+      allow(dish).to receive(:name).and_return(11)
+      expect{subject.add_dish(dish)}.to raise_error 'invalid name'
+    end
+
+    it 'raises error when dish passed in does not respond to price' do
+      expect{subject.add_dish('not_a_dish')}.to raise_error 'invalid dish'
+    end
+
+    it 'raises error when dish pass has a none numerical price' do
+      dish = double(:dish)
+      allow(dish).to receive(:name).and_return('dish_name')
+      allow(dish).to receive(:price).and_return('this is a joke')
+      expect{subject.add_dish(dish)}.to raise_error 'invalid price'
+    end
+
+    it 'adds a dish to menu with passed dish-like object' do
+      dish = double(:dish)
+      allow(dish).to receive(:name).and_return('dish_name')
+      allow(dish).to receive(:price).and_return(9.99)
+      subject.add_dish(dish)
+      expect(subject.dishes).to eq([dish])
+    end
+
+  end
 end
