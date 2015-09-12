7f49b3f8ae7b083d69ca37a64745d5bbbca3d013
Michael Roger <bodgeuk12@gmail.com>
2015-09-12 16:27:35 +0100

    Test add for listing dishes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,12 +2,20 @@ require 'menu'
 
 describe Menu do
 
-  let(:dish){double(:dish)}
+  let(:dish1){double(:dish1, name: "Dishy1", price: 3.87)}
 
   describe '#add' do
     it 'adds a new dish to the menu' do
-      subject.add(dish)
-      expect(subject.dishes_available.include?(dish)).to be true
+      subject.add(dish1)
+      expect(subject.dishes_available.include?(dish1)).to be true
+    end
+  end
+
+  describe '#view' do
+    it 'lists dishes available with price' do
+      subject.add(dish1)
+      expect{ subject.view }.to output("1: Dishy1 | Price: £3.87\n").to_stdout
+
     end
   end
 
