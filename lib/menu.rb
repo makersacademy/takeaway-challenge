9a5c8f019308b6b66955fc2e2d97549fd345acd6
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-12 13:56:42 +0100

    Can only order dishes from the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,19 +1,21 @@
 class Menu
 
-  attr_writer :menu
+  attr_writer :menu, :order
 
   def initialize
     @menu =   { "crispy chilli beef" => 5.99,
         "sweet and sour chicken" => 4.99,
-        "egg fried rice" => 2.99
-      }
+        "egg fried rice" => 2.99 }
+    @order = []
   end
 
   def display_menu
-    @menu.each do |dish, price|
-      puts "#{dish}, £#{price}"
-      puts @menu
-    end
+    @menu.each { |key, value| "#{key}, £#{value}" }
+  end
+
+  def choose_dish(dish)
+    fail "This dish is not on the menu" unless @menu.has_key?(dish)
+    @order << dish
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,9 +2,24 @@ require 'menu'
 
 describe Menu do
 
-  describe "#display_menu"
+  describe "#display_menu" do
+
     it "displays dish and price" do
-      expect(subject.display_menu).to eq ( { "crispy chilli beef" => 5.99, "sweet and sour chicken" => 4.99, "egg fried rice" => 2.99})
+      expect(subject.display_menu).to eq ( {
+        "crispy chilli beef" => 5.99,
+        "sweet and sour chicken" => 4.99,
+        "egg fried rice" => 2.99
+        } )
+    end
+  end
+
+  describe "#choose_dish" do
+
+    it "can choose dish from menu" do
+      expect(subject).to respond_to(:choose_dish).with(1).argument
     end
 
+    it "can only order form the menu" do
+      expect(subject.choose_dish "crispy chilli beef").to eq ["crispy chilli beef"]      end
+    end
 end
