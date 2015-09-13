1e692aa14e341d004de34d328fce916ec3b5924e
chweeks <chrisharryweeks@gmail.com>
2015-09-13 20:11:42 +0100

    added add_dish and show method to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,19 @@
+require_relative 'dish'
+
 class Menu
 
   attr_reader :dishes
 
   def initialize
-    dishes = []
+    @dishes = []
   end
 
   def add_dish(dish)
+    dishes << dish
   end
 
   def show
+    puts "Menu: \n"
+    dishes.each{ |dish| puts "#{dish.show_details}" + "\n"}
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,20 +1,21 @@
 require 'menu'
 
 describe Menu do
-  let(:dish) {double :chicken_chow_mein, name: 'Chicken Chow Mein', price: '£5.95'}
+  let(:dish) {double :dish, show_details: 'Coke: £1'}
+  
   it { is_expected.to respond_to :dishes }
 
   describe '#add_dish' do
     it 'should add dish to menu' do
-      subject.add_dish :dish
-      expect(subject.dishes).to eql [:chicken_chow_mein]
+      subject.add_dish dish
+      expect(subject.dishes).to eql [dish]
     end
   end
 
   describe '#show' do
     it 'should show the entire menu' do
-      subject.add_dish :dish
-      expect(subject.show).to eql 'Menu: Chicken Chow Mein: £5.95'
+      subject.add_dish dish
+      expect{subject.show}.to output("Menu: \nCoke: £1\n").to_stdout
     end
   end
 end
