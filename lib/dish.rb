eb7bc4a9fb2b6dfe962a2a14981dfa16339b1b1b
chweeks <chrisharryweeks@gmail.com>
2015-09-13 13:34:07 +0100

    updated Dish class with #initialize and #show_details

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,2 +1,13 @@
 class Dish
+
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def show_details
+    "#{name}: #{price}"
+  end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -3,12 +3,13 @@ require 'dish'
 chicken_chow_mein = Dish.new('Chicken Chow Mein', '£5.95')
 
 describe Dish do
+
   describe '#show_details' do
 
-    it { is_expected.to respond_to :show_details }
+    it { expect(chicken_chow_mein).to respond_to :show_details }
 
     it 'should show name and price' do
-      expect(chicken_chow_mein.show_details).to be 'Chicken Chow Mein: £5.95'
+      expect(chicken_chow_mein.show_details).to eql 'Chicken Chow Mein: £5.95'
     end
   end
 end
