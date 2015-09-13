afb259704757c465ea7d9bb0d4054311b5caa44a
Michael Roger <bodgeuk12@gmail.com>
2015-09-13 19:28:28 +0100

    Order summary available and submit with payment to call #send_text method

--- lib/order.rb
+++ lib/order.rb
@@ -12,4 +12,20 @@ class Order
     end
   end
 
+  def summary
+    basket_hash = {}
+    basket.uniq.each do |a|
+      basket_hash[a] = basket.count(a)
+    end
+    basket_hash.each_pair do |k, v|
+      puts "#{v}x #{k.name} | £#{'%.2f' % (v * k.price)}"
+    end
+    puts "Total cost: £#{'%.2f' % basket.inject(0){|sum, x| sum + x.price}}"
+  end
+
+  def submit(payment)
+    fail "Payment does not match total cost" if basket.inject(0){|sum, x| sum + x.price} != payment
+    send_text
+  end
+
 end
