f26133e89bed2a1ca0c6aad9b2ac0800f12642d6
Harry <harryandrew.dix@gmail.com>
2015-09-13 17:15:54 +0100

    lib and spec files added

---
+++ lib/twilio_api.rb
@@ -0,0 +1,21 @@
+require 'twilio-ruby' 
+
+module Messenger
+
+def sendMessage(message_out,receiver)
+  
+  # account credentials:
+  account_sid = ENV["ACCOUNT_SID"]
+  auth_token = ENV["AUTH_TOKEN"]
+   
+  @client = Twilio::REST::Client.new account_sid, auth_token 
+   
+  @client.account.sms.messages.create(
+    :from => ENV["FROM"],
+    :to => ENV["TO"],
+    :body => message_out,
+    )
+  @sent = true
+end
+
+end
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+require_relative "menu"
+require_relative "twilio_api.rb"
+
+class User
+  
+end
