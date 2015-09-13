b5bf0131e1251916aba73ca3bef9d475c5204382
Proskurina <yproskurina@yahoo.com>
2015-09-13 20:04:18 +0100

    Added tests and code for the first three parts of the user story

---
+++ spec/customer_spec.rb
@@ -0,0 +1,30 @@
+require 'customer'
+
+describe Customer do
+
+  it "can see list of dishes" do
+    expect(subject.list_of_dishes).to eq(Menu::MENU)
+  end
+
+  it "can check if given total equals to order's total" do
+    order = double :order
+    allow(order).to receive(:total).and_return(35)
+    cust = Customer.new(order)
+    expect(cust.total_correct(35)).to eq(true)
+  end
+
+  it "can not place an order if it is empty" do
+    order = double :order
+    cust = Customer.new(order)
+    expect{cust.place_order({}, 0)}.to raise_error "Order is empty"
+  end
+
+  it "can not place an order if total is wrong" do
+    order = double :order, empty?: false
+    allow(order).to receive(:total).and_return(35)
+    cust = Customer.new(order)
+    expect{cust.place_order({'pizza'=>2}, 33)}.to raise_error "Total is not correct"
+  end
+
+
+end
