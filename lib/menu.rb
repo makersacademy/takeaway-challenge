8c0b527eedda7a7c0e1719b67f4b68cc388d0c44
Michael Roger <bodgeuk12@gmail.com>
2015-09-13 11:50:15 +0100

    Refactor

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,19 +7,23 @@ class Menu
   end
 
   def add(dish)
-    if dishes_available.count > 0
-      menu_number = dishes_available.keys.max + 1
-    else
+    if no_dishes_available?
       menu_number = 1
+    else
+      menu_number = dishes_available.keys.max + 1
     end
     @dishes_available[menu_number] = dish
   end
 
   def view
-    raise "No meal options have been added to this menu" if dishes_available.count == 0
+    raise "No meal options have been added to this menu" if no_dishes_available?
     dishes_available.each do |k, v|
       puts "#{k}: #{v.name} | Price: £#{v.price}"
     end
+  end
+
+  def no_dishes_available?
+    dishes_available.count == 0
   end
 
 end
