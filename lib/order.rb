a36f3b464d348ac6cfd11cf1560f1b8cc1a4a0ed
Michael Roger <bodgeuk12@gmail.com>
2015-09-13 15:04:41 +0100

    Placing orders adds the dishes to the basket

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,15 @@
 class Order
 
+  attr_reader :basket
+
+  def initialize
+    @basket = []
+  end
+
+  def place_order(menu, dish_number, quantity)
+    quantity.times do
+      @basket << menu.select(dish_number)
+    end
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,21 @@ require 'order'
 
 describe Order do
 
+  let(:dish1){double(:dish1, name: "Dishy1", price: 1.11)}
+  let(:dish2){double(:dish2, name: "Dishy2", price: 2.22)}
+  let(:menu){double(:menu, select: dish1)}
+
+  describe '#place_order' do
+    it 'calls the menu#select method' do
+      expect(menu).to receive(:select)
+      subject.place_order(menu, 1, 2)
+    end
+
+    it 'given the menu, dish number and quantity adds the dish/dishes to the order' do
+      # allow(menu).to receive(:select){dish1}
+      subject.place_order(menu, 1, 2)
+      expect( subject.basket.count(dish1) ).to eq 2
+    end
+  end
+
 end
