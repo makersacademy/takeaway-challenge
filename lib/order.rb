46a2a9956f96b7bb190f470b71bc08cfb89f84cf
Dom Batten <dominic.batten@googlemail.com>
2015-09-13 13:33:22 +0100

    Added functionality for adding any number of the same item to the basket

--- lib/order.rb
+++ lib/order.rb
@@ -1,24 +1,25 @@
 class Order 
 
   attr_accessor :basket
-  attr_reader :total
+  attr_reader :total, :menu
 
-  def initialize
+  def initialize(menu = Menu.new)
+    @menu = menu
     @basket = []
     @total = 0.0
   end
 
-  def check_dishes(menu)
+  def check_dishes
     menu.dishes_available
   end
 
-  def add_item(menu, dish)
+  def add_item(dish, quantity = 1)
     fail 'That item is not on the menu' unless menu.dishes_available.has_key?(dish)
     basket << dish
     @total += menu.dishes_available[dish]
   end
 
-  def remove_item(menu, dish)
+  def remove_item(dish, quantity = 1)
     fail 'Item was not in the basket' unless basket.include?(dish)
     basket.delete(dish)
     @total -= menu.dishes_available[dish]
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,24 +6,24 @@ describe Order do
 
   it { is_expected.to respond_to(:add_item).with(2).arguments }
   it { is_expected.to respond_to(:remove_item).with(2).argument }
-  it { is_expected.to respond_to(:check_dishes).with(1).argument }
+  it { is_expected.to respond_to(:check_dishes) }
   it { is_expected.to respond_to(:basket) }
   
 
   it 'should be able to check the list of dishes with prices' do
     menu = Menu.new
-    expect(subject.check_dishes(menu)).to be_truthy
+    expect(subject.check_dishes).to be_truthy
   end 
 
   it 'should be able to add a dish to the basket' do
     menu = Menu.new
-    subject.add_item(menu, "Duck Pancakes")
+    subject.add_item("Duck Pancakes")
     expect(subject.basket.include?("Duck Pancakes")).to be(true)
   end
 
   it 'should increase the total price correctly after adding an item' do
     menu = Menu.new
-    subject.add_item(menu, "Duck Pancakes")
+    subject.add_item("Duck Pancakes")
     expect(subject.total).to eq(4.99)
   end
 
@@ -38,21 +38,21 @@ describe Order do
 
   it 'should be able to remove an item from the basket' do
     menu = Menu.new
-    subject.add_item(menu, "Duck Pancakes")
-    subject.remove_item(menu, "Duck Pancakes")
+    subject.add_item("Duck Pancakes")
+    subject.remove_item("Duck Pancakes")
     expect(subject.basket.empty?).to eq(true)
   end
 
   it 'should not be able to remove an item not in the basket' do
     menu = Menu.new
-    subject.add_item(menu, "Duck Pancakes")
-    expect{ subject.remove_item(menu, "Egg Fried Rice") }.to raise_error 'Item was not in the basket'
+    subject.add_item("Duck Pancakes")
+    expect{ subject.remove_item("Egg Fried Rice") }.to raise_error 'Item was not in the basket'
   end
 
   it 'should decrease the total price accordingly if an item is removed' do
     menu = Menu.new
-    subject.add_item(menu, "Duck Pancakes")
-    subject.remove_item(menu, "Duck Pancakes")
+    subject.add_item("Duck Pancakes")
+    subject.remove_item("Duck Pancakes")
     expect(subject.total).to eq(0)
   end
 
