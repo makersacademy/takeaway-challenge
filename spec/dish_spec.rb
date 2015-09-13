d3bd029c463a35cfbc4cb40ebc75f4bd227fa1dc
chweeks <chrisharryweeks@gmail.com>
2015-09-13 13:26:12 +0100

    updated Dish class tests

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,12 +1,14 @@
 require 'dish'
 
+chicken_chow_mein = Dish.new('Chicken Chow Mein', '£5.95')
+
 describe Dish do
   describe '#show_details' do
 
     it { is_expected.to respond_to :show_details }
 
     it 'should show name and price' do
-      expect(subject.show_details).to be 'Chicken Chow Mein: £5.95'
+      expect(chicken_chow_mein.show_details).to be 'Chicken Chow Mein: £5.95'
     end
   end
 end
