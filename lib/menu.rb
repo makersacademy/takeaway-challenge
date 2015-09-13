e6e018056457f8bde4f75d3e135190a7ede7ba80
chweeks <chrisharryweeks@gmail.com>
2015-09-13 19:52:15 +0100

    added menu Class tests

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,14 @@
 class Menu
+
+  attr_reader :dishes
+
+  def initialize
+    dishes = []
+  end
+
+  def add_dish(dish)
+  end
+
+  def show
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,20 @@
 require 'menu'
 
 describe Menu do
-  
+  let(:dish) {double :chicken_chow_mein, name: 'Chicken Chow Mein', price: '£5.95'}
+  it { is_expected.to respond_to :dishes }
+
+  describe '#add_dish' do
+    it 'should add dish to menu' do
+      subject.add_dish :dish
+      expect(subject.dishes).to eql [:chicken_chow_mein]
+    end
+  end
+
+  describe '#show' do
+    it 'should show the entire menu' do
+      subject.add_dish :dish
+      expect(subject.show).to eql 'Menu: Chicken Chow Mein: £5.95'
+    end
+  end
 end
