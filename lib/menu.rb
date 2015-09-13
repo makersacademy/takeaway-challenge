ce42ac8f8d770a165693854b4d94d8c7797e102b
Michael Roger <bodgeuk12@gmail.com>
2015-09-13 14:31:36 +0100

    Refactor - renaming for clarity

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,40 +1,40 @@
 class Menu
 
-  attr_reader :dishes_available
+  attr_reader :menu
 
   def initialize
-    @dishes_available = {}
+    @menu = {}
   end
 
   def add(dish)
-    menu_number = next_menu_number
-    @dishes_available[menu_number] = dish
+    number = next_dish_number
+    @menu[number] = dish
   end
 
   def view
-    fail "No meal options have been added to this menu" if no_dishes_available?
-    dishes_available.each do |k, v|
-      puts "#{k}: #{v.name} | Price: £#{v.price}"
+    fail "No dishes available on this menu" if no_dishes_available?
+    menu.each do |k, v|
+      puts "#{k}: #{v.name} | Price: £#{'%.2f' % v.price}"
     end
   end
 
-  def select(order_number)
-    fail "No meal options have been added to this menu" if no_dishes_available?
-    fail "There are no dishes matching the number given" unless valid_order_number?(order_number)
-    dishes_available[order_number]
+  def select(dish_number)
+    fail "No dishes available on this menu" if no_dishes_available?
+    fail "There are no dishes matching the number given" unless valid_order_number?(dish_number)
+    menu[dish_number]
   end
 
   def no_dishes_available?
-    dishes_available.count == 0
+    menu.count == 0
   end
 
   def valid_order_number?(number)
-    dishes_available.keys.include?(number)
+    menu.keys.include?(number)
   end
 
-  def next_menu_number
+  def next_dish_number
     return 1 if no_dishes_available?
-    dishes_available.keys.max + 1
+    menu.keys.max + 1
   end
 
 end
