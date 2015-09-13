78b27e816e910e874b391d2e7ce7eeeb366be255
Aaron <aarongkendall@gmail.com>
2015-09-13 12:16:52 +0100

    Creates a bill

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,7 @@
 class Takeaway
 
+  def create_bill(order_array)
+    price = order_array.inject { |acc, num| acc + num }
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,5 +2,13 @@ require 'takeaway.rb'
 
 describe Takeaway do
 
+  it 'responds to create_bill method with an argument' do
+    is_expected.to respond_to(:create_bill).with(1).argument
+  end
+
+  it 'should add up the total bill correctly' do
+    t = Takeaway.new
+    expect(t.create_bill [1, 2, 3]).to eq(6)
+  end
 
 end
