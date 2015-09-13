cdbcb5ff60412273d2947aaedec895d511da6581
Jongmin Kim <kjm141215@gmail.com>
2015-09-13 21:33:59 +0100

    Working on text

--- lib/text.rb
+++ lib/text.rb
@@ -1,27 +1,27 @@
-require 'dotenv'
-Dotenv.load
-
-require 'rubygems'
-require 'twilio-ruby'
-
-
+# require 'dotenv'
+# Dotenv.load
+#
+# require 'rubygems'
+# require 'twilio-ruby'
+#
+#
 module Text
-
-
-    account_sid = ENV["ACCOUNT_SID"]
-    auth_token = ENV["AUTH_TOKEN"]
-    @client = Twilio::REST::Client.new account_sid, auth_token
-
-    Twilio.configure do |config|
-      config.account_sid = account_sid
-      config.auth_token = auth_token
-    end
-
-    @client = Twilio::REST::Client.new
-
-    @client.messages.create(
-      from: '+441942316749',
-      to: '+447497811148',
-      body: 'hihihi')
-  
+#
+#
+#     account_sid = ENV["ACCOUNT_SID"]
+#     auth_token = ENV["AUTH_TOKEN"]
+#     @client = Twilio::REST::Client.new account_sid, auth_token
+#
+#     Twilio.configure do |config|
+#       config.account_sid = account_sid
+#       config.auth_token = auth_token
+#     end
+#
+#     @client = Twilio::REST::Client.new
+#
+#     @client.messages.create(
+#       from: '+441942316749',
+#       to: '+447497811148',
+#       body: 'hihihi')
+#
 end
