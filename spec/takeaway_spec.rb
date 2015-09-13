53c12ef1dd5878530047825da094463f78fa03f8
Tim Harrison <mail@trwh.co.uk>
2015-09-13 18:22:50 +0100

    Fixed indentation in spec/takeaway_spec.rb

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,8 +3,8 @@ require_relative '../lib/takeaway.rb'
 
 describe Takeaway do
 
-   it 'returns a properly formatted list of dishes available when asked' do
-    expect(subject.see).to eq '44 : Duck a la banana : £4.50, 10 : Deep fried Mars bar : £0.10, 20 : Birdseye potato waffles : £1.99, 69 : Trio! Trio. : £1.50'
+  it 'returns a properly formatted list of dishes available when asked' do
+  expect(subject.see).to eq '44 : Duck a la banana : £4.50, 10 : Deep fried Mars bar : £0.10, 20 : Birdseye potato waffles : £1.99, 69 : Trio! Trio. : £1.50'
   end
 
   it 'accepts an order and populates a menu' do
