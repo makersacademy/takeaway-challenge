bf592c7b72c7818a7882e0bd50c322eeaebfb49a
Michael Roger <bodgeuk12@gmail.com>
2015-09-13 11:39:25 +0100

    Lists mutliple dishes with order numbers

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,11 +3,22 @@ class Menu
   attr_reader :dishes_available
 
   def initialize
-    @dishes_available = []
+    @dishes_available = {}
   end
 
   def add(dish)
-    @dishes_available << dish
+    if dishes_available.count > 0
+      menu_number = dishes_available.keys.max + 1
+    else
+      menu_number = 1
+    end
+    @dishes_available[menu_number] = dish
+  end
+
+  def view
+    dishes_available.each do |k, v|
+      puts "#{k}: #{v.name} | Price: £#{v.price}"
+    end
   end
 
 end
