9956c675b55dd7779d175670a2ba295cc0abf162
Dom Batten <dominic.batten@googlemail.com>
2015-09-13 12:12:16 +0100

    Added tests for menu

---
+++ lib/menu.rb
@@ -0,0 +1,17 @@
+require_relative 'dish'
+
+class Menu
+
+  attr_reader :dishes_available
+
+  def initialize 
+    @dishes_available = { "Duck Pancakes" => 4.99,
+      "Chicken Chow Mein" => 3.80,
+      "Egg Fried Rice" => 1.99,
+      "Crispy Beef" => 4.55,
+      "Coke" => 0.99
+    }
+  end
+
+
+end
