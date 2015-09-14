8ea38ab45d6078b3161d115dd3a48bda7a5edf8d
kyle macpherson <kyle1@kyles-MBP.home>
2015-09-14 06:45:11 +0100

    some changes

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -1,16 +1,8 @@
 require 'text.rb'
 
-describe Text do
-  let(:text) {described_class.new}
-  let(:fake_client) {double :fake_client}
-  let(:order_time) {(Time.new + 3600)}
-
-before(:example) do
-  fake_client.stub_chain("account.messages.create") { "Hi, your food will be with you before #{order_time}!" }
-  allow(Twilio::REST::Client).to receive(:new).and_return(fake_client)
-end
-
   it 'can confirm an order via text' do
+    text = Text.new
+    allow(text).to receive(:send_text) {"Hi, your food will be with you before #{order_time}!"}
     expect(text.send_text).to eq("Hi, your food will be with you before #{order_time}!")
   end
 
