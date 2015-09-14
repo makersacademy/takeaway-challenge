d35d640beabe1739ff0e172439942159e7a695ae
zsid <zhivko.siderov@gmail.com>
2015-09-14 08:40:31 +0100

    Refactored take_away method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -11,15 +11,23 @@ class Restaurant
   end
 
   def take_away
-    delivery = Time.now + 3600
-    delivery_time = delivery.strftime("%H:%M")
-
-     @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
-
-     message = @client.account.messages.create(
-       :from => ENV['TWILIO_NUM'],
-       :to => ENV['NUM'],
-       :body => "Thank you! Your order was placed and will be delivered before #{delivery_time}."
-     )
+    send_text_confirmation
   end
+
+  private
+
+  def estimated_delivery_time
+    delivery = Time.now + 3600
+    delivery.strftime("%H:%M")
+  end
+
+  def send_text_confirmation
+    @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
+
+    message = @client.account.messages.create(
+      :from => ENV['TWILIO_NUM'],
+      :to => ENV['NUM'],
+      :body => "Thank you! Your order was placed and will be delivered before #{estimated_delivery_time}."
+    )
+ end
 end
