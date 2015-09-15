0a5fe200ad61347e2d87dd0abd5bff63d62e0781
Proskurina <yproskurina@yahoo.com>
2015-09-15 21:27:02 +0100

    Massive refactoring + couple of tests

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -6,6 +6,13 @@ describe Customer do
     expect(subject.list_of_dishes).to eq(Menu::MENU)
   end
 
+  it "can add dish" do
+    order = double :order
+    cust = Customer.new(order)
+    expect(order).to receive(:add_dish).with('pizza',1)
+    cust.add_dish('pizza',1)
+  end
+
   it "can check if given total equals to order's total" do
     order = double :order
     allow(order).to receive(:total).and_return(35)
@@ -29,9 +36,17 @@ describe Customer do
   it "receive a message if order is ok" do
     order = double :order, empty?: false
     allow(order).to receive(:total).and_return(35)
-    cust = Customer.new(order)
-    allow(cust).to receive(:send_text).and_return("Text sent!")
+    twilio_text = double :twilio_text
+    allow(twilio_text).to receive(:send_text).and_return("Text sent!")
+    cust = Customer.new(order, twilio_text)
     expect(cust.place_order({'pizza'=>2}, 35)).to eq("Text sent!")
+  end
+
+  it "knows the list of ordered dishes" do
+    order = double :order
+    allow(order).to receive(:dishes).and_return({'pizza'=>1})
+    cust = Customer.new(order)
+    expect(cust.list).to eq({'pizza'=>1})
   end
 
 
