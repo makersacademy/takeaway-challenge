f383df4e887457385ed980d4062d276c7706eee2
chweeks <chrisharryweeks@gmail.com>
2015-09-16 17:44:40 +0100

    updated ENV variables

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -1,12 +1,11 @@
 require 'text'
-require '../.env.rb'
 require 'twilio-ruby'
 
 describe Text do
   it { is_expected.to respond_to :time }
 
   it { is_expected.to respond_to :send_text}
-  
+
   describe '#send_text' do
     it 'should send text when order is placed' do
      client = double :client
