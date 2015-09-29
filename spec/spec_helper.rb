b7b148a790718b8635dca57b0f650a69a7b56731
chn.challenger <chn.challenger@gmail.com>
2015-09-29 08:50:00 +0100

    Adding coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,23 @@
 require 'coveralls'
+require 'simplecov'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 Coveralls.wear!
+#SimpleCov.start
 require 'capybara/rspec'
+
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec do |expectations|
+     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+   end
+
+   config.mock_with :rspec do |mocks|
+     mocks.verify_partial_doubles = true
+   end
+
+end
