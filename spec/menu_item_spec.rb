1c1a5ce473067e6ef40c3a5b58fa78e3a5c14db1
philip <philip.clarke@redhood.co.uk>
2015-10-04 15:00:54 +0100

    added new tests for menu items

--- spec/menu_item_spec.rb
+++ spec/menu_item_spec.rb
@@ -4,9 +4,22 @@ describe Menu_item do
 
 
 
+
   it 'initialize raises error when missing menu name and price' do
     expect{described_class.new()}.to raise_error(ArgumentError)
   end
+
+  it 'has a name' do
+    curry = Menu_item.new("curry", 5)
+    expect(curry.name).to eql("curry")
+  end
+
+  it 'has a price' do
+    curry = Menu_item.new("curry", 5)
+    expect(curry.price).to eql(5)
+  end
+
+
 
 
 
