244e4dbef584f7d14178e038318abd03db744649
blancopado <pablo.dominguezblanco@gmail.com>
2015-10-24 18:08:08 +0200

    list_of_dishes

---
+++ lib/order.rb
@@ -0,0 +1,11 @@
+class Order
+
+  def initialize
+  	@total_price = 0
+  end
+
+  def place_order
+
+  end
+  
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,13 @@
+require 'takeaway'
+
+
+
+describe Takeaway do
+
+  
+  describe '#initialize' do
+  	 it { is_expected.to respond_to :list_of_dishes }
+  end
+
+
+end
