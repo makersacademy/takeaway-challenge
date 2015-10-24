f9394769c9b4331fc55807f3ea69fdb51c955217
hasulica <raduasandei@gmail.com>
2015-10-24 17:49:17 +0100

    Refactored tests

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -19,28 +19,37 @@ describe Takeaway do
     expect(subject.basket).to include("chilli"=>3)
   end
 
-  it 'should display total amount for order' do
+  before(:each) do
     subject.add_dish('chilli', 2.99)
     subject.add_dish('lasagne', 4.5)
     subject.order('chilli', 3)
     subject.order('lasagne')
-    expect(subject.total).to eq (2.99 * 3 + 4.5)
   end
 
-  it 'checkout should confirm order' do
-    subject.add_dish('chilli', 2.99)
-    subject.add_dish('lasagne', 4.5)
-    subject.order('chilli', 3)
-    subject.order('lasagne')
-    expect{ subject.checkout(13.47) }.to output("Your order is on its way!\n").to_stdout
-  end
+  describe 'ordering' do
 
-  it 'checkout should raise error if amount not matching total' do
-    subject.add_dish('chilli', 2.99)
-    subject.add_dish('lasagne', 4.5)
-    subject.order('chilli', 3)
-    subject.order('lasagne')
-    expect{ subject.checkout(10) }.to raise_error "You need to pay exactly 13.47!"
+    it 'order should display confirmation message' do
+      expect{ subject.order("chilli", 3) }.to output("3x chilli(s) added to your basket.\n").to_stdout
+    end
+
+    it 'order should add to basket' do
+      subject.order("chilli", 3)
+      expect(subject.basket).to include("chilli"=>3)
+    end
+
+    it 'should display total amount for order' do
+
+      expect(subject.total).to eq (2.99 * 3 + 4.5)
+    end
+
+    it 'checkout should confirm order' do
+      expect{ subject.checkout(13.47) }.to output("Your order is on its way!\n").to_stdout
+    end
+
+    it 'checkout should raise error if amount not matching total' do
+      expect{ subject.checkout(10) }.to raise_error "You need to pay exactly 13.47!"
+    end
+
   end
 
 end
