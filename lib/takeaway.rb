f50cf0677a61fb6d15b7ff32bcd524f7b6cd7ea7
hasulica <raduasandei@gmail.com>
2015-10-24 17:43:48 +0100

    Added checkout and total methods and tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,22 +3,35 @@ class Takeaway
   attr_reader :basket
 
   def initialize
-    @menu = []
-    @basket = []
+    @menu = {}
+    @basket = {}
     @total = 0
   end
 
   def add_dish(name, price)
-    @menu << {name => price}
+    @menu.store(name, price)
   end
 
   def show_menu
     @menu.to_s
   end
 
-  def order(name, quantity)
-    @basket << {name => quantity}
+  def order(name, quantity = 1)
+    @basket.store(name, quantity)
     puts "#{quantity}x #{name}(s) added to your basket."
+  end
+
+  def total
+    @total = 0
+    @basket.each { |name, quantity|
+      @total += @menu[name] * quantity
+    }
+    @total
+  end
+
+  def checkout(amount)
+    fail "You need to pay exactly #{total}!" unless amount == total
+    puts "Your order is on its way!"
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,7 +7,7 @@ describe Takeaway do
   it 'show_menu should display all items and prices' do
     subject.add_dish('chilli', 2.99)
     subject.add_dish('lasagne', 4.5)
-    expect(subject.show_menu).to eq "[{\"chilli\"=>2.99}, {\"lasagne\"=>4.5}]"
+    expect(subject.show_menu).to eq "{\"chilli\"=>2.99, \"lasagne\"=>4.5}"
   end
 
   it 'order should display confirmation message' do
@@ -18,4 +18,29 @@ describe Takeaway do
     subject.order("chilli", 3)
     expect(subject.basket).to include("chilli"=>3)
   end
+
+  it 'should display total amount for order' do
+    subject.add_dish('chilli', 2.99)
+    subject.add_dish('lasagne', 4.5)
+    subject.order('chilli', 3)
+    subject.order('lasagne')
+    expect(subject.total).to eq (2.99 * 3 + 4.5)
+  end
+
+  it 'checkout should confirm order' do
+    subject.add_dish('chilli', 2.99)
+    subject.add_dish('lasagne', 4.5)
+    subject.order('chilli', 3)
+    subject.order('lasagne')
+    expect{ subject.checkout(13.47) }.to output("Your order is on its way!\n").to_stdout
+  end
+
+  it 'checkout should raise error if amount not matching total' do
+    subject.add_dish('chilli', 2.99)
+    subject.add_dish('lasagne', 4.5)
+    subject.order('chilli', 3)
+    subject.order('lasagne')
+    expect{ subject.checkout(10) }.to raise_error "You need to pay exactly 13.47!"
+  end
+
 end
