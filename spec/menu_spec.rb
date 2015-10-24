69b8ff33cc2ee2703f66cbbc225378702206732a
mahmudh <mahmudhssn246@gmail.com>
2015-10-24 17:35:52 +0100

    added menu.rb and failed read menu test

---
+++ spec/menu_spec.rb
@@ -0,0 +1,13 @@
+require './docs/menu'
+
+describe Menu do
+
+  it 'read menu' do
+    menu = {:fish_burger => 2.0,
+            :beef_burger => 4.0,
+            :chicken_burger => 4.0,
+            :vege_burger => 2.5}
+
+    expect(subject.read_menu).to eq menu
+  end
+end
