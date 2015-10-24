6e3bfa60d8e5156e97dc5985451aa8ccb7d34e4c
George Maddocks <gwpmad@Georges-MacBook-Pro.local>
2015-10-24 14:00:30 +0100

    passed menu test

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,14 @@
 class Takeaway
+  attr_reader :menu_file
+
   def initialize
     @menu_file = 'menu-file.txt'
   end
 
   def menu
-    File.read(menu_file).each_line do |line|
+      File.read(menu_file).each_line do |line|
       puts line
     end
   end
 
-  private
-  attr_reader :menu_file
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,11 +3,14 @@ require 'takeaway'
 describe Takeaway do
 
   let(:takeaway) {described_class.new}
+  let(:menu_file) {double(:each_line=> nil)}
+
 
   describe 'viewing the list of dishes' do
     it 'displays the list of dishes' do
+      allow(File).to receive(:read).and_return(menu_file)
+      expect(File).to receive(:read)
       takeaway.menu
-      expect(takeaway.instance_variable_get(:@menu_file)).to have_received(:read)
     end
   end
 
