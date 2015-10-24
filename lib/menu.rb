047a63ea25a474f8e7e99df63618a100aad6f7ff
mahmudh <mahmudhssn246@gmail.com>
2015-10-24 17:47:50 +0100

    can read menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,14 @@
 class Menu
 
+  def initialize
+    @menu = {}
+  end
+
+  def read_menu
+    @menu = {fish_burger: 2.0,
+            beef_burger: 4.0,
+            chicken_burger: 4.0,
+            vege_burger: 2.5}
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,10 +3,10 @@ require './lib/menu'
 describe Menu do
 
   it 'read menu' do
-    menu = {:fish_burger => 2.0,
-            :beef_burger => 4.0,
-            :chicken_burger => 4.0,
-            :vege_burger => 2.5}
+    menu = {fish_burger: 2.0,
+            beef_burger: 4.0,
+            chicken_burger: 4.0,
+            vege_burger: 2.5}
 
     expect(subject.read_menu).to eq menu
   end
