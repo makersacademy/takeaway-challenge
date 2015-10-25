1044a2233609f7a9d578a593d06c01620927bbfa
ALRW <awerner1@googlemail.com>
2015-10-25 12:26:52 +0000

    Added ability to select menu items and add them to an order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,10 +1,24 @@
+require './lib/menu'
+require './lib/dish'
+
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :order
   def initialize
-      @menu = [{ name: "Hamburger", price: 1},{name: "Pizza", price: 2}]
+      @menu = [{ name: "Hamburger", price: 1},
+        {name: "Pizza", price: 2},
+        {name: "Hotdog", price: 2},
+        {name: "Tacos", price: 3},
+        {name: "Nachos", price: 1}
+      ]
+      @order = []
   end
 
-  def dishes
+  def list_dishes
     @menu.map{ |dish| p "#{dish[:name]}: £#{dish[:price]}"}*", "
+  end
+
+  def add(term)
+    @order << @menu.select{|dish| dish[:name] == term}
+    @order.flatten!
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,6 +2,12 @@ require 'takeaway'
 
 describe Takeaway do
   it 'lists available dishes and their prices' do
-    expect(subject.dishes).to eq("Hamburger: £1, Pizza: £2")
+    expect(subject.list_dishes).to eq("Hamburger: £1, Pizza: £2, Hotdog: £2, Tacos: £3, Nachos: £1")
   end
+  it 'can choose dishes for my order' do
+    subject.add "Pizza"
+    subject.add "Nachos"
+    expect(subject.order).to eq ([{name: "Pizza", price: 2}, {name: "Nachos", price: 1}])
+  end
+
 end
