55b885cff5f965e67a5bb0bf94d8061dacd517b2
thisdotrob <thisdotrob@gmail.com>
2015-10-25 14:44:47 +0000

    Add #add_to_order method and tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,18 +1,25 @@
 class Takeaway
 
+  attr_reader :order
+
   def initialize(dishes)
     @dishes = dishes
+    @order = []
   end
 
   def list_dishes
     list = ""
-    list << three_col_line('Num','Name','Price')
-    list << three_col_line('---','----','-----')
+    #list << three_col_line('Num','Name','Price')
+    #list << three_col_line('---','----','-----')
     @dishes.each_with_index do |dish, index|
       num, name, price = (index + 1).to_s, dish.name, dish.price.to_s
       list << three_col_line(num, name, price)
     end
     list
+  end
+
+  def add_to_order(dish_num)
+    @order << @dishes[dish_num]
   end
 
   private
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,13 +7,20 @@ describe Takeaway do
   let(:dish){ double('dish', name: name, price: price) }
 
   let(:menu_array){ [dish] }
-  let(:menu_string){ '1'.ljust(10) + name.ljust(20) + price.to_s.rjust(6) + "\n" }
+  let(:menu_string){ '1'.ljust(10) + name.ljust(10) + price.to_s.rjust(6) + "\n" }
 
   subject(:takeaway){ Takeaway.new(menu_array) }
 
   context '#list_dishes' do
     it 'lists the available dishes' do
       expect(takeaway.list_dishes).to eq menu_string
+    end
+  end
+
+  context '#add_to_order' do
+    it 'adds a dish to the order' do
+      takeaway.add_to_order(0)
+      expect(takeaway.order).to eq [dish]
     end
   end
 
