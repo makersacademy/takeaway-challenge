48d829111019f883550853eaaf6a138207bc27a3
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-10-25 14:18:38 +0000

    submit fuction complete

--- lib/submitorder.rb
+++ lib/submitorder.rb
@@ -2,13 +2,31 @@ require './lib/sendtext'
 
 class SubmitOrder
 
-  attr_reader :client
+  attr_reader :client, :total
   attr_accessor :order
 
   def initialize sendtext_klass, account_sid, auth_token
     @client = sendtext_klass.new account_sid,auth_token
-
   end
+
+  def get order,total
+    self.order = order
+    @total = total
+  end
+
+  def validated?
+    total == order.total
+  end
+
+  def submit mobilenumber
+    if validated?
+      client.send_message = "Thank you! Your order was placed and will be delivered before 18:52"
+      "Order OK and sent expect a text message confirmation"
+    else
+      "Order total incorrect"
+    end
+  end
+
 
 
 end
--- spec/submitorder_spec.rb
+++ spec/submitorder_spec.rb
@@ -7,15 +7,33 @@ describe SubmitOrder do
   let(:sendthing) {double(:sendthing)}
   subject {SubmitOrder.new sendthing, account_sid, auth_token}
   let(:order) {double(:order)}
+  let(:total1) {10}
+  let(:mobilenumber) {'+447914245451'}
+
 
   before(:each) do
     allow(sendthing).to receive(:new).with(account_sid,auth_token).and_return(client)
+    allow(client).to receive(:send_message=).with("Thank you! Your order was placed and will be delivered before 18:52").and_return("Message sent")
+    allow(order).to receive(:total).and_return(total1)
+    subject.get(order,total1)
   end
 
   context 'submitting an order' do
-    it '#get order' do
-      subject.order = order
+    it '#order' do
       expect(subject.order).to eq order
+    end
+    it '#get' do
+      expect(subject.total).to eq total1
+    end
+    it '#validated' do
+      expect(subject).to be_validated
+    end
+    it '#submit validated' do
+      expect(subject.submit mobilenumber).to eq "Order OK and sent expect a text message confirmation"
+    end
+    it '#submit not validated' do
+      allow(order).to receive(:total).and_return(total1+1)
+      expect(subject.submit mobilenumber ).to eq "Order total incorrect"
     end
   end
 
