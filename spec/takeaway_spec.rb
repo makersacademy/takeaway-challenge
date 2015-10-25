9235b3120f93bd19a1413cfb836625e1f3f58b08
samover <samuel.overloop@gmail.com>
2015-10-25 13:48:06 +0000

    refactored takeaway

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -14,43 +14,56 @@ describe Takeaway do
   end
 
   describe '#order' do
+    context 'item not available' do
+      it 'raises an error' do
+        msg = 'This item is not on the menu'
+        expect {takeaway.order('Hamburger', 25)}.to raise_error msg
+      end
+    end
     context 'first order' do
       it 'stores name and quantity of dish in basket' do
         takeaway.order(:menuitem1, 2)
-        expect(takeaway.basket).to include [:menuitem1, 2]
+        expect(takeaway.basket).to include({menuitem1: 2})
       end
     end
     context 'additional orders' do
       it 'stores name and quantity of dish in basket' do
         takeaway.order(:menuitem1, 2)
-        takeaway.order(:menuitem2, 5)
-        expect(takeaway.basket).to include [:menuitem2, 5]
+        takeaway.order(:menuitem1, 2)
+        expect(takeaway.basket).to include({menuitem1: 4})
       end
     end
   end
 
-  describe '#complete_order' do
+  describe '#checkout' do
     context 'when total given by user corresponds to calculated total' do
       it 'sends a confirmation text message' do
         takeaway.order :menuitem1, 2
-        expect(takeaway).to receive(:send_text).with('Thank you for your order: £2')
-        takeaway.complete_order(2)
+        expect(takeaway).to receive(:send_text).with("menuitem1 x2: £2", "Total price: £2")
+        takeaway.checkout(2)
       end
     end
     context 'total given by user does not correspond to calculated total' do
       it 'raises an error' do
         takeaway.order( :menuitem2,  5)
-        expect {takeaway.complete_order(7)}.to raise_error 'Wrong total'
+        expect {takeaway.checkout(7)}.to raise_error 'Wrong total'
       end
     end
   end
 
-  describe '#basket_summary' do
-    it 'shows the ordered items and the total' do
-      result = "menuitem1 x2: £2\nmenuitem2 x5: £7.5"
-      takeaway.order :menuitem1, 2
-      takeaway.order :menuitem2, 5
-      expect(takeaway.basket_content).to eq result
+  describe '#basket_content' do
+    context 'empty' do
+      it 'raises an error' do
+        expect {takeaway.basket_content}.to raise_error 'Basket is empty'
+      end
+    end
+    context 'has items' do
+      it 'shows items and the total' do
+        result = "menuitem1 x2: £2\nmenuitem2 x5: £7.5"
+        takeaway.order :menuitem1, 2
+        takeaway.order :menuitem2, 5
+        expect(takeaway.basket_content).to eq result
+      end
     end
   end
 
@@ -59,6 +72,14 @@ describe Takeaway do
       takeaway.order :menuitem1, 2
       takeaway.order :menuitem2, 5
       expect(takeaway.total).to eq "Total price: £9.5"
+    end
+  end
+
+  describe '#reset_order' do
+    it 'after checkout basket is empty' do
+      takeaway.order :menuitem1, 2
+      takeaway.reset_order
+      expect {takeaway.basket_content}.to raise_error 'Basket is empty'
     end
   end
 end
