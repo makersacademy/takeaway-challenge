6d88d84e57267b83b82d0ca90acaae942091bb99
Hamza Sheikh <hsheikhm@gmail.com>
2015-10-25 11:39:13 +0000

    added another test for sms messenger

--- spec/textmessenger_spec.rb
+++ spec/textmessenger_spec.rb
@@ -4,6 +4,12 @@ describe TextMessenger do
 
   context "#send_text" do
 
+    it "notifies user that they will receive an SMS" do
+      message = "You will shortly receive an SMS confirming your order."
+      allow(described_class).to receive(:send_text) { message }
+      expect(described_class.send_text).to eq message
+    end
+
     it "sends a confirmation text" do
       expect(described_class).to respond_to(:send_text)
     end
