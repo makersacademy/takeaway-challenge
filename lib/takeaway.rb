9e46b200e77f154b85b5f26966ded878edc0558f
thisdotrob <thisdotrob@gmail.com>
2015-10-25 14:23:45 +0000

    Replace feature_spec tests with interface

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,10 +6,11 @@ class Takeaway
 
   def list_dishes
     list = ""
+    list << three_col_line('Num','Name','Price')
+    list << three_col_line('---','----','-----')
     @dishes.each_with_index do |dish, index|
       num, name, price = (index + 1).to_s, dish.name, dish.price.to_s
-      line = three_col_line(num, name, price)
-      list << line
+      list << three_col_line(num, name, price)
     end
     list
   end
@@ -17,8 +18,7 @@ class Takeaway
   private
 
   def three_col_line(str1, str2, str3)
-    str1.ljust(10) + str2.ljust(20) + str3.rjust(6) + "\n"
+    str1.ljust(10) + str2.ljust(10) + str3.rjust(6) + "\n"
   end
-
 
 end
