dbb5ce1565b8505b4b1a6c837a5d28575741a9b8
octavianrotari <octavianrotari@gmail.com>
2015-10-25 12:25:51 +0000

    bug same dish

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,20 +2,34 @@ require_relative "menu"
 
 class Takeaway
 
-  attr_reader :selected_dishes
+  attr_reader :selected_dishes, :total
 
   def initialize (menu = Menu.new)
     @menu = menu
     @selected_dishes = []
+    @total = 0
   end
 
   def menu_restaurant
     @menu.dishes
   end
 
-  def select_dishes(dish)
-  @selected_dishes <<  @menu.dishes.select {
-      |key, value| key.to_s.match(dish)
-    }
+  def select_dishes(dish, quantity = 1 )
+    total_order(dish, quantity)
+    @selected_dishes.each do |e|
+       if e.has_key?(dish)
+         e[dish] += quantity
+       else
+         @select_dishes  << e[dish] = quantity 
+       end
+     end
   end
+
+  def total_order(dish, quantity)
+    selected = @menu.dishes.select {
+      |key| key.to_s.match(dish)
+    }
+    @total += selected[dish] * quantity
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,15 +4,22 @@ describe Takeaway do
 
   let(:dishes){ {'hamburger' => 2} }
   let(:dish){'hamburger'}
+  let(:sel_dish){{'hamburger' => 4}}
+  let(:total_cost){8}
 
 
   it "expect takeaway to print the menu" do
     expect(subject.menu_restaurant).to eq(dishes)
   end
 
-  it "expect to select the dishes from the menu" do
-    subject.select_dishes(dish)
-    expect(subject.selected_dishes).to eq([dishes])
+  it "expect to select more quantity of the same item" do
+    subject.select_dishes(dish, 4)
+    expect(subject.selected_dishes).to eq([sel_dish])
+  end
+
+  it "expect to calculate the total of the ordered dishes" do
+    subject.select_dishes(dish, 4)
+    expect(subject.total).to eq(total_cost)
   end
 
 end
