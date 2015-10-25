4a3026c5ed5bdf44d4bd7c992083018751cdeffb
mahmudh <mahmudhssn246@gmail.com>
2015-10-25 16:40:43 +0000

    user can check basket or purchases and check total sum of purchase

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,16 @@
 class Menu
 
-  attr_reader :menu
+  attr_reader :dish
 
   def initialize
-    @menu = {fish_burger: 2.0,
+    @dish = {fish_burger: 2.0,
             beef_burger: 4.0,
             chicken_burger: 4.0,
             vege_burger: 2.5}
   end
 
   def read_menu
-    @menu.each { |item, price| "#{item}: £#{price}, " }
+    @dish.each { |item, price| "#{item}: £#{price}, " }
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,12 +1,35 @@
 require './lib/order'
 
 describe Order do
-  let(:menu) {double(:journey, hash = [:beef_burger, 4.0])}
-  let(:menu_klass){double(:menu_klass, :new => :menu)}
+  let(:menu) { double(:menu, dish: {:beef_burger => 4.0, :fish_burger => 2.0}) }
+  let(:menu_klass){ double(:menu_klass, :new => :menu) }
+  let(:item) {:beef_burger}
+  let(:item2) {:fish_burger}
+  let(:qty) {2}
 
   it 'select some number of several available dishes from the menu' do
     hash = {}
-    hash[:beef_burger] = 2
-    expect(subject.add_order(:beef_burger, 2)).to eq hash
+    hash[item] = qty
+    expect(subject.add_order(item, qty)).to eq hash
+  end
+
+  describe '#basket_sum' do
+    it 'should raise error if basket is empty' do
+      expect{subject.basket_sum}.to raise_error('Basket is empty')
+    end
+
+    it 'should return basket summary' do
+      subject.add_order(item, qty)
+      message = "#{item} x #{qty} = £#{(menu.dish[item]*qty)}"
+      expect(subject.basket_sum).to eq message
+    end
+  end
+
+  it 'should ouput the total sum of the order' do
+    total = 12.0
+    message =   "Your total is: #{total}"
+    subject.add_order(item, qty)
+    subject.add_order(item2, qty)
+    expect(subject.total_sum).to eq message
   end
 end
