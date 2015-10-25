eca990141979eb9a0b5c5cd4ddf8c3ce9a8febf3
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-25 10:52:11 +0000

    Changed test and code to now display a list of dishes and their prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,13 +1,24 @@
 class Menu
 
-attr_accessor :list
 
-  def initialize
-    @list = []
+
+ def dishes
+   {  :Burger => 5.00,
+      :Fries => 2.00,
+      :Pizza => 8.00,
+      :Wings => 3.00
+  }
   end
 
-  def add_item(item, price)
-    @list << {item => '%.02f' % price}
-  end
+
+# attr_accessor :list
+#
+#   def initialize
+#     @list = []
+#   end
+#
+#   def add_item(item, price)
+#     @list << {item => '%.02f' % price}
+#   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,10 +1,24 @@
 require 'menu'
 
 describe Menu do
-  it 'should hold a list of dishes with prices' do
-    menu = Menu.new
-    menu.add_item('fries', 3.0)
-    expect(menu.list).to include ({'fries' => '%.02f' % 3.0})
-  end
+
+  let (:menu) { Menu.new }
+
+    it 'Menu should have a list of dishes' do
+      dishes = {  :Burger => 5.00,
+                  :Fries => 2.00,
+                  :Pizza => 8.00,
+                  :Wings => 3.00
+        }
+        expect(menu.dishes).to eq dishes
+    end
+
+
+
+  # it 'should hold a list of dishes with prices' do
+  #   menu = Menu.new
+  #   menu.add_item('fries', 3.0)
+  #   expect(menu.list).to include ({'fries' => '%.02f' % 3.0})
+  # end
 
 end
