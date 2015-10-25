77f12c878e317c67ff2c7518172a42b9fdd37a35
thisdotrob <thisdotrob@gmail.com>
2015-10-25 12:21:06 +0000

    Refactor to private #three_col_line method in Takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -8,10 +8,17 @@ class Takeaway
     list = ""
     @dishes.each_with_index do |dish, index|
       num, name, price = (index + 1).to_s, dish.name, dish.price.to_s
-      line = num.ljust(10) + name.ljust(20) + price.rjust(6) + "\n"
+      line = three_col_line(num, name, price)
       list << line
     end
     list
   end
+
+  private
+
+  def three_col_line(str1, str2, str3)
+    str1.ljust(10) + str2.ljust(20) + str3.rjust(6) + "\n"
+  end
+
 
 end
