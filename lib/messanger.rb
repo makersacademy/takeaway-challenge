ee8fc1e33c2b80530d75d3f948b95e718a4bdd34
Yev Dyko <evgeny.dyko@gmail.com>
2015-10-26 11:51:29 +0300

    Added a method #send_text to the class Messenger

--- lib/messanger.rb
+++ lib/messanger.rb
@@ -1,3 +1,16 @@
-class Messenger
+require 'twilio-ruby'
 
+class Messenger
+  account_sid = "PN2ba53d6e4525039c3c3c17ebb2bc2a15"
+  auth_token = "d8bfb8790ec316cd64d6d254a4bcc6c6"
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+  @time = (Time.now + (60*60)).strftime("%H:%M")
+
+  def send_text
+    @client.account.messages.create(
+        to: "+375293069300",
+      from: "+43676800505017",
+      body: "Thank you! Your order was placed and will be delivered before #{@time}.")
+  end
 end
--- spec/messenger_spec.rb
+++ spec/messenger_spec.rb
@@ -1,5 +1,13 @@
-require 'messenger'
+require 'textmessenger'
 
 describe Messenger do
 
+  context "#send_text" do
+
+    it 'sends a payment confirmation text message' do
+      allow(subject).to receive(:send_text)
+      expect(subject).to receive(:send_text).with("Thank you for your order: £27.50")
+      takeaway.checkout(27.50)
+    end
+  end
 end
