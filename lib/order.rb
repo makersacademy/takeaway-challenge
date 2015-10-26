7561cf9465c38775a8ae038596d1b4a21fd11dc0
Yev Dyko <evgeny.dyko@gmail.com>
2015-10-26 11:58:19 +0300

    Updated the class Order

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,20 @@
 class Order
+  attr_reader :basket, :total
 
+  def initialize
+    @basket = Hash.new(0)
+  end
+
+  def add_basket(item, quantity)
+    basket[item] += quantity
+  end
+
+  def basket_summary(menu)
+    basket.map { |item, quantity| "#{item} x#{quantity} = £#{sprintf('%.2f', menu.dishes[item] * quantity)}" }.join(', ')
+  end
+
+  def total(menu)
+    @total = order.basket.map { |item, quantity| menu.dishes[item] * quantity }.inject(:+)
+    "Total: £#{sprintf('%.2f', @total)}"
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,37 @@
 require 'order'
 
 describe Order do
+  subject(:order) { described_class.new }
+  let(:item) { 'kimchi' }
+  let(:quantity) { 2 }
+  let(:menu) { double(:menu, dishes: { "kimchi" => 2 }) }
 
+
+  context '#initialize' do
+    it 'has an empty basket' do
+      expect(order.basket).to be_empty
+    end
+  end
+
+  context '#add_basket' do
+    it 'adds items to basket' do
+      expect(order.add_basket(item, quantity)).to change{order.basket.length}.by(1)
+    end
+  end
+
+  context '#basket_summary' do
+    it 'creates an basket summary' do
+      order.add_basket('kimchi', 2)
+      order.add_basket('salmon maki', 3)
+      expect(order.basket_summary).to eq("kimchi x2 = £6.00, salmon maki x3 = £16.50")
+    end
+  end
+
+  context '#total' do
+    it 'shows the total price' do
+      order.add_basket('kimchi', 2)
+      order.add_basket('salmon maki', 3)
+      expect(order.total).to eq("Total: £22.50")
+    end
+  end
 end
