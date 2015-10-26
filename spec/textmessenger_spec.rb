d2862c5eaa175786aeeebd81aeed43c9baf023df
Hamza Sheikh <hsheikhm@gmail.com>
2015-10-26 13:19:45 +0000

    removed a pointless test

--- spec/textmessenger_spec.rb
+++ spec/textmessenger_spec.rb
@@ -6,12 +6,6 @@ describe TextMessenger do
 
   context "#send_text" do
 
-    it "notifies user that they will receive an SMS" do
-      message = "You will shortly receive an SMS confirming your order."
-      allow(subject).to receive(:send_text) { message }
-      expect(subject.send_text).to eq message
-    end
-
     it "sends a confirmation text" do
       allow(client).to receive(:send_text)
       time = (Time.now + (60*60)).strftime("%H:%M")
