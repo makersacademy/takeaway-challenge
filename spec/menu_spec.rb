59719440d93c37e78c620b9636e4e2c6db07efda
mongolianprincess <ayc.yang@gmail.com>
2015-10-26 01:00:59 +0000

    Takeaway Challenge - second commit

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -11,9 +11,5 @@ describe Menu do
     it 'has a default menu of dishes with prices' do
       expect(subject.dishes[:Coke]).to eq (5)
     end
-
-    it "has 5 dishes on default menu" do
-      expect(subject.menu.dishes.length).to eq 5
-    end
   end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -10,5 +10,8 @@ describe Restaurant do
     it "is initialized with a default menu" do
       expect(subject.menu).to eq menu
     end
+    it "has 5 dishes on default menu" do
+      expect(subject.menu.dishes.length).to eq 5
+    end
   end
 end
