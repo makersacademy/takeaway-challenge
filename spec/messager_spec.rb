dcb703eed48727e11abfd4872e8675be033fd24d
samover <samuel.overloop@gmail.com>
2015-10-27 08:06:09 +0000

    added further tests for messager

--- spec/messager_spec.rb
+++ spec/messager_spec.rb
@@ -20,4 +20,18 @@ describe Messager do
     allow(messager).to receive(:compose_message).with(order).and_return(order)
     messager.send_text(order)
   end
+  
+  it 'composes a message' do
+    msg = "Your order has been confirmed! \
+    Expect your yummie food by #{messager.delivery_time} \
+    \n#{order}"
+
+    expect(messager.compose_message order).to eq msg
+  end
+
+  it 'retuns a delivery time 1 hour from now' do
+    @time_now = Time.parse("Oct 25 20015 10:00AM")
+    allow(Time).to receive(:now).and_return(@time_now)
+    expect(messager.delivery_time).to eq ('11:00')
+  end
 end
