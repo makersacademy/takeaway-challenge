9bec001caa5ec2ddef89b0f82209b91a9b244a88
octavianrotari <octavianrotari@gmail.com>
2015-11-10 23:53:27 +0000

    (WIP) has a menu class which accepts a hash with all the dishes and at the same when calling the method print it transforms the hash into a string

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,20 @@ require 'menu'
 
 describe Menu do
 
+  subject(:menu){ described_class.new(dishes) }
+  let(:dishes) do
+    {
+      chicken: 3.99,
+      falafel: 4.00
+    }
+  end
+
+  it "has a list of dishes and prices" do
+    expect(menu.dishes).to eq(dishes)
+  end
+
+  it 'prints a list off dishes with prices'  do
+    printed_menu = 'Chicken £3.99, Falafel £4.00'
+    expect(menu.print).to eq(printed_menu)
+  end
 end
