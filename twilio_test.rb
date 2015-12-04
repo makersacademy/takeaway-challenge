1c934d851e79a179dcea341513474eee676e5f72
Tony Young <ty_training@hotmail.co.uk>
2015-12-04 22:22:47 +0000

    Added Twilio_ test.rb

---
+++ twilio_test.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'sinatra'
+
+get '/' do
+  'Hello Mudder Fudgers! Currently running version ' + Twilio::VERSION + \
+      ' of the twilio-ruby library'
+end
