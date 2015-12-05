a4f9536b3bd8a25440abaf4e691c45f464c9e2e6
Chris Wynne <chris.wynndow@gmail.com>
2015-12-05 13:33:37 +0000

    Refactors user story 1

--- spec/feature_tests/user_stories_spec.rb
+++ spec/feature_tests/user_stories_spec.rb
@@ -3,15 +3,16 @@ require 'takeaway'
 describe "User stories" do
 
 let(:takeaway) {Takeaway.new}
+let(:menu) {"starter : 5\nmains : 10\ndesert : 3"}
+
   # As a customer
   # So that I can check if I want to order something
   # I would like to see a list of dishes with prices
   describe 'User Story 1' do
 
     it 'allows me to see a list of dishes with prices' do
-      expect{takeaway.show_dishes}.to output("starter : 5\nmains : 10\ndesert : 3").to_stdout
+      expect{takeaway.show_dishes}.to output(menu).to_stdout
     end
-#expect { my_method }.to output("my message").to_stdout
 
   end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,13 +2,14 @@ require 'takeaway'
 
 describe Takeaway do
   subject(:takeaway) {described_class.new}
+  let(:menu) {"starter : 5\nmains : 10\ndesert : 3"}
 
   describe 'looking at a list of dishes with prices' do
 
     it { is_expected.to respond_to(:show_dishes) }
 
     it 'returns a list of dishes with price' do
-    expect{takeaway.show_dishes}.to output("starter : 5\nmains : 10\ndesert : 3").to_stdout
+    expect{takeaway.show_dishes}.to output(menu).to_stdout
     end
 
   end
