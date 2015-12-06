1666e01b517cafcecf0564a9ffce509e01e8497a
Andrew Burnie <apburnie@hotmail.co.uk>
2015-12-06 16:31:31 +0000

    Refactoring

--- lib/order_list.rb
+++ lib/order_list.rb
@@ -12,33 +12,30 @@ class OrderList
   end
 
   def place_order(total=nil,amount_list)
-  fail 'An ordered dish is unavailable' unless ordered_dishes_available?(amount_list)
-  fail 'Total does not match sum of dishes in order' unless total == nil || total == dish_total(amount_list)
+    fail 'An ordered dish is unavailable' unless ordered_dishes_available?(amount_list)
+    fail 'Total does not match sum of dishes in order' unless total == nil || total == dish_sum(amount_list)
   end
 
   private
-
-  def available_dish_names
-    @dish_list.dishes.map{|dish| dish.name}
-  end
 
   def ordered_dishes_available?(amount_list)
     amount_list.keys.all?{|(name)| available_dish_names.include? name }
   end
 
-  def dish_total(amount_list)
-    total_payment = each_dish_payment(amount_list).inject(0){|sum, dish| sum += dish[1] }
+  def available_dish_names
+    @dish_list.dishes.map{|dish| dish.name}
   end
 
-  def ordered_price_list(amount_list)
-    price_list.delete_if{|key, value| amount_list[key] == nil}
+  def dish_sum(amount_list)
+    each_dish_payment(amount_list).inject(0){|sum, dish| sum += dish[1] }
   end
 
   def each_dish_payment(amount_list)
     ordered_price_list(amount_list).merge(amount_list){|key, price, quantity| price * quantity}
   end
 
-
-
+  def ordered_price_list(amount_list)
+    price_list.delete_if{|key, value| amount_list[key] == nil}
+  end
 
 end
