6cbfb012d630c7caffee38f06c45669f14b2cdf1
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-06 19:58:49 +0000

    menu and restaurant classes updated

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,20 +2,10 @@ class Menu
   attr_reader :list
 
   def initialize
-    @list = [
-      {"Chips" => 3},
-      {"Salad" => 4},
-      {"Chicken" => 7},
-      {"Fish" => 9},
-      {"Daily special" => 15}
-    ]
+    @list = {"Chips" => 3, "Salad" => 4, "Chicken" => 7, "Fish" => 9, "Daily special" => 15}
   end
 
   def view
-    list.each do |hash|
-      hash.each do |food, price|
-        puts food.ljust(20) + ("£"+price.to_s).rjust(20)
-      end
-    end
+    list
   end
 end
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,7 +1,7 @@
 require_relative 'menu'
 
 class Restaurant
-  attr_reader :menu, :customer_order
+  attr_reader :menu, :customer_order, :actual_total
 
   def initialize(menu: Menu.new)
     @menu = menu
@@ -16,10 +16,12 @@ class Restaurant
     @customer_order = []
     ordering_instructions
     take_order
+    fail "Your order is incorrect" unless customer_total_correct?
     customer_order
   end
 
   private
+  attr_reader :customer_total
 
   def ordering_instructions
     puts "Please enter your order in this format- "
@@ -35,5 +37,25 @@ class Restaurant
       customer_order << answer
       break if customer_order[-1][0].match(/total/i)
     end
+  end
+
+  def customer_total_correct?
+    calculate_customer_total
+    calculate_actual_total
+    customer_total == actual_total
+  end
+
+  def calculate_customer_total
+    @customer_total = customer_order[-1][-1].to_i
+  end
+
+  def calculate_actual_total
+    @actual_total = 0
+    customer_order.each do |item|
+      if menu.list.has_key?(item[0])
+        @actual_total += ((item[1].to_i) * menu.list[item[0]])
+      end
+    end
+    actual_total
   end
 end
