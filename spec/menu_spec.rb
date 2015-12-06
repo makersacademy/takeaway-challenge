b75cc8e916ea8d592d6a99f17b739f7241742fb6
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-06 21:51:17 +0000

    Menu spec updaed

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,13 +5,7 @@ describe Menu do
 
   describe '#view' do
     it 'shows the list of dishes with prices' do
-      expect(menu.view).to eq [
-        {"Chips" => 3},
-        {"Salad" => 4},
-        {"Chicken" => 7},
-        {"Fish" => 9},
-        {"Daily special" => 15}
-      ]
+      expect(menu.view).to eq menu.list
     end
   end
 end
