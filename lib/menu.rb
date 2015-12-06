5a12d96d067b1af76dfb7734a82d662bec087b89
michaellennox <michaelctlennox@gmail.com>
2015-12-06 13:49:57 +0000

    Added failing test for adding items to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,13 @@
 class Menu
 
+  attr_reader :list
+
+  def initialize
+    @list = {}
+  end
+
+  def add_item(item, price)
+
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,7 +4,12 @@ describe Menu do
   subject(:menu) { described_class.new }
 
   it 'should initialize with an empty menu_list' do
-    expect(menu.menu_list).to be empty
+    expect(menu.list).to be_empty
+  end
+
+  it 'should be possible to add items to the menu' do
+    menu.add_item(:shrimp, 1.88)
+    expect(menu.list).to include(shrimp: 1.88)
   end
 
 
