fb1c221a35c689ddb697afba3c13fc1aa74a33b7
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-12-06 18:44:02 +0000

    Us3 - can verify that bill matches expected bill

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,5 +1,5 @@
 class Takeaway
-attr_reader :menu, :current_order
+attr_reader :menu, :order
 
   def initialize
     @menu = { pizza: 6,
@@ -7,10 +7,22 @@ attr_reader :menu, :current_order
               curry: 5,
               chips: 3,
               cake: 4 }
-    @current_order = []
+    @order = []
+    @bill = 0
   end
 
   def add_to_order(dish, quantity)
-    @current_order << [ dish, quantity ]
+    @order << [ dish, quantity ]
+  end
+
+  def bill_calc
+    @order.each { |x| @bill += x[1] * menu[x[0].to_sym] }
+    @bill
+  end
+
+  def place_order(expected_bill)
+    bill_calc
+    fail 'bill and expected bill are different' if @bill != expected_bill
+    'order successfully placed'
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,29 +1,51 @@
 require 'takeaway'
 
 describe Takeaway do
-  subject(:takeway) { described_class.new }
-  let(:pizza) { double :pizza }
+  subject(:takeaway) { described_class.new }
 
   describe '#menu' do
     it 'can show a list of dishes with prices' do
-      expect(takeway.menu).to eq(pizza: 6,
-                              hamburger: 7,
-                              curry: 5,
-                              chips: 3,
-                              cake: 4)
+      expect(takeaway.menu).to eq( pizza: 6,
+                                  hamburger: 7,
+                                  curry: 5,
+                                  chips: 3,
+                                  cake: 4 )
     end
   end
 
   describe '#current_order' do
     it 'is empty by default' do
-      expect(takeway.current_order).to be_empty
+      expect(takeaway.order).to be_empty
     end
   end
 
   describe '#add_to_order(dish, quantity)' do
     it 'can add a quantity of a dish to the current order' do
-      takeway.add_to_order(pizza, 1)
-      expect(takeway.current_order).to include [ pizza, 1 ]
+      takeaway.add_to_order('pizza', 1)
+      expect(takeaway.order).to include [ 'pizza', 1 ]
+    end
+  end
+
+  describe '#bill_calc' do
+    it 'returns the order\'s bill' do
+      takeaway.add_to_order('pizza', 2)
+      takeaway.add_to_order('chips', 1)
+      expect(takeaway.bill_calc).to eq(15)
+    end
+  end
+
+  describe '#place_order(expected_bill)' do
+    it 'raises an error if bill and expected bill are different' do
+      different = 'bill and expected bill are different'
+      takeaway.add_to_order('pizza', 2)
+      takeaway.add_to_order('chips', 1)
+      expect{ takeaway.place_order(10) }.to raise_error different
+    end
+
+    it 'confirms that the order was placed successfully' do
+      takeaway.add_to_order('pizza', 2)
+      takeaway.add_to_order('chips', 1)
+      expect(takeaway.place_order(15)).to eq 'order successfully placed'
     end
   end
 end
