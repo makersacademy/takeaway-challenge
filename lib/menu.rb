8f46afc6881ebad8cfeec137b7abc2ada19beea3
Thomas Bradley <bradley.tom.r@gmail.com>
2015-12-06 20:12:00 +0000

    User story 1 test changed

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,7 +3,9 @@ class Menu
   attr_reader :contents
 
   def initialize
-    @contents = 'pizza'
+    @contents = { 'Dish 1' => 5.00,
+                  'Dish 2' => 6.00,
+                  'Dish 3' => 7.00 }
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,10 +2,11 @@ require 'takeaway'
 
 describe Takeaway do
   subject(:takeaway) { described_class.new }
-  let(:menu) { double(:menu, contents: contents) }
+  let(:menu) { double(:menu, contents: dishes) }
+  let(:dishes) { {'Dish 1' => 5.00,'Dish 2' => 6.00,'Dish 3' => 7.00} }
   describe '#read_menu' do
     it 'lists the items on the menu' do
-      expect(takeaway.read_menu).to include(menu.contents)
+      expect(takeaway.read_menu).to include dishes
     end
   end
 end
