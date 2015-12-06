1613b3f39f6f747e0c360092e58036162a02aff4
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-06 22:44:25 +0000

    spec file refactoring

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,16 +1,21 @@
 require 'restaurant'
 
 describe Restaurant do
-  subject(:restaurant) { described_class.new(menu: menu) }
+  subject(:restaurant) { described_class.new(menu: menu, msg: msg) }
+  let(:msg) { double :msg }
   let(:menu) { double :menu, view: list }
-  let(:list) { [
-    {"Chips" => 3},
-    {"Salad" => 4},
-    {"Chicken" => 7},
-    {"Fish" => 9},
-    {"Chef's special" => 15}
-  ] }
-#we have a real class, restaurant, with an injected fake class, menu. When our fake menu gets the command 'view' it just returns the list
+  let(:list) { {"Chips" => 3, "Salad" => 4, "Chicken" => 7, "Fish" => 9, "Daily special" => 15} }
+
+  describe 'defaults' do
+    it 'the customer_order does not exist' do
+      expect(restaurant.customer_order).to be nil
+    end
+
+    it 'the actual_total does not exit' do
+      expect(restaurant.actual_total).to be nil
+    end
+  end
+
   describe '#request_menu' do
     it 'returns the list of dishes with prices' do
       expect(restaurant.request_menu).to eq list
