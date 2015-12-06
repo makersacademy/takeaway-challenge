ee63adefadf30569fde2d6bbfd70535def6dc9af
giamir <giamir.buoncristiani@gmail.com>
2015-12-06 16:25:22 +0000

    add menu method to show dishes to TakeAway class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,14 @@
+require_relative 'menu'
+
 class TakeAway
+
+  attr_reader :menu
+
+  def initialize(menu_klass = Menu.new)
+    self.menu = menu_klass.new
+  end
+
+  private
+
+  attr_writer :menu
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,13 @@
 require 'takeaway'
 
 describe TakeAway do
-  let(:take_away) { described_class.new }
+  let(:menu) { double :menu }
+  let(:menu_klass) { double :menu_klass, new: menu}
+  let(:takeaway) { described_class.new(menu_klass) }
+
+  describe '#menu' do
+    it 'has to show dishes available in menu' do
+      expect(takeaway.menu).to eq menu
+    end
+  end
 end
