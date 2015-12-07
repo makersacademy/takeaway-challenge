cadf5a2853477e28d2032d1346616fe5293fa03c
tmgree <tmgree@outlook.com>
2015-12-07 16:18:03 +0000

    gfgfgfgf

--- lib/order.rb
+++ lib/order.rb
@@ -1,13 +1,24 @@
 require_relative 'menu'
 
 class Order
+  attr_reader :current_order, :current_total
+  def initialize
+   @current_order=[]
+   @current_total=0
+  end
   def add_dish(menu, dish, quantity)
     if menu.dish_list.include?(dish)
+      add_money(menu, dish, quantity)
+      current_order << {dish => quantity}
     "You have ordered #{quantity} #{dish}"
   else
-    raise "Error you must select an item on the menu"
+    raise 'Error you must select an item on the menu'
     end
 
+  end
+
+  def add_money(menu, dish, quantity)
+    @current_total = @current_total + (menu.dish_list[dish] * quantity)
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,7 +2,7 @@ require 'order'
 
 describe Order do
   subject(:order) { described_class.new }
-  let(:menu) {double :menu, dish_list: {chicken: 3}}
+  let(:menu) {double :menu, dish_list: {pizza: 5, chicken: 3}}
 
   it 'must allow you to select dishes to order' do
     expect(order.add_dish(menu, :chicken, 3)).to eq "You have ordered 3 chicken"
@@ -13,10 +13,15 @@ describe Order do
   end
 
   it 'must fail if item ordered is not on the menu' do
-    expect(order.add_dish(menu, :pork, 2)).to raise_error("Error you must select an item on the menu")
+    expect{order.add_dish(menu, :pork, 2)}.to raise_error("Error you must select an item on the menu")
   end
 
-
+  it 'must check that the total given is correct' do
+    order.add_dish(menu, :chicken, 2)
+    order.add_dish(menu, :pizza, 2)
+    #order.current_order = [chicken: 2, pizza: 3]
+    expect(order.current_total).to be 16
+  end
 
 
 end
