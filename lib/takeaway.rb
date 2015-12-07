4e224734e6a4a76b89d0d3994c17735949f7d77e
Jelgar <jonathanelgardiner@gmail.com>
2015-12-07 10:40:26 +0000

    text working

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,19 @@ require_relative 'menu'
 require_relative 'order'
 require 'rubygems'
 require 'twilio-ruby'
+require 'dotenv'
 
 class Takeaway
 
-  attr_reader :menu, :order, :order_time
+  attr_reader :menu, :order, :order_time, :client_number, :twilio_number
+
+  Dotenv.load
 
   def initialize
     @menu = Menu.new
     @order = Order.new
+    @twilio_number = ENV["TWILIO_NUMBER"]
+    @client_number = ENV["CLIENT_NUMBER"]
   end
 
   def show_menu
@@ -28,7 +33,7 @@ class Takeaway
   def checkout_order(price = 0)
     order.checkout(price)
     @order_time = Time.now + 3600
-    send_sms(number)
+    send_sms(client_number)
   end
 
   def reset_order
@@ -41,17 +46,18 @@ class Takeaway
 
   private
 
-  TWILIO_NUMBER = '+44 1442 796246'
+  def send_sms(client_number)
+  # account_sid = 'AC6c3b2f378924f8f5ff1587863865e8a6'
+  # auth_token = '518f76c2b5b9533333af831e1a98baef'
 
-  def send_sms(number)
-  account_sid = 'AC6c3b2f378924f8f5ff1587863865e8a6'
-  auth_token = '518f76c2b5b9533333af831e1a98baef'
+  account_sid = ENV["ACCOUNT_SID"]
+  auth_token = ENV["AUTH_TOKEN"]
 
   @client = Twilio::REST::Client.new account_sid, auth_token
 
   @client.account.messages.create({
-	:from => '+441442796246',
-	:to => 'number',
+	:from => twilio_number,
+	:to => client_number,
 	:body => "Thank you for your order!! Your order will be delivered at #{order_time}",
   })
   end
