baa175dbbb32447b2faf596d5f48c166ff8f515d
Henry Gordon <hwgordon247@gmail.com>
2016-01-29 16:45:05 +0000

    can now receive messages

--- lib/test.rb
+++ lib/test.rb
@@ -1,17 +1,36 @@
 require 'twilio-ruby'
+require 'rubygems'
 
 class Test
 
-  account_sid = 'ACa017c4d8a2611154a0f08f692e71ff37'
-  auth_token = '90e883e4f42336bcc377f4a14afb11ff'
+  @@account_sid = 'ACa017c4d8a2611154a0f08f692e71ff37'
+  @@auth_token = '90e883e4f42336bcc377f4a14afb11ff'
 
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  def initialize
+    @client = Twilio::REST::Client.new @@account_sid, @@auth_token
+    # @msg_sid = 'MM123'
+    @replies = []
+  end
 
-  @client.messages.create(
-  from: '+441922214133',
-  to: '+447780469088',
-  body: 'Hey there!'
-)
+  def text
+    @client.messages.create(
+    from: '+441922214133',
+    to: '+447780469088',
+    body: 'hey there'
+    )
+  end
 
+  def reply(choice)
+    @client.messages.list.each do |message|
+      @replies << message.body
+    end
+    p @replies[choice]
+  end
+
+
+  # def specific
+  #   @msg = @client.messages.get('MM123')
+  #   p @msg
+  # end
 
 end
