e4d7073e9bb4e0377b03e5deb2140c8ed06dc4b9
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-29 01:22:41 +0000

    Added funcitonality on restaurant class; massive stubbing on restaurant_spec

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,18 +5,25 @@ class Restaurant
   extend Forwardable
 
   def_delegator :@menu, :list_items, :menu
+  #def_delegator :@query_order, :order_details, :my_order
 
-  def initialize menu_klass=Menu
+  def initialize menu_klass=Menu, order_klass=Order
     @menu_klass = menu_klass
+    @order_klass = order_klass
     @menu = @menu_klass.new
+    @orders = []
   end
 
   def change_menu new_menu
     @menu = @menu_klass.new new_menu
   end
-  # should be class Menu functionality?
 
-  def new_order
+  def new_order order_details, customer
+    @orders << @order_klass.new(order_details, customer)
+  end
+
+  def orders
+    @orders.clone
   end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -3,17 +3,23 @@ require 'restaurant'
 describe Restaurant do
 
   before do
+    allow(order_klass).to receive(:new) {order}
+    allow(order).to receive(:order_details) {details}
     allow(menu_klass).to receive(:new) {menu}
     allow(menu2).to receive(:list_items) {items2}
     allow(menu).to receive(:list_items) {items}
   end
-  
+
+  let(:customer) {double :customer}
+  let(:order_klass) {double :order_klass}
+  let(:order) {double :order}
+  let(:details) {double :details}
   let(:menu_klass) {double :menu_klass}
   let(:menu) {double :menu}
   let(:menu2) {double :menu}
   let(:items) {double :items}
   let(:items2) {double :items}
-  subject(:restaurant) {described_class.new menu_klass}
+  subject(:restaurant) {described_class.new menu_klass, order_klass}
 
   it{is_expected.to respond_to(:menu)}
   it{is_expected.to respond_to(:new_order)}
@@ -28,6 +34,15 @@ describe Restaurant do
     it 'allows menu change' do
       subject.change_menu menu2
       expect(subject.menu).to be items
+    end
+
+  end
+
+  context 'order management' do
+
+    it 'allows the creation of a new order' do
+      subject.new_order details, customer
+      expect(subject.orders).to include order
     end
 
   end
