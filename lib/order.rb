5ea059160c13d74eb8cede13889eb31346ff066b
itsindigo <matthewbridgesbusiness@gmail.com>
2016-01-30 13:52:20 +0000

    Created order class with menu constant, and spec file

---
+++ lib/order.rb
@@ -0,0 +1,11 @@
+class Menu
+  attr_reader :menu
+
+  def initialize
+    @menu = {soup: 2      ,
+             bread_roll: 2,
+             scones: 1    ,
+             sandwich: 3  ,
+             }
+  end
+end
