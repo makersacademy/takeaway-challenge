6ecaa36beb7e3a139f1056292211928ede9d4325
Henry Gordon <hwgordon247@gmail.com>
2016-01-30 11:25:04 +0000

    implemeted environment variables for all auth codes and numbers

--- lib/test.rb
+++ lib/test.rb
@@ -3,8 +3,9 @@ require 'rubygems'
 
 class Test
 
-  @@account_sid = 'ACa017c4d8a2611154a0f08f692e71ff37'
-  @@auth_token = '90e883e4f42336bcc377f4a14afb11ff'
+
+  @@account_sid = ENV['ACCOUNT_SID']
+  @@auth_token = ENV['AUTH_TOKEN']
 
   def initialize
     @client = Twilio::REST::Client.new @@account_sid, @@auth_token
@@ -15,8 +16,8 @@ class Test
 
   def text
     @client.messages.create(
-    from: '+441922214133',
-    to: '+447780469088',
+    from: ENV['TWILIO_PHONE'],
+    to: ENV['MY_PHONE'],
     body: 'hey there'
     )
   end
