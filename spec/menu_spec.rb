cc8d4db906c76bf05805f1e0d9910c8682c6cdac
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-30 19:26:34 +0000

    added small unit test for Menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,13 +5,18 @@ describe Menu do
   let(:items) {{orange: 1, noodles: 10, pie: 6}}
   let(:message) {"No menu here yet!"}
   let(:new_menu) {described_class.new}
-  subject(:menu) {described_class.new items}
+  subject(:menu) {described_class.new}
 
   it{is_expected.to respond_to(:list_items)}
   it{is_expected.to respond_to(:change_menu)}
 
   it 'returns an "empty" message until menu is set' do
-    expect(new_menu.list_items).to eql message
+    expect(subject.list_items).to eql message
+  end
+
+  it 'allows a change of menu' do
+    subject.change_menu items
+    expect(subject.list_items).to eql items
   end
 
 end
