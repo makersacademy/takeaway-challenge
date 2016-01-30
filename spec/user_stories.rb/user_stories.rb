10047647190e8d4dfca8487780a7f53b461567da
JoeWroe <joe_wroe@icloud.com>
2016-01-30 14:39:53 +0000

    Feature Test 1
      # As a customer
      # So that I can check if I want to order something
      # I would like to see a list of dishes with prices

---
+++ spec/user_stories.rb/user_stories.rb
@@ -0,0 +1,10 @@
+describe 'user stories' do
+  
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+  it 'so a customer can see a menu, it has a list of dishes' do
+    expect(joes.menu).to include { 'Startes', 'Mains', 'Sides', 'Desserts' }
+  end
+
+end
