ddca9022ddd035aa4933b09b7dd19e45ee5a0db1
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-30 14:06:49 +0000

    add menu'

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,11 @@
 class Menu
-  
+  attr_reader :dishes
+  def initialize
+    @dishes = {'prawn crackers' => 2,
+               'ribs' => 4,
+               'special fried rice' => 6,
+               'singapore noodles' => 5,
+               'duck pancakes' => 8,
+               'beef satay' => 7,}
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,6 +4,6 @@ describe Menu do
   subject(:menu) { described_class.new }
 
   it 'initializes with a hash of dishes' do
-    expect(menu.dishes).to be_a_hash
+    expect(menu.dishes).to be_a Hash
   end
 end
