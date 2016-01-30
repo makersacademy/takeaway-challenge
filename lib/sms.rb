3fb5f36faf0baa55ad148682f043d3f9c7b315c8
Alex Avlonitis <avlonitis@msn.com>
2016-01-30 19:28:27 +0000

    added dotenv

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,21 +1,22 @@
+require 'dotenv'
+Dotenv.load
 require 'twilio-ruby'
 
 class Sms
 
   def initialize
     @account_sid = 'AC5f0916482a7dd3822a118d4b27a4e49b'
-    @auth_token = 'a58d36544e1daf403e1d3448c820cfd1'
   end
 
   def client
-    @client = Twilio::REST::Client.new @account_sid, @auth_token
+    @client = Twilio::REST::Client.new @account_sid, ENV['AUTH_TOKEN']
   end
 
   def send(order_number)
     text(order_number)
     client.account.messages.create(
     	from: '+441625800275',
-      to:   '+447719164551',
+      to:   ENV['MOBILE'],
       body:  @message)
   end
 
