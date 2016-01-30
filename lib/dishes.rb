fb04db153fdcf9eb81e5267bfa3ee3590ed428b4
Finble <elainefinn@hotmail.com>
2016-01-30 06:13:27 +0000

    Created user story, class test and class structure - waiting for coveralls + capybara load issues fix to progress

---
+++ lib/dishes.rb
@@ -0,0 +1,19 @@
+class Dishes
+
+	# def list
+
+	# end
+
+	# def select
+
+	# end
+
+	# def total
+
+	# end
+
+	# def text
+
+	# end
+
+end
---
+++ spec/dishes_spec.rb
@@ -0,0 +1,38 @@
+require 'dishes'
+
+describe Dishes do 
+
+	subject(:dishes) {described_class.new}
+
+	describe '#list' do
+
+		it 'shows a list of dishes with prices' do
+
+		end
+
+	end
+end
+
+# 	describe '#select' do
+
+# 		it 'selects some number of available dishes' do
+
+# 		end
+
+# 	end
+
+# 	describe '#total' do
+
+# 		it 'checks the total for dishes in my order' do
+
+# 		end
+
+# 	end
+
+# 	describe '#text' do
+
+# 		it 'receives a text to say "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered' do
+
+# 		end
+
+# end
