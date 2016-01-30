5f800e39556723cce29c723f661cb80daa83dbce
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-30 18:42:03 +0000

    whats dis

--- lib/text.rb
+++ lib/text.rb
@@ -1,14 +1,11 @@
 require 'twilio-ruby'
 require 'dotenv'
-
-Dotenv.load
-
 class Text
 
-  attr_reader :sender, :client, :credentials
+  attr_reader :sender, :client
 
   def initialize
-    @credentials = Dotenv.load
+    @sender = ENV["phone_number"]
   end
 
   def delivery_time
@@ -16,11 +13,16 @@ class Text
   end
 
   def send_text
-    @client = Twilio::REST::Client.new(credentials['account_sid'], credentials['auth_token'])
+    @client = Twilio::REST::Client.new(ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"])
     @client.messages.create(
     body: "Thank you for your order. It will be delivered before #{delivery_time}",
-    to: credentials['phone_number'],
+    to: ENV["phone_number"],
     from: sender)
     'text sent'
+  end
+
+  def whats_dis
+    p ENV["ACCOUNT_SID"]
+    p ENV["AUTH_TOKEN"]
   end
 end
