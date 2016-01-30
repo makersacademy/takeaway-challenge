b496ee7fef9241c0667bd64c1ac42614144d00a2
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-30 14:29:22 +0000

    Add menu method to Takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,11 @@
+require_relative 'menu.rb'
+
 class Takeaway
+  def initialize(menu_klass = Menu)
+    @menu = menu_klass.new
+  end
+
+  def menu
+    @menu.dishes.dup
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,16 @@
 require 'takeaway.rb'
 
 describe Takeaway do
-  
+  let(:menu) { double :menu }
+  let(:menu_klass) { double :menu_klass, new: menu }
+  subject(:takeaway) { described_class.new(menu_klass) }
+
+  describe '#menu' do
+    it 'returns a copy of the menu' do
+      allow(menu).to receive(:dishes) { {'prawn crackers' => 2} }
+      expect(takeaway.menu).to eq ({'prawn crackers' => 2})
+    end
+
+
+  end
 end
