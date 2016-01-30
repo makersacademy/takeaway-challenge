20240156bfc8aa8b598949520344f855027568bf
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-30 17:11:33 +0000

    Add takeaway methods

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,33 @@
 require_relative 'menu.rb'
 
 class Takeaway
+
+  attr_reader :total
+
   def initialize(menu_klass = Menu)
     @menu = menu_klass.new
+    @basket = Hash.new(0)
+    @total = 0
   end
 
   def menu
     @menu.dishes.dup
+  end
+
+  def order(dish, quantity=1)
+    fail 'Sorry, that item is not on the menu' if menu.has_key?(dish) == false
+    @basket[dish] += quantity
+    @total += menu[dish] * quantity
+  end
+
+  def confirm_order(estimate)
+    fail 'Total is not correct, please try again' if estimate != total
+    time = Time.new
+    "Thanks for your order, it will be delivered by #{time.hour+1}:#{time.min}"
+  end
+
+  def basket
+    @basket["Total"] = "£#{total}"
+    @basket
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,7 @@
 require 'takeaway.rb'
 
 describe Takeaway do
-  let(:menu) { double :menu }
+  let(:menu) { double :menu, dishes: {'prawn crackers' => 2} }
   let(:menu_klass) { double :menu_klass, new: menu }
   subject(:takeaway) { described_class.new(menu_klass) }
 
@@ -10,7 +10,41 @@ describe Takeaway do
       allow(menu).to receive(:dishes) { {'prawn crackers' => 2} }
       expect(takeaway.menu).to eq ({'prawn crackers' => 2})
     end
-
-
   end
+
+  describe '#order' do
+    it 'allows user to order from the menu' do
+      expect(takeaway).to respond_to(:order).with(2).argument
+    end
+
+    it 'updates the basket with the item and quantity' do
+      takeaway.order('prawn crackers', 2)
+      expect(takeaway.basket).to eq ({'prawn crackers' => 2, "Total" => "£4"})
+    end
+
+    it 'checks the ordered item is on the menu and returns an error if not' do
+      expect{takeaway.order('potato')}.to raise_error 'Sorry, that item is not on the menu'
+    end
+
+    it 'updates the total' do
+      takeaway.order('prawn crackers', 2)
+      expect(takeaway.total).to eq 4
+    end
+  end
+
+  describe '#basket' do
+    it 'returns the items in the basket' do
+      takeaway.order('prawn crackers', 2)
+      expect(takeaway.basket).to eq Hash ({'prawn crackers' => 2, "Total" => "£4"})
+    end
+  end
+
+  describe '#confirm_order' do
+    it 'raises an error if the total given does not match the total of the order' do
+      takeaway.order('prawn crackers', 3)
+      expect{takeaway.confirm_order(5)}.to raise_error 'Total is not correct, please try again'
+    end
+  end
+
+
 end
