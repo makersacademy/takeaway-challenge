638a3bdd71c8a50c2aa16075687fb41881721d41
seanhawkridge <sean@hawkridge.co>
2016-01-31 18:43:17 +0000

    refactoring tests

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -18,19 +18,19 @@ describe 'User Stories' do
 
   it 'allows a user to add items' do
     restaurant.add_item("Bread")
-    expect(restaurant.order).to include({"Bread" => 1})
+    expect(restaurant.order).to include "Bread" => 1
   end
 
   it 'allows a user to add multiple items' do
-    expect(restaurant.order).to include({"Chicken Tagine" => 4})
+    expect(restaurant.order).to include "Chicken Tagine" => 4
   end
 
   it 'raises an error when an item is not on the menu' do
-    expect{restaurant.add_item("Cheese")}.to raise_error('No such item on the menu')
+    expect{restaurant.add_item("Cheese")}.to raise_error 'No such item on the menu'
   end
 
   it 'allows a user to see an order summary' do
-    expect(restaurant.order_summary).to eq ('2 x Bread = £1.00, 4 x Chicken Tagine = £20.00')
+    expect(restaurant.order_summary).to eq '2 x Bread = £1.00, 4 x Chicken Tagine = £20.00'
   end
 
   it 'allows a customer to remove items' do
@@ -39,15 +39,15 @@ describe 'User Stories' do
   end
 
   it 'raises an error when an item is not in the order' do
-    expect{restaurant.remove_item("Cheese")}.to raise_error('No such item in your order')
+    expect{restaurant.remove_item("Cheese")}.to raise_error 'No such item in your order'
   end
 
   it 'allows a user to see an order total' do
-    expect(restaurant.order_total).to eq ('Order Total: £21.00')
+    expect(restaurant.order_total).to eq 'Order Total: £21.00'
   end
 
   it 'raises an error if the wrong payment amount is submitted' do
-    expect{restaurant.place_order(7.00)}.to raise_error("Wrong payment amount")
+    expect{restaurant.place_order(7.00)}.to raise_error 'Wrong payment amount'
   end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -10,7 +10,7 @@ describe Restaurant do
 
   before do
     allow(menu_klass).to receive(:new)
-    allow(menu_klass).to receive(:list).and_return(:item_1 => 5, :item_2 => 3)
+    allow(menu_klass).to receive(:list).and_return :item_1 => 5, :item_2 => 3 
     allow(text_klass).to receive(:new)
     allow(text_klass).to receive(:send_message)
     subject.add_item(:item_1)
@@ -20,7 +20,7 @@ describe Restaurant do
   describe '#display_menu' do
 
     it 'displays a menu' do
-      expect(subject.display_menu).to eq(:item_1 => 5, :item_2 => 3)
+      expect(subject.display_menu).to eq :item_1 => 5, :item_2 => 3
     end
 
   end
@@ -28,11 +28,11 @@ describe Restaurant do
   describe '#add_item' do
 
     it 'allows a customer to add items' do
-      expect(subject.order).to eq(:item_1 => 1, :item_2 => 1)
+      expect(subject.order).to eq :item_1 => 1, :item_2 => 1
     end
 
     it 'raises an error when an item is not on the menu' do
-      expect{subject.add_item(:item_3)}.to raise_error('No such item on the menu')
+      expect{subject.add_item(:item_3)}.to raise_error 'No such item on the menu'
     end
 
   end
@@ -45,7 +45,7 @@ describe Restaurant do
     end
 
     it 'raises an error when an item is not in the order' do
-      expect{subject.remove_item(:item_3)}.to raise_error('No such item in your order')
+      expect{subject.remove_item(:item_3)}.to raise_error 'No such item in your order'
     end
 
   end
@@ -61,13 +61,13 @@ describe Restaurant do
   describe '#order_summary' do
 
     it 'provides a summary of the current order' do
-      expect(subject.order_summary).to eq('1 x item_1 = £5.00, 1 x item_2 = £3.00')
+      expect(subject.order_summary).to eq '1 x item_1 = £5.00, 1 x item_2 = £3.00'
     end
 
     it 'allows the default quantity to be over-ridden' do
       subject.add_item(:item_1, 3)
       subject.add_item(:item_2, 2)
-      expect(subject.order_summary).to eq('3 x item_1 = £15.00, 2 x item_2 = £6.00')
+      expect(subject.order_summary).to eq '3 x item_1 = £15.00, 2 x item_2 = £6.00'
     end
 
   end
@@ -75,7 +75,7 @@ describe Restaurant do
   describe '#place_order' do
 
     it 'raises an error if the wrong payment amount is submitted' do
-      expect{subject.place_order(7.00)}.to raise_error("Wrong payment amount")
+      expect{subject.place_order(7.00)}.to raise_error  "Wrong payment amount"
     end
 
     it 'activates a payment confirmation text message' do
