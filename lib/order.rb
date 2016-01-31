a089ddcce815020ac96fbb2a1f11632d37163d7d
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-31 13:58:52 +0000

    item selected

--- lib/order.rb
+++ lib/order.rb
@@ -1,27 +1,33 @@
 require_relative 'menu'
 require_relative 'text'
 class Order
-  attr_reader :current_order
+  attr_reader :current_order, :order_total
 
-  def initialize(menu)
+  def initialize
     @current_order = []
   end
 
-  def select_item(item)
-    if item == 'chips' or item == 'sausage' or item =='fish'
-      @current_order << item
-    else
-      raise 'please select item from menu'
-    end
+  def select_item(menu, item, quantity)
+    item_included?(menu, item)
   end
 
-  def total_price
-    @current_order[1].inject(0) { |a, b| a + b }
+  def calculate_price
+
   end
 
   def complete_order
     # totalprice
     Text.new.send_text
     @current_order
+  end
+
+  private
+
+  def item_included?(menu, item)
+    if item == 'chips' or item == 'sausage' or item =='fish'
+      @current_order << item
+    else
+      raise 'please select item from menu'
+    end
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,28 +1,29 @@
 require 'order'
 
 describe Order do
-  subject(:order) { described_class.new('menu')}
+  subject(:order) { described_class.new}
+  let(:menu) {double :menu}
 
-  it 'creates current order array' do
-    expect(order.current_order).to eq []
+  describe 'order'
+    it 'creates current order array' do
+      expect(order.current_order).to eq []
+    end
+
+    it 'selects item' do
+      order.select_item(menu,'chips',2)
+      expect(order.current_order).to include 'chips'
+    end
+
+    it 'raises error when item not on menu selected' do
+      expect {order.select_item('dogshit')}.to raise_error
+    end
+
+    it 'calculates price' do
+      order.select_item(menu,'fish',1)
+      order.select_item(menu,'chips',1)
+      expect(order.calculate_price).to eq 5
+    end
   end
-
-  it 'selects item' do
-    order.select_item('chips')
-    expect(order.current_order).to include 'chips'
-  end
-
-  it 'raises error when item not on menu selected' do
-    expect {order.select_item('dogshit')}.to raise_error
-  end
-
-  it 'calculates price' do
-    order.select_item('fish')
-    order.select_item('chips')
-    expect(order.total_price).to eq 5
-  end
-end
-
 #   it 'completes an order' do
 #     expect(order.complete_order).to eq []
 #   end
