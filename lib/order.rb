bb854a4d06ba6a24900fe98e16e435136d775879
Eilw <eirikwiig@gmail.com>
2016-01-31 23:37:03 +0000

    fixed not on menu issue

--- lib/order.rb
+++ lib/order.rb
@@ -11,6 +11,7 @@ class Order
 
   def add(dish,quantity=1)
     fail ArgumentError, 'Need to select how many you want' if quantity < 1
+    fail ArgumentError, "#{dish} is not on the menu" unless on_menu?(dish)
     price_of_dish = menu[dish]
     current << [quantity, dish, price_of_dish]
   end
@@ -23,7 +24,12 @@ class Order
     ["Total sum for order is: £#{show_sum}", current]
   end
 
+
   private
+
+  def on_menu?(dish)
+    menu.has_key?(dish)
+  end
 
   def calculate_price
     current.inject(0){|sum,selection| sum + (selection[0]*selection[2])}
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,10 +1,12 @@
 require 'order'
 
 describe Order do
-  subject(:order){ described_class.new(menu)}
   let(:dish1){ double('menu') }
   let(:dish2){ double('menu') }
-  let(:menu){ {dish1 => 7.5, dish2 => 10.0 } }
+  let(:menu){ {dish3: 7.5, dish4: 10.0 } }
+  subject(:order){ described_class.new(menu)}
+
+
 
 
   describe '#initialize' do
@@ -15,14 +17,18 @@ describe Order do
 
   describe '#add' do
     it 'insert a single dish with 1 in quantity into the order' do
-      order.add(dish1)
-      expect(order.current[0]).to include 1, dish1, 7.5
+      order.add(:dish3)
+      expect(order.current[0]).to include 1, :dish3, 7.5
     end
 
     it 'insert multiple dish with 2 in quantity into the order' do
-      order.add(dish1,2)
-      order.add(dish2,2)
+      order.add(:dish3,2)
+      order.add(:dish4,2)
       expect(order.current.length).to be 2
+    end
+
+    it 'raise argument error if dish is not on the menu' do
+      expect{order.add(dish1)}.to raise_error ArgumentError
     end
 
     it 'raise argument error if quantity is zero or less' do
@@ -32,8 +38,8 @@ describe Order do
 
   describe '#show_sum' do
     it 'should calculate and show the user the sum of the order' do
-      order.add(dish1)
-      order.add(dish2)
+      order.add(:dish3)
+      order.add(:dish4)
       expect(order.show_sum).to be 17.5
     end
 
