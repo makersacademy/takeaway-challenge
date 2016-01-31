499d91c965fb1310aa8af718e0c33cca66c6980e
Reiss Johnson <reissjohnson@me.com>
2016-01-31 14:54:10 +0000

    now checks that what the customer has ordered exists on the original menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,26 @@ require './lib/menu.rb'
 
 class Takeaway
 
-attr_reader :your_order
+attr_reader :customer_order
 
  def view_menu
   Menu.new.menu
  end
 
  def make_order(order_hash, exact_total)
-  @your_order = order_hash
+  @customer_order = order_hash
+  order_checker
  end
+
+
+private
+
+def order_checker
+  message = "sorry you have ordered something that doesn't exist"
+  message2 = " on the menu, please check your order and try again"
+  if (@customer_order.keys - view_menu.keys).empty? != true
+    raise "#{message}#{message2}"
+  end
+end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,6 +3,12 @@ require 'Takeaway'
 describe Takeaway do
 
   subject(:takeaway1) {described_class.new}
+  order_hash = {"Chicken Noodle Soup" => 1,
+           "Burger and Fries" => 1,
+           "Chicken Tikka Madras" => 1,
+           "Fresh Caesar Salad" => 1}
+
+  wrong_order = {"Chicken Noodle Soup" => 1,"Chicken Tikka Madras" => 1, 'Beans On Toast' => 1}
 
   it 'creates a new Takeaway' do
     expect(described_class.new).to be_a(Takeaway)
@@ -13,8 +19,13 @@ describe Takeaway do
   end
 
   it 'takes an order in form of a hash' do
-    order_hash = {"Chicken Noodle Soup" => 1,"Chicken Tikka Madras" => 1, "Fresh Caesar Salad" => 1,}
     subject.make_order(order_hash,nil)
-    expect(subject.your_order).to be_a (Hash)
+    expect(subject.customer_order).to be_a (Hash)
     end
-   end
+
+  it 'checks customer has ordered only existing dishes' do
+    message = "sorry you have ordered something that doesn't exist"
+    message2 = " on the menu, please check your order and try again"
+    expect{subject.make_order(wrong_order,nil)}.to raise_error(message+message2)
+  end
+end
