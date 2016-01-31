d6ee950d4df8336121c6803052f4e34151224b79
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-31 22:44:55 +0000

    Refactored tests and code for SmsHandler

--- lib/sms_handler.rb
+++ lib/sms_handler.rb
@@ -3,17 +3,20 @@ require 'twilio-ruby'
 Dotenv.load
 
 class SmsHandler
+  ONE_HOUR_SECS = 3600
 
-  def initialize
+  def send(amount)
     @client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
-  end
-
-  def send_sms(message)
-    # delivery_time = (Time.now + 3600).strftime("%I:%M%p")
     @client.messages.create(
       from: ENV['FROM_PHONE'],
       to: ENV['TO_PHONE'],
-      body: message
+      body: message(amount)
     )
+  end
+
+  def message(amount)
+    delivery_time = (Time.now + ONE_HOUR_SECS).strftime('%I:%M%p')
+    "Thanks for ordering! Total is £#{amount}. "\
+    "Expect delivery before #{delivery_time}."
   end
 end
