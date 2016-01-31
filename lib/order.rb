1feedbd73d9261a54adb53ed2ced3fec4d5930d6
Marion LV <marion.levisage@gmail.com>
2016-01-31 22:47:26 +0000

    Adds dishes and views the order summary

--- lib/order.rb
+++ lib/order.rb
@@ -2,18 +2,22 @@ require_relative 'menu'
 
 class Order
 
-  attr_reader :menu_list, :add, :basket, :dish
+  attr_reader :menu_list, :basket
 
-MENU_LIST = Menu::MENU_LIST
-
-  def initialize
-    @menu_list = MENU_LIST
-    @basket = {}
+  def initialize(menu_list = Menu::MENU_LIST)
+    @menu_list = menu_list
+    @basket = []
   end
 
-  def add(dish)
-    price = menu_list[dish]
-    @basket[dish] = price
+  def add(dish, quantity)
+    price = @menu_list[dish]
+    price = menu_list[dish]*quantity
+    @basket << {dish => price}
+    "#{quantity}x #{dish}(s) added to your basket"
+  end
+
+  def view_order
+    basket.each{| hash | hash.each {|dish, value| puts dish.to_s + ' : ' + value.to_s }}
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,18 +2,35 @@ require 'order'
 require 'menu'
 
 describe Order do
-  subject(:order) {described_class.new}
-  let(:menu_list) {double (:menu_list)}
-  let(:basket) {double (:basket)}
-  let(:dish) {double (:dish)}
+  let(:menu_list) { {
+    :caesar_salad => 4,
+    :gazpacho => 3,
+    :tartiflette => 6,
+    :butternut_squash_risotto => 6,
+    :roasted_vegetables => 6,
+    :pumpkin_pie => 2,
+    :eclair => 2 } }
+  subject(:order) {described_class.new(menu_list)}
+  # let(:menu_list) {double (:menu_list)}
+  # let(:basket) {double (:basket)}
 
 
   it 'should display the menu' do
-    expect(order.menu_list).to eq Menu::MENU_LIST
+    expect(order.menu_list).to eq menu_list
   end
 
-  it 'should add dishes to an empty basket' do
-    expect(order.add(dish)).to eq @basket
+  describe '#add(dish)' do
+    it 'should check that dishes are added to an empty basket' do
+      order.add(:eclair, 3)
+      expect(order.basket).to include ({:eclair => 6})
+    end
+  end
+
+  describe '#view_order' do
+    it 'should display the content of a basket (dish, quantity)' do
+      order.add(:eclair, 3)
+      expect(order.view_order).to include ({:eclair => 6})
+    end
   end
 
 end
