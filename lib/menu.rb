876b00149eb9d7c4650973d39197dc1fdd5e9ae6
Tom Pickard <tcpickard94@gmail.com>
2016-02-01 11:34:33 +0000

    Allowed menu to change by passing in an argument corresponding to the dishes at the current restaurant

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,12 +1,12 @@
 class Menu
   attr_reader :dishes
   
-  def initialize
-    @dishes = {"Salted Chilli Ribs" => 5.10,
-               "Spring Roll" => 1.50,
-               "Prawn Toast" => 2.50,
-               "Scallops With Ginger & Spring Onion" => 5.60,
-               "Chicken in Blackbean" => 4.80,
-               "Egg Fried Rice" => 1.90}
+  def initialize(dishes = {"Salted Chilli Ribs" => 5.10,
+             "Spring Roll" => 1.50,
+             "Prawn Toast" => 2.50,
+             "Scallops With Ginger & Spring Onion" => 5.60,
+             "Chicken in Blackbean" => 4.80,
+             "Egg Fried Rice" => 1.90})
+    @dishes = dishes
   end
 end
