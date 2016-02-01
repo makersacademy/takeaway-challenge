68686c4992d025da644ecc110cb0809dc7aeaf55
Mark Hill <futonfutonda@gmail.com>
2016-02-01 11:26:35 +0000

    DRYed calculate

--- lib/calculate.rb
+++ lib/calculate.rb
@@ -10,11 +10,8 @@ module Calculate_Order
   end
 
   def check_calculation(order_list, menu)
-    food = order_list[0]
-    quantities = order_list[1]
     sum = order_list[2][0]
-    price_array = food.map{|x| menu[x]}
-    sum == price_array.each_with_index.map{|x,i| x * quantities[i]}.inject(:+)
+    sum == calculate(order_list, menu)
   end
 
 
