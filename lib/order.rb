3c26f6a5fb19620cec3b7a1793e4b13e2cbc02bf
Ben Forrest <ben@silvabox.com>
2016-02-02 08:15:04 +0000

    adds order

---
+++ lib/order.rb
@@ -0,0 +1,20 @@
+class Order
+  extend Forwardable
+
+  def_delegators :items, :each, :include?, :member?, :empty?
+  def initialize
+    @items = {}
+  end
+
+  def add(name, quantity = 1)
+    items[name] = quantity_of(name) + quantity
+  end
+
+  def quantity_of(name)
+    items[name] || 0
+  end
+
+  private
+
+  attr_reader :items
+end
