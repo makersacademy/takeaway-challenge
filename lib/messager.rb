fedecccad53ef5bef3222e129500cea1299f8031
valogopedina <valentina.monetta@tiscali.it>
2016-02-02 09:02:21 +0000

    Added dotenv gem to hide personal details

--- lib/messager.rb
+++ lib/messager.rb
@@ -1,23 +1,20 @@
 require 'twilio-ruby'
 require 'date'
+require 'dotenv'
 
 class Messager
+  def initialize
+    @env = Dotenv.load
+  end
 
   def send_text
-    account_sid = "ACc640aebc9ed667fe38f3481bab876368"
-    auth_token = "134e02cb4127679d53aaed26bd40af7d"
-    client = Twilio::REST::Client.new account_sid, auth_token
+    client = Twilio::REST::Client.new @env["account_sid"],@env["auth_token"]
 
-    from = "+441702806346" # Your Twilio number
-
-    friends = {
-      "+4407767831356" => "Valentina"
-    }
     friends.each do |key, value|
       client.account.messages.create(
-        :from => from,
-        :to => key,
-        :body => "Thank you! Your order was placed and will be delivered before #{DateTime.now + (1/24.0)}"
+        from: @env[my_twilio_number],
+        to: @env[my_number],
+        body: "Thank you! Your order was placed and will be delivered before #{DateTime.now + (1/24.0)}"
         )
       puts "Sent message to #{value}"
     end
