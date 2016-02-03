529ee1fd6262ef7b16e8d925ba1056daf5c9f457
Ben Forrest <ben@silvabox.com>
2016-02-03 08:28:59 +0000

    fixes typo in customer_spec

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,7 +1,7 @@
 require 'customer'
 
 describe Customer do
-  subject(:custoemr) { Customer.new('Test', '07712987654') }
+  subject(:customer) { Customer.new('Test', '07712987654') }
 
   it { is_expected.to have_attributes(name: 'Test', phone: '07712987654') }
 end
