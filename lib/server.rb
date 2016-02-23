048a52bd9881823a0f4058e55ba59cf875bcbe92
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-02-23 19:47:13 +0000

    All tests passing, feature testing shows select_dish and take_order methods require more work.

--- lib/server.rb
+++ lib/server.rb
@@ -3,17 +3,16 @@ require_relative './cuisine.rb'
 
 class Server
 
-  attr_reader :menu
+  attr_reader :menu, :order_total
 
   def initialize(menu_klass, cuisine_klass, price_calculator_klass)
     @menu_klass = menu_klass
     @cuisine_klass = cuisine_klass
     @price_calculator_klass = price_calculator_klass
+    @order_total = 0
   end
 
-  def request_cuisine
-    puts "What cuisine would you like?"
-    cuisine = gets.chomp
+  def request_cuisine(cuisine)
     @cuisine = @cuisine_klass.new(cuisine, dish = Dish)
     @cuisine.create_dishes
   end
@@ -22,14 +21,26 @@ class Server
     @calc = @price_calculator_klass.new(self)
     @menu = @menu_klass.new(calc = @calc)
     @cuisine.dishes.each do |dish|
-      @menu.add_dishes(dish)
+      menu.add_dishes(dish)
     end
   end
 
   def present_menu
-    @menu.view_prices
-    @menu.price_list.each do |dish,price|
+    menu.view_prices
+    menu.price_list.each do |dish,price|
       puts "#{dish} --- £#{price}"
     end
+  end
+
+  def take_order
+    menu.make_order
+  end
+
+  def total
+    menu.selected_dishes.each do |dish,quantity|
+      @order_total += dish.price * quantity
+    end
+    raise "Order Total incorrect." if order_total != @calc.grand_total
+    order_total
   end
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,37 +1,39 @@
 require 'server'
 
 describe Server do
-  subject(:server) {described_class.new(menu_klass, cuisine_klass, price_calculator_klass)}
+  subject(:server) do
+    described_class.new(menu_klass, cuisine_klass, price_calculator_klass)
+  end
+  let(:dish) {double :dish, name: "Chow Mein", price: 3.99}
   let(:menu_klass) {double :menu_klass, new: menu}
   let(:menu) do
     double :menu,
     view_prices: price_list,
     price_list: {dish.name => dish.price},
     add_dishes: [dish],
-    make_order: price_calculator
+    make_order: price_calculator,
+    selected_dishes: {dish => 1}
   end
   let(:cuisine_klass) {double :cuisine_klass, new: cuisine}
   let(:cuisine) {double :cuisine, create_dishes: dish, dishes: [dish]}
   let(:price_calculator_klass) do
     double :price_calculator_klass, new: price_calculator
   end
-  let(:price_calculator) {double :price_calculator, price: 3.99}
-  let(:dish) {double :dish, name: "Chow Mein", price: 3.99}
+  let(:price_calculator) do
+    double :price_calculator, price: 3.99, grand_total: 3.99
+  end
   let(:price_list) {double :price_list}
+  let(:chinese) {double :cuisine}
 
   context 'producing the desired the cuisine' do
     describe '#request_cuisine' do
-      it 'asks for a cuisine type from the user' do
-        expect(STDOUT).to receive(:puts).with("What cuisine would you like?")
-        server.request_cuisine
-      end
       it 'instantiates cuisine' do
         expect(cuisine_klass).to receive(:new)
-        server.request_cuisine
+        server.request_cuisine(chinese)
       end
       it 'calls the cuisine\'s create_dishes method' do
         expect(cuisine).to receive(:create_dishes)
-        server.request_cuisine
+        server.request_cuisine(chinese)
       end
     end
   end
@@ -39,7 +41,7 @@ describe Server do
   context 'creating the menu' do
     describe '#create_menu' do
       before do
-        server.request_cuisine
+        server.request_cuisine(chinese)
       end
       it 'instantiates the price_calculator' do
         expect(price_calculator_klass).to receive(:new)
@@ -56,32 +58,40 @@ describe Server do
     end
   end
 
-  context 'presenting the menu to the customer' do
-    describe '#present_menu' do
-      before do
-        server.request_cuisine
-        server.create_menu
-      end
-      it 'puts a human readable view of the dishes and prices.' do
-        expect(STDOUT).to receive(:puts).with("Chow Mein --- £3.99")
-        server.present_menu
+  context 'working with a generated menu' do
+    before do
+      server.request_cuisine(chinese)
+      server.create_menu
+    end
+    context 'presenting the menu to the customer' do
+      describe '#present_menu' do
+        it 'puts a human readable view of the dishes and prices.' do
+          expect(STDOUT).to receive(:puts).with("Chow Mein --- £3.99")
+          server.present_menu
+        end
       end
     end
-  end
 
-  context 'taking the order' do
-    describe '#take_order' do
-      it 'calls the menu\'s make_order method' do
-        expect(menu).to receive(:make_order)
-        server.take_order
+    context 'taking the order' do
+      describe '#take_order' do
+        it 'calls the menu\'s make_order method' do
+          expect(menu).to receive(:make_order)
+          server.take_order
+        end
       end
     end
-  end
 
-  context 'checking the total' do
-    describe '#total' do
-      it 'totals up the cost of the customer\'s order' do
-        expect(server.total).to equal(price_calculator.price)
+    context 'checking the total' do
+      describe '#total' do
+        it 'totals up the cost of the customer\'s order' do
+          server.total
+          expect(server.order_total).to equal(price_calculator.price)
+        end
+
+        it 'raises an error if the order total is incorrect' do
+          allow(price_calculator).to receive(:grand_total) {5}
+          expect{server.total}.to raise_error "Order Total incorrect."
+        end
       end
     end
   end
