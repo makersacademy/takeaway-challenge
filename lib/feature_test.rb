b6c97aff7c4c58d8bd30b7d429319032a1c735a4
Misa Ogura <misa.ogura01@gmail.com>
2016-03-11 19:38:24 +0000

    Add feature_test.rb

---
+++ lib/feature_test.rb
@@ -0,0 +1,8 @@
+MENU = [
+        {name: 'sushi', price: 12},
+        {name: 'ramen', price: 7},
+        {name: 'soup', price: 4.5},
+        {name: 'fish and chips', price: 10},
+        {name: 'bread', price: 3},
+        {name: 'olives', price: 1.5}
+        ]
