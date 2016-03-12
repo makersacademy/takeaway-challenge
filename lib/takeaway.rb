f59f7313af13acb84e8db614b249b557ccd46bf3
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-03-12 12:02:33 +0000

    Added items to menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,7 +3,11 @@ class TakeAway
   attr_reader :menu
 
   def initialize
-    @menu = {}
+    @menu = {"fish and chips" => 4.34,
+             "chicken korma" => 3.56,
+             "sausges and mash" => 5.46,
+             "cheeze pizza" => 2.87,
+             "pepperoni pizza" => 3.93}
   end
 
   def list_menu
