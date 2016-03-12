5155ccfe761c90d321a1d97654373e9b3e989b13
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-12 17:54:39 +0000

    Added feature test for second user story

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,10 +1,17 @@
-# As a customer
-# So that I can check if I want to order something
-# I would like to see a list of dishes with prices
-
 require './lib/takeaway'
 
 takeaway = TakeAway.new
 
+# As a customer
+# So that I can check if I want to order something
 # I would like to see a list of dishes with prices
- takeaway.menu
+takeaway.menu
+
+# As a customer
+# So that I can order the meal I want
+# I would like to be able to select some number of several available dishes
+takeaway.order(1,"Bratwurst in a bun")
+takeaway.order(1,"Käsekrainer in a bun")
+takeaway.order(2,"Sauerkraut")
+takeaway.order(2,"Sidesalad")
+takeaway.order(2,"Weißbier")
