c1b1fdc795640d7e0f2636bed76779fcd8a3d410
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-03-12 17:33:13 +0000

    Can add items to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,19 +1,11 @@
 class Menu
 
-  MENU_ITEMS = {
-    'Beef burger' => 8.5,
-    'Cheese burger' => 9,
-    'Chicken burger' => 9.5,
-    'Vegetable burger' => 7.5,
-    'Chips' => 3.25,
-    'Onion rings' => 3.5,
-    'Coleslaw' => 3,
-    'Side salad' => 3,
-    'Sauce' => 1
-  }.freeze
-
   def initialize
-    @menu = MENU_ITEMS
+    @menu = Hash.new
+  end
+
+  def add_to_menu(item,price)
+    @menu[item] = price
   end
 
   def list
@@ -21,6 +13,6 @@ class Menu
   end
 
   def price item
-    MENU_ITEMS[item]
+    @menu[item]
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,9 +3,20 @@ require 'menu'
 describe Menu do
   subject(:menu) { described_class.new }
 
+  MENU_ITEM = 'Cheese sandwich'
+  MENU_ITEM_PRICE = 3
+
+  describe '#add_to_menu' do
+    it 'should add item to menu hash' do
+      menu.add_to_menu(MENU_ITEM,MENU_ITEM_PRICE)
+      expect(menu.list).to include MENU_ITEM => MENU_ITEM_PRICE
+    end
+  end
+
   describe '#price' do
     it 'should return price of item' do
-      expect(menu.price('Beef burger')).to eq 8.5
+      menu.add_to_menu(MENU_ITEM,MENU_ITEM_PRICE)
+      expect(menu.price(MENU_ITEM)).to eq MENU_ITEM_PRICE
     end
   end
 end
