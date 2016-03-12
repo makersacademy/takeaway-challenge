28528aefcc4b41c6f1831c4479961c3a99210a98
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-12 20:07:11 +0000

    implement controller class

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,28 @@
 class Controller
 
+  attr_reader :menu, :order_class, :current_order
+
+  def initialize menu_ofday , order_class
+    @order_class = order_class
+    @menu = menu_ofday
+  end
+
+  def welcome
+  #  print "welcome"
+    @current_order = @order_class.new menu
+  end
+
+  def list_dishes
+    @menu.list_dishes
+  end
+
+  def update_order dish
+    @current_order.add_dish dish
+    @current_order.update_total
+  end
+
+  def checkout payment
+    raise "Incorrect Payment!" unless payment == current_order.bill
+  end
+
 end
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,22 +1,81 @@
 require 'controller'
 
 describe Controller do
-  subject(:test_controller) {described_class.new}
+  subject(:test_controller) {described_class.new dummy_menu , dummy_order_class}
+  let(:dummy_menu) {double :menu, list_dishes: nil}
+  let(:dummy_order_class) {double :Order, new: dummy_order}
+  let(:dummy_order) {double :order, add_dish: nil, update_total: nil, bill: 0}
+  let(:dummy_dish) {double :dish}
 
-  describe "#list dishes" do
+  describe '#initalize' do
+
+    it 'is initalized with a menu of the day' do
+      expect(test_controller.menu).to eq dummy_menu
+    end
+
+    it 'is initalized with a order class' do
+      expect(test_controller.order_class).to eq dummy_order_class
+    end
 
   end
 
-  describe "update_order" do
+  describe '#welcome' do
+
+      xit 'expects to return a welcome message to the user' do
+        expect{test_controller.welcome}.to output('welcome').to_stdout
+      end
+
+      it 'expect to create a new active order' do
+        expect(test_controller.current_order.class).not_to eq nil
+      end
+  end
+
+  describe '#list_dishes' do
+
+    it 'calls the function on menu' do
+      expect(dummy_menu).to receive(:list_dishes)
+      test_controller.list_dishes
+    end
 
   end
 
-  describe "#welcome" do
+  describe '#update_order' do
+
+    before(:each) do
+      test_controller.welcome
+    end
+
+    it 'add the order to the list' do
+      expect(dummy_order).to receive(:add_dish).with(dummy_dish)
+      test_controller.update_order dummy_dish
+    end
+
+    it 'update the total due' do
+      expect(dummy_order).to receive(:update_total)
+      test_controller.update_order dummy_dish
+    end
 
   end
 
-  describe "#checkout" do
+  describe '#checkout' do
+
+    before(:each) do
+      test_controller.welcome
+    end
+
+    it 'checks the payment against the total' do
+      expect(dummy_order).to receive(:bill)
+      test_controller.checkout 0
+    end
+
+    it 'raises an error if the payment is incorrect' do
+      expect {test_controller.checkout 1}.to raise_error('Incorrect Payment!')
+    end
+
+    xit 'calls the confirmation text' do
+      
+    end
 
   end
-  
+
 end
