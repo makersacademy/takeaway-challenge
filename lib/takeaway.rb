34355275ba596a61c70fbc3ffee03c7bab4445f3
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-03-12 13:13:24 +0000

    Added order method with error message if item not in menu, and tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,8 @@
 
 class TakeAway
-  attr_reader :menu, :selection
+  attr_reader :menu
+
+  NOT_IN_MENU = "Item not in menu"
 
   def initialize
     @menu = {"fish and chips" => 4.34,
@@ -8,11 +10,14 @@ class TakeAway
              "sausges and mash" => 5.46,
              "cheeze pizza" => 2.87,
              "pepperoni pizza" => 3.93}
-    @selection = {}
   end
 
   def list_menu
     @menu.dup
+  end
+
+  def order(item)
+    raise NOT_IN_MENU if !menu.include? item
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,10 +2,15 @@ require 'takeaway'
 
 describe TakeAway do
   subject(:takeaway) { described_class.new }
-  let(:menu) {double :menu}
+  let(:menu) {double :menu }
 
   it 'should respond to list_menu method' do
     expect(takeaway).to respond_to :list_menu
+  end
+
+  # CHANGE TO 2 ARGUMENTS WHEN ADDING IN QUANTITY *********
+  it 'should respond to order method' do
+    expect(takeaway).to respond_to(:order).with(1).argument
   end
 
   describe '#initialize' do
@@ -13,13 +18,13 @@ describe TakeAway do
       expect(takeaway.menu).to be_a Hash
     end
 
-    it 'should initialize with selection hash' do
-      expect(takeaway.selection).to be_a Hash
-    end
-
-    it 'should initializa the selection hash as empty' do
-      expect(takeaway.selection).to be_empty
-    end
+    # it 'should initialize with selection hash' do
+    #   expect(takeaway.selection).to be_a Hash
+    # end
+    #
+    # it 'should initializa the selection hash as empty' do
+    #   expect(takeaway.selection).to be_empty
+    # end
   end
 
   describe '#list_menu' do
@@ -27,6 +32,17 @@ describe TakeAway do
     it 'should list the food items and prices' do
       allow(takeaway).to receive(:list_menu).and_return menu
       expect(takeaway.list_menu).to eq menu
+    end
+  end
+
+  describe '#order' do
+    it 'should raise error message if item inputted is not in menu' do
+      message = 'Item not in menu'
+      expect{takeaway.order(menu)}.to raise_error message
+    end
+
+    it 'should select the item inputted and add to selection hash' do
+      
     end
   end
 end
