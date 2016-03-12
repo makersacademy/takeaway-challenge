24d174882e28c76ea41fc9592eb61bf44afdf23b
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-12 15:12:32 +0000

    added unavailable error

--- lib/order.rb
+++ lib/order.rb
@@ -2,12 +2,20 @@ require_relative "menu"
 
 class Order
   attr_reader :basket
+  UNAVAIABLE_ERROR = "Item not available."
 
-  def initialize
+  def initialize(menu)
+    @menu = menu
     @basket = Hash.new(0)
   end
 
   def add(item, quantity = 1)
+    raise UNAVAIABLE_ERROR unless available?(item)
     @basket[item] += quantity
   end
+
+  private
+    def available?(item)
+      @menu.display.include? item
+    end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,10 +2,18 @@ require "order"
 
 describe Order do
 
+  let(:menu) { double :menu, display: { "skinny fries" => 2.95 } }
+  subject(:order) { described_class.new(menu) }
+
   describe "#order" do
     it "should add a dish to the basket" do
       subject.add("skinny fries", 2)
       expect(subject.basket).to include("skinny fries" => 2)
+    end
+
+    it "should raise error when dish is not available" do
+      message = described_class::UNAVAIABLE_ERROR
+      expect { subject.add("chunky fries") }.to raise_error message
     end
   end
 
