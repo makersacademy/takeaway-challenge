2d3aafebc7499998681a66e1b4261602d123e250
Misa Ogura <misa.ogura01@gmail.com>
2016-03-12 15:03:25 +0000

    Feature test

--- lib/feature_test.rb
+++ lib/feature_test.rb
@@ -2,45 +2,10 @@ require_relative 'order'
 require_relative 'menu'
 
 order1 = Order.new
-p order1.place_order({name: 'sushi', amout: 4},{name: 'soup', amout: 3}, 28)
+p order1.menu
+order1.place_order({name: 'sushi', amount: 5},{name: 'ramen', amount: 1}, 28)
+p order1.total
+p order1.basket << "order"
 
-p order1.basket
-puts order1.show_menu
-
-p menu = Menu.new
-p menu.show_menu
-p Menu::MENU
-
-
-# MENU = [
-#         {name: 'sushi', price: 12},
-#         {name: 'ramen', price: 7},
-#         {name: 'soup', price: 4.5},
-#         {name: 'fish and chips', price: 10},
-#         {name: 'bread', price: 3},
-#         {name: 'olives', price: 1.5}
-#         ]
-
-# extract correct dish on the menu given a name of the dish
-# p MENU.select {|item| (item[:name] == 'soup') || (item[:name] == 'bread') }
-
-# 2.2.3 :001 > def testin(*dishes, total)
-# 2.2.3 :002?>   puts dishes
-# 2.2.3 :003?>   puts total
-# 2.2.3 :004?>   end
-#  => :testin
-# 2.2.3 :005 > testin({name: 'sushi', amout: 4}, 28)
-# {:name=>"sushi", :amout=>4}
-# 28
-#  => nil
-# 2.2.3 :006 > testin({name: 'sushi', amout: 4},{name: 'soup', amout: 3}, 28)
-# {:name=>"sushi", :amout=>4}
-# {:name=>"soup", :amout=>3}
-# 28
-#  => nil
-# 2.2.3 :007 > testin({name: 'sushi', amout: 4},{name: 'soup', amout: 3},{name: 'bread', price: 3}, 28)
-# {:name=>"sushi", :amout=>4}
-# {:name=>"soup", :amout=>3}
-# {:name=>"bread", :price=>3}
-# 28
-#  => nil
+menu = Menu.new
+menu.find_price({name: 'sushi', amout: 5})
