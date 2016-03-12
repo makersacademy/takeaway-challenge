b6c60975684a4a77b15a3fc73b082ddcbb500eb5
Misa Ogura <misa.ogura01@gmail.com>
2016-03-12 15:02:18 +0000

    Move menu functionality to Menu Class

--- lib/order.rb
+++ lib/order.rb
@@ -1,50 +1,24 @@
 class Order
 
-  MENU = [
-          {name: 'sushi', price: 7},
-          {name: 'ramen', price: 12},
-          {name: 'soup', price: 4.5},
-          {name: 'fish and chips', price: 10},
-          {name: 'bread', price: 3},
-          {name: 'olives', price: 1.5}
-          ]
+  attr_reader :menu
 
-  attr_reader :basket
-
-  def initialize
+  def initialize(menu_class=Menu)
+    @menu = menu_class.new
     @basket = []
   end
 
-  def show_menu
-    menu = ''
-    MENU.each {|item| menu << "#{item[:name]}: £#{item[:price]}\n"}
-    menu
+  def basket
+    @basket.dup.freeze
   end
 
   def place_order(*dishes, total)
-    dishes.each do |dish|
-      MENU.each {|item| dish[:price] = item[:price] if dish[:name] == item[:name]}
-      @basket << dish
-    end
+    dishes.each { |dish| @basket << @menu.find_price(dish) }
+    @basket = @basket.flatten
   end
-
-  # @basket << dish.each {|item| item[:price] = MENU[:(item[:name])]}
-  #  MENU.select {|dish| price = dish[:price] if dish[:name] == item[:name]}
-  # needs to find the way to look for the right price from the MENU!!!
-
-
-  # def remove_dish(dish)
-  #   message = "You did not add #{dish[:name]}!"
-  #   raise message if @basket.none? {|item| item[:name] == dish[:name]}
-  #   @basket.delete(dish)
-  # end
 
   def total
     price = 0
-    @basket.each {|dish| price += dish[:amount]*dish[:price]}
+    @basket.each {|dish| price += (dish[:amount] * dish[:price])}
     price
   end
 end
-
-# initialize ([{name: 'sushi', amout: 4}, {name: 'soup', amout: 3}], total)
-#         => (*dishes, total)
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,75 +2,40 @@ require 'order'
 
 describe Order do
 
-  menu = Order::MENU
+  # menu = Order::MENU
 
-  subject(:order) { described_class.new }
+  let(:menu) { double(:menu, find_price: [dish1_with_price]) }
+  let(:menu_class) { double(:menu_class, new: menu)}
+  subject(:order) { described_class.new(menu_class) }
   let(:dish1) { {name: 'sushi', amount: 3} }
-  let(:dish2) { {name: 'ramen', amount: 1} }
-  let(:basket1) { {name: 'sushi', amount: 3, price: 7} }
-  let(:basket2) { {name: 'ramen', amount: 1, price: 12} }
+  let(:dish1_with_price) { {name: 'sushi', amount: 3, price: 7} }
 
   describe '#initialize' do
-    it '1.0 initializes an order with an empty list of dishes' do
+    it '0.0 initializes an order with defalut menu' do
+      expect(order.menu).to eq menu
+    end
+    it '0.1 initializes an order with an empty list of dishes' do
       expect(order.basket).to eq([])
     end
   end
 
-  describe '#show_menu' do
-    it '2.0 shows a list of dishes available to order with price' do
-      expect(order.show_menu).to include('sushi: £7')
-    end
-  end
-
   describe '#place_order' do
-    # xit '2.0 adds a dish to the basket' do
-    #   order.place_order(dish1)
-    #   expect(order.basket).to include(dish1)
-    # end
-    # it '2.1 accepts a list of dishes and expected total' do
-    #   list = {name: 'sushi', amount: 3}, {name: 'ramen', amount: 1}
-    #   total = 33
-    # end
-    it '2.2 assigns a price to each dish and adds it to the basket' do
-      order.place_order(dish1, dish2, 33)
-      expect(order.basket).to include(basket1 && basket2)
+    before(:each) do
+      order.place_order(dish1, 33)
     end
-
+    it '1.0 calls find_price method on menu' do
+      expect(order.menu).to have_received(:find_price)
+    end
+    it '1.1 adds dishes (with price info) to the basket' do
+      expect(order.basket).to include(dish1_with_price)
+    end
   end
-
-  # describe '#removes_dish' do
-  #   xit '3.0 removes a dish from the basket' do
-  #     order.place_order(dish2)
-  #     order.remove_dish(dish2)
-  #     expect(order.basket).not_to include(dish2)
-  #   end
-  #   it '3.1 raises an error when trying to remove non-existent dish' do
-  #     message = "You did not add #{dish2[:name]}!"
-  #     order.place_order(dish1)
-  #     expect{order.remove_dish(dish2)}.to raise_error message
-  #   end
-  # end
 
   describe '#total' do
-    it '4.0 calculates the total price of the order' do
-      order.place_order(dish1, dish2, 33)
-      dish1_total = dish1[:amount]*dish1[:price]
-      dish2_total = dish2[:amount]*dish2[:price]
-      expect(order.total).to eq(dish1_total + dish2_total)
+    it '2.0 calculates the total price of the order' do
+      order.place_order(dish1, 33)
+      dish1_total = dish1_with_price[:amount]*dish1_with_price[:price]
+      expect(order.total).to eq(dish1_total)
     end
   end
 end
-
-# what order needs to know: dish_name, amount, price
-# basket => hash in an array? e.g. [{dish: 'sushi', amount: 4, price: 7},...]
-#           => nested array? e.g. [[sushi, 4, 7]]
-
-# when passing in dish to place_order dish needs to know name, amount, price
-# when removing dish by remove_dish, method only needs to know dish name
-
-
-# methods order class should have
-# - add a dish
-# - remove a dish
-# - calc_total
-# - place_order
