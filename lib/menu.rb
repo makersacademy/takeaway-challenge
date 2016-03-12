8e57f582633c2d99e5df5d6e150bf3b49186f6da
phoebe <phoebe.greig@gmail.com>
2016-03-12 11:58:55 +0000

    menu spec and file

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,19 @@
+require_relative 'dish'
+
 class Menu
+
+attr_reader :list
+
+	def initialize
+		@list = Array.new
+	end
+
+	def add_dish(dish)
+		@list << dish
+	end
+
+	def delete_dish(dish)
+		@list.delete(dish)
+	end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,9 +2,30 @@ require 'menu'
 
 describe Menu do 
 
+subject(:menu) {described_class.new}
+let(:dish) { double :dish }
+
 	describe '#initialize' do
 
+		it 'has an empty array' do
+			expect(menu.list).to be_empty
+		end
 
+	end
+
+	describe '#add_dish' do
+	
+		it 'to the menu' do
+			expect{menu.add_dish(dish)}.to change{menu.list.length}.by 1
+		end
+	end
+
+	describe '#delete_dish' do
+
+		it 'from the menu' do
+			menu.add_dish(dish)
+			expect{menu.delete_dish(dish)}.to change{menu.list.length}.by -1
+		end
 	end
 
 end
