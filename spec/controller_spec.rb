df049bede2c1cd6f8f36821b6bd7c6dc40f82b2b
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-12 18:29:36 +0000

    create controller class

---
+++ spec/controller_spec.rb
@@ -0,0 +1,22 @@
+require 'controller'
+
+describe Controller do
+  subject(:test_controller) {described_class.new}
+
+  describe "#list dishes" do
+
+  end
+
+  describe "update_order" do
+
+  end
+
+  describe "#welcome" do
+
+  end
+
+  describe "#checkout" do
+
+  end
+  
+end
