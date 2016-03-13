736f1ba9c994bd946678e9a11605457dd078f9d0
Misa Ogura <misa.ogura01@gmail.com>
2016-03-13 19:32:13 +0000

    Added config loop to stub constant in unit tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ Coveralls.wear!
 require 'capybara/rspec'
 require 'byebug'
 require 'dotenv'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_const("Twilio::REST::Client", FakeTwilio)
+  end
+end
