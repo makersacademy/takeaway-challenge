4b1bc5b5526299bb985d6ba0cf308f6efbfa8551
olmesm <olmesm@gmail.com>
2016-03-13 21:57:44 +0000

    order#from menu added

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,21 @@
+require_relative 'menu'
+
 class Order
 
-  def initialize
-    @summary = []
+  def initialize(menu: Menu.new)
+    @summary = {}
+    @menu = menu
   end
 
-  def add dish
-    @summary << dish
+  def add item
+    @summary[@menu.dishes[item]].nil? ? @summary[@menu.dishes[item]] = 1 : @summary[@menu.dishes[item]] += 1
   end
 
   def summary
     @summary.dup
+  end
+
+  def from_menu
+    @menu.display
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,18 +1,32 @@
 require 'order'
 
 describe Order do
-  subject(:order) { described_class.new }
-  let(:menu) { double(:Menu) }
+  subject(:order) { described_class.new(menu: menu) }
+  let(:menu) { double(:Menu, dishes: [dish1, dish2, dish3]) }
   let(:dish1) { double(:Dish, name: "Chicken", price: 1) }
+  let(:dish2) { double(:Dish, name: "Fish", price: 2) }
+  let(:dish3) { double(:Dish, name: "Steak", price: 3) }
 
   describe '#accept' do
     it 'accepts an order' do
 
     end
 
+    describe '#from_menu' do
+      it 'displays the menu for selection' do
+        expect(menu).to receive(:display)
+        order.from_menu
+      end
+    end
+
     describe '#add' do
+      it 'gets the dish from the menu' do
+        expect(menu).to receive(:dishes)
+        order.add 0
+      end
+
       it 'adds a meal to the order' do
-        expect{ order.add dish1 }.to change { order.summary }.to include dish1
+        expect{ order.add 0 }.to change { order.summary }.to include dish1
       end
     end
   end
