7179bfc5de1d72fcae11fd3c4733291a44c71715
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-03-13 14:58:55 +0000

    Trying to get tests to work

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,21 @@
+require 'checkout.rb'
+
+describe Checkout do
+  subject(:checkout) { described_class.new }
+  let(:client) { double :client, messages: messages }
+  let(:messages) { double :messages, create: nil }
+  let(:from) { 123 }
+  let(:to) { 456 }
+  let(:body) { "hello" }
+
+  it 'should respond to send_sms method' do
+    expect(checkout).to respond_to :send_sms
+  end
+
+  describe '#send_sms' do
+    it 'should send a text message' do
+      checkout.send_sms
+      expect(client.messages).to have_received(:create).with({from: from, to: to, body: body})
+    end
+  end
+end
