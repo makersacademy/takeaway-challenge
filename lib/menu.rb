077fadcd7aa415bf5894e08200e2525af2782b0b
Misa Ogura <misa.ogura01@gmail.com>
2016-03-13 14:12:49 +0000

    Amend find_price method to return the price itself

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,20 +1,29 @@
 class Menu
 
+  attr_reader :list
+
   LIST = [
-          {name: 'Miso soup', price: 1.5},
-          {name: 'Sticky rice', price: 2},
-          {name: 'Tofu salad', price: 4.5},
-          {name: 'Sushi', price: 7},
-          {name: 'Okonomi-yaki', price: 10},
-          {name: 'Tonkotsu Ramen', price: 12},
-          {name: 'Katsu curry', price: 12}
+          {name: 'miso soup', price: 1.5},
+          {name: 'sticky rice', price: 2},
+          {name: 'tofu salad', price: 4.5},
+          {name: 'sushi', price: 7},
+          {name: 'okonomi-yaki', price: 10},
+          {name: 'tonkotsu ramen', price: 12},
+          {name: 'katsu curry', price: 12}
           ].freeze
 
   def initialize
-    @list = ''
+    @list = make_list
   end
 
-  def list
+  def find_price(dish)
+    LIST.each {|i| return i[:price] if i[:name] == dish}
+  end
+
+  private
+
+  def make_list
+    @list = ''
     LIST.each {|dish| @list << "#{dish[:name]}: £#{dish[:price]}\n"}
     @list.dup.freeze
   end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,10 +5,17 @@ describe Menu do
   #list = Menu::LIST
 
   subject(:menu) { described_class.new }
+  let(:dish1) { 'sushi' }
 
   describe '#initialize' do
     it '0.0 initializes with a list of menu' do
-      expect(menu.list).to include('Sushi: £7' && 'Ramen: £12')
+      expect(menu.list).to include('sushi: £7' && 'tonkotsu ramen: £12')
+    end
+  end
+
+  describe '#find_price' do
+    it '1.0 finds a price of a given dish from the menu' do
+      expect(menu.find_price(dish1)).to eq(7)
     end
   end
 end
