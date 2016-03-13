16024422d402a07141c6a2cc9ec81fcd50763f54
Tobenna <tobennandu@gmail.com>
2016-03-13 14:43:39 +0000

    menu spec and menu scaffolding added

---
+++ spec/menu_spec.rb
@@ -0,0 +1,52 @@
+require 'menu'
+
+describe Menu do
+  let(:dish) { double(:dish, price: nil, name: nil) }
+  let(:dish_klass) { double(:dish_klass, new: dish) }
+  subject(:menu) do
+    described_class.new(dish_klass: dish_klass)
+  end
+
+  describe '#add' do
+    it 'adds a new dish to the menu' do
+      menu.add('dish', 5)
+      expect(menu.dishes).to include dish
+    end
+
+    it 'raises an error if dish already exists' do
+      menu.add('dish', 5)
+      already_exists = described_class::ALREADY_EXISTS_ERROR
+      expect { menu.add('dish', 9) }.to raise_error already_exists
+    end
+  end
+
+  describe '#remove' do
+    it 'removes the dish from the menu' do
+      allow(dish).to receive(:name).and_return('dish')
+      menu.add('dish', 5)
+      menu.remove('dish')
+      expect(menu.dishes).not_to include dish
+    end
+
+    it 'raises an error if the dish doesn\'t exist' do
+      doesnt_exist = described_class::DOES_NOT_EXIST_ERROR
+      expect { menu.remove('dish') }.to raise_error doesnt_exist
+    end
+  end
+
+  describe '#update_price' do
+    it 'Changes the price of a dish' do
+      menu.add('dish', 5)
+      expect(dish).to receive(:update_price)
+      menu.change_price('dish', 6)
+    end
+  end
+
+  describe '#change_name' do
+    it 'Changes the name of a dish on the menu' do
+      menu.add('dish', 5)
+      expect(dish).to receive(:rename)
+      menu.change_name('dish', 6)
+    end
+  end
+end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -8,68 +8,67 @@
 #
 # As a customer
 # So that I can verify that my order is correct
-# I would like to check that the total I have been given matches the sum of the various dishes in my order
+# I would like to check that the total I have been given matches the
+# sum of the various dishes in my order
 #
 # As a customer
 # So that I am reassured that my order will be delivered on time
-# I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered
+# I would like to receive a text such as "Thank you! Your order was placed and
+# will be delivered before 18:52" after I have ordered
 require 'restaurant'
 
 describe Restaurant do
   let(:menu) { double(:menu, view: nil) }
-  let(:order) { double(:menu, add: nil) }
+  let(:order) { double(:order, add: nil, view: 'Orders') }
   let(:menu_klass) { double(:menu_klass, new: menu) }
   let(:order_klass) { double(:order_klass, new: order) }
+  let(:dish_name) { 'Pizza' }
+  let(:quantity) { 7 }
   subject(:restaurant) do
     described_class.new(menu_klass: menu_klass, order_klass: order_klass)
   end
-
-  describe '#view_menu' do
-    it 'returns a list of available dishes' do
-      expect(menu).to receive(:view)
-      restaurant.view_menu
+  context 'There is an active order' do
+    before { restaurant.order_add(dish_name, quantity) }
+    describe '#view_menu' do
+      it 'returns a list of available dishes' do
+        expect(menu).to receive(:dishes)
+        restaurant.view_menu
+      end
     end
-  end
 
-  describe '#order_add' do
-    it 'adds a dish to your order' do
-      dish_name = 'Pizza'
-      quantity = 7
-      expect(order).to receive(:add).with(dish_name, quantity)
-      restaurant.order_add(dish_name, quantity)
+    describe '#order_add' do
+      it 'adds a dish to your order' do
+        expect(order).to receive(:add).with(dish_name, quantity)
+        restaurant.order_add(dish_name, quantity)
+      end
     end
-  end
 
-  describe '#order_remove' do
-    it 'removes a dish from your order' do
-      dish_name = 'Pizza'
-      quantity = 7
-      expect(order).to receive(:remove).with(dish_name, quantity)
-      restaurant.order_remove(dish_name, quantity)
+    describe '#order_remove' do
+      it 'removes a dish from your order' do
+        expect(order).to receive(:remove).with(dish_name, quantity)
+        restaurant.order_remove(dish_name, quantity)
+      end
     end
-  end
 
-  describe '#checkout' do
-    it { is_expected.to respond_to(:checkout) }
-    it 'finishes your order' do
-      expect(order).to receive(:complete)
-      restaurant.checkout
+    describe '#checkout' do
+      it 'finishes your order' do
+        expect(order).to receive(:complete)
+        restaurant.checkout
+      end
     end
-  end
 
-  describe '#view_order' do
-    it { is_expected.to respond_to(:view_order) }
-    it 'returns list of all the items ordered' do
-      expect(order).to receive(:view)
-      restaurant.view_order
+    describe '#view_order' do
+      it 'returns list of all the items ordered' do
+        expect(order).to receive(:view)
+        restaurant.view_order
+      end
     end
-  end
 
-  describe '#order_summary' do
-    it { is_expected.to respond_to(:order_summary) }
-    it 'returns list of all the items ordered' do
-      expect(order).to receive(:summary)
-      restaurant.order_summary
+    describe '#order_summary' do
+      it 'Returns the total price of your goods and a summary' do
+        expect(order).to receive(:summary)
+        restaurant.order_summary
+      end
     end
   end
 end
