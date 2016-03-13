696e80adf3e97498b3ec1dea985e40572e36afbd
olmesm <olmesm@gmail.com>
2016-03-13 21:19:52 +0000

    added order

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,14 @@
 class Order
 
+  def initialize
+    @summary = []
+  end
+
+  def add dish
+    @summary << dish
+  end
+
+  def summary
+    @summary.dup
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,12 +4,16 @@ describe Order do
   subject(:order) { described_class.new }
   let(:menu) { double(:Menu) }
   let(:dish1) { double(:Dish, name: "Chicken", price: 1) }
-  let(:dish2) { double(:Dish, name: "Fish", price: 2) }
-  let(:dish3) { double(:Dish, name: "Steak", price: 3) }
 
   describe '#accept' do
     it 'accepts an order' do
-      
+
+    end
+
+    describe '#add' do
+      it 'adds a meal to the order' do
+        expect{ order.add dish1 }.to change { order.summary }.to include dish1
+      end
     end
   end
 end
