3ee6f7d7dcf0a6f45b389aeb5d482a786dd3264f
Paul <paulalexrees@gmail.com>
2016-03-13 13:36:48 +0000

    attempt #10

--- lib/sms.rb
+++ lib/sms.rb
@@ -9,15 +9,16 @@ get '/sms' do
   incoming = params[:Body]
 
   twiml = Twilio::TwiML::Response.new do |r|
-    begin order = eval(incoming)
-      if order.empty?
-        r.Message "Your order contained no items."
-      else
-        r.Message SMSMessage.new.format_order(order)
-      end
-    rescue Exception => exc
-      r.Message "Exception: #{exc}"
-    end
+    # begin order = eval(incoming)
+    #   if order.empty?
+    #     r.Message "Your order contained no items."
+    #   else
+    #     r.Message SMSMessage.new.format_order(order)
+    #   end
+    # rescue Exception => exc
+    #   r.Message "Exception: #{exc}"
+    # end
+    r.Message "Does this work?"
   end
   twiml.text
 end
