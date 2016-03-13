314d6f65623b6b85e37b0054af6dd0713550823a
eripheebs <erikapheby@gmail.com>
2016-03-13 13:09:11 +0000

    Still trying to make spec tests pass without env keys

--- spec/messenger_spec.rb
+++ spec/messenger_spec.rb
@@ -5,12 +5,14 @@ describe Messenger do
   subject(:messenger) { described_class.new }
   let(:client) { double :client }
   let(:phone) { ENV['TWILIO_DESTINATION_PHONE'] }
+  let(:from_phone) { ENV["TWILIO_PHONE"] }
 
   describe '#send' do
     it '> should send a text message to phone' do
+      allow(Twilio::REST::Client).to receive(:new).and_return client
       message = "Rspec Test Passing!"
-      twilio = { from: @from_phone, to: phone, body: message}
-      allow(client).to receive_message_chain(:account, :messages, :create).with(twilio)
+      twilio = { from: from_phone, to: phone, body: message}
+      expect(client).to receive_message_chain(:account, :messages, :create).with(twilio)
       messenger.send(message, phone)
     end
   end
