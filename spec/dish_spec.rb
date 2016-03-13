feb1ca7f43c14fdb19744b8d7da5dcead41cc9d2
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-13 09:25:56 +0000

    added quantity to dish class

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -4,9 +4,11 @@ describe Dish do
 
   let(:name) { double(:name)}
   let(:price) { double(:price) }
-  subject(:dish) { described_class.new(name, price)}
+  let(:quantity) { double(:quantity) }
+  subject(:dish) { described_class.new(name, price, quantity)}
 
   it {is_expected.to respond_to(:name)}
   it {is_expected.to respond_to(:price)}
+  it {is_expected.to respond_to(:quantity)}
 
 end
