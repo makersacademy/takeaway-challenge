071d4d974f2543f8164e5aa0f93b3bf7cca9dd26
eripheebs <erikapheby@gmail.com>
2016-03-13 20:19:03 +0000

    Replaced dish's attr_reader with duplication methods to keep variables from being altered

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,10 +1,16 @@
 class Dish
 
-  attr_reader :price, :name
-
   def initialize(name: name, price: price)
     @name = name
     @price = price
+  end
+
+  def price
+    @price * 1
+  end
+
+  def name
+    @name.dup
   end
 
 end
