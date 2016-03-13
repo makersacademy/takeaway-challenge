75e92c3dc1b43aacb8c039a446a62dbe15cc452d
phoebe <phoebe.greig@gmail.com>
2016-03-13 17:00:41 +0000

    message class

--- lib/message.rb
+++ lib/message.rb
@@ -1,17 +1,20 @@
 require 'twilio-ruby'
 require 'dotenv'
+
 Dotenv.load
 
- 
-account_sid = ENV["ACCOUNT_SID"]
-auth_token = ENV["AUTH_TOKEN"]
-@client = Twilio::REST::Client.new account_sid, auth_token
- 
-from = '+441600800013'
-super_secret = ENV['SUPER_SECRET']
+class Checkout
 
-@client.account.messages.create({
-	:from => from,
-	:to => super_secret,  
-	:body => 'Thank you! Your order was placed and will be delivered soon'  
-})
+  	def initialize
+    	@client = Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
+  	end
+
+  	def send_sms(to, message)
+    	@client.messages.create(
+      		from: '+441600800013',
+      		to: to,
+      		body: message
+    	)
+  	end
+  	
+end
