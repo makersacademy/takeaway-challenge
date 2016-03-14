061fdc73762c3138e8be81bed1ca4e8bee1e74dd
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-14 10:59:06 +0000

    Refactored tests

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,12 +3,7 @@ require 'checkout'
 describe Checkout do
   let(:client) { double :client, messages: messages }
   subject(:checkout) { described_class.new(client) }
-  let(:messages) { double :messages, create: nil }
-
-  # ENV = { "ACCOUNT_SID" => "FAKE ID",
-  #         "AUTH_TOKEN" => "FAKE TOKEN",
-  #         "TWILIO_NUM" => "FAKE FROM",
-  #         "MY_NUM" => "FAKE TO" }
+  let(:messages) { double :messages, create: nil }  
 
   it 'should respond to send_sms method' do
     expect(checkout).to respond_to :send_sms
@@ -16,11 +11,8 @@ describe Checkout do
 
   describe '#send_sms' do
     it 'should send a text message' do
-      now = Time.parse("2016-03-12 22:57:38 +0000")
-      allow(Time).to receive(:now).and_return(now)
-      message = "Thank you! Your order was placed and will be delivered before 23:57"
       checkout.send_sms
-      expect(client.messages).to have_received(:create).with({from: ENV["TWILIO_NUM"], to: ENV["MY_NUM"], body: message})
+      expect(client.messages).to have_received(:create)
     end
   end
 end
