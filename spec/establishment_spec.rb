6dca383d91024c7b24a4bf98ae9b6f28defb422d
ollieh-m <ollie.hm@cantab.net>
2016-04-23 16:40:25 +0100

    Test that .place_order in establishment asks checker object whether order passes

--- spec/establishment_spec.rb
+++ spec/establishment_spec.rb
@@ -1,13 +1,22 @@
 require 'establishment'
 
 describe Establishment do
-	let(:menu){ spy(:menu) }
-	subject(:establishment){ described_class.new(menu) }
+	let(:menu){ double(:menu, list: nil) }
+	let(:checker){ spy(:checker) }
+	subject(:establishment){ described_class.new(menu,checker) }
 
 	context '#view_menu' do
 		it 'tells its menu object to show the list of items and prices' do
+			expect(menu).to receive(:list)
 			establishment.view_menu
-			expect(menu).to have_received(:list)
+		end
+	end
+
+	context '#place_order' do
+		it 'asks the checker object whether the order passes' do
+			allow(menu).to receive(:list){ "MOCK LIST"}
+			establishment.place_order(100,item1:1,item2:2,item3:3)
+			expect(checker).to have_received(:pass?).with(100,{item1:1,item2:2,item3:3},"MOCK LIST")
 		end
 	end
 
