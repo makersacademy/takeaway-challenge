6c055ebe907c46ec5169ac4748d1183879a23057
ollieh-m <ollie.hm@cantab.net>
2016-04-23 15:54:16 +0100

    Tests for checker class check_items and check_price methods

---
+++ spec/checker_spec.rb
@@ -0,0 +1,30 @@
+require 'checker'
+
+describe Checker do
+	let(:menu){ { item1: {price: 1}, item2: {price: 2}, item3: {price: 3} } }
+	
+	let(:order_invalid_items){ {item1: 1, item2: 2, item10: 3} }
+	let(:order_valid_items){ { item1: 1, item2: 2} }
+
+	context '#check_items' do
+		it 'raises an error if the order contains an item not in the menu' do
+			expect{subject.check_items(order_invalid_items,menu)}.to raise_error "Sorry, item10 is not on the menu. Please re-order."
+		end
+		it 'doesn\'t raise an error if all the order items are included in the menu' do
+			expect{subject.check_items(order_valid_items,menu)}.not_to raise_error "Sorry, item10 is not on the menu. Please re-order."
+		end
+	end
+
+	context '#check_price' do
+		it 'raises an error if the total given is more than the correct price for the order' do
+			expect{subject.check_price(100,order_valid_items,menu)}.to raise_error "Sorry, the total price is £5. You have paid £95 too much! Please try again."
+		end
+		it 'raises an error if the total given is less than the correct price for the order' do
+			expect{subject.check_price(1,order_valid_items,menu)}.to raise_error "Sorry, the total price is £5. You need to pay £4 more! Please try again."
+		end
+		it 'doesn\'t raise an error if the total given equals the correct price for the order' do
+			expect{subject.check_price(5,order_valid_items,menu)}.not_to raise_error
+		end
+	end
+
+end
