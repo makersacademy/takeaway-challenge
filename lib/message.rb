50e343e37f88baf6af586bb04397003b36d5a228
Alex Robertson <lex1989@gmail.com>
2016-04-23 19:53:51 +0100

    Updated message for travis

--- lib/message.rb
+++ lib/message.rb
@@ -6,11 +6,10 @@ Dotenv.load
 class Message
   def initialize(del_time)
     @del_time = del_time
-    @client = Twilio::REST::Client.new ENV['TWIL_SID'], ENV['TWIL_AUTH']
   end
 
   def send
-
+    @client = Twilio::REST::Client.new ENV['TWIL_SID'], ENV['TWIL_AUTH']
     @client.account.messages.create(
     	from: ENV['FROM_PHONE'],
     	to: ENV['TO_PHONE'],
