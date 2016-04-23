f4659567a23452a5e9e35218f03ac7a1e08d9459
Matthew Ward <matthewward@Matthews-MacBook-Pro-6.local>
2016-04-23 11:31:57 +0100

    order and basket functions added

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,27 @@ class Takeaway
 
   def initialize
     @menu = {
-      'onion_bhaji' => 3.50,
+      'onion bhaji' => 3.50,
       'chicken dhansak' => 7.95,
       'lamb jalfrezi' => 8.95
     }
+    @basket_items = []
   end
 
   def menu
     @menu.dup
   end
-  
+
+  # As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+
+  def order(item)
+    "#{item} was added to your order"
+  end
+
+  def basket
+    @basket_items.dup
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,14 +1,29 @@
 require 'takeaway'
 
 describe Takeaway do
-  let(:menu) { {'onion_bhaji' => 3.50, 'chicken dhansak' => 7.95, 'lamb jalfrezi' => 8.95} }
+  let(:takeaway) { described_class.new }
+  let(:item) { 'onion bhaji' }
+  let(:item_confirmation) { "#{item} was added to your order" }
+  let(:empty_basket) { [] }
+  let(:order) { [] }
+  let(:menu) { {'onion bhaji' => 3.50, 'chicken dhansak' => 7.95, 'lamb jalfrezi' => 8.95} }
 
   describe '#menu' do
     it 'shows a list of dishes with prices' do
-      expect(subject.menu).to eq menu
+      expect(takeaway.menu).to eq menu
     end
   end
 
+  describe '#basket' do
+    it 'is empty when no items ordered' do
+      expect(takeaway.basket).to eq empty_basket
+    end
+  end
 
+  describe '#order' do
+    it 'confirms an item is added to basket' do
+      expect(takeaway.order(item)).to eq item_confirmation
+    end
+  end
 
 end
