7fa75fd979ecb688232953e7d69a7d0100be9648
ollieh-m <ollie.hm@cantab.net>
2016-04-23 16:42:49 +0100

    Make test pass

--- lib/establishment.rb
+++ lib/establishment.rb
@@ -1,11 +1,16 @@
 class Establishment
 
-	def initialize(menu)
+	def initialize(menu,checker=Checker.new)
 		@menu = menu
+		@checker = checker
 	end
 
 	def view_menu
 		@menu.list
+	end
+
+	def place_order(total,order)
+		@checker.pass?(total,order,view_menu)
 	end
 
 end
