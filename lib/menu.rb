9dabb11c516b15d41ae12492db4a130450817b40
Omar Alvarez <omajul2@hotmail.com>
2016-04-23 12:55:24 +0000

    Add Menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,26 +1,26 @@
 class Menu
   attr_reader :dishes
   
-  def initialize
-    @dishes = {}
+  def initialize(dishes = {})
+    @dishes = dishes
   end
   
-  def add_dish(name:, price:)
-    dishes[name] = price
+  def add_dish(dish, price)
+    dishes[dish] = price
   end
   
-  def remove_dish(name)
-    dishes.delete name
+  def remove_dish(dish)
+    dishes.delete dish if contains?(dish)
   end
   
-  def to_s
+  def read_menu
     return "The menu is empty" if dishes.empty?
-    dishes.each do |dish, price|
-      puts "#{dish} : £#{price}"
-    end
+    dishes.map do |dish, price|
+      "%s: £%.2f" % [dish, price]
+    end.join("\n")
   end
   
-  def print
-    self.to_s
+  def contains?(dish)
+    dishes.has_key? dish
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,21 +1,27 @@
 require 'menu'
 
 describe Menu do
-  subject(:menu) { described_class.new }
-  let(:dish) { {"Chicken" => 10} }
+  subject(:menu) { described_class.new(dishes) }
+  let(:dishes) { {"Chicken"=>4.5, "Pork"=>5.5, "Steak"=>9.3} }
   
   describe '#add_dish' do
     it "adds a dish to the menu" do
-      menu.add_dish(name: "Chicken", price: 10)
-      expect(menu.dishes).to include dish
+      menu.add_dish("Fish", 10)
+      expect(menu.contains?("Fish")).to be true
     end
   end
   
   describe '#remove_dish' do
     it "removes a dish from the menu" do
-      menu.add_dish(name: "Chicken", price: 10)
-      menu.remove_dish("Chicken")
-      expect(menu.dishes).not_to include dish
+      menu.remove_dish("Pork")
+      expect(menu.contains?("Pork")).to be false
+    end
+  end
+  
+  describe "#read" do
+    it "prints the menu in a user friendly way" do
+      printed_menu = "Chicken: £4.50\nPork: £5.50\nSteak: £9.30"
+      expect(menu.read_menu).to eq printed_menu
     end
   end
 end
