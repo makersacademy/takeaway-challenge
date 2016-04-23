475e7635e79f8bc6a3095caec5e73027aa25c1ea
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-23 22:50:12 +0100

    added more files, started with rsoec

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,14 @@
 class Menu
 
+  attr_reader :menu_list
+
+  def initialize
+    @menu_list = []
+  end
+
+
+  def list
+    @menu_list
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,6 +2,15 @@ require 'menu'
 
 describe Menu do
 
+it { is_expected.to respond_to(:list) }
+
+  describe '#intialize' do
+    it "starts with a list of food" do
+      expect(subject.list).not_to be_nil
+    end
+  end
+
+# expect(person).to have_attributes(
 
 
 end
