d03f7a32e0039b90ca00d70e2ec574d1394ed7cb
ollieh-m <ollie.hm@cantab.net>
2016-04-23 16:49:26 +0100

    Test that establishment tells texter to send confirmation if order passes checks

--- spec/establishment_spec.rb
+++ spec/establishment_spec.rb
@@ -2,8 +2,9 @@ require 'establishment'
 
 describe Establishment do
 	let(:menu){ double(:menu, list: nil) }
-	let(:checker){ spy(:checker) }
-	subject(:establishment){ described_class.new(menu,checker) }
+	let(:checker){ double(:checker, pass?: nil) }
+	let(:texter){ spy(:texter) }
+	subject(:establishment){ described_class.new(menu,checker,texter) }
 
 	context '#view_menu' do
 		it 'tells its menu object to show the list of items and prices' do
@@ -15,8 +16,13 @@ describe Establishment do
 	context '#place_order' do
 		it 'asks the checker object whether the order passes' do
 			allow(menu).to receive(:list){ "MOCK LIST"}
+			expect(checker).to receive(:pass?).with(100,{item1:1,item2:2,item3:3},"MOCK LIST")
 			establishment.place_order(100,item1:1,item2:2,item3:3)
-			expect(checker).to have_received(:pass?).with(100,{item1:1,item2:2,item3:3},"MOCK LIST")
+		end
+		it 'tells texter object to send order confirmation if pass? returns true' do
+			allow(checker).to receive(:pass?){ true }
+			establishment.place_order(100,item1:1,item2:2,item3:3)
+			expect(texter).to have_received(:send_confirmation)
 		end
 	end
 
