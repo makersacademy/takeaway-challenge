ccc1e9d12de5ecf06b80e55b477532bb1fb82143
ollieh-m <ollie.hm@cantab.net>
2016-04-23 18:45:31 +0100

    Modify test for texter class

--- spec/texter_spec.rb
+++ spec/texter_spec.rb
@@ -1,13 +1,17 @@
 require 'texter'
 
 describe Texter do
-	let(:client){ spy(:client) }
+	let(:client){ double(:client, account: account) }
+	let(:account){ double(:account, messages: messages) }
+	let(:messages){ double(:messages, create: nil) }
 	subject(:texter){ described_class.new(client) }
 
 	context '#send_confirmation' do
 		it 'tells the client to create a text using twilio' do
+			expect(client).to receive(:account)
+			expect(account).to receive(:messages)
+			expect(messages).to receive(:create).with({from: "+441277424334", to: "+447921046417", body: "Thank you! Your order was placed successfully and will be delivered an hour from #{Time.now}"})
 			texter.send_confirmation
-			expect(client).to have_received(:account,:messages,:create).with({from: "+441277424334", to: "+447921046417", body: "Thank you! Your order was placed successfully and will be delivered an hour from #{Time.now}"})
 		end
 	end
 
