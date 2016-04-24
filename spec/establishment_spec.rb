550716d1458c2d90b48cf60b947b81622dcdf518
ollieh-m <ollie.hm@cantab.net>
2016-04-24 11:05:04 +0100

    Update establishment tests to expect order to be placed with key word arguments

--- spec/establishment_spec.rb
+++ spec/establishment_spec.rb
@@ -17,16 +17,16 @@ describe Establishment do
 		it 'asks the checker object whether the order passes' do
 			allow(menu).to receive(:list){ "MOCK LIST"}
 			expect(checker).to receive(:pass?).with(100,{item1:1,item2:2,item3:3},"MOCK LIST")
-			establishment.place_order(100,item1:1,item2:2,item3:3)
+			establishment.place_order(total: 100,order: {item1:1,item2:2,item3:3})
 		end
 		it 'tells texter object to send order confirmation if pass? returns true' do
 			allow(checker).to receive(:pass?){ true }
-			establishment.place_order(100,item1:1,item2:2,item3:3)
+			establishment.place_order(total: 100,order: {item1:1,item2:2,item3:3})
 			expect(texter).to have_received(:send_confirmation)
 		end
 		it 'doesn\'t tell texter object to send order confirmation if pass? does not return true' do
 			allow(checker).to receive(:pass?){ false }
-			establishment.place_order(100,item1:1,item2:2,item3:3)
+			establishment.place_order(total: 100,order: {item1:1,item2:2,item3:3})
 			expect(texter).not_to have_received(:send_confirmation)
 		end
 	end
