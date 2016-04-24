998265b8f4aa183579278c1448a85dd20397c74a
Mara Wanot <m.d.wanot@hotmail.com>
2016-04-24 17:10:43 +0100

    soft tabs changed

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,42 +1,42 @@
 require 'order'
 
 describe Order do
-	subject(:order){ described_class.new }
+  subject(:order){ described_class.new }
 
-	describe "#initialize" do 
-		it "new order" do
-			expect(order).to respond_to :list_of_items
-		end
-	end
+  describe "#initialize" do 
+    it "new order" do
+      expect(order).to respond_to :list_of_items
+    end
+  end
 
-	describe "#add_item" do
-		it "raises an error if the item isn't on the menu" do
-			expect{order.add_item(:fish, 1)}.to raise_error "item isn't on the menu"
-		end
+  describe "#add_item" do
+    it "raises an error if the item isn't on the menu" do
+      expect{order.add_item(:fish, 1)}.to raise_error "item isn't on the menu"
+    end
 
-		it "adds an item to basket" do
-			order.add_item(:chips, 2)
-			expect(order.list_of_items).to eq [[:chips, 2]]
-		end
+    it "adds an item to basket" do
+      order.add_item(:chips, 2)
+      expect(order.list_of_items).to eq [[:chips, 2]]
+    end
 
-		it "updates total price with the new item" do
-			test_order = described_class.new
-			test_order.add_item(:chips, 2)
-			expect(test_order.total_price). to eq 6
-		end
-	end
+    it "updates total price with the new item" do
+      test_order = described_class.new
+      test_order.add_item(:chips, 2)
+      expect(test_order.total_price). to eq 6
+    end
+  end
 
-	describe "#submit_order" do
-		it "raises an error if the total is incorrect" do
-			test_order = described_class.new
-			test_order.add_item(:chips, 2)
-			expect{order.submit_order(100)}.to raise_error "price does not match"
-		end
-		it "tells the user their order has been placed" do
-			test_order = described_class.new
-			test_order.add_item(:chips, 2)
-			expect(test_order.submit_order(6)).to include "Thank you!"
-		end
-	end
+  describe "#submit_order" do
+    it "raises an error if the total is incorrect" do
+      test_order = described_class.new
+      test_order.add_item(:chips, 2)
+      expect{order.submit_order(100)}.to raise_error "price does not match"
+    end
+    it "tells the user their order has been placed" do
+      test_order = described_class.new
+      test_order.add_item(:chips, 2)
+      expect(test_order.submit_order(6)).to include "Thank you!"
+    end
+  end
 
 end
