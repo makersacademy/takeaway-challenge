8bc973063ad5507fb2d3e5659568f16db6137746
Fergus Orbach <fo4786@gmail.com>
2016-04-24 11:11:27 +0100

    order_log add and list

--- lib/order_log.rb
+++ lib/order_log.rb
@@ -1,10 +1,18 @@
 class OrderLog
 
-  def list
-    []
+  def initialize menu
+    @menu = menu
+    @current = []
   end
 
-  def add dish, quantity
+  def list
+    current.inject(''){|sum,item| sum << "#{item.name}: #{item.price}\n"}
   end
+
+  def add dish, quantity = 1
+    quantity.times {@current << @menu.get(dish)}
+  end
+
+  attr_reader :current
 
 end
--- spec/order_log_spec.rb
+++ spec/order_log_spec.rb
@@ -2,9 +2,11 @@ require 'order_log'
 
 describe OrderLog do
 
-  it {is_expected.to respond_to(:add).with(2).arguments}
+  # it {is_expected.to respond_to(:add).with(1).arguments}
 
-  subject(:order_log) {OrderLog.new}
+  let(:menu) {double :menu, get: menu_item}
+  let(:menu_item) {double :menu_item, name: "dish", price: 1}
+  subject(:order_log) {OrderLog.new(menu)}
 
   describe "#initalize" do
 
@@ -14,11 +16,46 @@ describe OrderLog do
     it 'starts empty' do
       expect(order_log.list).to be_empty
     end
+
+    context "after a dish has been logged" do
+      it 'shows dish' do
+        order_log.add "dish"
+        expect(order_log.list).not_to be_empty
+      end
+
+      it 'format - "dish: price" on its own line' do
+        msg = "#{menu_item.name}: #{menu_item.price}"
+        order_log.add "dish"
+        order_log.add "dish"
+        expect(order_log.list).to eq "#{msg}\n#{msg}\n"
+      end
+
+    end
+
   end
 
   describe '#add' do
-    it 'adds a dish' do
+    it 'gets item from the menu' do
+      expect(menu).to receive(:get).with("item")
+      order_log.add "item"
     end
+
+    it 'can add multiple items' do
+      quantity = 7
+      expect(menu).to receive(:get).with("item").exactly(quantity).times
+      order_log.add "item", quantity
+    end
+
+    it 'default add only 1 item' do
+      quantity = 1
+      expect(menu).to receive(:get).with("item").exactly(quantity).times
+      order_log.add "item"
+    end
+
+    # it 'adds a dish' do
+    #   order_log.add "name"
+    # end
+
   end
 
 
