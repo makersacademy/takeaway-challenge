1e951fde75cc39a40fb2bf24c5716d6e8979a87c
AdamSkuse <a.t.skuse@gmail.com>
2016-04-24 16:50:46 +0100

    User story 3 implemented

--- lib/customer.rb
+++ lib/customer.rb
@@ -7,6 +7,7 @@ attr_reader :menu, :basket
   def initialize(menu = Menu.new)
     @menu = menu
     @basket = []
+    @running_total = 0
   end
 
   def read_menu
@@ -16,6 +17,17 @@ attr_reader :menu, :basket
   def order(item:, quantity:)
     quantity.times do | times |
       @basket << menu.list.select { |k,v| k == item }
+    end
+  end
+
+  def checkout(expected_total)
+    @basket.each do |item|
+      @running_total += @menu.list[item.keys[0]]
+    end
+    if @running_total != expected_total
+      raise "Your expected total amount due is not correct. Please try to checkout again"
+    else
+      print "Order complete, thanks!"
     end
   end
 
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -18,9 +18,36 @@ describe 'User Stories' do
     customer = Customer.new
     customer.read_menu
     customer.order(item: :a, quantity: 1)
-    expect(customer.basket).to be_an_instance_of(Array)
+    expect(customer.basket).to eq [{:a=>10}]
     # the above should be changed to expect basket to include the customer order above
+
+    #ADD it 'raises an error telling the customer that they must include an item and a quantity if they do not do so'
+
   end
+
+  # As a customer
+# So that I can verify that my order is correct
+# I would like to check that the total I have been given matches the sum of the various dishes in my order
+
+  it 'lets customers checkout their order if they enter the correct total' do
+    customer = Customer.new
+    customer.order(item: :a, quantity: 1)
+    expect { customer.checkout(10) }.to output('Order complete, thanks!').to_stdout
+
+
+  end
+
+  it 'raises an error if a customer enters the wrong total when trying to checkout their order' do
+    customer = Customer.new
+    customer.order(item: :a, quantity: 1)
+    expect { customer.checkout(9) }.to raise_error('Your expected total amount due is not correct. Please try to checkout again')
+  end
+
+
+
+
+
+
 
 
 end
