8d70ea467029b2729060fdaa39d68eb4d9123928
Nick <nm.richards@yahoo.com>
2016-04-25 07:42:51 -0400

    Travis fix hopefully

--- lib/messages.rb
+++ lib/messages.rb
@@ -2,17 +2,20 @@ class Messages
   require 'twilio-ruby'
   SID = ENV["SID"]
   TOKEN = ENV["TOKEN"]
+  TWI = ENV["TWILIO_NUMBER"]
+  TO = ENV["PHONE"]
 
-  def initialize(from = ENV["TWILIO_NUMBER"], account_sid = SID, auth_token = TOKEN)
+  def initialize(from = TWI, to = TO, account_sid = SID, auth_token = TOKEN)
     @client = Twilio::REST::Client.new account_sid, auth_token
     @from =  from
+    @to = to
     current = Time.now
     @t = format('%d:%02d', current.hour + 1, current.min)
   end
 
   def send_message
     m = "Thank you! Your order was placed and will be delivered before #{@t}"
-    @client.account.messages.create(from: @from, to: ENV["PHONE"], body: m)
+    @client.account.messages.create(from: @from, to: @to, body: m)
     "Sent message to Nick"
   end
 end
--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -3,7 +3,7 @@ describe Messages do
   number = "+15005550006"
   id = "AC83610dffc2aba0550cbeb134e7f7b61e"
   token = "b9761263fe46a5726a718486ca3a04d1"
-  subject(:messages){described_class.new(number, id, token)}
+  subject(:messages){described_class.new(number, number, id, token)}
 
   it 'sends text to a number' do
     expect(messages.send_message).to eq "Sent message to Nick"
