5c512421ebcbe7a70ebceae5ac446746756d98e0
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-25 15:15:32 +0200

    Updated message_spec.rb

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -4,6 +4,8 @@ describe Message do
   subject(:message) { described_class.new }
   let (:t) { Time.new + 60*60 }
   let (:message_body) { "Thank you! Your order will be delivered before #{t.strftime("%H")}:#{t.strftime("%M")}!" }
+  let (:TWILIO_ACCOUNT_SID) { 'SID' }
+  let (:TWILIO_AUTH_TOKEN) { 'TOKEN' }
 
   it 'sends a message confirming the order and that it will bedelivered within one hour' do
     expect(message.send(message_body)).to eq message_body
@@ -12,6 +14,13 @@ describe Message do
   it 'calls twilio_send' do
     expect(message).to receive(:twilio_send).with(message_body)
     message.send(message_body)
+
+
+    # message = 'order complete'
+    # twilio_message_body = { from: ENV['TWILIO_PHONE'], to: ENV['TWILIO_DESTINATION_PHONE'], body: message }
+    # allow(@client).to receive_message_chain(:account, :messages, :create).with(twilio_message_body)
+    # expect(Twilio::REST::Client).to receive(:new).with(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']).and_return(@client)
+    # subject.send(message)
   end
 
 end
