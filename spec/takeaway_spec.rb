d87e296223f50a17cdbe52dad06dfc82dd15a2ad
chriselevate <coates.c@hotmail.com>
2016-04-25 09:53:49 +0100

    adding new stubs

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,18 +4,18 @@ describe Takeaway do
 
   subject(:takeaway)  { described_class.new(menu: menu, order: order) }
 
-  let(:menu)          { double(:menu, show: shows_menu) }
+  let(:menu)          { double(:menu, print_menu: print_menu, has_dish?: has_dish?, price: price) }
   let(:order)         { double(:order) }
-  let(:shows_menu)    { "The menu is..." }
+  let(:printed_menu)  { "The menu is..." }
   let(:dishes)        { { Chicken_Tikka: 1, Rice: 1, Naan_Bread: 1 } }
 
 
   xit "shows a list of dishes with prices" do
-    expect(takeaway.show_menu).to eq shows_menu
+    expect(takeaway.print_menu).to eq printed_menu
   end
 
   xit "can order a number of available dishes" do
    total = "Total is £7.50"
-   expect(takeway.create_order(dishes)).to eq total
+   expect(takeway.place_order(dishes)).to eq total
  end
 end
