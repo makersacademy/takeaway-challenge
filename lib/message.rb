470a13d5afcf74f9ee4d6d0ba09ddcb439625dea
sitypop <sityemilyshah@gmail.com>
2016-04-29 17:54:27 +0100

    added dotenv gem

--- lib/message.rb
+++ lib/message.rb
@@ -1,12 +1,14 @@
+require 'twilio-ruby'
+require 'dotenv'
+Dotenv.load
+
 class Message
 
   def send
-    require 'twilio-ruby'
-
-    account_sid = "AC412bbc135865f0af2e2c2968f8b3cb54"
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-    from = "+441277424311"
-    to = ENV["PHONE_NUMBER"]
+    account_sid = ENV['ACC_SID']
+    auth_token = ENV['AUTH']
+    from = ENV['TWILIO_NUMBER']
+    to = ENV['RECEIVER_NUMBER']
     delivery_time = (Time.now + 3600).strftime("%H:%M")
 
     body = "Thank you! Your order will be delivered before #{delivery_time}"
