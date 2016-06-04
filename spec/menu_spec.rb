8287a7d7ae3361a1f51b8d77f591141320fae6f4
lomlo <michael.lo@live.co.uk>
2016-06-04 16:52:42 +0100

    refactor of step 2 rspec tests

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,6 +2,7 @@ require 'menu'
 
 describe Menu do
   subject(:menu) { described_class.new }
+  
   describe 'responds to' do
     it { is_expected.to respond_to :show }
     it { is_expected.to respond_to(:order).with(1).argument }
@@ -15,9 +16,17 @@ describe Menu do
   end
 
   describe '#order' do
+    it "accepts an item from the menu and a quantity" do
+      expect{menu.order('milkshake', 4)}.to_not raise_error
+    end
+    context 'when no quantity is inputted' do
+      it 'should assume a default input of 1' do
+        expect{menu.order('chips')}.to_not raise_error
+      end
+    end
     context 'when an unknown item is ordered'
       it 'raises an error' do
-        expect{menu.order('cake')}.to raise_error 'unknown item inputted, please enter an item on the menu'
+        expect{menu.order('cake', 4)}.to raise_error 'unknown item inputted, please enter an item on the menu'
       end
   end
 end
