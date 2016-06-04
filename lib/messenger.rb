f943b01106613030fbaf5a6e0fc625b631ee84c6
Marlon Date-Chong <mdatechong@gmail.com>
2016-06-04 17:10:46 +0100

    spiking alteration

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -2,22 +2,22 @@
 require 'twilio-ruby'
 
 
-class Messenger
+# class Messenger
+#
+#   account_sid = "{{ aAC30b9000efac4bc5220d635af980a7564 }}"
+#   auth_token = "{{ 8f9a5ba6f0a69b3e911942a76aeef4d5 }}"
+#
+#   @client = Twilio::REST::Client.new account_sid, auth_token
+#
+#   @client.messages.create(
+#   from: '+447545253288',
+#   to: '+447545253288',
+#   body: 'Hey there!'
+# )
 
-  account_sid = "{{ aAC30b9000efac4bc5220d635af980a7564 }}"
-  auth_token = "{{ 8f9a5ba6f0a69b3e911942a76aeef4d5 }}"
-
-  @client = Twilio::REST::Client.new account_sid, auth_token
-
-  @client.messages.create(
-  from: '+447545253288',
-  to: '+447545253288',
-  body: 'Hey there!'
-)
-
-  # def send
-  #   p "order confirmed, expected deliver in one hour. Thank you."
-  # end
+  def send
+    p "order confirmed, expected deliver in one hour. Thank you."
+  end
 
 
 end
