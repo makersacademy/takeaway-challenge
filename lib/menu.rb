978b16be599b22b53f60722be2bac4f743c6457f
srMarquinho <sr.marquinho@gmail.com>
2016-06-04 23:20:51 +0100

    first commit, classes sketch

---
+++ lib/menu.rb
@@ -0,0 +1,22 @@
+class Menu
+
+  MENU = {
+    'Love'          => 1.00,
+    'Consideration' => 1.00,
+    'Affection'     => 1.00,
+    'Sensitiveness' => 1.00,
+    'Tenderness'    => 1.00,
+    'Devotion'      => 1.00,
+    'Attraction'    => 1.00,
+    'Passion'       => 1.00,
+    'Admiration'    => 1.00,
+    'Warmth'        => 1.00,
+    'Touch'         => 1.00,
+    'Sympaty'       => 1.00,
+    'Close'         => 1.00,
+    'Confort'       => 1.00,
+  }
+
+  def show
+  end
+end
---
+++ lib/order.rb
@@ -0,0 +1,15 @@
+class Order
+
+  def initialize
+  end
+
+  def add
+  end
+
+  def status
+  end
+
+  def end
+  end
+
+end
