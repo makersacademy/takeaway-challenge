4c762ea7a7d8e40a420849e198d7b25aa19519ff
Apoorva Saxena <apsaxena24@gmail.com>
2016-06-04 19:49:43 +0100

    Refactoring1

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,9 @@
 
-
 describe Menu do
-  it 'should display the complete menu' do
-    expect(subject.show_menu).to eq "chicken"=>3, "fish"=>2, "pork"=>3
+  subject(:menu) {described_class.new}
+  describe '#show_menu' do
+    it 'should display the complete menu' do
+      expect(menu.show_menu).to eq "chicken"=>3, "fish"=>2, "pork"=>3
+    end
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,17 +1,22 @@
 describe Order do
+  
+subject(:order) {described_class.new}
 
   describe '#add' do
+
     context 'valid order' do
       it 'take the order' do
-        subject.add("chicken", 2)
-        expect(subject.order_made).to eq [["chicken", 2]]
+        order.add("chicken", 2)
+        expect(order.order_made).to eq [["chicken", 2]]
       end
     end
+
     context 'invalid order' do
       it 'not take the order' do
-        subject.add("duck", 2)
-        expect(subject.order_made).to eq []
+        order.add("duck", 2)
+        expect(order.order_made).to eq []
       end
     end
+
   end
 end
