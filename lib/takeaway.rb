981112202144b49a958670d6012f5149d29e347f
mtaner <mervetaner88@gmail.com>
2016-06-04 18:40:46 +0100

    refactored code

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,7 +2,7 @@ require_relative 'menu'
 
 class Takeaway
 
-attr_reader :menu, :basket
+	attr_reader :menu, :basket
 
 	def initialize
 		@menu = Menu.new
@@ -15,19 +15,20 @@ attr_reader :menu, :basket
 
 	def order(amount = 1, food)
 		fail "Unfortunately we do not have #{food}. Please select from the menu." unless menu.list.keys.include?(food.to_sym)
-		if basket.include? food
-			"#{food} is already in the basket. Would you like to add another one? (Yes/No)"
-			customer_answer = gets.chomp
-			if customer_answer.downcase == "yes"
-				basket[food] += amount
-				"#{amount} x #{food}(s) added to the basket."
-			else
-				"Thank you for confirming. #{food} is not added to the basket."
-			end
-		else
-			basket[food] += amount
-			"#{amount} x #{food}(s) added to the basket."
-		end
+		basket.include?(food) ? check_with_customer_to_add_food(amount, food) : add_food_to_basket(amount, food)
+	end
+
+	private
+
+	def check_with_customer_to_add_food(amount = 1, food)
+		"#{food} is already in the basket. Would you like to add another one? (Yes/No)"
+		customer_answer = gets.chomp
+		customer_answer.downcase == "yes" ? add_food_to_basket(amount, food) : "Thank you for confirming. #{food} is not added to the basket."
+	end
+
+	def add_food_to_basket(amount = 1, food)
+		basket[food] += amount
+		"#{amount} x #{food}(s) added to the basket."
 	end
 
 end
