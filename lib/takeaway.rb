d2479983bd26d8334d32e44c657fb39ddc33f3d2
a-mellor <alexander.mellor@gmail.com>
2016-06-05 14:41:56 +0100

    added a takeaway spec and class

---
+++ lib/takeaway.rb
@@ -0,0 +1,14 @@
+class TakeAway
+
+  def initialize(menu)
+    @menu = menu
+  end
+
+  def see_menu
+    menu.print
+  end
+
+private
+
+  attr_reader :menu
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,15 @@
+require 'takeaway'
+
+describe TakeAway do
+
+  subject(:takeaway) { described_class.new(menu) }
+
+  let(:menu) { double :menu, print: printed_menu }
+  let(:printed_menu) { "Chicken: £5.00" }
+
+  describe '#see_menu' do
+    it 'allows a customer to see the dishs and prices' do
+      expect(takeaway.see_menu).to eq(printed_menu)
+    end
+  end
+end
