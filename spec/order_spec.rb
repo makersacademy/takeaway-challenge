30892e8fe47537c6a00d64051058f0f3155bbfb4
Thomas Harker <tom.harker@hotmail.co.uk>
2016-06-05 23:55:26 +0000

    Add basic order class

---
+++ spec/order_spec.rb
@@ -0,0 +1,16 @@
+require 'order'
+
+describe Order do
+    subject(:order) {described_class.new}
+    let(:dishes) do
+        {
+            pizza: 3,
+            nuggets: 1
+        }
+    end
+    
+    it 'allows user to select dishes from the menu'
+    order.add(:pizza, 3)
+    order.add(:nuggets, 1)
+    expect(order.dishes).to eq(dishes)
+end
