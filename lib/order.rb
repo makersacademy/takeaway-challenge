a55d20a2d4ec87a6a336840334874658781f3855
Carlos <chdezmar@gmail.com>
2016-06-05 23:23:12 +0100

    progress

--- lib/order.rb
+++ lib/order.rb
@@ -1,13 +1,17 @@
 class Order
 
-	attr_reader :food_list
+	attr_reader :food_list, :menu
 
-	def initialize
+	def initialize(menu)
 		@food_list = {}
+		@menu = menu
 	end
 
 	def add(item, quantity)
+		fail "Item is not on the menu" unless menu.has_item?(item)
 		food_list.store(item,quantity)
 	end
+
+
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,17 +1,31 @@
 require "order"
 
 describe Order do
-	subject(:order) { described_class.new }
+	subject(:order) { described_class.new(menu) }
 
+	let(:menu) { double(:menu) }
 	let(:food_list) do
 		{
 			pizza: 1,
 			Green_Shake: 2
 		}
 	end
+
+	before do
+		allow(menu).to receive(:has_item?).with(:pizza).and_return(true)
+		allow(menu).to receive(:has_item?).with(:Green_Shake).and_return(true)
+	end
+
 	it "selects orders from the menu" do
 		order.add(:pizza, 1)
 		order.add(:Green_Shake, 2)
 		expect(order.food_list).to eq(food_list)
 	end
+
+	it "prevents adding items that are not on the menu" do
+		allow(menu).to receive(:has_item?).with(:chicken).and_return(false)
+		expect{order.add(:chicken,1)}.to raise_error("Item is not on the menu")
+	end
+
+
 end
