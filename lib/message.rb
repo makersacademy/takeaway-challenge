4b8bb2f9fc3ac272fcb83aee3283d70f6a55faf3
Van Le <singularity@vannio.co.uk>
2016-06-05 15:02:01 +0100

    Hound pls

--- lib/message.rb
+++ lib/message.rb
@@ -1,13 +1,13 @@
 class Message
   def generate(total = 0)
-    "Thank you! Your order came to £#{ total }. " +
-    "Our baristas are new, so expect your drinks at " +
-    "#{ get_delivery_time.strftime('%I:%M%P') }"
+    "Thank you! Your order came to £#{ total }. "\
+    "Our baristas are new, so expect your drinks "\
+    "at #{ delivery_time.strftime('%I:%M%P') }"
   end
 
   private
 
-  def get_delivery_time
+  def delivery_time
     Time.new + 3600
   end
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -5,9 +5,9 @@ describe Message do
   let(:total) { 10 }
   let(:delivery_time) { Time.new + 3600 }
   let(:str) do
-    "Thank you! Your order came to £#{ total }. " +
-    "Our baristas are new, so expect your drinks at " +
-    "#{ delivery_time.strftime('%I:%M%P') }"
+    "Thank you! Your order came to £#{ total }. "\
+    "Our baristas are new, so expect your drinks "\
+    "at #{ delivery_time.strftime('%I:%M%P') }"
   end
 
   describe "#generate" do
