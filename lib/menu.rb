15fa8a819d4c967dd950083bc1a4ec1eb807442c
Elena <elena.vilimaite@gmail.com>
2016-06-05 18:51:23 +0100

    order class made

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,13 @@
 class Menu 
-	def initialize
-		@dishes = [['Pizza', 7.99], ['Prawn toast', 1.50], ['Butter chicken', 3], ['The kebab', 4.90], ['The burger', 3.70], ['Sushi', 5], ['Pasta', 8], ['Chicken tikka masala', 4.50], ['Crispy aromatic duck', 5.75], ['Chow mein', 2.99], ['Ice cream', 3], ['Beer', 2]]
-	end
+		DISHES = [['Pizza', 7.99], ['Prawn toast', 1.50], ['Butter chicken', 3], ['The kebab', 4.90], ['The burger', 3.70], ['Sushi', 5], ['Pasta', 8], ['Chicken tikka masala', 4.50], ['Crispy aromatic duck', 5.75], ['Chow mein', 2.99], ['Ice cream', 3], ['Beer', 2]]
 	def view
 		returnString = ''
-		@dishes.each_with_index do |d, i|
+		DISHES.each_with_index do |d, i|
 			returnString += "#{i+1 < 10 ? ' ' : ''}#{i+1}:\t #{d[0]} \t £#{d[1]} \n"
 		end
 		returnString
 	end
 end 
 
-
 m = Menu.new
-p m.view
+puts m.view
--- lib/order.rb
+++ lib/order.rb
@@ -1,2 +1,15 @@
+require_relative 'menu'
+
 class Order
-end 
+	def initialize *args
+		@order = []
+		@total = 0
+		args.each do |a|
+			@order << Menu::DISHES[a-1][0]
+			@total += Menu::DISHES[a-1][1]
+		end
+		p @total.round(2)
+	end
+end
+
+# p o = Order.new(5,8)
