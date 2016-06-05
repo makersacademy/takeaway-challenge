63757aa2c8315d1a4df415f7e6b0d599966dbcd1
LukaszGr <lukasz.grychowski@gmail.com>
2016-06-05 15:23:12 +0100

    Menu rspec improved

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,21 +2,23 @@ class Menu
 
   attr_reader :menu
 
-  def initialize
-    @menu = {"Fries Bucket" => 4,
-             "Onion Ring " => 3,
+  BIG_BURGER_MENU = {"Fries Bucket" => 4,
+             "Onion Ring Ring" => 3,
              "Potato Skin Bag" => 3.50,
              "Big Kahuna Burger" => 7,
              "Triple Cow Burger" => 12,
              "Quadruple Bypass Burger" => 20,
              "Koka Cola" => 3,
-             "7down" => 3,
+             "Seven Down" => 3,
              "Pudweiser" => 4
-            }
+            }.freeze
+
+  def initialize(menu = BIG_BURGER_MENU)
+    @menu = menu
   end
 
   def show_menu
-    @menu.dup
+    @menu.each {|item,  price| "#{item}: £#{price}"}
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,12 +2,20 @@ require 'menu'
 
 describe Menu do
 
+  subject(:menu) {described_class.new}
+
   it "initializes with a menu" do
     expect(subject).to have_attributes(:menu => Hash)
   end
 
   it "responds to show menu" do
     expect(subject).to respond_to :show_menu
+  end
+
+  describe "#show_menu" do
+    it "lists all items on the menu with prices" do
+      expect(menu.show_menu).to eq Menu::BIG_BURGER_MENU
+    end
   end
 
 end
