c98dc745bb0a058284943f5fdfe8845ae30d406e
riyapabari <riya.pabari@gmail.com>
2016-06-05 20:29:35 +0100

    refactored rspec tests

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -9,48 +9,55 @@ describe Takeaway do
 	let(:quantity) {double :quantity}
 	let(:sms_class) {double :sms_class, new: sms}
 	let(:sms) {double :sms, send: ()}
+
+	context 'takeaway order currently has no items' do
 	
-	describe '#view_menu' do
-		it 'allows customer to view menu' do
-			expect(menu).to receive(:view)
-			takeaway.view_menu
+		describe '#view_menu' do
+			it 'allows customer to view menu' do
+				expect(menu).to receive(:view)
+				takeaway.view_menu
+			end
 		end
-	end
 
-	describe '#add_to_order' do
-		it 'adds item to order' do
-			expect(order).to receive(:add).with(item, quantity)
-			takeaway.add_to_order(item, quantity)
+		describe '#add_to_order' do
+			it 'adds item to order' do
+				expect(order).to receive(:add).with(item, quantity)
+				takeaway.add_to_order(item, quantity)
+			end
 		end
-	end
 
-	describe '#view_order' do
-		it 'allows customer to view order' do
-			takeaway.add_to_order(item, quantity)
-			expect(order).to receive(:view)
-			takeaway.view_order
-		end
-	end
+end
 
-	describe '#checkout' do
-		it 'allows customer to check total' do
+	context 'item already added to takeaway order' do
+		before do
 			takeaway.add_to_order(item, quantity)
-			expect(order).to receive(:total).twice
-			takeaway.checkout
 		end
-	end
 
-	describe 'confirm_order' do
-		it 'raises error if wrong amount entered' do
-			takeaway.add_to_order(item, quantity)
-			message = 'Incorrect amount! Please try again.'
-			expect{takeaway.confirm_order('£5')}.to raise_error message
+		describe '#view_order' do
+			it 'allows customer to view order' do
+				expect(order).to receive(:view)
+				takeaway.view_order
+			end
 		end
-		it 'sends an sms if correct amount entered' do
-			takeaway.add_to_order(item, quantity)
-			expect(sms_class).to receive(:new)
-			takeaway.confirm_order('£10')
+
+		describe '#checkout' do
+			it 'allows customer to check total' do
+				expect(order).to receive(:total).twice
+				takeaway.checkout
+			end
 		end
-	end
+
+		describe 'confirm_order' do
+			it 'raises error if wrong amount entered' do
+				message = 'Incorrect amount! Please try again.'
+				expect{takeaway.confirm_order('£5')}.to raise_error message
+			end
+			it 'sends an sms if correct amount entered' do
+				expect(sms_class).to receive(:new)
+				takeaway.confirm_order('£10')
+			end
+		end
 	
+	end
+
 end
