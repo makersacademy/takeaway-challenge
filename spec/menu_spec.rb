f7bd1b10fb16dadb0dc0b43197e3718016113ff1
Thomas Harker <thomasharker@thomass-mbp.home>
2016-06-05 19:20:24 +0100

    Add Menu class

---
+++ spec/menu_spec.rb
@@ -0,0 +1,19 @@
+require 'menu'
+
+describe Menu do
+	subject(:menu) {described_class.new(dishes)}
+	let(:dishes) do
+		{
+			Pizza: 14.00,
+			Nuggets: 5.50
+		}
+	end
+
+	it 'lists the different dishes and prices' do
+		expect(menu.dishes).to eq(dishes)
+	end
+	it 'prints out the list of dishes and prices' do
+		printed_menu = "Pizza £14.00, Nuggets £5.50"
+		expect(menu.print).to eq(printed_menu)
+	end
+end
