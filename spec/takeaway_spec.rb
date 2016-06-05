e08fe1c724e43656c3968ac2ca5ed9e8685cd33d
Thomas Harker <thomasharker@thomass-mbp.home>
2016-06-05 18:04:26 +0100

    Add basic takeaway.rb functionality

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,11 @@
 require 'takeaway'
 
 describe Takeaway do
+	subject(:takeaway) {described_class.new(menu: menu)}
+	let(:menu) { double(:menu, print: printed_menu) }
+	let(:printed_menu) {"Pizza: £14.00"}
+
 	it 'will show the menu' do
-		expect(takeaway.show_menu).to eq("Pizza: £14.00")
+		expect(takeaway.show_menu).to eq(printed_menu)
 	end
 end
