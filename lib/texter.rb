7b4c664a26dc670ae91f11739b74d1b14529b1b6
lomlo <michael.lo@live.co.uk>
2016-06-06 11:02:37 +0100

    ENV variables added

--- lib/texter.rb
+++ lib/texter.rb
@@ -4,17 +4,20 @@ class Texter
 
   def message(time)
       # put your own credentials here
-    account_sid = '***********************'
-    auth_token = '*************************'
+    account_sid = ENV['account_sid']
+    auth_token = ENV['auth_token']
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     @client.account.messages.create({
-      :from => '***************',
-      :to => '***************',
+      :from => ENV['from'],
+      :to => ENV['to'],
       :body => "Thank you! Your order was placed and will be delivered before #{time}",
     })
   end
 
 end
+
+# use this way in command line 
+# export var_name=xxxxxxxxxxxxxx
