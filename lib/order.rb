bbc381f1e131facec355fb132e72afdedaad6145
danielschambers <daniel.s.chambers@gmail.com>
2016-06-06 10:49:06 +0100

    hounding

--- lib/order.rb
+++ lib/order.rb
@@ -23,39 +23,39 @@ class Order
 
   attr_reader :shopping_basket, :bill, :customer_order, :user_input, :order
 
-     def display_current_order
-      shopping_basket.each{ |food| bill << food[:price] }
-      bill_total
-      shopping_basket.each { |x| x[:food] }
-    end
+  def display_current_order
+    shopping_basket.each{ |food| bill << food[:price] }
+    bill_total
+    shopping_basket.each { |x| x[:food] }
+  end
 
-    def customer_order_message
-      p welcome_message
-      @user_input = gets.to_i
-      fail raise_message if @user_input > MENU.length
-      p quantity_check
-      amount = gets.to_i
-      amount.times { @shopping_basket << MENU[user_input] }
-    end
+  def customer_order_message
+    p welcome_message
+    @user_input = gets.to_i
+    fail raise_message if @user_input > MENU.length
+    p quantity_check
+    amount = gets.to_i
+    amount.times { @shopping_basket << MENU[user_input] }
+  end
  
-    def welcome_message
-     "Please place your order: "
-    end
+  def welcome_message
+    "Please place your order: "
+  end
  
-    def quantity_check
-     "How many would you like: "
-   end
+  def quantity_check
+    "How many would you like: "
+  end
  
-    def raise_message
-      "This choice is outside the boundaries of time and space"
-    end
+  def raise_message
+    "This choice is outside the boundaries of time and space"
+  end
  
-    def add_to_the_basket
-      "You have added #{ MENU[@user_input][:food] } to the basket"
-    end
+  def add_to_the_basket
+    "You have added #{ MENU[@user_input][:food] } to the basket"
+  end
  
-   def bill_total
-     bill.reduce(:+)
+  def bill_total
+    bill.reduce(:+)
   end
 
 
