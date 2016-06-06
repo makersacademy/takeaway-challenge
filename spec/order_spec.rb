b762e6d5f49419f8adb2d2025f660003d7979bde
Faisal Mahmood <fahmahmood@gmail.com>
2016-06-06 10:31:11 +0100

    added menu double

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,12 +3,13 @@ require 'order'
 describe Order do
 
 subject(:order) {described_class.new}
+let (:menu) {double(:menu)}
 
   describe '#add' do
 
     context 'item is available' do
       it 'add the order' do
-        expect{order.add("Burger", 2)}.to change {subject.current_order}.by [["Burger", 2]]
+        expect{order.add("Burger", 2)}.to change {order.current_order}.by [["Burger", 2]]
       end
     end
 
