e1ea0a6b5d1c84277814860dbd10454b3db74c80
Adam <adamjbrowner@mgmail.com>
2016-07-15 21:18:36 +0100

    Added menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,9 @@
 class Menu
+
+  attr_reader :menu
+
+  def initialize
+    @menu = {:Chicken_Burger => 4, :Chip => 1.5, :Cheeseburger => 4.5 }
+  end
+  
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,7 @@
 require 'menu'
 
 describe Menu do
-
+  it "initializes with a menu" do
+    expect(subject.menu).to include({:Chicken_Burger => 4, :Chip => 1.5, :Cheeseburger => 4.5})
+  end
 end
