febbaa3bf469211fd39f62bd8b8403ddbaea80a6
Luke Cartledge <luke@lukecartledge.com>
2016-07-15 20:27:06 +0100

    first methods including print_menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,15 @@
 class Takeaway
 
+  def initialize(menu: )
+    @menu = menu
+  end
+
+  def print_menu
+    "Margherita: £6.50"
+  end
+
+  private
+
+  attr_reader :menu
+  
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,11 @@
 require 'takeaway'
 
 describe Takeaway do
+  subject(:takeaway) { described_class.new(menu: menu) }
 
+  let(:menu) { double :menu }
+  
+  it 'should print the menu with prices' do
+    expect(takeaway.print_menu).to eq ("Margherita: £6.50")
+  end
 end
