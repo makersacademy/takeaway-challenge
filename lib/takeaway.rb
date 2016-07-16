bcba1db75b1162c5b28acf9fde8f9642ccf1239f
Sophie Gill <sophie-gill@hotmail.com>
2016-07-16 11:05:12 +0100

    adding fail check to order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,34 @@ require_relative "menu"
 
 class Takeaway
 
-  attr_reader :order
+  attr_reader :basket, :menu
 
-  def print_menu
-    menu = Menu.new.menu
+  def initialize
+    @basket = {}
+    @menu = Menu.new.menu
   end
 
-  def place_order(item)
-    @order = item
+  def print_menu
+    @menu
+  end
+
+  def order(item, quantity = 1)
+    fail "Sorry, that item is not on the menu" unless check_menu(item)
+    @basket.store(item, quantity)
+    confirm(item, quantity)
+  end
+
+  def order_total
+  end
+
+  private
+
+  def check_menu(item)
+    @menu.has_key?(item)
+  end
+
+  def confirm(item, quantity)
+    "#{quantity} #{item} has been added to your basket"
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -12,13 +12,34 @@ describe Takeaway do
     end
   end
 
-  describe '#place_order' do
-    #it 'creates an order hash' do
-    #  expect(takeaway.order()).to(be_a(Hash))
-    #end
-    it 'accepts a dish name and adds it to the order hash' do
-      takeaway.place_order("Omlette")
-      expect(takeaway.order).to(eq("Omlette"))
+  describe '#order' do
+    it 'checks that the item ordered is on the menu' do
+      expect{takeaway.order("Not an item")}.to(raise_error("Sorry, that item is not on the menu"))
+    end
+    it 'accepts a dish name and adds it to the order' do
+      takeaway.order("Omlette")
+      expect(takeaway.basket).to(have_key("Omlette"))
+    end
+    it 'assigns a default of 1 when no quantity is provided' do
+      takeaway.order("Omlette")
+      expect(takeaway.basket).to(have_value(1))
+    end
+    it 'adds the order quantity to the basket' do
+      takeaway.order("Omlette", 2)
+      expect(takeaway.basket).to(have_value(2))
+    end
+    it 'confirms that the item and quantity is in the basket' do
+      expect(takeaway.order("Omlette")).to(eq("1 Omlette has been added to your basket"))
+    end
+  end
+
+  describe '#order_total' do
+    before do
+      takeaway.order("Omlette")
+      takeaway.order("Pancakes", 2)
+    end
+    it 'returns the total price of the order' do
+      expect(takeaway.order_total).to(eq(20))
     end
   end
 
