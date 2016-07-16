b40de1a2d7ee88e7c14e765ab63e4e0a6f47c5cd
Mali Michael <malimccalla@gmail.com>
2016-07-16 14:32:23 +0100

    Adds #select_dish method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,8 +6,14 @@ class Takeaway
     @menu = {"Chicken Gyoza" => 4.20,
              "Katsu Curry & Rice" => 8.50,
              "Nettle Soup" => 2.00,
-             "Sushi Rolls (6 pieces)" => 5.60,
+             "Sushi Roll" => 5.60,
              "Edamame" => 3.00,}
+  end
+
+  def select_dish(item,quanity=1)
+    quanity == 1 ? s = '' : s = 's'
+    quanity == 1 ? ve = 's' : ve = 've'
+    "#{quanity} order#{s} of '#{item}' ha#{ve} been added to your basket."
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,13 +2,34 @@ require 'takeaway'
 
 describe Takeaway do
 
-  describe '#menu' do
+  subject {described_class.new}
 
+  describe '#menu' do
 
     it {is_expected.to respond_to(:menu)}
 
     it 'shows a list of dishes with prices' do
       expect(subject.menu).to be_a(Hash)
+    end
+
+  end
+
+  describe '#select_dish' do
+
+    it 'confirms that your order selection has been added to basket' do
+      expect(subject.select_dish("Edamame",1)).to eq "1 order of 'Edamame' has been added to your basket."
+    end
+
+    it 'allows for plural confirmation if quanity is greater than 1' do
+      expect(subject.select_dish("Katsu Curry & Rice",3)).to eq "3 orders of 'Katsu Curry & Rice' have been added to your basket."
+    end
+
+    it 'deafaults to 1 if no quanity is given' do
+      expect(subject.select_dish("Edamame")).to eq "1 order of 'Edamame' has been added to your basket."
+    end
+
+    it 'stores selections in basket' do
+      expect{subject.select_dish("Edamame",3)}.to change{subject.basket.size}.by(1)
     end
 
   end
