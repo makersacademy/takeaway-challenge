bdbe1ea8d1c67ea93d9ad95026bffbce764ff95b
acsauk <acsauk@gmail.com>
2016-07-16 21:03:40 +0100

    adds select_dish method to allow users to specify which dish to order and compile a meal

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,21 +1,29 @@
 class Menu
 
-  attr_reader :menu
+  attr_reader :menu, :meal
 
   def initialize
-
-  @menu = [ {dish: "Crispy Shredded Beef", price: 5 },
-            {dish: "Chicken Chow Mein", price: 4.50 },
-            {dish: "Beef and Broccoli", price: 4.95 },
-            {dish: "Spring rolls", price: 2.50 },
-            {dish: "Crispy Duck Pancakes", price: 6.75 },
-            {dish: "Chicken Yuk Sung" , price: 5.10 },
-            {dish: "Pork Yuk Sung", price: 4.85 },
-            {dish: "Singapore Noodles" , price: 3.95 }
-          ]
+    @menu = [ {dish_no: 1, dish_name: "Crispy Shredded Beef", dish_price: 5 },
+              {dish_no: 2, dish_name: "Chicken Chow Mein", dish_price: 4.50 },
+              {dish_no: 3, dish_name: "Beef and Broccoli", dish_price: 4.95 },
+              {dish_no: 4, dish_name: "Spring rolls", dish_price: 2.50 },
+              {dish_no: 5, dish_name: "Crispy Duck Pancakes", dish_price: 6.75 },
+              {dish_no: 6, dish_name: "Chicken Yuk Sung" , dish_price: 5.10 },
+              {dish_no: 7, dish_name: "Pork Yuk Sung", dish_price: 4.85 },
+              {dish_no: 8, dish_name: "Singapore Noodles" , dish_price: 3.95 }
+            ]
+    @meal = []
   end
 
   def display_menu
+    menu
+  end
+
+  def select_dish(dish_no)
+    @meal << menu[dish_no - 1]
+  end
+
+  def order_meal
 
   end
 
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,8 +6,49 @@ describe Menu do
     it "#display_menu" do
       expect(subject).to respond_to(:display_menu)
     end
+    it "#select_dish" do
+      expect(subject).to respond_to(:select_dish).with(1).argument
+    end
+    it "#order_meal" do
+      expect(subject).to respond_to(:order_meal)
+    end
   end
 
-  
+  context "#display_menu" do
+    it "returns @menu" do
+      menu = subject.menu
+      expect(subject.display_menu).to eq menu
+    end
+  end
 
+  context "#select_dish" do
+    it "should start with an empty meal array" do
+      expect(subject.meal).to be_empty
+    end
+
+    let(:dish_no)    { double :dish_no }
+    let(:dish_name)  { double :dish_name }
+    let(:dish_price) { double :dish_price}
+    let(:dish_info)  { { dish_no: dish_no, dish_name: dish_name, dish_price: dish_price} }
+    let(:meal)       { [dish_info, dish_info, dish_info] }
+
+    it "should display dish and price when user selects_dish" do
+      expect(subject.select_dish(dish_no)).to eq dish_info
+    end
+
+    it "should add selected dish to meal" do
+      expect{ subject.select_dish(dish_no) }.to change{ meal.length }.by(1)
+    end
+
+    it "should create an array of meal numbers" do
+      subject.select_dish(dish_no)
+      subject.select_dish(dish_no)
+      subject.select_dish(dish_no)
+      expect(subject.meal).to eq meal
+    end
+  end
+
+  context "#order_meal" do
+     #it "returns"
+  end
 end
