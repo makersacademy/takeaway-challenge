2a1d56e25ff5923a886ca287e1f6358705ab035b
Jose <jcopoviking@outlook.com>
2016-07-16 22:12:00 +0100

    tidying up alignments'

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,11 +7,11 @@ describe Menu do
   describe '#item_list' do
     it 'should be a hash' do
       expect(subject.item_list).to be_an_instance_of(Hash)
-      end
-  end
+    end
 
     it "has a list of menu items" do
       expect(menu.item_list).to include(:balti => 5.00, vindaloo: 4.50)
+    end
   end
 
   describe "#print_menu" do
