2fed79cb53ada666421bc428abe67daaa1c2d557
Richard Bates <richo225@gmail.com>
2016-07-16 20:15:42 +0100

    Add TakeAway class and see_menu method. Complete 1st user story.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,22 @@
 class TakeAway
+
+  attr_reader :menu
+
+  def initialize
+    @menu = {
+      :wonton_soup => 2,
+      :spring_rolls => 2,
+      :kung_po => 5,
+      :fried_squid => 6,
+      :chow_mein => 9,
+      :roasted_duck => 9
+    }
+  end
+
+  def see_menu
+    @menu
+  end
+
 end
 
 # feature test
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,11 +3,22 @@ require "takeaway"
 describe TakeAway do
   subject(:takeaway) {TakeAway.new}
 
+  describe "#initialize" do
+    it "begins with a menu" do
+      expect(takeaway.menu).to_not be nil
+    end
+  end
+
   describe "#see_menu" do
     it "allows customer to see menu" do
       expect(takeaway).to respond_to(:see_menu)
     end
+
+    it "displays the menu list" do
+      expect(takeaway.see_menu).to eq takeaway.menu
+    end
   end
+
 end
 
 # As a customer
