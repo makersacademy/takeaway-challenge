7fbdc6224d37d0e60f1748abd321196b94caca93
Jack Oddy <jackoddy91@gmail.com>
2016-07-17 12:17:25 +0100

    allows entry of phone number for confimation

--- lib/texter.rb
+++ lib/texter.rb
@@ -3,7 +3,7 @@ require 'twilio-ruby'
 class Texter
   def self.text(number)
 
-    account_sid = 'AC4aae672be2a82fc5580e5401a6ade1a3'
+   account_sid = 'AC4aae672be2a82fc5580e5401a6ade1a3'
    auth_token = '715791741fc525d16ae00a40b8dd0114'
    twilio_number = '+447481345653'
    confirmation = "Thank you, your order will be delivered by 18:52."
@@ -12,7 +12,7 @@ class Texter
 
    @client.account.messages.create(
    from: twilio_number,
-   to: '07854015421',
+   to: number,
    body: confirmation
    )
  end
