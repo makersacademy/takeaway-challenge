c956ca8598563c9db25dd52307b60b12a5d28714
Arunas Skirius <arukomp@gmail.com>
2016-07-17 20:29:27 +0100

    fixed a few styling issues to make the Hound happy

--- lib/twilio_handler.rb
+++ lib/twilio_handler.rb
@@ -7,12 +7,12 @@ class TwilioHandler
   def self.send_message
     @client = Twilio::REST::Client.new ENV['ACC_SID'], ENV['AUTH_TOKEN']
     time = (Time.now + (60 * 60)).strftime("%H:%M")
-    @client.account.messages.create({
-        :to => ENV['TWILIO_TO_NUMBER'],
-        :from => ENV['TWILIO_FROM_NUMBER'],
-        :body => "Thank you! Your order was placed and will be delivered " \
+    @client.account.messages.create(
+        to: ENV['TWILIO_TO_NUMBER'],
+        from: ENV['TWILIO_FROM_NUMBER'],
+        body: "Thank you! Your order was placed and will be delivered " \
                  "before #{time}",
-    })
+    )
   end
 
 end
