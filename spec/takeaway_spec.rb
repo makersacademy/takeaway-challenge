d4acb8cf80398fcd86c61281b35f5b3d0194664f
noby <n.fujioka@abacusmaths.info>
2016-07-17 15:55:42 +0100

    removes some let lines in Takeaway class.

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,29 +3,27 @@ describe Takeaway do
   let(:takeaway) { described_class.new }
   let(:list) { Takeaway::LIST }
   let(:order_quantities) { {"mushy peas" => 1, "kebab" => 1, "fish" => 1, "chips" => 2} }
-  let(:my_total) { 16 }
-  let(:my_wrong_total) { 10 }
-  describe '#show_menu' do
 
+  describe '#show_menu' do
     it 'provides a list of dishes with prices' do
       expect(takeaway.show_menu).to eq list
     end
   end
   context 'When order is placed' do
     before do
-      takeaway.take_order(order_quantities, my_total)
+      takeaway.take_order(order_quantities, 16)
     end
     describe '#take_order' do
       it "records the list of dishes with their quantities" do
         expect(takeaway.order_quantities).to eq order_quantities
       end
       it "records my total" do
-        expect(takeaway.my_total).to eq my_total
+        expect(takeaway.my_total).to eq 16
       end
     end
     describe '#total' do
       it "works out the total price of the dishes ordered" do
-        expect(takeaway.total).to eq my_total
+        expect(takeaway.total).to eq 16
       end
     end
     context 'After the total price is calculated' do
@@ -46,7 +44,7 @@ describe Takeaway do
   end
   context "If total is not equal to my total" do
     it 'returns an error' do
-      takeaway.take_order(order_quantities, my_wrong_total)
+      takeaway.take_order(order_quantities, 10)
       takeaway.total
       expect{ takeaway.match_total }.to raise_error("Your sum is not correct!")
     end
