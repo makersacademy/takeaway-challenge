9d097f7ae896a4502fe90d2629125bed424d8f03
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-07-17 17:50:48 +0000

    method addedfor ordering multiple dishes

--- lib/order.rb
+++ lib/order.rb
@@ -2,4 +2,14 @@ require_relative 'menu'
 
 class Order
     
+    attr_accessor :basket
+    
+    def initialize
+        @basket = []
+    end
+    
+    def selection (order, number)
+        @basket.push(order,number)
+    end
+    
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,6 +4,6 @@ describe Order do
     subject (:order) {Order.new}
     it 'adds user order to basket' do
         order.selection("Bruschetta", 5)
-        expect(order.basket).to eq 5.times.({:"Bruschetta" => 2.00})
+        expect(order.basket).to eq (5*({:"Bruschetta" => 2.00}))
     end
 end
