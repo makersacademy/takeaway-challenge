ed89b7253b3dedfe1bcbb3befc3827994f5da0f9
dan-cl <djclissold@yahoo.co.uk>
2016-07-17 18:19:40 +0100

    Readme and Menu updated

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,8 +5,15 @@ attr_reader :list
 
   def initialize
 
-@list = { "eggs" => 2.99,
-          "bacon" => 5.00 }
+@list = { "Beef Taco" => 4.99,
+          "Chicken Tace" => 4.50,
+          "Fish Taco" => 4.99,
+          "Vege Taco" => 3.99,
+          "Chicken Quesadillas" => 5.50,
+          "Beef Quesadillas" => 5.99,
+          "Fish Quesadillas" => 5.99,
+          "Vege Quesadillas" => 5.50,
+          "Nacho Grande" => 4.00}
   end
 
 
