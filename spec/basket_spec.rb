83c15cc58011b173336669b7383cf240ba6f9cd1
Mali Michael <malimccalla@gmail.com>
2016-07-17 13:06:31 +0100

    Add tests for basket

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -3,7 +3,9 @@ require 'basket'
 
 describe Basket do
 
+  subject {described_class.new}
   let(:takeaway) {double(:takeaway)}
+  let(:item) {{name: "Edamame", quantity: 3, price: 6.00}}
 
   describe '#total_price' do
 
@@ -11,6 +13,26 @@ describe Basket do
       expect(subject.total_price).to eq(0)
     end
 
+    it 'sums the total of all items in the basket' do
+      subject.add(item)
+      subject.add(item)
+      expect(subject.total_price).to eq 12.00
+    end
+
+  end
+
+  describe '#add' do
+
+    it {is_expected.to respond_to(:add).with(1).argument}
+
+    it 'adds items to the basket' do
+      subject.add(item)
+      expect(subject.items).to include item
+    end
+
+    it 'increments item count' do
+      expect{subject.add(item)}.to change{subject.item_count}.by(3)
+    end
   end
 
 end
