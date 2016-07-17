8488574f9e45defd3fb430497b1f158f9ccb9746
Arunas Skirius <arukomp@gmail.com>
2016-07-17 10:53:32 +0100

    Added a name and a price to the Dish class

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,6 +2,14 @@ require 'dish'
 
 describe Dish do
 
+  subject { described_class.new(name: "Caesar Salad", price: 3) }
 
+  it 'has a name' do
+    expect(subject.name).to eq "Caesar Salad"
+  end
+
+  it 'has a price' do
+    expect(subject.price).to eq 3
+  end
 
 end
