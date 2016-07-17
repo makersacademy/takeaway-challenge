f88ac472ef6057fc7a97d806b3a71cc3851632ea
Jose <jcopoviking@outlook.com>
2016-07-17 18:37:00 +0100

    Adds basic order class and tests.

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,14 @@
 class Order
+  attr_accessor :dish, :quantity, :order
+
+  def initialize
+    @dish = ""
+    @quantity = 0
+    @order = []
+  end
+
+  def add_to_order
+    quantity.times {order << dish}
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,6 +1,14 @@
 require 'order'
+require 'menu'
+
 describe Order do
-  describe '#capture_order' do
-    it 'prompts for a dish'
+
+  let(:new_order) {Order.new}
+
+  it "allows the selection of several dishes to be added to an order" do
+    new_order.dish = "balti"
+    new_order.quantity = 4
+    new_order.add_to_order
+    expect(new_order.order.count).to eq 4
   end
 end
