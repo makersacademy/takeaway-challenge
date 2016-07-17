b1105dfe86b07ab02cff867821b7d543d04ef3e6
Mali Michael <malimccalla@gmail.com>
2016-07-17 18:13:02 +0100

    Add sms spec

---
+++ spec/sms_spec.rb
@@ -0,0 +1,20 @@
+require 'sms'
+
+describe Sms do
+
+  subject {described_class.new(15.00)}
+  let (:time) {double(:time, hour: 18, min: 45)}
+
+  describe '#expected_delivery_time' do
+    it 'formats the time to 45 mins later than the current time' do
+      expect(subject.expected_delivery_time(time)).to eq "19:30"
+    end
+  end
+
+  describe '#send' do
+    it 'sends a confirmation text' do
+      expect{subject.send}.to change{subject.sent}.from(false).to(true)
+    end
+  end
+
+end
