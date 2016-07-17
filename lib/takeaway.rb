59436f151da7cd94a84f99a3a5fbafd47d2b0d47
nikklein <objcoding@gmail.com>
2016-07-17 13:08:15 +0100

    Add place an order, print menu, basket methods

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,26 @@
 class Takeaway
 
-  def menu
-    @menu = {
-      "King Prawn Noodles" => 5.80,
-      "Chiken Fried Rice" => 3.90,
-      "Beef Fried Rice" => 3.90,
-      "Duck Fried Rice" => 5.60
-    }
+def initialize
+  @menu = {
+    "King Prawn Noodles" => 5.80,
+    "Chicken Fried Rice" => 3.90,
+    "Beef Fried Rice" => 3.90,
+    "Duck Fried Rice" => 5.60
+  }
+  @basket = []
+end
 
+  def print_menu
     @menu.each{|k,v| puts "#{k} - £#{v}0"}
+  end
+
+  def place_an_order(dish, qnty)
+    @basket << {dish => qnty}
+
+  end
+
+  def basket
+    message = "Items in your basket:"
+    "#{message} #{@basket.each{|x| x.each{|k,v| puts "#{k} - #{v} pcs"}}}"
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,12 +2,27 @@ require 'takeaway'
 
 describe Takeaway do
 
-  let(:subject) {Takeaway.new}
+  let(:subject) {described_class.new}
+  let (:dish) {double :dish}
+  let (:qnty) {double :qnty}
+  it { is_expected.to respond_to(:print_menu) }
 
-  it { is_expected.to respond_to(:menu) }
+describe "#place_an_order" do
+  it { is_expected.to respond_to(:place_an_order).with(2).argument }
+  end
 
-#  describe '#menu' do
-#    it 'when customer want to order it shows the menu'
-#    expect(subject.menu).
-  #end
+
+describe '#basket' do
+  
+  it 'stores an order' do
+    subject.place_an_order(:dish, :qnty)
+    expect(subject.basket).to eq "Items in your basket: #{[{:dish => :qnty}]}"
+end
+ it 'can stores a number of orders' do
+   subject.place_an_order(:dish, :qnty)
+   subject.place_an_order(:dish, :qnty)
+   message = "Items in your basket: #{[{:dish => :qnty}, {:dish=>:qnty}]}"
+   expect(subject.basket).to eq message
+ end
+end
 end
