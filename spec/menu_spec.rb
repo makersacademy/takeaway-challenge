ddeaa14cf275912adfb9be9f97f5b27c56834572
Rylan Gooch <rylangooch@gmail.com>
2016-07-17 22:25:34 +0100

    tweaked previous commit

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -17,20 +17,3 @@ describe Menu do
     end
   end
 end
-
-
-
-# require 'menu'
-#
-# describe Menu do
-#     let(:menu) { Menu.new }
-#
-#   it 'has a list of dishes with pricing' do
-#     expect(menu.dishes).to be_a(Hash)
-#   end
-#
-#   it "has 3 items on the menu" do
-#     expect(menu.dishes.length).to eq(3)
-#   end
-#
-# end
