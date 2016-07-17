7a90444b475d582711281aeb7cc36f061792b1a5
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-17 17:36:02 +0100

    Raising error correction

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,9 +2,8 @@ require 'order'
 
 describe Order do
   let(:menu){double :menu, :dish_list => {"pizza"=>3}}
-  let(:order){described_class.new(menu)}
 
-  context 'when adding dish'do
+  context 'ordering dish'do
     it 'adds dish to order' do
       subject.add_to_order("pizza",2)
       expect(subject.basket).to eq "pizza" => 2
@@ -17,6 +16,9 @@ describe Order do
     it 'sets quantity to 1 by default' do
       subject.add_to_order("chips")
       expect(subject.basket).to eq "chips" => 1
+    end
+    it 'raises an error when dish not on menu' do
+      expect{subject.add_to_order("cheese")}.to raise_error 'No such dish on menu'
     end
   end
 
