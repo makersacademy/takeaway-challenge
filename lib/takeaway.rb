1b23c0c6eed54a1540cb0bc0aa35958b531f4e05
Arunas Skirius <arukomp@gmail.com>
2016-07-17 20:56:36 +0100

    Moved Twilio SMS sending to the Takeaway class file

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,6 @@
-require_relative 'twilio_handler'
+require 'dotenv'
+require 'twilio-ruby'
+Dotenv.load
 
 class Takeaway
 
@@ -48,7 +50,14 @@ class Takeaway
   end
 
   def confirm_order
-    TwilioHandler.send_message
+    @client = Twilio::REST::Client.new ENV['ACC_SID'], ENV['AUTH_TOKEN']
+    time = (Time.now + (60 * 60)).strftime("%H:%M")
+    @client.account.messages.create(
+        to: ENV['TWILIO_TO_NUMBER'],
+        from: ENV['TWILIO_FROM_NUMBER'],
+        body: "Thank you! Your order was placed and will be delivered " \
+                 "before #{time}",
+    )
     "Payment successful! You will receive a text message shortly to confirm" \
     " the order."
   end
--- spec/twilio_handler_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'twilio_handler'
-
-describe TwilioHandler do
-
-  it 'responds to ".send_message"' do
-    expect(described_class).to respond_to(:send_message)
-  end
-
-end
