7af16c0306df0112d961e203761aea19e50750bf
Thady Condon <thadycondon@hotmail.com>
2016-07-17 09:20:33 +0100

    Created unit test for starters and main course. They are loaded into a hash from csv file. Tests pass

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,12 +4,17 @@ require 'csv'
 class Menu
 
 attr_accessor :starters
+attr_accessor :main_course
 
 
   def initialize
-    CSV.open("/Users/thadycondon/Documents/Projects/takeaway-challenge/lib/dishes.csv", :row_sep => :auto, :col_sep => ",") do |dishes|
+    CSV.open("/Users/thadycondon/Documents/Projects/takeaway-challenge/lib/starters.csv", :row_sep => :auto, :col_sep => ",") do |dishes|
     dishes.each { |dish, price| @starters = {dish.to_sym => price} }
+    end
+
+    CSV.open("/Users/thadycondon/Documents/Projects/takeaway-challenge/lib/main_courses.csv", :row_sep => :auto, :col_sep => ",") do |dishes|
+    dishes.each { |dish, price| @main_course = {dish.to_sym => price} }
   end
 
-  end
+end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,10 +6,18 @@ describe Menu do
 describe '#initialize' do
   subject {Menu.new}
   context 'when menu is created' do
-    it 'holds a list of dishes and prices in a hash' do
+    it 'holds a list of starters and prices in a hash' do
       puts "=================#{subject.starters}================"
       expect(subject.starters).to include(TomKaKai: "£6.50")
     end
   end
+
+  context 'when menu is created' do
+    it 'holds a list of main courses and prices in a hash' do
+      puts "=================#{subject.main_course}================"
+      expect(subject.main_course).to include(PhadThai: "£9.50")
+    end
+  end
+
 end
 end
