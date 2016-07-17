ad21d57a43da60ef504260ec1e946db1dcc0523d
nikklein <objcoding@gmail.com>
2016-07-17 14:07:24 +0100

    Update basket method, create a hash instead of array

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -4,19 +4,19 @@ class Takeaway
 
 def initialize(menu = Menu.new)
   @menu = menu
-  @basket = []
+  @basket = {}
 end
 
 
 
 def place_an_order(dish, qnty = 1)
     fail 'Item is not listed in the menu' if !@menu.menu.has_key? dish
-    @basket[dish] += qnty
-    "<#{dish} x #{qnty}> added to your basket"
+    @basket[dish] = qnty
+    "<#{dish} x #{qnty} x £#{@menu.menu[dish]}0> added to your basket"
 end
 
   def basket
     message = "Items in your basket:"
-    "#{message} #{@basket.each{|x| x.each{|k,v| puts "#{k} - #{v} pcs"}}}"
+    "#{message} #{@basket.each{|k, v| puts "#{k} - #{v} pcs"}}}"
   end
 end
