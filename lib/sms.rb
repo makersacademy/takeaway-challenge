e3f3cb8d3d22da144533bc7783ff816e98dc91d1
Mali Michael <malimccalla@gmail.com>
2016-07-17 18:12:14 +0100

    Change class vars to instance vars

--- lib/sms.rb
+++ lib/sms.rb
@@ -3,35 +3,37 @@ require_relative 'takeaway'
 
 class Sms
 
-  @@account_sid = ENV['TWILIO_ACCOUNT_SID']
-  @@auth_token = ENV['TWILIO_AUTH_TOKEN']
+  attr_reader :sent
+
+  @account_sid = ENV['TWILIO_ACCOUNT_SID']
+  @auth_token = ENV['TWILIO_AUTH_TOKEN']
 
   def initialize(total)
     @total = total
+    @account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @auth_token = ENV['TWILIO_AUTH_TOKEN']
+    @sent = false
   end
 
-  def set_time # ewww
-    now = Time.now
-    min = now.min + 45
-    hour = now.hour
-    if min > 60
-      min = min - 60
-      hour += 1
-      if min < 10
-        min *= 10
-      end
+  def expected_delivery_time(time) # ewww, whyy??
+    mins = time.min + 45
+    hours = time.hour
+    if mins > 60
+      mins = mins - 60
+      hours += 1
+      mins *= 10 if mins < 10
     end
-    "#{hour}:#{min}"
+    hours < 10 ? zero = 0 : zero = ''
+    "#{zero.to_s}#{hours}:#{mins}"
   end
 
   def send
-    set_time
-    @client = Twilio::REST::Client.new @@account_sid, @@auth_token
-
+    @client = Twilio::REST::Client.new @account_sid, @auth_token
     @client.account.messages.create(
       from: '+441629690047',
       to: ENV['MY_NUMBER'],
-      body: "Thank you for ordering from M.A sushi, Your order came to a total of £#{@total}0, you can expect your food to be with you by #{set_time}."
+      body: "Thank you for ordering from M.A Sushi. Your order came to a total of £#{@total}0, you can expect your food to be with you by #{expected_delivery_time(Time.now)}."
     )
+    @sent = true
   end
 end
