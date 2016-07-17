d99d4465199edf15ad198c3ef0131bdcec32c66b
BartJudge <BartJudge@gmail.com>
2016-07-17 23:24:28 +0100

    user story one - complete

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,21 +1,20 @@
 
 class Menu
-attr_reader :meals
-  def initalize
 
-  @meals= {
-    :pasta => 4.50,
-    :curry => 3.70,
-    :burger => 2.80,
+  attr_reader :meals
+
+  def initialize
+    @meals = {
+    pasta:  4.50,
+    curry:  3.70,
+    burger:  2.80
     }
   end
 
   def display_meals
-
     meals.map do |dish, price|
-    "%s £%.2f" % [dish.to_s.capitalize, price]
-  end.join(", ")
-
+      "%s £%.2f" % [dish.to_s.capitalize, price]
+    end.join(", ")
   end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,21 +1,21 @@
 require 'restaurant'
 require 'menu'
 
-  describe Restaurant do
+describe Restaurant do
 
-    context '#show_menu' do
-      subject(:restaurant) { described_class.new }
-      subject(:menu) { described_class.new}
+  context '#show_menu' do
+    subject(:restaurant) { described_class.new }
+    subject(:menu) { Menu.new}
 
 
-      it 'should be able to show the customer a menu including prices' do
-
+    it 'should be able to show the customer a menu including prices' do
       expect(restaurant.show_menu).to eq (menu.display_meals)
-      end
+    end
 
-    # describe "#order"
-    # it 'should allow a customer to order a number of items for the menu' do
-    #
-    # end
+   describe "#order"
+   it 'should allow a customer to order a number of items for the menu' do
+
+   end
+   
   end
-  end
+end
