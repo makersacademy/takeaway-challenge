0f3c39a59256d9d1c4dc510425127db2ac98573a
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-07-17 17:47:38 +0000

    Rspec of order selection complete

--- lib/order.rb
+++ lib/order.rb
@@ -2,24 +2,4 @@ require_relative 'menu'
 
 class Order
     
-    attr_reader :choices
-    
-    def initialize
-        @choices = []
-    end
-    
-    def decision
-        puts "Please choose an item from the menu"
-        choice = gets.chomp
-        menu = Menu.new.dish_list
-        picks = menu.select { |dish, price| dish.to_s.match(/^choice\d+/) }
-        @choices << picks
-    end
-    
-    def total
-        @choices.map { |h| h[:"b"] }.sum
-    end
 end
-menu = Menu.new.dish_list
-order = Order.new.decision
-order.total
---
+++ spec/order_spec.rb
@@ -0,0 +1,9 @@
+require 'order.rb'
+
+describe Order do
+    subject (:order) {Order.new}
+    it 'adds user order to basket' do
+        order.selection("Bruschetta", 5)
+        expect(order.basket).to eq 5.times.({:"Bruschetta" => 2.00})
+    end
+end
