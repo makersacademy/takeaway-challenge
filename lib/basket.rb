4386851bc329712f38e9aa9e477831a1bbe27151
Mali Michael <malimccalla@gmail.com>
2016-07-17 15:17:20 +0100

    Add #clear to reset basket after an order has been placed

--- lib/basket.rb
+++ lib/basket.rb
@@ -3,15 +3,19 @@ class Basket
   attr_reader :items, :item_count, :total_price
 
   def initialize
-    @item_count = 0
-    @total_price = 0
-    @items = []
+    clear
   end
 
   def add(item)
     @item_count += item[:quantity]
     @items << item
     @total_price += item[:price]
+  end
+
+  def clear
+    @item_count = 0
+    @total_price = 0
+    @items = []
   end
 
 end
