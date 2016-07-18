923224fda61ccb875d91844ac80a6e1a0f0393b7
6eff <6eff@mail.ru>
2016-07-18 11:57:37 +0300

    support for twilio

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'pry'
 require 'webmock/rspec'
 require 'vcr'
 
+
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.allow_http_connections_when_no_cassette = true
--- spec/test_twilio_spec.rb
+++ spec/test_twilio_spec.rb
@@ -1,12 +1,14 @@
-=begin
+# =begin
+require 'spec_helper'
+
 feature 'twilio confirmation message' do
   scenario 'user receives order confirmation sms' do
     VCR.use_cassette('twilio order confirmation') do
-      visit_root_path
-      fill_in 'twilio order confirmation from', with: 'XXX'
+      visit root_path
+      fill_in 'twilio order confirmation from', with: 'order was placed'
       expect(current_path).to eq(twilio_request_message_success_path)
     end
   end
 end
 
-=end
+# =end
