4358ddc1243dee0bd691d8c92c115ed4d5a356c6
sus111 <sgraham535@gmail.com>
2016-07-18 10:06:25 +0100

    time's up

--- spec/messenger_spec.rb
+++ spec/messenger_spec.rb
@@ -2,22 +2,25 @@ require 'messenger'
 
 describe Messenger do
 
-subject(:text) { described_class.new(:codes) }
-let(:codes) { {account_sid: "chars", auth_token: "token"} }
-let(:account_sid) { double :account_sid}
-let(:auth_token) { double :auth_token}
-let(:delivery_time) { Time.now + 3600 }
-let(:body) { "Thank you! Your order will be delivered before #{ delivery_time }." }
-
-  before do
-    allow(subject).to receive(:send_message)
-  end
-
-  describe "#send_message" do
-    it 'sends a confirmation of delivery time' do
-    allow(text).to receive(:send_message).and_return(:body)
-  end
-end
-
+it { is_expected.to respond_to :send_message }
 
 end
+# subject(:text) { described_class.new(codes) }
+# let(:codes) { {account_sid: "chars", auth_token: "token"} }
+# let(:delivery_time) { Time.now + 3600 }
+# let(:body) { "Thank you! Your order will be delivered before #{ delivery_time }." }
+#
+#   before do
+#     allow(subject).to receive(:send_message)
+#   end
+#
+#   describe "#send_message" do
+#     it 'sends a confirmation of delivery time' do
+#     allow(text).to receive(:send_message).and_return(body)
+#   end
+# end
+#
+# end
+
+# let(:account_sid) { double :account_sid}
+# let(:auth_token) { double :auth_token}
