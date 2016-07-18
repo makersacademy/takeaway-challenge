f46263436fc47a866d8f1db8754999978019249d
hannako <jessjones1@gmail.com>
2016-07-18 13:38:24 +0100

    reinstates twilio

--- lib/message.rb
+++ lib/message.rb
@@ -1,21 +1,21 @@
-# require 'twilio-ruby'
-#
-#
-# class Message
-#
-#   def send_confirmation
-#
-#      account_sid = '<AC71091e45b8b259664e487539658c839d>'
-#      auth_token = '<252d0d6447352f65aacd42e6870395bb>'
-#
-#      # set up a client to talk to the Twilio REST API
-#      @client = Twilio::REST::Client.new account_sid, auth_token
-#
-#      @client.account.messages.create({
-#          :to => '07756685861>',
-#          :from => '<441631402037>',
-#          :body => "Thank you! Your order was placed and will be delivered before #{((Time.new.hour) + 1)}:#{Time.new.min}"
-#      })
-#
-#   end
-# end
+require 'twilio-ruby'
+
+
+class Message
+
+  def send_confirmation
+
+     account_sid = '<AC71091e45b8b259664e487539658c839d>'
+     auth_token = '<252d0d6447352f65aacd42e6870395bb>'
+
+     # set up a client to talk to the Twilio REST API
+     @client = Twilio::REST::Client.new account_sid, auth_token
+
+     @client.account.messages.create({
+         :to => '07756685861>',
+         :from => '<441631402037>',
+         :body => "Thank you! Your order was placed and will be delivered before #{((Time.new.hour) + 1)}:#{Time.new.min}"
+     })
+
+  end
+end
