22170c44bb9b5d1f1206656f1e52c6138766990a
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-18 10:59:34 +0100

    Fixed twilio message sending

--- lib/message.rb
+++ lib/message.rb
@@ -4,14 +4,16 @@ Dotenv.load
 
 class Message
 
+  attr_reader :send_text
+
   def delivery_time
     time = Time.new + 3600
     time.strftime("%H:%M")
   end
 
   def send_text
-    account_sid = ENV['ACCOUNT_SID'],
-    auth_token = ENV['AUTH_TOKEN'],
+    (account_sid = ENV['ACCOUNT_SID'])
+    (auth_token = ENV['AUTH_TOKEN'])
     @client = Twilio::REST::Client.new(account_sid, auth_token)
     @client.account.messages.create(
     from: ENV['TWILIO_NO'],
