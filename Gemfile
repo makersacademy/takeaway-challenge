b921ab758d9af475688d829bb4a07a965afb8ec0
hannako <jessjones1@gmail.com>
2016-07-18 13:31:19 +0100

    comments out tiwilio

--- Gemfile
+++ Gemfile
@@ -5,4 +5,4 @@ gem 'rake'
 gem 'rspec'
 gem 'coveralls', require: false
 gem 'capybara'
-gem 'twilio-ruby'
+# gem 'twilio-ruby'
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,13 +1,13 @@
-require 'message'
-require 'twilio-ruby'
-
-describe Message do
-
-  subject (:message) {described_class.new}
-
-  xit 'sends a confirmation sms when the order is paid for' do
-    expect(message.send_confirmation).to eq "Thank you! Your order was placed and will be delivered before #{((Time.new.hour) + 1)}:#{Time.new.min}"
-  end
-
-
-end
+# require 'message'
+# require 'twilio-ruby'
+#
+# describe Message do
+#
+#   subject (:message) {described_class.new}
+#
+#   xit 'sends a confirmation sms when the order is paid for' do
+#     expect(message.send_confirmation).to eq "Thank you! Your order was placed and will be delivered before #{((Time.new.hour) + 1)}:#{Time.new.min}"
+#   end
+#
+#
+# end
