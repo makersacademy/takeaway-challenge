755ef68e0ac10a34fe4c7c84147a26041ee8202e
Adam <adamjbrowner@mgmail.com>
2016-07-18 08:42:14 +0100

    Refactored tests

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,9 +6,9 @@ describe Order do
     @order = Order.new([:Chicken_Burger, :Chips])
   end
   it "totals up the price of the order" do
-    expect(@order.total_up([:Chicken_Burger, :Chips])).to eq 5.5
+    expect(@order.total_up([:Chicken_Burger, :Chips])).to eq(5.5)
   end
   it "returns a confirmation message" do
-    expect(@order.complete).to eq "Order complete! Your order of Chicken_Burger, Chips will arrive before #{Time.now + (60 * 60)}"
+    expect(@order.complete).to eq("Order complete! Your order of Chicken_Burger, Chips will arrive before #{Time.now + (60 * 60)}")
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,10 +3,10 @@ require 'takeaway'
 describe Takeaway do
   let(:menu) {double(:menu)}
   it "Gives the user a menu on request" do
-    expect(subject.menu).to eq ({:Chicken_Burger => 4, :Chips => 1.5, :Cheeseburger => 4.5, :Kebab => 3.5})
+    expect(subject.menu).to eq({:Chicken_Burger => 4, :Chips => 1.5, :Cheeseburger => 4.5, :Kebab => 3.5})
   end
   it 'checks to see whether the price given matches the correct price' do
     subject.order(5.5, :Chicken_Burger, :Chips)
-    expect(subject.price).to eq 5.5
+    expect(subject.price).to eq(5.5)
   end
 end
