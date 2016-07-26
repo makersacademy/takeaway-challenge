16dfa2fdd5d6497402412b6daecd9f1446e162dc
JAstbury <jess.astbury@hotmail.com>
2016-07-26 23:17:07 +0100

    Sorts out environment variables

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.3.1'
 
+gem 'dotenv'
 gem 'rake'
 gem 'rspec'
 gem 'coveralls', require: false
--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -1,16 +1,18 @@
 require 'twilio-ruby'
+require 'dotenv'
+Dotenv.load
 
 class Confirmation
 
   def send_sms
-    account_sid = 'AC951a12c5042b401c761036d015fc86ff'
-    auth_token = 'a252b704db9b66f594ee16a754ecb567'
+    (account_sid = ENV['TWILIO_ACCOUNT_SID'])
+    (auth_token = ENV['TWILIO_AUTH_TOKEN'])
 
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     @client.account.messages.create(
-      from: '441580392065',
-      to: '07976504440',
+      from: ENV['TWILIO_NO'],
+      to: ENV['MY_NO'],
       body: "Thank you! Your order will be delivered before #{delivery_time}"
     )
   end
