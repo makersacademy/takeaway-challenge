0baa2246c517dd0fd065e53c6e2c0e300ba9089f
johnnydee8 <j.dworzynski@me.com>
2016-08-27 12:12:36 +0100

    all test pass, menu now reads values from CSV, minor refactring

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,9 @@
-include 'CSV'
- 
+require 'csv'
+
 class Menu
 
   def initialize
-    @items_list = {"miso soup" => 3.50}
+    @items_list = {}
   end
 
   def print_list
@@ -14,8 +14,16 @@ class Menu
     items_list[name]
   end
 
+  def load_list(aFile)
+    loaded_list = {}
+    CSV.foreach(aFile, headers: true) do |row|
+        loaded_list[row["item"]] = row["price"].to_f
+    end
+    self.items_list = loaded_list
+  end
+
 private
 
-  attr_reader :items_list
+  attr_accessor :items_list
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,18 +1,19 @@
 require 'menu'
 
 describe Menu do
-
+  before [:each] do
+    subject.load_list('menu.csv')
+  end
   it 'prints out a list of available items' do
-    expect(subject.print_list).to include("miso soup" => 3.50)
+    expect(subject.print_list).to include({"Miso Soup" => 3.50})
   end
 
   it 'returns price for selected item' do
-    expect(subject.get_item("miso soup")).to eq(3.50)
+    expect(subject.get_item("Miso Soup")).to eq(3.50)
   end
 
   it 'reads a csv file with a menu' do
-    subject.load_list('menu.csv')
-    expect(subject.print_list).to include("miso soup" => 3.50)
+    expect(subject.print_list).to include("Miso Soup" => 3.50)
   end
 
 end
