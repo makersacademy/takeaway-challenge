39068a047d4405b2a159c853f4a5bf1de7375257
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-28 00:20:30 +0100

    menu displays items and can check prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,19 @@
 class Menu
   def initialize
-    @menu = {"Tomato, Garlic & Oregano" => 4.50}
+    @menu = {"Margherita" => 6,
+      "Pepperoni" => 7,
+      "Vegetariana" => 7,
+      "Hawaiian" => 7,
+      "Garlic Pizza Bread" => 4}
   end
 
   def show
     puts menu
+  end
+
+  def get_price(item)
+    fail 'This item is not currently on our menu' if !menu.include?(item.capitalize)
+    menu[item.capitalize]
   end
 
   private
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,4 +5,14 @@ describe Menu do
   describe '#show' do
       specify{expect{menu.show}.to output.to_stdout}
   end
+
+  describe '#get_price' do
+    it 'returns the price of a given item' do
+      expect(menu.get_price("margherita")).to eq 6
+    end
+    it 'raises an error when an item is selected that is not on the menu' do
+      msg='This item is not currently on our menu'
+      expect{menu.get_price("seafood")}.to raise_error(msg)
+    end
+  end
 end
