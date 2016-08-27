2374c6e9607195633fce13042ab25047f1f2d4d0
johnnydee8 <j.dworzynski@me.com>
2016-08-27 17:39:10 +0100

    mocked out behaviour of the File class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,10 @@
 require 'menu'
 
 describe Menu do
+  let(:data) {"item,price\nMiso Soup,3.50"}
+
   before [:each] do
+    allow(File).to receive(:open).with("menu.csv", {:universal_newline=>false, :headers=>true}) {StringIO.new(data)}
     subject.load_list
   end
   it 'prints out a list of available items' do
