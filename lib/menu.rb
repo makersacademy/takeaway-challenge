990e48fb3410fb2a81c17b1bb65f9a216dad6dce
johnnydee8 <j.dworzynski@me.com>
2016-08-27 10:03:41 +0100

    Added a test for reading CSV files

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,17 @@
+include 'CSV'
+ 
 class Menu
 
   def initialize
     @items_list = {"miso soup" => 3.50}
   end
-  
+
   def print_list
     items_list
+  end
+
+  def get_item(name)
+    items_list[name]
   end
 
 private
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,17 +1,18 @@
 require 'menu'
 
 describe Menu do
-  # xit 'reads a list of menu from a .csv file' do
-  #
-  # end
 
   it 'prints out a list of available items' do
     expect(subject.print_list).to include("miso soup" => 3.50)
   end
 
-  it 'returns selected item' do
+  it 'returns price for selected item' do
     expect(subject.get_item("miso soup")).to eq(3.50)
   end
 
+  it 'reads a csv file with a menu' do
+    subject.load_list('menu.csv')
+    expect(subject.print_list).to include("miso soup" => 3.50)
+  end
 
 end
