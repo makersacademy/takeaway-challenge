706302028640ef4db68ec5e2c0e3a515728d5fd2
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-08-28 17:39:48 +0100

    Tidied the Menu class a little, menu is no longer in initialize method

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,6 +2,11 @@ class Menu
 attr_reader :dishes
 
   def initialize
+    @dishes = {}
+    menu
+  end
+
+  def menu
     @dishes = {"Chow Mein" => 5.50, "Sweet & Sour Chicken" => 5.85,
        "Aromatic Duck" => 7.95, "Crispy Beef" => 5.95}
   end
