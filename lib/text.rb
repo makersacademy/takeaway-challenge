f03a20c81dce66f9bb03682d69eb7e01ba4f65f5
Stéphan Gaud <stephgoeseast@gmail.com>
2016-08-28 23:02:12 +0100

    test works in IRB but still problem to test in rspec

--- lib/text.rb
+++ lib/text.rb
@@ -1,5 +1,4 @@
 require 'twilio-ruby'
-require './lib/order.rb'
 
 class Text
 
--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -2,17 +2,22 @@ require 'text'
 
 describe Text do
 
+let(:twilio_ruby) {double :twilio_ruby, new: twilio}
   subject(:text) {described_class.new}
-  let(:twilio_class_double) {double :twilio_class_double, new: twilio}
+
   let(:twilio) {double :twilio, :messages => messages}
-  let(:messages) {double :messages, :create => nil}
+  let(:messages) {double :messages, :create => "Thank you! Your delivery should arrive by 21:10"}
 
   before (:each) do
   allow(Time).to receive(:now) {Time.new(2016,8,28, 20,10,0)}
   end
 
-  it "send a text confirm the delivery eta" do
+  it "send a text using twilio" do
     text.send_message
     expect(messages).to have_received(:create)
+  end
+
+  it 'send a text with eta' do
+    expect{subject.send_message}.to output("Thank you! Your delivery should arrive by 21:10").to_stdout_from_any_process
   end
 end
