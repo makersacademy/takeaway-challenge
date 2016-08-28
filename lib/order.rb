de63fd7bc0b101f875f7451f9c1d5dbad8b791fc
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-08-28 20:26:03 +0100

    Added a test for displaying dishes, and for what dish they would like.

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,31 @@
+require_relative 'menu'
+
 class Order
+
+  def initialize(menu_class: Menu)
+    @menu_class = menu_class
+  end
+
+  def begin_order
+    order_greeting
+    display_dishes
+
+  end
+
+  def order_greeting
+    puts "What dish would you like?"
+    puts "you can choose from the following:"
+    puts
+  end
+
+  private
+
+  def display_dishes
+    @menu_class.new.dishes.each do |dish,price|
+      puts "#{dish} £#{price}"
+      puts
+    end
+  end
+
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,4 +1,22 @@
 require 'order'
 
 describe Order do
+
+  subject(:order) {described_class.new}
+
+  describe "#choosing a dish" do
+
+    it "can display available dishes" do
+      expect(order.display_dishes).to include("Chow Mein")
+    end
+
+    it "asks what dish they would like" do
+      expect{subject.order_greeting}.to output("What dish would you like?\nyou can choose from the following:\n\n").to_stdout
+    end
+
+    # it "lists available dishes" do
+    #   expect{subject.begin_order}.to output("").to_stdout
+    # end
+
+  end
 end
