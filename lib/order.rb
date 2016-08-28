39fffca43f0ec1ec21d600a809f47972956a0d17
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-28 14:29:27 +0100

    removes items from basket and cost from subtotal

--- lib/order.rb
+++ lib/order.rb
@@ -10,11 +10,26 @@ class Order
   def add(item)
     item.capitalize!
     @subtotal += @menu.get_price(item)
-    if @basket.include?(item)
-      @basket[item] += 1
+    if basket.include?(item)
+      basket[item] += 1
     else
-      @basket[item] = 1
+      basket[item] = 1
     end
   end
+
+  def remove(item)
+    item.capitalize!
+    fail 'Item not in basket' if basket[item].nil?
+    if basket[item] == 1
+      basket.delete(item)
+    else
+      basket[item] -= 1
+    end
+    @subtotal -= @menu.get_price(item)
+  end
+
+  private
+
+  attr_reader :basket
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -20,4 +20,31 @@ describe Order do
       expect(order.instance_variable_get(:@basket)["Margherita"]).to eq 2
     end
   end
+
+  describe '#remove(item)' do
+    before do
+      order.add("margherita")
+      2.times {order.add("pepperoni")}
+    end
+    it 'removes an item from basket' do
+      order.remove("margherita")
+      expect(order.instance_variable_get(:@basket)).not_to include "Margherita"
+    end
+    it 'removes only one of an item when there are multiple' do
+      order.remove("pepperoni")
+      expect(order.instance_variable_get(:@basket)["Pepperoni"]).to eq 1
+    end
+    it 'removes the price of item from the subtotal' do
+      order.remove("margherita")
+      expect(order.instance_variable_get(:@subtotal)).to eq 14
+    end
+    it 'removes the price of only one item when removing one of several' do
+      order.remove("pepperoni")
+      expect(order.instance_variable_get(:@subtotal)).to eq 13
+    end
+    it 'raises an error when the item to delete is not in the basket' do
+      msg = 'Item not in basket'
+      expect {order.remove("hawaiian")}.to raise_error msg
+    end
+  end
 end
