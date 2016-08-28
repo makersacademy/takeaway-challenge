9ad7bbf3ff3e160e4da72647dd1fb428839495c7
johnnydee8 <j.dworzynski@me.com>
2016-08-28 12:31:17 +0100

    creates test for messaging and messaging API integration

--- lib/despatch.rb
+++ lib/despatch.rb
@@ -1,6 +1,12 @@
+require 'twilio-ruby'
 class Despatch
 
 # => `export SOME_API_KEY="qwerty123456"`in Ruby as `ENV["SOME_API_KEY"]`
+
+  def initialize(twilio_client=Twilio::REST::Client)
+    account_sid = 'AC76807998c792abd7e12cb8e95689f27f'
+    @client = twilio_client.new(account_sid, ENV["TWILIO_TOKEN"])
+  end
 
   def send_order
 
@@ -9,6 +15,14 @@ class Despatch
   def estimate_eta
     eta_time = Time.now + (60*60)
     "#{eta_time.hour}:#{eta_time.min}"
+  end
+
+  def send_text
+    @client.messages.create(
+      from: '+441793250442',
+      to: ENV["MY_NUMBER"],
+      body: "Thank you! Your order was placed and will be delivered before #{estimate_eta}."
+      )
   end
 
 end
--- spec/despatch_spec.rb
+++ spec/despatch_spec.rb
@@ -2,9 +2,22 @@ require 'despatch'
 
 describe Despatch do
 
+let(:twilio_class_double) {double :twilio_class_double, new: twilio}
+subject(:despatch) {described_class.new(twilio_class_double)}
+let(:twilio) {double :twilio, :messages => messages}
+let(:messages) {double :messages, :create => nil}
+
+before (:each) do
+  allow(Time).to receive(:now) {Time.new(2016,8,27, 19,13,0)}
+end
+
   it 'genertes ETA at time + 1' do
-    allow(Time).to receive(:now) {Time.new(2016,8,27, 19,13,0)}
     expect(subject.estimate_eta).to eq("20:13")
+  end
+
+  it 'sends texts using Twilio API' do
+    subject.send_text
+    expect(messages).to have_received(:create)
   end
 
 end
