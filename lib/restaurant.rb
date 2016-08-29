158729e6bc17601c9241f9d90df547d3880321b3
Joseph <joseph.jr@gmail.com>
2016-08-29 18:40:38 +0100

    error and fail tests and methods

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,18 +1,19 @@
 require_relative 'menu'
 class Restaurant
 
-attr_reader :order_cart, :menu, :total_dishes
+attr_reader :order_cart, :total_dishes, :menu
 
 def initialize
   @order_cart = []
-
+  @menu = Menu.new
 end
 
-def list(menu)
-menu.items
+def list(things)
+things.items
 end
 
 def select(dish)
+  fail "That item isn't on the menu" if menu.price(dish) == nil
   @order_cart << dish
 end
 
@@ -20,7 +21,12 @@ def total_dishes
   order_cart.length
 end
 
+def bill_total(things)
+  (things.price(@order_cart))
+end
+
 def order
+  fail "You submitted an empty order" if @order_cart == []
   an_hour = 60*60
   t = Time.new + an_hour
   g = (t.hour).to_s + ":" + t.min.to_s
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -20,10 +20,32 @@ it 'returns the number of items you ordered' do
   expect(restaurant.total_dishes).to eq 3
 end
 
+it 'returns quantity of dishes you ordered' do
+  restaurant.select("shishito peppers")
+  restaurant.select("shishito peppers")
+end
+
+it 'returns your order total' do
+  menu = Menu.new
+  restaurant.select("boquerones")
+  expect(restaurant.bill_total).to eq 5
+end
+
+it 'will not let you order something not on the menu' do
+  expect{restaurant.select("taco")}.to raise_error "That item isn't on the menu"
+end
+
+it 'won\'t let you place an empty order' do
+  expect{restaurant.order}.to raise_error "You submitted an empty order"
+end
+
+
 it 'returns a message that denotes order will be delivered in an hour' do
+  restaurant.select("shishito peppers")
   t = Time.new + 60*60
-  message = "Thank you! Your order was placed and will be delivered before "
-  expect(restaurant.order).to eq (message + (t.hour).to_s + ":" + t.min.to_s)
+  hour_and_minute = t.hour.to_s + ":" + t.min.to_s
+  m = "Thank you! Your order was placed and will be delivered before "
+  expect(restaurant.order).to eq (m + hour_and_minute)
 end
 
 end
