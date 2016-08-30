4b5b80229d8f797dc50b4a6636bcc3a652d199b0
Tam Borine <tamborine@Tams-MacBook-Air.local>
2016-08-30 09:40:11 +0100

    tests

--- spec/selection_spec.rb
+++ spec/selection_spec.rb
@@ -4,13 +4,23 @@ describe Selection do
 
   context '#select_items' do
 
-    it 'returns the correct selection items inputted'
+    xit 'returns the correct selection items inputted' do
+      allow(subject).to receive(:gets).and_return('jam', 3, 'quit')
+      expect{subject.select_items}.to eq [{food: 'jam', quantity: 3}]
+
+    end
+
+    it "should echo" do
+      pending 'a failing test so close!'
+      allow(STDIN).to receive(:gets).and_return('jam', '3', 'quit')
+      expect(subject.select_items).to eq [{food: 'jam', quantity: 3}]
+    end
+
 
     it 'allows the user to specify how many of each food they want'
 
     xit 'returns empty selection order' do
-      pending 'infinite loop'
-      allow(STDIN).to receive(:gets) {'quit'}
+      allow(STDIN).to receive(:gets).and_return('quit')
       expect(subject.select_items).to eq []
     end
 
