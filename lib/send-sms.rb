4d0612c509b20279c79dd640a00edb3a0527df76
david anderson <d.a.m.anderson@gmail.com>
2016-10-07 16:07:45 +0100

    Working SMS sender with Twilio

---
+++ lib/send-sms.rb
@@ -0,0 +1,13 @@
+require 'twilio-ruby'
+
+account_sid = 'AC6e70d6dcc83c097a6e913c3afbd424a3'
+auth_token = '901c21df552c5cfcf478c47b4aa310e3'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+@client.account.messages.create(
+  from: '+441493202211',
+  to: '+447759548193',
+  body: 'Robot invasion! Reply back with any sightings.'
+)
