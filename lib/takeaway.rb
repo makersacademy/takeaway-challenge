b59d63eafe0255b2026a93e4cdd027dd80a08d05
Glenn <glynester@hotmail.com>
2016-10-07 21:59:25 +0000

    Tests that order adds to orders array - test is not right although test result is valid!

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,18 +1,22 @@
 class TakeAway
   
-  attr_reader :show_menu        #, :menu  
+  attr_reader :show_menu, :menu, :item, :order, :orders       #, :menu  
   
+  def initialize
+    @orders = []
+    @menu = {"spring roll"=>0.99, "char sui bun"=>3.99, "pork dumpling"=>2.99, "peking duck"=>7.99, "fu-king fried rice"=>5.99}
+  end
   
-  #def initialize
-    
-  #end
-  
-  def show_menu
-    menu = {"spring roll"=>0.99, "char sui bun"=>3.99, "pork dumpling"=>2.99, "peking duck"=>7.99, "fu-king fried rice"=>5.99}
+  def show_menu                         #(menu = {"spring roll"=>0.99})       
     menu.each{|k,v| puts "#{k} £#{v}"}
   end
   
-  def order
+  def order(item)
+    if menu[item]
+      orders <<  menu.select { |k, v| k[item] }       
+    else
+      print :"That item is not on the menu."
+    end
     
   end
   
@@ -22,3 +26,6 @@ end
 
 thai = TakeAway.new
 thai.show_menu
+thai.order "spring roll"
+#thai.order "xxx"
+print thai.inspect
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,14 +1,26 @@
 require 'takeaway' 
 
 describe TakeAway do
+  subject(:takeaway) { described_class.new }
+  let(:item){double :item}
+  #before do
+ #end
   
   it "responds to the show_menu method" do
-    expect(subject).to respond_to(:show_menu)
+    expect(subject).to respond_to(:show_menu)     #.with (1).argument
     
   end
   
   it "responds to the order method" do
-    expect(subject).to respond_to(:order)
+    expect(subject).to respond_to(:order).with(1).argument
+  end
+  
+  it "adds an item to the orders array" do
+    item = "spring roll"
+    #allow{{takeaway.order(item)}.to be true
+    #allow(takeaway).to receive(takeaway.order(:item)) { true }
+    expect{takeaway.order(item)}.to change{takeaway.orders.length}.by(+1)
+    puts takeaway.orders.length
   end
   
   
