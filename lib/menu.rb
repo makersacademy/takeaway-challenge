bef7546853ecd573ba82f8b58c8c77f6d308182d
B-raw <michaelbarry1989@hotmail.com>
2016-10-07 22:07:56 +0100

    Add select_dish tests + method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,42 @@
+$dishes = [
+{name: "Pasta", price: 3.50},
+{name: "Pizza", price: 2.40}
+]
+
 class Menu
+  attr_reader :selections
+
+  def initialize(menu, order_class = Order)
+    @menu = menu
+    @selections = []
+    @order_class = order_class
+  end
+
+  # def user_interface
+  #   puts "What would you like to do?\n\n"
+  #   while true
+  # end
+
+  def view
+    @menu.each_with_index do |dish, i|
+      puts "#{i+1}: #{dish[:name]} costs #{dish[:price]}"
+    end
+  end
+
+  def select_dish(choice)
+    $dishes.each do |dish|
+      @selections << dish if dish[:name].downcase == choice.downcase
+    end
+    @selections
+  end
+
+
+
+
 
 end
+
+
+
+# my_menu=Menu.new(dishes)
+# my_menu.view
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,51 @@
 require 'menu'
 
 describe Menu do
-  subject {described_class.new}
+  dishes = [
+  {name: "Pasta", price: 3.50},
+  {name: "Pizza", price: 2.40}
+  ]
+  subject {described_class.new(dishes)}
+
+
 
   it {is_expected.to respond_to :view}
+
+  it {is_expected.to respond_to :select_dish}
+
+  describe "#view" do
+    it "outputs dishes in a format" do
+      view_output = dishes.each_with_index {|dish, i| "#{i+1}: #{dish[:name]} costs #{dish[:price]}"}
+      expect(subject.view).to eq view_output
+    end
+  end
+
+  describe "#select" do
+    before(:each) do
+      subject.select_dish("Pasta")
+    end
+    it "selects a dish and stores in selections" do
+      expect(subject.selections).to include dishes[0]
+    end
+
+    it "should select several separate dishes" do
+      subject.select_dish("pizza")
+      expect(subject.selections).to include dishes[0], dishes[1]
+    end
+
+    it "should order several of the same dish" do
+      subject.select_dish("Pasta")
+      subject.select_dish("Pasta")
+      expect(subject.selections).to include dishes[0], dishes[0], dishes[0]
+    end
+  end
+
+  describe "#order" do
+    it "should create an order class" do
+
+    end
+  end
+
+
+
 end
