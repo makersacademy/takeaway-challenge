bde88548b53a0aac20697ba478fbf3daa70717af
Tadas Majeris <tadas.majeris@gmail.com>
2016-10-08 12:58:07 +0100

    Loading dishes from file

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,31 @@
+require "csv"
+
 class Takeaway
 
+  def initialize
+    @dishes = []
+  end
+
+  def dishes
+    @dishes.dup
+  end
+
+  def load_dishes(filename)
+    if File.exist?(filename)
+      import_dishes_from(filename)
+    else
+      "File '#{filename}' not found!"
+    end
+  end
+
+  private
+
+  def import_dishes_from(filename)
+    CSV.foreach(filename) { |line| add_dish(line) }
+  end
+
+  def add_dish(data)
+    name, price = data
+    @dishes << { name: name, price: price }
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,31 @@
 require "takeaway"
 
 describe Takeaway do
-  
+
+  describe 'initialization' do
+    it 'should have no dishes' do
+      expect(subject.dishes).to be_empty
+    end
+  end
+
+  describe '#load_menu' do
+    let (:filename) { 'menu.csv' }
+    let (:false_filename) { 'some.csv' }
+
+    it 'should check if file exists' do
+      expect(File).to receive(:exist?).with(filename)
+      subject.load_dishes(filename)
+    end
+
+    it 'should return error message when file doesnt exist' do
+      allow(File).to receive(:exist?).with(false_filename).and_return false
+      expect(subject.load_dishes(false_filename)).to eq "File '#{false_filename}' not found!"
+    end
+
+    it 'should try to import dishes from a file if it exists' do
+      allow(File).to receive(:exist?).with(filename).and_return true
+      expect(subject).to receive(:import_dishes_from).with(filename)
+      subject.load_dishes(filename)
+    end
+  end
 end
