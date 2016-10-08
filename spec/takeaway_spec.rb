e1ed9528f72d0b19c61a397cf1e3a83c68df9691
andy jenkins <andygjenkins@gmail.com>
2016-10-08 17:27:03 +0100

    Add user stories

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,9 +2,9 @@ require 'takeaway'
 
 describe Takeaway do
   subject(:takeaway) { Takeaway.new }
-
+  let(:menu) { "Doner Kebab: £4.00"}
   it 'shows the takeaway menu' do
-    expect(takeaway.show_menu).to eq ("Doner Kebab: £4.00")
+    expect(takeaway.show_menu).to eq (menu)
   end
 
 
