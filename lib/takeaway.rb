98be2514af2e2169ded93cf7ae7979bbb9363b9b
Glenn <glynester@hotmail.com>
2016-10-08 12:24:49 +0000

    Calculates total, outputs summary, test done for total but it is dendant on the menu being the same.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -9,11 +9,32 @@ class TakeAway
   
   def read_menu                         #(menu = {"spring roll"=>0.99})       
     menu.each{|k,v| puts "#{k} £#{v}"}
+    print "Use '.order(food_description)' to add items.\n"
   end
   
   def order(item, qty=1)
-    print :"That item is not on the menu." unless on_menu?(item) 
-    (qty.times {add_item(item)}) if on_menu?(item)      
+    print :"That item is not on the menu.\n" unless on_menu?(item) 
+    (qty.times {add_item(item)}) if on_menu?(item)
+    print "#{qty} x #{item}(s) added to your order.\n" if on_menu?(item)
+    print "Use '.summary' to show your order. Use '.total' to show the total cost.\n"
+  end
+  
+  def summary
+      counts = Hash.new 0
+      orders.each {|p| counts[p] += 1}
+      output_arr = []
+      counts.each{|key,val| 
+      	key.each{|k,v|
+      	output_arr << "\"#{k}\" x #{val} = £#{key[k] * val}"
+      	}
+      }	
+      print output_arr.join(", ")
+  end
+  
+  def total
+    tot = 0
+    orders.each{ |item| item.each_value{|v| tot += v} }
+    print "Total: £#{tot.round(2)}"
   end
   
   private
@@ -29,7 +50,7 @@ class TakeAway
   
 end
 
-#=begin
+=begin
 #require './lib/takeaway'
 thai = TakeAway.new
 thai.read_menu
@@ -38,5 +59,7 @@ thai.order "peking duck", 2
 thai.order "spring roll"
 thai.order "fu-king fried rice",3
 thai.order "xxx"
+thai.summary
+thai.total
 print thai.inspect
-#=end
+=end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -13,16 +13,31 @@ describe TakeAway do
   it "responds to the order method" do
     expect(subject).to respond_to(:order).with(2).arguments
   end
+  describe "makes an order" do
   
-  it "adds a single item to the orders array" do
-    item = "spring roll"        #This will fail if the menu changes
-    expect{takeaway.order(item)}.to change{takeaway.orders.length}.by(+1)
+    it "adds a single item to the orders array" do
+      item = "spring roll"        #This will fail if the menu changes
+      expect{takeaway.order(item)}.to change{takeaway.orders.length}.by(+1)
+    end
+    
+    it "adds a number of items to the orders array" do
+      item = "spring roll"        #This will fail if the menu changes
+      expect{takeaway.order(item,3)}.to change{takeaway.orders.length}.by(+3)
+    end
+    
+    it "should produce the correct total" do
+       item1 = "spring roll"
+       item2 = "peking duck"
+       takeaway.order(item1, 3)    #0.99 * 3 = 2.97
+       takeaway.order(item2, 4)    #7.99 * 4 = 31.96, Total = £34.93
+       expect{(takeaway.total).to output('Total: £34.93').to_stdout }
+       print takeaway.total
+       
+    end
+    
   end
   
-  it "adds a number of items to the orders array" do
-    item = "spring roll"        #This will fail if the menu changes
-    expect{takeaway.order(item,3)}.to change{takeaway.orders.length}.by(+3)
-  end
+  
   
   
 end
