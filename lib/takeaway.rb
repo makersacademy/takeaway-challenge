6e5084a279c050e8b680b70974530055a0f5bb57
Glenn <glynester@hotmail.com>
2016-10-08 09:51:56 +0000

    Added order method can handle more than 1 item. Test added for this.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,19 +1,19 @@
 class TakeAway
   
-  attr_reader :show_menu, :menu, :item, :order, :orders       #, :menu  
+  attr_reader :read_menu, :menu, :item, :order, :orders       #, :menu  
   
   def initialize
     @orders = []
     @menu = {"spring roll"=>0.99, "char sui bun"=>3.99, "pork dumpling"=>2.99, "peking duck"=>7.99, "fu-king fried rice"=>5.99}
   end
   
-  def show_menu                         #(menu = {"spring roll"=>0.99})       
+  def read_menu                         #(menu = {"spring roll"=>0.99})       
     menu.each{|k,v| puts "#{k} £#{v}"}
   end
   
-  def order(item)
+  def order(item, qty=1)
     print :"That item is not on the menu." unless on_menu?(item) 
-    add_item(item) if on_menu?(item)      
+    (qty.times {add_item(item)}) if on_menu?(item)      
   end
   
   private
@@ -29,11 +29,14 @@ class TakeAway
   
 end
 
+#=begin
+#require './lib/takeaway'
 thai = TakeAway.new
-thai.show_menu
+thai.read_menu
 thai.order "spring roll"
-thai.order "peking duck"
+thai.order "peking duck", 2
 thai.order "spring roll"
+thai.order "fu-king fried rice",3
 thai.order "xxx"
 print thai.inspect
-
+#=end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,17 +6,22 @@ describe TakeAway do
 
   
   it "responds to the show_menu method" do
-    expect(subject).to respond_to(:show_menu)     #.with (1).argument
+    expect(subject).to respond_to(:read_menu)     #.with (1).argument
     
   end
   
   it "responds to the order method" do
-    expect(subject).to respond_to(:order).with(1).argument
+    expect(subject).to respond_to(:order).with(2).arguments
   end
   
-  it "adds an item to the orders array" do
+  it "adds a single item to the orders array" do
     item = "spring roll"        #This will fail if the menu changes
     expect{takeaway.order(item)}.to change{takeaway.orders.length}.by(+1)
+  end
+  
+  it "adds a number of items to the orders array" do
+    item = "spring roll"        #This will fail if the menu changes
+    expect{takeaway.order(item,3)}.to change{takeaway.orders.length}.by(+3)
   end
   
   
