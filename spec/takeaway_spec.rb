86e58bd8a55fe0a092944f49b0d83258d0baea69
Chet Sanghani <crsanghani@gmail.com>
2016-10-08 16:55:32 +0000

    first test passed: menus can show dishes and prices using a double

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,9 +2,12 @@ require 'takeaway'
 
 describe Takeaway do
     
-    subject(:takeaway) {described_class.new}
+    subject(:takeaway) {described_class.new(menu: menu)}
+    
+    let(:menu) {double(:menu, print: printed_menu)}
+    let(:printed_menu) {"Ratkajino: 5 isk"}
     
     it 'shows the menu with the dishes and prices' do
-       expect(takeaway.print_menu).to eq("Ratkajino: 5 isk") 
+       expect(takeaway.print_menu).to eq(printed_menu) 
     end
 end
