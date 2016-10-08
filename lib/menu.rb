10a0423b8efbb7e3adedcfb738668a3d3d8629f9
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-08 19:53:26 +0100

    Refactor menu display

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,20 @@
 class Menu
 
+  attr_reader :dish, :price
   def initialize
-    @menu = {"Sabich" => 6, "Houmous" => 4, "Baba Ganoush" => 4, "Salona" => 8, "Shashuka" => 9, "Tabbouleh" => 5}
+    @menu = {"Sabich" => 6,
+            "Houmous" => 4,
+            "Baba Ganoush" => 4,
+            "Salona" => 10,
+            "Shashuka" => 9,
+            "Tabbouleh" => 5,
+            "Pitta" => 2,
+            "Falafel" => 6}
   end
 
   def display
-  print @menu.map{ |x, i| [i, x] }
+    @menu.each do |dish, price|
+      puts "#{dish} £#{price}"
   end
-
+end
 end
