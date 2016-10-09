956b1faca070ce044c207c61f0243e10df3829b0
Frances Maxwell <fmaxwell@gmail.com>
2016-10-09 23:15:28 +0100

    Added tests for adding an item to the order list with a default quantity of 1

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,13 @@
 class Order
 
-  def add_item(item, quantity)
+  attr_reader :list
 
+  def initialize
+    @list = []
+  end
+
+  def add_item(item, quantity = 1)
+    @list << {item: item, quantity: quantity}
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,10 +4,21 @@ describe Order do
 
   describe '#add_item' do
 
-    it 'responds to two arguments' do
+    it 'responds to one to two arguments' do
       expect(subject).to respond_to(:add_item).with(2).arguments
     end
 
-  end
+    before do
+      subject.add_item("Chicken Teriyaki")
+    end
 
+    it 'adds an item to the order list' do
+      expect(subject.list.count).to eq 1
+    end
+
+    it 'adds a quantity of one by default' do
+      expect(subject.list.last[:quantity]).to eq 1
+    end
+
+  end
 end
