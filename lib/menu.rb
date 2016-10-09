8de2a3290f2511996b9367cc795089baa53987ab
Glenn <glynester@hotmail.com>
2016-10-09 14:04:43 +0000

    menu in a separate class is working.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,17 @@
 require 'csv'
 
-
 class Menu
   
-  attr_reader :dishes, :menu
+  attr_reader :dishes, :menu          #, :filename
   
-  def initialize
+  def initialize(filename = "menu.csv")    #= "menu.csv"
     #@dishes = {"spring roll"=>0.99, "char sui bun"=>3.99, "pork dumpling"=>2.99, "peking duck"=>7.99, "fu-king fried rice"=>5.99}     
-    @menu = Hash.new 
+    #@filename = filename
+    @menu = Hash.new
+    load_menu (filename) 
   end
   
-  def load_menu (filename = "menu.csv")    
+  def load_menu (filename)             #filename = "menu.csv")    
       
     CSV.foreach(filename, col_sep: ',') do |row|     
       item, price = row
@@ -19,18 +20,41 @@ class Menu
     print "Use \".load_menu(menu_name.csv)\" to load a non default menu\n"
   end
   
+  
+  def read_menu                         #(menu = {"spring roll"=>0.99})       
+    menu.each{|k,v| puts "#{k} £#{v}"}
+    print "Use '.order(food_description)' to add items.\n"
+  end
+  
+  def contains?(item)
+    !!menu[item]
+  end
+  
+  def get_details(item)             #Works but returns a string value of amount and I don't have time to sort this out!!!!!
+    menu.select { |k, v| k[item] }  #so will stick with method in takeaway.    
+  end
+  
+  
   def to_s
     'the menu is empty'   # menu = Menu.new,  puts menu, to_s is called by default when `puts`ing an object in Ruby
   end
   
   private
+  
   def load_items(item,price)
-    @menu[item] = price
+    @menu[item] = price.to_f
   end
 
 end
 
 
 menu = Menu.new
+#menu = Menu.new("menu1.csv")
+#menu.load_menu("menu.csv")
 menu.load_menu("menu1.csv")
-puts menu.menu.inspect
+                      #puts menu.get_details("peking duck")
+puts"~~~~~~~~~~~~~~~~~~~~~"
+#puts menu.menu.inspect
+puts menu.menu
+menu.read_menu
+puts menu.contains?("pork dumpling")
--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,21 +6,29 @@ class TakeAway
   attr_reader :read_menu, :menu, :item, :order, :orders, :messager      
   
   def initialize(menu = Menu.new, messager = Messager.new)
-    @menu1 = menu
+    @menu_obj = menu
     @messager = messager
     @orders = []
-    @menu = {"spring roll"=>0.99, "char sui bun"=>3.99, "pork dumpling"=>2.99, "peking duck"=>7.99, "fu-king fried rice"=>5.99}
+    @menu =  @menu_obj.menu
   end
   
-  def read_menu                         #(menu = {"spring roll"=>0.99})       
-    menu.each{|k,v| puts "#{k} £#{v}"}
-    print "Use '.order(food_description)' to add items.\n"
+  def change_menu(file)
+    @menu_obj = Menu.new(file)
+  end
+  
+  # def read_menu                         #(menu = {"spring roll"=>0.99})       
+  #   menu.each{|k,v| puts "#{k} £#{v}"}
+  #   print "Use '.order(food_description)' to add items.\n"
+  # end
+  
+  def read_menu
+    @menu_obj.read_menu
   end
   
   def order(item, qty=1)
-    print :"That item is not on the menu.\n" unless on_menu?(item) 
-    (qty.times {add_item(item)}) if on_menu?(item)
-    print "#{qty} x #{item}(s) added to your order.\n" if on_menu?(item)
+    print :"That item is not on the menu.\n" unless @menu_obj.contains?(item) 
+    (qty.times {add_item(item)}) if @menu_obj.contains?(item)
+    print "#{qty} x #{item}(s) added to your order.\n" if @menu_obj.contains?(item)
     print "Use '.summary' to show your order. Use '.total' to show the total cost. Use '.checkout(val)' to submit your order.\n"
   end
   
@@ -60,24 +68,25 @@ class TakeAway
   private
   
   def add_item(item)
-    orders <<  menu.select { |k, v| k[item] }
+    #orders <<  menu.select { |k, v| k[item] }
+    orders << @menu_obj.get_details(item)       #returns a string value for amount which I can't use.       
   end
   
-  def on_menu?(item)
-    menu[item]
-  end
+  # def contains?(item)
+  #   menu[item]
+  # end
   
   
 end
 
 #=begin
 # require './lib/takeaway'
-thai = TakeAway.new
-puts thai.read_menu                
+menu = Menu.new("menu1.csv")
+thai = TakeAway.new(menu)
+thai.change_menu("menu.csv")
+thai.read_menu                
 thai.order "spring roll", 3
 thai.order "peking duck", 4
-          #thai.order "spring roll"
-          #thai.order "fu-king fried rice",3
 puts thai.order "xxx"
 puts thai
 print thai.inspect
