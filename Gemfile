5d66057dd627354133182fc8f5eb791242a0fe38
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-09 20:22:52 +0100

    Add menu.csv, Menu class and menu test file

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem 'rake'
 gem 'rspec'
 gem 'coveralls', require: false
 gem 'capybara'
+gem 'rubocop'
+gem 'twilio-ruby'
---
+++ lib/menu.rb
@@ -0,0 +1,25 @@
+class Menu
+
+  attr_reader :dishes
+
+  def initialize(filename)
+    @dishes = []
+    load_menu(filename)
+  end
+
+  def read_menu
+    puts @dishes
+  end
+
+  private
+
+  def load_menu(filename)
+    file = File.open(filename, "r")
+    file.readlines.each do |line|
+      dish, price = line.chomp.split(',')
+      @dishes << {dish => price.to_i}
+    end
+    file.close
+  end
+
+end
