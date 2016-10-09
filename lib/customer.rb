e125b88b989f813063a0b60d737c91ae875b914b
hanrattyjen <hanratty.jen@gmail.com>
2016-10-09 14:02:24 +0100

    Re-named some methods in Customer class to help readability.

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,26 +5,30 @@ require 'csv'
 class Customer
 
   attr_reader :menu
-  attr_accessor :new_order, :pending_order, :selection
+  attr_accessor :order_created, :pending_order, :selection
 
   def initialize
     @pending_order = []
     @menu = Menu.new
-    @new_order = nil
+    @order_created = nil
     @selection = selection
   end
 
-  def new_order
+  def select_dish
     puts "Please enter a menu choice:"
-    @selection = gets.chomp.to_i
-    create_order(@selection)
+    selection = gets.chomp.to_i
+    add_to_order(selection)
   end
 
-  def create_order(selection)
-    @pending_order << @menu.menu[selection]
+  def add_to_order(selection)
+    pending_order << @menu.menu[selection]
   end
 
   def confirm_order
-    @new_order = Order.new(@pending_order)
+    @order_created = Order.new(pending_order)
+  end
+
+  def print_total
+    @order_created.confirm_total
   end
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,16 @@
 class Order
 
+  attr_reader :order
 
   def initialize(order)
     @order = order
   end
 
+  def confirm_total
+    order.each do |item|
+      item.each do |dish|
+        print dish
+      end
+    end
+  end
 end
