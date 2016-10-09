459f0f4e0f41ca7e0ee510aba4b1b025a234d9b4
Frances Maxwell <fmaxwell@gmail.com>
2016-10-10 00:13:56 +0100

    Added tests and code for calculating order total

--- lib/order.rb
+++ lib/order.rb
@@ -7,10 +7,24 @@ class Order
   def initialize(item_class = Menu)
     @menu = item_class.new.list
     @basket = []
+    @total = 0
   end
 
-  def add_item(item, quantity = 1)
-    @basket << {item: item, quantity: quantity}
+  def add_item(item, price, quantity = 1)
+    basket << {item: item, price: price, quantity: quantity}
   end
 
+  def total
+    calculate_total
+    @total
+  end
+
+  private
+
+  def calculate_total
+    @total = 0
+    basket.each do |item|
+      @total += item[:price] * item[:quantity]
+    end
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -25,7 +25,7 @@ describe Order do
     end
 
     before do
-      subject.add_item(subject.menu[1])
+      subject.add_item(subject.menu[1].name, subject.menu[1].price)
     end
 
     it 'adds an item to the basket' do
@@ -37,8 +37,25 @@ describe Order do
     end
 
     it 'adds the quantity specified' do
-      subject.add_item(subject.menu[2], 4)
+      subject.add_item(subject.menu[2].name, subject.menu[2].price, 4)
       expect(subject.basket.last[:quantity]).to eq 4
+    end
+  end
+
+  describe '#total' do
+
+    before do
+      subject.add_item(subject.menu[1].name, subject.menu[1].price)
+      subject.add_item(subject.menu[3].name, subject.menu[3].price)
+      subject.add_item(subject.menu[4].name, subject.menu[4].price)
+      subject.add_item(subject.menu[7].name, subject.menu[7].price, 2)
+      subject.add_item(subject.menu[8].name, subject.menu[8].price, 3)
+      subject.add_item(subject.menu[10].name, subject.menu[10].price, 3)
+      subject.add_item(subject.menu[12].name, subject.menu[12].price, 2)
+    end
+
+    it 'accurately calculates the total' do
+      expect(subject.total).to eq 55.95
     end
   end
 end
