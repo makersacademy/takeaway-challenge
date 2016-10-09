47d74c57805062218d434a31edcdb37b0ab985c3
James <bakerje@mac.com>
2016-10-09 09:04:24 +0100

    Initial commit for menu class and rspec test

---
+++ lib/menu.rb
@@ -0,0 +1,25 @@
+class Menu
+
+attr_reader :print_menu, :todays_menu
+
+    def initialize
+      @print_menu = []
+      @todays_menu = []
+    end
+
+    def todays_menu
+      @todays_menu = [{:pizza => :Margherite, :price => 1.25},
+                      {:pizza => :Pepperoni, :price => 1.75},
+                      {:pizza => :Spicy, :price => 2.75}]
+                   end
+
+    def print_menu
+
+      puts "#{todays_menu}"
+
+      puts todays_menu.collect { |p| "#{p[:pizza]} pizza, Price £#{p[:price]}"}
+
+    end
+
+
+end
