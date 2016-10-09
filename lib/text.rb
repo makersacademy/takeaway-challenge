8cde8bf77a3680d6b49e3ea1e6f8c1229e94a09c
ManuCiao <mn.sabatino@gmail.com>
2016-10-09 19:06:55 +0100

    Send sms using Twilio-Ruby: Complete 4th User Stories

---
+++ lib/text.rb
@@ -0,0 +1,30 @@
+require "twilio-ruby"
+
+class Text
+TIME = "%H:%M"
+
+attr_reader :client
+
+  def initialize(client: nil)
+    @client = client || Twilio::REST::Client.new(accound_sid, auth_token)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token  = ENV['TWILIO_AUTH_TOKEN']
+  end
+
+  def deliver
+    client.messages.create(messages)
+  end
+
+  def messages
+    {
+    from:  ENV['TWILIO_NUM'],
+    to:    ENV['MY_NUM'],
+    body:  "Thank you! Your order was placed and will be delivered before #{time_deliver}"
+    }
+  end
+
+  def time_deliver
+    (Time.now + 60 + 60).strftime(TIME)
+  end
+
+end
