1e90fd1ba21d4cea4e4b2965cab3b94ef9c5d325
Dionysis <Dionysis.k22@gmail.com>
2016-10-09 01:00:17 +0100

    order initialize method completed with rspec success

--- lib/order.rb
+++ lib/order.rb
@@ -2,10 +2,12 @@ require 'menu'
 
 class Order
 
-  attr_reader :menu
+  attr_reader :menu, :current_order, :order_total
 
   def initialize
     @menu = Menu.new
+    @current_order = []
+    @order_total = 0
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -12,6 +12,14 @@ describe Order do
     it "with an instance of the menu class" do
       expect(order.menu).to be_instance_of(Menu)
     end
+
+    it "has an empy current order list at the start" do
+      expect(order.current_order.empty?).to be true
+    end
+
+    it "has zero order total" do
+      expect(order.order_total).to be(0)
+    end
   end
 
 
