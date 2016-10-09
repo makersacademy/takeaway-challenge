0f95468ff5b0ca5498d3e570af20d11d4a5f2ce7
Frances Maxwell <fmaxwell@gmail.com>
2016-10-09 22:00:31 +0100

    Refactored so menu uses Dish class to generate Dish objects

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,22 +1,23 @@
+require_relative 'dish'
+
 class Menu
 
-  attr_reader :menu
+  attr_reader :list
 
-  def initialize
-    @menu = [ {:name => "Chicken Teriyaki", :price => 6.95},
-              {:name=> "Salmon Teriyaki", :price => 7.95},
-              {:name=> "Chicken Karaage", :price => 4.95},
-              {:name=> "Pork Karaage", :price => 4.95},
-              {:name=> "Chicken Katsu Curry", :price => 7.95},
-              {:name=> "Yellow Tail Sashimi", :price => 5.95},
-              {:name=> "Salmon Sashimi", :price => 5.95},
-              {:name=> "Tuni Sashimi", :price => 5.95},
-              {:name=> "Cucumber Maki", :price => 1.95},
-              {:name=> "Miso Soup", :price => 1.50},
-              {:name=> "Boiled Rice", :price => 1.95},
-              {:name=> "Pickles", :price => 0.50},
-              {:name=> "Hot Sake", :price => 5.75},
-              {:name=> "Asahi", :price => 3.75},
+  def initialize(dish = Dish)
+    @list = [ dish.new("Chicken Teriyaki", 6.95),
+              dish.new("Salmon Teriyaki", 7.95),
+              dish.new("Chicken Karaage", 4.95),
+              dish.new("Pork Karaage", 4.95),
+              dish.new("Chicken Katsu Curry", 7.95),
+              dish.new("Yellow Tail Sashimi", 5.95),
+              dish.new("Salmon Sashimi", 5.95),
+              dish.new("Tuni Sashimi", 5.95),
+              dish.new("Cucumber Maki", 1.95),
+              dish.new("Miso Soup", 1.50),
+              dish.new("Boiled Rice", 1.95),
+              dish.new("Asahi", 3.75),
+              dish.new("Hot Sake", 5.75)
             ]
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,7 +5,7 @@ describe Menu do
   describe '#view_menu' do
 
     it 'returns an array' do
-      expect(subject.menu).to be_an_instance_of Array
+      expect(subject.list).to be_an_instance_of Array
     end
 
   end
