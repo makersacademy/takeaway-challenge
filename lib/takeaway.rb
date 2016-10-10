1020708a05cd913285e47a639ca2a7dd81457b58
crispinandrews <crispin.andrews@gmail.com>
2016-10-10 09:20:55 +0100

    Order class added to move order methods to

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,8 +2,6 @@ require_relative 'menu'
 
 class Takeaway
 
-
-
 attr_reader :menu, :order
 
   def initialize(menu = Menu.new)
@@ -15,6 +13,7 @@ attr_reader :menu, :order
     @menu.show
   end
 
+#Move methods to order class
   def add(dish, amount)
     fail 'Thats not on the menu!' if menu.do_we_have(dish) == false
     @order << {item: dish, qty: amount, total: (amount * menu.price(dish))}
