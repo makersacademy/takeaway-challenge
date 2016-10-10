aba1b8ddf9055774d684d77326f0d566b5f8a7e7
James <bakerje@mac.com>
2016-10-10 07:47:49 +0100

    Rspec for text confirmation

--- spec/make_call_spec.rb
+++ spec/make_call_spec.rb
@@ -1,12 +1,9 @@
-#require 'make_call'
+require 'make_call'
 
-#describe Checkout do
+describe Checkout do
+  describe '#initialize' do
+    expect(subject.account_sid).to eq 'ACdb0c001d98d842c9465935ffde45fb6b'
+    expec(subject.auth_token).to eq 'aa648095dec33b70637f15de4c6c9964'
+    end
 
-#  describe '#initialize' do
-#    expect(subject.account_sid).to eq 'ACdb0c001d98d842c9465935ffde45fb6b'
-#    expec(subject.auth_token).to eq 'aa648095dec33b70637f15de4c6c9964'
-#  end
-
-
-
-#end
+end
