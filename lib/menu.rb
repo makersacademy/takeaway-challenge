d03a8587e5935c74a5866eef1419be51891180aa
david anderson <d.a.m.anderson@gmail.com>
2016-10-10 10:34:31 +0100

    Fixed csv bulk menu upload

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,5 @@
 require_relative 'dish'
+require 'csv'
 
 class Menu
 
@@ -16,6 +17,16 @@ class Menu
     @dishes.each do |dish|
     puts "#{dish.name}: £#{dish.price}"
     end
+  end
+
+# Allows adding of menu items in bulk
+  def add_menu(filename = 'menu.csv')
+    csv_menu = File.open(filename, 'r')
+    csv_menu.readlines.each do |l|
+      arr = l.chomp.split(',')
+      @dishes << Dish.new(arr[0].to_sym, arr[1].to_f)
+    end
+  csv_menu.close
   end
 
 end
