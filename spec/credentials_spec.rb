9ed81ee284fa065a42d3acbef3b7d9d100afd2f8
Dagmara <daga.sz@hotmail.com>
2016-10-10 12:02:49 +0100

    Fixing the remote tests breaking without .env, added .env_example

--- spec/credentials_spec.rb
+++ spec/credentials_spec.rb
@@ -2,12 +2,12 @@ require "credentials"
 
 describe Credentials do
   it "allows you to access the Twilio account SID" do
-    expect(Credentials.acc_sid).to be_truthy
+    expect(Credentials).to respond_to(:acc_sid)
   end
   it "allows you to access the Twilio account authentication token" do
-    expect(Credentials.auth_token).to be_truthy
+    expect(Credentials).to respond_to(:auth_token)
   end
   it "allows you to access the Twilio account sender telephone number" do
-    expect(Credentials.sender).to be_truthy
+    expect(Credentials).to respond_to(:sender)
   end
 end
