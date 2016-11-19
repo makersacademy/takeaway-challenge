a8ac9da2cb5351a7eb71eeb69de50a3a42b555e2
George Eaton <george_eaton@hotmail.co.uk>
2016-11-19 13:09:32 +0000

    added food items to menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,15 @@
 class Menu
   attr_reader :list
   def initialize
-    @list = {}
+    @list = {
+      bacon_sandwich: 4,
+      scrambled_eggs: 5,
+      poached_eggs: 4,
+      toast: 1
+
+
+
+    }
   end
 
   def see_menu
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,9 +3,12 @@ require 'menu'
 describe Menu do
   subject(:menu) {described_class.new}
 
-  context "list of items" do
+  context "menu items" do
     it 'should initialize with a list hash' do
       expect(menu.list).to be_instance_of Hash
+    end
+    it 'should have a list of foods' do
+      expect(menu.list).to include(:scrambled_eggs)
     end
   end
   describe "#see_menu" do
