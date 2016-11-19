03f91b4445b5430ef7a591e35e7b75590a6861b0
Dan Pelensky <dan@pelensky.com>
2016-11-19 14:47:42 +0000

    total price working

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,12 @@
 class Order
 
-  attr_reader :selection, :shopping_cart, :menu
+  attr_reader :selection, :shopping_cart, :menu, :prices, :total
 
   def initialize
     @selection = {}
     @shopping_cart = []
     @menu = Menu.new
+    @prices = []
   end
 
   def select_items(item, quantity)
@@ -21,11 +22,14 @@ class Order
     @shopping_cart.each do |food_item|
       food_item.each do |item, quantity|
         @price = menu.menu_items[item]
-        p "#{quantity} x #{item.to_s} at £#{@price} = £#{@price * quantity}"
+        p "#{quantity} x #{item.to_s} at £#{@price} = £#{@price *= quantity}"
+        @prices << @price
       end
     end
   end
 
-
+  def total_price
+    @total = @prices.reduce { |memo, sum| memo += sum }
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -12,13 +12,24 @@ describe Order do
   end
 
 
-it "should add the selection to the order" do
-  expect(order.shopping_cart).to eq ([{ :burrito => 2 }])
-end
+  it "should add the selection to the order" do
+    expect(order.shopping_cart).to eq ([{ :burrito => 2 }])
+  end
 
-it "should get the price of each item in the order" do
-  expect{order.get_price}.to output.to_stdout
-end
+  it "should get the price of each item in the order" do
+    expect{order.get_price}.to output.to_stdout
+  end
+
+  it "should keep track of the prices" do
+    order.get_price
+    expect(order.prices).to eq([12])
+  end
+
+  it "should sum the totals of the orders" do
+    order.get_price
+    order.total_price
+    expect(order.total).to eq 12
+  end
 
 
 end
