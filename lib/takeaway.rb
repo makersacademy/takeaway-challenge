09e8caf1b41661d6e8c8fdb221c4ff7b90c24ce7
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-19 10:14:56 +0000

    hash typo

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,12 +1,12 @@
 class Takeaway
-MENU = { ":Margarita" => 12,
-":The special" => 18,
-":Chicago classic"  => 18,
-":Meat & more meat" => 19,
-":Fresh spinach" => 14,
-":Super veggie" => 18,
-":Bacon bbq chicken" => 18,
-":Chicken sausage deluxe" => 18,}
+MENU = { "Margarita" => 12,
+"The special" => 18,
+"Chicago classic"  => 18,
+"Meat & more meat" => 19,
+"Fresh spinach" => 14,
+"Super veggie" => 18,
+"Bacon bbq chicken" => 18,
+"Chicken sausage deluxe" => 18,}
 
 attr_reader :menu,:order
 
