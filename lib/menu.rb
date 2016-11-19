1655cebf13c2aff1d43689e241866cb53c6ca2f8
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-19 14:37:14 +0000

    Select dish method saves the dish in my_order array

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,15 @@
 class Menu
+  attr_reader :choices, :my_order
+  def initialize
+    @choices ={burger: 10,  salad: 5, coke: 2}
+    @my_order = []
+  end
+
+  def select_dish(dish, portions)
+    selected_dish = @choices.select{|k,v | k == dish}
+    portions.times { @my_order<< selected_dish}
+  end
+
+  # def select_my_dishes
+  # end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,10 +1,31 @@
-require "menu"
+require 'menu'
 
 describe Menu do
 
-subject(:menu) {described_class.new}
+  subject(:menu) {described_class.new}
 
 
+  context "Set up" do
+    it "sets my order as empty as default" do
+      expect(menu.my_order).to eq []
+    end
+
+    it "has a list of dishes with prices" do
+      expect(menu.choices).to eq({burger: 10,  salad: 5, coke: 2})
+    end
+  end
+
+  context "when selecting the dishes" do
+    it "order 1 portion of burger from the menu and save it in my order array" do
+      menu.select_dish(:burger, 1)
+      expect(menu.my_order).to eq([{burger: 10}])
+    end
+
+    it "order 2 portions of burger and save it in my order array" do
+      menu.select_dish(:burger, 2)
+      expect(menu.my_order).to eq([{burger: 10}, {burger: 10}])
+    end
 
 
+  end
 end
