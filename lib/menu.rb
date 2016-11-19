0740e13e2f8f8b5600ac972769e20d8f154a44ab
Dan Pelensky <dan@pelensky.com>
2016-11-19 11:30:32 +0000

    menu prints on screen

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,15 @@
 class Menu
 
+  def welcome
+    "Welcome to Dan's Mexican Resturaunt.\n We are a brand new resturaunt and appreciate your business!\n"
+  end
+
+  def list_menu
+    menu = [ {number: 1, name: "Burrito", price: 6}, {number: 2, name: "Quesadilla", price: 4}, {number: 3, name: "Tacos", price: 5}, {number: 4, name: "Taquitos", price: 6}, { number: 5, name: "Corn", price: 2} ]
+
+       menu.collect do |item|
+        p "Item Number: #{item[:number]}      #{item[:name]}      £#{item[:price]}"
+     end
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,14 @@
 require "menu"
 
 describe Menu do
+  subject(:menu) {described_class.new}
+
+  it "should display a welcome to the resturaunt" do
+    expect(menu.welcome).to eq "Welcome to Dan's Mexican Resturaunt.\n We are a brand new resturaunt and appreciate your business!\n"
+  end
+
+  it "should display a list of menu items" do
+    expect{menu.list_menu}.to output.to_stdout
+  end
 
 end
