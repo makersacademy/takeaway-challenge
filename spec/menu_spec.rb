2251ccd3d358dc3e03da18f0469393a5ab643965
Teako Warfield-Graham <teakowg@gmail.com>
2016-11-19 18:00:47 +0000

    Created order class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,7 +6,7 @@ describe Menu do
     expect(subject.menu_list).to eq([])
   end
 
-  it 'saves dishes with their prices' do
+  it 'saves dishes with prices' do
     expect(subject.add_meal("meal", 2)).to eq( [{:meal => "meal", :price => 2}] )
   end
 
@@ -14,8 +14,8 @@ describe Menu do
     expect{subject.add_meal("meal", 2.0)}.to change{subject.menu_list}
   end
 
-  it 'allows the user to choose meals to order' do
-    expect(subject.select_meal("meal")).to eq( [{:meal => "meal", :price => 2}] )
-  end
+  # it 'allows the user to choose meals to order' do
+  #   expect(subject.select_meal("meal")).to eq( [{:meal => "meal", :price => 2}] )
+  # end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,7 +2,7 @@ require 'takeaway'
 
 describe Takeaway do
 
-  let( :meals ){ double :meals }
+  # let( :meals ){ double :meals }
 
   it 'pulls a menu from the Meals class' do
     expect(subject.menu).to eq(meals.menu_list)
