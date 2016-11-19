bebb20ae2cb792c233ba4d4d49e3f9dc58295fd4
Dan Pelensky <dan@pelensky.com>
2016-11-19 11:39:53 +0000

    all tests passing

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,7 +4,7 @@ describe Menu do
   subject(:menu) {described_class.new}
 
   it "should display a welcome to the resturaunt" do
-    expect(menu.welcome).to eq "Welcome to Dan's Mexican Resturaunt.\n We are a brand new resturaunt and appreciate your business!\n"
+    expect(menu.welcome).to eq "Welcome to Dan's Mexican Resturaunt."
   end
 
   it "should display a list of menu items" do
