b85f5d57ad132a6a128561ab807b358f5348ab97
ajbater <amanda22@hotmail.co.uk>
2016-11-19 12:25:27 +0000

    it can print menu to screen

--- lib/menu.rb
+++ lib/menu.rb
@@ -9,4 +9,11 @@ class Menu
     "coca-cola" => 2
   }
 
+  def show_menu
+    line_width = 30
+    MENU.each do |item, price|
+      puts "#{item}".ljust(line_width / 2) + "£#{price.to_s}".rjust(line_width / 2)
+    end
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,8 +2,16 @@ require 'menu'
 
 describe Menu do
 
+  subject(:menu) { described_class.new }
+  let(:item) { double(item) }
+  let(:price) { double(price) }
+
   it 'has a menu saved as a hash' do
     expect(Menu::MENU).to be_kind_of(Hash)
+  end
+
+  it 'can print the menu items and prices to the screen' do
+    expect{menu.show_menu}.to output.to_stdout
   end
 
 end
