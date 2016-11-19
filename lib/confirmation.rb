19b1324af7e8e3632da408964849a14fb9450c66
Dan Pelensky <dan@pelensky.com>
2016-11-19 16:13:11 +0000

    started to implement confirmation

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -1,8 +1,25 @@
+require 'twilio-ruby'
 require_relative "order"
 
 class Confirmation
 
+  def initialize
+    account_sid = 'AC388d941b9fda6bd8669648cfe03dc9e0'
+    auth_token = 'c80382e571d3b4415c7111a572bbbf96'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
 
+  def message
+    @client.messages.create(
+    from: '+447403927326',
+    to: '+447397510122',
+    body: "Thank you! Your order was placed and will be delivered before #{@time}"
+    )
+  end
+
+  def time
+    @time = (Time.now + 3600).strftime("%H:%M")
+  end
 
 
 end
--- spec/confirmation_spec.rb
+++ spec/confirmation_spec.rb
@@ -3,4 +3,13 @@ require "./lib/confirmation"
 describe Confirmation do
   subject(:confirmation) {described_class.new}
 
+  before do
+    allow(confirmation).to receive(:send_message)
+  end
+
+  it "should send a text message that says the delivery time" do
+    expect(confirmation).to receive(:send_message) {"Thank you! Your order was placed and will be delivered before 8:00pm"}
+    confirmation.time
+  end
+
 end
