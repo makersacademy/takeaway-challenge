82fce10993bc897dafac5f1903fe520e5c0a0cfb
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-19 12:41:21 +0000

    started add to order method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,10 +1,16 @@
 #Will be placing and checking out orders
 class Restaurant
-  attr_reader :name, :menu
+  attr_reader :name, :menu, :order
 
   DEFAULT_NAME = "Top 5"
-  def initialize(name = DEFAULT_NAME)
+  def initialize(name = DEFAULT_NAME, menu_klass = Menu)
     @name = name
-    @menu = []
+    @menu = menu_klass.new
+    @order
   end
+
+  def add_to_order(dish, amount, order_klass = Order)
+
+  end
+
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,19 +2,25 @@ require "restaurant"
 
 describe Restaurant do
   subject(:restaurant) {described_class.new}
+  let(:dish) {double(:dish)}
   describe "when instantiated" do
     it "should have a default name" do
       expect(restaurant.name).to eq Restaurant::DEFAULT_NAME
     end
 
-    it "should have an empty menu" do
-      expect(restaurant.menu).to be_empty
+    it "should start with a menu" do
+      expect(restaurant.menu).not_to be_nil
+    end
+
+    it "should start with no order" do
+      expect(restaurant.order).to be_nil
     end
   end
 
-  describe "when handling the menu" do
-    it "should be able to add a dish" do
-
+  describe "when managing orders" do
+    it "should be able to start an order" do
+      restaurant.add_to_order
+      expect(restaurant.order).not_to be_nil
     end
   end
 
