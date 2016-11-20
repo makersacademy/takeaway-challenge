17d31ef838e539dc7dbce7cf322ebf983f8ebe6d
Mike Hurl <mikehurl@gmail.com>
2016-11-20 23:52:13 +0000

    adds functionality to verify order by inputting total

--- lib/order.rb
+++ lib/order.rb
@@ -1,27 +1,40 @@
 class Order
-attr_accessor :list_of_dishes
+attr_accessor :list_of_dishes, :menu
 
   def initialize
     @list_of_dishes = []
   end
 
-  def select_dish(amount, choice)
-    while amount != 0
+  def select_dish(choice, how_many)
+    while how_many != 0
       @list_of_dishes << "#{choice}: #{menu[choice.to_sym]}"
-      amount -= 1
+      how_many -= 1
     end
     @list_of_dishes
   end
 
   def menu
     @menu = {
-      cheeseburger: "£8.50",
-      fries: "£3.75"
+      cheeseburger: "8.50",
+      fries: "3.75"
     }
   end
 
   def print_menu
-    menu.each {|key, value| puts "#{key}: #{value}" }
+    printed_menu = menu.map { |dish, price| "#{dish}: £#{price}" }
+    printed_menu.each { |dish_with_price| puts dish_with_price }
   end
+
+  def verify_order(total_cost)
+    menu_list = @list_of_dishes.map { |menu_item| menu_item.to_s.split(" ") }
+    price_list = menu_list.map { |item_and_price| item_and_price[1].to_f }
+    real_cost = price_list.inject(0) { |sum, x| sum + x }
+    if real_cost != total_cost.to_f
+      false
+    else
+      true
+    end
+  end
+
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,8 +5,8 @@ describe Order do
 
   let(:menu) do
     {
-      cheeseburger: "£8.50",
-      fries: "£3.75"
+      cheeseburger: "8.50",
+      fries: "3.75"
     }
   end
 
@@ -15,23 +15,28 @@ describe Order do
   end
 
   it "allows the selection of a dish" do
-    selection = order.select_dish(1, "cheeseburger")
-    expect(order.list_of_dishes).to eq(["cheeseburger: £8.50"])
+    selection = order.select_dish("cheeseburger", 1)
+    expect(order.list_of_dishes).to eq(["cheeseburger: 8.50"])
   end
 
   it "saves the selecion of all dishes" do
-    order.select_dish(1, "fries")
-    order.select_dish(1, "cheeseburger")
-    expect(order.list_of_dishes).to eq(["fries: £3.75", "cheeseburger: £8.50"])
+    order.select_dish("fries", 1)
+    order.select_dish("cheeseburger", 1)
+    expect(order.list_of_dishes).to eq(["fries: 3.75", "cheeseburger: 8.50"])
   end
 
   it "allows the customer to order multiple quantities of a dish" do
-    expect(order.select_dish(2, "fries")).to eq(["fries: £3.75", "fries: £3.75"])
+    expect(order.select_dish("fries", 2)).to eq(["fries: 3.75", "fries: 3.75"])
+  end
+
+  it "allows a customer to verify their order is correct by inputting the total cost" do
+    order.select_dish("cheeseburger", 2)
+    expect(order.verify_order(17.00)).to eq true
   end
 
   xit "prints a list of dishes with prices" do
-    expect(subject.print_menu).to eq("Cheeseburger: £8.50
-    Fries: £3.75"
+    expect(subject.print_menu).to eq("cheeseburger: £8.50
+    fries: £3.75"
     )
   end
 
