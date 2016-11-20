c850f4481baa0c71cc9d9d657c2af00244b7e6cb
Russell <rusyeo@gmail.com>
2016-11-20 10:51:18 +0000

    rename Menu class to restaurant

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,4 +1,4 @@
-class Menu
+class Restaurant
 
   attr_reader :order
 
---
+++ spec/restaurant_spec.rb
@@ -0,0 +1,26 @@
+require 'restaurant'
+
+describe Restaurant do
+
+  let(:restaurant) {described_class.new}
+
+  it 'shows the list of pizzas' do
+    expect{restaurant.print_menu}.to output(restaurant.print_menu).to_stdout
+  end
+
+  it 'returns a the price of a pizza' do
+    expect(restaurant.pizza["Marinara"]).to eq 5.95
+    expect(restaurant.pizza["Capagnola"]).to eq 9.95
+  end
+
+  it 'responds to place order' do
+    expect(restaurant).to respond_to :place_order
+  end
+
+  it 'saves an order' do
+    new_order = { "Diavola" => 1, "Capricosa" => 2 }
+    restaurant.place_order(new_order)
+    expect(restaurant.order).to eq new_order
+  end
+
+end
