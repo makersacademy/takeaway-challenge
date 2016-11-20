ba2f02489718b0fb23dfa41305abced97d220c42
pflarcology <rhrg500@gmail.com>
2016-11-20 11:38:20 +0000

    adds list_of_dishes to menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,10 @@
 class Menu
 
+attr_accessor :list_of_dishes
+
+  def initialize
+    @list_of_dishes = { "margherita" => 9, "pepperoni" => 10, "americano" => 11 }
+    # @current_order_price = 0
+ end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,14 @@ require 'menu'
 
 describe Menu do
 
+  context '#initialize' do
+
+    it {is_expected.to respond_to :list_of_dishes}
+
+    it 'should respond with a hash' do
+      expect(subject.list_of_dishes).to include("margherita" => 9)
+    end
+
+  end
+
 end
