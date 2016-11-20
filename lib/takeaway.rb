da22e6adf376ed04a0ce3698e7ad233c23feaf3a
Jack <jack_6894@hotmail.com>
2016-11-20 13:02:02 +0000

    add order class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,24 @@
 class Takeaway
 
-  def initialize(menu:)
+attr_reader :order
+
+  def initialize(menu:, order: nil)
   @menu = menu
+  @order = order || Order.new
   end
 
   def print_menu
     menu.print
   end
 
+  def order_takeaway(dishes)
+    dishes.map do |dish, quantity|
+      order.add(dish, quantity)
+    end
+  end
+
   private
 
   attr_reader :menu
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,12 +1,20 @@
 require 'takeaway'
 
 describe Takeaway do
-  let(:takeaway) { described_class.new(menu: menu) }
+  let(:takeaway) { described_class.new(menu: menu, order: order) }
 
   let(:menu) { double(:menu, print: printed_menu) }
+  let(:order) { double(:order) }
   let(:printed_menu) { "Hawaiian: £9.99" }
+
+  let(:dishes) {  {hawaiian: 2, margherita: 1} }
 
   it 'should show a menu with dishes and prices' do
     expect(takeaway.print_menu).to eq(printed_menu)
+  end
+
+  it 'should be allowed to select several dishes for order' do
+    expect(order).to receive(:add).twice
+    takeaway.order_takeaway(dishes)
   end
 end
