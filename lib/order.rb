39d1c1555258ee75000b7cb07209dfbcf0f7ff4d
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-11-20 13:13:39 +0000

    Updated order and order_spec files

--- lib/order.rb
+++ lib/order.rb
@@ -1,30 +1,44 @@
 require_relative 'menu.rb'
+require_relative 'sms.rb'
 
 class Order
 
-  attr_accessor :orders, :menu_class
+    attr_accessor :orders, :menu_class
 
-  def initialize(menu_Klass)
-    @menu_class = menu_Klass
-  end
+    def initialize(menu_Klass)
+        @menu_class = menu_Klass
+        @current_order = nil
+    end
 
-  def see_menu
-    @orders = @menu_class.new
-  end
+    def see_menu
+        @orders = @menu_class.new
+    end
 
-  def order_food(order_number, quantity)
-    orders.make_selection(order_number, quantity)
-  end
+    def order_food(order_number, quantity)
+        orders.make_selection(order_number, quantity)
+    end
 
-  def display_bill
-    orders.users_order
-  end
+    def display_bill
+        orders.users_order
+    end
 
-  def calculate_price
-    current_order = orders.users_order.map {|choice| choice[:price]}.inject(0, :+)
-    return "The total cost is £#{current_order}"
-  end
+    def calculate_price
+        @current_order = orders.users_order.map {|choice| choice[:price]}.inject(0, :+)
+        return "The total cost is £#{current_order}"
+    end
 
+    def send_sms(message)
+        fail "Cannot send sms: Amount has not yet been calculated" unless price_calculated?
+        sms = Sms.new if price_calculated?
+        sms.send_message(message)
+    end
 
+    private
+
+    attr_accessor :current_order
+
+    def price_calculated?
+        @current_order = current_order
+    end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,34 +1,52 @@
 require 'order'
 
 describe Order do
-  subject(:order) {described_class.new(Menu)}
+    subject(:order) {described_class.new(Menu)}
+    let(:menu) { double :menu }
+    let(:sms) {double :sms, message: "Thank you, your order is on its way" }
 
-  it 'should test that checkout responds to display bill' do
-    expect(order).to respond_to :display_bill
-  end
+      it 'should test that checkout responds to display bill' do
+        expect(order).to respond_to :display_bill
+      end
 
-  it 'should test that a new menu is returned when see menu is called' do
-    expect(order.see_menu).to be_kind_of Menu
-  end
+      it 'should test that a new menu is returned when see menu is called' do
+        expect(order.see_menu).to be_kind_of Menu
+      end
 
-context "orders and prices" do
-  before do
-    order.see_menu
-  end
+    context "orders and prices" do
 
-  it 'should test that ordering food returns food ordered' do
-    expect(order.order_food(1,1)).to include {order1}
-  end
+      before do
+        order.see_menu
+      end
 
-  it 'should test that all items and prices ordered are returned' do
-    expect(order.display_bill).to include {users_order}
-  end
+      it 'should test that ordering food returns food ordered' do
+        expect(order.order_food(1,1)).to include {order1}
+      end
 
-  it 'should test that the calculate price returns the total of the bill' do
-    order.order_food(1,1)
-    order.order_food(3,3)
-    order.order_food(7,5)
-    expect(order.calculate_price).to eq "The total cost is £53"
-  end
-end 
+      it 'should test that all items and prices ordered are returned' do
+        expect(order.display_bill).to include {users_order}
+      end
+
+      it 'should test that the calculate price returns the total of the bill' do
+        order.order_food(1,1)
+        order.order_food(3,3)
+        order.order_food(7,5)
+        expect(order.calculate_price).to eq "The total cost is £53"
+      end
+    end
+
+    describe '#send sms' do
+      it 'should test that an sms is sent' do
+        expect(order).to receive(:send_message).with("Thank you, your order is on its way")
+        order.send_message("Thank you, your order is on its way")
+      end
+
+      context 'Raise error' do
+
+        it 'should raise an error if order has not been calculated' do
+          order.see_menu
+          expect{ order.send_sms("Your order has been dispatched") }.to raise_error "Cannot send sms: Amount has not yet been calculated"
+        end
+      end
+    end
 end
