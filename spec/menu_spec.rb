41f391229eb292d6ef361d7ec442a25d1417f176
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-11-20 13:13:05 +0000

    Updated menu and menu_spec files

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,20 +1,20 @@
 require 'menu'
 
 describe Menu do
-  subject(:menu) { described_class.new }
+    subject(:menu) { described_class.new }
 
-  it "should test that menu responds to view menu" do
-    expect(menu).to respond_to(:make_selection).with(2).arguments
-  end
+    it "should test that menu responds to view menu" do
+      expect(menu).to respond_to(:make_selection).with(2).arguments
+    end
 
-  it "should test that make selection returns an array" do
-    expect(menu.make_selection(1, 2)).to be_kind_of Array
-  end
+    it "should test that make selection returns an array" do
+      expect(menu.make_selection(1, 2)).to be_kind_of Array
+    end
 
   context "Users orders" do
-    let(:order1) { {number1: 1, starter1: :soup, price: 5} }
-    it "should return users order" do
-       expect(menu.make_selection(1, 1)).to include(order1)
-    end
+      let(:order1) { {number1: 1, starter1: :soup, price: 5} }
+      it "should return users order" do
+         expect(menu.make_selection(1, 1)).to include(order1)
+      end
   end
 end
