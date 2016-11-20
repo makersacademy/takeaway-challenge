9341ed9a793da0242ca0429fc98b3a566c08b6fd
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-20 18:48:03 +0000

    Added raise error functionality when adding order

--- lib/order.rb
+++ lib/order.rb
@@ -1,2 +1,13 @@
 class Order
+
+  def initialize(menu)
+    @menu = menu
+    @order = []
+  end
+
+  def add_to_order(choice)
+     raise "Invalid Choice: Please pick a food on the menu" if @menu.available_dishes(choice) == false
+
+     #@menu.select
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,4 +1,20 @@
 require 'order'
 
 describe Order do
+
+let(:menu) {double(:menu)}
+let(:order)   {Order.new(menu)}
+
+  context "When adding to order" do
+    xit "should add the item to order when item is an available dish" do
+      expect(order.add_to_order("Pizza")).to eq([{"Pizza" => 5}])
+    end
+
+    it "should raise an error if item does not exist on the menu" do
+      allow(menu).to receive(:available_dishes).and_return(false)
+      expect{order.add_to_order("Fish and Potatoes")}.to raise_error("Invalid Choice: Please pick a food on the menu")
+    end
+  end
+
+
 end
