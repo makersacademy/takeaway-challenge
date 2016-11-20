c6da84684c5cff6c9b307c7a93317b0daba7b473
Mike Hurl <mikehurl@gmail.com>
2016-11-20 17:38:16 +0000

    adds option to select a dish

--- lib/order.rb
+++ lib/order.rb
@@ -4,14 +4,17 @@ attr_reader :menu
 
   def menu
     @menu = {
-      Cheeseburger: "£8.50",
-      Fries: "£3.75"
+      cheeseburger: "£8.50",
+      fries: "£3.75"
     }
   end
 
   def print_menu
-    printed_menu = menu
-    menu.each {|key, value| puts "#{key}: #{value}\n" }
+    menu.each {|key, value| puts "#{key}: #{value}" }
+  end
+
+  def select_dish(choice)
+      "#{choice}: #{menu[choice.to_sym]}"
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,13 +5,19 @@ describe Order do
 
   let(:menu) do
     {
-      Cheeseburger: "£8.50",
-      Fries: "£3.75"
+      cheeseburger: "£8.50",
+      fries: "£3.75"
     }
   end
 
   it "has a list of dishes with prices" do
     expect(order.menu).to eq(menu)
+  end
+
+  # cannot get below test to work properly
+  it "allows the selection of a dish" do
+    selection = order.select_dish("cheeseburger")
+    expect(selection).to eq("cheeseburger: £8.50")
   end
 
   # cannot get below test to work
