c36880389072aaa149e4ae0780cfe25d56e9a822
Veronica Sonzini <mavesonzini@gmail.com>
2016-11-20 14:28:05 +0000

    customer can select 1 dish from menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,17 +1,28 @@
 require_relative 'dish'
 
-class Menu
+class TakeawayMenu
 
 attr_reader :menu
+attr_accessor :select_dishes
 
   def initialize
-    dish1 = Dish.new("Rice", "£1.99")
-    dish2 = Dish.new("Noodles", "£2.99")
-    dish3 = Dish.new("Fried Wok", "£4.99")
-    dish4 = Dish.new("Fried Rice", "£5.99")
-    dish5 = Dish.new("Greens", "£2.99")
-    menu_dishes = [dish1, dish2, dish3, dish4, dish5]
-    @menu = menu_dishes
+    dish1 = Dish.new("Rice", 1.99)
+    dish2 = Dish.new("Noodles", 2.99)
+    dish3 = Dish.new("Fried Wok", 4.99)
+    dish4 = Dish.new("Fried Rice", 5.99)
+    dish5 = Dish.new("Greens", 2.99)
+
+    @menu = [dish1, dish2, dish3, dish4, dish5]
   end
+
+  def select_dish(dish_number)
+    @menu[dish_number]
+  end
+
+  # def select_dishes(dish_number_array)
+  #   for dish_number in dish_number_array
+  #     select_dish(dish_number)
+  #   end
+  # end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,21 +1,39 @@
 require 'menu'
 
-describe Menu do
-  let(:takeaway) { Menu.new }
-  let(:dish) { double :dish_class}
-  let(:takeaway) { Menu.new(dish) }
+describe TakeawayMenu do
 
-  context '#initialize' do
+subject(:takeaway) {described_class.new}
+  describe '#initialize' do
 
-    context 'with parameters' do
+    it 'initializes with a menu' do
+      expect(subject.menu).not_to eq nil
+    end
 
-      it 'shoul use specified values' do
-        expect(takeaway.menu).to eq
+    it 'initializes with 5 items' do
+      expect(subject.menu.count).to eq 5
+    end
+  end
 
-      end
+  describe '#select_dish' do
+
+    it { is_expected.to respond_to(:select_dish).with(1).argument }
+
+    it 'selects 1 dish from menu' do
+      expect(subject.select_dish(3)).to eq subject.menu[3]
+    end
+
+    it 'selects different dishes from menu' do
 
     end
 
+  end
+
+  describe '#selected' do
+    it { is_expected.to respond_to(:select_dishes) }
+
+    # it 'should select a number of dishes' do
+    #   expect([subject.select_dishes([3,2])]).to eq [subject.dish_number_array]
+    # end
   end
 
 end
