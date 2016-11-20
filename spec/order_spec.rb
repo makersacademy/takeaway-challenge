c0eceba56946e082cbc0deb195f812af615fdd69
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-20 19:05:24 +0000

    Adds test for method 'total'

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,7 +3,7 @@ require "order"
 describe Order do
 
   first_order = {:burger=>{:portions=>1, :price=>10}}
-  second_order = {:burger=>{:portions=>2, :price=>10}}
+  second_order = {:burger=>{:portions=>2, :price=>10}, :salad=>{:portions=>2, :price=>5}}
 
   subject(:order1) {described_class.new(first_order)}
   subject(:order2) {described_class.new(second_order)}
@@ -13,6 +13,14 @@ describe Order do
       expect(order1.bucket).to eq( {:burger=>{:portions=>1, :price=>10}})
     end
 
+
+    it "gives you a total cost of the order when ordering one dish" do
+        expect(order1.total).to eq 10
+    end
+
+    it "gives you a total cost of the order when ordering multiple dishes" do
+        expect(order2.total).to eq 30
+    end
   end
 
 
