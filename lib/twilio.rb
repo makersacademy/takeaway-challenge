648830c98883fae800c84e019594b939b3c548d0
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-21 10:49:56 +0000

    refactored SMS class

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -3,21 +3,19 @@ require 'twilio-ruby'
 require 'dotenv'
 class SendMessage
 
-  attr_reader :account_sid, :auth_token, :client, :from
+  attr_reader :account_sid, :auth_token, :client
 
   def initialize
     Dotenv.load
     @account_sid = ENV['ACCOUNT_SID']
     @auth_token = ENV['AUTH_TOKEN']
     @client = Twilio::REST::Client.new account_sid, auth_token
-    @from = ENV['TWILIO_NUM']
-
   end
 
   def send
       delivery_time = Time.new + 3600
       self.client.account.messages.create(
-      :from => self.from,
+      :from => ENV['TWILIO_NUM'],
       :to => ENV['NUM'],
       :body => "Thank you! Your order was placed and will be delivered before #{delivery_time.strftime("%H:%M")}")
   end
