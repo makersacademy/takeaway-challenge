2f180e672ce424de063a92ab0a4b19ef6aff5390
ajbater <amanda22@hotmail.co.uk>
2016-11-21 10:49:21 +0000

    got dotenv to work

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,20 +1,22 @@
 
 require 'rubygems'
 require 'twilio-ruby'
+require 'dotenv'
 
 class Sms
 
   def initialize
-    account_sid = "AC950551adcf48a40cba8cf09ad9f4c71c"
-    auth_token = "67a9e7c99fad1393388a4493c24e649e"
+    Dotenv.load
+    account_sid = ENV["ACCOUNT_SID"]
+    auth_token = ENV["AUTH_TOKEN"]
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
   def send_confirmation
     delivery_time = Time.now + (60 * 60)
     @client.account.messages.create(
-    :from => "+441286727021",
-    :to => "+447828085891",
+    :from => ENV["FROM_NUMBER"],
+    :to => ENV["TO_NUMBER"],
     :body => "Thanks for placing your order! Your food will be here before #{delivery_time.strftime('%H:%M')}.")
   end
 
