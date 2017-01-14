088c0dd433ee5f5351dcb0fd651b49b67f9be40a
Kat Hicks <kmhicks92@gmail.com>
2017-01-14 14:17:45 +0000

    Wrote feature test expecting menu to respond to select_dishes in line with my feature test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,6 +2,10 @@ require 'menu'
 
 describe Menu do
   it 'should respond to the method see_dishes' do
-    expect(subject).to respond_to :see_dishes
+    expect(subject).to respond_to(:see_dishes)
+  end
+
+  it 'should respond to the method select_dishes' do
+    expect(subject).to respond_to(:select_dishes)
   end
 end
