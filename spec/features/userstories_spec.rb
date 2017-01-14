b525c3756d2b22a0dbc964c06a92f5e3049e898e
Kat Hicks <kmhicks92@gmail.com>
2017-01-14 17:07:03 +0000

    Edited old tests to take into account new argument in select_dishes method. All tests now passing

--- spec/features/userstories_spec.rb
+++ spec/features/userstories_spec.rb
@@ -11,13 +11,13 @@ describe 'Takeaway' do
 
   it 'should allow my customer to select any number of available dishes' do
     takeaway = Menu.new
-    expect{ takeaway.select_dishes('Margherita Pizza') }.not_to raise_error
-    expect{ takeaway.select_dishes('Spaghetti Carbonara', 'Calzone') }.not_to raise_error
+    expect{ takeaway.select_dishes('Margherita Pizza', 1) }.not_to raise_error
+    expect{ takeaway.select_dishes('Spaghetti Carbonara', 'Calzone', 2) }.not_to raise_error
   end
 
   it 'should allow my customer to check that their order size matches the sum of dishes in order' do
     takeaway = Menu.new
-    order = takeaway.select_dishes('Spaghetti Carbonara', 'Calzone')
+    order = takeaway.select_dishes('Spaghetti Carbonara', 'Calzone', 2)
     expect{ order.check }.not_to raise_error
     expect( order.check ).to eq(2)
   end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -8,9 +8,9 @@ describe Order do
     expect( subject ).to respond_to(:check)
   end
 
-  it 'should return the number of items in order when calling method check' do
+  it 'should return the number of items when calling method check' do
     takeaway = Menu.new
-    order = takeaway.select_dishes('Spaghetti Carbonara', 'Calzone')
+    order = takeaway.select_dishes('Spaghetti Carbonara', 'Calzone', 2)
     expect( order.check ).to eq(2)
   end
 
