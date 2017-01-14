1d12c13fdaeac14cef286b8e49193414349e13d7
Shereen Finney <shereenfinney@gmail.com>
2017-01-14 11:10:54 +0000

    Added second user story to feature test in user_stories_spec.rb

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -7,4 +7,12 @@ describe "User Stories" do
     expect { t.show_menu }.not_to raise_error
   end
 
+  # As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+  it "so a customer can select available dishes, allow a customer to place an order" do
+    t = Takeaway.new
+    expect { t.order }.not_to raise_error
+  end
+
 end # of user stories describe block
