61e1cec409a72fc9b4947ba4a50a4078a1420069
Joe Marriott <marriott90@gmail.com>
2017-01-14 14:32:40 +0000

    .rubocop.yml modified

---
+++ lib/menu.rb
@@ -0,0 +1,16 @@
+module Menu
+
+  def read_menu
+    menu = {
+      "Deluxe kebab box"=>7.99,
+      "Chicken Kebab"=>4.50,
+      "Shish Kebab"=>4.00,
+      "Pizza"=>5.00,
+      "Chips"=>2.50,
+      "Cheesy Chips"=>2.99,
+      "Coleslaw"=>0.99,
+      "BBQ Beans"=>0.99
+    }
+  end
+
+end
