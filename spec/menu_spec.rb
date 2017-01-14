996ab1e3c2f840307d5b1fa4c3cb16b9bc3785d5
Kat Hicks <kmhicks92@gmail.com>
2017-01-14 17:05:44 +0000

    Added a new unit test to ensure that exception is raised if total number of items in order is not as expected

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -11,4 +11,8 @@ describe Menu do
   it 'should respond to the method select_dishes' do
     expect(takeaway).to respond_to(:select_dishes)
   end
+
+  it 'should raise an error if the wrong number of dishes is entered' do
+    expect { takeaway.select_dishes("Calzone", "Margherita Pizza", 1) }.to raise_error("You've entered 2 dishes rather than the 1 that you expected!")
+  end
 end
