3e112f3aad7ce2e322c7d3cf1a6d694ccdec5e9d
Shereen Finney <shereenfinney@gmail.com>
2017-01-14 11:14:55 +0000

    Added second feature and rspec test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,7 @@
 require 'takeaway.rb'
 
 describe Takeaway do
-#  describe "#show_menu" do
+  describe "#show_menu" do
     context "when requested" do
       it "shows menu" do
         expect { subject.show_menu }.not_to raise_error
@@ -9,5 +9,15 @@ describe Takeaway do
         expect(subject.show_menu).to be_a Array
       end
     end
-#  end
+  end
+
+  describe "#order" do
+    context "when requested" do
+      it "can order food" do
+        expect { subject.order }.not_to raise_error
+      end
+    end
+  end
+
+
 end
