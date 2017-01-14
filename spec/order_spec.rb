a46b79253363172c551e419ad83439c684347919
Kat Hicks <kmhicks92@gmail.com>
2017-01-14 14:30:42 +0000

    Wrote a new unit test to build latest feature

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,4 +6,8 @@ describe Order do
     expect(subject).to respond_to(:check)
   end
 
+  it 'should return the number of items in order when calling method check' do
+    expect { subject.check }.to eq(2)
+  end
+
 end
