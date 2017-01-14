b5f08e0036f8f719cfd2f7c6936aba7215b32c59
Kat Hicks <kmhicks92@gmail.com>
2017-01-14 14:17:20 +0000

    Added feature test for second user story

--- spec/features/userstories_spec.rb
+++ spec/features/userstories_spec.rb
@@ -4,5 +4,11 @@ describe 'Takeaway' do
     takeaway = Menu.new
     expect { takeaway.see_dishes }.not_to raise_error
   end
-  
+
+  it 'should allow my customer to select any number of available dishes' do
+    takeaway = Menu.new
+    expect { takeaway.select_dishes('Margherita Pizza') }.not_to raise_error
+    expect { takeaway.select_dishes('Spaghetti Carbonara', 'Calzone') }.not_to raise_error
+  end
+
 end
