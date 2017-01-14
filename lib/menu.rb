fe4f938008d859b3db7bcb0c4cdbceb0629da4f6
Kat Hicks <kmhicks92@gmail.com>
2017-01-14 15:58:18 +0000

    Corrected require file paths and amended code so that dish objects are injected into the menu on initialisation

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,38 @@
+require './lib/dish.rb'
+require './lib/order.rb'
+
 class Menu
 
+  attr_reader :list
+
+  def initialize
+    @list = [
+      Dish.new('Margherita Pizza', 8.45),
+      Dish.new('Spaghetti Carbonara', 10.75),
+      Dish.new('Calzone', 12.45),
+      Dish.new('Spaghetti Bolognese', 9.95),
+      Dish.new('Caprese Salad', 4.35)
+    ]
+  end
+
   def see_dishes
+    price_list.map { |item| "#{item.name} (£#{item.price})"}.join("; ")
   end
 
   def select_dishes(*dishes)
-    Order.new
+    basket = Order.new
+    dishes.each { |dish|
+      list.each { |item|
+        basket.add(item) if item.name == dish
+      }
+    }
+    basket
+  end
+
+  private
+
+  def price_list
+    @list.dup
   end
 
 end
