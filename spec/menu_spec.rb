1b11de7f6c721c96c1f7e371519d056d11a869fc
Kat Hicks <kmhicks92@gmail.com>
2017-01-14 16:12:48 +0000

    Updated menu spec to use named subject

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,11 +1,14 @@
 require 'menu'
 
 describe Menu do
+
+  subject(:takeaway) { described_class.new }
+
   it 'should respond to the method see_dishes' do
-    expect(subject).to respond_to(:see_dishes)
+    expect(takeaway).to respond_to(:see_dishes)
   end
 
   it 'should respond to the method select_dishes' do
-    expect(subject).to respond_to(:select_dishes)
+    expect(takeaway).to respond_to(:select_dishes)
   end
 end
