4822546d5abbbc9f553bff9c40be93acf83097e4
Rick Clark <rkingsleyclark@gmail.com>
2017-01-14 18:44:11 +0000

    check_total method working

--- lib/order_total_checker.rb
+++ lib/order_total_checker.rb
@@ -1,6 +1,8 @@
 module OrderTotalChecker
 
   def self.check_total(order)
-
+    correct_total = 0
+    order.ordered_dishes.each { |dish, quantity| correct_total += (dish.price * quantity)  }
+    correct_total == order.expected_order_total
   end
 end
