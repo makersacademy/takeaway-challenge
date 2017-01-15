db5d259148eaeaf990c29366621cfa2c522352ca
Justinio14 <jbutwell@hotmail.com>
2017-01-15 16:31:24 +0000

    WIP User story 4 sending an SMS to confirm delivery time

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,15 @@
 require_relative 'menu'
-
+require_relative 'order'
+require_relative 'sms'
 
 class Takeaway
 
 attr_reader :price, :current_order
 
-  def initialize(menu:, order: nil)
+  def initialize(menu:, config:, order: nil, sms: nil)
      @menu = menu
-     @order = order
+     @order = order || Order.new(menu)
+     @sms = sms || Sms.new
     #  @price = []
     #  @current_order = []
 
@@ -15,9 +17,8 @@ attr_reader :price, :current_order
 
 
   def place_order(dishes)
-    dishes.each do |dish, quantity|
-      order.add(dish, quantity)
-    end
+    add_dishes(dishes)
+    sms.deliver
     order.total
   end
   # def show_menu
@@ -43,7 +44,14 @@ attr_reader :price, :current_order
 
 private
 
-attr_reader :menu, :order, :total
+attr_reader :menu, :order, :total, :sms
+
+
+  def add_dishes(dishes)
+    dishes.each do |dish, quantity|
+      order.add(dish, quantity)
+    end
+  end
 
   def total
     @order_total = @price.inject(:+)
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,31 +3,42 @@ require 'takeaway'
 
 describe Takeaway do
 
-  subject(:takeaway) { described_class.new(menu: menu, order: order)}
+  subject(:takeaway) { described_class.new(menu: menu, order: order, sms: sms, config: {}) }
   let(:menu) { double(:menu, show_menu: print_menu) }
   let(:print_menu) { "Spring rolls £1.50" }
   let(:order) { instance_double("Order", total: 15.50) }
-  let(:dishes) {{chicken: 2, fish:1, beef: 4 }}
+  let(:sms) { instance_double("SMS", deliver: nil) }
+  let(:dishes) {{chicken: 2, fish:1}}
 
   # let(:menu) { double :menu, dishes: {"seaweed" => 1.20} }
   # let(:place_order) { double :place_order }
   # let(:selection) { double :selection }
   # let(:quantity) { double :quantity }
 
-  it " shows the list of dishes with prices" do
+  before do
+    allow(order).to receive(:add)
+  end
+
+  it "shows the list of dishes with prices" do
   #  allow(takeaway).to receive(:show_menu).and_return({"seaweed" => 1.20})
     expect(takeaway.list_dishes).to eq(print_menu)
   end
 
   it "can order any number of available dishes" do
-    expect(order).to receive(:add).at_least(:once)
+    expect(order).to receive(:add).twice
     takeaway.place_order(dishes)
   end
 
   it "knows the order total" do
-    expect(order).to receive(:add).at_least(:once)
+    allow(order).to receive(:add)
     total = takeaway.place_order(dishes)
     expect(total).to eq(15.50)
+  end
+
+  it "sends an SMS when the order has been placed" do
+    expect(sms).to receive(:deliver)
+    #allow(order).to receive(:add)
+    takeaway.place_order(dishes)
   end
 
   # it "order takes two arguments" do
