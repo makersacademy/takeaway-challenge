4794ed7bab073b444e92e5ff24078b845e105498
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-15 20:09:03 +0000

    twilio auth tokens are now env variables

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.3.1'
 
 gem 'rake'
+gem 'envyable'
 gem 'rspec'
 gem 'coveralls', require: false
 gem 'capybara'
--- lib/text.rb
+++ lib/text.rb
@@ -1,12 +1,12 @@
 require 'twilio-ruby'
+require 'envyable'
 
 class Text
 
   attr_reader :message
 
   def initialize
-    @account_sid = 'AC9a1d50f8331660db7399b9b6e348b883'
-    @auth_token = 'ed75b51b9f53324d5d125723fa870ec6'
+    Envyable.load('./config/env.yml', 'development')
     @message = 0
   end
 
@@ -24,7 +24,7 @@ private
   end
 
   def client_send(message, client = Twilio::REST::Client)
-    client.new(@account_sid, @auth_token).account.messages.create(
+    client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']).account.messages.create(
     :body => @message,
     :to => "+447970670390",
     :from => "+441133205565")
