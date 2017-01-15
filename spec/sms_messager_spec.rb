930e5c9fcb2f9eb618167b7e6d78bfa3e2229bc4
Rick Clark <rkingsleyclark@gmail.com>
2017-01-15 17:58:47 +0000

    Adding rspec tests for sms messager and twilio client

--- spec/sms_messager_spec.rb
+++ spec/sms_messager_spec.rb
@@ -3,8 +3,15 @@ require "sms_messager.rb"
 describe SMSMessager do
 
   subject(:sms_messager) { described_class.new }
-
+  let(:client) { double("TwilioClient") }
+  let(:messages) { double("Messages") }
   it { is_expected.to respond_to(:message).with(1).argument }
 
-
+  describe "#message" do
+    it "calls to TwilioClient without raising error" do
+      allow(client).to receive(:messages) { messages }
+      allow(messages).to receive(:create)
+      expect{sms_messager.message("message string")}.not_to raise_error
+    end
+  end
 end
--- spec/twilio_client_spec.rb
+++ spec/twilio_client_spec.rb
@@ -1,21 +1,11 @@
-# require "twilio_client.rb"
-#
-# describe TwilioClient do
-#
-#  #  let(:client_class) { class_double("Client") }
-#  #  let(:client_instance) { instance_double("Client") }
-#  #  subject(:twilio_client) { described_class.new }
-#  #
-#  # describe "#initialize" do
-#  #   subject(:twilio_client_class) { described_class }
-#  #   it "creates Twilio client" do
-#  #     allow(client_class).to receive(:new) { client_instance }
-#  #     twilio_client_class.new
-#  #     expect(twilio_client_clasclient).to be_a(client_class)
-#  #   end
-#  # end
-#
-#
-#
-#
-# end
+require "twilio_client.rb"
+
+describe TwilioClient do
+
+  subject(:twilio_client) { described_class.new }
+
+  describe "#client" do
+    it { is_expected.to respond_to(:client) }
+  end
+
+end
