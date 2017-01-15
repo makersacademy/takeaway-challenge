3f8ca411d052e4f24cb89f447962bd0b6faa3cb9
Kat Hicks <kmhicks92@gmail.com>
2017-01-15 15:35:53 +0000

    Refactored to use a before block to instantiate menu class each time

--- spec/features/userstories_spec.rb
+++ spec/features/userstories_spec.rb
@@ -4,20 +4,21 @@ require 'dish'
 
 describe 'Takeaway' do
 
+  before do
+    @takeaway = Menu.new
+  end
+
   it 'should allow my customer to see a list of dishes with prices' do
-    takeaway = Menu.new
-    expect{ takeaway.see_dishes }.not_to raise_error
+    expect{ @takeaway.see_dishes }.not_to raise_error
   end
 
   it 'should allow my customer to select any number of available dishes' do
-    takeaway = Menu.new
-    expect{ takeaway.select_dishes('Margherita Pizza', 1) }.not_to raise_error
-    expect{ takeaway.select_dishes('Spaghetti Carbonara', 'Calzone', 2) }.not_to raise_error
+    expect{ @takeaway.select_dishes('Margherita Pizza', 1) }.not_to raise_error
+    expect{ @takeaway.select_dishes('Spaghetti Carbonara', 'Calzone', 2) }.not_to raise_error
   end
 
   it 'should allow my customer to check that their order size matches the sum of dishes in order' do
-    takeaway = Menu.new
-    order = takeaway.select_dishes('Spaghetti Carbonara', 'Calzone', 2)
+    order = @takeaway.select_dishes('Spaghetti Carbonara', 'Calzone', 2)
     expect{ order.check }.not_to raise_error
     expect( order.check ).to eq(2)
   end
