ea0ce7901a2064f07b876b2b3cca779fa8e5dabf
Shereen Finney <shereenfinney@gmail.com>
2017-01-15 16:23:18 +0000

    Added an order summary method to takeaway.rb

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,11 +2,10 @@ class Takeaway
 
 attr_reader :menu , :basket
 
-def initialize
-  @menu = {"Pizza"=> 6, "Curry"=> 4, "Chips"=> 2}
-  @basket = []
-
-end
+  def initialize
+    @menu = {"Pizza"=> 6, "Curry"=> 4, "Chips"=> 2}
+    @basket = []
+  end
 
   def order(dish, quantity)
     raise not_on_menu_msg if !@menu.has_key?(dish)
@@ -14,6 +13,15 @@ end
      @basket << {dish => quantity}
      p "#{quantity} x #{dish} added to your basket!"
   end
+
+  def order_summary
+  end
+
+
+
+
+
+  private
 
   def not_on_menu_msg
     "Not on the menu, please try again"
