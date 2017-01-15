0abcf2ada91fdcbfce6ecd1020a908a3d4e1ed0a
olwend <olwend@gmail.com>
2017-01-15 09:44:55 +0000

    Change for rspec test coverage

--- lib/menuhash.rb
+++ lib/menuhash.rb
@@ -5,11 +5,11 @@ attr_reader :list
 
 #key is name, value is price
 def initialize
- @list = {"Greek-style roast fish" => 4.59,
-                  "Pea falafels with minty couscous salad" => 3.59,
-                  "Mixed bean chilli with wedges" => 4.09,
-                  "Crab & Sweetcorn chowder" => 4.59,
-                  "Quinoa frittata" => 4.99}
+ @list = {"1.Greek-style roast fish" => 4.59,
+                  "2.Pea falafels with minty couscous salad" => 3.59,
+                  "3.Mixed bean chilli with wedges" => 4.09,
+                  "4.Crab & Sweetcorn chowder" => 4.59,
+                  "5.Quinoa frittata" => 4.99}
 end
 #could use to get index
 def list_to_array
@@ -17,15 +17,16 @@ def list_to_array
 end
 
 #test.add_dishes("Mango Quorn Curry", 5.95)
-def add_dishes(dish, money)
+def add_dish(dish, money)
   @list.store(dish, money)
 end
 #test.remove_dishes("Greek-style roast fish")
 def remove_dish(dish)
   @list.delete(dish)
 end
+# @ ID  = list [:ID]
 # @name    = list [:name]
 # @price   = list [:price]
 #
-#  list = Hash[:name, :price]
+#  list = Hash[:ID, :name, :price]
 end
--- spec/menuhash_spec.rb
+++ spec/menuhash_spec.rb
@@ -5,6 +5,19 @@ describe Foodbuddy do
   #double allow to receive return from hash
 
   it "stores dishes with their prices in a hash" do
-    expect(subject.list.fetch("Greek-style roast fish")).to eq(4.59.round(2))
+    expect(subject.list.fetch("1.Greek-style roast fish")).to eq(4.59.round(2))
   end
-  end
+
+ context "dish changes"
+  before(:each) do
+    subject.add_dish("6.Turbo Tagliatelle", 5.59)
+    end
+
+    it "adds a dish and price to the hash" do
+    expect{subject.add_dish("7.Pumping pumpkin pie", 5.59)}.to change{subject.list}
+    end
+
+    it "removes a dish and price to the hash" do
+      expect{subject.remove_dish("6.Turbo Tagliatelle")}.to change{subject.list}
+    end
+end
