d428671b9c514bfca8c0ee56be3adc5bca3add19
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-15 23:12:57 +0000

    extracted class SMS, can no longer stub message sending

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,5 @@
 require 'twilio-ruby'
+require './lib/sms'
 
 class Order
   attr_reader :current_order
@@ -27,30 +28,10 @@ class Order
     end
   end
 
-def place(given_total)
-  error_message = "Order total incorrect. Current total is #{self.total}"
-  raise error_message if given_total != self.total
-  send_message
-end
-
-def get_time
-  new_time = "#{Time.now.hour+1}:#{'%02d' % Time.now.min}"
-  other_new_time = "00:#{Time.now.min}"
-  Time.now.hour!=24 ? new_time : other_new_time
-end
-
-def the_message(client, time)
-  client.messages.create(
-    to: ENV["TO"],
-    from: ENV["FROM"],
-    body: "Your order has been placed and will arrive at approx. #{time}"
-  )
-end
-
-def send_message
-  time = get_time
-  client = Twilio::REST::Client.new( "AC3dc8f9d2b8b60c1e6cc4ccbb9ebc83f7","d450981a93efaa355c231db9bcf35c0e")
-  the_message(client, time)
-end
+  def place(given_total)
+    error_message = "Order total incorrect. Current total is #{self.total}"
+    raise error_message if given_total != self.total
+    Sms.new.send_message
+  end
 
 end
---
+++ lib/sms.rb
@@ -0,0 +1,23 @@
+class Sms
+
+  def get_time
+    new_time = "#{Time.now.hour+1}:#{'%02d' % Time.now.min}"
+    other_new_time = "00:#{Time.now.min}"
+    Time.now.hour!=24 ? new_time : other_new_time
+  end
+
+  def the_message(client, time)
+  client.messages.create(
+    to: ENV["TO"],
+    from: ENV["FROM"],
+    body: "Your order has been placed and will arrive at approx. #{time}"
+  )
+  end
+
+  def send_message
+    time = get_time
+    client = Twilio::REST::Client.new( "AC3dc8f9d2b8b60c1e6cc4ccbb9ebc83f7","d450981a93efaa355c231db9bcf35c0e")
+    the_message(client, time)
+  end
+
+end
