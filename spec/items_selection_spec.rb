f1393efcbdbd6b4be34062a8bd2a1957a99bace4
olwend <olwend@gmail.com>
2017-01-15 20:16:10 +0000

    fix failing test

--- spec/items_selection_spec.rb
+++ spec/items_selection_spec.rb
@@ -2,20 +2,22 @@ require './lib/items_selection'
 
 
   describe Select_items do
+       before(:each) do
+          choice = Select_items.new
+          choice.items << {id: 2, quantity: 4}
+        end
 
-    it "sets dish and quantity according to user\'s input" do
-      dbl = double("input_items":id => 3, :quantity => 4)
-      allow(subject.input_items).to receive(:gets).and_return(dbl).twice
-      expect(dbl).to receive(:gets).and_return(id)
-      expect(dbl).to receive(:gets).and_return(quantity)
+        it "fills array with menu choice" do
+          choice = Select_items.new
+          choice.items << {id: 2, quantity: 4}
+          expect(choice.items.length).to eq(1)
+        end
     end
-
-    # it "sets dish and quantity according to user\'s input" do
-    #   subject.input_items
-    # end
-    #   expect("input selection", :dish => "3", :quantity => "4")
-    #   expect(subject.instance_variable_get(dish)).to eq :dish
-    #   expect(subject.instance_variable_get(quantity)).to eq :quantity
-    # end
-
-end
+  #   it "sets dish and quantity according to user\'s input" do
+  #       choice.input_items
+  #       input_items = double("input_items":id => 3, :quantity => 4)
+  #     allow(subject.input_items).to receive(:gets).and_return(dbl).twice
+  #     expect(dbl).to receive(:gets).and_return(id)
+  #     expect(dbl).to receive(:gets).and_return(quantity)
+  #   end
+  # end
