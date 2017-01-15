04593648ed0df01cadb7893f870a560a52fa3123
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-01-15 16:29:16 +0000

    Added Twilio api and updated README.md

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -16,6 +16,9 @@ end
 before do
   allow(menu).to receive(:has_dish?).with(:lamb).and_return(true)
   allow(menu).to receive(:has_dish?).with(:chicken).and_return(true)
+
+  allow(menu).to receive(:price).with(:lamb).and_return(4.50)
+  allow(menu).to receive(:price).with(:chicken).and_return(3.50)
 end
 
   it "Selects dishes from the menu and adds to order" do
@@ -27,5 +30,13 @@ end
   it "Doesn't allow dishes that are not on the menu to the order" do
     allow(menu).to receive(:has_dish?).with(:fish).and_return(false)
     expect{ order.add(:fish, 2) }.to raise_error "Fish is not on the Menu"
+  end
+
+  it "Calculates the total for the order" do
+    order.add(:lamb, 2)
+    order.add(:chicken, 2)
+    total = 16.00
+    expect(order.total).to eq(total)
+
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,7 +4,7 @@ describe Takeaway do
   subject(:takeaway) { described_class.new(menu: menu, order: order) }
 
   let(:menu) { double(:menu, print: printed_menu) }
-  let(:order) { double(:order) }
+  let(:order) { instance_double("Order", total: 14.00) }
   let(:printed_menu) { "Lamb: £4.50" }
 
   let(:dishes) { {Lamb: 4, Chicken: 3} }
@@ -16,5 +16,11 @@ describe Takeaway do
   it 'Can select some number of several available dishes' do
     expect(order).to receive(:add).twice
     takeaway.place_order(dishes)
+  end
+
+  it 'Knows the total for the order' do
+    allow(order).to receive(:add)
+    total = takeaway.place_order(dishes)
+    expect(total).to eq(14.00)
   end
 end
