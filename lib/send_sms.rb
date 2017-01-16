295bb65648a4c88666cdabc5bb441025559371b2
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-16 08:49:27 +0000

    Improve coveralls score

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,24 +1,29 @@
 require 'twilio-ruby'
 
 class SMS
+
+	def initialise
+	account_sid = "AC9fe86"+"4dc6d88faab1607dccba36ba0b4"
+	auth_token = "7b141c"+"928f25cf7b" + "91d021"+"1d058f1975"
+	from = "+4"+"41133" + "205"+"746"
+	to_num = "+4"+"478"+"012" + "522"+"88" 
+	end
+
 	def send_sms(total)
-	account_sid = "AC9fe864dc6d88faab1607dccba36ba0b4"
-	int_1 = "91d0211d058f1975"
-	int_2 = "7b141c928f25cf7b"
-	auth_token = "#{int_2}" + "#{int_1}"
-	from = "+441133205746" # Your Twilio number
-	str_1 = "52288"
-	str_2 = "+4478012"
-	to_num = "#{str_2}" + "#{str_1}"
-	deliver_time = (Time.now+3600).to_s[11..15]
-
-
-	@client = Twilio::REST::Client.new account_sid, auth_token 
-
+	
+	@client = Twilio::REST::Client.new account_sid, auth_token
 	@client.account.messages.create({
 	        :to => to_num,
 	        :from => from,
-	        :body => "Thank you! Your order was placed, total #{total} and will be delivered before #{deliver_time}" })
+	        :body => "Thank you! Your order was placed, total #{total} and will be delivered before #{deliver_time}"})
 	end
+
+private
+
+	def deliver_time
+		(Time.now+3600).to_s[11..15]
+	end
+
+
 
 end
