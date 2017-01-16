e5dc9ad3f5c415d65889a05a61f8f03493d7da5e
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-16 10:16:15 +0000

    Update send_sms.rb

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,21 +1,16 @@
 require 'twilio-ruby'
 
 class SMS
-
-	def initialise
-	account_sid = "AC9fe86"+"4dc6d88faab1607dccba36ba0b4"
+	def send_sms(total)
+	account_sid = "AC9fe864dc6d88faab1607dccba36ba0b4"
 	auth_token = "7b141c"+"928f25cf7b" + "91d021"+"1d058f1975"
 	from = "+4"+"41133" + "205"+"746"
 	to_num = "+4"+"478"+"012" + "522"+"88" 
-	end
-
-	def send_sms(total)
-	
 	@client = Twilio::REST::Client.new account_sid, auth_token
 	@client.account.messages.create({
 	        :to => to_num,
 	        :from => from,
-	        :body => "Thank you! Your order was placed, total #{total} and will be delivered before #{deliver_time}"})
+	        :body => "#{total} and will be delivered before #{deliver_time}"})
 	end
 
 private
