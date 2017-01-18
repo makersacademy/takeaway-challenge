0ac84820f5ae814ddc9390b1a6f0ba90e8d710f9
Kat Hicks <kmhicks92@gmail.com>
2017-01-18 21:06:55 +0000

    Added comment headers to remind myself of new learning processes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -12,6 +12,8 @@ describe Menu do
     @menu_list = "Margherita Pizza (£8.45); Spaghetti Carbonara (£10.75); Calzone (£12.45); Spaghetti Bolognese (£9.95); Caprese Salad (£4.35); King Prawn Linguine (£12.95); Italian Hot Pizza (£10.25); Chicken Ceasar Salar (£10.5); Lasagne (£10.95); Mushroom Risotto (£11.95); Penne Arrabbiata (£9.95); Salad Nicoise (£5.65)"
   }
 
+  # ---------------------------------- ACTUAL TESTS ----------------------------------
+  
   it 'should respond to the method see_dishes' do
     expect(menu).to respond_to(:see_dishes)
   end
