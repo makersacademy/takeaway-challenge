0a0b1fd9dfaf2bd99e509adbd3465d5b4c326b7b
Kat Hicks <kmhicks92@gmail.com>
2017-01-18 21:35:04 +0000

    Wrote a suitable first test for public method

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -1,10 +1,19 @@
 require 'sms'
 
 describe SMS do
+
+  # ------------------------------------ DOUBLES -------------------------------------
+
   let(:time) { double("Time") }
 
-  it 'should return the right message' do
-    allow(time).to receive(:strftime).and_return("17:52")
-    expect(subject.confirmation_message(time)).to eq("Thank you! Your order was placed at 17:52 and will be delivered in half an hour.")
+  # --------------------------------- NAMED SUBJECT ----------------------------------
+
+  subject(:sms) { described_class.new(time) }
+
+  # ---------------------------------- ACTUAL TESTS ----------------------------------
+
+  it 'should respond to the method send_text' do
+    expect(sms).to respond_to(:send_text)
   end
+
 end
