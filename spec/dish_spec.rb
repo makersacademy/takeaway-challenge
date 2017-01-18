a4e9c6d3b0e045bc42268f60cac574a6de8a66f3
Kat Hicks <kmhicks92@gmail.com>
2017-01-18 21:06:33 +0000

    Added comment headers to remind myself of new learning processes

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,9 +2,14 @@ require 'dish'
 
 describe Dish do
 
+  # --------------------------------- NAMED SUBJECT ----------------------------------
+
   subject(:dish) { described_class.new('Margherita Pizza', 8.45) }
+
+  # ---------------------------------- ACTUAL TESTS ----------------------------------
 
   it 'should have two instance variables for name and price' do
     expect(dish).to have_attributes(name: 'Margherita Pizza', price: 8.45)
   end
+
 end
