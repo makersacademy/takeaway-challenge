3a55175c00b3d4e3ca3640e6afdc7301e6c687ba
Paul Goodman <paulyjgooders@gmail.com>
2017-01-27 09:00:37 +0000

    add dotenv

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ gem 'coveralls', require: false
 gem 'capybara'
 gem 'money'
 gem 'twilio-ruby', '~> 4.11.1'
+gem 'dotenv'
--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,18 +1,41 @@
 require 'twilio-ruby'
-require_relative 'undisclosed'
+# require_relative 'undisclosed'
+#
+# module Twilio
+#
+#   ACCOUNT_SID = Undisclosed::ACCOUNT_SID
+#   AUTH_TOKEN = Undisclosed::AUTH_TOKEN
+#
+#   def Twilio.send_sms(value)
+#
+#     client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+#
+#     client.account.messages.create(
+#         :from => Undisclosed::TWILIO_NO,
+#         :to => Undisclosed::MY_NO,
+#         :body => "The total to pay is #{value}. Your food will arrive in 30 mins."
+#       )
+#   end
+# end
+
+require 'dotenv'
+
+Dotenv.load
 
 module Twilio
 
-  ACCOUNT_SID = Undisclosed::ACCOUNT_SID
-  AUTH_TOKEN = Undisclosed::AUTH_TOKEN
+  ACCOUNT_SID = ENV['ACCOUNT_SID']
+  AUTH_TOKEN = ENV['AUTH_TOKEN']
+  TWILIO_NO = ENV['TWILIO_NO']
+  MY_NO = ENV['MY_NO']
 
   def Twilio.send_sms(value)
 
     client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 
     client.account.messages.create(
-        :from => Undisclosed::TWILIO_NO,
-        :to => Undisclosed::MY_NO,
+        :from => TWILIO_NO,
+        :to => MY_NO,
         :body => "The total to pay is #{value}. Your food will arrive in 30 mins."
       )
   end
