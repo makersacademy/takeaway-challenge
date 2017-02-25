c387dc53a58832900def944d8804a51f00597795
Ashwini <ashie.oh@gmail.com>
2017-02-25 22:25:05 +0000

    body of SMS set as a default message

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -2,7 +2,7 @@ require 'twilio-ruby'
 
 class MessengerService
 
-  def send_SMS
+  def send_SMS(message = "Thank you! Your order was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}")
     account_sid = 'AC151290d57f0d80c9f6886bf405a84cca'
     auth_token = 'cb17e6b65aceaae13351315ceaab2a68'
 
@@ -12,7 +12,7 @@ class MessengerService
     @client.account.messages.create(
       from: '+441730352017',
       to: '+447803309144',
-      body: "Thank you! Your order was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}"
+      body: message
       )
   end
 
