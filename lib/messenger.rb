1075af0dbec46ffd5995544aa3e2d9902d95ee09
Ashwini <ashie.oh@gmail.com>
2017-02-25 22:35:07 +0000

    variables injected as defaults

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -2,16 +2,18 @@ require 'twilio-ruby'
 
 class MessengerService
 
-  def send_SMS(message = "Thank you! Your order was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}")
-    account_sid = 'AC151290d57f0d80c9f6886bf405a84cca'
-    auth_token = 'cb17e6b65aceaae13351315ceaab2a68'
+  def initialize(sid = 'AC151290d57f0d80c9f6886bf405a84cca', token = 'cb17e6b65aceaae13351315ceaab2a68')
+    @sid = sid
+    @token = token
+  end
 
+  def send_SMS(from = '+441730352017', to = '+447803309144', message = "Thank you! Your order was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}")
     # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new @sid, @token
 
     @client.account.messages.create(
-      from: '+441730352017',
-      to: '+447803309144',
+      from: from,
+      to: to,
       body: message
       )
   end
