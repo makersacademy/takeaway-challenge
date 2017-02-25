cc17e85a61058d67c59b8c5e9f90654c0c43bb30
Ashwini <ashie.oh@gmail.com>
2017-02-25 22:55:32 +0000

    trying to use Dotenv

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -1,19 +1,23 @@
 require 'twilio-ruby'
+require 'dotenv'
 
 class MessengerService
 
-  def initialize(sid = 'AC151290d57f0d80c9f6886bf405a84cca', token = 'cb17e6b65aceaae13351315ceaab2a68')
-    @sid = sid
-    @token = token
+  def initialize()
+    Dotenv.load
+    @sid = ENV[TWILIO_AC_SID]
+    @token = ENV[TWILIO_AC_TOKEN]
+    @from = ENV[TWILIO_PHONE_NO]
+    @to = ENV[CUST_PHONE_NO]
   end
 
-  def send_SMS(from = '+441730352017', to = '+447803309144', message = "Thank you! Your order was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}")
+  def send_SMS(message = "Thank you! Your order was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}")
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new @sid, @token
 
     @client.account.messages.create(
-      from: from,
-      to: to,
+      from: @from,
+      to: @to,
       body: message
       )
   end
