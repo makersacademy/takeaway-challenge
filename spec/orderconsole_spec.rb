f7e78dd89a9dee5ebd4d0bee05343754b5a0379a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-02-25 18:21:22 +0000

    order taking user input and test stubbing STDIN

--- spec/orderconsole_spec.rb
+++ spec/orderconsole_spec.rb
@@ -1,16 +1,29 @@
 require 'orderconsole'
 
-describe OrderConsole, :o do
+describe OrderConsole do
   let(:chinese) { double }
   subject(:orderconsole) { described_class.new(chinese) }
 
-  it "returns the menu dish" do
-    menu = {"Charsui pork" => 5, "Choisum" => 3, "Drink" => 1}
-    allow(chinese).to receive_messages(restaurant_menu: menu)
-    expect(orderconsole.show_menu).to eq(menu)
+  describe "#show_menu" do
+    it "returns the menu dish" do
+      menu = {"Charsui pork" => 5, "Choisum" => 3, "Drink" => 1}
+      allow(chinese).to receive_messages(restaurant_menu: menu)
+      expect(orderconsole.show_menu).to eq(menu)
+    end
   end
 
-  # it "records the user's choices" do
-  #   expect(orderconsole.order).to
-  # end
+  describe "#order", :o do
+    before do
+      $stdin = StringIO.new("input")
+    end
+
+    # after do
+    #   $stdin = STDIN
+    # end
+
+    it "records the user's choices" do
+      expect(orderconsole.order).to eq("input")
+    end
+  end
+
 end
