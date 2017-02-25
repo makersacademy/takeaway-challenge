b4a17790dc7d75a1ce728ca20ddcc867de5e7278
Ashwini <ashie.oh@gmail.com>
2017-02-25 23:11:57 +0000

    moved account crenditials to env variables

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -5,21 +5,24 @@ class MessengerService
 
   def initialize()
     Dotenv.load
-    @sid = ENV[TWILIO_AC_SID]
-    @token = ENV[TWILIO_AC_TOKEN]
-    @from = ENV[TWILIO_PHONE_NO]
-    @to = ENV[CUST_PHONE_NO]
+    @sid = ENV['TWILIO_AC_SID']
+    @token = ENV['TWILIO_AC_TOKEN']
+    @from = ENV['TWILIO_PHONE_NO']
+    @to = ENV['CUST_PHONE_NO']
   end
 
   def send_SMS(message = "Thank you! Your order was placed and will be delivered before #{(Time.now + 3600).strftime("%H:%M")}")
+
     # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new @sid, @token
+    @client = Twilio::REST::Client.new sid, token
 
     @client.account.messages.create(
-      from: @from,
-      to: @to,
+      from: from,
+      to: to,
       body: message
       )
   end
 
+  private
+  attr_reader :sid, :token, :from, :to
 end
