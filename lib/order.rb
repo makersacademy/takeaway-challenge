5972278c70fd17b646593edbf19e6607699a6cb4
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-02-25 19:24:39 +0000

    Checks total feature added and tests passed

---
+++ lib/order.rb
@@ -0,0 +1,23 @@
+class Order
+
+  attr_reader :dishes, :menu
+
+  def initialize(menu)
+    @dishes = {}
+    @menu = menu
+  end
+
+  def add(dish, amount)
+    dishes[dish] = amount
+  end
+
+  def total
+    dish_totals.inject(:+)
+  end
+
+  def dish_totals
+    dishes.map do |dish, amount|
+      menu.price(dish) * amount
+    end
+  end
+end
