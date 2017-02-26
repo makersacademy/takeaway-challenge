96ff1722d070ef5f1aceca759351ac9e9620df1d
Vicky Roberts <vicki_uk@hotmail.com>
2017-02-26 15:50:37 +0000

    test stubs added for SMS confirmation

--- lib/text.rb
+++ lib/text.rb
@@ -3,17 +3,23 @@ require 'twilio-ruby'
 
 class Text
 
-def send_SMS(phone_number)
-account_sid = 'AC69064164571cfd340c23429315dab09d'
-auth_token = 'ea1d61a5bad892e013f212a72b6af4df'
+  def confirmation(total_price, phone_number)
+    @phone_number = phone_number
+    send_SMS("Thank you! Your order was placed and will be delivered before #{Time.now + (60*60)}. Total price: £#{total_price}")
+  end
 
-@client = Twilio::REST::Client.new account_sid, auth_token
+  def send_SMS(message)
+    account_sid = 'AC69064164571cfd340c23429315dab09d'
+    auth_token = 'ea1d61a5bad892e013f212a72b6af4df'
 
-@client.account.messages.create({
-  :from => '+441303720111',
-  :to => phone_number,
-  :body => "Thank you! Your order was placed and will be delivered before #{Time.now + (60*60)}",
-})
+    @client = Twilio::REST::Client.new account_sid, auth_token
 
-end
+    @client.account.messages.create({
+      :from => '+441303720111',
+      :to => @phone_number,
+      :body => message,
+    })
+
+  end
+
 end
