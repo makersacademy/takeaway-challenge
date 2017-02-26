007f2d7b4551592eb293a80a7d1c7df42d679c93
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-26 23:26:43 +0000

    (WIP) Create basket total methods

--- lib/order.rb
+++ lib/order.rb
@@ -1,18 +1,29 @@
 class Order
-  attr_reader :basket
+  attr_reader :basket, :menu
 
-    def initialize
+    def initialize(menu)
       @basket = {}
+      @menu = menu
     end
 
     def add_to_basket(dish, quantity)
-      # fail "#{dish} isn't on our menu." if off_menu?
+      # fail "#{dish} isn't on our menu." if  @dishes.in_stock?(dish)
       @basket[dish] = quantity
       return "#{quantity}x #{dish} has been added to your basket."
     end
 
-    #
-    # def total
-    #
-    # end
+    def basket_subtotal
+      total_qty * total_price
+      #this is just returning the first item on the menu: pizza, multiplied by the qty we ordered: 2
+    end
+
+    private
+
+    def total_qty
+      @basket.each{ |dish, qty| return qty }
+    end
+
+    def total_price
+      @menu.each { |dish, price| return price }
+    end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,13 +1,17 @@
 require 'order'
-require 'menu'
+require 'dishes'
 
 describe Order do
-  subject(:order) {described_class.new}
+  subject(:order) {described_class.new(menu)}
+  let (:menu) { {"Pizza" => 7.99, "Pasta" => 5.50, "1L Coke" => 1.40} }
   let (:example_order) do
     { Pizza: 2,
       Pasta: 1 }
   end
-  # let (:example_dish) { double example_dish: 'Pizza', quantity: 1}
+
+  it 'is initialized with the menu' do
+    expect(order.menu).to eq menu
+  end
 
   context 'basket' do
 
@@ -15,7 +19,7 @@ describe Order do
       expect(order.basket).to be_empty
     end
 
-    it 'allows the customer add a dish from the menu' do
+    it 'allows the customer add a dish' do
       order.add_to_basket(:Pizza, 2)
       order.add_to_basket(:Pasta, 1)
       expect(order.basket).to eq example_order
@@ -29,11 +33,11 @@ describe Order do
       # order.add_to_basket('Burger', 1)
       expect{order.add_to_basket('Burger', 1)}.to raise_error "That item is not on the menu"
     end
-    # This one isn't working. Also want it to be outside this method.
-
-    xit 'calculates the total cost' do
-      # Make a fake menu, add some items to the basket and calculate the values.
-      expect(order.total).to eq 10
+  
+    it 'calculates the total cost' do
+      order.add_to_basket('Pizza', 2)
+      expect(order.basket_subtotal).to eq 15.98
     end
+    #This test it not passing for the right reasons. The code is incorrect.
   end
 end
