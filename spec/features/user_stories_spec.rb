2324d944dd7cdab86458f23839fdce858bf8da87
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-25 17:19:41 +0000

    Refactor user stories

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,31 +1,21 @@
 describe 'User stories' do
   let(:takeaway) {Takeaway.new}
   let(:menu) {Menu.new}
-  let(:dish_name) {:dish_name}
-  let(:price) {:price}
-  let(:options) { {dish_name: "Dim sum", price: 2} }
 
-
+  let(:raw_dish_database) { { dim_sum: 2, fried_rice: 3, crispy_chicken: 4, pepper_squid: 5 } } 
 
 	# As a customer
 	# So that I can check if I want to order something
 	# I would like to see a list of dishes with prices
 	context 'when making an order' do
-    it 'displays a menu' do
-		  expect(takeaway.view(menu)).to eq menu
-    end
+	  it 'displays a menu' do
+	  	expect(takeaway.view_menu).to eq menu.dishes
+	  end
   end
 
   context 'when displaying menu' do
-	  it 'contains dishes and prices' do
-	    expect(menu.options).to eq options
+	  it 'contains the list of dishes and prices' do
+	    expect(menu.dishes).to eq raw_dish_database
 	  end
-
-    it 'has a dish name' do
-  	  expect(menu.options).to include dish_name
-    end
 	end
-
-
-	
 end
