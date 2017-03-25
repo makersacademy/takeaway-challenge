e2349aedc286ecf603c0a242fef97e5d9a87063c
Nick Rupp <nickrupp95@gmail.com>
2017-03-25 16:16:39 +0000

    Add public #add and private #update_order to Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,22 @@
 class Order
 
+  def initialize
+    @current_order = []
+  end
 
+  def view
+    current_order.dup
+  end
 
+  def add(menu_item, quantity)
+    quantity.times {update_order(menu_item)}
+  end
+
+  private
+
+  def update_order(menu_item)
+    self.current_order = current_order << menu_item
+  end
+
+  attr_accessor :current_order
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,7 +4,29 @@ describe Order do
 
   subject(:order) { described_class.new }
 
-  
+  describe '#view' do
+
+    it 'blank order when initialized' do
+      expect(order.view).to be_empty
+    end
+  end
+
+  describe '#add' do
+    it 'adds single item to current_order' do
+      order_size = 1
+      order.add("Lasagne", order_size)
+      expect(order.view.length).to eq(order_size)
+    end
+
+    it 'adds multiple items to current_order' do
+      order_size = 5
+      order.add("Lasagne",order_size)
+      expect(order.view.length).to eq(order_size)
+    end
+
+
+
+  end
 
 
 
