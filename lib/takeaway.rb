caa8181cf9906c520ef647102b7765387b23d3f1
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-25 17:23:04 +0000

    Create functionality that integrates dish and price database, calling it from takeaway - Code refactor

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,13 +3,19 @@ require_relative 'menu'
 
 class Takeaway
 
-	attr_reader
+	attr_reader :import_menu, :view_menu
 
 	def initialize
-
+    @import_menu
+    @view_menu
 	end
 
-	def view(menu)
-		@menu = menu
+	def import_raw_menu
+		@import_menu = Menu.new
+		menu = self.import_menu.dishes
+	end
+
+	def view_menu
+		import_raw_menu
 	end
 end
