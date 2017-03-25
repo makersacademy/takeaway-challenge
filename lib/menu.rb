eeb2b6ae4fb65388b420bace2926e411faf20b90
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-25 12:05:44 +0000

    Implement display list of dishes and prices functionality

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,15 @@
 #Contains a list of menu with dishes and prices
 class Menu
-	
+
+  attr_reader :dish_name
+
+  def initialize
+  	@dish_name
+  	@price
+  end
+
+	def options	
+	  @options = {dish_name: "Dim sum", price: 2}
+	end
+
 end
