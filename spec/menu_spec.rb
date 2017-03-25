7ab6fcd4fcde188add2c04ec00c6d6d92b6dd5bc
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-25 12:04:05 +0000

    create test for menu list to contain dishes and prices

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,4 +1,22 @@
 require 'menu'
 
-describe Menu do 
+describe Menu do
+	subject(:menu) {described_class.new}
+	let(:dish_name) {:dish_name}
+  let(:price) {:price}
+	let(:options) { {dish_name: "Dim sum", price: 2}}
+  
+  
+
+  context 'when displaying menu' do
+	  it 'contains dishes and prices' do
+	    expect(menu.options).to eq options
+	  end
+
+    it 'has a dish name' do
+  	  expect(menu.options).to include dish_name
+    end
+	end
 end
+
+
