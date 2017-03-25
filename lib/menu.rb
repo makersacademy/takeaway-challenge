fc7e81ed17e4bd498d9adb7ce83ff7ff9d60248b
alicespyglass <alicespyglass@gmail.com>
2017-03-25 14:19:29 +0000

    menu refactored with private load-menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,5 @@
 class Menu
-# class responsible for showing menu
-# require './menu.csv'
+# class responsible for loading and showing menu
 
 attr_reader :menu
 
@@ -15,6 +14,10 @@ attr_reader :menu
     end
   end
 
+private
+
+attr_writer :menu
+
   def load_menu(filename = "./lib/menu.csv")
     file = File.open(filename, "r")
     file.readlines.each do |line|
@@ -24,41 +27,4 @@ attr_reader :menu
     file.close
   end
 
-
 end
-
-=begin
-def save_students
-  # pen the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    #hobby country height
-    student_data = [student[:name], student[:cohort]]#, student[:hobby], student[:country], student[:height]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-      @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def try_load_students
-  filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit # quit the program
-  end
-end
-=end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,7 +2,6 @@ require 'menu'
 
 describe Menu do
 
-it { is_expected.to respond_to(:load_menu) }
 it { is_expected.to respond_to(:open) }
 
 end
