999d922ee91395cc658d897f6426c139559dfe0c
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-25 17:20:08 +0000

    Refactor menu tests

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,20 +2,13 @@ require 'menu'
 
 describe Menu do
 	subject(:menu) {described_class.new}
-	let(:dish_name) {:dish_name}
-  let(:price) {:price}
-	let(:options) { {dish_name: "Dim sum", price: 2}}
+  let(:raw_dish_database) { { dim_sum: 2, fried_rice: 3, crispy_chicken: 4, pepper_squid: 5 } } 
   
-  
-
   context 'when displaying menu' do
-	  it 'contains dishes and prices' do
-	    expect(menu.options).to eq options
+	  it 'contains the list of dishes and prices' do
+	    expect(menu.dishes).to eq raw_dish_database
 	  end
 
-    it 'has a dish name' do
-  	  expect(menu.options).to include dish_name
-    end
 	end
 end
 
