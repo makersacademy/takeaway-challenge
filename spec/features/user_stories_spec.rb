2ebcd8ea13ee3ef1b8e13dc3134ebf5cfdf1546e
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-25 12:03:28 +0000

    feature test display menu list containing dishes and prices

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,14 +1,31 @@
 describe 'User stories' do
-    let(:takeaway) {Takeaway.new}
-    let(:menu) {Menu.new}
+  let(:takeaway) {Takeaway.new}
+  let(:menu) {Menu.new}
+  let(:dish_name) {:dish_name}
+  let(:price) {:price}
+  let(:options) { {dish_name: "Dim sum", price: 2} }
 
 
 
 	# As a customer
 	# So that I can check if I want to order something
 	# I would like to see a list of dishes with prices
-	it 'displays a list of dishes and prices' do
-		expect(takeaway.view(menu)).to eq menu
+	context 'when making an order' do
+    it 'displays a menu' do
+		  expect(takeaway.view(menu)).to eq menu
+    end
+  end
+
+  context 'when displaying menu' do
+	  it 'contains dishes and prices' do
+	    expect(menu.options).to eq options
+	  end
+
+    it 'has a dish name' do
+  	  expect(menu.options).to include dish_name
+    end
 	end
+
+
 	
 end
