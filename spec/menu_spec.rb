a853de07573efca79b49a2ca8e885278e0c72d22
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-26 12:31:15 +0100

    Improve test coverage - refactor doubles

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,9 +2,10 @@ require 'menu'
 
 describe Menu do
 	subject(:menu) {described_class.new}
-  let(:raw_dish_database) { { dim_sum: 2, fried_rice: 3, crispy_chicken: 4, pepper_squid: 5 } } 
+  let(:raw_dish_database) { { "Dim sum"=>2, "Fried rice"=>3, "Crispy chicken"=>4, "Pepper squid"=>5 } } 
   
   context 'when displaying menu' do
+
 	  it 'contains the list of dishes and prices' do
 	    expect(menu.dishes).to eq raw_dish_database
 	  end
