cd8696f0cf8ad2b643148134fe83161157813808
kittysquee <kittysquee@gmail.com>
2017-03-26 14:52:42 +0100

    Stubs time with Timecop

--- Gemfile
+++ Gemfile
@@ -5,11 +5,13 @@ ruby '2.3.1'
 gem 'rake'
 gem 'twilio-ruby', '~> 4.11.1'
 
+
 group :development, :test do
   gem 'rspec'
   gem 'coveralls', require: false
   gem 'simplecov', require: false
   gem 'capybara'
+  gem 'timecop'
 end
 
 group :development do
--- spec/time_function_spec.rb
+++ spec/time_function_spec.rb
@@ -1,5 +1,16 @@
 require 'time_function'
+require 'timecop'
 
 describe TimeFunction do
   it { is_expected.to be_instance_of(TimeFunction)}
+
+  describe '#time_plus_one_hour' do
+    before do
+      Timecop.freeze(Time.new(2017, 3, 26, 13, 0, 0 ))
+    end
+    it 'expects to return time plus 1 hour' do
+
+        expect(subject.time_plus_one_hour).to eq "14:00"
+    end
+  end
 end
