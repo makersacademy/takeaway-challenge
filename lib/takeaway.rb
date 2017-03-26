e83b98086ca4765dd9a9d5bd1aa26dd2ccfdf391
Freddy Fallon <freddyfallon@gmail.com>
2017-03-26 13:22:17 +0100

    Move one_hour_ahead into its own method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -4,7 +4,7 @@ require 'date'
 class Takeaway
   # This class allows you to view and order dishes
 
-  attr_reader :items, :dishes, :total_order, :menu
+  attr_reader :items, :dishes, :total_order, :menu, :one_hour_ahead
 
   def initialize(menu = Menu.new)
     @menu = menu
@@ -25,19 +25,22 @@ class Takeaway
   end
 
   def place_order(cost)
-    total_order_cost
-    one_hour_ahead = DateTime.now + (1/24.0)
     fail_message = "Order total is £#{total_order_cost}. You expected £#{cost}."
     message = "Thank you! Your order costs £#{total_order_cost} and will be delivered before #{one_hour_ahead.strftime "%H:%M"}"
     fail fail_message if cost != total_order_cost
     send_sms(message)
+    message
+  end
+
+  def one_hour_ahead
+    DateTime.now + (1/24.0)
   end
 
   def send_sms(message)
     account_sid = 'AC033ff5fd1ab50ecf84e702ae695c4053'
     auth_token = '74465a4227537a433741dd0749629b3a'
     client = Twilio::REST::Client.new account_sid, auth_token
-    
+
     client.messages.create(
     from: ['+441256830268'],
     to: ['+447515356421'],
