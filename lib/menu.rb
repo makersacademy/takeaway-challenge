3837b41c1b7e2dc5dc44deb04fac36834b480560
Jack Bittiner <jackbittiner@hotmail.com>
2017-03-26 10:16:00 +0100

    Add and pass test for Menu class options

---
+++ lib/menu.rb
@@ -0,0 +1,12 @@
+class Menu
+
+  attr_reader :options
+
+  def options
+    dishes = {"Hot'n'Spicy Chicken Balls" => 3.00,
+      "Corn on the Cob" => 1.50,
+      "Potato Dauphinoise" => 2.50,
+      "Pepsi Cola" => 0.99}
+  end
+
+end
