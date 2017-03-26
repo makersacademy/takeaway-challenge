a380e7aeb7dd59ddd4f3d59330f650a8d26970ef
kittysquee <kittysquee@gmail.com>
2017-03-26 13:39:21 +0100

    Raises an error if the total is incorrect

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -16,4 +16,17 @@ describe Order do
       expect(subject.ordered_items.count).to eq(1)
     end
   end
+
+  describe '#check_order_total' do
+    it 'checks the total price of the order' do
+      subject.save_order_items({ price: "1.50" })
+      subject.save_order_items({ price: "1.50" })
+      expect(subject.check_order_total(3)).to eq(3)
+    end
+
+    it 'raises an error if the order total is incorrect' do
+      subject.save_order_items({ price: "1.50" })
+      expect{subject.check_order_total(3)}.to raise_error 'Order total incorrect'
+    end
+  end
 end
