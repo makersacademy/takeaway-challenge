45e1c9e39e3f06ce246f66e9649ff4283178a2a3
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-26 12:27:02 +0100

    Implement create order functionality

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,19 +3,32 @@ require_relative 'menu'
 
 class Takeaway
 
-	attr_reader :import_menu, :view_menu
+	attr_reader :import_menu, :view_menu, :orders, :basket, :dish, :amount, :price
 
 	def initialize
-    @import_menu
+    @import_menu = Menu.new
     @view_menu
-	end
-
-	def import_raw_menu
-		@import_menu = Menu.new
-		menu = self.import_menu.dishes
+    @orders = []
+    @dish
+    @amount
 	end
 
 	def view_menu
-		import_raw_menu
+		menu = self.import_menu.dishes
 	end
+
+	def place_order(dish, amount)
+		if view_menu.key?[dish]
+		  puts "#{amount}x #{dish} added to basket" 
+		  @dish = dish
+		  @amount = amount
+		  @price = view_menu[dish]
+		  self.add_to_orders(dish, price, amount)
+		end
+	end
+
+	def add_to_orders(dish, price, amount)
+		@orders << {dish_name: dish, price: price , amount: amount }
+	end
+  
 end
