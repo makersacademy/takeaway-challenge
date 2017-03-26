964cc99877b014338c2bba9d972e9561b90dbabb
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-26 18:32:38 +0100

    Adding Total Amount Method and Unit Test for it

--- lib/order.rb
+++ lib/order.rb
@@ -1,15 +1,25 @@
 class Order
 
-  attr_reader :selection, :menu
+  attr_reader :selection, :menu, :total
 
   def initialize(menu)
     @selection = []
     @menu = menu
+    @total = 0
   end
 
   def ordering(dish_number)
     selection << menu.board[dish_number - 1]
   end
+
+  def total_amount
+    @total = 0
+    selection.each { |dish| @total += dish.dish_price }
+    @total
+  end
+
+
+  private
 
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,7 +5,7 @@ require "interface"
 describe Order do
 
   subject(:order) {described_class.new(Menu.new)}
-  let(:food) {double :dish}
+  let(:food) {double(:dish, :dish_name => "Fish", :dish_price => 5.5)}
 
   before do
     order.menu.add_dish(food)
@@ -16,11 +16,19 @@ describe Order do
   # So that I can verify that my order is correct
   # I would like to check that the total I have been given matches the sum of the various dishes in my order
 
-  it "want to check the total is right" do
+  it "want to check the total of dishes is right" do
     order.ordering(1)
     order.ordering(1)
     expect(order.selection.count).to eq 2
     expect(order.selection.first).to eq food
+  end
+
+  it "want to check the total amount is right" do
+    order.ordering(1)
+    order.ordering(1)
+    expect(order.total_amount).to eq 11
+    order.ordering(1)
+    expect(order.total_amount).to eq 16.5
   end
 
 end
