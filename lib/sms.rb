990b6fe532e13d290fd9f380567839d92328a000
Freddy Fallon <freddyfallon@gmail.com>
2017-03-26 23:36:54 +0100

    Removed token and number from SMS send

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,7 +1,15 @@
+require 'dat'
 class SMS
+  attr_reader :token, :number
+
+  def initialize
+    @token = Token::AUTH_TOKEN
+    @number = Token::NUMBER
+  end
+
   def send_sms(message)
     account_sid = 'AC033ff5fd1ab50ecf84e702ae695c4053'
-    auth_token = '74465a4227537a433741dd0749629b3a'
+    auth_token = token
     client = Twilio::REST::Client.new account_sid, auth_token
 
     client.messages.create(
