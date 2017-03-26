a5d379b3e71c9bf8f328eeb47de9bb234c8f4764
Naz <nazmalik08@gmail.com>
2017-03-26 13:56:01 +0100

    added lookup so take_order ensures dish exists

--- lib/order.rb
+++ lib/order.rb
@@ -1,16 +1,16 @@
 #takes selection of dishes
 #looks up in menu to ensure dish is valid
-require_relative 'interface'
+require_relative 'takeaway'
 require_relative 'menu'
 
 class Order
 
-  attr_reader :items, :total
-  attr_writer :total
+  attr_reader :items, :total, :menu
 
   def initialize
     @items = {}
     @total = 0
+    @menu = Menu.new
   end
 
   def take_order
@@ -18,6 +18,10 @@ class Order
     loop do
       print "Dish: "
       dish = gets.chomp.downcase
+      until menu.dishes.key?(dish) || dish == "done"
+        puts "Input is not a dish. Try again:"
+        dish = gets.chomp.downcase
+      end
       break if dish == "done"
       print "Quantity: "
       quantity = gets.chomp.to_i
@@ -28,8 +32,9 @@ class Order
 
   private
 
+  attr_writer :total
+
   def calculate(dish, quantity)
-    menu = Menu.new
     self.total += menu.dishes[dish] * quantity
   end
 
