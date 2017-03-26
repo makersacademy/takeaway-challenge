6e8f92707e0e4de510bcda9dbda86717aa546d57
emmpak <eiliev05@yahoo.com>
2017-03-26 21:27:11 +0100

    Add environemnt variables

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,24 +1,23 @@
 require 'twilio-ruby'
+require 'dotenv/load'
 
 class SMS
 
   def initialize
-    @account_sid = "AC838244b92a9293a5b91fade646c0a2fa"
-    @auth_token = "20ec4e143070d8604ba53c177fe5c155"
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new(
+    ENV["TWILIO_ACCOUNT_SID"],
+    ENV["TWILIO_AUTH_TOKEN"]
+    )
   end
-
-    # "AC838244b92a9293a5b91fade646c0a2fa"
-    # auth_token = "20ec4e143070d8604ba53c177fe5c155"
 
   def send_message
     client.account.messages.create(
-      from: '+441454300035',
-      to: '+447548177942',
+      from: ENV["TWILIO_PHONE_NUMBER"],
+      to: ENV["PERSONAL_PHONE_NUMBER"],
       body: "Thank you! Your order was placed and will be delivered before #{(Time.now + 45*60).strftime("%H:%M")}"
       )
   end
 
   private
-  attr_reader :account_sid, :auth_token, :client
+  attr_reader :client
 end
