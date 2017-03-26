a262c3aaed763362f67afbd9c9f1cb9dd046cab7
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-26 12:32:12 +0100

    Implement create order feature tests

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,9 +1,15 @@
 describe 'User stories' do
   let(:takeaway) {Takeaway.new}
   let(:menu) {Menu.new}
-
-  let(:raw_dish_database) { { dim_sum: 2, fried_rice: 3, crispy_chicken: 4, pepper_squid: 5 } } 
-
+  let(:added_to_basket) {:added_to_basket}
+  let(:raw_dish_database) { { "Dim sum"=>2, "Fried rice"=>3, "Crispy chicken"=>4, "Pepper squid"=>5 } } 
+  let(:dish) {:dish}
+  let(:number) {:number}
+  let(:dish) {:dish}
+  let(:price) {:price}
+  let(:amount) {:amount}
+  let(:basket) { {dish_name: dish, price: price, amount: amount} }
+  
 	# As a customer
 	# So that I can check if I want to order something
 	# I would like to see a list of dishes with prices
@@ -18,4 +24,21 @@ describe 'User stories' do
 	    expect(menu.dishes).to eq raw_dish_database
 	  end
 	end
+
+	# As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+  context 'when placing an order' do
+  	context 'when checking basket' do
+      it 'displays empty orders if no orders added' do
+      	expect(takeaway.orders).to be_empty
+      end
+
+  	  it 'displays selected orders' do
+  	  	takeaway.add_to_orders(dish, price, amount)
+  	  	expect(takeaway.orders).to eq [basket]
+  	  end
+  	end
+  end
+
 end
