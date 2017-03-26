befcdc44e856f49bd7beea182a7d5e644e2ff08f
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-26 19:24:54 +0100

    Implement order checkout and complete order feature tests, which will send a confirmation message to client

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,6 +1,7 @@
 describe 'User stories' do
   let(:takeaway) {Takeaway.new}
   let(:menu) {Menu.new}
+  let(:checkout) {:checkout}
   let(:added_to_basket) {:added_to_basket}
   let(:raw_dish_database) { { "Dim sum"=>2, "Fried rice"=>3, "Crispy chicken"=>4, "Pepper squid"=>5 } } 
   let(:dish) {:dish}
@@ -9,7 +10,12 @@ describe 'User stories' do
   let(:price) {:price}
   let(:amount) {:amount}
   let(:basket) { {dish_name: dish, price: price, amount: amount} }
-  
+  let(:total) {:total} 
+
+  before do
+    allow(takeaway).to receive(:send_text)
+  end
+
 	# As a customer
 	# So that I can check if I want to order something
 	# I would like to see a list of dishes with prices
@@ -41,4 +47,25 @@ describe 'User stories' do
   	end
   end
 
+	# As a customer
+	# So that I can verify that my order is correct
+	# I would like to check that the total I have been given matches the sum of the various dishes in my order
+  # context 'when checking out' do
+  # 	context 'when calculating order' do
+  # 		xit 'calculates order' do
+  # 		  takeaway.add_to_orders(dish, price, amount)
+  # 		  expect(takeaway.checkout).to eq total
+  # 		end
+  # 	end
+  # end
+
+
+	# So that I am reassured that my order will be delivered on time
+	# I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered
+  context 'when confirming order' do
+  	it 'sends payment confirmation message' do
+  		expect(takeaway).to receive(:send_text).with("Thank you for your order: £#{takeaway.total}")
+  	  takeaway.complete_order
+  	end
+  end
 end
