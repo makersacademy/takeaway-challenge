dfe65879fc710009b07adb62f7b9160d6c810a9e
Magnus Holm <mail@magnusholm.com>
2017-03-27 19:16:30 +0100

    add order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,12 @@
 require_relative 'menu'
 class Order
 
+  attr_reader :items, :total
+  attr_writer :total
+
+  def initialize
+    @items = []
+    @total = 0
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,4 +1,17 @@
 require 'order'
 
 describe Order do
+  subject(:order) {described_class.new}
+  let(:dish) {double(:dish)}
+
+  it 'holds dishes in an array' do
+    order.items << dish
+    expect(order.items[0]).to eq dish
+  end
+
+  it 'stores the total price of order' do
+    order.total = 10
+    expect(order.total).to eq 10
+  end
+
 end
