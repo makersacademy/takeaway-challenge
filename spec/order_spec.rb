42d9fcdfa54e05f49cf362c8b06e726d6ddac367
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-22 00:07:44 +0100

    Adds Takeaway class and spec test -Passes initial unit tests -Adds alias methods to unit tests

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,11 @@
 require './lib/order'
 
 describe Order do
-  
+alias_method :order, :subject
+
+it 'creates instance of Order class' do
+  expect(order).to eq(order)
+end
+
+
 end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,9 @@
+require './lib/takeaway'
+
+describe Takeaway do
+  alias_method :takeaway, :subject
+
+  it 'creates instance of Takeaway class' do
+    expect(takeaway).to eq(takeaway)
+  end
+end
