6a79f40a3c251b2ceee0b2eed885f4eb9dac32c4
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-22 15:40:58 +0100

    #select_dish takes an argument

--- lib/order.rb
+++ lib/order.rb
@@ -2,7 +2,12 @@ require_relative 'takeaway'
 
 class Order
 
-  def select_dish
+  def initialize
+    @order_items = []
+    @order_price = 0
   end
-  
+
+  def select_dish(from_menu)
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,10 @@
 require 'order'
 
 describe Order do
-  it { is_expected.to respond_to :select_dish }
+  it 'responds to #select_dish with 1 argument' do
+    expect(subject).to respond_to(:select_dish).with(1).argument
+  end
+    # context '#select_dish' do
+  #   it { is_expected.to }
+  # end
 end
