fcb6ee25961f5b3987e44269c814b72980ca2cec
Paula <pmuldoon@umich.edu>
2017-04-22 17:31:41 +0100

    .pretty_print working

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,6 +6,11 @@ class Restaurant
     @menu = [{"Battered squid" => 5}, {"Rump steak" => 16}, {"Chips" => 2}, {"Merlot" => 5}]
   end
 
+  def pretty_print
+    return @menu.each do |a|
+      a.each { |k, v| "#{k}: £#{v}" }
+    end
+  end
 
   def show_menu
     @menu
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -10,7 +10,16 @@ describe 'restaurant' do
     it 'expects restaurant to show the menu' do
       expect(subject.show_menu).to eql (subject.menu)
     end
+  end
 
+  describe '#pretty_print' do
+    it 'expects restaurant to respond to pretty_print' do
+      expect(subject).to respond_to(:pretty_print)
+    end
+
+    it 'expects restaurant to pretty_print' do
+      expect(subject.pretty_print). to eq("Battered squid: £5")
+    end
   end
 
 end
