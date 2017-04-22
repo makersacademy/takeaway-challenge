a609f6adbdb2042b33b162b7f367406fca8ebb93
Chris Mitchell <chrisjmit@gmail.com>
2017-04-22 22:03:45 +0100

    Menu added with items and values

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,6 +2,17 @@ class Takeaway
 
   def menu
 
+    {
+      :chicken_balti => 6.50,
+      :vegetable_biryani => 6,
+      :lamb_saag => 7,
+      :chicken_naga => 7.50,
+      :tarka_dall => 3,
+      :saag_aloo => 3,
+      :plain_naan => 1.50,
+      :pilau_rice => 2
+    }
+    
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,8 +4,20 @@ describe Takeaway do
 
   let(:takeaway) { described_class.new }
 
-  it "checks that the takeaway menu exists" do
-    expect(takeaway).to respond_to(:menu)
+  describe "#dishes with prices" do
+
+    it "checks that the takeaway menu exists" do
+      expect(takeaway).to respond_to(:menu)
+    end
+
+    it "checks that the menu contains a tasty choice" do
+      expect(takeaway.menu).to include :chicken_balti
+    end
+
+    it "finds a value of a menu item" do
+      expect(takeaway.menu[:saag_aloo]).to eq 3
+    end
+
   end
 
 end
