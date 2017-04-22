3a8dd342203f4d57d97d6f9439177a23fc81405b
Paula <pmuldoon@umich.edu>
2017-04-22 16:03:07 +0100

    added tests for all 4 menu areas

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,18 @@
 class Menu
-  attr_reader :starters, :mains, :desserts, :drinks
-  # 
-  # @starters = [
-  #   {"Battered squid" => }
-  # ]
+  attr_reader :starters, :mains, :sides, :drinks
+
+  def initialize
+    @starters = [
+      { "Battered squid" => "£5" }
+    ]
+    @mains = [
+      { "Rump steak" => "£16" }
+    ]
+    @sides = [
+      { "Chips" => "£2" }
+    ]
+    @drinks = [
+      { "Merlot" => "£5"}
+    ]
+  end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,7 +1,7 @@
 require 'restaurant'
 
 describe 'restaurant' do
-  subject {Restaurant.new}
+  subject { Restaurant.new }
   describe '#menu' do
     it 'expects restaurant to respond to show_menu' do
     expect(subject).to respond_to(:show_menu)
