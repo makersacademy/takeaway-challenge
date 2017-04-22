79dbc72eac0c18edbc283057592054a3e316f88e
Sophie Mann <sklm@live.co.uk>
2017-04-22 19:02:06 +0100

    adds default_menu test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,7 +3,18 @@ require 'menu'
 describe Menu do
   subject { described_class.new([{ item: :item, price: :price }]) }
   it { is_expected.to respond_to(:dishes) }
-  it { is_expected.to respond_to}
+
+  describe '#default_menu' do
+    it 'is an array' do
+      expect(subject.default_menu).to be_an(Array)
+    end
+  end
+
+  describe '#initialize' do
+    it 'initializes with a default_menu' do
+      expect(subject.dishes).to include({ item: :item, price: :price })
+    end
+  end
 
   describe '#print_menu' do
     it 'shows items on the default_menu' do
