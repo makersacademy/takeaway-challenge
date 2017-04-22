aaaf725d82b626a714950636df5051bf492d6b38
Sophie Mann <sklm@live.co.uk>
2017-04-22 17:07:25 +0100

    adds order array and add_to_order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,19 @@ require_relative 'menu'
 
 class Takeaway
 
-  attr_reader :menu
+  attr_reader :menu, :order
 
   def initialize(menu = Menu.new)
     @menu = menu
+    @order = []
   end
 
   def see_menu
     menu.print_menu
+  end
+
+  def add_to_order(item)
+    @order << @menu.dishes[item-1]
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,7 +2,10 @@ require 'takeaway'
 
 describe Takeaway do
   subject { described_class.new(menu) }
-  let(:menu) { double :menu }
+  let(:menu) { double [:menu] }
+
+  it {is_expected.to respond_to(:add_to_order).with(1).argument}
+  it {is_expected.to respond_to(:order)}
 
   describe '#initialize' do
     it 'has a menu' do
@@ -15,6 +18,15 @@ describe Takeaway do
       expect(menu).to receive(:print_menu)
       subject.see_menu
     end
+  end
+
+  describe '#add_to_order' do
+    it 'adds items on menu to order' do
+      expect(menu).to receive(:dishes) {[:item]}
+      subject.add_to_order(1)
+      expect(subject.order).to eq [:item]
+    end
+
   end
 
 end
