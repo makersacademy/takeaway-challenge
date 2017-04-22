11d05c025604e3d4feea9040c894f829bf801768
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-22 11:23:49 +0100

    Adds #list_of_dishes method to Takeaway class -#list_of_dishes returns a hash which is stored in a constant -DISHES constant added as a let to takeaway_spec

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,9 @@
 class Takeaway
+
+  DISHES = { :beef_pho => 5.99, :vegatable_pho => 3.99, :noodles => 5.99, :chicken_wings => 4.99 }
+
+  def list_of_dishes
+    DISHES
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,9 +2,14 @@ require './lib/takeaway'
 
 describe Takeaway do
   alias_method :takeaway, :subject
+  let(:dishes) { Takeaway::DISHES }
 
   it 'creates instance of Takeaway class' do
     expect(takeaway).to eq(takeaway)
   end
-  
+
+  it 'has a list of dishes stored as constant' do
+    expect(takeaway.list_of_dishes).to eq dishes
+  end
+
 end
