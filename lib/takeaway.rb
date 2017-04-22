08d773d851713cb6ec227e214b2b5934a81dde70
Robert Jones <robert.g.jones@gmail.com>
2017-04-22 18:54:32 +0100

    Stores multiple #order in #basket

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,24 @@ require_relative 'menu'
 
 class Takeaway
 
-  attr_reader :menu
+  attr_reader :menu, :food_item, :basket
 
   def initialize
     @menu = Menu.new
+    @basket = []
   end
 
   def read_menu
     menu.list_dishes
   end
+
+  def order(item)
+    self.food_item = item
+    self.basket << item
+  end
+
+
+  private
+  attr_writer :food_item
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,14 +2,29 @@ require 'takeaway'
 
 describe Takeaway do
   subject(:t) { described_class.new }
+  let(:food) { double(:food) }
 
     describe '#read_menu' do
-      it { is_expected.to respond_to :read_menu }
-
       context do
         it 'returns Brains in menu' do
           expect(t.read_menu).to include 'Brains'
         end
+      end
+    end
+
+    describe '#order' do
+      it 'responds to order' do
+        expect(t).to respond_to(:order).with(1).arguments
+      end
+
+      it 'stores an order' do
+        expect(t.order(food)).to include t.food_item
+      end
+
+      it 'stores more than one item in a basket' do
+        t.order(food)
+        t.order(food)
+        expect(t.basket).to include food
       end
 
     end
