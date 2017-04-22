fe01a1b5cbe1ad88c02f3d5c14cc244eae523a71
Alex Chalk <alexchalk17@gmail.com>
2017-04-22 14:03:35 -0400

    Output a menu to stdout using #list_item

--- lib/list.rb
+++ lib/list.rb
@@ -1,5 +1,7 @@
 require 'CSV'
-require 'list_item'
+require_relative 'list_item'
+$line_width = 80
+$column_width = 40
 class List
 
   def initialize
@@ -9,7 +11,7 @@ class List
   def view_items(file="takeawaylist.csv")
     reset_menu_items
     load_csv(file)
-    print_menu_items
+    print_menu
   end
 
   private
@@ -27,10 +29,34 @@ class List
   end
 
   def reset_menu_items
+    self.menu_items = []
+  end
+
+  def print_menu
+    print_header
+    print_menu_items
+    print_footer
+  end
+
+  def print_header
+    puts "Menu Items".upcase.center($line_width)
+    print "\n"
+    # headers like name/price could go here
+    # print "\n"
+    print ("-" * $line_width)
+    print "\n"
   end
 
   def print_menu_items
+    menu_items.each do |item|
+      print item.name.capitalize.ljust($column_width)
+      print item.price.to_s.rjust($column_width)
+      print "\n"
+    end
+  end
 
+  def print_footer
+    puts ("-" * $line_width)
   end
 
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -4,12 +4,7 @@ describe List do
   subject(:list) { described_class.new }
 
   describe '#view_items' do
-    before(:context) do
-      File.open("test_list.csv", "w") do |file|
-        file.puts "Margherita, 6"
-        file.puts "Trois Fromages, 8"
-      end
-    end
+    before(:context) { File.open("test_list.csv", "w") {|file| file.puts "Margherita, 6"} }
 
     it 'resets menu items' do
       expect(list).to receive(:reset_menu_items)
@@ -21,12 +16,13 @@ describe List do
       list.view_items("test_list.csv")
     end
 
-    it 'calls #print_menu_items' do
-      expect(list).to receive(:print_menu_items)
+    it 'calls #print_menu' do
+      expect(list).to receive(:print_menu)
       list.view_items("test_list.csv")
     end
 
-    specify { expect { list.view_items("test_list.csv") }.to output('Margherita 6').to_stdout }
+    specify { expect { list.view_items("test_list.csv") }.to output(/Margherita/).to_stdout }
+    specify { expect { list.view_items("test_list.csv") }.to output(/6/).to_stdout }
 
     after(:context) { File.delete("test_list.csv") }
   end
