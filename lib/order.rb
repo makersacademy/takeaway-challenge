78d093e2141cdd6c439cc0c53f225b23304482f9
Filipe Fasolin <ffasolin@gmail.com>
2017-04-22 17:06:19 +0100

    Adds view_order method (3rd user story)

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,29 @@
+require_relative 'menu'
+
 class Order
 
-  attr_reader :trolley
+  attr_reader :trolley, :menu
 
-  def my_order(number)
-    menu = Menu.new
+  def initialize
+    @menu = Menu.new
     @trolley = []
-    @trolley << menu.pizzas.values[number - 1]
+  end
+
+  def place_order(number)
+    choice = menu.pizzas.values[number - 1]
+    @trolley << choice
+  end
+
+  def view_order
+    puts "Your Order:"
+    @trolley.each_with_index { |choices, i| choices.each { |dish, price| puts "#{i+1}. #{dish}, #{price}" } }
+    puts "Total: $#{total_price}"
+  end
+
+  def total_price
+    prices = []
+    @trolley.each { |choices| choices.each { |dish, price| prices << price[1..-1].to_i } }
+    prices.reduce(:+)
   end
 
 end
