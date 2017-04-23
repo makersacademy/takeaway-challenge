e76d8325747574dcc21e746bd02e530ddd8b165e
Nigel Said <nigelsaid@gmail.com>
2017-04-23 20:26:06 +0100

    adds to order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -1,1 +1,33 @@
 # Allows user to make their selection
+class Order
+  attr_accessor :customer_order, :selection, :quantity
+
+  def initialize
+    @customer_order = []
+  end
+
+  def take_order
+    select_item
+    select_item_quantity
+    @customer_order << [@selection, @quantity]
+    clear_current_choices
+  end
+
+  def select_item
+    puts "Please select the item you want by its code"
+    @selection = $stdin.gets.chomp
+    raise "Selection cancelled" unless selection == "a" || "b" || "c"
+  end
+
+  def select_item_quantity
+    puts "Please specify the quantity you'd like"
+    @quantity = $stdin.gets.chomp.to_i
+    raise "Selection cancelled" if quantity.zero?
+  end
+
+  def clear_current_choices
+    @selection = nil
+    @quantity = nil
+  end
+
+end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,1 +1,49 @@
 require 'order'
+
+describe 'Order' do
+  subject = Order.new
+
+  it "starts with an empty list" do
+    expect(subject.customer_order).to eq []
+  end
+end
+
+describe 'Order' do
+  subject = Order.new
+
+  before do
+      IO.any_instance.stub(:puts)
+      $stdin = StringIO.new("test\n")
+      subject.select_item
+  end
+
+  after do
+   $stdin = STDIN
+ end
+
+  it "selects of an item called 'test'" do
+    expect(subject.selection).to eq "test"
+  end
+
+  before do
+      IO.any_instance.stub(:puts)
+      $stdin = StringIO.new("5\n")
+      subject.select_item_quantity
+  end
+
+  after do
+   $stdin = STDIN
+ end
+
+  it "selects a quantity of 5" do
+    expect(subject.quantity).to eq 5
+  end
+
+
+it "clears current choices" do
+  subject.clear_current_choices
+  expect(subject.selection).to eq nil
+  expect(subject.quantity).to eq nil
+end
+
+end
