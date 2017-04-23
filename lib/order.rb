41b69ddf1eb06f0bb7e0c7865470fa99241d20fe
danielemanuel <daniel.costea@vkernel.ro>
2017-04-23 10:18:40 +0100

    Some functionality from takeaway class moved to order class

--- lib/order.rb
+++ lib/order.rb
@@ -4,12 +4,20 @@ class Order
 
   attr_reader :menu, :order_list
 
-  def initialize
-    @menu = Menu.new
+  def initialize(menu)
+    @menu = menu
     @order_list = {}
   end
 
+  def place_order(item, quantity)
+    @order_list[item.to_sym] = total_price(item, quantity)
+  end
 
+ # private
+
+  def total_price(item, quantity)
+    quantity * menu.list[item.to_sym]
+  end
 
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,6 +2,23 @@ require 'order'
 
 describe Order do
 
+  subject(:order_system) { described_class.new(Menu.new) }
 
+ describe "#order" do
+    context "when give an item and quantity" do
+      before :each do
+      @item = order_system.menu.list.first[0]
+      order_system.place_order(@item, 2)
+    end
 
+      it "add the item to the order_list hash" do
+      expect(order_system.order_list.first[0]).to be @item
+    end
+
+    it "multiplies the item price by the quantity" do
+      price = order_system.menu.list.first[1]
+      expect(order_system.order_list.first[1]).to eq(price * 2)
+    end
+  end
+end
 end
