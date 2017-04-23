3951bb829819c1ee85afbb3f0eba78194f6c744c
Alex Chalk <alexchalk17@gmail.com>
2017-04-23 14:28:35 -0400

    Correct Twilio sender/receiver mixup

--- lib/sms_sender.rb
+++ lib/sms_sender.rb
@@ -4,7 +4,7 @@ module SMSSender
   
   def send_sms(sender, receiver, message)
     client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
-    client.account.messages.create(to: sender, from: receiver, body: message)
+    client.account.messages.create(to: receiver, from: sender, body: message)
   end
 
 end
