dc9914af0d8e2da3e02cd1463de247f9d5e71ab9
Filipe Fasolin <ffasolin@gmail.com>
2017-04-23 20:09:30 +0100

    Adds stub testing for text notification

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -3,15 +3,18 @@ require 'hour'
 
 describe Sms do
 
+  subject(:sms) { described_class.new }
+
   describe '#send_message' do
     it { is_expected.to respond_to(:send_message) }
 
-    # it 'should send a text notification' do
-    #   order = Order.new.place_order
-    #   message = "Thank you! Your order was placed and will be delivered before #{Hour.new.eta_to_string}"
-    #   allow_any_instance_of(order).to receive(:send_message).with(message)
-    #   expect(order).to eq message
-    # end
-  end
+    before do
+      allow(sms).to receive(:send_message)
+    end
 
+    it 'sends a payment confirmation text message' do
+      expect(sms).to receive(:send_message)
+      sms.send_message
+    end
+  end
 end
