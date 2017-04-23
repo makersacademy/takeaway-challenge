e253308e0b7f2809fa4f7587de35efc0ea81d36a
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-04-23 22:26:16 +0100

    Total matches sub-total

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,10 +1,11 @@
 require_relative 'take_away'
 
 class Takeaway
-  attr_accessor :menu, :basket, :order_sum
+  attr_accessor :menu, :basket, :order_sum, :sub_total
 
   def initialize(basket = [])
     @basket = basket
+    @sub_total = 0
   end
 
   def menu
@@ -17,15 +18,23 @@ class Takeaway
   end
 
   def order(*order)
-    @basket += order
+    "Basket: #{@basket += order}"
   end
 
   def order_summary
     @order_sum = menu.values_at(*basket).compact
+    items = @order_sum.map{|x| x[:item]}
+    b = Hash.new(0)
+    items.each{ |v| b[v] += 1}
+    food_total = @order_sum.map{|x| x[:price]}
+    @sub_total = food_total.sum
+    b.each{ |k, v| }
   end
 
-  def order_total
-    total_price = @order_sum.map{|x| x[:price]}
-    "Total to pay: £#{total_price.sum}"
+  def total
+    food_total = @order_sum.map{|x| x[:price]}
+    total = food_total.sum
+    raise ArgumentError, "Total incorrect" if total != @sub_total
+    "Thank you! Your order was placed and will be delivered before 18:52"
   end
 end
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -3,26 +3,39 @@ require 'take_away'
 describe Takeaway do
   describe '#order' do
     it 'order equals empty array' do
-      expect(subject.order).to eq []
+      expect(subject.order).to eq "Basket: []"
     end
 
     it 'orders plain noodles' do
-      expect(subject.order(1)).to eq [1]
+      expect(subject.order(1)).to eq "Basket: [1]"
     end
   end
 
   describe '#order summary' do
     it 'shows order summary' do
       subject.order(1)
-      expect(subject.order_summary).to eq [{"Plain noodle"=>3}]
+      hash = {"Plain noodle"=>1}
+      expect(subject.order_summary).to eq hash
     end
   end
 
-  describe '#order_total' do
-    it 'calculates total of order' do
+  context 'subject has more than one order' do
+    before do
       subject.order(1)
       subject.order(2)
-      expect(subject.order_total).to eq 6
+      subject.order_summary
+    end
+
+  describe '#order_total' do
+    it 'calculates total of order' do
+      expect(subject.total).to eq "Total to pay: £6"
+    end
+
+    it 'raises error if total does not match sub total' do
+      subject.sub_total = 100
+      expect{ subject.total }.to raise_error "Total incorrect"
     end
   end
+end
+
 end
