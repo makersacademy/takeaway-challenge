b9bf165ce12a0de3be8281abad600fb46a05ae8c
danielemanuel <daniel.costea@vkernel.ro>
2017-04-23 10:22:42 +0100

    Initialing doubles on the order tests

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,10 +3,13 @@ require 'order'
 describe Order do
 
   subject(:order_system) { described_class.new(Menu.new) }
+  subject(:menu) { described_class.new(menu) }
+  let(:menu) { double(:menu) }
 
  describe "#order" do
     context "when give an item and quantity" do
       before :each do
+      allow(menu).to receive(:list).and_return({:pizza => 5.99 })
       @item = order_system.menu.list.first[0]
       order_system.place_order(@item, 2)
     end
