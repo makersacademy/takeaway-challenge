af5bc8e5eb661005b1c2eb18479e9cd29d8842d3
Sara Riley <saralynnaz@gmail.com>
2017-04-23 09:57:23 +0100

    Update Menu class and add print method

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,18 +2,26 @@ class Menu
 
   attr_reader :menu_items
 
-  def initialize
-    @menu_items = {
-    Hamburger: 4.00,
-    Cheeseburger: 4.50,
-    Hot_Dog: 3.50,
-    Mac_n_Cheese: 3.00,
-    Fries: 2.00,
-    Onion_Rings: 2.00,
-    Salad: 2.00,
-    Milkshake: 2.50,
-    Soda: 1.50
-    }
+  DEFAULT_MENU = {
+  Hamburger: 4.00,
+  Cheeseburger: 4.50,
+  Hot_Dog: 3.50,
+  Mac_n_Cheese: 3.00,
+  Fries: 2.00,
+  Onion_Rings: 2.00,
+  Salad: 2.00,
+  Milkshake: 2.50,
+  Soda: 1.50
+  }
+
+  def initialize(menu_items = DEFAULT_MENU)
+    @menu_items = menu_items
+  end
+
+  def print_menu
+    menu_items.map do |item, price|
+      "%s £%.2f" % [item.to_s.capitalize, price]
+    end.join(", ")
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,27 @@
 require 'menu'
 
 describe Menu do
-  let(:menu) { described_class.new }
 
-  it 'has a list of food items and prices' do
+  subject(:menu) { described_class.new(menu_items) }
+
+  let(:menu_items) do
+    {
+      pizza: 5.00,
+      nachos: 3.50
+    }
+  end
+
+  it 'has menu with content' do
     expect(menu.menu_items).not_to be_empty
   end
+
+  it 'has a list of food items and prices' do
+    expect(menu.menu_items).to eq(menu_items)
+  end
+
+  it 'prints a list of food items with prices' do
+    food_list = "Pizza £5.00, Nachos £3.50"
+    expect(menu.print_menu).to eq(food_list)
+  end
+
 end
