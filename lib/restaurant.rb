e07bc4d722e10b9d64cfd95dd733a7337a03fe29
Alex Chalk <alexchalk17@gmail.com>
2017-04-23 11:54:48 -0400

    A restaurant will initialize with a list and can be asked to view it

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,18 @@
+require_relative 'list'
+
 class Restaurant
+
+  def initialize
+    @menu = List.new
+  end
+
+  def view_menu(menu_file = nil)
+    menu_file.nil? ? menu.view_items : menu.view_items(menu_file)
+  end
+
+
+  private
+
+  attr_reader :menu, :waiter
 
 end
