35be0c0d33864c2de50b965279a72d7369b74599
Filipe Fasolin <ffasolin@gmail.com>
2017-04-23 01:26:16 +0100

    Fixes time string bug

--- lib/hour.rb
+++ lib/hour.rb
@@ -4,9 +4,9 @@ class Hour
 
   def eta_to_string
     @current_time = Time.now.strftime("%H:%M")
-    hour = @current_time.split("")[1]
     return midnight if @current_time[0..1] == "23"
-    @current_time.delete(hour).insert(1, (hour.to_i + 1).to_s)
+    hour = @current_time.slice!(1)
+    @current_time.insert(1, (hour.to_i + 1).to_s)
   end
 
   def midnight
