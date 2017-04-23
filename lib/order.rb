e292299e0ffe576991d2140505f08d67e944c45f
SimonTanner <simonthetanner@hotmail.com>
2017-04-23 18:24:26 +0000

    Updated order and order_spec file to create an empty menu upon instantiation and allow you to add a menu. Order_spec now uses a double of a menu instance with .dishes method callable

--- lib/order.rb
+++ lib/order.rb
@@ -2,15 +2,28 @@ require './lib/menu'
 
 class Order
     
-    def show_menu(menu)
+    attr_reader :menu
+    
+    def initialize(menu = [])
+        @menu = menu
+        @order = []
+    end
+    
+    def add_menu(menu)
+        
+        @menu = menu
+        
+    end
+    
+    def show_menu
         i = 1
-        menu.each do |item|
+        @menu.dishes.each do |item|
             puts "#{i}. #{item[:dish]} #{item[:price]}"
             i +=1
         end
     end
     
     def order(dish)
-        
+        @order << @menu.dishes[dish -1]
     end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,22 +2,30 @@ require './lib/order'
 
 describe Order do
     
-    let(:test_menu) { [{:dish => "Soup", :price => 7.00}, {:dish => "Salad", :price => 8.00}] }
+    let(:test_menu) { double(:dishes => [{:dish => "Soup", :price => 7.00}, {:dish => "Salad", :price => 8.00}]) }
     
     describe '#menu' do
+        before(:example) { subject.add_menu(test_menu) }
+        
         it "Gives me a menu from which to order" do
-            expect(subject.show_menu(test_menu)).to eq test_menu
+            expect(subject.menu).to eq test_menu
         end
         
         it "Gives the menu with each item numbered" do
-            expect { subject.show_menu(test_menu) }.to output("1. #{test_menu[0][:dish]} #{test_menu[0][:price]}\n2. #{test_menu[1][:dish]} #{test_menu[1][:price]}\n").to_stdout
+            expect { subject.show_menu }.to output("1. #{test_menu.dishes[0][:dish]} #{test_menu.dishes[0][:price]}\n2. #{test_menu.dishes[1][:dish]} #{test_menu.dishes[1][:price]}\n").to_stdout
         end
     end
     
     it { is_expected.to respond_to(:order).with(1).arguments }
     
-    # it "Allows me to order a dish and will add this to our order" do
-    #     expect(subject
+    describe '#order' do
         
-    # end
+        before(:example) { subject.add_menu(test_menu) }
+        
+        it "Stores the dishes ordered" do
+            expect(subject.order(1)).to include test_menu.dishes[0]
+        end
+        
+    end
+
 end
