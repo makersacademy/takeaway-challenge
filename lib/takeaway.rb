f1292d03a8b59395dec3bfe2d41ecfc7f23b9208
danielemanuel <daniel.costea@vkernel.ro>
2017-04-23 09:53:33 +0100

    Printing the menu method initialized

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -4,4 +4,15 @@ require_relative 'order'
 
 class Takeaway
 
+  attr_reader :menu, :order_list
+
+  def initialize
+    @menu = Menu.new
+    @order_list = {}
+  end
+
+  def show_menu
+    menu.print_menu
+  end 
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,6 +2,15 @@ require 'takeaway'
 
 describe Takeaway do
 
-  
+  subject(:takeaway) {described_class.new}
+
+  describe '#show_menu' do
+    context 'Shows the menu list' do
+      it 'calls #menu_print' do
+        expect(takeaway.menu).to receive(:print_menu)
+        takeaway.show_menu
+      end
+    end
+  end 
 
 end
