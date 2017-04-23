a7f9968cda24e4a7e845f09b1b3e63fdc5aa39b6
Alex Law <achlawuk@gmail.com>
2017-04-23 20:14:35 +0100

    Add order.rb, order_spec.rb and menu dish quantitiy

--- lib/menu.rb
+++ lib/menu.rb
@@ -8,8 +8,12 @@ class Menu
   end
 
   def print_menu
-  	dishes.map do |item, price|
-    "%s £%.2f" % [item.to_s.capitalize, price]
+    dishes.map do |item, price|
+      "%s £%.2f" % [item.to_s.capitalize, price]
     end.join(', ')
+  end
+
+  def has_dish?(dish)
+    !dishes[dish].nil?
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -13,11 +13,19 @@ describe Menu do
   	}
   end
   it "contains list of dishes and prices" do
-  	expect(menu.dishes).to eq(dishes)
+    expect(menu.dishes).to eq(dishes)
   end
 
   it "prints dishes list with prices" do
     shown_menu = "Sushi £9.99, Ramen £5.99, Udon £5.99, Miso £3.99"
     expect(menu.print_menu).to eq(shown_menu)
+  end
+
+  it "tells if dish is on the menu" do
+    expect(menu.has_dish?(:sushi)).to be true
+  end
+
+  it "tells if dish is not on the menu" do
+    expect(menu.has_dish?(:curry)).to be false
   end
 end
