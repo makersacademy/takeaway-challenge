778ecf6011e72a8da1d40a72f16ea713eb3046be
Robert Jones <robert.g.jones@gmail.com>
2017-04-23 23:59:30 +0100

    Updates twilio_api and tests

--- lib/twilio_api.rb
+++ lib/twilio_api.rb
@@ -1,14 +1,24 @@
+require 'dotenv/load'
 require 'twilio-ruby'
 
-# put your own credentials here
-account_sid = 'AC09df84fe1d2a8ea043d55ac93dc90fc5'
-auth_token = 'c897fa5455b04e8979a5ae59cd6eeddc'
 
-# set up a client to talk to the Twilio REST API
-@client = Twilio::REST::Client.new account_sid, auth_token
+module TwilioAPI
 
-@client.account.messages.create(
-  from: '+441626572068',
-  to: '+447711997652',
-  body: 'Robot invasion! Reply back with any sightings.'
-)
+  def send_sms
+    account_sid = ENV['TWILIO_ACC_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+    @client = Twilio::REST::Client.new(
+    account_sid,
+    auth_token
+    )
+
+    @client.account.messages.create(
+      from: ENV['TWILIO_NUMBER_NO'],
+      to: ENV['CUSTOMER_PHONE_NO'],
+      body: "Thanks! Your order was placed & will be delivered within the hour."
+    )
+    puts "A text has been sent to confim your order"
+  end
+
+end
