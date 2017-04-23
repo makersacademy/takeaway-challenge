891552bdd8d11c4afe8153227cb9aa76c0de6dd2
hendrik <g9a2hvk9@me.com>
2017-04-23 20:46:18 +0100

    Adds functionality to check all order items against menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,17 @@
 class Order
-  attr_reader :menu
+  attr_reader :menu, :pending_order
 
  def initialize
     @menu = Menu.new.menu
+    @pending_order = []
+ end
+
+ def new_order
+   @pending_order = ["Salad", "Pasta", "Ice Cream", "Pepsi"]
+ end
+
+ def review_order
+   @pending_order.all? {|item| hash.key? item}
  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,10 +2,24 @@ require "order"
 
 describe Order do
   it { is_expected.to be_an_instance_of Order }
+  it { is_expected.to respond_to(:new_order) }
 
   describe "#initialize" do
-    it "initializes with a new instance of the Menu class" do
-      expect(subject).to have_attributes(:menu => Menu.new.menu)
+    it "initializes with a new instance of the Menu class and @pending_order which equals nil" do
+      expect(subject).to have_attributes(:menu => Menu.new.menu, :pending_order => [])
+    end
+  end
+
+  describe "#new_order" do
+    it "should place an order of 'Salad', 'Pasta', 'Ice Cream', 'Pepsi'" do
+      subject.new_order
+      expect(subject.pending_order).to eq(["Salad", "Pasta", "Ice Cream", "Pepsi"])
+    end
+  end
+
+  describe "#review_order" do
+    it "should check the default order against the menu and evaluate to true" do
+      expect(subject.review_order).to eq(true)
     end
   end
 end
