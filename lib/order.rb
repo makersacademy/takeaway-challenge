5c0c6dbf49aa26467c2fac029a90d9f0d258f4c2
SimonTanner <simonthetanner@hotmail.com>
2017-04-23 16:13:00 +0000

    Order.menu method outputs a list of dishes with a number infront, e.g. '1. Soup 5.0'

--- lib/order.rb
+++ lib/order.rb
@@ -2,9 +2,15 @@ require './lib/menu'
 
 class Order
     
-    def menu(menu)
-        
-        menu
+    def show_menu(menu)
+        i = 1
+        menu.each do |item|
+            puts "#{i}. #{item[:dish]} #{item[:price]}"
+            i +=1
+        end
+    end
+    
+    def order(dish)
         
     end
 end
