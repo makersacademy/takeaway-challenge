42d3e5e67c5c200a0a1f021b17861e55c900b78f
Filipe Fasolin <ffasolin@gmail.com>
2017-04-25 22:11:49 +0100

    Uses dotenv to hide info

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,18 +1,19 @@
 require 'twilio-ruby'
 require_relative 'hour'
+require 'dotenv/load'
 
 class Sms
 
   def initialize
-    account_sid = "AC6f140ad85801f0ff6f70294dbe5adeb0"
-    auth_token  = "622956b94a663225ae9a15803f5881f8"
+    account_sid = ENV['ACCOUNT_SID']
+    auth_token  = ENV['AUTH_TOKEN']
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def send_message
     text = "Thank you! Your order was placed and will be delivered before #{Hour.new.eta_to_string}"
     message = @client.account.messages.create(:body => text,
-        :to => "+447450634070",
+        :to => ENV['PHONE'],
         :from => "+441293311504")
     puts message.sid
   end
