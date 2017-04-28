4376d87985d87ec0f89a9554e07a51dfe144c9b1
Kay Lovelace <kay@makersacademy.com>
2017-04-28 11:59:00 +0100

    Fix bug in commented line in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,11 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
-)
+])
 SimpleCov.start
 
 RSpec.configure do |config|
