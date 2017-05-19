c58daf7b800e3a2c95df26dbcd24f5ebd6099886
wemmm <jenny.wem@gmail.com>
2017-05-19 22:03:43 +0100

    Added failing test for new restaurant instances initializing with a menu

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,6 +2,12 @@ require 'restaurant'
 
 describe Restaurant do
 
-  it { is_expected.to respond_to(:menu) } # .with(1).argument 
+  it { is_expected.to respond_to(:menu) } # .with(1).argument
+
+  describe '#initialize' do
+    it 'should initialize with a new instance of the Menu class' do
+      expect(Restaurant.new.menu).to eq Menu.new
+    end
+  end
 
 end
