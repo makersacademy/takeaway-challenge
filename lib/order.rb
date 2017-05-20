325029010eee6389f441c3fffc5d5fdd97df6d34
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-20 21:01:47 +0100

    Added 'add_dish method to order class.'

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,14 @@
 # deals with the orders
 class Order
 
+  attr_reader :order_items
+
+  def initialize
+    @order_items = Hash.new{0}
+  end
+
+  def add_dish(dish, quantity = 1)
+    @order_items[dish] += quantity
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,8 +2,11 @@ require 'order'
 
 describe Order do
   subject(:order) { described_class.new }
+  let(:dish) { double :dish, name: "fried chicken", price: 7.5, quantity: 2 }
 
-  
+  it 'allows to add dish to order list' do
+    expect(order.add_dish(:dish, 2)).to eq 2
+  end
 
 
 
