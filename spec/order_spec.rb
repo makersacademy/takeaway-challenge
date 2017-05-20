b85b161f074e60b3b813a1b18df3062d02781af6
jade <jadealvares@hotmail.co.uk>
2017-05-20 20:33:22 +0100

    tests for #selection_of_dish are now passing

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,19 +9,15 @@ subject(:order) { described_class.new }
 
   describe "#selection_of_dish" do
     it "should raise an error if the dish a user has chosen is not on the menu" do
-      expect{ order.selection_of_dish(:dish, :number) }.to raise_error "Dish not in menu: please select a dish from the menu"
+      expect{ order.selection_of_dish("pork", 2) }.to raise_error "Dish not in menu: please select a dish from the menu"
     end
 
     it "should raise an error if the number of dishes inputted is not an integer" do
-      # allow(:order) { double('order', dish: 'chicken', number: 'not a number') }
-      expect{ order.selection_of_dish(:dish, :number) }.to raise_error "That is not a number, please enter a valid number"
+      expect{ order.selection_of_dish("chicken", "not a number") }.to raise_error "That is not a number, please enter a valid number"
     end
 
-    let(:chicken) { double(:chicken) }
-    it "should select the dish if it is a dish on the menu" do
-      # menu.selection_of_dish(:chicken) {:chicken}
-      allow(order).to receive(:selection_of_dish) { :chicken }
-      expect(order.meal).to eq(:chicken)
+    it "should select the dish and the amount of that dish a user wants" do
+      expect(order.selection_of_dish("chicken", 2)).to eq([{dish: "chicken", amount: 2}])
     end
 
     it "once you have selected your order, it should allow you to increment or keep the same quantity of the meal" do
