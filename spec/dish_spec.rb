759312e5a076032fdc2f3c258048016b387c929e
Simon Ashbery <si.ashbery@gmail.com>
2017-05-20 17:22:39 +0100

    Created test to show that instances of the dish class have a price -Added a price argument to the initialize method of the dish class -Stored this argument in a price instance variable -Made that variable readable from outside instances of the dish class by adding it to the attribute reader

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -3,7 +3,8 @@ require "dish"
 describe Dish do
   let(:name) { "Saag Paneer" }
   let(:description) { "Cheesy green goodness" }
-  subject(:dish) { described_class.new(name, description) }
+  let(:price) { 4 }
+  subject(:dish) { described_class.new(name, description, price) }
 
   it "has a name" do
     expect(dish.name).to eq name
@@ -11,5 +12,9 @@ describe Dish do
 
   it "Has a description" do
     expect(dish.description).to eq description
+  end
+
+  it "Has a price" do
+    expect(dish.price).to eq price
   end
 end
