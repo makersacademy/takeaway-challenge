b97221464e11c02906d2c79d0bebb7bc02658a26
Dom <dom.mcdonnell@gmail.com>
2017-05-20 20:28:10 +0100

    -Added second test to check for menu items. -Displays formatted menu list.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,20 @@
 class Menu
-  def list_menu
-    "Cheeseburger   £5"
+  attr_reader :menu_list
+  def initialize
+    @menu_list = {
+      "Cheeseburger" => 5,
+      "BBQ Ranch Burger" => 8,
+      "Curly Fries" => 3,
+      "Hotdog" => 4,
+      "Fake Burger" => 6,
+      "Spicy Fries" => 5
+    }
   end
+
+  def list_menu
+    @menu_list.each do |item, price|
+    puts "#{item.ljust(20)}    £#{price}"
+    end
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,7 +2,10 @@ require 'menu'
 
 describe Menu do
   subject(:menu) { described_class.new }
+  it 'should display a menu item' do
+    expect(menu.list_menu).to include "Cheeseburger" => 5
+  end
   it 'should display a list of menu items' do
-    expect(menu.list_menu).to include "Cheeseburger   £5"
+    expect(menu.list_menu).to include "BBQ Ranch Burger" => 8
   end
 end
