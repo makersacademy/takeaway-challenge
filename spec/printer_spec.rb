fd01790824800106b14e479955b0fca5fabe0b86
Michael Jacobson <michael.jacobson89@gmail.com>
2017-05-20 14:58:06 +0100

    • add unit test for Printer class

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,5 +2,12 @@
 require_relative '../lib/printer'
 
 describe Printer do
+  subject(:printer) { Printer.new }
+  let(:dish) { double(:dish, name: 'Pretzels', description: 'These pretzels are making me thirsty!', price: 2) }
+  let(:menu) { double(:menu, dishes: [dish]) }
+  let(:expected_output) { "---\n1. Pretzels - £2\nThese pretzels are making me thirsty!\n---\n" }
 
+  it 'prints a formatted menu to the stdout' do
+    expect { printer.print_menu(menu)}.to output(expected_output).to_stdout
+  end
 end
