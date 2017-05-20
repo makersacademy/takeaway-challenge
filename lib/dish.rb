6e9ebcf64fdcb35852ff0d1059e8a0841b2ddae2
Lubos Michalic <lnmichalic@gmail.com>
2017-05-20 21:34:41 +0100

    Write tests for Dish and code to pass them

---
+++ lib/dish.rb
@@ -0,0 +1,9 @@
+class Dish
+
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
---
+++ spec/takeaway.rb
@@ -0,0 +1,5 @@
+describe Takeaway do
+  it 'instantiates a new takeaway' do
+    
+  end
+end
