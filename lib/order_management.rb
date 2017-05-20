043430d6da54e371b6a69264773c4d574cdb737d
“rogrenke” <roland@rolandgrenke.com>
2017-05-20 23:34:35 +0100

    Adds empty method #order_by_commandline

--- lib/order_management.rb
+++ lib/order_management.rb
@@ -1,2 +1,6 @@
 class OrderManagement
+
+  def order_by_commandline
+  end
+
 end
--- spec/order_management_spec.rb
+++ spec/order_management_spec.rb
@@ -1,5 +1,11 @@
 require 'order_management'
 
 describe OrderManagement do
-  
+
+  subject(:order_management) { described_class.new }
+
+  it 'should respond to #order_by_commandline' do
+    expect(order_management).to respond_to(:order_by_commandline)
+  end
+
 end
