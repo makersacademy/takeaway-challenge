9d820d443f512740bef2a25c551149980cfd31bd
“rogrenke” <roland@rolandgrenke.com>
2017-05-20 18:47:09 +0100

    New test to check if instance of Dish is initialized with a price

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -6,4 +6,8 @@ describe Dish do
   it 'should be initialized with a name' do
     expect(subject.name).not_to be_nil
   end
+
+  it 'should be initialized with a price' do
+    expect(subject.price).not_to be_nil
+  end
 end
