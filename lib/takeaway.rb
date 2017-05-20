da39fb1270c546b6acc34cd38d4e85c76cf36ce3
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-20 21:25:42 +0100

    Adds in a selection menu and selection and display methods- needs to be refactored -#selection_menu case method added -#course selection methods added -#displays_options & #displays_order_confirmation_message & #displays_controller  methods added -spec updated but need to refactor tests to be correct

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -25,43 +25,85 @@ class Takeaway
     "Starters: #{extracts_starters} -- Main Course: #{extracts_main_course} -- Dessert: #{extracts_dessert}."
   end
 
+# work out how to do 0 and Q on controller menu & methods
   def displays_controller
-    puts "Welcome to our takeaway service. Please make your order by selecting 0 to be taken to the order controler"
+    "Welcome to our takeaway service. Please make your order by selecting 0 to be taken to the selection menu, press 'Q' at anytime to escape the program:"
   end
 
-  #case statement where user selects what dish they want from the menu
+  def selection_menu(choice)
+    case choice
+    when :starters
+      starters_selection
+    when :main_course
+      main_course_selection
+    when :dessert
+      dessert_selection
+    end
+  end
 
-  # def select_dishes
-  #
-  #   starters = gets.chomp
-  #   puts "If you would like any main courses please write in the name here, e.g. fried duck:"
-  #   main_course = gets.chomp
-  #   puts "If you would like any desserts please write in the name here, e.g. jam roll:"
-  #   dessert = gets.chomp
-  #   puts "You have ordered: #{starters}, #{main_course}, #{dessert}."
-  #   @basket << [starters, main_course, dessert]
-  # end
+# TODO refactor all selection methods to avoid repetition - DRY 3's 
+# TODO update with a loop to let them make 2 choices & push into basket
+  def starters_selection
+    displays_options(extracts_starters)
+    starters = gets.chomp
+    @basket << [starter]
+    displays_order_confirmation_message
+  end
+
+# TODO update with a loop to let them make 2 choices & push into basket
+  def main_course_selection
+    displays_options(extracts_main_course)
+    mains = gets.chomp
+    @basket << [main]
+    displays_order_confirmation_message
+  end
+
+# TODO update with a loop to let them make 2 choices & push into basket
+  def dessert_selection
+    displays_options(extracts_dessert)
+    mains = gets.chomp
+    @basket << [dessert]
+    displays_order_confirmation_message
+  end
+
+
+  def displays_options(menu_extract)
+    "The starters that we have available are #{menu_extract}:"
+    "Please enter the order number you would like to add to your basket:"
+  end
+
+# TODO customise this so the message confirms the order number(s) so customer is sure of what they ordered
+  def displays_order_confirmation_message
+    "Thanks for your selection we've added your orders to your basket."
+  end
+
 
 
   private
 
-# TODO refactor to avoid repetition
+# TODO refactor to avoid repetition- DRY 3's
   def extracts_starters
     starter1 = @menu[:starters][1].map { |k, v| "#{k} = £#{v}" }.join(', ')
     starter2 = @menu[:starters][2].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    "#{starter1}, #{starter2}"
+    starter1_key = @menu[:starters].keys[0]
+    starter2_key = @menu[:starters].keys[1]
+    "#{starter1_key}. #{starter1}, #{starter2_key}. #{starter2}"
   end
 
   def extracts_main_course
     main1 = @menu[:main_course][3].map { |k, v| "#{k} = £#{v}" }.join(', ')
     main2 = @menu[:main_course][4].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    "#{main1}, #{main2}"
+    main1_key = @menu[:main_course].keys[0]
+    main2_key = @menu[:main_course].keys[1]
+    "#{main1_key}. #{main1}, #{main2_key}. #{main2}"
   end
 
   def extracts_dessert
     desert1 = @menu[:dessert][5].map { |k, v| "#{k} = £#{v}" }.join(', ')
     desert2 = @menu[:dessert][6].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    "#{desert1}, #{desert2}"
+    desert1_key = @menu[:dessert].keys[0]
+    desert2_key = @menu[:dessert].keys[1]
+    "#{desert1_key}. #{desert1}, #{desert2_key}. #{desert2}"
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -13,11 +13,35 @@ describe Takeaway do
     end
   end
 
+# TODO update tests with proper stdout format e.g expect(takeaway.lists_dishes_with_prices).to output(all_dishes).to_stdout
   describe '#lists_dishes_with_prices' do
     it 'lists all menu dishes with prices' do
-      all_dishes = "Starters: Olives = £4, Pitta bread = £5 -- Main Course: Fried duck = £12, Fish & Chips = £9 -- Dessert: Jam roll = £5, Apple crumble = £6."
+      all_dishes = "Starters: 1. Olives = £4, 2. Pitta bread = £5 -- Main Course: 3. Fried duck = £12, 4. Fish & Chips = £9 -- Dessert: 5. Jam roll = £5, 6. Apple crumble = £6."
       expect(takeaway.lists_dishes_with_prices).to eq all_dishes
-      #expect(takeaway.lists_dishes_with_prices).to output(all_dishes).to_stdout
+
+    end
+  end
+
+# TODO update these tests to make sure that can choose 0 or Q 
+  describe '#displays_controller' do
+    it 'displays a message to prompt user to move to navigate to the selection menu and instructions to quit' do
+      expect(takeaway.displays_controller).to eq "Welcome to our takeaway service. Please make your order by selecting 0 to be taken to the selection menu, press Q at anytime to escape the program:"
+    end
+  end
+
+# TODO update these tests with doubles & mocks make them function
+  describe '#selection_menu' do
+    it 'can select starter options' do
+      expect(takeaway.selection_menu(:starters)).to eq "Thanks for your selection we've added your orders to your basket."
+    end
+    it 'can select main course options' do
+      expect(takeaway.selection_menu(:main_course)).to eq "Thanks for your selection we've added your orders to your basket."
+    end
+    it 'can select main course options' do
+      expect(takeaway.selection_menu(:main_course)).to eq "Thanks for your selection we've added your orders to your basket."
+    end
+    it 'can select dessert options' do
+      expect(takeaway.selection_menu(:dessert)).to eq "Thanks for your selection we've added your orders to your basket."
     end
   end
 
