d85002bf13ce3acfed0ced99475fe92193c503f7
Kynosaur <kye.bracey@gmail.com>
2017-05-20 15:40:30 +0100

    Begin moving Menu to its own class, create menu_spec.rb and move menu test over from restaurant_spec

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -8,10 +8,4 @@ describe Restaurant do
   it 'has an empty basket upon instantiation' do
     expect(restaurant.basket).to eq({})
   end
-
-  describe '#show_menu' do
-    it 'prints out the items on the menu with prices' do
-      expect(restaurant.show_menu).to eq ({"quarter chicken": 3.5, "half chicken": 6.0, "whole chicken": 10.50, "small fries": 1.99, "large fries": 2.50})
-    end
-  end
 end
