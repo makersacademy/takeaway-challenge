2969f6dc394d23a1ffaf2dc4b95cd5518b274967
Mário Ribeiro <mail@marioribeiro.com>
2017-05-21 16:06:01 +0100

    Define the initializer method for Restaurant

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,13 @@
 
 class Restaurant
+
+  attr_reader :name, :address, :menu
+
+  def initialize(name, address, menu = Menu.new, notifier = Notifier.new)
+    @name = name
+    @address = address
+    @notifier = notifier
+    @menu = menu
+    @orders = []
+  end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -4,4 +4,6 @@ describe Restaurant do
 
   subject(:restaurant) { described_class.new }
 
+  it { is_expected.to respond_to(:accept_order )}
+
 end
