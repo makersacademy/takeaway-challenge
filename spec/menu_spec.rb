03ae6612d767be02d1ba10721034a26b806e8687
Kavita <kkavita92@gmail.com>
2017-05-21 10:26:38 +0100

    Initializes #Menu with empty list of dishes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,4 +1,10 @@
 require 'menu'
 
 describe Menu do
+  subject(:menu) { described_class.new }
+  let(:dish) { double :dish, name: 'Taco', price: 5}
+
+  it 'should initialize with an empty array of dishes' do
+    expect(menu.dishes).to be_empty
+  end
 end
