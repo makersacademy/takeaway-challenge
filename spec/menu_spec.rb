a9c66084c047df4d0bcfe8107d0fb4b4b32e5439
Lubos Michalic <lnmichalic@gmail.com>
2017-05-21 19:30:44 +0100

    Modify Gemfile to inclue twilio-ruby gem

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -11,9 +11,7 @@ describe Menu do
   end
 
   it 'adding an item to the menu list changes its length by 1' do
-    puts "visibility beg #{menu.items.count}"
     expect { menu.add_item(item.name, item.price) }.to change { menu.items.count }.from(0).to(1)
-
   end
 
 end
