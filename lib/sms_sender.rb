5b33e808ae3b7a04e4295ed6a84a32fedbad5802
“rogrenke” <roland@rolandgrenke.com>
2017-05-21 17:46:21 +0100

    Create #send method in SmsSender class - accepts 'message' as argument - uses Twilio's API to send out SMS

--- lib/sms_sender.rb
+++ lib/sms_sender.rb
@@ -2,4 +2,14 @@ require 'twilio-ruby'
 
 class SmsSender
 
+  def send
+    account_sid = 'AC3ded645e335f955868d2d72e389f7549'
+    auth_token = 'b47327303937ab93646bcc3343042a8e'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client.account.messages.create({
+      :from => '+4915735987696',
+      :to => '+4915115734729',
+      :body => 'Test Message.'
+    })
+  end
 end
--- spec/sms_sender_spec.rb
+++ spec/sms_sender_spec.rb
@@ -1,5 +1,11 @@
 require 'sms_sender'
 
 describe SmsSender do
-  
+
+  subject(:sms_sender) {double :sms_sender}
+
+  it 'responds to #send' do
+    expect(sms_sender).to respond_to(:send).with(1).argument
+  end
+
 end
