6bf8fa115fd69bb677ffd55add350a36bc801c12
wemmm <jenny.wem@gmail.com>
2017-05-21 11:38:20 +0100

    Added failing test for copying items from a menu to the current order

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -16,9 +16,15 @@ describe Menu do
     end
   end
 
-  describe '#order_item' do
-    it 'should move a selected item from @dishes to @current_order' do
+  describe '#order' do
+    it 'has an empty array, @current order' do
       expect(subject.current_order).to eq []
+    end
+
+    it 'should copy an item from @dishes to @current_order'do
+      subject.instance_variable_set("@dishes", [{ 'generic_food' => 5 }, { 'another_food' => 10 } ])
+      subject.order(2)
+      expect(subject.current_order).to eq [{ 'another_food' => 10 }]
     end
   end
 
