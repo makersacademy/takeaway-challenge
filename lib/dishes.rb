7882a05506f790575e55a0448e46a93812df4312
Spencer <spencer.bartonfisher@me.com>
2017-05-21 20:35:21 +0100

    moved dishes to menu instead of having a dishes class as well as menu

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -1,11 +1,1 @@
 
-
-class Dishes
-
-  attr_reader :dish_name, :price
-
-  def initialize(dish_name, price)
-    @dish_name = dish_name
-    @price = price
-  end
-end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -1,13 +1,13 @@
-require 'dishes'
-
-
-describe Dishes do
-    let(:dishes) { described_class.new :pizza, 9 }
-  it "returns the name of a dish" do
-    expect(dishes.dish_name).to eq(:pizza)
-  end
-
-  it "returns the price of a dish" do
-    expect(dishes.price).to eq(9)
-  end
-end
+# require 'dishes'
+#
+# describe Dishes do
+#   let(:dishes) { described_class.new }
+#
+#     it "returns a list of the dishes which are on the menu" do
+#       dish_1 = {:pizza => 8}
+#       dish_2 = {:cheese_burger => 11}
+#       dish_3 = {:Burrito => 7}
+#       expect(dishes.dish_names).to eq ([dish_1, dish_2, dish_3])
+#
+#   end
+#  end
