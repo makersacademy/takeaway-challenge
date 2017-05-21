cf29da5c66fcab57a4edc1189bf41b510ae15193
Kavita <kkavita92@gmail.com>
2017-05-21 10:15:58 +0100

    Enable #Menu to load list of dishes from file

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,26 @@
+require_relative './dish.rb'
+
 class Menu
 
+  DEFAULT_DISHES = "mexican_dishes.csv"
+
 def initialize
-  #@dishes = []
+  @dishes = []
 end
 
 def show_menu
+  load_menu
+  @dishes
 end
 
 private
 
-def load_menu
-  #load up from file stuff
+def load_menu(filename = DEFAULT_DISHES)
+  file = File.open(filename,"r")
+  File.foreach(file) do |line|
+    name, price, description = line.chomp.split(',')
+    @dishes << Dish.new(name, price, description)
+  end
 end
 
 end
