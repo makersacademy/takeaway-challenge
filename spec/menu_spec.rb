54add74748bdc3852133885ce631cffe2a34cf61
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-21 15:28:22 +0100

    Initialized menu_list within Menu class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,10 @@ require 'menu'
 
 describe Menu do
   subject(:menu) {described_class.new}
-end  
+
+
+  it "should give list of menu items" do
+    allow(menu).to receive(:menu_list) {"Menu items"}
+    expect(menu.menu_list).to eq "Menu items"
+  end
+end
