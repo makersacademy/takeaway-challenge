1ba4e389fc9be11bf5e3e91709e0e84b7afcf10a
Lubos Michalic <lnmichalic@gmail.com>
2017-05-21 21:49:45 +0100

    Create a test for Printer to output interactive menu and write code to pass it

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,2 +1,6 @@
 class Printer
+
+  def interactive_menu
+    print '1. See Menu'
+  end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,5 +1,12 @@
 require 'printer'
 
 describe Printer do
+  let(:printer) {described_class.new}
+  it { is_expected.to respond_to(:interactive_menu) }
 
+  describe '#interactive_menu' do
+    it 'prints menu to STDOUT ' do
+      expect{printer.interactive_menu}.to output('1. See Menu').to_stdout
+    end
+  end
 end
