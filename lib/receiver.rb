889feee3f3d80d574f4274e6f0a0b2141a014f3f
Michael Jacobson <michael.jacobson89@gmail.com>
2017-05-22 00:15:39 +0100

    • begin to experiment with receiver

--- lib/receiver.rb
+++ lib/receiver.rb
@@ -3,13 +3,5 @@ require 'sinatra'
 
 post '/receiver' do
   body = params['Body'].downcase
-
-  order = File.open('orders.txt', 'w')
-  order.puts("#{body}\n")
-  order.close
-
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message 'Hi there, thanks for the message!'
-  end
-  twiml.text
+  
 end
