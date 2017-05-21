949d955ef031b891f76db08b0b7278cc047c3f72
wemmm <jenny.wem@gmail.com>
2017-05-21 09:53:52 +0100

    Refactored test to test for menu info being shown in an easily readable way

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,9 +5,9 @@ describe Menu do
   it { is_expected.to respond_to :dishes }
 
   describe '#show_menu' do
-    it 'should show a food type and its price' do
-      subject.instance_variable_set("@dishes", [{ "edemame" => 3 }])
-      expect(subject.show_menu).to eq [{ "edemame" => 3 }]
+    it 'should show a food type and its price in a readable way' do
+      subject.instance_variable_set("@dishes", [{ 'generic_food' => 5 }])
+      expect(subject.show_menu).to eq "1. 'generic_food', £5.00"
     end
   end
 
