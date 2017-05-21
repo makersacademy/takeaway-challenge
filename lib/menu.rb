2e78e2dac279a06b6de017aee2c4be93cfced2c6
toby <toby.hw@live.co.uk>
2017-05-21 01:58:11 +0100

    User Story 2 - Adds feature test for US2 & Updates list of dishes

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,15 @@
 class Menu
   attr_reader
 
-  LIST_OF_DISHES = { "Chicken Chow Mein" => 5.50 }
+  LIST_OF_DISHES = {
+    "Chicken Chow Mein" => 5.50,
+    "Sweet And Sour Chicken" => 5.00,
+    "Chicken Satay" => 3.50,
+    "Spicy Chilli Beef" => 5.00,
+    "Egg Fried Rice" => 2.00,
+    "Steamed Rice" => 1.50,
+    "Fanta" => 1.50,
+    "Diet Coke" => 1.50 }
 
   def initialize
 
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,11 +1,26 @@
 require_relative '../spec_helper'
-# As a customer
-# So that I can check if I want to order something
-# I would like to see a list of dishes with prices
 
 describe 'takeaway' do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
   it 'so customers can select their order, display a list of dishes' do
     order = Order.new
     expect(order.view_menu).to eq(Menu::LIST_OF_DISHES)
+  end
+  # As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+  it 'so we can order multiple dishes, select and store each dish chosen' do
+    order.view_menu
+    order.add_dish("Chicken Chow Mein")
+    order.add_dish("Spicy Chilli Beef")
+    order.add_dish("Egg Fried Rice", "Diet Coke")
+    expect{ order.add_dish("Kung Pow Chicken") }.to change{ @current_order }.to eq({
+       "Chicken Chow Mein" => 5.50,
+       "Spicy Chilli Beef" => 5.00,
+       "Egg Fried Rice" => 2.00,
+       "Diet Coke" => 1.50
+       })
   end
 end
