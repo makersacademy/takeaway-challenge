e036546db9b5151f30a8443098c8a7716429929c
Kynosaur <kye.bracey@gmail.com>
2017-05-21 20:11:26 +0100

    Add Order#try_complete and #is_correct_amount? methods

--- lib/order.rb
+++ lib/order.rb
@@ -18,4 +18,13 @@ class Order
   def calculate_total
     '%.2f' % @list.map { |dish, amount| @menu.dishes[dish.to_sym] * amount }.sum
   end
+
+  def try_complete(amount)
+    is_correct_amount?(amount)
+  end
+
+  private
+  def is_correct_amount?(amount)
+    calculate_total.to_f == amount
+  end
 end
