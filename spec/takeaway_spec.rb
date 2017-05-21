d76055ab3d2dc4fbefa7d9859b7a025c5e14660a
Kynosaur <kye.bracey@gmail.com>
2017-05-21 20:22:47 +0100

    Isolate tests for Takeaway#checkout with Order class double

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,6 +3,7 @@ require 'takeaway'
 describe Takeaway do
   subject(:takeaway) { described_class.new }
   let(:menu) { double(:menu, :dishes => {"chicken burger": 3.49, "chicken wings": 4.99}, :print_dishes => "chicken burger: 3.49, chicken wings: 4.99") }
+  let(:full_order) { double(:order, :list => {:"chicken wings"=>2, :"chicken burger"=>3})}
   before(:each) do
     takeaway.instance_variable_set(:@menu, menu)
   end
@@ -27,13 +28,16 @@ describe Takeaway do
     before do
       takeaway.add_to_order("chicken wings", 2)
       takeaway.add_to_order("chicken burger", 3)
+      takeaway.instance_variable_set(:@basket, full_order)
     end
 
     it 'confirms the order is complete' do
+      allow(full_order).to receive(:try_complete).with(20.45).and_return true
       expect(takeaway.checkout(20.45)).to eq "Order placed - please see your mobile for delivery details"
     end
 
     it 'raises an error if given amount is incorrect' do
+      allow(full_order).to receive(:try_complete).with(20.46).and_return false
       expect{ takeaway.checkout(20.46) }.to raise_error "Sorry, that amount does not match your order total, please try again"
     end
   end
