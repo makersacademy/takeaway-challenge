114bff4f2517246f792369b854ca4e722b66e6e1
“rogrenke” <roland@rolandgrenke.com>
2017-05-21 15:58:18 +0100

    Refactoring to adjust for rubocop recommendations

--- lib/order_management.rb
+++ lib/order_management.rb
@@ -1,7 +1,7 @@
 class OrderManagement
 
   def order_by_commandline(order_hash, menu_items)
-    order_hash[:basket].each {|item| raise "Sorry, '#{item[:name]}' is not on our menu" unless is_item_of_menu?(item[:name],menu_items)}
+    order_hash[:basket].each { |item| raise "Sorry, '#{item[:name]}' is not on our menu" unless item_of_menu?(item[:name], menu_items) }
     raise "Sorry, your total was incorrect. Order was not placed" unless calculate_total(order_hash, menu_items) == order_hash[:total]
   end
 
@@ -17,20 +17,12 @@ class OrderManagement
     total
   end
 
-
-
-
-
-
   private
 
-  def is_item_of_menu?(dish_name, menu_items)
+  def item_of_menu?(dish_name, menu_items)
     menu_items.each do |menu_item|
       return true if menu_item.name == dish_name
     end
     false
   end
-
-
-
 end
--- spec/order_management_spec.rb
+++ spec/order_management_spec.rb
@@ -3,7 +3,7 @@ require 'order_management'
 describe OrderManagement do
 
   subject(:order_management) { described_class.new }
-  let(:menu) {double :menu}
+  let(:menu) { double :menu }
 
   it 'should respond to #order_by_commandline' do
     expect(order_management).to respond_to(:order_by_commandline).with(2).arguments
@@ -13,18 +13,18 @@ describe OrderManagement do
 
     it 'should raise an error if user orders a dish that is not on the menu' do
       menu = Menu.new
-      array_of_hashes = [{name: "Spaghetti carbonara", price: 8}, {name: "Spaghetti arabiata", price: 7}, {name: "Lasagna al forno", price: 9}, {name: "Pizza Margherita", price: 7}, {name: "Pizza Quattro Stagioni", price: 10}]
+      array_of_hashes = [{ name: "Spaghetti carbonara", price: 8 }, { name: "Spaghetti arabiata", price: 7 }, { name: "Lasagna al forno", price: 9 }, { name: "Pizza Margherita", price: 7 }, { name: "Pizza Quattro Stagioni", price: 10 }]
       menu.bulk_add(array_of_hashes)
       order_hash = { basket: [{ name: "Pizza Tonno", quantity: 2 }], total: 14 }
-      expect{ order_management.order_by_commandline(order_hash, menu.items) }.to raise_error("Sorry, 'Pizza Tonno' is not on our menu")
+      expect { order_management.order_by_commandline(order_hash, menu.items) }.to raise_error("Sorry, 'Pizza Tonno' is not on our menu")
     end
 
     it 'should raise an error if user orders and enters a total that is not the calculated total' do
       menu = Menu.new
-      array_of_hashes = [{name: "Spaghetti carbonara", price: 8}, {name: "Spaghetti arabiata", price: 7}, {name: "Lasagna al forno", price: 9}, {name: "Pizza Margherita", price: 7}, {name: "Pizza Quattro Stagioni", price: 10}]
+      array_of_hashes = [{ name: "Spaghetti carbonara", price: 8 }, { name: "Spaghetti arabiata", price: 7 }, { name: "Lasagna al forno", price: 9 }, { name: "Pizza Margherita", price: 7 }, { name: "Pizza Quattro Stagioni", price: 10 }]
       menu.bulk_add(array_of_hashes)
       order_hash = { basket: [{ name: "Lasagna al forno", quantity: 2 }], total: 14 }
-      expect{ order_management.order_by_commandline(order_hash, menu.items) }.to raise_error("Sorry, your total was incorrect. Order was not placed")
+      expect { order_management.order_by_commandline(order_hash, menu.items) }.to raise_error("Sorry, your total was incorrect. Order was not placed")
     end
 
   end
