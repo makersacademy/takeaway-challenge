b42d3203840c9168d5fd86b88d6f6ebb5d0ef927
Aaron Rodrigues <aaronrodrigues@Aarons-MBP.home>
2017-05-21 13:23:47 +0100

    Can select several dishes from menu and check if an item is not on the menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,19 @@
 class Order
   attr_reader :dishes
 
-  def initialize
+  def initialize(menu)
     @dishes = {}
+    @menu = menu
   end
 
 	 def add(dish, quantity)
+    fail  NoItemError, "#{dish.capitalize} is not on the menu!" unless @menu.include?(dish)
     dishes[dish] = quantity
  	 end
+
+   private
+
+   
 end
+
+class NoItemError < StandardError; end
