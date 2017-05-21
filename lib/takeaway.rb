48a27e3a859b06f9e6def1eef2fb6b64a362716f
Panteha <panteha.ahmadi@gmail.com>
2017-05-21 13:37:10 +0100

    Added and tested select_dish and current_order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,23 @@
 class Takeaway
+  attr_reader :current_order
 
   def initialize(menu_class = Menu)
     @menu = menu_class.new
+    @current_order = nil
   end
 
   def read_menu
-    @menu
+    list_of_food = {}
+    @menu.dishes.each { |dish| list_of_food[dish.name] = dish.price }
+    list_of_food
+  end
+
+  def select_dish(selected_dish)
+    current_order.add_dish(@menu.find_dish(selected_dish))
+  end
+
+  def current_order
+    @current_order ||= Order.new
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,12 +2,19 @@ require 'takeaway'
 
 describe Takeaway do
 
-  let(:menu) { double :menue }
+  let(:dish1) { double :dish, :name => 'milk', :price => 2 }
+  let(:dish2) { double :dish, :name => 'chips', :price => 3 }
+  let(:menu) { double :menu, :dishes => [dish1, dish2], :find_dish => dish2 }
   let(:menu_class) { double :menu_class, :new => menu }
   let(:subject) { Takeaway.new(menu_class) }
 
-  it 'read menu returns a menu object' do
-    expect(subject.read_menu). to eq menu
+  it 'read_menu returns a menu object' do
+    expect(subject.read_menu). to eq({ 'milk' => 2, 'chips' => 3 })
+  end
+
+  it 'select_dish adds the chosen dish to the current order' do
+    subject.select_dish('chips')
+    expect(subject.current_order.dishes).to include dish2
   end
 
 end
