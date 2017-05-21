d9d33e96b11fc35a5c715233873976b756aa44f7
Spencer <spencer.bartonfisher@me.com>
2017-05-21 22:01:54 +0100

    initialize method created with new order array instance

--- lib/order.rb
+++ lib/order.rb
@@ -2,4 +2,10 @@
 
 class Order
 
+  attr_reader :new_order
+
+    def initialize(menu)
+      @new_order = []
+    end
+    
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,5 +3,9 @@ require 'order'
 
 describe Order do
 
-  
+subject(:order) { described_class.new('menu') }
+
+  it 'creates the new order array' do
+    expect(order.new_order).to eq ([])
+  end
 end
