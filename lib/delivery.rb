0e12ef2b8d4d6abca35ee970aeb695bf8cb57586
bannastre <bannastre@gmail.com>
2017-05-21 11:38:18 +0100

    Create message_sent_notification string with expected delivery time - Blank out Twilio for testing

--- lib/delivery.rb
+++ lib/delivery.rb
@@ -8,22 +8,22 @@ class Delivery
   DELIVERY_ESTIMATE = 60 * 60
 
   def initialize(order_time)
-    @delivery_time = order_time + DELIVERY_ESTIMATE
+    @delivery_time = (order_time + DELIVERY_ESTIMATE).strftime("%I:%M %p")
     @sms_number = '+441952780147'
     @user_contact_number = '+447843627130'
   end
 
   def send_notification
-    account_sid = 'AC59a274e32e8dabfb2e8d4299c2ca7503'
-    auth_token = '549ea7cdba5c041228b186232c0de92d'
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
-
-    @client.account.messages.create({
-      :from => @sms_number,
-      :to => @user_contact_number,
-      :body => 'This is your delivery notification'
-      })
-    :message_sent
+    # account_sid = 'AC59a274e32e8dabfb2e8d4299c2ca7503'
+    # auth_token = '549ea7cdba5c041228b186232c0de92d'
+    # @client = Twilio::REST::Client.new(account_sid, auth_token)
+    #
+    # @client.account.messages.create({
+    #   :from => @sms_number,
+    #   :to => @user_contact_number,
+    #   :body => 'This is your delivery notification'
+    #   })
+    message_sent_notification = "Thank you! You're order will be delivered before #{@delivery_time}"
   end
 
 end
