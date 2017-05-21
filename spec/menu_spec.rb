df3c4cf7887cc05a58193157acee83ed649eaf17
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-21 21:21:29 +0100

    Updated Menu class and tests, coverage 100%, still one test failing...

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,14 +2,23 @@ require 'menu'
 
 describe Menu do
   subject(:menu) { described_class.new }
-  let(:dish) { double :dish, name: "beef", price: 7.5 }
+  let(:dish) { double :dish, name: 'fried chicked' }
 
   it { expect(menu).to respond_to :read_menu }
 
   describe '#default_menu' do
     it 'checks if menu is initialized with default dishes' do
-      menu1 = Menu.new
-      expect(menu1[0]).to include('fried chicken')
+      expect(menu.read_menu.first).to eq 'fried chicken'
+    end
+  end
+
+  it 'allows to check if a dish is on the menu' do
+    expect(menu.contains?(:dish)).not_to be
+  end
+
+  describe '#read_menu' do
+    it 'allows to read the full menu' do
+      expect(menu.read_menu.count).to eq 6
     end
   end
 
