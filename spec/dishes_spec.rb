877cac3e4ac1e4da0afd0a70d3be4d6c4b09a9e2
Spencer <spencer.bartonfisher@me.com>
2017-05-21 15:12:39 +0100

    Added an initialize method with the name and price of an item

--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -2,5 +2,12 @@ require 'dishes'
 
 
 describe Dishes do
+    let(:dishes) { described_class.new :pizza, 9 }
+  it "returns the name of a dish" do
+    expect(dishes.dish_name).to eq(:pizza)
+  end
 
+  it "returns the price of a dish" do
+    expect(dishes.price).to eq(9)
+  end
 end
