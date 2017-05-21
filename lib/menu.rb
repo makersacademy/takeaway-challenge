1026a4119be72785bd86e44bb7fecec9fe994593
Dom <dom.mcdonnell@gmail.com>
2017-05-21 20:50:59 +0100

    ammended commit

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,20 @@
 class Menu
-  def list_menu
-    "Cheeseburger   £5"
+  attr_reader :menu_list
+  def initialize
+    @menu_list = {
+      "Cheeseburger" => 5,
+      "BBQ Ranch Burger" => 8,
+      "Curly Fries" => 3,
+      "Hotdog" => 4,
+      "Fake Burger" => 6,
+      "Spicy Fries" => 5
+    }
   end
+
+  def list_menu
+    @menu_list.each_with_index do |(dish, price), index|
+      puts "#{index + 1}. #{dish.ljust(20)}    £#{price}"
+    end
+  end
+
 end
