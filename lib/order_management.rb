99391d7143d09f3636b6880e96ed3e95f60ba77e
“rogrenke” <roland@rolandgrenke.com>
2017-05-21 12:37:12 +0100

    Adds private method #is_item_of_menu? to OrderManager which checks if a dish name is name of item on the menu

--- lib/order_management.rb
+++ lib/order_management.rb
@@ -3,4 +3,18 @@ class OrderManagement
   def order_by_commandline(order_hash)
   end
 
+
+
+
+
+
+  private
+
+  def is_item_of_menu?(dish_name,menu_items)
+    menu_items.each do |menu_item|
+      return true if menu_item.name == dish_name
+    end
+    false
+  end
+
 end
