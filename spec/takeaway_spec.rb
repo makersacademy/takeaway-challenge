b1e2a8beac3ed30a07efde8eb7633392e6609d97
Kynosaur <kye.bracey@gmail.com>
2017-05-21 15:22:59 +0100

    back to the future? Revert "Update TakeAway#total to always return number to two decimal places (now returns as a string)"
    This reverts commit 262bd469d77e6c05995862f239e5ad32229ed1f7.

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -29,9 +29,9 @@ describe TakeAway do
     describe '#total' do
       it 'shows the total price of current order' do
         takeaway.add_to_order("chicken wings", 2)
-        expect(takeaway.total).to eq "9.98"
+        expect(takeaway.total).to eq 9.98
         takeaway.add_to_order("chicken burger", 3)
-        expect(takeaway.total).to eq "20.45"
+        expect(takeaway.total).to eq 20.45
       end
     end
   end
