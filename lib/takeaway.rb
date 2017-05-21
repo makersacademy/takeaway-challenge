5919adf4752bcf3a458a63cea4f7e6e6b33c2e98
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-21 11:27:50 +0100

    Corrects extract methods to work with the Menu class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,4 @@
-require 'menu'
+require_relative 'menu'
 class Takeaway
 
   attr_reader :menu, :basket
@@ -8,13 +8,39 @@ class Takeaway
     @basket = []
   end
 
-# TODO shorten this long line, e.g. print 3 x outputs for each course
+  # TODO shorten this long line, e.g. print 3 x outputs for each course
   def lists_dishes_with_prices
-    "Starters: #{extracts_starters} -- Main Course: #{extracts_main_course} -- Dessert: #{extracts_dessert}."
+    print "#{extracts_starters} #{extracts_main_course} #{extracts_dessert}"
   end
 
-# TODO shorten this long line, e.g. print 3 x outputs for each course
-# TODO work out how to do 0 and Q on controller menu & methods
+  # TODO refactor to avoid repetition- DRY 3's
+  # TODO maybe create method for @menu.starters
+  def extracts_starters
+    starter1 = @menu.starters[1].map { |k, v| "#{k} = £#{v}" }.join(', ')
+    starter2 = @menu.starters[2].map { |k, v| "#{k} = £#{v}" }.join(', ')
+    starter1_key = @menu.starters.keys[0]
+    starter2_key = @menu.starters.keys[1]
+    print "Starters: #{starter1_key}. #{starter1}, #{starter2_key}. #{starter2} "
+  end
+
+  def extracts_main_course
+    main1 = @menu.main_course[3].map { |k, v| "#{k} = £#{v}" }.join(', ')
+    main2 = @menu.main_course[4].map { |k, v| "#{k} = £#{v}" }.join(', ')
+    main1_key = @menu.main_course.keys[0]
+    main2_key = @menu.main_course.keys[1]
+    print "Main Courses: #{main1_key}. #{main1}, #{main2_key}. #{main2} "
+  end
+
+  def extracts_dessert
+    dessert1 = @menu.dessert[5].map { |k, v| "#{k} = £#{v}" }.join(', ')
+    dessert2 = @menu.dessert[6].map { |k, v| "#{k} = £#{v}" }.join(', ')
+    dessert1_key = @menu.dessert.keys[0]
+    dessert2_key = @menu.dessert.keys[1]
+    print "Desserts: #{dessert1_key}. #{dessert1}, #{dessert2_key}. #{dessert2}"
+  end
+
+  # TODO shorten this long line, e.g. print 3 x outputs for each course
+  # TODO work out how to do 0 and Q on controller menu & methods
   def displays_controller
     "Welcome to our takeaway service. Please make your order by selecting 0 to be taken to the selection menu, press 'Q' at anytime to escape the program:"
   end
@@ -30,8 +56,8 @@ class Takeaway
     end
   end
 
-# TODO refactor all selection methods to avoid repetition - DRY 3's
-# TODO update with a loop to let them make 2 choices & push into basket
+  # TODO refactor all selection methods to avoid repetition - DRY 3's
+  # TODO update with a loop to let them make 2 choices & push into basket
   def starters_selection
     displays_options(extracts_starters)
     starter = STDIN.gets.chomp
@@ -39,7 +65,7 @@ class Takeaway
     displays_order_confirmation_message
   end
 
-# TODO update with a loop to let them make 2 choices & push into basket
+  # TODO update with a loop to let them make 2 choices & push into basket
   def main_course_selection
     displays_options(extracts_main_course)
     main = STDIN.gets.chomp
@@ -47,7 +73,7 @@ class Takeaway
     displays_order_confirmation_message
   end
 
-# TODO update with a loop to let them make 2 choices & push into basket
+  # TODO update with a loop to let them make 2 choices & push into basket
   def dessert_selection
     displays_options(extracts_dessert)
     dessert = STDIN.gets.chomp
@@ -55,12 +81,12 @@ class Takeaway
     displays_order_confirmation_message
   end
 
-# TODO line too long refactor 
+  # TODO line too long refactor
   def displays_options(menu_extract)
     "Please choose the order number for the dish you would like from the menu options: #{menu_extract}:"
   end
 
-# TODO customise this so the message confirms the order number(s) so customer is sure of what they ordered
+  # TODO customise this so the message confirms the order number(s) so customer is sure of what they ordered
   def displays_order_confirmation_message
     "Thanks for your selection we've added your orders to your basket."
   end
@@ -70,31 +96,5 @@ class Takeaway
     "Thanks for your orders. Your order numbers are: #{@balance}"
   end
 
-  private
-
-# TODO refactor to avoid repetition- DRY 3's
-  def extracts_starters
-    starter1 = @menu[:starters][1].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    starter2 = @menu[:starters][2].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    starter1_key = @menu[:starters].keys[0]
-    starter2_key = @menu[:starters].keys[1]
-    "#{starter1_key}. #{starter1}, #{starter2_key}. #{starter2}"
-  end
-
-  def extracts_main_course
-    main1 = @menu[:main_course][3].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    main2 = @menu[:main_course][4].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    main1_key = @menu[:main_course].keys[0]
-    main2_key = @menu[:main_course].keys[1]
-    "#{main1_key}. #{main1}, #{main2_key}. #{main2}"
-  end
-
-  def extracts_dessert
-    desert1 = @menu[:dessert][5].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    desert2 = @menu[:dessert][6].map { |k, v| "#{k} = £#{v}" }.join(', ')
-    desert1_key = @menu[:dessert].keys[0]
-    desert2_key = @menu[:dessert].keys[1]
-    "#{desert1_key}. #{desert1}, #{desert2_key}. #{desert2}"
-  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,27 +2,45 @@ require './lib/takeaway.rb'
 require './lib/menu.rb'
 
 describe Takeaway do
-  let(:takeaway) { described_class.new }
-  let(:menu) { takeaway.menu }
-  # TODO use doubles throughout. e.g. allow(fake_menu).to receive(:menu).and_return(fake_menu)
+  subject(:takeaway) { described_class.new }
 
+  # TODO use doubles throughout. e.g. allow(fake_menu).to receive(:menu).and_return(fake_menu)
   describe '#initialize' do
-    it 'is initialized with a menu' do
-      expect(takeaway.menu).to eq menu
+    # TODO express differently/delete
+    xit 'is initialized with a menu' do
+      expect(takeaway.menu).to eq takeaway.menu
     end
     it 'is initialized with a basket' do
       expect(takeaway.basket).to eq []
     end
   end
 
-# TODO update tests with proper stdout format e.g expect(takeaway.lists_dishes_with_prices).to output(all_dishes).to_stdout
-  xdescribe '#lists_dishes_with_prices' do
-    it 'lists all menu dishes with prices' do
-      all_dishes = specify { expect { print(all_dishes) }.to output.to_stdout }
+  # TODO update tests with proper stdout format e.g expect(takeaway.lists_dishes_with_prices).to output(all_dishes).to_stdout
+  describe "#extracts_starters" do
+    it 'lists starters from menu' do
+      expect { takeaway.extracts_starters }.to output(/Starters: 1. Olives = £4, 2. Pitta bread = £5/).to_stdout
     end
   end
 
-# TODO update these tests to make sure that can choose 0 or Q
+  describe "#extracts_main_course" do
+    it 'lists main courses from menu' do
+      expect { takeaway.extracts_main_course }.to output(/Main Courses: 3. Fried duck = £12, 4. Fish & Chips/).to_stdout
+    end
+  end
+
+  describe "#extracts_dessert" do
+    it 'lists desserts from menu' do
+      expect { takeaway.extracts_dessert }.to output(/Desserts: 5. Jam roll = £5, 6. Apple crumble = £6/).to_stdout
+    end
+  end
+
+  describe '#lists_dishes_with_prices' do
+    it 'lists all menu dishes with prices' do
+      expect { takeaway.lists_dishes_with_prices }.to output(/Starters: 1. Olives = £4, 2. Pitta bread = £5 Main Courses: 3. Fried duck = £12, 4. Fish & Chips = £9 Desserts: 5. Jam roll = £5, 6. Apple crumble = £6/).to_stdout
+    end
+  end
+
+  # TODO update these tests to make sure that can choose 0 or Q
   xdescribe '#displays_controller' do
     it 'displays a message to prompt user to move to navigate to the selection menu and instructions to quit' do
       expect(takeaway.displays_controller).to eq "Welcome to our takeaway service. Please make your order by selecting 0 to be taken to the selection menu, press Q at anytime to escape the program:"
@@ -30,7 +48,7 @@ describe Takeaway do
     end
   end
 
-# TODO update these tests with doubles & mocks make them function
+  # TODO update these tests with doubles & mocks make them function
   xdescribe '#selection_menu' do
     it 'can select starter options' do
       expect(takeaway.selection_menu(:starters)).to eq "Thanks for your selection we've added your orders to your basket."
@@ -48,7 +66,7 @@ describe Takeaway do
 
   xdescribe '#select_dishes' do
     it 'has a basket that contains the selected dishes' do
-      takeaway.select_dishes("Starters: Olives = £4", "Main Course: Fish & Chips = £9", "Dessert: Jam roll = £5")
+      takeaway.select_dishes("Starters: 1. Olives = £4, 2. Pitta bread = £5 -- Main Course: 3. Fried duck = £12, 4. Fish & Chips = £9 -- Dessert: 5. Jam roll = £5, 6. Apple crumble = £6.")
       expect(takeaway.basket).to eq "Starters: Olives = £4 -- Main Course: Fish & Chips = £9 -- Dessert: Jam roll = £5."
     end
   end
