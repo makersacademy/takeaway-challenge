02ac7c4476bbc4bc59dadc811675d409973741b0
Lubos Michalic <lnmichalic@gmail.com>
2017-05-21 19:44:25 +0100

    Create restaurant_spec and restaurant file, write test to check if restaurant has a menu upon instantiation and write code to pass test

---
+++ lib/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant
+
+  attr_reader :menu
+
+  def initialize
+    @menu = Menu.new
+  end
+end
---
+++ spec/restaurant_spec.rb
@@ -0,0 +1,9 @@
+require 'restaurant'
+
+describe Restaurant do
+  let(:restaurant) { described_class.new }
+
+  it 'has a menu upon initialization' do
+    expect(restaurant.menu).to be_an_instance_of Menu
+  end
+end
