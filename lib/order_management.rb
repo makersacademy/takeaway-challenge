da38a9d1f0fb9bfd7d904305deae0d391ba8a02d
“rogrenke” <roland@rolandgrenke.com>
2017-05-21 23:35:43 +0100

    Version 0.9, full functionality according to user stories - connects the #order_by_command line with the SmsSender class - calls the Twilio Api if an order is placed successfully

--- lib/order_management.rb
+++ lib/order_management.rb
@@ -1,9 +1,18 @@
 class OrderManagement
 
+  def initialize(notification_tool = SmsSender.new)
+    @notification_tool = notification_tool
+  end
+
   def order_by_commandline(order_hash, menu_items)
     order_hash[:basket].each { |item| raise "Sorry, '#{item[:name]}' is not on our menu" unless item_of_menu?(item[:name], menu_items) }
     raise "Sorry, your total was incorrect. Order was not placed" unless calculate_total(order_hash, menu_items) == order_hash[:total]
+    message = "Thank you! Your order was placed and will be delivered before "+"#{Time.now.strftime('%H').to_i + 1}"+":"+Time.now.strftime('%M')+"."
+    @notification_tool.send(message)
+    message + " A confirmation was sent to your phone as a text."
   end
+
+  private
 
   def calculate_total(order_hash, menu_items)
     total = 0
@@ -16,8 +25,6 @@ class OrderManagement
     end
     total
   end
-
-  private
 
   def item_of_menu?(dish_name, menu_items)
     menu_items.each do |menu_item|
--- spec/order_management_spec.rb
+++ spec/order_management_spec.rb
@@ -3,7 +3,6 @@ require 'order_management'
 describe OrderManagement do
 
   subject(:order_management) { described_class.new }
-  let(:menu) { double :menu }
 
   it 'should respond to #order_by_commandline' do
     expect(order_management).to respond_to(:order_by_commandline).with(2).arguments
@@ -27,6 +26,12 @@ describe OrderManagement do
       expect { order_management.order_by_commandline(order_hash, menu.items) }.to raise_error("Sorry, your total was incorrect. Order was not placed")
     end
 
+    it 'returns the correct string at the end' do
+      menu = Menu.new
+      array_of_hashes = [{ name: "Spaghetti carbonara", price: 8 }, { name: "Spaghetti arabiata", price: 7 }, { name: "Lasagna al forno", price: 9 }, { name: "Pizza Margherita", price: 7 }, { name: "Pizza Quattro Stagioni", price: 10 }]
+      menu.bulk_add(array_of_hashes)
+      order_hash = { basket: [{ name: "Lasagna al forno", quantity: 2 }], total: 18 }
+      expect(order_management.order_by_commandline(order_hash, menu.items)).to eq "Thank you! Your order was placed and will be delivered before "+"#{Time.now.strftime('%H').to_i + 1}"+":"+Time.now.strftime('%M')+". A confirmation was sent to your phone as a text."
+    end
   end
-
 end
