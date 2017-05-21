eb74281aa3fb539ca0dc93639e6672db9395d61f
Kynosaur <kye.bracey@gmail.com>
2017-05-21 14:41:47 +0100

    Add test for pretty printing Menu#dishes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,4 +6,12 @@ describe Menu do
   it 'creates the list of items on the menu with prices upon instantiation' do
     expect(menu.dishes).to eq ({ "chicken burger": 3.49, "chicken wings": 4.99, "whole chicken": 10.49, "fries": 1.99, "mozzarella sticks": 1.99 })
   end
+
+  it 'prints out the list of dishes with prices' do
+    expect(menu.print_dishes).to output('":chicken burger": 3.49
+                                        "chicken wings": 4.99
+                                        "whole chicken": 10.49
+                                        "fries": 1.99
+                                        "mozzarella sticks": 1.99').to_stdout
+  end
 end
