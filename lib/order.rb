fa0e44bd3c56b088f57b164540006c67163acc2e
wemmm <jenny.wem@gmail.com>
2017-05-21 20:56:37 +0100

    Added failing test for getting the total from an order

--- lib/order.rb
+++ lib/order.rb
@@ -1,9 +1,14 @@
 class Order
 
-  attr_reader :current_order
+  attr_reader :current_order, :order_total
 
   def initialize
     @current_order = []
+    @order_total = nil
   end
 
+  def get_total
+    price_list = @current_order.map { |e| e.values }
+    @order_total = price_list.flatten.inject(:+)
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,4 +4,14 @@ describe Order do
 
   it { is_expected.to respond_to :current_order }
 
+  describe '#get_total' do
+    it 'should sum the order and set @order_total' do
+      restaurant = Restaurant.new
+      restaurant.menu.order_item(1, 2)
+      restaurant.checkout
+      subject.get_total
+      expect(restaurant.order.order_total).to eq 10
+    end
+  end
+
 end
