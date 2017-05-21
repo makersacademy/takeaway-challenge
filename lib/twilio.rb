84a6cef7cc5eb689492c4dacd36df68f9efd4bfd
Kynosaur <kye.bracey@gmail.com>
2017-05-21 22:07:15 +0100

    Implement dotenv to hide twilio information

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -1,15 +1,18 @@
+require 'dotenv/load'
+Dotenv.load('twilio.env')
+
 class Sms
   def send_sms(order, time)
     require 'twilio-ruby'
 
-    account_sid = "ACb2fdb625ae7da22b6887ec67f2c8faa7"
-    auth_token = "14591596dfd70c675baae610b632dac8"
+    account_sid = ENV['account_sid']
+    auth_token = ENV['auth_token']
 
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     @message = @client.messages.create(
-      to: "+447926613228",
-      from: "+441284277033",
+      to: ENV['to_number'],
+      from: ENV['from_number'],
       body: "Thank you! Your order of #{order} will be delivered by #{time.strftime("%H:%M")}. Enjoy!"
     )
   end
