a836fbc3ff1fcb9c49ab6abb86df3212b2f68981
Mário Ribeiro <mail@marioribeiro.com>
2017-05-22 08:11:23 +0100

    Reduce number of messages for sms_initial_menu

--- lib/receive_sms.rb
+++ lib/receive_sms.rb
@@ -17,49 +17,48 @@ end
 
 def send_message(message_body, to_number)
   @client.account.messages.create(:body => "#{message_body}",
-    :to   => to_number,    
+    :to   => customer_number,    
     :from => ENV['FROM_NUMBER'])
 end
 
 post '/receive-sms' do
   message_body = params["Body"]
-  from_number  = params["From"]
+  customer_number  = params["From"]
   content_type 'text/xml'
-  if !@customers.include? from_number
-    send_message("Welcome to SMS2EAT!", from_number)
-    sms_initial_menu(from_number)
-    @customers << from_number
-    KFC.create_customer_order(from_number)
+  if !@customers.include? customer_number
+    send_message("Welcome to SMS2EAT!", customer_number)
+    sms_initial_menu(customer_number)
+    @customers << customer_number
+    KFC.create_customer_order(customer_number)
     case message_body
     when "1"
-      KFC.add_items_to_order(from_number, "Chicken Burguer", 1)
-      send_message("Chicken Burguer added to your order", from_number)
+      KFC.add_items_to_order(customer_number, "Chicken Burguer", 1)
+      send_message("Chicken Burguer added to your order", customer_number)
     when "2"
-      KFC.add_items_to_order(from_number, "Chicken Wings", 1)
-      send_message("Chicken Wings added to your order", from_number)
+      KFC.add_items_to_order(customer_number, "Chicken Wings", 1)
+      send_message("Chicken Wings added to your order", customer_number)
     when "3"
-      KFC.finalize_customer_order(from_number)
-      send_message("Thanks. Your order will be confirmed soon", from_number)
-      KFC.accept_customer_order(from_number)
+      KFC.finalize_customer_order(customer_number)
+      send_message("Thanks. Your order will be confirmed soon", customer_number)
+      KFC.accept_customer_order(customer_number)
     end
-  elsif @customers.include? from_number
+  elsif @customers.include? customer_number
     case message_body
     when "1"
-      KFC.add_items_to_order(from_number, "Chicken Burguer", 1)
-      send_message("Chicken Burguer added to your order", from_number)
+      KFC.add_items_to_order(customer_number, "Chicken Burguer", 1)
+      send_message("Chicken Burguer added to your order", customer_number)
     when "2"
-      KFC.add_items_to_order(from_number, "Chicken Wings", 1)
-      send_message("Chicken Wings added to your order", from_number)
+      KFC.add_items_to_order(customer_number, "Chicken Wings", 1)
+      send_message("Chicken Wings added to your order", customer_number)
     when "3"
-      KFC.finalize_customer_order(from_number)
-      send_message("Thanks. Your order will be confirmed soon", from_number)
-      KFC.accept_customer_order(from_number)
+      KFC.finalize_customer_order(customer_number)
+      send_message("Thanks. Your order will be confirmed soon", customer_number)
+      KFC.accept_customer_order(customer_number)
     end
   end
 end
 
-def sms_initial_menu(from_number)
-  send_message("Send 1 to order: a Chicken Burguer (£8)", from_number)
-  send_message("Send 2 to order: Chicken Wings (£2)", from_number)
-  send_message("Send 3 finalize order", from_number)
+def sms_initial_menu(to_number)
+  send_message("SMS2EAT. Send 1 for Chicken Burguer (£8), 2 for Chicken Wings (£2)", customer_number)
+  send_message("Send 3 to finalize order", customer_number)
 end
