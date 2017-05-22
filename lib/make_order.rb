6285a707fc81e933c7eff9a9d7ccdc050ea724f1
Colin Scally <colin.scally@gmail.com>
2017-05-22 14:34:42 +0100

    started implementing twilio testing

--- lib/make_order.rb
+++ lib/make_order.rb
@@ -12,7 +12,7 @@ class MakeOrder
     @printer_class = printer_class
   end
 
-# aaaaaaargh monster class
+# aaaaaaargh a monster
   def start
     puts "Welcome to our takeaway. Please peruse our menu below. To add a dish to your order, please type the dish NUMBER and hit Enter. When you are happy with your order, please type 'order' and hit Enter"
     @printer_class.printmenu
@@ -29,15 +29,16 @@ class MakeOrder
     end
   end
 
-  def complete_order
-    @printer_class.printsummary()
-    $client.send_message
-  end
-
   def add_to_order(num)
     $current_order ||= CurrentOrder.new
     $current_order.ordered_dishes << Menu.new.dishes[num]
     print @printer_class.printorder
+  end
+
+  def complete_order
+    @printer_class.printsummary()
+    SMS.new.send_message
+    $current_order = nil
   end
 
   def current_order
--- lib/twilio.rb
+++ lib/twilio.rb
@@ -4,20 +4,19 @@ require 'dotenv'
 Dotenv.load
 
 
+class SMS
 
-public
+  attr_reader :client
 
+  def initialize
+    @client = Twilio::REST::Client.new(account_sid = ENV['TWILIO_SID'], auth_token = ENV['TWILIO_TOKEN'])
+  end
 
-
-def send_message
-  account_sid = ENV['TWILIO_SID'] # Your Account SID from www.twilio.com/console
-  auth_token = ENV['TWILIO_TOKEN']   # Your Auth Token from www.twilio.com/console
-
-  $client = Twilio::REST::Client.new account_sid, auth_token
-
-  message = $client.account.messages.create(:body => "Thank you! Your order was placed and will be delivered before #{Time.now.hour + 1}:#{Time.now.min}\n",
+  def send_message
+    message = @client.account.messages.create(:body => "Thank you! Your order was placed and will be delivered before #{Time.now.hour + 1}:#{Time.now.min}\n",
       :to => ENV['TWILIO_TO'],    # Replace with your phone number
       :from => ENV['TWILIO_FROM'])  # Replace with your Twilio number
+    puts message.sid
+  end
 
-      puts message.sid
 end
