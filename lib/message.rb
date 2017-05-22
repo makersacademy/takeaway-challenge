5fb8525976c5511c1695db6b860a4a44f56cb3d0
RSijelmass <R.Sijelmass@gmail.com>
2017-05-22 08:59:51 +0100

    Hide twilio details in seperate file

--- lib/message.rb
+++ lib/message.rb
@@ -1,23 +1,21 @@
 require 'twilio-ruby'
+require 'dotenv/load'
 
 # sends a text message confirming an order
 class Message
   attr_reader :time_order
 
   def initialize
-    @account_sid = 'xxxxxxxxxx'
-    @auth_token = 'xxxxxxxxxx'
-    @twilio_number = 'xxxxxxxxxx'
-    @customer_number = 'xxxxxxxxxx'
+
   end
 
   def send_message(price)
-    @client = Twilio::REST::Client.new @account_sid, @auth_token
+    @client = Twilio::REST::Client.new ENV[ACCOUNT_SID], [AUTH_TOKEN]
 
     calculate_time
     message = @client.account.messages.create(
-       :from => @twilio_number,
-       :to => @customer_number,
+       :from => ENV[TWILIO_NUMBER],
+       :to => ENV[CUSTOMER_NUMBER],
        :body => "So delicious, and no animals?! Here is one happy piggy. \u{1F437}
        Total cost will be £#{price}, and arrives at #{@time_order}. Enjoy!"
     )
