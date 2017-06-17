baf7ace03d0aa62e162b39fc83658491910f0e43
MarySalemme <marysalemme1@gmail.com>
2017-06-17 11:34:13 +0100

    Implementing show method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,14 @@
 class Menu
 
+  attr_reader :dishes
+
+  def initialize(dishes)
+    @dishes = dishes
+  end
+
+  def show
+    dishes.map do |dish|
+      "#{dish[:name]}: £#{dish[:price]}"
+    end.join("\n")
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,5 +2,23 @@
 require 'menu.rb'
 
 describe Menu do
+  let(:dishes) do [
+    { name: 'Spaghetti', price: 10 },
+    { name: 'Bolognese', price: 12 },
+    { name: 'Pizza', price: 8 }]
+  end
 
+  subject(:menu) { described_class.new(dishes) }
+
+  describe '#initialize' do
+    it 'is initialized with a list of dishes' do
+      expect(menu.dishes).to eq dishes
+    end
+  end
+
+  describe '#show' do
+    it 'outputs the list of dishes' do
+      expect(subject.show).to eq "Spaghetti: £10\nBolognese: £12\nPizza: £8"
+    end
+  end
 end
