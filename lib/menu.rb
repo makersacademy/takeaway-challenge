73ccb85c64882734ccfea4e76ce33da362f4bfe5
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-17 12:33:50 +0100

    Add welcome and print_dishes methods

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,21 @@
 class Menu
 
+def initialize
+  @korean_dishes = {
+    kimchi_jjigae: 8.99,
+    galbi: 10.99,
+    kimbap: 4.99,
+    bibimbap: 6.99,
+    kimchi_jeon: 4.99
+  }
+end
 
-end 
+  def welcome
+    "Welcome to Simon's Korean Restaurant!"
+    "Our specials today are:"
+  end
+
+  def print_dishes
+    @korean_dishes.map { |k,v| p "#{k}, £#{v}" }
+  end
+end
