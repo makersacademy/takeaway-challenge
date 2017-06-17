47680df0a5b834f11a6f1b142bd6bd38a95ce961
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-17 15:28:46 +0100

    Adding functionality to update order basket (e.g., update total) and related specs

--- lib/order.rb
+++ lib/order.rb
@@ -4,12 +4,18 @@ class Order
   attr_reader :basket, :total, :printer
 
   def initialize(printer = OrderPrinter.new)
-    @basket = {}
-    @total = 0
+    @basket = []
+    @total = 0.0
     @printer = printer
   end
 
-  def add
+  def add(dish)
+    basket << dish
+    note_price_of(dish)
+  end
+
+  def note_price_of(dish)
+    @total += dish[1]
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -13,4 +13,15 @@ describe Order do
         it {is_expected.to respond_to(:basket)}
       end
 
+      describe "#add" do
+        it "adds a dish to the order basket" do
+          dish = [:Pizza, 10.0]
+          expect{order.add(dish)}.to change{order.basket}.from([]).to([dish])
+        end
+        it "adds the dish's price to the order's total" do
+          dish = [:Pizza, 10.0]
+          expect{order.add(dish)}.to change{order.total}.from(0.0).to(dish[1])
+        end
+      end
+
 end
