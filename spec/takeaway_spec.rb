887469bda145631c209d426753aa2599faef1478
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-18 13:59:06 +0100

    Adding checkout spec for takeaweay object

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,40 +3,17 @@ require_relative "../lib/takeaway"
 
 describe Takeaway do
 
-  subject(:takeaway) { described_class.new(menu, order, message) }
+  subject(:takeaway) { described_class.new(menu, order, messager) }
 
   let(:menu) { double(:menu) }
   let(:order) { double(:order) }
-  let(:message) { double(:message) }
+  let(:messager) { double(:messager) }
 
-  describe "attributes" do
-    it { is_expected.to respond_to(:menu) }
-    it { is_expected.to respond_to(:order) }
-    it { is_expected.to respond_to(:message) }
 
-    context "on init" do
-      describe "@menu" do
-        it "will default to my menu class" do
-          expect(takeaway.menu).to be menu
-        end
-      end
-      describe "@order" do
-        it "will default to my order class" do
-          expect(takeaway.order).to be order
-        end
-      end
-      describe "@message" do
-        it "will default to my message class" do
-          expect(takeaway.message).to be message
-        end
-      end
-    end
-  end
-
-  describe "#see_menu" do
+  describe "#show_menu" do
     it "asks the menu to show its dishes" do
-      allow(menu).to receive(:show_dishes)
-      expect(menu).to receive(:show_dishes)
+      allow(menu).to receive(:show_food)
+      expect(menu).to receive(:show_food)
       takeaway.show_menu
     end
   end
@@ -48,6 +25,7 @@ describe Takeaway do
         expect { takeaway.add(:Truffles) }.to raise_error("That's not on the menu!")
       end
     end
+
     context "when user orders a dish on menu" do
       it "adds the dish to the order" do
         allow(menu).to receive(:has?).with(:Pizza).and_return([:Pizza, 10.0])
@@ -64,6 +42,16 @@ describe Takeaway do
         expect { takeaway.checkout(5.0) }.to raise_error("You're short. Please pay the correct sum man.")
       end
     end
+
+    context "when user pays correct sum" do
+    it "asks the messanger to send confirmation" do
+      allow(order).to receive(:total).and_return(10.00)
+      allow(messager).to receive(:send_confirmation)
+      expect(messager).to receive(:send_confirmation)
+      takeaway.checkout(10.00)
+    end
+   end
+
   end
 
   describe "summary" do
