ce60373f4fec90a605c0b112a04caaf88753cca4
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-15 12:37:53 +0200

    Dish class ready and tested

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,3 +1,15 @@
 # this class will hold instances of individual dishes incl. their name and price
 class Dish
+
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def menu_readable
+    "#{name}, £#{price}"
+  end
+
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,5 +1,21 @@
 require 'dish'
 
 describe Dish do
-  
+
+  subject(:dish) { described_class.new("Fries", 5) }
+
+  describe '#initialization' do
+    it 'has a name variable upon initialization' do
+      expect(dish.name).to eq "Fries"
+    end
+    it 'has a price variable upon initialization' do
+      expect(dish.price).to eq 5
+    end
+  end
+
+  describe '#menu_readable' do
+    it 'returns a string with menu_readable' do
+      expect(dish.menu_readable).to eq "Fries, £5"
+    end
+  end
 end
