0a1f99b577b0ccf3a76bae45504caf0d47d152b2
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-15 14:53:47 +0200

    Menu class working and tested

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,20 @@
 # this class returns readable list of dishes and allows for placing an order
+require_relative 'dish'
+
 class Menu
+
+  attr_reader :list_of_dishes
+
+  def initialize
+    @list_of_dishes = []
+  end
+
+  def add_dish(name, price, dish_class = Dish)
+    @list_of_dishes << dish_class.new(name, price)
+  end
+
+  def show_menu
+    list_of_dishes.each_with_index.map { |dish, index| "#{index + 1}. #{dish.menu_readable}" }.join("\n")
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,31 @@
 require 'menu'
+require 'dish'
 
 describe Menu do
-  
+
+  subject(:menu) { described_class.new }
+
+  describe '#initialization' do
+    it 'builds an empty menu upon initialization' do
+      expect(subject.list_of_dishes.count).to eq 0
+    end
+  end
+
+  describe '#add_dish' do
+    it 'adds a dish into the menu instance variable' do
+      subject.add_dish("Fries", 5)
+      subject.add_dish("Pizza", 6)
+      subject.add_dish("Sushi", 8)
+      expect(subject.list_of_dishes.count).to eq 3
+    end
+  end
+
+  describe '#show_menu' do
+    let(:dish) { double :dish }
+    it 'shows menu when requested' do
+      subject.add_dish("Fries", 5)
+      subject.add_dish("Pizza", 6)
+      expect(subject.show_menu).to eq "1. Fries, £5\n2. Pizza, £6"
+    end
+  end
 end
