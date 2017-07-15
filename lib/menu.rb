63a55a943eca8cfa14b9c3c7c52d5b6c8019d7ec
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-15 16:55:56 +0200

    Orders can be placed

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,12 +1,14 @@
 # this class returns readable list of dishes and allows for placing an order
 require_relative 'dish'
+require_relative 'order'
 
 class Menu
 
-  attr_reader :list_of_dishes
+  attr_reader :list_of_dishes, :new_order
 
   def initialize
     @list_of_dishes = []
+    @new_order = []
   end
 
   def add_dish(name, price, dish_class = Dish)
@@ -17,4 +19,12 @@ class Menu
     list_of_dishes.each_with_index.map { |dish, index| "#{index + 1}. #{dish.menu_readable}" }.join("\n")
   end
 
+  def add_to_order(index, quantity)
+    selected_dish = list_of_dishes[index-1]
+    @new_order << { dish: selected_dish.name, quantity: quantity, price: (quantity*selected_dish.price) }
+  end
+
+  def place_order(order_class = Order)
+    order_class.new(new_order)
+  end
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,11 @@
 # this class contains an order of dishes chosen from the menu
 class Order
+
+  attr_reader :current_order
+
+  def initialize(new_order)
+    @current_order = new_order
+  end
+
+
 end
