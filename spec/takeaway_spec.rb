aabc453d3646c7f558f0f09600aede50edecc3b0
nandhini <nandhini31@gmail.com>
2017-07-16 21:14:42 +0100

    refactored

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,9 +4,9 @@ require 'twilio-ruby'
 describe Takeaway do
   let(:takeaway) {described_class.new}
   # let(:menu) {double :menu}
-  let(:my_order) {{"Dal Tadka" =>1, "Rice" =>1, "Naan" =>1}}
-  let(:total) {9.50}
-  let(:eta) {"22:00"}
+  let(:my_order) {{ "Dal Tadka" => 1, "Rice" => 1, "Naan" => 1 }}
+  let(:total) { 9.50 }
+  let(:eta) { "22:00" }
 
     context 'customer can see the menu' do
       it 'allows customers to see the menu' do
@@ -20,30 +20,27 @@ describe Takeaway do
       end
     end
 
-
-
     context 'customer can confirm total price of placed order' do
       it 'can confirm the total price of orders' do
         takeaway.place_order(my_order,total)
-        expect(takeaway.price).to eq (total)
+        expect (takeaway.price).to eq (total)
       end
 
       it 'raises error if total does not match that' do
         takeaway.place_order(my_order,total)
         takeaway.price
-        expect{takeaway.confirm_order}.to raise_error 'Sorry total does not match'
+        expect {takeaway.confirm_order }.to raise_error 'Sorry total does not match'
       end
 
-        context 'send sms on complete order' do
-          before do
-            allow(takeaway).to receive(:eta).and_return(eta)
-            allow(takeaway).to receive(:send_notification)
-          end
-          it 'sends text message' do
-            expect(takeaway).to receive(:send_notification).with("Thank you! Your order was placed and will be delivered before 22:00")
-            takeaway.complete_order
-        end
+    context 'send sms on complete order' do
+      before do
+        allow(takeaway).to receive(:eta).and_return(eta)
+        allow(takeaway).to receive(:send_notification)
       end
-
+      it 'sends text message' do
+        expect(takeaway).to receive(:send_notification).with("Thank you! Your order was placed and will be delivered before 22:00")
+        takeaway.complete_order
+      end
     end
+  end
 end
