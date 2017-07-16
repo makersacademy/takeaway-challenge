b2ac7b24d5a865b34392088a32d3c301e2224b5b
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-16 12:13:37 +0100

    Add Menu methods

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,27 @@
 class Menu
 
+  attr_reader :dishes, :list_dishes
+
+  def initialize
+    @dishes = []
+  end
+
+  def add_dish(dish)
+    add_to_dishes(dish)
+  end
+
+  def list_dishes
+    print_each_dish
+  end
+
+private
+
+  def add_to_dishes(dish)
+    @dishes << dish
+  end
+
+  def print_each_dish
+    @dishes.each { |dish| print dish }
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,26 @@ require 'menu'
 
 describe Menu do
 
+  subject(:menu) { described_class.new }
+  let(:dish) { double "burrito" }
+
+  it 'responds to dishes' do
+    expect(menu).to respond_to(:dishes)
+  end
+
+  context 'Dish added to menu' do
+    before { menu.add_dish("burrito") }
+
+    describe '#dishes' do
+      it 'dishes can be added to menu' do
+        expect(menu.dishes).to include("burrito")
+      end
+    end
+
+    describe "#list_dishes" do
+      specify { expect { menu.list_dishes }.to output("burrito").to_stdout }
+    end
+
+  end
+
 end
