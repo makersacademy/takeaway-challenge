e076b3ad9d5aa4cd2e9090ea0f6138d3cdb6de2e
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-17 00:53:21 +0100

    Refactor code and tests

--- lib/order.rb
+++ lib/order.rb
@@ -4,21 +4,25 @@ require_relative 'text_sender'
 class Order
   attr_reader :total, :order_data
 
-  def initialize(**options)
-    # raise "Not enough data to create an order." unless options[:order]
-    @list = options[:list] || DishList.new
-    @order_data = options[:order]
+  def initialize
     @total = 0
-    update_total
     @text_sender = TextSender.new
   end
 
-  def place
-    message = "Thank you! Your order was placed and will be delivered before #{Time.now.hour + 1}:#{Time.now.min}"
-    text_sender.text(message)
+  def place(options)
+    @list = options[:list]
+    @order_data = options[:order]
+    update_total
+    raise "Error! Wrong total." if options[:total] != total
+    send_text
   end
 
   private
+
+  def send_text
+    message = "Thank you! Your order was placed and will be delivered before #{Time.now.hour + 1}:#{Time.now.min}"
+    @text_sender.text(message)
+  end
 
   def list_data
     @list.data
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,17 +4,19 @@ describe Order do
   let(:test_order) { { lobster: 1, nachos: 2 } }
   let(:test_list_data) { { burger: 15, lobster: 19, nachos: 5 } }
   let(:test_list) { double 'test_list', data: test_list_data }
-  subject(:order) { described_class.new({ order: test_order, list: test_list }) }
+  let(:test_total) { 29 }
+  subject(:order) { described_class.new }
 
-  describe '#initialize' do
-    it 'expected to create new orders' do
-      expect { described_class.new({ order: test_order, list: test_list }) }.not_to raise_error
+  describe '#make' do
+    it 'expected to place new orders' do
+      expect { order.place({ list: test_list, order: test_order, total: test_total }) }.not_to raise_error
     end
   end
 
   describe '#total' do
     it 'expected to calculate order total' do
-      expect(order.total).to eq 29
+      order.place({ list: test_list, order: test_order, total: test_total })
+      expect(order.total).to eq test_total
     end
   end
 end
