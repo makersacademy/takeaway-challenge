5ce8081223be8a16a59657668d2651d43f3ce4ad
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-17 00:06:57 +0200

    Text twilio sender partially implemented

--- lib/msg_sender.rb
+++ lib/msg_sender.rb
@@ -1,17 +1,18 @@
 # this classes responsibility is to send a text with a string once an order is placed
+require 'twilio-ruby'
+
 class MsgSender
-=begin
-  require 'twilio-ruby'
 
-  account_sid = "ACae4d77700e129e646608cea2e957ce32"
-  auth_token = "5fd7cfd5940811b1c6dea7bc36ed42dd"
+  def send_text
+    account_sid = "ACae4d77700e129e646608cea2e957ce32"
+    auth_token = "5fd7cfd5940811b1c6dea7bc36ed42dd"
 
-  @client = Twilio::REST::Client.new account_sid, auth_token
-  message = @client.messages.create(
-      body: "Thank you! Your order was placed and will be delivered before in 1 hour",
-      to: "+420720759109",
-      from: "+420720759109")
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.messages.create(
+        body: "Thank you! Your order was placed and will be delivered in 1 hour",
+        to: "+420720759109",
+        from: "+420538890095")
 
-  puts message.sid
-=end
+        puts message.sid
+  end
 end
