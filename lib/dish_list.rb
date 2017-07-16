68bd42133af782c6cd9546dd10829665338c3e64
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-16 12:09:18 +0100

    Add dish id fancy output

--- lib/dish_list.rb
+++ lib/dish_list.rb
@@ -4,14 +4,14 @@ class DishList
 
   def initialize
     @list = Array.new
-    add(Dish.new(name: 'Beef Burger', price: 7.5))
-    add(Dish.new(name: 'Nachos',      price: 4.75))
-    add(Dish.new(name: 'Lobster',     price: 19.75))
+    add(Dish.new(name: 'Beef Burger', id: 'BB', price: 7.5))
+    add(Dish.new(name: 'Nachos',      id: 'NC', price: 4.75))
+    add(Dish.new(name: 'Lobster',     id: 'LR', price: 19.75))
   end
 
   def show
     puts "\n\nM E N U\n\n"
-    list.each { |item| puts "#{item.name}".ljust(25,'.') + " £#{item.price}" }
+    list.each { |item| puts "#{item.id} - " + "#{item.name}".ljust(25,'.') + " £#{item.price}" }
     puts "\n"
     list.dup
   end
