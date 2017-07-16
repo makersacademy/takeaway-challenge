100ad3a54ee7379c4358d1783a795aa6090bb44a
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-16 22:24:25 +0100

    Fixed issue with sending real texts

---
+++ lib/dish.rb
@@ -0,0 +1,8 @@
+class Dish
+  attr_reader :name, :price
+  
+  def initialize(data)
+    @name = data[:name]
+    @price = data[:price]
+  end
+end
--- lib/text_sender.rb
+++ lib/text_sender.rb
@@ -2,17 +2,16 @@ require 'twilio-ruby'
 
 class TextSender
   def initialize
-    account_sid = ENV["TWILIO_TEST_SID"]
-    auth_token = ENV["TWILIO_TEST_TOKEN"]
+    account_sid = ENV["TWILIO_LIVE_SID"]
+    auth_token = ENV["TWILIO_LIVE_TOKEN"]
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
   def text
-    message = "Thank you! Your order was placed and will be delivered before #{Time.now.hour + 1}:#{Time.now.min}"
     @client.api.account.messages.create(
-      from: '+15005550006',
+      from: ENV["TWILIO_FROM_NO"],
       to: ENV["MY_PHONE_NO"],
-      body: message
+      body: "Thank you! Your order was placed and will be delivered before #{Time.now.hour + 1}:#{Time.now.min}"
     )
   end
 end
