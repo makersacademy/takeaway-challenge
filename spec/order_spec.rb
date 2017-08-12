ab2b8eb67834b4c40c8d50708092af7943260d69
Jenny Bacon <jenniferbacon01@gmail.com>
2017-08-12 12:21:11 +0100

    multiple items in an order

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,12 +3,18 @@ require 'order'
 describe Order do
   let(:order) { described_class.new }
   let(:order_item) { double "order_item" }
+  let(:order_item2) { double "order_item2" }
   it "is initialized with an empty array" do
     expect(order.contents).to eq []
   end
-  it "can receive 1 order item" do
+  it "can receive and store 1 order item" do
     order.receive(order_item)
     expect(order.contents).to eq [order_item]
   end
-  
+  it "can receive and store 2 order items" do
+    order.receive(order_item)
+    order.receive(order_item2)
+    expect(order.contents).to eq [order_item, order_item2]
+  end
+
 end
--- spec/takeaway_app_spec.rb
+++ spec/takeaway_app_spec.rb
@@ -2,7 +2,7 @@ require "takeaway_app"
 
 describe TakeawayApp do
   let(:takeaway_app) { described_class.new }
-  let(:order_item) { double "order_item" }
+  # let(:order_item) { double "order_item" }
   let(:order) { double "order" }
 
   it "shows a list of dishes and prices" do
@@ -11,7 +11,7 @@ describe TakeawayApp do
   it "prompts user to make an order" do
     expect { takeaway_app.prompt_order }.to output("Please enter the dish number followed by a space followed by the quantity you would like to order. When you are finished please press enter twice.\n").to_stdout
   end
-  it "can receive and store an order of 1 item" do
+  it "can receive and store an order" do
     subject.receive_order(order)
     expect(subject.order).to eq order
   end
