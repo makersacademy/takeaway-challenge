67ba74fad85528ff7bcfd3e9cff9e68c0541815e
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-08-12 11:00:06 +0100

    menu tests updated

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,23 +3,29 @@ require_relative '../lib/menu'
 describe 'Menu' do
 
   subject(:menu) { Menu.new }
+  let(:print_menu) { "Curry - 8.95\n" "Rice - 3.5\n" "Naan - 2.5\n" }
 
   # how to intialize with 2 arguments
 
-  context 'view the menu' do
+  context 'new instance' do
 
-    describe 'shows the menu' do
-      it 'initializes with dish and price' do
-        menu = Menu.new( 'dish', 'price')
-        expect(menu.dish).to eq('dish')
+    describe 'initializes with dishes' do
+      it 'outputs the right hash for the key' do
+        expect(subject.dish.keys[0]).to eq(:curry)
       end
+    end
+  end
 
+  context 'shows the menu' do
 
+    describe'printing menu to user' do
       it 'responds to show menu' do
-        menu = Menu.new( 'dish', 'price')
-        expect(menu).to respond_to(:show_menu)
+        expect(subject).to respond_to(:print_menu)
       end
 
+      it 'prints the menu out to user' do
+        expect{menu.print_menu}.to output(print_menu).to_stdout
+      end
     end
   end
 end
