3bce9d1dfcd6560f704e508783dc0d3a53473ac5
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-13 17:44:40 +0100

    Refactoring

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,10 @@
 class Menu
-  attr_reader :menu
+  attr_reader :current_menu
 
   def initialize
-    @current_menu = { item1: "soup bowl", price1: 12,
-       item2: "detox salad", price2: 15,
-       item3: "tokyo bowl", price3: 12 }
+    @current_menu = { soup_bowl: 12,
+       detox_salad: 15,
+       tokyo_bowl: 12 }
   end
 
   def print_menu
