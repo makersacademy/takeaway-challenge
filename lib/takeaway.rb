80af995292e303e93e31d016dee9a16b83373b92
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-13 22:40:23 +0100

    Updated takeaway homepage

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,13 @@
 require 'menu'
+require 'order'
+require 'messenger'
 
 class Takeaway
-  attr_reader :menu
-  
+  attr_reader :menu, :order, :messenger
+
   def initialize
     @menu = Menu.new
+    @order = Order.new
+    @messenger = Messenger.new
   end
 end
