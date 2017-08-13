def2636655f3f4e2e4a74dac5d02781449a6ca57
David Divilly <d.divilly@gmail.com>
2017-08-13 16:44:43 +0100

    adds dish_order and amount

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,9 +5,10 @@ sweet_and_sour_chicken: 5.50
 
 class Menu
 
-  attr_reader :dishes
+  attr_reader :dishes, :order
 
   def initialize
+    @order = []
     @dishes = DISHES
   end
 
@@ -17,7 +18,21 @@ class Menu
     end
   end
 
+  def dish_order(dish, amount = 1)
+    amount.times { @order << dish }
+  end
+
+  def order_amount
+    @order.map { |dish| @dishes[dish] }.inject(:+)
+  end
+     
 end
 
 menu = Menu.new
 menu.print_dishes
+menu.dish_order(:chicken_chow_mein)
+menu.dish_order(:chicken_chow_mein, 5)
+p menu.order
+menu.dish_order(:sweet_and_sour_chicken)
+p menu.order_amount
+p menu.order
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -10,10 +10,33 @@ describe Menu do
   end
 
   context '#print_dishes' do
-
     it 'should print the dishes to stdout' do
       expect { menu.print_dishes }.to output.to_stdout
     end
   end
+
+  context '#dish_order' do
+    it 'should store the order' do
+      menu.dish_order(:chicken_chow_mein)
+      expect(menu.order).to include(:chicken_chow_mein)
+    end
+  end
+
+  context '#order_amount' do
+    it 'should calculate the total order amount' do
+      menu.dish_order(:chicken_chow_mein)
+      menu.dish_order(:chicken_chow_mein)
+      expect(menu.order_amount).to eq 10
+    end
+  end
+
+  # context '#show_order' do
+  #   it "should show the amount of items per dish" do
+  #     menu.dish_order(:chicken_chow_mein)
+  #     menu.dish_order(:chicken_chow_mein)
+  #     expect(menu.show_order).to include(chicken_chow_mein: 2)
+  #   end
+  # end
+
 
 end
