65d947d9d234dc4e71a1297ecbe50d10657baf6f
Abi Travers <atravers4007@gmail.com>
2017-08-13 21:38:03 +0100

    Still working on Rspec tests for stubbing out time

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -7,10 +7,16 @@ describe Text do
     expect { text.calculating_time }.to change { text.delivery_hour }
   end
 
-  it 'writes a message for the text' do
-    allow(text.text_for_message).to receive(:calculating_time).and_return "22:15"
+  it 'possible stubbs test for calculating time' do
+    allow(text).to receive(:calculating_time) { "22:15" }
+    expect(text.calculating_time).to eq("22:15")
+  end
+
+  it 'the output message includes the time and order total' do
+    allow(text).to receive(:calculating_time) { "22:15" }
     output = "Thank you! Your order was placed and will be delivered before 22:15 Year: 9906. We hope you enjoy your average at best meal./n"
-    expect { text.text_for_message }.to output(output).to_stdout
+    expect(text.text_for_message).to eq(output)
+    text.text_for_message
   end
 
 end
