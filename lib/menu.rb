a8c4f0555f42d51073e63bcd7a0ec359dc478745
alessiobortone2 <alessiobortone2@gmail.com>
2017-08-14 01:37:25 +0100

    First 3 methods designed, 2 tests pending

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,12 +1,48 @@
 class Menu
 
-  attr_reader :list
+  attr_reader :options
 
-  def initialize(list)
-    @list = list
+  def initialize(options = nil)
+    @options = options
   end
 
-  def view(menu)
+  def create
+    puts 'Input dish'
+    dish = gets.chomp
+    puts 'Input price, or x for exit'
+    price = gets.chomp
+    if gets.chomp == "x"
+      return @options
+    else
+      option = { dish => price }
+      @options << option
+      return @options
+    end
   end
 
+  def view
+    @options.each do |o|
+      puts o
+    end
+  end
+
+  def select(index)
+    return @options.at(index)
+  end
+
+  def check(index)
+    if select(index) == @options.at(index)
+      puts "all ok"
+    else
+      puts "order system not working"
+    end
+  end 
 end
+
+# puts "Select option No. from menu, or x for exit"
+# choice = gets.chomp
+# arr1 = Array.new
+# while choice != "x" do
+#   arr1 << choice
+#
+# end
