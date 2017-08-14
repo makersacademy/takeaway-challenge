b0533eebd377286fc097cd4012a95531289586a4
Abi Travers <atravers4007@gmail.com>
2017-08-14 11:31:59 +0100

    Latest. Not complete. See readme

--- lib/text.rb
+++ lib/text.rb
@@ -8,6 +8,7 @@ class Text
   def initialize
     @delivery_hour = 0
     @delivery_min = 0
+    @total_amount = User.new
   end
 
 
@@ -19,6 +20,11 @@ class Text
   def text_for_message
     calculating_time
     puts "Thank you! Your order was placed and will be delivered before #{@delivery_hour}:#{@delivery_min} Year: 9906. We hope you enjoy your average at best meal."
+  end
+
+  def total_price_of_basket
+    @total_amount = (@total_amount.order_selection(2, 3)).total_price_of_basket
+    return @total_amount
   end
 
   # def twilio_text_send
--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -2,6 +2,7 @@ require 'text'
 
 describe Text do
   let(:text) { described_class.new }
+  let(:user) { double :user }
 
   it 'calculates the current time, then adds one hour' do
     expect { text.calculating_time }.to change { text.delivery_hour }
@@ -11,6 +12,10 @@ describe Text do
     allow(text).to receive(:calculating_time) { "22:15" }
     expect(text.calculating_time).to eq("22:15")
   end
+
+  # it 'passes the total_amount over from the user' do
+  #   expect(text.total_price_of_basket).to eq()
+  # end
 
   # it 'the output message includes the time and order total' do
   #   allow(text).to receive(:calculating_time) { "22:15" }
