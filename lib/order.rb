af0238ae52912a9d46d544206cb815753f7657c4
James Hughes <hugjimbo77@gmail.com>
2017-09-10 21:12:53 +0100

    order add dish, item_count & order_total added

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,28 @@
 class Order
 
+  attr_reader :orders, :count, :total
+
+  def initialize
+    @orders = {}
+    @count = 0
+    @total = 0
+  end
+
+  def add_dish(dish, quantity)
+    @orders[dish] = quantity
+  end
+
+  def display_current_order
+    @orders
+  end
+
+  def item_count
+    orders.each { |_dish, amount| @count += amount }
+    @count
+  end
+
+  def order_total
+    orders.each { |dish, amount| @total += (dish.price * amount) }
+    @total
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,25 @@
 require 'order'
 
 RSpec.describe Order do
+  subject(:order) { described_class.new }
+  let(:butter_chicken) { double :dish, price: 7.00 }
+  let(:pad_thai) { double :dish, price: 8.00 }
+
+  it 'add dish to your order' do
+    order.add_dish(butter_chicken, 1)
+    expect(order.orders).to include(butter_chicken)
+  end
+
+  it 'knows how many of each item are in the order' do
+    order.add_dish(pad_thai, 2)
+    order.add_dish(butter_chicken, 1)
+    expect(order.item_count).to eq(3)
+  end
+
+  it 'can calculate the total of an order' do
+    order.add_dish(pad_thai, 3)
+    order.add_dish(butter_chicken, 1)
+    expect(order.order_total).to eq(31.00)
+  end
 
 end
