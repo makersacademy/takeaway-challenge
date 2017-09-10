df5522b8c5679ef393e2da208c23005ba9485327
keith(mac) <ka33145@googlemail.com>
2017-09-10 09:47:51 +0100

    Checked read returned either new menu or default menu

--- lib/takeway.rb
+++ lib/takeway.rb
@@ -1,7 +1,25 @@
 class Takeway
 
-  def read
+  attr_reader :menu_list
 
+  DEFAULT_MENU_LIST = {
+    'Napolitana Pizza' => 16.32,
+    'Proscuiutto Pizza' => 13.50,
+    'Hawaiian Pizza' => 12.35,
+    'Quattro Formaggi Pizza' => 10.50,
+    'Fiorentina Pizza' => 13.45,
+    'Ruspante Pizza' => 11.20,
+    'Milano pizza' => 12.60,
+    'Gamberoni Pizza' => 8.75,
+    'Capricciosa Pizza' => 10.99
+  }
+
+  def initialize(menu_list = DEFAULT_MENU_LIST)
+    @menu_list = menu_list
+  end
+
+  def read
+    @menu_list.each { |item, price| "#{item} : #{price}" }
   end
 
 end
--- spec/takeway_spec.rb
+++ spec/takeway_spec.rb
@@ -4,5 +4,15 @@ describe Takeway do
     it 'has method' do
       is_expected.to respond_to(:read)
     end
+    it'return default menu list' do
+      output = subject.menu_list.each { |item, price| "#{item} : #{price}" }
+      expect(subject.read).to eq output
+    end
+    it'return new menu list' do
+      list = { "Spring Roll" => 0.99, "Fried Prawn" => 2.99 }
+      takeway = Takeway.new(list)
+      output = takeway.menu_list.each { |item, price| "#{item} : #{price}" }
+      expect(takeway.read).to eq output
+    end
   end
 end
