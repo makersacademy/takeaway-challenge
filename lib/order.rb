5b3068ac1ed47749f767e5ce7a45a8fd7b785f25
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-10 12:31:29 +0100

    Update Order with #valid_dish? method

--- lib/order.rb
+++ lib/order.rb
@@ -10,12 +10,13 @@ class Order
   end
 
   def add_dish(dish, quantity = 1)
+    valid_dish?(dish)
     @items[dish] = 0 unless items[dish]
     @items[dish] += quantity
   end
 
   def remove_dish(dish)
-
+    
   end
 
   def order_total
@@ -24,6 +25,13 @@ class Order
 
   def finalize_order
 
+  end
+
+  def valid_dish?(dish)
+    @menu.list.each do |menu_item|
+      return true if dish == menu_item[:name]
+    end
+    fail 'Item not found'
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,4 +9,10 @@ describe Order do
   it { is_expected.to respond_to(:order_total) }
   it { is_expected.to respond_to(:finalize_order) }
 
+  describe '#valid_dish?' do
+    it 'returns error message if dish is not valid' do
+      expect { order.add_dish("Hamburger", 2) }.to raise_error 'Item not found'
+    end
+  end
+
 end
