9dcfdc0b2304a2e8396e0da994e1d887b11fcf63
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-10 09:36:54 +0100

    Define #initializer method for Restaurant

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,15 @@
+require_relative 'notifier'
+require_relative 'menu'
+
 class Restaurant
 
+  attr_reader :name, :address, :menu
+
+  def initialize(name, address, menu = Menu.new, notifier = Notifier.new)
+    @name = name
+    @address = address
+    @notifier = notifier
+    @menu = menu
+    @orders = []
+  end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,6 +1,11 @@
 require 'restaurant'
 
 describe Restaurant do
-  subject(:restaurant) { describe_class.new }
-  
+  subject(:restaurant) { described_class.new("Busaba Eathai", "Chelsea", menu: menu, notifier: notifier) }
+
+  let(:menu) { instance_double("Menu") }
+  let(:notifier) { instance_double("Notifier") }
+
+  it { is_expected.to respond_to(:menu) }
+
 end
