249954578377444bdbc1c82263c058e1fc348fc9
Luan van Pletsen <luanvanpletsen@msn.com>
2017-09-10 15:44:15 +0100

    order view

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,28 @@
 class Order
-  attr_reader :selection
+  attr_reader :selection, :menu
 
-  def initialize(selection)
+  def initialize(selection, menu)
     @selection = selection
+    @menu = menu
   end
-  
+
+  def total
+    total = 0
+    selection.each do |item|
+      total += @menu.items[item-1][1]
+    end
+    total
+  end
+
+  def view
+    output = ""
+    selection.each do |item|
+      output += "#{@menu.items[item-1][0]} \t #{@menu.items[item-1][1]}\n"
+    end
+    output += "-----------------------\n"
+    output += "total: \t #{total}"
+    puts output
+    return output
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,4 +1,5 @@
 require 'order'
+require 'pry'
 
 RSpec.describe Order do
   subject(:order) { described_class.new }
@@ -11,23 +12,36 @@ RSpec.describe Order do
   context 'order selection' do
     it 'stores an initial selection' do
       selection = [1,1,2,3]
-      order = described_class.new(selection)
+      order = described_class.new(selection, menu)
       expect(order.selection).to eq selection
     end
   end
 
   describe '#view' do
-    it 'displays same number of items as in selection' do
-        selection = [1,1,2,3]
-        order = described_class.new(selection)
-    end
-  end
 
-  context 'displaying order' do
-    it 'displaying the total' do
+    it  'formatted message contains correct items' do
       selection = [1,1,2,3]
-      order = described_class.new(selection)
+      order = described_class.new(selection, menu)
+      message = "catnip 	 5\ncatnip 	 5\nCATnip 	 10\nCATNIP 	 15\n-----------------------\ntotal: 	 35"
+      expect(order.view).to eq message
+    end
 
+  end
+
+  describe '#total' do
+    it 'calculates the correct total' do
+      selection = [1,1,2,3]
+      order = described_class.new(selection, menu)
+      expect(order.total).to eq 35
     end
   end
+
+  #
+  # context 'displaying order' do
+  #   it 'displaying the total' do
+  #     selection = [1,1,2,3]
+  #     order = described_class.new(selection)
+  #
+  #   end
+  # end
 end
