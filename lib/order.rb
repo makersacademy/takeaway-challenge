41f64fc15da038230ed263bf92ab7bedcd351ee9
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-10 13:09:51 +0100

    Update Order so #add_dish method adds dishes to the items array

--- lib/order.rb
+++ lib/order.rb
@@ -4,28 +4,42 @@ class Order
   attr_reader :menu
 
   def initialize(menu)
-    @items = Hash.new(0)
+    @items = []
     @status = :new
     @menu = menu
   end
 
   def add_dish(dish, quantity = 1)
     valid_dish?(dish)
-    @items[dish] = 0 unless items[dish]
-    @items[dish] += quantity
+    get_dish_info_from_menu(dish)
+    @items << @dish
   end
 
   def remove_dish(dish)
-    
+    valid_dish?(dish)
+    get_dish_info_from_menu(dish)
+    @items.delete(dish)
   end
 
   def order_total
-
+    total = 0
+    @items.each do |item|
+      total += item[:price]
+    end
+    total
   end
 
   def finalize_order
 
   end
+
+  def get_dish_info_from_menu(dish)
+    @menu.list.each do |menu_item|
+      @dish = menu_item if dish == menu_item[:name]
+    end
+  end
+
+  private
 
   def valid_dish?(dish)
     @menu.list.each do |menu_item|
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,9 +9,25 @@ describe Order do
   it { is_expected.to respond_to(:order_total) }
   it { is_expected.to respond_to(:finalize_order) }
 
-  describe '#valid_dish?' do
-    it 'returns error message if dish is not valid' do
-      expect { order.add_dish("Hamburger", 2) }.to raise_error 'Item not found'
+  describe '#add_dish' do
+    it 'adds a dish to the order list' do
+      order.add_dish("Green Curry", 1)
+      expect(order.items).to eq [{ name: "Green Curry", description: "Chicken", price: 10 }]
+    end
+  end
+
+  describe '#remove_dish' do
+    it 'deletes dish from the order list' do
+      order.add_dish("Green Curry", 2)
+      order.remove_dish("Green Curry")
+      expect(order.items).to eq [{ name: "Green Curry", description: "Chicken", price: 10 }]
+    end
+  end
+
+  describe '#order_total' do
+    it 'gives the order total' do
+      order.add_dish("Green Curry", 1)
+      expect(order.order_total).to eq 10
     end
   end
 
