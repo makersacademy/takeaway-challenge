dbe388b6b37fac33d6bba8f0a250be53c91b8843
Thomas Ikimi <themerc01@gmail.com>
2017-09-11 02:44:19 +0100

    final

---
+++ spec/menu_spec.rb
@@ -0,0 +1,26 @@
+# require 'menu'
+# require 'pry'
+# require 'order'
+#
+# describe Menu do
+#
+#   it 'ORDER RICE' do
+#     allow(subject).to receive(:gets).and_return('1')
+#     expect(subject.main_menu).to eq true
+#   end
+#
+#   it 'ORDER CURRY' do
+#     allow(subject).to receive(:gets).and_return('2')
+#     expect(subject.main_menu).to eq true
+#   end
+#
+#   it 'ORDER CHIPS' do
+#     allow(subject).to receive(:gets).and_return('3')
+#     expect(subject.main_menu).to eq true
+#   end
+#
+#   it 'ORDER GARLIC BREAD' do
+#     allow(subject).to receive(:gets).and_return('4')
+#     expect(subject.main_menu).to eq true
+#   end
+# end
