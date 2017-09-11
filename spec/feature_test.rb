79086034e9935e0de09afcd969322d76b893d99d
Luan van Pletsen <luanvanpletsen@msn.com>
2017-09-11 07:56:34 +0100

    confirmation sms

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -4,12 +4,13 @@ require_relative '../lib/order'
 require 'pry'
 
 menu = Menu.new([['catnip', 5], ['CATnip', 10], ['CATNIP', 15]])
+messenger = Messenger.new
 
-puts "Welcome to The Catnip Emporium\nPlease take a look at our menu"
+puts "Welcome to The Catnip Emporium\nPlease take a look at our menu\n"
 
 menu.show
 
-puts "Make your selection by entering each number on a new line"
+puts "Make your selection by entering each number on a new line\n"
 
 selection = []
 input = 'default'
@@ -22,6 +23,7 @@ order = Order.new(selection, menu)
 # binding.pry
 order.view
 
-puts "enter 'yes' to confirm"
+puts "enter 'yes' to confirm\n"
 
-order.confirm if gets.chomp == 'yes'
+message = order.confirm if gets.chomp == 'yes'
+messenger.send_sms(message)
