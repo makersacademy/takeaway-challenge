5f5637340d9036993aeb87b43b0ad0d0bd379ef8
Jon Sanders <jonsanders101@aol.com>
2017-10-07 14:52:16 +0100

    Removed redudent test; refactored test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,12 +3,10 @@ require 'menu'
 describe Menu do
   subject(:menu) { described_class.new }
 
-  it 'stores menu items' do
-    expect(menu).to respond_to(:menu_items)
-  end
-
-  it 'returns menu_items in readable form' do
-    allow(menu).to receive(:menu_items) { [{item_num: 1, item: "Murgh Masala", cost: 6.9}] }
-    expect(menu.view_menu).to eq("Avo Spice Menu:\n1) Murgh Masala - 6.9\n")
+  describe "#view_menu" do
+    it 'returns menu_items in readable form' do
+      allow(menu).to receive(:menu_items) { [{item_num: 1, item: "Murgh Masala", cost: 6.9}] }
+      expect(menu.view_menu).to eq("Avo Spice Menu:\n1) Murgh Masala - 6.9\n")
+    end
   end
 end
