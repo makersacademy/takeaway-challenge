5732ac63a2b3ccbf63f886d1be5f543079c99378
Jon Sanders <jonsanders101@aol.com>
2017-10-07 11:41:20 +0100

    Added menu_items

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,4 @@
 class Menu
+attr_reader :menu_items
+  @menu_items = []
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,7 @@
 require 'menu'
 
 describe Menu do
-  
+  it 'stores menu items' do
+    expect(subject).to respond_to(:menu_items)
+  end
 end
