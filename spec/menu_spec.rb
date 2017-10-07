b1a89b42e2f5a91c766b8f5fe9337dc18cc14b40
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-07 17:42:30 +0100

    Added tests for #to_string and #select on the menu class and they now pass.

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,21 @@
 require 'menu'
 
 describe Menu do
-  
+  let(:order) { double(:order, items: 'Margherita, Meat Feast') }
+  describe '#select' do
+    it 'should respond to the select method' do
+      expect(subject).to respond_to :select
+    end
+
+    it 'should return the items ordered along with their prices' do
+      expect(subject.select(order)).to eq [{ name: 'Margherita', price: 5.5 }, { name: 'Meat Feast', price: 7.5 }]
+    end
+  end
+
+  describe '#to_string' do
+    let(:menu_string) { "Margherita, £5.50\nPepperoni, £6.00\nVeggie, £6.50\nHawaiian, £6.50\nMeat Feast, £7.50" }
+    it 'should return the menu in a readable format' do
+      expect(subject.to_string).to eq menu_string
+    end
+  end
 end
