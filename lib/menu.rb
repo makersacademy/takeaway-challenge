4eb861c48bc997181050a420e0c3b11a305712c2
Jon Sanders <jonsanders101@aol.com>
2017-10-07 12:15:08 +0100

    Added #view_menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,13 @@
 class Menu
-attr_reader :menu_items
+
+  attr_reader :menu_items
+
   @menu_items = [
     {item_num: 1, item: "Murgh Masala", cost: 6.9},
     {item_num: 2, item: "Avo Chick Moricha", cost: 7.25},
     {item_num: 3, item: "Tandori King Prawn Masala", cost: 8.5}
   ]
+
+  def view_menu
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,8 +2,13 @@ require 'menu'
 
 describe Menu do
   subject(:menu) { described_class.new }
-  
+
   it 'stores menu items' do
     expect(menu).to respond_to(:menu_items)
+  end
+
+  it 'prints menu_items in readable form' do
+    #allow(menu).to receive(:menu_items) { [{item_num: 1, item: "Murgh Masala", cost: 6.9}] }
+    expect(menu).to respond_to(:view_menu)
   end
 end
