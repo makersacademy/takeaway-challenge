b949f8a04447477c5cf263517b5ba6650d5bc427
Jon Sanders <jonsanders101@aol.com>
2017-10-07 16:44:00 +0100

    Added basket class & order instantiates with basket

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,13 @@
+require 'menu'
+
 class Order
+
+  attr_reader :order_menu, :order_basket
+
+  def initialize(menu = Menu.new, basket = Basket.new)
+    @order_menu = menu
+    @order_basket = basket
+  end
 
   def add_item
   end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,12 +1,21 @@
 require 'order'
 
 describe Order do
-  subject(:order) { described_class.new }
-  let(:basket) { double(basket) }
-  let(:menu) { double(menu)}
+  let(:menu) { double(:menu) }
+  let(:basket) { double(:basket) }
+  subject(:order) { described_class.new(menu, basket) }
+
 
   it "#add_item" do
     expect(order).to respond_to(:add_item)
+  end
+
+  it "instantiates with a menu" do
+    expect(order.order_menu).to eq(menu)
+  end
+
+  it "instantiates with a basket" do
+    expect(order.order_basket).to eq(basket)
   end
 
 end
