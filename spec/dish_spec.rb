b61df95aa554fc265f6a5f9820f897e312402f89
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-07 10:03:21 +0100

    Refactored tests.

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -4,17 +4,22 @@ describe Dish do
 
   subject(:dish) { described_class.new(name: 'Test Dish 1', description: 'Test description.', price: 10.95) }
 
-  it { is_expected.to respond_to(:to_s) }
+  describe 'initialization' do
+    it 'creates a Dish with a name, description, and price' do
+      expect(subject).to be_an_instance_of Dish
+    end
 
-  it { is_expected.to respond_to(:summary_s) }
+    it { is_expected.to respond_to(:name) }
 
-  it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:description) }
 
-  it { is_expected.to respond_to(:description) }
+    it { is_expected.to respond_to(:price) }
 
-  it { is_expected.to respond_to(:price) }
+  end
 
   context '#summary_s' do
+    it { is_expected.to respond_to(:summary_s) }
+
     it 'should return a custom summary string' do
       p dish.summary_s
       expect(dish.summary_s).to eq "#{dish.name}           #{dish.price}"
@@ -22,6 +27,9 @@ describe Dish do
   end
 
   context '#to_s' do
+
+    it { is_expected.to respond_to(:to_s) }
+
     it 'should return a custom formatted string' do
       p dish.to_s
       expect(dish.to_s).to eq "#{dish.name}     #{dish.description}           #{dish.price}"
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,7 +2,6 @@ require './lib/menu.rb'
 
 describe Menu do
 
-  it 'should initialize with an empty dishes array' do
 
-  end
+
 end
