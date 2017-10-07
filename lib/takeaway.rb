7736eed3a691425ffe787e98d9666842502d8843
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-07 15:56:49 +0100

    refactored list_dishes_prices method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,17 +1,18 @@
 class Takeaway
 
-  dishes = {
-  chicken_bhuna: 4.25,
-  pilau_rice: 2,
-  peshwari_naan: 1.75,
-  creamy_lentils: 3,
-  onion_bhaaji: 2.75
-  }
+  def dishes
+    @dishes = {
+    chicken_bhuna: 4.25,
+    pilau_rice: 2,
+    peshwari_naan: 1.75,
+    creamy_lentils: 3,
+    onion_bhaaji: 2.75
+    }
+  end
 
   def list_dishes_prices(dishes)
-    list = dishes.map do |key, value|
+    dishes.map do |key, value|
      "#{key}, #{value}"
     end
-    list
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,13 +4,7 @@ describe Takeaway do
   subject(:takeaway) { Takeaway.new }
 
   it 'allows you to see a list of dishes with prices' do
-    dishes = {
-    chicken_bhuna: 4.25,
-    pilau_rice: 2,
-    peshwari_naan: 1.75,
-    creamy_lentils: 3,
-    onion_bhaaji: 2.75
-    }
+    dishes = takeaway.dishes
     expect(takeaway.list_dishes_prices(dishes)).to eq ["chicken_bhuna, 4.25",
       "pilau_rice, 2",
       "peshwari_naan, 1.75",
