78d41fd5d3e2b25f12438bd9702edf8d45b5b50c
Jon Sanders <jonsanders101@aol.com>
2017-10-07 12:07:25 +0100

    Added sample menu items as hash

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,8 @@
 class Menu
 attr_reader :menu_items
-  @menu_items = []
+  @menu_items = [
+    {item_num: 1, item: "Murgh Masala", cost: 6.9},
+    {item_num: 2, item: "Avo Chick Moricha", cost: 7.25},
+    {item_num: 3, item: "Tandori King Prawn Masala", cost: 8.5}
+  ]
 end
