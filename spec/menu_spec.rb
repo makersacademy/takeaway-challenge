2c94c628be575e740127e228e2a28bd368ed810a
Canace Wong <canacewongtoday@hotmail.com>
2017-10-07 20:34:33 +0100

    menu

---
+++ spec/menu_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/menu'
+
+describe Menu do
+  subject(:list) { described_class.new }
+  fake_menu = {dumplings: 5, udon: 6, curry: 5, beer: 4}
+
+  it "lets customers view menu" do
+    expect(list).to respond_to(:view)
+  end
+
+  it "displays menu properly" do
+    allow(list).to receive(:menu).and_return(fake_menu)
+    view_menu = "dumplings, £5; udon, £6; curry, £5; beer, £4"
+    expect(list.view).to eq(view_menu)
+  end
+end
