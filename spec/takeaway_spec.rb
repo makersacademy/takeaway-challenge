83a74a4fda4598c0b85fae6d6d2b35cf6a23c4da
Tim Jones <timjones10@hotmail.com>
2017-10-07 13:05:15 +0100

    Menu added on initialisation

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,4 +2,10 @@ require 'takeaway'
 
 describe Takeaway do
 
+subject(:takeaway) {described_class.new}
+
+it 'has a menu on inititialisation' do
+expect(takeaway.menu).to be_instance_of(Hash)
+end
+
 end
