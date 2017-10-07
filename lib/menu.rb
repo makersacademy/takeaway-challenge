16bcae139b1a569201c5aae26fc146c59a4b70af
Jon Sanders <jonsanders101@aol.com>
2017-10-07 14:51:00 +0100

    Returns meny_items in readable form

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,12 +2,20 @@ class Menu
 
   attr_reader :menu_items
 
-  @menu_items = [
-    {item_num: 1, item: "Murgh Masala", cost: 6.9},
-    {item_num: 2, item: "Avo Chick Moricha", cost: 7.25},
-    {item_num: 3, item: "Tandori King Prawn Masala", cost: 8.5}
-  ]
+  def initialize
+    @menu_items = [
+      {item_num: 1, item: "Murgh Masala", cost: 6.9},
+      {item_num: 2, item: "Avo Chick Moricha", cost: 7.25},
+      {item_num: 3, item: "Tandori King Prawn Masala", cost: 8.5}
+    ]
+  end
 
   def view_menu
+    #"Menu:\n\n" +
+    readable_menu = "Avo Spice Menu:\n"
+    menu_items.each do |item|
+        readable_menu += "#{item[:item_num]}) #{item[:item]} - #{item[:cost]}\n"
+    end
+    readable_menu
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,8 +7,8 @@ describe Menu do
     expect(menu).to respond_to(:menu_items)
   end
 
-  it 'prints menu_items in readable form' do
-    #allow(menu).to receive(:menu_items) { [{item_num: 1, item: "Murgh Masala", cost: 6.9}] }
-    expect(menu).to respond_to(:view_menu)
+  it 'returns menu_items in readable form' do
+    allow(menu).to receive(:menu_items) { [{item_num: 1, item: "Murgh Masala", cost: 6.9}] }
+    expect(menu.view_menu).to eq("Avo Spice Menu:\n1) Murgh Masala - 6.9\n")
   end
 end
