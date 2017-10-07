1e66155227009bdf89756a418864dcfbce7ec2f6
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-07 09:10:58 +0100

    Refactored to run build method on instantiation

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,10 +4,7 @@ class Menu
   attr_reader :dishes
   DISHES = [['Tagliatelle with duck ragu', 9], ['Pizza Diavola', 10], ['Salad Caprese', 8.5]]
   def initialize
-    @dishes = []
-  end
-  def build
-    DISHES.each { | name_price | @dishes.push Dish.new name_price[0], name_price[1] }
+    @dishes = DISHES.map { | name_price | Dish.new name_price[0], name_price[1] }
   end
   def see
     menu_text = @dishes.map { |dish| dish.readable }.join("\n")
--- spec/units/menu_spec.rb
+++ spec/units/menu_spec.rb
@@ -2,28 +2,18 @@ require 'menu'
 
 describe Menu do
   describe '#initialize' do
-    it 'stores an empty array in a dishes instance variable' do
-      expect(subject.dishes).to eq []
-    end
-  end
-  describe '#build' do
     it 'adds a Dish object to the menu.dishes array' do
-      subject.build
       expect(subject.dishes[0]).to be_a Dish
     end
     it 'adds adds the right number of objects to the menu.dishes array' do
-      subject.build
       expect(subject.dishes.length).to eq Menu::DISHES.length
     end
   end
-  context 'with dishes array built' do
-    before(:each) {subject.build}
-    describe '#see' do
-      it 'enables users to see everything on the menu' do
-        menu_text = subject.see
-        Menu::DISHES.each do |dish_array|
-          expect(menu_text).to include dish_array[0]
-        end
+  describe '#see' do
+    it 'enables users to see everything on the menu' do
+      menu_text = subject.see
+      Menu::DISHES.each do |dish_array|
+        expect(menu_text).to include dish_array[0]
       end
     end
   end
