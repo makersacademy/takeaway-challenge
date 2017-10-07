d671cc85e4a027714e5e2576ae1e4c3723f7310f
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-07 10:07:43 +0100

    Added Menu initialization tests and implementation.

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,6 +2,19 @@ require './lib/menu.rb'
 
 describe Menu do
 
+  subject(:menu1) { described_class.new(dishes: []) }
 
+  describe 'initialization' do
+    it 'creates a Menu with a empty dishes array' do
+      expect(subject).to be_an_instance_of Menu
+    end
+
+    # it 'should initialize with an empty dishes array' do
+    #
+    # end
+
+    it { is_expected.to respond_to(:dishes) }
+
+  end
 
 end
