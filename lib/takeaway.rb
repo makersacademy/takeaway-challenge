f6b8f00c10d1919fd4e46030edbb27f31b7ed0b4
Ainsley <ab.chang@me.com>
2017-10-07 19:30:13 +0100

    added methods for order, order_summary, order_total & checkout_order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,46 @@
+require_relative './menu.rb'
+
 class Takeaway
 
-  def menu 
+  attr_reader :menu, :basket
+
+  def initialize(menu = Menu.new)
+
+    @menu = menu
+    @basket = []
+    @total = 0
 
   end
 
+  def order_total
+    "Total = £#{@total}"
+  end
 
+  def print_menu
+    menu.print
+  end
+
+  def order(dish, quantity = 1)    
+    fail 'item is not on the menu' unless menu.items[dish]
+    quantity.times do
+      @basket << [quantity, dish, menu.items[dish]] 
+      @total += menu.items[dish]
+    end
+    "#{quantity} #{dish} added to basket"
+  end
+
+  def order_summary
+    to_return = []
+    @basket.each do |item|
+      to_return << "#{item[0].to_s} #{item[1]} = #{item[0]*item[2]}"
+    end
+    to_return.join(', ')
+  end
+
+  def checkout_order(payment)
+    fail 'please pay the correct amount' unless payment == @total
+    @basket, @total = [], 0
+    "Thank you for your order"
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,15 +2,108 @@ require 'takeaway'
 
 describe Takeaway do
 
-  subject(:takeaway) { described_class.new }
+  let(:dish1) { 'wonton' }
+  let(:dish1_price) { 3.46 }
+  let(:dish2) { 'seaweed' }
+  let(:dish2_price) { 3.57 }
+  let(:menu) { double('menu',print: nil,items: { dish1 => dish1_price, dish2 => dish2_price }) }
+
+  subject(:takeaway) { described_class.new(menu) }
 
   describe '#initialize' do
 
-    it 'shows me the menu' do
-      expect(takeaway).to respond_to :menu
+    it 'has a default menu' do
+      expect(takeaway.menu).to eq menu
+    end
+    it 'allows a different menu to be used' do
+      expect(Takeaway.new(menu).menu).to eq menu
     end
 
   end
 
+  describe '#print_menu' do
+
+    it 'prints the menu' do
+      expect(takeaway.menu).to receive :print
+      takeaway.print_menu
+    end
+
+  end
+
+  describe '#order_summary' do
+
+    it 'is empty by default' do
+      expect(takeaway.order_summary).to be_empty
+    end
+    it 'shows what is currently in the basket' do
+      takeaway.order  dish1
+      expect(takeaway.order_summary).to eq "1 #{dish1} = #{dish1_price}"
+    end
+
+  end
+
+  describe '#order_total' do
+
+    it 'has an order total of 0 by default' do
+      expect(takeaway.order_total).to eq "Total = £0"
+    end
+    it 'shows the total cost of all items in the basket' do
+      takeaway.order  dish1
+      takeaway.order  dish2
+      expect(takeaway.order_total).to eq "Total = £#{dish1_price+dish2_price}"
+    end
+
+  end
+
+  describe '#order' do
+
+    it 'adds an item to the basket' do
+      takeaway.order  dish1
+      expect(takeaway.basket).to include ([1, dish1, dish1_price])
+    end
+    it 'adds the cost of the item to the total' do
+      takeaway.order  dish1
+      expect(takeaway.order_total).to eq "Total = £#{dish1_price}"
+    end
+    it 'allows a quantity to be specified' do
+      takeaway.order( dish1, 2)
+      expect(takeaway.basket.count).to eq 2
+    end
+    it 'adds more than one item to the basket' do
+      takeaway.order  dish1
+      takeaway.order  dish2
+      expect(takeaway.basket.count).to eq 2
+    end
+    it "will raise error if item isn't on the menu" do
+      expect{ takeaway.order 'fish sauce' }.to raise_error 'item is not on the menu'
+    end
+    it 'confirms that the items have been added to the basket' do
+      expect(takeaway.order  dish1).to eq "1 #{dish1} added to basket"
+    end
+
+  end
+
+  describe '#checkout' do
+
+    it "raises an error if the wrong payment is received" do
+      takeaway.order  dish1
+      expect{ takeaway.checkout_order(dish1_price + 1) }.to raise_error 'please pay the correct amount'
+    end
+    it "confirms the user's order" do
+      takeaway.order  dish1
+      expect(takeaway.checkout_order(dish1_price)).to eq "Thank you for your order"
+    end
+    it "empties the basket" do
+      takeaway.order  dish1
+      takeaway.checkout_order(dish1_price)
+      expect(takeaway.order_summary).to be_empty
+    end
+    it "sets the total back to 0" do
+      takeaway.order  dish1
+      takeaway.checkout_order(dish1_price)
+      expect(takeaway.order_total).to eq "Total = £0"
+    end
+
+  end
 
 end
