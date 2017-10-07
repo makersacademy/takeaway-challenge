51d74c17ee70d3fc1d9c2d95c832ebb535da702f
David Halewood <davidnhalewood@hotmail.com>
2017-10-07 21:23:38 +0100

    adding feature tests

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -11,4 +11,12 @@ describe 'takeaway' do
     eat.order('sushi')
     expect(eat.basket).to eq 'sushi' => 5.99
   end
+  it 'lets you empty your basket if you made a mistake' do
+    eat.empty_basket
+    expect(eat.basket).to be_empty
+  end
+  it 'lets you add multiples of the same dish to your basket' do
+    eat.order('pie', 3)
+    expect(eat.basket).to eq 'pie' => menu.dishes['pie']*3
+  end
 end
