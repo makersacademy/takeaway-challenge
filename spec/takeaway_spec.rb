95c47d54a7cdc66719a7cf57c05b6f3ac2c0697b
Lucas Salmins <lucasasalmins@gmail.com>
2017-10-07 11:37:03 +0100

    removed respond_to test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,8 +3,6 @@ require 'takeaway'
 describe Takeaway do
   subject(:takeaway) { described_class.new }
 
-  it { is_expected.to respond_to :menu }
-
   it 'is initialsed with a meals list' do
     expect(takeaway.menu).to be_empty
   end
