5c4813d5a2ed378ee59fc1b57daec3b45890d35a
Lucas Salmins <lucasasalmins@gmail.com>
2017-10-07 12:21:44 +0100

    added add_item method to dishes class

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -1,7 +1,13 @@
 class Dishes
 
-  def list
-    {}
+  attr_reader :list
+
+  def initialize
+    @list = []
+  end
+
+  def add_item(name, price)
+    @list << { name: name, price: price }
   end
 
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -2,10 +2,18 @@ require 'dishes'
 
 describe Dishes do
   subject(:dishes) { described_class.new }
-
-  it { is_expected.to respond_to :list }
+  let(:name) { double(:name) }
+  let(:price) { double(:price) }
 
   it 'is initialsed with a menu' do
-    expect(dishes.list).to eq({})
+    expect(dishes.list).to eq([])
   end
+
+  it 'adds dishes' do
+    dishes.add_item(name, price)
+    expect(dishes.list).to include({ name: name, price: price })
+  end
+
+  # expect(:a => 1, :b => 2).to include(:a)
+
 end
