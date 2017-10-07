7ad9d2108c96174d4d9ece1c4a1d8ba067583bc6
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-07 09:42:43 +0100

    Enabled users to select dishes from the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,27 @@
 require 'dish'
 
 class Menu
-  attr_reader :dishes
+  attr_reader :dishes, :selection
   DISHES = [['Tagliatelle with duck ragu', 9], ['Pizza Diavola', 10], ['Salad Caprese', 8.5]]
   def initialize
     @dishes = DISHES.map { | name_price | Dish.new name_price[0], name_price[1] }
+    @selection = []
   end
   def see
-    menu_text = @dishes.map { |dish| dish.readable }.join("\n")
+    menu_text = @dishes.map(&:readable).join("\n")
     puts menu_text
     menu_text
+  end
+  def select dish_name
+    dish_found = false
+    dishes.each do |dish|
+      if dish_name == dish.name
+        selection << dish
+        dish_found = true
+      end
+    end
+    if !dish_found
+      'Sorry, that is not available'
+    end
   end
 end
--- spec/units/menu_spec.rb
+++ spec/units/menu_spec.rb
@@ -2,6 +2,9 @@ require 'menu'
 
 describe Menu do
   describe '#initialize' do
+    it 'creates an empty array called selection' do
+      expect(subject.selection).to eq []
+    end
     it 'adds a Dish object to the menu.dishes array' do
       expect(subject.dishes[0]).to be_a Dish
     end
@@ -15,6 +18,15 @@ describe Menu do
       Menu::DISHES.each do |dish_array|
         expect(menu_text).to include dish_array[0]
       end
+    end
+  end
+  describe '#select' do
+    it 'enables users to choose an item on the menu and save it to the selection array' do
+      subject.select 'Pizza Diavola'
+      expect(subject.selection[0].name).to eq 'Pizza Diavola'
+    end
+    it 'tells a user if they are trying to order something that is not available' do
+      expect(subject.select 'Pizza Napolitana').to eq 'Sorry, that is not available'
     end
   end
 end
