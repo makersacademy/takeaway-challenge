bc51e86e9e357407d993409858f64c96fe459602
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-07 08:46:18 +0100

    Added Dish :description tests and implementation.

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,13 +1,14 @@
 class Dish
 
-  attr_reader :name, :price
+  attr_reader :name, :description, :price
 
-  def initialize(name, price)
-    @name = name
-    @price = price
+  def initialize(params = {})
+    @name = params[:name]
+    @description = params[:description]
+    @price = params[:price]
   end
 
   def to_s
-    "#{name}                #{price}"
+    "#{name}     #{description}           #{price}"
   end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,18 +2,20 @@ require './lib/dish.rb'
 
 describe Dish do
 
-  subject(:dish) { described_class.new('Test Dish 1', 10.95) }
+  subject(:dish) { described_class.new(name: 'Test Dish 1', description: 'Test description.', price: 10.95) }
 
   it { is_expected.to respond_to(:to_s) }
 
   it { is_expected.to respond_to(:name) }
+
+  it { is_expected.to respond_to(:description) }
 
   it { is_expected.to respond_to(:price) }
 
   context '#to_s' do
     it 'should return a custom formatted string' do
       p dish.to_s
-      expect(dish.to_s).to eq "#{dish.name}                #{dish.price}"
+      expect(dish.to_s).to eq "#{dish.name}     #{dish.description}           #{dish.price}"
     end
   end
 end
