c46ada874ac6c064739b1b7d40947c25503ecb52
Lucas Salmins <lucasasalmins@gmail.com>
2017-10-07 11:44:20 +0100

    updated README

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,8 +3,10 @@ require 'takeaway'
 describe Takeaway do
   subject(:takeaway) { described_class.new }
 
-  it 'is initialsed with a meals list' do
-    expect(takeaway.menu).to be_empty
+  describe "showing dishes" do
+    it 'is initialsed with a meals list' do
+      expect(takeaway.menu).to be_empty
+    end
   end
 
 end
