1e5414a2c692fd6fcd8b5cf333f7f4c58bf935e4
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-08 17:35:31 +0100

    Rewrote correct_total_price method so that it is accessible everywhere in class

--- lib/order.rb
+++ lib/order.rb
@@ -2,12 +2,20 @@ require 'basket'
 
 class Order
 
-  def place_order(items_and_quantities, total_price)
-    fail "The sum is not correct" unless correct_total_price?
+  attr_reader :total_price, :correct_total_price
+
+  def place_order
+    fail "The sum is not correct" unless @correct_total_price
+    # order = [items_and_quantities, total_price]
+    "Thanks! Your order is placed and will be delivered."
   end
 
   def correct_total_price?(total_price, prices_and_quantities_in_basket)
-    total_price == prices_and_quantities_in_basket.map {|k, v| k*v }.inject(:+)
+    if total_price == prices_and_quantities_in_basket.map {|k, v| k*v }.inject(:+)
+      @correct_total_price = true
+    else
+      @correct_total_price = false
+    end
       #this method effectively checks the total price was correctly calculated
       #by redoing it in a different manner
   end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,13 +1,16 @@
 require 'order'
 
 describe Order do
-
-  let(:basket) { double(:basket) }
-  let(:dishes) { double(:dishes) }
-
   it 'checks the price is correct' do
     prices_and_quantities_in_basket = ({4.25 => 2, 2 => 3})
     total_price = 14.5
     expect(subject.correct_total_price?(total_price, prices_and_quantities_in_basket)).to eq true
+  end
+
+  it 'allows you to place an order' do
+    prices_and_quantities_in_basket = ({4.25 => 2, 2 => 3})
+    total_price = 14.5
+    subject.correct_total_price?(total_price, prices_and_quantities_in_basket)
+    expect(subject.place_order).to eq "Thanks! Your order is placed and will be delivered."
   end
 end
