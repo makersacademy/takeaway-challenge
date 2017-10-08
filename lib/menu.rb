422339c1a4e222431318296d657302ebb818faf4
Jon Sanders <jonsanders101@aol.com>
2017-10-08 20:19:58 +0100

    Loads menu from CSV

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,19 +3,28 @@ class Menu
   attr_reader :menu_items
 
   def initialize
-    @menu_items = [
-      {item_num: 1, item: "Murgh Masala", cost: 6.9},
-      {item_num: 2, item: "Avo Chick Moricha", cost: 7.25},
-      {item_num: 3, item: "Tandori King Prawn Masala", cost: 8.5}
-    ]
+    @menu_items = []
+    load_menu
   end
 
   def view_menu
-    "Avo Spice Menu:\n" + menu_formatter
+    "Menu:\n" + menu_formatter
   end
 
   private
+
+  def load_menu
+    File.open(File.dirname(__FILE__) + '/../menu/menu.txt') do |file|
+      file.each do |line|
+        @menu_items << {item_num:  line.split(",")[0].to_i,
+                        item:       line.split(",")[1],
+                        cost:       line.split(",")[2].chomp.to_f}
+      end
+    end
+  end
+
   def menu_formatter
     menu_items.map {|item| "#{item[:item_num]}) #{item[:item]} - £#{'%.2f' % item[:cost]}\n"}.join.chomp
   end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,12 +1,15 @@
 require 'menu'
 
 describe Menu do
+
   subject(:menu) { described_class.new }
 
   describe "#view_menu" do
+
     it 'returns menu_items in readable form' do
       allow(menu).to receive(:menu_items) { [{item_num: 1, item: "Murgh Masala", cost: 6.9}] }
-      expect(menu.view_menu).to eq("Avo Spice Menu:\n1) Murgh Masala - £6.90")
+      expect(menu.view_menu).to eq("Menu:\n1) Murgh Masala - £6.90")
     end
+
   end
 end
