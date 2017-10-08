2d6b35345438e80928e6129093e8e0f34eeff320
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-10-08 17:18:28 +0100

    added test for quantity

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -22,14 +22,16 @@ describe Order do
     it 'should be initialised with an empty basket' do
       expect(order.basket).to be_empty
     end
-
     it 'customer should be able to add items to the basket' do
       order.add('Pasta', 1)
       expect(order.basket).to eq ['Pasta']
     end
-
     it 'raises an error if customer adds unknown food' do
       expect{ order.add('fish', 1) }.to raise_error 'This is not on the menu!'
+    end
+    it 'customer can add more than one of each dish' do
+      order.add('Salad', 2)
+      expect(order.basket).to eq ['Salad', 'Salad']
     end
   end
 
