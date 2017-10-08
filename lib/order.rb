9291074a564cdb6dac5c1611c0e91f1d2fa35915
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-08 17:53:22 +0100

    Added preliminary functionality that allows time to be added to the test message

--- lib/order.rb
+++ lib/order.rb
@@ -4,10 +4,10 @@ class Order
 
   attr_reader :total_price, :correct_total_price, :meal_order
 
-  def place_order(items_and_quantities, total_price)
+  def place_order(items_and_quantities, total_price, time_ordered)
     fail "The sum is not correct" unless @correct_total_price
     @meal_order = [items_and_quantities, total_price]
-    "Thanks! Your order is placed and will be delivered."
+    "Thanks! Your order is placed and will be delivered at #{time_ordered + 1}."
   end
 
   def correct_total_price?(total_price, prices_and_quantities_in_basket)
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -11,16 +11,18 @@ describe Order do
     prices_and_quantities_in_basket = ({4.25 => 2, 2 => 3})
     items_and_quantities = ({chicken_bhuna: 2, pilau_rice: 1})
     total_price = 14.5
+    time_ordered = 18
     subject.correct_total_price?(total_price, prices_and_quantities_in_basket)
-    expect(subject.place_order(items_and_quantities, total_price)).to eq "Thanks! Your order is placed and will be delivered."
+    expect(subject.place_order(items_and_quantities, total_price, time_ordered)).to eq "Thanks! Your order is placed and will be delivered at 19."
   end
 
   it 'correctly records the meal order' do
     prices_and_quantities_in_basket = ({4.25 => 2, 2 => 3})
     items_and_quantities = ({chicken_bhuna: 2, pilau_rice: 1})
     total_price = 14.5
+    time_ordered = 18
     subject.correct_total_price?(total_price, prices_and_quantities_in_basket)
-    subject.place_order(items_and_quantities, total_price)
+    subject.place_order(items_and_quantities, total_price, time_ordered)
     expect(subject.meal_order).to eq ([{chicken_bhuna: 2, pilau_rice: 1}, 14.5])
   end
 end
