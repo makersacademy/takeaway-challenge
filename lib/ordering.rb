e2fdf5d7f112c2a909cc97ccd24bbe2b46f7eed2
Olivia <oliviaberesford@hotmail.co.uk>
2017-10-08 12:57:56 +0100

    Adding and displaying quantities

--- lib/ordering.rb
+++ lib/ordering.rb
@@ -2,16 +2,21 @@ require_relative 'menu'
 
 class Ordering
 
-  attr_reader :menu_items, :selected_dishes, :view_menu
+  attr_reader :selected_dishes
 
   def initialize(menu = Menu.new)
     @menu = menu
     @selected_dishes = []
   end
 
-  def selection(items)
+  def selection(items, quantity = 1)
     fail "Sorry that item isn't on the menu! Please choose from the menu." unless @menu.menu_items.include?(items)
-    @selected_dishes << items
+    quantity.times { @selected_dishes << items }
+  end
+
+  def view_dishes
+    print selected_dishes
+    p"You have ordered #{selected_dishes.count} dishes."
   end
 
 end
--- spec/ordering_spec.rb
+++ spec/ordering_spec.rb
@@ -3,6 +3,8 @@ require 'menu'
 
 describe Ordering do
   subject(:order) { described_class.new }
+  let(:selection) { double :selection }
+  let(:selected_dishes) { double :selected_dishes }
 
   it "doesn't allow the customer to order something not on the menu" do
     expect { order.selection('not on menu') }.to raise_error "Sorry that item isn't on the menu! Please choose from the menu."
@@ -11,6 +13,16 @@ describe Ordering do
   it "allows the customer to select a dish on the menu" do
     order.selection('classic margherita')
     expect(order.selected_dishes).to include 'classic margherita'
+  end
+
+  it "allows the customer to select several dishes from the menu" do
+    order.selection('classic margherita', 2)
+    expect(order.selected_dishes).to include "classic margherita", "classic margherita"
+  end
+
+  it "allows the customer to check how many dishes they have ordered" do
+    order.selection('pepperoni', 4)
+    expect(order.view_dishes).to eq "You have ordered 4 dishes."
   end
 
 end
