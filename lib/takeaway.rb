1aaed9b247e5f7aae3a1dccf0ee0a8ae8d508b39
RMCollins175 <rorycollins@me.com>
2017-10-08 12:03:01 +0100

    place-order method with some notes on next stages

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,6 @@
 class Takeaway
 
-  attr_reader :menu, :basket
+  attr_reader :menu, :basket, :total
 
   def initialize
     @menu = Dishes.new
@@ -12,9 +12,20 @@ class Takeaway
   end
 
   def select_dish(dish)
+    @basket[dish] = @menu.list[dish]
+  end
 
+  def total
+    # @total = @list.inject(0) { |key, value| key += value[1] }
+    @total = @menu.list.values.sum
   end
 
 
+  def place_order#(order_total = gets.chomp)
+    # puts "please confirm your order total"
+    # order_total
+    # raise "total is not correct, unable to secure order" if order_total.to_s != @total
+    "Thank you! Your order with a total of #{total} has been confirmed"
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,6 +7,9 @@ describe Takeaway do
     "xcurry" => 1.00 }
   end
 
+  subject(:total) { takeaway.basket.values.sum }
+
+
   it 'has a list of dishes' do
     expect(takeaway.menu).to be_an_instance_of(Dishes)
   end
@@ -19,17 +22,44 @@ describe Takeaway do
     expect(takeaway).to respond_to(:select_dish).with(1).argument
   end
 
+  it 'selects from the menu' do
+    takeaway.select_dish("curry")
+    expect(takeaway.basket).to include "curry"
+  end
+
   it 'starts with an empty basket' do
     # expect(takeaway.basket).to be_empty
     expect(takeaway.basket).to be_an_instance_of Hash
   end
 
-  
+  # it "adds up the total of one dish " do
+  #   takeaway.select_dish("curry")
+  #   expect(takeaway.total).to eq 1
+  # end
+
+  it "adds up the total of all dishes" do
+    takeaway.select_dish("curry")
+    takeaway.select_dish("xcurry")
+    expect(takeaway.total).to eq total
+  end
 
 
+  it 'places and order' do
+    takeaway.select_dish("curry")
+    takeaway.select_dish("xcurry")
+    expect(takeaway.total).to eq total
+    # total = takeaway.basket.values.sum
+    expect(takeaway.place_order).to eq "Thank you! Your order with a total of #{total} has been confirmed"
+  end
 
+  # it 'raises an error if customer total is not correct' do
+  # takeaway.select_dish("curry")
+  # takeaway.select_dish("xcurry")
+  # total = takeaway.basket.values.sum
+  # expect {takeaway.place_order}.to raise_error
+  # end
 
-
+  # it
 
 
 end
