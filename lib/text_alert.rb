54f4e1fb0cadf6c205021f4f56a4004a811bc4fb
Jon Sanders <jonsanders101@aol.com>
2017-10-08 18:07:00 +0100

    text alert added

--- lib/text_alert.rb
+++ lib/text_alert.rb
@@ -7,7 +7,6 @@ class TextAlert
     @account_sid = 'ACc432bc414067fc1d92cc2b94155eaab9'
     @auth_token = '42a694dd82361d5d0d88fbec21dbe8dd'
     @client = Twilio::REST::Client.new account_sid, auth_token
-
     @from = '+441438301049'
     @customer_number = '+447947731175'
   end
@@ -18,7 +17,6 @@ class TextAlert
       to: @customer_number,
       body: "Thank you! Your order was placed and will be delivered before #{(Time.new + 3600).strftime("%-I:%M%P")}."
     )
-    puts "Sent message to customer"
   end
 
 end
--- spec/text_alert_spec.rb
+++ spec/text_alert_spec.rb
@@ -1,5 +1,5 @@
 require 'text_alert'
 
 describe TextAlert do
-  
+  # No tests for external API
 end
