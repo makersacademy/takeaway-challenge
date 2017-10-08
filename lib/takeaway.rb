e5d89fd834e7fed6cfcea94766f60b1bb886a088
Alex <alex@scotttonge.com>
2017-10-08 16:37:38 +0100

    Unit test for printing order summary

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,10 +2,11 @@ require_relative 'menu'
 
 class Takeaway
 
-  attr_reader :menu
+  attr_reader :menu, :order_subtotal
 
   def initialize
     @menu = Menu.new
+    @order_subtotal = {}
   end
 
   def read_menu
@@ -13,9 +14,20 @@ class Takeaway
   end
 
   def order(menu_item)
-    @menu.dishes.each_key do |k|
-      return k if k == menu_item
+    @menu.dishes.each_pair do |k, v|
+      if k == menu_item
+        order_subtotal.store(k, v)
+      end
     end
   end
 
+  def order_summary
+    puts order_subtotal
+  end
+
 end
+
+# t = Takeaway.new
+# t.order('Rock Cakes')
+# t.order('Cockroach Clusters')
+# p t.order_subtotal
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,12 +4,27 @@ describe Takeaway do
 
   subject(:takeaway) { Takeaway.new }
 
-  it "shows a list of dishes" do
-    expect(takeaway.read_menu).to eq takeaway.read_menu
+  let(:dish) { double :dish }
+  let(:price) { double :price }
+
+  let(:order_subtotal) { { dish: dish, price: price } }
+
+  describe '#initialize' do
+    it "shows a list of dishes" do
+      expect(takeaway.read_menu).to eq takeaway.read_menu
+    end
   end
 
-  it "can select several dishes" do
-    expect(takeaway).to respond_to(:order).with(1).argument
+  describe '#order' do
+    it "can select several dishes" do
+      expect(takeaway).to respond_to(:order).with(1).argument
+    end
+  end
+
+  describe '#order_summary' do
+    it "prints out an order summary" do
+      expect(takeaway.order_summary).to eq takeaway.order_summary
+    end
   end
 
 end
