a2d3135a3dab1c520391542e4fef3e9dec64afd3
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-08 12:16:20 +0100

    Added instructions on how to use into Readme

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,9 +1,9 @@
 require 'fooderoo'
 
 describe 'takeaway app' do
-  # fooderoo = Fooderoo.new
-  # fooderoo.show_menu
-  # fooderoo.select 'Pizza Diavola', 2
-  # fooderoo.select 'Tagliatelle with duck ragu', 8
-  # fooderoo.check_order 92
+  fooderoo = Fooderoo.new
+  fooderoo.show_menu
+  fooderoo.select 'Pizza Diavola', 2
+  fooderoo.select 'Tagliatelle with duck ragu', 8
+  # puts fooderoo.check_order 92 ### Uncommenting this line will send text messages...
 end
