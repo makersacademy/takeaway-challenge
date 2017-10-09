cc7e599f3291f5f715bef0e2ec4f6ac835175ac6
RMCollins175 <rorycollins@me.com>
2017-10-09 09:14:58 +0100

    changes to twilio file, single responsibility

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -1,9 +1,12 @@
 require 'twilio-ruby'
 
+
+
+
 class Message
 
-
   def time
+    @time = time
     time = Time.now
     delivery_time = (time + 45*60).strftime("%R")
   end
@@ -14,13 +17,12 @@ class Message
     account_sid = 'ACdee469cd997b8b892665ed9ae3c94c07'
     auth_token = '64da49b1aacfc64a21ff278e50482e64'
 
-
     # time = Time.now
     # delivery_time = (time + 45*60).strftime("%R")
 
     @client = Twilio::REST::Client.new(account_sid, auth_token)
     message = @client.messages.create(
-      body: "Thank you!, Your order has been placed and should be with you by #{time}",  #{delivery_time}
+      body: "Thank you!, Your order has been placed and should be with you by #{@time}",
       from: '+441293311593',
       to: '+447921122767'
     )
