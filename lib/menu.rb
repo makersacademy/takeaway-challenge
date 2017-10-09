49c1063d37c442c41d95e5e26db575168d069a13
David Halewood <davidnhalewood@hotmail.com>
2017-10-09 11:32:03 +0100

    injecting stream to menu for puts

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,24 @@
 class Menu
   attr_reader :dishes
 
-  def initialize
-    @dishes = { 'sushi' => 5.99,
-      'pizza' => 7.99, 'burger' => 4.99,
-      'pie' => 15.99, 'water' => 2.99,
+  #option to inject the puts method as you can stub it then
+  def initialize (stream: $stdout) #keyword argument - when you come to call you use stream:
+
+    @stream = stream
+
+    @dishes = {
+      'sushi' => 5.99,
+      'pizza' => 7.99,
+      'burger' => 4.99,
+      'pie' => 15.99,
+      'water' => 2.99,
       'beer' => 3.99
     }
   end
 
   def show
-    puts dishes.map { |dish| "#{dish[0]}: £#{dish[1]}" }.join("\n")
+    @stream.puts dishes.map { |dish| "#{dish[0]}: £#{dish[1]}" }.join("\n")
+    #puts dishes.map { |dish| "#{dish[0]}: £#{dish[1]}" }.join("\n")
   end
 
   def on_menu?(dish)
