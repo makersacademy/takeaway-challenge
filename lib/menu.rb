4055c7e911c3d05c7e0695a3f5c70327d8ee4929
David Halewood <davidnhalewood@hotmail.com>
2017-10-09 11:32:53 +0100

    refactoring

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,8 @@
 class Menu
   attr_reader :dishes
 
-  #option to inject the puts method as you can stub it then
-  def initialize (stream: $stdout) #keyword argument - when you come to call you use stream:
-
+  def initialize(stream: $stdout)
     @stream = stream
-
     @dishes = {
       'sushi' => 5.99,
       'pizza' => 7.99,
@@ -18,7 +15,6 @@ class Menu
 
   def show
     @stream.puts dishes.map { |dish| "#{dish[0]}: £#{dish[1]}" }.join("\n")
-    #puts dishes.map { |dish| "#{dish[0]}: £#{dish[1]}" }.join("\n")
   end
 
   def on_menu?(dish)
