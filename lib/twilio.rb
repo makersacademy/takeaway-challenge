42e7bf492ee12e96da0728a339479eb227c816f0
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-09 11:56:21 +0100

    Made the twilio authorisation key into an environment variable so it's unaccessible on github

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -1,8 +1,8 @@
 require 'twilio-ruby'
 class Text
   def send_text(message)
-    account_sid = "ACf1090abe282db7bbe645870d3b9ae23f"
-    auth_token = "f5b1cffa4808785e57d39cbbb630a611" # Your Auth Token from www.twilio.com/console
+    account_sid = ENV['TWILIOSID']
+    auth_token = ENV['TWILIOAUTH'] # Your Auth Token from www.twilio.com/console
 
     @client = Twilio::REST::Client.new account_sid, auth_token
     @client.messages.create(
