04e021466cc7967a8946831076800a2cb1c6a57b
Jon Sanders <jonsanders101@aol.com>
2017-10-09 10:44:13 +0100

    Commented out private details

--- lib/text_alert.rb
+++ lib/text_alert.rb
@@ -4,19 +4,11 @@ require 'twilio-ruby'
 class TextAlert
 
   def initialize
-    # @account_sid = # ADD ACCOUNT SID HERE
-    # @auth_token = # ADD AUTH TOKEN HERE
-    # @client = Twilio::REST::Client.new @account_sid, @auth_token
-    #
-    # @from = # ADD TWILIO NUMBER HERE
-    # @customer_number = # ADD CUSTOMER NUMBER HERE
-
-    @account_sid = 'ACc432bc414067fc1d92cc2b94155eaab9'
-    @auth_token = '42a694dd82361d5d0d88fbec21dbe8dd'
+    @account_sid = # ADD ACCOUNT SID HERE
+    @auth_token = # ADD AUTH TOKEN HERE
     @client = Twilio::REST::Client.new @account_sid, @auth_token
-
-    @from = '+441438301049'
-    @customer_number = '+447947731175'
+    @from = # ADD TWILIO NUMBER HERE
+    @customer_number = # ADD CUSTOMER NUMBER HERE
   end
 
   def send_confirmation
