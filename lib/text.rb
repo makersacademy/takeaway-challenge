71deb5ef7241ba5c1ff89ae8430cbd0872c2fc02
Allan Wazacz <cazwazacz@gmail.com>
2017-11-01 13:26:56 +0000

    Places Twilio auth credentials in file that is git ignored

--- lib/text.rb
+++ lib/text.rb
@@ -1,19 +1,20 @@
 require 'twilio-ruby'
+require './auth_credentials'
 
 class Text
   def send_text(total)
     
     hour_later = ((Time.now)+60*60).strftime('%H:%M')
     
-    account_sid = "***REMOVED***" # Your Account SID from www.twilio.com/console
+    account_sid = ACCOUNT_SID # Your Account SID from www.twilio.com/console
 
-    auth_token = "***REMOVED***"   # Your Auth Token from www.twilio.com/console
+    auth_token = AUTH_TOKEN   # Your Auth Token from www.twilio.com/console
 
     @client = Twilio::REST::Client.new account_sid, auth_token
     message = @client.messages.create(
         body: "Your order total is #{total} and you will receive it no later than #{hour_later}. Thank you!",
-        to: "***REMOVED***",    # Replace with your phone number
-        from: "***REMOVED***")  # Replace with your Twilio number
+        to: TO,    # Replace with your phone number
+        from: FROM)  # Replace with your Twilio number
 
     message.sid
   end
