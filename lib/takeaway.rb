828384d182dbdef3d094c4740cc22ba23b94e4d2
Eustaquio122 <tiagofsousa@gmail.com>
2017-11-03 14:27:38 +0000

    Adds Takeaway class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,30 +1,49 @@
 require_relative 'order'
 require_relative 'menu'
 
-
 class Takeaway
 
+  attr_reader :order, :menu, :order_history
+
   def initialize
-    @order = Order.new
     @menu = Menu.new
+    @order = Order.new(@menu)
+    @order_history = []
   end
 
   def show_menu
+    @menu.show
   end
 
-  def add_dish
+  def add_dish(number)
+    @order.add_dish(number)
+  end
+
+  def remove_dish(number)
+    @order.remove_dish(number)
   end
 
   def show_order
+    @order.show
   end
 
-  def finish_order
-  end
-
-  def cancel_order
+  def reset_order
+    @order = Order.new
   end
 
   def confirm_order
+    send_text
+    finish_order
+  end
+
+private
+
+  def finish_order
+    @order_history << @order
+    reset_order
+  end
+
+  def send_text
   end
 
 end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,50 @@
+require 'takeaway'
+
+describe Takeaway do
+
+  subject(:takeaway) { Takeaway.new }
+
+  let(:menu) { double('menu') }
+  let(:order) { double('order') }
+  let(:dish) { double('dish') }
+
+  context 'Initialization' do
+      it 'initializes an empty @order_history array' do
+        expect(takeaway.order_history).to eq []
+      end
+  end
+
+  context 'Menu interaction methods' do
+    it 'Requests Menu to print itself' do
+      allow(takeaway.menu).to receive(:show).and_return('Menu Details')
+      expect(takeaway.show_menu).to eq 'Menu Details'
+    end
+  end
+
+  context 'Order interaction methods' do
+    before do
+      allow(takeaway.order).to receive_messages(add_dish: 'Dish Added',
+        remove_dish: 'Dish Removed', show: 'Order Details')
+    end
+
+    it 'Requests Order to add a dish' do
+      expect(takeaway.add_dish(1)).to eq 'Dish Added'
+    end
+
+    it 'Requests Order to remove a dish' do
+      expect(takeaway.remove_dish(1)).to eq 'Dish Removed'
+    end
+
+    it 'Requests Order to print its detials' do
+      expect(takeaway.show_order).to eq 'Order Details'
+    end
+
+    it 'adds order to order_history array' do
+      takeaway.confirm_order
+      expect(takeaway.order_history).not_to be_empty
+    end
+  end
+
+
+
+end
