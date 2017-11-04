b5721de601ad1af5fd354860564562938adcaa8e
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-04 16:18:47 +0000

    Dishes added allergy info and refactored

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,10 +1,11 @@
 require 'dish'
 describe Dish do
-  subject(:dish) { described_class.new(name, price) }
+  subject(:dish) { described_class.new(name, price, allergy_info) }
+  let(:name) { "spring rolls" }
+  let(:price) { 3.99 }
+  let(:allergy_info) {"No specifications regarding allergies"}
   describe '#name' do
     context 'name of the dish'
-    let(:name) { "spring rolls" }
-    let(:price) { 3.99 }
     it 'returns name' do
       # dish = Dish.new("spring rolls")
       expect(dish.name).to eq 'spring rolls'
@@ -12,17 +13,18 @@ describe Dish do
   end
   describe '#price' do
     context 'price of the dish'
-    let(:name) { "spring rolls" }
-    let(:price) { 3.99 }
     it 'returns price' do
       expect(dish.price).to eq 3.99
     end
   end
   describe '#dish_info' do
-  let(:name) { "spring rolls" }
-  let(:price) { 3.99 }
-  it 'should show me the dishes price and name in a hash' do
-    expect(dish.dish_info).to eq ({"spring rolls" => 3.99})
+    it 'should show me the dishes price and name in a hash' do
+      expect(dish.dish_info).to eq ({"spring rolls" => 3.99})
+    end
   end
+  describe '#allergy_info' do
+    it 'should return allergy info' do
+      expect(dish.allergy_info).to eq "No specifications regarding allergies"
+    end
   end
 end
