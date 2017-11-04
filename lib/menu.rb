866b544c8f186f49f8787f556fe8344e395f4eca
Peter Johnstone <peter.wdj@gmail.com>
2017-11-04 21:50:38 +0000

    Added passing tests for first user story - printing a menu of dishes and prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,16 +7,25 @@ class Menu
     @dishes = []
   end
 
-  def load_menu menu_items
+  def print_menu
+    load_menu
+    @dishes.each do |item|
+      puts "#{@dishes.index(item) + 1}. #{item.fetch(:dish)}, £#{'%.2f' % item.fetch(:price).to_f}"
+    end
+  end
+
+  private
+
+  def add_dish dish, price
+    @dishes << { dish: dish, price: price }
+  end
+
+  def load_menu
     @menu = File.open("menu_items.csv", "r") do |f|
       CSV.foreach(f) do |line|
         dish, price = line[0], line[1]
         add_dish(dish, price)
       end
     end
-  end
-
-  def add_dish dish, price
-    @dishes << { dish: dish, price: price }
   end
 end
