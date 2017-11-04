6a3a5ae2b536e8b1d98f902cf2e33bd8e1ef188f
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-04 19:59:41 +0000

    adds helloworld test

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra/base'
 require 'twilio-ruby'
 
 class App < Sinatra::Base
-  get '/sms' do
+
+  get '/helloworld' do
+    'hello world'
+  end
+
+  post '/sms' do
     content_type 'text/xml'
     twiml = Twilio::TwiML::MessagingResponse.new do |response|
       response.message(body: 'hello there')
