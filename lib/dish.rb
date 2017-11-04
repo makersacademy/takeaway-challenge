2ff783893de02d041d801debb99b65f9ab245309
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-04 16:07:59 +0000

    Dishes now contain all necessary info

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,6 +1,8 @@
 class Dish
-  attr_reader :name
-  def initialize(name)
+  attr_reader :name, :price, :dish_info
+  def initialize(name, price)
     @name = name
+    @price = price
+    @dish_info = {name => price}
   end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,12 +1,28 @@
 require 'dish'
 describe Dish do
-  subject(:dish) { described_class.new(name) }
+  subject(:dish) { described_class.new(name, price) }
   describe '#name' do
     context 'name of the dish'
     let(:name) { "spring rolls" }
+    let(:price) { 3.99 }
     it 'returns name' do
       # dish = Dish.new("spring rolls")
       expect(dish.name).to eq 'spring rolls'
     end
+  end
+  describe '#price' do
+    context 'price of the dish'
+    let(:name) { "spring rolls" }
+    let(:price) { 3.99 }
+    it 'returns price' do
+      expect(dish.price).to eq 3.99
+    end
+  end
+  describe '#dish_info' do
+  let(:name) { "spring rolls" }
+  let(:price) { 3.99 }
+  it 'should show me the dishes price and name in a hash' do
+    expect(dish.dish_info).to eq ({"spring rolls" => 3.99})
+  end
   end
 end
