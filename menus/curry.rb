1641b6bf51ac6c71d4ad60791141ff164c48b18d
Tom Moir <trmoir9@gmail.com>
2017-11-04 15:32:21 +0000

    changed curry options to all be the same

--- menus/curry.rb
+++ menus/curry.rb
@@ -1,19 +1,3 @@
-def korma
-  {chicken: 5.95, king_prawn: 8.75, lamb:5.95, prawn:6.95, vegetable:5.55}
-end
-
 def curry
-  {chicken: 5.95, king_prawn: 8.75, lamb: 5.95, prawn: 6.95, vegetable: 5.55}
-end
-
-def madras
-  {chicken: 5.95, king_prawn: 8.75, lamb: 5.95, prawn: 6.95, vegetable: 5.55}
-end
-
-def rogan_josh
-  {chicken: 5.95, king_prawn: 8.75, lamb: 5.95, prawn: 6.95, vegetable: 5.55}
-end
-
-def bhuna
   {chicken: 5.95, king_prawn: 8.75, lamb: 5.95, prawn: 6.95, vegetable: 5.55}
 end
