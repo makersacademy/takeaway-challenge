f4637921b022ab8f9cdc393c41837638623a3523
antonio <antoniobelmardacosta@gmail.com>
2017-11-04 16:20:07 +0000

    Adds possibility to print menu from restaurant in Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,25 @@
+require_relative "takeaway"
+
 class Order
-  attr_reader :id
+  attr_reader :id, :takeaway, :total, :items
 
   @@orders = []
 
   def initialize
     @id = 1 + @@orders.count
     @@orders.push(self)
+    @total = 0
+    @items = []
   end
 
+  def print_restaurants(takeaway)
+    raise "There are no restaurants at this takeaway." if takeaway.restaurants.empty?
+    takeaway.print_restaurants
+  end
+
+  def print_menu(takeaway, restaurant)
+    raise "This takeaway doesn't contain this restaurant." unless takeaway.restaurants.include?(restaurant)
+    takeaway.print_menu(restaurant)
+  end
+  
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,8 +3,18 @@ require "order"
 describe Order do
 
   subject(:order) { described_class.new }
+  let(:takeaway) { double(:takeaway) }
+  let(:restaurant) { double(:restaurant) }
 
   describe "#initialize" do
+    it "ensures it gets initialized with a total =0" do
+      expect(subject.total).to eq(0)
+    end
+
+    it "ensures it gets initialized with no items" do
+      expect(subject.items).to eq([])
+    end
+
     it "ensures a new order gets initialized with an id number" do
       expect(subject.id).to be_kind_of(Numeric)
     end
@@ -21,5 +31,29 @@ describe Order do
     end
   end
 
-  
+  describe "#print_restaurants" do
+    it "calls method print_restaurants on takeaway instance when takeaway has restaurants" do
+      allow(takeaway).to receive(:restaurants).and_return([1])
+      allow(takeaway).to receive(:print_restaurants).and_return(1)
+      expect(subject.print_restaurants(takeaway)).to eq(1)
+    end
+    it "raises error calling print_restaurants on takeaway instance when takeaway has no restaurants" do
+      allow(takeaway).to receive(:restaurants).and_return([])
+      allow(takeaway).to receive(:print_restaurants).and_return(1)
+      expect { subject.print_restaurants(takeaway) }.to raise_error("There are no restaurants at this takeaway.")
+    end
+  end
+
+  describe "#print_menu(restaurant)" do
+    it "raises error calling print menu of a restaurant not in this takeaway" do
+      allow(takeaway).to receive(:restaurants).and_return([])
+      expect { subject.print_menu(takeaway, restaurant) }.to raise_error("This takeaway doesn't contain this restaurant.")
+    end
+    it "calls print menu on the takeaway given as argument" do
+      allow(takeaway).to receive(:restaurants).and_return([restaurant])
+      allow(takeaway).to receive(:print_menu).and_return(1)
+      expect(subject.print_menu(takeaway, restaurant)).to eq(1)
+    end
+  end
+
 end
