cd3fe408b999f82bf3b451cfd54fe23920d8ba72
cristhiandas <cristhiandf94@gmail.com>
2017-11-04 20:27:21 +0000

    It ask for and specific dish from order to dishes

--- lib/order.rb
+++ lib/order.rb
@@ -10,4 +10,7 @@ class Order
     @menu.print_menu
   end
 
+  def asks_for_dish(dish)
+    @menu.selecting_dish(dish)
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,4 +6,10 @@ describe Order do
       expect { subject.asks_menu }.to output.to_stdout
     end
   end
+
+  context 'asking for a dish' do
+    it 'returns the specified dish' do
+      expect(subject.asks_for_dish('pancake')).to eq ({:Pancake=>1.0})
+    end
+  end
 end
