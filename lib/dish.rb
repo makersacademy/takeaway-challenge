f8fddbf644a4333b4a66f8b19cba78b00d597b9b
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-04 15:55:58 +0000

    Added dish methods

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,3 +1,6 @@
 class Dish
-
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,4 +1,12 @@
 require 'dish'
 describe Dish do
-
+  subject(:dish) { described_class.new(name) }
+  describe '#name' do
+    context 'name of the dish'
+    let(:name) { "spring rolls" }
+    it 'returns name' do
+      # dish = Dish.new("spring rolls")
+      expect(dish.name).to eq 'spring rolls'
+    end
+  end
 end
