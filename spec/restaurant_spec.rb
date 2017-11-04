fcbdc65465836a1434dadc7c0192580e974d44e2
Tabish Raza <tabishraza@gmail.com>
2017-11-04 12:06:12 +0000

    test for ordering from menu

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -3,11 +3,20 @@ require 'restaurant'
 describe Restaurant do
 
   subject(:restaurant) { described_class.new }
+  let(:selection) { double("Galloway Tartare")}
 
   describe '#menu' do
     context 'when requesting to see menu' do
       it 'should show menu' do
         expect(restaurant.steakhouse).to eq restaurant.steakhouse
+      end
+    end
+  end
+
+  describe '#order' do
+    context 'when ordering from the menu' do
+      it 'should add order to basket' do
+        expect(restaurant.order).to eq selection
       end
     end
   end
