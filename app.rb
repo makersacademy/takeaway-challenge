991377ae5e3a82796766bd072aae82c3a0cfc364
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-04 19:38:00 +0000

    modifies app.rb for heroku test

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'twilio-ruby'
 
 class App < Sinatra::Base
   post '/sms' do
+    content_type 'text/xml'
     twiml = Twilio::TwiML::MessagingResponse.new do |r|
       r.message 'The Robots are coming! Head for the hills!'
     end
-    content_type 'text/xml'
-    twiml.to_s
+    twiml.to_xml
   end
 end
