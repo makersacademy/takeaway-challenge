20813684a8c5b9d56e889addedbf696b940c00e2
cristhiandas <cristhiandf94@gmail.com>
2017-11-04 19:11:43 +0000

    It prints the menu on the output

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -9,18 +9,31 @@ class Dishes
     @menu = []
   end
 
+
+  def print_menu
+    list
+    print_each_dish
+  end
+
+private
+  def print_each_dish
+    @menu.each do |dish_with_price|
+      dish_with_price.each_pair do |dish, price|
+        puts "#{dish} ----> #{price}£"
+      end
+    end
+  end
+
   def list
     CSV.foreach('lib/dish_list.csv') do |line|
       dish = line[0]
       price = line[1]
-      print_menu(dish.to_sym, price.to_f)
+      add_menu(dish.to_sym, price.to_f)
     end
   end
 
-private
-
-  def print_menu(dish, price)
-    @menu << {dish: dish, price: price}
+  def add_menu(dish, price)
+    @menu << {dish=>price}
   end
 
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -1,18 +1,22 @@
 require 'dishes'
 describe Dishes do
 
+  let(:printed_menu) {described_class.new.print_menu}
   context 'When asking for an list' do
     it 'have a list of dishes' do
-      subject.list
-      p subject.menu
+      subject.print_menu
       expect(subject.menu).to be_a Array
     end
 
-    it 'prints the list of dishes' do
-      subject.list
-      expect(subject.menu[0]).to eq ({:dish=>:Pancake, :price=>1})
-      expect(subject.menu[1]).to eq ({:dish=>:Chocolate_pancake, :price=>1.5})
-      expect(subject.menu[-1]).to eq ({:dish=>:Banana_shake, :price=>3.2})
+    it 'returns the list of dishes' do
+      subject.print_menu
+      expect(subject.menu[0]).to eq ({:Pancake=>1})
+      expect(subject.menu[1]).to eq ({:Chocolate_pancake=>1.5})
+      expect(subject.menu[-1]).to eq ({:Banana_shake=>3.2})
+    end
+
+    it 'Prints the list in the ouput' do
+      expect { printed_menu }.to output.to_stdout
     end
   end
 
