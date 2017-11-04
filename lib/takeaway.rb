5b3786182ecc824f7d6f9d353fb84eed76759e4a
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-04 22:10:06 +0000

    adds incomplete TakeAway class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,22 +1,34 @@
 #!/usr/bin/env ruby
 
-require 'sinatra'
 require_relative 'menu'
 require_relative 'order'
 require_relative 'orderparser'
 
-module TakeAway
+class TakeAway
+  attr_reader :menu, :parser
+
   MENU_CSV = './dishes.csv'
 
-  def parse_order(order, parser: OrderParser.new)
-    item_hash = parser.parse(order) 
+  def initialize(path: MENU_CSV, menu_class: Menu, parser: OrderParser.new)
+    @menu = menu_class.from_csv(path)
+    @parser = parser
   end
 
-  def menu(path: MENU_CSV, menu_class: Menu)
-    menu_class.from_csv(path)
+  def build_order(order, order_class: Order)
+    Order.from_selection(@menu, @parser.to_hash(order))
   end
 
+  def get_menu
+    @menu.format
+  end
 
-
-
+  def place(order)
+    begin
+      build_order(order).format
+    rescue ParseError
+      @parser.get_errors(order)
+    rescue DuplicateError
+      @parser.get_duplicates(order)
+    end
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,16 +4,37 @@ require 'takeaway'
 
 describe TakeAway do
 
-  let(:menu_class) { double(menu_class, from_csv: menu) }
-  let(:order_class) { double(order_class, from_selection: order) }
+  let(:menu) { double(:menu, format: 'string') }
+  let(:menu_class) { double(:menu_class, from_csv: menu) }
+  let(:order_class) { double(:order_class, from_selection: order) }
   let(:parser) { double(:parser) }
 
-  let(:takeaway) do
-    described_class.new(menu_location: 'path', menu_class: menu_class,
-                        parser: parser, order_class: order_class
-                       )
-  end
+  let(:takeaway) { described_class.new(menu_class: menu_class) }
+  subject { takeaway }
 
   describe '#initialize' do
+    context 'when created' do
+      it 'has menu' do
+        expect(subject.menu).to eq menu
+      end
+    end
   end
+
+  describe '#get_menu' do
+    context 'when creating menu' do
+      after(:each) { subject.get_menu }
+
+      it 'calls menu format method' do
+        expect(menu).to receive(:format)
+      end
+    end
+
+    context 'when returning menu' do
+      it 'returns output of menu\'s format method' do
+        expect(subject.get_menu).to eq 'string'
+      end
+    end
+  end
+
+  describe '
 end
