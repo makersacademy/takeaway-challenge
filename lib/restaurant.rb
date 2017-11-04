22ee28e0c40451024d9fb6be15cdbf8fb7107793
Tabish Raza <tabishraza@gmail.com>
2017-11-04 11:47:12 +0000

    refactoring of menu pull and push methods

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,12 +1,12 @@
 class Restaurant
+  attr_reader :menu
 
-  def initialize (menu = Menu.new)
+  def initialize(menu = Menu.new)
     @menu = menu
   end
 
   def steakhouse
     @menu.steakhouse
   end
-
 
 end
