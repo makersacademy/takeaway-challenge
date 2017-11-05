a31a80df9db04c1cbe8a02faa7f810dc82a25dec
majdeddine <majda-jambouri@hotmail.fr>
2017-11-05 10:48:59 +0000

    add print menu method

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,2 +1,13 @@
 class TakeAway
+
+  attr_reader :selection
+
+  def initialize(menu)
+    @menu = menu
+  end
+
+  def print_menu
+    @menu.show
+  end
+
 end
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -2,5 +2,17 @@ require 'take_away'
 
 describe TakeAway do
 
-  
+  let(:menu) { double :menu }
+
+  subject(:take_away) { described_class.new(menu) }
+
+  it 'intialize with menu' do
+    expect(TakeAway).to respond_to(:new).with(1)
+  end
+
+  it '#show_menu return list of dishes with price' do
+    allow(menu).to receive(:show).and_return([{ pizza: 12 }, { burger: 8 }])
+    expect(take_away.print_menu).to eq([{ pizza: 12 }, { burger: 8 }])
+  end
+
 end
