0230368481d8731ed15727ca1a090303f194742a
Peter Johnstone <peter.wdj@gmail.com>
2017-11-05 09:43:34 +0000

    Added passing test to allow user to print the total of their current basket

--- lib/basket.rb
+++ lib/basket.rb
@@ -4,9 +4,17 @@ class Basket
   def initialize
     @menu = Menu.new
     @current_order = []
+    @total = 0
   end
 
   def add(item_number)
     @current_order << @menu.dishes.at(item_number - 1) if item_number <= @menu.dishes.length
+  end
+
+  def total
+    @current_order.each do |dish|
+      @total += dish.fetch(:price).to_f
+    end
+    @total
   end
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,4 +1,5 @@
 require 'basket'
+require 'menu'
 
 describe Basket do
   context 'Adding new items to the basket' do
@@ -6,6 +7,16 @@ describe Basket do
       it "Should add a new item to the user's basket" do
         subject.add(1)
         expect(subject.current_order).to eq([{:dish=>"Pizza Rossa", :price=>"9"}])
+      end
+    end
+  end
+
+  context 'Printing the total cost of the current order' do
+    describe '#total' do
+      it "Should print the total cost of items in @current_order" do
+        subject.add(1)
+        subject.add(7)
+        expect(subject.total).to eq 23.5
       end
     end
   end
