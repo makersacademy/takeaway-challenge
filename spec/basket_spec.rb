c57920ce156327748da2fa7ff7ed56161ada0416
Peter Johnstone <peter.wdj@gmail.com>
2017-11-05 10:23:25 +0000

    Refactored basket_spec.rb to DRY out code

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -2,6 +2,11 @@ require 'basket'
 require 'menu'
 
 describe Basket do
+  let (:loaded_basket) do
+    subject.add(1)
+    subject.add(7)
+  end
+
   context 'Adding new items to the basket' do
     describe '#add_item' do
       it "Should add a new item to the user's basket" do
@@ -14,15 +19,13 @@ describe Basket do
   context 'Reviewing the current order' do
     describe '#total' do
       it "Should print the total cost of items in @current_order" do
-        subject.add(1)
-        subject.add(7)
+        loaded_basket
         expect(subject.total).to eq 23.5
       end
     end
     describe '#print_basket' do
       it "Should print out all the items in @current_order" do
-        subject.add(1)
-        subject.add(7)
+        loaded_basket
         expect { subject.print_basket }.to output("1. Pizza Rossa, £9.00\n2. Take It Cheesy, £14.50\n").to_stdout
       end
     end
