27938cb2aac2998819da6f02b79ca5656e2d7b50
Peter Johnstone <peter.wdj@gmail.com>
2017-11-05 11:14:05 +0000

    Setting up lib and spec files for Order class

---
+++ lib/order.rb
@@ -0,0 +1,31 @@
+require 'twilio-ruby'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# account_sid = "AC9bdc53ce16000ee247b2f71d99876d87"
+# auth_token = "1a2ad891876c6ef0a1fbefde27e6b214"
+#
+# @client = Twilio::REST::Client.new account_sid, auth_token
+#
+# @message = @client.messages.create(
+#   to: "+447928624753",
+#   from: "+441202835495",
+#   body: "Hey from Twilio!"
+# )
