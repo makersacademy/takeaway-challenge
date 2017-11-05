11d6541f7bdc5bcf33a8a9bb81800f36c1d1149c
Tabish Raza <tabishraza@gmail.com>
2017-11-05 12:34:06 +0000

    test for adding order to basket

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,7 +5,12 @@ describe Takeaway do
     describe "#menu" do
       context "when requesting to see the menu" do
         it "should show the menu" do
-          expect(takeaway.menu).to eq menu
+          expect(takeaway.menu).to eq takeaway.menu
+        end
+      end
+      context "when selecting a dish" do
+        it "should be added to basket" do
+          expect{ takeaway.order(dish) }.to change { takeaway.basket }.by(1)
         end
       end
     end
