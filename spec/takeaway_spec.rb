491a3edae971bb727e29a7637ca9663940eb7067
Tabish Raza <tabishraza@gmail.com>
2017-11-05 12:51:40 +0000

    test for checking total

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -8,9 +8,20 @@ describe Takeaway do
           expect(takeaway.menu).to eq takeaway.menu
         end
       end
+    end
+
+    describe "#order" do
       context "when selecting a dish" do
         it "should be added to basket" do
           expect(takeaway.order("Chips")).to eq (takeaway.basket)
+        end
+      end
+    end
+
+    describe "#total" do
+      context "when checking order total" do
+        it "should return order total" do
+          expect(takeaway.total).to eq takeaway.total
         end
       end
     end
