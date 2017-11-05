dad811ac086958885c12c930303ee734a6a9297f
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-05 15:00:35 +0000

    adds Dialogue object

--- lib/dialogue.rb
+++ lib/dialogue.rb
@@ -1,48 +1,35 @@
-#!/usr/bin/env ruby
-
+# Dialogue provides formatted messages for responses to clients.
 class Dialogue
-
-  def confirmed(time)
-    confirmed_msg % time.strftime('%H:%M:%S')
+  def confirmed(order)
+    'Your order has been placed! It will arrive before %s.' % order.time
   end
 
   def malformed(errors)
-    malformed_msg % errors
+    'We weren\'t able to read your order. It looks like the following ' \
+      'bits may have caused some problems: %s.' % errors
   end
 
-  def duplicated(items)
-    duplicated_msg % items.join(', ')
+  def duplicated(errors)
+    'You\'ve ordered some items twice. To be sure that this isn\'t a ' \
+      'mistake, please take another look. Duplicates are: %s.' % errors
   end
 
-  def unknown(items)
-    unknown_msg % items.join(', ')
+  def missing(items)
+    'You\'ve ordered some items that don\'t seem to exist. Please try ' \
+      'again. We didn\'t recognise: %s' % items
   end
 
-  def confirm?(order)
-    confirm_msg % [order.format, order.total]
+  def finalise(order)
+    'You\'ve ordered:\n%s\nYour order totals to %s. Please text us back to ' \
+      'confirm (send us Y or N).' % [order.format, order.total]
   end
 
-  def confirmed_msg
-    "Your order has been placed! It will arrive before %s."
-  end 
-
-  def malformed_msg
-    "We weren\'t able to read your order. It looks like the following " \
-      "bits may have caused some problems: %s."
+  def no_order
+    'You have no order in progress at the moment.'
   end
 
-  def duplicated_msg
-    "You've ordered some items twice. To be sure that this isn't a " \
-      "mistake, please take another look. Duplicates are: %s."
-  end
-
-  def unknown_msg
-    "You've ordered some items that don\'t seem to exist. Please try " \
-      "again. We didn\'t recognise: %s"
-  end
-
-  def confirm_msg
-    "You've ordered:\n%s\nYour order totals to %s. Please text us back to " \
-      "confirm (send us Y or N)."
+  def in_progress
+    'An order is currently in progress. Please confirm or cancel before' \
+      'ordering again.'
   end
 end
--- spec/dialogue_spec.rb
+++ spec/dialogue_spec.rb
@@ -1,122 +1,71 @@
-#!/usr/bin/env ruby
-
 require 'dialogue'
 
+# All formatted strings return the same thing
+class String
+  def %(*args)
+    'output'
+  end
+end
+
 describe Dialogue do
-
-  let(:order) { double(:order, total: 'price', format: 'some dishes') }
-  let(:string) { double(:string, :% => 'output') }
+  let(:order) { double(:order, total: 'price', format: 'dishes', time: '1') }
   let(:dialogue) { described_class.new }
-  let(:time) { double(:time, strftime: 'time') }
-    
-  before(:each) do
-    dialogue.methods.grep(/.*_msg/).each do |name|
-      allow(dialogue).to receive(name).and_return(string)
+
+  shared_examples 'interpolates once' do |message|
+    let(:arg) { 'input' }
+
+    context 'when building message' do 
+      after(:each) { dialogue.send(message, arg) }
+
+      it 'returns formatted message' do
+        expect(dialogue.send(message, arg)).to eq 'output'
+      end
     end
   end
 
-  shared_examples 'returns value' do |message|
-    it 'returns formatted message' do
-      expect(subject.send(message, *args)).to eq 'output'
-    end
+  describe '#malformed' do
+    include_examples 'interpolates once', :malformed, 'input'
   end
 
-  subject { dialogue }
+  describe '#duplicated' do
+    include_examples 'interpolates once', :duplicated, 'input'
+  end
+
+  describe '#missing' do
+    include_examples 'interpolates once', :missing, 'input'
+  end
+
+  describe '#finalise' do
+    context 'when building message' do 
+      let(:object) { double(:object, total: '', format: '') }
+      after(:each) { dialogue.finalise(object) }
+
+      it 'returns formatted message' do
+        expect(dialogue.finalise(object)).to eq 'output'
+      end
+    end
+  end
 
   describe '#confirmed' do
-    context 'when given delivery time' do
-      after(:each) { dialogue.confirmed(time) }
+    context 'when building message' do 
+      let(:object) { double(:object, time: '') }
+      after(:each) { dialogue.confirmed(object) }
 
-      it 'returns confirmation containing time' do
-        expect(time).to receive(:strftime).with('%H:%M:%S')
+      it 'returns formatted message' do
+        expect(dialogue.confirmed(object)).to eq 'output'
       end
-
-      it 'interpolates into confirmed message' do
-        expect(string).to receive(:%).with('time')
-      end
-    end
-
-    context 'when returning value' do
-      let(:args) { [time] }
-      include_examples 'returns value', :confirmed
     end
   end
 
-  describe '#malformed_message' do
-    context 'when given errors' do
-      after(:each) { dialogue.malformed('errors') }
-
-      it 'interpolates into confirmed message' do
-        expect(string).to receive(:%).with('errors')
-      end
-    end
-
-    context 'when returning value' do
-      let(:args) { ['some error'] }
-      include_examples 'returns value', :malformed
+  describe 'no_order' do
+    it 'returns string' do
+      expect(subject.no_order).to be_a String
     end
   end
 
-  describe '#duplication' do
-    context 'when given duplicated' do
-      let(:array) { ["100", "200"] }
-      after(:each) { dialogue.duplicated(array) }
-
-      it 'converts array to string without brackets' do
-        expect(array).to receive(:join).with(', ')
-      end
-
-      it 'interpolates into confirmed message' do
-        expect(string).to receive(:%).with('100, 200')
-      end
-    end
-
-    context 'when returning value' do
-      let(:args) { [[1, 2]] }
-      include_examples 'returns value', :duplicated
-    end
-  end
-
-  describe '#unknown_items' do
-    context 'when given unknown items' do
-      let(:array) { ["100", "200"] }
-      after(:each) { dialogue.unknown(array) }
-
-      it 'converts array to string without brackets' do
-        expect(array).to receive(:join).with(', ')
-      end
-
-      it 'interpolates into confirmed message' do
-        expect(string).to receive(:%).with('100, 200')
-      end
-    end
-
-    context 'when returning value' do
-      let(:args) { [[1, 2]] }
-      include_examples 'returns value', :unknown
-    end
-  end
-
-  describe '#confirm?' do
-    context 'when given order' do
-      after(:each) { dialogue.confirm?(order) }
-
-      it 'converts order to formatted string' do
-        expect(order).to receive(:format)
-      end
-
-      it 'converts order to price' do
-        expect(order).to receive(:total)
-      end
-
-      it 'interpolates into confirmed message' do
-        expect(string).to receive(:%).with(['some dishes', 'price'])
-      end
-    end
-
-    context 'when confirming' do
-      let(:args) { [order] }
-      include_examples 'returns value', :confirm?
+  describe 'in_progress' do
+    it 'returns string' do
+      expect(subject.in_progress).to be_a String
     end
   end
 end
