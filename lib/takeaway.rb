86b998d20e10803daadecd6b7b1031100d5a9521
Andrew Davey <ajdavey8@gmail.com>
2017-11-05 19:10:32 +0000

    Add Takeaway class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,24 @@
 require './lib/menu.rb'
+require './lib/order.rb'
 
 class Takeaway
 
   attr_reader :menu
 
-  def initialize(menu = Menu.new)
+  def initialize(menu = Menu.new, order = Order.new)
     @menu = menu
+    @order = order
   end
 
   def show_menu
     @menu.show
+  end
+
+  def add(dish, quantity = 1)
+    @order.add_dish(dish, quantity)
+  end
+
+  def check_total
+    @order.total
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,11 +2,26 @@ require 'takeaway'
 
 describe Takeaway do
   subject(:takeaway) { described_class.new }
+  let(:new_order) { [{ name: 'burger', price: 5 }, { name: 'burger', price: 5 }] }
+  let(:order) { double(add_dish: new_order, total: 10) }
 
   describe '#show_menu' do
     it "should show the menu" do
       allow(takeaway.menu).to receive(:show).and_return 'full_menu'
       expect(takeaway.show_menu).to eq 'full_menu'
+    end
+  end
+
+  describe '#add' do
+    it "should add a dish to order" do
+      expect(takeaway.add('burger', 2)).to eq new_order
+    end
+  end
+
+  describe '#check_total' do
+    it "should show total of order" do
+      takeaway.add('burger', 2)
+      expect(takeaway.check_total).to eq 10
     end
   end
 end
