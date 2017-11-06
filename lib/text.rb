385e3e98376c5ce395fd6fd319a6bd388098298a
cristhiandas <cristhiandf94@gmail.com>
2017-11-06 10:21:50 +0000

    Added the hour to say at which time the takeaway will arrive

--- lib/text.rb
+++ lib/text.rb
@@ -1,12 +1,15 @@
 require 'twilio-ruby'
 class Text
   def send_text
+
+    time_plus_hour = (Time.new + (60 * 60)).strftime("%H:%M")
+
     account_sid = "" # Your Account SID from www.twilio.com/console
     auth_token = ""   # Your Auth Token from www.twilio.com/console
 
     @client = Twilio::REST::Client.new account_sid, auth_token
     @message = @client.messages.create(
-        body: "Thank you! Your order was placed and will be delivered before 18:52",
+        body: "Thank you! Your order was placed and will be delivered before #{time_plus_hour}",
         to: "",    # Replace with your phone number
         from: "")  # Replace with your Twilio number
 
