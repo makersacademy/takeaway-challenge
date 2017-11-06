476f4930df2a1aea4e4f359addcaf8a8d069175c
Tabish Raza <tabishraza@gmail.com>
2017-11-06 13:01:55 +0000

    refactoring of tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -4,41 +4,41 @@ require './lib/text.rb'
 
 class Takeaway
 
-  attr_reader :basket,  :list, :text
+  attr_reader :basket, :list, :text, :menu, :sum
 
-  def initialize(menu = Menu.new, sum = Calculate.new, text = Text.new)
+  def initialize(menu_class: Menu, sum_class: Calculate, text_class: Text)
     @basket = []
-    @menu = menu
-    @sum = sum
-    @text = text
+    @menu = menu_class.new
+    @sum = sum_class.new
+    @text = text_class.new
   end
 
-  def menu
+  def menu_list
     @menu.list
   end
 
-  def order(dish)
-    @basket << add_dish(dish)
-  end
-
-  def total
-    @sum.total(@basket)
-  end
-
-  def check_out
-    empty_basket?
-    text.message(total)
-    @basket.clear
-    puts "Your order has been confirmed!"
-  end
-
-  def empty_basket?
-    raise "You haven't ordered anything!" if @basket.empty?
-  end
-
-  private
-  def add_dish(dish)
-    @menu.list.select { |key| key[:name].include?(dish) }
-  end
+  # def order(dish)
+  #   @basket << add_dish(dish)
+  # end
+  #
+  # def total
+  #   @sum.total(@basket)
+  # end
+  #
+  # def check_out
+  #   empty_basket?
+  #   text.message(total)
+  #   @basket.clear
+  #   puts "Your order has been confirmed!"
+  # end
+  #
+  # def empty_basket?
+  #   raise "You haven't ordered anything!" if @basket.empty?
+  # end
+  #
+  # private
+  # def add_dish(dish)
+  #   @menu.list.select { |key| key[:name].include?(dish) }
+  # end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,50 +1,81 @@
 require 'takeaway'
 
 describe Takeaway do
-  subject(:takeaway) { described_class.new }
+  subject(:takeaway) { described_class.new(menu_class: menu_class, sum_class: sum_class, text_class: text_class) }
 
-  before do
-    allow(takeaway).to receive(:check_out)
-  end
+  let(:menu_class) { double(:menu_class, new: fake_menu) }
+  let(:fake_menu) { double(:fake_menu, list: [{name: "Chicken", quantity: 1, price: 4.5}]) }
+  let(:sum_class) { double(:sum_class, new: fake_sum) }
+  let(:fake_sum) { double(:fake_sum) }
+  let(:text_class) { double(:text_class, new: fake_text) }
+  let(:fake_text) { double(:fake_text) }
 
-    describe '#menu' do
-      context 'when requesting to see the menu' do
-        it 'should show the menu' do
-          expect(takeaway.menu).to eq takeaway.menu
+  # before do
+  #   allow(takeaway).to receive(:check_out)
+  # end
+
+    describe '#initialize' do
+      it 'shows the menu' do
+        expect(takeaway.menu).to eq fake_menu
+      end
+      it 'has a sum' do
+        expect(takeaway.sum).to eq fake_sum
+      end
+      it 'returns an empty basket' do
+        expect(takeaway.basket).to eq []
+      end
+      it 'initializes text' do
+        expect(takeaway.text).to eq fake_text
+      end
+    end
+
+    describe '#menu_list' do
+      context 'when viewing menu' do
+        it 'returns menu' do
+          expect(fake_menu).to receive(:list).and_return [{name: "Chicken", quantity: 1, price: 4.5}]
+          takeaway.menu_list
         end
       end
     end
 
-    describe '#order' do
-      context 'when selecting a dish' do
-        it 'should be added to basket' do
-          expect(takeaway.order('Chips')).to eq takeaway.basket
-        end
-      end
-      context 'when adding dishes to your order' do
-        it 'order total should be the sum of dishes added' do
-          takeaway.order('Chips')
-          takeaway.order('Chips')
-          expect(takeaway.total).to eq 4
-        end
-      end
-    end
-
-    describe '#total' do
-      context 'when checking order total' do
-        it 'should return order total' do
-          expect(takeaway.total).to eq takeaway.total
-        end
-      end
-    end
-
-    describe '#empty_basket' do
-      context 'when trying to checkout with an empty basket' do
-        it 'should raise an error' do
-          expect { takeaway.empty_basket? }.to raise_error "You haven't ordered anything!"
-        end
-      end
-    end
+    # describe '#menu' do
+    #   context 'when requesting to see the menu' do
+    #     it 'should show the menu' do
+    #       expect(takeaway.menu).to eq takeaway.menu
+    #     end
+    #   end
+    # end
+    #
+    # describe '#order' do
+    #   context 'when selecting a dish' do
+    #     it 'should be added to basket' do
+    #       expect(takeaway.order('Chips')).to eq takeaway.basket
+    #     end
+    #   end
+    #   context 'when adding dishes to your order' do
+    #     it 'order total should be the sum of dishes added' do
+    #       takeaway.order('Chips')
+    #       takeaway.order('Chips')
+    #       expect(takeaway.total).to eq 4
+    #     end
+    #   end
+    # end
+    #
+    # describe '#total' do
+    #   context 'when checking order total' do
+    #     it 'should return order total' do
+    #       expect(takeaway.total).to eq takeaway.total
+    #     end
+    #   end
+    # end
+    #
+    # describe '#empty_basket' do
+    #   context 'when trying to checkout with an empty basket' do
+    #     it 'should raise an error' do
+    #       expect { takeaway.empty_basket? }.to raise_error "You haven't ordered anything!"
+    #     end
+    #   end
+    # end
 
 
 end
