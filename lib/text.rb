99ebadf9f0783631dc3b466f77314af7727caa39
Allan Wazacz <cazwazacz@gmail.com>
2017-11-07 21:59:59 +0000

    Improves test coverage to 100% by adding improving Text class tests

--- lib/text.rb
+++ lib/text.rb
@@ -2,21 +2,18 @@ require 'twilio-ruby'
 require './auth_credentials'
 
 class Text
+
+  def initialize(client = Twilio::REST::Client)
+    @client = client.new ACCOUNT_SID, AUTH_TOKEN
+  end
     
   $hour_later = (Time.now + 60 * 60).strftime('%H:%M')
     
   def send_text(total)
-    account_sid = ACCOUNT_SID
-
-    auth_token = AUTH_TOKEN
-
-    @client = Twilio::REST::Client.new account_sid, auth_token
-    message = @client.messages.create(
-        body: message_body(total),
-        to: TO,
-        from: FROM)
-
-    message.sid
+    @client.messages.create(
+    body: message_body(total),
+    to: TO,
+    from: FROM)
   end
 
   def text_response(total)
--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -1,14 +1,25 @@
 require 'text'
 
 describe Text do
+
+  let(:twilio_messages) { double(:twilio_class, create: true) }
+  let(:twilio_instance) { double(:twilio_class, messages: twilio_messages) }
+  let(:twilio_class) { double(:twilio_class, new: twilio_instance) }
+  subject(:text) { described_class.new(twilio_class) }
+
   describe '#send_text' do
     it 'responds to send_text' do
-      expect(subject).to respond_to(:send_text).with(1).argument
+      expect(subject.send_text(34)).to eq true
     end
   end
   describe '#text_response' do
     it 'returns text response in TWIML when total is 34' do
       expect(subject.text_response(34)).to eq "<Response>\n      <Message>\n        Your order total is £34 and you will receive it no later than #{$hour_later}. Thank you!\n      </Message>\n    </Response>"
+    end
+  end
+  describe '#send_menu' do
+    it 'returns string in TWIML with hello' do
+      expect(subject.send_menu('hello')).to eq "<Response>\n      <Message>\n        hello\n      </Message>\n    </Response>"
     end
   end
 end
