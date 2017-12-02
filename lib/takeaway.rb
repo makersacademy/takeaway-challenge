07f8bf31f4dced535d3adac01c494e7f4f2453a9
Lan Pham <lanpham312@gmail.com>
2017-12-02 13:51:52 +0000

    the menu is initalized when creating a new instance of takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,17 @@
 class Takeaway
-  def menu
-    {
-      "prawn crackers"=>2.00,
-      "summer roll"=>4.50,
-      "spring rolls"=>3.99,
-      "pho"=>10.99,
-      "bun thit nuong"=>9.50,
-      "seafood stir fry"=>13.99,
-      "coke"=>1.50,
-      "water"=>1.25
-    }
+
+  attr_reader :menu
+
+  def initialize
+    @menu = {
+              "prawn crackers"=>2.00,
+              "summer roll"=>4.50,
+              "spring rolls"=>3.99,
+              "pho"=>10.99,
+              "bun thit nuong"=>9.50,
+              "seafood stir fry"=>13.99,
+              "coke"=>1.50,
+              "water"=>1.25
+            }
   end
 end
