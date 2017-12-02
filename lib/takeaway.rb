4c424e4b69107e36402b04e88beeee7aa40289ea
Lan Pham <lanpham312@gmail.com>
2017-12-02 13:39:46 +0000

    adding menu method with a list of dishes and drinks with prices in a hash

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,14 @@
 class Takeaway
+  def menu
+    {
+      "prawn crackers"=>2.00,
+      "summer roll"=>4.50,
+      "spring rolls"=>3.99,
+      "pho"=>10.99,
+      "bun thit nuong"=>9.50,
+      "seafood stir fry"=>13.99,
+      "coke"=>1.50,
+      "water"=>1.25
+    }
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,11 @@
 require 'takeaway'
 
 describe Takeaway do
+  it {is_expected.to respond_to(:menu)}
 
+  describe "#menu" do
+    it "should show a list of dishes when looking at the menu" do
+      expect(subject.menu).to eq ({"prawn crackers"=>2.00, "summer roll"=>4.50, "spring rolls"=>3.99, "pho"=>10.99, "bun thit nuong"=>9.50, "seafood stir fry"=>13.99, "coke"=>1.50, "water"=>1.25})
+    end
+  end
 end
