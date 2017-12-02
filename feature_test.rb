298be2b1bd0ff8df1aae1a378a4a2437b14d2051
Keran <kerbraich@gmail.com>
2017-12-02 18:03:16 +0000

    add feature tests for user story 2

--- feature_test.rb
+++ feature_test.rb
@@ -1,5 +1,13 @@
 require './lib/menu'
+require './lib/order'
 
 # user story 1
-p menu = Menu.new
-menu.print_menu
+# p menu = Menu.new
+# menu.print_menu
+
+# user story 2
+p order = Order.new
+p order.add("Poutine")
+p order.add("Poutine", 2)
+p order.add("Aloo Ghobi")
+p order
