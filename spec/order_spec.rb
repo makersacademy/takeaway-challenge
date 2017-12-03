2be0f682c5ea9d2b004e329411465d254bf0ec7e
Keran <kerbraich@gmail.com>
2017-12-03 18:39:15 +0000

    add test for sending message to notification

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,8 +2,9 @@ require 'order'
 require 'menu'
 
 describe Order do
-  let(:menu) { double(:menu, price: 1.00) }
-  subject(:order) { described_class.new(menu) }
+  let(:menu) { double :menu, price: 1.00 }
+  let(:notification) { double :notification, send_text: nil }
+  subject(:order) { described_class.new(menu, notification) }
 
   context "#initialize" do
     it "should create an empty order" do
@@ -27,13 +28,23 @@ describe Order do
     it "should give the customer a correct order total" do
       order.add("Poutine")
       order.add("Caesar")
-      expect(order.total).to eq("Total: £2.00")
+      expect(order.total).to eq(2)
     end
     it "should give the customer a correct total when multiples of same item " do
       order.add("Poutine", 2)
       order.add("Caesar")
       order.add("Caesar")
-      expect(order.total).to eq("Total: £4.00")
+      expect(order.total).to eq(4)
+    end
+  end
+
+  context "#complete" do
+    it "should send message to notification" do
+      order.add("Poutine", 2)
+      order.add("Caesar")
+      message = "Thank you! Your total is £3.00 and your order has been placed."
+      allow(notification).to receive(:send_text).with(message)
+      order.complete
     end
   end
 end
