6c6fddba4702ab4f519645a078b56caa3bf595ed
Tim <Timwilliamson1337@gmail.com>
2017-12-03 16:49:19 +0000

    adds Dish

---
+++ spec/dish_spec.rb
@@ -0,0 +1,16 @@
+require 'dish'
+
+describe Dish do
+  subject {described_class.new("Lamb Biryani", 4.99)}
+  describe "#name" do
+    it "returns the name of the dish" do
+      expect(subject.name).to eq "Lamb Biryani"
+    end
+  end
+    describe "#price" do
+      it "returns the price of the dish" do
+        expect(subject.price).to eq 4.99
+      end
+    end
+
+end
