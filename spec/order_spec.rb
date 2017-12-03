9227dd3a4b7bf66e6b54a71dcca7b8299edbc143
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-03 22:17:29 +0000

    User Story #2 - Order - unit tests for pricing failed

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,14 +1,20 @@
 require_relative '../lib/order.rb'
 
 describe Order do
-  subject(:order) { described_class.new(:dish) }
+  subject(:order) { described_class.new(:dish, 5) }
   let(:dish) { double(:dish) }
 
   it 'should show the current order' do
-    expect(subject.current_order).to eq [:dish]
+    expect(subject.current_order).to eq [:dish => 5]
   end
 
   it 'should add a dish to the current order' do
-    expect { subject.add_to_order(dish) }.to change { order.current_order.length }.by 1
+    expect { subject.add_to_order(dish, 5) }.to change { order.current_order.length }.by 1
+  end
+
+  it 'should calculates the total for the current order' do
+    order.add_to_order(:menu_item, 5)
+    order.add_to_order(:menu_item, 5)
+    expect(order.order_total).to eq 15
   end
 end
