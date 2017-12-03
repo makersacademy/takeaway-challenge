4938cc48e53e6dd2eb3df94da68435f3211630dd
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-03 22:24:51 +0000

    User Story #2 - Order - unit tests for pricing passed

--- lib/order.rb
+++ lib/order.rb
@@ -2,11 +2,20 @@
 class Order
   attr_reader :current_order
 
-  def initialize(dish)
-    @current_order = [dish]
+  def initialize(dish, price)
+    @current_order = [{ dish => price }]
+    @order_total = 0
   end
 
-  def add_to_order(dish)
-    @current_order << dish
+  def add_to_order(dish, price)
+    @current_order << { dish => price }
   end
+
+  def order_total
+    @current_order.each do |item|
+      item.each {|dish_name, dish_price| @order_total += dish_price}
+    end
+    @order_total
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,12 +9,12 @@ describe Order do
   end
 
   it 'should add a dish to the current order' do
-    expect { subject.add_to_order(dish, 5) }.to change { order.current_order.length }.by 1
+    expect { subject.add_to_order(:dish, 5) }.to change { order.current_order.length }.by 1
   end
 
   it 'should calculates the total for the current order' do
-    order.add_to_order(:menu_item, 5)
-    order.add_to_order(:menu_item, 5)
+    order.add_to_order(:dish, 5)
+    order.add_to_order(:dish, 5)
     expect(order.order_total).to eq 15
   end
 end
