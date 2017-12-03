253601fd645c2c4bee9bfba75a7ef23ebf416a01
Lan Pham <lanpham312@gmail.com>
2017-12-03 23:28:10 +0000

    updating test for text class

--- lib/sms.rb
+++
@@ -1,2 +0,0 @@
-class Sms
-end
--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -1,29 +1,36 @@
-# require 'text'
-#
-# describe Text do
-#
-#   subject(:text) { described_class.new }
-#   subject(:mock_time) {double(:mock_time, (Time.new + 60 * 60).strftime("%H:%M"))}
-#
-#   describe "#send_text" do
-#     before do
-#       allow(text).to receive(:send_text)
-#     end
-#
-#     it "sends a text message with order confirmation" do
-#       expect(text.send_text).to receive(:send_text).with("Order successful. Estimated delivery time 19:00")
-#     end
-#   end
-#
-#   describe "#order_summary" do
-#     it "should confirm the order with a delivery time" do
-#     expect(subject.order_summary).to eq "Thank you for your order. The estimate delivery time is #{mock_time}"
-#     end
-#   end
-# end
+require 'text'
 
+describe Text do
 
-# it "sends a text message with order confirmation" do
-#   # text.send_sms
-#   expect(text.send_text).to receive(:send_text).and_return("Order successful. Estimated delivery time 19:00")
-# end
+  subject(:text) { described_class.new }
+  # allow(Time).to receive(:now).and_return(Time.new(2017, 10, 12, 12, 30))
+  # subject(:mock_time) {double(:mock_time, (Time.new + 60 * 60).strftime("%H:%M"))}
+
+  describe "#send_text" do
+    # before do
+    #   allow(subject).to receive(:send_text)
+    # end
+
+    it "sends a text message with order confirmation" do
+      subject.send_text
+    end
+
+    it "sends a confirmation text message" do
+      expect(subject).to receive(:send_text).and_return("Order successful. Estimated delivery time 19:00")
+      subject.send_text
+    end
+
+  end
+
+  describe "#order_summary" do
+    it "should confirm the order with a delivery time" do
+    expect(subject.order_summary).to eq "Thank you for your order. The estimate delivery time is #{(Time.new + 60 * 60).strftime(Text::TIME_FORMAT)}"
+    end
+  end
+
+  describe "#delivery_time" do
+    it "should show the delivery time an hour after the current time" do
+      expect(subject.delivery_time). to eq "#{(Time.new + 60 * 60).strftime(Text::TIME_FORMAT)}"
+    end
+  end
+end
