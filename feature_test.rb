dfc528323b66456b947700b416624da4ecf200e1
Keran <kerbraich@gmail.com>
2017-12-03 14:18:36 +0000

    update feature test

--- feature_test.rb
+++ feature_test.rb
@@ -5,12 +5,15 @@ require './lib/order'
 # p menu = Menu.new
 # menu.print_menu
 #
-# # # user story 2
+# user story 2
 # p order = Order.new
 # p order.add("Poutine")
-# # p order.add("Poutine", 2)
+# p order.add("Poutine", 2)
 # p order.add("Aloo Ghobi")
-# # p order
+# p order
 #
-# # user story 3
-p order.total
+# user story 3
+# p order.total
+
+# user story 4
+p order.complete
