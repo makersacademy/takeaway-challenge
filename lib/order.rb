2bd7426023815eaf9d9f7985491847f319120e03
Keran <kerbraich@gmail.com>
2017-12-03 12:37:36 +0000

    add initialize, add and total methods to order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,22 @@
 class Order
-  attr_accessor :order
+  attr_accessor :order_list
   DEFAULT_QUANTITY = 1
 
-  def initialize
-    @order = {}
+  def initialize(menu = Menu.new)
+    @order_list = {}
+    @menu = menu
   end
 
   def add(item, quantity = DEFAULT_QUANTITY)
-    @order.include?(item) ? @order[item] += quantity : @order[item] = quantity
+    @order_list.include?(item) ? @order_list[item] += quantity : @order_list[item] = quantity
+    "#{quantity} #{item}(s) added to your order"
+  end
+
+  def total
+    total_price = 0
+    @order_list.each do |item, quantity|
+      total_price += quantity * @menu.price(item)
+    end
+    total_price
   end
 end
