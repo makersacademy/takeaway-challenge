a1692b7c0f6529fa3908f22ae4de3d00bb1f7d8b
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-03 18:41:26 +0000

    User Story #1 - Takeaway - unit test to view menu failed

--- spec/takeway_spec.rb
+++ spec/takeway_spec.rb
@@ -1,1 +1,10 @@
-require "takeaway"
+require_relative "../lib/takeaway"
+
+describe TakeAway do
+  subject(:takeaway) {described_class.new}
+
+  it "should display the menu" do
+    expect(subject).to respond_to(:view_menu)
+  end
+
+end
