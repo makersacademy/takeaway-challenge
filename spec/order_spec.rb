376e4391b11da7242689b8d84e9224107c84adef
Keran <kerbraich@gmail.com>
2017-12-03 12:36:48 +0000

    add tests for order class

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,21 +1,39 @@
 require 'order'
+require 'menu'
 
 describe Order do
+  let(:menu) { double(:menu, price: 1.00) }
+  subject(:order) { described_class.new(menu) }
+
   context "#initialize" do
     it "should create an empty order" do
-      expect(subject.order).to be_empty
+      expect(order.order_list).to be_empty
     end
   end
 
   context "#add_item" do
     it "should add one order of a menu item" do
-      subject.add("Poutine")
-      expect(subject.order["Poutine"]).to eq(Order::DEFAULT_QUANTITY)
+      order.add("Poutine")
+      expect(order.order_list["Poutine"]).to eq(Order::DEFAULT_QUANTITY)
     end
     it "should add multiple orders of a menu item" do
-      subject.add("Poutine")
-      subject.add("Poutine", 3)
-      expect(subject.order["Poutine"]).to eq(4)
+      order.add("Poutine")
+      order.add("Poutine", 3)
+      expect(order.order_list["Poutine"]).to eq(4)
+    end
+  end
+
+  context "#total" do
+    it "should give the customer a correct order total" do
+      order.add("Poutine")
+      order.add("Caesar")
+      expect(order.total).to eq(2.00)
+    end
+    it "should give the customer a correct total when multiples of same item " do
+      order.add("Poutine", 2)
+      order.add("Caesar")
+      order.add("Caesar")
+      expect(order.total).to eq(4.00)
     end
   end
 end
