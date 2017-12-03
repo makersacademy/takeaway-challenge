ba5f4612f34ca55c4fcb529b1422ffacc7f8b8fd
James Lizamore <jwlizamore@gmail.com>
2017-12-03 14:26:08 +0000

    Added get_price method and test

---
+++ lib/order.rb
@@ -0,0 +1,7 @@
+class Order
+  def initialize
+    items = []
+  end
+
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,11 @@
+require 'menu'
+
+describe Menu do
+  it 'should display a menu of items and prices' do
+    expect(subject.to_s).to include(:pizza)
+  end
+
+  it 'should return a price when provided an item' do
+    expect(subject.get_price(:pizza)).to eq 1.15
+  end
+end
