2eb97b448d3b64470c5b1e75b0e123898ef5d73c
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-12-03 22:12:21 +0000

    confirmation message given for successful order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,22 +1,44 @@
 require_relative 'menu'
+require_relative 'order'
 
 class Takeaway
+
+  def initialize
+    @menu = Menu.new
+    @order_history = []
+  end
+
+  def handle_order(order)
+    order_output(process_order(take_order(order)))
+  end
 
   def take_order(order, order_class = Order)
     order_lines = order.split("\n")
     orders_with_price = order_lines.each { |line| line.split(',') }
-    process_order(orders_with_price, order_class)
+    create_order(orders_with_price, order_class)
   end
 
-  def process_order(orders_with_price, order_class)
+  def order_output(order)
+    "Your order is complete! Total: £#{"%.2f" % order.price}, Arriving by: #{order.time}"
+  end
+
+  private
+  attr_reader :menu
+
+  def process_order(order)
+    order.translate(menu)
+    order
+  end
+
+  def verify_order?(order)
+    order.correct?
+  end
+
+  def create_order(orders_with_price, order_class)
     price = orders_with_price.pop
     orders = orders_with_price.select { |item| item[0] }
     quantities = orders_with_price.select { |item| item[1] }
     order_class.new(orders, quantities, price)
   end
-
-  # def verify_order?(order)
-  #   order.correct?
-  # end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,18 +3,23 @@ require 'takeaway'
 describe Takeaway do
   subject(:ta) { described_class.new }
   let(:menu) { double(:my_menu) }
-  let(:sample_order) { "1,1\n2,3\n20.50" }
+  let(:user_order) { "1,1\n2,3\n20.50" }
   let(:order_intermediate) { double(:my_order_intermediate, new: order) }
-  let(:order) { double(:my_order, price: 20.50, orders: [1, 1]) }
+  let(:order) { double(:my_order, price: 20.50, orders: [1, 1], time: 'Now') }
+  let(:message) { "Your order is complete! Total: £20.50, Arriving by: Now" }
 
   it 'takes orders as string' do
     expect(ta).to respond_to(:take_order).with(1).argument
   end
 
   it 'transforms order into order objects' do
-    ta.take_order(sample_order, order_intermediate)
+    ta.take_order(user_order, order_intermediate)
     expect(order.price).to eq 20.50
     expect(order.orders[0]).to eq 1
+  end
+
+  it 'notifies customer of successful order' do
+    expect(ta.order_output(order)).to eq message
   end
 
 end
