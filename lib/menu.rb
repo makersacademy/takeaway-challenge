bbb0ea958d58969bf7e1ab930b260e877b044c7a
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-03 18:13:15 +0000

    User Story #1 - Unit Test with Dishes passing

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,15 @@
 class Menu
 
   def initialize
-    @dishes = {Chicken: 3.70, Pasta: 4.59, Salad: 2.86 }
+    @dishes = {Chicken: 3.78, Pasta: 4.59, Salad: 2.86 }
   end
 
   def show_dishes
+    list = @dishes.map do|name, price|
+      "#{name.to_s} £#{price.round(2)}"
+    end
 
+    list.join(" | ")
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -8,7 +8,7 @@ it "should show a list of dishes" do
 end
 
 it "should list the dishes available" do
-  expect(menu.show_dishes).to eq "Chicken £3.70 | Pasta £4.59 | Salad £2.86"
+  expect(menu.show_dishes).to eq "Chicken £3.78 | Pasta £4.59 | Salad £2.86"
 end
 
 end
