005f649b23ba01004c27370fffe85f9958188b62
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-03 23:53:44 +0000

    User Story #4 - Message - unit test for time stamp added & failed

--- lib/message.rb
+++ lib/message.rb
@@ -1,3 +1,4 @@
+require_relative "./order.rb"
 class Message
 
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,5 +1,9 @@
 require_relative "../lib/message.rb"
 
 describe Message do
-  
+subject(:message) {described_class.new}
+
+it "should know what time the order was completed" do
+  expect(message.completed_timestamp).to eq Time.now
+end
 end
