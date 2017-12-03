26cd9916ddb592b9f0ae8e93275d6ca6a711d65b
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-12-03 18:58:52 +0000

    print formatting added

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,5 +1,12 @@
 class Printer
+
   def print_menu(menu)
+    format_menu(menu)
     menu.each { |item| puts "##{item.item_id}: #{item.name}, #{item.description}, £#{item.price}." }
   end
+
+  def format_menu(menu)
+    menu.sort_by(&:item_id)
+  end
+  
 end
