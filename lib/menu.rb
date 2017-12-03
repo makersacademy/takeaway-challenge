836ee83c1cc50230338d1c94e27771f7da81bbff
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-03 18:03:13 +0000

    User Story #1 - Unit Test with Dishes failing

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,11 @@
 class Menu
 
   def initialize
-    @dishes = {}
+    @dishes = {Chicken: 3.70, Pasta: 4.59, Salad: 2.86 }
   end
 
   def show_dishes
-    
+
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,4 +7,8 @@ it "should show a list of dishes" do
   expect(menu).to respond_to(:show_dishes)
 end
 
+it "should list the dishes available" do
+  expect(menu.show_dishes).to eq "Chicken £3.70 | Pasta £4.59 | Salad £2.86"
+end
+
 end
