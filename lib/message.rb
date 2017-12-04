3ba4fad2000c65580059bffe635aaa271651ee0a
umairb1 <umairbashir95@hotmail.com>
2017-12-04 10:13:21 +0000

    Updated sms

--- lib/message.rb
+++ lib/message.rb
@@ -4,9 +4,9 @@ class TextMessage
   attr_reader :send_sms
 
   TWILIO_ACC_SID = 'AC4b920e4f9d755d618d14e248b3f39fa8'
-  TWILIO_AUTH_TOKEN = '818c6b68bb18c8aaf354cb54d1ea07cd'
-  TWILIO_ACC_NUMBER = '+441669912048'
-  PHONE_NUMBER = '+447453572037' 
+  TWILIO_AUTH_TOKEN = '!818c6b68bb18aaf354cb54d1ea07cd'
+  TWILIO_ACC_NUMBER = '!+4416612048'
+  PHONE_NUMBER = '!+4474532037' 
 
   def send_sms
     time = (Time.now + 60 * 60).strftime("%k:%M")
