ba5cb41990818a5206f340e456e74569df528458
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-04 00:07:48 +0000

    User Story #4 - Message - unit test for delivery time passed

--- lib/message.rb
+++ lib/message.rb
@@ -2,10 +2,13 @@ require_relative "./order.rb"
 
 class Message
 
-attr_reader :compl_timestamp
+DELIVERY_TIME = 3600
+
+attr_reader :compl_timestamp, :delivery_time 
 
 def initialize(compl_timestamp)
   @compl_timestamp = compl_timestamp
+  @delivery_time = compl_timestamp + DELIVERY_TIME
 
 end
 
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -8,4 +8,8 @@ subject(:message) {described_class.new(time)}
 it "should know what time the order was completed" do
   expect(subject.compl_timestamp).to eq time
 end
+
+it "should know how long delivery will take" do
+  expect(subject.delivery_time). to eq (time + Message::DELIVERY_TIME)
+end
 end
