4c1e4978f1741aef97bd60f49dc2d54ba33e3e67
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-04 00:01:32 +0000

    User Story #4 - Message - unit test for time stamp passed

--- lib/message.rb
+++ lib/message.rb
@@ -1,4 +1,12 @@
 require_relative "./order.rb"
+
 class Message
+
+attr_reader :compl_timestamp
+
+def initialize(compl_timestamp)
+  @compl_timestamp = compl_timestamp
+
+end
 
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,9 +1,11 @@
 require_relative "../lib/message.rb"
 
 describe Message do
-subject(:message) {described_class.new}
+time = Time.now
+subject(:message) {described_class.new(time)}
+
 
 it "should know what time the order was completed" do
-  expect(message.completed_timestamp).to eq Time.now
+  expect(subject.compl_timestamp).to eq time
 end
 end
