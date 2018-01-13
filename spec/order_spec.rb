fda0964540cf38467fde626032852f7ce267ddd5
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-13 18:50:59 +0000

    puts key value hash into basket

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,30 +5,38 @@ describe Order do
   subject(:order) { described_class.new }
   let(:cafe) { double :cafe, :order }
 
-  describe 'ordering' do
+  describe '#add_to_order' do
     it 'stores the item in an array' do
       order.add_to_order("CHIA_PUDDING")
-      expect(order.full_order).to include :CHIA_PUDDING
+      expect(order.basket).to include ({:CHIA_PUDDING=>5.75})
     end
 
     it 'stores multiple items in an array' do
       order.add_to_order("CHIA_PUDDING")
       order.add_to_order("BANANA_BREAD")
-      expect(order.full_order).to include :BANANA_BREAD
+      expect(order.basket).to include ({:BANANA_BREAD=>5.50})
     end
 
     it 'stores multiple items in an array' do
       order.add_to_order("CHIA_PUDDING")
       order.add_to_order("BANANA_BREAD")
-      expect(order.full_order).to include :CHIA_PUDDING
+      expect(order.basket).to include ({:CHIA_PUDDING=>5.75})
     end
 
     it 'converts input to a symbol' do
       order.add_to_order("CHIA_PUDDING")
-      expect(order.full_order).to include :CHIA_PUDDING
+      expect(order.basket).to include ({:CHIA_PUDDING=>5.75})
     end
-
   end
+
+  describe 'looking up hash' do
+    it 'finds the key value pair' do
+      order.add_to_order("CHIA_PUDDING")
+      expect(order.basket).to include ({CHIA_PUDDING: 5.75})
+    end
+  end
+
+
 
 end
 
