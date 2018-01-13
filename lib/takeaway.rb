73cc5d47bf426631d649790c425e1f5cebf33f1c
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-13 21:20:34 +0000

    adds view order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,10 @@
 class TakeAway
-  attr_reader :order
+  attr_reader :order, :total_cost
 
   def initialize(menu_class:)
     @menu = menu_class
     @order = []
+    @total_cost = 0
   end
 
   def display_menu
@@ -12,7 +13,15 @@ class TakeAway
 
   def choose_item(item, quantity)
     raise 'Cannot choose this item!' unless on_menu? item
-    @order << { item: item, quantity: quantity }
+    order << { 'item' => item, 'quantity' => quantity, 'price' => get_price(item) }
+  end
+
+  def view_order
+    raise 'Please select an item first' if order.empty?
+    order.each do |item|
+      puts "#{item['item']}: #{item['quantity']} @ £%0.2f" % item['price']
+    end
+    update_total
   end
 
   private
@@ -21,5 +30,13 @@ class TakeAway
 
   def on_menu?(item)
     menu.items.include? item
+  end
+
+  def get_price(item)
+    menu.items[item]
+  end
+
+  def update_total
+    @total_cost = 1.5
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -13,15 +13,39 @@ describe TakeAway do
 
   describe '#choose_item' do
     before do
-      allow(menu).to receive(:items).and_return({ 'Pizza' => 1 })
+      allow(menu).to receive(:items).and_return({ 'Pizza' => 1.50 })
     end
+
     it 'chooses an item and a quantity to order' do
       takeaway.choose_item('Pizza', 1)
-      expect(takeaway.order).to include({ item: 'Pizza', quantity: 1 })
+      expect(takeaway.order).to include({ 'item' => 'Pizza', 'quantity' => 1, 'price' => 1.5 })
     end
 
     it 'checks if the item is on the menu' do
-      expect{ takeaway.choose_item('exoticfood', 100) }.to raise_error 'Cannot choose this item!'
+      expect { takeaway.choose_item('exoticfood', 100) }.to raise_error 'Cannot choose this item!'
+    end
+  end
+
+  describe '#view_order' do
+    context 'There is nothing to order' do
+      it 'raises an error' do
+        expect { takeaway.view_order }.to raise_error 'Please select an item first'
+      end
+    end
+
+    context 'There have been items chosen' do
+      before do
+        allow(menu).to receive(:items).and_return({ 'Pizza' => 1.50 })
+        takeaway.choose_item('Pizza', 1)
+      end
+
+      it 'shows the order' do
+        expect { takeaway.view_order }.to output("Pizza: 1 @ £1.50\n").to_stdout
+      end
+
+      it 'updates the total' do
+        expect { takeaway.view_order }.to change { takeaway.total_cost }.by(1.50)
+      end
     end
   end
 end
