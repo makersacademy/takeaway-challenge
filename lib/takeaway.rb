16e605f8c1788720e113f70c2d4c88634444ab82
Ellie <elliewem@gmail.com>
2018-01-13 18:04:46 +0000

    amends takeaway and takeaway spec

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,24 +1,16 @@
+require_relative './menu.rb'
+require 'forwardable'
+
 class Takeaway
 
-  MENU = {
-    "tuna soaked nut butter" => 2.00,
-    "sweaty eel sausage" => 2.50,
-    "microwaved xmas crab" => 4.00,
-    "easy peasy hot & eely cheesy balls" => 3.50,
-    "Kenny Loggins' mackeral tagine" => 9.00,
-    "chubby tea bags" => 2.00,
-    "mushed tadpole burrito" => 6.00,
-    "cod monsieur" => 6.50,
-    "steamy bream hot box" => 6.00,
-    "fishy goods and services" => 7.00,
-    "muddy fish-finger aeoli" => 3.00,
-    "hot pollacks" => 4.00,
-    "scaley protein shake" => 3.00,
-    "freaky lamp fish" => 6.00
-  }
+  extend Forwardable
 
-  def viewmenu
-    MENU.each { |key, value| puts "#{key}, £#{value}0" }
+  def_delegators :@menu, :viewmenu, :order
+
+  attr_reader :menu
+
+  def initialize
+    @menu = Menu.new
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,10 +2,5 @@ require 'takeaway'
 
 describe Takeaway do
 
-  describe '#viewmenu' do
-    it 'displays the menu' do
-      expect { subject.viewmenu }.to output.to_stdout
-    end
-  end
 
 end
