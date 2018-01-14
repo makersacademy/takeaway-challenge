adfe5a61c61e1548f14f6a94ed9765a3b2adaccb
Alex Sergides <32393486+alexse5121@users.noreply.github.com>
2018-01-14 11:23:14 +0000

    print menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,7 +2,15 @@ require 'menu'
 
 class Menu
   attr_reader :dishes
+
   def initialize(dishes)
     @dishes = dishes
   end
+
+  def print
+    dishes.map do |item, cost|
+      "%s $%.2f" % [item.to_s, cost]
+    end.join(", ")
+  end
+
 end
