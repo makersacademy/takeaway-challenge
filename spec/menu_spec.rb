706de3110cb5af37d1e6ecb1992b5935c5c40b2b
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-14 21:12:58 +0000

    Check if the dish is or not on the menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -21,4 +21,13 @@ describe Menu do
     showed_menu = 'Pierogi £4.00, Kopytka £2.00, Bigos £5.20'
     expect(menu.show).to eq(showed_menu)
   end
+
+  it 'says if the dish is on the menu' do
+    expect(menu.has_dish?(:pierogi)).to be true
+  end
+
+  it 'says if the dish is not on the menu' do
+    expect(menu.has_dish?(:ramen)).to be false
+  end
+
 end
