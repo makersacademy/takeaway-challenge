ce82eed1d9f89d74b947db8f9841ec7e06d3059f
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-14 12:44:41 +0000

    Add #get_total and edit #add_to_order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,10 +6,21 @@ class Takeaway
 
   def initialize(menu)
     @menu = menu
-    @order = []
+    @order = {}
   end
 
-  def place_order(item, quantity)
-    @order << [item, quantity]
+  def add_to_order(item, quantity)
+    @order[item].nil? ? @order[item] = quantity : @order[item] += quantity
+    "You just added '#{quantity} x #{item}'. Your order so far is: '#{display_order(@order)}'"
+  end
+
+  def get_total
+    @order.map { |item, quantity| quantity * menu.dishes[item] }.inject(0, :+)
+  end
+
+  private
+
+  def display_order(order)
+    order.map { |k, v| "#{v} x #{k}" }.join(", ")
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -12,11 +12,34 @@ describe Takeaway do
     end
   end
 
-  describe "#place_order" do
+  describe "#add_to_order" do
 
-    it "places an order" do
-      takeaway.place_order("spring rolls", 3)
-      expect(takeaway.order).to include(["spring rolls", 3])
+    it "adds items" do
+      takeaway.add_to_order("spring rolls", 3)
+      takeaway.add_to_order("dumplings", 7)
+      expect(takeaway.order).to eq({ "spring rolls" => 3, "dumplings" => 7 })
+    end
+
+    it "adds items twice" do
+      takeaway.add_to_order("spring rolls", 3)
+      takeaway.add_to_order("dumplings", 7)
+      takeaway.add_to_order("spring rolls", 2)
+      expect(takeaway.order).to eq({ "spring rolls" => 5, "dumplings" => 7 })
+    end
+
+    it "returns a message with the order" do
+      takeaway.add_to_order("spring rolls", 3)
+      takeaway.add_to_order("dumplings", 7)
+      expect(takeaway.add_to_order("spring rolls", 2)).to eq "You just added '2 x spring rolls'. Your order so far is: '5 x spring rolls, 7 x dumplings'"
+    end
+  end
+
+  describe "#get_total" do
+
+    it "returns the total cost of the dishes in the order" do
+      takeaway.add_to_order("spring rolls", 4)
+      allow(menu).to receive(:dishes).and_return({ "spring rolls" => 2 })
+      expect(takeaway.get_total).to eq 8
     end
   end
 end
