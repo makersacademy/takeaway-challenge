7525e4a5e0c9d6d8e64faa3274d9f25346371804
Alex Sergides <32393486+alexse5121@users.noreply.github.com>
2018-01-14 15:28:39 +0000

    updated order spec

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,18 @@
-
 require 'order'
-describe Order do 
 
+describe Order do
+  subject(:order) { described_class.new }
+
+  let(:dishes) do
+    {
+      eggs: 5,
+      milk: 6
+    }
+  end
+
+  it 'chooses couple of dishes from menu' do
+    order.add(:eggs, 5)
+    order.add(:milk, 6)
+    expect(order.dishes).to eq(dishes)
+  end
 end
