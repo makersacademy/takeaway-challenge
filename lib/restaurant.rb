fca80a82e2d70833ea76c525ad59d2e8b1295843
alextwilson <github@alextwilson.co.uk>
2018-01-14 17:45:18 +0000

    Order adds dish and number to basket. Ordering the same dish again overwrites the number

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,6 +1,18 @@
 class Restaurant
+
+  attr_reader :basket
+
+  def initialize
+    @menu = { 'spring_rolls' => 1.99, 'prawn_toast' => 1.99,
+       'egg_fried_rice' => 2.50, 'kung_po_chicken' => 4.50 }
+    @basket = {}
+  end
+
   def read_menu
-    { spring_rolls: 1.99, prawn_toast: 1.99,
-       egg_fried_rice: 2.50, kung_po_chicken: 4.50 }
+    @menu
+  end
+
+  def order(dish, number = 1)
+    @basket[dish] = number
   end
 end
--- spec/unit/restaurant_spec.rb
+++ spec/unit/restaurant_spec.rb
@@ -5,9 +5,25 @@ describe Restaurant do
 
   describe '#read_menu' do
     it 'returns a menu' do
-      menu = { spring_rolls: 1.99, prawn_toast: 1.99,
-         egg_fried_rice: 2.50, kung_po_chicken: 4.50 }
+      menu = { 'spring_rolls' => 1.99, 'prawn_toast' => 1.99,
+         'egg_fried_rice' => 2.50, 'kung_po_chicken' => 4.50 }
       expect(restaurant.read_menu).to eq menu
+    end
+  end
+
+  describe '#order' do
+    it 'stores a dish' do
+      expect(restaurant.order('spring_rolls')).to eq 1
+    end
+
+    it 'stores the number of dishes as an argument' do
+      expect(restaurant.order('spring_rolls', 3)).to eq 3
+    end
+
+    it 'can change order' do
+      basket = { 'spring_rolls' => 1 }
+      restaurant.order('spring_rolls', 3)
+      expect { restaurant.order('spring_rolls') }.to change { restaurant.basket }. to basket
     end
   end
 end
