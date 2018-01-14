cf4748d975a00e768c6218d47848269e306cae47
Tom Grand <thomas.a.grand@gmail.com>
2018-01-14 17:28:35 +0000

    add list viewer for order

--- lib/order.rb
+++ lib/order.rb
@@ -6,17 +6,32 @@ class Order
   def initialize(time: time = DateTime.now, menu: menu = Menu.new )
   @time = time
   @menu = menu
+  @list = []
   end
 
   def request(order)
     "Thank you! Your order will be delivered at #{deadline_set}"
   end
 
-  def deadline_set
+  def list
+    @list.dup
+  end
+  
+  def add(option)
+    choice = option_finder(option)
+    @list << choice
+  end
+
+
+   private
+   def deadline_set
     hour = @time.hour < 23 ? @time.hour + 1 : 0 
     minutes = @time.minute < 10 ? "0#{@time.minute}" : @time.minute    
     "#{hour}:#{minutes}"
    end
 
+   def option_finder(option)
+     @menu.choices.each {|choice| return choice if choice.food == option}
+   end
 
 end
