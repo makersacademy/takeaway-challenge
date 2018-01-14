95bbd3d1014c02ed1c578e7b5bb134f9e85b8ee7
Alex Sergides <32393486+alexse5121@users.noreply.github.com>
2018-01-14 14:18:28 +0000

    second user story

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,12 +1,19 @@
 class Takeaway
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order
   end
   def print_menu
     menu.print
   end
 
+  def place_order(dishes)
+    dishes.each do |food, amount|
+      order.add(food, amount)
+    end
+  end
+
   private
 
-  attr_reader :menu
+  attr_reader :menu, :order
 end
