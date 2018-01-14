9914ca94cf2958f021be5dff1558545b8d32a06f
Tom Grand <thomas.a.grand@gmail.com>
2018-01-14 17:07:01 +0000

    add deadline calculator to order

--- lib/order.rb
+++ lib/order.rb
@@ -3,14 +3,20 @@ require "date"
 
 class Order 
    
-  def initialize(time: time = DateTime, menu: menu = Menu.new )
+  def initialize(time: time = DateTime.now, menu: menu = Menu.new )
   @time = time
   @menu = menu
   end
 
   def request(order)
-    "Thank you! Your order will be delivered at 2:00"
+    "Thank you! Your order will be delivered at #{deadline_set}"
   end
+
+  def deadline_set
+    hour = @time.hour < 23 ? @time.hour + 1 : 0 
+    minutes = @time.minute < 10 ? "0#{@time.minute}" : @time.minute    
+    "#{hour}:#{minutes}"
+   end
 
 
 end
