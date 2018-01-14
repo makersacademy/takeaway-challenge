0a702d2687581731e6804178f75c00ef27319d2d
ealitten <ea.litten@gmail.com>
2018-01-14 13:54:26 +0000

    Typo in menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,7 +2,7 @@ class Menu
 
   DISHES = [
     {name: "Spring rolls", price: 5.50},
-    {name: "Korean Hot wings", price: 5.50},
+    {name: "Korean hot wings", price: 5.50},
     {name: "Prawn crackers", price: 2.00},
     {name: "Chicken katsu curry", price: 9.50},
     {name: "Malaysian coconut curry", price: 6.95},
