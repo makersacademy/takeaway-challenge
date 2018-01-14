3dda28decb4b8a741f6e0b7033219f8628ad0117
Anjana <anjana.lpn@gmail.com>
2018-01-14 15:57:58 +0000

    update menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,9 @@
 class Menu
   attr_reader :meals
 
+  meals = { 'salad' => 5.45, 'burger' => 7.99, 'pizza' => 8.99, 'sandwich' => 3.75,
+          'burrito' => 5, 'pie' => 4, 'quiche' => 3 }
+
   def initialize(meals)
     @meals = meals
   end
@@ -16,5 +19,5 @@ class Menu
   def price(meal)
     meals[meal]
   end
-  
+
 end
