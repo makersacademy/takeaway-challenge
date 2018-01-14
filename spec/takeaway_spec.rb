dec4f10c5c8c34ca1416a0ac5a3e2b398a7c991d
Alexander Sergides <alexse5121@gmail.com>
2018-01-14 22:19:14 +0000

    finished building codebase

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,7 @@
 require 'takeaway'
 
 describe Takeaway do
-  subject(:takeaway) { described_class.new(menu: menu, order: order, sms: sms) }
+  subject(:takeaway) { described_class.new(menu: menu, order: order, sms: sms, config: {}) }
 
   let(:menu) { double(:menu, print: printed_menu)}
   let(:order) { double(:order, total: 43.50) }
