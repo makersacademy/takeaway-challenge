08d89eb3719e131e8a002cfa376c8d24be1397d7
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-14 14:34:02 +0000

    fixes typo on one test and adds a second test for menu class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,8 +4,12 @@ describe Menu do
   subject(:menu) { described_class.new }
 
   describe '#show' do
-    it 'returns a hash' do
+    it 'returns an Array' do
       expect(menu.show).to be_a Array
+    end
+
+    it 'inside the array are hashes' do
+      expect(menu.show).to all(be_a Hash) 
     end
   end
 end
