958f8723f63732de41266a6cc117357a968f2602
Alex Sergides <32393486+alexse5121@users.noreply.github.com>
2018-01-14 19:56:44 +0000

    summing total

--- lib/order.rb
+++ lib/order.rb
@@ -2,12 +2,27 @@ class Order
 
   attr_reader :dishes
 
-  def initialize
+  def initialize(menu)
     @dishes = {}
+    @menu = menu
   end
 
   def add(plate, amount)
     dishes[plate] = amount
   end
+
+  def total
+    item_totals.sum
+  end
+
+  private
+
+  def item_totals
+    dishes.map do |dish, quantity|
+      menu.price(dish) * quantity
+    end
+  end
+
+  attr_reader :menu
 
 end
