d1c8e30042142bf7a1ec30e79ddad4c36308de8d
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-14 14:19:02 +0000

    reads a csv file into an array of hashes, returns that array with #show

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,8 +4,8 @@ describe Menu do
   subject(:menu) { described_class.new }
 
   describe '#show' do
-    it 'prints the menu' do
-      expect { menu.show }.to output(include('Drink: £3.50')).to_stdout
+    it 'returns a hash' do
+      expect(menu.show).to be_a Array
     end
   end
 end
