62f9af8f62df2ee7a331c701c41b0e333cd66c85
Tom Grand <thomas.a.grand@gmail.com>
2018-01-14 10:31:06 +0000

    add read option to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,21 @@
+require "./lib/option"
 
 class Menu
   
-  def initialize 
-    @selection = [{ food: "tofu", price: 5 }, { food: "tofu", price: 5 }, 
-    	{ food: "tofu", price: 5 }, { food: "tofu", price: 5 }, 
-    	{ food: "tofu", price: 5 }]   
+  def initialize(option_type = Option) 
+    @option_type = option_type
+    @selection = [option_type.new, option_type.new, option_type.new, option_type.new,
+                  option_type.new]   
   end
 
   def choices
     @selection.dup
+  end
+
+  def read
+    @selection.each do 
+       |option| puts "#{option.food}, price: #{option.price}" 
+     end
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,6 +1,8 @@
 require "menu"
 
 describe Menu do 
+  let(:option_class) {double :option_class, new: option} 
+  let(:option) {double :option, price: 5, food: "tofu"}
   subject(:menu) { described_class.new }
 
   describe "#choices" do
@@ -11,9 +13,19 @@ describe Menu do
   end
 
   describe "#read" do
-    it "" do
-      expect(menu.choices.length).to be >= 5 
+
+    it "should print out 5 lines of tofu and price of 5" do
+      test_text = "tofu, price: 5\n" * 5
+      expect{menu.read}.to output(test_text).to_stdout
     end
+
+    it "should print out 5 lines of meat and price of 1000" do
+      test_text = "meat, price: 1000\n" * 5
+      allow(option).to receive(:price).and_return(1000)
+      allow(option).to receive(:food).and_return("meat") 
+      expect{menu.read}.to output(test_text).to_stdout
+    end
+
   end
      
 end
