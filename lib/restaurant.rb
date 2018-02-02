5cffc6038b0c42cec0f05807277b7d82e889d322
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-02 23:50:33 +0000

    Init commit

---
+++ lib/restaurant.rb
@@ -0,0 +1,17 @@
+class Restaurant
+
+  MENU = { tofu: 1,
+           quorn: 2,
+           tempeh: 3,
+           jackfruit: 4
+         }
+
+  def initialize
+  end
+
+  def menu
+    MENU
+  end
+
+
+end
---
+++ spec/restaurant_spec.rb
@@ -0,0 +1,17 @@
+require 'restaurant'
+
+describe Restaurant do
+
+  subject(:restaurant) { Restaurant.new}
+
+  it "contains a menu" do
+    expect(Restaurant::MENU).to be_instance_of Hash
+  end
+
+  describe "#menu" do
+    it "returns the menu" do
+      expect(restaurant.menu).to eq Restaurant::MENU
+    end
+  end
+
+end
