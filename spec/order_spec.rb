a47051361500c95b26189c66a1e04b5c6553616e
Hannah Lillis <hannahamanda16@gmail.com>
2018-02-03 18:29:53 +0000

    Refactor to use before hook when testing add method

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -15,49 +15,34 @@ describe Order do
   end
 
   describe '#add' do
-    it "adds item to basket if it's on the menu" do
+
+    before(:each) do
       allow(menu).to receive(:dishes).and_return(test_menu)
       allow(menu).to receive(:includes?).and_return(true)
+    end
+
+    it "adds item to basket if it's on the menu" do
       order.add("Dish_1")
       expect(order.basket).to include("Dish_1" => 1)
     end
 
     it "returns an error if item is not on the menu" do
-      allow(menu).to receive(:dishes).and_return(test_menu)
       allow(menu).to receive(:includes?).and_return(false)
-      expect { order.add("Dish_3") }.to raise_error "This dish is not on the menu"
+      message = "This dish is not on the menu"
+      expect { order.add("Dish_3") }.to raise_error message
     end
 
     it "adds several of the same item at once if specified" do
-      allow(menu).to receive(:dishes).and_return(test_menu)
-      allow(menu).to receive(:includes?).and_return(true)
       order.add("Dish_1", 3)
       expect(order.basket).to eq({ "Dish_1" => 3 })
     end
 
     it 'combines duplicate items in the basket' do
-      allow(menu).to receive(:dishes).and_return(test_menu)
-      allow(menu).to receive(:includes?).and_return(true)
       order.add("Dish_1", 2)
       order.add("Dish_1")
       expect(order.basket).to eq({ "Dish_1" => 3 })
     end
 
-  end
-
-  describe '#includes?' do
-    it 'returns true if an item is already in the basket' do
-      allow(menu).to receive(:dishes).and_return(test_menu)
-      allow(menu).to receive(:includes?).and_return(true)
-      order.add("Dish_1")
-      expect(order.order_includes?("Dish_1")).to be true
-    end
-
-    it 'returns false if item is not already in the basket' do
-      allow(menu).to receive(:dishes).and_return(test_menu)
-      allow(menu).to receive(:includes?).and_return(true)
-      expect(order.order_includes?("Dish_1")).to be false
-    end
   end
 
 end
