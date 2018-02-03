c51554c8eac42b3cbba1ec227a00f0a4b861068d
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-03 18:01:02 +0000

    Customer can select 1 or more dishes, currently strings. To be refactored into dish objects

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -16,4 +16,15 @@ describe Order do
     expect(subject.current_order).to eql(["Beef Burger", "Cheese Burger"])
   end
 
+  it "Allows the customer to select a dish" do
+    subject.select_dish(dish_1)
+    expect(subject.current_order).to eql(dish_1)
+  end
+
+  it "Allows the customer to select multiple dishes" do
+    subject.select_dish(dish_1)
+    subject.select_dish(dish_2)
+    expect(subject.current_order).to eql([dish_1], [dish_2])
+  end
+
 end
