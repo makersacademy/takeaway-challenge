e5d5e70b5f7fb1aee12bb9a81d35d811d984bc47
Terry Allitt <tallitt88@gmail.com>
2018-02-03 13:22:20 +0000

    Store menu items in a hash

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,18 @@
 class Menu
 
+  attr_reader :meals
+
+  def initialize
+    @meals = {
+      pappadom: 0.50,
+      chutney: 0.50,
+      samosa: 2.25,
+      curry: 6.50,
+      tandoori_grill: 8.95,
+      veg_side: 2.75,
+      rice: 1.95,
+      naan: 1.80
+    }
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,27 @@
 require 'menu'
 
 describe Menu do
-  
+
+  subject(:menu) { described_class.new }
+
+  it 'stores meals as an instance variable' do
+    expect(menu.instance_variable_defined?(:@meals)).to be true
+  end
+
+  describe '#meals' do
+    it 'stores meals as a hash' do
+      meals = {
+        pappadom: 0.50,
+        chutney: 0.50,
+        samosa: 2.25,
+        curry: 6.50,
+        tandoori_grill: 8.95,
+        veg_side: 2.75,
+        rice: 1.95,
+        naan: 1.80
+      }
+      expect(menu.meals).to include meals
+    end
+  end
+
 end
