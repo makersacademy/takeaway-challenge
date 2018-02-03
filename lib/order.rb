d25bf5a4b76c366e6cbef0c80d63694ceb607fc4
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-03 10:02:58 +0000

    Add Order#select_menu_items() and tests.

--- lib/order.rb
+++ lib/order.rb
@@ -1,2 +1,13 @@
 class Order
+
+  attr_reader :selection, :estimated_total
+
+  def initialize
+  end
+
+  def select_menu_items(selection, estimated_total)
+    @estimated_total = estimated_total
+    @selection = selection
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,21 @@ require 'order'
 
 describe Order do
 
+  subject(:order)      { Order.new }
+  let(:selection)      { double "a hash of selections" }
+  let(:estimated_total) { double "the estimated cost" }
+
+
+  describe "#select_menu_items" do
+    it "stores the estimated total in @estimated_total" do
+      expect{order.select_menu_items(selection, estimated_total)}
+        .to change{ order.estimated_total }.to estimated_total
+    end
+
+    it "stores the selection in @selection" do
+      expect{order.select_menu_items(selection, estimated_total)}
+        .to change{ order.selection }.to selection
+    end
+  end
+
 end
