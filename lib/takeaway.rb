e17a09a8ea2de0e813e98f250e1712f85f18c0c4
Serena Hathi <hathi.serena@gmail.com>
2018-02-03 14:07:12 +0000

    Ensure that basket updates each time a new dish is added

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -5,13 +5,16 @@ class Takeaway
 
   attr_reader :menu, :order
 
+  def initialize
+    @order = Order.new
+  end
+
   def see_menu
     @menu = Menu.new
     @menu.print
   end
 
   def add_to_order(dish, quantity = 1)
-    @order = Order.new
     @order.add(dish, quantity)
   end
 
