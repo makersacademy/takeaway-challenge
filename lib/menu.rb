506ab72dca84466561b83830c217f01ab50eb053
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-03 21:02:26 +0000

    Select items and update balance OK. Also tested twilio api, working fine, removed until needed. Next: make sure amount matches with selection and verify order

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,13 +1,19 @@
 class Menu
+  attr_reader :selection, :amount
+
   def initialize
     @menu = {
       'Nutella on rice cakes and banana on top' => 3,
-      'Scrumbled eggs' => 4.99,
+      'Scrambled eggs' => 4.99,
       'Roasted aubergine & tomato curry' => 5.60,
       'Jacket Potato with whipped feta' => 8.25,
       'Mushroom, ricotta & rocket tart' => 9.20,
       'Cod with beans & artichokes' => 11.40,
     }
+
+    @selection = []
+    
+    @amount = 0
   end
 
   def list
@@ -17,5 +23,15 @@ class Menu
       puts "#{@item_n}. Dish: #{dish} - Price: #{price}"
       @item_n += 1
     end
+  end
+
+  def select(dish, quantity = 1)
+    @menu.select do |k, v|
+      if k == dish
+        @selection << k + " (#{quantity})"
+        @amount += v * quantity
+      end
+    end
+    @selection
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,20 +2,39 @@ require 'menu'
 
 describe Menu do
 
-  describe "#menu output" do
-    default_menu = {
-      'Nutella on rice cakes and banana on top' => 3,
-      'Scrumbled eggs' => 4.99,
-      'Roasted aubergine & tomato curry' => 5.60,
-      'Jacket Potato with whipped feta' => 8.25,
-      'Mushroom, ricotta & rocket tart' => 9.20,
-      'Cod with beans & artichokes' => 11.40,
-    }
+  let(:quantity) { 6 }
+  let(:dish) { 'Scrambled eggs' }
 
+  default_menu = {
+    'Nutella on rice cakes and banana on top' => 3,
+    'Scrambled eggs' => 4.99,
+    'Roasted aubergine & tomato curry' => 5.60,
+    'Jacket Potato with whipped feta' => 8.25,
+    'Mushroom, ricotta & rocket tart' => 9.20,
+    'Cod with beans & artichokes' => 11.40,
+  }
+
+  describe "#menu output" do
+    # Vacuous test
     it { is_expected.to respond_to :list }
 
     it 'should output a list with dishes and prices' do
       expect(subject.list).to eq default_menu
+    end
+  end
+
+  describe "#select dish(es)" do
+    # Vacuous test
+    it { is_expected.to respond_to(:select).with(2).argument }
+
+    it 'should select the right number of dishes as per user choice' do
+      expect(subject.select(dish, quantity)).to eq [dish + " (#{quantity})"]
+    end
+
+    it 'should update the current amount as per the dishes selected' do
+      subject.select(dish, quantity)
+
+      expect(subject.amount).to eq 29.94
     end
   end
 end
