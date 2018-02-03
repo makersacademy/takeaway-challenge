16591acc3fb16f47dbcb2321c4d23661ab51f45f
Terry Allitt <tallitt88@gmail.com>
2018-02-03 14:40:13 +0000

    Create class  to have instance variable

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,7 +4,7 @@ describe Menu do
 
   subject(:menu) { described_class.new }
 
-  it 'stores meals as an instance variable' do
+  it 'it has an instance variable `meals`' do
     expect(menu.instance_variable_defined?(:@meals)).to be true
   end
 
