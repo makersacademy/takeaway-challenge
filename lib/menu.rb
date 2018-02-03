0e006e78ef486a589f4d6ce15e4618b3e80b27f4
Daniel Keen <keendaniel01@gmail.com>
2018-02-03 14:44:49 +0000

    Can sum selected dishes price

--- lib/menu.rb
+++ lib/menu.rb
@@ -17,4 +17,12 @@ class Menu
     @selected << { dish => nil }
     @selected.last[dish] = @dishes[dish]
   end
+
+  def sum
+    @sum = 0
+    @selected.each do |dish|
+      dish.each_value { |price| @sum += price }
+    end
+    @sum
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,6 +3,11 @@ require "menu"
 describe Menu do
 
   subject(:menu) { described_class.new }
+  subject(:order) do
+    menu.select(:pizza)
+    menu.select(:red_wine)
+    menu
+  end
 
   it 'contains a list of dishes' do
     expect(menu.dishes).to eq({
@@ -18,6 +23,12 @@ describe Menu do
     it 'allows selection of a dish' do
       menu.select(:pizza)
       expect(menu.selected).to eq [{ pizza: 10 }]
+    end
+  end
+
+  describe '#sum' do
+    it 'adds the total prices of the selected items' do
+      expect(order.sum).to eq 14
     end
   end
 end
