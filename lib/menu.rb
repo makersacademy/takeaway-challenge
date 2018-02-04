92438583cbfdf302c20d1a510f7e769417b27afb
Noel Vock <noel1uk@gmail.com>
2018-02-04 13:30:12 +0000

    added test for new display object and created display related files

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,12 @@
 class Menu
 
-  attr_reader :items
+  attr_reader :items,
+              :shopping_cart,
+              :display
 
-  def initialize
+  def initialize(display=Display.new)
     @items = { 'hu soup' => '4.99' }
+    @shopping_cart = []
+    @display = items
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -8,5 +8,11 @@ describe Menu do
     it "should have an items hash" do
       expect(menu.items).to be_a(Hash)
     end
+    it "creates a new display object" do
+      expect(menu.display).to eq Display.new
+    end
+    xit "creates an empty array for shopping cart" do
+      expect(menu.shopping_cart). to eq []
+    end
   end
 end
