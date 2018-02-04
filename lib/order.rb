cd3ce48ecfa16a500c89e607ecfcb7ea42cc4ad3
Ben <ben.challenor@btopenworld.com>
2018-02-04 17:59:17 +0000

    Added delete a dish and quantity from the basket

--- lib/order.rb
+++ lib/order.rb
@@ -3,18 +3,33 @@ require_relative "menu"
 class Order
 
 
-attr_reader :basket, :menu
+attr_reader :menu
+attr_accessor :basket
 
 def initialize(menu = Menu.new)
   @basket = []
   @menu = menu
 
+
 end
 
-def add(dish, quantity)
+def add(dish, quantity = 1)
   raise "This dish is not on the menu" unless @menu.menu.has_key?(dish)
-  @basket << {dish => quantity}
+  @basket << { :dish => dish, :quantity => quantity }
 end
+
+def delete(dish, quantity = 1)
+    @basket.delete({ :dish => dish, :quantity => quantity })
+end
+
+def total
+    total = 0
+    @basket.each { |dish| total += @menu.menu[dish[:dish]] * dish[:quantity] }
+    total
+end
+
+
+
 
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,6 +4,7 @@ describe "order" do
 
   let(:order) { Order.new }
 
+
   describe "#initialize" do
     it "should create an empty basket" do
       expect(order.basket).to be_empty
@@ -13,7 +14,7 @@ describe "order" do
   describe "#add" do
     it "should add a dish and the quantity to basket" do
       order.add("Pepperoni Pizza", 2)
-        expect(order.basket).to eq [{"Pepperoni Pizza"=>2}]
+        expect(order.basket).to eq [{:dish=>"Pepperoni Pizza", :quantity=>2}]
     end
 
     it "should raise error if the item is not on the menu" do
@@ -21,6 +22,23 @@ describe "order" do
     end
 
   end
+
+  describe "#total" do
+    it "should give the total price of the basket" do
+      order.add("Fish & Chips", 2)
+      order.add("Ceasar Salad", 3)
+      expect(order.total).to eq 17
+    end
+  end
+
+  describe "#delete" do
+    it "should delete a dish and quantity from the basket" do
+      order.add("Fish & Chips", 2)
+      order.delete("Fish & Chips", 1)
+      expect(order.basket).to eq [{:dish=>"Fish & Chips", :quantity=>1}]
+    end
+  end
+
 
 
 
