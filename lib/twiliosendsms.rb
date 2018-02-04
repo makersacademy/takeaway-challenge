d6c7ab421871b2a8b9323c4bc2ef5f233e8b9c6f
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-04 20:57:36 +0000

    minor refactor of twiliosendsms.rb

--- lib/twiliosendsms.rb
+++ lib/twiliosendsms.rb
@@ -4,13 +4,14 @@ class TwilioSendSMS
 
   ACCOUNT_SID = ENV["TWILIO_ACCOUNT_SID"]
   AUTH_TOKEN = ENV["TWILIO_AUTH_TOKEN"]
+  TWILIO_TEL_NUMBER = ENV["TWILIO_TEL_NUMBER"]
 
   def self.send_sms(to_phone_number)
     client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
     client.messages.create(
       body: message_body,
       to: to_phone_number,
-      from: '+447403932186',
+      from: TWILIO_TEL_NUMBER,
       )
   end
 
