9b97d1e093bd244a0b73a9325650cfc0b3db85e5
Stephen Tilley <stephentilley@gmail.com>
2018-02-04 14:51:10 +0000

    menu class contains a hash of dishes and has a print_menu method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,21 @@
 class Menu
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = {
+      1 => ["Hui Guo Rou          ", 4.25],
+      2 => ["Yu Xiang Qie Zi      ", 2.50],
+      3 => ["Gan Guo Hua Cai      ", 4.00],
+      4 => ["Gong Bao Ji Ding     ", 5.75],
+      5 => ["Mapo DouFu           ", 3.00]
+    }
+  end
+
+  def print_menu
+    @dishes.each do |key, value|
+      puts "#{key}: #{value[0]} £#{'%.2f' % value[1]}"
+    end
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,18 @@ require 'menu'
 
 describe Menu do
 
+  let(:menu) { described_class.new }
+
+  context "#initialize" do
+    it "dishes is a hash of available dishes and prices" do
+      expect(menu.dishes).to be_a Hash
+    end
+  end
+
+  context "#print_menu" do
+    it "print_menu prints the menu" do
+      expect { menu.print_menu }.to output.to_stdout
+    end
+  end
+
 end
