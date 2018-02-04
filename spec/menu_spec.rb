53d67413740bcf28eaad94ced7ac57cc5467e39f
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-04 15:36:51 +0000

    Menu printed products correctly

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -12,16 +12,14 @@ require "menu"
    let(:product_1) {double("Beef burger", number: 1, name: "Beef burger", price: 10, quantity: 1)}
    let(:product_2) {double("Cheese burger", number: 2, name: "Cheese burger", price: 12, quantity: 1)}
 
-   # let(:printed_menu) {
-   #  "Beef burger: £10",
-   #  "Cheese burger: £12"
-   # }
+   let(:printed_menu) do
+     "Dish number: 1. Dish name: Beef burger. Price: £10\nDish number: 2. Dish name: Cheese burger. Price: £12\n"
+   end
 
    it {is_expected.to respond_to(:print_menu) }
 
    it 'Prints the menu with prices' do
      menu.print_menu
-     p printed_menu
      expect{ menu.print_menu }.to output(printed_menu).to_stdout
    end
 
