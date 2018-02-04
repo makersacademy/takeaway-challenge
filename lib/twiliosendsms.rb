e8e84e96e2b976a18f0cc6cbaa07ce7ca4b76c5c
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-04 20:57:36 +0000

    minor refactor of twiliosendsms.rb

--- lib/twiliosendsms.rb
+++ lib/twiliosendsms.rb
@@ -4,19 +4,21 @@ class TwilioSendSMS
 
   ACCOUNT_SID = ENV["TWILIO_ACCOUNT_SID"]
   AUTH_TOKEN = ENV["TWILIO_AUTH_TOKEN"]
+  TWILIO_TEL_NUMBER = ENV["TWILIO_TEL_NUMBER"]
 
   def self.send_sms(to_phone_number)
     client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
     client.messages.create(
       body: message_body,
       to: to_phone_number,
-      from: '+447403932186',
+      from: TWILIO_TEL_NUMBER,
       )
   end
 
   private
 
   def self.message_body
-    "Thank you for your order. It will be delivered before #{Time.now.strftime("%H:%M")}"
+    delivery_time = Time.now + 60 * 60
+    "Thank you for your order. It will be delivered before #{delivery_time}"
   end
 end
