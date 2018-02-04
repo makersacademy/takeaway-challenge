65c7a21b17117b646da94aaa04429fcd1dfbc443
Terry Allitt <tallitt88@gmail.com>
2018-02-04 22:53:14 +0000

    Modify init.rb to launch programme in command line

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ APP_ROOT = File.dirname(__FILE__)
 $:.unshift(File.join(APP_ROOT, 'lib'))
 require 'take_away'
 
-# take_away_app = TakeAwayApp.new
-#
-# take_away_app.launch!
+take_away_app = TakeAwayApp.new
+
+take_away_app.launch!
