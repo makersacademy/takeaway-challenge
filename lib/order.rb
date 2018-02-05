bb4e949d6d323a70422f10fec963640037de35f5
Laura Chan <laurachan205@gmail.com>
2018-02-05 11:32:27 +0000

    Update with environment variables as twilio credentials

--- lib/order.rb
+++ lib/order.rb
@@ -70,8 +70,8 @@ class Order
   end
 
   def run_twilio(user_phone_number, message)
-    account_sid = "xxx"
-    auth_token = "xxx"
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
 
     @client = Twilio::REST::Client.new account_sid, auth_token
     @client.messages.create(
