d476d2d78b178ec8ee0e5537ca629bdb5afa5adf
Daniel Keen <keendaniel01@gmail.com>
2018-02-05 10:17:49 +0000

    Added hidden data

--- lib/phone.rb
+++ lib/phone.rb
@@ -1,14 +1,16 @@
 require "twilio-ruby"
+require "dotenv"
+Dotenv.load('hidden_data.env')
 
 class Phone
   def send_text
-    account_sid = "AC0c433944cbed788826573b3f2679ea2e"
-    auth_token = "f6b2b0bcfddbc631aa3de7f35b763b9c"
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
 
     @client = Twilio::REST::Client.new account_sid, auth_token
     @client.messages.create(
       body: "Thankyou! Your order was placed and will be delivered before 19:45",
-      to: "+447922027441",
-      from: "+441242650204")
+      to: ENV["MY_NUMBER"],
+      from: ENV["TWILIO_NUMBER"])
   end
 end
