bd911d9ecdfdbf7b72ee55442641c5c532b17320
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-03 19:18:03 +0000

    Added quantity attribute to menu items

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,29 +3,34 @@ class Menu
 
   def initialize
     @current_menu = [
+      # can move items into a seperate csv file
       {
         category: 'Starters',
         name: 'HFC 6 Hotwings Meal',
         price: 5.00,
-        description: 'comes with 6 wings, served with a regular drink and fries'
+        description: 'includes 6 wings, served with a regular drink and fries',
+        quantity: 0
       },
       {
         category: 'Mains',
         name: 'Fish & Chips',
         price: 28.45,
-        description: 'served with chunky chips and tomato sauce'
+        description: 'served with chunky chips and tomato sauce',
+        quantity: 0
       },
       {
         category: 'Desert',
         name: 'Chocolate Brownie with Ice Cream',
         price: 5.95,
-        description: 'served with hazelnut ice cream'
+        description: 'served with hazelnut ice cream',
+        quantity: 0
       },
       {
         category: 'Sides',
         name: 'Tossed Salad',
         price: 3.50,
-        description: 'served with a light drizzling of balsamic vinegar'
+        description: 'served with a light drizzling of balsamic vinegar',
+        quantity: 0
       }
     ]
   end
