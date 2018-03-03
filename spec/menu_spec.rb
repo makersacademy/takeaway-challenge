d6bd7d258c3a5960162d636e38fc83e7e808e5d9
danielcampos <ddregalo@gmail.com>
2018-03-03 10:47:40 +0000

    Pass test for menu items array initialize

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,3 +1,12 @@
+require 'menu'
+
 describe Menu do
-  
+
+  it 'Should initialize with menu array' do
+    expect(subject.menu).to eq [
+      { item: "Devil's Cauliflower", price: 4 },
+      { item: "Rainbow Pot", price: 3 },
+      { item: "Oreo Brownie", price: 3 },
+    ]
+  end
 end
