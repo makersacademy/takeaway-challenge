a139b1152feb86e4dd2d82c3dded8f7e6237aa4f
Somayeh <mail.somayeh@gmail.com>
2018-03-04 17:15:54 +0000

    can order some number of dishes

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,18 @@
 require 'takeaway'
 describe Takeaway do
-  subject(:takeaway) { described_class.new(menu: menu) }
+
+  subject(:takeaway) { described_class.new(menu: menu, order: order) }
   let(:menu) { double(:menu, print: printed_menu) }
   let(:printed_menu) { "pasta: £4.30" }
+  let(:order) { double(:order) }
+  let(:dishes) { { pizza: 2, pasta: 1 } }
+
   it 'shows the menu with the dishes and pices' do
-    expect(subject.print_menu).to eq(printed_menu)
+    expect(takeaway.print_menu).to eq(printed_menu)
+  end
+
+  it 'can order some number of several available dishes' do
+    expect(order).to receive(:add).twice
+    takeaway.place_order(dishes)
   end
 end
