2dd6b01c1f8ff7160c966550766bc3b3d35e1eee
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-04 12:15:18 +0000

    removed quantity from menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,32 +4,32 @@ class Menu
   # move current menu into seperate csv file
   CURRENT_MENU = [
     {
+      id: 1,
       category: 'Starters',
       name: 'HFC 6 Hotwings Meal',
       price: 5.00,
       description: 'includes 6 wings, served with a regular drink and fries',
-      quantity: 0
     },
     {
+      id: 2,
       category: 'Mains',
       name: 'Fish & Chips',
       price: 28.45,
       description: 'served with chunky chips and tomato sauce',
-      quantity: 0
     },
     {
+      id: 3,
       category: 'Desert',
       name: 'Chocolate Brownie with Ice Cream',
       price: 5.95,
       description: 'served with hazelnut ice cream',
-      quantity: 0
     },
     {
+      id: 4,
       category: 'Sides',
       name: 'Tossed Salad',
       price: 3.50,
       description: 'served with a light drizzling of balsamic vinegar',
-      quantity: 0
     }
   ]
 
