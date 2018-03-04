ecd9b45a226d5bc5e3a4f31ed4ace7d7630cd7dd
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-04 17:25:04 +0000

    minor refactored rspecs

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,6 +2,6 @@ require 'menu'
 
 describe Menu do
   it 'holds a hash of items' do
-    expect(subject.menu_list).to include {}
+    expect(subject.menu_list).to include({})
   end
 end
--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -1,9 +1,16 @@
 require 'receipt'
 
 describe Receipt do
+  #let(:menu) {double('menu', :menu_list => {:pizza => 8, :cheese_burger => 7})}
+  subject {described_class.new('pizza', 7,)} # add double
   describe '#print_receipt' do
     it 'returns the order and prices' do
-    #  expect(subject.print_receipt).to eq "pizza - £7"
+      # expect(subject.print_receipt).to eq 'pizza - £7'
+    end
+  end
+  describe '#print_header' do
+    it 'prints a header' do
+      expect(subject.print_header).to eq 'You ordered:'
     end
   end
 end
