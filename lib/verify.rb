3fdbac386fd75ca433369800692c73118e0d0057
danielcampos <ddregalo@gmail.com>
2018-03-05 13:22:59 +0000

    Updated Verify class file with ENV variables fixed

---
+++ lib/verify.rb
@@ -0,0 +1,33 @@
+require 'twilio-ruby'
+require 'dotenv/load'
+
+class Verify
+
+  def initialize
+    account_sid = ENV['TWILIO_ACCOUNT_SID'] # Account SID from www.twilio.com/console
+    auth_token = ENV['TWILIO_TOKEN']   # Auth Token from www.twilio.com/console
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @time = (Time.now + 1*60*60).strftime('%r')
+  end
+
+  def verify_order
+    puts ""
+    puts ""
+    puts "PLEASE CONFIRM YOUR ORDER AND TOTAL"
+    puts
+    puts "Press Y to confirm / Press N to edit order"
+    `say PLEASE CONFIRM YOUR ORDER AND TOTAL`
+    user_confirm = gets.chomp.downcase
+    send_delivery_confirmation if user_confirm == "y"
+  end
+
+  def send_delivery_confirmation
+
+
+    message = @client.messages.create(
+      to: ENV['MY_MOBILE'],
+      from: ENV['MY_TWILIO_NUMBER'],
+      body: "Thank you for your order. Your delicious food will arrive an hour from #{@time}")
+    message.sid
+  end
+end
