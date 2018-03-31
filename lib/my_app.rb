564981220cd017c37b94c2b09f37fb5fbf855d2e
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-03-31 14:43:19 +0100

    test post request

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require_relative 'meal_list'
+require_relative 'meal'
+require_relative 'order'
 
 get '/' do
   'Hello world!'
+end
+
+post '/' do 
+  body = request.params["Body"]
+  puts body
 end
