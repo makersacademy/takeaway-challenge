7083f17dfd6dab6c4214cfa011eaed8294a577a8
Daniel Miller <danielmiller5791@gmail.com>
2018-03-31 20:22:26 +0100

    3 done

--- w2-dm/lib/customer.rb
+++ w2-dm/lib/customer.rb
@@ -1,7 +1,7 @@
 class Customer
 
-  attr_reader :selected_items, :total
-  
+  attr_reader :selected_items, :total, :user_cost
+
   def initialize
     @menu = Menu.new
     @selected_items = []
@@ -12,34 +12,41 @@ class Customer
     @menu.print
   end
 
-  def select
-    @menu.options.each do |item, price|
-      @item = item
-      @price = price.to_i
-      select_item
-    end
+  def select(user_cost)
+    @user_cost = user_cost
+    list_items
     @selected_items
   end
 
   def show_total
-    calculate
+    raise "Incorrect amount stated" if @user_cost != calculate
+    @total = calculate
+    @total
   end
 
 end
 
 private
 
+  def list_items
+     @menu.options.each do |item, price|
+      @item = item
+      @price = price.to_i
+      select_item
+    end
+  end
+
   def select_item
     puts "How many of each #{@item} do you want"
     quant = gets.chomp.to_i
-    temp = {quant => @price}
-    @selected_items << temp
+    @selected_items << {quant => @price}
   end
 
   def calculate
+    total = 0
     @selected_items.each do | hash |
-      hash.each_pair do | k, v | @total += (k * v) end
+      hash.each_pair do | k, v | total += (k * v) end
     end
-    @total
+    total
   end
 
--- w2-dm/spec/customer_spec.rb
+++ w2-dm/spec/customer_spec.rb
@@ -3,10 +3,7 @@ require 'app'
 describe 'Customer' do
 
   subject(:customer) {Customer.new}
-  subject(:menu) {double(
-  :menu, 
-  :options => {:Pizza => 10, :Chips => 5, :Fish => 12, :Salad => 9, :Burger => 9},
-  )}
+  subject(:menu) {double( :menu, :options => {:Pizza => 10, :Chips => 5, :Fish => 12, :Salad => 9, :Burger => 9},)}
 
   describe "#show_menu" do
     it "prints the menu for the customer" do
@@ -17,15 +14,22 @@ describe 'Customer' do
   describe "#select" do
     #requires input from the user
     it "allows the customer to select items from the menu and store them" do
-      expect(customer.select).to eq customer.selected_items
+      expect(customer.select(20)).to eq customer.selected_items
     end
   end 
 
   describe "#show_total" do
-    it "prints the total price of the order for the user" do
-      expect(customer.show_total).to eq customer.total
+    it "raises an error is the user enters amount that's not equal to total" do
+      customer.stub(:total) {100}
+      customer.stub(:user_cost) {50}
+      expect {customer.show_total}.to raise_error "Incorrect amount stated" 
     end
-  end
 
+    it "prints the total price of the order for the user" do
+      customer.stub(:user_cost) {45}
+      customer.stub(:total) {45}
+      expect(customer.user_cost).to eq customer.total
+    end 
+  end
 
 end
