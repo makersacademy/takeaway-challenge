6675f3254741430e4af27d72631d4a3c5e2ecab9
Matthew <matt_burgoyne@live.co.uk>
2018-03-31 19:45:30 +0100

    test written and passed for an item be able to be added to an order_list

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,10 +1,19 @@
 require 'order'
 describe Order do
   subject(:order) { described_class.new }
+  let(:menu_item){ double('item') }
   describe '#initialize' do
 
     it "has an empty array of items on initialization" do
       expect(order.order_items).to be_empty
+    end
+
+  end
+
+  describe '#save_item' do
+
+    it "saves an item to the order list" do
+      expect(order.save_item(menu_item)).to contain_exactly menu_item
     end
 
   end
