a4735380b5eedd95e16c9666ba5615d6c9edc159
Daniel Miller <danielmiller5791@gmail.com>
2018-04-01 13:35:53 +0100

    refactored

--- w2-dm/lib/phone.rb
+++ w2-dm/lib/phone.rb
@@ -4,17 +4,17 @@ class Phone
     @account_sid = 'AC743acc5594ff64f05dc907422f204b4d'
     @auth_token = '63741f91cb6b9143d0d1ba70e6fef95e'
     @client = Twilio::REST::Client.new @account_sid, @auth_token
+    @from = '+441173252750'
+    @to = '+447923174752'
   end
 
   def send_confirmation
-    from = '+441173252750'
-    to = '+447923174752'
     h = Time.now.strftime('%H').to_i + 1
     m = Time.now.strftime('%M').to_i
     body = "Your order will be placed at #{h}:#{m}"
     @client.api.account.messages.create(
-      from: from,
-      to: to, 
+      from: @from,
+      to: @to, 
       body: body
     )
   end
