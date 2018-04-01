92f7a862b6dfad5a30f95174bcd52f736e630499
Matthew <matt_burgoyne@live.co.uk>
2018-04-01 13:33:59 +0100

    refactored code into two private methods

--- lib/order.rb
+++ lib/order.rb
@@ -9,16 +9,32 @@ class Order
 
   def save_items(menu)
     loop do
-      puts "What would you like to order? Press return to finish"
-      selection = gets.chomp
+      selection = get_selection
       break if selection.empty?
-      selection = selection.gsub(/\s+/,'_')
-      raise "We don't currently serve this dish!" if menu.options[selection.downcase.to_sym].nil?
+
+      raise "We don't currently serve this dish!" if menu.options[selection].nil?
+
       puts "How many would you like?"
-      quantity = gets.chomp
-      @order_items[selection.to_sym] = quantity.to_i
+      quantity = get_quantity
+      @order_items[selection] = quantity
     end
     'order noted'
+  end
+
+
+  private
+
+  def get_selection
+    puts "What would you like to order? Press return to finish"
+    selection = gets.chomp
+    selection = selection.gsub(/\s+/,'_').downcase.to_sym
+
+  end
+
+  def get_quantity
+    puts "How many would you like?"
+    quantity = gets.chomp.to_i
+
   end
 
 
