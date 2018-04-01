b476f6e01e0435c28de59d78ec8d712b2ebaa967
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-01 10:57:52 +0100

    Third user story with test

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -2,11 +2,13 @@ class TakeAway
 
   attr_reader :menu
   attr_reader :basket
+  attr_reader :total
 
   def initialize
     @menu = { "Goi Ga" => 5.99, "Cha Gio" => 4.99,
     "Vietnamese dumpling" => 2.99, "peking duck" => 5.00, "Pho Bo" => 6.99 }
     @basket = []
+    @total = 0
   end
 
   def read_menu
@@ -15,7 +17,12 @@ class TakeAway
 
   def order(dish, amount)
     basket << "#{dish} X #{amount} = £#{menu[dish] * amount}"
+    @total += (menu[dish] * amount)
     "#{amount}X #{dish} added to your basket"
+  end
+
+  def receipt
+    "#{basket.join("\n")}\n Total = #{total}"
   end
 
 end
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -16,4 +16,11 @@ describe TakeAway do
     subject.order('Goi Ga', 2)
     expect(subject.basket).to include("Goi Ga X 2 = £11.98")
   end
+  describe '#receipt' do
+    it "shows whole ordered list" do
+      subject.order('Goi Ga', 2)
+      subject.order('peking duck', 3)
+      expect(subject.receipt).to eq("Goi Ga X 2 = £11.98\npeking duck X 3 = £15.0\n Total = 26.98")
+    end
+  end
 end
