4160f21074ae98e7446558f3e5a80d519d80a1a2
Andrew Kemp <akemp850@gmail.com>
2018-04-02 16:24:21 +0100

    updated menu tests

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,11 +2,11 @@ require 'menu'
 
 describe Menu do
   subject(:menu) { Menu.new }
-  let(:dishes) { { "Margherita"=> 7.00, "Pepperoni"=> 7.50, "Ham & Pineapple"=> 7.50, "Vegetarian"=> 7.00, "Meat Feast"=> 8.00, "Chicken Supreme"=> 8.00, "Veggie Supreme"=> 8.00, "Ham & Mushroom"=> 7.25 } }
 
   describe '#readable_menu' do
     it 'shows the dishes available' do
-      expect(menu.readable_menu).to eq(dishes)
+      readable_presentation = "Margherita pizza : £7.00, Pepperoni pizza : £7.50, Ham & Pineapple pizza : £7.50, Vegetarian pizza : £7.00, Meat Feast pizza : £8.00"
+      expect(menu.readable_menu).to eq(readable_presentation)
     end
   end
 
