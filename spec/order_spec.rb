220706f87f1462a2dc1d24d132fffb9e952d4c27
Irbe Krumina <irbekrm@gmail.com>
2018-04-02 16:58:21 +0100

    refactor

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,70 +2,62 @@ require 'order'
 
 describe Order do
   
-  subject { described_class.new }
+  subject(:order) { described_class.new }
+  subject(:empty_order) { described_class.new }
   
   before(:each) do
-    @order_item_class = class_double("OrderItem")
     @order_item = instance_double("OrderItem")
-    @dish = instance_double("Dish")
-    allow(@dish).to receive(:describe) { "Fake dish" }
     allow(@order_item).to receive(:description) { "Fake dish x 3" }
     allow(@order_item).to receive(:price) { 9.99 }
     allow(@order_item).to receive(:id) { 'A8888' }
+    allow(OrderItem).to receive(:new).with("Fake dish", 3).and_return(@order_item)
+    order.add_item "Fake dish", 3
   end
 
   it 'is initialized with state :in_progress' do
-    expect(subject.state).to eq :in_progress
+    expect(order.state).to eq :in_progress
   end
 
   describe '#add_item' do
     it 'creates new order item with the given dish and quantity' do
-      allow(OrderItem).to receive(:new).with("Fake dish", 3).and_return(@order_item)
-      subject.add_item "Fake dish", 3
-      expect(subject.instance_variable_get(:@items)).to eq [@order_item]
+      empty_order.add_item "Fake dish", 3
+      expect(empty_order.instance_variable_get(:@items)).to eq [@order_item]
     end 
 
     it 'returns an info string' do
-      allow(OrderItem).to receive(:new).with("Fake dish", 3).and_return(@order_item)
-      expect(subject.add_item "Fake dish", 3).to eq "Added to order: Fake dish x 3"
+      expect(order.add_item "Fake dish", 3).to eq "Added to order: Fake dish x 3"
     end
   end
 
   describe '#remove_item' do
     it 'raises an error if item not found' do
-      allow(OrderItem).to receive(:new).with(@dish, 3).and_return(@order_item)
-      subject.add_item @dish, 3
-      expect { subject.remove_item 'ABCDE' }.to raise_error Shop::MESSAGES[:not_found]
+      expect { order.remove_item 'ABCDE' }.to raise_error Shop::MESSAGES[:not_found]
     end
 
   it 'removes a matching item' do
-    allow(OrderItem).to receive(:new).with(@dish, 3).and_return(@order_item)
-    subject.add_item @dish, 3
-    subject.remove_item 'A8888'
-    expect(subject.instance_variable_get(:@items)).not_to include(@order_item)
+    order.remove_item 'A8888'
+    expect(order.instance_variable_get(:@items)).not_to include(@order_item)
   end
   end
       
   describe '#calculate_total' do
     it 'returns the total of the order' do
-      allow(OrderItem).to receive(:new).with("Fake dish", 3).and_return(@order_item)
-      subject.add_item "Fake dish", 3
-      expect(subject.calculate_total).to eq 9.99
+      empty_order.add_item "Fake dish", 3
+      expect(order.calculate_total).to eq 9.99
     end
   end
 
   describe '#complete!' do
     it 'sets the state of order to :complete' do
-      expect { subject.complete! }.to change { subject.state }.from(:in_progress).to(:complete)
+      expect { order.complete! }.to change { order.state }.from(:in_progress).to(:complete)
     end
   end
 
   describe '#describe' do
     it 'passes info to OrderPrinter, returns a string with order info' do
-      subject.instance_variable_set(:@items, [@order_item])
-      message = "Order summary\n#{subject.instance_variable_get(:@items).map(&:description).join("\n")}\nTotal: #{subject.calculate_total}\nState: #{subject.state}"
+      message = "Order summary\n#{order.instance_variable_get(:@items).map(&:description).join("\n")}\nTotal: #{order.calculate_total}\nState: #{order.state}"
       allow_any_instance_of(OrderPrinter).to receive(:pretty_print).and_return(message)
-      expect(subject.describe).to eq message
+      expect(order.describe).to eq message
     end
   end
 end
