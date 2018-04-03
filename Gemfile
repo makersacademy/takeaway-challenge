8b2c19407a5970e535e3715b91be6690264d2d3e
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-03 22:11:18 +0100

    07: sms | class created & gem set up

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ gem 'rspec'
 gem 'rubocop'
 gem 'simplecov', require: false, group: :test
 gem 'simplecov-console', require: false, group: :test
+gem 'twilio-ruby', '~> 5.7.2'
+gem 'dotenv-rails', groups: [:development, :test]
--- lib/sms.rb
+++ lib/sms.rb
@@ -1,1 +1,22 @@
+require 'rubygems'
 require 'twilio-ruby'
+require 'dotenv/load'
+
+class Sms
+
+  def initialize
+    @client = Twilio::REST::Client.new(
+      ENV['ACCOUNT_SID'],
+      ENV['AUTH_TOKEN']
+    )
+  end
+
+  def send
+    one_hour_later = (Time.now + 60*60).strftime "%H:%M:%S"
+    @client.messages.create(
+      from: ENV['TWILIO_NUMBER'],
+      to: ENV['RECEIVER_NUMBER'],
+      body: "Thank you! Your order has been placed and will be delivered before #{one_hour_later}"
+    )
+  end
+end
