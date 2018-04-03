42f056a5ee990050b48928423f7373b2f61418d3
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-03 22:17:06 +0100

    09: restaurant | order & ordersummary & checkout methods created

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,17 +1,35 @@
 require './lib/menu'
 require './lib/order'
+require './lib/sms'
 
 class Restaurant
 
-  attr_reader :takeaway_menu
+  attr_reader :takeaway_menu, :current_basket
 
-  def initialize(menu = Menu.new)
+  def initialize(menu = Menu.new, current_basket = nil)
     @takeaway_menu = menu
+    @current_basket = current_basket
   end
 
   def print_takeaway_menu
     add_takeaway_menu
     @takeaway_menu.print_list
+  end
+
+  def order(dish, quantity = 1)
+    @takeaway_menu.contain?(dish)
+    order_manager(dish, quantity)
+    "#{dish} x #{quantity} added to your basket:)"
+  end
+
+  def order_summary
+    raise basket_error if @current_basket == nil
+    @current_basket.summary
+  end
+
+  def checkout(amount, sms = Sms.new)
+    raise checkout_error unless amount == order_total
+    sms.send
   end
 
   private
@@ -23,6 +41,26 @@ class Restaurant
     @takeaway_menu.add_dish("Vegetariana", 12.95)
     @takeaway_menu.add_dish("Tiramisu", 5.95)
     @takeaway_menu.add_dish("Prosecco", 7.00)
+  end
+
+  def order_manager(dish, quantity)
+    price = @takeaway_menu.price?(dish)
+    if @current_basket == nil
+      @current_basket = Order.new
+    end
+    @current_basket.add(dish, price, quantity)
+  end
+
+  def basket_error
+    "Your basket is empty!"
+  end
+
+  def checkout_error
+    "The amount you paid was wrong."
+  end
+
+  def order_total
+    @current_basket.sum
   end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -11,10 +11,13 @@ describe Restaurant do
   end
 
   describe '#print_takeaway_menu' do
+    let(:fakemenu) { double :fakemenu }
     it 'Should print out the takeaway menu' do
+      subject { described_class.new(fakemenu) }
       expect(subject.print_takeaway_menu).to eq "These are our dishes and price: Dish Name: Vongole, Price: 14.25, Dish Name: Lasagne, Price: 12.6, Dish Name: Margherita, Price: 8.95, Dish Name: Vegetariana, Price: 12.95, Dish Name: Tiramisu, Price: 5.95, Dish Name: Prosecco, Price: 7.0"
     end
 
+    ##below this line: trying mocking method but still trying
     # let(:menu) { double :menu }
     # let(:dish) { double :dish }
     # it '' do
@@ -22,6 +25,17 @@ describe Restaurant do
     #   allow(menu).to receive(:add_takeaway_menu).and_return(dish)
     #   expect(subject.print_takeaway_menu).to eq dish
     # end
+
   end
+
+  # describe '#order' do
+  #   let(:fakemenu) { double :fakemenu }
+  #   let(:dish){ double :dish }
+  #   it '' do
+  #     subject { described_class.new(fakemenu) }
+  #     allow(fakemenu).to receive(:contain?).with(dish).and_return(true)
+  #     expect(subject.order(dish)).to eq "dish x 1 added to your basket:)"
+  #   end
+  # end
 
 end
