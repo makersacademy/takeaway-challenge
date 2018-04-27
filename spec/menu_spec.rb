95266237276ed0845207dee5086359c28a8bc569
Asia Antczak <aasiaantczak@gmail.com>
2018-04-27 23:08:59 +0100

    add menu list

---
+++ spec/menu_spec.rb
@@ -0,0 +1,17 @@
+require 'menu'
+
+describe Menu do
+  subject(:menu) { described_class.new }
+
+  describe "#initialize" do
+    it "has got the full list of dishes on initialize" do
+      expect(menu.list).to eq [{ "Southern Chicken" => 5.50 },
+              { "Chicken Katsu Curry" => 5.50 },
+              { "Jerk Chicken" => 5.50 },
+              { "Mamas Meatballs" => 5.30 },
+              { "Kofta Patties" => 5.90 },
+              { "Pasta Primavera" => 5.00 },
+              { "Vegan Moussaka" => 5.20 }]
+    end
+  end
+end
