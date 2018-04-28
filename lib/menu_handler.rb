96577878bc2c0895d0c497aedb84e26ecec92ef1
George Sykes <georgesykes86@gmail.com>
2018-04-28 12:29:36 +0100

    Adds initialisation of Menu_handler

--- lib/menu_handler.rb
+++ lib/menu_handler.rb
@@ -1,3 +1,9 @@
 class Menu_handler
 
+  ROOT = '~/Users/georgesykes/Projects/MakersAcademy/WeekendChallenges/takeaway-challenge/resources/menus/'
+
+  def initialize(root = ROOT)
+    @root = root
+  end
+
 end
--- spec/menu_handler_spec.rb
+++ spec/menu_handler_spec.rb
@@ -1,3 +1,15 @@
 describe Menu_handler do
-  
+
+  describe '#initialize' do
+    let(:menu_handler) { described_class.new('fake_root/test')}
+
+    it 'defaults the root' do
+      expect(subject.instance_variable_get(:@root)).to eq described_class::ROOT
+    end
+
+    it 'defaults the root' do
+      expect(menu_handler.instance_variable_get(:@root)).to eq 'fake_root/test'
+    end
+
+  end
 end
