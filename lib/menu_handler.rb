6d527ccb35a26abe0ecbe64d85b208717e19c254
George Sykes <georgesykes86@gmail.com>
2018-04-28 12:44:16 +0100

    Adds search for csv files

--- lib/menu_handler.rb
+++ lib/menu_handler.rb
@@ -1,9 +1,30 @@
+require 'csv'
+require 'find'
+
 class Menu_handler
 
-  ROOT = '~/Users/georgesykes/Projects/MakersAcademy/WeekendChallenges/takeaway-challenge/resources/menus/'
+  attr_reader :files
+
+  ROOT = '/Users/georgesykes/Projects/MakersAcademy/WeekendChallenges/takeaway-challenge/resources/menus/'
 
   def initialize(root = ROOT)
     @root = root
+    @files = get_files
+  end
+
+  def get_summaries
+
+  end
+
+
+  private
+
+  def get_files
+    csv_files = []
+    Find.find(@root) do |path|
+      csv_files << path if path =~ /.*\.csv$/
+    end
+    csv_files
   end
 
 end
--- spec/menu_handler_spec.rb
+++ spec/menu_handler_spec.rb
@@ -1,15 +1,19 @@
 describe Menu_handler do
 
   describe '#initialize' do
-    let(:menu_handler) { described_class.new('fake_root/test')}
+    let(:menu_handler) { described_class.new('/Users/georgesykes/Projects/MakersAcademy/WeekendChallenges/takeaway-challenge/resources')}
 
     it 'defaults the root' do
       expect(subject.instance_variable_get(:@root)).to eq described_class::ROOT
     end
 
-    it 'defaults the root' do
-      expect(menu_handler.instance_variable_get(:@root)).to eq 'fake_root/test'
+    it 'initializes an array' do
+      expect(subject.files).to be_an_instance_of(Array)
     end
 
+    it 'defaults the root' do
+      expect(menu_handler.instance_variable_get(:@root)).to eq '/Users/georgesykes/Projects/MakersAcademy/WeekendChallenges/takeaway-challenge/resources'
+    end
   end
+
 end
