5d3f721ca598f58843a4737a29f18f98945b79b1
Charlene <charlenechetcuti@gmail.com>
2018-04-28 17:23:53 +0100

    save customer orders

--- lib/customer_order.rb
+++ lib/customer_order.rb
@@ -1,6 +1,14 @@
 class CustomerOrder
 
+  attr_reader :selection
 
+  def initialize(dishes = Dishes.new)
+    @dishes = dishes
+    @selection = []
+  end
 
+  def select_dish(dish, quantity)
+    @selection << [dish, quantity]
+  end
 
 end
--- spec/customer_order_spec.rb
+++ spec/customer_order_spec.rb
@@ -3,19 +3,23 @@ describe CustomerOrder do
   let(:order) { CustomerOrder.new }
   let(:dish) { 'Hawaiian' }
   let(:quantity) { 2 }
+  let(:menu) { [{dish: 'Hawaiian' , price: '8.99'},
+    {dish: 'BBQ Original', price: '6.99'},]}
+  let(:dishes) { double :dishes, menu: menu}
 
   describe '#select_dishes' do
 
-    it 'saves dish' do
+    it 'saves selection' do
       order.select_dish(dish, quantity)
-      expect(order.dish).to eq 'Hawaiian'
+      expect(order.selection).to eq [[dish, quantity]]
     end
 
-    it 'saves quantity' do
+    it 'saves multiple selections' do
       order.select_dish(dish, quantity)
-      expect(order.quantity).to eq quantity
+      order.select_dish(dish, quantity)
+      expect(order.selection).to eq [[dish, quantity], [dish, quantity]]
     end
-
+    
   end
 
 end
