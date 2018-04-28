c3fcbd6256357c9641ed9264fc6b313dacdb6845
Charlene <charlenechetcuti@gmail.com>
2018-04-28 18:06:18 +0100

    add feature to raise error if dish is not in menu

--- spec/customer_order_spec.rb
+++ spec/customer_order_spec.rb
@@ -1,6 +1,6 @@
 describe CustomerOrder do
 
-  let(:order) { CustomerOrder.new }
+  let(:order) { CustomerOrder.new(dishes) }
   let(:dish) { 'Hawaiian' }
   let(:quantity) { 2 }
   let(:menu) { [{dish: 'Hawaiian' , price: '8.99'},
@@ -8,6 +8,8 @@ describe CustomerOrder do
   let(:dishes) { double :dishes, menu: menu}
 
   describe '#select_dishes' do
+
+    before{ allow(dishes).to receive(:in_menu?).with(dish).and_return(true) }
 
     it 'saves selection' do
       order.select_dish(dish, quantity)
@@ -20,9 +22,11 @@ describe CustomerOrder do
       expect(order.selection).to eq [[dish, quantity], [dish, quantity]]
     end
 
-    it 'returns error if quantity is not an integer' do
-      message = "Quantity must be an integer!"
-      expect { dishes.in_menu?(quantity) }.to return_error message
+    it 'returns error if dish is not in menu' do
+      invalid_dish = 'Tortellini'
+      message = "Error: Dish is not in menu!"
+      allow(dishes).to receive(:in_menu?).with(invalid_dish).and_return(false)
+      expect { order.select_dish(invalid_dish, quantity) }.to raise_error message
     end
 
   end
