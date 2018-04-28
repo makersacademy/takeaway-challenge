285691914c9ba16b3a611e9012c9ccc55823d479
Muzzi Aldean <amiraldean@gmail.com>
2018-04-28 19:43:11 +0100

    menu and dish classes functioning with print and add methods

---
+++ lib/menu.rb
@@ -0,0 +1,19 @@
+require_relative 'dish'
+
+class Menu
+  attr_reader :list
+
+  def initialize
+    @list = []
+  end
+
+  def add(dish)
+    @list << dish
+  end
+
+  def pretty_print
+    @list.each_with_index do |dish, index|
+      puts "#{index + 1}) #{dish.name} - £#{dish.price}"
+    end
+  end
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,26 @@
+require 'menu'
+
+describe Menu do
+
+  let(:dish) { double :dish , name: 'Curry', price: '5' }
+
+  describe '#initializes' do
+    it 'it initializes with an array' do
+      expect(subject.list).to be_instance_of(Array)
+    end
+  end
+
+  describe '#add' do
+    it 'adds dishes to the list array' do
+      subject.add(dish)
+      expect(subject.list).to include(dish)
+    end
+  end
+
+  describe '#pretty_print' do
+    it 'pretty prints a list of dishes and prices' do
+      subject.add(dish)
+      expect { subject.pretty_print }.to output("1) Curry - £5\n").to_stdout
+    end
+  end
+end
