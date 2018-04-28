784d7be4154d7887938a7ae9417130be47668dc4
benfurber <ben@furber-consultancy.com>
2018-04-28 18:00:02 +0100

    Tests and methods working for Menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,12 +1,31 @@
-class Menu
-  attr_reader :dishes
+# require_relative 'dish'
 
-  def initialize
+class Menu
+  def initialize(class_type = Dish)
     @dishes = []
+    @class_type = class_type
+  end
+
+  def dishes
+    @dishes.dup
   end
 
   def add_dish(name, price)
+    dish_name_check(name)
+
     @dishes << [name, price]
-    "#{name} added"
+    "Dish added: #{name} - £#{price}"
+  end
+
+  def print_dishes
+    @dishes.map.with_index { |dish, index|
+      "#{index}. #{dish[0]} - £#{dish[1]}"
+    }.join()
+  end
+
+  private
+
+  def dish_name_check(name)
+    raise if dishes.any? { |dish| dish[0] == name }
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,14 +1,46 @@
 require 'menu'
 
 describe Menu do
-  context 'On initialize' do
+  subject { Menu.new('mock') }
+  let(:dish_name_1) { 'dish_1' }
+  let(:dish_price_1) { 4.5 }
+  let(:add_dish) { subject.add_dish(dish_name_1, dish_price_1) }
+
+  context '#initialize' do
     it '@dishes is empty' do
       expect(subject.dishes.length).to eq 0
     end
   end
+
   context '#add_dish' do
-    it 'can add a dish' do
-      expect(subject.add_dish('dish_1', 4.5)).to eq 'dish_1 added'
+    it 'Can add a dish' do
+      expect(add_dish).to eq "Dish added: #{dish_name_1} - £#{dish_price_1}"
+    end
+    it 'Raises an error if a dish with that name exists already' do
+      add_dish
+      expect { subject.add_dish(dish_name_1, dish_price_1) }.to raise_error RuntimeError
+    end
+  end
+
+  context '#dishes' do
+    let(:one_item_array_return) { [[dish_name_1, dish_price_1]] }
+
+    it 'Returns an copy of @dishes' do
+      add_dish
+      expect(subject.dishes).to eq one_item_array_return
+    end
+
+    it "Items can't be removed from @dishes" do
+      add_dish
+      subject.dishes.pop
+      expect(subject.dishes).to eq one_item_array_return
+    end
+  end
+
+  context '#print_dishes' do
+    it "Prints the dishes nicely" do
+      add_dish
+      expect(subject.print_dishes).to eq "0. #{dish_name_1} - £#{dish_price_1}"
     end
   end
 end
