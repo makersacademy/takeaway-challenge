ee76a8ff35e841101435df075ec34833428a8f2b
Dione Estabillo <dione.estabillo@gmail.com>
2018-04-29 19:51:10 +0100

    Added Order class and unit tests

---
+++ spec/order_spec.rb
@@ -0,0 +1,21 @@
+require 'order'
+
+describe Order do
+  subject(:order) { described_class.new }
+  describe '#items' do
+    it 'is created with a blank list of items' do
+      expect(order.items).to eq []
+    end
+  end
+
+  describe '#total' do
+    let(:dish) { double :dish, name: 'Steak', price: 25 }
+    let(:dish2) { double :dish, name: 'Chips', price: 8 }
+    let(:dish3) { double :dish, name: 'Caesar Salad', price: 9 }
+    it 'finds the sum of of dish prices and returns result' do
+      order.items += [dish, dish2, dish3]
+      expect(subject.total).to eq(42)
+    end
+  end
+
+end
