611c8c2562a399bb642779a3324fad662104a52d
Charlene <charlenechetcuti@gmail.com>
2018-04-29 14:15:49 +0100

    added some feature tests for irb testingg

--- spec/feature_tests.rb
+++ spec/feature_tests.rb
@@ -1,8 +1,8 @@
 # irb require './spec/feature_tests.rb'
-
-require './lib/dishes'
+require './lib/menu'
 require './lib/customer_order'
 require './lib/bill'
+require './lib/text'
 
 @menu = [{dish: 'Hawaiian' , price: '8.99'},
   {dish: 'BBQ Original', price: '6.99'},
@@ -13,22 +13,49 @@ require './lib/bill'
   {dish: 'Ceasar Salad' , price: '5.90'}
  ]
 
-@order_1 = [{dish:'Hawaiian', quantity: 2}]
-@order_2 = ['Tortellini', 3]
+@order_1 = {dish:'Hawaiian', quantity: 2}
+@order_2 = {dish:'Seafood', quantity: 4}
 
-def print_list
-  dishes = Dishes.new(@menu)
-  dishes.print
+def typical_order
+  menu = Menu.new(@menu)
+  bill = Bill.new
+  text = Text.new
+  menu.printed
+  order = CustomerOrder.new(menu, bill, text)
+  order.select_dish(@order_1[:dish], @order_1[:quantity])
+  order.select_dish(@order_2[:dish], @order_2[:quantity])
+  order.process(53.94)
 end
 
-def order
-  p dishes = Dishes.new(@menu)
-  p order = CustomerOrder.new(dishes)
-  p order.select_dish(@order_1[0], @order_1[1])
-  p order.select_dish(@order_2[0], @order_2[1])
+def payment_not_validated
+  menu = Menu.new(@menu)
+  bill = Bill.new
+  text = Text.new
+  menu.printed
+  order = CustomerOrder.new(menu, bill, text)
+  order.select_dish(@order_1[:dish], @order_1[:quantity])
+  order.select_dish(@order_2[:dish], @order_2[:quantity])
+  order.process(50)
 end
 
-def bill
-  bill = Bill.new(@menu, @order_1)
-  p bill.calculate_bill
+def order_not_valid_menu_item
+  menu = Menu.new(@menu)
+  bill = Bill.new
+  text = Text.new
+  menu.printed
+  order = CustomerOrder.new(menu, bill, text)
+  order.select_dish('Tortellini', @order_1[:quantity])
+  order.select_dish(@order_2[:dish], @order_2[:quantity])
+  order.process(53.94)
+end
+
+def order_not_valid_item
+  menu = Menu.new(@menu)
+  bill = Bill.new
+  text = Text.new
+  menu.printed
+  order = CustomerOrder.new(menu, bill, text)
+  order.select_dish(@order_1[:dish], 'wrong')
+  order.select_dish(@order_2[:dish], @order_2[:quantity])
+  order.process(53.94)
 end
