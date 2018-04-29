78c5589bca62e35e7a278441c62309f4d65b9d39
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-04-29 15:58:40 +0100

    First user story abilty to read menu created

---
+++ lib/menu.rb
@@ -0,0 +1,29 @@
+class Menu
+
+attr_reader :menu
+
+ def initialize
+    @menu = {
+              "Chiken Tika" => 5.00,
+              "Rogan Josh" => 5.00,
+              "Vindaloo" => 7.00,
+            }
+  end
+
+  def show_menu
+    spacing = 32
+    menu.each do |dish, price|
+      print "#{dish} ";
+      print "." * (spacing - dish.length)
+      print " £#{price}\n"
+    end
+  end
+
+  def price_of_dish(dish)
+    @menu[dish]
+  end
+
+  def add(dish, price)
+    @menu.store(dish, price)
+    menu
+end
