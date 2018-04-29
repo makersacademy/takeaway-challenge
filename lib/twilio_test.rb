d93dfb33470f1e1521d37de4a70df49cb0be4270
Igor Ryabchuk <riabtchuk@gmail.com>
2018-04-29 13:17:37 +0100

    modify twilio_test to use ENV variables for credentials

--- lib/twilio_test.rb
+++ lib/twilio_test.rb
@@ -1,7 +1,7 @@
 require 'twilio-ruby'
 
-account_sid = 'ACace3793f369b745605c73555f0078ab8'
-auth_token = '25ff44bd7497acbb6412c927c9b8c4f8'
+account_sid = ENV["TWILIO_ACCOUNT_SID"]
+auth_token = ENV['TWILIO_AUTH_TOKEN']
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 
