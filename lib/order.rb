33d446935dcfb92cbf09ef1f53f51847c8b54a69
benfurber <ben@furber-consultancy.com>
2018-04-29 11:19:08 +0100

    All v1 tests/features for Order working - I'm even mocking correctly

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,13 @@
 require_relative 'dish'
 
 class Order
-  attr_reader :current_total
-
-  def initialize(class_type = Dish)
+  def initialize
     @basket = {}
     @current_total = 0
-    @class_type = class_type
+  end
+
+  def current_total
+    (@current_total = total_calculation).round(2)
   end
 
   def basket
@@ -15,17 +16,30 @@ class Order
 
   def add_to_basket(item, number)
     if in_basket?(item)
-      @basket[item] += number
-      return "Added #{number} more of #{item} to basket"
+      @basket[item.name][1] += number
+      return "Added #{number} more of #{item.name} to basket"
     else
-      @basket[item] = number
-      "Added #{number} of #{item} to basket"
+      @basket[item.name] = [item.price, number]
+      "Added #{number} of #{item.name} to basket"
     end
+  end
+
+  def print_order
+    output = []
+    @basket.each { |key, value| output << "#{key} - £#{value[0]} x#{value[1]}" }
+    output << "Total: £#{current_total}"
+    output.join("\n")
   end
 
   private
 
   def in_basket?(item)
-    @basket.has_key? item
+    @basket.has_key? item.name
+  end
+
+  def total_calculation
+    total = 0
+    @basket.each_value { |value| total += value[0] * value[1] }
+    total
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,12 +1,15 @@
 require 'order'
 
 describe Order do
-  let(:object_type) { Dish }
-  subject { Order.new(object_type) }
-
+  let(:dish_id_1) { 1 }
   let(:dish_name_1) { 'dish_1' }
+  let(:dish_price_1) { 4.5 }
+
+  let(:dish) { double('a dish', id: dish_id_1, name: dish_name_1, price: dish_price_1) }
+
   let(:how_many_to_add_1) { 2 }
-  let(:add_to_basket) { subject.add_to_basket(dish_name_1, how_many_to_add_1) }
+  let(:add_to_basket) { subject.add_to_basket(dish, how_many_to_add_1) }
+  let(:add_to_basket_2) { subject.add_to_basket(dish, how_many_to_add_1) }
 
   context '#initialize' do
     it 'Has a @basket which is empty' do
@@ -23,8 +26,24 @@ describe Order do
     end
     it 'If item already in basket, will add the extra quantity to the basket' do
       add_to_basket
-      expect(subject.add_to_basket(dish_name_1, how_many_to_add_1)
-        ).to eq "Added #{how_many_to_add_1} more of #{dish_name_1} to basket"
+      expect(add_to_basket_2
+      ).to eq "Added #{how_many_to_add_1} more of #{dish_name_1} to basket"
+    end
+  end
+
+  context '#current_total' do
+    it 'Adding to the basket updates the total' do
+      add_to_basket
+      expect(subject.current_total).to eq dish_price_1 * how_many_to_add_1
+    end
+  end
+
+  context '#print_order' do
+    it 'Pretty prints the order and order total' do
+      add_to_basket
+      expect(subject.print_order
+      ).to eq "#{dish_name_1} - £#{dish_price_1} x#{how_many_to_add_1}\n" +
+      "Total: £#{dish_price_1 * how_many_to_add_1}"
     end
   end
 end
