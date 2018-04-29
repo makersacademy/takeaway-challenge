06a0b644060909498a90555bf5652ac543591163
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-04-29 22:36:41 +0100

    updated check_order method

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,23 +1,31 @@
-require 'menu'
+require './lib/menu.rb'
 
 class TakeAway
 
-  attr_reader :basket, :menu, :show_menu
+  attr_reader :basket, :menu, :show_menu, :bill, :total
 
   def initialize(menu = Menu.new)
     @menu = menu
     @basket = []
     @bill = []
+    @total = 0
   end
 
   def view_menu
-    show_menu
+    menu.show_menu
   end
 
   def place_order(dish, quantity)
     sum = menu.price_of_dish(dish).to_f * quantity.to_f
     basket << "#{dish} X #{quantity} = £#{sum}"
     @bill << sum
+    @total += (menu.price_of_dish(dish).to_f * quantity.to_i)
     "#{quantity} X #{dish} added to your basket"
   end
+
+  def check_order
+    print "#{basket.join("\n") }\n"
+    print "Total = £#{@total}"
+  end
+
 end
