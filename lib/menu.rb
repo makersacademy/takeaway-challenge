6e863bf6b9ae2a7893ca92d7b78daca7bf497cf4
Habin <habin@hotmail.co.uk>
2018-04-29 19:02:53 +0100

    menu class responds to read_menu method, prints all items and prices, passes test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,8 @@
-class Takeaway
-attr_reader :full_menu
-
-  def full_menu
-    []
+class Menu
+attr_reader :menu
 
   def initialize
-    @full_menu = [ { name: "Spring rolls", price: 2 },
+    @menu = [ { name: "Spring rolls", price: 2 },
     { name: "Char Sui bun", price: 3.99 },
     { name: "Pork dumpling", price: 2.99 },
     { name: "Peking duck", price: 7.99 },
@@ -14,5 +11,10 @@ attr_reader :full_menu
     { name: "Pickled egg", price: 1.99 },
     { name: "Sweet and sour chicken", price: 6.99 } ]
   end
-end
+
+  def full_menu
+    @menu.each do |item|
+      puts "#{ item[:name] } - £#{ menu[:price] }\n"
+    end
+  end
 end
