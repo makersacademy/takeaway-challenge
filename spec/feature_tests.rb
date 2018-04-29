87244d7a8c9074ddb67a7fc7bc802468695ac4ce
Dione Estabillo <dione.estabillo@gmail.com>
2018-04-29 19:35:31 +0100

    Edited how feature tests to be used in IRB

--- spec/feature_tests.rb
+++ spec/feature_tests.rb
@@ -3,35 +3,30 @@ require './lib/menu.rb'
 require './lib/basket.rb'
 
 def new_menu
-  dish1 = Dish.new(name: 'Steak', price: 25)
-  dish2 = Dish.new(name: 'Chips', price: 8)
-  dish3 = Dish.new(name: 'Caesar Salad', price: 9)
-  dish4 = Dish.new(name: 'Ice cream', price: 3)
-  dish1 = dish1.set_details
-  dish2 = dish2.set_details
-  dish3 = dish3.set_details
-  dish4 = dish4.set_details
+  dish1 = Dish.new('Steak', 25)
+  dish2 = Dish.new('Chips', 8)
+  dish3 = Dish.new('Caesar Salad', 9)
+  dish4 = Dish.new('Ice cream', 3)
   menu = Menu.new
-  menu.add_item(dish1)
-  menu.add_item(dish2)
-  menu.add_item(dish3)
-  menu.add_item(dish4)
-  menu.item_list
+  menu.add(dish1)
+  menu.add(dish2)
+  menu.add(dish3)
+  menu.add(dish4)
   menu.display
 end
 
-def new_basket
-  new_menu
-  basket = Basket.new
-  basket.add_dish(dish: 'Steak', price: 25, quantity: 1)
-  basket.add_dish(dish: 'Chips', price: 8, quantity: 2)
-  basket.add_dish(dish: 'Caesar Salad', price: 9, quantity: 3)
-  basket.display_summary
-  basket.total_cost
-  basket.display_total
-end
+# def new_basket
+#   new_menu
+#   basket = Basket.new
+#   basket.add_dish(dish: 'Steak', price: 25, quantity: 1)
+#   basket.add_dish(dish: 'Chips', price: 8, quantity: 2)
+#   basket.add_dish(dish: 'Caesar Salad', price: 9, quantity: 3)
+#   basket.display_summary
+#   basket.total_cost
+#   basket.display_total
+# end
 
-# p new_menu
-p new_basket
+p new_menu
+# p new_basket
 
 # irb -r './spec/feature_tests.rb'
