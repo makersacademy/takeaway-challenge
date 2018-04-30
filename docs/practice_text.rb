a3c1135ca9ee0b55ef7f3012337afdfa58a81ead
Charly Mannion <charlottemannion100@gmail.com>
2018-04-30 10:44:15 +0100

    auth removed from practice text

--- docs/practice_text.rb
+++ docs/practice_text.rb
@@ -1,8 +1,8 @@
 require 'twilio-ruby'
 require 'dotenv/load'
 
-account_sid = "ACf57fe993220533543d389c7c17d3398c"
-auth_token = "8ad60fc96d300a9a9214f353d1404aeb"
+account_sid = ENV["TWILIOSID"]
+auth_token = ENV["TWILIOAUTHTOKEN"]
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 message = @client.messages.create(
