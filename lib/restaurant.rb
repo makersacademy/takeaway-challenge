8b1737562a8e1a1167e11e692d8e11213610bbe9
Laura Petre <laurapetre@lauras-air.home>
2018-05-26 20:55:03 +0100

    First user story

---
+++ lib/restaurant.rb
@@ -0,0 +1,17 @@
+class Restaurant
+  attr_reader :dish
+  def initialize
+    @dish = { "Runny Risotto" => 5,
+              "No Carbs Carbonara" => 6,
+              "Cold Pizza" => 7,
+              "Sugarless Tiramisu" => 4,
+              "Low Veg Meat Salad" => 3,
+              "Warm Wine" => 10,
+            }
+  end
+
+  def price(dish_key)
+    @dish[dish_key]
+  end
+
+end
---
+++ spec/restaurant_spec.rb
@@ -0,0 +1,13 @@
+require "restaurant"
+
+describe Restaurant do
+  it "it has a list of dishes that can be seen (is not empty)" do
+    expect(subject.dish.empty?).to be false
+  end
+
+  describe "#price" do
+    it "it returns the price of a certain dish from the menu" do
+      expect(subject.price("Cold Pizza")).to eq 7
+    end
+  end
+end
