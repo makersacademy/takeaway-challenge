9437e26b1f9c5288157d7468eade5b8d66e9c256
Robert Faldo <robertfaldo@gmail.com>
2018-05-26 14:42:00 +0100

    refactored user story 2

--- lib/order.rb
+++ lib/order.rb
@@ -1,15 +1,13 @@
 class Order 
 
+  attr_reader :current_items
+
   def initialize
     @current_items = []
   end
 
   def add_item(item, quantity)
     @current_items.push([item, quantity])
-  end
-
-  def current_items
-    @current_items
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,24 +3,28 @@ require 'order.rb'
 describe Order do 
   subject(:order) { described_class.new }
 
-  it 'can create an order' do
-    expect { Order.new }.not_to raise_error
+  context '#create_order' do
+    it 'can create an order' do
+      expect { Order.new }.not_to raise_error
+    end
   end
 
-  it 'can add an item to the menu' do
-    expect { order.add_item("chicken curry", 4) }.not_to raise_error
+  context "#add_item" do
+    it 'can add an item to the menu' do
+      expect { order.add_item("chicken curry", 4) }.not_to raise_error
+    end
   end
 
-  it 'can show the current items in the order' do
-    order.add_item("chicken curry", 4)
-    expect(order.current_items[0]).to eq ["chicken curry", 4]
-  end
+    it 'can show the current items in the order' do
+      order.add_item("chicken curry", 4)
+      expect(order.current_items[0]).to eq ["chicken curry", 4]
+    end
 
-  it 'can store more than 1 item in its current items' do
-    order.add_item("Pork Belly", 6)
-    order.add_item("Chicken Curry", 5)
-    order.add_item("Beef Curry", 3)
-    expect(order.current_items.length).to eq 3
-  end
+    it 'can store more than 1 item in its current items' do
+      order.add_item("Pork Belly", 6)
+      order.add_item("Chicken Curry", 5)
+      order.add_item("Beef Curry", 3)
+      expect(order.current_items.length).to eq 3
+    end
 
 end
