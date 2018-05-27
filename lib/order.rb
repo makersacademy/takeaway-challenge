29354721d0b533912340525d507b5ae4ef8bafd2
Khie Biggs <khiebigzy@gmail.com>
2018-05-27 18:20:28 +0100

    .Order#show_order

--- lib/order.rb
+++ lib/order.rb
@@ -5,6 +5,7 @@ class Order
     @total = 0
   end
 
+  # adds to the order
   def add(array)
     array.each do |item|
       itemname = item[0]
@@ -12,5 +13,15 @@ class Order
       @items[itemname] = array.count(item)
       @total += price
     end
+  end
+
+  # show the current order
+  def show_order
+    text = ""
+    text << "Your order was:\n"
+    @items.each do |key, value|
+      text << "#{key} x #{value}\n"
+    end
+    text
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,5 +7,17 @@ describe Order do
       expect(subject.items).to eq({ "chicken" => 1, "rice" => 1 })
       expect(subject.total).to eq 20
     end
+    it 'can add multiple items to the order' do
+      subject.add([["chicken", "10"], ["chicken", "10"], ["chicken", "10"], ["rice", "10"]])
+      expect(subject.items).to eq({ "chicken" => 3, "rice" => 1 })
+      expect(subject.total).to eq 40
+    end
+  end
+
+  describe '#show_order' do
+    it 'can return the order with the total' do
+      subject.add([["chicken", "10"], ["chicken", "10"], ["chicken", "10"], ["rice", "10"]])
+      expect(subject.show_order).to eq("Your order was:\nchicken x 3\nrice x 1\n")
+    end
   end
 end
