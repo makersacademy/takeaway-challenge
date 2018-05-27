85ccce7387519972bdd255a7a2f2a48d82e260f1
Laura Petre <laurapetre@lauras-air.home>
2018-05-27 17:29:49 +0100

    £rd user story redone - now raising error

--- lib/order.rb
+++ lib/order.rb
@@ -8,14 +8,27 @@ class Order
 
   def select_dishes(current_order)
     @order = current_order
+    raise "Incorrect order - please select a dish from the menu" unless incorrect_order?
+    @order
   end
 
-  def dishes_costs_sum
+  def checking_total(given_total)
+    @given_total = given_total
+    raise "Incorrect sum - please retry to place order" unless incorrect_sum?
+    @given_total
+  end
+
+  def calculate_total_cost
     @order.each{ |dish, number| @calculated_sum += @menu.dish[dish] * number}
     @calculated_sum
   end
 
-  def check_sum?(given_total)
-    self.dishes_costs_sum == given_total
+  private
+  def incorrect_order?
+    (@order.keys - @menu.dish.keys) == []
+  end
+
+  def incorrect_sum?
+    self.calculate_total_cost == @given_total
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,21 +7,22 @@ describe Order do
                     "Cold Pizza" => 3 }}
 
   it { is_expected.to respond_to(:select_dishes).with(1).argument }
-  it { is_expected.to respond_to(:check_sum?).with(1).argument }
+  it { is_expected.to respond_to(:checking_total).with(1).argument }
 
-  it "allows the customer to select some number of several dishes" do
-    expect(order.select_dishes(current_order)).to eq current_order
-  end
+  context "---placing the order---" do
+    it "allows the customer to select some number of several dishes" do
+      expect(order.select_dishes(current_order)).to eq current_order
+    end
 
-  it "checks if the selected dishes are included in the menu" do
-    order.select_dishes(current_order)
-    menu = Restaurant.new.dish
-    expect(current_order.keys - menu.keys).to eq []
-  end
+    it "raises an error if the selected dishes are not on the menu" do
+      error_order = {"Runnny Risottto" => 2}
+      expect {order.select_dishes(error_order)}.to raise_error"Incorrect order - please select a dish from the menu"
+    end
 
-  it "allows the custumer to check if the given total matches the sum of the various dishes" do
-    order.select_dishes(current_order)
-    given_total = 2 * 5 + 3 * 7
-    expect(order.check_sum?(given_total)).to be true
+    it "raises an error if the given total doesn't match the sum of the various dishes" do
+      order.select_dishes(current_order)
+      error_given_total = 10
+      expect {order.checking_total(error_given_total)}.to raise_error"Incorrect sum - please retry to place order"
+    end
   end
 end
