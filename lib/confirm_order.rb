edeaba40e0f8e8c45970cbae5103e81fdaf5c443
Jen Strong <jenniferastrong@gmail.com>
2018-05-27 21:17:39 +0100

    added test to total order

--- lib/confirm_order.rb
+++ lib/confirm_order.rb
@@ -3,14 +3,25 @@ require 'pry'
 
 class ConfirmOrder
 
-  def initialize(menu, requested_order)
-    @menu = menu
+  def initialize(menu = Menu.new, requested_order)
+    @menu = menu.menu_list
     @requested_order = requested_order
+    @total = 0
   end
 
-  def order_checked?
-    list_of_menu_items = @menu.menu_list.keys
+  def order_valid?
+    list_of_menu_items = @menu.keys
     list_of_requested_items = @requested_order.keys
     (list_of_requested_items - list_of_menu_items).length == 0 ? true : false
+  end
+
+  def raise_error_for_invalid_order
+    raise 'Order not valid' unless order_valid?
+  end
+
+  def calculate_total
+    total = @menu.map { |item, price|
+        price * @requested_order[item] if @requested_order.key?(item)
+      }.compact.sum.round(2)
   end
 end
--- spec/unit/confirm_order_spec.rb
+++ spec/unit/confirm_order_spec.rb
@@ -4,38 +4,42 @@ describe ConfirmOrder do
   let (:menu) { double('menu') }
   subject(:confirm_order) { described_class.new(menu, requested_order) }
 
+  before do
+    allow(menu).to receive(:menu_list).and_return(
+      {
+        banana: 1.99,
+        strawberry: 2.99,
+        peach: 3.99,
+        coconut: 4.99
+        }
+      )
+  end
+
   context '#order_checked?' do
     context 'when requested order is valid' do
       let(:requested_order) { {banana: 2, peach: 1} }
 
       it 'confirms ordered items are on the menu' do
-        allow(menu).to receive(:menu_list).and_return(
-          {
-            banana: 1.99,
-            strawberry: 2.99,
-            peach: 3.99,
-            coconut: 4.99
-            }
-          )
+        expect(confirm_order.order_valid?).to eq true
+      end
 
-        expect(confirm_order.order_checked?).to eq true
+      it 'totals the order' do
+        expect(confirm_order.calculate_total).to eq 7.97
       end
     end
 
     context 'when requested order is invalid' do
       let(:requested_order) { {pear: 2, peach: 1} }
 
+      it 'raises an error when order is invalid' do
+        error = 'Order not valid'
+        expect{ confirm_order.raise_error_for_invalid_order }.to raise_error(error)
+      end
+
       it 'confirms ordered items are not on the menu' do
-        allow(menu).to receive(:menu_list).and_return(
-          {
-            banana: 1.99,
-            strawberry: 2.99,
-            peach: 3.99,
-            coconut: 4.99
-            }
-          )
-        expect(confirm_order.order_checked?).to eq false
+        expect(confirm_order.order_valid?).to eq false
       end
     end
+
   end
 end
