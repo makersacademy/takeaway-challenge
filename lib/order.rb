727cd6d37147312f71d3e64623a8b3a85fb188d0
Laura Petre <laurapetre@lauras-air.home>
2018-05-27 16:17:35 +0100

    £rd user story

--- lib/order.rb
+++ lib/order.rb
@@ -1,9 +1,21 @@
+require_relative "./restaurant.rb"
 class Order
-  def select_dishes(order)
-    @order = order
+  attr_reader :order
+  def initialize
+    @calculated_sum = 0
+    @menu = Restaurant.new
   end
 
-  def check_sum(given_total)
-    true
+  def select_dishes(current_order)
+    @order = current_order
+  end
+
+  def dishes_costs_sum
+    @order.each{ |dish, number| @calculated_sum += @menu.dish[dish] * number}
+    @calculated_sum
+  end
+
+  def check_sum?(given_total)
+    self.dishes_costs_sum == given_total
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,29 +3,25 @@ require "restaurant"
 
 describe Order do
   let(:order) { described_class.new }
+  let(:current_order) {{ "Runny Risotto" => 2,
+                    "Cold Pizza" => 3 }}
 
   it { is_expected.to respond_to(:select_dishes).with(1).argument }
+  it { is_expected.to respond_to(:check_sum?).with(1).argument }
 
   it "allows the customer to select some number of several dishes" do
-    current_order = { "Runny Risotto" => 2,
-                      "Cold Pizza" => 3 }
     expect(order.select_dishes(current_order)).to eq current_order
   end
 
   it "checks if the selected dishes are included in the menu" do
-    current_order = { "Runny Risotto" => 2,
-                      "Cold Pizza" => 3 }
     order.select_dishes(current_order)
     menu = Restaurant.new.dish
     expect(current_order.keys - menu.keys).to eq []
   end
 
-  # it "allows the custumer to check if the given total matches the sum of the various dishes" do
-  #   current_order = { "Runny Risotto" => 2,
-  #                     "Cold Pizza" => 3 }
-  #   order.select_dishes(current_order)
-  #   given_total = 2 * 5 + 3 * 7
-  #   expect(order.check_sum(given_total)).to be true
-  # end
-
+  it "allows the custumer to check if the given total matches the sum of the various dishes" do
+    order.select_dishes(current_order)
+    given_total = 2 * 5 + 3 * 7
+    expect(order.check_sum?(given_total)).to be true
+  end
 end
