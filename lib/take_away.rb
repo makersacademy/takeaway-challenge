67dc25b8b46386eaf6fae637ed54353afb335467
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-28 22:13:28 +0100

    add test for printing and change read_menu into print_menu

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -8,11 +8,15 @@ class TakeAway
 
   def initialize
     @menu = { "Goi Ga" => 5.99, "Cha Gio" => 4.99,
-    "Vietnamese dumpling" => 2.99, "peking duck" => 5.00, "Pho Bo" => 6.99 }
+ "Vietnamese dumpling" => 2.99, "peking duck" => 5.00, "Pho Bo" => 6.99 }
   end
 
-  def read_menu
-    @menu
+  def print_menu
+    print = []
+    print << @menu.map do |dish, price|
+      "#{dish} £#{ '%.2f' % price}"
+    end
+      print.join(' || ')
   end
 
   def message
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -4,10 +4,9 @@ describe TakeAway do
 
   subject(:takeaway) { described_class.new }
 
-  describe '#read_menu' do
+  describe '#print_menu' do
     it 'shows menu' do
-      expect(subject.read_menu).to eq({ "Goi Ga" => 5.99, "Cha Gio" => 4.99,
-      "Vietnamese dumpling" => 2.99, "peking duck" => 5.00, "Pho Bo" => 6.99 })
+      expect(subject.print_menu).to eq( 'Goi Ga £5.99 || Cha Gio £4.99 || Vietnamese dumpling £2.99 || peking duck £5.00 || Pho Bo £6.99')
     end
   end
 
