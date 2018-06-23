bd99de49defafdf8df4627f8305c122545833b8c
Elishka Flint <elishkaf@gmail.com>
2018-06-23 11:54:25 +0100

    Add #add method to order and selection classes

--- lib/order.rb
+++ lib/order.rb
@@ -1,15 +1,21 @@
 require_relative './menu.rb'
+require_relative './selection.rb'
 
 class Order
 
-  attr_reader :menu
+  attr_reader :menu, :selection
 
-  def initialize(menu_class = Menu)
+  def initialize(menu_class = Menu, selection_class = Selection)
     @menu = menu_class.new
+    @selection = selection_class.new
   end
 
   def list_options
     @menu.options
+  end
+
+  def add(choice)
+    @selection.add(choice)
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,14 +2,25 @@ require 'order'
 
 describe Order do
 
-  let(:order)         { described_class.new(menu) }
+  it { is_expected.to respond_to(:add).with(1).argument }
+
+  let(:order)         { described_class.new(menu, selection) }
+
   let(:menu)          { double :menu, new: list }
   let(:list)          { double :list, options: "chicken" }
+
+  let(:selection)     { double :selection, new: choices }
+  let(:choices)       { double :choices, choices: [] }
 
   describe '#initialize' do
     it 'initializes with a menu' do
       expect(order.menu).to eq list
     end
+
+    it 'initializes with an empty selection array' do
+      expect(order.selection).to eq choices
+    end
+
   end
 
   describe '#list_options' do
@@ -18,4 +29,12 @@ describe Order do
     end
   end
 
+  describe '#add' do
+    it 'adds a choice to the selection array' do
+      # test needs to be mocked
+      order = Order.new
+      order.add("chicken")
+      expect(order.selection.choices).to include "chicken"
+    end
+  end
 end
