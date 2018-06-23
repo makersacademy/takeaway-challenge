2351f4fa55c7d3a6806d58ab657c852514f466a2
Elishka Flint <elishkaf@gmail.com>
2018-06-23 09:57:47 +0100

    Refactor test and code for order and menu classes

--- lib/order.rb
+++ lib/order.rb
@@ -2,9 +2,14 @@ require_relative './menu.rb'
 
 class Order
 
+  attr_reader :menu
+
+  def initialize(menu_class = Menu)
+    @menu = menu_class.new
+  end
+
   def list_options
-    menu = Menu.new
-    menu.options
+    @menu.options
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,12 +4,16 @@ describe Order do
 
   it { is_expected.to respond_to(:list_options) }
 
-  describe '#list_options' do
-    # test needs to be mocked correctly
-    it 'displays the options on the menu' do
-      menu_double = double(:menu, options: "options")
-      expect(subject.list_options).to eq "options"
-    end
+  let(:order)         { described_class.new(menu) }
+  let(:menu)          { double :menu, new: options }
+  let(:options)       { double :options, options: "chicken" }
+
+  it 'initializes with a menu' do
+    expect(order.menu).to eq options
+  end
+
+  it 'shows the options' do
+    expect(order.list_options).to eq "chicken"
   end
 
 end
