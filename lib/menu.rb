5d5cc60a9a1774c61351c27f833a873f8dfa3723
Benjamin <ben.smith.gordon@gmail.com>
2018-06-24 20:04:20 +0100

    what I have so far

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,14 @@
 class Menu
 
-dishes = { "oreo cookie sandwich " => 1, "chocolate eggo waffle" => 2, 
-"crispy chicken nuggets" => 3, "tangy beef jerky" => 1, "spicy lamb donner" => 4 }
-
-
-order_list = []
 
 
 total = order_list.inject(0) {|sum, hash| sum + hash[:total]}
 
 
 def input_order
+  dishes = { "oreo cookie sandwich " => 1, "chocolate eggo waffle" => 2, 
+  "crispy chicken nuggets" => 3, "tangy beef jerky" => 1, "spicy lamb donner" => 4 }
+  order_list = []
   puts "What would you like to order?"
   puts "To finish, please hit return twice"
   order = gets.chomp
