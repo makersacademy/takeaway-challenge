809f0ffc43fbec6f4e25ad5456b06f360290f7e1
Benjamin <ben.smith.gordon@gmail.com>
2018-06-24 21:50:20 +0100

    better?

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,36 +1,26 @@
 class Menu
 
 
-
-total = order_list.inject(0) {|sum, hash| sum + hash[:total]}
-
-
-def input_order
-  dishes = { "oreo cookie sandwich " => 1, "chocolate eggo waffle" => 2, 
-  "crispy chicken nuggets" => 3, "tangy beef jerky" => 1, "spicy lamb donner" => 4 }
-  order_list = []
-  puts "What would you like to order?"
-  puts "To finish, please hit return twice"
-  order = gets.chomp
-  while !order.empty? do
-  add_to_order_list
-  puts "Your total comes to #{total}"
+  def initialize 
+    @dishes = { "oreo cookie sandwich " => 1, "chocolate eggo waffle" => 2, 
+    "crispy chicken nuggets" => 3, "tangy beef jerky" => 1, "spicy lamb donner" => 4 }
+    @order_list = []
   end
-end
 
 
-
-def add_to_order_list(order, price)
-  @order_list << {order: order, price: price.to_sym}
-end
-
-
-def print_dishes
-  loop do
-  print dishes
+  def add_to_order_list(order, price)
+    @order_list << {order: order, price: price.to_sym}
   end
+
+
+def order1
+@order_list
 end
 
+
+  def total
+    total = order_list.inject(0)
+  end
 end
 
 
---
+++ spec/menu_spec.rb
@@ -0,0 +1,6 @@
+require "menu.rb"
+describe menu do
+  it "should add the orders to the array" do
+    expect (subject.input_order).to 
+  end
+end
