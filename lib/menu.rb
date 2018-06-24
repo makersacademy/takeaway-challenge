45db0684b56f6f3a940d01ae73ebaeb8cdd19519
Layth <laythq@gmail.com>
2018-06-24 23:17:02 +0100

    Add spec_helper

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,9 @@
-# class Menu
-#   attr_reader :menu
-#
-#   def initialize
-#     @menu = @menu = {salad: 5, chips: 5, chicken: 8, steak: 10}
-#   end
-#
-#
-# end
+class Menu
+  attr_reader :menu
+
+  def initialize
+    @menu = @menu = {salad: 5, chips: 5, chicken: 8, steak: 10}
+  end
+
+
+end
