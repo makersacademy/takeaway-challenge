4e4342ea30c7b7b5123a6bec4fc203d0a30276da
Marcus <marcus.kerr94@gmail.com>
2018-07-22 12:27:52 +0100

    Unit test: expect menu array to initilize to empty (RED)

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,6 +1,12 @@
 require "menu"
 describe Menu do
   subject(:menu) { Menu.new }
+
+  describe "#initialize" do
+    it "initializes with an empty array" do
+      expect(menu.menu).to be_empty
+    end
+  end
   
   describe "#add_dish" do
     it "respond to add_dish" do
