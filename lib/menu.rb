41994e7b4075bbbc9301e1a666f00aef1d4a047c
dani <daniboo83@gmail.com>
2018-07-22 11:51:22 +0100

    Incorporated better functionality for #1

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,15 +1,24 @@
 class Menu
   attr_reader :food
   
-  def initialize
-    @food = [
-      {"item1" => 2.50},
-      {"item2" => 2.80},
-      {"item3" => 5.00},
-      {"item4" => 3.00},
-      {"item5" => 8.00},
-      {"item6" => 6.30},
-      {"item7" => 7.00}
-    ]
+  def initialize(food)
+    @food = food
+  end
+
+  def show
+    food.map do |item, price|
+      "%s £%.2f" % [item.to_s.capitalize, price]
+    end.join(", ") # got this from the exemplar video
   end
 end
+
+#   @food = [
+  #     {"item1" => 2.50},
+  #     {"item2" => 2.80},
+  #     {"item3" => 5.00},
+  #     {"item4" => 3.00},
+  #     {"item5" => 8.00},
+  #     {"item6" => 6.30},
+  #     {"item7" => 7.00}
+  #   ]
+  # end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,14 +1,29 @@
-# "fake" is used as the test instance, for easy understanding
-require 'Menu'
+# "test" is used as the test instance, for easy understanding
+require 'menu'
 
 describe Menu do
-  let(:fake_menu) { described_class.new }
+  subject(:test_menu) { described_class.new(food) }
+
+  let(:food) do
+    { 
+      thing1: 3.00,
+      thing2: 5.00
+    }
+  end
   
   context 'display menu' do # user can see menu
 
-    it 'shows an array of hashes (menu items with prices)' do
-      expect(subject).to respond_to(:food) # Menu class reads :food attribute
-      expect(subject.food).to be_a(Array) # :food is a hash     
+    # it 'has an array of hashes (menu items with prices)' do
+    #   expect(subject).to respond_to(:food) # Menu class reads :food attribute
+    #   expect(subject.food).to be_a(Array) # :food is a hash     
+    # end
+    it 'has a list of items with prices' do
+      expect(subject.food).to eq(food)
+    end
+
+    it 'shows the menu items with prices' do
+      displayed_menu = "Thing1 £3.00, Thing2 £5.00"
+      expect(subject.show).to eq(displayed_menu)
     end
   end
 end
