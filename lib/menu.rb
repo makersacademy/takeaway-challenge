e18c3bbc2202dd7143c118b9483a7d52f79ac025
Marcus <marcus.kerr94@gmail.com>
2018-07-22 10:21:00 +0100

    Created dish class, attribute reader in menu class for dish (GREEN)

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,12 @@
 #Return all the dishes
 #start by returning one dish 
 #name and price
+require_relative "dish"
 class Menu
+
+  attr_reader :dish
+  
+  def initialize
+    @dish = Dish.new
+  end
 end
