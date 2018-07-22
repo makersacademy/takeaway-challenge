198c4412e0a112769abdd83078893391c2dce5b3
Marcus <marcus.kerr94@gmail.com>
2018-07-22 11:28:07 +0100

    Additional unit tests for dish, test name and price can be passed when created (GREEN)

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,13 +1,25 @@
 require "dish"
 describe Dish do
   subject(:dish) { Dish.new }
+  let(:name_of_dish) { "Shrimp fried rice" }
+  let(:price_of_dish) { 15 }
 
   describe "#initialize" do
-    it "Iniitalisez with a default name, meat" do
+    it "iniitalizes with a default name, meat" do
       expect(dish.name).to eq("meat")
     end
-    it "Iniitalisez with a default price, 10" do
+    it "iniitalizes with a default price, 10" do
       expect(dish.price).to eq(10)
+    end
+
+    it "allows name to be set when initialized" do
+      new_dish = Dish.new(name_of_dish)
+      expect(new_dish.name).to eq name_of_dish  
+    end
+
+    it "allows price to be set when initialized" do
+      new_dish = Dish.new(nil, price_of_dish)
+      expect(new_dish.price).to eq price_of_dish  
     end
   end 
 end 
