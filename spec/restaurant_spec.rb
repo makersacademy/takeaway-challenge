e36c4bd9c105f7106c7aab16d0a4560f32ad6d0c
Marcus <marcus.kerr94@gmail.com>
2018-07-22 09:03:47 +0100

    Unit test: print_menu should return a menu object

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,7 +1,13 @@
 require "restaurant"
 describe Restaurant do
   
-  it "Responds to print_menu" do
+  it "responds to print_menu" do
     expect(subject).to respond_to(:print_menu)
+  end 
+
+  describe "#print_menu" do
+    it "returns a menu object" do
+      expect(subject.print_menu).to be_an_instance_of(Menu)
+    end 
   end 
 end
