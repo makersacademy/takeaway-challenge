2ca12f48c01aabef9d67ba915b2c7b5377906ea8
limcuili <limcuili@hotmail.com>
2018-07-22 14:19:20 +0100

    check total method

--- lib/order.rb
+++ lib/order.rb
@@ -6,21 +6,31 @@ class Order
     @order_array = []
   end
 
-  def add(food)
+  def add(food, quantity)
     @food = food
     Menu::MENU.each do |hash|
       if hash[:food] == @food.to_s
-        @order_array << { :food => hash[:food], :price => hash[:price] }
-        puts "1x #{@food}(s) has been added to your basket"
+        quantity.times do
+          @order_array << { :food => hash[:food], :price => hash[:price] }
+        end
+        puts "#{quantity}x #{@food}(s) has been added to your basket"
         return @order_array
       end
     end
   end
 
-  def check
+  def check_dishes
     @order_array.each do |hash|
       puts "1x #{hash[:food]}"
     end
+  end
+
+  def check_total
+    total = 0
+    @order_array.each do |hash|
+      total += hash[:price]
+    end
+    total
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,27 +1,48 @@
 require 'order'
 require 'menu'
 
-describe Order do
+RSpec.describe Order do
 
   describe '#add' do
-    it { is_expected.to respond_to(:add).with(1).argument }
+    it { is_expected.to respond_to(:add).with(2).arguments }
 
     it 'accepts a dish order and lets you know what has been added' do
-      expect { subject.add('pizza') }.to output("1x pizza(s) has been added to your basket\n").to_stdout
+      expect { subject.add('pizza', 1) }.to output("1x pizza(s) has been added to your basket\n").to_stdout
     end
 
     it 'returns the order array of one item' do
-      expect(subject.add('pizza')).to match_array([{ :food => 'pizza', :price => 6.5 }])
+      expect(subject.add('pizza', 1)).to match_array([{ :food => 'pizza', :price => 6.5 }])
+    end
+
+    it ' returns the order array of 3 items' do
+      subject.add('rice side dish', 2)
+      expect(subject.add('beef', 1)).to match_array([{ :food => 'rice side dish', :price => 1.5 }, { :food => 'rice side dish', :price => 1.5 }, { :food => 'beef', :price => 5.5 }])
     end
   end
 
-  describe '#check' do
-    it { is_expected.to respond_to(:check) }
+  describe '#check_dishes' do
+    it { is_expected.to respond_to(:check_dishes) }
 
     it 'shows you a 2-item order' do
-      subject.add('pizza')
-      subject.add('chicken')
-      expect{subject.check}.to output("1x pizza\n1x chicken\n").to_stdout
+      subject.add('pizza', 1)
+      subject.add('chicken', 1)
+      expect { subject.check_dishes }.to output("1x pizza\n1x chicken\n").to_stdout
+    end
+
+    it 'shows you a 3-item order, repeating one' do
+      subject.add('vegetarian', 2)
+      subject.add('beef', 1)
+      expect { subject.check_dishes }.to output("1x vegetarian\n1x vegetarian\n1x beef\n").to_stdout
+    end
+  end
+
+  describe '#check_total' do
+    it { is_expected.to respond_to(:check_total) }
+
+    it 'calculates the total price' do
+      subject.add('risotto', 1)
+      subject.add('vegetarian', 1)
+      expect(subject.check_total).to eq 10
     end
   end
 
