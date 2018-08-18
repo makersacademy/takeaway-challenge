76106c134ec5d3578190c4fe05b7a4aaa25688b4
Samir Soormally <samirsoormally@gmail.com>
2018-08-18 16:06:03 +0100

    User Story 1
    As a customer
    So that I can check if I want to order something
    I would like to see a list of dishes with prices

---
+++ lib/menu.rb
@@ -0,0 +1,6 @@
+class Menu
+
+  def see_dishes
+    {spring_rolls: 3.99 , crispy_duck: 11.95, chop_suey: 10.95, fried_rice: 4.99}
+  end
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,11 @@
+require 'menu'
+
+describe Menu do
+subject(:menu) {described_class.new}
+  it { is_expected.to respond_to(:see_dishes) }
+  describe '#see_dishes' do
+    it 'shows available dishes' do
+      expect(menu.see_dishes).to eq({spring_rolls: 3.99 , crispy_duck: 11.95, chop_suey: 10.95, fried_rice: 4.99})
+    end
+  end
+end
