8426404c56631944f48596fe530bfa8ece1fbeb3
James Sutherland <jrsutherland78@googlemail.com>
2018-08-18 16:35:57 +0100

    .Order #menu menu created and stored

--- lib/order.rb
+++ lib/order.rb
@@ -1,20 +1,17 @@
+require_relative 'menu'
+
 class Order
 
   attr_reader :menu
 
-  def initialize
-    @menu = [{ food: "Chick'n Burger", price: 7 },
-             { food: "Chick'n Spicy Burger", price: 7 },
-             { food: "Hamburger Deluxe", price: 6 },
-             { food: "Hamburger Cheese", price: 5 },
-             { food: "Fries", price: 2 },
-             { food: "Mac 'n Cheese", price: 5 },
-             { food: "Coleslaw", price: 3 },
-             { food: "Gravy", price: 1 }]
+  def initialize(menu = Menu.new)
+    @menu = menu
   end
 
   def list_menu
-    @menu.each_with_index { |menu, index| puts "#{index + 1}: #{menu[:food]} £#{menu[:price]}" }
+    @menu.each_with_index { |menu, index|
+      puts "#{index + 1}: #{menu[:food]} £#{menu[:price]}"
+    }
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,9 +2,22 @@ require 'order'
 
 describe Order do
 
+  menu = [{ food: "food1", price: 1 }]
+  let(:order) { Order.new(menu) }
+
+  it 'sets @menu on initialize' do
+    expect(order.menu).to eq menu
+  end
+
   describe '#list_menu' do
     context 'returns #menu' do
-      it { expect(subject.list_menu).to eq subject.menu }
+      it { expect(order.list_menu).to eq order.menu }
+    end
+  end
+
+  describe '#menu' do
+    context 'stores menu' do
+      it { expect(order.menu).to eq menu }
     end
   end
 
