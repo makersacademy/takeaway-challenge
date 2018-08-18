3ac1fdffc098a0e281f6e0c60e90e7e7362f7741
jdm79 <james.malvern@gmail.com>
2018-08-18 21:21:15 +0100

    adds comments to look at tomorrow

--- lib/order.rb
+++ lib/order.rb
@@ -4,10 +4,12 @@ class Order
 
   attr_reader :menu
 
+  # is it worth having menu in a separate module?
   def initialize(menu = Menu::MENU)
     @menu = menu
   end
   
+  # this could be its own class/module of format_menu
   def display_menu
       puts "\n"
       puts "Our Incredible Menu"
