039ceb70400e98f962d2f566d32c59d7a3d6dd0f
leekarho <k-h_lee@hotmail.co.uk>
2018-08-18 20:17:40 +0100

    send text with time

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,10 +1,22 @@
 require 'twilio-ruby'
 
-account_sid = "AC63e265e321d14d7abcc4d080a20be8db"
-auth_token = "6c5a1d7fe6cb8361c094f06bcfb6a7df"
+class Sms
 
-@client = Twilio::REST::Client.new account_sid, auth_token
-message = @client.messages.create(
-    body: "Hello from Ruby",
-    to: "",
-    from: "+441344231763")
+  def initialize
+    account_sid = "AC63e265e321d14d7abcc4d080a20be8db"
+    auth_token = "6c5a1d7fe6cb8361c094f06bcfb6a7df"
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  def send_message
+      message = @client.messages.create(
+        body: "Thank you! Your order will be delivered by #{delivery_time}",
+        to: "+447425275774",
+        from: "+441344231763")
+  end
+
+  def delivery_time
+    Time.now + 3600
+  end
+
+end
