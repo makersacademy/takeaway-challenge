455611278d4feb0d5fc2822058b7b2d83756fd23
leekarho <k-h_lee@hotmail.co.uk>
2018-08-18 14:43:51 +0100

    censoring twilio account details

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,10 +1,10 @@
 require 'twilio-ruby'
 
-account_sid = "AC63e265e321d14d7abcc4d080a20be8db"
-auth_token = "6c5a1d7fe6cb8361c094f06bcfb6a7df"
+account_sid = ""
+auth_token = ""
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 message = @client.messages.create(
     body: "Hello from Ruby",
-    to: "+447425275774",
-    from: "+441344231763")
+    to: "",
+    from: "")
