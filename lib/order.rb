fc08627111d924974dc76f230aa5f42945bf8aa0
James Sutherland <jrsutherland78@googlemail.com>
2018-08-18 13:34:40 +0100

    .Order #list_menu returns #menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,20 @@
 class Order
 
+  attr_reader :menu
+
+  def initialize
+    @menu = [{ food: "Chick'n Burger", price: 7 },
+             { food: "Chick'n Spicy Burger", price: 7 },
+             { food: "Hamburger Deluxe", price: 6 },
+             { food: "Hamburger Cheese", price: 5 },
+             { food: "Fries", price: 2 },
+             { food: "Mac 'n Cheese", price: 5 },
+             { food: "Coleslaw", price: 3 },
+             { food: "Gravy", price: 1 }]
+  end
+
   def list_menu
-    'food list'
+    @menu.each_with_index { |menu, index| puts "#{index + 1}: #{menu[:food]} £#{menu[:price]}" }
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,8 +3,8 @@ require 'order'
 describe Order do
 
   describe '#list_menu' do
-    it 'lists the menu' do
-      expect(subject.list_menu).to eq 'food list'
+    context 'returns #menu' do
+      it { expect(subject.list_menu).to eq subject.menu }
     end
   end
 
