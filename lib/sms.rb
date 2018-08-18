c2e3983aa313690987d75875326bfd701548a855
leekarho <k-h_lee@hotmail.co.uk>
2018-08-18 20:53:50 +0100

    details censored

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,19 +1,18 @@
 require 'twilio-ruby'
-require 'dotenv/load'
 
 class Sms
 
   def initialize
-    account_sid = ENV['"AC63e265e321d14d7abcc4d080a20be8db"']
-    auth_token = ENV['"AC63e265e321d14d7abcc4d080a20be8db"']
+    account_sid = 'twilio acc sid'
+    auth_token = 'twilio auth token'
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
   def send_message
       message = @client.messages.create(
         body: "Thank you! Your order will be delivered by #{delivery_time}",
-        to: "+447425275774",
-        from: "+441344231763")
+        to: "my number",
+        from: "your number")
   end
 
   def delivery_time
