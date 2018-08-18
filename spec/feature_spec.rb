1d13219da6a4357088ec6e1d8035c58b55a8383f
josh nickson <jnickson1506@gmail.com>
2018-08-18 13:47:39 +0100

    add order class

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,26 +4,37 @@ require 'menu'
 describe '__TAKEAWAY FEATURE TESTS__' do
 
   it 'shows a list of dishes with prices' do
-    given_that_there_are_some_dishes
-    the_dishes_can_be_added_to_a_menu
-    the_menu_can_be_viewed
+    a__given_that_there_are_some_dishes
+    b__the_dishes_can_be_added_to_a_menu
+    c__the_menu_can_be_viewed
   end
 
-  def given_that_there_are_some_dishes
+  it 'allows customer to select a number of several dishes' do
+    a__given_that_there_are_some_dishes
+    b__the_dishes_can_be_added_to_a_menu
+    d__an_order_can_be_created
+    # e__the_customer_can_add_menu_items_to_order
+  end
+
+  def a__given_that_there_are_some_dishes
     @dish_1 = Dish.new('name1', 1)
     @dish_2 = Dish.new('name2', 2)
     @dish_3 = Dish.new('name3', 3)
   end
 
-  def the_dishes_can_be_added_to_a_menu
+  def b__the_dishes_can_be_added_to_a_menu
     @menu = Menu.new
     @menu.add(@dish_1)
     @menu.add(@dish_2)
     @menu.add(@dish_3)
   end
 
-  def the_menu_can_be_viewed
+  def c__the_menu_can_be_viewed
     @menu.view_menu
+  end
+
+  def d__an_order_can_be_created
+    @order = Order.new
   end
 
 end
---
+++ spec/order_spec.rb
@@ -0,0 +1,13 @@
+require 'order'
+
+describe Order do
+
+  it 'creates an order' do
+    expect(described_class).to respond_to(:new)
+  end
+  
+  it'adds an item to the order' do
+
+  end
+
+end
