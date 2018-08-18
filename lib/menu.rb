18dfceb33045c5c6e4cb37181982015b57a5cd16
leekarho <k-h_lee@hotmail.co.uk>
2018-08-18 11:12:23 +0100

    sets default menu and view menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,15 +1,22 @@
-
+# require_relative 'orders'
 
 class Menu
 
-  def initialize
-    menu = {pie: 4.50
-    sirloin_steak: 12.99
-    cranberry_juice: 2.50
-    
-    }
+  attr_reader :menu_items
+
+  MENU_SELECTIONS = {pie: 4.50,
+  sirloin_steak: 12.99,
+  cranberry_juice: 2.50,
+  chips: 2.50,
+  boiled_egg: 0.99
+  }
+
+  def initialize(menu_items = MENU_SELECTIONS)
+    @menu_items = menu_items
   end
 
-
+  def view_menu
+    @menu_items.each { |item, price| puts "#{item.to_s}: £#{price}"}
+  end
 
 end
