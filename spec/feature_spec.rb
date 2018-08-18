035cac33f864693dc108dfa45f36c2a9e9cf79ba
Lucien Najev <luciennajev@hotmail.com>
2018-08-18 22:28:04 +0100

    menu spec and lib files set up

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -14,8 +14,10 @@
 # So that I am reassured that my order will be delivered on time
 # I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered
 
-# 
+#
 # #feature tests
 # require './lib/takeaway.rb'
+# require './lib/menu.rb'
+# menu = Menu.new
 # takeout = Takeaway.new
 # takeout.view_menu
