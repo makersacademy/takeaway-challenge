e088b90763bb3303074c268a6e8aa94d6b3a8ce1
jdm79 <james.malvern@gmail.com>
2018-08-18 15:43:58 +0100

    amends test to check if show_menu returns a hash

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,8 +4,8 @@ describe Order do
 
   describe '#show_menu' do
     it 'displays a menu with prices' do
-      expect(subject.show_menu).to eq Hash
+      expect(subject.show_menu).to be_a_kind_of(Hash)
     end
   end
-  
+
 end
