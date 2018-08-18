76348731dfb8d0d5c0e3d86185179dfa913fa9fc
Lucien Najev <luciennajev@hotmail.com>
2018-08-18 21:46:43 +0100

    initialize method added with menu as an instance variable put in using dependency injection, basic test of view_menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,18 @@
 class Takeaway
 
+  attr_reader :menu
+
+  def initialize(menu)
+    @menu = menu
+  end
+
 
   def view_menu
+    "Pizza: £7.50"
 
   end
 
-  
+
 
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,11 +1,18 @@
+require 'takeaway'
+
 describe Takeaway do
 
   describe "#view_menu" do
 
-    let(:takeaway) { described_class.new }
+    let(:takeaway) { described_class.new(menu: menu) }
+    let(:menu) { double(:menu) }
 
     it "responds to this method" do
       expect(takeaway).to respond_to(:view_menu)
+    end
+
+    it "allows us to view a menu" do
+      expect(takeaway.view_menu).to eq("Pizza: £7.50")
     end
   end
 
