6a651a2ba8f4094fc95c51681a74fcf0debd5277
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-18 14:26:31 +0100

    add .chose_dish and #current

--- lib/orders.rb
+++ lib/orders.rb
@@ -3,8 +3,18 @@ require './lib/dishes'
 class Orders
   include Dishes
 
+  attr_reader :current
+
+  def initialize
+    @current = {}
+  end
+
   def view_menu
     list 
+  end
+
+  def choose_dish(dish, quantity)
+    @current[dish] = quantity
   end
 
 end
--- spec/orders_spec.rb
+++ spec/orders_spec.rb
@@ -3,9 +3,30 @@ require 'orders'
 describe Orders do
 
   let(:order) { Orders.new }
+  let(:dish)  { "Donner Kebab" }
+  let(:qty)   { 2 }
+  
 
-  it "shows menu with prices" do
-    expect { order.view_menu }.to output("Burger & Chips - £7\nDonner Kebab - £8\nMystery Curry - £9\nHoisin Duck Pankcakes - £10\n").to_stdout
+  describe "#current" do
+    it "creates an empty hash upon initialization" do
+      expect(order.current).to eq({})
+    end
+  end
+
+  describe ".view_menu" do
+    it "shows menu with prices" do
+      expect { order.view_menu }.to output("Burger & Chips - £7\nDonner Kebab - £8\nMystery Curry - £9\nHoisin Duck Pankcakes - £10\n").to_stdout
+    end
+  end
+
+  describe ".choose_dish" do
+    it { is_expected.to respond_to(:choose_dish).with(2).argument }
+
+    it "will input dish and quantity to #current hash" do
+      order.choose_dish(dish, qty)
+      expect(order.current).to eq({ dish => qty })
+    end
+
   end
 
 end
