d9c1b71958cea9b119ff372bb888703b357c2531
Aneel Marshall <aneel.marshall@outlook.com>
2018-08-19 10:21:35 +0100

    add #dishes to return dishes and prices

---
+++ lib/takeaway.rb
@@ -0,0 +1,5 @@
+class Takeaway
+  def dishes
+    @dishes = [{dish: 'Fish and chips', price: 8}]
+  end
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,1 +1,11 @@
 require 'takeaway'
+
+describe Takeaway do
+
+  it '#dishes returns a list of dishes and prices' do
+    expect(
+      subject.dishes
+    ).to include(dish: 'Fish and chips', price: 8)
+  end
+
+end
