2606a0e0e798a2621a0a81178c818ded0e04aa92
Lucien Najev <luciennajev@hotmail.com>
2018-08-19 13:18:26 +0100

    added initialize method and put the menu in an instance variable

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,10 +1,14 @@
 class Takeaway
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = { "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 }
+  end
 
 
   def view_menu
-    { "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 }
-
+    dishes
   end
 
 
