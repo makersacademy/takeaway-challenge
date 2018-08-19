8a2fb065f0770a435dd69dba193b050470bf571d
Lucien Najev <luciennajev@hotmail.com>
2018-08-19 13:15:35 +0100

    trying to add the menu in small steps

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,9 +3,10 @@ class Takeaway
 
 
   def view_menu
+    { "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 }
 
   end
 
-  
+
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,6 +7,12 @@ describe Takeaway do
     it "shows a list of dishes with prices" do
       expect(subject).to respond_to(:view_menu)
     end
+
+    
+
+    it "shows a shows a list of dishes with prices" do
+      expect(subject.view_menu).to eq({ "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 })
+    end
   end
 
 
