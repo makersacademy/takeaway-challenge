b0ad5f4b727b1ee7e07cf580dee21a93b87b15a8
Lucien Najev <luciennajev@hotmail.com>
2018-08-19 18:23:30 +0100

    basis for the menu class created added the print menu method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,21 @@
 class Menu
 
+  attr_reader :dishes
+
+
+  def initialize(dishes = { "pepperoni" => 7.50, "hawaiian" => 6.50, "vegetarian" => 5.50 })
+    @dishes = dishes
+  end
+
+
+
+
   def print_menu
+    dishes.map { |dish, price| "#{dish.capitalize} £#{'%.2f' % price}" }.join(", ")
 
   end
 
-  
+
 
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,12 +2,22 @@ require 'menu'
 
 describe Menu do
 
-  subject(:menu) { described_class.new }
+  subject(:menu) { described_class.new(dishes) }
+  let(:dishes) { { "pepperoni" => 7.50, "hawaiian" => 6.50, "vegetarian" => 5.50 } }
 
 describe "#print_menu" do
   it "should print the menu" do
     expect(menu).to respond_to(:print_menu)
   end
+  it "should print the menu" do
+    expect(menu.dishes).to eq(dishes)
+  end
+  it "should print the menu" do
+    printed_menu = "Pepperoni £7.50, Hawaiian £6.50, Vegetarian £5.50"
+    expect(menu.print_menu).to eq(printed_menu)
+  end
+
+
 end
 
 end
