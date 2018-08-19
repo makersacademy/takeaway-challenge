4a8205eb6e676e450a58285807ec62816bbc81f7
jdm79 <james.malvern@gmail.com>
2018-08-19 17:37:30 +0100

    adds .add_item as well as @basket

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,19 @@
+require './data/takeaway_menu.rb'
+
 class Takeaway
 
   attr_reader :menu
+  attr_accessor :basket
   def initialize(menu = MENU_LIST)
     @menu = menu
+    @basket = []
   end
 
   def display_menu
     menu
+  end
+
+  def add_item(item)
+    @basket.push(item)
   end
 end
