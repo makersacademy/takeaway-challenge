cf5aa8178d358d78aafbda3fded162263d35b864
jdm79 <james.malvern@gmail.com>
2018-08-19 17:25:30 +0100

    adds test to check if menu is returned from .display_menu and is a type Hash

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,4 +1,11 @@
-require './lib/takeaway.rb'
+require 'takeaway'
+require 'order'
 
 describe Takeaway do
+  
+  describe "#display_menu" do
+    it "should be able to access the menu" do
+      expect(subject.display_menu).to be_a_kind_of(Hash)
+    end
+  end
 end
