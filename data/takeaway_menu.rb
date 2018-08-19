96644cdf389b34fff3b1c8421b46c260eaaa034c
jdm79 <james.malvern@gmail.com>
2018-08-19 21:20:51 +0100

    amends list layout as dictated by rubocop

--- data/takeaway_menu.rb
+++ data/takeaway_menu.rb
@@ -1,1 +1,5 @@
-MENU_LIST = { "Katsu" => 6, "Curry" => 5, "Pizza" => 4, "Kebab" => 3, "Congee" => 2 }
+MENU_LIST = { "Katsu" => 6,
+              "Curry" => 5, 
+              "Pizza" => 4, 
+              "Kebab" => 3, 
+              "Congee" => 2 }
