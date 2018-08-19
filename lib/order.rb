98073b69f4cd3c7c4b5e9460c1c4766e356b832d
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-19 22:22:39 +0100

    Adds #calculate_total method to Order

--- lib/order.rb
+++ lib/order.rb
@@ -1,21 +1,19 @@
 class Order
-# the Order class has an instance variable @o_items
-# o_items is an array to store any user-selected dishes (instances of OrderItem)
-# the Order class has a method to create order items.
-# This takes user input of the MenuItem's number, and the quantity. If no qty is given, default to 1.
-# Then an OrderItem is created from the MenuItem corresponding to this info, and pushed into the @o_items array.
-# There is a method for calculating the total
-# This done by multiplying the price & quantity of each element in the o_items array,
-# and is then formatted to look pretty / have £ sign etc.
-# User can then confirm an order! Which triggers Twilio to send the message (and
-# also shows the time of now +1hr as delivery time)
-attr_reader :o_items
+  attr_reader :o_items
 
-def initialize
-  @o_items = []
-end
+  def initialize
+    @o_items = []
+  end
 
   def create_order_item(menu_item:, quantity:)
     @o_items << OrderItem.new(menu_item.number, menu_item.dish, menu_item.price, quantity)
+  end
+
+  def calculate_total
+    total = 0
+    @o_items.each do | o_item |
+      total += o_item.price.to_f * o_item.quantity.to_i
+    end
+    "£#{sprintf("%0.2f", total)}"
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,15 +5,25 @@ describe Order do
 
     it "takes a user's input and creates a new OrderItem" do
 
-    menu_item_double = double(number: "01", dish: "spaghetti", price: "5.40")
-    qty = "2"
+      menu_item_double = double(number: "01", dish: "spaghetti", price: "5.40")
+      qty = "2"
 
-    order_item_double = double
-    expect(OrderItem).to receive(:new).with(menu_item_double.number, menu_item_double.dish, menu_item_double.price, qty).and_return(order_item_double)
+      order_item_double = double
+      expect(OrderItem).to receive(:new).with(menu_item_double.number, menu_item_double.dish, menu_item_double.price, qty).and_return(order_item_double)
 
-    subject.create_order_item(menu_item: menu_item_double, quantity: qty)
+      subject.create_order_item(menu_item: menu_item_double, quantity: qty)
 
-    expect(subject.o_items).to eq([order_item_double])
+      expect(subject.o_items).to eq([order_item_double])
+    end
+  end
+
+  describe "#calculate_total" do
+    it "returns the order total, formatted nicely" do
+      order_item_one = double(price: "5.00", quantity: "1")
+      order_item_two = double(price: "3.00", quantity: "2")
+      subject.instance_variable_set(:@o_items, [order_item_one, order_item_two] )
+
+      expect(subject.calculate_total).to eq "£11.00"
 
     end
   end
