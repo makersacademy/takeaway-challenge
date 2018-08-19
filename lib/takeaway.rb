aab6a7a60c574a194030906f136ff621ed2bdda3
Lucien Najev <luciennajev@hotmail.com>
2018-08-19 13:31:08 +0100

    added a method add_to_order with a responds to test

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,19 @@ class Takeaway
 
   attr_reader :dishes
 
-  def initialize
-    @dishes = { "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 }
+  def initialize(dishes = { "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 })
+    @dishes = dishes
   end
-
 
   def view_menu
     dishes
   end
+
+
+  def add_to_order(dish, quantity)
+  end
+
+
 
 
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,19 +2,19 @@ require 'takeaway'
 
 describe Takeaway do
 
+  subject(:takeaway) { described_class.new }
+
 
   describe "#view_menu" do
-    it "shows a list of dishes with prices" do
-      expect(subject).to respond_to(:view_menu)
-    end
-
-    
-
     it "shows a shows a list of dishes with prices" do
-      expect(subject.view_menu).to eq({ "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 })
+      expect(takeaway.view_menu).to eq({ "Pepperoni" => 7.50, "Hawaiian" => 6.50, "vegetarian" => 5.50 })
     end
   end
 
-
+  describe "#add_to_order" do
+    it "can select some number of several available dishes" do
+      expect(takeaway).to respond_to(:add_to_order).with(2).arguments
+    end
+  end
 
 end
