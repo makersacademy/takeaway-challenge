ffb240170879bd9f476c03417d45e1993c3157ce
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-19 23:44:01 +0100

    Adds main program functionality

--- lib/main.rb
+++ lib/main.rb
@@ -1,19 +1,36 @@
-# This main file should be run on start up
-# It will have all the references to other files ('require'-s etc)
-# There will be a class called TakeawayProcess or something similar
-# It will have a method on self called start (this will have a loop in it)
-# This class method will call the other methods to:
-# load the given menu
-# display the menu, prettily
-# Order.create_order_items (from user input) <- this is the looped bit
-# Order.formatted total (the pretty version) (when user has finished ordering stuff)
-# Confirm order (the bit that says "thanks" and sends Twilio confirmation)
+require "dotenv/load"
+require "twilio-ruby"
+require_relative "menu"
+require_relative "menu_item"
+require_relative "messager"
+require_relative "order"
+require_relative "order_item"
 
-# In order to run the program, a user just has to require 'main.rb' and run: OrderProcess.start
+class OrderProcess
+  def self.start
+    menu = Menu.new
+    menu.load_from_csv
+    puts "Hello and welcome to the takeaway! Please see our menu:"
+    menu.display
 
-require 'twilio-ruby'
-require 'dotenv/load'
-require_relative 'menu'
-require_relative 'menu_item'
-require_relative 'order_item'
-require_relative 'order'
+    order = Order.new
+    loop do
+      puts "Please enter the ID number of an item to order"
+      item = gets.chomp.downcase
+      break if item == "q"
+      puts "Please enter the quanity of this item"
+      qty = gets.chomp.downcase # default to 1 if blank string /
+      break if qty == "q"
+      order.create_order_item(menu_item: menu.m_items[item], quantity: qty)
+      puts "When you've added all your items, please press Q to continue"
+    end
+
+    puts "Your order total is #{order.calculate_total}. If you're happy to proceed with this order, press 'y' to confirm. Otherwise press any other key to exit."
+    input = gets.chomp.downcase
+    if input == "y"
+      puts "Great, thanks for your order! You'll receive a SMS confirmation."
+      order.confirm
+    end
+    
+  end
+end
