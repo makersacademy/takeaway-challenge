3767477a790fd075d8db0e848dcfed8e7d1f9b87
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-19 16:57:01 +0100

    Adds #display method to Menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -6,10 +6,20 @@ class Menu
   DEFAULT_FILENAME = './lib/takeaway_menu.csv'
   attr_reader :m_items
 
+  def initialize
+    @m_items = {}
+  end
+
   def load_from_csv
     @m_items = {}
     CSV.foreach(DEFAULT_FILENAME, headers: true) do |row|
-      @m_items[row["number"]] = MenuItem.new(number: row["number"], dish: row["name"], price: row["price"])
+      @m_items[row["number"]] = MenuItem.new(number: row["number"], dish: row["dish"], price: row["price"])
+    end
+  end
+
+  def display
+    @m_items.each_value do | item |
+      puts "#{item.number}. #{item.dish.capitalize.ljust(22, " ")} £#{item.price.rjust(6, " ")}"
     end
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,7 +5,7 @@ describe Menu do
   describe '#load_from_csv' do
     it 'takes data from a csv and loads it into an instance variable' do
       csv_file = Tempfile.new('test.csv')
-      csv_file.syswrite("number,name,price\n01,potato,3.00")
+      csv_file.syswrite("number,dish,price\n01,potato,3.00")
       stub_const("Menu::DEFAULT_FILENAME", csv_file.path)
 
       menu_item_double = double
@@ -14,6 +14,16 @@ describe Menu do
 
       subject.load_from_csv
       expect(subject.m_items).to eq(expected_m_items)
+    end
+  end
+
+  describe '#display' do
+
+    it 'prints out a nicely aligned list of number, dish, & price' do
+      menu_item_double = double(number: "00", dish: "some food", price: "1.20")
+      subject.instance_variable_set(:@m_items, { "00" => menu_item_double })
+      expected_string = "00. Some food              £  1.20\n"
+      expect{subject.display}.to output(expected_string).to_stdout
     end
   end
 end
