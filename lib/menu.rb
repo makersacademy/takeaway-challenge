2ce07627c9ffdf3d4ba0e77a10e3146a63c16bc7
Lucien Najev <luciennajev@hotmail.com>
2018-08-19 12:59:50 +0100

    scrapping the project and starting agin, keep getting conflicts between the feature tests and unit tests and cant find my way back

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,18 +1,1 @@
-class Menu
 
-  attr_reader :menu
-
-  def initialize
-    @menu = { "Pepperoni" => 7.50, "Vegetarian" => 6.50, "hawaiian" => 5.50 }
-  end
-
-
-
-  def print_menu(menu)
-    menu
-  end
-
-
-
-
-end
