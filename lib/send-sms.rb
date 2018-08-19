5d55823408180d69fbc8225c9d00b4aa0d63efca
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-19 15:36:08 +0100

    refactor SMS class to use ENV

--- lib/send-sms.rb
+++ lib/send-sms.rb
@@ -1,5 +1,8 @@
 require 'twilio-ruby'
 require './lib/deliverytimecalc.rb'
+require 'dotenv'
+
+Dotenv.load
 
 class SMS
 
@@ -8,13 +11,13 @@ class SMS
   end
 
   def send_sms
-    account_sid = "AC9a38d2a7818f803e9c23ded219232787"
-    auth_token = "1b70eae15d0b80dcf309a3475f52f3d6"
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
 
     client = Twilio::REST::Client.new(account_sid, auth_token)
 
     from = "+447480486810"
-    to = "+447838205185"
+    to = ENV['PHONE_NUMBER']
 
     client.messages.create(
       from: from,
