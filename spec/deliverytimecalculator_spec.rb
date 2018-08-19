6ad572600269cd054fb592a166a4bf29aa8a0d97
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-19 15:10:34 +0100

    add README

--- spec/deliverytimecalculator_spec.rb
+++ spec/deliverytimecalculator_spec.rb
@@ -4,4 +4,12 @@ describe DeliveryTimeCalc do
 
   it { is_expected.to respond_to(:add_one_hour) }
 
+  it "will display one hour from now" do 
+    now = Time.new
+    hour = now.strftime("%H").to_i
+    minute = now.strftime("%M")
+
+    expect(subject.add_one_hour).to eq("#{hour + 1}:#{minute}")
+  end
+
 end
