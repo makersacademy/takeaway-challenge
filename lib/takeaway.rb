430eabf29331c05fb00b0893258c79bda2e5a32b
Aneel Marshall <aneel.marshall@outlook.com>
2018-08-19 10:58:16 +0100

    add #print_dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,5 +1,13 @@
 class Takeaway
+  attr_reader :dishes
   def dishes
-    @dishes = [{dish: 'Fish and chips', price: 8}]
+    @dishes = [{dish: 'Fish and chips', price: 8},
+    {dish: 'Paella', price: 10}]
+  end
+
+  def print_dishes
+    dishes.each do
+      |item| puts "#{item[:dish]}: £#{item[:price]}"
+    end
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -8,4 +8,11 @@ describe Takeaway do
     ).to include(dish: 'Fish and chips', price: 8)
   end
 
+  it '#print_dishes prints dishes and prices to screen' do
+    expect {
+      subject.print_dishes
+    }.to output(
+      "Fish and chips: £8\nPaella: £10\n"
+    ).to_stdout
+  end
 end
