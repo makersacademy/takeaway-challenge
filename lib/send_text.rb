45098e6dec78732051ed1cfcbd8dd0242b92fa4b
davidlawes <davidlawes@hotmail.co.uk>
2018-08-20 17:14:45 +0100

    Updated with environment variables

--- lib/send_text.rb
+++ lib/send_text.rb
@@ -1,13 +1,11 @@
 require 'twilio-ruby'
 require_relative 'delivery_time'
+require 'dotenv/load'
 
 class SendText
 
-  ACCOUNT_SID = 'AC762c2dec42d80e04def18dbbd0023dcf'
-  AUTH_TOKEN = '9f10624ce074e048597ea1ec849578aa'
-
   def initialize(twilio = Twilio::REST::Client, del_time = DeliveryTime.new)
-    @client = twilio.new(ACCOUNT_SID, AUTH_TOKEN)
+    @client = twilio.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
     @delivery_time = del_time
   end
 
@@ -15,8 +13,8 @@ class SendText
     time = @delivery_time.generate_time
     @message = @client.messages.create(
       body: "Thanks! Your order was placed, it'll be delivered before #{time}",
-      to: "+447891111865",
-      from: "+447480538749"
+      to: ENV['TO_NO'],
+      from: ENV['FROM_NO']
     )
     raise "Sorry, text message failed to send!" unless message_sent?
   end
