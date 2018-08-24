8410d59a58eebbe02ccd255fac33dccb6977c309
leekarho <k-h_lee@hotmail.co.uk>
2018-08-24 20:42:14 +0100

    improve testing

--- spec/bill_spec.rb
+++ spec/bill_spec.rb
@@ -2,20 +2,17 @@ require 'bill'
 
 describe Bill do
 
-  # let(:menu) { double :menu, :menu_items[:pie] => 4.5, :menu_items[:cranberry_juice] => 2.50 }
+  subject(:cust_bill) { described_class.new(get_orders, menu) }
   let(:get_orders) { { :pie => 2, :cranberry_juice => 2 } }
   let(:menu) { { :pie => 4.5, :cranberry_juice => 2.5 } }
 
   let(:conf) { double :conf }
 
   it "returns the bill" do
-    # get_orders = { :pie => 2, :cranberry_juice => 2 }
-    cust_bill = Bill.new(get_orders, menu)
     expect(cust_bill.bill).to eq 14
   end
 
   it 'settles the bill' do
-    # get_orders = { :pie => 2, :cranberry_juice => 2 }
     cust_bill = Bill.new(get_orders, menu)
     cust_bill.bill
     expect { cust_bill.conf_message(0, conf) }.to raise_error "please settle the bill"
--- spec/orders_spec.rb
+++ spec/orders_spec.rb
@@ -4,8 +4,8 @@ require 'orders'
 describe Orders do
 
   let(:menu) { double :menu, :menu_items => { :pie => 4.5, :chips => 2.50 } }
-  let(:mockbill) { double :mockbill, :bill => 14 }
   let(:bill_klass) { double :bill_klass, :new => mockbill }
+  let(:mockbill) { double :mockbill, :bill => 14 }
 
   it 'chooses the menu' do
     expect(subject.choose_menu(menu)).to eq menu.menu_items
@@ -16,29 +16,27 @@ describe Orders do
   end
 
   it "can place multiple orders" do
-    subject.choose_menu
+    subject.choose_menu(menu)
     subject.cust_order("pie", 3)
     subject.cust_order("chips", 2)
     expect(subject.cust_orders).to eq({ pie: 3, chips: 2 })
   end
 
   it "raises an error if dish is not on the menu" do
-    subject.choose_menu
+    subject.choose_menu(menu)
     expect { subject.cust_order('chicken', 2) }.to raise_error "sorry, dish is not available"
   end
 
   it "confirms customer order" do
-    subject.choose_menu
+    subject.choose_menu(menu)
     subject.cust_order("pie", 2)
     subject.cust_order("chips", 2)
     expect(subject.order_conf).to eq({ :pie => 2, :chips => 2 })
   end
 
   it "returns the bill" do
-    subject.choose_menu
-    subject.cust_order("pie", 2)
-    subject.cust_order("chips", 2)
-    expect(subject.bill(bill_klass)).to eq 14
+    expect(mockbill).to receive(:bill)
+    subject.bill(bill_klass)
   end
 
 end
