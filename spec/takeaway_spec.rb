7be2e8b0a022341d93f790c6aa31756d631cbfac
Vu Le <core.safecrlf>
2018-09-14 14:30:11 +0100

    refactored rspec unit test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,8 +1,8 @@
 require './lib/takeaway'
 
 describe Takeaway do
-  
+  let(:menu2) {menu2 = {:fish_and_chips => '£3.20', :burger_and_chips => '£4.20', :chips => '£1'}}
   it 'to be able to see the menu' do
-    expect(subject.menu).not_to raise_error
+    expect(subject.menu).to eq(menu2)
   end
 end
