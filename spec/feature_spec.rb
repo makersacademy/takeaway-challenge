725d01e1eca54306eba30971c45603c4afe8fa21
Vu Le <core.safecrlf>
2018-09-14 13:38:46 +0100

    added code to pass first error in feature test

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,10 +1,9 @@
-
-```
+'''
 As a customer
 So that I can check if I want to order something
 I would like to see a list of dishes with prices
 
-require './lib/takeaway.rb'
+require "./lib/takeaway.rb"
 takeaway = Takeaway.new
 takeaway.menu
 
@@ -32,4 +31,4 @@ I would like to check that the total I have been given matches the sum of the va
 As a customer
 So that I am reassured that my order will be delivered on time
 I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered
-```
+'''
