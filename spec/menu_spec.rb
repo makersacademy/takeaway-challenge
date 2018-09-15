85e8ce3792ea650d979338d6422bc26f1431a918
Emma Albury <emma_albury@hotmail.com>
2018-09-15 17:28:56 +0100

    created test for menu_list hash

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,14 @@
 require 'menu'
 
 describe Menu do
-  
+
+  let(:menu) { described_class.new }
+
+  context "menu_list" do
+    let(:menu_list) { { :a => 6.95, :b => 8.95 } }
+    it "provides a list of menu items and prices" do
+      expect(menu.menu_list).to include( { :a => 6.95, :b => 8.95 } )
+    end
+  end
+
 end
