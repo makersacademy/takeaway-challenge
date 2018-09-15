2eab389d51dbe21aac2d57eff49c8eb12a9bbab3
NadiaAiraf <aidanfaria@gmail.com>
2018-09-15 15:59:39 +0100

    4x basket_total tests to go and have fun trying to satisfy

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -35,6 +35,29 @@ describe TakeAway do
     it 'fails when you try to add a dish that is not on the menu' do
       expect{subject.add_dish('fish & chips')}.to raise_error("that dish isn't on the menu")
     end
+  end
 
+  describe '#basket_total' do
+      it 'gives you the total amount currently in your basket when basket is empty' do
+        expect(subject.basket_total).to eq 'there is nothing in your basket'
+      end
+
+      it 'gives the total amount when one thing is in your basket' do
+        subject.add_dish('Egg Fried Rice',1)
+        expect(subject.basket_total).to eq '1x Egg Fried Rice = £3.00. Total £3.00'
+      end
+      
+      it 'gives the total amount when one thing is in your basket' do
+        subject.add_dish('Egg Fried Rice',4)
+        subject.add_dish('Chicken Chowmein',2)
+        expect(subject.basket_total).to eq '4x Egg Fried Rice = £12.00 and 2x Chicken Chowmein = £9.00. Total £21.00'
+      end
+
+      it 'gives the total amount when one thing is in your basket' do
+        subject.add_dish('Egg Fried Rice',4)
+        subject.add_dish('Chicken Chowmein',2)
+        subject.add_dish('Crispy Shredded Beef',4)
+        expect(subject.basket_total).to eq '4x Egg Fried Rice = £12.00, 2x Chicken Chowmein = £9.00 and 4x Crispy Shredded Beef = £23.60. Total £44.60'
+      end
   end
 end
