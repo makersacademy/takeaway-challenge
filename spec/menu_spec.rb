dcaeefe5d7cb98f1230dbe06836c414208f4d68d
Jon Urry <jon@urry.me>
2018-09-15 16:40:39 +0100

    populate menu with dishes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,8 +7,16 @@ describe Menu do
       expect(menu).to receive(:dishes).and_return Array
       menu.dishes
     end
-    it 'should contain one or more dishes' do
+    it 'should contain one or more items' do
       expect(menu.dishes.length).to be >= 1
+    end
+    it 'should contain dishes with names' do
+      first_dish = menu.dishes.first
+      expect(first_dish.name).to be_a(String)
+    end
+    it 'should contain dishes with prices' do
+      first_dish = menu.dishes.first
+      expect(first_dish.price).to be_a(Float)
     end
   end
 end
