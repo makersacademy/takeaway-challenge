f005d16c2198e5988f2132c8839570d0d1606352
Cameron Whitehead <camjw119@gmail.com>
2018-09-15 15:43:12 +0100

    Completed takeaway spec and class. All tests passing with 100% coverage

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,57 @@
-require 'menu'
+require './lib/menu'
+require './lib/text_handler'
 
 class Takeaway
 
-  def initialize(menu_type: Menu)
+  attr_reader :basket
+
+  def initialize(menu_type: Menu, text_handler: TextHandler)
     @menu = menu_type.new
+    @basket = Hash.new(0)
+    @text_handler = text_handler.new
   end
 
   def display_menu
     @menu.display_menu
-  end 
+  end
 
+  def add_to_basket(dish, number = 1)
+    raise "That dish is not on the menu" unless on_the_menu?(dish)
+    @basket[@menu.search_menu(dish)] += number
+  end
 
+  def display_basket
+    @basket.each do |dish, quantity|
+      puts "#{dish[:name]} x #{quantity} = #{num_to_currency(dish[:price] * quantity)}"
+    end
+  end
+
+  def checkout(sum)
+    raise "Wrong sum entered, please enter correct sum" unless basket_sum?(sum)
+    puts "You will shortly receive a text confirming your order"
+    @text_handler.confirm_order(@basket)
+    empty_basket
+  end
+
+  private
+
+  def basket_sum?(sum)
+    basket_total = @basket.reduce(0) do |total, (dish, quantity)|
+      total + (dish[:price] * quantity)
+    end
+    basket_total == sum
+  end
+
+  def on_the_menu?(dish)
+    @menu.on_the_menu?(dish)
+  end
+
+  def empty_basket
+    @basket = Hash.new(0)
+    puts "Your basket is now empty"
+  end
+
+  def num_to_currency(price)
+    sprintf("£%2.2f", price)
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,13 +1,54 @@
 require 'takeaway'
+require 'pry'
 
 RSpec.describe Takeaway do
 
   let(:mock_menu_type) { double :mock_menu_type, new: mock_menu }
-  let(:mock_menu)      { double :mock_menu, display_menu: "Menu items and prices" }
-  subject { described_class.new(menu_type: mock_menu_type) }
+  let(:mock_menu) { double :mock_menu, display_menu: "Menu items and prices",
+                    on_the_menu?: true, search_menu:
+                    { name: "Spaghetti and Meatballs", price: 5.00 } }
+  let(:mock_dish) { double :mock_dish, name: "Spaghetti and Meatballs", price: "5" }
+  let(:mock_text_handler_type) { double :mock_text_handler_type, new: mock_text_handler }
+  let(:mock_text_handler) { double :mock_text_handler, confirm_order: true }
+
+
+  subject { described_class.new(menu_type: mock_menu_type, text_handler: mock_text_handler_type) }
 
   it "should display a list of dishes with prices" do
     expect(subject.display_menu).to eq "Menu items and prices"
+  end
+
+  it "should allow the user to select a number of dishes" do
+    subject.add_to_basket(mock_dish)
+    expect(subject.basket).to eq({ { name: "Spaghetti and Meatballs", price: 5.00 } => 1 })
+  end
+
+  it "should allow the user to order more than one dish at once" do
+    subject.add_to_basket(mock_dish, 4)
+    expect(subject.basket).to eq({ { name: "Spaghetti and Meatballs", price: 5.00 } => 4 })
+  end
+
+  it "should allow the user to see their basket printed in a clear format" do
+    subject.add_to_basket("Spaghetti and Meatballs")
+    expect { subject.display_basket }.to output("Spaghetti and Meatballs x 1 = £5.00\n").to_stdout
+  end
+
+  it "should allow the user to checkout if they give the correct total price of their basket" do
+
+  end
+
+  it "should raise an error if the user enters the incorrect total price on checkout" do
+    expect { subject.checkout(10) }.to raise_error "Wrong sum entered, please enter correct sum"
+  end
+
+  it "should text the user their order after checkout along with the time of arrival" do
+    subject.add_to_basket(mock_dish)
+    message = "You will shortly receive a text confirming your order\nYour basket is now empty\n"
+    expect { subject.checkout(5.00) }.to output(message).to_stdout
+  end
+
+  it "should allow the user to order by text" do
+
   end
 
 end
