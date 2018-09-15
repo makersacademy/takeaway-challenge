2809f659b370dd0ee1d6ae7c6f7091e05b4218af
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-15 16:24:59 +0000

    #add

---
+++ lib/order.rb
@@ -0,0 +1,43 @@
+class Order
+
+  def initialize
+    @order = []
+  end
+
+  def add(dish_price_quantity)
+    @order << dish_price_quantity
+    printer
+  end
+
+
+  private
+
+  def printer
+    three_new_lines
+    puts "Current order:"
+    one_new_line
+    @order.each do |dish|
+      puts "#{dish[:quantity]} #{dish[:name]}"
+    end
+    one_new_line
+    puts "Total: #{total_cost} silver sickles"
+    three_new_lines
+  end
+
+  def total_cost
+    total = 0
+    @order.each do |dish|
+      total += dish[:quantity] * dish[:price]
+    end
+    total
+  end
+
+  def three_new_lines
+    puts "\n\n\n"
+  end
+
+  def one_new_line
+    puts "\n"
+  end
+
+end
