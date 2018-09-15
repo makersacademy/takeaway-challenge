2b0198129f7379c97fd035e1197880c48a385de2
Florence Hapgood <flohapgood@icloud.com>
2018-09-15 13:30:35 +0100

    Fixed error from caused by adding £ and changing key value names, added Order class

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,10 +1,10 @@
 require "printer"
 
 describe Printer do
-  
+
   describe "#format" do
     it "prints the menu" do
-      expect { Printer.new.format([{ key: "Duck", value: "£12" }]) }.to output("Duck: £12\n").to_stdout
+      expect { Printer.new.format([{ Dish: "Duck", Price: "£12" }]) }.to output("Duck: £12\n").to_stdout
     end
 
   end
