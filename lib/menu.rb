2fe7dc11c36764251cec86dceabcb4d7f04cb6dc
Antonio <antoniodaria@msn.com>
2018-09-15 17:56:59 +0100

    Implementation user story one

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,16 @@
 class Menu
 
-attr_reader :dishes
+  attr_reader :dishes
 
   def initialize(dish_hash)
     @dishes = dish_hash
 
   end
 
-  # def printing_menu
-  #   @dishes
-  # end
+  def printing_menu
+    dishes.map do |name, price|
+      "#{name.to_s.capitalize} #{price}"
+    end.join(", ")
+  end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,24 +2,26 @@ require 'menu'
 require 'takeaway'
 
 describe Menu do
-   subject(:menu) { described_class.new(dish_hash) }
+  subject(:menu) { described_class.new(dish_hash) }
 
-   let(:dish_hash) do
-     {
-     pasta: 4.50,
-     pizza: 5.00,
-     veggy_wrap: 3.85,
-     noodles: 4.50
+  let(:dish_hash) do
+    {
+     pasta: 4.5,
+     pizza: 5.0,
+     veggy_wrap: 3.8,
+     noodles: 4.5
      }
-   end
+  end
 
-   it 'has a list of dishes and prices' do
-     expect(menu.dishes).to eq(dish_hash)
-   end
+  it 'has a list of dishes and prices' do
+    expect(menu.dishes).to eq(dish_hash)
+  end
 
-  # describe '#printing_menu' do
-    # it 'prints the menu' do
-    # end
-  # end
+  describe '#printing_menu' do
+    it 'prints the menu' do
+      formatted_menu = "Pasta 4.5, Pizza 5.0, Veggy_wrap 3.8, Noodles 4.5"
+      expect(menu.printing_menu).to eq(formatted_menu)
+    end
+  end
 
 end
