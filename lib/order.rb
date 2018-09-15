b17a05b5f7ea28196ba63352c4eb4ac3bfff4901
Florence Hapgood <flohapgood@icloud.com>
2018-09-15 14:02:14 +0100

    Order dependance injection

--- lib/order.rb
+++ lib/order.rb
@@ -4,12 +4,13 @@ class Order
 
   attr_reader :complete_order, :food_options
 
-  def initialize #(food_options = Menu.new.food_options)
+  def initialize(food_options = Menu.new.food_options)
     @complete_order = []
+    @food_options = food_options
   end
 
   def new_item(item)
-    Menu.new.food_options.each{ |hash|
+    food_options.each{ |hash|
       if item == hash[:Dish]
         @complete_order << hash
       end
