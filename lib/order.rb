fcccea6be8bbc581abcdeb15c5fe1977ce34a5ef
Darcie <darciewalsh@hotmail.com>
2018-09-15 19:39:33 +0100

    Can add dishes with prices and view basket

--- lib/order.rb
+++ lib/order.rb
@@ -2,32 +2,38 @@ require './lib/menu'
 
 class Order
 
-  attr_reader :basket, :total, :menu
+  attr_reader :basket, :total, :menu, :price_count
 
   def initialize(menu = Menu.new)
     @menu = menu
-    @basket = {}
+    @basket = []
     @total = 0
-    @count = 0
+    @price_count = []
   end
 
-  def add(dish, quantity)
-    @basket[dish] = quantity
-    #@basket.store(dish, quantity)
+  def add(dish)
+    menu.dishes.each do |item, price|
+      @basket << { dish => price } if dish == item
+    end
+    menu.dishes.each do |item, price|
+      @price_count << price if dish == item
+    end
   end
 
-  # def item_count
-  #   #@count += @count + quantity
-  #   @current_total = @count *= quantity
-  #   @basket[@current_total] = @count
-  # end
+  def prices
+    @price_count
+  end
 
   def view_basket
     @basket
   end
 
   def view_total
-
+    # sum of price_count array
+    # @total = @price_count.reduce { |sum, item| sum + item }
   end
-
 end
+
+#  def get_total(order)
+#   format_order(order).map { |item, quantity| quantity * menu.dishes[item] }.inject(0, :+)
+# end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,11 +1,12 @@
 require 'order'
 
 describe Order do
-  let(:order)     { described_class.new }
-  let(:dish)      { double('dish') }
-  let(:dish)      { double 'doughnut', price: 1.99 }
-  let(:menu)      { double  }
-  # let(:quantity)  { double(3) }
+  # let(:menu_double) { double :menu }
+  let(:order)       { described_class.new }
+  let(:dish_choice) { double :dish }
+  let(:quantity)    { double(1) }
+  # let(:dish) { double('dish) }
+
 
   describe 'a new order' do
     it 'starts with an empy basket' do
@@ -18,14 +19,37 @@ describe Order do
   end
 
   describe 'creating an order' do
-    it 'adds a dish from the menu to an order' do
-      order.add(dish, 3)
-      expect(order.basket).to include(dish => 3)
-      #expect(order.add(dish, 3)).to change { order.item_count }.by(3)
+
+    # before do
+    #   allow(menu_double).to receive(:dishes).and_return({ dish_choice => 1 })
+    # end
+
+    it 'adds a dish and its quantity from the menu to an order' do
+      order.add("doughnut")
+      order.add("cake")
+      expect(order.basket).to eq([{ "doughnut" => 2 }, { "cake" => 2 }])
     end
 
+    it 'adds the cost of a dish to #price_count' do
+      order.add("doughnut")
+      order.add("cake")
+      expect(order.price_count).to eq [2, 2]
+    end
+
+    # it 'can display the current total of an order' do
+    #   order.add_dish(dish, 2)
+    #   order.add_dish(dish, 3)
+    #   expect(order.view_total).to eq(8)
+    # end
+
     # it 'can view the basket' do
-    #   order.add
+    #   expect(order.view_basket).to eq basket
+    # end
+
+    # it 'updates the total of the basket' do
+    #   order.add(dish, 3)
+    #   order.add(dish, 4)
+    #   expect(order.total).to
     # end
   end
 end
