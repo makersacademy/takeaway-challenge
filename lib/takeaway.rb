cd19958cdd428c8ff1cb9e5409b499d01bc2e9ee
Alex Kharouk <akharoukpp@gmail.com>
2018-09-15 07:20:35 +0100

    solve 2nd and 3rd user story

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,33 @@
 class Takeaway
   attr_reader :menu
+
   def initialize
     @menu = {
       "pelmeni" => 6.99, "borscht" => 4.99, 
       "salat" => 5.99, "olivier" => 9.99, 
       "tvarog" => 2.99, "smetyana" => 1.99
     }
+    @order_total = []
+  end
+
+  def order(food_choice, quantity = 1)
+    raise "Sorry, we don't have that item." unless @menu.include? food_choice
+    calculate_total(food_choice, quantity)
+    "#{quantity}x - #{food_choice.capitalize} added to basket."
+  end
+
+  def order_total
+    "£#{@order_total.sum}"
+  end
+
+  private
+  
+  def calculate_total(food_choice, quantity)
+    @menu.each do |food, price|
+      if food_choice == food
+        @order_total << (price * quantity)
+      end
+    end
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,7 +2,7 @@ require 'takeaway'
 
 describe Takeaway do
   
-  context 'being able to create a takeaway class' do
+  context 'being able to use takeaway' do
 
     it "exists" do
       expect(subject).to eq subject
@@ -12,6 +12,23 @@ describe Takeaway do
       expect(subject.menu).to eq({ "pelmeni" => 6.99, "borscht" => 4.99, 
       "salat" => 5.99, "olivier" => 9.99, "tvarog" => 2.99, "smetyana" => 1.99 })
     end
+
   end
   
+  context 'being able to select food and check basket' do
+    
+    it "allows user to select choice with amount" do
+      expect(subject.order("pelmeni", 2)).to eq "2x - Pelmeni added to basket."
+    end
+
+    it "checks to make sure food item is on menu" do
+      expect { subject.order("a racecar", 9000) }.to raise_error "Sorry, we don't have that item."
+    end
+
+    it "checks to see the order total" do
+      subject.order("olivier", 2)
+      subject.order("borscht", 1)
+      expect(subject.order_total). to eq "£24.97"
+    end
+  end
 end
