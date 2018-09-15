4a3d4f124d08cebeed8f5c25c8ff075cd0f08a40
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-15 16:24:18 +0000

    testing #add

---
+++ spec/order_spec.rb
@@ -0,0 +1,12 @@
+require 'order'
+
+describe Order do
+  context "#add" do
+    let(:order) { described_class.new }
+    it "adds dish to @order" do
+      selected_dish = { name: "Butterbeer", price: 3, quantity: 44 }
+      return_string = "\n\n\nCurrent order:\n\n44 Butterbeer\n\nTotal: 132 silver sickles\n\n\n\n"
+      expect { order.add(selected_dish) }.to output(return_string).to_stdout
+    end
+  end
+end
