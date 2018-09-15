b9953dc76b3007aa0b617e60c3214cf69f86acda
Cameron Whitehead <camjw119@gmail.com>
2018-09-15 14:45:43 +0100

    Completed menu and menu spec, all tests passing, 100% coverage, and no rubocop offenses

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,7 +5,7 @@ class Menu
                   { name: "Chicken curry",           price: 7.00 },
                   { name: "Chop Suey",               price: 8.00 },
                   { name: "Pepperoni Pizza",         price: 6.99 },
-                  { name: "Pho",                     price: 10 },
+                  { name: "Pho",                     price: 9.50 },
                   { name: "Fried Chicken",           price: 4.50 },
                   { name: "Pad Thai",                price: 9.50 },
                   { name: "Hawaiian Pizza",          price: 5.50 },
@@ -22,7 +22,7 @@ class Menu
 
   def display_menu
     @menu_items.each do |dish|
-      puts "#{dish[:name]}: £#{dish[:price]}"
+      puts "#{dish[:name]}: #{num_to_currency(dish[:price])}"
     end
   end
 
@@ -33,5 +33,11 @@ class Menu
 
   def search_menu(dish_name)
     @menu_items.select { |item| item[:name] == dish_name }[0]
+  end
+
+  private
+
+  def num_to_currency(price)
+    sprintf("£%2.2f", price)
   end
 end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,29 @@
+require 'menu'
+
+RSpec.describe Menu do
+
+  subject { described_class.new(menu_items: [{ name: "Spaghetti and Meatballs", price: 5.00 }]) }
+  context "on initializing" do
+    it "has a menu" do
+      expect(subject.menu_items).to eq [{ name: "Spaghetti and Meatballs", price: 5.00 }]
+    end
+
+    it "can add items to the menu" do
+      subject.add_item("Pizza", 6.00)
+      expect(subject.menu_items).to eq [{ name: "Spaghetti and Meatballs", price: 5.00 },
+                                        { name: "Pizza", price: 6.00 }]
+    end
+  end
+
+  it "can display the menu" do
+    expect { subject.display_menu }.to output("Spaghetti and Meatballs: £5.00\n").to_stdout
+  end
+
+  it "can check if items are on the menu" do
+    expect(subject.on_the_menu?("Fish and Chips")).to eq false
+  end
+
+  it "can return a hash containing name and price given the name of the dish" do
+    expect(subject.search_menu("Spaghetti and Meatballs")).to eq({ name: "Spaghetti and Meatballs", price: 5.00 })
+  end
+end
