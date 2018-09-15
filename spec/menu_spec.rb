965679adebe2216319f3d4e84bdba154cc9091ea
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-15 12:54:50 +0000

    testing #choose

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -9,6 +9,8 @@ describe Menu do
   end
 
   context "#choose" do
+    let(:order_double) { double :Order_Double, add: "Dish added to order" }
+    let(:menu) { described_class.new(order_double) }
     it "adds dish to order" do
       expect(menu.choose("Butterbeer")).to eq("Dish added to order")
     end
