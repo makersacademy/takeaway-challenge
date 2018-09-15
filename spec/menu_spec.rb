fe7d351a71cd44cc5b3dfb2c58ff348e741f92f5
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-15 12:45:52 +0000

    testing #choose

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,16 @@
 require "menu"
 
 describe Menu do
-  context "#see"do
+  context "#see" do
+    let(:menu) { described_class.new }
     it "prints out menu" do
-      expect(subject.see).to_return("1. Butterbeer 1 Sickle")
+      expect { menu.see }.to output.to_stdout
+    end
+  end
+
+  context "#choose" do
+    it "adds dish to order" do
+      expect(menu.choose("Butterbeer")).to eq("Dish added to order")
     end
   end
 end
