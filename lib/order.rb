d35a11712be6c69daf8eda16d48ff2e9d1f2b60a
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-15 13:37:12 +0100

    change quantity when chooses the same dish

--- lib/order.rb
+++ lib/order.rb
@@ -2,10 +2,28 @@ class Order
   attr_reader :summary
 
   def initialize
-    @summary = []
+    @summary = Hash.new(0)
   end
 
   def add(dish, quantity)
-    @summary << { name: dish.name, price: dish.price, quantity: quantity }
+    order = { name: dish.name, price: dish.price }
+    @summary[order] += quantity
+
+    #
+    # if @summary.include?(dish.name)
+    #   @summary[order] += quantity
+    # end
+    # @summary[order] = 1
+
   end
+  # def add(dish, quantity)
+  #   @summary.map! do |d|
+  #     if d[:name] == dish.name
+  #       d[:quantity] += quantity
+  #
+  #     end
+  #   end
+  #   @summary << { name: dish.name, price: dish.price, quantity: quantity }
+  # end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,13 +2,28 @@ require 'order'
 
 describe Order do
   let(:order) { described_class.new }
-  let(:dish) { double :dish, name: 'pizza', price: 5 }
+  let(:dish1) { double :dish, name: 'pizza', price: 5 }
+  let(:dish2) { double :dish, name: 'pasta', price: 4 }
+  let(:dish3) { double :dish, name: 'chicken', price: 7 }
+
   let(:menu) { double :menu }
 
   context '#take_order' do
     it 'takes order from customer' do
-      order.add(dish, 1)
-      expect(order.summary).to eq([{name: 'pizza', price: 5, quantity: 1}])
+      order.add(dish1, 1)
+      expect(order.summary).to eq({ { name: 'pizza', price: 5 } => 1 })
     end
+
+    it 'selects several dishes' do
+      order.add(dish1, 1)
+      order.add(dish2, 1)
+      expect(order.summary).to eq({ { name: 'pizza', price: 5 } => 1, { name: 'pasta', price: 4 } => 1 })
+    end
+  end
+
+  it 'selects the same dish' do
+    order.add(dish1, 1)
+    order.add(dish1, 1)
+    expect(order.summary).to eq({ { name: 'pizza', price: 5 } => 2 })
   end
 end
