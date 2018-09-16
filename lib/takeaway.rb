ee6439e21b6198c1946091a544b91c1d04def00c
Vu Le <core.safecrlf>
2018-09-16 22:50:55 +0100

    implemented code for sending of sms upon completion of order - via use of twilio api

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -30,4 +30,22 @@ class Takeaway
     @total = "£%.2f" % ((@order.values.inject (:+)).to_s)
   end
   
+  def place_order
+    account_sid = 'AC878acd04f5f48bec769574ff5a0b1e97'
+    auth_token = '7d2b38d4b879d50c61b3d002614660ab'
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = '+447449672445' # Your Twilio number
+    to = '+447905659510' # Your mobile phone number
+
+    now = Time.new
+    one_hour = now + 3600
+
+    client.messages.create(
+    from: from,
+    to: to,
+    body: "Thank you! Your ordered was placed. You have ordered #{@order}. The total for your order is: #{@total}. Your order will will be delivered before #{one_hour.strftime("%d of %B, %Y —  %H:%M")}"
+    )
+  end
+
 end 
