1223f4c0ef062d1494ffc0fee0fdbb9d5fcb4371
Andres Faraone <andresfaraone@outlook.com>
2018-09-16 12:54:11 +0100

    menu tests using mock dish objects

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,18 +1,30 @@
 require 'menu'
 
 describe Menu do
-  let(:mockprinter) { double(:mockprinter)}
-  subject { described_class.new(mockprinter) }
+  let(:mockprinter) { double(:mockprinter) }
+  # maybe remove mockdish name and price??
+  let(:mockdish)    { double(:mockdish, new: "new", name: "name", price: "price") }
+  subject           { described_class.new(mockprinter, mockdish) }
+
   describe 'instantiation' do
-    it '@list is set to LIST' do
-      expect(subject.list).to eq(Menu::LIST)
+    it 'sets @printer to mock Printer' do
+      expect(subject.printer).to eq(mockprinter)
+    end
+
+    it 'sets @dish to mock Dish class' do
+      expect(subject.dish).to eq(mockdish)
+    end
+
+    it 'creates Dish objects and stores in @list1' do
+      allow(mockdish).to receive(:new).and_return(mockdish)
+      expect(subject.list).to eq([mockdish, mockdish, mockdish])
     end
   end
 
-  describe '#display' do
-    before { allow(mockprinter).to receive(:display) }
-    it 'calls printer.display' do
-      expect(subject.printer).to receive(:display)
+  describe 'display' do
+    it 'calls #display on printer' do
+      allow(mockdish).to receive(:new).and_return(mockdish)
+      3.times { expect(subject.printer).to receive(:display) }
       subject.display
     end
   end
