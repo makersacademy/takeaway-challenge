42464fd09f61113ca4edf261b3421e3ecb1e895e
Darcie <darciewalsh@hotmail.com>
2018-09-16 15:17:34 +0100

    remove sensitive info

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -1,18 +1,19 @@
 require 'twilio-ruby'
 
 class Confirmation
-  account_sid = 'AC4bcca295ad97a98cacde7dc983f7d14a' # set env variables https://www.twilio.com/blog/2017/01/how-to-set-environment-variables.html
-  auth_token = '97ff951a0229ab09da6c092b1ebf89a3' # set env variables
+
+  def send_text
+  account_sid = 'AC4bc..........................'
+  auth_token = '97ff............................'
 
   @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-  from = '+447403926214' # Your Twilio number  # set env variables
-  to = '+447944602672' # Your mobile phone number  # set env variables
-
   time = Time.now + 3600
-  @message = @client.messages.create(
-  from: from,
-  to: to,
+  message = @client.messages.create(
+  from: '+4474......', # Your Twilio number
+  to: '+447944......', # Your mobile phone number
   body: "Thank you! Your order was placed and will be delivered before #{time}"
   )
+  end
+
 end
--- spec/confirmation_spec.rb
+++ spec/confirmation_spec.rb
@@ -1,8 +1,13 @@
 require 'confirmation'
 
 describe Confirmation do
+  subject(:confirmation)	{ described_class.new }
+	let(:send_text)	        { double(:send_text) }
 
   describe 'confirmation message' do
-    it 'sends a text to confirm the order has been places'
+    it 'sends a text to confirm the order has been placed' do
+      allow(confirmation).to receive(:send_text)
+      expect { confirmation.send_text }.not_to raise_error
+    end
   end
 end
