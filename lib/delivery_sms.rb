d4a74982c10c46500ca3d4e11dd411a1145178ff
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-16 11:19:04 +0000

    initialized instance variables

--- lib/delivery_sms.rb
+++ lib/delivery_sms.rb
@@ -4,15 +4,18 @@ class DeliverySMS
 
   ONE_HOUR = 3600
 
-  def send
+  def initialize
     account_sid = ENV["TWILIO_ACCOUNT_SID"]
     auth_token = ENV["TWILIO_AUTH_TOKEN"]
-    client = Twilio::REST::Client.new(account_sid, auth_token)
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    @from = ENV["TWILIO_PHONE_NUMBER"]
+    @to = ENV["MY_PHONE_NUMBER"]
+  end
+
+  def send
 
     delivery_time = Time.new + ONE_HOUR
-
-    from = ENV["TWILIO_PHONE_NUMBER"]
-    to = ENV["MY_PHONE_NUMBER"]
 
     client.messages.create(
       from: from,
@@ -20,5 +23,9 @@ class DeliverySMS
       body: "Thank you! Your order was placed and will be delivered before #{delivery_time.strftime("%H:%M")}"
     )
   end
+
+  private
+
+  attr_reader :client, :from, :to
 
 end
