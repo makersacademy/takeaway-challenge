8d6898859fe12fdc9b78d4e2ee6ec90f0daee505
Jon Urry <jon@urry.me>
2018-09-16 10:36:29 +0100

    Set default config for sms messaging

--- lib/sms.rb
+++ lib/sms.rb
@@ -3,22 +3,26 @@ require 'twilio-ruby'
 
 class SMS
   def initialize
-    @account_sid = 'AC3d8670509df17ad7546b9ec2bcf013dd'
-    @auth_token = 'e67c55c82113398cd2ae19a7bb538173'
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
-    @from = '+441522246322' # Your Twilio number
-    @to = '+447986371299' # Your mobile phone number
+    @client = Twilio::REST::Client.new(DEFAULT_CONFIG[:account_sid], DEFAULT_CONFIG[:auth_token])
   end
 
-  def send
+  def send(message = 'Hey, there!', to = DEFAULT_CONFIG[:to], from = DEFAULT_CONFIG[:from])
     client.messages.create(
       from: from,
       to: to,
-      body: "Hey friend!"
+      body: message
     )
     end
   
   private
 
-  attr_reader :from, :to, :body, :account_sid, :auth_token, :client
+  attr_reader :client
+
+  DEFAULT_CONFIG = {
+    account_sid: 'AC3d8670509df17ad7546b9ec2bcf013dd',
+    auth_token: 'e67c55c82113398cd2ae19a7bb538173',
+    from: '+441522246322',
+    to: '+447986371299'
+  }
+
 end
