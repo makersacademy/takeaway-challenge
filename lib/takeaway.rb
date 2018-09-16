57031e349947b58dde56bf816f5860bcb19b39c3
Andres Faraone <andresfaraone@outlook.com>
2018-09-16 17:42:17 +0100

    Takeaway delegates to order and menu

---
+++ lib/takeaway.rb
@@ -0,0 +1,27 @@
+require_relative 'dish'
+require_relative 'printer'
+require_relative 'menu'
+require_relative 'order'
+require_relative 'sender'
+
+class Takeaway
+  attr_reader :menu, :order, :sender
+
+  def initialize(menu = Menu.new, order = Order.new, sender = Sender.new)
+    @menu = menu
+    @order = order
+    @sender = sender
+  end
+
+  def view_menu
+    print menu.display
+  end
+
+  def add(name, qty = 1)
+    order.add(name, qty)
+  end
+
+  def view_order
+    print order.display
+  end
+end
