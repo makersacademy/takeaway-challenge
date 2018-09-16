ef166177d1cf5551fd9849e00153edcd3ef7bb00
Andres Faraone <andresfaraone@outlook.com>
2018-09-16 12:55:19 +0100

    linted

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -5,7 +5,12 @@ describe Printer do
     context 'when called by Menu' do
       it 'displays name and price of each dish' do
         response = "\u2022 Fish and Chips      £12.95\n"
-        expect { subject.display("Fish and Chips",1295) }.to output.to_stdout
+        expect { subject.display("Fish and Chips", 1295) }.to output(response).to_stdout
+      end
+    end
+
+    context 'when called by Order' do
+      xit 'display name, quantity, individual price and subtotal' do
       end
     end
   end
