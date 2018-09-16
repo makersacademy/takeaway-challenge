ee3f1ffc610e67c9f063341e1e8dfdcf0dcf00fd
Cameron Whitehead <camjw119@gmail.com>
2018-09-16 15:29:13 +0100

    Finished sinatra sms receiver app, need to test though.

--- lib/sms_receiver_app.rb
+++ lib/sms_receiver_app.rb
@@ -3,15 +3,14 @@ require './lib/sms_receiver'
 require 'shotgun'
 require 'twilio-ruby'
 
-
 sms_receiver = SMSReceiver.new
 
 get '/sms-takeaway-order' do
-  body = params['Body'].downcase
+  body = params['Body'].downcase.strip
   order = sms_receiver.parse_order(body)
 
-  twiml = sms_receiver.text_api.new do |r|
-    r.message(body: sms_receiver.order_confirmation(order))
+  twiml = Twilio::TwiML::MessagingResponse.new do |r|
+    r.message(body: sms_receiver.respond(order))
   end
 
   twiml.to_s
