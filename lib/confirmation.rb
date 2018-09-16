48e998c66cbf1b97e80c35ee89dab06a9d5878a7
Darcie <darciewalsh@hotmail.com>
2018-09-16 16:25:38 +0100

    formatted time

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -3,16 +3,19 @@ require 'twilio-ruby'
 class Confirmation
 
   def send_text
-  account_sid = 'AC4bc..........................'
-  auth_token = '97ff............................'
+  account_sid = 'AC4bcca295ad97a98cacde7dc983f7d14a'
+  auth_token = '97ff951a0229ab09da6c092b1ebf89a3'
 
   @client = Twilio::REST::Client.new(account_sid, auth_token)
 
+  from = '+447481363412'
+  to =   '+447944602672'
+
   time = Time.now + 3600
   message = @client.messages.create(
-  from: '+4474......', # Your Twilio number
-  to: '+447944......', # Your mobile phone number
-  body: "Thank you! Your order was placed and will be delivered before #{time}"
+  from: from, # Your Twilio number
+  to:   to, # Your mobile phone number
+  body: "Thank you! Your order was placed and will be delivered before #{time.strftime("%H:%M")}"
   )
   end
 
