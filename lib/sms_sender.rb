aea11c3a61d78fd7b1f9af328fd9f3c6675c8fa4
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-09-17 13:51:45 +0100

    Updated SMS_sender file

--- lib/sms_sender.rb
+++ lib/sms_sender.rb
@@ -1,22 +1,25 @@
 require 'twilio-ruby'
 require 'time'
+require 'dotenv'
+
+Dotenv.load 'config.env'
 
 class SmsSender
   attr_reader :account_sid, :auth_token, :client, :from, :to
   def initialize
-    @account_sid = 'AC84579b5711fe20c9d367832993b0d830'
-    @auth_token = '5afa537d06fc7069ab9f32e172ca1017'
+    @account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @auth_token = ENV['TWILIO_AUTH_TOKEN']
     @client = Twilio::REST::Client.new(account_sid, auth_token)
-    @from = '+34955160758'
-    @to = '+34722420360'
+    @from = ENV['FROM_PHONE_NUMBER']
+    @to = ENV['TO_PHONE_NUMBER']
   end
 
   def text_confirmation
     client.messages.create(
     from: from,
     to: to,
-    body: "Thank you! Your order was placed and will be delivered before #{delivery_time}."
-    )
+    body: "Thank you! Your order was placed and will be delivered before" +
+          " #{delivery_time}.")
   end
 
   private
