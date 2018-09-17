41438f7925daf5cff65488baa2e5e402b8eff350
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-09-17 08:54:02 +0100

    final changes in the allowed time - added README etc..

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,15 +1,17 @@
+require_relative 'Secrets'
+
 class SendSMS
 
   def sms
 
     require 'twilio-ruby'
 
-    account_sid = 'AC505597d6b16af4525a8a00a41e234bf0'
-    auth_token = '884f38ce426e624e11b6c1ca464fbc5e'
+    account_sid = Secrets::ACCOUNT_SID
+    auth_token = Secrets::AUTH_TOKEN
     client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    from = '+441970800063'
-    to = '+447771862828'
+    from = Secrets::FROM
+    to = Secrets::TO
 
     client.messages.create(
       from: from,
