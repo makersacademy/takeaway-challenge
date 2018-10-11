dcd2c43f33dab095ff1689d48679e23ddecf4517
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-11 21:05:16 +0100

    11-10-2018 @9pm: Some refactoring, which took far too much time...

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,10 +1,12 @@
 require './takeaway'
 
 describe 'takeaway' do
-  
+
+  # let (:diary_not_locked) { double :secure, :secure? => false }
+  let (:dishes) { Dishes.new }
+
   it 'prints a list of dishes with prices' do
-    dishes = Dishes.new
-    expect(dishes.print(dishes)).not_to eq nil
+    expect(dishes.display).not_to eq nil
   end
 
 end
--- takeaway.rb
+++ takeaway.rb
@@ -1,13 +1,23 @@
 class Dishes
   
   def initialize
-    @dishes = { "Pizza" => 10, "Cheese_burger" => 5, "Chicken_Strips" => 4, "Garlic_Bread" => 3, "Fries" => 2 }
+    @menu = {
+      "Pizza"           => 10,
+      "Cheese_burger"   => 5,
+      "Chicken_Strips"  => 4,
+      "Garlic_Bread"    => 3,
+      "Fries"           => 2
+    }
   end
 
-  def print(dishes)
+  def display
     index = 1
-    @dishes.each do |key, value|
-      puts "#{index}  #{key}           #{value}"
+    puts "'Go-Away' - Take-Away Menu".center(50)
+    @menu.each do |key, value|
+      puts "Number: #{index}".center(50)
+      puts "#{key}".center(50)
+      puts "Price: £#{value}".center(50)
+      puts "-----".center(50)
       index +=1
     end
   end
