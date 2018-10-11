7786e8a6e52e121e09ee9ab5b543b5ea492de549
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-11 22:16:55 +0100

    Calculates order total

--- lib/order.rb
+++ lib/order.rb
@@ -9,4 +9,14 @@ class Order
   def add_dish(dish, price)
     @basket << { dish => price }
   end
+
+  def total
+    order_total = 0
+
+    @basket.each do |item|
+      order_total += item.values[0]
+    end
+
+    order_total
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,7 +3,13 @@ require 'order'
 describe Order do
   describe '#add_dish' do
 
-    it 'adds a dish to a basket incl price' do
+    it 'starts with an empty basket' do
+      order = Order.new
+
+      expect(order.basket).to eq []
+    end
+
+    it 'adds a dish with price to the basket' do
       order = Order.new
 
       order.add_dish("chicken soup", 5)
@@ -28,6 +34,30 @@ describe Order do
       order.add_dish("chicken soup", 5)
 
       expect(order.basket).to eq [{ "chicken soup" => 5 }, { "pizza" => 2.30 }, { "chicken soup" => 5 }]
+    end
+
+    it 'shows total price of zero dishes order' do
+      order = Order.new
+
+      expect(order.total).to eq 0
+    end
+
+    it 'shows total price of one dish order' do
+      order = Order.new
+
+      order.add_dish("chicken soup", 5)
+
+      expect(order.total).to eq 5
+    end
+
+    it 'shows total price of dishes' do
+      order = Order.new
+
+      order.add_dish("chicken soup", 5)
+      order.add_dish("pizza", 2.30)
+      order.add_dish("chicken soup", 5)
+
+      expect(order.total).to eq 12.30
     end
 
   end
