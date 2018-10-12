0dae4103a3da7da9f5d9b4bce5a69eca4af9712f
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-12 10:33:56 +0100

    isolated takeaway spec

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,16 +2,16 @@ require "takeaway"
 
 describe Takeaway do
 
-  it "should show dishes and prices" do
-    expect(subject.show_menu).to eq([{dish: "burger", price: 4},
-      {dish: "cheeseburger", price: 5},
-      {dish: "chicken burger", price: 4.5},
-      {dish: "chips", price: 2},
-      {dish: "cheesy chips", price: 2.5},
-      {dish: "onion rings", price: 2},
-      {dish: "mozarella sticks", price: 2},
-      {dish: "garlic bread", price: 3},
-      {dish: "cheesy garlic bread", price: 3.5}])
+  subject { Takeaway.new(menu) }
+  let(:menu) { double :menu, items: [{dish: "burger", price: 4}] }
+
+  describe '#show_menu' do
+
+    it "should show dishes and prices" do
+      expect(menu).to receive(:items)
+      subject.show_menu
     end
+
+  end
 
 end
