b69937716520292da3a8016ccf132a5b94acf6ca
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-12 10:04:21 +0100

    Added test and code for initializing menu class with dishes and prices

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,16 @@ require 'menu'
 
 describe Menu do
 
+  it "should initialize with an array of dishes and prices" do
+    expect(subject.items).to eq([{dish: "burger", price: 4},
+      {dish: "cheeseburger", price: 5},
+      {dish: "chicken burger", price: 4.5},
+      {dish: "chips", price: 2},
+      {dish: "cheesy chips", price: 2.5},
+      {dish: "onion rings", price: 2},
+      {dish: "mozarella sticks", price: 2},
+      {dish: "garlic bread", price: 3},
+      {dish: "cheesy garlic bread", price: 3.5}])
+    end
+
 end
