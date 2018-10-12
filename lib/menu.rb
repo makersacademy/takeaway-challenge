25f27e8c77c810f63b476e45954d719330385bda
Aimée Craig <aimeecraig1@gmail.com>
2018-10-12 10:39:59 +0100

    unit test written and code started to pass

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,18 @@
 class Menu
+  def initialize
+    @items = {
+      :burgers => ["Hamburger", "Cheeseburger", "Chicken burger"],
+      :hot_dogs => ["Cheese dog", "Chili dog", "Corn dog"],
+      :chicken => ["Southern fried chicken", "Chicken wings"]
+    }
+  end
 
+  def display
+    @items.each do |cat, item|
+      puts cat.to_s.gsub(/_/, " ").split.map(&:capitalize).join(" ")
+      puts "---"
+      puts item
+      print "\n"
+    end
+  end
 end
