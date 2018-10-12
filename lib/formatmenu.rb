829298643af3cfd49b37078999015ae7372264c0
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-12 11:51:31 +0100

    Added test and method for formatting menu

--- lib/formatmenu.rb
+++ lib/formatmenu.rb
@@ -7,6 +7,16 @@ class FormatMenu
     @footer = "Hope you enjoy!"
   end
 
+  def format_menu(menu)
+    puts @header
+    puts "--------------------"
+    menu.each do |item|
+      puts "#{item[:dish].capitalize} - #{format_price(item[:price])}"
+    end
+    puts "--------------------"
+    puts @footer
+  end
+
   def format_price(price)
     return "£#{sprintf( '%.2f', price)}"
   end
--- spec/formatmenu_spec.rb
+++ spec/formatmenu_spec.rb
@@ -2,6 +2,9 @@ require "formatmenu"
 
 describe FormatMenu do
 
+  let(:menu) { double :menu, items: [{dish: "burger", price: 4},
+    {dish: "cheeseburger", price: 5}] }
+
   describe '#initialize' do
 
     it "should create a header" do
@@ -22,6 +25,14 @@ describe FormatMenu do
 
     it "should convert 5.5 to £5.50" do
       expect(subject.format_price(5.5)).to eq("£5.50")
+    end
+
+  end
+
+  describe '#format_menu' do
+
+    it "should show a menu" do
+      expect{subject.format_menu(menu.items)}.to output("Mel's Place\n--------------------\nBurger - £4.00\nCheeseburger - £5.00\n--------------------\nHope you enjoy!\n").to_stdout
     end
 
   end
