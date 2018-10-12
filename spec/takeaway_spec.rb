1a077f29084cfdc2d46d19887539cc8ef1bd261d
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-12 16:49:29 +0100

    12-10-2108 @5pm: Long overdue commit - Tests reconfigured though requiring more coverage. Feature test working for stories 1-3.

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,12 +1,21 @@
 require './takeaway'
 
-describe 'takeaway' do
+describe Takeaway do
 
-  # let (:diary_not_locked) { double :secure, :secure? => false }
-  let (:dishes) { Dishes.new }
-
+  let (:takeaway) { Takeaway.new }
   it 'prints a list of dishes with prices' do
-    expect(dishes.display).not_to eq nil
+    expect { subject.create_menu }.to output("'Go-Away' - Take-Away Menu\n1: Pizza, £10\n      ---\n      \n2: Fries, £2\n      ---\n      \n3: Garlic Bread, £3\n      ---\n      \n").to_stdout
   end
+
+  #Highest value dish is pizza at £10. **:order 1, 2** orders pizza and fries (£2) for a total of £12.
+  #This test therefore, tests both story 2 and story 3.
+  let (:chek) { double :check_object, :order => [1, 2]}
+  it 'allows me to order several dishes' do
+    @check = Check.new
+    @order = chek.order
+    @check.collate_check(@order)
+    expect(takeaway.display_check(@check)).to eq 12
+  end
+
 
 end
--- takeaway.rb
+++ takeaway.rb
@@ -1,24 +1,90 @@
-class Dishes
-  
-  def initialize
-    @menu = {
-      "Pizza"           => 10,
-      "Cheese_burger"   => 5,
-      "Chicken_Strips"  => 4,
-      "Garlic_Bread"    => 3,
-      "Fries"           => 2
-    }
+class Takeaway
+  attr_reader :number, :name, :price, :menu, :check, :order
+
+  def initialize(meal = Meal, check = Check)
   end
 
-  def display
-    index = 1
-    puts "'Go-Away' - Take-Away Menu".center(50)
-    @menu.each do |key, value|
-      puts "Number: #{index}".center(50)
-      puts "#{key}".center(50)
-      puts "Price: £#{value}".center(50)
-      puts "-----".center(50)
-      index +=1
+  def create_menu
+    @menu = []
+    create_meals
+    display_menu
+  end
+
+  def create_order
+    @check = Check.new
+    @check.place_order
+    @order = @check.order
+    @check.collate_check(@order)
+    display_check(@check)
+  end
+
+  def create_meals
+    @menu.push(Meal.new(1, "Pizza", 10))
+    @menu.push(Meal.new(2, "Fries", 2))
+    @menu.push(Meal.new(3, "Garlic Bread", 3))
+  end
+
+  def display_menu
+    puts "'Go-Away' - Take-Away Menu"
+    @menu.each do |item|
+      puts "#{item.number}: #{item.name}, £#{item.price}
+      ---
+      "
+    end
+  end
+
+  def display_check(check = Check)
+    puts "Your order was as follows:".center(60)
+    total = 0
+    @bill = check.check
+    @bill.each do |item|
+      puts "#{item.number}: #{item.name}, £#{item.price}".ljust(60)
+      total += item.price
+    end
+    puts "Your order will cost a total of £#{total}"
+    total
+  end
+
+end
+
+class Meal
+  attr_reader :number, :name, :price, :menu
+
+  def initialize(number, name, price)
+    @number = number
+    @name = name
+    @price = price
+  end
+end
+
+class Check
+  attr_reader :check, :order, :number, :name, :price
+
+  def initialize
+    @check = []
+    @order = []
+  end
+
+  def place_order
+    choice = 0
+    while choice != 6 do
+      puts "Please enter the number of the dish you would like to order.
+      When you have finished your selection, press 6."
+      choice = gets.chomp.to_i
+      fail 'That is not a relevant item number.' if choice.between?(0,6) == false
+      @order.push(choice) if choice.between?(1,5) == true
+    end
+  end
+
+  def confirm_message
+
+  end
+
+  def collate_check(order)
+    order.each do |choice|
+    @check.push(Meal.new(1, "Pizza", 10)) if choice == 1
+    @check.push(Meal.new(2, "Fries", 2)) if choice == 2
+    @check.push(Meal.new(3, "Garlic Bread", 3)) if choice == 3
     end
   end
 
