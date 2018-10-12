4e5a26189f275c2e03def62fbc5a4f885a88892f
Aimée Craig <aimeecraig1@gmail.com>
2018-10-12 11:57:39 +0100

    unit test created

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,6 +2,8 @@ require 'menu'
 
 describe Menu do
   context '#display' do
-    
+    it 'displays the menu in a nice format' do
+      expect { subject.display }.to output("Margarita Pizza, £8\nHamburger, £6\nChips, £2\n").to_stdout
+    end
   end
 end
