1e7aae11585582ac04fd48138c012ef01c151908
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-12 14:02:09 +0100

    Added test and method for item_price

--- lib/price.rb
+++ lib/price.rb
@@ -1,3 +1,11 @@
 class Price
 
+  def initialize(menu = Menu.new)
+    @menu = menu
+  end
+
+  def item_price(item)
+    return @menu.items.detect { |hash| hash[:dish] == item }[:price]
+  end
+
 end
--- spec/price_spec.rb
+++ spec/price_spec.rb
@@ -2,4 +2,16 @@ require "price"
 
 describe Price do
 
+  subject { Price.new(menu)}
+  let(:order) { double :order, order: {burger: 2, chips: 1} }
+  let(:menu) { double :menu, items: [{dish: "burger", price: 4}, {dish: "chips", price: 2}] }
+
+  describe '#item_price' do
+
+    it "should return the price of a burger" do
+      expect(subject.item_price("burger")).to eq(4)
+    end
+
+  end
+
 end
