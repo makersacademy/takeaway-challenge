62b461a62d941b323375a677682a29704791813e
Todd Langford-Archer <toddpla@gmail.com>
2018-10-12 11:36:23 +0100

    initial commit with Dish class and spec

---
+++ spec/dish_spec.rb
@@ -0,0 +1,9 @@
+require 'dish'
+
+describe Dish do
+  subject { described_class.new(name: 'Pizza', price:7.85)}
+  it 'should create Pizza with price equalling 7.85' do
+    expect(subject.name).to eq 'Pizza'
+    expect(subject.price).to eq 7.85
+  end
+end
