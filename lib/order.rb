be2b28c1c5cf6ac36550a5943dbf86bb06c6e4e2
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-12 12:13:25 +0100

    Added tests and method for adding to order

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,7 @@
 class Order
 
+  def add_to_order(dish, quantity)
+    return "#{dish.capitalize} x#{quantity}"
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,14 @@ require "order"
 
 describe Order do
 
+  describe '#add_to_order' do
+
+    it { is_expected.to respond_to(:add_to_order).with(2).arguments }
+
+    it "adds item to order" do
+      expect(subject.add_to_order("burger", 2)).to eq("Burger x2")
+    end
+
+  end
+
 end
