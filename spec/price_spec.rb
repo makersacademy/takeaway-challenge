a0ec2a874912c86e326035787847e561a1f2a7ab
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-12 14:10:49 +0100

    Added second test for checking order total

--- spec/price_spec.rb
+++ spec/price_spec.rb
@@ -4,7 +4,8 @@ describe Price do
 
   subject { Price.new(menu)}
   let(:order) { double :order, order: {burger: 2, chips: 1} }
-  let(:menu) { double :menu, items: [{dish: "burger", price: 4}, {dish: "chips", price: 2}] }
+  let(:order2) { double :order, order: {burger: 4, :"onion rings" => 3}}
+  let(:menu) { double :menu, items: [{dish: "burger", price: 4}, {dish: "chips", price: 2}, {dish: "onion rings", price: 2}] }
 
   describe '#item_price' do
 
@@ -18,6 +19,10 @@ describe Price do
 
     it "should return total of order" do
       expect(subject.total(order.order)).to eq(10)
+    end
+
+    it "should return total of order2" do
+      expect(subject.total(order2.order)).to eq(22)
     end
 
   end
