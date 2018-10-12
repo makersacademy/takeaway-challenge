0a73c0ce29f2ff8d7b354c1a7a9d985b201e6968
Aimée Craig <aimeecraig1@gmail.com>
2018-10-12 12:12:51 +0100

    unit test written

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,3 +1,9 @@
 describe Order do
-
+  let(:menu) { double :menu, :items => {"Margarita Pizza" => 8, "Hamburger" => 6, "Chips" => 2} }
+  context '#add' do
+    it 'adds an item to the order' do
+      subject.add("Margarita Pizza")
+      expect(subject.total).to eq 8
+    end
+  end
 end
