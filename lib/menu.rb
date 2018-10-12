0dafe9cf2df39f658ee09511b166f8dd50778140
jebax <joel.eliot.baxter@gmail.com>
2018-10-12 12:04:31 +0100

    added menu list and formatted list method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,26 @@
 class Menu
   def initialize
-    @items = []
+    @dishes = [{ name: "Margherita", price: 7.5 },
+              { name: "Bufala", price: 8 },
+              { name: "Anchovy", price: 8 },
+              { name: "Pepperoni", price: 8.5 },
+              { name: "Calabria", price: 8.5 },
+              { name: "Quattro Formaggi", price: 10 },
+              { name: "Truffle", price: 12.5 },
+              { name: "Gold Pizza", price: 1200 }
+    ]
+    @title = "* * * PAPA'S * PIZZERIA * * *\n"
   end
 
-  def items
-    @items.dup
+  def list_dishes
+    @title + formatted_list.join
+  end
+
+  private
+
+  def formatted_list
+    @dishes.map.with_index do |dish, index|
+      "#{index + 1}) #{dish[:name]}: £#{sprintf('%.2f', dish[:price])}\n"
+    end
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,22 @@
 require 'menu'
 
 describe Menu do
+  let(:list) { <<~HEREDOC
+              * * * PAPA'S * PIZZERIA * * *
+              1) Margherita: £7.50
+              2) Bufala: £8.00
+              3) Anchovy: £8.00
+              4) Pepperoni: £8.50
+              5) Calabria: £8.50
+              6) Quattro Formaggi: £10.00
+              7) Truffle: £12.50
+              8) Gold Pizza: £1200.00
+              HEREDOC
+  }
 
-  it { expect(subject.items).to be_empty }
-
+  describe "#list_dishes" do
+    it 'should list a title + the available dishes with their name and price' do
+      expect(subject.list_dishes).to eq list
+    end
+  end
 end
