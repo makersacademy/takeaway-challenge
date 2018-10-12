638fa5d893cb44f41ad84245426199dd02fe73cd
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-12 16:14:14 +0100

    Added menu class and tests

---
+++ lib/menu.rb
@@ -0,0 +1,14 @@
+# This class holds all of a resturants dish options
+class Menu
+
+  def initialize
+    @list = []
+  end
+
+  def add_dish(dish) # Adds dish to the list array
+    @list << dish
+  end
+
+
+
+end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,8 @@
+require 'menu'
+
 describe Menu do
   it 'should hold an array of dishes' do
-    
+    dish = { name: "chicken", price: 20}
+    expect(subject.add_dish(dish)).to eq [dish]
   end
 end
