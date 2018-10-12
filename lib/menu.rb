44e64fbbc139b4fa5e87867892ebbcef7914b937
Aimée Craig <aimeecraig1@gmail.com>
2018-10-12 12:03:02 +0100

    code amended to pass unit test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,15 @@
 class Menu
-  
+  def initialize
+    @items = {
+      "Margarita Pizza" => 8,
+      "Hamburger" => 6,
+      "Chips" => 2
+    }
+  end
+
+  def display
+    @items.each do |item, price|
+      puts "#{item}, £#{price}"
+    end
+  end
 end
