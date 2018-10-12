a7d901b203c660c87e840641c2bb87e9c16d3f4f
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-12 10:25:38 +0100

    Added tests and method for formatting number to be shown as a price

--- lib/formatmenu.rb
+++ lib/formatmenu.rb
@@ -1,3 +1,7 @@
 class FormatMenu
 
+  def format_price(price)
+    return "£#{sprintf( '%.2f', price)}"
+  end
+
 end
--- spec/formatmenu_spec.rb
+++ spec/formatmenu_spec.rb
@@ -2,4 +2,16 @@ require "formatmenu"
 
 describe FormatMenu do
 
+  describe '#format_price' do
+
+    it "should convert 4 to £4.00" do
+      expect(subject.format_price(4)).to eq("£4.00")
+    end
+
+    it "should convert 5.5 to £5.50" do
+      expect(subject.format_price(5.5)).to eq("£5.50")
+    end
+
+  end
+
 end
