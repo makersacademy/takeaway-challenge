e142905875240e070115d1f6b12286619a6ffc8a
jebax <joel.eliot.baxter@gmail.com>
2018-10-12 12:33:07 +0100

    added first test for takeaway class - customer can list dishes from a menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,8 +1,16 @@
 class Takeaway
-
   def initialize(menu_class)
     @menu_class = menu_class
     @menu = nil
   end
 
+  def list_dishes
+    menu.list_dishes
+  end
+
+  private
+
+  def menu
+    @menu ||= @menu_class.new
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,24 @@
 require 'takeaway'
 
 describe Takeaway do
+  let(:menu) { double(:menu, list_dishes: <<~HEREDOC
+              * * * PAPA'S * PIZZERIA * * *
+              1) Margherita: £7.50
+              2) Bufala: £8.00
+              3) Anchovy: £8.00
+              4) Pepperoni: £8.50
+              5) Calabria: £8.50
+              6) Quattro Formaggi: £10.00
+              7) Truffle: £12.50
+              8) Gold Pizza: £1200.00
+              HEREDOC
+            )}
+  let(:menu_class) { double(:menu_class, new: menu) }
+  let(:subject) { Takeaway.new(menu_class) }
 
+  describe "#list_dishes" do
+    it 'should list dishes from the initialized menu class' do
+      expect(subject.list_dishes).to eq menu.list_dishes
+    end
+  end
 end
