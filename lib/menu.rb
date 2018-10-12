34a4eb45d2ffcb338d74ef8cd62578326b0eb7a2
Aimée Craig <aimeecraig1@gmail.com>
2018-10-12 10:54:16 +0100

    nuked unit test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,17 +1,19 @@
 class Menu
   def initialize
     @items = {
-      :burgers => ["Hamburger", "Cheeseburger", "Chicken burger"],
-      :hot_dogs => ["Cheese dog", "Chili dog", "Corn dog"],
-      :chicken => ["Southern fried chicken", "Chicken wings"]
+      :burgers => {"Hamburger" => 5, "Cheeseburger" => 6, "Chicken burger" => 4},
+      :hot_dogs => {"Cheese dog" => 5, "Chili dog" => 6, "Corn dog" => 4},
+      :chicken => {"Southern fried chicken" => 4, "Chicken wings" => 6}
     }
   end
 
   def display
-    @items.each do |cat, item|
+    @items.each do |cat, items|
       puts cat.to_s.gsub(/_/, " ").split.map(&:capitalize).join(" ")
       puts "---"
-      puts item
+      items.each do |food, price|
+        puts food + ", £" + price.to_s
+      end
       print "\n"
     end
   end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,25 +2,6 @@ require 'menu'
 
 describe Menu do
   context '#display' do
-    it 'displays a list of menu items' do
-      expect { subject.display }.to output(<<-MESSAGE).to_stdout
-Burgers
----
-Hamburger, £5
-Cheeseburger, £6
-Chicken burger, £4
-
-Hot Dogs
----
-Cheese dog, £5
-Chili dog, £6
-Corn dog, £4
-
-Chicken
----
-Southern fried chicken, £4
-Chicken wings, £6
-MESSAGE
-    end
+    
   end
 end
