fc9b67f9f0ce086a8896855ef799f1bb57a78fb3
Tolvic <terence.h.mace@gmail.com>
2018-10-12 12:54:54 +0100

    User story 2 feature tests written

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -8,7 +8,7 @@ describe 'User Stories' do
   # So that I can check if I want to order something
   # I would like to see a list of dishes with prices
 
-  it 'so the customer can order something it shows the ist of dishes with prices' do
+  it 'so that the customer can order something it shows the ist of dishes with prices' do
     expect(takeaway.show_menu).to eq({ 'pizza' => 6.99, 'chips' => 1.50, 'burger' => 3.00 })
   end
 
@@ -16,7 +16,22 @@ describe 'User Stories' do
   # As a customer
   # So that I can order the meal I want
   # I would like to be able to select some number of several available dishes
-  #
+
+  it 'allows customer to add an item to their order' do
+    takeaway.add_to_order(pizza)
+    expect(order.summary).to eq('1x pizza £6.99')
+  end
+
+  it 'allows a customer to add a quantitity of an item to their order' do
+    takeaway.add_to_order(pizza, 2)
+    expect(order.summary).to eq('2x pizza £13.98')
+  end
+
+  it 'allows customer to add multiple items in multiple quantities to their order' do
+    takeaway.add_to_order(pizza, 2)
+    expect(order.summary).to eq('2x pizza £13.98')
+  end
+
   # Unit Test 3
   # As a customer
   # So that I can verify that my order is correct
