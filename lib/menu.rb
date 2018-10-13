73816a29ad62fe66d3704bdf43a8526b48475768
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-14 00:11:56 +0100

    added comments and refractored

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,24 +2,36 @@
 class Menu
 
   def initialize
-    @list = []
-    @loaded_dish = nil
+    @list = []          # holds dishes
+    @loaded_dish = nil  # needed so that find dish returns a dish
   end
 
   def add_dish(dish)
-    @list << dish # Adds dish to the list array
+    @list << dish # adds dish to the list array
   end
 
   def list_dishes
+    print_dishes  # prints the menu
+  end
+
+  def find_dish(dish)
+    search_menu(dish) # searches for the dish in the menu
+    @loaded_dish      # returns the found dish
+  end
+
+  private
+
+  # Itterates the list array and prints the dishes and prices
+  def print_dishes
     @list.each do |dish|
       print "| #{dish.name} $#{dish.price} |"
     end
   end
 
-  def find_dish(dish)
+  # Itterates the list array to find the dish that matches
+  def search_menu(dish)
     @list.each do |name|
       @loaded_dish = name if name.name == dish
     end
-    @loaded_dish
   end
 end
