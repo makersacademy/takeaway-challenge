3902734acd6df297c5ed2709842675acbae1ccc0
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-13 11:08:57 +0100

    added #add_to_order which stores a dish & quantity in an order hash

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,7 +6,19 @@ class Takeaway
     "Chicken Wing" => 1
   }
 
+  def initialize
+    @order = {}
+  end
+
   def menu
     MENU.map { |dish, price| "#{dish}: £#{price}" }.join("\n")
+  end
+
+  def add_to_order(dish, quantity)
+    @order[dish] = quantity
+  end
+
+  def current_order
+    "Your current order:\n" + @order.map { |dish, quantity| "#{quantity} x #{dish}"}.join("\n")
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,16 @@
 require 'takeaway'
-
+require 'pry'
 describe Takeaway do
-  it "should display a list of dishes with prices" do
-    expect(subject.menu).to eq "Quarter Chicken: £4\nHalf Chicken: £8\nWhole Chicken: £13\nChicken Wing: £1"
+  describe "#menu" do
+    it "should display a list of dishes with prices" do
+      expect(subject.menu).to eq "Quarter Chicken: £4\nHalf Chicken: £8\nWhole Chicken: £13\nChicken Wing: £1"
+    end
+  end
+
+  describe "#add_to_order" do
+    it "should add the dish and quantity to the current order" do
+      subject.add_to_order("Half Chicken", 2)
+      expect(subject.current_order).to eq "Your current order:\n2 x Half Chicken"
+    end
   end
 end
