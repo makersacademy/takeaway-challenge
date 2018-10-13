a215463123c3d806f3df65c306e749b9a676e408
Tom <tombjh@hotmail.co.uk>
2018-10-13 17:03:24 +0100

    completed tests for menu object

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,33 @@
 require 'rspec'
+require_relative '../lib/menu'
 
-describe 'My behaviour' do
-
-  it 'should do something' do
-
-    true.should == false
+describe Menu do
+  describe '#new' do
+    let(:menu) { described_class.new }
+    it 'should create a new menu object' do
+      expect(described_class).to eq(Menu)
+    end
+  end
+  describe 'items' do
+    context 'where the menu is empty' do
+      let(:menu) { described_class.new }
+      it 'should return an empty hash' do
+        expect(menu.items.empty?).to eq true
+      end
+    end
+    context 'where the menu is populated' do
+      let(:menu) { described_class.new("spring roll" => 0.99,
+                                       "char sui bun" => 3.99,
+                                       "pork dumpling" => 2.99,
+                                       "peking duck" => 7.99,
+                                       "fu-king fried rice" => 5.99)
+      }
+      it 'should return a populated hash' do
+        expect(menu.items.empty?).to eq false
+      end
+      it "should return the first value of the key 'spring roll' " do
+        expect(menu.items['spring roll']).to eq(0.99)
+      end
+    end
   end
 end
