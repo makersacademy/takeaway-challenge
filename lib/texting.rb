a1241aeb8b204e3aa5fc4583428e16c9ae92a456
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-14 00:19:04 +0100

    finished

--- lib/texting.rb
+++ lib/texting.rb
@@ -1,15 +1,18 @@
+# This class handles the sms functionality with twilio
 require 'twilio-ruby'
 require 'customer'
 
 
 class SmsText
 
+  # sends the message
   def send_text
     init_msg(confirm_order)
   end
 
   private
 
+  # Links functionality with twilio
   def init_msg(msg)
     account_sid = 'ACcb0e21414d3ab3d87192fa33e93fc1c0'
     auth_token = 'fc23137ee0ee4262f9c677df35211a4d'
@@ -23,6 +26,7 @@ class SmsText
     )
   end
 
+  # Prints the confirm message
   def confirm_order
     time = Time.new
    "Thank ya. We'll be there at #{time.hour + 1}:#{time.min}."
