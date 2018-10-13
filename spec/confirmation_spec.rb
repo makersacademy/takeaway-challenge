dbe89647e0988f109e1bb816e8367828f0c3a53c
Caitlin Cooling <ccooling97@gmail.com>
2018-10-13 08:51:14 +0100

    fix issue with adding 1 hour: '

--- spec/confirmation_spec.rb
+++ spec/confirmation_spec.rb
@@ -5,8 +5,13 @@ describe Confirmation do
 
   describe '#time' do
     it 'tells me the time that my order was placed' do
-      time = Time.new
+      time = Time.new + 3600
       expect(confirmation.time). to eq time.strftime("%k:%M")
+    end
+  end
+  describe '#send_message' do
+    it 'receives the send_message method' do
+      expect(confirmation).to receive(:send_message)
     end
   end
 end
--- spec/user_stories_spec.rb
+++ spec/user_stories_spec.rb
@@ -61,8 +61,14 @@ describe Confirmation do
 
   describe '#time' do
     it 'tells me the time that my order was placed' do
-      time = Time.new
+      time = Time.new + 3600
       expect(confirmation.time). to eq time.strftime("%k:%M")
+    end
+  end
+
+  describe '#send_message' do
+    it 'calls the send_message method on my confirmation object' do
+      expect(confirmation).to receive(:send_message)
     end
   end
 end
