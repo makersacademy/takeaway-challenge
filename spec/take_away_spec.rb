6f205588705aa87b2f0c84a9622f6097979b390e
Tom <tombjh@hotmail.co.uk>
2018-10-14 14:21:45 +0100

    added bits to the takeaway challenge

--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -1,9 +1,11 @@
 require 'rspec'
+require_relative '../lib/take_away'
 
-describe 'My behaviour' do
-
-  it 'should do something' do
-
-    true.should == false
+describe TakeAway do
+  subject(:take_away){described_class.new}
+  describe '#new' do
+    it 'should create a new take away object' do
+      expect(described_class).to eq(TakeAway)
+    end
   end
 end
