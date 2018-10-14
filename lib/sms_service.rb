f741be3e23bbd085d3cb5f836a1f6e099645c0d4
Reka <rekapap28@gmail.com>
2018-10-14 22:03:29 +0100

    rubocop fixes

--- lib/sms_service.rb
+++ lib/sms_service.rb
@@ -4,12 +4,14 @@ require_relative 'confirmation_message'
 
 class SMSService
 
-  def initialize(confirmation_message_class: ConfirmationMessage, client: nil, number: nil)
+  def initialize(confirmation_message_class: ConfirmationMessage, client: nil,
+    number: nil)
     @confirmation_message_class = confirmation_message_class
     @sid = Base64.decode64("QUMzMDExMGE4OGJmOGMyY2RmODhhZmZhYmFmYzM4OTU1Mw==\n")
     @token = Base64.decode64("MTg5NWQzZmE3YWQ1ZWYzNzlhZWNiOWIyN2Q4NjYxMGE=\n")
     @client = client.nil? ? Twilio::REST::Client.new(@sid, @token) : client
-    @number = number.nil? ? Base64.decode64("KzQ0IDI4IDk2OTIgODI4NQ==\n") : number
+    @number = number.nil? ?
+    Base64.decode64("KzQ0IDI4IDk2OTIgODI4NQ==\n") : number
   end
 
   def send_sms
