947a7c1cdf890a54441d5d99094bb04786e0d546
rbbri <robin.s.johnson@gmail.com>
2018-10-14 12:10:39 +0100

    User Story 1 As a customer So that I can check if I want to order something I would like to see a list of dishes with prices

---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,15 @@
+# As a customer
+# So that I can check if I want to order something
+# I would like to see a list of dishes with prices
+
+require 'takeaway'
+
+describe Takeaway do
+
+  let(:takeaway) { described_class.new(menu) }
+  let(:menu) { double(:menu, print: 'Falafel: £3') }
+
+  it 'allows the user to see a list of dishes and prices' do
+    expect(takeaway.print_menu).to eq 'Falafel: £3'
+  end
+end
