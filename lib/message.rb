cf9f0b1f3cb5818a8846f66ed440859182c6ca4d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-14 19:51:39 +0100

    added a time, one hour from exection, to the message

--- lib/message.rb
+++ lib/message.rb
@@ -10,9 +10,13 @@ class Message
 
   def send
     @client.messages.create(
-      body: "Thank you for your order! It will be arriving shortly",
+      body: "Thank you for your order! It will be delivered before #{time}",
       to: ENV["MY_PHONE_NUMBER"],
       from: ENV["MY_TWILIO_PHONE_NUMBER"]
     )
+  end
+
+  def time
+    "#{Time.now.hour + 1}:#{Time.now.min}"
   end
 end
