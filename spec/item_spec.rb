ba984db1d6bc6bc6ae3c2b09e2a56466b1444bb1
Alfie Freeman <runticle@gmail.com>
2018-10-14 17:38:21 +0100

    renamed item file

---
+++ spec/item_spec.rb
@@ -0,0 +1,12 @@
+require 'item'
+
+describe Item do
+  it 'should have a name' do
+    bolognese = Item.new("Bolognese", 5)
+    expect(bolognese.name).to eq "Bolognese"
+  end
+  it 'should have a price' do
+    bolognese = Item.new("Bolognese", 5)
+    expect(bolognese.price).to eq 5
+  end
+end
