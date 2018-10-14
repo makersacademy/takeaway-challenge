d5ff2aed600c9165805a8f7b59e38996fba827e8
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-14 19:07:46 +0100

    Added tests for sending texts

--- lib/customercontact.rb
+++ lib/customercontact.rb
@@ -2,20 +2,25 @@ require 'twilio-ruby'
 
 class CustomerContact
 
-  def send_message(body)
-    account_sid = ENV["TWILIO_ACCOUNT_SID"]
-    auth_token = ENV["TWILIO_AUTH_TOKEN"]
+  def initialize(client = nil)
+    @client = client || Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
+  end
 
-    client = Twilio::REST::Client.new(account_sid, auth_token)
+  def send_message(total, details = {from: ENV["TWILIO_NUMBER"],
+      to: ENV["MY_NUMBER"],
+      body: "Thank you for your order - your total is £#{total}. Your food will be delivered by #{order_time}."
+      })
 
     from = ENV["TWILIO_NUMBER"] # Your Twilio number
     to = ENV["MY_NUMBER"] # Your mobile phone number
 
-    client.messages.create(
-    from: from,
-    to: to,
-    body: body
-    )
+    @client.messages.create(details)
+
+  end
+
+private
+  def order_time
+    (Time.now + 60*60).strftime("%H:%M")
   end
 
 end
--- spec/customercontact_spec.rb
+++ spec/customercontact_spec.rb
@@ -2,6 +2,30 @@ require 'customercontact'
 
 describe CustomerContact do
 
-  
+  subject { CustomerContact.new(client) }
+  let(:client)    { double :client, messages: messages}
+  let(:messages)  { double :messages }
+
+  describe '#send_message' do
+
+    it "sends a text with total and a delivery time" do
+      details = {from: 1,
+      to: 2,
+      body: "Thank you for your order - your total is £10. Your food will be delivered by 19:00."}
+      allow(subject).to receive(:order_time).and_return("19:00")
+      expect(messages).to receive(:create).with(details)
+      subject.send_message(10, details)
+    end
+
+  end
+
+  # describe '#order_time' do
+  #
+  #   it "returns one hour from current time" do
+  #     allow(Time).to receive(:now).and_return(Time.parse('13:30'))
+  #     expect(subject.order_time).to eq("14:30")
+  #   end
+  #
+  # end
 
 end
