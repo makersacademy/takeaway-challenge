27563ca8b112ec244e9bb28eebab4b7ba66e856e
mariusgg <marius.g.guei@gmail.com>
2018-10-14 23:44:18 +0100

    added error message for incorrect amount

--- lib/takeaway_order.rb
+++ lib/takeaway_order.rb
@@ -1,18 +1,22 @@
 
 class Order
 
-attr_reader :order, :price, :menu
+  attr_reader :order, :price, :menu
 
- def initialize(menu = TakeawayMenu.new)
-   @menu, @order, @price = menu, {}, 0
- end
+  def initialize(menu = TakeawayMenu.new)
+    @menu, @order, @price = menu, {}, 0
+  end
 
- def add_item(name, quantity)
-   @order[name] = quantity
- end
+  def add_item(name, quantity)
+    @order[name] = quantity
+  end
 
- def return_total
+  def return_total
     @order.to_a.each { |dish, amount| @price += @menu.dishes[dish] * amount }
-end
+  end
+
+  def pay(amount)
+    raise "Incorrect amount, please try again." unless amount == @price
+  end
 
 end
--- spec/takeaway_order_spec.rb
+++ spec/takeaway_order_spec.rb
@@ -2,24 +2,29 @@ require 'takeaway_order'
 
 describe Order do
 
-  let(:menu) { double :menu, dishes: { 'dish-1' => 1, 'dish-2' => 2 } }
+  let(:menu) { double :menu, dishes: { "dish-1" => 1, "dish-2" => 2 } }
   let(:subject) { described_class.new(menu) }
 
   before do
-    subject.add_item('dish-1', 1)
-    subject.add_item('dish-2', 2)
+    subject.add_item("dish-1", 1)
+    subject.add_item("dish-2", 2)
   end
 
   describe '#add_item' do
     it 'adds item to customer order list' do
-      expect(subject.order.to_a.flatten.join ', ').to eq 'dish-1, 1, dish-2, 2'
+      expect(subject.order.to_a.flatten.join ', ').to eq "dish-1, 1, dish-2, 2"
     end
   end
 
   describe '#return_total' do
     it 'retruns the total value' do
-      expect { subject.update_price }.to change { subject.price }.by(5)
+      expect { subject.return_total }.to change { subject.price }.by(5)
     end
   end
 
+  describe '#pay' do
+    it 'raises error if incorrect amount given' do
+      expect { subject.pay(5) }.to raise_error("Incorrect amount, please try again.")
+    end
+  end
 end
