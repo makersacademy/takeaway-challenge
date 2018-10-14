286584141d298f6226cc51ef0285201b7c6ca9ba
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-14 10:54:29 +0100

    added env variables to replace phone number and twilio details

--- lib/message.rb
+++ lib/message.rb
@@ -3,16 +3,16 @@ require 'twilio-ruby'
 class Message
   def initialize(client: Twilio::REST::Client.new(
     ENV["MY_TWILIO_SID"],
-    ENV["MY_TWILIO_AUTH_TOKEN"]).messages)
+    ENV["MY_TWILIO_AUTH_TOKEN"]))
 
     @client = client
   end
 
   def send
-    @client.create(
+    @client.messages.create(
       body: "Thank you for your order! It will be arriving shortly",
-      to: "+447539225381",
-      from: "+441133205022"
+      to: ENV["MY_PHONE_NUMBER"],
+      from: ENV["MY_TWILO_PHONE_NUMBER"]
     )
   end
 end
