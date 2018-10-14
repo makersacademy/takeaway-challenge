33e21a212cd86e50d310c5399662bb56fce55c4e
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-10-14 11:49:52 +0100

    added unit test for Basket class

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -2,6 +2,13 @@ require 'basket'
 
 describe Basket do
 
-  let(:basket) { Basket.new }
+  context 'describe review_order' do
+    before do
+      subject.basket["Satay Chicken"] = 6
+    end
+    it 'returns the item in the basket and the total cost' do
+      expect(subject.review_order).to eq "Satay Chicken, £6\nTotal: £6"
+    end
+  end
 
 end
