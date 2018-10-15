437ef0718e714fa69de574ca336b1cd3180e787f
tomlightfoot <tomlightfoot96@gmail.com>
2018-10-15 10:18:11 +0100

    Single responsibility - story 3

--- lib/menu.rb
+++ lib/menu.rb
@@ -14,6 +14,7 @@ class Menu
       puts heading.to_s
       dishes.each { |dish, cost| puts dish + " - £#{cost}" }
     end
+    return nil
   end
 
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,7 @@
 require './lib/menu.rb'
 
 class Order
-  attr_reader :dishes
+  attr_reader :dishes, :menu
 
   def initialize(menu = Menu.new)
     @dishes = []
@@ -16,9 +16,20 @@ class Order
 
   def summary
     @summary = ["\nSummary"]
-    dish_names = dishes.flatten.uniq.select { |x| x.is_a? String }
-    dish_names.each { |dish| @summary << "#{@dishes.flatten.count(dish)}x " + dish + " - £#{'%.2f' % (@dishes.flatten.count(dish) * @menu.menu.map { |_,value| value[dish] }.compact[0])}"}
-    @summary <<  "Order Total - £#{'%.2f' % @dishes.flatten.select { |x| x.is_a? Numeric }.inject(:+)}"
+    dishes_to_summary
+    total_to_summary
     puts @summary
+  end
+
+  private
+
+  def dishes_to_summary
+    dishes.flatten.uniq.select { |x| x.is_a? String }.each do |dish|
+      @summary << "#{@dishes.flatten.count(dish)}x " + dish + " - £#{'%.2f' % (@dishes.flatten.count(dish) * @menu.menu.map { |_,value| value[dish] }.compact[0])}"
+    end
+  end
+
+  def total_to_summary
+    @summary <<  "Order Total - £#{'%.2f' % @dishes.flatten.select { |x| x.is_a? Numeric }.inject(:+)}"
   end
 end
