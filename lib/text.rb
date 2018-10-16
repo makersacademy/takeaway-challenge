de3d9557d048b629669a7a7e9012647f0461abf2
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-10-16 07:11:14 +0100

    updated personal info

--- lib/text.rb
+++ lib/text.rb
@@ -5,7 +5,7 @@ class Text
   ACCOUNT_SID = 'ACc68fbb7c5f4b5a7c5bb1a06faaab901d'
   AUTH_TOKEN = '4e5bdc58fcc22d2b586f396a43288263'
 
-  def initialize(number = '+441785472436', client_number = '+447478158606', object = Object)
+  def initialize(number = XXX, client_number = XXX, object = Object)
     @twilio_phone_number = number
     @client_number = client_number
     @object = object
