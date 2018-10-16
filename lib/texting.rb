b553259936de8067043bf6f9523e37fd3ad63484
Alfie <runticle@gmail.com>
2018-10-16 21:25:41 +0100

    remove phone number

--- lib/texting.rb
+++ lib/texting.rb
@@ -7,7 +7,7 @@ class Texter
   AUTH_TOKEN = '9a47da217ea61d2c2035b4454ebf5224'
   CLIENT = Twilio::REST::Client.new(SID, AUTH_TOKEN)
   TWILIO_NUMBER = '+441637820059'
-  MY_NUMBER = '+447503151132'
+  MY_NUMBER = '+DONT STEAL MY PHONE NUMBER'
 
   def initialize
     @to = MY_NUMBER
