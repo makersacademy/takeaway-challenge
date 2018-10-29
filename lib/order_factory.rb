09127389cb0a603599910253959c65adfc263563
Reka <rekapap28@gmail.com>
2018-10-29 21:13:10 +0000

    order_factory refactoring

--- lib/order_factory.rb
+++ lib/order_factory.rb
@@ -14,10 +14,16 @@ class OrderFactory
 
   def make_items_list(order_array)
     order_array.map do |subarray|
-      name = subarray[0]
-      quantity = subarray[1]
-      [@menu.find_item_by_name(name), quantity]
+      make_item_quantity_list(subarray)
     end
   end
 
+  private
+
+  def make_item_quantity_list(array)
+    name = array[0]
+    item = @menu.find_item_by_name(name)
+    quantity = array[1]
+    [item, quantity]
+  end
 end
