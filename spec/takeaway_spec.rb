0424cc6e064f52159895343aa94d94df5122b88e
Will Wayman <will.jdw@gmail.com>
2018-11-09 10:10:36 +0000

    wrote first test to check that when using view_dishes method, not to eq nil

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,4 +1,9 @@
 require 'takeaway'
 
 describe Takeaway do
+  describe '#view_dishes' do
+    it 'should provide a list of fishes' do
+      expect(subject.view_dishes).not_to eq nil
+    end
+  end
 end
