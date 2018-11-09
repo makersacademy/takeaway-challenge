e8be169433a6342f43ddff9173dad59f93f41554
Will Wayman <will.jdw@gmail.com>
2018-11-09 11:02:10 +0000

    view_menu uses string interpolation to show items and prices

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -7,7 +7,12 @@ class Takeaway
   end
 
   def view_menu
-    @menu.items
+    @menu.items.each_with_index do |item, index|
+      puts "#{index+1}: #{item[:name]} £#{item[:cost]}"
+    end
   end
 
 end
+
+shop = Takeaway.new(Menu.new)
+shop.view_menu
