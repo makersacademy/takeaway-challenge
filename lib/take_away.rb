eff4dd4d82bdd070efb58e7dde312eaa79c09138
SBriscoe2 <shabris@tiscali.co.uk>
2018-11-09 14:54:19 +0000

    added method to add to order and removed item file and spec file

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,18 +1,32 @@
 class Takeaway
-  attr_reader :pricelist
+  attr_reader :pricelist, :name, :quantity, :prices
 
   def initialize
     @pricelist = []
+    @totals = []
   end
 
   def show_price_list
     @price_list = [
-      { name: "Jacket Potato (plain)", price: "£1.00" },
-      { name: "Jacket Potato with cheese", price: "£1.50" },
-      { name: "Jacket Potato with Tuna", price: "£2.00" },
-      { name: "Spaghetti Bolognese", price: "£3.50" },
-      { name: "Spaghetti Carbonara", price: "£3.00" },
-      { name: "Chicken Curry", price: "£5.00" }
+      { names: "Jacket Potato (plain)", price: 1.00 },
+      { names: "Jacket Potato with cheese", price: 1.50 },
+      { names: "Jacket Potato with Tuna", price: 2.00 },
+      { names: "Spaghetti Bolognese", price: 3.50 },
+      { names: "Spaghetti Carbonara", price: 3.00 },
+      { names: "Chicken Curry", price: 5.00 }
     ]
+  end
+
+  def add_order(names, quantity, prices)
+    @name = names.to_s
+    @quantity = quantity.to_i
+    @price = prices.to_f
+    @pricelist = [@name, @quantity, @price]
+  end
+
+  def total
+    original = @quantity * @price
+    @totals << original
+    @totals.reduce(:+)
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -13,4 +13,13 @@ describe Takeaway do
     expect(takeaway.show_price_list).to eq takeaway.show_price_list
   end
 
+  it 'should let customer allow customer to select an item and add it to the selection' do
+    takeaway.add_order("Jacket Potato with cheese", 2, 3.00)
+    expect(takeaway.pricelist).to eq ["Jacket Potato with cheese", 2, 3.00]
+  end
+
+  it 'should produce a total' do
+    takeaway.add_order("Jacket Potato with cheese", 2, 3.00)
+    expect(takeaway.total).to eq 6
+  end
 end
