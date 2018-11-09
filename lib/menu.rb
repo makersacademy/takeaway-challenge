728e04f16a16b9c73bb4c95039a4f61a825348c2
Max Stevenson <maxstevenson@msn.com>
2018-11-09 14:49:58 +0000

    Developed get_selection() for Menu class - takes user input and adds the corresponding item to cart instance variable

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,9 @@
 class Menu
-	attr_reader :available_dishes
+	attr_accessor :available_dishes, :cart
 
 	def initialize
 		@available_dishes = []
+		@cart = []
 	end
 
 	def add_dish(dish)
@@ -12,6 +13,16 @@ class Menu
 	def show_menu
 		@available_dishes.each_with_index do |dish, index|
 			puts "#{index + 1}.#{dish.name} - #{dish.price}"
+		end
+	end
+
+	def get_selection(stdin: $stdin)
+		puts "Please enter the number of the item you wish to order"
+		selection = stdin.gets
+		@available_dishes.each_with_index do |dish, index|
+			if (selection - 1) == index
+				cart << dish
+			end
 		end
 	end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,28 +5,41 @@ describe Menu do
 	let(:dish1) {double("chicken", :name => "chicken", :price => 1.99)}
 	let(:dish2) {double("wings", :name => "wings", :price => 2.99)}
 
-	it 'should be able to add dishes to menu' do
-		subject.add_dish(dish1)
-		expect(subject.available_dishes).to include(dish1)
+	context 'a menu with multiple dishes already added' do
+		before (:each) do
+			subject.add_dish(dish1)
+			subject.add_dish(dish2)
+		end
+
+		it 'should be able to add dishes to menu' do
+			expect(subject.available_dishes).to include(dish1)
+		end
+
+		it 'should be able to store multiple dishes' do
+			expect(subject.available_dishes).to include(dish1, dish2)
+		end
+
+
+
+		it 'should return the name and price of multiple dishes' do
+			expect{subject.show_menu}.to output("1.#{dish1.name} - #{dish1.price}\n2.#{dish2.name} - #{dish2.price}\n").to_stdout
+		end
 	end
-
-	it 'should be able to store multiple dishes' do
-		subject.add_dish(dish1)
-		subject.add_dish(dish2)
-
-		expect(subject.available_dishes).to include(dish1, dish2)
-	end
-
 	it 'should return the name and price of a dish' do
 		subject.add_dish(dish1)
-
 		expect{subject.show_menu}.to output("1.#{dish1.name} - #{dish1.price}\n").to_stdout
 	end
 
-	it 'should return the name and price of multiple dishes' do
-		subject.add_dish(dish1)
-		subject.add_dish(dish2)
+	describe '#get_selection' do
+		it 'should ask the user for their choice' do
+			expect{subject.get_selection}.to output("Please enter the number of the item you wish to order""\n").to_stdout
+		end
+		it 'should retreive the appropiate dish and add that to cart' do
+			allow(STDIN).to receive(:gets).and_return(1)
+			subject.add_dish(dish1)
+			subject.get_selection
 
-		expect{subject.show_menu}.to output("1.#{dish1.name} - #{dish1.price}\n2.#{dish2.name} - #{dish2.price}\n").to_stdout
+			expect(subject.cart).to include(dish1)
+		end
 	end
 end
