c491b301fc076c90e408f0bf4231eb44517d2b0a
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-09 14:13:28 +0000

    adds a texter classs that can send texts

---
+++ lib/texter.rb
@@ -0,0 +1,24 @@
+require 'twilio-ruby'
+
+class Texter
+
+  def initialize
+    @account_sid = 'ACf89596d0fba6c01a7ba140ade7edfefc'
+    @auth_token = '5f9eadd286fa95924ddf3a86a02cfd59'
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    @from = '+441233800623'
+    @to = '+447504848546'
+  end
+
+  def send_text(cost)
+    time = Time.now
+    soon = time + 3600
+    @client.messages.create(
+      from: @from,
+      to: @to,
+      body: "You'll get your £#{cost} worth of "\
+        "food by #{soon.strftime("%H:%M")}"
+    )
+  end
+
+end
