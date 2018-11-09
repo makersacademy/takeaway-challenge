ab2fcc1413bd0dd98765955fdf7b715090f7bf6e
Al <>
2018-11-09 12:14:09 +0000

    added #view functionality to Menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,11 @@
 class Menu
+  def initialize(dishes)
+    @dishes = dishes
+  end
+
   def view
+    formatted_list = @dishes.map do |dish|
+      "#{dish.name}: %.2f" % dish.price
+    end
   end
 end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,17 @@
+require 'menu'
+
+describe Menu do
+
+  let(:fish) { instance_double('Dish', :name => 'Fish', :price => 2.50) }
+  let(:chips) { instance_double('Dish', :name => 'Chips', :price => 1.20) }
+  let(:subject) { Menu.new([fish, chips]) }
+
+  context '#view' do
+    it 'returns a list of dishes and their prices' do
+      expect(subject.view).to eq [
+        "#{fish.name}: %.2f" % fish.price,
+        "#{chips.name}: %.2f" % chips.price
+      ]
+    end
+  end
+end
