09b23b0473862e03d525ad7c33702d711f2083fc
Obsiye <mrabdirizak@gmail.com>
2018-11-09 10:32:35 +0000

    Test and refactor for selecting dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,16 +1,20 @@
 class Takeaway
-    attr_reader  :dishes
+  attr_reader :dishes, :selected
 
-    def initialize
-      @dishes = [{dish: "Pizza", price: 20},
-        {dish: "Kebab", price: 12},
-        {dish: "Sushi", price: 22},
-        {dish: "Burger", price: 10}
-      ]
-    end
+  def initialize
+    @dishes = [{ item_no: 1, dish: "Pizza", price: 20 },
+      { item_no: 2, dish: "Kebab", price: 12 },
+      { item_no: 3, dish: "Sushi", price: 22 },
+      { item_no: 4, dish: "Burger", price: 10 }
+    ]
+    @selected = []
+  end  
 
-    def see_dishes
-        dishes
-    end
+  def see_dishes
+    dishes
+  end
+
+  def select_dish
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,15 +1,25 @@
 require 'takeaway'
 
 describe Takeaway do
-    it 'responds to .see_dishes' do
-      expect(subject).to respond_to(:see_dishes)
-    end
-        it 'responds to .dishes' do
-      expect(subject).to respond_to(:dishes)
-    end
 
-    it 'shows a list of dishes with prices' do
-        expect(subject.see_dishes.length).to be > 0
-    end
+  it 'responds to .see_dishes' do
+    expect(subject).to respond_to(:see_dishes)
+  end
+
+  it 'shows a list of dishes with prices' do
+    expect(subject.see_dishes.length).to be > 0
+  end
+  
+  it 'responds to .select_dish' do
+    expect(subject).to respond_to(:select_dish)
+  end
+
+  it 'responds to .selected' do
+    expect(subject).to respond_to(:selected)
+  end
+
+  it 'responds to .select_dish' do
+    expect(subject).to respond_to(:see_dishes)
+  end
 
 end
