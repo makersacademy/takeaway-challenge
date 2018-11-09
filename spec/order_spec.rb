ed51634ab6f18d1951bb568361474217be3d2242
will mcbrien <williamjamesmcbrien@gmail.com>
2018-11-09 16:47:35 +0000

    WIP: order class to be created

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -8,8 +8,9 @@ describe Order do
 
   subject(:order) { Order.new(menu) }
   it 'can create a dish based on data from the menu' do
-    created_dish = order.create_dish(menu_id)
-    expect(order.create_dish(menu_id, dish)).to have_att
+    created_dish = order.create_dish(menu, menu_id)
+    # TODO: add create dish method and expectation
+    expect(created_dish).to eq()
   end
 
   it 'can build an order based on the selection from a customer' do
