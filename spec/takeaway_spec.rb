03ac1a41d314bd3b74208ad08e5c9fc12ba8ec86
Will Wayman <will.jdw@gmail.com>
2018-11-09 10:15:27 +0000

    changed test to take dishes perameter

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,11 @@
 require 'takeaway'
+require 'dishes'
 
 describe Takeaway do
   describe '#view_dishes' do
     it 'should provide a list of fishes' do
-      expect(subject.view_dishes).not_to eq nil
+      shop = described_class.new(Dishes.new)
+      expect(shop.view_dishes).not_to eq nil
     end
   end
 end
