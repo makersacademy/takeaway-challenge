803e4ddd97f4f73fba84d49aeb9346d9b0c0eaf3
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-09 16:03:03 +0000

    add test for list method to show list_of_dishes variable to show an empty array

--- lib/takeway.rb
+++ lib/takeway.rb
@@ -1,5 +1,12 @@
 class Dishes
 
-  def list
+attr_reader :list_of_dishes
+
+  def initialize
+    @list_of_dishes = list_of_dishes
+  end
+
+  def dishes
+  @list_of_dishes = []
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,9 +2,13 @@ require "takeway"
 
 describe Dishes do
 
-  describe "#list" do
-    it { is_expected.to respond_to :list }
+  describe "#list_of_dishes" do
+
+    it "has an empty list of dishes" do
+      expect(subject.dishes).to eq []
+    end
 
   end
+
 
 end
