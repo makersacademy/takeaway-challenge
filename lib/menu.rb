0add5a6d9ac36d00869d7b7783e7cd1d179e1818
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-09 12:49:00 +0000

    customer can order food which will be stored in an array

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,17 +1,31 @@
 
 class Menu
 
+  attr_reader :item
+
   def initialize
     @menu_items = {
-      "burger" => 5,
-      "pizza" => 10,
-      "fries" => 2,
-      "salad" => 3
+      :burger => 5,
+      :pizza => 10,
+      :fries => 2,
+      :salad => 3
     }
+    @item = []
   end
 
   def list_items
-    @menu_items
+    p "Menu items"
+    @menu_items.map { |key, value| [key.to_sym, value.to_i] }
   end
 
+  def order_items
+    input = ""
+    p "What would you like to order?"
+    p "To finish ordering please type end"
+    while input == gets.chomp
+      @item << input
+      break if input == "end"
+    end
+    p @item.reverse.drop(1)
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,22 @@
 require './lib/menu'
 
 describe Menu do
+  subject(:menu) { Menu.new }
+
   it 'will list menu items' do
-    menu = Menu.new
-    expect(menu).to respond_to(:list_items)
+    expect(menu.list_items).to include([:burger, 5], [:pizza, 10], [:fries, 2], [:salad, 3])
+  end
+
+  it 'will allow a customer to order items' do
+    expect(menu).to respond_to(:order_items)
+  end
+
+  it ' #@items will store an order' do
+    expect(@item).to eq(nil)
+  end
+
+  context('after a customer has placed an order')
+  it 'will print an order' do
+    expect { menu.order_items }.to output(@item).to_stdout
   end
 end
