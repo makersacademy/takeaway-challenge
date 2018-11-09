74c4c4a26da41c0f698948f808a33bcca8427012
Obsiye <mrabdirizak@gmail.com>
2018-11-09 10:13:09 +0000

    Test and refactor user story: can see list of dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,16 @@
 class Takeaway
+    attr_reader  :dishes
+
+    def initialize
+      @dishes = [{dish: "Pizza", price: 20},
+        {dish: "Kebab", price: 12},
+        {dish: "Sushi", price: 22},
+        {dish: "Burger", price: 10}
+      ]
+    end
+
+    def see_dishes
+        dishes
+    end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,4 +1,15 @@
 require 'takeaway'
 
 describe Takeaway do
+    it 'responds to .see_dishes' do
+      expect(subject).to respond_to(:see_dishes)
+    end
+        it 'responds to .dishes' do
+      expect(subject).to respond_to(:dishes)
+    end
+
+    it 'shows a list of dishes with prices' do
+        expect(subject.see_dishes.length).to be > 0
+    end
+
 end
