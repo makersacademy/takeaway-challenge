6ccfeaa2b5a7ad3293cd1682e97acbc3c00d9de4
Michael Slevin <michaelslevin@pc-132-173.customer.cityuniversity.generation.lan>
2018-11-10 17:05:52 +0000

    removed customer class as was needed

--- spec/customer_spec.rb
+++
@@ -1,7 +0,0 @@
-require "customer"
-
-describe Customer do
-  let(:customer) {Customer.new("Michael Slevin")}
-  it { expect(customer.phone_number).to eq("07889151641") }
-  it { expect(customer.name).to eq "Michael Slevin" }
-end
