971d338f58905bce55f61e2c4cd129d3a5a1c726
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-10 10:49:45 +0000

    added unit test for list of dishes and passes with an array of hashes

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,17 @@
 
 class Order
 
+  attr_reader :list_of_dishes
+
+  def initialize
+    @list_of_dishes = [{ "Roasted stuffed cauliflower" => 13},
+                       { "Stuffed pumpkin" => 12},
+                       { "Vegan chilli" => 15},
+                       { "Vegan brownies" => 8}]
+  end
+
   def dish
+    @list_of_dishes
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,7 +3,15 @@ require 'order'
 describe Order do
   subject(:order) { described_class.new }
 
-  it "will show a list of dishes" do
+  it "allows customer to see dishes available" do
     expect(order).to respond_to(:dish)
+  end
+
+  it "will return a list of dishes" do
+    order.dish
+    expect(order.list_of_dishes).to eq [{ "Roasted stuffed cauliflower" => 13},
+                                        { "Stuffed pumpkin" => 12},
+                                        { "Vegan chilli" => 15},
+                                        { "Vegan brownies" => 8}]
   end
 end
