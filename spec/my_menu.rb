abf942b0e07502c4ac349a434380b20629efb264
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-10 10:37:00 +0000

    User Story 1  #As a customer   # So that I can check if I want to order something   # I would like to see a list of dishes with prices

---
+++ spec/my_menu.rb
@@ -0,0 +1,14 @@
+menu = {
+  'chicken korma' => 5.70,
+  'chicken bhuna' => 6.70,
+  'lamb rogan josh' => 6.50,
+  'lamb madras' => 5.80,
+  'king prawn dhansak' => 7.70,
+  'plain rice' => 1.90,
+  'pilau rice' => 2.00,
+  'bombay aloo' => 3.25,
+  'tarka daal' => 3.25,
+  'plain naan' => 2.50,
+  'garlic naan' => 2.75,
+  'peshwari naan' => 3.00
+}
