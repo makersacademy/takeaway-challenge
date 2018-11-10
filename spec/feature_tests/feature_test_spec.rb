7344e249c76e8c52cfb4cd1aa5652d3f99e0520b
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-10 10:20:04 +0000

    initial commit

---
+++ spec/feature_tests/feature_test_spec.rb
@@ -0,0 +1,8 @@
+describe 'User Stories' do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+  it 'allows me to see a list of dishes with prices' do
+    menu = Menu.new
+  end
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,7 @@
+require 'menu'
+
+describe Menu do
+  it 'allows me to see a list of dishes with prices' do
+    menu = Menu.new
+  end
+end
