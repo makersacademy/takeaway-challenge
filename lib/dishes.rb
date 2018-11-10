2814b8e5a9986dff626df3e1157a9a59ef65c2a3
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-10 10:07:55 +0000

    add empty variable list of dishes to list method with unit test

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -1,7 +1,13 @@
 class Dishes
 
-  def list(order)
+attr_reader :list_of_dishes
 
+  def initialize
+    @list_of_dishes = nil
+  end
+
+  def list(order)
+    @list_of_dishes
   end
 
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -6,4 +6,8 @@ describe Dishes do
   it "views a list of dishes with prices to allow an order to be made" do
     expect(dishes).to respond_to(:list).with(1).argument
   end
+
+  it "initializes the list of dishes to an empty array" do
+    expect(dishes.list_of_dishes).to eq_to nil
+  end
 end
