bf92a209331a4d33acdce9e29de13796c6c21d84
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-10 10:01:39 +0000

    refactored unit test to be more readable

--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -1,8 +1,9 @@
 require 'dishes'
 
 describe Dishes do
+  subject(:dishes) { described_class.new }
 
-  it "views a list of dishes with prices to allow an order ot be made" do
-    expect(subject).to respond_to(:list).with(1).argument
+  it "views a list of dishes with prices to allow an order to be made" do
+    expect(dishes).to respond_to(:list).with(1).argument
   end
 end
