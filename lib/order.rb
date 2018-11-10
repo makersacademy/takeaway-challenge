025af55869353fb3608318618dc8a8afdb453540
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-10 14:32:05 +0000

    non-working twilio

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,5 @@
 require_relative 'restaurant'
+require "twilio-ruby"
 
 class Order
 
@@ -22,9 +23,26 @@ class Order
     total = 0
     @order.each do |dish|
       total += dish.price
-      order_check << "#{dish.name}\n"
+      order_check << "#{dish.name}-#{dish.price}\n"
     end
     order_check << "Your total is £#{total}.\n"
+  end
+
+  def confirm
+    @restaurant.confirm(@order)
+
+
+    account_sid = "AC86dc289721e21ddbdad9b772ba094d37" # Your Account SID from www.twilio.com/console
+    auth_token = "a40461cf687f5d27129c668b8c95ff66"   # Your Auth Token from www.twilio.com/console
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.messages.create(
+        body: self.check,
+        to: "+447713920330",    # Replace with your phone number
+        from: "+441469727010")  # Replace with your Twilio number
+
+    puts message.sid
+
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,4 +1,5 @@
 require 'order'
+require 'twilio-ruby'
 
 describe Order do
 
@@ -34,8 +35,23 @@ describe Order do
     end
 
     it "prints the order and checks the total is correct" do
-      order_printed = "Eggs\nEggs\nSausages\nYour total is £10.\n"
+      order_printed = "Eggs-3\nEggs-3\nSausages-4\nYour total is £10.\n"
       expect(order.check).to eq order_printed
+    end
+  end
+
+  describe "#confirm" do
+
+    before "adds 5 eggs and 3 sausages to the order" do
+      allow(restaurant).to receive(:dish).with("Eggs").and_return(dish_eggs)
+      allow(restaurant).to receive(:dish).with("Sausages").and_return(dish_sausages)
+      order.add("Eggs",2)
+      order.add("Sausages",1)
+    end
+
+    it "sends the order to the restaurant" do
+      expect(restaurant).to receive(:confirm).with(dishes)
+      order.confirm
     end
   end
 
