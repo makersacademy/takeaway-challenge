4358b3d32bdaec2b618db03b676e0da27d3cf098
Will Wayman <will.jdw@gmail.com>
2018-11-11 13:24:51 +0000

    added test for empty_basket

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,7 +3,7 @@ require 'menu'
 
 describe TakeAway do
   describe '#view_menu' do
-    it 'should respond to view_menu' do
+    it 'should' do
 
     end
   end
@@ -20,8 +20,9 @@ describe TakeAway do
       end
     end
     describe '#empty_basket' do
-      it 'should' do
-
+      it 'should show an empty array' do
+        subject.empty_basket
+        expect(subject.basket).to eq []
       end
     end
     describe '#check_basket' do
