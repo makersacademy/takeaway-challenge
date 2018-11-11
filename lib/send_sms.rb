40a72b1c0ce14971529f2bbe39b9319dad3889bc
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-11 22:33:05 +0000

    api key hidden

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -4,8 +4,8 @@ class SendSms
 
   def send(message)
 
-    account_sid = 'ACce313cdc20d9b638b5a565e06b67f8de'
-    auth_token = 'aef68e0ab5c3d1825d494385a73adccf'
+    account_sid = '~'
+    auth_token = '~'
 
     @client = Twilio::REST::Client.new(account_sid, auth_token)
 
