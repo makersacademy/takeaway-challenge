696a3f04c5a24c22cb8f26aba87c49f3dad4e2c8
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-11 20:40:23 +0000

    Step 2 - Completed

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,19 +4,14 @@ describe Menu do
 
   subject(:menu) { described_class.new(dishes) }
 
-  let(:dishes) do
-    {
-      chicken: 4.99,
-      kebab: 2.00
-    }
-  end
+  let(:dishes) { {chicken: 5.00, kebab: 2.00} }
 
   it "Has a list of dishes with prices" do
     expect(menu.dishes).to eq(dishes)
   end
 
   it "Prints a list of dishes with their prices" do
-    printed_menu = "Chicken: £4.99, Kebab: £2.00"
+    printed_menu = "Chicken: £5.00, Kebab: £2.00"
     expect(menu.print).to eq(printed_menu)
   end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,13 +1,20 @@
-# require "takeaway"
-#
-# describe Takeaway do
-#
-#   subject(:takeaway) { described_class.new(menu: menu) }
-#
-#   let(:menu) { double(:menu) }
-#
-#   it "Displays a menu of dishes and prices" do
-#     expect(takeaway.show_menu).to eq("Menu")
-#   end
-#
-# end
+require "takeaway"
+
+describe Takeaway do
+
+  subject(:takeaway) { described_class.new(menu, order) }
+
+  let(:menu) { double(:menu) }
+  let(:order) { double(:order) }
+  let(:dishes) { {chicken: 5, kebab: 2} }
+
+  it "Displays a menu of dishes and prices" do
+    expect(takeaway.show_menu).to eq(menu)
+  end
+
+  it "Can order several dishes" do
+    expect(order).to receive(:add).twice
+    takeaway.place_order(dishes)
+  end
+
+end
