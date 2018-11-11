aaa52f57fe072baf8650de0295009bcbdac780be
will mcbrien <williamjamesmcbrien@gmail.com>
2018-11-11 13:32:40 +0000

    Remove redundant tests from takeaway_spec

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,20 +7,8 @@ describe Takeaway do
   let(:expected_menu_print_output) { TestData::expected_menu_print_output }
   subject(:takeaway) { Takeaway.new }
 
-  #it 'accepts command line arguments to pass in twillio credentials' do
-  #  expect(takeaway.inputs).to eq([])
-  #end
-
-  it 'welcomes customers to the shop' do
-    expect{ takeaway.welcome }.to output{ welcome_message }.to_stdout
-  end
-
   it 'accepts command line arguments to pass in twillio credentials' do
 
-  end
-
-  it 'shows the menu' do
-    expect{ takeaway.print_menu }.to output{ expected_menu_print_output }.to_stdout
   end
 
   it 'prompts users to confirm their order' do
