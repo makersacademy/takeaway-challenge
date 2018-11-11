3acf9d0ed9a6e3e4074292d3092b4cf0521260a1
Max Stevenson <maxstevenson@msn.com>
2018-11-11 16:14:28 +0000

    checkout now sends a confirmation text

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,6 @@
 require 'menu'
 require 'dish'
+require 'text'
 
 describe Menu do
 	let(:dish1) {double("chicken", :name => "chicken", :price => 1.99)}
@@ -47,17 +48,30 @@ describe Menu do
 		end
 		
 	end
+
+	# Every test below here is broken - running the test sends a text.
 	describe '#checkout' do
 		before (:each) do
 			allow(subject).to receive(:gets).and_return("1\n", "2\n", "done\n")
 		end
 		it 'should total up items in the cart' do
-			expected_total = dish1.price + dish2.price
 			subject.add_dish(dish1)
 			subject.add_dish(dish2)
 			subject.get_selection
 
 			expect{subject.checkout}.to output("Your order comes to a total of £#{dish1.price + dish2.price}""\n").to_stdout
+		end
+
+		it 'send a text message' do
+			text = Text.new
+			total = dish1.price + dish2.price
+			message = "Thank you for your order: £#{total}"
+			expect(text).to receive(:send_message) {:total}
+			subject.add_dish(dish1)
+			subject.add_dish(dish2)
+			subject.get_selection
+
+			expect(subject.checkout).to eq(message)
 		end
 	end
 end
