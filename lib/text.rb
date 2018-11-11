af1683456285ff09d405802222f62b2b17c831d0
Max Stevenson <maxstevenson@msn.com>
2018-11-11 12:26:21 +0000

    Developed a Text class that is capable of sending a delivery confirmation message

---
+++ lib/text.rb
@@ -0,0 +1,20 @@
+require 'twilio-ruby'
+
+class Text
+	account_sid = 'ACa1c932796ecd271062330acc94582c1b'
+	auth_token = '6d0f848aed9f061211ad09f5c76b84a0'
+	client = Twilio::REST::Client.new(account_sid, auth_token)
+
+	from = '+447481338117' 
+	to = '+447738623761'
+
+	
+
+	def send_message (total_price)
+		client.messages.create(
+		from: from,
+		to: to,
+		body: "Thank you for your order: £#{total_price}"
+		)
+	end
+end
