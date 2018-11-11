35dc63921a51dc60f2a2c05ff174185b27a6ef7d
Obsiye <mrabdirizak@gmail.com>
2018-11-11 15:14:48 +0000

    Remove takeawaydraft file

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,11 +1,11 @@
 require "order"
 
 describe Order do
-  let(:basket) { double(basket) }
-  subject(:order) { Order.new(basket,20) }
+#   let(:basket) { double(basket) }
+#   subject(:order) { Order.new(basket,20) }
 
-  it 'should respond to process' do
-    expect(subject).to respond_to(:process)
-  end
+#   it 'should respond to process' do
+#     expect(subject).to respond_to(:process)
+#   end
 
 end
