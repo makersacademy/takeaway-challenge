efe6c44657ff6ccfe9eafc1f65cf8146c895fb71
Will Wayman <will.jdw@gmail.com>
2018-11-11 15:13:00 +0000

    added test for viewing menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,9 +6,8 @@ describe Menu do
   end
   describe '#view' do
     it 'should provide a list of dishes' do
-      expect(STDOUT).to receive(:puts).with("Peri Peri Chicken")
-      require 'menu'
-      subject.view
+      message = "--------------------------\nMenu\n--------------------------\n1: Peri Peri Chicken £8\n2: American Hot Pizza £7\n3: Tandori Chicken Wrap £6\n4: Madras Curry £8\n5: Sweet and Sour Duck £9\n6: Beef Burrito £6\n7: Bacon Cheeseburger £6\n8: Katsu Curry £8\n--------------------------\n"
+      expect{ subject.view }.to output(message).to_stdout
     end
   end
 end
