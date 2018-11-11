28242e1ed2ce1ad26e91a0156be906adf8bd10d0
Darryl Banks <darryl.banks@zombie9.com>
2018-11-11 20:35:32 +0000

    refactor add_items to private method for order.select

--- lib/order.rb
+++ lib/order.rb
@@ -3,17 +3,20 @@ class Order
   attr_reader :my_order
 
   def initialize(my_order = [])
-
     @my_order = my_order
   end
 
   def select(item, quantity)
-    Menu::LIST.each do |hash|
-      if item == hash[:item]
-        quantity.times { @my_order << hash }
-      end
-    end
+    add_items(item, quantity)
     @my_order
+  end
+
+private
+
+  def add_items(item, quantity)
+    Menu::LIST.each do |hash|
+        quantity.times { @my_order << hash } if item == hash[:item]
+    end
   end
 
 end
