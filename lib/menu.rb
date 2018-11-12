a21264e3f8487e6d3ccf68c52c44cbdbce41174e
James de Winton <james.dewinton@gmail.com>
2018-11-12 08:51:35 +0000

    weird ass code

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,33 @@
 class Menu
-
-
+DELIVERY_CHARGE = 2.50
+  def initialize(menu = [{"Mouse stew" => 13},
+    {"fish pizza" => 12},
+    {"kirill kebab" => 7},
+    {"rat stew" => 6},{"avocado on toast" => 3}])
+    @menu = menu
+    @basket = []
+    @menu_prices = {"Mouse stew" => 13,
+    "fish pizza" => 12,
+    "kirill kebab" => 7,
+    "rat stew" => 6,"avocado on toast" => 3}
+  end
+  def select
+    @order = @menu[0..2]
+  end
   def menu
-    puts [{"Mouse stew" => 12.50}, {"fish pizza" => 12},
-    {"kirill kebab" => 7},{"rat stew" => 6},{"avocado on touser" => 3}]
+    @menu
+  end
+  def view
+    @menu[0..2]
+  end
+
+  def save_to_basket
+    @basket << @menu[0..2]
+  end
+  def total
+    @menu_prices["Mouse stew"] + @menu_prices["fish pizza"] + @menu_prices["kirill kebab"] + DELIVERY_CHARGE
+
+
 
   end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,16 +1,35 @@
 require 'menu'
 
 describe Menu do
-
-it "prints out the menu" do
-  expect(subject.menu).to eq [{"Mouse stew" => 12.50}, {"fish pizza" => 12},
-  {"kirill kebab" => 7},{"rat stew" => 6},{"avocado on touser" => 3}]
+let(:station){ double(:station, zone: 1)}
+it "holds an array of hashes which contains the menu" do
+  expect(subject.menu).to eq [{"Mouse stew" => 13}, {"fish pizza" => 12},
+  {"kirill kebab" => 7},{"rat stew" => 6},{"avocado on toast" => 3}]
 end
 
+it "prints out the menu for customer to view" do
+  expect(subject.menu).to eq [{"Mouse stew" => 13}, {"fish pizza" => 12},
+  {"kirill kebab" => 7},{"rat stew" => 6},{"avocado on toast" => 3}]
+end
+it "shows the customers basket for the customer to view" do
+  expect(subject.view).to eq [{"Mouse stew" => 13}, {"fish pizza" => 12},{"kirill kebab" => 7}]
+end
 
+it "selects a number of items from the menu" do
+  expect(subject.select).to eq [{"Mouse stew" => 13}, {"fish pizza" => 12},{"kirill kebab" => 7}]
+end
 
+it "saves selected items to the basket (empty array)" do
+  expect(subject.save_to_basket).to eq [[{"Mouse stew" => 13}, {"fish pizza" => 12},{"kirill kebab" => 7}]]
+end
 
+it "returns the total price of the order" do
+  expect(subject.total).to eq 34.50
+end
+# menu = double(:menu)
+it "raises error when the order is wrong" do
 
+end
 
 
 end
