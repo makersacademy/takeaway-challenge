0e9f336026a713a76c7c433d36815ec5889c31a3
will mcbrien <williamjamesmcbrien@gmail.com>
2018-11-12 13:09:35 +0000

    Add instructive comment to start.rb

--- lib/start.rb
+++ lib/start.rb
@@ -1,5 +1,8 @@
 require_relative 'takeaway'
 
+
+# Start app by running '$ ruby start.rb <twilio account sid> <twilio auth token> <phone number>'
+
 creds = { account_sid: ARGV[0], auth_token: ARGV[1] , from_number: ARGV[2] }
 takeaway = Takeaway.new(creds)
 takeaway.start
