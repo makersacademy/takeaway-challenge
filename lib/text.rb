16fda8d7f5741a3a0b82903eca9e31599c3ddf01
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-12 13:55:07 +0000

    got rid of api keys

--- lib/text.rb
+++ lib/text.rb
@@ -3,9 +3,6 @@ Dotenv.load
 
 class Text
 
-  AUTH = "AC86dc289721e21ddbdad9b772ba094d37"
-  KEY = "72dc6dafc62c0187a3ea45406e3aeeeb"
-
   def initialize(client = Twilio::REST::Client, sid = ENV["TWILIO_SID"], key = ENV["TWILIO_KEY"])
     @client = client.new(sid, key)
   end
