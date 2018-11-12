540be60a1166f00a397857ba0273c4c68718a410
Darryl Banks <darryl.banks@zombie9.com>
2018-11-12 02:03:05 +0000

    refactor sms to initialise client ready for stubbing

--- lib/sms.rb
+++ lib/sms.rb
@@ -2,16 +2,25 @@ require 'twilio-ruby'
 
 class Sms
 
+  attr_reader :client
+
+  def initialize
+    @client = Twilio::REST::Client.new(
+      'ACc08ab097e2f139cfb0bceab20eb24b48',
+      '885e51e20463204e6c4c094e29da5739',
+    )
+  end
+
   def send
 
-    account_sid = 'ACc08ab097e2f139cfb0bceab20eb24b48'
-    auth_token = '885e51e20463204e6c4c094e29da5739'
-    client = Twilio::REST::Client.new(account_sid, auth_token)
+    # account_sid = 'ACc08ab097e2f139cfb0bceab20eb24b48'
+    # auth_token = '885e51e20463204e6c4c094e29da5739'
+    # client = Twilio::REST::Client.new(account_sid, auth_token)
 
     from = '+441688212023' # Your Twilio number
     to = '+447809357149' # Your mobile phone number
 
-    client.api.account.messages.create(
+    @client.api.account.messages.create(
     from: from,
     to: to,
     body: "Thank you! Your order will be delivered within the hour!"
--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -2,10 +2,12 @@ require 'sms'
 
 describe Sms do
 
+  it { is_expected.to respond_to(:client).with(0).argument }
+
   describe '#send' do
 
     it 'should send an sms to confirm order' do
-      
+
     end
 
   end
