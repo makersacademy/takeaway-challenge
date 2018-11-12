e1a68b88504458e05f7269dde0cf1bf7d54637b3
Luca <eto.lucafrancesco@gmail.com>
2018-11-12 10:03:09 +0000

    Testing

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -4,15 +4,15 @@ require 'twilio-ruby'
 class Message
   def initialize
     @time = Time.new
-    @account_sid = 'AC83ceb2dbca3efdd4e5b7e0694eeb70cb'
-    @auth_token = '48a77764ee0d2450c2d67a28f4131911'
+    @account_sid = ''
+    @auth_token = ''
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
   end
 
 def send_message
     @client.messages.create(
-    from: '+441704320460', # Your Twilio number
-    to: '+xxxx.', # Your mobile phone number
+    from: '+', # Your Twilio number
+    to: '+', # Your mobile phone number
     body: "Thank you! Your order was placed and will be delivered before #{@time + 3600}"
     )
   end
