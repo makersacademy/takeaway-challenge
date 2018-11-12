ddf558f12886137fa188c623c4cac0b4d35c5aa6
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-12 13:05:54 +0000

    removed placeholder comments

--- lib/menu.rb
+++ lib/menu.rb
@@ -8,8 +8,6 @@ class Menu
       :fries => 2,
       :salad => 3
     }
-    # @item = []
-    # @total = []
   end
 
   def list_items
@@ -17,33 +15,3 @@ class Menu
     @menu_items.map { |key, value| [key.to_sym, value.to_i] }
   end
 end
-
-  # def order_items
-  #   p "What would you like to order?"
-  #   p "To finish ordering please type end"
-  #   input = ""
-  #   while input = gets.chomp
-  #     @item << input
-  #     break if input == "end"
-  #   end
-  #   p @item.reverse.drop(1)
-  # end
-
-  # def order_total
-  #
-  #   @item.reverse.drop(1).each do |item|
-  #
-  #     @total << 5  if item == "burger"
-  #
-  #     @total << 10 if item == "pizza"
-  #
-  #     @total << 2 if item == "fries"
-  #
-  #     @total << 3 if item == "salad"
-  #   end
-  #
-  #   sum = 0
-  #   @total.each do |i| (sum += i)
-  #   end
-  #   p "Your order total is #{sum} pounds"
-  # end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,26 +7,4 @@ describe Menu do
   it 'will list menu items' do
     expect(menu.list_items).to include([:burger, 5], [:pizza, 10], [:fries, 2], [:salad, 3])
   end
-
-  # it 'will allow a customer to order items' do
-  #   expect(menu).to respond_to(:order_items)
-  # end
-  #
-  # it 'will store an order' do
-  #   expect(@item).to eq(nil)
-  # end
-  #
-  # context('after a customer has placed an order')
-  # it 'will print an order' do
-  #   expect { menu.order_items }.to output(@item).to_stdout
-  # end
-  #
-  # it 'will add an item to the order total' do
-  #   expect(@total).to eq(nil)
-  # end
-  #
-  # context('it can calculate the order total')
-  # it 'will print the order total' do
-  #   expect { menu.order_total }.to output("\"Your order total is 0 pounds\"\n") .to_stdout
-  # end
 end
