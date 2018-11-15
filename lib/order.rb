f45b73cfb45ba361ce8acaf1368e1fa905ce4bf9
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-15 10:23:32 +0000

    update merge

--- lib/order.rb
+++ lib/order.rb
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-
-class Order
-
-  attr_reader :list_of_dishes
-
-  def initialize
-    @list_of_dishes = [{ "Roasted stuffed cauliflower" => 13},
-                       { "Stuffed pumpkin" => 12},
-                       { "Vegan chilli" => 15},
-                       { "Vegan brownies" => 8}]
-  end
-
-  def dish
-    @list_of_dishes
-  end
-
-=======
 class Order
 
   def initialize(list_of_dishes = [{dish_name: nil, dish_price: nil}])
@@ -41,5 +23,4 @@ class Order
       sum += dish[:dish_price] * hash[:quantity]
     end
   end
->>>>>>> ffee618... create test and methods for totalling the order amount
 end
