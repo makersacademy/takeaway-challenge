d545bd43169a4566c1e45204c3b37e8af7991c79
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-08 22:44:26 +0000

    Dishes have a name and a price

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,8 +1,11 @@
 describe 'User stories' do
-# As a customer
-# So that I can check if I want to order something
-# I would like to see a list of dishes with prices
-it 'so customers can order something, list dishes with prices' do
+  # # As a customer
+  # # So that I can check if I want to order something
+  # # I would like to see a list of dishes with prices
+  # it 'so customers can order something, list dishes with prices' do
+  #
+  # end
+
   
-end
+
 end
