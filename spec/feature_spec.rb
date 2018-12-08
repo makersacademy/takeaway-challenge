b279c8481b718891a169912822a86e979029d40a
Shaneil Clarke <shaneilske@yahoo.com>
2018-12-08 11:43:39 +0000

    initial upload of takeaway spec and code files

---
+++ spec/feature_spec.rb
@@ -0,0 +1,10 @@
+require 'takeaway'
+
+describe Takeaway do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+  it "list dishes" do
+
+  end
+end
