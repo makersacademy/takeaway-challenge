d5fe0ad71e8232ca9b29422517bd5c8de3b49c34
beatricecourage <bea_courage@hotmail.co.uk>
2018-12-09 16:07:41 +0000

    viewing list of items in menu

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -7,6 +7,12 @@ class Dishes
       "Eyeballs" => 25,
       "Camel Breast" => 40,
       "shoes" => 78 }
-    end
-
   end
+
+
+  def viewlist
+    puts "Beatrice's restaurant menu"
+    @menu.each { |food, price| puts "#{food}£#{price.to_s}" }
+  end
+
+end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -6,10 +6,16 @@ describe Dishes do
   it { is_expected.to respond_to(:menu)}
 
 
-  it 'allows user to see a list of dishes with prices' do
+  it 'allows user to see a dishes with prices' do
     dish = Dishes.new
     expect(dish.menu).to include({ "shoes" => 78 })
   end
+
+  it 'shows whole list of dishes' do
+    dish = Dishes.new
+    message = "Beatrice's restaurant menu\n1: Seeds £50\n2: Eyeballs £25\n3: Camel Breast £40\n4: shoes £78\n"
+    expect{ dish.viewlist }.to output(message).to_stdout
+   end
 
 
 end
