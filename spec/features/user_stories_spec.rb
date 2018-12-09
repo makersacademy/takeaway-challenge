510853f3ea2eec824b756c72181283e5b563344f
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-09 00:31:11 +0000

    Added feature test for 1st user story

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -3,9 +3,18 @@ describe 'User stories' do
   # So that I can check if I want to order something
   # I would like to see a list of dishes with prices
   it 'so customers can order something, list menu with prices' do
-  
+    dish1 = Dish.new(:butter_chicken, 4)
+    dish2 = Dish.new(:lamb_tikka, 5)
+
+    menu = Menu.new
+    menu.add(dish1)
+    menu.add(dish2)
+
+    curry_house = Takeaway.new(menu)
+
+    expect(curry_house.menu).to eq({
+      butter_chicken: 4,
+      lamb_tikka: 5
+    })
   end
-
-
-
 end
