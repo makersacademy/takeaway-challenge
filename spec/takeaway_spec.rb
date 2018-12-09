f99320b2b171484eb024632b76df1842c2c1283e
Pauline <pbaker@hotmail.co.uk>
2018-12-09 10:02:33 +0000

    correctly aligned tests

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -25,27 +25,27 @@ RSpec.describe Takeaway do
     expect(takeaway.get_total_cost).to eq 10
   end
 
-    it 'will return true or false when asked if sum is correct' do
-      takeaway.menu
-      takeaway.select_item("dish", 2)
-      takeaway.get_total_cost
-      expect(takeaway.sum_correct?).to be(true).or be(false)
-    end
+  it 'will return true or false when asked if sum is correct' do
+    takeaway.menu
+    takeaway.select_item("dish", 2)
+    takeaway.get_total_cost
+    expect(takeaway.sum_correct?).to be(true).or be(false)
+  end
 
-    it 'will raise an error if total cost is incorrect' do
-      takeaway.menu
-      takeaway.select_item("dish", 5)
-      takeaway.get_total_cost
-      allow(takeaway).to receive(:sum_correct?).and_return(false)
-      expect { takeaway.print_order }.to raise_error 'Incorrect total'
-    end
+  it 'will raise an error if total cost is incorrect' do
+    takeaway.menu
+    takeaway.select_item("dish", 5)
+    takeaway.get_total_cost
+    allow(takeaway).to receive(:sum_correct?).and_return(false)
+    expect { takeaway.print_order }.to raise_error 'Incorrect total'
+  end
 
-    it 'will print order if total price is correct' do
-      takeaway.menu
-      takeaway.select_item("dish", 5)
-      takeaway.get_total_cost
-      allow(takeaway).to receive(:sum_correct?).and_return(true)
-      expect(takeaway.print_order).to be_instance_of(String)
-    end
+  it 'will print order if total price is correct' do
+    takeaway.menu
+    takeaway.select_item("dish", 5)
+    takeaway.get_total_cost
+    allow(takeaway).to receive(:sum_correct?).and_return(true)
+    expect(takeaway.print_order).to be_instance_of(String)
+  end
 
 end
