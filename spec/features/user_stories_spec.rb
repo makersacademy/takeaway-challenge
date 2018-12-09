25e5086d4e5e261f28612f8aa875caf64b655bf4
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-09 00:20:01 +0000

    Takeaway instantiates with a menu

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,10 +1,10 @@
 describe 'User stories' do
-  # # As a customer
-  # # So that I can check if I want to order something
-  # # I would like to see a list of dishes with prices
-  # it 'so customers can order something, list menu with prices' do
-  #
-  # end
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+  it 'so customers can order something, list menu with prices' do
+  
+  end
 
 
 
