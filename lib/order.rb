12cca9cb0c9f15a60db23d0ed059cb6126d12222
Tom Russell <TR115251@hotmail.co.uk>
2018-12-09 13:58:34 +0000

    workstate

--- lib/order.rb
+++ lib/order.rb
@@ -1,13 +1,16 @@
 class Order
 
   require_relative "./menu.rb"
+  require_relative "./menu_displayer.rb"
 
-  attr_reader :total, :cart
+  attr_reader :total, :cart, :payment_type, :payment
 
-  def initialize
+  def initialize(payment_type = "card")
     @menu = Menu.new
     @cart = []
     @total = 0
+    @payment_type = payment_type
+    @payment = false
   end
 
   def add(item, amount = 1)
@@ -22,6 +25,33 @@ class Order
       amount = item.dig(:amount).to_f
       @total += (price * amount)
     }
+  end
+
+  def place
+    if @payment_type == "cash" || (@payment_type = "card"  && @payment == true)
+      send_to_kitchen(@cart)
+    elsif @payment_type == "card" || @payment == false
+      "order not payed for, please use .change_payment_type if paying cash"
+    end
+  end
+
+  def secure_payment
+    pay(@total)
+  end
+
+  def change_payment_type
+    @payment_type = "cash"
+  end
+
+  private
+
+  def  send_to_kitchen(order)
+    #sends kitchen(order) to print
+    "Thank you! Your order was placed and will be delivered before #{((Time.new.hour)+(1))}:#{(Time.new.min)}"
+  end
+
+  def pay(total)
+    @payment = true
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -21,6 +21,20 @@ describe Order do
     expect { order.add(1) }.to change { order.cart.count }.from(0).to(1)
   end
 
+  it 'should have default payment_type of card' do
+    expect(order.payment_type).to eq("card")
+  end
+
+  it 'payment defaults as false' do
+    expect(order.payment).to eq(false)
+  end
+
+  it 'wont send order to kitchen unless payment = true' do
+    expect(order.place).to eq("order not payed for, please use .change_payment_type if paying cash")
+  end
+
+
+
   # As a customer
   # So that I can verify that my order is correct
   # I would like to check that the total I have been given matches the sum of the various dishes in my order
@@ -31,13 +45,24 @@ describe Order do
 
     it 'should provide total for Order' do
       expect(order.total).to eq(20)
-
     end
 
+
+
+    it 'gives estimate for delivery' do
+      order.change_payment_type
+      expect(order.place).to eq("Thank you! Your order was placed and will be delivered before #{((Time.new.hour)+(1))}:#{(Time.new.min)}")
+    end
+
+
+end
     # As a customer
     # So that I am reassured that my order will be delivered on time
-    # I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered
+    # I would like to receive a text such as
+    # "Thank you! Your order was placed and will be delivered before 18:52"
+    # after I have ordered
 
+    context "after order placed" do
 
 
   end
