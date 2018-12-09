509fc96c33dc92829d0ff575322a5be4ccdf01eb
Shaneil Clarke <shaneilske@yahoo.com>
2018-12-09 23:43:55 +0000

    WIP - updated take_away spec with one failing test

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,22 +1,30 @@
 class TakeAway
-  attr_reader :dish, :dish_name, :selected_dishes
+  attr_reader :dish, :selected_dishes, :quantity
 
   def initialize
-    @dish = {
-    'fried chicken' => 7.00,
-    'plain rice' => 2.00,
-    'steamed vegetables' => 4.50,
-    }
+    @dish = [
+      { "fried chicken" => 7.00 },
+      { "plain rice" => 2.00 },
+      { "steamed vegetables" => 4.50 },
+      { "cheese cake" => 3.50 }
+    ]
   end
 
   def list
-    @dish.map do |key, value|
-      "#{key} - £ #{value}"
+    @dish.each do |key, value|
+      "#{key}-£ #{value}"
     end
   end
 
-  def select_dishes(*dish_name)
-  
+  def select_dishes(dish_name, quantity)
+    @dish.each do |dish|
+      raise "Selected dish not available" unless dish.key?(dish_name)
+      @quantity = quantity
+      @selected_dish = []
+      @quantity.times do
+        @selected_dish << dish_name
+      end
+    end
   end
 
 end
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -2,21 +2,29 @@ require 'take_away'
 describe TakeAway do
 
   subject(:takeaway) { described_class.new }
-  let(:dish) { double(:dish) }
-  let(:dish_name) { double(:dish_name) }
-  describe 'A TakeAway' do
-    it "knows it's dishes" do
-      expect(takeaway.dish['fried chicken']).to(be_a Float)
-    end
+  let(:dish) { double :dish}
+  let(:list) {
+    [
+      { "fried chicken" => 7.00 },
+      { "plain rice" => 2.00 },
+      { "steamed vegetables" => 4.50 },
+      { "cheese cake" => 3.50 }
+    ]
+  }
 
+  describe 'A TakeAway' do
     it "can list dishes with prices" do
       expect(takeaway).to respond_to(:list)
-      expect(takeaway.list).to be_truthy
+      expect(takeaway.list).to eq list
     end
-    
-    before { allow(takeaway).to receive(:select_dishes).with(any_args) }
-    it "selects several dishes available" do
-      expect(takeaway).to respond_to(:select_dishes).with_unlimited_arguments
+
+    it "selects dishes" do
+      expect(takeaway.select_dishes("fried chicken", 2)).to eq dish
+    end
+    context 'dish not available' do
+      it "raises an error" do
+        expect { takeaway.select_dishes("chicken", 2) }.to raise_error("Selected dish not available")
+      end
     end
 
   end
