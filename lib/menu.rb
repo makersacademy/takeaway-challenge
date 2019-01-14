39ae9af2fecfa3ca617df419d4319ca92ae9cd12
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-14 00:25:46 +0000

    Adding 2 restaurants to menu module, so the client can choose

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,27 @@
 module Menu
 
   MENU = {
-      :soup => 6.00,
-      :meat => 10.00,
-      :chicken => 8.00,
-      :bruschetta => 5.00,
-      :hummus => 5.00,
-      :halloumi => 5.00,
-      :coke => 2.00,
-      :juice => 2.00,
-      :beer => 1.50,
-      :wine => 6.00
-  }
+      :oki => {
+          :soup => 6.00,
+          :meat => 10.00,
+          :chicken => 8.00,
+          :yaksoba => 7.00,
+          :rice => 3.00,
+          :coke => 2.00,
+          :juice => 2.00,
+          :beer => 1.50,
+          :wine => 6.00
+      },
 
+      :red_lion => {
+          :fish => 5.00,
+          :chicken => 6.00,
+          :meat => 8.00,
+          :breakfast => 9.00,
+          :eggs => 5.00,
+          :ale => 2.50,
+          :coke => 2.00,
+          :juice => 1.50
+      }
+  }
 end
