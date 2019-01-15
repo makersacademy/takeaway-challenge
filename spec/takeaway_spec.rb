650632ef1cc3abbf3bfae6628e692ae617161876
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-15 07:51:38 +0000

    Add unit test and initial pass

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,15 +4,15 @@ describe Takeaway do
 
   subject(:takeaway) { described_class.new }
 
-  # As a customer
-  # So that I can check if I want to order something
-  # I would like to see a list of dishes with prices
-
   context 'customers should be able to view the menu' do
 
-    it 'should show customers a menu' do
+    it '#menu' do
       menu = ""
       expect(subject.menu).to eq(menu)
     end
+
+
+
   end
+
 end
