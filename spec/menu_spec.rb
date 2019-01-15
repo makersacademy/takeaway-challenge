2f0d41cc55c81165634032b2a034da737db09c3f
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-15 08:09:22 +0000

    Add injection of product list for menu class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,10 +2,14 @@ require 'menu'
 
 describe Menu do
 
-  subject(:menu) { described_class.new }
+  subject(:menu) { described_class.new(@list) }
+
+  before(:each) do
+    @list = {korma: 4, jalfrezi: 6, pilau: 2, naan: 2.5}
+  end
 
   it 'should show a list of items and prices' do
-    expect(menu.list).to eq({korma: 4, jalfrezi: 6, pilau: 2, naan: 2.5})
+    expect(menu.list).to eq(@list)
   end
 
 
