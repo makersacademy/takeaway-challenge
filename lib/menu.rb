f105dfa65adc2ffe0a6e8a90d0ca957da9db037f
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-15 08:10:59 +0000

    Implement injection of product list for Menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,7 @@
 class Menu
 
-  def initialize
-    @list = { korma: 4, jalfrezi: 6, pilau: 2, naan: 2.5 }
+  def initialize(list = { korma: 4, jalfrezi: 6, pilau: 2, naan: 2.5 })
+    @list = list
   end
 
   def list
