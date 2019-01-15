1372ca76bc79a54f333a6b52643a66d3d943914c
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-15 19:43:10 +0000

    WIP commit & push

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,10 +2,23 @@ class Takeaway
 
   def initialize(menu = Menu.new)
     @menu = menu
+    # TODO data structure to store basket?
+    # Either take the hash keys?
+    # Or make the array up on the fly?
+    # But doesn't allow for backtracking orders...
+
   end
 
   def menu
     @menu.list
+  end
+
+  def order(dish, quantity = 1)
+
+  end
+
+  def basket
+
   end
 
 end
