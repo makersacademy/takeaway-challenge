7084cff8c2f38e0eae4393a02d1757c32870b9d9
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-16 07:39:43 +0000

    Test for specifying quantity when ordering

--- spec/units/takeaway_spec.rb
+++ spec/units/takeaway_spec.rb
@@ -36,10 +36,25 @@ describe Takeaway do
 
     it 'should add dishes to the basket' do
       takeaway.order('korai')
+      expect(takeaway.basket).to eq(['korai'])
+    end
+
+    it 'should add multiple dishes to the basket' do
+      takeaway.order('korai')
+      takeaway.order('korma')
+      expect(takeaway.basket).to eq(['korai', 'korma'])
+    end
+
+    it 'should allow quantities to be specified when ordering' do
+      takeaway.order('korai', 2)
+      takeaway.order('korma', 2)
+      expect(takeaway.basket).to eq(['korai', 'korai', 'korma', 'korma'])
+    end
+
+    xit 'placeholder' do
       # takeaway.order('vindaloo', 2)
       # takeaway.order('korma')
       # expect(takeaway.basket).to eq('korai x 1 (£3.5), vindaloo x 2 (£8), korma x 1 (£3)')
-      expect(takeaway.basket).to eq(['korai'])
     end
 
     xit 'should refuse orders that are not on the menu' do
