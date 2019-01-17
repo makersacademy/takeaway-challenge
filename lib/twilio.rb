91d4e011003d6fdf7491be5ea2259842a0518043
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-17 23:25:03 +0000

    Added dotenv gem and edited twilio.rb

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -6,11 +6,11 @@ account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = ENV['TWILIO_PHONE_NUMBER'] # Your Twilio number
-to = ENV['MOBILE'] # Your mobile phone number
+from = ENV['TWILIO_PHONE_NUMBER'] 
+to = ENV['MOBILE'] 
 
 client.messages.create(
-from: SUBGENIUS CORP,
+from: ENV['TWILIO_PHONE_NUMBER'],
 to: ENV['MOBILE'],
 body: "Your order has been received! It will be delivered in 1 hour"
 )
