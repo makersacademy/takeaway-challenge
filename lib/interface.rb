8742ac790348ecd44e8c7c748a03fe1efea54701
habakkukyong <habakkukyong@gmail.com>
2019-01-18 12:48:38 +0000

    Add select_dish

--- lib/interface.rb
+++ lib/interface.rb
@@ -4,12 +4,17 @@ require_relative 'menu'
 class Interface
   def initialize(menu = Menu.new)
     @menu = menu
+    @selected_dishes = {}
   end
 
   def see_list
     output = []
     menu.items.each { |k, v| output << k + ", " + v.to_s }
     output.join("\n")
+  end
+
+  def select_dish(dish, quantity = 1)
+    @selected_dishes[dish] = quantity
   end
 
   private
--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -4,9 +4,29 @@ describe Interface do
   let(:menu) { double :menu, :items => ({ "Pizza" => 15, "Goat" => 22 }) }
 
   describe "#see_list" do
-    it 'prints a menu' do
+    it 'returns a menu' do
       interface = Interface.new(menu)
       expect(interface.see_list).to eq "Pizza, 15\nGoat, 22"
+    end
+  end
+
+  describe "#select_dish" do
+    it 'stores an argument in selected dishes' do
+      interface = Interface.new
+      interface.select_dish("Pizza")
+      expect(interface.instance_variable_get(:@selected_dishes)).to eq ({ "Pizza" => 1 })
+    end
+
+    it 'accepts a second parameter which it also stores' do
+      interface = Interface.new
+      interface.select_dish("Pizza", 3)
+      expect(interface.instance_variable_get(:@selected_dishes)).to eq ({ "Pizza" => 3 })
+    end
+  end
+
+  describe "#verify_order" do
+    it 'returns a list showing name, quantity & total price' do
+      
     end
   end
 end
