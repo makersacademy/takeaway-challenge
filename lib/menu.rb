a6aec536f2013ab62f7f92ddc5e2aaafa3f334b7
isabelcooper <isabel@triptease.com>
2019-01-18 11:18:53 +0000

    menu call formatter to return formatter list

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,19 +1,24 @@
 require_relative 'item'
+require_relative 'formatter'
 
 class Menu
 
   attr_reader :names, :items
 
-  def initialize(names, item = Item)
+  def initialize(names, item = Item, formatter = Formatter)
     @names = names
     @items = create_items(item)
+    @formatter = formatter.new
   end
+
+  def print_menu
+    @formatter.format_items(@items)
+  end
+
+  private
 
   def create_items(item)
     @items = @names.map{ |name| item.new(name) }
   end
 
-  # def list_items
-  #   @items.each{ |item| item.name }
-  # end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,11 @@
 require 'menu'
 
 describe Menu do
-   let(:item_fish)       { double(:item, name: "fish") }
+   let(:item_fish)   { double(:item ) }
    let(:item_class)  { double(:item_class, new: item_fish) }
-   let(:subject)     { Menu.new(["fish"], item_class)}
+   let(:formatter)    { double(:formatter, format_items: "Fish --- 8") }
+   let(:formatter_class)   { double(:formatter_class, new: formatter) }
+   let(:subject)     { Menu.new(["fish"], item_class, formatter_class)}
 
   it 'should initialize with a list of item names' do
     expect(subject.names).to eq ["fish"]
@@ -13,7 +15,12 @@ describe Menu do
     expect(subject.items).to eq [item_fish]
   end
 
-  # it 'should return a list of items' do
-  #   expect(subject.list_items).to eq ["fish", "pizza", "pasta", "cake"]
-  # end
+  it 'should run formatter' do
+    expect(formatter).to receive(:format_items)
+    subject.print_menu
+  end
+
+  it 'should return a formatted menu' do
+    expect(subject.print_menu).to eq "Fish --- 8"
+  end
 end
