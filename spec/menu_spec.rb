a1903dae33a50e1d4dd59d4628ee9b4e8497251d
Simon Green <simon.green@thielsen.co.uk>
2019-01-18 12:03:38 +0000

    Added csv reader test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,14 @@
-require "menu"
+require 'menu'
 
 RSpec.describe Menu do
-  it "returns a list of dishes and prices"   do
+  let(:menu) { 'menu.csv' }
+
+  it 'returns a list of dishes and prices' do
     expect(subject.list).to be_a(Hash)
   end
+
+  it 'reads in menu and converts to hash' do
+    expect { subject.read_menu(menu) }.to change { subject.list }
+  end
+
 end
