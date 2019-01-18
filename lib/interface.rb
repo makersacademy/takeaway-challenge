131a10797f3c40034408042557428f331854efa1
habakkukyong <habakkukyong@gmail.com>
2019-01-18 13:47:59 +0000

    Add comments describing methods

--- lib/interface.rb
+++ lib/interface.rb
@@ -7,16 +7,25 @@ class Interface
     @selected_dishes = {}
   end
 
+  # Returns an array of arrays containg a string
+  # naming the dish and an integer - the price
   def see_list
     output = []
     menu.items.each { |key, value| output << [key, value] }
     output
   end
 
+  # Accepts a dish name as a string and an
+  # integer which is the quantity and stores
+  # these in the instance variable
+  # @selected_dishes which is a hash
   def select_dish(dish, quantity = 1)
     @selected_dishes[dish] = quantity
   end
 
+  # Returns an array of arrays containing a string
+  # naming the dish, the quantity, the total price
+  # for that quantity
   def verify_order
     output = []
     selected_dishes.each { |dish, quantity| output << [dish, quantity] }
@@ -28,6 +37,8 @@ class Interface
 
   attr_reader :menu, :selected_dishes
 
+  # Takes a dish and its quantity and returns the
+  # total price for that quantity
   def price(dish, quantity)
     menu.items[dish] * quantity
   end
