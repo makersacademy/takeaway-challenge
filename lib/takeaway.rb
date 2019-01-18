1b08991772dc8be93875756b48fe6c0addba0b83
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-18 11:39:09 +0000

    add items to basket from takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,15 +1,21 @@
 require_relative 'menu'
+require_relative 'basket'
 
 class Takeaway
 
-  attr_reader :menu
+  attr_reader :menu, :basket
 
-  def initialize(menu = Menu)
+  def initialize(menu = Menu, basket = Basket)
     @menu = menu.new
+    @basket = basket.new
   end
 
   def print_menu
     @menu.print_menu
+  end
+
+  def add_to_basket(dish_number)
+    @basket.add(@menu.list[dish_number])
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,14 +1,20 @@
 require 'takeaway'
 
 describe Takeaway do
-  let(:menu) { double(:menu, print_menu: true) }
+  let(:menu) { double(:menu, print_menu: true, list: ["pizza", "chips"]) }
   let(:menu_class) { double(:menu_class, new: menu) }
-  let(:takeaway) { Takeaway.new(menu_class) }
+  let(:basket) { double(:basket, add: true)}
+  let(:basket_class) { double(:basket_class, new: basket) }
+  let(:takeaway) { Takeaway.new(menu_class, basket_class) }
 
   describe '#initialize' do
 
     it 'should initialize with a menu' do
       expect(takeaway.menu).to eq menu
+    end
+
+    it 'should initialize with a basket' do
+      expect(takeaway.basket).to eq basket
     end
   end
 
@@ -18,6 +24,15 @@ describe Takeaway do
       expect(menu).to receive(:print_menu)
       takeaway.print_menu
     end
+  end
+
+  describe '#basket' do
+
+    it 'add dishes to the basket' do
+      expect(basket).to receive(:add)
+      takeaway.add_to_basket(1)
+    end
+
   end
 
 end
