fad1542ef3023e7c3a18e13ba179bb8b5fde93bf
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-18 11:05:02 +0000

    add basket class

---
+++ spec/basket_spec.rb
@@ -0,0 +1,17 @@
+require 'basket'
+
+describe Basket do
+
+  describe '#store' do
+
+    it 'should store dishes in the basket' do
+
+    end
+
+    it 'should start with an empty basket' do
+      expect(subject.basket).to be_empty
+    end
+
+  end
+
+end
