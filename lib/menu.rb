da3a7cbca5533ea2742716a3c6eaf9a3d65bec8e
isabelcooper <isabel@triptease.com>
2019-01-18 10:55:05 +0000

    menu creating item objects on initialization based on names provided

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,19 @@
+require_relative 'item'
+
 class Menu
 
-  def read_menu
-    @items.each
+  attr_reader :names, :items
+
+  def initialize(names, item = Item)
+    @names = names
+    @items = create_items(item)
   end
+
+  def create_items(item)
+    @items = @names.map{ |name| item.new(name) }
+  end
+
+  # def list_items
+  #   @items.each{ |item| item.name }
+  # end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,19 @@
-# require 'menu'
-#
-# describe Menu do
-#
-#   it 'should return a list of items with prices' do
-#     expect(subject.read_menu).to eq "tbc"
-#   end
-# end
+require 'menu'
+
+describe Menu do
+   let(:item_fish)       { double(:item, name: "fish") }
+   let(:item_class)  { double(:item_class, new: item_fish) }
+   let(:subject)     { Menu.new(["fish"], item_class)}
+
+  it 'should initialize with a list of item names' do
+    expect(subject.names).to eq ["fish"]
+  end
+
+  it 'should create items on initialization' do
+    expect(subject.items).to eq [item_fish]
+  end
+
+  # it 'should return a list of items' do
+  #   expect(subject.list_items).to eq ["fish", "pizza", "pasta", "cake"]
+  # end
+end
