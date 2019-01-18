fc61282b5a757752c7a4cfd520d73e38d8987589
Simon Green <simon.green@thielsen.co.uk>
2019-01-18 10:31:42 +0000

    Started thinking through user stories

---
+++ spec/functional/user_stories_spec.rb
@@ -0,0 +1,25 @@
+# nouns: list, dishes, prices, meal, total, text
+
+# list = object Menu
+# dish = subset of Menu
+# meal = chosen items
+# total =  total price
+# takeaway = main program
+# text = text object
+
+require 'Takeaway'
+
+RSpec.describe 'Functional tests' do
+let(:pieshop) { Takeaway.new }
+let(:menu) {}
+
+# As a customer
+# So that I can check if I want to order something
+# I would like to see a list of dishes with prices
+
+  it 'shows list of dishes with prices' do
+    expect(pieshop.list_menu).to eq menu
+  end
+
+
+end
