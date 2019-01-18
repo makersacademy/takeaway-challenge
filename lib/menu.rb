a9425ccd6adc3618d46c2597797c5fd4fbcd6096
jesslns <jesslns@yahoo.com.hk>
2019-01-18 11:43:52 +0000

    menu has dish and price

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,12 @@
 class Menu
+
+attr_reader :food
+
+def initialize
+  @food = { pizza: 8, pasta: 7}
+end
+
+def print
+end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,24 @@
 require 'menu'
 
 describe Menu do
-  
+
+subject(:menu) { Menu.new }
+
+context 'contains information on dishes and price' do
+  it 'does pizza' do
+    expect(menu.food[:pizza]).to eq 8
+  end
+
+  it 'does pasta' do
+    expect(menu.food[:pasta]).to eq 7
+  end
+end
+
+context 'list dish with price' do
+  it { is_expected.to respond_to :print}
+
+  it "shows pizza £8, pasta £7" do
+    expect(menu.print).to eq "pizza £8, pasta £7"
+  end
+end
 end
