9f5dbd15657a07c3f1c553e6ce820f4b96b32e2e
Simon Green <simon.green@thielsen.co.uk>
2019-01-18 12:23:10 +0000

    Updated menu input

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,15 @@
+require 'csv'
+
 class Menu
+
+  def initialize(menu_csv)
+    data = CSV.read(menu_csv, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+    hashed_data = data.map { |d| d.to_hash }
+    p hashed_data
+  end
+
   def list
     {"Vegetable pie"=>2, "Lamb pie"=>3, "Steak pie"=>3, "Kangaroo pie"=>4, "Dog pie"=>1, "Monkey pie"=>5, "Whale pie"=>6}
   end
-  def read_menu(menu_csv)
-    
-  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,14 +1,15 @@
 require 'menu'
 
 RSpec.describe Menu do
-  let(:menu) { 'menu.csv' }
+  let(:menu_file) { 'menu.csv' }
+  let(:new_menu) { described_class.new(menu_file) }
 
   it 'returns a list of dishes and prices' do
-    expect(subject.list).to be_a(Hash)
+    expect(new_menu.list).to be_a(Hash)
   end
 
   it 'reads in menu and converts to hash' do
-    expect { subject.read_menu(menu) }.to change { subject.list }
+    expect { new_menu }.to change { subject.list }
   end
 
 end
