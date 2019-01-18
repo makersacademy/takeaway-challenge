376d63f26eaa10ec90b0f77d5935e66a087cc694
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-18 11:50:05 +0000

    tiny refactoring

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,8 +2,7 @@ require 'menu'
 
 describe Menu do
   it "should load menu from csv file" do
-    dishes = Menu.new
-    dishes.load_menu
-    expect(dishes.menu).to_not eq nil
+    subject.load_menu
+    expect(subject.menu).to_not eq nil
   end
 end
