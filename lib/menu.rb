f29d216853a36271057615fee41f3947de078c68
Paul <paul.ayling@bglgroup.co.uk>
2019-01-18 10:35:23 +0000

    Functional print_menu method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,24 @@
+class Menu
+  attr_reader :dishes
+
+  def initialize(dishes)
+    @dishes = dishes
+  end
+
+  def print_menu
+    dishes.map do |food, price|
+      "%s £%.2f" % [food.to_s.capitalize, price]
+    end.join(", ")
+  end
+
+
+end
+
 # As a customer
 # So that I can check if I want to order something
-# I would like to see a list of dishes with prices
+# # I would like to see a list of dishes with prices
+# dishes.map do |title, price|
+#
+#
+# dish =
+#   { "Chicken": 3.00 }
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,25 @@
 require 'menu'
 
-describe '#Print Menu' do
-  it 'Should print a list of dishes with relevant prices' do
-    (subject.print_menu).to eq "Kebab - £1.00"
+describe Menu do
+  subject(:burger_menu) { described_class.new(dishes) }
+  let(:dishes) do
+    {
+      chicken: 2.99,
+      beef: 3.49,
+      veggie: 1.99,
+      fries: 1.50
+    }
+  end
+
+  describe '#new' do
+    it 'Initialises with argument dishes' do
+      expect(burger_menu.dishes.count).to eq 4
+    end
+  end
+
+  describe '#Print Menu' do
+    it 'Should print a list of dishes with relevant prices' do
+      expect(burger_menu.print_menu).to eq "Chicken £2.99, Beef £3.49, Veggie £1.99, Fries £1.50"
+    end
   end
 end
