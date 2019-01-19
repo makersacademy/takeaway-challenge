d094391325eb88ca8d4d7d775dd8d276b270ba58
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-19 13:20:35 +0000

    added a defualt order of [] to be returned and tested

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,11 @@
 class Order
 
+  def initialize
+    @order = []
+  end
+
+  def return_order
+    @order
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,8 @@ require 'order'
 
 describe Order do
 
+  it "should by default return an empty order" do
+    expect(subject.return_order).to eq []
+  end
+
 end
