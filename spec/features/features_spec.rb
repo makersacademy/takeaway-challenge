729f832ef7ce383047c8fa15e528f7edfbed6c24
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-19 11:20:52 +0000

    first feature test described, first Restauranta method test written

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe "Features and user stories:" do
+
+
+
+# As a customer
+# So that I can check if I want to order something
+# I would like to see a list of dishes with prices
+
+it 'so that a customer can order, display a menu for their options' do
+  restaurant = Restaurant.new
+  menu = Menu.new
+  expect { restaurant.display_menu(menu) }.not_to raise_error
+end
+
+# As a customer
+# So that I can order the meal I want
+# I would like to be able to select some number of several available dishes
+#
+# As a customer
+# So that I can verify that my order is correct
+# I would like to check that the total I have been given matches the sum of the various dishes in my order
+#
+# As a customer
+# So that I am reassured that my order will be delivered on time
+# I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered
+end
