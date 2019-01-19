f6c72e32e8618209f5b15bdb7df69abc862a53d1
William Dunk <williamdunk@gmail.com>
2019-01-19 15:32:52 +0000

    added #list_dishes method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,17 @@
 class Menu
 
-  attr_reader :dishes
+  attr_reader :dishes, :dish_list
 
   def initialize(dishes)
     @dishes = dishes
+    @dish_list = []
+  end
+
+  def list_dishes
+    @dishes.each do |hash|
+       @dish_list << "#{hash[:item]} - £#{hash[:price]}"
+    end
+    puts @dish_list
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,8 +3,8 @@ require 'menu'
 describe Menu do
 
   before(:each) {
-    ppp_menu = [{:item => "pizza", :price => 10}, {:item => "pasta", :price => 12}, {:item => "pancakes", :price => 6}]
-    @ppp_cafe = Menu.new(ppp_menu)
+    @ppp_menu = [{:item => "pizza", :price => 10}, {:item => "pasta", :price => 12}, {:item => "pancakes", :price => 6}]
+    @ppp_cafe = Menu.new(@ppp_menu)
   }
 
   # As a customer
@@ -12,6 +12,15 @@ describe Menu do
   # I would like to see a list of dishes with prices
   it 'has a list of dishes' do
     expect(@ppp_cafe.dishes.length).to be > (-1)
+  end
+
+  it 'responds to #list_dishes' do
+    expect(@ppp_cafe).to respond_to(:list_dishes)
+  end
+
+  it 'can list dishes' do
+    @ppp_cafe.list_dishes
+    expect(@ppp_cafe.dish_list[0]).to eq "#{@ppp_menu[0][:item]} - £#{@ppp_menu[0][:price]}"
   end
 
 end
