068a8f5c03852e3b962645ecad1b0dcc5e1e67d0
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-01-19 15:10:40 +0000

    implemented twilio and feature tested. Removed personal number for pushing purposes

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,14 @@
 require_relative 'menu'
+require_relative 'send_sms'
 
 class Order
 
 attr_reader :food_order, :menu
 
-  def initialize(menu = Menu.new)
+  def initialize(menu = Menu.new, message= Sendsms.new)
     @menu = menu
     @food_order = []
+    @message = message
   end
 
   def order_food(food, quantity)
@@ -31,5 +33,6 @@ attr_reader :food_order, :menu
 
   def checkout(price)
   raise "Incorrect total cost!" if !correct_cost(price)
+  @message.send_message("Order complete! Your food is being prepared and will be with you shortly!")
 end
 end
