58fcc7a5feb32179d1bbb507e2c81ce2e6222a6e
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-19 15:14:14 +0000

    Added method select_dish - passing the test

--- lib/Menu.rb
+++ lib/Menu.rb
@@ -4,10 +4,15 @@ class Menu
 
   def initialize(food_we_offer = { burger: 5, chips: 2 })
     @food_we_offer = food_we_offer
+    @your_order = []
   end
 
   def list_of_dishes
     @food_we_offer
+  end
+
+  def select_dish(dish, quantity)
+    @your_order.push({dish => quantity})
   end
 
 
