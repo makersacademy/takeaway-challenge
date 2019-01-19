a1ddbfaf7d1b928e49a5455efcc9ca8d2d626f5d
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-01-19 11:47:17 +0000

    refactored menu hash to be outside an array

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,10 +3,10 @@ class Menu
 attr_reader :display_menu
 
   def food_menu
-     [{ 'Pizza' => 6.00 },
-      { 'Burger' => 7.00 },
-      { 'Katsu Wrap' => 4.50},
-      { 'Curry' => 5.50 }]
+      {'Pizza' => 6.00,
+      'Burger' => 7.00,
+      'Katsu Wrap' => 4.50,
+      'Curry' => 5.50 }
     end
 
     def display_menu
