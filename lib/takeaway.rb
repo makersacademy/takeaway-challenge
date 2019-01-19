2f4ebd25947f777378d6839377e8c67f97262c98
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-19 19:36:18 +0000

    Formatting

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -8,6 +8,7 @@ class Takeaway
   ERROR = "Please enter the exact sum"
   TIME = Time.new
   MESSAGE = "Success! Your order was placed and will be delivered before #{(TIME.hour + 1) < 10 ? "0" : ""}#{TIME.hour + 1}#{TIME.min < 10 ? "0" : ""}#{TIME.min}"
+  
   def initialize(menu = Menu.new, order = Order.new, text = Text.new)
     @menu = menu
     @order = order
