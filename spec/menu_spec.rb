98cd12f293645ae47d4ebaa11d97072103b503ed
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-01-19 11:54:48 +0000

    refactored test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,6 +3,6 @@ require 'menu'
 RSpec.describe Menu do
 
   it "Should display the menu" do
-  expect(subject.display_menu).to eq (p subject.food_menu)
+  expect(subject.display_menu).to eq subject.food_menu
   end
 end
