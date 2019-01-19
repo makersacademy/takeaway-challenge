7da5f4d701ca0f232ced66cb438aff029975d75d
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-19 13:54:27 +0000

    Test: list of dishes exist, list includes dishes and prices

---
+++ spec/Menu_spec.rb
@@ -0,0 +1,16 @@
+require 'Menu'
+
+
+RSpec.describe Menu do
+  subject(:menu) { menu = Menu.new }
+
+  it 'checks whether list of dishes exist' do
+    expect(menu).to respond_to(:list_of_dishes)
+  end
+
+  it 'checks whether list includes dishes and prices' do
+    expect(menu.list_of_dishes).to eq(menu.food_we_offer)
+  end
+
+
+end
