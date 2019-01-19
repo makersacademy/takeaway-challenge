c420e3b576adef5d3f96cacd4854f8eb9b791be0
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-19 19:05:08 +0000

    dependency injection of Menu class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,7 +1,11 @@
 require_relative 'menu'
 
 class Restaurant
+  attr_reader :menu
+  def initialize
+    @menu = Menu.new
+  end
   def show_menu
-
+    puts @menu
   end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,10 +2,15 @@ require 'restaurant'
 
 describe Restaurant do
   subject(:takeaway) { described_class.new }
+
+  describe 'Class access:' do
+    it 'creates a new instance of a menu on initialize' do
+      expect(takeaway.menu).to be_an_instance_of(Menu)
+    end
+  end
   describe 'Display requests:' do
     it 'responds to a request to display a restaurant menu' do
       expect(takeaway).to respond_to(:show_menu)
     end
-
   end
 end
