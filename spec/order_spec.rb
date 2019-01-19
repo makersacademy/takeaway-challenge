2d91653be16d254ba09ffa23898db43da3641bb7
William Dunk <williamdunk@gmail.com>
2019-01-19 17:01:40 +0000

    added order quantity total

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,7 +3,7 @@ require 'order'
 describe Order do
 
   before(:each) {
-    @ppp_items = [2, "pizza", 1, "pasta"]
+    @ppp_items = [[2, "pizza", 1, "pasta"], 3]
     @ppp_order = Order.new(@ppp_items)
   }
 
@@ -12,4 +12,10 @@ describe Order do
     # dbl_order = double("order", items: [2, "pizza", 1, "pasta"])
     expect(@ppp_order.items).to eq @ppp_items
   end
+
+  # As a customer
+  # So that I can verify that my order is correct
+  # I would like to check that the total I have been given matches the sum of the various dishes in my order
+
+
 end
