5bc902b91a1744752a0b24812a2ada01909e2f92
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-01-19 13:10:07 +0000

    created a order array & doubles test - incomplete user story2

---
+++ lib/order.rb
@@ -0,0 +1,14 @@
+require_relative 'menu'
+
+class Order
+
+attr_reader :food_order
+
+  def initialize
+    @food_order = []
+  end
+
+  def order_food(food, quantity)
+    @food_order << {:food => food, :quantity => quantity}
+  end
+end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,10 +1,11 @@
 require 'order'
 
-Rspec.describe Order do
+RSpec.describe Order do
 
   let(:food) { double :food }
+  let(:quantity) { double :quantity }
 
   it "orders food from the menu" do
-  expect (subject.order_food(food)). to eq food
+  expect(subject.order_food(food, quantity)).to eq [ {:food => food, :quantity => quantity }   ]
   end
 end
