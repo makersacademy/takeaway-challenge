ab07c549bb92c8552dbbe6b94760894c635d5cad
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-20 19:45:33 +0000

    Menu_spec test changes to use mocks & doubles

--- spec/Menu_spec.rb
+++ spec/Menu_spec.rb
@@ -1,18 +1,30 @@
 require 'Menu'
 
 RSpec.describe Menu do
-  subject(:menu) { described_class.new }
+  let(:menu) { described_class.new }
 
-  it 'checks whether list of dishes exist' do
-    expect(menu).to respond_to(:list_of_dishes)
-  end
+  context 'list of dishes' do
+    it 'checks whether list of dishes exist' do
+      expect(menu).to respond_to(:list_of_dishes)
+    end
 
-  it 'checks whether list includes dishes and prices' do
-    expect(menu.list_of_dishes).to eq(menu.food_we_offer)
-  end
+    it 'checks whether list includes dishes and prices' do
+      expect(menu.list_of_dishes).to eq(menu.food_we_offer)
+    end
 
-  it 'allows to select number of dishes' do
-    expect(menu).to respond_to(:select_dish).with(2).arguments
+    it 'responds to dishes selector' do
+      expect(menu).to respond_to(:select_dish).with(2).arguments
+    end
+
+    it 'allows to select a number of dishes' do
+      food_we_offer = double('food on menu')
+      your_order = double('order')
+      dish = double('dish')
+      quantity = double('quantity')
+      expect(your_order).to receive(:push)
+      test_menu = Menu.new(food_we_offer, your_order)
+      test_menu.select_dish(dish, quantity)
+    end
   end
 
 end
