0b58dc4f246abc04043166339634505c88613fbe
Tameka <tamekasmall@hotmail.com>
2019-01-20 11:04:16 +0000

    Test: User story 3 (WIP):sum matches the exact total

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,21 @@
 require 'takeaway'
 
-RSpec.describe Takeaway do
+describe Takeaway do
   # let (:menu_list) { double :menu_list }
 
   it 'shows a list of dishes with prices' do
-  expect(subject.menu).to_not be_empty
+    expect(subject.menu).to_not be_empty
   end
+
+  describe '#menu' do
+    it 'to select dishes and number of dishes from menu' do
+      expect(subject).to respond_to(:order).with(2).arguments
+  end
+
+  it 'to check the sum amount matches the exact total of various dishes' do
+    subject.order("cola", 1)
+    subject.order("salad", 1)
+      expect(subject.exact_total).to eq(subject.customer_total(7))
+  end
+end
 end
