77388ed1097ea4786a1e9754c21f0f017748dde5
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-20 10:30:49 +0000

    Takeaway prints menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,9 @@
 class Takeaway
+  def initialize(menu)
+    @menu = menu
+  end
+
+  def print_menu
+    @menu.print_menu
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,4 +1,12 @@
 require 'takeaway'
 
 RSpec.describe Takeaway do
+  let(:menu) { double :menu, dishes: menu_items, print_menu: menu_items }
+  let(:menu_items) {{ "Classic Chicken": 9.95 }}
+
+  subject(:takeaway) { described_class.new(menu)}
+
+  it 'prints the menu' do
+    expect(takeaway.print_menu).to eq menu.print_menu
+  end
 end
