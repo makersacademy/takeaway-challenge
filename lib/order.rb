6fa87ddc83708cdc8334d753808d456dc9be7282
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-20 11:50:25 +0000

    order now has the ability to have quanitity as well as working out the total price

--- lib/order.rb
+++ lib/order.rb
@@ -1,20 +1,32 @@
+require_relative 'pizza'
+
 class Order
 
-attr_reader :order
+attr_reader :basket, :price
 
-  def initialize
-    @order = []
+  def initialize(pizza_class = Pizza)
+    @basket = []
+    @confirmation_of_order = []
+    @menu = pizza_class
+    @price = 0
   end
 
-  def add_to_order(item)
-    @order << item
+  def add_to_order(item, quantity)
+    quantity.times { @basket << item }
     return_order
+  end
+
+  def total_price
+    @basket.each do |item|
+      @price += @menu.new.view_food_and_price[item.to_sym].to_f
+    end
+    @price
   end
 
 private
 
   def return_order
-    @order
+    @basket
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,15 +1,29 @@
 require 'order'
 
 describe Order do
-  let(:item) { double{:item} }
+  let(:item) { double(:item, to_sym: :item) }
+  let(:price) { double(:price) }
 
-  it "should by default return an empty order" do
-    expect(subject.order).to eq []
+  it "should by default return an empty basket" do
+    expect(subject.basket).to eq []
   end
 
-  it "should store an item which has been added to the order" do
-    subject.add_to_order(:item)
-    expect(subject.order).to eq [:item]
+  it "should store an item which has been added to the basket" do
+    subject.add_to_order(:item, 1)
+    expect(subject.basket).to eq [:item]
   end
 
+  it "should add the item the quantity number of times" do
+    expect(subject.add_to_order(item, 2)).to eq [item, item]
+  end
+
+  it "should return the full price of the order" do
+    subject.add_to_order(item, 1)
+    expect { subject.total_price }.to change { subject.price }
+  end
+
+  it "should return the price once calcualted" do
+    subject.add_to_order(item, 1)
+    expect(subject.total_price).to be_an_instance_of Float
+  end
 end
