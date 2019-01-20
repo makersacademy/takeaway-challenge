3e770714c45fdd3e45b11c253d8bcf9ec2e46363
Debora <debora.piu92@gmail.com>
2019-01-20 00:19:55 +0000

    place_order method returns total cost

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,26 +2,25 @@ require_relative 'menu'
 
 class Takeaway
 
-  attr_reader :menu, :new_order_list
+  attr_reader :menu, :new_order_list, :total_order
 
   def initialize(menu = Menu.new)
     @menu = menu
     @new_order_list = Array.new
+    @total_order = 0
   end
 
   def show_menu
-    return @menu.to_string
+    @menu
   end
 
   def place_order(quantity, dish_wanted)
-    @new_order_list.push({ quantity => dish_wanted })
+    @new_order_list.push({quantity => dish_wanted})
+    update_total_order(quantity, dish_wanted)
   end
-end
 
-# takeaway = Takeaway.new
-# takeaway.menu.add("Dough Balls", 4.49)
-# takeaway.menu.add("Cheeseburger", 6.79)
-# takeaway.menu.add("Garlic Pizza Bread", 3.99)
-# takeaway.menu.add("Chicken Wings", 5.99)
-# takeaway.menu.add("Pizza Margherita", 8.99)
-# p takeaway.show_menu
+  def update_total_order(quantity, dish_wanted)
+    sprintf('%.2f', (@total_order += (quantity * @menu.menu[dish_wanted]))).to_f
+  end
+
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,24 +4,18 @@ RSpec.describe Takeaway do
 
   context "#show_menu" do
     it "should print the menu" do
-      subject.menu.add("Burger", 6.50)
-      expect(subject.show_menu).to eq "Burger at £6.50\n"
+      expect(subject.show_menu).to be_kind_of Menu
     end
   end
 
   context "#place_order" do
     it "should return a hash of quantity and Dish instance" do
-      subject.menu.add("Burger", 6.50)
-      subject.menu.add("Cheeseburger", 5.49)
-      expect(subject.place_order(5, "Cheeseburger")).to eq([{ 5 => "Cheeseburger" }])
+      expect(subject.place_order(5, "Cheeseburger")).to eq(33.95)
     end
 
     it "should return multiple orders in a hash" do
-      subject.menu.add("Pizza", 8.50)
-      subject.menu.add("Cheeseburger", 5.49)
       subject.place_order(2, "Cheeseburger")
-      subject.place_order(2, "Pizza")
-      expect(subject.new_order_list).to eq([{ 2 => "Cheeseburger" }, { 2 => "Pizza" }])
+      expect(subject.place_order(2, "Chicken Wings")).to eq(25.56)
     end
   end
 end
