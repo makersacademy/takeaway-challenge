774c3216a66c9cca651a73790e5b09a63abc9535
Jiseong Park <41450070+JPProgrammingTime@users.noreply.github.com>
2019-01-20 21:21:30 +0000

    Removed unnecessary comments from the send_sms file.

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -3,15 +3,15 @@ require 'dotenv'
 
 Dotenv.load
 
-# This code below creates a new instance of the Message resource and sends an HTTP POST to the Messages resource URI.
+
 class SMSMessage
 
   def initialize
     @account_sid = ENV["TWILIO_ACCOUNT_SID"]
     @auth_token = ENV["TWILIO_ACCOUNT_AUTH_TOKEN"]
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
-    @from = ENV["FROM_NUMBER"] # Your Twilio number
-    @to = ENV["TO_NUMBER"] # Your mobile phone number
+    @from = ENV["FROM_NUMBER"]
+    @to = ENV["TO_NUMBER"]
   end
 
   def send_sms_message(total_order, total_price)
