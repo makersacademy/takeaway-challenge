75a7f43bed25839cee1f7e71d0cbf58468cd91c2
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 22:06:16 +0000

    Refactor: menu_items.each to private method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,22 @@
 class Menu
   attr_reader :menu_items
 
-  FOOD = {pizza: 2.99, burger: 3.50, burrito: 4.50, 
-  lobster: 50.00}
+  FOOD = { pizza: 2.99, burger: 3.50, burrito: 4.50, 
+  lobster: 50.00 }
 
   def initialize(menu_items = FOOD)
     @menu_items = menu_items
   end
 
   def print
-    puts "Takeout Menu"
-    @menu_items.each {|food, price| puts "#{food.capitalize} £#{'%.2f' % price}" }
+    puts "Takeout Menu".center(30)
+    formatted_menu
   end
 
+  private
+  def formatted_menu
+    @menu_items.each do 
+      |food, price| puts "#{food.capitalize} £#{'%.2f' % price}".center(30)
+    end 
+  end
 end
