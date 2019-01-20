1526a67c6d15f231bb612a9150422974ecef08f7
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-20 21:01:15 +0000

    Further test refactoring

--- spec/Restaurant_spec.rb
+++ spec/Restaurant_spec.rb
@@ -1,19 +1,25 @@
 require 'Restaurant'
 
 RSpec.describe Restaurant do
-  subject(:restaurant) { described_class.new }
+  let(:restaurant) { described_class.new }
 
-  it 'total bill matches the sum of the various dishes in my order' do
-    3.times { restaurant.menu.select_dish('burger', 1) }
-    expect(restaurant.bill).to eq(15)
-  end
+  context "Bill and confirmation" do
+    it 'total bill matches the sum of the various dishes in my order' do
+      3.times { restaurant.menu.select_dish('burger', 1) }
+      expect(restaurant.bill).to eq(15)
+    end
 
-  it 'will raise an error if meal is not selected' do
-    expect { restaurant.place_your_order }.to raise_error('Please select your meal')
-  end
+    it 'will raise an error if meal is not selected' do
+      expect { restaurant.place_your_order }.to raise_error('Please select your meal')
+    end
 
-  it 'places an order and message is being sent to customer' do
-    expect(restaurant).to respond_to(:place_your_order)
+    it 'places an order and message is being sent to customer' do
+      allow(restaurant).to receive(:require_relative)
+      restaurant.menu.select_dish('burger', 1)
+      restaurant.bill
+      restaurant.place_your_order
+      expect(restaurant.kitchen).to eq(restaurant.menu.your_order)
+    end
   end
 
 end
