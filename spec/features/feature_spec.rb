3db1c1846ca935b7bc3ecb8aea91e17ac8f085b6
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 22:10:41 +0000

    Attempt to use RSpec feature test

---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,21 @@
+require 'menu'
+require 'order'
+
+RSpec.describe 'features' do
+  context 'User Story 1' do
+    # As a customer
+    # So that I can check if I want to order something
+    # I would like to see a list of dishes with prices
+  
+    # menu = Menu.new
+    # menu.print
+  end
+
+  context 'User Story 2' do
+    # As a customer
+    # So that I can order the meal I want
+    # I would like to be able to select some number of several available dishes
+    
+  end
+
+end
