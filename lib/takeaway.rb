03ecfbafe33f1f825890745e2869cb19584a59f9
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-20 11:54:49 +0000

    it can now passsd 2 arguements to place_order as well as return the price of the order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,27 +1,33 @@
 require_relative 'pizza'
 require_relative 'order'
+require_relative 'confirmation'
 
 class Takeaway
 
   attr_reader :menu, :order
 
-  def initialize(menu_class = Pizza.new, order_class = Order)
+  def initialize(menu_class = Pizza.new, order_class = Order.new, confirmation_class = Confirmation.new)
     @menu = menu_class
     @order = order_class
+    @confirm = confirmation_class
   end
 
   def view_menu
     @menu.view_food_and_price
   end
 
-  def place_order(item)
-    create_order.add_to_order(item)
+  def place_order(item, quantity)
+    @order.add_to_order(item, quantity)
+  end
+
+  def price
+    @order.total_price
   end
 
   private
 
   def create_order
-    @order.new
+    @order = Order.new
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -16,11 +16,19 @@ let(:new_order) { double(:new_order) }
 
 
   describe '#order' do
-    it { is_expected.to respond_to(:place_order).with(1) }
+    it { is_expected.to respond_to(:place_order).with(2) }
 
-    it "should add an item to the order and return the order" do
+    it "can call the add to order method on an instance of order allowing an item to be added to the order" do
       expect_any_instance_of(Order).to receive(:add_to_order)
-      subject.place_order(item)
+      subject.place_order(item, 1)
+    end
+
+  end
+
+  describe '#price' do
+    it "can call the total price method on an instance of order allowing the price to be returned" do
+      expect_any_instance_of(Order).to receive(:total_price)
+      subject.price
     end
   end
 end
