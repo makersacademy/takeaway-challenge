b4ec54cf5124b3b969e96c1b20dc1bc33fe121e9
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 23:00:20 +0000

    Added tests for pushing order hash to array

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,4 +1,14 @@
 require 'order'
 
 describe Order do
+  let(:item) { double :item }
+  let(:amount) { double :amount }
+  it 'orders food from menu' do
+    expect(subject.customer_order(item, amount)).to eq [{ :item => item,
+    :amount => amount }]
+  end
+
+  it 'takes an order hash and pushes it into an array' do
+    expect(subject.take_order).to eq []
+  end 
 end
