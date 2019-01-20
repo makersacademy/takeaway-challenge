e4db8cc8b9c39caf6e097e39f19a9c37eb67ad59
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 23:01:34 +0000

    Added functionality for pushing order hash to array - pasing the tests

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,14 @@
 require_relative 'menu'
 
 class Order
+  attr_reader :take_order
+
+  def initialize
+    @take_order = []
+  end
+
+  def customer_order(item, amount)
+    @take_order << { item: item, amount: amount }
+  end
+  
 end
