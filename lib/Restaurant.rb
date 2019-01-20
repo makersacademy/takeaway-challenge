11121b4a1f1a45d8176a5bc36370187b92e35f46
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-20 21:02:31 +0000

    Added kitchen method that collects placed orders

--- lib/Restaurant.rb
+++ lib/Restaurant.rb
@@ -15,9 +15,14 @@ class Restaurant
     calculate(@order)
   end
 
+  def kitchen
+    @order_accepted
+  end
+
   def place_your_order
     fail 'Please select your meal' if @order == nil
     require_relative 'twilio.rb'
+    @order_accepted = @order
   end
 
 end
