6101d31d0270afc74d49c17ef9cadcbf81fcdeba
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-20 11:34:43 +0000

    Refactor tests

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,12 +1,15 @@
 require 'menu'
 
 RSpec.describe Menu do
-  let(:dishes) { { "Classic Chicken": 8.85 } }
-  let(:printed_menu) { "Classic Chicken £8.85" }
-
   subject(:menu) { described_class.new(dishes) }
 
-  it 'prints the menu' do
-    expect(menu.print_menu).to eq printed_menu
+  let(:dishes) {{ "Classic Chicken": 8.85 }}
+  let(:printed_menu) { "Classic Chicken £8.85" }
+
+
+  describe '#print_menu' do
+    it 'prints the menu' do
+      expect(menu.print_menu).to eq printed_menu
+    end
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,13 +1,15 @@
 require 'takeaway'
 
 RSpec.describe Takeaway do
+  subject(:takeaway) { described_class.new(menu) }
+
   let(:menu) { double :menu, dishes: menu_items, print_menu: printed_menu }
   let(:menu_items) { { "Classic Chicken": 9.95 } }
   let(:printed_menu) { "Classic Chicken £9.95" }
 
-  subject(:takeaway) { described_class.new(menu) }
-
-  it 'prints the menu' do
-    expect(takeaway.print_menu).to eq printed_menu
+  describe '#print_menu' do
+    it 'prints the menu' do
+      expect(takeaway.print_menu).to eq printed_menu
+    end
   end
 end
