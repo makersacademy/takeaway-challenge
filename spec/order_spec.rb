f69a8413fba072f1ad443199f0efc9af62db0368
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 23:37:38 +0000

    Added tests to check if the correct amount of money was being charged for the order.

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -11,4 +11,11 @@ describe Order do
   it 'takes an order hash and pushes it into an array' do
     expect(subject.take_order).to eq []
   end 
+
+  it 'calculates total price of order' do
+    subject.customer_order(:burrito, 3)
+    subject.customer_order(:lobster, 2)
+    subject.customer_order(:burger, 2)
+    expect(subject.total_cost).to eq 120.50
+  end
 end
