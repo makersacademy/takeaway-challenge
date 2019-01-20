52fff20282cec8415e8b85c6200873f698902734
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 21:29:31 +0000

    Added menu printing functionality - passing the tests.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,16 @@
 class Menu
   attr_reader :menu_items
 
-  FOOD = {}
+  FOOD = {pizza: 2.99, burger: 3.50, burrito: 4.50, 
+  lobster: 50.00}
 
   def initialize(menu_items = FOOD)
-    @menu_items
+    @menu_items = menu_items
   end
 
   def print
-    p @menu_items
+    puts "Takeout Menu"
+    @menu_items.each {|food, price| puts "#{food.capitalize} £#{'%.2f' % price}" }
   end
 
 end
