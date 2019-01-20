a22cfeb25c2d8776d5abafb819010bcee6712e70
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-20 15:57:30 +0000

    Code refactoring. Breaking down calculate method

--- lib/Calculator_module.rb
+++ lib/Calculator_module.rb
@@ -1,17 +1,17 @@
 module Calculator
 
-def calculate(array)
-@total = []
-    array.each do |item|
-      if item.keys.join("") == "burger"
-        @total << (item.values.join("").to_i * 5)
-      elsif item.keys.join("") == "chips"
-        @total << (item.values.join("").to_i * 2)
-      elsif item.keys.join("") == "salad"
-        @total << (item.values.join("").to_i * 3)
-      end
-    end
+  def calculate(array)
+    @total = []
+    helper(array)
     return @total.reduce(:+)
+  end
+
+  def helper(array)
+    array.each do |item|
+      @total << (item.values.join("").to_i * 5) if item.keys.join("") == "burger"
+      @total << (item.values.join("").to_i * 2) if item.keys.join("") == "chips"
+      @total << (item.values.join("").to_i * 3) if item.keys.join("") == "salad"
+    end
   end
 
 end
