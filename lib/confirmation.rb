a09dd9ce756ab4ab013f6a0521f1adbde3aaa6d5
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-20 23:41:29 +0000

    stubbed the twilio application to prevent texts every time tests are run

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -4,29 +4,20 @@ require 'twilio-ruby'
 
 class Confirmation
 
-include Details
-TIMEPLUS1 = Time.now + (60*60)
+  include Details
+  TIMEPLUS1 = Time.now + (60 * 60)
 
   def confirmation_of_order(saved_order)
-    if saved_order.price == 0
+    if saved_order.price.zero?
       saved_order.total_price
     end
-      puts "Do you confirm your order of #{saved_order.basket} for the value of #{saved_order.price}?"
-      puts "Press enter to confirm"
-      answer = gets.chomp.to_s
-      if answer == ""
-        puts "Thank you, please wait for confirmation text."
-        confirmation_text
-      else
-        "Order not confirmed"
-      end
+    check_confirmation(saved_order)
   end
 
   def confirmation_text
     account_sid = @sid
     auth_token = @token
 
-    # set up a client to talk to the Twilio REST API
     client = Twilio::REST::Client.new account_sid, auth_token
 
     from = '+441772367485'
@@ -37,6 +28,20 @@ TIMEPLUS1 = Time.now + (60*60)
     to: to,
     body: "This is a message to confirm the order of your food delivery to arrive at #{TIMEPLUS1}"
     )
+  end
+
+  private
+
+  def check_confirmation(saved_order)
+    puts "Do you confirm your order of #{saved_order.basket} for the value of #{saved_order.price}?"
+    puts "Press enter to confirm"
+    answer = gets.chomp.to_s
+    if answer == ""
+      puts "Thank you, please wait for confirmation text."
+      confirmation_text
+    else
+      "Order not confirmed"
+    end
   end
 
 end
--- spec/confirmation_spec.rb
+++ spec/confirmation_spec.rb
@@ -1,22 +1,22 @@
 require 'confirmation'
 
 describe Confirmation do
-let(:item) { double(:item) }
+  let(:item) { double(:item) }
 
-  it "should sent a text when the order is confirmed" do
-    expect(subject.confirmation_text).to eq "This is a message to confirm the order of your food delivery to arrive at #{Confermation::TIMEPLUS1}"
+  before(:each) do
+    allow(subject).to receive(:confirmation_text).and_return "This is a message to confirm the order of your food delivery to arrive at #{Confirmation::TIMEPLUS1}"
   end
 
   context '#confirm order (e.g. enter pressed as input)' do
-    it "should get the user input of '' return confirmation text" do
+    it 'should get the user input of '' return confirmation text' do
       allow($stdout).to receive(:write)
       allow(subject).to receive(:gets).and_return("")
-      expect(subject.confirmation_of_order(Order.new)).to eq("Text")
+      expect(subject.confirmation_of_order(Order.new)).to eq("This is a message to confirm the order of your food delivery to arrive at #{Confirmation::TIMEPLUS1}")
     end
   end
 
   context '#do not confirm order (e.g. abc pressed as input)' do
-    it "should get the user input of 'abc' and return 'Order not confirmed'" do
+    it 'should get the user input of "abc" and return "Order not confirmed"' do
       allow($stdout).to receive(:write)
       allow(subject).to receive(:gets).and_return("abc")
       expect(subject.confirmation_of_order(Order.new)).to eq("Order not confirmed")
