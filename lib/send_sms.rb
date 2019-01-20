801d5153427467b15f49045873d92a0890b0ecfd
Erin Stevenson <estevenson_94@live.com>
2019-01-20 19:20:46 +0000

    13. Anonimized twilio details

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -4,15 +4,16 @@ require 'twilio-ruby'
 
 class Sendsms
   def send_message(body)
-    account_sid = 'AC90ba00c3b14765248a33c1c36e863800'
-    auth_token = '9303091f83aa503a1bad647de9b21f3c'
+    account_sid = ENV['Twilio_acc_sid'] 
+    auth_token = ENV['Twilio_auth_code']
     @client = Twilio::REST::Client.new(account_sid, auth_token)
     
     message = @client.messages.create(
-                                 from: 'twilio_number',
+                                 from: ENV['Twilio_number'],
                                  body: body,
-                                 to: 'personal_number'
+                                 to: ENV['Personal_number']
                                )
-    
+    return message.sid
     end
+
 end
