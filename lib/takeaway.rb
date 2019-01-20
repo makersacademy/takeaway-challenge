e915a7ea65c141f4e2463d09645051428dcd80d9
Tameka <tamekasmall@hotmail.com>
2019-01-20 11:04:48 +0000

    Test: User story 3 (WIP):sum matches the exact total

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,16 +2,48 @@ class Takeaway
 
   attr_reader :menu_list
   def initialize
-    @menu_list = [
-      {"rice" => 3},
-      {"chicken" => 4},
-      {"salad" => 3},
-      {"pizza" => 5},
-      {"cola" => 2}
-    ]
+    @menu_list = {
+      3 => ["rice"],
+      4 => ["chicken"],
+      5 => ["salad"],
+      2 => ["cola"]}
+
+    @dishes = []
+    @quantity = []
+    @sum = []
+    @total = 0
+    @exact_total = []
+
   end
 
   def menu
     @menu_list
   end
+
+  def order(dish, quantity)
+     @dishes << dish
+     @quantity << quantity
+     p "dishes"
+     p @dishes
+     p "quantity"
+     p @quantity
+  end
+
+  def customer_total(sum)
+    @sum << sum
+    sum
+  end
+
+  def exact_total
+    @dishes.each do |food|
+      @menu_list.map do |key, value|
+        if @menu_list[key].include?(food)
+          @total =+ key * @quantity.last
+            @exact_total << @total
+            end
+          end
+        end
+        @exact_total.reduce :+
+        end
+
 end
