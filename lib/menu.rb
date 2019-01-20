c14b45b1f12f85890cfb30894f257ca314c43d83
Debora <debora.piu92@gmail.com>
2019-01-20 00:01:38 +0000

    menu clas refactored to list of dishes and detatched from Dish class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,23 +1,15 @@
-require_relative 'dish'
-
 class Menu
 
-  attr_reader :menu_list
+  attr_reader :menu
 
   def initialize
-    @menu_list = []
-  end
-
-  def add(dish_name, price, dish = Dish)
-    @menu_list.push(dish.new(dish_name, price))
-  end
-
-  def to_string
-    menu_to_list = ""
-    @menu_list.map { |dish|
-      menu_to_list << "#{dish.dish_name} at £#{sprintf('%.2f', dish.price)}\n"
+    @menu = {
+      "Dough Balls" => 4.49,
+      "Cheeseburger" => 6.79,
+      "Garlic Pizza Bread" => 3.99,
+      "Chicken Wings" => 5.99,
+      "Pizza Margherita" => 8.99
     }
-    return menu_to_list
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,24 +3,8 @@ require 'menu'
 RSpec.describe Menu do
 
   context "#menu" do
-    let(:pizza) { described_class.new("Pizza", 10.50) }
     it "should print a menu in an hash" do
-      expect(subject.menu_list).to be_kind_of Array
-    end
-    it "should contain instances of Dish" do
-      subject.add("Burger", 6.50)
-      expect(subject.menu_list[0]).to be_kind_of Dish
-    end
-  end
-  context "#to_string" do
-    it "should return a string of menu" do
-      subject.add("Burger", 6.50)
-      expect(subject.to_string).to eq "Burger at £6.50\n"
-    end
-    it "should return multiple lines of menu" do
-      subject.add("Burger", 6.50)
-      subject.add("Cheeseburger", 5.49)
-      expect(subject.to_string).to eq "Burger at £6.50\nCheeseburger at £5.49\n"
+      expect(subject.menu).to be_kind_of Hash
     end
   end
 end
