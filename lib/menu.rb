a3a0daad387e62cca96cbc911077c1cd02b12df8
dangroze <dangroze@gmail.com>
2019-01-20 16:15:48 +0000

    Initialized a list of dishes, passed test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,12 @@
 class Menu
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = [{ name: "pizza", price: 9.99 },
+      { name: "calzone", price: 11.50 },
+      { name: "kebab", price: 4.99 },
+      { name: "soup", price: 5.50 },
+      { name: "sandwich", price: 3.50 },
+      { name: "garlic bread", price: 3.99 }]
   end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,17 @@
 require 'menu.rb'
 
 RSpec.describe Menu do
+  let(:subject){ Menu.new }
+  let(:dishes){[
+    { name: "pizza", price: 9.99 },
+    { name: "calzone", price: 11.50 },
+    { name: "kebab", price: 4.99 },
+    { name: "soup", price: 5.50 },
+    { name: "sandwich", price: 3.50 },
+    { name: "garlic bread", price: 3.99 }
+  ]}
+  it 'initializes a list of #dishes' do
+    expect(subject.dishes).not_to be_empty
+  end
 
 end
