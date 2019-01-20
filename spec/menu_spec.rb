30662e8932cb460324d6813b4ab5ae6cd27649af
Diptis Halder <diptis.halder@gmail.com>
2019-01-20 21:37:10 +0000

    v2.2 Added #initialize to Menu class with a hash of dishes and prices

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,6 +1,6 @@
 require 'menu'
 
 describe Menu do
-  it { is_expected.to respond_to :dishes_and_prices }
+  it { is_expected.to respond_to :dishes }
   it { is_expected.to respond_to :see_menu }
 end
