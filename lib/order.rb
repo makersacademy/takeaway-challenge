f1ea4c547937cb51b00570bdeaad1e2c5262f0a6
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-20 23:27:42 +0000

    added a check that the item is on the menu. use doubles and stubs to allow tests

--- lib/order.rb
+++ lib/order.rb
@@ -2,23 +2,25 @@ require_relative 'pizza'
 
 class Order
 
-attr_reader :basket, :price
+  attr_reader :basket, :price
 
-  def initialize(pizza_class = Pizza)
+  def initialize(pizza_class = Pizza.new)
     @basket = []
-    @confirmation_of_order = []
     @menu = pizza_class
     @price = 0
   end
 
   def add_to_order(item, quantity)
-    quantity.times { @basket << item }
-    return_order
+    if @menu.view_food_and_price.include?(item.to_sym)
+      add_quantity_of_item(item, quantity)
+    else
+      "Sorry but this is not on our menu."
+    end
   end
 
   def total_price
     @basket.each do |item|
-      @price += @menu.new.view_food_and_price[item.to_sym].to_f
+      @price += @menu.view_food_and_price[item.to_sym].to_f
     end
     @price
   end
@@ -27,6 +29,11 @@ private
 
   def return_order
     @basket
+  end
+
+  def add_quantity_of_item(item, quantity)
+    quantity.times { @basket << item }
+    return_order
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,28 +2,45 @@ require 'order'
 
 describe Order do
   let(:item) { double(:item, to_sym: :item) }
-  let(:price) { double(:price) }
+  let(:menu) { double(:menu, view_food_and_price: {item: 7.7}) }
+  let(:order) { described_class.new(menu) }
 
-  it "should by default return an empty basket" do
-    expect(subject.basket).to eq []
+  context '#item_is_on_menu' do
+    before(:each) do
+      allow(menu).to receive_message_chain("view_food_and_price.include?") { true }
+    end
+
+    it 'should by default return an empty basket' do
+      expect(order.basket).to eq []
+    end
+
+    it 'should store an item which has been added to the basket' do
+      order.add_to_order(:item, 1)
+      expect(order.basket).to eq [:item]
+    end
+
+    it 'should add the item the quantity number of times' do
+      expect(order.add_to_order(item, 2)).to eq [item, item]
+    end
+
+    it 'should return the full price of the order' do
+      order.add_to_order(item, 1)
+      expect { order.total_price }.to change { order.price }
+    end
+
+    it 'should return the price once calcualted' do
+      order.add_to_order(item, 1)
+      expect(order.total_price).to be_an_instance_of Float
+    end
   end
 
-  it "should store an item which has been added to the basket" do
-    subject.add_to_order(:item, 1)
-    expect(subject.basket).to eq [:item]
-  end
+  context '#item_isnt_on_menu' do
+    before(:each) do
+      allow(menu).to receive_message_chain("view_food_and_price.include?") { false }
+    end
 
-  it "should add the item the quantity number of times" do
-    expect(subject.add_to_order(item, 2)).to eq [item, item]
-  end
-
-  it "should return the full price of the order" do
-    subject.add_to_order(item, 1)
-    expect { subject.total_price }.to change { subject.price }
-  end
-
-  it "should return the price once calcualted" do
-    subject.add_to_order(item, 1)
-    expect(subject.total_price).to be_an_instance_of Float
+    it 'should return a message if the item if not on the menu' do
+      expect(order.add_to_order(item, 2)).to eq "Sorry but this is not on our menu."
+    end
   end
 end
