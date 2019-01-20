50680eef3b65fa3900be6b99216cb97a24f21634
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 23:44:23 +0000

    Added total order price - passing the tests

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,24 @@
 require_relative 'menu'
 
 class Order
-  attr_reader :take_order
+  attr_reader :take_order, :menu
 
-  def initialize
+  def initialize(menu = Menu.new)
+    @menu = menu
     @take_order = []
   end
 
   def customer_order(item, amount)
     @take_order << { item: item, amount: amount }
   end
-  
+
+  def total_cost
+    total = 0
+
+    @take_order.each do |food_item|
+      total += @menu.menu_items[food_item[:item]] * food_item[:amount]
+    end
+    total
+  end
+
 end
