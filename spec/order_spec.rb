f319c091fc0083da67a8526e3be1d66b5e18989f
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-20 20:09:51 +0000

    refactoring

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,22 +1,21 @@
 require 'order'
 
 describe Order do
+  let(:order_3_tofish) { subject.select_dish("Tofish Bites", 3) ; subject}
 
   it "customer can select a dish and add price to bill" do
-    subject.select_dish("Tofish Bites", 3)
-    expect(subject.receipt).to eq [6, 6, 6]
+    expect(order_3_tofish.receipt).to eq [6, 6, 6]
   end
+
   it "customer can order a number of dishes from the menu" do
-    expect(subject.select_dish("Tofish Bites", 2)).to eq("2x Tofish Bites(s) added to your basket.")
+    expect(subject.select_dish("Tofish Bites", 3)).to eq "3x Tofish Bites(s) added to your basket."
   end
 
   it "adds up total of selected dishes" do
-    subject.select_dish("Tofish Bites", 3)
-    expect(subject.total).to eq "Total: £18"
+    expect(order_3_tofish.total).to eq "Total: £18"
   end
 
   it "sends sms on checkout" do
-    subject.select_dish("Tofish Bites", 3)
-    expect(subject.checkout).to eq nil
+    expect(order_3_tofish.checkout).to eq nil
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,15 +3,8 @@ require 'takeaway'
 describe Takeaway do
 
   describe "read_menu" do
-    it "shows a list of dishes with prices" do
-      dishes = Takeaway.new
-      expect(subject.read_menu).to eq(dishes.read_menu)
+    it "should load menu" do
+      expect(subject.read_menu).to include("Tofish Bites": 6)
     end
   end
-
-  it "should load menu" do
-    subject.read_menu
-    expect(subject.menu).to include("Tofish Bites": 6)
-  end
-
 end
