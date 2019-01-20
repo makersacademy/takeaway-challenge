323100f670c6b1026e0a248ed181f4c172967757
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-20 15:52:14 +0000

    Test: places an order and message is being sent to customer

--- spec/Restaurant_spec.rb
+++ spec/Restaurant_spec.rb
@@ -1,12 +1,16 @@
 require 'Restaurant'
 
 RSpec.describe Restaurant do
-
-  subject(:restaurant) { restaurant = Restaurant.new }
+  subject(:restaurant) { described_class.new }
 
   it 'total bill matches the sum of the various dishes in my order' do
-    3.times{ restaurant.menu.select_dish('burger',1) }
+    3.times { restaurant.menu.select_dish('burger', 1) }
     expect(restaurant.bill).to eq(15)
+  end
+
+  it 'places an order and message is being sent to customer' do
+    allow(restaurant).to receive(:place_your_order).and_return(false)
+    expect(restaurant).to respond_to(:place_your_order)
   end
 
 end
