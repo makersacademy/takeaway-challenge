1474e2819a6134a5316bd22332f798682c304b43
Aiden Gleave <aidengleave@gmail.com>
2019-01-20 21:28:01 +0000

    Changed test to cover prices with 2 decimal places.

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Menu do
   it 'prints menu so customer can order food' do
     expect(subject.print).to eq(subject.menu_items)
   end
+  
+  it 'has a list of food items and prices' do
+    expect(subject.menu_items).to eq({pizza: 2.99, burger: 3.50, burrito: 4.50, 
+    lobster: 50.00})
+  end
 
 end
 
