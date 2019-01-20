edbf8cf12209d16868b32eeed9b2ef5fa1d058ac
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-20 15:44:41 +0000

    added item quantity to add_to_basket method

--- lib/basket.rb
+++ lib/basket.rb
@@ -10,11 +10,15 @@ class Basket
     @menu = Menu.new
   end
 
-  def add_to_basket(item_code)
+  def add_to_basket(item_code, quantity = 1)
     fail "Please enter a valid item code." if item_code.is_a? String
     fail "Please enter a valid item code." if item_code < MINIMUM_ITEM_CODE
     fail "Please enter a valid item code." if item_code > MAXIMUM_ITEM_CODE
-    @basket << @menu.menu[item_code - 1]
+    quantity.times do
+      @basket << @menu.menu[item_code - 1]
+    end
+    chosen_item = @menu.menu[item_code -1][:item]
+    "#{quantity} #{chosen_item} added to basket"
   end
 
   def remove_from_basket(item_code)
