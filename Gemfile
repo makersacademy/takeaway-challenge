a4e0fcd5f291a70eefd17b2d6b454745c79a1e9f
jesslns <jesslns@yahoo.com.hk>
2019-01-20 15:56:11 +0000

    Personal details saved in .env

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem 'simplecov', require: false, group: :test
 gem 'simplecov-console', require: false, group: :test
 
 gem 'twilio-ruby'
+gem 'dotenv'
--- lib/text.rb
+++ lib/text.rb
@@ -1,21 +1,23 @@
-require ("bundler")
-Bundler.require()
+#require ("bundler")
+#Bundler.require()
 
 require 'rubygems'
 require 'twilio-ruby'
+require 'dotenv'
+Dotenv.load ('./twilio_sms.env')
 
 class Text
 
   # Your Account Sid and Auth Token from twilio.com/console
-  account_sid = 'AC18c856b9d47de2d9e6386709a5e65b9e'
-  auth_token = 'e9b0292323f32a3f469837d56582fcb7'
+  account_sid = ENV['account_sid']
+  auth_token = ENV['auth_token']
   @client = Twilio::REST::Client.new(account_sid, auth_token)
   @time = Time.new.strftime("at %I:%M%p")
   message = @client.messages
     .create(
        body: "Thank you! Your order was placed and will be delivered " + @time,
-       from: '+441564432075',
-       to: '+447561442804'
+       from: ENV['twilio_num'],
+       to: ENV['my_num']
      )
 
   puts message.sid
