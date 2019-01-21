65333cf019abd9da0767d207ac816e592a4292df
William Dunk <williamdunk@gmail.com>
2019-01-21 12:19:58 +0000

    fixed issue with environmental variables

--- lib/success_sms.rb
+++ lib/success_sms.rb
@@ -2,15 +2,12 @@
 require 'dotenv/load'
 require 'twilio-ruby'
 
-puts "enviro vari"
-puts $WPDUNK_PHONE
-
-account_sid = '$TWILIO_ACCOUNT_SID'
-auth_token = '$TWILIO_AUTH_TOKEN'
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = '$TWILIO_PHONE' # Your Twilio number
-to = '$WPDUNK_PHONE' # Your mobile phone number
+from = ENV['TWILIO_PHONE'] # Your Twilio number
+to = ENV['WPDUNK_PHONE'] # Your mobile phone number
 
 client.messages.create(
 from: from,
