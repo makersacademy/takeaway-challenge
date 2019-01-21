f9cc8ded038acd0b0bfc6e1687fdce1795cb830d
Debora <debora.piu92@gmail.com>
2019-01-21 20:42:31 +0000

    refactored with add_menu from Menu class adding Dish instances

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,15 +1,15 @@
+require_relative 'dish'
+
 class Menu
 
-  attr_reader :menu
+  attr_reader :menu_list
 
   def initialize
-    @menu = {
-      "Dough Balls" => 4.49,
-      "Cheeseburger" => 6.79,
-      "Garlic Pizza Bread" => 3.99,
-      "Chicken Wings" => 5.99,
-      "Pizza Margherita" => 8.99
-    }
+    @menu_list = []
   end
 
+
+  def add(dish_name, price = 0, dish = Dish)
+    @menu_list.push(dish.new(dish_name, price))
+  end
 end
