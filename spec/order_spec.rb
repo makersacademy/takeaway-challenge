c31023655fca8ed24c518459e0a3bd25aee69176
“Brenda” <odonnell.brenda@gmail.com>
2019-02-16 21:56:24 +0000

    lets me select several available dishes.

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,8 +4,15 @@ describe Order do
 
   subject(:order) { described_class.new }
 
-  it 'lets me store multiple selections of dishes' do
-    
+  let(:dishes) do
+    {chicken: 2,
+    fish: 1}
+  end
+
+  it 'lets me select several available dishes' do
+    order.add(:chicken, 2)
+    order.add(:fish, 1)
+    expect(order.dishes).to eq(dishes)
   end
 
 end
