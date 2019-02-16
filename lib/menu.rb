2e10e634c8b5c6b49f12ed5ab6deec79ab386139
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-16 14:58:40 +0000

    A dish can be added to the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,4 +2,12 @@ class Menu
   def initialize(dish_list = [])
     @dishes = dish_list
   end
+
+  def dishes
+    @dishes.dup
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,15 +1,29 @@
 require 'menu'
 describe Menu do
+  let (:double_dish) { double :dish }
+  let (:another_double_dish) { double :another_dish }
+
   it 'gets created with an empty list of dishes when list of dishes not passed in' do
     menu = Menu.new
     expect(menu.instance_variable_get(:@dishes)).to eq []
   end
 
   it 'gets created with a list of dishes when list of dishes is passed in' do
-    double_dish = double :dish
-    another_double_dish = double :another_dish
     dish_list = [double_dish, another_double_dish]
     menu = Menu.new(dish_list)
     expect(menu.instance_variable_get(:@dishes)).to equal dish_list
+  end
+
+  it 'returns list of dishes' do
+    dish_list = [double_dish, another_double_dish]
+    menu = Menu.new(dish_list)
+    expect(menu.dishes).not_to equal dish_list
+    expect(menu.dishes).to eq [double_dish, another_double_dish]
+  end
+
+  it 'adds dish to dish list' do
+    menu = Menu.new
+    menu.add_dish(double_dish)
+    expect(menu.dishes).to include double_dish 
   end
 end
