b05eb3c4a9e15ae97a125796ac89067546946edf
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-16 09:09:12 +0000

    update order #add_dish test to use Dish double

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -10,8 +10,9 @@ describe Order do
   end
 
   it 'adds a dish to its selected_dishes instance variable' do
-    subject.add_dish("rice")
-    expect(subject.selected_dishes). to include "rice"
+    rice = double(:price => 3, :name => "rice")
+    subject.add_dish(rice)
+    expect(subject.selected_dishes). to include rice
   end
 
   pending 'it sends an SMS confirmaton'
