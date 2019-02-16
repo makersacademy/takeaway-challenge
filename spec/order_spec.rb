4d9e48568fbb898855574dbfe8966a7ee2419674
Clare Pinder <Clare.pinder@live.co.uk>
2019-02-16 18:05:36 +0000

    corrected error in rspec re calculating order total

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -25,11 +25,14 @@ describe Order do
   end
 
   it "calculates total" do
-    view_menu_double = [{ dish: "chips", quantity: 2, price: 2.00 },
-      { dish: "chicken chow mein", quantity: 1, price: 2.80 },
-      { dish: "doner kebab", quantity: 4, price: 12.80 }]
+    view_menu_double = double([{ "chips" => 1.00 },
+        { "chicken chow mein" => 2.80 },
+        { "doner kebab" => 3.20 }])
     o = Order.new
-    allow(o).to receive(:view_basket).and_return(view_menu_double)
+    allow(o).to receive(:view_menu).and_return(view_menu_double)
+    o.select_dish("chips", 2)
+    o.select_dish("chicken chow mein")
+    o.select_dish("doner kebab", 4)
     expect(o.total).to eq(17.60)
   end
 
