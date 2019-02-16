f597901b894a68fa441bdbc8e4fd3c3ece416470
Amina Jusic <amina_jusic@yahoo.com>
2019-02-16 15:01:26 +0000

    Initialize each instance of #Dish with name

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,2 +1,10 @@
+# Creating a dish name, price and optional description
+
 class Dish
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,4 +1,12 @@
 require 'dish'
 
 describe Dish do
+
+  subject(:dish) { described_class.new('Wok-fried greens') }
+
+  it 'should have name' do
+    expect(dish.name).to eq 'Wok-fried greens'
+  end
+
+
 end
