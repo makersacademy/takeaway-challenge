861c3d54b993b423bcf31770de87a7241e48ee53
“Brenda” <odonnell.brenda@gmail.com>
2019-02-16 14:29:49 +0000

    It shows a list of dishes and prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,8 +3,20 @@ class Menu
 attr_reader :dishes
 
     def initialize
-      @dishes = ["Chicken £3.50", "Pizza £4"]
+      @dishes = [{:dish => "Chicken", :price => "£3.50"},
+        {:dish => "Pizza", :price => "£4"}
+      ]
     end
 
+    # def print_menu
+    #   printed_menu = []
+    #   @dishes.each do |dish, price|
+    #     puts "#{dish}, #{price}"
+    #   end
+    # end
+
+    def print_menu
+      @dishes.map {|x| x.values.join(" ") }
+    end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,8 +3,13 @@ require 'menu'
 describe Menu do
 
 subject(:menu) { described_class.new }
-  it 'is created with a list of dishes and prices' do
-    expect(subject.dishes).to eq(["Chicken £3.50", "Pizza £4"])
+  it 'MMMMis created with a list of dishes and prices' do
+    expect(subject.dishes).to eq([{:dish => "Chicken", :price => "£3.50"},
+      {:dish => "Pizza", :price => "£4"}])
+  end
+
+  it 'prints a list of dishes and prices' do
+    expect(subject.print_menu).to eq(["Chicken £3.50", "Pizza £4"])
   end
 
 end
