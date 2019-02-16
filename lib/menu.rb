63dd2365fc8a07444469c2988710a92753d7b085
Luke Phyall <lukeprospect@yahoo.com>
2019-02-16 13:22:33 +0000

    Adds menu.list and an actual menu.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,17 @@
 class Menu
 
+  def initialize
+
+    @menu = [
+      {item: 1, pizza: "Kenjutsu: Chicken, Pepperoni, Green Chili"},
+      {item: 2, pizza: "Ninjitsu: Spicy Beef, Sweetcorn, Onion, Sausage"},
+      {item: 3, pizza: "Taijutsu: Prawn, Anchovy, Pineapple, Tripe"}
+    ]
+
+  end
+
+  def list
+    @menu
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,11 +2,19 @@ require 'menu'
 
 describe Menu do
 
+  let ( :menu ) { Menu.new }
+
   it "has a menu" do
-    menu = Menu.new
+    menu
   end
 
-  
+  it "will list the menu for you" do
+    expect(menu.list).to eq([
+      {item: 1, pizza: "Kenjutsu: Chicken, Pepperoni, Green Chili"},
+      {item: 2, pizza: "Ninjitsu: Spicy Beef, Sweetcorn, Onion, Sausage"},
+      {item: 3, pizza: "Taijutsu: Prawn, Anchovy, Pineapple, Tripe"}
+    ])
+  end
 
 
 end
