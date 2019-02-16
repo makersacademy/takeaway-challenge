17e44d1e8c78e536f9f323b901d05e3a5b588adc
Luke Phyall <lukeprospect@yahoo.com>
2019-02-16 14:59:46 +0000

    Deletes redundat tests.

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -8,10 +8,6 @@ describe Interface do
     interface
   end
 
-  it "shows you a welcome screen" do
-    expect { interface.welcome }.to output("Welcome to Samurai Pizza.\nOur pizzas represent the cutting edge of quality and taste:\n#1 - Kenjutsu: Chicken, Pepperoni, Green Chili\n#2 - Ninjitsu: Spicy Beef, Sweetcorn, Onion, Sausage\n#3 - Taijutsu: Prawn, Anchovy, Pineapple, Tripe\n").to_stdout
-  end
-
   it "shows you the welcome screen with the menu" do
     expect { interface.welcome }.to output("Welcome to Samurai Pizza.\nOur pizzas represent the cutting edge of quality and taste:\n#1 - Kenjutsu: Chicken, Pepperoni, Green Chili\n#2 - Ninjitsu: Spicy Beef, Sweetcorn, Onion, Sausage\n#3 - Taijutsu: Prawn, Anchovy, Pineapple, Tripe\n").to_stdout
   end
