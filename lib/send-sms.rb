ece3a56394928d6855f9b6cf1c3ca2bd05596882
Clare Pinder <Clare.pinder@live.co.uk>
2019-02-17 12:13:02 +0000

    wrapped the SendSMS functionality in a class so it can be executed from the Order interface

--- lib/send-sms.rb
+++ lib/send-sms.rb
@@ -2,16 +2,20 @@ require "./lib/text.rb"
 require("bundler")
 Bundler.require()
 
-account_sid = "ACb4566e2ff2f108e9705aff97af967eca"
-auth_token = "620471e00f3a455761d098cc9d91dfb5"
+class SendSMS
+  def initialize
+    @account_sid = "ACb4566e2ff2f108e9705aff97af967eca"
+    @auth_token = "620471e00f3a455761d098cc9d91dfb5"
+  end
 
-@client = Twilio::REST::Client.new(account_sid, auth_token)
+  def send
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    @message = Text.new.generate
+    @client.messages.create(
+      to: "+447507093700",
+      from: "+441499377023",
+      body: @message
+    )
+  end
 
-@message = Text.new.generate
-
-private
-  @client.messages.create(
-    to: "+447507093700",
-    from: "+441499377023",
-    body: @message
-  )
+end
