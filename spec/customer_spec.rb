87bad0d7c282a69a4407d7d3ff863850e9ba7ec0
Steve Kahn <steve@kahn.org.uk>
2019-02-17 13:13:07 +0000

    Wrote test for adding to the order

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,9 +1,22 @@
 require 'customer'
 
 describe Customer do
+  let(:dish) {double :dish}
 
+  before :each do
+    @customer = Customer.new
+  end
   it "Has nothing in the order when created" do
-    customer = Customer.new
-    expect(customer.order).to eq []
+    expect(@customer.order).to eq []
+  end
+
+  it "Can add a single dish to the order" do
+    @customer.add_to_order(dish)
+    expect(@customer.order).to eq [dish]
+  end
+
+  it "Can add multiple of the same dish to the order" do
+    @customer.add_to_order(dish, 2)
+    expect(@customer.order).to eq [dish, dish]
   end
 end
