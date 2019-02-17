34e856a027700b056790725a1ae3edbed8d8804d
Amina Jusic <amina_jusic@yahoo.com>
2019-02-17 16:08:28 +0000

    Added a test and code so order can select several dishes from menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,12 @@
 class Order
-  
+
+attr_reader :dishes  # acces the dishes
+
+  def initialize
+    @dishes = {}
+  end
+
+  def add(dish, quantity)
+    dishes[dish] = quantity
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,19 @@
 require 'order'
 describe Order do
+  subject(:order) { described_class.new }
+
+  let(:dishes) do
+    {
+      chicken: 2,
+      fish: 1
+    }
+  end
+
+  it 'selects seceral dishes from menu' do
+    order.add(:chicken, 2)
+    order.add(:fish, 1)
+    expect(order.dishes).to eq(dishes)
+  end
 
 
 end
