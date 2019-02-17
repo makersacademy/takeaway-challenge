4666edd7279b1852161ba948d5cbfed967cf6688
James Ault <james.ault@johnlewis.co.uk>
2019-02-17 21:12:42 +0000

    Added updated takeaway guard condition tests

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,10 @@
 require './lib/takeaway.rb'
 describe Takeaway do
 
-  let(:dish_double) { double :dish, dish_number: 1, dish_name: 'Chicken Tikka', dish_cost: 5 }
+  let(:dish_double) { double :dish, dish_number: 1, dish_number_2: 5, dish_name: 'Chicken Tikka', dish_cost: 5 }
   let(:menu_double) { double :menu, dishes: [dish_double] }
   let(:order_double) { double :order, add: [dish_double], contents: [dish_double], cost: 5, remove: [] }
+  let(:order_double2) { double :order2, add: [dish_double], contents: [], cost: 5, remove: [] }
   let(:confirmation_double) { double :confirmation, send_confirmation: 'Hey James!, Your order has been confirmed' }
   before :each do
     @takeaway = Takeaway.new(menu_double, order_double, confirmation_double)
@@ -29,12 +30,23 @@ describe Takeaway do
         "Dish Number: 1, Name: Chicken Tikka, Price: 5\n"\
         "The total cost of your order is 5\n").to_stdout
       end
+
+      it 'fails if there is nothing in the current order' do
+        @takeaway = Takeaway.new(menu_double, order_double2, confirmation_double)
+        expect { @takeaway.view_order }.\
+        to raise_error "Nothing has been added to the current order!"
+      end
     end
     
     describe '#add to order' do
       it 'includes a subitted dish in the current order' do
         @takeaway.add_to_order(dish_double.dish_number, 2)
         expect(@takeaway.current_order.contents).to include dish_double
+      end
+
+      it 'fails if a dish number is given which is not on the menu' do
+        expect { @takeaway.add_to_order(dish_double.dish_number_2, 2) }.\
+        to raise_error "You provided an incorrect dish number!"
       end
     end
 
@@ -45,6 +57,11 @@ describe Takeaway do
         @takeaway.instance_variable_set(:@current_order, [])
         expect(@takeaway.current_order).not_to include dish_double
       end
+
+      it 'fails if a dish number is given which is not on the menu' do
+        expect { @takeaway.remove_from_order(dish_double.dish_number_2) }.\
+        to raise_error "You provided an incorrect dish number!"
+      end
     end
 
     describe '#confirm order' do
@@ -53,6 +70,12 @@ describe Takeaway do
        
         expect(confirmation_double).to receive(:send_confirmation).and_return('Hey James!, Your order has been confirmed')
         @takeaway.confirm_order
+      end
+
+      it 'fails if there is nothing in the current order' do
+        @takeaway = Takeaway.new(menu_double, order_double2, confirmation_double)
+        expect { @takeaway.confirm_order }.\
+        to raise_error "Nothing has been added to the current order!"
       end
     end
   end
