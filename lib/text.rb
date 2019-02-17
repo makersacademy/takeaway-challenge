096596d3ad572f74fd6c3466e37bc9bef562404d
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-17 16:37:09 +0000

    all tests passing

--- lib/text.rb
+++ lib/text.rb
@@ -5,18 +5,21 @@ class Text
   @account_sid = "AC436e6f2512545447559a7e2acec8acf5"
   @auth_token = "b122cff226597aba1477cbc7e1c0ab1a"
 
-  def initialize(client = nil)
-    @client = client || Twilio::REST::Client.new @account_sid, @auth_token
+  def initialize(client_class = Client)
+    client = client_class.new(account_sid = @account_sid, auth_token = @auth_token)
+    @client = client || ::Twilio::REST::client.new(account_sid, auth_token)
   end
 
   def deliver
-    client.messages.create
+    delivered_message = @client.messages.create
   end
 
   def create
-    body: "Thank you! Your order was placed and will be delivered before 18:52.",
-    to: "+447535866628",
-    from: "+441547220145"
+    {
+    from: "+441547220145",
+    to: "+447903616384",
+    body: "Thank you! Your order was placed and will be delivered before 18:52."
+    }
   end
 
   def del_time
