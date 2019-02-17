d01b772a535a0ec65fb24bb513c29d9d674303ef
Amina Jusic <amina_jusic@yahoo.com>
2019-02-17 15:20:27 +0000

    Update #Menu, edit list and prints of dishes with prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,19 +1,17 @@
-
-
 class Menu
 
   attr_reader :dishes
+
 
   def initialize(dishes)
     @dishes = dishes
   end
 
-  def show_menu
-  end
 
-private
-
-  def load_menu
+ def print
+    dishes.map do |title, price|  # we need to get a dishes. On Hash we can call map
+      '%s £%.2f' % [title.to_s.capitalize, price]  # map is gping through each value in Hash and changing in something different and we need to change into string
+    end.join(', ') #join this array
     # load up from file stuff
   end
 
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,4 @@
 require 'menu'
-
 # Show a list of dishes and prices
 # # when we create Menu object, we want to Menu object to pass into Dishes to make up the Menu
 
@@ -8,12 +7,17 @@ describe Menu do
   subject(:menu) { described_class.new(dishes) }# we can get dishes inside of menu with DEPENDENCY INJECTION
   let(:dishes) do
     {              #we need Hash because we want to change Menu and it's easier with Hashes
-      chicken: 4,
-      falafel: 5
+      chicken: 3.99,
+      falafel: 4.50
     }
   end
 
   it 'has a list of dishes and prices' do
     expect(menu.dishes).to eq(dishes)
+  end
+
+  it 'prints a list of dishes with prices' do
+    printed_menu = 'Chicken £3.99, Falafel £4.50'
+    expect(menu.print).to eq(printed_menu)
   end
 end
