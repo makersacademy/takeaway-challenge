76a2d672dd086d03896dd381de55f76d9dd20bd8
Steve Kahn <steve@kahn.org.uk>
2019-02-17 14:10:07 +0000

    Created tests for order total

--- feature_test.md
+++ feature_test.md
@@ -1,5 +1,6 @@
 require './lib/dish.rb'
 require './lib/menu.rb'
+require './lib/customer.rb'
 
 menu = Menu.new
 pepperoni = Dish.new("pepperoni",600)
@@ -10,3 +11,5 @@ menu.list
 customer = Customer.new
 customer.add_to_order(pepperoni, 1)
 customer.add_to_order(hotnspicy, 3)
+customer.order_total
+customer.reciept
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -6,17 +6,38 @@ describe Customer do
   before :each do
     @customer = Customer.new
   end
-  it "Has nothing in the order when created" do
-    expect(@customer.order).to eq []
+  context "when created" do
+
+    it "has nothing in the order" do
+      expect(@customer.order).to eq []
+    end
+
+    it "can return the total price of the order is 0" do
+      expect(@customer.order_total).to eq 0
+    end
   end
 
-  it "Can add a single dish to the order" do
+  it "can add a single dish to the order" do
     @customer.add_to_order(dish)
     expect(@customer.order).to eq [dish]
   end
 
-  it "Can add multiple of the same dish to the order" do
+  it "can add multiple of the same dish to the order" do
     @customer.add_to_order(dish, 2)
     expect(@customer.order).to eq [dish, dish]
+  end
+
+  it "has an order that costs £20 when £20 worth of dishes are ordered" do
+    allow(dish).to receive(:price).and_return(500)
+    dish2 = double(:dish2, :price => 1000)
+    @customer.add_to_order(dish, 2)
+    @customer.add_to_order(dish2, 1)
+    expect(@customer.order_total).to eq "£20.00"
+  end
+
+  it "has an order that costs £30 when £30 worth of dishes are ordered" do
+    allow(dish).to receive(:price).and_return(3000)
+    @customer.add_to_order(dish)
+    expect(@customer.order_total).to eq "£30.00"
   end
 end
