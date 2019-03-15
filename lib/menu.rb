c58eb19c44768312ca8734dfbf8bb005048c1cca
Student <student@makerss-mbp.home>
2019-03-15 21:47:49 +0000

    Playing with menus

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,29 +1,42 @@
 # Menu class
 class TakeawayMenu
   def initialize
-    splash_screen
+    @menu_options = MenuHeader.new
+    menu_header = MenuHeader.new
+    menu_header.print_header
+    @line_length = 61
+    splash = SplashScreen.new
+    splash.animation
+    menu(:main)
   end
 
   def menu_arrays(menu)
-    case menu
-    when :main
-      return ["Veggie", "Meaty", "Fishy"]
-    when :veggie
-
-    when :meat
-
-    when :fish
-
-    else
-
-    end
+    return ["Veggie", "Meaty", "Fishy", "Which category would you like to see?"] if menu == :main
   end
 
-  def main_menu(menu)
+  def menu(menu)
     case menu
     when :main
-      menu_arrays(:main).each_with_index { |item, index| puts "|" + index + item.center(40) + "|" }
-      puts "Which category would you like to see?"
+      menu_arrays(:main).each_with_index { |item, index| puts '|' + index + item.center(40) + '|' }
+      puts '-' * @line_length
+      option = gets.strip
+      @menu_options.options(:main, option)
     end
+  end
+end
+
+class MenuHeader
+  def print_header
+    system 'clear'
+    puts '-' * @line_length
+    puts '|'.ljust(22) + "Slice, slice baby".center(10) + "|".rjust(22)
+    puts '-' * @line_length
+  end
+end
+
+class MenuOptions
+
+  def options(menu, option)
+
   end
 end
--- lib/splash_screen.rb
+++ lib/splash_screen.rb
@@ -1,4 +1,6 @@
-def splash
+# Splachscreen
+
+def animation
   animation = ['welcome.txt', 'slice1.txt', 'slice2.txt', 'baby.txt']
   where = 0
   while where != 4
@@ -8,6 +10,12 @@ def splash
     sleep 1
     where += 1
   end
+  print_header
 end
 
-splash
+def print_header
+  system 'clear'
+  puts '-' * 61
+  puts '|'.ljust(22) + "Slice, slice baby".center(10) + "|".rjust(22)
+  puts '-' * 61
+end
