3880798ad81c4a9dd9eadf53f8c30acacd134422
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-15 20:45:29 +0000

    Added basic dish class with display and available? methods

---
+++ lib/dish.rb
@@ -0,0 +1,25 @@
+class Dish
+  def initialize(menu_number, description, price, available = true)
+    @menu_number = menu_number
+    @description = description
+    @price = price
+    @available = available
+  end
+
+  def display
+    if available?
+      "#{@menu_number}. #{@description} £#{@price}"
+    else
+      "Dish unavailable"
+    end
+  end
+
+  def make_unavailable
+    @available = false
+  end
+
+  private
+  def available?
+    @available
+  end
+end
