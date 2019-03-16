5104a589fc39afbbe5b9fb4e62b8452d966af918
Amy Jordan <amy_jordan@live.co.uk>
2019-03-16 10:03:05 +0000

    Created Takeaway class with print method

---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,18 @@
+require 'takeaway'
+
+describe Takeaway do
+
+  let(:menu) { double :menu }
+
+  subject { described_class.new(menu) }
+
+  it 'returns the menu' do
+    allow(menu).to receive(:print).and_return Menu::ITEMS
+    expect(subject.print_menu).to eq Menu::ITEMS
+  end
+
+  # unsure how to test this or whether to test this - it is behaviour of the menu class that I have already tested
+  # it 'prints the menu' do
+  #     allow(menu).to receive(:print).and_output("Nan: £2.20\nRice: £2.70\nAloo Gobi: £4.10\nSag Paneer: £4.30\nDaal Makhani: £6.10\n").to_stdout
+  # end
+end
