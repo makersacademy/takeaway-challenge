2c60498df036b74aa840cff975bca32cb85228f7
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-16 20:40:07 +0000

    Refactored order_list_spec to use a double for dish

--- spec/order_list_spec.rb
+++ spec/order_list_spec.rb
@@ -1,17 +1,7 @@
 require 'order_list'
 
 describe OrderList do
-  # let(:dish) { double :dish }
-  # let(:dish_class) { double :dish_class, new: dish }
-  # let(:menu) { Menu.new(dish_class) }
-  #
-  # context '#' do
-  #   it "" do
-  #     menu.add(dish)
-  #     dish_display = "36. Chicken Korma £7.95"
-  #     expect(dish).to receive(:display).and_return(dish_display)
-  #     expect(menu.view).to eq "#{dish_display}\n"
-  #   end
+  let(:dish) { double :dish }
 
   it "adds items to an order list" do
     expect(subject.add("Chicken Korma", 1)).to eq "Item added"
@@ -25,6 +15,8 @@ describe OrderList do
   it "checks the cost of an item" do
     current_price = 7.95
     subject.add("Chicken Korma", 1)
-    expect(subject.calculate_cost([Dish.new(36, "Chicken Korma", 7.95)])).to eq current_price
+    allow(dish).to receive(:price) { 7.95 }
+    allow(dish).to receive(:description) { "Chicken Korma" }
+    expect(subject.calculate_cost([dish])).to eq current_price
   end
 end
