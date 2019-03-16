3c35e7f976ba55574d7af01905f7dc0a8dca0bd0
Paul Martin <communty.hivemind@gmail.com>
2019-03-16 17:11:26 +0000

    reword some of the documentation

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -5,14 +5,14 @@ describe 'User Stories' do
   # As a customer
   # So that I can check if I want to order something
   # I would like to see a list of dishes with prices
-  context 'so i can check if i want to order something' do
-    it 'returns a list of menu items' do
+  context 'so I can check if I want to order something' do
+    it 'i can get a list of menu items' do
       expect(takeaway.menu).to_not be_empty
     end
-    it 'returns dishes with prices' do
+    it 'the menu items have prices' do
       expect(takeaway.menu).to all have_key :price
     end
-    it 'returns dishes with names' do
+    it 'the menu items have names' do
       expect(takeaway.menu).to all have_key :name
     end
   end
@@ -20,16 +20,15 @@ describe 'User Stories' do
   # As a customer
   # So that I can order the meal I want
   # I would like to be able to select some number of several available dishes
-  context 'so that i can order the meal i want' do
-    it 'i can select from the available dishes in a particular quantity' do
+  context 'so that I can order the meal I want' do
+    it 'I can select from particular quantities of the available dishes' do
       dish = takeaway.menu[3]
       takeaway.add_to_order(dish, 2)
       expect(takeaway.get_order.count(dish)).to eq 2 
     end
 
-    it 'if i order something not on the menu it raises an error' do
+    it 'it raises an error if I order something not on the menu' do
       expect { takeaway.add_to_order("", 1) }.to raise_error "Cannot add to order: item is not available. Select items from the menu."
     end
   end
-
 end
