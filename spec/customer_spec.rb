0bf09532990a0f723c946143ff9f0046620303fa
James Palmer <jamesdcpalmer1@gmail.com>
2019-03-16 14:22:04 +0000

    Test for adds two orders passed

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -23,6 +23,13 @@ describe Customer do
     expect(customer.contents).to eq [{ :name => "Pepperoni", :price => 7, :quantity => 2 }]
   end
 
+  it 'can add two orders to the array' do
+    customer.add("Meat feast", 1)
+    customer.add("Pepperoni", 2)
+    expect(customer.contents).to eq [{ :name => "Meat feast", :price => 8, :quantity => 1 },
+    { :name => "Pepperoni", :price => 7, :quantity => 2 }]
+  end
+
   it 'responds to basket method' do
     expect(customer).to respond_to(:basket)
   end
