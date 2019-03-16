7b4664e3699b99ae5991de518cdbb18dde21c54d
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-03-16 15:54:52 +0000

    Added tests for place_order method on Takeaway class and wrote code to pass

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,10 +1,15 @@
 class Takeaway
 
-  def initialize(menu = Menu.new)
+  def initialize(menu = Menu.new, order_class = Order)
     @menu = menu
+    @order = order_class
   end
 
   def view_menu
     @menu
+  end
+
+  def place_order(order_hash, total)
+    @order.new.create_order(order_hash, total)
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,9 +2,17 @@ require 'takeaway'
 
 describe Takeaway do
   let(:menu) { double :menu }
-  subject(:takeaway) { Takeaway.new(menu) }
+  let(:dish) { double :dish }
+  let(:order) { double :order }
+  let(:order_class) { double :order_class, new: order}
+  subject(:takeaway) { Takeaway.new(menu, order_class) }
 
   it "should return a menu object when view_menu method is called" do
     expect(takeaway.view_menu).to eq menu
+  end
+
+  it "should return order when place_order method is called succesfully" do
+    allow(order).to receive(:create_order) { order }
+    expect(takeaway.place_order({ dish: dish, number: 2 }, 10)).to eq order
   end
 end
