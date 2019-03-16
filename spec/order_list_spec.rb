c8b84aec4fe738109b207928ae624f99553056e7
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-16 20:32:49 +0000

    Added new test for order list to check calculate_cost. Needs refactoring to use doubles

--- spec/order_list_spec.rb
+++ spec/order_list_spec.rb
@@ -1,6 +1,17 @@
 require 'order_list'
 
 describe OrderList do
+  # let(:dish) { double :dish }
+  # let(:dish_class) { double :dish_class, new: dish }
+  # let(:menu) { Menu.new(dish_class) }
+  #
+  # context '#' do
+  #   it "" do
+  #     menu.add(dish)
+  #     dish_display = "36. Chicken Korma £7.95"
+  #     expect(dish).to receive(:display).and_return(dish_display)
+  #     expect(menu.view).to eq "#{dish_display}\n"
+  #   end
 
   it "adds items to an order list" do
     expect(subject.add("Chicken Korma", 1)).to eq "Item added"
@@ -9,5 +20,11 @@ describe OrderList do
   it "checks the last item added includes the item added" do
     subject.add("Chicken Korma", 1)
     expect(subject.check_last).to eq "Chicken Korma - 1"
+  end
+
+  it "checks the cost of an item" do
+    current_price = 7.95
+    subject.add("Chicken Korma", 1)
+    expect(subject.calculate_cost([Dish.new(36, "Chicken Korma", 7.95)])).to eq current_price
   end
 end
