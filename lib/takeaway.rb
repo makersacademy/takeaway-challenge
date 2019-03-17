8b0392c3952cc185be477fa3b14358f7426b7193
Wardah <wlt_93@hotmail.com>
2019-03-17 12:43:57 +0000

    Passes test for selecting number of dishes to order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,13 @@
-class Takeaway  
-  def show_menu
+class Takeaway
+  def initialize(menu:, order: nil)
+    @menu = menu
+    @order = order
   end
+
+  def place_order(dishes)
+    dishes.each do |dish,quantity|
+      @order.add(dish,quantity)
+    end
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,13 @@
 require 'takeaway'
 
 describe Takeaway do
+  subject(:takeaway) { Takeaway.new(menu: menu, order: order) }
+  let(:menu) {double :menu}
+  let(:order) {double :order}
+  let(:dishes) { {curry: 6, rice: 5, burger: 4, chips: 2, wings: 5} }
 
-  it { is_expected.to respond_to(:show_menu) }
-
+  it "selects a number of dishes to order" do
+    5.times { expect(order).to receive(:add) }
+    takeaway.place_order(dishes)
+  end
 end
