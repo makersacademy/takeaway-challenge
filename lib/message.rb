5bcc07177c4de85adfa7e38e63fda69de2435cfc
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-17 10:13:04 +0000

    Added first two methods to Message class

--- lib/message.rb
+++ lib/message.rb
@@ -1,1 +1,15 @@
 require 'dotenv/load'
+
+class Message
+
+  def send_text
+    "Message sent"
+  end
+
+  def create
+    delivery_window = 60*60 # 60*60 seconds = 1 hour
+    delivery_time = Time.now + delivery_window
+    arrival_time = "#{delivery_time.strftime('%H')}:#{delivery_time.strftime('%M')}"
+    "Thank you! Your order was placed and will be delivered before #{arrival_time}"
+  end
+end
