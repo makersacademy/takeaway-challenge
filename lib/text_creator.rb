3ecd8f1b90cdf8f7d23034c01c84263aed6423bc
masha-b <mashabudryte@gmail.com>
2019-03-17 16:41:18 +0000

    text creator created

---
+++ lib/text_creator.rb
@@ -0,0 +1,13 @@
+class TextCreator
+  def initialize(time = Time.now)
+    @time = time
+  end
+
+  def time_limit
+    "#{@time.hour+1}:#{@time.minutes}"
+  end
+
+  def text
+    "Thank you! Your order was placed and will be delivered before #{time_limit}"
+  end
+end
