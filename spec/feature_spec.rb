9a1de0db000d8c4732c942363abc184ba07fc70d
James Palmer <jamesdcpalmer1@gmail.com>
2019-03-17 13:45:41 +0000

    can add to basket

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,8 +4,8 @@ require 'menu_list'
 
 describe Takeaway do
 
-  let (:menu_list) {List.new}
-  let (:italian) {Takeaway.new}
+  let (:menu_list) { List.new }
+  let (:italian) { Takeaway.new }
   let(:customer) { Customer.new("Joe Bloggs") }
 
   describe '#display' do
@@ -15,15 +15,21 @@ describe Takeaway do
   end
 end
 
-
 describe Customer do
 
-  let (:menu_list) {List.new}
-  let (:italian) {Takeaway.new}
+  let (:menu_list) { List.new }
+  let (:italian) { Takeaway.new }
   let(:customer) { Customer.new("Joe Bloggs") }
 
   it 'has a name when initialized' do
     expect(customer.name).to eq 'Joe Bloggs'
+  end
+
+  describe '#add' do
+    it 'adds order to our basket' do
+      customer.add("Pepperoni", 2)
+      expect(customer.contents).to eq [{ :name => "Pepperoni", :price => 7, :quantity => 2 }]
+    end
   end
 
 end
