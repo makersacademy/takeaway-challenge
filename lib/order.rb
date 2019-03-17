0b90cf481327217f64923abcbe66335f1145560b
Wardah <wlt_93@hotmail.com>
2019-03-17 15:29:15 +0000

    Prevents non-existent items from getting added to the bucket

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,14 @@
 class Order 
-  attr_reader :bucket
+  attr_reader :bucket, :menu 
 
-  def initialize
+  def initialize(menu)
     @bucket = {}
+    @menu = menu 
   end
    
   def add(dish, quantity)
+    fail 'Not in menu!' if !menu.include(dish)
     @bucket[dish] = quantity
   end
+  
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,16 +1,28 @@
 require 'order'
 
 describe Order do
-  subject(:order) { Order.new }
-  
+  subject(:order) { Order.new(menu) }
+
+  let(:menu) {double(:menu) }
   let(:bucket) do
     { curry: 1, 
       chips: 2 }
-  end  
+  end
+
+  before do
+    allow(menu).to receive(:include).with(:curry).and_return(true)
+    allow(menu).to receive(:include).with(:chips).and_return(true)
+  end
 
   it "selects several dishes from the menu" do
     order.add(:curry, 1)
     order.add(:chips, 2)
     expect(order.bucket).to eq (bucket)
   end
+
+  it "prevents items from adding to bucket that are not in menu" do
+    allow(menu).to receive(:include).with(:pasta).and_return(false)
+    expect{order.add(:pasta, 1)}.to raise_error 'Not in menu!'
+  end
+  
 end
