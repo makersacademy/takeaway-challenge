3e9e9c57dedd71db22cc535e5b86840ddf689a0b
Amy Jordan <amy_jordan@live.co.uk>
2019-03-17 18:06:11 +0000

    Removed unnecessary mock and added comment on test coverage

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,4 +1,4 @@
-# 73.33% coverage on tests 
+# 73.33% coverage on tests
 # but not sure how to increase this as a lot of the working is from the Twilio gem
 
 require 'message'
@@ -8,7 +8,6 @@ describe Message do
   let(:auth_token) { double :auth_token }
   let(:twilio_number) { double :twilio_number }
   let(:customer_number) { double :customer_number }
-  let(:messsid) { double :message_sid }
   let(:client) { double :client }
 
   subject { described_class.new(account_sid, auth_token, twilio_number) }
