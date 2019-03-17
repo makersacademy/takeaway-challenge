ef6a3e616414081977126942cd966a674b30c330
Evelyn <evelyn@ipsoft.es>
2019-03-17 00:18:06 +0000

    Show list of dishes with prices

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,13 @@
 class Takeaway
   
+  attr_reader :dishes
+
+  def initialize
+    @dishes = { "rice": 1.25, "banana": 1.00, "fries": 2.10 }
+  end
+
+  def show_list
+    @dishes
+  end
+
 end
--- spec/takeway_spec.rb
+++ spec/takeway_spec.rb
@@ -2,5 +2,15 @@ require 'takeaway'
 
 describe Takeaway do
 
+# As a customer
+# So that I can check if I want to order something
+# I would like to see a list of dishes with prices
+
+  it 'should show list of dishes with prices' do
+    takeaway = Takeaway.new
+
+    expect(takeaway.show_list).to eq takeaway.dishes
+
+  end
 
 end
