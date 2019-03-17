e4d2fbef9bba3bcc22dbfeb301711edfa31de441
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-17 19:38:00 +0000

    removal

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,11 +1,6 @@
 require 'twilio-ruby'
 
-account_sid = 'AC7870dfcc2baf11c988ecbaede2774bff'
-auth_token = '161f609fbf99aa9c07a86abd937e110b'
-client = Twilio::REST::Client.new(account_sid, auth_token)
-
-from = '+447446151836' # Your Twilio number
-to = '+447851945027' # Your mobile phone number
+# Your mobile phone number
 
 client.messages.create(
 from: from,
