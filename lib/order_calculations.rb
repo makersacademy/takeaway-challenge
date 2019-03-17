3b1837553bd157d66d45ccf9fa55fa85e472481b
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-03-17 19:16:49 +0000

    Fixed error in order_calculations_spec file

--- lib/order_calculations.rb
+++ lib/order_calculations.rb
@@ -4,14 +4,14 @@ module OrderCalculations
     order_hash[:dish].price
   end
 
-  def self.dish_number(order_hash)
+  def self.dish_quantity(order_hash)
     order_hash[:quantity]
   end
 
   def self.calculate_total(order)
     cost = 0
     order.orderlines.each do |order_hash|
-      cost += dish_price(order_hash) * dish_number(order_hash)
+      cost += dish_price(order_hash) * dish_quantity(order_hash)
     end
     cost
   end
