728040318dac9cc4ea6be333a682665fc253eb6a
Amy Jordan <amy_jordan@live.co.uk>
2019-03-18 11:53:41 +0000

    Injected items list into Menu class and updated tests

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,22 +1,14 @@
 class Menu
   attr_reader :items
-  
-  ITEMS = {
-    "Nan" => 2.20,
-    "Rice" => 2.70,
-    "Aloo Gobi" => 4.10,
-    "Sag Paneer" => 4.30,
-    "Daal Makhani" => 6.10
-  }
 
-  def initialize(items = ITEMS)
+  def initialize(items)
     @items = items
   end
 
   def print
-    ITEMS.each do |item, price|
-      puts "#{item}: £#{'%.2f' % price}\n"
-    end
+    @items.map do |item, price|
+      "#{item}: £#{'%.2f' % price}\n"
+    end.join("")
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,11 +1,19 @@
 require 'menu'
 
 describe Menu do
+
+  let(:items) { {
+    "Nan" => 2.20,
+    "Rice" => 2.70,
+    "Aloo Gobi" => 4.10,
+    "Sag Paneer" => 4.30,
+    "Daal Makhani" => 6.10
+  } }
+
+  subject { described_class.new(items) }
+
   it 'print a list of menu items' do
-    expect { subject.print }.to output("Nan: £2.20\nRice: £2.70\nAloo Gobi: £4.10\nSag Paneer: £4.30\nDaal Makhani: £6.10\n").to_stdout
+    expect(subject.print).to eq "Nan: £2.20\nRice: £2.70\nAloo Gobi: £4.10\nSag Paneer: £4.30\nDaal Makhani: £6.10\n"
   end
 
-  it 'returns a list of menu items' do
-    expect(subject.print).to eq Menu::ITEMS
-  end
 end
