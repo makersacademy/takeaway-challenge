c53e56cdfab78c2d53f654705719152eabf39818
Deanna Green <deanna@16vmini.co.uk>
2019-03-29 12:24:37 +0000

    merged in code to send sms

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,4 @@
 require 'menu'
-require 'send_sms'
 
 class Order
   def initialize
@@ -13,4 +12,22 @@ class Order
     }
     @total
   end
+
+  def send_text
+    @client.messages.create
+  end
 end
+
+require("bundler")
+Bundler.require()
+
+account_sid = ENV["TWILIO_ACCOUNT_SID"]
+auth_token = ENV["TWILIO_AUTH_TOKEN"]
+
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+@client.messages.create(
+  to: ENV["MY_PHONE_NUMBER"],
+  from: "+441594372067",
+  body: "Your order will be delivered in an hour"
+  )
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,7 +2,37 @@ require 'order'
 
 describe Order do
   let(:order) { [{ dish: 'fish', price: 5 }] }
+  
   it 'checks total of order' do
     expect(subject.check_total(order)).to eq 5
   end
+
+  it 'sends a text' do
+    expect(subject).to respond_to(:send_text)
+  end
 end
+
+
+#class FakeSMS
+  #Message = Struct.new(:from, :to, :body)
+
+  #cattr_accessor :messages
+  #self.messages = []
+
+  #def initialize(_account_sid, _auth_token)
+  #end
+
+  #def messages
+  #  self
+  #end
+
+  #def create(from:, to:, body:)
+  #  self.class.messages << Message.new(from: from, to: to, body: body)
+  #end
+#end
+
+#RSpec.configure. do |config|
+  #config.before(:each) do
+  #  stub_const("Twilio::REST::Client", FakeSMS)
+  #end
+#end
