037b3aa77d77103e5da04c4aa64ee7d3dcb7d2f8
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-29 11:22:07 +0000

    added order class and tests

--- lib/user_menu.rb
+++ lib/user_menu.rb
@@ -1,34 +1,68 @@
-
+require_relative 'food_menu'
+require_relative 'order'
 
 class UserMenu
 
+  def initialize
+    @food_order = Order.new
+  end
+
   def print_options
-  #  loop do
-      return options
-  #    process_menu_selection(STDIN.gets.chomp)
-  #  end
+    puts options
+    process_menu_selection(STDIN.gets.chomp)
   end
 
   def options
-  "
+  return "Please Select An Option\n
   1. Show Menu\n
   2. Order\n
-  3. Total Cost\n
-  4. Place Order\n
+  3. Show Order\n
+  4. Total Cost\n
+  5. Place Order\n
   "
   end
 
   def process_menu_selection(option)
-    if option = 1
-      "Showing Menu"
-    elsif option = 2
-      "Order Your Meal"
-    elsif option = 3
-      "Showing Total Cost of Order"
-    elsif option = 4
-      "Finalise Your Order"
+    if option == "1"
+      puts "Showing Menu"
+      return show_menu
+    elsif option == "2"
+      puts "Order Your Meal"
+      return order_food
+    elsif option == "3"
+      puts "Showing Your Order"
+      return show_order
+    elsif option == "4"
+      return "Showing Total Cost of Order"
+    elsif option == "5"
+      return "Finalise Your Order"
+    elsif option == "exit"
+      exit
     else
       "Please select an option 1 - 4"
+      print_options
     end
   end
+
+  def show_menu
+    fm = FoodMenu.new
+    puts fm.print
+    print_options
+  end
+
+  def order_food
+    puts "Enter Menu Number and Qty"
+    puts @food_order.take_order
+    print_options
+  end
+
+  def show_order
+    puts "Here is What You Have Ordered"
+    puts @food_order.show_orders
+    print_options
+  end
+
 end
+
+#um = UserMenu.new
+#um.print_options
