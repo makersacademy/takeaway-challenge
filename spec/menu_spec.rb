7c017e01c4fc97f148e07a7e4b1d054b85a7eb27
Deanna Green <deanna@16vmini.co.uk>
2019-03-29 18:25:35 +0000

    extra test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,6 +2,7 @@ require 'menu'
 
 describe Menu do
   let(:dish) { ["fish", 5, 1] }
+  let(:dish2) { ["burger", 7, 1] }
 
   it 'has a menu' do
     expect(subject.menu).to include dish
@@ -21,6 +22,12 @@ describe Menu do
     it 'can order a dish' do
       subject.order_dishes("fish")
       expect(subject.order).to eq [dish]
+    end
+
+    it 'can order multiple items' do
+      subject.order_dishes("fish")
+      subject.order_dishes("burger")
+      expect(subject.order).to eq [dish, dish2]
     end
   end
 end
