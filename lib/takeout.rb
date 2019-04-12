a165f66148c8080ed8532fd3bcc93b705c2d49ae
Hannah Frost <h.7frost@gmail.com>
2019-04-12 13:50:57 +0100

    Raises error if item not on menu is selected, test passes

--- lib/takeout.rb
+++ lib/takeout.rb
@@ -2,9 +2,16 @@ require_relative 'takeout_menu'
 
 class Takeout
 
-  def show_menu
-    menu = TakeoutMenu.new
-    menu.menu_list
+  def initialize
+    @takeout_menu = TakeoutMenu.new
   end
-  
+
+  def show_menu
+    @takeout_menu.menu_list
+  end
+
+  def select(food)
+    raise "That item is not on the menu!" if !@takeout_menu.menu.include?(food)
+  end
+
 end
--- spec/takeout_spec.rb
+++ spec/takeout_spec.rb
@@ -8,4 +8,9 @@ describe Takeout do
       expect(subject.show_menu).to eq menu.menu_list
     end
   end
+  describe '#select' do
+    it 'returns error if selected food is not on the menu' do
+      expect { subject.select("foobar") }.to raise_error "That item is not on the menu!"
+    end
+  end
 end
