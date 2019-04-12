ab2c1a21440d143ce6aed60d46d4f4debe95cc6a
Hannah Frost <h.7frost@gmail.com>
2019-04-12 12:49:56 +0100

    Menu_list method added.

---
+++ lib/takeout_menu.rb
@@ -0,0 +1,20 @@
+class TakeoutMenu
+
+  def menu_list
+    puts " "
+    puts "~~~~~~~~~~ MENU ~~~~~~~~~~".center(40)
+    puts " "
+    puts "--- Sides ---".center(40)
+    puts "Mozzarella Sticks : £3.49".center(40)
+    puts "Garlic Bread : £3.99".center(40)
+    puts "Chicken Bites : £2.99".center(40)
+    puts " "
+    puts "--- Mains ---".center(40)
+    puts "Margherita Pizza : £12.99".center(40)
+    puts "Hawaiian Pizza : £15.99".center(40)
+    puts "Pepperoni Pizza : £16.99".center(40)
+    puts "Vegetarian Pizza : £16.99".center(40)
+    puts " "
+  end
+
+end
