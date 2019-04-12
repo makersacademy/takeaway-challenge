a1fdba6d2419298b79668c5517a53f1ebbf332a7
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-04-12 11:59:04 +0100

    added list of dishes with prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,9 @@
 class Menu
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = [{ 'pizza': 6.00, 'pasta': 6.50 }]
+  end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,1 +1,10 @@
 require 'menu'
+
+describe Menu do
+
+  describe '#initialize' do
+    it 'has a list of dishes with prices' do
+      expect(subject.dishes).not_to be_empty
+    end
+  end
+end
