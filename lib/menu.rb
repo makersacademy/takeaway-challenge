9acb70d83fc302931e0d90489ba24278fa0f0d0d
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-12 14:08:17 +0100

    tried to add method to count each item in the array

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,6 @@
 class Menu
 
-  attr_reader :dishes, :order
+  attr_reader :dishes, :order, :quantity
 
   def initialize
     @dishes = [{ name: "Pizza", price: 6.00 },
@@ -10,10 +10,21 @@ class Menu
       { name: "Lobster", price: 25.00 },
       { name: "Oysters", price: 20.00 }]
     @order = []
+    @quantity = []
   end
 
   def take_order(item)
     @order << item
+  end
+
+  def quantities
+    @order.map {|item| item[:price]}
+    # @quantity << @order.count("Pizza")
+    # @quantity << @order.count("Pasta")
+    # @quantity << @order.count("Chips")
+    # @quantity << @order.count("Burger")
+    # @quantity << @order.count("Lobster")
+    # @quantity << @order.count("Oysters")
   end
 
 end
