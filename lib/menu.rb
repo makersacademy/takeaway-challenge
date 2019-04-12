7fcb2af8d8470f6216bebc5bf9dde733c04e3c05
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-04-12 12:13:26 +0100

    added list of dishes with prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,7 +3,12 @@ class Menu
   attr_reader :dishes
 
   def initialize
-    @dishes = [{ name: "pizza", price: 6.00 }, { name: "pasta", price: 6.50 }]
+    @dishes = [{ name: "pizza", price: 6.00 },
+      { name: "pasta", price: 6.50 },
+      { name: "chips", price: 3.00 },
+      { name: "halloumi fries", price: 5.50 },
+      { name: "calamari", price: 7.00 },
+      { name: "bruschetta", price: 3.50 }]                             
   end
 
 end
