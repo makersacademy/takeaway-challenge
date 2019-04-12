e2e738224e8130f9f3c212207145deb4bd4b5151
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-12 12:07:58 +0100

    created a more extensive menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,7 +3,12 @@ class Menu
   attr_reader :dishes
 
   def initialize
-    @dishes = [{ name: "Pizza", price: 6.00 }, { name: "Pasta", price: 8.00 }]
+    @dishes = [{ name: "Pizza", price: 6.00 },
+      { name: "Pasta", price: 8.00 },
+      { name: "Chips", price: 3.00 },
+      { name: "Burger", price: 10.00 },
+      { name: "Wrap", price: 4.00 },
+      { name: "Oysters", price: 20.00 }]
   end
 
 end
