14cf4399eb69ec5abb39663fb3f9232249f8bdf4
domw30 <domw30@gmail.com>
2019-04-14 09:55:13 +0100

    refactor takeaway tests to improve readability

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,24 +1,28 @@
 require 'takeaway'
-require 'order'
 
 describe Takeaway do
+
+  let(:pizza) { { name: :pizza, price: 8 } }
+  let(:order) { { total_price: 0, select_pizza: [] } }
+  subject(:takeaway) { described_class.new }
+
+
   context 'To check if User wants to order something' do
-    it 'creates a list of pizzas with prices' do
-      pizza = double('pizza', :name => 'pizza', :price => 8)
-      allow(subject).to receive(:pizzas).and_return([pizza])
-      expect(subject.pizzas).to include pizza
+    it 'creates a menu of pizzas with prices' do
+      allow(takeaway).to receive(:select_pizza).and_return([pizza])
+      expect(takeaway.select_pizza).to include pizza
     end
   end
 
   context 'So User can order the pizza they want' do
     it 'allows User to create an order list' do
-      expect(subject.create_order).to be_a Order
+      expect(takeaway.create_order).to be_a Order
     end
 
     it 'allows User to confirm order' do
-      subject.create_order
-      subject.confirm_order
-      expect(subject.order.confirm_order?).to eq true
+      takeaway.create_order
+      takeaway.confirm_order
+      expect(takeaway.order.confirm_order?).to eq true
     end
   end
 end
