e389a56e25b949946a7df8d96637288d2fd87f36
Jose Quinones <josemanuelqn.88@gmail.com>
2019-04-14 19:58:18 +0100

    Create take_away file and set the tests for take away class

---
+++ spec/take_away_spec.rb
@@ -0,0 +1,19 @@
+require 'take_away'
+
+describe TakeAway do
+  describe '#show_menu' do
+    it 'prints the menu'
+  end
+
+  describe '#order_dish' do
+    it 'adds a dish to the order'
+  end
+
+  describe '#receipt' do
+    it 'prints the receipt with the total'
+  end
+
+  describe '#confirm_order' do
+    it 'sends confirmation text'
+  end
+end
