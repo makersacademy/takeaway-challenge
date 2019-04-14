57a6b5cf083dee61fa12c9897bc3d5704fb42094
yadlra <yadi.sb1@gmail.com>
2019-04-14 13:47:32 +0100

    raises error if meal not in the menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,13 @@
 class Order
-attr_reader :basket
+attr_reader :basket, :menu
 
-  def initialize
+  def initialize(menu)
     @basket = {}
+    @menu = menu
   end
 
   def add(meal, quantity)
+    raise "Not in the menu!" if !menu.include(meal)
     @basket[meal] = quantity
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,14 +1,26 @@
 require 'order'
 describe Order do
-  subject(:order) { Order.new }
+  subject(:order) { Order.new(menu) }
 
+  let(:menu) { double(:menu) }
   let(:basket) do
     {tacos: 1, burrito: 2}
   end
+
+before do
+  allow(menu).to receive(:include).with(:tacos).and_return(true)
+  allow(menu).to receive(:include).with(:burrito).and_return(true)
+end
+
 
  it 'adds meals to basket from the menu' do
    order.add(:tacos, 1)
    order.add(:burrito, 2)
   expect(order.basket).to eq basket
+ end
+
+ it 'raises error if meal is not in the menu' do
+   allow(menu).to receive(:include).with(:chimichanga).and_return(false)
+   expect {order.add(:chimichanga, 1)}.to raise_error "Not in the menu!"
  end
 end
