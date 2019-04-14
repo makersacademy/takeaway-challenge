3317108a3c9932438e66511c50327855f14a4ee5
sarpong <abasimisarpong@rocketmail.com>
2019-04-14 22:37:44 +0100

    order object working

--- lib/order.rb
+++ lib/order.rb
@@ -5,10 +5,20 @@ class Order
     def initialize(menu)
       @users_order = Hash.new
       @menu = menu
+      @total ||= 0
     end
 
     def user_choice(dish, quantity)
       raise 'choice not found' if @menu.dishes[dish] == nil
+      total_order(dish,quantity)
       @users_order[dish] = quantity
+      puts "You ordered #{dish} x#{quantity}"
+      puts total_order(dish,quantity)
     end
+
+    def total_order(dish,quantity)
+      @total +=  @menu.dishes[dish] * quantity
+      "Your total order is currently £%.2f" % [@total]
+    end
+
 end
