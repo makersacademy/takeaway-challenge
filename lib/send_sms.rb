61ccb8353d175a63ceece3c75b8034332bc84ad9
shektor <44544977+shektor@users.noreply.github.com>
2019-04-14 23:42:05 +0000

    Create SMS class and test

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,14 +1,22 @@
 require 'twilio-ruby'
+require 'dotenv/load'
 
-account_sid = ENV['TWILIO_ACCOUNT_SID']
-auth_token = ENV['TWILIO_AUTH_TOKEN']
-client = Twilio::REST::Client.new(account_sid, auth_token)
-
-from = ENV['TWILIO_TRIAL_NUMBER'] # Your Twilio number
-to = ENV['TWILIO_VERIFIED_NUMBER'] # Your mobile phone number
-
-client.messages.create(
-from: from,
-to: to,
-body: "Twilio test!"
-)
+class SendSMS
+  
+  def initialize
+    @account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @auth_token = ENV['TWILIO_AUTH_TOKEN']
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+      
+    @from = ENV['TWILIO_TRIAL_NUMBER'] # Your Twilio number
+    @to = ENV['TWILIO_VERIFIED_NUMBER'] # Your mobile phone number
+  end
+  
+  def send
+    @client.messages.create(
+      from: @from,
+      to: @to,
+      body: "Twilio test!"
+      )
+  end
+end
