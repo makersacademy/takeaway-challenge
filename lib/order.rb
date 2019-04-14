4682777dad3dfdefb18b51ecaa7cad05a685a18e
Jordan <jordanroberts@talk21.com>
2019-04-14 11:25:15 +0100

    Time now sends 1 hour later

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,5 @@
 require_relative 'menu'
+require_relative 'sms'
 
 class Order
 
--- lib/sms.rb
+++ lib/sms.rb
@@ -5,11 +5,10 @@ require_relative 'order'
 class Sms
 
   def send_text
-    # (order)
     time = Time.new
-
+    later_time = Time.now + 1 * 60 * 60
     message = "Thank you for your order. It will be delivered at
-    #{time.hour}:#{time.min}"
+    #{later_time.hour}:#{time.min}"
     Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     ).messages.create(
       from: ENV['TWILIO_PHONE'],
