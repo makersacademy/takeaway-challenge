0d7b61bb94954e75483a50239b64773c45a382c5
sarpong <abasimisarpong@rocketmail.com>
2019-04-14 11:02:25 +0100

    adding take_away test

--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -1,13 +1,22 @@
+
 require 'take_away'
 
 RSpec.describe TakeAway do
-  let(:menu) { double :Menu, show_dish: 'keykey and shito £4' }
-
-  let(:take_away) { described_class.new(menu) }
-
-  describe '#show_menu' do
-    it 'shows list of menu' do 
-      expect(take_away.show_menu).to eq(menu.show_dish)
+  let(:printed_dish) {'keykey £2.49, waakye £3.99'}
+  let(:menu) { double :menu, :list_dishes => printed_dish }
+  let(:take_away) { described_class.new(menu)}
+  
+  let(:dish_on_menu) do
+    {
+      kenkey: 2.49,
+      waakey: 3.99
+    }
+  end
+   
+  describe 'show_menu' do
+    it 'must be able to show menu' do
+      
+      expect(take_away.show_menu).to eq(printed_dish)
     end
   end
 end
