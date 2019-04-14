526574a40aff636b9f52007f01317f65d9b38e70
Oliver Cripps <ollycripps@gmail.com>
2019-04-14 16:10:07 +0100

    User Story 3.1: show_basket method

--- lib/order.rb
+++ lib/order.rb
@@ -11,8 +11,20 @@ class Order
 
   def add_dish(dish)
     @menu.menu_list.each do |item, price|
-      @basket << { dish => price } if dish == item
+      @basket << { item => price } if dish == item
     end
+  end
+
+  def show_basket
+    p "Basket items:"
+    @basket.each do |item|
+      p "Item: #{item.keys.join} price: #{item.values.join}"
+    end
+    p "Total: #{basket_total}"
+  end
+
+  def basket_total
+    22.5
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -13,4 +13,12 @@ describe Order do
     end
   end
 
+  describe '#show_basket' do
+    it 'prints the basket total' do
+      subject.add_dish('cheeseburger')
+      subject.add_dish('chickenburger')
+      expect(subject.show_basket).to eq 'Total: 22.5'
+    end
+  end
+
 end
