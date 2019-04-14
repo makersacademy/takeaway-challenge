d3b8071624644449210195d4762eb11797f46b67
Jose Quinones <josemanuelqn.88@gmail.com>
2019-04-14 21:34:50 +0100

    Create order.rb

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,2 +1,13 @@
 class TakeAway
+
+  def initialize
+    @order = {}
+  end
+
+  def show_menu
+    "Pizza - £5\nPasta - £7\nSalad - £6"
+  end
+
+  def order_dish(dish, qty)
+  end
 end
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -2,11 +2,18 @@ require 'take_away'
 
 describe TakeAway do
   describe '#show_menu' do
-    it 'prints the menu'
+    it 'prints the menu' do
+      menu = "Pizza - £5\nPasta - £7\nSalad - £6"
+      expect(subject.show_menu).to eq menu
+    end
   end
 
   describe '#order_dish' do
-    it 'adds a dish to the order'
+    it 'adds dish and qty to the order' do
+      subject.order_dish('pizza', 3)
+      pending('order should respond to add_dish')
+        raise 'pending'
+    end
   end
 
   describe '#receipt' do
