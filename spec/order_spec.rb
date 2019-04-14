db499db5331d9defad9475309d5999ce9d070d7b
shektor <44544977+shektor@users.noreply.github.com>
2019-04-14 20:24:37 +0000

    Refactor price_list to use double

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,15 +1,20 @@
 require 'order'
 
 describe Order do
+  let(:price_list_double) { double :price_list }
   let(:subject) { 
-    dishes_double = double(:dishes, price_list: { fish: 5, chips: 2, curry_sauce: 1 })
+    dishes_double = double(:dishes, price_list: :price_list_double)
     described_class.new(dishes_double.price_list)
   }
 
   
   describe '#initialize' do
     it 'accepts and assigns a Dishes price list' do
-      expect(subject.price_list).to eq({ fish: 5, chips: 2, curry_sauce: 1 })
+      expect(subject.price_list).to eq :price_list_double
     end
   end
+  
+  # descibe '#create' do
+    
+  # end
 end
