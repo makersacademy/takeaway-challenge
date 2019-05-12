2dda9d851ab78a242afb14b1b5adf0eb3b42e660
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-12 16:10:56 +0100

    refactoring for ease of development, initialised menu within menu class, rather than passing as argument

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,17 @@
 class Menu
 
-  def initialize(dishes)
-    @dishes = dishes
+  attr_reader :dishes
+
+  def initialize
+    @dishes = { burrito: '6.80'}
   end
 
   def view
     @dishes
+  end
+
+  def available?(meal)
+    @dishes.to_s.include?(meal) ? true : false
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,12 +2,22 @@ require 'menu'
 
 describe Menu do
 
-  let(:menu) { Menu.new(dishes) }
-  let(:dishes) { { burrito: '£6.80' } }
+  let(:menu) { Menu.new }
+  let(:dish) { "burrito" }
+  let(:notdish) { "burger" }
 
   describe "#show" do
     it "should show a dish with a price" do
-      expect(menu.view).to eq(dishes)
+      expect(menu.view).to eq(menu::dishes)
+    end
+  end
+
+  describe "#available?" do
+    it "should return true if the dish is on the menu" do
+      expect(menu.available?(dish)).to eq(true)
+    end
+    it "should return false if the dish is not on the menu" do
+      expect(menu.available?(notdish)).to eq(false)
     end
   end
 end
