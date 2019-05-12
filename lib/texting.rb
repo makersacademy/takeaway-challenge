1aef63b769ed1c9e3f4dfeb334bee0015b0eb229
Nicholas Barnes <n3ttl3t@gmail.com>
2019-05-12 19:04:07 +0100

    changed texting to use environment variables for my phone number etc

--- lib/texting.rb
+++ lib/texting.rb
@@ -4,17 +4,20 @@ class Texting
 
   attr_reader :delivery_time
 
-  NUMBER = '+447969797101'
+  SID = ENV['TWILIO_ACCOUNT_SID']
+  AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
+  T_NUMBER = ENV['TWILIO_NUMBER']
+  NUMBER = ENV['MY_NUMBER']
 
-  def initialize(send_to_number = NUMBER)
+
+  def initialize
     @delivery_time = delivery_time
 
-    @account_sid = 'AC39add806aa7238c6a3a1985d07d48fee'
-    @auth_token = '4b64bcec1af317515d12218801a5c491'
+    @account_sid = SID
+    @auth_token = AUTH_TOKEN
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
-    @from = '+447723445237' # Your Twilio number
-    @to = send_to_number # Your mobile phone number
-  end
+    @from = T_NUMBER
+    @to = NUMBER 
 
   def delivery_time
     "#{Time.new.hour + 1}.#{Time.new.min}"
