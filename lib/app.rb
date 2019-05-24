4cbe488cf0845fcec167c6f1370842bed78bbdc3
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-24 12:06:02 +0100

    yasss passing test with doubles

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,14 @@
 class App
-  
-  def display_dishes(list)
+  attr_reader :list
+  def initialize(list_class = DishList)
+    @list = list_class.new
+  end 
+
+  def display_dishes
     list.display
   end
   
-  def select_dish(list, item, quantity)
+  def select_dish(item, quantity)
     list.select(item, quantity)
   end 
 end
