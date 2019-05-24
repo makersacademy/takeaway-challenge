6a63d2f3f4df22d4236d6ff4ac06a5af5b63ae88
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-05-24 13:21:04 +0100

    Adds Price calculation per dish

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,6 @@
 # Allows user to create an order, add to their basket, display their basket and display a total price" 
+require_relative 'menu'
+
 class Order
   attr_reader :basket_summary
 
@@ -13,15 +15,23 @@ class Order
   
   def total_cost
     @basket_summary.map do |dish|
-      @total += dish.dish_cost 
+      @total += dish.price_calc
     end
   end
 
-  def dish_quantity
+  def price_calc
+    menu = Menu.new
     @basket_summary.each do |food,quantity|
       qty = @basket_summary[food]
-      puts qty
+      price = menu.price(food)
+      cost = price * qty
+      puts cost       
     end
+  end
+# OUTSTANDING - add dishes price calculation (i.e. qty * dish price, per menu class items) 
 end
-# add dishes price calculation (i.e. qty * dish price, per menu class items) 
-end
+
+o = Order.new
+o.add("pizza",3)
+puts o.basket_summary
+o.price_calc
