ef8c72342d3fc3146bc40bffa2b7296908106a30
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-24 12:02:52 +0100

    working test for app telling list to select the dish, need to initialise with list

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,10 @@
 class App
+  
   def display_dishes(list)
     list.display
   end
+  
+  def select_dish(list, item, quantity)
+    list.select(item, quantity)
+  end 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,17 +1,17 @@
 require 'app'
 
 describe App do 
-  let(:app) {App.new}
-  # let(:list_class) {double(:dish_list_class, new => list)}
-  let(:list) { double(:dish_list)}
+  let(:app) { App.new }
+  let(:list_class) {double(:list_class, new => list)}
+  let(:list) { double(:list, :items => {item: "Margherita", price: 10 })}
 
   it 'asks the dishlist to display list' do 
     expect(list).to receive(:display)
     app.display_dishes(list)
   end 
 
-  # it 'asks the dish_list to select the item and quantity' do 
-  #   expect(list).to receive(:select)
-  #   app.select_dish("Item", 2)
-  # end
+  it 'asks the dish_list to select the item and quantity' do 
+    expect(list).to receive(:select)
+    app.select_dish(list, "Item", 2)
+  end
 end 
