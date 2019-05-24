ca8d363bff66804847fa9b4e4f3e3eaa1f7f5657
Megan Goode <megan.e.goode@hotmail.com>
2019-05-24 10:47:41 +0100

    Move dishes menu into order, should be created on initialize of order

---
+++ lib/order.rb
@@ -0,0 +1,19 @@
+class Order
+  attr_reader :menu
+
+  def initialize
+    @menu = [
+      {dish: "Jasmine Rice", price: 2.50},
+      {dish: "Coconut Rice", price: 3.50},
+      {dish: "Egg Noodles", price: 4.75},
+      {dish: "Egg Fried Rice", price: 3.50},
+      {dish: "Chicken Red Thai Curry", price: 4.75},
+      {dish: "Pork Green Thai Curry", price: 5.75},
+      {dish: "Stir-fry Duck with Ginger", price: 5.75},
+      {dish: "Stir-fry Beef with Mushroom", price: 5.75},
+      {dish: "Stir-fry Squid with Veg", price: 5.75},
+      {dish: "Mixed Seafood", price: 4.75},
+      {dish: "Spring Rolls (6)", price: 3.50}
+      ]
+  end
+end
