cb3d3454d273af987c77510153641a2473dd4835
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-05-24 10:54:14 +0100

    Adds Order features to add items to basket

--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,13 @@
 class Order
-
+  attr_reader :basket_summary
+  
   def initialize
-    @basket_summary = []
+    @basket_summary = Hash.new(0)
     @total = 0
   end
 
-
+  def add(dish, quantity = 1)
+    @basket_summary[dish] += quantity
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,24 +4,25 @@ require 'order'
     subject         { Order.new }
     # let(:pasta)     { double(:pasta)}
   
-    it 'can display a basket summary' do
-      expect(subject.basket_summary).to eq()
+    it 'basket summary is empty by default' do
+      expect(subject.basket_summary).to eq({})
     end
 
     it 'can add an item to basket' do
-      subject.add(pasta)
+      subject.add("pasta")
       expect(subject.basket_summary).to include({"pasta" => 1})
     end
   
     it 'can add multiple items to basket' do
-      subject.add(pasta, 3)
-      expect(subject.basket_summary).to include({"pasta" => 3})
+      subject.add("pasta", 3)
+      subject.add("bread", 2)
+      expect(subject.basket_summary).to include({"pasta" => 3}, {"bread" => 2})
     end
   
-    it 'can add price of items to total' do
-      subject.add(pasta)
-      expect{subject.add(pasta)}.to change{subject.total}.by(4.99)
-    end
+    # it 'can add price of items to total' do
+    #   subject.add(pasta)
+    #   expect{subject.add(pasta)}.to change{subject.total}.by(4.99)
+    # end
 
     # it 'can display total' do
     #   expect
