55eb3c03d8f0e219ae84320c85b2f5758410848d
Geoff Smith <smith260194@gmail.com>
2019-05-25 19:53:35 +0100

    Adding a feature test

---
+++ spec/feature_test.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require './lib/menu.rb'
+require './lib/order.rb'
+require './lib/sms_sender.rb'
+require './lib/takeaway.rb'
+
+my_menu = Menu.new
+my_menu.load_items('./lib/menus/italian.csv')
+my_sms = SmsSender.new
+my_takeaway = Takeaway.new(my_menu, Order, my_sms)
+
+
+my_takeaway.start_new_order
+my_takeaway.order_item('pizza')
+my_takeaway.order_item('salad')
+my_takeaway.order_item('bruschetta')
+my_takeaway.show_total
+my_takeaway.place_order("£14.50")
