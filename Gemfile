e720e1ed39347b92e21c17a86a43e3dfc7adc6cd
Geoff Smith <smith260194@gmail.com>
2019-05-25 20:28:15 +0100

    Adding the use of environment variables

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'capybara'
+gem 'figaro'
 gem 'money'
 gem 'rake'
 gem 'rspec'
@@ -8,4 +9,3 @@ gem 'rubocop', '0.56.0'
 gem 'simplecov', require: false, group: :test
 gem 'simplecov-console', require: false, group: :test
 gem 'twilio-ruby', '~> 5.23.0'
-gem 'figaro'
--- lib/sms_sender.rb
+++ lib/sms_sender.rb
@@ -2,10 +2,10 @@ require 'twilio-ruby'
 
 class SmsSender
   def initialize(sms_client = Twilio::REST::Client, time = Time)
-    @account_sid = 'ACb82ed782e6b2dc3cbd58c57a3593c602'
-    @auth_token = 'bfde0287d71df74cbc76ee13b08c5da0'
-    @from = '+441183246108' # Your Twilio number
-    @to = '+447468462500'
+    @account_sid = ENV.fetch('ACCOUNT_SID')
+    @auth_token = ENV.fetch('AUTH_TOKEN')
+    @from = ENV.fetch('TWILIO_NUMBER') # Your Twilio number
+    @to = ENV.fetch('GEOFFS_NUMBER')
     @client = sms_client.new(@account_sid, @auth_token)
     @time = time.new
   end
