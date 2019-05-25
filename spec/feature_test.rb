e45f232d5145ce11f0107b67d7296a00972a2e29
Geoff Smith <smith260194@gmail.com>
2019-05-25 20:17:11 +0100

    Minor changes

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 require './lib/menu.rb'
 require './lib/order.rb'
 require './lib/sms_sender.rb'
@@ -10,9 +9,9 @@ my_menu.load_items('./lib/menus/italian.csv')
 my_sms = SmsSender.new
 my_takeaway = Takeaway.new(my_menu, Order, my_sms)
 
-my_takeaway.start_new_order
-my_takeaway.order_item('pizza')
-my_takeaway.order_item('salad')
-my_takeaway.order_item('bruschetta')
-my_takeaway.show_total
-my_takeaway.place_order("£14.50")
+puts my_takeaway.start_new_order
+puts my_takeaway.order_item('pizza')
+puts my_takeaway.order_item('salad')
+puts my_takeaway.order_item('bruschetta')
+puts my_takeaway.show_total
+puts my_takeaway.place_order("£14.50")
