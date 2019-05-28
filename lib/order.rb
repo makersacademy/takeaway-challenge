5d63d0a3ef0e54c3831a7c071d762bff9bcfa784
Manisha Patel <ma9shap.gmail.com>
2019-05-28 09:23:46 +0100

    fixed to get the total value for all items ordered

--- lib/order.rb
+++ lib/order.rb
@@ -3,41 +3,31 @@ require './lib/menu.rb'
 class Order
 
 attr_reader :list_of_dishes, :selected_items
+
 def initialize(m = Menu.new)
- 
   @list_of_dishes = m
   @selected_items = []
 end
+
 def view_dishes
   @item = @list_of_dishes.list
 end
 
 def select_dishes(item, quantity)
   @total = 0
+  i = 0
   selection = @list_of_dishes.list.select do |hash|
-    hash[:name] == item
+  hash[:name] == item
   end
-  @selected_items.push(selection[0])
-
+  quantity.times {@selected_items.push(selection[i])}
+  i += 1
   @selected_items.each do |hash|
-    @total += (hash[:cost].to_i * quantity)
-end
+  @total += (hash[:cost].to_i)
+  end
 end
 
 def total
   @total
 end
-end
-# def total_cost
-#   total = 0
-#   @selected_items.each do |hash|
-#     total += hash[:cost].to_i
-#   end
-#   return total
-# end
-# end
 
-# m = Menu.new
-# o = Order.new(m)
-# o.select_dishes("biriyani", 2)
-# #o.total_cost
+end
