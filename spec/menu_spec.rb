e3717a8a86a9552a010ab1686781022e2ae1a803
LucyMHall <lucymhall1995@gmail.com>
2019-06-06 09:35:55 +0100

    Display menu when method view called

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,20 +1,25 @@
 require 'menu'
 
 RSpec.describe "Menu" do
-@menu = {"Fish" => 3, "Chips" => 2, "Veggie Burger" => 2, "Mushy Peas" => 0.5, "Curry Sauce" => 0.5, "Ketchup" => 0.2}
+  before(:each) do
+      @menu_contents = {"Fish" => 3, "Chips" => 2, "Veggie Burger" => 2, "Mushy Peas" => 0.5, "Curry Sauce" => 0.5, "Ketchup" => 0.2}
+  end
+
 
   describe '#initialize' do
 
     it 'stores the menu it is passed on initialization' do
-      menu = Menu.new(@menu)
-      expect(menu.menu_contents).to eq(@menu)
+      menu = Menu.new(@menu_contents)
+      expect(menu.menu_contents).to eq(@menu_contents)
     end
+
+  end
 
   describe '#view' do
 
     it 'displays a menu' do
-      menu = Menu.new(@menu)
-      expect(menu.view).to include(@menu)
+      menu = Menu.new(@menu_contents)
+      expect(menu).to respond_to(:view)
     end
 
   end
