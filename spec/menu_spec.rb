9dfbc2a4b008e96b62700e319b81a77953cf1aa7
LucyMHall <lucymhall1995@gmail.com>
2019-06-07 15:32:44 +0100

    Created order class, you can view menu, you can select and dish, you can't select a dish not on the menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,25 +1,18 @@
 require 'menu'
 
 RSpec.describe "Menu" do
+
   before(:each) do
       @menu_contents = {"Fish" => 3, "Chips" => 2, "Veggie Burger" => 2, "Mushy Peas" => 0.5, "Curry Sauce" => 0.5, "Ketchup" => 0.2}
+      @displayed_menu= "Menu\nFish: £3.0\nChips: £2.0\nVeggie Burger: £2.0\nMushy Peas: £0.5\nCurry Sauce: £0.5\nKetchup: £0.2\n"
   end
 
-
-  describe '#initialize' do
-
-    it 'stores the menu it is passed on initialization' do
-      menu = Menu.new(@menu_contents)
-      expect(menu.menu_contents).to eq(@menu_contents)
-    end
-
-  end
 
   describe '#view' do
 
     it 'displays a menu' do
-      menu = Menu.new(@menu_contents)
-      expect(menu).to respond_to(:view)
+      menu = Menu.new
+      expect{menu.view}.to output(@displayed_menu).to_stdout
     end
 
   end
