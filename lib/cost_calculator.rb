aec9fda17241ce1cd387611b1da88c0e6184cc5a
LucyMHall <lucymhall1995@gmail.com>
2019-06-07 16:40:28 +0100

    Method price_quantity uses the menu prices and the dish quantities to work out an overall price

--- lib/cost_calculator.rb
+++ lib/cost_calculator.rb
@@ -1,8 +1,18 @@
 class Cost_calculator
 
-  def price_quantity(order)
+  attr_reader :total_price
 
-
+  def price_quantity(order, menu)
+    @total_price = 0
+    order.each do |index|
+        @price_for_1_dish = menu[index["dish"]]
+        puts @price_for_1_dish
+        @price_for_quantity = @price_for_1_dish * index["quantity"]
+        puts @price_for_quantity
+        @total_price += @price_for_quantity
+        puts @total_price
+    end
   end
+
 
 end
--- spec/cost_calculator_spec.rb
+++ spec/cost_calculator_spec.rb
@@ -2,15 +2,16 @@ require "cost_calculator"
 
 RSpec.describe "Cost_calculator" do
 
-  before(:each) do
-    @order = double("order", quantity: 2, dish: "Fish")
-  end
-
   describe "price_quantity" do
-    xit 'multiplies the prices from the menu with the quantity of the dish on the order' do
-      calc = Cost_calculator.new
-      expect(calc.price_quantity(@order)).to eq(6)
+
+    it 'multiplies the prices from the menu with the quantity of the dish on the order' do
+      @order = double("order", current_order:[{"dish" => "Fish", "quantity" => 3},{ "dish" => "Chips", "quantity" => 1}])
+      @menu = double("menu", menu_contents: {"Fish" => 3, "Chips" => 2} )
+      calculator = Cost_calculator.new
+      calculator.price_quantity(@order.current_order, @menu.menu_contents)
+      expect(calculator.total_price).to eq(11)
     end
+
   end
 
 end
