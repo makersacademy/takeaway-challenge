596639ae51783f33964902b502ae2113e7f43079
Student <kaczanowskamonika@gmail.com>
2019-06-08 22:29:29 +0100

    the program now shows the dishes and prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,16 @@
 class Menu
 
+  attr_reader :dishes
+
   def initialize(dishes)
     @dishes = dishes
+  end
+
+  def print
+    @dishes.map do |dish, price|
+      #dish.to_s
+      #price.to_f
+      "#{dish.to_s} £#{'%.2f' % price}"
+    end.join(",")
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,15 +1,20 @@
 require 'menu'
 describe Menu do
 
-  subject(:menu) {descirbed_class.new(dishes)}
+  subject(:menu) {described_class.new(dishes)}
 
   let(:dishes)do
   {
-    Air_baked_fries: 6.75,
-    Guac_burger: 6.00
+    Fries:  6.75,
+    Burger:  6.00
   }
-  it'shows a list of dishes with prices' do
+  end
+
+  it'has a list of dishes with prices' do
     expect(menu.dishes).to eq(dishes)
     end
+  it 'prints a list of dishes with prices' do
+    printed_menu = "Fries £6.75,Burger £6.00"
+    expect(menu.print).to eq(printed_menu)
   end
-end 
+end
