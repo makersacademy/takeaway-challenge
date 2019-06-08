1c7ece82a1ae67ce71afa4e41bb250a7976eb4d6
joseph townshend <joetownshend@mac.com>
2019-06-08 11:23:05 +0100

    added menu class and wrote menu

---
+++ lib/menu.rb
@@ -0,0 +1,20 @@
+
+
+class Menu
+
+  def initialize
+    @menu = [
+      {:item => "Big Mac", :price => "5.99"},
+      {:item => "Cheese Burger", :price => "3.99"},
+      {:item => "Chicken Legend", :price => "4.99"},
+      {:item => "Nuggets", :price => "2.99"},
+      {:item => "Fries", :price => "0.99"},
+      {:item => "Coke", :price => "0.99"},
+      {:item => "Milk Shake", :price => "1.99"},
+    ]
+  end
+
+  def show()
+
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,4 @@
+
+describe Menu do
+  it 'can sor'
+end
