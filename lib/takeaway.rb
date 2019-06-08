1bc4b8e3079ab9666c4350c56bd578b63217a796
Luigi <l-palermo@hotmail.it>
2019-06-08 16:39:03 +0100

    Add #show_menu and MENU

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,17 @@
 class Takeaway 
+  MENU = {
+    olives: 4.00,
+    cheese_board: 10.00,
+    pizza: 8.00,
+    carbonara: 10.00,
+    chicken:  12.00,
+    salad: 8.00,
+    burrata: 6.00,
+    steak: 15.00,
+    brownie: 5.00
+  }
 
+  def show_menu
+    MENU
+  end
 end
