c70368efe54073f6dbcbc29201d163be99f97dea
joseph townshend <joetownshend@mac.com>
2019-06-08 11:49:29 +0100

    test written for show items behaviour

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,12 @@
 require 'menu'
 
 describe Menu do
-  it 'should have a list of items' do
+  it 'should have a list of items by default' do
     expect(subject.food).not_to be_empty
   end
 
+  it 'can show all items from the list' do
+    expect(subject.show).to include("Big Mac 5.99")
+  end
 
 end
