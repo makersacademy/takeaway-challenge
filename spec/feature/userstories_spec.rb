04b2ea54b47446962975ba462995483ab2b70426
Student <student@unknown-f0:18:98:41:b0:95.lan>
2019-06-08 16:39:03 +0100

    Add #show_menu and MENU

--- spec/feature/userstories_spec.rb
+++ spec/feature/userstories_spec.rb
@@ -2,4 +2,13 @@ require 'takeaway'
 
 RSpec.describe 'user stories' do
 
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+
+  it 'shows the list of dishes' do
+    takeaway = Takeaway.new
+    expect { takeaway.show_menu }.not_to raise_error
+    expect(takeaway.show_menu).to eq Takeaway::MENU
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,12 @@
 require 'takeaway'
 
 RSpec.describe Takeaway do
+  let(:subject) { described_class.new }
 
+  describe '#show_menu' do
+
+    it 'return a list of dishes' do
+      expect(subject.show_menu).to eq Takeaway::MENU
+    end
+  end
 end
