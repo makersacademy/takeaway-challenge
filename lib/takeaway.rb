b44bfccdbae1c7db9a3315e8b5d3038d8c92379d
Leon Linton <leon.linton@live.co.uk>
2019-06-08 18:38:35 +0100

    Add Order Class and add method with Tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,7 +1,10 @@
+require "order"
+
 class Takeaway
 
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order
   end
 
   def print_menu
@@ -9,7 +12,13 @@ class Takeaway
     @menu.print
   end
 
+  def place_order(dishes)
+    dishes.each do |dish, quantity|
+      order.add(dish, quantity)
+    end
+  end
+
   private
 
-  attr_reader :menu
+  attr_reader :menu, :order
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,9 +5,18 @@ RSpec.describe Takeaway do
 
 # menu double and added method double for display
   let(:menu) { double(:menu, print: printed_menu) }
+  let(:order) { double(:order) }
   let(:printed_menu) { "Chicken: £3.50" }
+
+  let(:dishes) { { chicken: 2, fish: 1 } }
 
   it "shows the menu with dishes and prices" do
     expect(takeaway.print_menu).to eq(printed_menu)
+  end
+
+# introducing new order method therefore we need to consider an Order class. Creating double above.
+  it "can order some number of several available dishes" do
+    expect(order).to receive(:add).twice
+    takeaway.place_order(dishes)
   end
 end
