386d9a79fb211d187961430e8ce4503853f6d891
Ben Scott <bengscott2@gmail.com>
2019-06-08 11:33:41 +0100

    refactored add_item

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,7 @@
 #require_relative 'item'
 
 class Menu
+  attr_reader :items
 
   def initialize(item = Item)
     @item = item
@@ -8,8 +9,13 @@ class Menu
   end
 
   def add_item(name, price)
-    @items << @item.new(name, price)
+    @items << create_new_item(name, price)
   end
 
+  private
 
+  def create_new_item(name, price)
+    { name => @item.new(name, price) }
+  end
+  
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,11 +7,10 @@ before (:each) do
   @menu = Menu.new(@item_class)
 end
   describe '#add_item' do
-     it 'can add item object to items array' do
+     it 'can add item object' do
        @item = double('item', name: self)
-       allow(@item_class).to receive(:new).with('cake', 6).and_return @item
+       expect(@item_class).to receive(:new).with('cake', 6).and_return @item
        @menu.add_item('cake', 6)
-       expect(@menu.get('cake')).to eq([@item])
      end
   end
 
