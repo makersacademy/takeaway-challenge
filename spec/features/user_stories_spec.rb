d6d60cdddf09465609c61058b922521c98637559
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-09 15:52:27 +0100

    adds first feature and unit test

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,10 @@
+describe 'User stories' do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+  it 'shows menu' do
+    order = Order.new
+    expect { order.show_menu }.not_to raise_error
+    expect { order.show_menu }.to output(/Menu/).to_stdout
+  end
+end
