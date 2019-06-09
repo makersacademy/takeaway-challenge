eda8f152abd6d868c3937c82f5b490f000e1a7a6
Student <kaczanowskamonika@gmail.com>
2019-06-09 15:14:45 +0100

    customer can now order several number of dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,15 +1,22 @@
 class Takeaway
 
-  attr_reader :menu
+  attr_reader :menu, :order
 
-  def initialize(menu:)
+  def initialize(menu:, order:)
     @menu = menu
+    @order = order
   end
 
   def show_menu
     @menu.print
   end
 
+  def place_order(dishes)
+    dishes.each do |dish, amount|
+      order.add(dish, amount)
+    end 
+
+  end
 ##private
 
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,12 +5,21 @@ require 'takeaway'
 # I would like to see a list of dishes with prices
 
 describe Takeaway do
-  subject(:takeaway) { described_class.new(menu: menu) }
+  subject(:takeaway) { described_class.new(menu: menu, order: order) }
 
   let(:menu) { double(:menu, print: printed_menu) }
+
+  let(:order) { double(:order) }
+
   let(:printed_menu) { "Air baked fries 6.75£"}
 
+  let(:dishes) {{ fries: 5, toast: 4}}
   it 'shows the menu' do
     expect(subject.show_menu).to eq(printed_menu)
+  end
+
+  it 'can order a several number of dishes' do
+    expect(order).to receive(:add).twice
+    takeaway.place_order(dishes)
   end
 end
