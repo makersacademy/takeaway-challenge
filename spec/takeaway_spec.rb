2918d7daa90a8aecbd34e3c86e79ca70505100d7
petraartep <petra.pogacsas@gmail.com>
2019-06-09 09:47:51 +0100

    takeaway added viewmenu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,8 +1,8 @@
 require 'takeaway'
 
 RSpec.describe Takeaway do
-  subject (:takeaway) { described_class.new(menu) }
-  let (:menu) { double :menu, print_menu: 'formatted_menu', items: 'hotpot 20' }
+  subject(:takeaway) { described_class.new(menu) }
+  let(:menu) { double :menu, print_menu: 'formatted_menu', items: 'hotpot 20' }
   
   it 'has a menu' do
     expect(takeaway.view_menu).to eq menu.print_menu
