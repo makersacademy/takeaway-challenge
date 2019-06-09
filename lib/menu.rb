4dbc2e92362770673e02545e2177d10d0d4bffd7
Danielandro <mtme13@hotmail.com>
2019-06-09 15:32:47 +0100

    Restart - change model

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,12 +1,3 @@
-class Menu
-  MENU_OPTIONS = {
-        "jollof rice": { price: 6 },
-        "fried plantain": { price: 3 },
-        "jerk chicken": { price: 8 },
-        "vegetable patty": { price: 4.5 },
-        "ice cream": { price: 4 }
-      }
-  def print_menu
-    MENU_OPTIONS
-  end
+class Menu  
+ 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,15 +1,6 @@
 require 'menu'
 
 describe Menu do
-  it 'customer can see a list of dishes with prices' do
-    menu = Menu.new
-    
-    expect(menu.print_menu).to eq({
-        "jollof rice": { price: 6 },
-        "fried plantain": { price: 3 },
-        "jerk chicken": { price: 8 },
-        "vegetable patty": { price: 4.5 },
-        "ice cream": { price: 4 }
-      })
-  end
+  it 'customer can see a list of dishes with prices' 
+  
 end
