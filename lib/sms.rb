155f303ddebcc2a0b3294d79b64b1db9616779de
petraartep <petra.pogacsas@gmail.com>
2019-06-10 17:59:52 +0100

    sms refactored

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,9 +1,32 @@
 require 'twilio-ruby'
 require 'dotenv'
 
-class Sms
+class SMS
 
-  def send_sms
-  
+  TIME_FORMAT = "%H:%M"
+
+  def initialize(config, client: nil)
+    @client = client || Twilio::REST::Client.new(config[:account_sid], config[:auth_token])
+    @config = config
+  end
+
+  def deliver
+    client.messages.create(message_args)
+  end
+
+  private
+
+  attr_reader :client, :config
+
+  def message_args
+    {
+      from: config[:from],
+      to: config[:to],
+      body: config[:body] % delivery_time
+    }
+  end
+
+  def delivery_time
+    (Time.now + 3600).strftime(TIME_FORMAT)
   end
 end
--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -1,10 +1,30 @@
 require 'sms'
 
-RSpec.describe Sms do
+RSpec.describe SMS do
+	subject(:sms) { described_class.new(config, client: client) }
+	let(:client) { double(:client, messages: messages) }
+	let(:messages) { spy(:messages) }
 
-    describe '#send_sms' do
-      it 'sends a payment confirmation text message' do
-        
-      end
-    end
+	let(:config) do
+			{
+					account_sid: ENV['TWILIO_ACCOUNT_SID'],
+					auth_token: ENV['TWILIO_AUTH_TOKEN'],
+					from: ENV['TWILIO_PHONE_FROM'],
+					to: ENV['TWILIO_TO_NUMBER'],
+					body: "Thank you! Your order will be delivered before %s" 
+			}
+	end
+
+	it 'delivers an SMS with the estimated time' do
+			args = {
+				from: config[:from],
+				to: config[:to],
+				body: "Thank you! Your order will be delivered before 07:00"
+
+			}
+
+		allow(Time).to receive(:now).and_return(Time.parse("06:00"))
+		sms.deliver
+		expect(messages).to have_received(:create).with(args)
+	end
 end
