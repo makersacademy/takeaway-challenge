77a10be29449061f820231410b29410a9511c96a
TBrockster <tobybrock93@gmail.com>
2019-07-06 13:14:29 +0100

    User Story 4
    As a customer
    So that I am reassured that my order will be delivered on time
    I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'twilio-ruby'
 require_relative 'order'
 require_relative 'load_menu_module'
 
@@ -7,6 +8,11 @@ class Menu
   def initialize(restaurant: "fishnchips.csv", order: Order.new(restaurant: restaurant))
     @order = order
     load_menu(restaurant)
+    account_sid = ENV['TWILIOSID']
+    auth_token = ENV['TWILIOAUTH']
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    @from = ENV['TWILIONUMBER'] # Your Twilio number
+    @to = ENV['PHONENUMBER'] # Your mobile phone number
   end
 
   def display_menu
@@ -23,5 +29,22 @@ class Menu
 
   def check_order(order: @order)
     order.receipt
+  end
+
+  def confirm_order(order: @order)
+    puts order.receipt
+    puts 'Would you like to confirm your order? (y/n)'
+    user_input = gets.chomp
+    if user_input == 'y'
+      now = Time.new
+      delivery_time = now + (60 * 60)
+      @client.messages.create(
+        from: @from,
+        to: @to,
+        body: "Hey friend! Your food is on it's way! It should arrive by #{delivery_time.strftime("%I:%M %p")}!"
+        )
+    else
+      puts 'Order not sent'
+    end
   end
 end
