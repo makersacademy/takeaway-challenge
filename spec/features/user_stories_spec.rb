c690574dbbf20a8ff4f9e8bee2adb2bbc1b78708
Freddie <f.varnals@gmail.com>
2019-07-06 14:48:02 +0100

    Updated feature tests for User Case 2, to include Order class

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,27 @@
+describe "User Stories" do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+  it "so user can see dishes and prices, instruct menu to provide dishes and prices" do
+    menu = Menu.new
+    dish = Dish.new
+    expect { menu.price(dish) }.not_to raise_error
+  end
+  #   As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+  it "so  that I can order a meal, I want to select several dishes" do
+    menu = Menu.new
+    dish = Dish.new
+    order = Order.new
+    expect { order.choose(dish) }.not_to raise_error
+  end
+  #   As a customer
+  # So that I can verify that my order is correct
+  # I would like to check that the total I have been given matches the sum of the various dishes in my order
+  # it "checks that the total matches the sum of the dishes in the customer order" do
+  #     menu = Menu.new
+  #     dish = Dish.new
+  #     order = Order.new
+  # end
+end
