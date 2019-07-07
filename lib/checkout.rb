f4266367a24c07058765335fc8997b58f436fae6
timdavidcole <timdavidcole@gmail.com>
2019-07-07 18:52:00 +0100

    used environment variables

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -5,8 +5,8 @@ require 'time'
 class Checkout
 
   def initialize(client = (Twilio::REST::Client))
-    @account_sid = "AC3dacdc49158a31763601f3cd87e88f48" # Your Account SID from www.twilio.com/console
-    @auth_token = "14a1891177aba79770d237c22e57b4de"   # Your Auth Token from www.twilio.com/console
+    @account_sid = ENV["TWILIO_KEY"] # Your Account SID from www.twilio.com/console
+    @auth_token = ENV["TWILIO_TOKEN"]   # Your Auth Token from www.twilio.com/console
     @client = client.new @account_sid, @auth_token
   end
 
@@ -17,8 +17,8 @@ class Checkout
   def send_text
     @client.messages.create(
         body: "Thank you! Your order was placed and will be delivered before #{time_plus_hour}",
-        to: "+447879633386",    # Replace with your phone number
-        from: "+441543624317")  # Replace with your Twilio number
+        to: ENV["P_PHONE"],    # Replace with your phone number
+        from: ENV["T_PHONE"])  # Replace with your Twilio number
   end
 
 private
