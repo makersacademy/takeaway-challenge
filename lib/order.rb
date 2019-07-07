8a8d3c6985d054fed4f711114f356c5305a30c90
marvinmatovu@gmail.com <student@makerss-mbp.home>
2019-07-07 22:11:28 +0100

    Making order - passing order from Menu to Order

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,8 @@
 
 class Order
-  attr_reader :current_order
+  attr_reader :current_order, :menu
   def initialize
-    @current_order = ""
+    @current_order = {}
     @menu
   end
 
@@ -11,5 +11,15 @@ class Order
     p "Menu passed to orders"
   end
 
+  def make_order(number)
+    @menu.each { |key, value|
+      if key == number
+        @current_order[key] = value
+        p "Meal selected"
+      else
+        p "Error with meal choice"
+      end
+    }
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,9 +1,25 @@
 require 'order'
 
 describe Order do
+  let(:the_menu) {double :the_menu}
+  let(:choice) {double :choice}
   describe '#make_order' do
     it 'Should make a order' do
       expect(subject.current_order).to_not eq nil
+    end
+  end
+  describe '#load_menu' do
+    it 'Should load menu' do
+      expect{subject.load_menu(the_menu)}.to change{subject.menu}.from(nil).to(the_menu)
+    end
+  end
+
+  describe '#make_order' do
+    it 'Should let user pick from the menu' do
+      subject.load_menu(the_menu)
+      expect(subject.make_order(choice)).to_not eq nil
+      # my_choice = subject.make_order(choice)
+      # expect(subject.menu).to include(my_choice)
     end
   end
 end
