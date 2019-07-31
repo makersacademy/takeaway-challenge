5eeca73d933e8f89626812effce9f11cc9284e5a
RussellWind <russell.wind@gmail.com>
2019-07-31 08:36:21 +0100

    current order is now generated through menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -5,10 +5,17 @@ class Takeaway
 
   def initialize
     @menu = {bread: 5, apples: 2}
-    @current_order = {bread: 0, apples: 0}
+    @current_order = new_order
   end
 
   def select(item)
-    @current_order[item.to_sym] = @current_order[item.to_sym] + 1
+    @current_order[item.to_sym] += 1
   end
+
+  private
+
+  def new_order
+    Hash[@menu.map{|item , quantity| [item, 0]}]
+  end
+
 end
