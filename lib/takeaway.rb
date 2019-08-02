20cba44dec67a03a7104224225f047981b7ab7f9
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-08-02 13:25:33 +0100

    Second user story complete - Tests passed

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,9 @@
 class Takeaway
 
+attr_reader :order
+
   def initialize
+    @order = []
     @menu = {:pepperoni => 10,
              :hawaiian => 12,
              :meat_feast => 15
@@ -9,5 +12,9 @@ class Takeaway
 
   def menu
     @menu
+  end
+
+  def menu_select(order, amount=1)
+      amount.times{ @order << @menu.slice(order) }
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,11 +1,37 @@
 require './lib/takeaway.rb'
 describe Takeaway do
 let(:takeaway) { described_class.new }
+let(:pizza1) {:pepperoni}
+let(:pizza2) {:hawaiian}
+
+context 'User story 1' do
 # As a customer
 # So that I can check if I want to order something
 # I would like to see a list of dishes with prices
 it 'Should have a list of prices' do
   expect(takeaway.menu).to eq ({:pepperoni => 10, :hawaiian => 12, :meat_feast => 15})
 end
+end
+context 'User Story 2' do
+# As a customer
+# So that I can order the meal I want
+# I would like to be able to select some number of several available dishes
+it 'Should allow menu selection' do
+  takeaway.menu_select(pizza1)
+  expect(takeaway.order).to eq [:pepperoni => 10 ]
+end
+
+it 'Should allow order amount selection' do
+  takeaway.menu_select(pizza2, 2)
+  expect(takeaway.order).to eq [{:hawaiian => 12}, {:hawaiian => 12}]
+end
+end
+
+# As a customer
+# So that I can verify that my order is correct
+# I would like to check that the total I have been given matches the sum of the various dishes in my order
+
+
+
 
 end
