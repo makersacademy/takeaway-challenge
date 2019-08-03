942012588e11d1ecbe3c6d3d5cef229cfee7b5c1
Hanna <hannaaikas@gmail.com>
2019-08-03 17:53:03 +0100

    Test: Able to show menu

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,12 +1,19 @@
 require_relative '../lib/menu'
+require_relative '../lib/order'
 
 puts "--- TEST - Menu has a pizza, puts the price of 7 ---"
 my_menu = Menu.new
 p my_menu.menu[:pizza]
 puts "------TEST OVER-------"
 
-puts "--- TEST - Add a pizza to the order, return the order ---"
+puts "--- TEST - See the list of dishes with prices ---"
+my_menu = Menu.new
+my_menu.show_menu
+puts "------TEST OVER-------"
+
+puts "--- TEST - Add a pizza to the order, then 2 kebabs, puts the order ---"
 my_order = Order.new
 my_order.add_item(:pizza)
+my_order.add_item(:kebab, 2)
 p my_order
 puts "------TEST OVER-------"
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,4 +4,7 @@ describe Menu do
   it 'contains a menu with a pizza costing 7' do
     expect(subject.menu[:pizza]).to eq 7
   end
+  it 'shows a list of dishes with prices' do
+    expect(subject).to respond_to(:show_menu)
+  end
 end
