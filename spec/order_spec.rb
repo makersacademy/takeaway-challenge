7e6fb4a70c47bfecb5fe5b5dafbab4aa16705baf
Hanna <hannaaikas@gmail.com>
2019-08-03 19:22:16 +0100

    Refactor: Remove redundant unit tests from order_spec

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,14 +1,6 @@
 require 'order'
 
 describe Order do
-  it 'adds 1 pizza to the order' do
-    subject.add_item(:pizza)
-    expect(subject.total_order).to eq [{ pizza: 1 }]
-  end
-  it 'adds 3 kebabs to the order' do
-    subject.add_item(:kebab, 3)
-    expect(subject.total_order).to eq [{ kebab: 3 }]
-  end
   it 'adds 1 pizza to then order, then 3 kebabs' do
     subject.add_item(:pizza)
     subject.add_item(:kebab, 3)
