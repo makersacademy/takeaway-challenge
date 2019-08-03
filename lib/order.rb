56cf199e78e170d3c71f0c6df4d2067281b72022
Guillaume <liard.guillaume@gmail.com>
2019-08-03 12:44:05 +0100

    Refactoring Order#add

--- lib/order.rb
+++ lib/order.rb
@@ -12,11 +12,7 @@ class Order
     raise "This dish is not available" unless @restaurant.menu.dishes_list.include?(item)
     raise "This order is closed" if closed?
     item = item.name
-    if !@items[item]
-      items[item] = n
-    else
-      items[item] += n
-    end
+    push_item(item, n)
   end
 
   def close
@@ -27,4 +23,12 @@ class Order
     @locked
   end
 
+  private
+  def push_item(item, n)
+    if !@items[item]
+      items[item] = n
+    else
+      items[item] += n
+    end
+  end
 end
