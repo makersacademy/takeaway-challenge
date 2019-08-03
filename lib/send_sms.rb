5d3d6c1dc29b2e8732b3120d1d4578bba049996d
kierepkae <kierepkaedyta@gmail.com>
2019-08-03 21:50:41 +0100

    number update

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -5,10 +5,10 @@ auth_token = '2b926f00562bb4a77c29fa76c274b6b9'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+441527962031' # Your Twilio number
-to = '+447831800300' # Your mobile phone number
+to = '+447587367437' # Your mobile phone number
 
 client.messages.create(
 from: from,
 to: to,
-body: "Hey friend!"
+body: "Takeaway Challenge!"
 )
