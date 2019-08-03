b9621dcb281c38d0170d66afdfece435d2cb13d2
Jodi <jodiallysonfoster@gmail.com>
2019-08-03 12:14:58 +0100

    can place order by giving the list of dishes, their quantities and total cost

--- lib/order.rb
+++ lib/order.rb
@@ -2,18 +2,23 @@
 class Order
 
 
-attr_reader :order, :menu
+attr_reader :order, :menu, :total
 
 
 def initialize
   @selection = {}
   @order = []
 
+
+
+
 end
 
   def show_order
     @order
   end
+
+
 
   def add(item, quantity)
     @item = item
@@ -27,12 +32,19 @@ end
     @selection = {}
   end
 
+
+  def place_order
+
+    array_of_prices = @order.map{|h| (h[ "price"] * h[ "quantity"])}
+   @total = array_of_prices.sum
+
+   "Here is your order #{@order}, the total is #{@total}"
+    end
+
+
+
   # def place_order
-  #
-  #
+  #   @total
+  # #  "Here is your order #{@order}, the total is #{@total}"
   # end
-
-
-
-
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -16,13 +16,21 @@ describe Order do
        {"item"=>"Tea", "price"=>4, "quantity"=>2}])
   end
 
+  describe "#place_order" do
+    #NEED TO ADD TOTAL
 
-  it "can place the order by giving the list of dishes, their quantities and a number" do
-    subject.add("Water", 1)
-    subject.add("Tea", 2)
-    expect(subject.place_order).to eq 1
+    it "can place the order by giving the list of dishes, their quantities and total cost" do
+      subject.add("Water", 1)
+      subject.add("Tea", 2)
+      expect(subject.place_order).to eq "Here is your order [{\"item\"=>\"Water\", \"price\"=>3, \"quantity\"=>1}, {\"item\"=>\"Tea\", \"price\"=>4, \"quantity\"=>2}], the total is 11"
 
+     end
+
+  # it "can add up the total of the order" do
+  #   subject.add("Water", 1)
+  #   subject.add("Tea", 2)
+  #   expect(subject.total).to eq 11
+
+    end
   end
-
-
-end
+# end
