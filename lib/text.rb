cdfad3bb1a0cb382bbd40340215087721f6c9130
Guy Calloway <[guyfcalloway@gmail.com]>
2019-08-04 20:59:02 +0100

    c

--- lib/text.rb
+++ lib/text.rb
@@ -2,9 +2,9 @@ require 'twilio-ruby'
 
 def text(mobile, time, change)
   account_sid = "AC2e1ac993aca31d47424ddfde7fcf6003"
-  auth_token = "fcb1d50ca850ac2e4562cde3ce09f561"
+  auth_token = "fxxxxxxxxxxxxsxsxsxsxsxsxsxsxsx61"
   @client = Twilio::REST::Client.new(account_sid, auth_token)
-  from = '+441288255120' 
+  from = '+441288255120'
   to = mobile
   @client.messages.create(
   from: from,
