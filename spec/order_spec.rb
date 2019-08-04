071b5d0391c7e96bb3fcddcfe88433ad6f8600e4
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-04 13:30:15 +0100

    Unit test draft update

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,9 +1,21 @@
 require 'order'
 
 describe Order do
-  describe '#order' do
-    it 'can respond to order' do
-      expect(subject).to respond_to(:order)
-    end
+
+  it 'is initially empty' do
   end
+
+  it 'can add dish to order' do
+  end
+
+  it 'can remove a dish from the order' do
+  end
+
+  it 'raises an error if dish is not on the menu' do
+  end
+
+  it 'can show customer order so far' do
+  end
+
+
 end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,23 @@
+require 'takeaway'
+
+describe Takeaway do
+
+    it 'it can get the basket summary' do
+    end
+
+    it 'raises an error when basket is empty' do
+    end
+    
+    it 'can show a price total of the order' do
+    end
+
+    it 'confirms that the order has been place' do
+    end
+    
+    it 'can send a text when the order is placed' do
+    end
+
+
+  
+
+end
