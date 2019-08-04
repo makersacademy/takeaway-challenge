2ce4afdb6cb9a2c150f30775adc79a79eab721c8
Guy Calloway <[guyfcalloway@gmail.com]>
2019-08-04 21:00:20 +0100

    texting method in separate file

--- lib/takeout.rb
+++ lib/takeout.rb
@@ -6,7 +6,7 @@ class Takeout
   def initialize(menu)
     @menu = menu
     @order = {}
-    
+
   end
 
   def add_to_menu(items_hash)
@@ -20,21 +20,5 @@ class Takeout
   def confirm_order(mobile, time, change)
     text(mobile, time, change)
   end
-
-  # private
-  #
-  # def text(mobile, time, change)
-  #   account_sid ="AC2e1ac993aca31d47424ddfde7fcf6003"
-  #   auth_token = "fcb1d50ca850ac2e4562cde3ce09f561"
-  #   @client = Twilio::REST::Client.new(account_sid, auth_token)
-  #
-  #   from = '+441288255120' # Your Twilio number
-  #   to = number # Your mobile phone number
-  #   @client.messages.create(
-  #   from: from,
-  #   to: to,
-  #   body: "Thank you! Your order was placed and will be delivered before #{time}! Here is your change: £#{change}"
-  # )
-  # end
 
 end
