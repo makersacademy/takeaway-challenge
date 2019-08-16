5950713db8cd83531f79a7ec7166be8150c823ba
EloisePrasad <eloise.prasad@outlook.com>
2019-08-16 14:46:26 +0100

    added argument to select

--- docs/order.rb
+++ docs/order.rb
@@ -8,7 +8,7 @@ class Order
     @total = 0
   end
 
-  def select # (dish)
+  def select(dish)
     @dishes_class = @dishes.new
     # @total += @dishes_class.price(dish)
   end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,14 +1,19 @@
 require './docs/order'
 describe Order do
+
   describe '#select' do
-    xit 'can respond to select with an argument' do
-      expect(subject).to respond_to(:select).with(1).argument
-    end
+
     it 'responsible for creating new dishes' do
       dishes_class = double(:dishes_class)
+      dish = double(:dish)
       order = Order.new(dishes_class)
       allow(dishes_class).to receive(:new)
-      order.select
+      order.select(dish)
     end
+
+    it 'can respond to select with an argument' do
+      expect(subject).to respond_to(:select).with(1).argument
+    end
+
   end
 end
