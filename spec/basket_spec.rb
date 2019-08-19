8814d499feb960b2fd2bd61d6085fd57ed1257e1
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-08-19 10:02:40 +0100

    Takeaway and Basket class unit tests written and passing

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -2,26 +2,37 @@ require 'basket'
 require 'formatter'
 
 describe Basket do
-  let(:dish) { double(:dish, name: 'Foo soup', price: 2, quantity: 1) }
-  it 'can add a new dish' do
-    subject.add(dish, 1)
+  let(:order) { double(:order, name: 'Foo soup', price: 2, quantity: 1) }
+  it 'can add a new order' do
+    subject.add(order, 1)
     expect(subject.empty?).to be(false)
   end
-  it 'can add to the quantity of an existing dish' do
-    subject.add(dish, 1)
-    another_dish = double(:another_dish, name: 'Bar mulch', quantity: 1)
-    subject.add(another_dish, 1)
+  it 'can add to the quantity of an existing order' do
+    subject.add(order, 1)
+    allow(order).to receive(:quantity=).and_return(1)
+    another_order = double(:another_order, name: 'Foo soup', quantity: 1)
+    subject.add(another_order, 1)
     expect(subject.empty?).to be(false)
   end
   it 'can empty itself' do
-    subject.add(dish, 1)
+    subject.add(order, 1)
     subject.empty
     expect(subject.empty?).to be(true)
   end
   it 'provides a subtotal price' do
-    subject.add(dish, 1)
-    another_dish = double(:another_dish, name: 'Bar mulch', price: 1, quantity: 1)
-    subject.add(another_dish, 1)
+    subject.add(order, 1)
+    another_order = double(:another_order, name: 'Bar mulch', price: 1, quantity: 1)
+    subject.add(another_order, 1)
     expect(subject.total).to eq(3)
+  end
+  it 'can print its total' do
+    subject.add(order, 1)
+    expect { subject.print_total }.to output("Total: £2.00\n").to_stdout
+  end
+  it 'can list its contents' do
+    subject.add(order, 1)
+    another_order = double(:another_order, name: 'Bar mulch', price: 1, quantity: 1)
+    subject.add(another_order, 1)
+    expect { subject.list }.to output("Foo soup x1: £2.00\nBar mulch x1: £1.00\n").to_stdout
   end
 end
