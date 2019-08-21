649582dd681977d6b5334b1108de0691d201c2db
Sam Kitchen <46498669+samkitchen94@users.noreply.github.com>
2019-08-21 20:41:24 +0100

    Update text.rb

--- lib/text.rb
+++ lib/text.rb
@@ -8,15 +8,15 @@ class Text
   end
 
   def client_info
-    account_sid = 'AC40c976076aa2c78402839fe533990f9f'
-    auth_token = '9ce7df56bf7307cbf531263b2ac8acb8'
+    account_sid = ''
+    auth_token = ''
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
   def send_text
     @client.api.account.messages.create(
-    from: '+447723429606',
-    to: '+447540624339',
+    from: '',
+    to: '',
     body: "Thanks for your order from Sam's pizza! Your food will be delivered before #{@eta}."
     )
   end
