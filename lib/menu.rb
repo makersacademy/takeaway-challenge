5fb8d0f3626786c03fa68daad45de85751496ba3
Ben <ben@benzeng.co.uk>
2019-08-31 19:00:48 +0100

    #print_menu added and tested

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,29 @@
 class Menu
+  attr_reader :day_menu
 
+  def initialize
+    @day_menu = [
+        {item: 1, name: "Homemade tomato sauce with freshly chopped basil and buffalo mozzarella (rigatoni)", price: 10},
+        {item: 2, name: "Homemade creamy walnut sauce with chestnut mushrooms (casarecce)", price: 10.5},
+        {item: 3, name: "Smoked salmon (or pancetta) carbonara with pecorino and beaten eggs (bucatini)", price: 13},
+        {item: 4, name: "4 hour slow cooked béchamel bolognese with grated parmesan (pappardelle)", price: 12.5},
+        {item: 5, name: "Homemade parmesan, pecorino and basil pesto (casarecce", price: 11},
+        {item: 6, name: "Flaked yellowfin tuna fillet with homemade tomato sauce,peperoncino and freshly chopped parsley (rigatoni)", price: 13},
+        {item: 7, name: "Handmade ravioli with organic lamb, parsley and parmesan in a lightbutter and sage sauce", price: 14}
+    ]
+  end
 
+  def print_menu(menu_in)
+    menu_out = ""
+    menu_in.each do |menu|
+      menu_out << "#{menu[:item]}. #{menu[:name]} - #{menu[:price]} \n"
+    end
+    menu_out
+  end
 end
+
+
+
+
+
+
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,6 +1,13 @@
-require 'menu'
+require './lib/menu'
 
 describe Menu do
+  let(:menu) {[{item: 5, name: "rice", price: 10}]}
+
+  describe "#print_menu" do
+    it "takes a menu as a array of hashes and returns it as a formatted string" do
+      expect(subject.print_menu(menu)).to include("5. rice - 10")
+    end
+  end
 
 
 end
