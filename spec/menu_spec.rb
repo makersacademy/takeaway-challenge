9cff713d98f89b548858359b63ed41d11e8b7193
Daniel Segall <segalld@gmail.com>
2019-08-31 12:55:18 +0100

    Fix incorrect test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,7 +5,7 @@ describe Menu do
                    "Cafe Latte - 4.30\nCafe Latte - 4.30", 'print_line_items')
 
   it 'returns immutable items' do
-    original_items = subject.items
+    original_items = subject.items.dup
     new_items = subject.items
     new_items << ''
     expect(subject.items).to eq original_items
