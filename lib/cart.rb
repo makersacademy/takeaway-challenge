9652bdbd7cb49d4c7c1b220acd034bdd3441e0e9
Mark Dobbins <mjdobbins12@gmail.com>
2019-08-31 18:18:38 +0000

    add order review and total

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,11 +1,12 @@
 require './lib/menu'
 
 class Cart
-	attr_reader :selection
+	attr_reader :selection, :order_total
 	
 	def initialize
 		@menu = Menu.new
 		@selection = []
+		@order_total = 0
 	end
 	
 	def view_menu
@@ -13,6 +14,13 @@ class Cart
 	end
 	
 	def select(*items)
-		items.each { |item| @selection << @menu.dish_list[item - 1] } 
+		items.each do |item| 
+			@selection << @menu.dish_list[item - 1][:name] 
+			@order_total += @menu.dish_list[item - 1][:price]
+		end 
+	end
+	
+	def show_cart
+		puts "Your order: #{@selection.join(', ')}. Total: £#{@order_total}"
 	end
 end
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -8,6 +8,16 @@ describe Cart do
 	
 	it "stages a list of items for purchase" do
 		cart.select(1,3)
-		expect(cart.selection).to include({:name=>"Miso", :price=>2})
+		expect(cart.selection).to include("Miso")
+	end
+	
+	it "lets the customer review selected items before purchase" do
+		cart.select(3)
+		expect{ cart.show_cart }.to output("Your order: Sushi combo B. Total: £11\n").to_stdout
+	end
+	
+	it "displays an order total" do
+		cart.select(1,2,3,4,4)
+		expect(cart.order_total).to eq(39)
 	end
 end
