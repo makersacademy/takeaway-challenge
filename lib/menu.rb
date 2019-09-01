0398a874e7e76ed2c3d61173196ac029f0ebc135
Ben <ben@benzeng.co.uk>
2019-09-01 11:46:32 +0100

    added #get_item_price and rspec tests

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,16 +1,17 @@
 class Menu
   attr_reader :menu
+  DEFAULT_MENU = [
+    { item: 1, name: 'Homemade tomato sauce with freshly chopped basil and buffalo mozzarella (rigatoni)', price: 10},
+    { item: 2, name: 'Homemade creamy walnut sauce with chestnut mushrooms (casarecce)', price: 10.5},
+    { item: 3, name: 'Smoked salmon (or pancetta) carbonara with pecorino and beaten eggs (bucatini)', price: 13},
+    { item: 4, name: '4 hour slow cooked béchamel bolognese with grated parmesan (pappardelle)', price: 12.5},
+    { item: 5, name: 'Homemade parmesan, pecorino and basil pesto (casarecce', price: 11},
+    { item: 6, name: 'Flaked yellowfin tuna fillet with homemade tomato sauce,peperoncino and freshly chopped parsley (rigatoni)', price: 13},
+    { item: 7, name: 'Handmade ravioli with organic lamb, parsley and parmesan in a lightbutter and sage sauce', price: 14}
+  ].freeze
 
-  def initialize
-    @menu = [
-      { item: 1, name: 'Homemade tomato sauce with freshly chopped basil and buffalo mozzarella (rigatoni)', price: 10},
-      { item: 2, name: 'Homemade creamy walnut sauce with chestnut mushrooms (casarecce)', price: 10.5},
-      { item: 3, name: 'Smoked salmon (or pancetta) carbonara with pecorino and beaten eggs (bucatini)', price: 13},
-      { item: 4, name: '4 hour slow cooked béchamel bolognese with grated parmesan (pappardelle)', price: 12.5},
-      { item: 5, name: 'Homemade parmesan, pecorino and basil pesto (casarecce', price: 11},
-      { item: 6, name: 'Flaked yellowfin tuna fillet with homemade tomato sauce,peperoncino and freshly chopped parsley (rigatoni)', price: 13},
-      { item: 7, name: 'Handmade ravioli with organic lamb, parsley and parmesan in a lightbutter and sage sauce', price: 14}
-    ]
+  def initialize(menu = DEFAULT_MENU)
+    @menu = menu
   end
 
   def print_menu(menu_in)
@@ -24,12 +25,17 @@ class Menu
   def item_available?(item)
     menu.any? { |menu| menu[:item] == item }
   end
+
+  def get_item_price(item)
+    menu.select { |menu| menu[:item] == item }.map { |menu| menu[:price] }.pop
+  end
+
 end
 
 
 
 
-
+#
 
 
 
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,21 +2,29 @@ require './lib/menu'
 
 describe Menu do
   let(:menu) {[{item: 5, name: "rice", price: 10}]}
+  subject(:test_menu) { described_class.new(menu) }
 
   describe "#print_menu" do
     it 'takes a menu as a array of hashes and returns it as a formatted string' do
-      expect(subject.print_menu(menu)).to include("5. rice - 10")
+      expect(test_menu.print_menu(menu)).to include("5. rice - 10")
     end
   end
 
-  describe "#item_available?" do
+  describe '#item_available?' do
     it 'returns true if item is available on the menu' do
-      expect(subject.item_available?(5)).to eq(true)
+      expect(test_menu.item_available?(5)).to eq(true)
     end
 
     it 'returns false if item is not on the menu' do
-      expect(subject.item_available?(10)).to eq(false)
+      expect(test_menu.item_available?(10)).to eq(false)
     end
   end
+
+  describe '#get_item_price' do
+    it 'returns the price of an item' do
+      expect(test_menu.get_item_price(5)).to eq(10)
+    end
+  end
+
 
 end
