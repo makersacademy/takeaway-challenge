6a08e2f671abb39e8be79e931bd5806583b5a2e6
Ben <ben@benzeng.co.uk>
2019-09-01 09:58:48 +0100

    updated Order class to send notification

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,30 @@
+require './lib/menu'
+require 'time'
+
 class Order
+
+  def initialize(menu = Menu.new)
+    @menu = Menu.new
+    @current_order = []
+    @order_total = 0
+  end
+
+  def select(item)
+    fail "dish does not exist" unless @menu.day_menu.any? {|menu| menu[:item] == item}
+    @current_order << item
+  end
+
+  def calc_tot
+    @current_order.each do |item|
+      @order_total += @menu.day_menu[item-1][:price]
+    end
+    @order_total
+  end
+
+  def send_notification
+    eta = (Time.now + 3600).strftime("%H:%M")
+    notification = "Thank you, you order has been received and will be delivered before #{eta}"
+  end
 
 
 end
