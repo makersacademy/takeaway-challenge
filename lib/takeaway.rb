0d22ed94ccc4c1028d3c0f31071cfaaa50de08ec
DanielleInkster <danielle.inkster@gmail.com>
2019-09-23 22:10:43 +0100

    List_dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,5 +1,11 @@
 require_relative 'menu'
 
 class Takeaway
+
+  def list_dishes
+    MENU_ITEMS.each_with_index do |(name, price), index|
+      puts "#{index + 1}. #{name} - #{price}"
+    end
+  end
 end
   
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,4 +5,12 @@ describe Takeaway do
   it 'exists!' do
     expect(subject.class).to eq Takeaway
   end
+
+  it { is_expected.to respond_to(:list_dishes) }
+
+  describe '#list_dishes' do
+    it 'lists menu items' do
+      expect(subject.list_dishes).to eq MENU_ITEMS
+    end
+  end
 end
