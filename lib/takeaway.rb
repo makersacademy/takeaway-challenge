adc673726e4104b299123a5277dbccdc6e9e751a
DanielleInkster <danielle.inkster@gmail.com>
2019-09-23 22:07:48 +0100

    Create classes

---
+++ lib/takeaway.rb
@@ -0,0 +1,5 @@
+require_relative 'menu'
+
+class Takeaway
+end
+  
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,8 @@
+require 'takeaway'
+
+describe Takeaway do
+
+  it 'exists!' do
+    expect(subject.class).to eq Takeaway
+  end
+end
