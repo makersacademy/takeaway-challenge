84ee794b8399a76cdc89000a99ad2cb8393e4260
Melvin Lau <lau.melvin@icloud.com>
2019-09-25 16:27:38 +0100

    Wrote test to add dishes to a menu

---
+++ spec/restaurant_spec.rb
@@ -0,0 +1,24 @@
+# restaurant_spec.rb
+require "restaurant"
+
+describe Restaurant do
+  let(:cafe_melvino) { described_class.new }
+
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+
+  # INFERRED USER STORY
+  # As a restaurant owner
+  # So that my customers can know what I sell
+  # I would like to add dishes to a menu list, and view the menu
+
+  describe "#add_dish" do
+    it "creates a new Dish instance and returns an updated menu list" do
+      pizza = double("Dish.new")
+      pasta = double("Dish.new")
+      cafe_melvino.add_dish(pizza)
+      expect(cafe_melvino.add_dish(pasta)).to eq [pizza, pasta]
+    end
+  end
+end
