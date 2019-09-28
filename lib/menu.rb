4057538ac9bd90aa505ef7f7a90a817ea08cb397
zraso <zaira.rasool@hotmail.co.uk>
2019-09-28 14:53:14 +0100

    Add choose method and order variable, pass tests

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,15 +1,22 @@
 class Menu
 
-  def see
-    list = {:Chicken=>5, :Fish=>7, :Lamb=>6, :Vegan=>5}
+  attr_reader :list, :order
 
-    list.each do |dish, price|
+  def initialize
+    @list = {"Chicken"=>5, "Fish"=>7, "Lamb"=>6, "Vegan"=>5}
+    @order = []
+  end
+
+  def see
+    @list.each do |dish, price|
       puts "#{dish}: #{price}"
     end
   end
   #
-  # def choose(*dish)
-  #
-  # end
+  def choose(*dish_selection)
+    dish_selection.each do |dish|
+        @order << dish
+    end
+  end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,14 +5,14 @@ describe Menu do
 
   describe '#see' do
     it 'displays a list of dishes with prices' do
-      expect(menu.see).to eq Chicken: 5, Lamb: 6, Fish: 7, Vegan: 5
+      expect(menu.see).to eq "Chicken"=>5, "Fish"=>7, "Lamb"=>6, "Vegan"=>5
     end
   end
 
-  # describe 'choose' do
-  #   it 'selects some number of several available dishes' do
-  #     expect(menu.choose).to eq list[:chicken], list[:fish]
-  #   end
-  # end
+  describe 'choose' do
+    it 'selects some number of several available dishes' do
+      expect(menu.choose("Chicken", "Fish")).to eq ["Chicken", "Fish"]
+    end
+  end
 
 end
