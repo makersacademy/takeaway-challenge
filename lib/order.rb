c00d0be99543ebfc5fe679222665c4f694ef48b2
Jessica Marais <jessica_marais@hotmail.com>
2019-09-28 12:49:37 +0100

    User stoy 3 Customer can verify their total order cost is correct with the check_out method and then place an order

--- lib/order.rb
+++ lib/order.rb
@@ -17,7 +17,39 @@ class Order
     order_item = {}
     order_item[item] = quantity
     @total_order << order_item
-    puts "#{quantity}x #{item}(s) added to your basket"
+    puts "#{item} x#{quantity} added to your basket"
+  end
+
+  def order_summary
+    order_summary = []
+    @total_order.each do |order_hash|
+      order_hash.each do |item, quantity|
+        order_summary << "#{item} x#{quantity} = £#{@menu.menu[item] * quantity}"
+      end
+    end
+    puts order_summary.join(', ')
+    puts total_basket
+  end
+
+  def total_basket
+    "Your total cost for this order is £#{calculate_cost}"
+  end
+
+  def check_out(expected_cost)
+    fail "Difference between expected cost and calculated cost" if expected_cost != calculate_cost
+    "Press 'Place Order' to submit your order through to the restaurant"
+  end
+
+  private
+
+  def calculate_cost
+    cost = 0
+    @total_order.each do |order_hash|
+      order_hash.each do |item, quantity|
+        cost += @menu.menu[item] * quantity
+      end
+    end
+    cost
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -23,13 +23,51 @@ describe Order do
     it 'adds 3 chips to the order and returns a message to the customer' do
       expect do
         subject.add_item(:chips, 3)
-      end.to output("3x chips(s) added to your basket\n").to_stdout
+      end.to output("chips x3 added to your basket\n").to_stdout
     end
 
     it 'adds 1 pizza and 2 kebabs to the order' do
       subject.add_item(:pizza)
-      subject.add_item(:kebab, 2)
-      expect(subject.total_order).to eq [{ pizza: 1 }, { kebab: 2 }]
+      subject.add_item(:burger, 2)
+      expect(subject.total_order).to eq [{ pizza: 1 }, { burger: 2 }]
+    end
+  end
+
+  describe '#order_summary' do
+
+    it { is_expected.to respond_to :order_summary }
+
+    context 'customer orders 2x pizza, 3x chips, 1x burger' do
+      it 'shows the customers their list of dishes, quantity, price and total cost' do
+        subject.add_item(:pizza, 2)
+        subject.add_item(:chips, 3)
+        expect do
+          subject.order_summary
+        end.to output("pizza x2 = £16.0, chips x3 = £6.0\nYour total cost for this order is £22.0\n").to_stdout
+      end
+    end
+  end
+
+  describe '#total_basket' do
+    it { is_expected.to respond_to :total_basket }
+  end
+
+  describe '#check_out' do
+
+    it { is_expected.to respond_to :check_out }
+
+    it 'raises an error if expected_cost does not equal calculated cost' do
+      subject.add_item(:pizza)
+      subject.order_summary
+      message = "Difference between expected cost and calculated cost"
+      expect { subject.check_out(6.0) }.to raise_error(message)
+    end
+
+    it 'directs customer to place order if order is correct' do
+      subject.add_item(:pizza)
+      subject.order_summary
+      message = "Press 'Place Order' to submit your order through to the restaurant"
+      expect(subject.check_out(8.0)).to eq message
     end
   end
 
