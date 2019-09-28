082fff7e7706243cdbfb36305aa5cc4370ab66f4
John Dennehy <john.dennehy@gmail.com>
2019-09-28 17:27:34 +0100

    adds feature test for user story 2

--- spec/feature_tests/user_stories_spec.rb
+++ spec/feature_tests/user_stories_spec.rb
@@ -3,11 +3,23 @@ require "menu"
 # As a customer
 # So that I can check if I want to order something
 # I would like to see a list of dishes with prices
-
 describe "User story 1" do
   it "displays a list of dishes with prices" do
     menu = Menu.new
     menu.add("Ribeye", 2000)
     expect { menu.list }.to output("Ribeye (£20)\n").to_stdout
+  end
+end
+
+# As a customer
+# So that I can order the meal I want
+# I would like to be able to select some number of several available dishes
+describe "User story 2" do
+  it "may order a certain number of each dish" do
+    menu = Menu.new
+    menu.add("Ribeye", 2000)
+    menu.add("Lobster", 3000)
+    menu.list
+    expect { menu.order("Ribeye", 2) }.not_to raise_error
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,17 +4,20 @@ describe Menu do
   subject(:menu) { described_class.new }
   before do
     menu.add("Ribeye", 2000)
+    menu.add("Lobster", 3000)
   end
 
   describe "#list" do
     it "returns a list of available dishes" do
-      expect { menu.list }.to output("Ribeye (£20)\n").to_stdout
+      expect { menu.list }.to output("Ribeye (£20)\nLobster (£30)\n").to_stdout
     end
   end
   describe "#add" do
     it "adds a dish and price to the menu" do
       menu.add("Pizza", 1000)
-      expect { menu.list }.to output("Ribeye (£20)\nPizza (£10)\n").to_stdout
+      expect { menu.list }.to output("Ribeye (£20)\nLobster (£30)\nPizza (£10)\n").to_stdout
     end
   end
+
+
 end
