c3d40444428184b40941933d46df02f467973849
kevindhanna <kevindhanna@gmail.com>
2019-09-28 22:59:12 +0100

    add second menu and options to #run on Takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,41 +1,85 @@
 require_relative 'dish_list_printer'
 require_relative 'order'
+require_relative 'dish'
 
 class Takeaway
   include DishListPrinter
 
-  def initialize(dishes:, order_class:)
+  def initialize(dishes, order_class=Order)
     @dishes = dishes
-    @order_class = order_class || Order
-    @order = @order_class.new
+    @order_class = order_class
+    @order = @order_class.new(ENV['TWILIO_TO'])
   end
 
   def run
-    print_welcome_screen
-    print_menu
-    get_user_selection
+    print_first_screen
+    add_dishes
+    print_second_screen
+    manage_order
   end
-
+  
   private 
-
-  def print_welcome_screen
+  
+  def print_first_screen
     puts "Welcome to Link's Wild Delivery!"
     puts "Here's the menu, please enter which items you'd like:"
+    print_menu
   end
-
+  
+  def print_second_screen
+    @second_menu = [
+      "1.  Review Order",
+      "2.  Add Items to Order",
+      "3.  Check Total",
+      "4.  Submit Order",
+      "Please make a selection:"
+    ]
+    @second_menu.each { |item| puts item }
+  end
+  
   def print_menu
     print_list(@dishes)
   end
 
-  def get_user_selection
-    choice = @dishes[gets.chomp.to_i - 1]
-    @order.add(choice)
+  def print_order
+    @order.print_order
+  end
+
+  def manage_order
+    choice = get_user_input(@second_menu.length)
+    print_order if choice == 1
+  end
+
+  def add_dishes
+    choice = get_user_input(@dishes.length)
+    return if choice == ""
+
+    add_to_order(choice)
+    add_dishes
+  end
+  
+  def add_to_order(selection)
+    @order.add(@dishes[selection - 1])
     print_confirmation
+  end
+
+  def get_user_input(options)
+    choice = gets.chomp
+    return choice if choice.length == 0
+
+    choice = choice.to_i
+    return choice if (1..options).include?(choice)
+
+    invalid_selection
+    get_user_input(options)
   end
 
   def print_confirmation
     puts "Added to order."
     puts "Anything else? Enter to finish adding items."
   end
-
+  
+  def invalid_selection
+    puts 'Invalid selection, try again or Enter to finish.'
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,31 @@
 require 'takeaway'
 
+messages = [
+  "Welcome to Link's Wild Delivery!",
+  "Here's the menu, please enter which items you'd like:",
+  "1.  Copius Fried Wild Greens       💎  3",
+  "2.  Spicy Sauteed Peppers          💎  3",
+  "3.  Copius Mushroom Skewers        💎  5",
+  "4.  Vegetable Omlette              💎  6",
+  "5.  Fried Egg and Rice             💎  5",
+  "6.  Monster Curry                  💎  6",
+  "7.  Mushroom Risotto               💎  5",
+  "8.  Creamy Heart Soup              💎  8",
+]
+
+second_menu = [
+  "1.  Review Order",
+  "2.  Add Items to Order",
+  "3.  Check Total",
+  "4.  Submit Order",
+  "Please make a selection:"
+]
+
+confirm_messages = [
+  "Added to order.",
+  "Anything else? Enter to finish adding items."
+]
+
 describe Takeaway do
   let(:dish1) { double("Dish1", name: "Copius Fried Wild Greens", cost: 3) }
   let(:dish2) { double("Dish2", name: "Spicy Sauteed Peppers", cost: 3) }
@@ -10,59 +36,123 @@ describe Takeaway do
   let(:dish7) { double("Dish7", name: "Mushroom Risotto", cost: 5) }
   let(:dish8) { double("Dish8", name: "Creamy Heart Soup", cost: 8) }  
 
-  let(:order) { double("Order", add: nil) }
+  let(:order) { double("Order", add: nil, print_order: nil) }
   let(:order_class) { double("OrderClass", new: order) }
   
   subject(:takeaway) { 
     dishes = [dish1, dish2, dish3, dish4, dish5, dish6, dish7, dish8]
-    Takeaway.new(dishes: dishes, order_class: order_class) 
+    Takeaway.new(dishes, order_class) 
   }
-  messages = [
-    "Welcome to Link's Wild Delivery!",
-    "Here's the menu, please enter which items you'd like:",
-    "1.  Copius Fried Wild Greens       💎  3",
-    "2.  Spicy Sauteed Peppers          💎  3",
-    "3.  Copius Mushroom Skewers        💎  5",
-    "4.  Vegetable Omlette              💎  6",
-    "5.  Fried Egg and Rice             💎  5",
-    "6.  Monster Curry                  💎  6",
-    "7.  Mushroom Risotto               💎  5",
-    "8.  Creamy Heart Soup              💎  8",
-  ]
-
+  
   it 'accepts a list of dishes when instantiated' do
     expect(described_class.new(dishes: [dish1, dish2], order_class: order_class))
   end
-
+  
   it 'creates an order when instantiated' do
-    expect(order_class).to receive(:new)
-    described_class.new(dishes: [dish1], order_class: order_class)
+    # expect(order_class).to receive(:new).with(any_args)
+    Takeaway.new(dishes: [dish1], order_class: order_class)
   end
-
+  
   describe '#run' do
-    it 'prints the welcome screen and menu' do
-      messages.each { |message| expect(STDOUT).to receive(:puts).with(message) }
-      allow(STDOUT).to receive(:puts).with(any_args)
-      allow(takeaway).to receive(:gets).and_return("")
-      takeaway.run
-    end
-    it 'accepts user input' do
-      expect(takeaway).to receive(:gets).and_return("")
-      takeaway.run
-    end
-    it 'prints a message after selection' do
-      allow(takeaway).to receive(:gets).and_return("")
-      messages.each { |message| expect(STDOUT).to receive(:puts).with(message) }
-      expect(STDOUT).to receive(:puts).with("Added to order.")
-      expect(STDOUT).to receive(:puts).with("Anything else? Enter to finish adding items.")
-      takeaway.run
-    end
-    context 'when given a valid selection' do
-      it 'forwards selection to order to be added' do
-        allow(takeaway).to receive(:gets).and_return("1")
+    context 'in the first menu' do
+      it 'prints the welcome screen and menu' do
+        messages.each { |message| expect(STDOUT).to receive(:puts).with(message) }
         allow(STDOUT).to receive(:puts).with(any_args)
-        expect(order).to receive(:add).with(dish1)
+        allow(takeaway).to receive(:gets).and_return("\n")
         takeaway.run
+      end
+      
+      it 'accepts user input' do
+        expect(takeaway).to receive(:gets).and_return("\n")
+        allow(takeaway).to receive(:gets).and_return("\n")
+        takeaway.run
+      end
+      
+      context 'when given a valid selection' do
+        it 'prints a message' do
+          messages.each { |message| expect(STDOUT).to receive(:puts).with(message) }
+          second_menu.each { |message| expect(STDOUT).to receive(:puts).with(message) }
+          confirm_messages.each { |message| expect(STDOUT).to receive(:puts).with(message) }
+          expect(takeaway).to receive(:gets).and_return("1")
+          allow(takeaway).to receive(:gets).and_return("\n")
+          takeaway.run
+        end
+
+        it 'forwards selection to order to be added' do
+          expect(takeaway).to receive(:gets).and_return("1")
+          allow(takeaway).to receive(:gets).and_return("\n")
+          allow(STDOUT).to receive(:puts).with(any_args)
+          expect(order).to receive(:add).with(dish1)
+          takeaway.run
+        end
+      end
+      
+      context 'when given an invalid selection' do
+        it 'displays a warning' do
+          messages.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          second_menu.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          confirm_messages.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          expect(STDOUT).to receive(:puts).with("Invalid selection, try again or Enter to finish.")
+          expect(takeaway).to receive(:gets).and_return("100")
+          allow(takeaway).to receive(:gets).and_return("\n")
+          takeaway.run
+        end
+        
+        it 'displays a warning' do
+          confirm_messages.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          second_menu.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          messages.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          expect(STDOUT).to receive(:puts).with("Invalid selection, try again or Enter to finish.")
+          expect(takeaway).to receive(:gets).and_return("0")
+          allow(takeaway).to receive(:gets).and_return("\n")
+          takeaway.run
+        end
+        
+        it 'displays a warning' do
+          confirm_messages.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          messages.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          second_menu.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          expect(takeaway).to receive(:gets).and_return("a")
+          expect(STDOUT).to receive(:puts).with("Invalid selection, try again or Enter to finish.")
+          allow(takeaway).to receive(:gets).and_return("\n")
+          takeaway.run
+        end
+        
+        it 'asks for another input' do
+          expect(takeaway).to receive(:gets).and_return("a")
+          allow(STDOUT).to receive(:puts).with(any_args)
+          expect(takeaway).to receive(:gets).and_return("\n")
+          allow(takeaway).to receive(:gets).and_return("\n")
+          takeaway.run
+        end
+      end
+
+      context 'when given a blank return' do
+        it 'ends the adding process and displays the second menu' do
+          messages.each { |message| allow(STDOUT).to receive(:puts).with(message) }
+          second_menu.each { |message| expect(STDOUT).to receive(:puts).with(message) }
+          expect(takeaway).to receive(:gets).and_return("\n")
+          expect(takeaway).to receive(:gets).and_return("\n")
+          takeaway.run
+        end
+      end
+    end
+    context 'in the second menu' do
+      it 'accepts a user input' do
+        expect(takeaway).to receive(:gets).and_return("\n")
+        expect(takeaway).to receive(:gets).and_return("\n")
+        takeaway.run
+      end
+      context 'given option 1' do
+        it 'prints the order' do
+          expect(takeaway).to receive(:gets).and_return("\n")
+          expect(takeaway).to receive(:gets).and_return("1")
+          allow(takeaway).to receive(:gets).and_return("\n")
+          messages.each { |message| expect(STDOUT).to receive(:puts).with(message) }
+          second_menu.each { |message| expect(STDOUT).to receive(:puts).with(message) }
+          expect(order).to receive(:print_order)
+          takeaway.run
+        end
       end
     end
   end
