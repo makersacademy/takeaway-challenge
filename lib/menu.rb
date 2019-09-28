d60093cc845d73d7a3294968238fc98560d24562
zraso <zaira.rasool@hotmail.co.uk>
2019-09-28 17:22:12 +0100

    Add total to choose method to calculate order charge

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,11 @@
 class Menu
 
-  attr_reader :list, :order
+  attr_reader :list, :order, :total
 
   def initialize
     @list = {"Chicken"=>5, "Fish"=>7, "Lamb"=>6, "Vegan"=>5}
     @order = []
+    @total = 0
   end
 
   def see
@@ -15,5 +16,7 @@ class Menu
   #
   def choose(dish_selection, quantity)
     @order << { dish_selection: dish_selection, quantity: quantity }
+    @total += @list[dish_selection] * quantity
   end
-  end
+  
+end
