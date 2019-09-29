f24b0808dffb3060a4ef73f643c2dac4a9a9ebb9
Student <student@Makerss-MBP.home>
2019-09-29 20:42:55 +0100

    added a menu and a way to view the menu, logic hiden in private

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,31 @@
 class Menu
+  def initialize
+    @menu = {
+    haddock: 7,
+    cod: 6,
+    scampi: 5,
+    large_chips: 3,
+    small_chips: 2
+    }
+  end
+
+   def view_menu
+    "Fish & Chip Shop Menu"
+     spacer
+     list_dishes
+     spacer
+   end
+
+   private
+
+   def spacer
+     "---------------------"
+   end
+
+   def list_dishes
+     @menu.each do |dish, price|
+       "#{dish} - £#{price}"
+     end
+   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,10 @@
 require 'menu'
 
-decribe 'Menu' do
+describe Menu do
+  subject(:menu){described_class.new}
+
+  it 'is created with a list of dishes' do
+    expect(menu.view_menu).not_to be_empty
+  end
 
 end
