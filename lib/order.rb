81558dff17c34683ce12e6c010855f363f9616b0
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-09-29 15:53:09 +0100

    Added basket_summary

--- lib/order.rb
+++ lib/order.rb
@@ -9,11 +9,12 @@ class Order
   end
 
   def add(item, quantity)
-    item = item.to_sym
+    @item = item.to_sym
+    @quantity = quantity
     @menu.all_dishes.each do |dish|
       @dish = dish
       dish.each do |name, price|
-        if name == item
+        if name == @item
           quantity.times {@basket << @dish}
           quantity.times {@total += price}
         end
@@ -22,8 +23,13 @@ class Order
     @basket
   end
 
+  def basket_summary
+    puts @basket
+    "Your total for this order is £#{@total}. Please proceed to checkout."
+  end
+
   def checkout(money)
-    
+
   end
 
 
