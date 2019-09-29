17ab6dc379905c3ed04e2a244d390620d9ec303b
Brad Phillips <bjrp@protonmail.com>
2019-09-29 20:20:50 +0100

    Adds preview order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -13,9 +13,35 @@ class Takeaway
   end
 
   def preview_order(*items)
-    items.each do |item|
-      puts item
-    end
+    puts order_header
+    puts construct_order(items)
+    puts "Your order comes to: £#{calculate_price(items)}"
+    puts order_footer
+  end
+
+  private
+
+  def order_header
+    'Your order:'
+  end
+
+  def order_footer
+    'Thank you for your custom. See you again!'
+  end
+
+  def calculate_price(order)
+    order.map do |i|
+    
+      @menu.menu.select { |hash| hash[:item] == i }
+    end.flatten.map { |x| x[:price] }.inject(:+)
+  end
+
+  def construct_order(order)
+    x = order.map do |i|
+    
+      @menu.menu.select { |hash| hash[:item] == i }
+    end.flatten 
+    x.each_with_index { |hash, index| puts "#{index + 1 }. #{hash[:item]} ~ £#{hash[:price]}" }
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,5 +6,6 @@ describe Menu do
   describe '#format menu' do
       menu = Menu.new([{item: 'pizza', price: 3}])
       specify { expect { menu.format_menu }.to output("1. pizza ~ £3\n").to_stdout }
+
   end
 end
