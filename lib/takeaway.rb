7a4db95c737aa86e7c8667e50b6d7aa52a10bb2e
zraso <zaira.rasool@hotmail.co.uk>
2019-09-29 12:48:53 +0100

    Takeaway class can #order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,10 +1,20 @@
 class Takeaway
 
+  attr_reader :dish_selection, :quantity, :order_list
+
   def initialize(menu = Menu.new)
     @menu = menu
+    @order_list = []
   end
 
   def menu
     @menu.see
   end
+
+  def order(dish_selection, quantity)
+    @order_list << { dish_selection: dish_selection, quantity: quantity }
+    @dish_selection = dish_selection
+    @quantity = quantity
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,7 +2,15 @@ require 'takeaway'
 
 describe Takeaway do
 
+  subject(:takeaway) { described_class.new }
+
   it 'displays a menu' do
-    expect(subject.menu).to eq "Chicken"=>5, "Fish"=>7, "Lamb"=>6, "Vegan"=>5
+    expect(takeaway.menu).to eq "Chicken"=>5, "Fish"=>7, "Lamb"=>6, "Vegan"=>5
+  end
+
+  it 'can select a number of dishes' do
+    takeaway.order("Chicken", 2)
+    takeaway.order("Fish", 3)
+    expect(takeaway.order_list).to eq [{dish_selection: "Chicken", quantity: 2}, {dish_selection: "Fish", quantity: 3}]
   end
 end
