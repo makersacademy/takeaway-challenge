cfa841c8747050e4f0984fdbba8446cbab414c3f
Student <student@Makerss-MBP.home>
2019-09-30 00:37:14 +0100

    written out menu for customers options.

--- lib/customer_interface.rb
+++ lib/customer_interface.rb
@@ -2,8 +2,40 @@
 
 class Interface
 
-  def request_input(customer_input = gets.chomp)
-    customer_input
+  display_menu
+
+  customer_input = gets.chomp
+
+  case customer_input
+    when "1"
+      puts 'order'
+    when "2"
+
+    when "3"
+
+    when "4"
+
+    when "5"
+
+    when "9"
+      exit
+    else
+      puts "invalid input, try agian"
   end
 
+  private
+  
+  def display_menu
+    puts "Welcome to Fish & Chip Shop"
+    puts "---------------------------"
+    puts "MENU:"
+    puts "- press 1 to see our menu."
+    puts "- press 2 order a dish."
+    puts "- press 3 to see your order."
+    puts "- press 4 to see your order total."
+    puts "- press 5 to place an order."
+    puts "- press 9 to exit."
+    puts "---------------------------"
+    print "What would you like to do?: "
+  end
 end
