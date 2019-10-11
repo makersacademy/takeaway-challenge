2489298c5e77c234c81135c7d84656b9b63ecaeb
Adam1824 <forrestadam07@gmail.com>
2019-10-11 12:11:18 +0100

    done second user story

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,53 @@
+require_relative 'restaurant'
+
 class Customer
+  attr_accessor :order
+
+  def initialize
+    @order = []
+  end
 
   def see_menu(restaurant)
     output = ""
     restaurant.menu.each do |dish, price|
       output += "#{dish} are £#{price}\n"
     end
+    puts output
     output
   end
 
+  def get_order(restaurant)
+    see_menu(restaurant)
+    current_order = []
+    puts "Enter item:"
+    item = gets.chomp
+    if restaurant.menu.include?(item)
+      current_order << item
+      puts "How many #{item} do you want?"
+      num = gets.chomp.to_i
+      if num.positive? && num.integer?
+        current_order << num
+        @order << current_order
+        puts "You requested to make an order of #{num} #{item}"
+        return @order
+      else
+        puts "Incorrect amount"
+      end
+    else
+      puts "Item doesn't exist on the menu"
+    end
+    raise 'Order was invalid'
+  end
+
+  # def order_status
+  #   if @order == []
+  #     raise 'No Order'
+  #   else
+  #     true
+  #   end
+  # end
 end
+
+# customer = Customer.new
+# restaurant = Restaurant.new
+# customer.get_order(restaurant)
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -16,4 +16,18 @@ describe Customer do
       )
     end
   end
+
+  describe '#get_order' do
+
+    it 'can test if the user inputs a correct order, raises an error if not and fails test' do
+      restaurant = double('restaurant')
+      allow(restaurant).to receive(:menu).and_return({
+        'chicken_burgers' => 2.50,
+        'beef_burgers' => 2.50,
+        'chips' => 1.00
+      })
+      order = subject.get_order(restaurant)
+      expect { raise order }.not_to raise_error 'Order was invalid'
+    end
+  end
 end
