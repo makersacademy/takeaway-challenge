96deb5f39366546fdfc17e3ef17652f2cd803346
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-10-11 15:54:20 +0100

    refactor

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,26 +1,28 @@
 require 'twilio-ruby'
 
 class Sms
+
+  attr_reader :account_sid, :auth_token, :client, :from, :to
+
   def initialize
+    @account_sid = 'ACeaf08799fdf07e04a450f4cbf303137f'
+    @auth_token = '1e6b983cfda00aa789175faa8466309b'
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    @from = '+13346506074' # Your Twilio number
+    @to = '+447452973941' # Your mobile phone number
+
   end
   def time
-    time = Time.new
-    hour = "#{time.hour + 1}:#{time.min}"
-
-    return hour
+    temp = Time.new
+    time = "#{temp.hour + 1}:#{temp.min}"
+    return time
   end
-  def sendMessage(order)
-    account_sid = 'ACeaf08799fdf07e04a450f4cbf303137f'
-    auth_token = '1e6b983cfda00aa789175faa8466309b'
-    client = Twilio::REST::Client.new(account_sid, auth_token)
-    from = '+13346506074' # Your Twilio number
-    to = '+447452973941' # Your mobile phone number
 
-    client.messages.create(
-    from: from,
-    to: to,
-    body:
-    "Order will arrive at #{time} \nTotal Paid: #{order}"
+  def sendMessage(order)
+    @client.messages.create(
+    from: @from,
+    to: @to,
+    body: "Order will arrive at #{time} \nTotal Paid: #{order}"
     )
   end
 end
--- spec/send_sms_spec.rb
+++ spec/send_sms_spec.rb
@@ -3,6 +3,7 @@ describe Sms do
 
   it 'gets time and sets delivery time' do
     actual_time = Time.new
+    
     expect(subject.time).to eq "#{actual_time.hour + 1}:#{actual_time.min}"
   end
 end
