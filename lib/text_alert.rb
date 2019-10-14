d2db587ebe82acd38035f13d2d44d3ff4b005902
Luke Jones <lukejonesice@yahoo.co.uk>
2019-10-14 09:15:33 +0100

    changing git ignore

---
+++ lib/text_alert.rb
@@ -0,0 +1,17 @@
+require "bundler"
+Bundler.require()
+# require "Envfile"
+
+account_sid = ENV["TWILIO_ACCOUNT_SID"]
+auth_token = ENV["TWILIO_AUTH_TOKEN"]
+
+class TextAlert
+
+  def send_text
+
+  end
+
+
+end
+
+p account_sid
