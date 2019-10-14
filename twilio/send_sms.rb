64b86d99c8dbd22fda5f75154cd9f8b997d9e6e9
hames <james_bingham@live.co.uk>
2019-10-14 09:34:51 +0100

    Update send_sms.rb

--- twilio/send_sms.rb
+++ twilio/send_sms.rb
@@ -1,8 +1,7 @@
 # Download the twilio-ruby library from twilio.com/docs/libraries/ruby
 require 'twilio-ruby'
 
-account_sid = 'AC579e4968fbbdc84cd908d0b213589106'
-auth_token = 'a45da61c48a7b9e44cae7851ca460dfa'
+no more
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+19735595553' # Your Twilio number
