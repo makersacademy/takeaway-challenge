758709af8145bf99367badbbe5cbe190e5338c94
adam.beresford <adam56.beresford@gmail.com>
2019-10-14 09:47:36 +0100

    testing for multiple meals inputted

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -8,8 +8,11 @@ describe Customer do
     # Act
     restaurant.menu
     subject.customer_order
+    subject.customer_order
     # Assert
-    expect(subject.customer_order_array).to include({:amount_ordered=>"3",
-       :customer_meal=>"pesto pasta"})
+    expect(subject.customer_order_array).to include(
+      {:amount_ordered=>"3", :customer_meal=>"pesto pasta"},
+       {:amount_ordered=>"4", :customer_meal=>"pigeon"})
   end
+
 end
