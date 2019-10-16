8e578dc4a616c3f4b6f18c0140d194c648157233
Chris Groves <chris_groves87@hotmail.co.uk>
2019-10-16 12:01:27 +0100

    feature: view 5 menu items with prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,5 @@
 class Menu
   def view
-    print "Pizza - £10"
+    print "Pizza - £10, Burger - £5, Chips - £2, Coffee - £3, Coke - £2"
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,8 @@
 require 'menu'
 
 describe Menu do
-  it 'displays 1 menu item with a price' do
+  it 'displays 5 menu items with prices' do
     menu = Menu.new
-    expect { menu.view }.to output("Pizza - £10").to_stdout
+    expect { menu.view }.to output("Pizza - £10, Burger - £5, Chips - £2, Coffee - £3, Coke - £2").to_stdout
   end
 end
