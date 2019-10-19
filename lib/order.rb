2911e9aafca00e90d56a55e2ff2d34613f85a149
Chris Groves <chris_groves87@hotmail.co.uk>
2019-10-19 10:15:27 +0100

    feature: can select a single dish from the menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,19 @@
 class Order
+  attr_reader :menu, :selected_dishes
 
+  def initialize(menu)
+    @menu = menu
+    @food_items = menu.food_items
+    @selected_dishes = []
+  end
+
+  def select_dish(item)
+    @food_items.each do |food, price|
+      @selected_dishes << food if food == item
+    end
+  end
+
+  def view_selected_dishes
+    print selected_dishes.join
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,6 +1,10 @@
 require 'order'
 
 describe Order do
-  
-
+  it 'can select a dish from the menu' do
+    menu = Menu.new
+    order = Order.new(menu)
+    order.select_dish('Pizza')
+    expect { order.view_selected_dishes }.to output('Pizza').to_stdout
+  end
 end
