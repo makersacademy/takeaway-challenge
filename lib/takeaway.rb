6929660d453c025413ff256b2479a9015cfe5e44
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-10-24 23:33:47 +0100

    Refactoring

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,15 @@
 class Takeaway
   
-  def show_menu(menu = Menu.new)
-    menu.dishes
+  def initialize(menu)
+    @menu = menu
   end
 
-  
+  def show_menu
+    @menu.dishes
+  end
 
+  # def add_item_to_cart(item_number, quantity)
+  #   chosen_item = @menu.select_item(item_number)
+  #   @cart.add_item
+  # end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,11 +2,10 @@ require 'takeaway'
 require 'dish'
 
 describe Takeaway do
-  subject { described_class.new }
-  menu = Menu.new
+  subject { described_class.new(Menu.new) }
 
   it 'checks that menu is a list of dishes' do
-    expect(subject.show_menu(menu)).to all(be_an(Dish))
+    expect(subject.show_menu).to all(be_an(Dish))
   end
   # it '' do
   # end
