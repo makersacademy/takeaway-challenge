74d354cedef9cf780468d28113c306e235cb7fb7
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-10-24 21:21:26 +0100

    Dishes can be added to the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,8 +3,12 @@ require 'dish'
 class Menu
 
   attr_reader :dishes
-  
+
   def initialize
     @dishes = []
+  end
+
+  def add_dish(dish = Dish.new("Swedish Meatballs", 2.50))
+    @dishes << dish
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,13 @@
 require 'menu'
+require 'dish'
 
 describe Menu do
-  
+  subject { described_class.new }
+  dish = Dish.new("Swedish Meatballs", 2.50) 
   it 'has an empty list of dishes' do
     expect(subject.dishes).to eq []
+  end
+  it 'has a method for adding a dish to the list of dishes' do
+    expect(subject.add_dish(dish)).to eq [dish]
   end
 end
