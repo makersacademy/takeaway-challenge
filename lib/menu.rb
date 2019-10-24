424014287822b605afec4242e890b098869090bd
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-10-24 18:23:58 +0100

    Menu class added, has empty list on initialization

---
+++ lib/menu.rb
@@ -0,0 +1,10 @@
+require 'dish'
+
+class Menu
+
+  attr_reader :dishes
+  
+  def initialize
+    @dishes = []
+  end
+end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,1 +1,8 @@
+require 'menu'
 
+describe Menu do
+  
+  it 'has an empty list of dishes' do
+    expect(subject.dishes).to eq []
+  end
+end
