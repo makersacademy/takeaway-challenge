01b7a1a937103d84ca28734243dc196cf188a1f8
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-25 11:25:40 +0100

    created failing view_menu method

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,9 +2,10 @@ class Menu
   attr_reader :menu_items
   
   def initialize 
-    @menu_items = { 
-      kungpochicken: 6.80,
-      chickenchowmein: 6.30,
-    }
+    @menu_items = [
+      {item: "kung po chicken", price: 6.80},
+      {item: "chicken chow mein", price: 6.30}
+    ]
   end
+  
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,7 +5,11 @@ describe Menu do
     expect(subject).to be_an_instance_of Menu
   end
 
-  it 'creats a hash with menu items and their prices' do
+  it 'creates an array with menu items and their prices' do
     expect(subject.menu_items).not_to be_empty
+  end
+
+  it 'prints out the menu items' do
+    expect(subject.view_menu).to output menu_items
   end
 end
