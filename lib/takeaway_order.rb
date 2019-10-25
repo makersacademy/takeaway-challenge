0dd90a1d3a7fa8fb48e9b74ac4cdad71b2d2bfb7
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-25 12:41:05 +0100

    :sparkles::white_check_mark: Add code to check basket total is correct and pass tests

--- lib/takeaway_order.rb
+++ lib/takeaway_order.rb
@@ -20,4 +20,10 @@ class TakeawayOrder
   def basket_total(total)
     @current_order_cost = total
   end
+
+  def check_basket_total(takeaway_order)
+    final_total = 0
+    takeaway_order.list_of_dishes.each { |takeaway_dish, amount_of_each_dish_ordered| final_total += (takeaway_dish.price * amount_of_each_dish_ordered) }
+    final_total == takeaway_order.current_order_cost
+  end
 end
