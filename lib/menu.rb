c55b96172e693b2b053b93647eedb847a0212644
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-25 11:48:47 +0100

    rewrote test using stdout

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,8 +4,11 @@ class Menu
   def initialize 
     @menu_items = [
       {item: "kung po chicken", price: 6.80},
-      {item: "chicken chow mein", price: 6.30}
+      {item: "chicken chow mein", price: 6.50}
     ]
+  end
+
+  def view_menu
   end
   
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -10,6 +10,7 @@ describe Menu do
   end
 
   it 'prints out the menu items' do
-    expect(subject.view_menu).to output menu_items
+    expect{subject.view_menu}.to output("Kung po chicken £6.80, 
+Chicken chow mein £6.50").to_stdout
   end
 end
