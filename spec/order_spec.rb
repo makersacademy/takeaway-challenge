f71a510af1ce8d0d777a9c70c1b22364c01f4ee7
Student <student@Admins-MacBook-Air.local>
2019-10-25 09:46:23 +0100

    tested get customer order

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,4 +5,8 @@ describe Process_order do
   it "has an instance of the process order class" do
     expect(process_order).to be_a(Process_order)
   end
+
+  it "has a method for checking what the customer wants to do" do
+    expect(process_order.get_customer_request).to respond_to(process_order)
+  end
 end
