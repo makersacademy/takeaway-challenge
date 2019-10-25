adadfb69c11b8eb054c961f85a1483131a392a7f
Rhys <mrevinz@gmail.com>
2019-10-25 09:35:12 +0100

    fixed comma bug + passed menu test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,10 +1,16 @@
 require 'rspec'
 require './src/menu'
+require './src/item'
 
 describe Menu do
+
     subject {Menu.new}
 
     it "should init with an empty basket" do
         expect(subject.basket).to eq(Array.new)
+    end
+
+    it "should display a list of items in the menu" do
+        expect(subject.menu).to include(a_kind_of(Item))
     end
 end 
--- src/Menu.rb
+++ src/Menu.rb
@@ -1,7 +1,10 @@
-class Menu
-    attr_accessor :basket
+require_relative 'item'
 
-    def initialize
+class Menu
+    attr_reader :basket, :menu
+
+    def initialize()
         @basket = []
+        @menu = [Item.new("Pizza", 3.99), Item.new("Burger", 4.99), Item.new("Fries", 2.99)]
     end
 end
