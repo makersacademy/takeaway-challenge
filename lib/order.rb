f8334d388a8af97d3b1d318da9bff9bc1b656029
Topher <topherb99@gmail.com>
2019-10-25 19:55:00 +0100

    Twilio integration added, need to add coherent text message on checkout

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,6 @@
+require 'twilio-ruby'
+require_relative 'twilio'
+
 class Order
   DEFAULT_MENU = {
     curry: 6,
@@ -6,8 +9,8 @@ class Order
     nuggets: 4,
     salad: 3
   }
-
   attr_reader :menu, :order
+  include Twilio
 
   def initialize(menu = DEFAULT_MENU)
     @menu = menu
@@ -15,6 +18,12 @@ class Order
   end
 
   def checkout
+    meals = []
+    @order.each { |hash| meals << hash[:quantity]
+    meals << hash[:meal]
+    }
+    body = "Thank you! Your order was placed at #{(Time.now).strftime("%k:%M")} and will be delivered before #{(Time.now + 1800).strftime("%k:%M")}"
+    send_text(body)
   end
 
   def select(item, quantity)
