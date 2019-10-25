e54b688b6cfe3d10c44fea7a676165c7c54b1c94
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-25 10:26:16 +0100

    :construction::white_check_mark: Add failing test to check that menu initializes with dishes

--- spec/takeaway_menu_spec.rb
+++ spec/takeaway_menu_spec.rb
@@ -1,11 +1,21 @@
 require "takeaway_menu"
 
 describe TakeawayMenu do
-  let(:takeaway_menu) { TakeawayMenu.new }
+  let(:takeaway_menu) { TakeawayMenu.new(:szechuan_chilli_beef, :szechuan_chilli_chicken, :kung_po_chicken, :vegetable_spring_rolls) }
+  let(:szechuan_chilli_beef) { double :TakeawayDish }
+  let(:szechuan_chilli_chicken) { double :TakeawayDish }
+  let(:kung_po_chicken) { double :TakeawayDish }
+  let(:vegetable_spring_rolls) { double :TakeawayDish }
 
-  context "initialize" do
+  context 'initialize' do
     it "successfully instantiates the TakeawayMenu class" do
       expect(takeaway_menu).to be_instance_of(TakeawayMenu)
+    end
+
+    describe "#takeaway_dishes" do
+      it "returns the dishes created at initiali" do
+        expect(takeaway_menu.takeaway_dishes).to include(:szechuan_chilli_beef, :szechuan_chilli_chicken, :kung_po_chicken, :vegetable_spring_rolls)
+      end
     end
   end
 end
