cb80fae3c7cfb5d31ad6e233da0a8a768ce94f56
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-25 09:35:19 +0100

    Added failing test for dishes array

--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -4,4 +4,8 @@ describe Dishes do
     it 'creates an instance of the dishes class' do
         expect(subject).to be_an_instance_of(Dishes)
     end
+
+    it 'has an array of dishes' do
+        expect(subject.dishes_arr).to include("Donner Kebab")
+    end
 end
