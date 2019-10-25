fe9fb83c7084887a1d46e43685f5a2031fdc8671
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-25 14:03:05 +0100

    created failing test for basket array

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,4 +4,8 @@ describe Order do
   it 'creates and instance of Order' do
     expect(subject).to be_an_instance_of Order
   end
+
+  it 'creats an empty array basket' do
+    expect(subject.basket).to be_empty
+  end
 end
