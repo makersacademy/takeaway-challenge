a0a36a0365c10f8c4883f8a9db126007a572a815
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-25 09:47:39 +0100

    failing test for manu_items

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,6 @@
 class Menu
+  attr_reader :menu_items
+  def initialize 
+    menu_items
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,4 +4,8 @@ describe Menu do
   it 'creates an instance of Menu' do
     expect(subject).to be_an_instance_of Menu
   end
+
+  it 'creats a hash with menu items and their prices' do
+    expect(subject.menu_items).to have_attributes(:key).and (1.00)
+  end
 end
