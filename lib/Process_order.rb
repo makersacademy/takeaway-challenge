8a775cf706986679831ae80a9d416c09f59a97d6
Student <student@Admins-MacBook-Air.local>
2019-10-25 10:07:57 +0100

    deleted previous test, added init one

--- lib/Process_order.rb
+++ lib/Process_order.rb
@@ -1,2 +1,5 @@
 class Process_order
+  def get_customer_request
+    true
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,12 +1,12 @@
 require "process_order"
+
 describe Process_order do
   let(:process_order) { Process_order.new }
 
   it "has an instance of the process order class" do
     expect(process_order).to be_a(Process_order)
   end
-
-  it "has a method for checking what the customer wants to do" do
-    expect(process_order.get_customer_request).to respond_to(process_order)
+  it "initializes with no order automatically" do
+    expect(process_order.want_to_order).to eq false
   end
 end
