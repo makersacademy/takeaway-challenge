033ace74b01324d930fdacba1b2a43c800267a04
slongboro <susanlongley@gmail.com>
2019-10-25 14:09:02 +0100

    changed the code to curly brackets, test passing

--- lib/dish.rb
+++ lib/dish.rb
@@ -5,7 +5,7 @@ class Dish
     end
 
     def menu
-        menu = ["Pizza" => 10, "Curry" => 5, "Pie" => 4, "Chips" => 2, "Salad" => 2]
+        menu = {"Pizza" => 10, "Curry" => 5, "Pie" => 4, "Chips" => 2, "Salad" => 2}
         @menu = menu
         
     end
