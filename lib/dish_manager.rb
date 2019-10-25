ba1db1a980b8ff9e6c0a009316a441866fe4c12b
jtsalva <tim@jtsalva.dev>
2019-10-25 10:43:25 +0100

    added functionality with no tests, bit of a lol 😱

--- lib/dish_manager.rb
+++ lib/dish_manager.rb
@@ -1,11 +1,26 @@
+require 'dish'
+
 class DishManager
   attr_accessor :available_dishes
 
-  def initialize
+  def initialize(dishes_store_file_name)
     @available_dishes = []
+
+    load_dishes(dishes_store_file_name)
   end
 
   def find_dish_by_name(name)
     @available_dishes.select { |dish| dish.name == name }.first
+  end
+
+  private
+
+  def load_dishes(filename)
+    return if File.zero? filename
+
+    File.foreach(filename) do |line|
+      data = line.split(', ')
+      @available_dishes << Dish.new(data.first, data.last.chomp.to_f)
+    end
   end
 end
