6b2d925b1a41e8956015485de3a0b2894ea9db61
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-25 15:37:33 +0100

    failing total test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -20,9 +20,18 @@ Egg fried rice £4.50
   end
 
   describe '#select' do
-    it 'it allows a customer to store an item in basket' do
+    it 'allows a customer to store an item in basket' do
       subject.select(0)
       expect(subject.basket).to eq([:item=>"Kung po chicken", :price=>6.8])
     end
   end
+
+  describe '#total' do
+    it 'sums up the total amout of items ordered in the basket' do
+      subject.select(0)
+      subject.select(1)
+      subject.select(2)
+      expect(subject.total).should eq 17.00
+    end
+  end  
 end
