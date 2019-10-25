ceed5189725344109957e0129222bc437d78aba7
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-10-25 10:32:11 +0100

    Dish double added in Menu test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,11 +3,11 @@ require 'dish'
 
 describe Menu do
   subject { described_class.new }
-  dish = Dish.new("Swedish Meatballs", 2.50) 
   it 'has a list of dishes' do
     expect(subject.dishes).not_to be_empty
   end
   it 'can add a dish to the list of dishes' do
+    dish =  instance_double('dish', name: "Swedish Meatballs", price: 2.50)
     expect(subject.add_dish(dish)).to include(dish)
   end
   it 'returns the list of dishes' do
