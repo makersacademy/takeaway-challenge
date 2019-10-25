0d3781de70fc7f88058fbea738d87d67516cc2c4
Rob Baile <robbaile12@outlook.com>
2019-10-25 10:56:13 +0100

    written test for order list initializer

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,4 +4,8 @@ describe Order do
     it 'should be a type of order' do 
         expect(subject).to be_an_instance_of(Order)
     end 
+
+    it 'should have an empty order list' do 
+        expect(subject.order_list).to eq([])
+    end
 end
