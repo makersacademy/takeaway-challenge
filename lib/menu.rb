76a23174ddfe9543849d47b0dbe4e26f250b5115
bradley <bradley.plater1@gmail.com>
2019-10-25 09:21:00 +0100

    Added items array filled with hashes

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,6 @@
 class Menu
+  attr_reader :items
+  def initialize
+    @items = [{name: "Burger", price: 2.00}, {name: "Hot Dog", price: 1.50}]
+  end  
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -9,7 +9,7 @@ describe Menu do
       expect(subject).to be_instance_of Menu
     end
 
-    it 'Has a list of items it can choose from' do
+    it 'Has a list of items' do
       expect(subject.items).not_to be_empty
     end
   end 
