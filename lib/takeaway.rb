a89f9e47e9d64b049d48e8682dbc0d2c9c451f1d
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-25 14:29:38 +0100

    :construction::white_check_mark: Add further test coverage for printing the menu and pass all tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,16 @@
 require 'takeaway_dish'
 require 'takeaway_menu'
 require 'takeaway_order'
+require 'print_menu'
 
 class Takeaway
 
-  attr_reader :takeaway_menu, :takeaway_order_class
+  attr_reader :takeaway_menu, :takeaway_order_class, :menu_printer
 
   def initialize(args)
     @takeaway_menu = args[:takeaway_menu]
     @takeaway_order_class = args[:takeaway_order_class] || TakeawayOrder
+    @menu_printer = args[:print_module] || PrintMenu
   end
 
   def place_order
@@ -21,5 +23,9 @@ class Takeaway
 
   def start_new_order
     takeaway_order_class.new(takeaway_menu)
+  end
+
+  def display_menu
+    menu_printer.print_menu(takeaway_menu.takeaway_dishes)
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,16 +3,19 @@ require 'takeaway'
 describe Takeaway do
   let(:takeaway) { Takeaway.new(args) }
   let(:takeaway_order_class) { class_double "takeawayOrder" }
-  let(:args) { { :takeaway_menu => takeaway_menu, :takeaway_order_class => takeaway_order_class } }
+  let(:args) { { :takeaway_menu => takeaway_menu, :takeaway_order_class => takeaway_order_class, :print_module => menu_printer } }
   let(:takeaway_order) { double :TakeawayOrder }
   let(:takeaway_menu) { double :TakeawayMenu }
   let(:szechuan_chilli_beef) { double :TakeawayDish }
   let(:szechuan_chilli_chicken) { double :TakeawayDish }
   let(:kung_po_chicken) { double :TakeawayDish }
   let(:vegetable_spring_rolls) { double :TakeawayDish }
+  let(:takeaway_dishes) { [szechuan_chilli_beef, szechuan_chilli_chicken, kung_po_chicken, vegetable_spring_rolls] }
+  let(:menu_printer) { PrintMenu }
   
   before(:each) do
     allow(takeaway_order_class).to receive(:new) { takeaway_order }
+    allow(menu_printer).to receive(:print_menu) { print "string" }
   end
 
   describe '#initialize' do
@@ -30,6 +33,17 @@ describe Takeaway do
       allow(takeaway_order).to receive(:takeaway_menu) { TakeawayMenu }
       takeaway.start_new_order
       expect(takeaway_order.takeaway_menu).to eq TakeawayMenu
+    end
+  end
+
+  describe "#display_menu" do
+    it 'responds to display_menu' do
+      expect(takeaway).to respond_to(:display_menu)
+    end
+
+    it "results in a string printed" do
+      allow(takeaway_menu).to receive(:takeaway_dishes)
+      expect { takeaway.display_menu }.to output("string").to_stdout
     end
   end
 end
