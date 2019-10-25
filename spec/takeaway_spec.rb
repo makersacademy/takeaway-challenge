dfe3d03414711923af68977dc6543b7db5289d61
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-25 09:58:47 +0100

    Added test to return all items in the dishes array

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,4 +4,8 @@ describe Takeaway do
     it 'creates and instance of the takeaway class' do
         expect(subject).to be_an_instance_of(Takeaway)
     end
+
+    it 'returns the list of items on the menu' do
+        expect(subject.show_menu).to include("Chips" => 1.00)
+    end
 end
