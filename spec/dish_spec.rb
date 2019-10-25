82d8b3708e347c349fed9882ae8efb339dfc79a1
jtsalva <tim@jtsalva.dev>
2019-10-25 09:39:42 +0100

    update dish spec to work with dynamic subject

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,15 +1,19 @@
 require 'dish'
 
 describe Dish do
+  subject { Dish.new(name, price) }
+  let(:name) { 'Crabs' }
+  let(:price) { 20 }
+
   it 'is an instance of Dish' do
     expect(subject).to be_an_instance_of Dish
   end
 
-	it 'has a name' do
+	it 'has a name as a String' do
 	  expect(subject.name).to be_an_instance_of String
   end
 
-  it 'has a price' do
-    expect(subject.name).to be_an_instance_of Float
+  it 'has a price as a Float' do
+    expect(subject.price).to be_an_instance_of Float
   end
 end
