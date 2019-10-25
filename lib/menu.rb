188c0c02c06f94a3cda7de9c37ee57a8a9a6d580
elfi yang <elfiyang16@gmail.com>
2019-10-25 12:44:49 +0100

    start oder function completed with no test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,15 +1,16 @@
 class Menu
+  attr_reader :order_list
   def initialize
     @menu = {
-      burger:8,
-      fries:4,
-      milkshake:4,
-      doughnut:2,
-      cake:4,
-      chickenbreast:9,
-      olive:2,
-      cheese:1,
-      salad:6
+      Burger:8,
+      Fries:4,
+      Milkshake:4,
+      Doughnut:2,
+      Cake:4,
+      Chickenbreast:9,
+      Olive:2,
+      Cheese:1,
+      Salad:6
     }
     @order_list = {}
     @order_total = 0
@@ -21,8 +22,10 @@ class Menu
     }.join(" ")
   end
 
-
-end
-
+  def add_dish(name,quantity)
+    #can make a new array to hold hashes of individual dish order
+    #or simply adding the key-value pair to exisiting hash
+    @order_list[name]= quantity
+  end
 
 end
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,14 +5,24 @@ attr_reader :new_menu
 
   def initialize
     @new_menu = Menu.new
-
   end
 
-  def check
+  def view
    puts @new_menu.show
   end
 
-
+  def start_order
+    puts "Begin order, type 'Done' when you finish."
+    puts "What do you want?"
+    @dish_name = gets.chomp.to_s
+    until @dish_name == "Done"
+      puts "How many do you want?"
+      @dish_quantity = gets.chomp.to_i
+      @new_menu.add_dish(@dish_name.to_sym,@dish_quantity)
+      puts "What do you want?"
+      @dish_name = gets.chomp.to_s
+    end
+  end
 
 
 end
