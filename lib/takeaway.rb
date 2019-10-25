7045b929d10ce29567b1938d09e2ef379a84ac06
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-25 11:10:29 +0100

    wrote code to pass first test, refactored test, began writing next test

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,1 +1,10 @@
-
+class Takeaway
+  def menu
+    menu = [
+      { item: "Pizza", price: 5.20 },
+      { item: "Kebab", price: 3.00 },
+      { item: "Chips", price: 1.20 },
+      { item: "Drink", price: 0.80 },
+    ]
+  end
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,13 +1,12 @@
-require "./lib/takeaway."
+require "takeaway"
 
 describe Takeaway do
   it "allows the user to see a menu including items and prices" do
     takeaway = Takeaway.new
-    expect(takeaway.menu).to include({
-      :pizza => 5.20,
-      :kebab => 3.00,
-      :chips => 1.20,
-      :drink => 0.80,
-    })
+    takeaway.menu {is_expected_to include(:item, :price)}
+  end
+
+  it "allows a user to select dishes to order from the menu" do
+  
   end
 end
