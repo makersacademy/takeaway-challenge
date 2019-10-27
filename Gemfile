c2d664bb1e90213df287e48531ef0053af20d2df
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-10-27 12:28:14 +0000

    added dotenv gem and env variables TextSender

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem 'rubocop', '0.56.0'
 gem 'simplecov', require: false, group: :test
 gem 'simplecov-console', require: false, group: :test
 gem 'twilio-ruby', '~> 5.28.0'
+gem 'dotenv', groups: [:development, :test]
--- lib/text_sender.rb
+++ lib/text_sender.rb
@@ -1,18 +1,19 @@
 
 require 'twilio-ruby'
+require 'dotenv/load'
 
 class TextSender
 
   def initialize
-    @account_sid = 'ACb49f9d0bddd850c0bf2b56982167b15c'
-    @auth_token = '33d1c3f3ecf051d17d371a49425961fc'
+    @account_sid = ENV['TWILIO_ACC_SID']
+    @auth_token = ENV['TWILIO_AUTH_TOKEN']
   end
 
   def send(message = "a text")
     @client = Twilio::REST::Client.new @account_sid, @auth_token
     @client.api.account.messages.create(
-      from: '++441665252041',
-      to: '+447738053947',
+      from: ENV['TWILIO_FROM_NUMBER'],
+      to: ENV['TWILIO_TO_NUMBER'],
       body: message
     )
     true
