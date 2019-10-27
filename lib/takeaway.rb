97dc5a1f357dcadd4273e35fb3baf9de2e4f77a7
Andy Jones <andytjones@icloud.com>
2019-10-27 16:23:24 +0000

    User story 2. Inital check item on menu complete. Test complete.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,5 @@
 require_relative 'menu'
+require_relative 'basket'
 
 class Takeaway
 
@@ -8,5 +9,16 @@ class Takeaway
 
   def show_menu
     @menu.display_menu
+  end
+
+  def add_to_order(item, quantity)
+    raise "Item not on menu!" if on_menu?(item) == false
+    
+  end
+
+  private 
+
+  def on_menu?(item)
+    @menu.menu.has_key?(item)
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,15 +1,26 @@
 require 'takeaway'
+require 'basket'
 
 describe Takeaway do
 
-  context 'getting a menu' do
+  context 'with menu double' do
     let(:menu_double) { double :menu }
     let(:subject) { Takeaway.new(menu_double) }
 
     it 'gets a menu' do 
 
-      allow(menu_double).to receive(:display_menu) { 'string' }
-      expect(subject.show_menu).to eq 'string'
+      allow(menu_double).to receive(:display_menu) { { "curry" => 1.99 } }
+      expect(subject.show_menu).to eq({ "curry" => 1.99 })
+    end
+
+  end
+
+  context 'ordering' do
+    let(:menu_double) { double(:menu, menu: { "curry" => 9.99 }) }
+    let(:subjet) { Takeaway.new(menu_double) }
+
+    it 'returns error if given item not on menu' do
+      expect{ subject.add_to_order('fish', 1) }.to raise_error("Item not on menu!")
     end
 
   end
