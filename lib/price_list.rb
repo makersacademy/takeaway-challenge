3996b1de9cdb4621027348405c6f1d336b5d8bca
Emma Van Oss <e.vanoss@gmail.com>
2019-10-27 13:35:05 +0000

    change menu to hash & puts out items & prices when "print" is called

--- lib/price_list.rb
+++ lib/price_list.rb
@@ -1,7 +1,13 @@
 class PriceList
   attr_reader :list
 
-  def initialize
-    @list = []
+  def initialize(menu = {})
+    @list = menu
+  end
+
+  def print
+    @list.each do |k, v|
+      puts "#{k.capitalize}: #{v}"
+    end
   end
 end
--- spec/price_list_spec.rb
+++ spec/price_list_spec.rb
@@ -1,9 +1,14 @@
 require 'price_list'
 
 describe PriceList do
-  let(:subject) { PriceList.new }
+  menu = { pizza: '£6', fries: '£1' }
+  let(:subject) { PriceList.new(menu) }
 
-  it 'returns an array when "list" is called' do
-    expect(subject.list).to be_an Array
+  it 'returns a hash when "list" is called' do
+    expect(subject.list).to be_a Hash
+  end
+
+  it 'prints each item and price when "print" is called' do
+    expect { subject.print }.to output("Pizza: £6\nFries: £1\n").to_stdout
   end
 end
