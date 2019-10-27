5e975dc849e6d77e5fe8ec6c29b426999d3a4e7a
Donald Newholm <newholm1@hotmail.com>
2019-10-27 17:12:44 +0000

    First test passing nicely

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,8 +4,8 @@ describe Menu do
   describe '#display_menu' do
 
     it 'returns a list of items with prices when requested' do
-      expect(subject.display_menu).to eq Menu::DEFAULT_MENU
+      expect(subject.display_menu).to eq "Fish - £1.49\nJelly - £0.49\nLiver - £1.99\nEgg - £0.49"
     end
-    
+
   end
 end
---
+++ spec/order_spc.rb
@@ -0,0 +1,7 @@
+=beginrequire 'order'
+
+describe Order do
+  describe '#select' do
+  it 'stores selected dish' do
+    expect()
+end
