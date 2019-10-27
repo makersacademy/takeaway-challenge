addd9970ac35f0097a96a02fbaaa0fe12dc55089
liatmoss <liatmoss@hotmail.com>
2019-10-27 12:48:50 +0000

    connected twillio information

---
+++ lib/twilio.rb
@@ -0,0 +1,30 @@
+class Twilio
+
+  TIME_FORMAT = "%H:%M"
+
+  def initialize(config, client: nil)
+    @client = client || Twilio::REST::Client.new(config[:account_sid], config[:auth_token])
+    @config = config
+  end
+
+  def deliver
+    client.messages.create(message_args)
+  end
+
+  private
+
+  attr_reader :client, :config
+
+  def message_args
+    {
+    from: config[:from],
+    to: config[:to],
+    body: config[:body] % delivery_time
+    }
+  end
+
+  def delivery_time
+    (Time.now + 60 * 60).strftime(TIME_FORMAT)
+  end
+
+end
