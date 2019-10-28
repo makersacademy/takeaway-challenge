d0a71a6cca22f00fbd69121508d29455df528072
Student <student@Makerss-MBP.connect>
2019-10-28 16:04:04 +0000

    added comments to explain and facilitate text test

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,5 @@
+# Twilio functionality successfully integrated but causes all tests to fail as rspec cannot find 'twilio-ruby'. To test functionality, please insert your phone number at line 46 and unhash all commented lines.
+
 # require 'twilio-ruby'
 
 class Takeaway
@@ -31,19 +33,22 @@ class Takeaway
   def checkout
     order_placed = Time.now
     delivery_due = Time.now+3600
-    # send_text
-    "Your order was placed at #{order_placed}. Your food should be with you by #{delivery_due}."
+    # send_text(order_placed, delivery_due)
+    "Your order was placed at #{order_placed}. Your food should be with you by #{delivery_due}." # You may need to comment out this line to test text functionality.
   end
 
-  # def send_text
+  # def send_text(order_placed, delivery_due)
   #   account_sid = ENV['TWILIO_ACCOUNT_SID']
   #   auth_token = ENV['TWILIO_AUTH_TOKEN']
   #   client = Twilio::REST::Client.new(account_sid, auth_token)
   #   client.messages.create(
   #   from: '+12132386475',
-  #   to: '+447391405893',
+  #   to: '+44XXXXXXXXXX',
   #   body: "Your order was placed at #{order_placed}. Your food should be with you by #{delivery_due}."
   #   )
   # end    
 
 end
+
+# takeaway = Takeaway.new
+# takeaway.checkout
