709a2fd516e76fd90f0fdb7adcfec7641180fc63
Marianne OConnor <marianneoco@gmail.com>
2019-11-12 10:36:11 +0000

    Failing test for responding to .order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,7 +3,10 @@ class Takeaway
   attr_reader :menu
 
   def initialize
-    @menu = {}
+    @menu = {:pizza => {"Margherita" => 8.95, "Hawaiian" => 9.95, "Vegetarian" => 9.95, "Pepperoni" => 10.95, "BBQ Chicken" => 10.95},
+    :drinks => {"Coke" => 1.95, "Diet Coke" => 1.85, "Water" => 1.00}}
+    puts "Welcome! Here is the menu. To order, type takeaway.order."
+    puts @menu
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,10 +4,14 @@ describe Takeaway do
   it { is_expected.to be_instance_of Takeaway }
 
   it "should contain a list of dishes with prices" do
-    expect(subject.menu). to be_instance_of Hash
+    expect(subject.menu).to be_instance_of Hash
   end
 
   it "should have a menu which is subdivided" do
     expect(subject.menu[:pizza]).to eq ({"Margherita" => 8.95, "Hawaiian" => 9.95, "Vegetarian" => 9.95, "Pepperoni" => 10.95, "BBQ Chicken" => 10.95})
+  end
+
+  it "should allow the user to place an order" do
+    expect(subject).to respond_to(:order)
   end
 end
