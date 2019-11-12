ccbfbc469a2d4bd5203144c540a613862a7e0a6a
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-12 14:04:37 +0000

    add method for menu.list, and initialise @menu attribute

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,17 @@
 class Menu
+  attr_accessor :menu
 
+  def initialize
+    @menu = [
+      { "meal" => "Fish", "price" => 2.00 },
+      { "meal" => "Chips", "price" => 1.00 },
+      { "meal" => "Hamburger", "price" => 2.00 },
+      { "meal" => "Spaghetti", "price" => 2.50 },
+      { "meal" => "Salad", "price" => 1.00 },
+    ]
+  end
+
+  def list
+
+  end
 end
