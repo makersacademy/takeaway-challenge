7a07ac1a4c7c6496dccebeec4944c8838a31f492
djlonboy <djlonboy@gmail.com>
2019-11-12 10:39:57 +0000

    passed test for new instance of Item class with correct arguments

--- lib/item.rb
+++ lib/item.rb
@@ -1,5 +1,11 @@
 # initialize item takes a name and a price and stores
+class Item
+ def initialize(name, price)
+   @item_name = name
+   @item_price = price
+ end
 
 # return name
 
 # return price
+end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,9 +1,9 @@
 require 'item'
 
 describe Item do
+  subject { Item.new("test", 5) }
 
-  it 'can create a new instance of an item' do
-  subject.new("test item", 5)
+  it 'can create a new instance of an item with correct arguments' do
   expect(subject).to be_instance_of Item
   end
 
