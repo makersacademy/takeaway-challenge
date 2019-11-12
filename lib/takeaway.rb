47aff9d736f63c2a36199aa1d7778529de1f0afd
Sam King <samuelking94@gmail.com>
2019-11-12 11:21:41 +0000

    #add method updated to store order items in hash

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,16 @@
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :order
+
   def initialize
     @menu = {"Pizza" => 5, "Chips" => 2, "Curry" => 6.5}
+    @order = {}
   end
 
   def add(item)
+    if @order.key?(item)
+      @order[item] += 1
+    else
+      @order[item] = 1
+    end
   end
 end
