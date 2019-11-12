39d41c746a78d18a8089ba799a70a712c9916b3b
Sam King <samuelking94@gmail.com>
2019-11-12 11:16:31 +0000

    Failing test written to assert that order attribute is updated to include item added

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,6 @@
 require './lib/takeaway.rb'
 
 describe Takeaway do
-
   it { is_expected.to be_instance_of(described_class) }
 
   it { is_expected.to respond_to(:menu) }
@@ -9,9 +8,22 @@ describe Takeaway do
   it { is_expected.to respond_to(:add).with(1).arguments }
 
   describe '#menu' do
-
     it 'should be hash type' do
       expect(subject.menu).to be_instance_of(Hash)
+    end
+
+  end
+
+  describe '#add' do
+
+    context 'item is added to order' do
+      let(:food_item) {double :food_item}
+
+      it 'should update the order attribute' do
+        subject.add(food_item)
+        expect(subject.order).to have_key(food_item)
+      end
+
     end
 
   end
