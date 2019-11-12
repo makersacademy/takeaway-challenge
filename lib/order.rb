ef4ca12fabd073adb33fa551c665b62d2f8092fc
Sam King <samuelking94@gmail.com>
2019-11-12 15:09:39 +0000

    #add method defined

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,14 @@
-class Order
-  attr_reader :items
+require './lib/menu.rb'
 
-  def initialize
+class Order
+  attr_reader :items, :menu
+
+  def initialize(menu_class = Menu)
+    @menu = menu_class.new
     @items = {}
+  end
+
+  def add(item, quantity)
+    @items[item] = {price: menu.dishes[item], quantity: quantity}
   end
 end
