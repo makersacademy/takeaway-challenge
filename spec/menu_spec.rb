867c7029dab6f63d5ad1edc68cf4e7460e2a7698
Sam King <samuelking94@gmail.com>
2019-11-12 13:58:12 +0000

    Failing test written for #add method

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,6 +5,15 @@ describe Menu do
 
   it { is_expected.to respond_to(:dishes) }
 
+  describe '#add' do
+    let(:dish) {double :dish}
+    let(:price) {double :price}
+    it 'should add item to the menu attribute dishes' do
+      subject.add(dish, price)
+      expect(subject.dishes.key?(dish)).to eq(true)
+    end
+  end
+
   describe '#on_menu?' do
     it 'should return true when item is present' do
       expect(subject.on_menu?("Pizza")).to eq(true)
