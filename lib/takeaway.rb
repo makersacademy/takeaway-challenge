ac0a02796801b2b2a276d3882443ae493efda36d
Richard <aagbajdos@gmail.com>
2019-11-12 14:22:08 +0000

    third test

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,26 +1,36 @@
 class Takeaway
 
-attr_reader :dish, :order, :list_of_dishes
+  attr_reader :dish, :order, :list_of_dishes
 
-  def initialize()
+  def initialize
     @dish1 = 7
     @dish2 = 14
-    @dish2 = 15
-    @list_of_dishes = [{:dish1 => "£7", :dish2 => "£14", :dish3 => "£15" }]
+    @dish3 = 15
+    @list_of_dishes = [{ :dish1 => "£7", :dish2 => "£14", :dish3 => "£15" }]
     @dish_selection = []
     @order = [@dish_selection]
+    @quantity = nil
   end
   def view_menu
+
     puts @list_of_dishes
-    end
-  def select_item(dish)
-    puts "what would you like to order?"
+  end
+  def select_item(*)
+
     @dish_selection[:dish1] = @dish1
     @dish_selection[:dish3] = @dish3
   end
-  def view_order()
+
+  def view_total
+
+    @dish1 = 7
+    @dish2 = 15
+    puts 7 + 15
+  end
+
+  def view_order
     puts "please see your order below"
-    @order << @dish_selection.each
+    @order << @dish_selection
     puts @order
   end
-  end
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,30 +1,37 @@
 require 'takeaway'
 
 describe Takeaway do
-  subject(:takeaway) {Takeaway.new}
-# As a customer
-# So that I can check if I want to order something
-# I would like to see a list of dishes with prices
+  subject(:takeaway) { Takeaway.new }
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
   it "lets you check the list of dishes" do
     takeaway = Takeaway.new
     takeaway.view_menu
     expect(subject.list_of_dishes).to eq [:dish1 => "£7", :dish2 => "£14", :dish3 => "£15"]
-end
-# As a customer
-# So that I can order the meal I want
-# I would like to be able to select some number of several available dishes
+  end
+  # As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
   it "lets you select some number of several available dishes" do
     subject.select_item("dish1")
-    subject.select_item("dish2")
-    expect(subject.dish_selection).to eq [{:dish1 => "£7", :dish3 => "£15"}]
-end
-# As a customer
-# So that I can verify that my order is correct
-# I would like to check that the total I have been given matches the sum of the various dishes in my order
+    subject.select_item("dish3")
+    expect(subject.dish_selection).to eq [{ :dish1 => "£7", :dish3 => "£15" }]
+  end
+  # As a customer
+  # So that I can verify that my order is correct
+  # I would like to check that the total I have been given matches the sum of the various dishes in my order
+  it "lets you get a total from several dishes" do
+    subject.select_item("dish1")
+    subject.select_item("dish3")
+    @dish1 = 7
+    @dish3 = 15
+    expect(subject.view_total).to eq 22
+  end
   it "lets you view your order" do
-  subject.select_item("dish1")
-  subject.select_item("dish2")
-  subject.view_order
-  expect(subject.order).to eq [{:dish1 => "£7", :dish3 => "£15"}]
-end
+    subject.select_item("dish1")
+    subject.select_item("dish3")
+    subject.view_order
+    expect(subject.order).to eq [{ :dish1 => "£7", :dish3 => "£15" }]
+  end
 end
