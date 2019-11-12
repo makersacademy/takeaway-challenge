5db4681158dbbb6bd3a419d29f62fd37400d8d47
Marianne OConnor <marianneoco@gmail.com>
2019-11-12 10:13:16 +0000

    Failing test for existence of menu hash

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,4 +2,8 @@ require 'takeaway'
 
 describe Takeaway do
   it { is_expected.to be_instance_of Takeaway }
+
+  it "should contain a list of dishes with prices" do
+    expect(subject.menu). to be_instance_of Hash
+  end
 end
