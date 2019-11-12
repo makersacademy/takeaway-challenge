8aad99e8b6639700e415b68729fe27b950d68249
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-12 14:29:12 +0000

    add test to show menu list of dishes with prices

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,8 +6,14 @@ describe Menu do
     expect(menu).to be_instance_of(Menu)
   end
 
-  it 'shows a list of all dishes with prices' do
+  it 'responds to list' do
     menu = Menu.new
     expect(menu).to respond_to(:list)
+  end
+
+  it 'shows menu list of dishes with prices' do
+    menu = Menu.new
+    menu.list
+    expect(:meal => "Fish", :price => 2.00).to include(:meal => "Fish", :price => 2.00)
   end
 end
