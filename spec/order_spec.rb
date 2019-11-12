6a44cfe8f3e325353a9f425652ef9ed260492269
Sam King <samuelking94@gmail.com>
2019-11-12 14:53:30 +0000

    Failing test written to assert that #items is an instance of Hash class

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,10 @@ require './lib/order.rb'
 
 describe Order do
   it { is_expected.to be_instance_of(described_class) }
+
+  describe '#items' do
+    it 'should respond be of type hash' do
+      expect(subject.items).to be_instance_of(Hash)
+    end
+  end
 end
