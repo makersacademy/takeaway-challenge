47494c71160911383d82a6441bd7d09fa1b33cb7
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-12 11:57:27 +0000

    hash list of dishes, 92.1%

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,16 @@
 class Takeaway
-end 
+  attr_reader :dishes
+
+  def initialize
+
+@dishes =
+[
+  { name: "Pizza" , price: "£15.50" },
+  { name: "Burger" , price: "£16.30" },
+  { name: "Pasta" , price: "14.20" },
+  { name: "Sushi" , price: "£14.60" },
+  { name: "Noodles" , price: "£15.20" }
+] 
+
+end
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,29 @@
 require './lib/takeaway'
 
 describe Takeaway do
-subject {Takeaway.new}
+  subject {Takeaway.new}
 
-it { is_expected.to be_an(Takeaway) }
+  it { is_expected.to be_instance_of(Takeaway) }
+
+  it { should respond_to :dishes }
+
+  describe "initialize" do
+    subject {:dishes}
+    let :dishes do
+      [
+        { name: "Pizza" , price: "£15.50" },
+        { name: "Burger" , price: "£16.30" },
+        { name: "Pasta" , price: "14.20" },
+        { name: "Sushi" , price: "£14.60" },
+        { name: "Noodles" , price: "£15.20" }
+      ]
+      # expect(@dishes).to be_an_instance_of(Array)
+
+    end
+
+
+  end
+
 
 
 end
