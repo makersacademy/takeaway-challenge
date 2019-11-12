c74b72b2f36fc3f82421b3ed1f3c6798357b7909
Sam King <samuelking94@gmail.com>
2019-11-12 10:10:23 +0000

    Failing test written to assert that #menu should not return nil

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,17 @@
 require './lib/takeaway.rb'
 
 describe Takeaway do
+
   it { is_expected.to be_instance_of(described_class) }
 
   it { is_expected.to respond_to(:menu) }
+
+  describe '#menu' do
+
+    it 'should not equal nil' do
+      expect(subject.menu).not_to be(nil)
+    end
+
+  end
+
 end
