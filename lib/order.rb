1489090747cbaad9a16d31e1224ddce343a98eca
Rebecca Varley <rebecca.varley@gmail.com>
2019-11-12 11:52:01 +0000

    End of third user story

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,28 @@
-require './lib/menu.rb'
+
 
 class Order
+  attr_reader :ordered, :menus, :order_total
 
-  def select_item
+  def initialize
+    @menus = {"Curry" => 12, "Sushi" => 4, "Edemame" => 2, "Burrata" => 6, "Salad" => 10}
+    @ordered = []
+    @order_total = 0
+end
+
+def display_menu
+  @menus.each {|key, value|
+  puts "#{key} : £#{value}"}
+end
+
+def select_item(item)
+  if @menus.key?(item)
+    @ordered << item
+end
+end
+
+def total
+  @ordered.each { |menu_item|
+  @order_total += @menus[menu_item]}
+  @order_total
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,6 +1,18 @@
 require './lib/order.rb'
 
 describe Order do
+
+  it 'should create the menu when initialized' do
+  order = Order.new
+    expect(order.menus).to include("Edemame" => 2)
+  end
+
+  it 'should display the menu correctly when asked to do so' do
+  order = Order.new
+    expect { order.display_menu }.to output{"vegetable curry : £12\nCalifornia Roll : £4\nEdemame : £2
+Burrata : £6\nTricolore Salad : £10"}.to_stdout
+  end
+
   it 'should create an instance of the order class' do
     order = Order.new
     expect(order).to be_an_instance_of(Order)
@@ -9,5 +21,17 @@ describe Order do
   it 'should allow me to select certain items from the menu' do
     order = Order.new
     expect(order).to respond_to(:select_item)
+  end
+
+  it 'should add items to the order' do
+    order = Order.new
+    order.select_item("Burrata")
+    expect(order.ordered).to include("Burrata")
+  end
+
+  it 'should return the correct total price' do
+    order = Order.new
+    order.select_item("Burrata")
+    expect(order.total).to eq(6)
   end
 end
