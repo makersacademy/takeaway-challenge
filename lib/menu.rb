2eff1a4167b218371358a42a339c635424c29beb
Anna Cotugno <annaeuridice@gmail.com>
2019-11-12 11:24:49 +0000

    creates an instance of a small menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,17 @@
 class Menu
 
+  def initialize
+    @dishes = [
+    {name: "Hummus", price: "3.70"},
+    {name: "Olives", price: "2.10" },
+    {name: "Taramasalata",  price: "3.70"},
+    {name: "Aubergine", price: "3.80"},
+    {name: "Kleftico"  , price: "13.50"},
+    {name: "Stifado"  , price: "12.50"},
+    {name: "Moussaka"  , price: "11.25"}
+    ]
+  end
+
 
 
 end
