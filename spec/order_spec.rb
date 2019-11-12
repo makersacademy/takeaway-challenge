9e7e8de18b1762dd91e95199a7d8a2e4d15fb1a0
Sam King <samuelking94@gmail.com>
2019-11-12 15:07:45 +0000

    Failing test written for #add method

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -8,4 +8,16 @@ describe Order do
       expect(subject.items).to be_instance_of(Hash)
     end
   end
+
+  describe '#add' do
+    let(:food_item) {double :food_item}
+    let(:quantity) {double :quantity}
+
+    context 'item is added to order' do
+      it 'should update the order attribute items' do
+        subject.add(food_item, quantity)
+        expect(subject.items).to have_key(food_item)
+      end
+    end
+  end
 end
