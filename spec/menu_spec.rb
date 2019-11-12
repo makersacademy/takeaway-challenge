a9b0597521bc4c80325e70922770abaf40c5fa86
Sam King <samuelking94@gmail.com>
2019-11-12 13:30:05 +0000

    Failing tests to assert that #on_menu? method returns true or false

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,4 +4,14 @@ describe Menu do
   it { is_expected.to be_instance_of(described_class) }
 
   it { is_expected.to respond_to(:dishes) }
+
+  describe '#on_menu?' do
+    it 'should return true when item is present' do
+      expect(subject.on_menu?("Pizza")).to eq(true)
+    end
+
+    it 'should return false when item is not present' do
+      expect(subject.on_menu?("Beans")).to eq(false)
+    end
+  end
 end
