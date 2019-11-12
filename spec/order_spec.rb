0712cfcb1422b39a3dda5df0a639a6f754184eeb
djlonboy <djlonboy@gmail.com>
2019-11-12 12:06:48 +0000

    Added and passed test for adding an item to an order list

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,5 +6,9 @@ describe Order do
     expect(subject).to be_instance_of(Order)
   end
 
+  it 'can add an item to an order list' do
+    subject.add_item("test", 1)
+    expect(subject.order_list).to eq [["test", 1]]
+  end
 
 end
