c20f6c9fa90fb8955a06b1cf7290d1ecbc40a63e
Marianne OConnor <marianneoco@gmail.com>
2019-11-12 10:30:52 +0000

    Failing test for subdivided menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,9 @@
 class Takeaway
 
+  attr_reader :menu
+
+  def initialize
+    @menu = {}
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,4 +6,8 @@ describe Takeaway do
   it "should contain a list of dishes with prices" do
     expect(subject.menu). to be_instance_of Hash
   end
+
+  it "should have a menu which is subdivided" do
+    expect(subject.menu[:pizza]).to eq ({"Margherita" => 8.95, "Hawaiian" => 9.95, "Vegetarian" => 9.95, "Pepperoni" => 10.95, "BBQ Chicken" => 10.95})
+  end
 end
