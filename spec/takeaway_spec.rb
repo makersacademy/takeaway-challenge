94c499246ae16bbc285fc0215dd0da0ed2fec155
Sam King <samuelking94@gmail.com>
2019-11-12 11:24:06 +0000

    Refactor of test to remove redundant tests

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,10 +3,6 @@ require './lib/takeaway.rb'
 describe Takeaway do
   it { is_expected.to be_instance_of(described_class) }
 
-  it { is_expected.to respond_to(:menu) }
-
-  it { is_expected.to respond_to(:add).with(1).arguments }
-
   describe '#menu' do
     it 'should be hash type' do
       expect(subject.menu).to be_instance_of(Hash)
