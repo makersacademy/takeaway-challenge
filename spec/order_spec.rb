e749ca09658f6dc7a47d02d44eef0c3292a607dd
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-12 11:14:16 +0000

    add second test to show list of dishes with prices

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,4 +5,9 @@ describe Order do
     order = Order.new
     expect(order).to be_instance_of(Order)
   end
+
+  it 'shows list of dishes with prices' do
+    order = Order.new
+    expect(order.list).to eq("pizza, 1.00")
+  end
 end
