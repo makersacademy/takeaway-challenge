3b96089c790bdd89f0d5879cd362e5604c066394
Sam King <samuelking94@gmail.com>
2019-11-13 13:49:08 +0000

    Refactor and add #price test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,30 +1,33 @@
 require './lib/menu.rb'
 
 describe Menu do
+  before(:each) do
+    subject.add("Pizza", 5)
+  end
+
   it { is_expected.to be_instance_of(described_class) }
 
   it { is_expected.to respond_to(:dishes) }
 
   describe '#add' do
     it 'should add item to the menu attribute dishes' do
-      dish = double(:dish)
-      price = double(:price)
-      subject.add(dish, price)
-      expect(subject.dishes).to include(dish => price)
+      expect(subject.dishes).to include("Pizza" => 5)
     end
   end
 
   describe '#has?' do
-    before(:each) do
-      subject.add("Pizza", 5)
-    end
-
     it 'should return true when item is present' do
       expect(subject.has?("Pizza")).to eq(true)
     end
 
     it 'should return false when item is not present' do
       expect(subject.has?("Beans")).to eq(false)
+    end
+  end
+
+  describe '#price' do
+    it 'should return the price' do
+      expect(subject.price("Pizza")).to eq(5)
     end
   end
 end
