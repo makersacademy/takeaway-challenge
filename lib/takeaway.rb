b34c297078ef4e8438d6ee103313c9b11b3e6679
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-13 09:58:14 +0000

    volume correct, total not

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,17 +1,18 @@
 class Takeaway
   attr_reader :dishes
   attr_reader :order
+  attr_accessor :ordered_number
 
   def initialize
-@dishes = []
-@order = []
+@dishes = dishes
+@order = Array.new
 
 def dishes
 @dishes =
 [
   { name: "Pizza" , price: "£15.50" },
   { name: "Burger" , price: "£16.30" },
-  { name: "Pasta" , price: "14.20" },
+  { name: "Pasta" , price: "£14.20" },
   { name: "Sushi" , price: "£14.60" },
   { name: "Noodles" , price: "£15.20" }
 ]
@@ -25,18 +26,21 @@ def select(name)
 end
 
 def volume
-  ordered_number = @order.each_with_object(Hash.new(0)) { |name, hash| hash[name] += 1 }
+  @ordered_items = {}
+@ordered_items = @order.each_with_object(Hash.new(0)) { |v, h| h[v] += 1 }
 
-ordered_number.map { |item, quantity| quantity.to_int }
+#for some reason this is causing problems by generating a value of nil.
+#This is causing a problem for the next method.
 
   end
 
-# def total 
-  #values into integers from orginial dishes
-  #multiply value
+def total
+
+puts @dishes.each do |x, y| puts x, y end
 
 
+@dishes.each do |hash| hash.each {|k, v| v * @ordered_items[v] if @ordered_items.key? k }
+# end}}
 end
-
-
-  # @order.each{|item| @order.counts(item)}
+end
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -15,7 +15,7 @@ describe Takeaway do
     expect(subject.dishes). to eq [
         { name: "Pizza" , price: "£15.50" },
         { name: "Burger" , price: "£16.30" },
-        { name: "Pasta" , price: "14.20" },
+        { name: "Pasta" , price: "£14.20" },
         { name: "Sushi" , price: "£14.60" },
         { name: "Noodles" , price: "£15.20" }
       ]
@@ -36,22 +36,19 @@ it "counts the quantity of each item in the order" do
 subject.select("Pizza")
 subject.select("Pizza")
   order = []
+  ordered_number = {}
   expect(subject.order).to be_an_instance_of(Array)
-
-#Need to insertt another method in here (also in lib) that will extract the hash and add it up
-
-  expect(subject.volume).to eq [2]
-# ordered_number = order.each_with_object(Hash.new(0)) { |name, hash| hash[name] += 1 }
-# expect(ordered_number[:Pizza]).to eq 2
-
-
-end
-  # expect(order.count("Pizza")).to eq 2
+  # expect(ordered_number).to have_key(:Pizza)
+  expect(subject.volume).to include("Pizza" => 2)
 
 end
 
-# it "adds up how many of each item are ordered" do
-#   item_frequency_hash = {}
+it "multiplies the volume of an item by its price" do
+subject.select("Pizza")
+subject.select("Pizza")
+  expect(subject.total ).to eq [31]
 
-#   expect(order.total).to eq 2
-# end
+#The last method is bringing uip the following error,  NoMethodError:
+# undefined method `key?' for nil:NilClass
+end
+end
