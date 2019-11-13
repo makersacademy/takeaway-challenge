527fbb222d7877ba39bfeec9d850207b58deaf12
Rebecca Varley <rebecca.varley@gmail.com>
2019-11-13 09:42:13 +0000

    Testing gitignore

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,7 +1,19 @@
+require 'twilio.env'
 # Download the helper library from https://www.twilio.com/docs/ruby/install
 require 'rubygems'
 require 'twilio-ruby'
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-Removed for security
+account_sid = TWILIO_ACCOUNT_SID
+auth_token = TWILIO_AUTH_TOKEN
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+message = @client.messages
+  .create(
+     body: 'Thank you! Your order was placed and will be delivered before 18:52',
+     from: TWILIO_PHONE_NUMBER,
+     to: TWILIO_PHONE_TO
+   )
+
+puts message.sid
