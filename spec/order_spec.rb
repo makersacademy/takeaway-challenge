b797b16c437eb698072e5e923854931b8abfe2c3
Sam King <samuelking94@gmail.com>
2019-11-15 19:50:12 +0000

    Menu double and menu class double created in order_spec.rb

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,6 +3,11 @@ require './lib/order.rb'
 describe Order do
   it { is_expected.to be_instance_of(described_class) }
 
+  let(:menu) { double("menu") }
+  let(:menu_class) { double("menu_class", :new => menu) }
+
+  subject { Order.new(menu_class) }
+
   describe '#items' do
     it 'should respond be of type hash' do
       expect(subject.items).to be_instance_of(Hash)
