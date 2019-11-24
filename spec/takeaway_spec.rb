5eb42331650a5e272867bd1f0b5b8996e8ce7b2c
kealanheena <kealanheena@gmail.com>
2019-11-24 13:52:50 +0000

    fix test for show menu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -9,9 +9,13 @@ describe Takeaway do
   end
 
   describe "#show_menu" do
-    it "return a list of food items and their price" do
-      expect(subject.show_menu).to be_a(Hash) # It's returning a hash I dont know why??
+    it "return a hash of food items and their price" do
+      expect(subject.show_menu).to eq({"Chicken Chow Mein"=>6.5, "Egg Fried Rice"=>4.8}) # It's returning a hash I dont know why??
     end
   end
+
+  # describe "#order" do
+  #   it ""
+  # end
 
 end
