f20138010e17714c0bab3ebeac57bca0542b2073
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-11-24 12:51:06 +0000

    Add TextProvider class and tests

--- spec/text_provider_spec.rb
+++ spec/text_provider_spec.rb
@@ -1,12 +1,13 @@
-# require "text_provider"
+require "text_provider"
 
-# describe TextProvider do
-#   let(:time_formatter) { double :time_formatter, delivery_time: "18:55" }
-#   let(:text_provider) { TextProvider.new }
+describe TextProvider do
+  let(:time_formatter) { double :time_formatter, delivery_time: "18:55" }
+  let(:text_provider) { TextProvider.new(time_formatter) }
 
-#   describe "#send_message" do
-#     it "should return a text saying that the order was placed successfully and it will be delivered 1 hour from now" do
-#       expect(text_provider.send_message).to eq "Thank you! Your order was placed and will be delivered before 18:55"
-#     end
-#   end
-# end
+  describe "#send_message" do
+    it "should return a text saying that the order was placed successfully and it will be delivered 1 hour from now" do
+      message = "Thank you! Your order was placed and will be delivered before 18:55"
+      expect(text_provider.send_message).to eq message
+    end
+  end
+end
