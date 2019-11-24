a3c36c21593fe18d88a95843dffd549c11e5c413
Jonathan Palma <jonathanpalma89@gmail.com>
2019-11-24 18:23:36 +0000

    add to order several amount of dishes from the menu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,14 +2,36 @@ require 'takeaway'
 
 describe Menu do
   subject(:menu) { described_class.new }
-
   let(:dishes) { Menu::DEFAULT_MENU }
 
   it 'has a list of dishes and prices' do
-    expect(menu.dishes_list).to eq(dishes)
+    expect(menu.dishes_list).to eq dishes
   end
   it 'prints a menu' do
-    menu_list = "Llomo_saltado £14.99, Ratatouille £9.00, Chicken_and_chips £5.50"
-    expect(menu.print_menu).to eq(menu_list)
+    menu_list = "Llomo_saltado £14.99, Ratatouille £9.00, Chicken_and_chips £5.50, Ceviche £10.00"
+    expect(menu.print_menu).to eq menu_list
+  end
+end
+
+describe Order do
+  subject(:order) { described_class.new(menu) }
+  let(:menu) { double :menu }
+  let(:dish) { double :dish }
+  let(:quantity) { double :quantity }
+
+  let(:dishes) do {
+    llomo_saltado: 2,
+    ceviche: 1
+   } 
+  end
+  it 'orders a several dishes from the menu' do
+    order.place_order(:llomo_saltado, 2)
+    order.place_order(:ceviche, 1)
+    expect(order.dishes_ordered).to eq dishes
+  end
+
+  it 'raises an error if ordering something not from the menu' do
+    error = "#{dish} doesn't exist on the menu"
+    expect { order.place_order(dish, quantity) }.to raise_error OrderError, error
   end
 end
