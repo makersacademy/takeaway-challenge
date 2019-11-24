20a1073e596dbd919beaf38414d6744d3fb60168
Jonathan Palma <jonathanpalma89@gmail.com>
2019-11-24 18:23:36 +0000

    add to order several amount of dishes from the menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,7 +3,8 @@ class Menu
   DEFAULT_MENU = {
     llomo_saltado: 14.99,
     ratatouille: 9,
-    chicken_and_chips: 5.50
+    chicken_and_chips: 5.50,
+    ceviche: 10
   }
 
   attr_reader :dishes_list
@@ -17,4 +18,34 @@ class Menu
       '%s £%.2f' % [dish.to_s.capitalize, price]
     end.join(", ")
   end
+
+  def includes?(dish)
+    dishes_list.include?(dish)
+  end
+end
+
+class Order
+
+  attr_reader :menu, :dishes_ordered
+
+  def initialize(menu)
+    @dishes_ordered = {}
+    @menu = menu
+  end
+
+  def place_order(dish, quantity)
+    @dish = dish
+    raise order_error unless Menu::DEFAULT_MENU.include?(dish)
+    dishes_ordered[dish] = quantity
+  end
+
+private
+
+  def order_error
+    error = "#{@dish} doesn't exist on the menu"
+    OrderError.new(error)
+  end
+end
+
+class OrderError < StandardError
 end
