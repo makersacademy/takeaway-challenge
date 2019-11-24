c24acd6b3b73eff0ef05e6c87176d3036275af52
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-24 22:29:11 +0000

    more refactoring

--- spec/message_client_spec.rb
+++ spec/message_client_spec.rb
@@ -10,14 +10,15 @@ RSpec.describe MessageClient do
     allow(twilio.messages).to receive(:create)
   end
 
-  context 'when sending messages'
-  it 'confirms a message has been sent' do
-    expect(test_message_client.confirm_order(sam)).to eq 'message sent'
-  end
+  context 'when sending messages' do
+    it 'confirms a message has been sent' do
+      expect(test_message_client.confirm_order(sam)).to eq 'message sent'
+    end
 
-  it 'reports when a message fails to send' do
-    allow(twilio.messages).to receive(:create) { raise 'error' }
+    it 'reports when a message fails to send' do
+      allow(twilio.messages).to receive(:create) { raise 'error' }
 
-    expect(test_message_client.confirm_order(sam)).to eq 'message failed to send'
+      expect(test_message_client.confirm_order(sam)).to eq 'message failed to send'
+    end
   end
 end
