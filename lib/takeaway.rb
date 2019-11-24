350397c7288a94c294d716c3ab586415f552d01b
kealanheena <kealanheena@gmail.com>
2019-11-24 13:48:21 +0000

    add show menu method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -5,4 +5,12 @@ class Takeaway
   def initialize
     @menu = { "Chicken Chow Mein" => 6.50, "Egg Fried Rice" => 4.80 }
   end
+
+  def show_menu
+    @menu.each_with_index {|(k,v), index|
+      p "#{index + 1}. #{k} = £#{v}\n" 
+    }
+  end
+
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,17 @@
 require "takeaway"
 
 describe Takeaway do
+
   describe "#initialze" do
     it "creates an array called menu on initalize" do
-      expect(Takeaway.new.menu).to be_a(Hash)
+      expect(subject.menu).to be_a(Hash)
     end
   end
+
+  describe "#show_menu" do
+    it "return a list of food items and their price" do
+      expect(subject.show_menu).to be_a(Hash) # It's returning a hash I dont know why??
+    end
+  end
+
 end
