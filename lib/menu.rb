eb5ee26676822c75d6a6836c6b8bd59ca5a0bd9c
root <root@LAPTOP-3KNUSSU3.localdomain>
2019-11-24 19:53:01 +0000

    take_away challenge

---
+++ lib/menu.rb
@@ -0,0 +1,17 @@
+
+class Menu
+
+attr_reader :dishes
+
+
+  def initialize(dishes)
+    @dishes = dishes
+  end
+
+  def print
+    dishes.map do |title, price| 
+      "%s $%.2f" % [title.to_s.capitalize, price]
+    end.join(', ')
+  end
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,23 @@
+require 'menu'
+
+describe Menu do
+  
+
+    subject(:menu) { described_class.new(dishes) }
+
+    let(:dishes) do
+      {
+        chicken: 3.99,
+        falafel: 4.50
+      }
+    end
+    it 'has a list of dishes with prices' do
+    expect(menu.dishes).to eq(dishes)
+
+  end
+
+    it 'prints a list of dishes with prices' do
+      printed_menu = 'Chicken $3.99, Falafel $4.50'
+      expect(menu.print).to eq(printed_menu)
+    end
+end
