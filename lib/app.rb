933c39f919d28aaf346155ff54cddd69e1b43673
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-24 21:28:05 +0000

    rubocop amend

--- lib/app.rb
+++ lib/app.rb
@@ -5,11 +5,9 @@ require 'sinatra'
 Bundler.require()
 
 post '/sms' do
-
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
     r.message body: 'Thank you for your order!'
   end
 
   twiml.to_s
-
 end
