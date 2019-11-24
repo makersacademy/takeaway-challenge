3f37d24f52601feacc16afbce8a7facd56172498
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-24 11:10:12 +0000

    write tests for OrderListing class, code to pass

---
+++ lib/order_listing.rb
@@ -0,0 +1,28 @@
+class OrderListing
+  attr_reader :dish, :quantity
+
+  SINGLE_SERVING = 1
+  
+  def initialize(dish, quantity = SINGLE_SERVING)
+    @dish = dish
+    @quantity = quantity
+  end
+
+  def add_serving
+    @quantity += 1
+  end
+
+  def remove_serving
+    @quantity -= 1
+  end
+
+  def view
+    "#{formatted(dish.name)} – Quantity: #{quantity}"
+  end
+
+  private
+
+  def formatted(symbol)
+    symbol.to_s.split('_').map { |word| word.capitalize }.join(' ')
+  end
+end
