2789136a1e5a9b88ea8bd442462bef4dbffdffbd
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-25 04:25:18 +0000

    total value method added

--- lib/place_order.rb
+++ lib/place_order.rb
@@ -1,12 +1,21 @@
 
-
 class Place_order
   attr_reader :current_order
   def initialize
     @current_order = Hash.new(0)
+    @total_value = 0
+    @menu = Menu.new
   end
 
   def add(dish, quantity)
     @current_order[dish] += quantity
+    p @current_order
+  end
+
+
+  def total
+    @current_order.each {|dish, quantity|
+    @total_value += @menu.dish[dish] * quantity}
+    "Your total bill is £#{@total_value}"
   end
 end
--- spec/place_order_spec.rb
+++ spec/place_order_spec.rb
@@ -4,11 +4,19 @@ describe Place_order do
   subject(:place_order) {described_class.new}
   let(:order) { {"sushi" => 4, "noodles" => 4} }
 
-  describe "adds" do
+  describe "#adds" do
     it "#add sushi and noodles with quantity of 4each into current order" do
       place_order.add("sushi", 4)
       place_order.add("noodles", 4)
       expect(place_order.current_order).to eq(order)
+    end
+
+    describe "#total" do
+      it "shows total value of the order" do
+        place_order.add("sushi", 4)
+        place_order.add("noodles", 4)
+        expect(place_order.total).to eq("Your total bill is £68")
+      end
     end
   end
 end
