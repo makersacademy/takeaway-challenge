bf6376a75db227d78463c8adcbcd581035d7aaf0
kealanheena <kealanheena@gmail.com>
2019-11-25 09:59:02 +0000

    add initialize and add item method to order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,12 @@
 class Order
 
+  attr_reader :order
+
+  def initialize
+    @order = []
+  end
+
+  def add_item(food_item)
+    @order << food_item
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,10 @@
 require "order"
 
 describe Order do 
-
+  describe "#add_item" do
+    it "should add an item to the order array" do
+      subject.add_item("Pizza")
+      expect(subject.order).to eq(["Pizza"])
+    end
+  end
 end
