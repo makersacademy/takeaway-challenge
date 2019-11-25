70f0fba3dbf43c1dc0c9b76917a8f16cfe43c32a
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-25 03:31:24 +0000

    menu items added

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,16 @@
 class Menu
 
   def initialize
+    @dish = {
+      "sushi" => 9,
+      "noodles" => 8,
+      "burger" => 7,
+      "rice" => 6
+    }
   end
 
   def list
+    @dish.each {|item, price|
+      puts "#{item} : £#{price}"}
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,8 @@
 require 'menu'
 describe Menu do
-
+  subject(:menu) { described_class.new }
   describe "#list" do
     it "displays a list of dishes with price" do
-      menu = Menu.new
       expect(menu).to respond_to(:list)
     end
   end
