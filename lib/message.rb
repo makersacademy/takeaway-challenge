82cffba7bda288d2b4cdaa5be4ebbdf87f4a37d1
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-12-10 11:50:21 +0000

    refactor

--- lib/message.rb
+++ lib/message.rb
@@ -1,11 +1,11 @@
 require 'twilio-ruby'
 
-account_sid = ''
-auth_token = ''
+account_sid = 'AC7f2ea5668c8a8fe460313b9bd7e31ee1'
+auth_token = '44a3206d789e6bbc7d97fc5e07108d69'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = '+15551234567' # Your Twilio number
-to = '+15555555555' # Your mobile phone number
+from = '441293311584' # Your Twilio number
+to = '+447462923192' # Your mobile phone number
 
 client.messages.create(
 from: from,
--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -5,3 +5,12 @@ o = Order.new
 o.add("pizza")
 o.add("curry", 5)
 o.check_order
+
+
+expect(order.view).to eq({
+  :curry => 10,
+  :pizza => 12,
+  :pasta => 8,
+  :fish => 10,
+  :beef => 18
+})
