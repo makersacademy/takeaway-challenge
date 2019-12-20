4c66169838b33b5817b20461a2c5b08490ab95e4
David Baez <david.baez@vodafone.com>
2019-12-20 14:11:04 +0000

    creating the method Add_to_basket so the customer can add dishes

--- lib/order.rb
+++ lib/order.rb
@@ -1,2 +1,11 @@
-# class Order
-# end
+class Order
+  attr_reader :basket, :item
+  def initialize
+    @basket = []
+  end
+
+  def add_to_basket(item, quantity)
+    @basket [item] += quantity
+  end
+
+end
