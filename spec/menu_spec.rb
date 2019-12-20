92e1c30502b6d406fd9d89ee6d1ee19eff9dcd84
Laura Kytonen <laura.kytonen@gmail.com>
2019-12-20 12:02:00 +0000

    added a fourth test which tests the actual output of view_menu method

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -9,10 +9,15 @@ describe Menu do
   it "contains a list of dishes with prices" do
     expect(subject.menu).to be_a Hash
     expect(subject.menu["Fried rice"]).to be_a Numeric
+    expect(subject.menu).to have_key("Fried rice")
   end
 
   it "responds to a method called view_menu" do
     expect(subject.menu).to respond_to(:view_menu)
+  end
+
+  it "allows the customer to view the list of dishes with prices" do
+    expect(subject.menu.view_menu).to include("Fried rice" => 8)
   end
 
 end
