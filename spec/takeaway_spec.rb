a72207dc506a7063e7d0517611a3f5b63cb3b262
Iryna Jurkiv <mariyayurkiv@MacBook-Pro.local>
2019-12-20 13:06:07 +0000

    User Story 3 undefined method total error message

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -18,9 +18,9 @@ describe Takeaway do
     it 'allows us to select items' do
       takeaway = Takeaway.new
       menu = Menu.new
-      takeaway.see(menu)
       menu.add("salad",10)
       menu.add("fish",20)
+      takeaway.see(menu)
       takeaway.select("fish")
       expect(takeaway.basket).to eq [{"fish" => 20 }]
     end
@@ -28,11 +28,24 @@ describe Takeaway do
     it 'it adds item by number of times specified' do
       takeaway = Takeaway.new
       menu = Menu.new
-      takeaway.see(menu)
       menu.add("salad",10)
       menu.add("fish",20)
+      takeaway.see(menu)
       takeaway.select("fish",2)
-      expect(takeaway.basket). to eq [{"fish"=>20}, {"fish"=>20}]
+      expect(takeaway.basket).to eq [{"fish"=>20}, {"fish"=>20}]
+    end
+
+    # Unit Test 3
+    describe '#total' do
+      it 'gives me total price of all items ordered' do
+        takeaway = Takeaway.new
+        menu = Menu.new
+        takeaway.see(menu)
+        menu.add("salad",10)
+        menu.add("water",2)
+        takeaway.select("water",2)
+        expect(menu.total).to eq 4
+      end
     end
   end
 end
