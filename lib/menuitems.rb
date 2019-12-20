dc72305ff0b7c8939b255f3dd30f35cdea8ff462
Aaron <aaron@aarons-imac.home>
2019-12-20 20:12:23 +0000

    changed from has_item? to item?

--- lib/menuitems.rb
+++ lib/menuitems.rb
@@ -2,13 +2,11 @@ class MenuItems
 
   attr_reader :menuitems
 
-  # pass in the munu hash
   def initialize(menuitems)
     @menuitems = menuitems
   end
 
-  # return true if item is listed in menuitems
-  def has_item?(item)
+  def item?(item)
     menuitems[item] ? true : false
   end
 
@@ -16,7 +14,6 @@ class MenuItems
     menuitems[item]
   end
 
-  # display will return a string with the menu list
   def display
     menuitems.map do |pizza, value|
       "#{pizza.capitalize} £#{value.to_f}"
