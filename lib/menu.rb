68dac75158f31ff4e7d97dc0773407f8f26192b1
Masha N <marianetchaeva@hotmail.com>
2019-12-20 12:53:57 +0000

    decided needed to add method for adding items to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,6 +2,17 @@ class Menu
   attr_reader :options
 
   def initialize
-    @options = {}
+    @options = {
+      :pasta => 5.50,
+      :sandwich => 3.75,
+      :soup => 3.25,
+      :coffee => 2.20
+    }
   end
+
+  # def list
+  #   @options.each do |n, p|
+  #     puts "#{n} - £%0.2f" % p
+  #   end
+  # end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,7 +3,14 @@ describe Menu do
   it 'has capacity to store menu options' do
     expect(subject.options).to be_a Hash
   end
-  it 'lists name and price of the dish' do
-    expect(subject.options).to include(a_kind_of(Symbol) => a_kind_of(Float))
+  it 'can add items to menu' do
+    subject.add("juice", 1.40)
+    expect(subject.options).to include("juice" => 1.40)
+  end
+
+
+  xit 'can show the menu' do
+
+    expect { subject.list }.to output(instance_of(Symbol) => instance_of(Float))
   end
 end
