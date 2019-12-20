5b0b33bc220b3ed0f176b2fbf9e1230146a09c25
Callum McDoiugall <cal-mcd@outlook.com>
2019-12-20 11:40:50 +0000

    adding failing test to display menu in appropriate format

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,15 @@
 class Menu
 
+  attr_reader :items
+
+  def initialize
+    @items = {
+      chicken: 5.0,
+      pizza: 6.5,
+      burrito: 4.0,
+      nachos: 3.5,
+      chips: 1.5
+    }
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,6 +7,11 @@ describe Menu do
 
   it 'holds a list of available items' do
     menu = Menu.new
-    expect(menu.items).to include ("chicken, £5.00")
+    expect(menu.items).to include :chicken => 5.0
+  end
+
+  it 'displays a list of all available items' do
+    menu = Menu.new
+    expect(menu.display_menu).to include "nachos, £5.00"
   end
 end
