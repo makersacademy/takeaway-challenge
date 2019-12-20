813a41dde51753fd7f415e97a7e45cf1fd260010
Masha N <marianetchaeva@hotmail.com>
2019-12-20 11:52:47 +0000

    created test for Menu class for capacity to store dishes

---
+++ spec/menu_spec.rb
@@ -0,0 +1,6 @@
+describe Menu do
+  it 'has capacity to store menu options' do
+    expect(subject.options).to be_a Hash
+  end
+
+end
---
+++ spec/order_spec.rb
@@ -0,0 +1,5 @@
+describe Order do
+  it 'has capacity to access menu options' do
+    # expect(subject.options).to 
+  end
+end
