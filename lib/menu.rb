b931249045a91f4dd983b87631b8d36bf60a18c1
David Baez <david.baez@vodafone.com>
2019-12-20 11:35:39 +0000

    creates the instance of Menu

---
+++ lib/menu.rb
@@ -0,0 +1,8 @@
+class Menu
+  DISHES_LIST = {
+      "Chicken Rice" => 0.99,
+      "Fried Chicken" => 2.99,
+      "Fried Rice" => 1.99,
+      "Side Salad" => 0.79,
+      "Noodle Soup" => 2.99}
+end
