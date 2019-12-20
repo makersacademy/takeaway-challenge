1dad37b82973ea0622f3bce3c634ea1289d7dd46
Fergus Corcoran <fergus.corcoran@gmail.com>
2019-12-20 11:13:22 +0000

    Test to print out menu hash

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,4 +4,10 @@ describe Takeaway do
   it 'instantiates a Takeaway class' do
     expect(subject).to be_a_kind_of Takeaway
   end
+
+  describe '#menu' do
+    it 'prints out the menu itmes and their prices' do
+      expect(subject.menu).to include("tacos" => 5)
+    end
+  end
 end
