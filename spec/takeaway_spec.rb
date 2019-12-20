d5cb3e36b9e605ccf8f880d386db23b9080198d8
Iryna Jurkiv <mariyayurkiv@MacBook-Pro.local>
2019-12-20 12:25:56 +0000

    User Story 2 error: wrong number of arguments expected takeaway.select to only pass item not quantity

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,7 @@
 require 'Takeaway'
 describe Takeaway do
+
+  #Unit Test 1
   describe '#see' do
     it {is_expected.to respond_to(:see).with(1).argument }
 
@@ -8,6 +10,29 @@ describe Takeaway do
     menu = Menu.new
     menu.add("salad",10)
     expect(takeaway.see(menu)).to eq [{"salad" => 10 }]
+    end
+  end
+
+  #Unit Test 2
+  describe "#select" do
+    it 'allows us to select items' do
+      takeaway = Takeaway.new
+      menu = Menu.new
+      takeaway.see(menu)
+      menu.add("salad",10)
+      menu.add("fish",20)
+      takeaway.select("fish")
+      expect(takeaway.basket).to eq [{"fish" => 20 }]
+    end
+
+    it 'it adds item by number of times specified' do
+      takeaway = Takeaway.new
+      menu = Menu.new
+      takeaway.see(menu)
+      menu.add("salad",10)
+      menu.add("fish",20)
+      takeaway.select("fish",2)
+      expect(takeaway.basket). to eq [{"fish"=>20}, {"fish"=>20}]
     end
   end
 end
