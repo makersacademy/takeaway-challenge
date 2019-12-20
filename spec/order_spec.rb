add4c9677721d3882bd19d98ba7e9a23a4b895a4
Rhodine <rorleans_lindsay@yahoo.co.uk>
2019-12-20 16:26:46 +0000

    I have included a failing test for undefined method 'add'

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,9 +1,24 @@
 require'order'
 
 describe Order do
-  subject(:order)
+  subject(:order) { described_class.new}
+  let(:dishes) do
+    {
+      rice: 1,
+      chips: 2,
+      fish: 1
+    }
+  end
+
   it "creates and instance of order" do
     expect(subject).to be_a Order
+  end
+
+  it "orders dishes" do
+    order.add(:rice, 1)
+    order.add(:chips, 2)
+    order.add(:fish, 1)
+    expect(order.dishes).to eq (dishes)
   end
 
 end
