6a5950e64b83f7ec7a9616a614d51b6efdca0e33
David Baez <david.baez@vodafone.com>
2019-12-20 12:44:30 +0000

    placing a new order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,15 @@
+require 'menu.rb'
 class Takeaway
+  attr_reader :menu
+def initialize(menu = Menu.new)
+  @menu = menu
+  @new_order = new_order
+
+end
+  def print_menu
+    menu.print
+  end
+  def place_order
+    new_order
+  end
 end
