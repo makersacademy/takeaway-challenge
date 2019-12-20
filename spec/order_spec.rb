68f3fb24fd4dfa7c2ba2c12d3b1711c38f1b7339
David Baez <david.baez@vodafone.com>
2019-12-20 15:15:28 +0000

    updating order_spec.rb

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,6 +3,7 @@ require 'takeaway.rb'
 describe Order do
 let(:item) {'Chicken Rice'}
 let(:quantity) { 1 }
+let(:menu) { double(:menu, dishes: { 'Chicken Rice' => 4.50 }) }
 it 'creates the instance of order'do
 order = Order.new
 end
@@ -18,5 +19,13 @@ context '#add_to_basket' do
     order.add_to_basket(item, quantity)
     expect(order.basket[item]).to eq 1
   end
+end
+context '#sum_of_basket'do
+  it 'it confirm the sum of all the dishes on the basket' do
+    order = Order.new
+    order.add_to_basket(item, quantity)
+    confirmation  = "You have order #{item} & #{quantity} = £#{(menu.dishes[item]*quantity)}, " #This is a confirmation message to the customer confirming the order
+    expect{order.sum_of_basket(menu)}.to output(confirmation).to_stdout
+end
 end
 end
