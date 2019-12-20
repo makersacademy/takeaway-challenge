ad942de769258c1c3b668ebc0e76532339db9652
Anne-Louise Darko <ms.darko@live.co.uk>
2019-12-20 10:46:35 +0000

    1st failed test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,11 @@
 require 'takeaway.rb'
 
 describe Takeaway do
-  
+  let (:takeaway) {Takeaway.new(menu: menu)}
+  let (:menu) { double(:menu, print: printed_menu) }
+  let (:printed_menu) {"Special Frice Rice: £6.50"}
+
+  it 'allows user to see a list of dishes with prices' do
+    expect(takeaway.print_menu).to eq(printed_menu)
+  end
 end
