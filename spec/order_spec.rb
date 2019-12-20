93157ee6d2f769c9207100b43b60d73e0af2ec07
Anne-Louise Darko <ms.darko@live.co.uk>
2019-12-20 15:26:56 +0000

    order class test 1 faiil

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,19 @@
-
+require 'order.rb'
 
 describe Order do
-  
+  let(:order) {Order.new}
+  let(:dishes) do
+    {
+      rice: 2,
+      curry: 1,
+      fish: 2
+    }
+  end
+  it ' allows user to order several dishes from menu' do
+    order.add(:rice, 2)
+    order.add(:curry, 1)
+    order.add(:fish, 2)
+    expect(order.dishes).to eq(dishes)
+  end
+
 end
