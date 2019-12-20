41b2fd5654d9ce8f268c76f8b6d37a17e2dc6def
Anne-Louise Darko <ms.darko@live.co.uk>
2019-12-20 14:07:25 +0000

    correcting syntax

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,12 +1,15 @@
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :order
 
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order
   end
 
   def print_menu
     menu.print #? syntax
   end
 
+  def order(dishes)
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,17 +1,18 @@
 require 'takeaway.rb'
 
 describe Takeaway do
-  let (:takeaway) {Takeaway.new(menu: menu, order: order}
+  let (:takeaway) {Takeaway.new(menu: menu, order: order)}
   let (:menu) { double(:menu, print: printed_menu)}
   let (:order) {double (:order)}
   let (:printed_menu) {"Special Fried Rice: £6.50"}
-  let (:dishes) { {rice: 2}
+  let (:dishes) { {rice: 2} }
 
   it 'allows user to see a list of dishes with prices' do
     expect(takeaway.print_menu).to eq(printed_menu)
   end
 
-  it 'allows user to order from a number of available dishes'
-  expect(takeaway.order(dishes)).to eq("Your order total is £13")
+  it 'allows user to order from a number of available dishes' do
+    expect(takeaway.order(dishes)).to eq("Your order total is £13")
   end
+
 end
