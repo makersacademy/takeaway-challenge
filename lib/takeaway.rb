b4eb958b9423d5b53e90a4eb0e9116904ea7816d
David Baez <david.baez@vodafone.com>
2019-12-20 16:14:33 +0000

    updated takeaway.rb

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,15 +1,19 @@
 require 'menu.rb'
+require 'order.rb'
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :place_order
 def initialize(menu = Menu.new)
   @menu = menu
-  @new_order = new_order
-
+  # @new_order = new_order
 end
   def print_menu
     menu.print
   end
-  def place_order
-    new_order
-  end
+  #  def place_order
+  #   new_order.add_to_basket(item, quantity)
+  #   "#{quantity}x #{item}(s) have been added to your order."
+  # end
+  # def basket_summary
+  #   order.basket_sum(menu)
+  # end
 end
