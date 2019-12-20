1c7a0e6118e279fba23cc7e82aa77c1e4a5476f4
Rhodine <rorleans_lindsay@yahoo.co.uk>
2019-12-20 11:58:47 +0000

    I have included a failing test which has the wrong number of arguments as no method has been initialized

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,12 +1,14 @@
 require 'takeaway'
 
 describe Takeaway do
+  subject(:takeaway) {described_class.new(menu: menu)}
+  let(:menu) { double(:menu) }
   it "creates an instance of Takeaway" do
     takeaway = Takeaway.new
     expect(takeaway).to be_a Takeaway
   end
 
   it 'creates a list of dishes with prices' do
-    expect(subject.dishes_with_prices). to eq("rice: £2.50")
+    expect(subject.dishes_with_prices).to eq("rice: £2.50")
   end
 end
