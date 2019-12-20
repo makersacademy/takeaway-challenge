c943c67356e47eafe3b5aa663e00f1cb3edd8da7
Iryna Jurkiv <mariyayurkiv@MacBook-Pro.local>
2019-12-20 11:22:36 +0000

    Redoing User Story 1 changing Dish to Menu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,10 +3,11 @@ describe Takeaway do
   describe '#see' do
     it {is_expected.to respond_to(:see).with(1).argument }
 
-    it 'allows us to see list of dishes with prices'do
+    it 'allows us to see menu with prices'do
     takeaway = Takeaway.new
-    salad = Dish.new(10)
-    expect(takeaway.see(salad)).to eq 10
+    menu = Menu.new
+    menu.add("salad",10)
+    expect(takeaway.see(menu)).to eq [{"salad" => 10 }]
     end
   end
 end
