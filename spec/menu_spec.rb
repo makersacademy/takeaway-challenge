3ec714cb4af770cbd22c28c6e337f6f3ba9a7df4
Rhodine <rorleans_lindsay@yahoo.co.uk>
2019-12-20 12:49:24 +0000

    I have included a failing test for wrong number of arguments initialized(0 instead of 1)

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,23 @@
 require "menu"
 
 describe Menu do
+
+  subject(:menu) { described_class.new(dishes) }
+
+  let(:dishes) do
+    {
+      rice: 2.50,
+      fish: 5.00,
+      chips: 3.00
+    }
+  end
+
   it "creates and instance of Menu" do
     menu = Menu.new
     expect(menu).to be_a Menu
   end
 
+  it "has a list of dishes with prices" do
+    expect(menu.dishes).to eq(dishes)
+  end
 end
