2d09e44636fb1b5de70ae3e19cf66bd06dc48859
Iryna Jurkiv <mariyayurkiv@MacBook-Pro.local>
2019-12-20 11:04:27 +0000

    User Story 1 expected pricetakeaway.see a dish price to be 10 but got nil

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,6 +1,12 @@
 require 'Takeaway'
 describe Takeaway do
   describe '#see' do
-  it {is_expected.to respond_to(:see).with(1).argument }
+    it {is_expected.to respond_to(:see).with(1).argument }
+
+    it 'allows us to see list of dishes with prices'do
+    takeaway = Takeaway.new
+    salad = Dish.new(10)
+    expect(takeaway.see(salad)).to eq 10
+    end
   end
 end
