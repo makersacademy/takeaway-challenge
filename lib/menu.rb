f62b8875f82f699ef697c8c0e4d8792e97ec3d06
David Baez <david.baez@vodafone.com>
2019-12-20 11:53:24 +0000

    prints the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,17 @@
 class Menu
+  attr_reader :dishes
   DISHES_LIST = {
-      "Chicken Rice" => 0.99,
-      "Fried Chicken" => 2.99,
-      "Fried Rice" => 1.99,
-      "Side Salad" => 0.79,
-      "Noodle Soup" => 2.99}
+    "Chicken Rice" => 0.99,
+    "Fried Chicken" => 2.99,
+    "Fried Rice" => 1.99,
+    "Side Salad" => 0.79,
+    "Noodle Soup" => 2.99
+  }
+
+  def initialize
+    @dishes = DISHES_LIST
+  end
+  def print_menu
+      @dishes.each { |dish, price| "#{dish}: £#{price}, " }
+  end
 end
