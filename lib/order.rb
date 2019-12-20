c34f04d4d024921ef373503a0da54337cb11f541
David Baez <david.baez@vodafone.com>
2019-12-20 16:14:11 +0000

    updated order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -1,18 +1,20 @@
 class Order
-  attr_reader :basket, :item, :quantity, :menu
+  attr_reader :basket, :item, :quantity
   def initialize
     @basket = Hash.new(0)
-    @menu = menu
+
   end
 
   def add_to_basket(item, quantity)
     basket[item] += quantity
   end
 
-  def sum_of_basket
+  def sum_of_basket(menu)
     basket.each do |item, quantity|
-        puts "You have order #{item} & #{quantity} = £#{(menu.dishes[item]*quantity)}, "
+      print  "You have order #{item} & #{quantity} = £#{(menu.dishes[item]*quantity)},"
         end
   end
+
+  # def total_basket
 
  end
