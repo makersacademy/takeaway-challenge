ab9be02b9201f0336287a631fd82897ae2615b85
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-01 19:16:44 +0000

    add functions: order, add and basket summary

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,13 +1,44 @@
 require_relative 'menu'
 
 class TakeAway
+  attr_reader :orders
 
   def initialize(menu = Menu.new)
     @menu = menu
+    @orders = Hash.new
   end
 
   def read_menu
     @menu.dishes
+  end
+
+  def order(item, amount=1)
+    raise "Not in menu" if !@menu.dishes.include? item
+    @orders[item] = amount
+    "#{amount}x #{item}(s) added to your basket."
+    # @menu.dishes.each do |food, price|
+    #   # if item == food
+    #   #   @orders[item] = amount
+    #   #   puts "#{amount}x #{item}(s) added to your basket."
+    #   # else
+    #   #   raise "Not in menu"  
+    #   # end
+    # end
+  end
+
+  def add(item, amount=1)
+    @orders[item] = amount
+    "#{amount}x #{item}(s) added to your basket."
+  end
+
+  def basket_summary
+    orders.each do |item, amount|
+      @menu.dishes.each do |food, price|
+        if food == item
+          puts "#{item} x#{amount} = £#{amount*price}"
+        end
+      end
+    end
   end
 
   def complete_order(total_price)
@@ -19,3 +50,8 @@ class TakeAway
   end
 
 end
+
+# t = TakeAway.new
+# t.order("spring roll", 2)
+# t.add("char sui bun", 5)
+# t.basket_summary
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -9,9 +9,14 @@ describe TakeAway do
   end
 
   it { expect(subject).to respond_to :read_menu }
+  it { expect(subject).to respond_to :order }
 
   it 'shows the customers a list of dishes with prices' do
     expect(subject.read_menu).to match_array({"spring roll" => 0.99, "char sui bun" => 3.99, "pork dumpling" => 2.99, "peking duck" => 7.99})
+  end
+
+  it 'prints out the order' do
+    expect(subject.order("spring roll")).to eq "1x spring roll(s) added to your basket."
   end
 
 end
