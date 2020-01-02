e3237045025ad100b31fc3c0e454bca8fcea9dca
David Papamichael <davidpapamichael@gmail.com>
2020-01-02 13:43:57 +0000

    User story 2, select some number of avaliable dishes with quantity

--- lib/pizza_place.rb
+++ lib/pizza_place.rb
@@ -1,13 +1,20 @@
 class PizzaPlace
 
-  attr_reader :menu
+  attr_reader :menu, :order
 
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order
   end
 
   def print_menu
     menu.prints
+  end
+
+  def place_order(items)
+    items.each do |item, quantity| 
+      order.add(item, quantity)
+    end
   end
 
 end
--- spec/pizza_place_spec.rb
+++ spec/pizza_place_spec.rb
@@ -2,13 +2,21 @@ require "pizza_place"
 
 describe PizzaPlace do
 
-  subject(:pizza) { PizzaPlace.new(menu: menu) }
+  subject(:pizza) { PizzaPlace.new(menu: menu, order: order) }
 
   let(:menu) { double(:menu, prints: menu_print) }
   let(:menu_print) { "Margarita: £9" }
+  let(:order) { double(:order) }
+
+  let(:items) { { Margerita: 9, Pepperoni: 11, Vegetable: 10 } }
 
   it "shows the items and prices" do
     expect(pizza.print_menu).to eq(menu_print)
+  end
+
+  it "can order number of avaliable dishes" do
+    expect(order).to receive(:add).exactly(3).times
+    pizza.place_order(items)
   end
   
 end
