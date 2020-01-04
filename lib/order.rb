3391d85d32f8eecc638451c1cd23c0900295326e
benauld <ben@portobello-consultancy.co.uk>
2020-01-04 16:42:40 +0000

    User stories - show and total orders

--- lib/order.rb
+++ lib/order.rb
@@ -12,8 +12,22 @@ class Order
 
   def create(selection, quantity)
     raise "Item not in menu" if @menu[selection - 1].nil?
+
     dish = @menu[selection - 1]
     @order << @order_line.new(quantity, dish["name"], dish["price"])
   end
 
+  def total
+    total = 0
+    @order.each { |item| total += (item.quantity * item.price) }
+    return total
+  end
+  
+  def show
+    puts "No.  Price     Name"
+    @order.each { |item| 
+      puts "#{item.quantity}    £#{item.price}  #{item.name}."
+    }    
+    puts "Total : £ #{total}"
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -18,4 +18,36 @@ describe 'Order' do
       expect { subject.create(20, 1) }.to raise_error 'Item not in menu'
     end
   end
+
+  describe '#total' do
+    it 'returns the total cost of an order (0)when no items entered' do
+      subject = Order.new(OrderLine)
+      expect(subject.total).to eq(0)
+    end
+    it 'returns a total of 0.75p when popadums selected (first file item)' do
+      subject = Order.new(OrderLine)
+      subject.create(1, 1)
+      expect(subject.total).to eq(0.75)
+    end
+    it 'returns a total of 6.75p when 1 popadums and 2 onion bhajis selected' do
+      subject = Order.new(OrderLine)
+      subject.create(1, 1)
+      subject.create(3, 2)
+      expect(subject.total).to eq(6.75)
+    end
+  end
+
+  describe '#show' do
+    it 'shows an empty order with a total of zero' do
+      subject = Order.new(OrderLine)
+      expect(subject.show).to eq(nil)
+    end
+    it 'shows an order with 3 items and a total of ' do
+      subject = Order.new(OrderLine)
+      subject.create(1, 4)
+      subject.create(3, 2)
+      subject.create(5, 2)
+      expect(subject.show).to eq(nil)
+    end  
+  end
 end
