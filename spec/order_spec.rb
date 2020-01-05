0f252874a2ffd26680eb712c0761ae88fea13897
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-05 15:31:41 +0000

    Test.7 - check items not on menu when #add it raises_error "not on menu"

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,6 +4,8 @@ describe Order do
 
   subject(:order) { described_class.new }
 
+  let(:dishes) { double(:menu) }
+
   let(:dishes) do
     {
       pizza: 1,
@@ -15,5 +17,9 @@ describe Order do
     order.add(:pizza,1)
     order.add(:felafel,2) #commands 
     expect(order.dishes).to eq(dishes) # queries / dishes set as stub
+  end
+
+  it "dosnt allow item not in menu to be added" do #add menu to test
+    expect{ order.add(:beef,1) }.to raise_error "This is not on the menu!"
   end
 end
