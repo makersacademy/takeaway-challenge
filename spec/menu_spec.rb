83b4e72a3b1f4e1b1277b124dbec2d8c7e7c262a
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-05 09:42:56 +0000

    Test.3 - check restult of printed menu == item in menu hash

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -11,5 +11,11 @@ describe Menu do
   it 'has list of dishes with prices' do
     expect(menu.dishes).to eq(dishes)
   end
+
+  it 'prints list of dished with prices' do
+    printed_menu = "Pizza: 5.99, Felafel: 3.50"
+    expect(menu.printed_menu).to eq(printed_menu)
+  end
 end
+
 
