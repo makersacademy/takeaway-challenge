8cd6ee58e56a0cccd021924fbf47a62f1f268c16
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-05 15:17:19 +0000

    Passed.6 - Order class - attribute and method #add wich adds dishes

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,16 @@
 
 class  Order
 
+  attr_reader :dishes
+
+  def initialize
+    @dishes = {} #Hash.new(0) create new hash with 0 add += quantity in add method
+    
+  end
+
+  def add(dish, quanitiy) #same two arguments from the add_order method in take_away class
+    dishes[dish] = quanitiy # += quanitiy if initialized hash with default (0)
+    
+  end
+
 end
