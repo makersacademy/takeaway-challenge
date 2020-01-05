aefb75659436b499214d442f9cede6d800357908
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-05 15:00:00 +0000

    Test.6 - build order test to check dishes are added, stub hash for dishes

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,19 @@
 require "order"
 
 describe Order do
-  
+
+  subject(:order) { described_class.new }
+
+  let(:dishes) do
+    {
+      pizza: 1,
+      felafel: 2
+    }
+  end
+
+  it "selects dishes from menu" do
+    order.add(:pizza,1)
+    order.add(:felafel,2) #commands 
+    expect(order.dishes).to eq(dishes) # queries / dishes set as stub
+  end
 end
