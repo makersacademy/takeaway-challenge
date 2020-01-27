95bc39d81bb6c8066f6d6177015d61d00bcfb236
CodeKrakken <newholm1@hotmail.com>
2020-01-27 20:35:51 +0000

    Cleaning up commit history

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,4 @@
-# Twilio functionality successfully integrated but causes all tests to fail as rspec cannot find 'twilio-ruby'. To test functionality, please insert your phone number at line 46 and unhash all commented lines.
-
-# require 'twilio-ruby'
+require 'twilio-ruby'
 
 class Takeaway
 
@@ -33,20 +31,20 @@ class Takeaway
   def checkout
     order_placed = Time.now
     delivery_due = Time.now+3600
-    # send_text(order_placed, delivery_due)
+    send_text(order_placed, delivery_due)
     "Your order was placed at #{order_placed}. Your food should be with you by #{delivery_due}." # You may need to comment out this line to test text functionality.
   end
 
-  # def send_text(order_placed, delivery_due)
-  #   account_sid = ENV['TWILIO_ACCOUNT_SID']
-  #   auth_token = ENV['TWILIO_AUTH_TOKEN']
-  #   client = Twilio::REST::Client.new(account_sid, auth_token)
-  #   client.messages.create(
-  #   from: '+12132386475',
-  #   to: '+44XXXXXXXXXX',
-  #   body: "Your order was placed at #{order_placed}. Your food should be with you by #{delivery_due}."
-  #   )
-  # end    
+  def send_text(order_placed, delivery_due)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+    client.messages.create(
+    from: '+12132386475',
+    to: '+447391405893',
+    body: "Your order was placed at #{order_placed}. Your food should be with you by #{delivery_due}."
+    )
+  end    
 
 end
 
