2d2197dba82051ca9e2f1acdc5d4a34f13eeb211
Josh Blumberg <jlblumberg@gmail.com>
2020-02-01 10:14:14 +0000

    add TakeAway#add_to_cart to add items to cart

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,7 +1,18 @@
 class TakeAway
 
+  attr_reader :cart
+
+  def initialize(cart = [])
+    @cart = cart
+  end
+
   def show_menu
     {"burger" => 5, "fries" => 3, "shake" => 2}
+  end
+
+  def add_to_cart(item)
+    cart << item
+    "1 #{item} added to your cart"
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,8 +1,18 @@
 require 'takeaway'
 describe TakeAway do
+
   describe '#show_menu' do
     it 'prints a list of dishes available' do
       expect(subject.show_menu).to eq({"burger" => 5, "fries" => 3, "shake" => 2})
     end
   end
+
+  describe '#add_to_cart' do
+    it 'adds items to the cart' do
+      expect{subject.add_to_cart("burger")}.to change{subject.cart.count}.by(1)
+    end
+  end
+
+
+  
 end
