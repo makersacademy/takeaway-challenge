fa346ec0528c22c230bd8d2e882659f6abc81d6f
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-01 22:16:33 +0000

    Created method order to return current order, currently skipping add method test due to planned functionality change

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,14 +2,19 @@ class Takeaway
   
   def initialize(menu: Menu.new)
     @menu = menu
+    @order = {}
   end
 
   def add(item)
-    item
+    @order[item] = 1
   end
 
   def show_menu
     @menu
+  end
+
+  def order
+    @order
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,9 +2,8 @@ require "takeaway.rb"
 
 describe Takeaway do
 
-  let(:menu){ double :menu, list: {"Pilau rice": 1.50, "Poppadom": 0.50, "Mango Chutney": 0.30}}
+  let(:menu){ double :menu, list: {"Pilau rice": 1.50, "Poppadom": 0.50, "Mango Chutney": 0.30}, item_exists?: true}
   subject {described_class.new(menu: menu)}
-  let(:item){"Pilau rice"}
 
 
   describe " #show_menu" do
@@ -23,15 +22,18 @@ describe Takeaway do
     it "can order can accept one argument" do
       expect(subject).to respond_to(:add).with(1).argument
     end
-
-    it "returns item name from order" do
-      expect(subject.add("Tikka Masala")).to eq "Tikka Masala"
+    skip " change test to return string for order" do
+      it "returns item name from order" do
+        expect(subject.add("Tikka Masala")).to eq "Tikka Masala"
+      end
     end
-    
-    # it "adds ordered item to array" do
-    #   subject.add(item)
-    #   expect(subject.order).to
 
+    it "adds ordered item to array" do
+      item = "Pilau rice"
+      key_value_pair = {"Pilau rice" => 1}
+      subject.add(item)
+      expect(subject.order).to eq(key_value_pair)
+    end
   end
 
 
