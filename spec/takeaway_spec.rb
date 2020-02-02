7dc5b41e77e7423923f1abf80c3f8b82c671547d
Liz Daly <liz7daly@gmail.com>
2020-02-02 22:23:29 +0000

    Refactor tests to isolate classes

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,6 +3,7 @@ require 'takeaway'
 describe Takeaway do
   let(:order) { double :order, num: 1, quantity: 2}
   let(:order_class_double) { double :order_class, new: order }
+  let(:takeaway) { Takeaway.new(order_class_double) } 
 
   it 'shows a menu' do
     expect { subject.show_menu }.to output.to_stdout
@@ -10,39 +11,41 @@ describe Takeaway do
 
   context 'when one dish is ordered' do
     before do
-      subject.order_item(1)
+      allow(order).to receive(:cost).and_return(4)
+      takeaway.order_item(1)
     end
 
-    it 'returns the item and cost' do
-      takeaway = Takeaway.new(order_class_double)
-      allow(order).to receive(:cost).and_return(8)
-      takeaway.order_item(1)
+    it 'stores order details' do
       expect(takeaway.orders).to include(order)
     end
 
     it 'shows the total cost of the order' do
-      expect(subject.total_cost).to eq 4
+      expect(takeaway.total_cost).to eq 4
     end
   end
 
   context 'when user orders a quantity of one dish' do 
     before do
-      subject.order_item(1, 2)
+      allow(order).to receive(:cost).and_return(8)
+      takeaway.order_item(1, 2)
     end
     
     it 'includes quantity in total cost' do
-      expect(subject.total_cost).to eq 8
+      expect(takeaway.total_cost).to eq 8
     end
   end
 
   context 'when user orders more than one dish' do 
     before do
-      subject.order_item(1, 2)
-      subject.order_item(2)
+      allow(order).to receive(:cost).and_return(8)
+      takeaway.order_item(1, 2)
+      takeaway.order_item(2)
     end
 
     it 'stores orders' do
-      expect(subject.orders.count).to eq 2
+      expect(takeaway.orders.count).to eq 2
     end
+
+
   end
 end
