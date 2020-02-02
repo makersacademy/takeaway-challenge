839a3315b97399af63a293e813fa526b08bd1210
Student <student@makerss-air-2.home>
2020-02-02 23:49:24 +0000

    Raise an error for wrong payment amount

--- lib/order.rb
+++ lib/order.rb
@@ -1,17 +1,22 @@
 class Order
-  attr_reader :my_order, :my_bill, :orde_details
+  attr_reader :my_order, :my_bill, :orde_details, :self_calculated_total, :total_amount
   def initialize
     @my_order = []
     @my_bill = []
-    @orde_details = {}
+    @self_calculated_total = 0
   end
 
-  def add_items(dish, quantity)
+  def add_items(dish, quantity, total)
     @my_order.push("#{dish} * #{quantity}")
     @my_bill.push(Menu::The_menu[dish] * quantity)
+    @self_calculated_total += total
   end
 
   def total_amount
-    @my_bill.inject(0, :+)
+    @total_amount = @my_bill.inject(0, :+)
+  end
+
+  def place_order
+    fail "Your payment amount is incorrect" unless @self_calculated_total == @total_amount
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,19 +1,27 @@
 require './lib/order.rb'
 describe Order do
   describe '#add_items' do
-    it 'allows to enter the dish name and quantity' do
-      expect{ subject.add_items("Pad_thai", 2) }.to change { subject.my_order }.to ["Pad_thai * 2"]
+    it 'allows to enter the dish name, quantity and total amount' do
+      expect{ subject.add_items("Pad_thai", 2, 16) }.to change { subject.my_order }.to ["Pad_thai * 2"]
     end
 
     it 'presents the amount to be paid for each dish' do
-      expect{ subject.add_items("Pad_thai", 3) }.to change { subject.my_bill }.to [24]
+      expect{ subject.add_items("Pad_thai", 3, 24) }.to change { subject.my_bill }.to [24]
     end
-
+  end
+  describe '#total_amount' do
     it 'shows the total amount for all dishes in the order' do
       order = Order.new
-      order.add_items("Pad_thai", 2)
-      order.add_items("Red_curry", 1)
+      order.add_items("Pad_thai", 2, 16)
+      order.add_items("Red_curry", 1, 9)
       expect(order.total_amount).to eq 25
+    end
+  end
+  describe '#place_order' do
+    it 'raises an error if the amount for payment does not match the total_amount' do
+      order = Order.new
+      order.add_items("Pad_thai", 2, 15)
+      expect { order.place_order }.to raise_error "Your payment amount is incorrect"
     end
   end
 end
