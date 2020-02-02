3adbd089fec7c0e7bc3fb2c509e5900dbbaa2acc
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-02 19:41:07 +0000

    added method total to calculate total in Calculator class

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,4 +1,7 @@
 class Calculator
+
+  attr_reader :result, :total
+
   # Would like to shorten method but not sure how
   def self.current_basket(order, dishes)
     result = []
@@ -7,6 +10,15 @@ class Calculator
         result.push({item_name: item.to_s , quantity: quantity, subtotal: price * quantity}) if item == name
       end
     end
-    result
+    @result = result
   end
+
+  def self.total
+    total = 0
+    @result.each do |line|
+      total +=line.values[2]
+    end
+    @total = total
+  end
+
 end
--- spec/calculate_spec.rb
+++ spec/calculate_spec.rb
@@ -1,6 +1,7 @@
 require "calculator.rb"
 
 describe Calculator do
+  
   describe " #checkout" do
     it "should respond to two arguments" do
       expect(Calculator).to respond_to(:current_basket).with(2).argument
@@ -18,5 +19,20 @@ describe Calculator do
       expect(Calculator.current_basket(order, dishes)).to eq ([{item_name: "Pilau rice", quantity: 1, subtotal: 3.0}, {item_name: "Tikka Masala", quantity: 3, subtotal: 15.0}])
     end
   end
+
+  describe " #total" do
   
+  before(:example) do
+    order = {"Pilau rice": 1, "Tikka Masala": 3}
+    dishes = {"Pilau rice": 3.00, "Tikka Masala": 5.00}
+    Calculator.current_basket(order, dishes)
+  end
+
+    it "should return sum based on current_basket output" do
+      expect(Calculator.total).to eq(18)
+    end
+
+  end
 end
+
+# [{item_name: "Pilau rice", quantity: 1, subtotal: 1.50}, {item_name: "Poppadom", quantity: 4, subtotal: 2.00}]
