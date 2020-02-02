b95679822aa28a0f472f07c70c087fca8dbf0ba3
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-02 15:39:49 +0000

    updated current_basket in calculator to accept multiple items in order and dishes arguments

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,16 @@
 class Calculator
   def self.current_basket(order, dishes)
-    {item_name: order.keys[0].to_s , quantity: order.values[0], subtotal: dishes.values[0] * order.values[0]}
+    result = []
+    order.each do |item, quantity|
+      dishes.each do |name, price|
+        if item == name
+          result.push({item_name: item.to_s , quantity: quantity, subtotal: price * quantity})
+        end
+      end
+    end
+    result
   end
 end
+
+
+# {item_name: order.keys[0].to_s , quantity: order.values[0], subtotal: dishes.values[0] * order.values[0]}
--- spec/calculate_spec.rb
+++ spec/calculate_spec.rb
@@ -9,7 +9,13 @@ describe Calculator do
     it "should return a hash with the item_name, quantity and subtotal" do
       order = {"Pilau rice": 1}
       dishes = {"Pilau rice": 3.00}
-      expect(Calculator.current_basket(order, dishes)).to eq ({item_name: "Pilau rice", quantity: 1, subtotal: 3.0})
+      expect(Calculator.current_basket(order, dishes)).to eq ([{item_name: "Pilau rice", quantity: 1, subtotal: 3.0}])
+    end
+
+    it "should return a hash with the item_name, quantity and subtotal for MULTIPLE items" do
+      order = {"Pilau rice": 1, "Tikka Masala": 3}
+      dishes = {"Pilau rice": 3.00, "Tikka Masala": 5.00}
+      expect(Calculator.current_basket(order, dishes)).to eq ([{item_name: "Pilau rice", quantity: 1, subtotal: 3.0}, {item_name: "Tikka Masala", quantity: 3, subtotal: 15.0}])
     end
   end
 end
