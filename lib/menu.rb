a1df2a28e03032ed628791032e2abbadec9cdbd5
Chris Eadie <chris.eadie@outlook.com>
2020-02-02 12:40:45 +0000

    Added menu item request

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,7 @@
 class Menu
   def list
   end
+  def item(number)
+    "item"
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,10 @@ require 'menu'
 
 describe Menu do
   it {is_expected.to respond_to(:list)}
+  
+  describe '#item' do
+    it 'should return a menu item' do
+      expect(subject.item(1)).to be_truthy
+    end
+  end
 end
