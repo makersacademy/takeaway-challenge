8b14d891cbb172db264253f03ac8c1fae036c76f
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-02 15:13:02 +0000

    refactored current_basket method in calculator

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,9 +1,5 @@
 class Calculator
   def self.current_basket(order, dishes)
-    item = order.keys[0].to_s
-    quantity = order.values[0]
-    price = dishes.values[0]
-    subtotal = quantity * price
-    {item_name: item , quantity: quantity, subtotal: subtotal}
+    {item_name: order.keys[0].to_s , quantity: order.values[0], subtotal: dishes.values[0] * order.values[0]}
   end
 end
