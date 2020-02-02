219e957a1f064029cb13010d8a04a2fbc55a621f
Liz Daly <liz7daly@gmail.com>
2020-02-02 21:13:02 +0000

    Add cost per order

--- lib/order.rb
+++ lib/order.rb
@@ -2,10 +2,11 @@ require 'menu'
 
 class Order
   include Menu
-  attr_reader :dish, :quantity
+  attr_reader :dish, :quantity, :cost
 
   def initialize(item_no, quantity = 1)
     @dish = MENU[item_no][:dish]
     @quantity = quantity
+    @cost = MENU[item_no][:cost] * quantity
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,9 +7,24 @@ describe Order do
     expect(order.dish).to eq "Egg Paneer Kathi Roll"
   end
 
-  it 'shows a quantity' do
-    multi_order = Order.new(1, 2)
-    expect(multi_order.quantity).to eq 2
+  it 'shows a quantity of 1' do
+    expect(order.quantity).to eq 1
+  end
+
+  it 'shows the cost from menu' do
+    expect(order.cost).to eq 4
+  end
+
+  context 'when quantity of more than one selected' do
+      let(:multi_order) { Order.new(1, 2) } 
+
+    it 'shows a quantity of more than one' do
+      expect(multi_order.quantity).to eq 2
+    end
+
+    it 'shows the total cost of multiple dishes' do
+      expect(multi_order.cost).to eq 8
+    end
   end
 
 end
