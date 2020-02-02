20e364f309f7c8e02e63d2d17193294eb135ffb5
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-02 17:21:14 +0000

    Added dishes method

---
+++ lib/menu.rb
@@ -0,0 +1,6 @@
+class Menu
+
+  def dishes
+    []
+  end
+end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,1 +1,10 @@
 require 'menu'
+
+describe Menu do
+
+  describe '#dishes' do
+    it 'gets dishes from list of dishes' do
+      expect(subject.dishes).to eq([])
+    end
+  end
+end
