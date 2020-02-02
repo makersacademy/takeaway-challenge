b079819e6fbae688f0cf477460357ac1d68a2193
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-02 19:57:18 +0000

    Added '#add_dish' method
    add_dish method will save new dish to dish_name

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,14 @@
 class Menu
 
   attr_reader :dishes
-  
+
   def initialize
 
     @dishes = []
 
+  end
+
+  def add_dish(new_dish)
+    new_dish
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,14 +4,18 @@ describe Menu do
 
   let(:menu) { Menu.new }
 
-  describe "new dishes" do
+  describe ".new dishes" do
     it "equals an empty hash" do
       expect(menu.dishes).to eq([])
     end
   end
-  # describe '#get_dishes' do
-  #   it 'gets dishes' do
-  #     expect(subject.getdishes).to eq("Salmon")
-  #   end
-  # end
+
+  describe "#add_dish" do
+
+    it "adds new dishes to dishes array" do
+      new_dish = "Salmon"
+      menu.add_dish(new_dish)
+      expect(menu.add_dish(new_dish)).to eq(new_dish)
+    end
+  end
 end
