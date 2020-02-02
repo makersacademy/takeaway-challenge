683d07cb4fc64a9b787ccf7ff5674b0f80f08b60
Student <emilywright212@gmail.com>
2020-02-02 11:18:45 +0000

    Initial order method accepted

--- lib/place_order.rb
+++ lib/place_order.rb
@@ -1,6 +1,7 @@
 class PlaceOrder
   
   def ask_for_order
+    "Please place your order in the form of order number, quantity. E.g. '23, 7'"
   end
 
 end
--- spec/place_order_spec.rb
+++ spec/place_order_spec.rb
@@ -4,6 +4,9 @@ describe PlaceOrder do
 
   describe "#takes_dishes" do
     it { is_expected.to respond_to(:ask_for_order) }
+    it 'asks for an order' do
+      expect(subject.ask_for_order).to eq ("Please place your order in the form of order number, quantity. E.g. '23, 7'")
+    end
   end
 
   describe "#checks total" do
