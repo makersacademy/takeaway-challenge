df33fbab4a9829dd2a0429494bed42f5d1bcdc0c
Student <student@makerss-air-2.home>
2020-02-02 19:33:59 +0000

    Add list of items + quantity to my_order, and show the total amount to be paid

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,17 @@
 class Order
-  attr_reader :my_order, :my_bill
+  attr_reader :my_order, :my_bill, :orde_details
   def initialize
     @my_order = []
     @my_bill = []
+    @orde_details = {}
   end
 
-  def select_item(dish, quantity)
+  def add_items(dish, quantity)
     @my_order.push("#{dish} * #{quantity}")
     @my_bill.push(Menu::The_menu[dish] * quantity)
+  end
+
+  def total_amount
+    @my_bill.inject(0, :+)
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,7 +1,19 @@
 require './lib/order.rb'
 describe Order do
-  it 'let you select items from the menu' do
-    expect{ subject.select_item("Pad_thai") }.to change { subject.my_order }.to ["Pad_thai"]
-  end
+  describe '#add_items' do
+    it 'allows to enter the dish name and quantity' do
+      expect{ subject.add_items("Pad_thai", 2) }.to change { subject.my_order }.to ["Pad_thai * 2"]
+    end
 
+    it 'presents the amount to be paid for each dish' do
+      expect{ subject.add_items("Pad_thai", 3) }.to change { subject.my_bill }.to [24]
+    end
+
+    it 'shows the total amount for all dishes in the order' do
+      order = Order.new
+      order.add_items("Pad_thai", 2)
+      order.add_items("Red_curry", 1)
+      expect(order.total_amount).to eq 25
+    end
+  end
 end
