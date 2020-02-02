d6065ba1a827c045872468716294aa2801eded19
Joe Ellingworth <joeellingworth@gmail.com>
2020-02-02 17:48:00 +0000

    added test for add_item methods

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,19 @@
 require './lib/menu.rb'
 class Order 
 
-    attr_reader :order, :menu
+    attr_reader :order_form, :menu
 
-  def initizialize 
-    @order = Hash.new(0)
+  def initialize 
+    @order_form = Hash.new(0)
     @menu = Menu.new
   end 
 
   def view_menu
     menu.view
+  end 
+
+  def add_item(dish, quantity)
+    order_form[dish.to_sym] = quantity
+    "#{quantity} x #{dish} added to your order"
   end 
 end 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,12 +3,21 @@ require 'order'
 describe Order do 
   let(:menu) { Menu.new }
   let(:order) { Order.new }
-  it { is_expected.to respond_to(:order) }
+  it { is_expected.to respond_to(:order_form) }
   it { is_expected.to respond_to(:menu) }
 
   describe ' #view' do 
     it 'should be able to view the menu' do 
       expect(menu.view).to be_a(Hash)
+    end 
+  end 
+
+  describe ' #add_item' do 
+    it 'should add the item to the order form' do 
+      expect(order.add_item("padthai", 1)).to eq("1 x padthai added to your order")
+    end 
+    it 'each add should increase the order size by one' do 
+      expect { expect(order.add_item("curry", 2)) }.to change{order.order_form.length}.by(1)
     end 
   end 
 
