3806274616bbdd602d23d3c62b29cc6ef16f9652
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-02 15:11:24 +0000

    created current_basket method in calculator and made basic test pass

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,9 @@
 class Calculator
-
+  def self.current_basket(order, dishes)
+    item = order.keys[0].to_s
+    quantity = order.values[0]
+    price = dishes.values[0]
+    subtotal = quantity * price
+    {item_name: item , quantity: quantity, subtotal: subtotal}
+  end
 end
--- spec/calculate_spec.rb
+++ spec/calculate_spec.rb
@@ -1,5 +1,15 @@
 require "calculator.rb"
 
 describe Calculator do
+  describe " #checkout" do
+    it "should respond to two arguments" do
+      expect(Calculator).to respond_to(:current_basket).with(2).argument
+    end
 
+    it "should return a hash with the item_name, quantity and subtotal" do
+      order = {"Pilau rice": 1}
+      dishes = {"Pilau rice": 3.00}
+      expect(Calculator.current_basket(order, dishes)).to eq ({item_name: "Pilau rice", quantity: 1, subtotal: 3.0})
+    end
+  end
 end
