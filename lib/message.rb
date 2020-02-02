366add82bac3b7a6650251081c843831ab638371
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-02 18:58:03 +0000

    Set environment variables for credentials

--- lib/message.rb
+++ lib/message.rb
@@ -1,12 +1,12 @@
 # Download the twilio-ruby library from twilio.com/docs/libraries/ruby
 require 'twilio-ruby'
 
-account_sid = 'AC1237f9ad87e71817a774ef5e7447c7b2'
-auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyy'
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = '+15551234567' # Your Twilio number
-to = '+15555555555' # Your mobile phone number
+from = ENV[TWILIO_NUMBER] # Your Twilio number
+to = ENV[MY_NUMBER] # Your mobile phone number
 
 client.messages.create(
 from: from,
