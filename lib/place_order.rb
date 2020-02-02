5c94c6508ed6fb0933ec382176e67124712eee67
Student <emilywright212@gmail.com>
2020-02-02 16:10:06 +0000

    method take_order created

--- lib/place_order.rb
+++ lib/place_order.rb
@@ -1,7 +1,13 @@
 class PlaceOrder
-  
+
   def ask_for_order
-    "Please place your order in the form of order number, quantity. E.g. '23, 7'"
+    p "Please place your order in the form of order number, quantity. E.g. '23, 7'"
   end
+
+  def take_order
+    $stdin = gets.chomp
+  end
+
+  
 
 end
--- spec/place_order_spec.rb
+++ spec/place_order_spec.rb
@@ -7,6 +7,12 @@ describe PlaceOrder do
     it 'asks for an order' do
       expect(subject.ask_for_order).to eq ("Please place your order in the form of order number, quantity. E.g. '23, 7'")
     end
+    it { is_expected.to respond_to(:take_order) }
+    it 'gets an order' do
+      allow($stdin).to receive(:gets).and_return('foo')
+      test_order = $stdin.gets
+      expect(test_order).to eq('foo')
+    end
   end
 
   describe "#checks total" do
