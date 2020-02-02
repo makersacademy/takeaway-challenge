614c015d858687a7364e93ad1eb9fd0f27cec167
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-02 21:29:34 +0000

    Commented out both checkout and checkout_spec and moving checkout functionailty to Takeaway to reduce complexity

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,15 +1,17 @@
-require "checkout.rb"
-describe Checkout do
+# require "checkout.rb"
+# describe Checkout do
   
-  describe " #checkout" do
-    it "should take one argument" do
-      expect(subject).to respond_to(:checkout).with(1).argument
-    end
+#   describe " #checkout" do
+#     it "should take one argument" do
+#       expect(subject).to respond_to(:checkout).with(1).argument
+#     end
 
-    it "should return a string confirming a payment is made" do
-      amount = 10
-      expect(subject.checkout(amount)).to eq "Your payment of #{amount} has been made, thank you for your order"
-    end
-  end
+#     it "should return a string confirming a payment is made" do
+#       amount = 10
+#       expect(subject.checkout(amount)).to eq "Your payment of #{amount} has been made, thank you for your order"
+#     end
 
-end
+#     it "should raise an error if amount does not equal "
+#   end
+
+# end
