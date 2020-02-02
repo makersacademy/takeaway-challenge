96b0f71f7b41c4a39afff65e073fab4003bf0697
BarriF13 <info@bdesignstudio.co.uk>
2020-02-02 10:00:30 +0000

    Test the first method

---
+++ lib/take_away.rb
@@ -0,0 +1,5 @@
+class TakeAway
+    def menu
+        "Sausage: £5"
+    end
+end
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -1,1 +1,9 @@
-require 'take-away'
+require 'take_away'
+
+describe TakeAway do
+    subject(:takeAway) { described_class.new }
+
+    it "Shows the menu with list of dishes and prices" do
+        expect(takeAway.menu).to eq('Sausage: £5')
+    end
+end
