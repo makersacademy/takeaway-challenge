b7c4389e0154ecfa6041175e52c873d1c56336b9
hansa-lallu <hansalallu@gmail.com>
2020-02-02 11:37:12 +0000

    Checks that class exists

---
+++ spec/menu_spec.rb
@@ -0,0 +1,12 @@
+require 'menu'
+
+describe Menu do 
+
+  it 'checks that a class exists' do
+    expect(subject).to be_a Menu
+  end
+end 
+
+# As a customer
+# So that I can check if I want to order something
+# I would like to see a list of dishes with prices
