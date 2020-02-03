3d3ef7039526bcc0b94484a06035855338af7fad
Chris Eadie <chris.eadie@outlook.com>
2020-02-03 18:42:13 +0000

    Ensured Dish encapsulation with injection method in Menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,19 +4,19 @@ class Menu
   
   def initialize
     @menu = [
-      Dish.new("Cookies & Cream", 5.0),
-      Dish.new("Chocolate", 4.3),
-      Dish.new("Vanilla", 4.4),
-      Dish.new("Chocolate Chip", 5.3),
-      Dish.new("French Vanilla", 5.1),
-      Dish.new("Strawberry", 5.0),
-      Dish.new("Peanut Butter Cup", 5.0),
-      Dish.new("Cookie Dough", 5.0),
-      Dish.new("Neapolitan", 4.7),
-      Dish.new("Rocky Road", 9.0),
-      Dish.new("Vanilla Bean", 6.0),
-      Dish.new("Coffee", 3.2),
-      Dish.new("Chocolate Brownie", 5.0)
+      new_dish("Cookies & Cream", 5.0),
+      new_dish("Chocolate", 4.3),
+      new_dish("Vanilla", 4.4),
+      new_dish("Chocolate Chip", 5.3),
+      new_dish("French Vanilla", 5.1),
+      new_dish("Strawberry", 5.0),
+      new_dish("Peanut Butter Cup", 5.0),
+      new_dish("Cookie Dough", 5.0),
+      new_dish("Neapolitan", 4.7),
+      new_dish("Rocky Road", 9.0),
+      new_dish("Vanilla Bean", 6.0),
+      new_dish("Coffee", 3.2),
+      new_dish("Chocolate Brownie", 5.0)
     ]
   end
   
@@ -28,5 +28,11 @@ class Menu
   
   def item(number)
     @menu[number - 1]
+  end
+
+  private
+
+  def new_dish(name, price)
+    Dish.new(name, price)
   end
 end
