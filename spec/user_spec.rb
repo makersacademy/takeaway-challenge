ff8c18353522d9783c5ba889289f551affe22f3d
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-03 18:33:26 +0000

    Added empty User class

---
+++ spec/user_spec.rb
@@ -0,0 +1,12 @@
+require 'user'
+
+describe User do
+
+  # let (:user) { User.new }
+  
+  # describe "#menu" do
+  #   it "displays the full menu to user" do
+  #     expect(user.menu).to eq(@menu)
+  #   end
+  # end
+end
