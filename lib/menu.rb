b9ff09f5b23ecfa6a7f9f684646685414f245ce5
lilawalker <lilawlker@gmail.com>
2020-02-29 11:17:21 +0000

    Add menu items and method to view menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,51 +1,53 @@
 class Menu
 
-  def initialize
-    @menu = { 
-      burger: { 
-        name: "burger", 
-        price: 10.00 
-      },
-      cheese_burger: { 
-        name: "cheese_burger", 
-        price: 11.00 
-      },
-      chicken_burger: { 
-        name: "chicken_burger", 
-        price: 11.00 
-      },
-      sietan_burger: { 
-        name: "seitan_burger", 
-        price: 12.50 
-      },
-      fries: { 
-        name: "fries", 
-        price: 4.00 
-      },
-      chunky_chips: { 
-        name: "chunky chips", 
-        price: 4.50 
-      },
-      coleslaw: { 
-        name: "coleslaw", 
-        price: 3.00 
-      },
-      chicken_wings: { 
-        name: "chicken wings", 
-        price: 6.50 
-      },
-      cola: { 
-        name: "cola", 
-        price: 2.50 
-      },
-      fanta: { 
-        name: "fanta", 
-        price: 2.50 
-        }
-      }
-  end
+  ITEMS = {
+    "burger" => {
+      name: "burger",
+      price: 10.00
+    },
+    "cheese burger" => {
+      name: "cheese burger",
+      price: 11.00
+    },
+    "chicken burger" => {
+      name: "chicken burger",
+      price: 11.00
+    },
+    "sietan burger" => {
+      name: "seitan burger",
+      price: 12.50
+    },
+    "fries" => {
+      name: "fries",
+      price: 4.00
+    },
+    "chunky chips" => {
+      name: "chunky chips",
+      price: 4.50
+    },
+    "coleslaw" => {
+      name: "coleslaw",
+      price: 3.00
+    },
+    "chicken wings" => {
+      name: "chicken wings",
+      price: 6.50
+    },
+    "cola" => {
+      name: "cola",
+      price: 2.50
+    },
+    "fanta" => {
+      name: "fanta",
+      price: 2.50
+    },
+  }
 
   def view
+    items = ITEMS.map do |_, item_details|
+      "#{item_details[:name]} #{item_details[:price]}\n"
+    end
+    puts items.join(" ")
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,14 +1,29 @@
 require 'menu'
 
 describe Menu do
-  
+
   subject(:menu) { described_class.new }
-  let(:name) { double :name }
-  let(:price) { double :price }
-  let(:dish) { { name: name, price: price } }
 
   describe "#view" do
     it { is_expected.to respond_to(:view) }
+
+    let(:stubbed_items) do
+      {
+        "burger" => {
+          name: "burger",
+          price: 10.00
+        },
+        "cheese burger" => {
+          name: "cheese burger",
+          price: 11.00
+        },
+      }
+    end
+
+    it "displays the formatted menu" do
+      stub_const("Menu::ITEMS", stubbed_items)
+      expect { menu.view }.to output { "burger 10.0\n cheese burger 11.0" }.to_stdout
+    end
   end
 
 end
