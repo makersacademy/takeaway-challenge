88134d5697c1b2f1ef62dd4c03d6a1232175edea
Sophia Bell <Kittaru87@gmail.com>
2020-02-29 15:12:56 +0000

    printing menu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,16 +1,20 @@
 require 'takeaway'
+require 'menu'
 
 describe Takeaway do
+  # subject(:takeaway) {described_class.new}
+  let (:menu) { double(:menu, print_menu: printed_menu) }
 
-  let (:menu) { double(:menu, print: printed_menu) }
-    
+  let (:printed_menu) { "Chicken: £5" }
     
   it "initializes with an empty basket" do
     expect(subject).to have_attributes(basket: {})
   end    
 
-#   it "initializes with the menu" do
-#   expect(subject).to receive(menu)
-#   end
+  it 'prints the menu' do
+    # menu.add_dish("chicken", 5)
+    expect(subject.print_menu).to eq(printed_menu)
+  end
+
 
 end 
