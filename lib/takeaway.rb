958e4eff97af0ea75ec92b46b29f341c15ec2e1b
Sophia Bell <Kittaru87@gmail.com>
2020-02-29 19:58:13 +0000

    adding print out of order and total, some un finished methods for checking correct order amount

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,22 +1,40 @@
 require_relative 'menu'
+require_relative 'order'
 
 class Takeaway
 
-  attr_reader :basket, :menu
+  attr_reader :menu, :order
 
-  def initialize(menu = Menu.new)
-    @basket = {}
-    @menu = menu
+  def initialize(menu, order = Order.new(menu))
+    @order = order
   end
 
   def print_menu
     @menu.show_menu
   end
 
+  def place_order(dish, quantity)
+    @order.adds_item(dish, quantity)
+    print_order
+    # complete_order()
+  end
 
+  def complete_order(price)
+    correct_amount?(price) 
+  end
 
+  private
 
+  def correct_amount?(price)
+    @order.total == price
+  end
 
+  def print_order
+    puts "Order\n----" 
+    @order.order.each do |dish, quantity|
+      puts "#{dish}: #{quantity}"
+    end
+    puts "Total: £#{@order.total}"
+  end
 
-  
 end
