2cecda4661a0b5f89fbf03eccd97c25df4a47a1d
Sophia Bell <Kittaru87@gmail.com>
2020-02-29 15:45:47 +0000

    adding tests for totals of items: quit exirt exit

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -10,12 +10,12 @@ subject(:order) { described_class.new(menu) }
 
   let(:menu) { instance_double("Menu") }
 
-#   let (:items) do 
-#     { 
-#       chicken: 2, 
-#       fish:  1
-#     }
-#   end
+  let (:items) do 
+    { 
+      chicken: 2, 
+      fish:  1
+    }
+  end
 
   it "initializes with an empty order hash" do
     expect(subject).to have_attributes(items = {})
@@ -38,5 +38,26 @@ subject(:order) { described_class.new(menu) }
 
   end
 
+  describe '#total' do
+
+    before do 
+      allow(menu).to receive(:has_dish?).with(:duck).and_return(true)
+      allow(menu).to receive(:price).with(:duck).and_return(10)
+      allow(menu).to receive(:has_dish?).with(:waffles).and_return(true)
+      allow(menu).to receive(:price).with(:waffles).and_return(8)
+    end
+
+    it "adds the total of multiples of one dish" do
+      order.adds_item(:duck, 2)
+      expect(subject.total_per_item).to eq [20]
+    end
+
+    it "adds the total price of the dishes" do
+      order.adds_item(:duck, 2)
+      order.adds_item(:waffles, 1)
+      total = 28
+      expect(subject.total).to eq 28
+    end
+  end
   
 end
