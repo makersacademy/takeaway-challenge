046d590726a39ff5be8b8c0c427714db6c069fe3
john-baxter <john.g.baxter@googlemail.com>
2020-02-29 22:13:31 +0000

    Add tests for checking the menu and verifying the menu contents, and a method to print the menu.

--- lib/food_list.rb
+++ lib/food_list.rb
@@ -1,10 +1,14 @@
+# to require or load
+# require '~/Dropbox/makers_projects/makers_weekends/takeaway_challenge_we2-3/takeaway-challenge/lib/food_list.rb'
+# load '~/Dropbox/makers_projects/makers_weekends/takeaway_challenge_we2-3/takeaway-challenge/lib/food_list.rb'
+
 class FoodList
   CURRENT_MENU = {
-    "cheesey jalapeno bites" => 5.49,
-    "cheese and tomato pizza" => 11.99,
-    "dipping sauce" => 0.45,
-    "giant choc chip cookie" => 4.99,
-    "pepsi max" => 2.49}
+    "Cheesey jalapeño bites" => 5.49,
+    "Cheese and tomato pizza" => 11.99,
+    "Dipping sauce" => 0.45,
+    "Giant choc chip cookie" => 4.99,
+    "Pepsi max" => 2.49}
 
     attr_reader :current_menu
 
@@ -12,5 +16,9 @@ class FoodList
     @current_menu = CURRENT_MENU
   end
 
-
+  def print_menu
+    @current_menu.each { |key, value|
+    print "#{key}".ljust(30)
+    puts "£#{value}".rjust(7) }
+  end
 end
--- spec/food_list_spec.rb
+++ spec/food_list_spec.rb
@@ -5,7 +5,25 @@ describe FoodList do
     expect(subject).to be_a(FoodList)
   end
 
-  it "has a list of food and prices" do
+  it "has a menu" do
     expect(subject.current_menu).to be_a(Hash)
+  end
+
+  it "has a menu that is populated by food and prices" do
+    expect(subject.current_menu).to eq ({
+      "Cheesey jalapeño bites" => 5.49,
+      "Cheese and tomato pizza" => 11.99,
+      "Dipping sauce" => 0.45,
+      "Giant choc chip cookie" => 4.99,
+      "Pepsi max" => 2.49})
+  end
+
+  it "should show the list of food and prices" do
+    expected_menu_string = "Cheesey jalapeño bites          £5.49\n"+
+    "Cheese and tomato pizza        £11.99\n"+
+    "Dipping sauce                   £0.45\n"+
+    "Giant choc chip cookie          £4.99\n"+
+    "Pepsi max                       £2.49\n"
+    expect { subject.print_menu }.to output(expected_menu_string).to_stdout
   end
 end
