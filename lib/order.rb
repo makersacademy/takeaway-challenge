5d2e2d5770b3b548fed2d9f393ce0b890cbf01f7
Jara Santamaria <jarasmar@gmail.com>
2020-02-29 14:34:31 +0000

    Add prices from selected dishes to array

--- lib/order.rb
+++ lib/order.rb
@@ -2,17 +2,18 @@ require_relative 'menu'
 
 class Order
 
-  attr_reader :current_order, :pay_amount
+  attr_reader :current_order, :prices_to_sum
 
   def initialize
     @current_order = []
-    @pay_amount = 0
+    @prices_to_sum = []
   end
 
   def add_dish(dish)
     fail "Sorry, that dish is not available" if existent_dish?(dish) == false
 
     @current_order << dish
+    @prices_to_sum << $menu_list[dish]
   end
 
   def existent_dish?(dish)
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,23 +7,35 @@ describe Order do
     expect(order.current_order).to eq []
   end
 
-  it "initializes with a @total_price of 0" do
-    expect(subject.pay_amount).to eq 0
+  it "initializes with an empty @prices_to_sum" do
+    order = Order.new
+    expect(order.prices_to_sum).to eq []
   end
 
-  it "can #add_dish to @current_order" do
-    order = Order.new
-    order.add_dish("Cheese Burger")
-    expect(order.current_order).to include("Cheese Burger")
+  describe "#add_dish" do
+
+    it "raises an error if #add_dish and it doesn't exist" do
+      order = Order.new
+      expect { order.add_dish("omelette") }.to raise_error("Sorry, that dish is not available")
+    end
+
+    it "can #add_dish to @current_order" do
+      order = Order.new
+      order.add_dish("Cheese Burger")
+      expect(order.current_order).to include("Cheese Burger")
+    end
+
+    it "can add the price of the dish to @prices_to_sum" do
+      order = Order.new
+      order.add_dish("Cheese Burger")
+      expect(order.prices_to_sum).to include(5)
+    end
   end
 
-  it "can check if #existent_dish in @menu" do
-    order = Order.new
-    expect(order.existent_dish?("Cheese Burger")).to eq true
-  end
-
-  it "raises an error if #add_dish and it doesn't exist" do
-    order = Order.new
-    expect { order.add_dish("omelette") }.to raise_error("Sorry, that dish is not available")
+  describe "#existent_dish?" do
+    it "can check if #existent_dish in @menu" do
+      order = Order.new
+      expect(order.existent_dish?("Cheese Burger")).to eq true
+    end
   end
 end
