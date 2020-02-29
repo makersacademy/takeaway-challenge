f32dd7ddf8b1bda6bbbb7097c2c846f748e01801
Sophia Bell <Kittaru87@gmail.com>
2020-02-29 19:59:21 +0000

    adding to make sure

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,11 +2,11 @@ require 'menu'
 
 describe Menu do
 
-  let(:dish){ double :dish} 
-  let(:price) { double :price}
+  let(:dish) { double :dish } 
+  let(:price) { double :price }
 
   it "initializes with an empty item hash" do
-    expect(subject).to have_attributes(items = {})
+    expect(subject).to have_attributes(items: {})
   end
 
   describe '#add_dish' do
@@ -20,10 +20,20 @@ describe Menu do
   describe "show_menu" do
   
     it "prints off the menu" do
-    subject.add_dish("chicken", 5.99)
-    expect{subject.show_menu}.to output("Takeaway Menu\nchicken: £5.99\n").to_stdout
+      subject.add_dish("chicken", 5.99)
+      expect { subject.show_menu }.to output("Takeaway Menu\n-------------\n1. Chicken: £5.99\n").to_stdout
     end
 
+  end
+
+  it "is true if there if item is on the menu" do
+    subject.add_dish("chicken", 5.99)
+    expect(subject.has_dish?("chicken")).to eq true
+  end
+
+  it "shows the price of an item on the menu" do
+    subject.add_dish("chicken", 5.99)
+    expect(subject.price("chicken")).to eq 5.99
   end
 
 end
