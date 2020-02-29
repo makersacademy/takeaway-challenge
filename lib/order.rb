5a6edc8ba25cfb20868b1f39a63a764d10bbcc2a
lilawalker <lilawlker@gmail.com>
2020-02-29 13:09:14 +0000

    Add method to add dish to order

--- lib/order.rb
+++ lib/order.rb
@@ -2,12 +2,23 @@ require_relative 'menu'
 
 class Order
 
+  attr_reader :dishes
+
   def initialize(menu = Menu.new)
     @menu = menu
+    @dishes = []
   end
 
   def view_menu
     @menu.view
+  end
+
+  def add(dish, quantity)
+    fail "Quantity not provided. Please try again." unless quantity.is_a?(Integer)
+
+    fail "Dish not available. Please try again." unless Menu::ITEMS.has_key?(dish)
+
+    @dishes << { dish: dish, quantity: quantity }
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,12 +6,27 @@ describe Order do
   subject(:order) { described_class.new }
 
   describe "#view_menu" do
-
     it { is_expected.to respond_to(:view_menu) }
 
     it "displays the menu" do
       expect { order.view_menu }.to output { /burger 10.0/ }.to_stdout
     end
+  end
 
+  describe "#add" do
+    it { is_expected.to respond_to(:add).with(2).arguments }
+
+    it "raises error if dish not available" do
+      expect { order.add("pizza", 1) }.to raise_error "Dish not available. Please try again."
+    end
+
+    it "raises error if no quantity provided" do
+      expect { order.add("burger", "1") }.to raise_error "Quantity not provided. Please try again."
+    end
+
+    it "adds the dish and quantities to the order" do
+      order.add("burger", 2)
+      expect(order.dishes).to eq [{ dish: "burger", quantity: 2 }]
+    end
   end
 end
