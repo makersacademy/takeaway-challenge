c91b43d0ec08f4b838256b5992dc4f71bc1a6daa
Student <student@Makerss-MacBook-Air.local>
2020-02-29 18:57:15 +0000

    Customer class now has .show_menu and .show_order with passing tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,16 @@
 class Customer
+
+  def initialize(menu = Menu.new, order = Order.new)
+    @menu = menu
+    @order = order
+  end
+
+  def show_menu
+    @menu.display
+  end
+
+  def show_order
+    @order.display
+  end
+
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,4 +2,31 @@ require 'customer'
 
 describe Customer do
 
+  # let(:pizza) { double :pizza, price: 10.50 }
+  # let(:curry) { double :curry, price: 8.00 }
+  # let(:burger) { double :curry, price: 6.50 }
+
+  let(:menu) { double :menu }
+  let(:order) { double :order }
+
+  subject(:customer) { described_class.new(menu, order) }
+  # dishes: [pizza, curry, burger]
+
+  # I would like to see a list of dishes with prices
+  describe '.show_menu' do
+    it { is_expected.to respond_to :show_menu }
+    it '#calls display method in menu class' do
+      expect(menu).to receive(:display)
+      customer.show_menu
+    end
+  end
+
+  describe '.show_order' do
+    it { is_expected.to respond_to :show_order }
+    it '#calls display method in order class' do
+      expect(order).to receive(:display)
+      customer.show_order
+    end
+  end
+
 end
