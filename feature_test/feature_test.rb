6d8924c3580e961a1eefcc334f1d26e71292324b
Nimzyow <n_soufiani@hotmail.com>
2020-03-01 22:39:49 +0000

    reordered logic based on irb feature test, added to gitignore, tested and created message class.

--- feature_test/feature_test.rb
+++ feature_test/feature_test.rb
@@ -1,4 +1,9 @@
 require "../lib/takeaway.rb"
 require "../lib/menu.rb"
+require "../lib/default_menu_list.rb"
+require "../lib/checkout.rb"
+require "../lib/basket.rb"
+require "../lib/message.rb"
 
-takeaway = Takeaway.new
+name = "Dishoom"
+takeaway = Takeaway.new(name: name)
