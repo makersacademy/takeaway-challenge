150a0a030c7f29f8dcb1754a40ad8b38ece5d17a
WilliamJGrace <williamjgrace@outlook.com>
2020-03-01 10:17:50 +0000

    added see_menu method, test passed

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -1,3 +1,20 @@
 class Dishes
+  attr_reader :menu
+  def initialize
+    @menu = [{item: "Pizza", price:7},
+      {item:"Fried Rice", price: 3 },
+      {item: "Burger and Chips", price: 7}
 
+
+
+      ]
+  end
+
+  def see_menu
+    @menu.each { |item|
+      puts "#{item[:item]} - £#{item[:price].to_s}"
+
+
+    }
+  end
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -2,8 +2,13 @@ require 'dishes'
 
 
 describe Dishes do
-
-
+  let(:dishes) {Dishes.new}
+  it "initializes with a list a of dishes" do
+    expect(dishes.menu).not_to be_empty
+  end
+  it "shows the user the menu items" do
+    expect{dishes.see_menu}.to output(String).to_stdout
+  end
 
 
 end
