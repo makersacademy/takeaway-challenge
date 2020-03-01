d0f8885dd6196bb5695e8d26c9db8ba371d2b4ac
Jara Santamaria <jarasmar@gmail.com>
2020-03-01 20:40:22 +0000

    Test refactoring for complete Rspec Mocking

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,10 +4,18 @@ require_relative 'message'
 
 class Restaurant
 
-  def initialize
-    @menu = Menu.new
-    @order = Order.new
-    @message = Message.new
+  # WHICH IS THE BEST WAY FOR INITIALIZING?
+  #
+  # def initialize
+  #   @menu = Menu.new
+  #   @order = Order.new
+  #   @message = Message.new
+  # end
+
+  def initialize(menu: Menu.new, order: Order.new, message: Message.new)
+    @menu = menu
+    @order = order
+    @message = message
   end
 
   def see_menu
@@ -20,7 +28,7 @@ class Restaurant
 
   def checkout
     @order.finish_order
-    # @message.send_message # (IT BREAKS THE TESTS AS TWILIO IS NOT WORKING)
+    @message.send_message 
   end
 
   # CALL FOR LOOP IN ORDER CLASS
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,25 +1,65 @@
 require 'restaurant'
 
 describe Restaurant do
-  it "can show a menu" do
-    menu = double("menu")
-    expect { subject.see_menu }.to output { menu.print_menu }.to_stdout
+  # doubles for Restaurant class. (Possible to add all variables and methods)
+  # let(:menu_class) { double("menu", menu_list: menu_list, print_menu: "print menu") }
+  let(:menu_class) { double("menu") }
+  let(:order_class) { double("order") }
+  let(:message_class) { double("message") }
+
+  # initialize subject with doubles
+  subject { described_class.new({ menu: menu_class, order: order_class, message: message_class }) }
+
+  describe "#initialize" do
+
+    it "initializes with an instance of menu" do
+      expect(subject.instance_variable_get(:@menu)).to eql(menu_class)
+    end
+
+    it "initializes with an instance of order" do
+      expect(subject.instance_variable_get(:@order)).to eql(order_class)
+    end
+
+    it "initializes with an instance of message" do
+      expect(subject.instance_variable_get(:@message)).to eql(message_class)
+    end
   end
 
-  # COULDNT MANAGE TO MAKE THIS TEST PASS
+  describe "subject responds to method" do
 
-  # it "can #place_order" do
-  #   subject.stub(:place_order).with("Milkshake", "1")
-  #   expect(:current_order).to include("Milkshake")
-  # end
+    it "responds to #see_menu" do
+      expect(subject).to respond_to(:see_menu)
+    end
 
-  it "shows a ticket at #checkout" do
-    order = double("order")
-    expect { subject.checkout }.to output { order.finish_order }.to_stdout
+    it "responds to #place_order" do
+      expect(subject).to respond_to(:place_order)
+    end
+
+    it "responds to #checkout" do
+      expect(subject).to respond_to(:checkout)
+    end
   end
 
-  # it "send a confirmation message at #checkout" do
-  #   message = double("message")
-  #   expect { subject.checkout }.to #send_text (LOOK FOR SYNTAX)
-  # end
+  describe "method functionality" do
+
+    it "#see_menu delegates on Menu.print_menu" do
+      allow(menu_class).to receive(:print_menu).and_return("menu")
+      expect(menu_class).to receive(:print_menu).once
+      subject.see_menu
+    end
+
+    it "#place_order delegates on Order.add_dish" do
+      allow(order_class).to receive(:add_dish).and_return("Cheese Burger", 1)
+      expect(order_class).to receive(:add_dish).once
+      subject.place_order("Cheese Burger", 1)
+    end
+
+    it "#checkout delegates in Order.finish_order and Message.send_message" do
+      allow(order_class).to receive(:finish_order).and_return("Receipt")
+      allow(message_class).to receive(:send_message).and_return("Confirmation SMS")
+      expect(order_class).to receive(:finish_order).once
+      expect(message_class).to receive(:send_message).once
+      subject.checkout
+    end
+  end
 end
