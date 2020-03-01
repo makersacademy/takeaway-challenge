4dc290b6f298232cc12293ef1f08132f48ecd461
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-03-01 10:22:25 +0000

    menu class passes first test

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,4 +1,14 @@
 require "menu"
 
 describe Menu do
+
+  subject(:menu) { described_class.new(dishes) }
+
+  let(:dishes) do 
+    { margarita: 5.00, sandwich: 2.99 }
+  end
+
+  it "list of dishes and their prices" do
+    expect(menu.dishes).to eq(dishes)
+  end
 end
