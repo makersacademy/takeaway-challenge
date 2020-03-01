db27384cc195bbc2cf85b5c3f508f1088e7b4cfd
Student <student@Makerss-MacBook-Air.local>
2020-03-01 18:17:10 +0000

    If quantity in order is < 1 it will not appear in order

--- lib/order.rb
+++ lib/order.rb
@@ -8,7 +8,7 @@ class Order
 
 # Improvements:
   # make it possible for items to be removed from order (.remove)
-  # make .add method more succinct
+  # create an .update_total (private) that recalculates @total using @order
 
   def initialize(menu = Menu.new)
     @order = []
@@ -30,9 +30,16 @@ class Order
     price = @menu.menu[dish_num - 1][:price]
     dish = @menu.menu[dish_num - 1][:dish]
 
-    @total += price * quantity
+    @total += price * quantity # might be better to create a method that calculates @total
     update_order dish, price, quantity
     # work out if this dish is already part of the order
+  end
+
+  def remove(dish_num, quantity)
+    # find price and dish
+    # update @total to remove this amount from total
+    # call update_order
+    # if a method that calculates @total is created, call it here rather than step above
   end
 
   def confirmed(phone_number)
@@ -52,6 +59,7 @@ class Order
   end
 
   def update_order dish, price, quantity
+    # add checks here for whether quantity is < 1. If it is, delete from @order
     added = false
     @order.map do |item|
       if item[:dish] == dish
@@ -61,6 +69,7 @@ class Order
       end
     end
     @order.push({ dish: dish, quantity: quantity, amount: price * quantity }) unless added
+    @order.select! { |item| item[:quantity] > 0}
   end
 
 end
