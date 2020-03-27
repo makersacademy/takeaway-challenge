17d3e184c710216f7f9e53da4b655e6421129b1f
Hodan <hodanahmed0000@gmail.com>
2020-03-27 14:56:36 +0000

    Failing test

--- spec/burgerKingSpec.js
+++ spec/burgerKingSpec.js
@@ -8,9 +8,9 @@ describe('Burgerking', function(){
   });
 //Testing user story 1: Can see the menu of dishes and prices
   it('can see list of dishes with prices',function(){
-    expect(burgerKing.getMenu()).toEqual({"Whopper":4.49,"Chicken Royale":4.49,"Prawn burger":3.79})
+    expect(burgerKing.getMenu()).toEqual({ Whopper: 4.49, ChickenRoyale: 4.49, PrawnBurger: 3.79 })
   });
   it('can order a dish on the menu',function(){
-    expect(burgerKing.orderDish("Whopper")).toContain("Whopper")
+    expect(burgerKing.orderDish("Lemonade")).toEqual("Lemonade is not on the menu.")
   });
   });
--- src/burgerKing.js
+++ src/burgerKing.js
@@ -1,13 +1,18 @@
 class Burgerking {
   constructor(){
-    this.dishes = {"Whopper":4.49,"Chicken Royale":4.49,"Prawn burger":3.79};
+    this.menu = new Object();
+    this.menu["Whopper"] = 4.49
+    this.menu["ChickenRoyale"] = 4.49
+    this.menu["PrawnBurger"] = 3.79
   }
 
 getMenu () {
-  return this.dishes
-}
+  return this.menu
+};
 
 orderDish (dishname) {
-  return "You have ordered" + " " + "the" + " " + dishname +"." 
-}
+  for(const x in this.menu) {
+      console.log(`${x}: ${this.menu[x]}`);
+    }
+  };
 };
