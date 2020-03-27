ed703bd6145891385411e6e01e4988a580f7d85f
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-27 10:05:56 +0000

    dishes class working

---
+++ src/dishes.js
@@ -0,0 +1,20 @@
+class Dishes {
+  constructor() {
+    this.allDishes = {
+      "Americano": 2.50,
+      "Cappuccino": 3,
+      "Espresso": 1.50,
+      "Flat White": 2.70,
+      "Custard Doughnut": 4.00,
+      "Chocolate Sprinkle Doughnut": 3.50,
+      "Smoked Salmon and Cream Cheese Bagel": 5.00,
+      "Avocado and Scrambled Egg Bagel": 5.00,
+      "Vegan BLT Bagel": 5.50,
+      "Chef's Special Seasonal Salad": 7.00,
+    };
+  };
+
+  all() {
+    return this.allDishes;
+  };
+};
