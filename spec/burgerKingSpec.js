6ae72547290d1c735108b3593739498556cbaf7b
Hodan <hodanahmed0000@gmail.com>
2020-03-27 10:09:33 +0000

    Set up JS environment and first failing instance test

---
+++ spec/burgerKingSpec.js
@@ -0,0 +1,6 @@
+describe('Burgerking', function(){
+  var burgerKing;
+  it('can make an instance of Burgerking',function(){
+  expect(burgerKing = new Burgerking).toBeInstanceOf(Burgerking)
+  });
+  });
