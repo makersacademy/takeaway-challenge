7a2ec7867f32a45e4e62e6d6c335422273934228
Ariane <xu.ariane@hotmail.fr>
2020-03-27 11:09:11 +0000

    displays prices failing test

--- spec/menuSpec.js
+++ spec/menuSpec.js
@@ -9,10 +9,17 @@ describe('Menu', function() {
 
 
   describe('viewing the menu', function() {
+
     it('shows a list of the dishes', function() {
       expect(menu.getDishes()).toEqual({});
     });
 
+    it('displays the price of the dishes', function() {
+      dishes = {mozzarella:5, burger:7, fries:3}
+      menu.displayDishes(dishes);
+      expect(menu.getDishes()).toEqual(dishes);
+    });
+
   });
-  
+
 });
