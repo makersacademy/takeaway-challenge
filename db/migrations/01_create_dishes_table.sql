76ad9463fa9d707579fbc995d1c1978cb9fd317a
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-27 11:10:26 +0000

    db set up with values

--- db/migrations/01_create_dishes_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dishes (
-  id SERIAL PRIMARY KEY,
-  dishname VARCHAR(60),
-  dishdescr VARCHAR(280),
-  dishprice NUMERIC (6,2)
-);
---
+++ db/migrations/migrations.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE menu;
+
+-- to create dishes table in menu database
+
+CREATE TABLE dishes (
+  id SERIAL PRIMARY KEY,
+  dishname VARCHAR(60),
+  dishdescr VARCHAR(280),
+  dishprice NUMERIC (6,2)
+);
+
+-- to insert values
+
+insert into dishes (dishname, dishdescr, dishprice) VALUES ('Pizza - Margherita', 'cheese and tomato pizza', '7.99');
+insert into dishes (dishname, dishdescr, dishprice) VALUES ('Pizza - Four Cheese', 'lots of cheese', '8.99');
+insert into dishes (dishname, dishdescr, dishprice) VALUES ('Pizza - Pepperoni', 'lots of cheese', '8.99');
+insert into dishes (dishname, dishdescr, dishprice) VALUES ('Pizza - Fiorentina', 'spinach and egg', '8.50');
+insert into dishes (dishname, dishdescr, dishprice) VALUES ('Garlic bread', 'mmm yummy garlic', '3.50');
+insert into dishes (dishname, dishdescr, dishprice) VALUES ('Cheesy garlic bread', 'gooey cheese', '3.50');
+insert into dishes (dishname, dishdescr, dishprice) VALUES ('Raspberry cheesecake', 'best dessert ever', '4.50');
