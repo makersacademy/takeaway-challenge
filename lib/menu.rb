b2eb5a577a8f7c5180d20535249442f9131153b9
Alexa <alexakearns@outlook.com>
2020-03-28 11:32:47 +0000

    menu add_item to list - red/green

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,14 @@
+require_relative 'menu_item'
+
 class Menu
-  
+
   attr_reader :list
 
   def initialize
     @list = []
+  end
+
+  def add_item(item)
+    @list << item
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,11 +1,20 @@
 require 'menu'
 
 describe Menu do
-
+  
+  
   context 'new menu contains empty list' do
 
     it 'menu has empty list(array)' do
       expect(subject.list).to eq []
     end
+  end
+
+  context 'adds menu item to list' do
+
+    it 'adds menu item to list' do
+      subject.add_item("item")
+      expect(subject.list[-1]).to eq "item"
+    end 
   end
 end
