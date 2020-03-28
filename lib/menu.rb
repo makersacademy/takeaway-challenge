e872b5604081e4faa23125953c1a5453dc36862f
Harry Turnbull <hturnbull@live.co.uk>
2020-03-28 09:30:42 +0000

    provide_dish returns Pie

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,4 +2,8 @@ class Menu
   def view
     "1. Pie - £6\n2. Mash - £4\n3. Chips - £5"
   end
+
+  def provide_dish(dish_number)
+    Pie.new
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,15 @@
 require 'menu'
 
 describe Menu do
-  it 'should display a list of dishes with prices' do
-    expect(subject.view).to eq "1. Pie - £6\n2. Mash - £4\n3. Chips - £5"
+
+  describe '#view' do
+    it 'should display a list of dishes with prices' do
+      expect(subject.view).to eq "1. Pie - £6\n2. Mash - £4\n3. Chips - £5"
+    end
+  end
+  describe '#provide_dish' do
+    it 'returns a Pie object when passed 1' do
+      expect(subject.provide_dish(1)).to be_kind_of(Pie)
+    end 
   end
 end
