cfd4cc92006a87615688790637b135befce6ddc1
tommyrharper <tommyrharper@gmail.com>
2020-03-28 15:49:51 +0000

    test failed - menu contains an array of dishes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,12 @@
 require 'menu'
 
 describe Menu do
+
   it 'returns a list of dishes on #list' do
     expect(subject.list).to eq "Chips: £3 \nFish: £5\nSalad: £2"
+  end
+
+  it 'contains an array of dishes' do
+    expect(subject.dishes).to eq [{1 => Chips}, {2 => Fish}, {3 => Salad}]
   end
 end
