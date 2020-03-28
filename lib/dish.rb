f2fb5c880f98f8e62fa83e2495c8f1f6742e9c7a
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-28 08:29:15 +0000

    Add guard cases to Dish initialize method
    The guards check for a price less than 0 and for an empty name,
    each throwing an ArgumentError.

--- lib/dish.rb
+++ lib/dish.rb
@@ -2,6 +2,9 @@ class Dish
   attr_reader :name, :price
 
   def initialize(name, price)
+    raise ArgumentError, 'dish name must not be empty' if name.empty?
+    raise ArgumentError, 'dish price must be greater than 0' if price <= 0
+
     @name = name
     @price = price
   end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -12,4 +12,12 @@ describe Dish do
   it 'set the price when you create it' do
     expect(dish.price).to eq(price)
   end
+
+  it 'errors if created with an empty string for the name' do
+    expect { Dish.new("", price) }.to raise_error(ArgumentError, 'dish name must not be empty')
+  end
+
+  it 'errors if created with a price <= 0' do
+    expect {  Dish.new(name, 0) }.to raise_error(ArgumentError, 'dish price must be greater than 0')
+  end
 end
