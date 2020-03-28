040c338f93f8269cd98f85fbaf9253c9394ad475
Angelica <Angelica137@users.noreply.github.com>
2020-03-28 16:05:09 +0000

    the user can choose an item from the list

--- lib/list.rb
+++ lib/list.rb
@@ -1,5 +1,16 @@
 class List
+  attr_reader :menu
+  def initialize
+    @menu = { 'apple' => 3 }
+  end
   def see
-    menu = { 'apple' => 3 }
+    @menu
+  end
+  def method_missing(m, *args, &blk)
+    if @menu.has_key?(m.to_s)
+      @menu
+    else
+      'We do not have that on the menu'
+    end
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,4 +1,5 @@
 require 'list'
+require 'meal'
 
 describe List do
   it { is_expected.to respond_to :see }
@@ -6,5 +7,12 @@ describe List do
   it 'shows dish price pairs' do
     menu = { 'apple' => 3 }
     expect(subject.see).to eq(menu)
+  end
+
+  it 'lets the user choose a number of a dish' do
+    menu = {
+      'apple' => 3
+    }
+    expect(subject.apple).to eq(menu)
   end
 end
