d7b8f340bbc2c930f8991b8c9b09370988d02841
Alexa <alexakearns@outlook.com>
2020-03-28 10:32:53 +0000

    menu item has name test passed - green

--- lib/menu_item.rb
+++ lib/menu_item.rb
@@ -1,4 +1,9 @@
 class MenuItem
-
   
+  attr_reader :name
+
+  def initialize(name:)
+    @name = name
+  end
+
 end
--- spec/menu_item_spec.rb
+++ spec/menu_item_spec.rb
@@ -2,6 +2,8 @@ require 'menu_item'
 
 describe MenuItem do
 
+  subject {MenuItem.new(name: "American Hot")}
+
   context 'new item name and price' do
     
     it 'menu item has a name' do
