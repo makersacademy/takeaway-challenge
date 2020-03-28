dcf8357b5527f56942d55dedb5d21373e277e356
Alex L <macbookpro@macbooks-mbp.home>
2020-03-28 12:24:55 +0000

    show_menu method & test

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,13 @@
 class Order
 
+  attr_reader :show_menu
+  def initialize
+    @show_menu = [{one: "Garlic Bread with cheese", price: 6},
+                  {two:"Pepperoni", price: 12},
+                  {three: "Hawaiian", price: 11}
+                  ]
+  end
+  #def show_menu
+
+  #end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,12 @@
 require 'order'
 
 describe Order do
+  it "responds to #show_menu" do
+    expect(subject).to respond_to :show_menu
+  end
 
+  # Need to think about the description here.
+  it "#show_menu prints the menu" do
+    expect(subject.show_menu).to be_instance_of Array
+  end
 end
