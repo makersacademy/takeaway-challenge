e6eebbf795d087dc08f235e36b1ccffe6cb7bbe0
Alexa <alexakearns@outlook.com>
2020-03-28 10:57:55 +0000

    rubocop

--- spec/menu_item_spec.rb
+++ spec/menu_item_spec.rb
@@ -2,16 +2,16 @@ require 'menu_item'
 
 describe MenuItem do
 
-  subject {MenuItem.new(name: "American Hot", price: 8.99)}
+  subject { MenuItem.new(name: "American Hot", price: 8.99) }
 
   context 'new item name and price' do
     
     it 'menu item has a name' do
-      expect(subject.name).to eq ("American Hot")
+      expect(subject.name).to eq "American Hot"
     end
 
     it 'menu item has a price' do
-      expect(subject.price).to eq (8.99)
+      expect(subject.price).to eq 8.99
     end
   end
 end
