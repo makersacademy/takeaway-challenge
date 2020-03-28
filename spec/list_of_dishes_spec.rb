fa5242f72adf7c9338048ffadbeecfc67ae217d5
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-28 09:50:46 +0000

    Add error checks to ListOfDishes initialise method
    Initialize now checks for non-hashes and empty hashes.

--- spec/list_of_dishes_spec.rb
+++ spec/list_of_dishes_spec.rb
@@ -1,15 +1,18 @@
 require 'list_of_dishes'
 
 describe ListOfDishes do
-  let(:dishes) { double(:dishes) }
-
-  it 'see a list when its empty' do
-    list = ListOfDishes.new([])
-    expect(list.see).to eq([])
-  end
+  let(:dishes) { double(:dishes, instance_of?: Hash, empty?: false, each: true) }
 
   it 'see a list when there are dishes in it' do
     list = ListOfDishes.new(dishes)
     expect(list.see).to eq(dishes)
+  end
+
+  it 'must be created with a hash' do
+    expect { ListOfDishes.new([]) }.to raise_error(ArgumentError, 'argument must be a hash')
+  end
+
+  it 'must be created with at least one dish' do
+    expect { ListOfDishes.new({}) }.to raise_error(ArgumentError, 'at least one dish must be specified')
   end
 end
