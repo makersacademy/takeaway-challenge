e5315860c4745f6422881075f1a58a5595ce159a
Harry Turnbull <hturnbull@live.co.uk>
2020-03-28 17:24:45 +0000

    moved ONE_HOUR to private area

--- lib/order.rb
+++ lib/order.rb
@@ -7,28 +7,27 @@ class Order
     @basket = []
     @notification = notification
   end
-
-  ONE_HOUR = 60 * 60
-
+  
   def view_menu
     @menu.view
   end
-
+  
   def add(dish_number)
     @basket << @menu.provide_dish(dish_number)
   end
-
+  
   def basket
     BASKET_HEADER + basket_list + total
   end
-
+  
   def place
     delivery_time = calculate_delivery_time(Time.now)
     @notification.send(delivery_time)
   end
-
+  
   private
-
+  
+  ONE_HOUR = 60 * 60
   BASKET_HEADER = "Your order:\n"
 
   def calculate_delivery_time(time)
