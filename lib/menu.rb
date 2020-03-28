f1868597ada314c226b64dee3cfc5d2f1e994e6d
Gina Frankel <g.frankel22@gmail.com>
2020-03-28 17:15:24 +0000

    customers can order the meal they want user can choose food items and select how much of each items they need from menu (#choose); the total is calculated (#total); the customers order is printed (#print order)

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,44 @@
 class Menu
-    def initialize(menu)
-      @menu = menu
-    end
-    def look_at_menu
-       @menu.each do |k,v|
-          v = sprintf '%.2f' % v
-          puts   "#{k}: £#{v}"
-       end 
+attr_reader :order, :total 
+  def initialize(menu)
+    @menu = menu
+    @order = []
+    
+  end
+
+  def look_at_menu
+    @menu.each do |k,v|
+      v = sprintf '%.2f' % v
+      puts  "#{k}: £#{v}"
     end 
   end
+
+  def choose(food_input,amount_input)
+    food_input = food_input.to_sym
+    @menu.each do |food, money|
+      if food_input == food
+        @order << {food: food_input, price: money*amount_input, amount: amount_input}
+      end 
+    end 
+  end
+
+  def total
+    total = 0
+    #@order[0][:price] + @order[1][:price]
+    @order.each do |hash|
+      total += hash[:price]
+    end 
+    @total = total
+  end 
+
+  def prints_order
+    @order.each do |hash|
+      puts "food: #{hash[:food]}, amount: #{hash[:amount]}, price: £#{hash[:price]}"
+    end 
+    puts "£#{@total}"
+  end 
+    
+
+end 
+    
+
