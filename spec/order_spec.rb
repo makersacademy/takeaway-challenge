3959d5ead70a9beb4c55871ceffd23f7b61ae013
Harry Turnbull <hturnbull@live.co.uk>
2020-03-28 12:11:25 +0000

    wrapped tests in describe block

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,14 +1,17 @@
 require 'order'
 
 describe Order do
-  it 'adding a dish to the order shows that dish in the order basket' do
-    subject.add(1)
-    expect(subject.basket).to eq "Your order:\n1x Pie (£6) - £6\nTotal: £6"
-  end
 
-  it 'adding a two dishes to the order shows both dishes in the order basket' do
-    subject.add(1)
-    subject.add(2)
-    expect(subject.basket).to eq "Your order:\n1x Pie (£6) - £6\n1x Mash (£4) - £4\nTotal: £10"
+  describe '#add and #basket' do
+    it 'adding a dish to the order shows that dish in the order basket' do
+      subject.add(1)
+      expect(subject.basket).to eq "Your order:\n1x Pie (£6) - £6\nTotal: £6"
+    end
+  
+    it 'adding a two dishes to the order shows both dishes in the order basket' do
+      subject.add(1)
+      subject.add(2)
+      expect(subject.basket).to eq "Your order:\n1x Pie (£6) - £6\n1x Mash (£4) - £4\nTotal: £10"
+    end
   end
 end
