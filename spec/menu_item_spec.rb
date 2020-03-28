a364a8fb04195e04f36b5440ee43835d2ebe0863
Alexa <alexakearns@outlook.com>
2020-03-28 10:21:47 +0000

    menu_item has a name test failed - red

--- spec/menu_item_spec.rb
+++ spec/menu_item_spec.rb
@@ -2,4 +2,10 @@ require 'menu_item'
 
 describe MenuItem do
 
+  context 'new item name and price' do
+    
+    it 'menu item has a name' do
+      expect(subject.name).to eq ("American Hot")
+    end
+  end
 end
