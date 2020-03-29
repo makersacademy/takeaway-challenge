ce7eb88135c62a4eefcd427724a96a512ad58c06
Steven-Klavins <stevenklavins94@gmail.com>
2020-03-29 23:42:31 +0100

    added total order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,10 +1,19 @@
 require 'order'
 class Takeaway
+
     def initialize (food_list)
         @food_list = food_list
     end
 
     def print_dishes 
         @food_list.each {|key, value| puts "#{key} - £#{value}0" }
+    end
+
+    def total_order (order)
+        total = 0 
+        order.each do |key|
+            total += @food_list[key]
+        end
+      total
     end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,13 +1,25 @@
 require 'takeaway'
+require 'order'
 
 describe Takeaway do
-    let(:customer) { Takeaway.new({ "Chicken Curry" => 6.00 }) }
+    let(:store) { Takeaway.new({ "Chicken Curry" => 6.00 }) }
 
-it 'allows the customer to see a list of dishes' do
-    expect(customer).to respond_to :print_dishes
+it 'allows the store to see a list of dishes' do
+    expect(store).to respond_to :print_dishes
 end
 
 it 'prints a list of dishes with the price' do
-    expect {customer.print_dishes}.to output("Chicken Curry - £6.00\n").to_stdout
+    expect {store.print_dishes}.to output("Chicken Curry - £6.00\n").to_stdout
 end
+
+it "totals an for a customer " do
+    expect(store).to respond_to :total_order 
+end
+
+it "gives the customer the total" do
+    new_store = Takeaway.new({ "Chicken Curry" => 6.00, "Rice" => 3.00 })
+    order = Order.new
+    expect(new_store.total_order(order.make_order("Chicken Curry", "Rice"))).to eq 9.00 
+end
+
 end
