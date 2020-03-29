62170aabb597c47034edb9375a70e1a54ddc9083
Steven-Klavins <stevenklavins94@gmail.com>
2020-03-29 18:47:34 +0100

    added order function

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,10 @@
 class Order
-    
-    def make_order
 
+    def make_order (a, *others)
+        arr = [] 
+         others.each do |i|
+            arr << i
+         end
+        arr.insert(0, a)
     end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,8 +1,13 @@
 require 'order'
 
 describe Order do
+    let(:customer) { Order.new }
+
     it "has the fuction for a user to make a new order" do
         expect(subject).to respond_to :make_order
     end
 
+    it "allows the user to generate a new oder" do
+        expect(customer.make_order("Chicken Curry", "Rice")).to eq ["Chicken Curry", "Rice"]
+    end
 end
