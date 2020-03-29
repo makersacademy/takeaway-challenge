4994205c29757757baf3b0800297d039a8b17ee2
tommyrharper <tommyrharper@gmail.com>
2020-03-29 17:16:12 +0100

    menu test refactored - test failed

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,9 +4,9 @@ describe Menu do
 
   it 'returns a list of dishes on #list' do
     expect(subject.list).to include(
-      :fish => 5,
-      :chips => 3,
-      :salad => 2
+      :fish => "£5",
+      :chips => "£3",
+      :salad => "£2"
     )
   end
 
