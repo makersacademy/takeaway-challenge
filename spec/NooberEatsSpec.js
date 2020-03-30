8a64b6e4f0733e995678ea265a021c6d3ffd0515
Student <student@makerss-air-2.home>
2020-03-30 11:04:07 +0100

    to clear

--- spec/NooberEatsSpec.js
+++ spec/NooberEatsSpec.js
@@ -6,27 +6,33 @@ describe('NooberEats', () => {
   beforeEach(() => {
     restuarant = new NooberEats();
     customer = new Noob();
+    order = new order();
   });
 
   describe('Menu rules', () => {
     it('starts with nil items', () => {
       expect(restuarant.getCurrentItems()).toEqual({});
     });
+    it('displays price of items', () => {
+      order = { customer:{wings:8,nuggets:4,chips:3} }
+      restuarant.getCurrentItems(display);
+      expect(restuarant.getCurrentItems()).toEqual(order)
+    });
   });
   
-  describe('Restuarant rules', () => {
+  /* describe('Restuarant rules', () => {
     it('can add items', () => {
       restuarant.addItem()
       expect(restuarant.getCurrentItems()).toEqual({ customer: {wings}});
     });
-   /*  it('can delete items', () => {
+    it('can delete items', () => {
       noob.addItem()
       expect(noob.getCurrentItems()).toEqual({ customer: {wings}});
       noob.removeItem()
       expect(noob.getCurrentItems()).toEqual({ customer:})
-    }); */
+    });
   });
-
+ */
   describe('Customer rules', () => {
     it('starts with one customer', () => {
       expect(restuarant.getCurrentItems()).toEqual({ customer: {}});
--- src/NooberEats.js
+++ src/NooberEats.js
@@ -9,6 +9,11 @@ class NooberEats{
   getCurrentItems() {
     return this.order
   }
+
+  displayCurrentItems() {
+
+  }
+  
   addItem() {
     
   }
