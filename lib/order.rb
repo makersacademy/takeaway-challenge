a3daafad4a9bf055a703220ae19f3b6edfa88aed
dpwdec <declankolakowski@gmail.com>
2020-04-25 19:01:29 +0100

    refactored add_item method

--- lib/order.rb
+++ lib/order.rb
@@ -3,8 +3,13 @@ class Order
 
   def initialize(menu)
     @menu = menu
+    @order_items = Hash.new
   end
 
   def add_item(name, quantity)
+    new_item = nil
+
+    new_item = menu.items.detect { |item| item.name == name }
+    @order_items[new_item].nil? ? @order_items[new_item] = quantity : @order_items[new_item] += quantity
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,7 +2,8 @@ require 'order'
 
 describe Order do
 
-  let(:italian_menu) { double() }
+  let(:carbonara) { double('Carbonara', name: 'Carbonara', price: '10.50')}
+  let(:italian_menu) { double('Italian', items: [carbonara]) }
   let(:subject) { described_class.new(italian_menu) }
   it { is_expected.to respond_to(:order_items) }
   it { is_expected.to respond_to(:add_item).with(2).arguments }
@@ -13,6 +14,9 @@ describe Order do
   end
 
   describe '#add_item' do
-
+    it 'it adds 3 orders of "Carbonara" to order_items' do
+      subject = described_class.new(italian_menu)
+      expect{ subject.add_item("Carbonara", 3) }.to change { subject.order_items }.to include(carbonara => 3)
+    end
   end
 end
