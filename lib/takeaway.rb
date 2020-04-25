0a8a0a35ec74a31272cabe7bf8bc7f86eed0727d
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-25 11:46:31 +0100

    Fixed formatting via rubocop

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,11 +2,11 @@ class Takeaway
   attr_reader :menu, :order
 
   def initialize(order = Order.new)
-    @menu = [{:chips => 1.80}, {:burger => 10.00}]
+    @menu = [{ :chips => 1.80 }, { :burger => 10.00 }]
     @order = order
   end
 
-  def menu
+  def display
     @menu
   end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,11 +1,11 @@
 require 'takeaway'
 
 describe Takeaway do
-  let(:takeaway) {described_class.new}
+  let(:takeaway) { described_class.new }
 
-  describe '#menu' do
+  describe '#display' do
     it 'displays menu' do
-      expect(takeaway.menu).to eq [{:chips => 1.80}, {:burger => 10.00}]
+      expect(takeaway.display).to eq [{ :chips => 1.80 }, { :burger => 10.00 }]
     end
   end
 
