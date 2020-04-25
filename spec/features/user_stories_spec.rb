a1e85bf9806df0753f9e39a00ea4619eecf6cac5
Ed <lookatemail@gmail.com>
2020-04-25 14:18:25 +0100

    refactor feature test user stories

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -2,25 +2,28 @@ require 'takeaway'
 require 'menu'
 
 describe 'User Stories' do
+  let(:takeaway) { takeaway = TakeAway.new }
   
   context 'TakeAway class' do
     # As a customer
     # So that I can check if I want to order something
     # I would like to see a list of dishes with prices
     it 'respond to menu' do
-      takeaway = TakeAway.new
       expect(takeaway.menu).not_to be nil
     end
 
     it 'should respond to @menu' do
-      takeaway = TakeAway.new
       expect(takeaway.menu).not_to be nil
     end
 
     it 'should print list of dishes' do
-      takeaway = TakeAway.new
       expect{ takeaway.menu_list }.to output.to_stdout
     end
+
+    # As a customer
+    # So that I can order the meal I want
+    # I would like to be able to select some number of several available dishes
+
 
   end
 
