73b83c4d2a4902b7b18ab62a98f3b897b32e500d
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-25 21:53:26 +0100

    Refactor: updates dish_spec.rb to use double for dish_namel; passes

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -3,6 +3,8 @@ require 'dish'
 context Dish do
   let(:dish_number) { Integer.new }
   let(:dish_price) { Float.new }
+  let(:dish_name) { String.new }
+
   
   describe '.new' do
     it 'takes three arguments' do
@@ -11,23 +13,23 @@ context Dish do
   end
   
   describe '.number returns dish number' do
-    dish = Dish.new(:dish_number, :dish_price, "Plain rice")
+    dish = Dish.new(:dish_number, :dish_price, :dish_name)
     it 'returns dish number when asked' do
       expect(dish.number).to eq :dish_number
     end
   end
 
   describe '.price returns dish price' do
-    dish = Dish.new(:dish_number, :dish_price, "Plain rice")
+    dish = Dish.new(:dish_number, :dish_price, :dish_name)
     it 'returns dish price when asked' do
       expect(dish.price).to eq :dish_price
     end
   end
 
   describe '.name returns dish name' do
-    dish = Dish.new(:dish_number, :dish_price, "Plain rice")
+    dish = Dish.new(:dish_number, :dish_price, :dish_name)
     it 'returns dish price when asked' do
-      expect(dish.name).to eq "Plain rice"
+      expect(dish.name).to eq :dish_name
     end
   end
 
