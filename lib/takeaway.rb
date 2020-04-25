fb38ce716d8d60bc493dd4a4931ccc992f014020
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-25 11:42:46 +0100

    Added place_order method and tested with double

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,16 @@
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :order
 
-  def initialize
+  def initialize(order = Order.new)
     @menu = [{:chips => 1.80}, {:burger => 10.00}]
+    @order = order
   end
 
   def menu
     @menu
   end
 
-  def place_order()
+  def place_order
+    @order
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -14,6 +14,12 @@ describe Takeaway do
       expect(takeaway).to respond_to(:place_order)
     end
 
+    it 'makes new order object instance' do
+      order_double = double("order")
+      my_takeaway = Takeaway.new(order_double)
+      expect(my_takeaway.place_order).to eq order_double
+    end
+
     # it 'adds order to empty order array' do
     #   takeaway.order(31.80, {:burger => 3, :chips => 1})
     #   expect(takeaway.order_array).to eq [{:burger=>10.0}, {:burger=>10.0}, {:burger=>10.0}, {:chips=>1.8}]
