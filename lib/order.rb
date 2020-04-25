3fa5a4c65ad24cffe31f14f46c9e84a5df61cc5c
mbrad26 <mbrad26@gmail.com>
2020-04-25 17:34:07 +0100

    Order class starts with an empty @basket

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,12 @@
 class Order
 
+  attr_reader :basket
+
+  def initialize(menu = Menu.new)
+    @menu = menu
+    @basket = Hash.new(0)
+  end
+
   def add_to_order(dish, qty)
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,7 +1,18 @@
 require 'order'
 
 describe Order do
-  subject { described_class.new }
+  subject { described_class.new(menu) }
+  let(:menu) { instance_double 'Menu' }
 
   it { is_expected.to respond_to(:add_to_order).with(2).arguments }
+
+  it 'starts with an empty basket' do
+    expect(subject.basket).to be_empty
+  end
+
+  describe '#add_to_order' do
+    it 'adds a dish to the basket' do
+
+    end
+  end
 end
