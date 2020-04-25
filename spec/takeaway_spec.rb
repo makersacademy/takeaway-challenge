1edc2aff62913dd37cf53edc88e0c74d24cc1b39
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-25 13:45:51 +0100

    Check place_order calls order method in order class

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -13,11 +13,12 @@ describe Takeaway do
     it 'takeaway responds to method place_order' do
       expect(takeaway).to respond_to(:place_order)
     end
-
-    # it 'returns order object instance' do
-    #   order_double = double("order")
-    #   my_takeaway = Takeaway.new(order_double)
-    #   expect(my_takeaway.place_order(31.80, {:burger => 3, :chips => 1})).to eq order_double
-    # end
+    
+    it 'calls the order method' do
+      order_double = double("order")
+      my_takeaway = Takeaway.new(order_double)
+      allow(order_double).to receive(:order).and_return([{:burger=>10.0}, {:burger=>10.0}, {:burger=>10.0}, {:chips=>1.8}])
+      expect(my_takeaway.place_order(31.80, {:burger => 3, :chips => 1})).to eq [{:burger=>10.0}, {:burger=>10.0}, {:burger=>10.0}, {:chips=>1.8}]
+    end
   end
 end
