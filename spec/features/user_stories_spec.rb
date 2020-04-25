78b9643b7d2d22676921ea6c26be22308d2c3876
Ed <lookatemail@gmail.com>
2020-04-25 15:58:20 +0100

    refactor spec

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -4,47 +4,40 @@ require 'menu'
 describe 'User Stories' do
   let(:takeaway) { takeaway = TakeAway.new }
   let(:order) { double() }
-  context 'TakeAway menu' do
-    # As a customer
-    # So that I can check if I want to order something
-    # I would like to see a list of dishes with prices
-    it 'receive menu' do
-      expect(takeaway.menu).not_to be nil
+
+    context 'Menu class' do
+      # As a customer
+      # So that I can check if I want to order something
+      # I would like to see a list of dishes with prices
+      it 'receive menu' do
+        expect(takeaway.menu).not_to be nil
+      end
+
+      it 'should print list of dishes' do
+        expect{ takeaway.menu_list }.to output.to_stdout
+      end
     end
 
-    it 'should print list of dishes' do
-      expect{ takeaway.menu_list }.to output.to_stdout
+    context 'TakeAway class' do
+      # As a customer
+      # So that I can order the meal I want
+      # I would like to be able to select some number of several available dishes
+      it 'otder total to be empty at start' do
+        expect(takeaway.order_total.empty?).to be true
+      end
+
+      it 'able to place order' do
+        takeaway.order "Tomato Salad"
+        expect(takeaway.order_total.empty?).to be false
+      end
+
+      it 'should be able to order several items' do
+        test_order = ["Grilled octopus", "Calamari"]
+        takeaway.order "Grilled octopus"
+        takeaway.order "Calamari"
+        expect(takeaway.order_total).to eq test_order
+      end
+
     end
-
-    # As a customer
-    # So that I can order the meal I want
-    # I would like to be able to select some number of several available dishes
-    it 'otder total to be empty at start' do
-      expect(takeaway.order_total.empty?).to be true
-    end
-
-    it 'able to place order' do
-      takeaway.order "Tomato Salad"
-      expect(takeaway.order_total.empty?).to be false
-    end
-
-    it 'should be able to order several items' do
-      test_order = ["Grilled octopus", "Calamari"]
-      takeaway.order "Grilled octopus"
-      takeaway.order "Calamari"
-      expect(takeaway.order_total).to eq test_order
-    end
-
-
-  end
-
-
-  context 'Menu class' do
-    it 'should respond to #menu' do
-      menu = Menu.new
-      expect { menu.print }.to output.to_stdout
-    end
-
-  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,35 +5,29 @@ describe TakeAway do
   subject(:takeaway) { described_class.new }
   let(:order) { double(:order => "Tomato Salad") }
   
-  describe '#menu' do
+  context '#menu_list' do
     it 'should list dishes' do
       expect{ takeaway.menu_list }.to output.to_stdout
     end
+  end
 
-    it 'order total to be empty at start' do
+  context '#order' do
+    it 'total to be empty at start' do
       expect(takeaway.order_total.empty?).to be true
     end
 
-    it 'should be able to place order' do
+    it 'able to place' do
       takeaway.order order.order
       expect(takeaway.order_total.shift).to eq "Tomato Salad"
     end
 
-    it 'should be able to order more than one order' do
+    it 'more than one order' do
       test_order = ["Lamb Burger", "Spicy Meatballs"]
       takeaway.order "Lamb Burger"
       takeaway.order "Spicy Meatballs"
       expect(takeaway.order_total).to eq test_order
     end
-
   end
 
-  describe '#order' do
-    
-  end
-
-  describe '#checkout' do
-    
-  end
 
 end
