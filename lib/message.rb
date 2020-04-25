b30c22ef8b3885259d1b713b7fbfe7154c5c12bd
dpwdec <declankolakowski@gmail.com>
2020-04-25 22:00:41 +0100

    added time formatting and one hour addition to message

--- lib/message.rb
+++ lib/message.rb
@@ -1,2 +1,13 @@
 class Message
+
+  SECS_IN_HOUR = 3600
+
+  def initialize(time = Time.now)
+    @time = time
+  end
+
+  def time_one_hour_ahead
+    new_time = @time + SECS_IN_HOUR
+    new_time.strftime('%H:%M')
+  end
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,5 +1,17 @@
 require 'message'
 
 describe Message do
-  
+
+  let(:time) { Time.new(2007,11,19,21,37,48,"-06:00") }
+  let(:subject) { described_class.new(time) }
+
+  describe "#confirmation_message" do
+    #expect(subject.confirmation_message(time)).to eq("Thank you! Your order was placed and will be delivered before 22:37")
+  end
+
+  describe '#time_one_hour_ahead' do
+    it 'returns the time +1 hour' do
+      expect(subject.time_one_hour_ahead).to eq('22:37')
+    end
+  end
 end
