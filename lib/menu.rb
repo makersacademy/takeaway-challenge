77bb36d56f25575160add8efeb585c837e9a45a3
mbrad26 <mbrad26@gmail.com>
2020-04-25 13:42:32 +0100

    added #add_dish_to_menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,18 @@
 class Menu
 
-  attr_reader :menu
+  attr_accessor :menu_list
 
   def initialize
-    @menu = {'Fish & Chips' => 4, 'Burger' => 3, 'Shawarma' => 5}
+    @menu_list = { 'Fish & Chips' => 4, 'Burger' => 3, 'Shawarma' => 5 }
   end
 
   def print_menu
-    menu.collect { |dish, price| p [dish, '£' + price.to_s].join(' ')}
+    menu_list.collect { |dish, price| p [dish, '£' + price.to_s].join(' ') }
         .join(', ')
+  end
+
+  def add_dish_to_menu(dish, price)
+    menu_list[dish] = price
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,12 +2,13 @@ require 'menu'
 
 describe Menu do
   subject { described_class.new }
-  let(:dishes) { {'Fish & Chips' => 4, 'Burger' => 3, 'Shawarma' => 5} }
+  let(:dishes) { { 'Fish & Chips' => 4, 'Burger' => 3, 'Shawarma' => 5 } }
 
   it { is_expected.to respond_to(:print_menu) }
+  it { is_expected.to respond_to(:add_dish_to_menu).with(2).arguments }
 
   it 'stores a menu of dishes and prices' do
-    expect(subject.menu).to eq dishes
+    expect(subject.menu_list).to eq dishes
   end
 
   describe '#print_menu' do
@@ -16,4 +17,10 @@ describe Menu do
     end
   end
 
+  describe '#add_dish_to_menu' do
+    it 'adds a new dish to menu' do
+      expect {subject.add_dish_to_menu('Mititei', 1) }
+      .to change { subject.menu_list }.to include({ 'Mititei' => 1 })
+    end
+  end
 end
