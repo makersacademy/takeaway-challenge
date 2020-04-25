b1594a97d496503bad3b80b6486b34ad2b36a23a
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-25 10:52:13 +0100

    Test: adds test to check dish.number returns dish number; passes

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,7 +1,11 @@
 class Dish
 
   def initialize(number, price, description)
-    
+
+  end
+  
+  def number
+     1
   end
   
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -3,8 +3,15 @@ require 'dish'
 context Dish do
 
   describe '.new' do
-    it "takes three arguments" do
+    it 'takes three arguments' do
       expect(Dish).to respond_to(:new).with(3).arguments
+    end
+  end
+  
+  describe '.number returns dish number' do
+    dish = Dish.new(1, 2.00, "Plain rice")
+    it 'returns dish number when asked' do
+      expect(dish.number).to eq 1
     end
   end
 
