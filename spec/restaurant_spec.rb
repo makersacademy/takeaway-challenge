66e783c7ef7b76c407fc794f8c95b22f6dbeefc7
mbrad26 <mbrad26@gmail.com>
2020-04-25 11:05:25 +0100

    it responds to #view_menu

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,9 +1,14 @@
 require 'restaurant'
 
 describe Restaurant do
+  subject { described_class.new(menu = menu) }
+  let(:menu) { instance_double :menu }
 
   it { is_expected.to respond_to(:view_menu) }
 
   describe '#view_menu' do
+    it 'returns a menu of dishes with prices' do
+      expect(subject.view_menu).to eq 'Fish & Chips £4'
+    end
   end
 end
