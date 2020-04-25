8bb25444d3e4fc53f5be731925af031aa3eb1247
mbrad26 <mbrad26@gmail.com>
2020-04-25 11:02:39 +0100

    it responds to #view_menu

---
+++ lib/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant
+
+  def view_menu
+  end
+end
---
+++ spec/restaurant_spec.rb
@@ -0,0 +1,9 @@
+require 'restaurant'
+
+describe Restaurant do
+
+  it { is_expected.to respond_to(:view_menu) }
+
+  describe '#view_menu' do
+  end
+end
