d3b7966eb663f01d5e83913b38b038c7aa8b876d
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-25 22:26:45 +0100

    Refactor: updates menu_spec to use doubles and before hook; passes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,13 +2,19 @@ require 'menu'
 require 'dish'
 
 context Menu do
+  let(:dish_number) { Integer.new }
+  let(:dish_price) { Float.new }
+  let(:dish_name) { String.new }
 
+  before(:each) do
+    @dish = Dish.new(:dish_number, :dish_price, :dish_name)
+  end
+  
   it { is_expected.to respond_to(:add).with(1).argument }
 
   describe '#add' do
-    dish = Dish.new(1, 2.00, "Plain rice")
     it 'returns dish added on success' do
-      expect(subject.add(dish)).to eq dish
+      expect(subject.add(@dish)).to eq @dish
     end
   end
   
