5a9eb1e5dabb90039334eed618221936e8ee60ce
mbrad26 <mbrad26@gmail.com>
2020-04-25 11:37:44 +0100

    #view_menu returns a menu

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,5 +1,12 @@
 class Restaurant
+  attr_reader :menu
+
+  def initialize(menu = Menu.new)
+    @menu = menu
+  end
 
   def view_menu
+    menu.print_menu
+    # 'Fish & Chips £4'
   end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,13 +1,15 @@
 require 'restaurant'
+# require 'menu'
 
 describe Restaurant do
-  subject { described_class.new(menu = menu) }
-  let(:menu) { instance_double :menu }
+  subject { described_class.new(menu) }
+  let(:menu) { instance_double 'Menu' }
 
   it { is_expected.to respond_to(:view_menu) }
 
   describe '#view_menu' do
     it 'returns a menu of dishes with prices' do
+      allow(menu).to receive(:print_menu).and_return('Fish & Chips £4')
       expect(subject.view_menu).to eq 'Fish & Chips £4'
     end
   end
