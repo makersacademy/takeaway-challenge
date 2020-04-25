33b2662b3e6a49f9dac8da967f197400c81ecda7
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-25 22:25:20 +0100

    Test: checks Menu.add(dish) returns dish; passes

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,7 @@
 class Menu
   
   def add(dish)
-    
+    dish
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,15 @@
 require 'menu'
+require 'dish'
 
 context Menu do
 
   it { is_expected.to respond_to(:add).with(1).argument }
 
+  describe '#add' do
+    dish = Dish.new(1, 2.00, "Plain rice")
+    it 'returns dish added on success' do
+      expect(subject.add(dish)).to eq dish
+    end
+  end
+  
 end
