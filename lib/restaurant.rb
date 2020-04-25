e0ee0c6d6cb85ee55564189d80fe069bb4dcee46
dpwdec <declankolakowski@gmail.com>
2020-04-25 09:37:47 +0100

    Added basic output for show_menu

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant
   attr_reader :name, :menu
+
+  def show_menu
+    puts "Falafel - £3.50"
+    puts "Egg Wrap - £5.10"
+    puts "Fresh Juice - £0.70"
+  end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -3,4 +3,13 @@ require 'restaurant'
 describe Restaurant do
   it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:menu) }
+
+  describe '#show_menu' do
+    it 'outputs a list of menu items' do
+      menu_list = "Falafel - £3.50\n"\
+      "Egg Wrap - £5.10\n"\
+      "Fresh Juice - £0.70\n"
+      expect{ subject.show_menu }.to output(menu_list).to_stdout
+    end
+  end
 end
