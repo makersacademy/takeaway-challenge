d6d5f8ac2e73ffbbc65993b01b0d5511ae0b73ab
Andrew Hulme <andrewhulme1993@gmail.com>
2020-04-25 20:40:10 +0100

    Adds a place_order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -24,13 +24,25 @@ class Takeaway
     puts "Current order total is £#{@current_total}."
   end
 
-  def check_order_price
+  def check_total_order_price
     @overall_total = 0
+
     @order.each do |dish|
       @overall_total += @dishes[dish]
     end
 
     puts "Final order total is £#{@overall_total}."
+  end
+
+  def delivery_time
+    time = Time.new
+    the_hour = time.hour
+    the_minute = time.min
+    "#{the_hour+1}:#{the_minute}"
+  end
+
+  def place_order
+    puts "Thank you! Your order was placed and will be delivered before #{delivery_time}. A text will be sent to your phone shortly to confirm your order."
   end
 
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,6 +7,14 @@ describe Takeaway do
     subject = described_class.new(dishes)
   end
 
+  let(:delivery_time) do
+    time = Time.new
+    the_hour = time.hour
+    the_minute = time.min
+    "#{the_hour+1}:#{the_minute}"
+
+  end
+
 
   describe '#see_dishes' do
     it 'responds to' do
@@ -35,21 +43,21 @@ describe Takeaway do
     end
   end
 
-  describe '#check_order_price' do
-    it 'checks overall price is equal to the current total' do
-      subject.add_dish(:burger, 4)
-      subject.add_dish(:sausage, 1)
-      subject.add_dish(:chips, 5)
-      subject.check_order_price
-
-      expect(subject.overall_total).to eq subject.current_total
-    end
-
+  describe '#check_total_order_price' do
     it 'outputs the total price' do
       subject.add_dish(:burger, 4)
       subject.add_dish(:sausage, 1)
       subject.add_dish(:chips, 5)
-      expect { subject.check_order_price }.to output("Final order total is £55.\n").to_stdout
+      expect { subject.check_total_order_price }.to output("Final order total is £55.\n").to_stdout
+    end
+  end
+
+  describe '#place_order' do
+    it 'outputs to stdout that they have placed the order' do
+      subject.add_dish(:burger, 4)
+      subject.add_dish(:sausage, 1)
+      subject.add_dish(:chips, 5)
+      expect { subject.place_order }.to output("Thank you! Your order was placed and will be delivered before #{delivery_time}. A text will be sent to your phone shortly to confirm your order.\n").to_stdout
     end
   end
 
