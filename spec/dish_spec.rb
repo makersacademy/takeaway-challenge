4b479b6299a6da35e1c348478983ab9bf5ae41f4
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-25 22:00:57 +0100

    Refactor: updates dish_spec to use before hook to set @dish test variable; passes

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -5,6 +5,9 @@ context Dish do
   let(:dish_price) { Float.new }
   let(:dish_name) { String.new }
 
+  before(:each) do
+    @dish = Dish.new(:dish_number, :dish_price, :dish_name)
+  end
   
   describe '.new' do
     it 'takes three arguments' do
@@ -13,23 +16,23 @@ context Dish do
   end
   
   describe '.number returns dish number' do
-    dish = Dish.new(:dish_number, :dish_price, :dish_name)
+#     dish = Dish.new(:dish_number, :dish_price, :dish_name)
     it 'returns dish number when asked' do
-      expect(dish.number).to eq :dish_number
+      expect(@dish.number).to eq :dish_number
     end
   end
 
   describe '.price returns dish price' do
-    dish = Dish.new(:dish_number, :dish_price, :dish_name)
+#     dish = Dish.new(:dish_number, :dish_price, :dish_name)
     it 'returns dish price when asked' do
-      expect(dish.price).to eq :dish_price
+      expect(@dish.price).to eq :dish_price
     end
   end
 
   describe '.name returns dish name' do
-    dish = Dish.new(:dish_number, :dish_price, :dish_name)
+#     dish = Dish.new(:dish_number, :dish_price, :dish_name)
     it 'returns dish price when asked' do
-      expect(dish.name).to eq :dish_name
+      expect(@dish.name).to eq :dish_name
     end
   end
 
