c9b4eb03a87e22e42f13fb8fa1f1e55caf1cc914
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-26 15:12:11 +0100

    Added ENV Variables

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -1,8 +1,8 @@
 require 'twilio-ruby'
 
 class RubyTwilio
-  RECEIVER = "+44 7919 912139"
-  SENDER = "+18452500895"
+  RECEIVER = ENV['RECEIVER']
+  SENDER = ENV['SENDER']
 
   def send_confirmation_text
     set_up
@@ -11,9 +11,9 @@ class RubyTwilio
 
   private
   def set_up
-    account_sid = "AC17c7f225973a0fdaf83cf81958edf728"
-    auth_token = "7ef4b3f68887aec6b7977df39a6801f2"
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    account_sid = ENV['TWILIO_SID']
+    auth_token  = ENV['TWILIO_AUTH_TOKEN']
+    @client     = Twilio::REST::Client.new account_sid, auth_token
   end
 
   def message
