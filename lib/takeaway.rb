5e1d397d74da8b36f6667c70616e95294d5fb051
HResalaty <Hmresalaty@yahoo.co.uk>
2020-04-26 18:30:26 +0100

    can select several dishes from the menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,23 @@
+require 'order'
+
 class Takeaway
 
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order || Order.new
   end
 
   def print_menu
     menu.print
   end
 
+  def place_order(dishes)
+    dishes.each do |dish, quantity|
+      order.add(dish, quantity)
+    end
+  end
+
   private
-  attr_reader :menu
+  attr_reader :menu, :order
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,13 +1,20 @@
 require 'takeaway'
 
 describe Takeaway do
-  subject(:takeaway) { described_class.new(menu: menu) }
+  subject(:takeaway) { described_class.new(menu: menu, order: order) }
 
   let(:menu) { double(:menu, print: printed_menu) }
+  let(:order) { double(:order) }
   let(:printed_menu) { "Chicken: £3.50" }
+  let(:dishes) { {chicken: 2, fish: 1} }
 
   it 'shows the menu with dishes and prices' do
     expect(takeaway.print_menu).to eq(printed_menu)
+  end
+
+  it 'can order some number of several available dishes' do
+    expect(order).to receive(:add).twice
+    takeaway.place_order(dishes)
   end
 
 end
