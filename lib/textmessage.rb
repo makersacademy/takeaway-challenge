7a76b4c63905e1f7f736df67cbcee3d5d64c0c2b
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-27 00:55:15 +0100

    Test: adds and implements TextMessage.send_message(order) basic interface

--- lib/textmessage.rb
+++ lib/textmessage.rb
@@ -1,3 +1,14 @@
 class TextMessage
 
+  def send_message(order)
+
+  end
+
+  private
+  
+  def do_send(message)
+    # TODO this would send the message through the Twilio API
+    puts message
+  end
+
 end
--- spec/textmessage_spec.rb
+++ spec/textmessage_spec.rb
@@ -1,5 +1,9 @@
 require 'textmessage'
 
 context TextMessage do
+  
+  describe '#send_message' do
+    it { is_expected.to respond_to(:send_message) }
+  end
 
 end
