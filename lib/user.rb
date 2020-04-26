2e78e034a7df02c355710c58983a877fd01aa38e
joannacollier <collj035@gmail.com>
2020-04-26 15:00:09 +0100

    User class creates a new Order instance and saves as @order variable

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,21 @@
 require "./lib/menu"
 
 class User
-    def request_full_menu(menu)
-        menu = Menu.new 
-        menu.full_menu 
-    end 
+  attr_reader :order
+
+  def initialize 
+    @order = nil 
+  end 
+
+  def request_full_menu(menu)
+    menu = Menu.new 
+      menu.full_menu 
+  end 
+
+  def new_order(unicorn)
+    @order = unicorn
+  end  
+    
+
+
 end 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,21 @@
 require './lib/user'
 require "./lib/menu"
+require "./lib/order"
 
 describe User do
   let(:menu) { Menu.new }
-  let(:menu_hash){ {pizza: 3} } 
+  let(:unicorn) { Order.new }
 
   describe "#Call menu items" do 
     it "Returns the full menu" do
       expect(subject.request_full_menu(menu)).to eq(menu.full_menu)
     end
   end
+
+  describe "#new_order" do 
+    it "Returns a new Order class to instance varible" do
+      subject.new_order(unicorn)
+      expect(subject.order).to eq(unicorn)
+    end 
+  end 
 end 
