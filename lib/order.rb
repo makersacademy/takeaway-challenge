a1d18c0c41bd09ca7ca1d7cc3c580fd33940d807
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-26 23:24:23 +0100

    Refactor: adds before hook to order_spec.rb and tidies up comments

--- lib/order.rb
+++ lib/order.rb
@@ -9,21 +9,18 @@ class Order
   
   def add(number, menu)
     raise ERROR_NOT_INTEGER unless number.is_a?(Integer)
-#     raise Menu::ERROR_DISH_NOT_EXISTS
 
-    # TODO: pass error raised by menu.get(number) and raise it again
-    begin
-      dish = menu.get(number)
-    rescue 
-      raise Menu::ERROR_DISH_NOT_EXISTS
-    end
+    # TODO: pass error raised by menu.get(number) and raise it again?
+#     begin
+#       menu.get(number)
+#     rescue 
+#       raise Menu::ERROR_DISH_NOT_EXISTS
+#     end
 
+    menu.get(number)
+    
     @order.push(number)
     @order.last
   end
   
-#   def order 
-#     @order
-#   end
-
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,51 +4,34 @@ require 'dish'
 
 context Order do
   # TODO: refactor to use double for dish_number
-  
+
+  let(:dish_number) { Integer.new }
+  let(:dish_price) { Float.new }
+  let(:dish_name) { String.new }
+
+  before(:each) do
+    @menu = Menu.new
+    @dish = Dish.new(:dish_number, :dish_price, :dish_name)
+    @dish_1 = Dish.new(1, :dish_price, :dish_name)
+    @dish_2 = Dish.new(2, :dish_price, :dish_name)
+    @menu.add(@dish_1)
+    @menu.add(@dish_2)
+  end
+
   describe '#add' do
     it { is_expected.to respond_to(:add).with(2).argument }
     
     it 'raises error if not passed an Integer' do
-      menu = Menu.new
-      dish_1 = Dish.new(1, 2.00, "Plain rice")
-      dish_2 = Dish.new(2, 3.00, "Special rice")
-      dish_3 = Dish.new(3, 113.00, "Very Special rice")
-      dish_4 = Dish.new(4, 0.01, "Unspecial rice")
-      
-      menu.add(dish_1)
-      menu.add(dish_2)
-      menu.add(dish_3)
-
-      expect { subject.add("Plain rice", menu) }.to raise_error Order::ERROR_NOT_INTEGER
+      expect { subject.add("Plain rice", @menu) }.to raise_error Order::ERROR_NOT_INTEGER
     end
     
 #     TODO: Find out why this test doesn't work
 #     it 'raises error if order_number not on menu' do
-#       menu = Menu.new
-#       dish_1 = Dish.new(1, 2.00, "Plain rice")
-#       dish_2 = Dish.new(2, 3.00, "Special rice")
-#       dish_3 = Dish.new(3, 113.00, "Very Special rice")
-#       dish_4 = Dish.new(4, 0.01, "Unspecial rice")
-#       
-#       menu.add(dish_1)
-#       menu.add(dish_2)
-#       menu.add(dish_3)
-#       
-#       expect { subject.add(1, menu) }.to raise_error Menu::ERROR_DISH_NOT_EXISTS
+#       expect { subject.add(1, @menu) }.to raise_error Menu::ERROR_DISH_NOT_EXISTS
 #     end
     
     it 'returns number on success' do
-      menu = Menu.new
-      dish_1 = Dish.new(1, 2.00, "Plain rice")
-      dish_2 = Dish.new(2, 3.00, "Special rice")
-      dish_3 = Dish.new(3, 113.00, "Very Special rice")
-      dish_4 = Dish.new(4, 0.01, "Unspecial rice")
-      
-      menu.add(dish_1)
-      menu.add(dish_2)
-      menu.add(dish_3)
-      
-      expect(subject.add(1, menu)).to eq 1
+      expect(subject.add(1, @menu)).to eq 1
     end
     
   end
@@ -57,20 +40,11 @@ context Order do
     it { is_expected.to respond_to(:order) }
     
     it 'returns Array containing order' do
-      menu = Menu.new
-      dish_1 = Dish.new(1, 2.00, "Plain rice")
-      dish_2 = Dish.new(2, 3.00, "Special rice")
-      dish_3 = Dish.new(3, 113.00, "Very Special rice")
-      dish_4 = Dish.new(4, 0.01, "Unspecial rice")
-      
-      menu.add(dish_1)
-      menu.add(dish_2)
-      menu.add(dish_3)
       numbers = Array.new
       numbers.push(1)
       numbers.push(2)
-      subject.add(1, menu)
-      subject.add(2, menu)
+      subject.add(1, @menu)
+      subject.add(2, @menu)
       expect(subject.order).to eq numbers
     end
     
