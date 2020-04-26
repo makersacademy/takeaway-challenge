24fbf68e0e7c04af6b53bece3b2247c30e1aac53
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-26 10:03:02 +0100

    Test: adds check that Order.add(number) returns number on success

--- lib/order.rb
+++ lib/order.rb
@@ -2,13 +2,15 @@ class Order
   
   ERROR_NOT_INTEGER = "Error: only numbers can be supplied"
   
-#   def initialize 
-#     @order = Array.new 
-#   end
+  def initialize 
+    @order = Array.new 
+  end
   
   def add(number)
     raise ERROR_NOT_INTEGER unless number.is_a?(Integer)
 
+     @order.push(number)
+     @order.last
   end
   
   def get 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,10 +9,23 @@ context Order do
       expect { subject.add("Plain rice") }.to raise_error Order::ERROR_NOT_INTEGER
     end
     
+    it 'returns number on success' do
+      expect(subject.add(1)).to eq 1
+    end
+    
   end
   
   describe '#get' do
     it { is_expected.to respond_to(:get) }
+    
+#     it 'returns Array containing order' do
+#       numbers = Array.new
+#       numbers.push(1)
+#       numbers.push(2)
+#       subject.add(1)
+#       subject.add(2)
+#       expect(subject.get).to eq numbers
+#     end
     
   end
 
