9b4a5e618c448a4411490d03afefd87d72b5622a
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-26 11:25:41 +0100

    added quantity test and method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,8 +3,13 @@ class Takeaway
     { Burger: :£4, Fish: :£4, Sausage: :£4, Chips: :£1 }
   end
 
-  def order
+  def item
     puts "What would you like to order?"
     item = gets.chomp
+  end
+
+  def quantity
+    puts "How many would you like?"
+    quanity = gets.chomp
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -11,11 +11,20 @@ describe Takeaway do
     end
   end
 
-  describe '#order' do
-    let(:input) {StringIO.new('Burger')}
-    it 'receives user input to order food' do
+  describe '#item' do
+    let(:input) {StringIO.new("Burger")}
+    it 'receives user input for item required' do
       $stdin = input
-      expect { subject.order }.to output("What would you like to order?\n").to_stdout
+      expect { subject.item }.to output("What would you like to order?\n").to_stdout
+      $stdin = STDIN
+    end
+  end
+
+  describe '#quantity' do
+    let(:input) {StringIO.new("1")}
+    it 'receives user input for quantity required' do
+      $stdin = input
+      expect { subject.quantity }.to output("How many would you like?\n").to_stdout
       $stdin = STDIN
     end
   end
