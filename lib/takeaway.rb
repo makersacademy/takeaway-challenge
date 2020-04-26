0f785ae1c7f40e5f56cb444fd29f1b8faa7e39e5
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-26 11:16:11 +0100

    added order test and method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,4 +2,9 @@ class Takeaway
   def view_menu
     { Burger: :£4, Fish: :£4, Sausage: :£4, Chips: :£1 }
   end
+
+  def order
+    puts "What would you like to order?"
+    item = gets.chomp
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,22 @@
 require 'takeaway'
+require 'stringio'
 
 describe Takeaway do
-  describe 'view_menu' do
+  let(:item) {'Burger'}
+  let(:quanity) {1}
+
+  describe '#view_menu' do
     it 'prints a menu' do
       expect(subject.view_menu).to include({ Burger: :£4 })
+    end
+  end
+
+  describe '#order' do
+    let(:input) {StringIO.new('Burger')}
+    it 'receives user input to order food' do
+      $stdin = input
+      expect { subject.order }.to output("What would you like to order?\n").to_stdout
+      $stdin = STDIN
     end
   end
 end
