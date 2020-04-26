1dc095c211157dbd7070d34fc31e8735694f5c90
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-26 12:39:05 +0100

    Refactored menu to raise specific error messages, moved twilio into seperate class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,5 @@
 require './lib/dish_list'
-require 'twilio-ruby'
+require './lib/twilio'
 
 class OrderMenu
   attr_reader :basket, :dish_list
@@ -9,7 +9,6 @@ class OrderMenu
     @dish_list = dish_list
     @basket = Array.new
     @current_selection = Hash.new
-    set_twilio
   end
 
   def print
@@ -17,12 +16,13 @@ class OrderMenu
   end
 
   def enter_selection(item, quantity)
-    raise "error" unless available?(item)
+    raise "Item not available" unless available?(item)
     add_to_basket(quantity)
     @current_selection = Hash.new
   end
 
   def place_order(sum)
+    raise "No selection made" if @basket.empty?
     check(sum)
   end
 
@@ -33,45 +33,17 @@ class OrderMenu
   end
 
   private
-  #Move to twilio object?
-  def set_twilio
-    account_sid = "AC17c7f225973a0fdaf83cf81958edf728"
-    auth_token = "6649eb8f7774da59af92a9a9eda153a0"
-    @client = Twilio::REST::Client.new account_sid, auth_token
-  end
-
-  #Move to twilio object?
-  def send_confirmation_text
-    message = "Thank you! Your order was placed and will be delivered before #{get_eta}"
-    @client.messages.create(
-      body: message,
-      to: "+44 7919 912139",
-      from: "+18452500895")
-  end
-
-  #Move to order object?
   def available?(item)
-    @dish_list.dishes.each do |dish|
-      @current_selection["item"] = dish if dish.item == item
-    end
+    @dish_list.dishes.each { |dish| @current_selection["item"] = dish if dish.item == item }
     !@current_selection["item"].nil?
   end
 
-  #Move to order object?
   def add_to_basket(quantity)
     @current_selection["quantity"] = quantity
     @basket << @current_selection
   end
 
-  #Move to order object?
   def check(sum)
-    total == sum ? send_confirmation_text : (raise "error")
-  end
-
-  #Move to twilio object?
-  def get_eta
-    time = Time.now
-    hour_ahead = time + (60 * 60)
-    hour_ahead.strftime("%k:%M")
+    total == sum ? RubyTwilio.send_confirmation_text : (raise "Incorrect amount")
   end
 end
---
+++ lib/twilio.rb
@@ -0,0 +1,29 @@
+require 'twilio-ruby'
+
+class RubyTwilio
+  RECEIVER = "+44 7919 912139"
+  SENDER = "+18452500895"
+
+  def self.send_confirmation_text
+    set_up
+    @client.messages.create(body: message, to: RECEIVER, from: SENDER)
+  end
+
+  #private
+  def self.set_up
+    account_sid = "AC17c7f225973a0fdaf83cf81958edf728"
+    auth_token = "7ef4b3f68887aec6b7977df39a6801f2"
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  def self.message
+    "Thank you! Your order was placed and will be delivered before #{get_eta}"
+  end
+
+  def self.get_eta
+    hour_ahead = Time.now + (60 * 60)
+    hour_ahead.strftime("%k:%M")
+  end
+
+  private_class_method :set_up, :message, :get_eta
+end
