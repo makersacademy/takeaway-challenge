43037cc92a77a4289b22daa5dbd158894a93023f
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-26 09:55:52 +0100

    Test: adds check that Order.add(number) is passed an Integer

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,14 @@
 class Order
   
+  ERROR_NOT_INTEGER = "Error: only numbers can be supplied"
+  
+#   def initialize 
+#     @order = Array.new 
+#   end
+  
   def add(number)
-    
+    raise ERROR_NOT_INTEGER unless number.is_a?(Integer)
+
   end
   
   def get 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,6 +5,10 @@ context Order do
   describe '#add' do
     it { is_expected.to respond_to(:add).with(1).argument }
     
+    it 'raises error if not passed an Integer' do
+      expect { subject.add("Plain rice") }.to raise_error Order::ERROR_NOT_INTEGER
+    end
+    
   end
   
   describe '#get' do
