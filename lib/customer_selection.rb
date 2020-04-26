6e8f87fdf2f1b5b27575eaeab79e9fee23ab96b8
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-26 06:45:43 +0000

    Add customer input to CUstomer Selection

--- lib/customer_selection.rb
+++ lib/customer_selection.rb
@@ -1,2 +1,90 @@
 class CustomerSelection
+    
+  attr_reader :selection, :dish, :quantity, :continue, :menu
+    
+  def initialize(menu = Menu.new)
+    @selection = {}
+    @dish = nil
+    @quantity = nil
+    @continue = "Y"
+    @menu = menu
+  end  
+    
+
+  def select_dish
+    puts "Please enter dish:"
+    @dish = gets.chomp
+  end
+  
+  def select_quantity
+    puts "Please enter quantity:"
+    @quantity = gets.chomp
+  end
+  
+  def select_continue
+    puts "Continue?"
+    @continue = gets.chomp
+  end
+  
+  # def place_order  
+  #       select_dish
+  #     select_quantity
+  #       @selection[@dish] = @quantity
+  #     select_continue
+      
+      
+  #     select_dish
+  #     select_quantity
+  #     select_continue
+  #     @selection[@dish] = @quantity
+      
+      
+  #   # @dish = nil
+  #     #@quantity = nil
+  # end
+  
+  def place_order
+    loop do
+      select_dish
+      select_quantity
+      @selection[@dish] = @quantity
+      #@dish = nil
+      #@quantity = nil
+      select_continue
+      p @selection
+      exit if @continue == "N"
+    end
+  end
+  
+  
+  # def place_order
+    
+  #   2.times do
+  #     select_dish
+  #     select_quantity
+  #     @selection[@dish] = @quantity
+  #     #@dish = nil
+  #     #@quantity = nil
+  #     select_continue
+  #     #p @continue
+  #   end
+  # end
+  
+  #   def place_order
+  #   while_continue = "Y"
+  #   while true do
+  #     select_dish
+  #     select_quantity
+  #     @selection[@dish] = @quantity
+  #     #@dish = nil
+  #     #@quantity = nil
+  #     select_continue
+  #     while_continue = @continue
+  #   end
+  # end
+  
+  def print_order
+    @selection
+  end
+  
 end
--- spec/customer_selection_spec.rb
+++ spec/customer_selection_spec.rb
@@ -1,4 +1,59 @@
 require "customer_selection"
+require "stringio"
 
 describe CustomerSelection do
+  let(:subject) { described_class.new }
+  let(:dish) { "croissant" }
+  let(:quantity) { 1 }
+
+  it "responds to selection_process method" do
+    expect(subject).to respond_to(:select_dish) 
+  end
+  
+  describe "#select_dish" do
+    let(:input) { StringIO.new("croissant") }
+    it "receives user input - dish" do
+      $stdin = input
+      expect { subject.select_dish }.to output("Please enter dish:\n").to_stdout
+      $stdin = STDIN
+    end
+  end
+  
+  describe "#select_quantity" do
+    let(:input) { StringIO.new("1") }
+    it "receives user input - quantity" do
+      $stdin = input
+      expect { subject.select_quantity }.to output("Please enter quantity:\n").to_stdout
+      $stdin = STDIN
+    end
+  end
+
+  describe "#select_continue" do
+    let(:input) { StringIO.new("1") }
+    it "receives user input - continue" do
+      $stdin = input
+      expect { subject.select_continue }.to output("Continue?\n").to_stdout
+      $stdin = STDIN
+    end
+  end
+
+  before do
+    allow($stdout).to receive(:write)
+  end
+  
+  describe "#place_order" do
+    let(:input1) { StringIO.new("croissant\n 2\n Y\n juice\n 3\n N\n") }
+    it "receives user input - dish" do
+      $stdin = input1
+      expect { subject.place_order }.to output("Please enter dish:\nPlease enter quantity:\nContinue?\nPlease enter dish:\nPlease enter quantity:\nContinue?\n").to_stdout
+      $stdin = STDIN
+    end
+  end
+  
+  describe "#print_order" do
+    it "prints selected dishes" do
+      expect(subject.print_order).to be_kind_of(Hash)
+    end
+  end
+  
 end
