3d52d843d7ce70f62af60c052db37c7a5ec684af
Jed Stanton <jedstanton@hotmail.com>
2020-04-27 00:30:24 +0100

    Unnecessary files deleted

--- spec/bill_spec.rb
+++ spec/bill_spec.rb
@@ -13,18 +13,4 @@ describe Bill do
       expect{ bill.pay }.to output("You've paid 3.0\n").to_stdout
     end
   end
-
-  let(:client) { double :client, messages: double()}
-
-  describe "#confirmation" do
-    it 'Sends a confirmation text' do
-
-      expect(client.messages).to receive(:create)
-
-      menu.order
-      menu.my_order.add_to_order("Egg Fried Rice", 2)
-      bill = Bill.new(menu.my_order)
-      bill.confirmation
-    end
-  end
 end
