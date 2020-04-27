ea4ec8d889140149966d16e11d16848ad80e7ee5
tanilserbes <serbestanil@gmail.com>
2020-04-27 06:30:35 +0100

    sms_spec file added

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -5,10 +5,10 @@ describe SMS do
   let(:messages) {double :messages}
   let(:config) do
     {
-      account_sid: "AC347e8287a5af16dd479242066115399d",
-      auth_token: "164a10ef8190c5616ffbd3ea0123ac0e",
-      from: "+12512921409",
-      to: "+447444101910",
+      account_sid: "123",
+      auth_token: "123",
+      from: "123",
+      to: "123",
       body: "Thank you! Your order will be delivered before %s "
     }
   end
