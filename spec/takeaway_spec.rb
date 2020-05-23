a33b8b5ddfc2938387e9ec25a9d11843a0d70797
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-23 16:35:45 +0100

    Refactored 1st view_dishes test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,9 +3,7 @@ require 'takeaway'
 describe Takeaway do 
 
   context '#view_dishes' do
-    it 'responds to takeaway object' do
-      expect(subject).to respond_to(:view_dishes)
-    end
+    it { is_expected.to respond_to(:view_dishes)}
 
     it 'returns list of dishes with prices' do 
       expect(subject.view_dishes).to eq({ chips: 2 })
