fa86f5d3d8e1379279c07a300ebec6c3d12ed781
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-23 17:22:20 +0100

    Refactored: added let method to return hash of takeaway_dishes

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,11 +2,12 @@ require 'takeaway'
 
 describe Takeaway do 
 
+  let(:takeaway_dishes) { {1 => "chips", 2 => "cheese burger", 3 => "chicken wings"} }
   context '#view_dishes' do
     it { is_expected.to respond_to(:view_dishes)}
 
     it 'returns list of dishes with prices' do 
-      expect(subject.view_dishes).to eq({ chips: 2 })
+      expect(subject.view_dishes).to eq(takeaway_dishes)
     end
   end
 end
