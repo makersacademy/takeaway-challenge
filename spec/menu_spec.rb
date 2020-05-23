1ab7a27293c1d9c0c0422f25e143d6eefb3b01ad
Rachael <rachaelewins@hotmail.com>
2020-05-23 16:40:12 +0100

    minor change to #check argument arrangement

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,12 +3,14 @@ require_relative '../lib/menu.rb'
 describe Menu do
   let(:name1) {double(:item1)}
   let(:name2) {double(:item2)}
+  let(:name3) {double(:item3)}
   let(:formatter) {double(:formatter)}
   let(:menu) {Menu.new([name1, name2], formatter)}
 
   before do
     allow(name1).to receive(:name) {"name1"}
     allow(name2).to receive(:name) {"name2"}
+    allow(name3).to receive(:name) {"name3"}
     allow(name1).to receive(:price) {1}
     allow(name2).to receive(:price) {1.5}
     allow(formatter).to receive(:format).with(1).and_return("£1.00")
@@ -24,10 +26,10 @@ describe Menu do
 
   describe '#check(item)' do
     it "should check the dish is on the menu (true)" do ## will need to be updated when later classes are made
-      expect(menu.check("name1")).to eq(true)
+      expect(menu.check(name1.name)).to eq(true)
     end
     it "should check the dish is on the menu (false)" do ## will need to be updated when later classes are made
-      expect(menu.check("name3")).to eq(false)
+      expect(menu.check(name3.name)).to eq(false)
     end
   end
 end
