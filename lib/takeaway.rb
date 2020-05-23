ea213e92a4b5f05f2682c12a37b131be620c67ed
Emanuele Pace <emanuele.10@outlook.it>
2020-05-23 13:21:38 +0100

    Create anh check menu hash

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,14 @@
 class Takeaway
 
+  def list_menu
+   {
+      rice: 2,
+      beef: 3,
+      pizza: 3.5,
+      beer: 2
+    }
+  end
+
+
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,10 +4,31 @@ describe Takeaway do
   subject { Takeaway.new }
 
   it 'check if a new takeaway project is an istance of Takeaway class' do
-    expect(subject).to be_an_instance_of Takeaway
+   expect(subject).to be_an_instance_of Takeaway
   end
+  context '#menu' do
+    it 'check if subject respond to a method menu' do
+      expect(subject).to respond_to(:list_menu)
+    end
 
-  it 'check see a list of dishes with prices' do
-    expect(subject.menu).to eq("Rice: 2.00£")
-  end
-end
+    # it 'check if a list of dishes with prices' do
+    #   expect(subject.list_menu).to eq(rice: 2.00)
+    # end
+
+    it 'subject needs back an hash' do
+      menu = {rice: 2,
+      beef: 3,
+      pizza: 3.5,
+      beer: 2}
+      expect(subject.list_menu).to eq(menu)
+    end
+
+
+
+  end #context #menu
+
+
+
+
+
+end #Describe
