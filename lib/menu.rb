38bcb577c85cd2677fff4ebdba98c7b5bd82eb9a
Nikita <latinisnotdead07@gmail.com>
2020-05-23 13:31:15 +0100

    returns a formatted list of dishes

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,4 +5,12 @@ attr_reader :dishes
   def initialize
     @dishes = {"Large Fish Supper" => 7.90, "Small Fish Supper" => 5.50,  "White Sausage" => 2.30, "Mars Bar in Batter" => 2.00}
   end
+
+  def print_menu
+    formatted_dishes_list = []
+    @dishes.each do |dish, price|
+      formatted_dishes_list << "#{dish} - #{'%.2f' %price}"
+    end
+    formatted_dishes_list
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,4 +6,12 @@ describe Menu do
   it 'has a list of dishes' do
     expect(menu.dishes).to be_an_instance_of(Hash)
   end
+
+  describe '#print_menu' do
+    it 'returns a formatted list of dishes' do
+      expect(menu.print_menu)
+        .to eq(["Large Fish Supper - 7.90", "Small Fish Supper - 5.50",
+          "White Sausage - 2.30", "Mars Bar in Batter - 2.00"])
+    end
+  end
 end
