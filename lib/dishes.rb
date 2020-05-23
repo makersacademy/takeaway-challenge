bc63db911fa242151209e0c7920564809447bb5e
Sophie <sophiebrown438@gmail.com>
2020-05-23 17:14:18 +0100

    Menu w prices in Dishes class

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -1,7 +1,21 @@
 class Dishes
   attr_accessor :menu
 
-  def view(menu)
-    @menu = menu
+  def initialize
+    @menu = {
+      "pavlova" => 4,
+      "financier" => 3.50,
+      "croissant" => 2,
+      "tart" => 3,
+      "gateau" => 15,
+      "canele" => 1.50
+    }
+  end
+
+  def view
+    new = @menu.each do | k, v|
+      puts "#{k} : £#{v.to_f}"
+    end
+    new
   end
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -2,21 +2,18 @@ require 'dishes'
 
 describe Dishes do
   subject { described_class.new }
-  let(:menu) { [{ toast: 3, cereal: 2, coffee: 2 }] }
 
   context "#view(dishes)" do
-    it "should respond to #view with one argument" do
-      expect(subject).to respond_to(:view).with(1).argument
-    end
-    
     it "should return a hash of dishes and prices" do
-      subject.view(menu)
-      expect(subject.menu).to eq menu 
-    end
-
-    it "should match the menu array " do
-      subject.view(menu)
-      expect(subject.menu).to match_array menu
+      expect(subject.view).to eq({
+        "pavlova" => 4,
+        "financier" => 3.50,
+        "croissant" => 2,
+        "tart" => 3,
+        "gateau" => 15,
+        "canele" => 1.50
+      })
+      # ("pavlova : £4\nfinancier : £3.50\ncroissant : £2\ntart : £3\ngateau : £15\ncanele : £1.50")
     end
   end 
 end
