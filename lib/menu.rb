f8c7263882f03de70c6bf0ccbea01449a53b309f
Nikita <latinisnotdead07@gmail.com>
2020-05-23 12:49:29 +0100

    has a list of dishes

---
+++ lib/menu.rb
@@ -0,0 +1,8 @@
+class Menu
+
+attr_reader :dishes
+
+  def initialize
+    @dishes = {"Large Fish Supper" => 7.90, "Small Fish Supper" => 5.50,  "White Sausage" => 2.30, "Mars Bar in Batter" => 2.00}
+  end
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,9 @@
+require 'menu'
+
+describe Menu do
+  subject(:menu) { Menu.new }
+
+  it 'has a list of dishes' do
+    expect(menu.dishes).to be_an_instance_of(Hash)
+  end
+end
