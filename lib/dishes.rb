c946c4f1abb21b7278f4185e1b922f91ce38cd56
Sophie <sophiebrown438@gmail.com>
2020-05-23 11:56:22 +0100

    First test. Testing, testing

---
+++ lib/dishes.rb
@@ -0,0 +1,6 @@
+class Dishes
+
+  def view
+    true
+  end
+end
---
+++ spec/dishes_spec.rb
@@ -0,0 +1,8 @@
+require 'dishes'
+
+describe Dishes do
+
+  it "should respond to dishes" do
+    expect(subject.view).to eq true
+  end
+end
