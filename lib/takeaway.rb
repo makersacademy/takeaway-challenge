c72c3ecb45149d4134e02d454469357f7209f358
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-23 11:37:07 +0100

    menu can be viewed from Takeaway class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,16 @@
 # As a customer
 # So that I can order the meal I want
 # I would like to be able to select some number of several available dishes
+require_relative 'menu'
 
 class Takeaway
-end 
+
+  def initialize
+    @menu = Menu.new
+  end
+
+  def view
+    @menu.view
+  end
+
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,7 +5,14 @@
 require 'takeaway'
 
 describe Takeaway do
+
   it 'Should have a takeaway class' do
     expect(Takeaway).to respond_to(:new)
+  end
+
+  describe '#view' do
+    it 'Should show items that can be ordered' do
+      expect{subject.view}.to output.to_stdout
+    end
   end
 end
