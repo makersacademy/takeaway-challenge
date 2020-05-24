cf4d9f09fe3b6351428f445d819c95079805c2d1
Tim <TimCPB@github.com>
2020-05-24 22:55:11 +0100

    order.rb passes 'responds to #add_to_order with 2 arguments'

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,16 @@
-require 'menu'
+require_relative 'menu'
 
-def intialize
-  @menu = Menu.new
-  @order = []
+class Order
+
+  attr_reader :order
+
+  def intialize
+    @menu = Menu.new
+    @order = []
+  end
+
+  def add_to_order(item, quantity)
+
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,6 +2,14 @@ require 'order'
 
 describe Order do
 
-  it ''
+  # it 'contains an empty array to which you will add an order' do
+  #   new_order = Order.new
+  #   expect(new_order.instance_variable_get(:@order)).to eq([])
+  # end
+
+
+  it 'adds an item to the order' do
+    expect(subject).to respond_to(:add_to_order).with(2).arguments
+  end
 
 end
