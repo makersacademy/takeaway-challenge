c32bca21a693b2568ec53d8e808f0a95690ce7c7
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-24 10:53:16 +0100

    Message initializes with current time + 1 hour

--- lib/message.rb
+++ lib/message.rb
@@ -1,2 +1,13 @@
+require 'twilio-ruby'
+require 'time'
+
 class Message
-end 
+
+  attr_reader :time
+
+  def initialize
+    @time = Time.now + 1*60*60
+    @time = @time.strftime("%k:%M")
+  end
+
+end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,7 +1,12 @@
 require 'message'
+require 'time'
 
 describe Message do
-  it ' Should have a message class' do
+  it 'Should have a message class' do
     expect(Message).to respond_to(:new)
+  end
+
+  it 'Should initialize with the current time + 1 hour' do
+    expect(subject.time).to eq((Time.now + 1*60*60).strftime("%k:%M"))
   end
 end
