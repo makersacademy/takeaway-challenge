965a60efb685b70ae75baab42f5a34d91c595ceb
Nikita <latinisnotdead07@gmail.com>
2020-05-25 17:50:30 +0100

    refactors to ensure non-nil return values for #see_menu ansd #see_order

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,7 +5,7 @@ class Menu
 attr_reader :dishes
 
   def initialize(formatter = Formatter.new)
-    @dishes = {"Large Fish Supper" => 7.90, "Small Fish Supper" => 5.50,  "White Sausage" => 2.30, "Mars Bar in Batter" => 2.00}
+    @dishes = {"Large Fish Supper" => 7.90, "Small Fish Supper" => 5.50,  "White Sausage" => 2.30, "Deep Fried Mars Bar" => 2.00}
     @formatter = formatter
   end
 
--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -10,12 +10,13 @@ class Takeaway
   end
 
   def see_menu
-    @menu.get_menu
+    puts @menu.get_menu
+    order_prompt
   end
 
   def select(dish,quantity = 1)
     if check_if_on_menu(dish)
-      puts "I'm sorry, we don't have that on our menu."
+      "I'm sorry, we don't have that on our menu."
     else
       price = get_price(dish)
       @order.add_selection(dish, quantity, price)
@@ -24,16 +25,19 @@ class Takeaway
 
   def see_order
     puts @order.get_order
-    print see_total
+    return see_total
   end
 
   def confirm_order(amount)
     raise "That isn't the right amount." if amount != @order.total
     send_sms
-    print "Order confirmed!"
   end
 
 private
+
+  def order_prompt
+    "What would you like to order?"
+  end
 
   def check_if_on_menu(dish)
      @menu.not_on_menu?(dish)
