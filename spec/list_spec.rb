5cd2a59b63b9ec88098de0d828220cb5837abf2d
victorvallet <victorvallet8@gmail.com>
2020-05-25 17:54:31 +0200

    Indent fixed

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -6,17 +6,18 @@ describe List do
     expect(subject.dishes).to be_empty
   end
 
-  describe '#add' do
-    dish = Dish.new('burger', 10)
-    it 'should add a new dish to the list of dishes' do
-      subject.add(dish)
-      expect(subject.print_list). to eq([{:name => 'burger', :price => 10}])
-    end
-  end
-
   describe '#print_list' do
     it 'should print the list of dishes' do
       expect(subject.print_list).to eq([])
     end
   end
+
+  describe '#add' do
+  dish = Dish.new('burger', 10)
+  it 'should add a new dish to the list of dishes' do
+    subject.add(dish)
+    expect(subject.print_list). to eq([{:name => 'burger', :price => 10}])
+  end
+  end
+
 end
