440942e72742c5144c9a04d5bb729be8d6497438
victorvallet <victorvallet8@gmail.com>
2020-05-25 19:02:05 +0200

    Used interpolation to output menu in a nice way

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,16 @@
 class Menu
   attr_reader :dishes
 
-  MY_MENU = {'marguerita':8, 'regina':10, 'buffala': 13, 'diavola': 12, 'romana': 11 }
+  MY_MENU = {:marguerita => 8, :regina => 10, :buffala => 13, :diavola => 12, :romana => 11 }
 
   def initialize
     @dishes = MY_MENU
   end
 
   def print_menu
-    @dishes
+    @dishes.map do |key, value| 
+      puts "* Pizza #{key}: £#{value}"
+    end
   end
 
 end
