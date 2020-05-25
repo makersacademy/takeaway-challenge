da64fc47f3fbe08bd7c111158fe0464b19cdec7c
Joshua Brook <joshuabrookuk@gmail.com>
2020-05-25 12:20:32 +0100

    GREEN: Menu class responded to view

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,4 @@
 class Menu
-end 
+  def view
+  end
+end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,7 +2,7 @@ require 'menu'
 
 describe Menu do
   let (:menu) {Menu.new}
-  it 'should respond_to view' do
+  it 'should respond to view' do
     expect(menu).to respond_to(:view)
   end
 end
