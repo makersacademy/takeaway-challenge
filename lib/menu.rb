c10e3019381ce889812da65e0055a67745b2dfe0
Joshua Brook <joshuabrookuk@gmail.com>
2020-05-25 15:24:38 +0100

    GREEN: Prints the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,19 @@
 class Menu
-  def view
+
+  attr_reader :list
+
+  def initialize
+    @list = {
+      "Apple" => 1,
+      "Banana" => 2,
+      "Mango" => 3,
+      "Peach" => 4,
+      "Pear" => 5
+      }
   end
+
+  def view
+    @list
+  end
+
 end
