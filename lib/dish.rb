cb202aa51fa68ad3ae3530a4e08293a2807fc6b2
victorvallet <victorvallet8@gmail.com>
2020-05-25 17:10:31 +0200

    Passed first test to check if dish has a name

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,2 +1,6 @@
 class Dish
+    attr_reader :name
+    def initialize(name)
+    @name = name
+    end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,5 +1,8 @@
 require 'dish'
 
 describe Dish do
-    
+    dish = Dish.new('burger')
+    it 'should have a name' do
+        expect(dish.name).to eq('burger')
+    end
 end
