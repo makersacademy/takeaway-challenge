e0a5ed3d00247a15eb5647befdf8c3955a09ad75
victorvallet <victorvallet8@gmail.com>
2020-05-25 17:38:04 +0200

    Passed test for the #print_list method

--- lib/list.rb
+++ lib/list.rb
@@ -1,6 +1,11 @@
 class List
     attr_reader :dishes
+
     def initialize
     @dishes = []
+    end
+
+    def print_list
+    @dishes
     end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,7 +1,13 @@
 require 'list'
 
 describe List do
-    it 'should have an empty list of dishes when initialized' do
-        expect(subject.dishes).to be_empty
+  it 'should have an empty list of dishes when initialized' do
+    expect(subject.dishes).to be_empty
+  end
+
+  describe '#print_list' do
+    it 'should print the list of dishes' do
+      expect(subject.print_list).to eq([])
     end
+  end
 end
