022b8d0cabfcbff8c200cb16aece378f160c3e82
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-25 14:39:04 +0100

    Add tests for auth_token and phone_num params

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -4,11 +4,17 @@ describe SMS do
   let(:text) { described_class.new("1234", "abc123", "+77") }
   
   context "#initialize" do
-  
-    it 'should initialise with an account_sid' do
+    it 'should initialize with an account_sid' do
       expect(text.account_sid).to eq("1234")
     end
 
+    it 'should initialize with auth_token' do
+      expect(text.auth_token).to eq("abc123")
+    end
+
+    it 'should initialize with a phone_number' do
+      expect(text.phone_number).to eq("+77")
+    end
   end
 
 end
