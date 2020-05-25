a0e82c3e47dd97a28fc1a8f0446e0feb93c378d4
Joshua Brook <joshuabrookuk@gmail.com>
2020-05-25 15:19:27 +0100

    RED: Should pring the menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,7 +2,12 @@ require 'menu'
 
 describe Menu do
   let(:menu) { Menu.new }
+
   it 'should respond to view' do
     expect(menu).to respond_to(:view)
+  end
+
+  it 'should print the menu' do
+    expect(menu.view).to eq ({"Apple" => 1, "Banana" => 2, "Mango" => 3, "Peach" => 4, "Pear" => 5})
   end
 end
