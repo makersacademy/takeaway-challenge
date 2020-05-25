b9d103cecf4d0902776559b4bdec56ba7fe08a1d
victorvallet <victorvallet8@gmail.com>
2020-05-25 22:34:56 +0200

    Thanks again rubocop!

--- lib/order.rb
+++ lib/order.rb
@@ -17,7 +17,8 @@ class Order
   def total
     sum = 0
     Menu::MY_MENU.map { |item, price| 
-    @order.map { |o| sum += o[:quantity]*price if o[:item] == item } }
+      @order.map { |o| sum += o[:quantity] * price if o[:item] == item } 
+    }
     sum
   end
 end
