d2dc5629ca73c7a433986eb49ac5c61925df623d
victorvallet <victorvallet8@gmail.com>
2020-05-25 20:36:31 +0200

    Added first before block to access @order

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,13 +1,20 @@
 require 'order'
 
 describe Order do
-   
+
+   before do
+     @order = []
+   end
+
   it 'should show the order' do
-    expect(subject.order).to eq([])
+    expect(subject.order).to eq(@order)
   end
+
   describe '#place' do
-    it 'should store x number of asked item ' do
+
+    it 'should store the item and the quantity asked for' do
       expect(subject.place(:marguerita, 2)).to eq([{ :item => :marguerita, :quantity => 2 }])
     end
+    
   end
 end
