b715ca6bc070458badf9164985a4f40958b655bd
Tim <TimCPB@github.com>
2020-05-25 12:45:23 +0100

    order.rb now passing 'contains an empty array to which you will add an order' test

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,10 +2,10 @@ require 'order'
 
 describe Order do
 
-  # it 'contains an empty array to which you will add an order' do
-  #   new_order = Order.new
-  #   expect(new_order.instance_variable_get(:@order)).to eq([])
-  # end
+  it 'contains an empty array to which you will add an order' do
+    new_order = Order.new
+    expect(new_order.instance_variable_get(:@selection)).to eq([])
+  end
 
   it 'responds to #add_to_order with 2 arguments' do
     expect(subject).to respond_to(:add_to_order).with(2).arguments
