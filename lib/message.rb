6e4bbbbe4559acb36c8a21e10953400b03b21f8f
Tim <TimCPB@github.com>
2020-05-26 01:29:26 +0100

    Tried to refactor message.rb

--- lib/message.rb
+++ lib/message.rb
@@ -3,8 +3,8 @@ class Message
   require 'date'
 
   def initialize
-    account_sid = 'AC70d417911c9dc33b2d343c6be8956f4f'
-    auth_token = '3f5ac212ee68ad4e9c09a5e61d88490c'
+    account_sid = ENV['AC70d417911c9dc33b2d343c6be8956f4f']
+    auth_token = ENV['2e1d5c0deb1c676fb13d5472c1758950']
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
@@ -12,7 +12,7 @@ class Message
     time = (Time.now + 3600).strftime("%I:%M")
     @client.messages.create(
       from: '+12069845087',
-      to: '+447919841263',
+      to: ENV['+447919841263'],
       body: "Thank you! Your order has been placed and will be with you before #{time}"
     )
   end
