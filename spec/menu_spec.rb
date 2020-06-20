cdf4cb4db7075accc3e2ab16e94393a03261ba1c
stupot1 <scpitc@gmail.com>
2020-06-20 17:46:46 +0000

    Rubocop-ed...

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,13 +4,13 @@ describe Menu do
   
   describe '#initialaise' do
     it 'should initialize and create a menu with 2 food items' do
-      expect(subject.menu.count).to eq(2)
+      expect(subject.menu.count).to eq 2
     end
   end
   
   describe '#return_price' do
     it 'should return the price of an item' do
-      expect(subject.return_price('food1')).to eq ("1.00")
+      expect(subject.return_price('food1')).to eq "1.00"
     end
   end
   
