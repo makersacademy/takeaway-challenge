fdb1355c18af84520163b2190af485cc417f41ac
stupot1 <64636440+stupot1@users.noreply.github.com>
2020-06-21 11:43:50 +0100

    Removed personal data

--- lib/text.rb
+++ lib/text.rb
@@ -2,11 +2,11 @@ require 'twilio-ruby'
 
 def send_text(message)
 
-  @client = Twilio::REST::Client.new 'ACcfe12bfc3a16eac82da99718c4016fcb', '4f57e5dc9dac1bccec5cd0b197b92324'
+  @client = Twilio::REST::Client.new 
   
   @client.messages.create(
     from: "+441273917629",
-    to: '+447958006866',
+    to: '',
     body: message
   )
   
