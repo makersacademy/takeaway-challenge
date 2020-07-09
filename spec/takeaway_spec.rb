a9801347161109cea5e96e30ba1f77579fbebe7a
Catriona <catrionamairi@blueyonder.co.uk>
2020-07-09 16:13:33 +0100

    Amend takeaway tests to use double

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,18 +1,23 @@
 require 'takeaway'
+require 'stringio'
 
 describe Takeaway do
-  let(:items) { "Burger" }
-  let(:quantities) { 1 }
+  let(:orders) { double('ordering') }
+  let(:takeaway) { Takeaway.new(orders) }
 
   describe '#view_menu' do
     it 'prints a menu' do
-      expect(subject.view_menu).to include({ Burger: 4 })
+      expect(takeaway.view_menu).to include({ Burger: 4 })
     end
   end
 
   describe '#check_order' do
+    let(:total) { StringIO.new("4") }
     it 'raises error if amount is incorrect' do
-      expect { subject.check_order }.to raise_error "Please recheck"
+      allow(orders).to receive(:ordered) { { Burger: 1, Chips: 1 } }
+      $stdin = total
+      expect { takeaway.check_order }.to raise_error "Please recheck"
+      $stdin = STDIN
     end
   end
 end
