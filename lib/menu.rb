46e24af16621baa2433c865319bd75f8f565593f
Student <student@Makerss-Air.broadband>
2020-07-19 10:53:52 +0100

    Added ability to save and load menus

--- lib/menu.rb
+++ lib/menu.rb
@@ -11,4 +11,22 @@ class Menu
   def add(dish)
     @dishes << dish
   end
+
+  def save
+    file = File.open("menu.csv", "w")
+    @dishes.each do |dish|
+      menu_item = [dish.name, dish.price]
+      csv_line =  menu_item.join(",")
+      file.puts csv_line
+    end
+    file.close
+  end
+  def load(filename = "menu.csv")
+    file = File.open(filename, "r")
+    file.readlines.each do |line|
+      name, price = line.chomp.split(",")
+      @dishes << Dish.new(name, price)
+    end
+    file.close
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,7 +7,7 @@ describe Menu do
 #  it 'lists available dishes' do
 #    expect(subject.dishes).not_to be_nil
 #  end
-  let(:dish) { double "dish" }
+  let(:dish) { double "dish", :name => "garlic bread", :price => 3.95 }
   it 'allows dishes to be added' do
     subject.add(dish)
     expect(subject.dishes).to include(dish)
@@ -15,5 +15,15 @@ describe Menu do
   it 'prints the menu' do
     subject.add(dish)
     expect(subject.dishes).to eq([dish])
+  end
+  it 'allows you to save a dish for future use' do
+    subject.add(dish)
+    subject.save
+    subject.load
+    expect(subject.dishes).to include(dish)
+  end
+  it 'allows you to load a past menu' do
+    subject.load("irb_menu.csv")
+    expect(subject.dishes).not_to be_empty
   end
 end
