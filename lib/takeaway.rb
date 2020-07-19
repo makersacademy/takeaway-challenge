f45df6ec7a21b2b580b22d66a9060bd6a1b8ed9b
Patricia <prmonday@gmail.com>
2020-07-19 16:44:44 +0100

    Select dishes from menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,19 @@
 class Takeaway
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order || Order.new
   end
   def printed_menu
     menu.print
   end
+def place_order(dishes)
+  dishes.each do |dish, quantity|
+  order.add(dish, quantity)
+  end
+end
+
+
   private
-  attr_reader :menu
+  attr_reader :menu, :order
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,11 +1,17 @@
 require 'takeaway'
 describe Takeaway do
-  subject(:takeaway) { described_class.new(menu: menu) }
+  subject(:takeaway) { described_class.new(menu: menu, order: order) }
 
   let(:menu){double(:menu, print: printed_menu)}
+  let(:order) { double(:order)}
   let(:printed_menu) {"Avocado salad: £4.50"}
+  let(:dishes) {{avocado: 2, lentils: 1}}
 
     it 'shows the menu with dishes and prices' do
       expect(takeaway.printed_menu).to  eq(printed_menu)
     end
-end
+    it 'order a number of dishes available' do
+      expect(order).to receive(:add).twice
+      takeaway.place_order(dishes)
+    end
+  end
