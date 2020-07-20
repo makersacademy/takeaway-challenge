890edc2cdaf08de69b98fb52ca73de16c2f9355d
Student <student@Makerss-Air.broadband>
2020-07-20 08:26:27 +0100

    Text functionality created, not working with environment variables

--- send_sms.rb
+++ send_sms.rb
@@ -1,11 +1,11 @@
 require 'twilio-ruby'
 
-account_sid = 'AD45ff85090d1846a01c79p1903749m68c'
-auth_token = '9179f71s9294b8p2435s0464db5rn929'
+account_sid = ENV["TWILIO_ACCOUNT_SID"]
+auth_token = ENV["TWILIO_AUTH_TOKEN"]
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = '+447411041875' # Your Twilio number
-to = '+15555555555' # Your mobile phone number
+from = ENV["TWILIO_NUMBER"] # Your Twilio number
+to = ENV["MY_NUMBER"] # Your mobile phone number
 
 client.messages.create(
 from: from,
