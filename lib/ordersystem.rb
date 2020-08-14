6c87d68c24c1c1b90d3a8d4632bc99ee48b63f1a
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-14 13:23:09 +0100

    Added failing tests to check that the order array increases when a dish is added

--- lib/ordersystem.rb
+++ lib/ordersystem.rb
@@ -1,10 +1,11 @@
 require_relative 'takeaway_kitchen'
 
 class OrderSystem
-  attr_reader :menu
+  attr_reader :menu, :order
 
   def initialize
     @menu = TakeawayKitchen.new.menu
+    @order = []
   end
 
   def view_menu
--- spec/ordersystem_spec.rb
+++ spec/ordersystem_spec.rb
@@ -1,17 +1,29 @@
 require 'ordersystem'
 
 describe OrderSystem do
-  let(:dish) { double :dish }
-  let(:price) { double :price }
+  let(:dish) { double :dish}
   let(:takeaway) { TakeawayKitchen.new.menu }
   
   it 'returns the menu from TakeawayKitchen' do
     expect(subject.menu).to eq takeaway
   end
 
+  it 'initializes with an empty order' do
+    expect(subject.order).to be_empty
+  end
+
   describe '#view_menu' do
     it 'returns a hash' do
       expect(subject.view_menu).to include({ dish: be_an(String), price: be_an(Integer) })
+    end
+  end
+
+  describe '#add_to_order' do
+    it { is_expected.to respond_to(:add_to_order).with(2).argument }
+
+    it 'increases order count' do
+      subject.add_to_order(dish)
+      expect(subject.order.count).to eq 1
     end
   end
 end
