88257f72fb0098c6f08d3a4b14330309694e2715
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-14 12:15:00 +0100

    created order class, created instance of order set to @order in takeaway, gave order attr_reader, passes tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,11 @@
 require_relative 'menu'
+require_relative 'order'
 
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :order
 
   def initialize
     @menu = Menu.new
+    @order = Order.new
   end
 end
