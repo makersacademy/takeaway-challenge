8a6dc6ea3c86c6eab7bd4f57afa0ea18b0deb921
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-14 12:31:22 +0100

    added tests to check if Order and Menu are instances of themselves, passes tests

---
+++ spec/menu_spec.rb
@@ -0,0 +1,7 @@
+require 'menu'
+
+describe Menu do
+  it "should exist" do
+    expect(subject).to be_instance_of Menu
+  end
+end
---
+++ spec/order_spec.rb
@@ -0,0 +1,7 @@
+require 'order'
+
+describe Order do
+  it "should exist" do
+    expect(subject).to be_instance_of Order
+  end
+end
