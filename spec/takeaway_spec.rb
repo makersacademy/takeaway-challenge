2df1f3542d7413a2a78685c92ef81582aebe126c
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-14 12:11:13 +0100

    added failing test for takeaway to initialize with intance of order class accessible with .order

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,7 +4,11 @@ describe Takeaway do
   it "should exist" do
     expect(subject).to be_instance_of Takeaway
   end
-  it 'intialize with instance of menu accessible with .menu' do
+  it 'intializes with instance of menu accessible with .menu' do
     expect(subject.menu).to be_instance_of Menu
   end
+  it 'intializes with instance of order accessible with .order' do
+    expect(subject.order).to be_instance_of Order
+  end
+
 end
