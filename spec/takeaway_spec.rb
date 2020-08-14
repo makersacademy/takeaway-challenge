1fa8e116afab70c6ce830d0d576cdf59f4022ba2
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-14 12:03:00 +0100

    corrected 1 test in takeaway_spec, created class Takeaway, passes 1 test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,7 +2,7 @@ require 'takeaway'
 
 describe Takeaway do
   it "should exist" do
-    expect(subject).to be_instance of Takaway
+    expect(subject).to be_instance_of Takeaway
   end
   it 'should respond to menu' do
     expect(subject).to respond_to(:menu)
