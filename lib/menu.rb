510bd236f42c6a48416c312fecfad073d9c0c3bc
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-14 12:38:29 +0100

    added test to check if menu initializes with a menu including at least one food item, added initialize to menu, it creats a menu instance varibiable currently with a static menu of 2 items

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,9 @@
 class Menu
-
+  attr_reader :menu
+  def initialize
+    @menu = [
+      { :item => "fried rice", :price => 3.50 },
+      { :item => "sweet and sour pork", :price => 4.00 }
+    ]
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,4 +4,7 @@ describe Menu do
   it "should exist" do
     expect(subject).to be_instance_of Menu
   end
+  it 'initializes with a menu including at least 1 food item' do
+    expect(subject.menu.count).to be >= 1
+  end
 end
