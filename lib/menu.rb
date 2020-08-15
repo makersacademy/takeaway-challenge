a8fbc3136012acd6dd2620407054200abcf79a77
joshSteadman <josh.steadman15@gmail.com>
2020-08-15 18:59:55 +0100

    changed test to check menu will be printed

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,11 @@
 
 class Menu
 
+    def initialize
+        @menu = {:Burger => 5.00, :Pizza => 4.00, :Fish => 6.00, :Chicken => 5.00}
+    end
+    
     def view
+      @menu
     end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -8,8 +8,8 @@ describe Menu do
   it { is_expected.to respond_to(:view) }
 
   describe '#view' do
-    it 'will output the menu' do
-        expect(subject.view).to eq({:Burger => 5.00, :Pizza => 4.00, :Fish => 6.00, :Chicken => 5.00})
+    it 'will print the menu' do
+        expect(subject.view).to output({:Burger => 5.00, :Pizza => 4.00, :Fish => 6.00, :Chicken => 5.00}).to_stdout
     end
   end
 end
