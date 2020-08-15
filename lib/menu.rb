6d1af8b93e26940a9e999e86a29b575eda871762
joshSteadman <josh.steadman15@gmail.com>
2020-08-15 18:41:35 +0100

    tests menu responds to view method

---
+++ lib/menu.rb
@@ -0,0 +1,6 @@
+
+class Menu
+
+    def view
+    end
+end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,4 +3,7 @@ require 'menu'
 describe Menu do
   it 'creates an instance of Menu' do
     expect(subject).to be_an_instance_of(Menu)   
+  end
+
+  it { is_expected.to respond_to(:view) }
 end
