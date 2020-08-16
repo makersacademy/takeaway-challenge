8a68ef7755d85b0e72fa5ee738862e1f41e9ae74
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-16 15:05:05 +0100

    add to order method tests failed

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,4 +6,16 @@ describe Order do
     expect(subject.order).to eq([])
   end
 
+  describe "#add" do
+    it { is_expected.to respond_to(:add).with(3).arguments }
+
+    it "pushes hash onto @order" do
+      subject.add("spring rolls",1,1.99)
+      item = {:item=> "spring rolls", :quantity=> 1, :price=> "£1.99 each"}
+      expect(subject.order).to eq([item])
+    end
+
+
+  end
+
 end
