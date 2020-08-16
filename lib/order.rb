fbdac7d7777e2ee2b1090582f86a8978b767bc30
imogenkraak <imogen.kraak@gmail.com>
2020-08-16 15:57:44 +0100

    Order comfirmation text working

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,11 @@
 require_relative 'menu'
+require_relative 'text'
+
+require 'twilio-ruby'
 
 class Order
 
-  include Menu
+  include Menu, Text
   attr_accessor :customer_order
 
   def initialize
@@ -14,6 +17,9 @@ class Order
     puts "This is our takeaway menu for today:"
     show_menu
     customer_selection
+    show_order
+    order_price
+    send_message
   end
 
   def show_menu
@@ -34,9 +40,17 @@ class Order
         break
       elsif DISHES.keys.include? @dish
         add_dish
-      else !DISHES.keys.include? @dish
+      elsif !DISHES.keys.include? @dish
         puts "This item isn't on the menu, please select something else."
       end
+    end
+  end
+
+  def show_order
+    puts "Order summary:"
+    @customer_order.each do |name, quantity|
+      price_of_item = DISHES[name]
+      puts "x#{quantity} #{name}- £#{price_of_item}"
     end
   end
 
@@ -44,10 +58,9 @@ class Order
   def order_price
     prices = []
     @customer_order.each do |name, quantity|
-    price_of_item = DISHES[name]
-    price = price_of_item * quantity.to_f
-
-    prices.push(price)
+      price_of_item = DISHES[name]
+      price = price_of_item * quantity.to_f
+      prices.push(price)
     end
      total = prices.sum
      puts "The total price of your order is £#{total.ceil(2)}"
---
+++ lib/text.rb
@@ -0,0 +1,31 @@
+require 'dotenv'
+Dotenv.load('.env')
+
+module Text
+
+   def send_message
+     account_sid = ENV['TWILIO_ACCOUNT_SID']
+     auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+     from = ENV['TWILIO_NUMBER']
+     to = ENV['MY_NUMBER']
+
+     @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+     @client.messages.create(
+     from: from,
+     to: to,
+     body: "Your order has been placed succesfully and should arrive before #{time}. Enjoy."
+     )
+
+     puts "Your order has been placed succesfully and should arrive before #{time}. Enjoy."
+
+   end
+
+
+  def time
+    time = Time.now + 1800
+    time.strftime("%k:%M")
+  end
+
+end
