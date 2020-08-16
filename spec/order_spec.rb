5f7fe4dac63e213e0ddc0725b2b31a96bc93e22c
Zish Jawaid <zisheanj@gmail.com>
2020-08-16 17:36:34 +0100

    Added order class and test for adding dishes to the order

---
+++ spec/order_spec.rb
@@ -0,0 +1,15 @@
+require 'order'
+
+describe Order do
+
+  subject(:order) {described_class.new}
+  let(:dishes) { {Burger: 1, Pizza: 2} }
+
+  it 'adds dishes to the order' do
+    order.add(:Burger, 1)
+    order.add(:Pizza, 1)
+    expect(order.dishes).to eq(dishes)
+  end
+
+
+end
