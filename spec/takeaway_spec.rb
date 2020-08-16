23202c88e641ca6034a10fac447dd57f2ed3609e
Zish Jawaid <zisheanj@gmail.com>
2020-08-16 16:34:13 +0100

    Added test that allows customer to order different quantities of available dishes

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,6 +5,7 @@ describe Takeaway do
   subject(:takeaway) { described_class.new(menu: menu) }
     
   let(:menu) { double("menu") }
+  let(:dishes) { {Pizza: 1, Burger: 1} }
    
   it 'displays a menu showing the dishes and prices' do
     allow(menu).to receive(:view).and_return("Pizza: £11.99")
@@ -12,4 +13,9 @@ describe Takeaway do
     expect(takeaway.view_menu).to eq("Pizza: £11.99")
   end
   
+  it 'allows customer to order different quantities of available dishes' do
+    expect(takeaway.order(dishes)).to eq("The total for your order is £27.90")
+
+  end   
+
 end
