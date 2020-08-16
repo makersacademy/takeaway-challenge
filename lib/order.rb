fa621c5a1bf51386c1184c2184fa1f8af8a6f1ff
Tamara Jeffrey <just-tam@live.com>
2020-08-16 17:30:45 +0100

    failing test for calculating order total

--- lib/order.rb
+++ lib/order.rb
@@ -6,9 +6,14 @@ class Order
 
   def initialize
     @order = []
+    @order_total = nil
   end
 
   def add_to_order(item, quantity=1)
     @order << {item => quantity}
+  end
+
+  def order_total
+
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,11 +9,23 @@ describe Order do
     expect(subject).to respond_to(:add_to_order)
   end
 
+  it "Expects order to respond to total_of_order" do
+    expect(subject).to respond_to(:order_total)
+  end
+
   let(:order) {Order.new}
 
   describe '#add_to_order' do
     it "Expects item to be added to an order" do
       expect{(order.add_to_order("Pizza"))}.to change { order.order.count}.by (1)
+    end
+  end
+
+  describe '#order_total' do
+    it "Expects correct total of order" do
+      order.add_to_order("Pizza")
+      order.add_to_order("Cheeseburger")
+      expect(order.order_total).to eq(12)
     end
   end
 end
