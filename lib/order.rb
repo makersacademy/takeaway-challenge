21b6d631ee14d6f279fca259bca32207c6b6f624
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-16 19:01:58 +0100

    edge case adding to quantity only test passed

--- lib/order.rb
+++ lib/order.rb
@@ -13,7 +13,8 @@ class Order
   end
 
   def add(item, quantity, price)
-    @order << { :item => item, :quantity => quantity, :price => price }
+    @order << { :item => item, :quantity => 0, :price => price } unless already_in_order(item)
+    update_quantity(item, quantity)
   end
 
   def total
@@ -24,4 +25,17 @@ class Order
     return total.round(2)
   end
 
+  private
+  def already_in_order(item)
+    @order.each do |hash|
+      return true if hash.value?(item)
+    end
+    false
+  end
+
+  def update_quantity(item, quantity)
+    @order.each do |hash|
+      hash[:quantity] += quantity if hash.value?(item)
+    end
+  end
 end
