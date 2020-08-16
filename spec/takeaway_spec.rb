dd44dbaad903e89f2f9c442ae251b16af6bc37af
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-16 11:43:05 +0100

    view_menu method test failed

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,16 +1,25 @@
+require 'takeaway'
+
 describe Takeaway do
 
   let(:menu) { double("menu") }
   let(:order) { double("order") }
 
-  let(subject) { described_class(menu,order) }
+  subject { described_class.new(:menu, :order) }
 
   it "has a menu" do
-    expect(subject.menu).to eq menu
+    expect(subject.menu).to eq(:menu)
   end
 
   it "has an order" do
-    expect(subject.order).to eq order
+    expect(subject.order).to eq(:order)
+  end
+
+  describe "#view_menu" do
+    it "returns @menu.view" do
+      expect(menu).to receive(:view)
+      subject.view_menu
+    end
   end
 
 end
