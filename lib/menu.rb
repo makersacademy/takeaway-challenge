162771fc53d9ffc57c07de6a42c9907b51de94d8
Tamara Jeffrey <just-tam@live.com>
2020-08-16 15:46:31 +0100

    failing test for view menu item with a price

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,4 +5,8 @@ attr_reader :dishes
   def initialize
     @dishes = { "Pizza" => 8, "Chicken wings" => 5, "Chips" => 2, "Battered cod" => 6, "Cheeseburger" => 4, "Veggie burger" => 4 }
   end
+
+  def view_dishes
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -13,10 +13,21 @@ describe Menu do
     expect(subject.dishes).to eq ({ "Pizza" => 8, "Chicken wings" => 5, "Chips" => 2, "Battered cod" => 6, "Cheeseburger" => 4, "Veggie burger" => 4 })
   end
 
+  it "Expects menu to respond to view_dishes" do
+    expect(subject).to respond_to(:view_dishes)
+  end
+
+  let(:menu) {Menu.new}
+
   describe '#dishes' do
     it "Expects to see price of select dish" do
-      menu = Menu.new
       expect(menu.dishes["Cheeseburger"]).to eq(4)
+    end
+  end
+
+  describe '#view_dishes' do
+    it "Expexts to see a menu item and price" do
+      expect(menu.view_dishes).to include("Cheeseburger: £4")
     end
   end
 
