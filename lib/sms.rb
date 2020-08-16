aec21f390cc695ba3a8850e0dccd9e422ed2702f
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-16 13:52:35 +0100

    Added the sms file

---
+++ lib/sms.rb
@@ -0,0 +1,14 @@
+require 'twilio-ruby'
+
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+from = ENV['TWILIO_NUMBER'] # Your Twilio number
+to = ENV['CUSTOMER_NUMBER'] # Your mobile phone number
+
+client.messages.create(
+from: from,
+to: to,
+body: "testing my text"
+)
