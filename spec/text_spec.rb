4c58cf0805e0598ed14e3ba83f577e4ffb756317
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-16 12:16:02 +0100

    added two tests for text class, tests it exists and method send_text takes 1 argument, passes tests

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -1,1 +1,12 @@
 require 'text'
+
+describe Text do
+  it 'exists' do
+    expect(subject).to be_instance_of Text
+  end
+  describe '#send_text' do
+    it 'takes one argument' do
+      expect(subject).to respond_to(:send_text).with(1).argument
+    end
+  end
+end
