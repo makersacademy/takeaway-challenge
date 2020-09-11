33b7f051b0dedcf68af177f85023af3f49771780
ollie beney <ombeney@gmail.com>
2020-09-11 20:07:55 +0100

    now stores orders in an array of hashes using a select method

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,28 @@
 require './lib/menu'
 class Order
-  attr_reader :order_price, :valid_item
+  attr_reader :order_price, :valid_item#, :current_order
   def initialize
     @menu = Menu.new
+   # @basket = Basket.new
     @current_order = []
     @order_price = 0
   end
 
+  def select(*items)
+    fail "Please select at least one dish" if items.empty?
+    items.each do |item|
+    @menu.food_items.each do |dish_hash|
+      @current_order << dish_hash if dish_hash[:food] == item
+
+
+    end
+  end
+  
+  
+    
+   
+  end
+    
 
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,5 +4,23 @@ describe Order do
   it "has an instance of Menu class as an instance variable" do
     expect(subject.instance_variable_get(:@menu)).to be_an_instance_of(Menu)
   end
+  it {is_expected.to respond_to(:select)}
+  describe "#select" do
 
+
+    it "is expected to raise an error if no dish selected" do
+      expect { subject.select() }.to raise_error "Please select at least one dish"
+    end
+  
+    it "should store a single order order" do
+      subject.select("roadkill jumbalaya")
+     expect(subject.instance_variable_get(:@current_order)).to eq ([{ food: "roadkill jumbalaya", price: 7 }])
+      #expect(subject.current_order).to eq ([{ food: "roadkill jumbalaya", price: 7 }])
+    end
+
+    it "should store multiple item orders" do
+      subject.select("roadkill jumbalaya", "stawberry lasagne", "vinegar yogurt")
+      expect(subject.instance_variable_get(:@current_order)).to eq ([{ food: "roadkill jumbalaya", price: 7 }, { food: "stawberry lasagne", price: 20 }, { food: "vinegar yogurt", price: 3 }])
+    end
+  end
 end
