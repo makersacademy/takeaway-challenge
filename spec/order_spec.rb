4d4af89cdf80e5bae4f5d40d40baf6983f794662
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-12 13:03:43 +0100

    added check that adding to order does not raise an error

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,7 +3,13 @@ require "dish"
 require "order"
 
 describe Order do
-  let(:order) { Order.new }
+  let(:menu) {
+    menu = [Dish.new("Soul Bowl", 13.99),
+            Dish.new("Tofu Donburi", 13.50),
+            Dish.new("Buffalo Chick'n Burger", 10.00),
+            Dish.new("Gyoza", 6.50)]
+  }
+  let(:order) { Order.new(menu) }
 
   it "expects order to be initalised with a menu" do
     expect(Order).to respond_to(:new).with(1).argument
@@ -14,7 +20,8 @@ describe Order do
       expect(order).to respond_to(:add).with(2).arguments
     end
 
-    it do
+    it "adds the dish to the order without raising an error" do
+      expect { order.add("Gyoza", 2) }.not_to raise_error
     end
   end
 end
