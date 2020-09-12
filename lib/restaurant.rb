85cad420ba6d328b9fdd65b0f3bd3b5c2ec89396
Veronica Lee <veronicavlee@icloud.com>
2020-09-12 18:22:51 +0100

    Remove setup methods from Restaurant
    Initialise @takeaway on load of Takeaway file

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,31 +1,10 @@
-require_relative 'dish'
+require_relative 'menu'
 
 class Restaurant
-  attr_reader :menu, :dish_class
+  attr_reader :menu
 
-  SAVED_MENU = [
-    { name: 'Tom yum soup', price: 4.50 },
-    { name: 'Pad thai', price: 8.95 },
-    { name: 'Pad see yew', price: 8.95 },
-    { name: 'Egg fried rice', price: 3.89 }
-  ]
-
-  def initialize(dish_class = Dish)
-    @menu = []
-    @dish_class = dish_class
-    setup_menu
-  end
-
-  private
-
-  def setup_menu
-    SAVED_MENU.each do |dish|
-      @menu << create_dish(dish[:name], dish[:price])
-    end
-  end
-
-  def create_dish(name, price)
-    @dish_class.new(name, price)
+  def initialize(menu)
+    @menu = menu
   end
 
 end
--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,20 +1,26 @@
 require_relative 'restaurant'
+require_relative 'menu'
+require_relative 'dish'
 
 class Takeaway
   attr_reader :restaurant
 
-  def initialize(restaurant = Restaurant.new)
+  def initialize(restaurant)
     @restaurant = restaurant
-    @menu = restaurant.menu
   end
 
-  def view_menu
-    puts format_menu.join("\n")
-  end
-
-  private
-
-  def format_menu
-    @menu.map { |dish| "#{dish.name} - £#{'%.2f' % dish.price}" }
+  def print_menu
+    puts @restaurant.menu.format
   end
 end
+
+@takeaway = Takeaway.new(
+  Restaurant.new(
+    Menu.new(
+      Dish.new('Tom yum soup', 4.50),
+      Dish.new('Pad thai', 8.95),
+      Dish.new('Pad see yew', 8.95),
+      Dish.new('Egg fried rice', 3.89),
+    )
+  )
+)
