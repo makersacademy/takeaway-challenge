4c89b9e761df4b3199cbe84f6e9e64e8048617e6
Veronica Lee <veronicavlee@icloud.com>
2020-09-12 21:15:42 +0100

    Add initial tests and method for order.add()

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,15 @@
+require 'restaurant'
+
 class Order
   attr_reader :restaurant, :basket
 
   def initialize(restaurant)
     @restaurant = restaurant
     @basket = []
+  end
+
+  def add(dish_name, qty)
+    @dish_name = dish_name
+    @qty = qty
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,9 +2,36 @@ require 'order'
 
 describe Order do
 
-  subject(:order) { Order.new }
+  subject(:order) { Order.new(restaurant_dbl) }
+  let(:restaurant_dbl) { double('restaurant', find_dish: dish_dbl) }
+  let(:dish_dbl) { double('dish') }
 
-  it 'initalises with an empty basket' do
+  it 'takes a restaurant as an argument' do
+    expect(Order).to respond_to(:new).with(1).argument
+  end
+
+  it 'stores the given restaurant' do
+    expect(order.restaurant).to eq(restaurant_dbl)
+  end
+
+  it 'initialises with an empty basket' do
     expect(order.basket).to eq []
   end
+
+  describe '#add(dish_name, qty)' do
+    it 'takes a dish name and quantity as arguments' do
+      expect(order).to respond_to(:add).with(2).arguments
+    end
+
+    it 'looks up the dish from the given dish name' do
+      expect(restaurant_dbl).to receive(:find_dish).once.with('Pepperoni pizza')
+
+      order.add('Pepperoni pizza', 1)
+    end
+
+    it 'adds the dish and selected quantity to the basket' do
+
+    end
+  end
+
 end
