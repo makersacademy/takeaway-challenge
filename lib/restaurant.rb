0e878a7a045168a161405d760cc4626069a82342
Veronica Lee <veronicavlee@icloud.com>
2020-09-12 13:29:46 +0100

    Setup saved restaurant menu

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,9 +1,29 @@
 class Restaurant
   attr_reader :menu, :dish_class
 
+  SAVED_MENU = [
+    { name: 'Tom yum soup', price: 4.50 },
+    { name: 'Pad thai', price: 8.95 },
+    { name: 'Pad see yiew', price: 8.95 },
+    { name: 'Egg fried rice', price: 3.89 }
+  ]
+
   def initialize(dish_class = 'Dish')
     @menu = []
     @dish_class = dish_class
+    setup_menu
+  end
+
+  private
+
+  def setup_menu
+    SAVED_MENU.each do |dish|
+      @menu << create_dish(dish[:name], dish[:price])
+    end
+  end
+
+  def create_dish(name, price)
+    @dish_class.new(name, price)
   end
 
 
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -3,13 +3,24 @@ require 'restaurant'
 describe Restaurant do
 
   subject(:restaurant) { Restaurant.new(dish_class_double) }
-  let(:dish_class_double) { double('dish_class')}
+  let(:dish_class_double) { double('dish_class', new: dish_double) }
+  let(:dish_double) {double('dish_double') }
 
   it 'has a menu' do
     expect(restaurant).to respond_to(:menu)
   end
 
-  it 'has a dish_class' do
-    expect(restaurant).to respond_to(:dish_class)
+  it 'sets the dish_class' do
+    expect(restaurant).to have_attributes(:dish_class => dish_class_double)
+  end
+
+  it 'creates new dishes of class dish_class from the saved menu' do
+    expect(dish_class_double).to receive(:new).exactly(Restaurant::SAVED_MENU.count).times
+    restaurant
+  end
+
+  it 'adds the new dishes to the menu' do
+    restaurant
+    expect(restaurant.menu.count).to eq(Restaurant::SAVED_MENU.count)
   end
 end
