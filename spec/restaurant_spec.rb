0bd829d0eed3753cd3534380f3f42e4487984e19
Veronica Lee <veronicavlee@icloud.com>
2020-09-12 18:21:06 +0100

    Add print_menu to Takeaway

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,26 +2,10 @@ require 'restaurant'
 
 describe Restaurant do
 
-  subject(:restaurant) { Restaurant.new(dish_class_double) }
-  let(:dish_class_double) { double('dish_class', new: dish_double) }
-  let(:dish_double) { double('dish_double') }
+  subject(:restaurant) { Restaurant.new(menu_dbl) }
+  let(:menu_dbl) { double('menu') }
 
-  it 'has a menu' do
-    expect(restaurant).to respond_to(:menu)
+  it 'stores the given menu' do
+    expect(restaurant).to have_attributes(:menu => menu_dbl)
   end
-
-  it 'sets the dish_class' do
-    expect(restaurant).to have_attributes(:dish_class => dish_class_double)
-  end
-
-  it 'creates new dishes of class dish_class from the saved menu' do
-    expect(dish_class_double).to receive(:new).exactly(Restaurant::SAVED_MENU.count).times
-    restaurant
-  end
-
-  it 'adds the new dishes to the menu' do
-    restaurant
-    expect(restaurant.menu.count).to eq(Restaurant::SAVED_MENU.count)
-  end
-
 end
