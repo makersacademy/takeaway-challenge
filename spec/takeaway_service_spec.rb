431a9d96aec8d6c229be7027030778c1b400f60d
Veronica Lee <veronicavlee@icloud.com>
2020-09-12 22:23:26 +0100

    Add create_order tests for TakeawayService

--- spec/takeaway_service_spec.rb
+++ spec/takeaway_service_spec.rb
@@ -4,9 +4,18 @@ describe TakeawayService do
   subject(:takeaway) { TakeawayService.new(restaurant_dbl) }
   let(:restaurant_dbl) { double('restaurant', format_menu: formatted_menu) }
   let(:formatted_menu) { "Mock example\nof a\nformatted menu" }
+  let(:order_dbl) { double('order') }
 
   it 'sets the restaurant' do
     expect(takeaway.restaurant).to eq(restaurant_dbl)
+  end
+
+  it 'initialises with an empty list of orders' do
+    expect(takeaway.orders).to eq []
+  end
+
+  it 'initialises with the order_counter set to 1' do
+    expect(takeaway.order_counter).to eq(1)
   end
 
   describe '#print_menu' do
@@ -21,5 +30,15 @@ describe TakeawayService do
     end
   end
 
+  describe '#create_order(order)' do
+    it 'adds the order object to the orders list with a unique order number' do
+      expect{ takeaway.create_order(order_dbl) }.to change{ takeaway.orders.count }.by(1)
+      expect(takeaway.orders[0]).to eq({id: 1, order: order_dbl})
+    end
+
+    it 'increases the order_counter by 1' do
+      expect{ takeaway.create_order(order_dbl) }.to change(order.counter).by(1)
+    end
+  end
 
 end
