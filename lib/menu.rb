7c40404758de748c5841083dcc0a4262f3d4d343
Veronica Lee <veronicavlee@icloud.com>
2020-09-12 16:33:38 +0100

    Move view menu method to Menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,8 +2,24 @@ class Menu
 
   attr_reader :dishes
 
-  def initialize
+  def initialize(*dish)
     @dishes = []
+    dish.each { |dish| @dishes << dish }
   end
 
+  def view
+    print_header
+    puts format_dishes.join("\n")
+  end
+
+  private
+
+  def print_header
+    puts "#{"Dish".ljust(20)} Price"
+    puts "#{"".ljust(28, '-')}"
+  end
+
+  def format_dishes
+    @dishes.map { |dish| "#{dish.name.ljust(20)} £#{'%.2f' % dish.price}" }
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,9 +2,20 @@ require 'menu'
 
 describe Menu do
 
-  subject(:menu) { Menu.new }
+  subject(:menu) { Menu.new(dish_dbl1, dish_dbl2, dish_dbl3) }
+  let(:dish_dbl1) { double('dish', name: 'Pepperoni pizza', price: 8.99) }
+  let(:dish_dbl2) { double('dish', name: 'Lasagne', price: 10.00) }
+  let(:dish_dbl3) { double('dish', name: 'Tiramisu', price: 5.20) }
 
   it 'has a list of dishes' do
     expect(menu.dishes).to be_an_instance_of(Array)
   end
+
+  describe '#view' do
+    it 'prints the menu in a nice format' do
+      expected = "Dish                 Price\n----------------------------\nPepperoni pizza      £8.99\nLasagne              £10.00\nTiramisu             £5.20\n"
+      expect{ menu.view }.to output(expected).to_stdout
+    end
+  end
+
 end
