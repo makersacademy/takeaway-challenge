3cbe55593d99b7612f41f5317e9ec5351812cf73
Veronica Lee <veronicavlee@icloud.com>
2020-09-12 15:19:53 +0100

    Add view_menu method to Takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,15 @@
+require_relative 'restaurant'
+
 class Takeaway
   attr_reader :restaurant
 
   def initialize(restaurant = Restaurant.new)
     @restaurant = restaurant
+    @menu = restaurant.menu
   end
 
   def view_menu
-
+    formatted_menu = @menu.map { |dish| "#{dish.name} - £#{'%.2f' % dish.price}" }
+    puts formatted_menu.join("\n")
   end
 end
