a1e5892a5ab4b1067c70927dc690cafd6f5aae93
cristina <c.fujiname@gmail.com>
2020-09-13 10:28:03 +0100

    Add method print_menu on Takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,14 @@
 class Takeaway
+  def initialize(menu:) # injecting menu as we don't want to creat menu in takeaway class
+    @menu = menu
 
+  end
+
+  def print_menu
+    menu.print # calling the menu from the Menu class, menu will handle printing the menu
+  end
+
+  private
+
+  attr_reader :menu # making menu accessible
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,11 @@
 require 'Takeaway'
 
 describe Takeaway do
+  subject(:takeaway) { Takeaway.new(menu: menu) } # inject menu to takeaway as print menu shouldn't be responsibility of takeaway class
+  let(:menu) { double(:menu, print: printed_menu) } # create double of menu to access menu from takeaway class, has method print in its Class, injected here to the takeaway class
+  let(:printed_menu) { "Coffee: 0.99" }
 
+  it 'prints the list of dishes with prices' do #print_menu inside takeaway class, now we start mit Menu zu fummeln
+    expect(takeaway.print_menu).to eq(printed_menu)
+  end
 end
