9aec0249f6255e4371528ba297fba3f12533f4b4
cristina <c.fujiname@gmail.com>
2020-09-13 17:00:22 +0100

    Changes to SMS

--- spec/SMS_spec.rb
+++ spec/SMS_spec.rb
@@ -4,7 +4,12 @@ describe SMS do
   subject(:sms) { SMS.new(client: client) }
 
   let(:client) { double(:client) }
-  let(:config) { double(:config) }
+  let(:config) do
+    {
+        account_sid: "23123",
+        auth_token: "djfhskf"
+    }
+  end
 
   it 'delivers sms with ETA' do
     sms.deliver_sms
