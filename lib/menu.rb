b2fa3629f7aa072c5e8c242d7837ab875fb83c4e
cristina <c.fujiname@gmail.com>
2020-09-13 11:09:26 +0100

    Add menu_spec printed_menu method, method print to menu.rb

--- lib/menu.rb
+++ lib/menu.rb
@@ -6,4 +6,11 @@ class Menu
     @list_of_dishes = list_of_dishes
   end
 
+  def print
+    list_of_dishes.map do |dish, price|
+      "%s £%.2f" % [dish.to_s.capitalize, price]
+      # transf to symbol with 2. float [ name of dish string capitalized, then price ]
+    end.join(", ") # join array with comma
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,7 +2,7 @@ require "Menu"
 
 describe Menu do
 
-  subject(:menu) { Menu.new(list_of_dishes)} #inject the hash to the menu (needs to be initialized!)
+  subject(:menu) { Menu.new(list_of_dishes) } #inject the hash to the menu (needs to be initialized!)
   #create a list of dishes hash
   let(:list_of_dishes) do {
       Coffee: 0.99,
@@ -11,7 +11,12 @@ describe Menu do
       Salad: 4.99
   }
   end
-  it "contains list of dishes with prices" do
+  it "contains list of dishes with prices" do # to be able to print the list, first we create the list
     expect(menu.list_of_dishes).to eq(list_of_dishes)
+  end
+
+  it "prints list_of_dishes with prices" do
+    printed_menu = "Coffee £0.99, Sandwich £3.99, Water £1.19, Salad £4.99"
+    expect(menu.print).to eq (printed_menu)
   end
 end
