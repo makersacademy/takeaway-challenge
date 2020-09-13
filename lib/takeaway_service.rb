f59d184b88da4ecc12ddafe3f559f824a38dbaee
Veronica Lee <veronicavlee@icloud.com>
2020-09-13 17:47:40 +0100

    Remove order storage and add_to_order from TakeawayService

--- lib/takeaway_service.rb
+++ lib/takeaway_service.rb
@@ -4,49 +4,30 @@ require_relative 'dish'
 require_relative 'order'
 
 class TakeawayService
-  attr_reader :restaurant, :orders, :order_counter
+  attr_reader :restaurant, :sms_service
 
-  def initialize(restaurant)
+  def initialize(sms_service, restaurant)
+    @sms_service = sms_service
     @restaurant = restaurant
-    @orders = []
-    @order_counter = 1
   end
 
   def print_menu
     puts @restaurant.format_menu
   end
 
-  def create_order(order = Order.new(@restaurant, @order_counter))
-    @orders << order
-    increment_order_counter
-    "Your order ID is #{order.id}"
-  end
-
-  def add_to_order(order_id, dish_name, qty)
-    order = find_order(order_id)
-    order.add(dish_name, qty)
-  end
-
-  private
-
-  def increment_order_counter
-    @order_counter += 1
-  end
-
-  def find_order(id)
-    @orders.find do |order|
-      id == order.id
-    end
+  def create_order(order = Order.new(@restaurant))
+    order
   end
 end
 
-@takeaway = TakeawayService.new(
-  Restaurant.new(
-    Menu.new(
-      Dish.new('Tom yum soup', 4.50),
-      Dish.new('Pad thai', 8.95),
-      Dish.new('Pad see yew', 8.95),
-      Dish.new('Egg fried rice', 3.89),
-    )
-  )
-)
+# @takeaway = TakeawayService.new(
+#   TwilioService.new,
+#   Restaurant.new(
+#     Menu.new(
+#       Dish.new('Tom yum soup', 4.50),
+#       Dish.new('Pad thai', 8.95),
+#       Dish.new('Pad see yew', 8.95),
+#       Dish.new('Egg fried rice', 3.89)
+#     )
+#   )
+# )
--- spec/takeaway_service_spec.rb
+++ spec/takeaway_service_spec.rb
@@ -1,21 +1,18 @@
 require 'takeaway_service'
 
 describe TakeawayService do
-  subject(:takeaway) { TakeawayService.new(restaurant_dbl) }
+  subject(:takeaway) { TakeawayService.new(twilio_dbl, restaurant_dbl) }
+  let(:twilio_dbl) { double('twilio') }
   let(:restaurant_dbl) { double('restaurant', format_menu: formatted_menu) }
   let(:formatted_menu) { "Mock example\nof a\nformatted menu" }
   let(:order_dbl) { double('order', add: 'add confirmation', id: 1) }
 
+  it 'sets the sms_service' do
+    expect(takeaway.sms_service).to eq(twilio_dbl)
+  end
+
   it 'sets the restaurant' do
     expect(takeaway.restaurant).to eq(restaurant_dbl)
-  end
-
-  it 'initialises with an empty list of orders' do
-    expect(takeaway.orders).to eq []
-  end
-
-  it 'initialises with the order_counter set to 1' do
-    expect(takeaway.order_counter).to eq(1)
   end
 
   describe '#print_menu' do
@@ -31,39 +28,8 @@ describe TakeawayService do
   end
 
   describe '#create_order(order)' do
-    it 'adds the order object to the orders list' do
-      expect{ takeaway.create_order(order_dbl) }.to change{ takeaway.orders.count }.by(1)
-      expect(takeaway.orders[0]).to eq(order_dbl)
-    end
-
-    it 'increases the order_counter by 1' do
-      expect{ takeaway.create_order(order_dbl) }.to change{ takeaway.order_counter }.by(1)
-    end
-
-    it 'returns the order ID' do
-      expect(takeaway.create_order(order_dbl)).to eq('Your order ID is 1')
+    it 'returns the order object' do
+      expect(takeaway.create_order(order_dbl)).to eq(order_dbl)
     end
   end
-
-  describe '#add_to_order(order_id, dish_name, qty)' do
-    it 'takes an order_id, dish_name and qty as arguments' do
-      expect(takeaway).to respond_to(:add_to_order).with(3).arguments
-    end
-
-    context 'when the order id is valid' do
-      it 'sends the add message to the relevant order object' do
-        takeaway.create_order(order_dbl)
-
-        expect(order_dbl).to receive(:add).once.with('Pepperoni pizza', 1)
-        takeaway.add_to_order(1, 'Pepperoni pizza', 1)
-      end
-
-      it 'returns the add details received from order.add' do
-        takeaway.create_order(order_dbl)
-
-        expect(takeaway.add_to_order(1, 'Pepperoni pizza', 1)).to eq(order_dbl.add)
-      end
-    end
-  end
-
 end
