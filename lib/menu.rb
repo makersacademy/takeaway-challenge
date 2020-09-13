d07e54a06889449091bbeb722fa5638b195c21e8
cristina <c.fujiname@gmail.com>
2020-09-13 10:43:20 +0100

    Add Menu class, list_of_dishes init

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,9 @@
 class Menu
+  # create attr_reader to pass method
+  attr_reader :list_of_dishes
+
+  def initialize(list_of_dishes)
+    @list_of_dishes = list_of_dishes
+  end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,16 @@ require "Menu"
 
 describe Menu do
 
+  subject(:menu) { Menu.new(list_of_dishes)} #inject the hash to the menu (needs to be initialized!)
+  #create a list of dishes hash
+  let(:list_of_dishes) do {
+      Coffee: 0.99,
+      Sandwich: 3.99,
+      Water: 1.19,
+      Salad: 4.99
+  }
+  end
+  it "contains list of dishes with prices" do
+    expect(menu.list_of_dishes).to eq(list_of_dishes)
+  end
 end
