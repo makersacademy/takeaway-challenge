4974d9c0efd60a17b426de2ed280f8417d2b5dc4
Victoria Jusko <vikjusko@gmail.com>
2020-09-13 11:53:08 +0100

    made sure that twilio account files are not available

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,3 +1,17 @@
 require 'twilio-ruby'
 class SMS 
+	def initialize
+		account_sid = ENV['TWILIO_ACCOUNT_SID']
+		auth_token = ENV['TWILIO_AUTH_TOKEN']
+		client = Twilio::REST::Client.new(account_sid, auth_token)
+		@from = "+15551234567" 
+		@to = ENV['VJMOB'] 
+	end 
+	
+	def send
+		@client.messages.create(
+			from: @from,
+			to: @to,
+			body: "Thank you for your order, it will be delivered withing the next hour!")
+	end 
 end 
