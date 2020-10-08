d8ce0fc3f22a6ab23387b8cf9ab3125c0b1ad7ef
Kiril <kiril@olgas-air.mynet>
2020-10-08 16:53:33 +0100

    #get_menu, #add_item tests for Order class

--- lib/order.rb
+++ lib/order.rb
@@ -3,7 +3,16 @@ require_relative 'menu'
 class Order
   attr_reader :basket
 
-  def initialize
+  def initialize(menu = Menu.new)
     @basket = []
+    @menu = menu
   end
+
+  def get_menu
+    puts @menu.print_menu
+  end
+
+  # def add_item(item)
+    
+  # end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,10 +1,22 @@
 require 'order'
 
 describe Order do
-  subject(:order) { Order.new }
+  let (:menu) { double :menu, items: { "Blini" => 7 }, print_menu: "Blini, £7" }
+  subject(:order) { Order.new(menu)}
   describe '#initialize' do
     it 'the basket is empty by default' do
       expect(order.basket).to be_empty
+    end
+
+    it 'gets the menu' do
+      expect { order.get_menu }.to output.to_stdout
+    end
+  end
+
+  describe '#add_item' do
+    it 'adds an item to basket' do
+      order.add_item("Blini")
+      expect(order.basket).to include("Blini")
     end
   end
 end
