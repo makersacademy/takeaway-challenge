dbc2571dab89d8b0466aeba2d822e1c6617ffde2
Dan Holmes <dh@danielholmes.com>
2020-10-08 12:40:33 +0100

    add print method on menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,12 +1,23 @@
 class Menu
   attr_reader :dishes
-  def initialize
-    @dishes = original_dishes
+  def initialize(dishes = default_dishes)
+    @dishes = dishes
   end
 
-  def original_dishes
+  def default_dishes
     dishes = []
-    dishes << Dish.new('curry',8)
-    dishes << Dish.new('rice',3)
+    dishes << Dish.new('Curry',8)
+    dishes << Dish.new('Rice',3)
+  end
+
+  def print
+    puts "Menu:"
+    @dishes.each { |dish| print_dish(dish)}
+  end
+
+  private
+
+  def print_dish(dish)
+    puts "#{dish.name} - £#{dish.price}"
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,14 +1,15 @@
 require 'Menu'
 
 describe Menu do
-  let(:menu) {Menu.new}
+  let(:curry) {double(:dish, name: 'Curry', price: 8)}
+  let(:rice) {double(:dish, name: 'Rice', price: 3)}
+  let(:menu) {Menu.new([curry,rice])}
   it "starts with a non-empty list of dishes" do
     expect(menu.dishes.length).to be > 0
   end
 
-  it "should only include dishes" do
-    menu.dishes.each do |object|
-      expect(object).to be_a Dish
-    end
+  it "can print its contents" do
+    menu_output = "Menu:\nCurry - £8\nRice - £3\n"
+    expect{ menu.print }.to output(menu_output).to_stdout
   end
 end
