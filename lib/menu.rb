14218997b31c22d93b0bbc85f988504a2ced5d9d
Kiril <kiril@olgas-air.mynet>
2020-10-08 15:51:43 +0100

    menu items are now an instance variable not a constant

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,13 @@
 class Menu
-  MENU = {
-    "Blini" => 7,
-    "Chicken Kiev" => 15,
-    "Shuba" => 11,
-    "Bortsch" => 8
-  }
+  attr_reader :items
+
+  def initialize
+    @items = {
+      "Blini" => 7,
+      "Chicken Kiev" => 15,
+      "Shuba" => 11,
+      "Bortsch" => 8
+    }
+  end
+  
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,11 @@
 require 'menu'
 
 describe Menu do
+  subject(:menu) { Menu.new }
+
   describe '#initialize' do
     it 'there is a menu with items' do
-      expect(Menu::MENU).not_to be_empty
+      expect(menu.items).not_to be_empty
     end
   end
 end
