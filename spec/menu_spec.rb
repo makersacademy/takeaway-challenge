dc89b4d172646ef094f561fe49c035bd0c48e2cd
Kiril <kiril@olgas-air.mynet>
2020-10-08 15:55:40 +0100

    test for #add_item to add an item with name and price

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -8,4 +8,11 @@ describe Menu do
       expect(menu.items).not_to be_empty
     end
   end
+
+  describe '#add_item' do
+    it 'can add an item with a price to the menu' do
+      menu.add_item("Ukha", 9)
+      expect(menu.items).to include("Ukha" => 9)
+    end
+  end
 end
