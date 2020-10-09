283cf7844f5287e334ef71aa13a4f7931416873b
Kiril <kiril@olgas-air.mynet>
2020-10-09 13:13:20 +0100

    pass error tests for Basket#add_item and #show

--- lib/basket.rb
+++ lib/basket.rb
@@ -10,11 +10,15 @@ class Basket
   end
 
   def add_item(item)
+    raise "This item doesn't exist!" if check_if_not_in_menu(item)
+
     @items << item
     add_to_total(item)
   end
 
   def show
+    raise "Your basket is empty." if @items.empty?
+
     @items.each do |item|
       puts "#{item}, £#{@menu.items[item]}"
     end
@@ -33,5 +37,9 @@ class Basket
 
   def print_total
     puts "Your total is: £#{@total}."
+  end
+
+  def check_if_not_in_menu(item)
+    !@menu.items.include?(item)
   end
 end
