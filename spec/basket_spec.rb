be3d9c8c99ee1f42da26e403e3a75bd2bed58bca
Kiril <kiril@olgas-air.mynet>
2020-10-09 12:50:59 +0100

    test Basket#add_item

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,6 +1,8 @@
 require 'basket'
 
 describe Basket do
+  let(:menu) { double :menu, items: { "Blini" => 7 }, print_menu: "Blini, £7" }
+
   describe "#initialize" do
     it "has an empty items array" do
       expect(subject.items).to be_empty
@@ -8,6 +10,13 @@ describe Basket do
 
     it "keeps a total that is 0 by default" do
       expect(subject.total).to eq 0
+    end
+  end
+
+  describe "#add" do
+    it "adds an item to the items array" do
+      subject.add_item("Blini")
+      expect(subject.items).to include("Blini")
     end
   end
 end
