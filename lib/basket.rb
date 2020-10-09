11441cc68a97c2253cc95062af5b5844bbd7991f
Kiril <kiril@olgas-air.mynet>
2020-10-09 12:57:51 +0100

    Basket#add_item passing code

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,12 +1,24 @@
+require_relative 'menu'
+
 class Basket
   attr_reader :items, :total
 
-  def initialize
+  def initialize(menu = Menu.new)
+    @menu = menu
     @items = []
     @total = 0
   end
 
   def add_item(item)
     @items << item
+    @total += @menu.items[item]
   end
+
+  # @basket.each do |item|
+  #   puts "#{item}, £#{@menu.items[item]}"
+  # end
+
+  # def add_to_total(item)
+  #   @total += @menu.items[item]
+  # end
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,6 +1,7 @@
 require 'basket'
 
 describe Basket do
+  subject(:subject) { described_class.new(menu) }
   let(:menu) { double :menu, items: { "Blini" => 7 }, print_menu: "Blini, £7" }
 
   describe "#initialize" do
@@ -13,10 +14,17 @@ describe Basket do
     end
   end
 
-  describe "#add" do
-    it "adds an item to the items array" do
+  describe "#add_item" do
+    before do
       subject.add_item("Blini")
+    end
+
+    it "adds an item to the items array" do
       expect(subject.items).to include("Blini")
+    end
+
+    it "adds price of an item to total" do
+      expect(subject.total).to eq 7
     end
   end
 end
