d592f02bc10c905f9a632976a60bc0e9ce42809d
Ben <ben.lynch1931@gmail.com>
2020-10-10 10:46:09 +0100

    ORDER: Red/Green - Displays menu when asked

--- lib/orders.rb
+++ lib/orders.rb
@@ -1,13 +1,18 @@
 class Order
   DISHES = [
-    {id: 001, item: "Pepperoni Pizza", price: 7.99},
-    {id: 002, item: "Margherita Pizza", price: 7.49},
-    {id: 003, item: "Chips", price: 3.00},
-    {id: 004, item: "Onion Rings", price: 3.50},
-    {id: 006, item: "Diet Coke", price: 2.50},
-    {id: 007, item: "Fanta", price: 2.50}
+    {id: 001, item: "Pepperoni Pizza", price: "7.99"},
+    {id: 002, item: "Margherita Pizza", price: "7.49"},
+    {id: 003, item: "Chips", price: "3.00"},
+    {id: 004, item: "Onion Rings", price: "3.50"},
+    {id: 006, item: "Diet Coke", price: "2.50"},
+    {id: 007, item: "Fanta", price: "2.50"}
   ]
 
-  def initialize
+  def print_menu
+    puts "Menu:"
+    puts "-----"
+    DISHES.each do |item|
+      puts "£#{item[:price]} : #{item[:item]}"
+    end
   end
 end
--- spec/orders_spec.rb
+++ spec/orders_spec.rb
@@ -2,7 +2,7 @@ require 'orders'
 
 describe Order do
 
-context '#initialize' do
+  context '#initialize' do
 
     it ' is an instance of Order' do
       order_inst = Order.new
@@ -13,5 +13,14 @@ context '#initialize' do
       expect(Order::DISHES).to_not be_empty
     end
 
+  end
+
+  context '#print_menu' do
+
+    it ' prints menu when called' do
+      menu = "Menu:\n-----\n£7.99 : Pepperoni Pizza\n£7.49 : Margherita Pizza\n£3.00 : Chips\n£3.50 : Onion Rings\n£2.50 : Diet Coke\n£2.50 : Fanta\n"
+      order_inst = Order.new
+      expect { order_inst.print_menu }.to output(menu).to_stdout
+    end
   end
 end
