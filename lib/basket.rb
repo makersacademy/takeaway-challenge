b1fdd1bb746ed2393f022e81ca4c8f7082e9c92b
Dan Holmes <dh@danielholmes.com>
2020-10-10 15:08:09 +0100

    refactor and add to basket class

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,16 +1,39 @@
 class Basket
-  attr_reader :dishes
+  attr_reader :dishes, :total_price
   def initialize
-    @dishes = []
+    @dishes = Hash.new
+    @total_price = 0
   end
 
-  def add(dish)
-    @dishes << dish
+  def add(dish, quantity = 1)
+    @dishes[dish] = 0 if @dishes[dish].nil?
+    @dishes[dish] += quantity
+    @total_price += quantity*dish.price
   end
 
-  def total_price
+  def correct_total?
+    raise "Total price is wrong. Please try again." if @total_price != calculate_total
+    true
+  end
+
+  def place_order
+    clear_basket
+    send_confirmation
+  end
+
+  private
+
+  def send_confirmation
+    "Thank you! Your order was placed and will be delivered before 18:52"
+  end
+
+  def clear_basket
+    @dishes.clear
+  end
+
+  def calculate_total
     total = 0
-    @dishes.each { |dish| total += dish.price}
+    @dishes.each { |dish, quantity| total += quantity*dish.price}
     total
   end
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -5,17 +5,54 @@ describe Basket do
   let(:curry) {double(:dish, :name => "curry", :price => 8)}
   let(:rice) {double(:dish, :name => "rice", :price => 3)}
   it "starts off with no dishes" do
-    expect(basket.dishes.length).to eq 0
+    expect(basket.dishes).to be_empty
   end
 
-  it "can add a dish" do
-    basket.add(curry)
-    expect(basket.dishes).to include curry
+  describe "#add" do
+    it "can add a dish" do
+      basket.add(curry)
+      expect(basket.dishes[curry]).to eq 1
+    end
+
+    it "can add multiple of the same dish at once" do
+      basket.add(curry,2)
+      expect(basket.dishes[curry]).to eq 2
+    end
+
+    it "updates the total price" do
+      expect{basket.add(curry,2)}.to change{basket.total_price}.by(16)
+    end
   end
 
-  it "can calculate a total price" do
-    basket.add(curry)
-    basket.add(rice)
-    expect(basket.total_price).to eq 11
+
+
+  describe "#correct_total?" do
+    it "should return true for an empty basket" do
+      expect(basket).to be_correct_total
+    end
+
+    it "should return true after dishes added" do
+      basket.add(curry)
+      basket.add(rice)
+      expect(basket).to be_correct_total
+    end
+
+    it "should raise an error if price changed after dish added" do
+      basket.add(curry)
+      allow(curry).to receive(:price).and_return(10)
+      expect{basket.correct_total?}.to raise_error "Total price is wrong. Please try again."
+    end
+  end
+
+  describe "#place_order" do
+    it "sends a confirmation the order has been placed" do
+      expect(basket.place_order).to eq "Thank you! Your order was placed and will be delivered before 18:52"
+    end
+
+    it "clears the basket" do
+      basket.add(curry)
+      basket.place_order
+      expect(basket.dishes).to be_empty
+    end
   end
 end
