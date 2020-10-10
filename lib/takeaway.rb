c0247160b967c681225e5e09de82b1fe406a309b
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-10 15:35:16 +0100

    Red/Green: Takeaway - Can now add items from the menu in takaway to the order and maintain test coverage

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,17 @@
 require_relative 'menu'
+require_relative 'order'
 class Takeaway 
   attr_reader :menu
-  def initialize(menu = Menu.new)
+  def initialize(menu = Menu.new, order = Order.new)
     @menu = menu
+    @order = order
   end
 
   def view_menu
     @menu.view_dishes
+  end
+
+  def add_to_order(dish, price, quantity)
+    @order.add(dish, price, quantity)
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,10 +1,18 @@
 require './lib/takeaway'
 describe Takeaway do
   let(:menu) { double :menu }
+  let(:order) { double :order }
   describe '#view_menu' do
     it 'can view a menu of dishes' do
       allow(menu).to receive(:view_dishes) { menu }
       subject.view_menu
+    end
+  end
+
+  describe '#add_to_order' do
+    it 'can order dishes from the menu' do
+      allow(order).to receive(:add).and_return("dish", 20, 1)
+      subject.add_to_order("dish", 20, 1)
     end
   end
 end
