118f9964db21558c6621204c465ce1024b2fc3fa
Ben <ben.lynch1931@gmail.com>
2020-10-10 11:13:15 +0100

    ORDER: Red/Green - Allows the user to order from menu

--- lib/orders.rb
+++ lib/orders.rb
@@ -1,18 +1,36 @@
 class Order
   DISHES = [
-    {id: 001, item: "Pepperoni Pizza", price: "7.99"},
-    {id: 002, item: "Margherita Pizza", price: "7.49"},
-    {id: 003, item: "Chips", price: "3.00"},
-    {id: 004, item: "Onion Rings", price: "3.50"},
-    {id: 006, item: "Diet Coke", price: "2.50"},
-    {id: 007, item: "Fanta", price: "2.50"}
+    {id: "001", item: "Pepperoni Pizza", price: "7.99"},
+    {id: "002", item: "Margherita Pizza", price: "7.49"},
+    {id: "003", item: "Chips", price: "3.00"},
+    {id: "004", item: "Onion Rings", price: "3.50"},
+    {id: "005", item: "Diet Coke", price: "2.50"},
+    {id: "006", item: "Fanta", price: "2.50"}
   ]
+
+  def initialize
+    @order_list = []
+  end
 
   def print_menu
     puts "Menu:"
     puts "-----"
     DISHES.each do |item|
-      puts "£#{item[:price]} : #{item[:item]}"
+      puts "ID#{item[:id]} - £#{item[:price]} : #{item[:item]}"
     end
   end
+
+  def order
+    while true
+      puts "Please enter the FULL ID number or 'end' to finish ordering: "
+      order_id = gets.chomp
+      break if order_id == 'end'
+      order_item = DISHES.select { |item| item[:id] == order_id }
+      puts "Please enter quantity of #{order_item[0][:item]}"
+      order_quantity = gets.chomp
+      @order_list.push([order_id, order_quantity])
+    end
+  end
+
+  attr_reader :order_list
 end
--- spec/orders_spec.rb
+++ spec/orders_spec.rb
@@ -18,9 +18,19 @@ describe Order do
   context '#print_menu' do
 
     it ' prints menu when called' do
-      menu = "Menu:\n-----\n£7.99 : Pepperoni Pizza\n£7.49 : Margherita Pizza\n£3.00 : Chips\n£3.50 : Onion Rings\n£2.50 : Diet Coke\n£2.50 : Fanta\n"
+      menu = "Menu:\n-----\nID001 - £7.99 : Pepperoni Pizza\nID002 - £7.49 : Margherita Pizza\nID003 - £3.00 : Chips\nID004 - £3.50 : Onion Rings\nID005 - £2.50 : Diet Coke\nID006 - £2.50 : Fanta\n"
       order_inst = Order.new
       expect { order_inst.print_menu }.to output(menu).to_stdout
+    end
+  end
+
+  context '#enter_order' do
+
+    it ' allows user to order using ID numbers and quantity' do
+      order_inst = Order.new
+      allow(order_inst).to receive(:gets).and_return("001", "2", "002", "1", "003", "1", "005", "3", "end")
+      order_inst.order
+      expect(order_inst.order_list).to eq [["001", "2"], ["002", "1"], ["003", "1"], ["005", "3"]]
     end
   end
 end
