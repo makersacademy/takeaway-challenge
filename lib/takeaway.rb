bb5888bf6cdbcb698e98a4fadc6358768693a81b
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-10 16:59:46 +0100

    Takeaway class created with a view_menu method.

---
+++ lib/takeaway.rb
@@ -0,0 +1,7 @@
+class Takeaway
+
+    def view_menu 
+        
+    end
+
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,9 @@
+require 'takeaway'
+
+describe Takeaway do
+    describe '#view_menu' do
+
+        it { is_expected.to respond_to(:view_menu) }
+        
+    end
+end
