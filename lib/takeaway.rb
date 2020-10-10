f8be35955520f3515ee5e12648dfb895d83e907e
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-10 17:02:45 +0100

    view_menu returns basic hash menu.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,7 +1,7 @@
 class Takeaway
 
     def view_menu 
-        
+        {"California Rolls" => 8, "Avocado Maki" => 7, "Edamame" => 3}
     end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,6 +4,10 @@ describe Takeaway do
     describe '#view_menu' do
 
         it { is_expected.to respond_to(:view_menu) }
+
+        it 'returns list of dishes and prices' do
+            expect(subject.view_menu).to eq({"California Rolls" => 8, "Avocado Maki" => 7, "Edamame" => 3})
+        end
         
     end
 end
