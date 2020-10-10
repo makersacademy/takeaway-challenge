1fdc888e70ee94cf8dc7653bb596f1a1bc775261
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-10 17:44:21 +0100

    Refactored Takeaway class, now initialized with Menu.new.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,9 +2,12 @@ require_relative 'menu'
 
 class Takeaway
 
-    def view_menu(menu = Menu.new)
+    def initialize(menu = Menu.new)
         @menu = menu
-        menu.print_menu
+    end
+
+    def view_menu
+        @menu.print_menu
     end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,13 +2,15 @@ require 'takeaway'
 
 describe Takeaway do
     let(:menu) { double :menu }
+    let (:takeaway) { described_class.new(menu) }
+
     describe '#view_menu' do
 
         it { is_expected.to respond_to(:view_menu) }
 
         it 'prints the menu' do
             expect(menu).to receive(:print_menu)
-            subject.view_menu(menu)
+            takeaway.view_menu
         end
         
     end
