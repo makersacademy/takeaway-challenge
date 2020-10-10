3024b5a9117e98652c4cd9e07b335f55c873b091
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-10 17:01:32 +0100

    Red/Green: Menu - Can now check if dishes are available on the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,17 +1,20 @@
 class Menu
   attr_reader :dishes
   def initialize
-    @dishes = [{
+    @dishes = {
         "Carbonara": 26,
         "Gnnoci": 24,
-        "Seafood Linguini": 30,
+        "Seafood linguine": 30,
         "Lasagna": 22,
         "Pizza": 18
-    }]
+    }
   end
 
   def view_dishes
-    @dishes.each { |dish, price| p "#{dish} #{price}" }
+    @dishes.each { |dish, price| puts "#{dish} £#{price}" }
   end
 
+  def available?(dish)
+    @dishes.include?(dish.to_sym)
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,6 +7,17 @@ describe Menu do
   it 'can view a list of dishes with prices' do
     expect(subject.view_dishes).to eq subject.dishes
   end
+
+  context 'checks to see if a dish is available' do
+    it 'is not avaliable' do
+      subject.dishes
+      expect(subject.available?("Lobster linguine")).to be false
+    end
+
+    it 'is avaliable' do
+      subject.dishes
+      expect(subject.available?("Pizza")).to be true
+    end
+  end
 end
  
-  
