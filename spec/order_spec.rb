f8f1e55fb30259c75a5ffefd137667e15854409c
Mace <maceplus@gmail.com>
2020-10-11 23:50:30 +0100

    Add test for order class

---
+++ spec/order_spec.rb
@@ -0,0 +1,19 @@
+require 'order'
+
+describe Order do
+  let(:menu_double) { double :Menu, list: { :fish => 5, :chips => 3, :salad => 2 } }
+  it 'returns the menu on #menu' do
+    order = Order.new(menu_double)
+    expect(order.menu).to include(
+      :fish => 5,
+      :chips => 3,
+      :salad => 2
+    )
+  end
+
+  it 'allows the user to #add to their order' do
+    order = Order.new(menu_double)
+    expect(order.add("1 fish, 3 salad, 2 chips")).to eq ["1 fish", "3 salad", "2 chips"]
+  end
+
+end
