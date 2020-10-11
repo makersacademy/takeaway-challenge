1093fe019c15fefceb3014f78a4af3acc2406e35
KC Thomas <thisiskcthomas@gmail.com>
2020-10-12 00:23:27 +0100

    Added Menu class and passing tests. Menu pulls from csv menu files.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,41 @@
+
 class Menu
+
+  def initialize(menu_file = "menu_1.csv")
+    @menu = []
+    @menu_file = "./lib/menus/#{menu_file}"
+    load_menu
+  end
+
+  def print_menu
+    @menu.each {|item| puts "#{item[:item]} ~ £#{item[:price]}"}
+    true
+  end
+
+  def find_price(item)
+    @menu.each do |food|
+      return food[:price] if same_food?(food[:item], item)
+    end
+  end
+
+  def menu
+    @menu
+  end
+
+  private
+
+  def same_food?(menu_item, user_input)
+    menu_item.to_s.downcase == user_input.downcase
+  end
+
+  def load_menu
+    File.foreach(@menu_file) do |line|
+      item, price = line.chomp.split(',')
+      add_items(item, price)
+    end
+  end
+
+  def add_items(item, price)
+    @menu << {item: item, price: price.to_i }
+  end
 end
