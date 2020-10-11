c146ed3d32db0960139f50b4f3a1bae2eb84c121
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-11 18:03:16 +0100

    Created #add to order method and passed basic test

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,12 @@
 class Order
   attr_reader :basket
-  
+
   def initialize
-    @basket = {}
+    @basket = Hash.new(0)
+  end
+
+  def add(food, quantity = 1)
+    basket[food] += quantity
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,4 +7,11 @@ describe Order do
       expect(order.basket).to eq({})
     end
   end
+
+  describe '#add_to_basket' do
+    it 'adds order input to basket' do
+      order.add("steak", 2)
+      expect(order.basket).to eq({"steak" => 2})
+    end
+  end
 end
