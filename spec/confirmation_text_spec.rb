5166f6a0bf12aed8e14bf371bb6002f9d8dbf56c
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-11 23:24:39 +0100

    created a working text sender, giving an delivery ETA of 1 hr

--- spec/confirmation_text_spec.rb
+++ spec/confirmation_text_spec.rb
@@ -1,5 +1,10 @@
 require 'confirmation_text'
-require 'twilio-ruby'
 
-describe ConfirmationText
+
+describe ConfirmationText do
+  subject(:confirmation_text) { ConfirmationText.new }
+  it 'sends text message' do
+    allow_any_instance_of(ConfirmationText).to receive(:send_message).and_return("Complete")
+    expect(confirmation_text.send_message).to eq("Complete")
+  end
 end
