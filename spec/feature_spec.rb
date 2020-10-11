3f7654d6359f4b6a0d2f925803d88031b9d8c373
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-11 13:46:51 +0100

    Add feature tests for printing menu, placing order and printing receipt

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,8 +4,29 @@ describe "takeaway features" do
 
   let(:restaurant) {Restaurant.new}
 
-  xit 'allows users to see the menu' do
-    expect{restaurant.print_menu}.to output()
+  it 'can print the menu' do
+    expect{restaurant.print_menu}.to output("1. trout_snout: £6.50\n2. hog_soup: £4.90\n3. mysterious_dumpling: £8.00\n4. boring_sandwich: £4.00\n5. cuttlefish_pate: £11.00\n6. hoof_broth: £3.30\n").to_stdout
+  end
+
+  before do
+    io = StringIO.new
+    io.puts"2"
+    io.puts"3"
+    io.puts"3"
+    io.puts"4"
+    io.rewind
+    $stdin = io
+  end
+  after do
+    $stdin = STDIN
+  end
+
+  it "can receive an order and print an accurate receipt" do
+    restaurant.create_order
+    restaurant.choose_dish
+    restaurant.choose_dish
+    restaurant.check_bill
+    expect{restaurant.print_receipt}.to output("hog_soup: £4.90\nhog_soup: £4.90\nhog_soup: £4.90\nmysterious_dumpling: £8.00\nmysterious_dumpling: £8.00\nmysterious_dumpling: £8.00\nmysterious_dumpling: £8.00\nTotal: £46.70\n").to_stdout
   end
 
 end
