0add91a252fe0dc8806bf700990afeb1a5b5e2b2
Mace <maceplus@gmail.com>
2020-10-11 23:48:55 +0100

    Add order class

---
+++ lib/order.rb
@@ -0,0 +1,13 @@
+require_relative 'menu'
+
+class Order
+  attr_reader :menu
+  def initialize(menu = Menu.new)
+    @menu = menu.list
+  end
+
+  def add(order)
+    order = order.split(", ")
+  end
+
+end
