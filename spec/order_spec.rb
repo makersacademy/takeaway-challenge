80ae2bf9e167f85eaa7776653835fe97e38b4d16
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-11 17:44:10 +0100

    initialized order with an empty basket

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,3 +1,10 @@
 require 'order'
 describe Order do
+  subject(:order) { Order.new }
+
+  describe '#initialize' do
+    it 'initializes with an empty basket' do 
+      expect(order.basket).to eq({})
+    end
+  end
 end
