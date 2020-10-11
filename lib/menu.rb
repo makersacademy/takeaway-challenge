13558cbae1a3693e1159a10b8edfab745ae88724
Adrian.T <t.adrian88@gmail.com>
2020-10-11 22:46:53 +0100

    Added a menu that can be displayed with read method, test passed for the latter two.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,22 +1,20 @@
 class Menu
   attr_reader :menu
 
-  menu = [
-    {item: 'Mighty bucket (10 pieces of chicken breast strips)', price: 12},
-    {item: 'Hot wings (2 wings in one portion)', price: 1},
-    {item: 'Chicken burger', price: 6},
-    {item: 'Fries (small)', price: 3},
-    {item: 'Fries large', price: 4},
-    {item: 'Drinks 330ml (coke, fanta, sprite, water)', price: 2}
-  ]
-
-  def initiliaze(menu = list_of_items)
-    @menu = list_of_items
+  def initialize(*)
+    @menu = [
+      { item: 'Mighty bucket (10 pieces of chicken breast strips)', price: 12 },
+      { item: 'Hot wings (2 wings in one portion)', price: 1 },
+      { item: 'Chicken burger', price: 6 },
+      { item: 'Fries (small)', price: 3 },
+      { item: 'Fries large', price: 4 },
+      { item: 'Drinks 330ml (coke, fanta, sprite, water)', price: 2 }
+    ]
   end
 
   def read
     @menu.each do |item, price|
-      print "#{item}: £#{price}"
+      puts "#{item}: £#{price}"
     end
   end
 end
