8803c5cf314831605f680251f8504be8c9328367
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-11 22:16:58 +0100

    refactored menu tests

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,8 +3,8 @@ describe Menu do
   subject(:menu) { Menu.new }
   
   describe "#display_menu" do
-    xit 'displays menu: food followed by pricing' do
-      expect(menu.display_menu).to eq("Egg fried rice: £12.50")
+    it 'displays menu: food followed by pricing' do
+      expect(menu.display_menu).to eq(["Margherita: £12.5", "Carbonara: £15", "Lasagna: £16", "Penne almodoro: £19.99"])
     end
   end 
 end
