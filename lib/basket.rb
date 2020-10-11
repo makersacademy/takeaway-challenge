874dfca298400fa64a3e0b736857449ddab5c2c5
Cameron Logie <cameronlogie24@gmail.com>
2020-10-11 18:29:27 +0100

    added an order total instance to basket

--- lib/basket.rb
+++ lib/basket.rb
@@ -4,10 +4,15 @@ class Basket
 
     def initialize
       @basket = []
+      @order_total = 0
     end
 
     def basket
       @basket
+    end
+
+    def order_total
+      @order_total
     end
 
     def order(item)
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -10,6 +10,12 @@ describe Basket do
     end 
   end
 
+  describe 'order_total' do 
+    it 'begins with a value of zero' do
+    expect(subject.order_total).to eq 0
+    end
+  end 
+
   describe 'order(item)' do
     it 'adds an item to the basket' do
     subject.order(item)
