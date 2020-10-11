28166742065e12e44d7fbd6c972475f90197109d
Cameron Logie <cameronlogie24@gmail.com>
2020-10-11 11:58:35 +0100

    made test pass for gets chomp for user to select a cheeseburger

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,17 +2,33 @@ class Takeaway
 
     def initialize
         @menu = {
-        "Double Double" => 4.35,
-        "Cheeseburger" => 3.15,
-        "Hamburger" => 2.85,
-        "French Fries" => 2.05,
-        "Milkshake" => 2.45,
-        "Small Soda" => 1.75
-    }
+        "1. Double Double" => 4.35,
+        "2. Cheeseburger" => 3.15,
+        "3. Hamburger" => 2.85,
+        "4. French Fries" => 2.05,
+        "5. Milkshake" => 2.45,
+        "6. Small Soda" => 1.75
+         }
+         @basket = []
     end
 
     def menu
         @menu
+    end
+
+    def basket
+        @basket
+    end
+
+    def order(menu)
+        puts 'please select as many items as you would like to put in your cart by typing the number of the item listed'
+        item = gets.chomp
+        @basket << item
+        while item != "" do
+            print(menu)
+            puts 
+        @basket << item
+        end
     end
 
     def print(menu)
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,9 +6,25 @@ describe Takeaway do
     expect(subject.menu).to include({"Cheeseburger" => 3.15})
     end
 
-    it 'can print the menu line by line with the item and price next to each other' do
-    menu = subject.menu
-    expect{ subject.print(menu) }.to output("Double Double: $4.35\nCheeseburger: $3.15\nHamburger: $2.85\nFrench Fries: $2.05\nMilkshake: $2.45\nSmall Soda: $1.75\n").to_stdout
+    describe '#print(menu)' do
+
+      it 'can print the menu line by line with the item and price next to each other' do
+        menu = subject.menu
+        expect{ subject.print(menu) }.to output("Double Double: $4.35\nCheeseburger: $3.15\nHamburger: $2.85\nFrench Fries: $2.05\nMilkshake: $2.45\nSmall Soda: $1.75\n").to_stdout
+     end
     end
+
+    describe '#Order' do
+      let(:fakeaway) { double = Takeaway.new }
+      
+      it 'receives user input as an order' do
+        menu = fakeaway.menu
+        allow(fakeaway).to receive(:gets).and_return("Cheeseburger") 
+        fakeaway.order(menu)
+        expect(fakeaway.basket).to include("Cheeseburger")
+      end
+    end
+        
+            
 
 end
