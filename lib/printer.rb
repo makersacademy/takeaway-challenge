4f99ca2dbc2f2107cd8bd5703363f0cc039fcc8d
Tim Castillo-Gill <timgillviola@gmail.com>
2020-10-11 17:30:34 +0100

    Added methods for order_total and item_totals

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,6 +1,10 @@
-class Printer do 
+require_relative 'takeaway'
 
+class Printer
 
+  def basket_summary
+    @basket
+  end
 
   def total_price
     
--- playground_aka_takeaway.rb
+++ playground_aka_takeaway.rb
@@ -1,12 +1,21 @@
-require './lib/menu.rb'
 require './lib/takeaway.rb'
+require './lib/printer.rb'
 
-# p carnal_menu = Menu.new
 p carnal = Takeaway.new
-# p carnal.print_dishes
-
+# p carnal_printer = Printer.new
+# p carnal_menu.print_dishes
+p 'This is the menu'
+p carnal.display_menu
 p 'These are the dishes I add'
 p carnal.add_dish('Guacamole', 2)
 p carnal.add_dish('Nachos')
 p 'This is the basket after:'
-p carnal.basket
+p carnal.basket_summary
+p 'This is just the prices of whats in your basket'
+p carnal.order_total
+p carnal.item_totals
+
+
+# @dishes.select { |food| food[:dish] == dish 
+# }
+
