4baad1c76b8ae6722f35ea43daf7d847cf7f03fa
KC Thomas <thisiskcthomas@gmail.com>
2020-10-11 23:01:33 +0100

    Added feature to send text to confirm order has been placed. Passed all tests.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,8 +1,9 @@
 require 'twilio-ruby'
+
 class Customer
 
-  def initialize
-    @phone = nil
+  def initialize(phone = nil)
+    @phone = phone
   end
 
   def input_number
@@ -15,18 +16,12 @@ class Customer
     puts "A text has been sent confirming your order"
   end
 
-  private
-
-  def print_confirmation
-    puts "A text has been sent confirming your order"
-  end
-
-  def send_text
+  def send_text(client = Twilio::REST::Client)
     account_sid = 'AC5500b960ae18a5f094a8171473453196'
-    auth_token = '3913acc29fa291c3dc4cd81ccb43c080'
-    client = Twilio::REST::Client.new(account_sid, auth_token)
+    auth_token = 'e4406214551185379e1bd41d6b7324e2'
+    client = client.new(account_sid, auth_token)
 
-    from = '+4407516726133'
+    from = '+447782337790'
     to = @phone
 
     client.messages.create(
@@ -35,6 +30,8 @@ class Customer
     body: "Thank you! Your order was placed and will be delivered before #{one_hour_from_now}."
     )
   end
+
+  private
 
   def one_hour_from_now
     "#{(Time.now.hour + 1)}:#{Time.now.min}"
