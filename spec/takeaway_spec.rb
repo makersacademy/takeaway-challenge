2d18b96dab66c2649afdb11925225a59bc826276
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-11 22:16:40 +0100

    tests for adding to basket and returning total cost

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,9 +7,15 @@ describe Takeaway do
 
   before do
     allow(menu).to receive(:display_menu).and_return({ "Carbonara" => 20 })  
+    allow(order).to receive(:total).and_return(20)
   end
 
   it 'adds item to basket' do
     expect(takeaway.read_menu).to eq({ "Carbonara" => 20 })
   end
+  
+  it 'returns order total cost' do
+    expect(takeaway.total_price).to eq (20)
+  end
+
 end
