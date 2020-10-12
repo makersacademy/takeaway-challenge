ac93d9ea03ce1a24e9a0dd2e29a7127e0c822b3c
Mace <maceplus@gmail.com>
2020-10-12 01:07:52 +0100

    refactor code & new test to return the #cost

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,19 +1,25 @@
 require 'order'
 
 describe Order do
-  let(:menu_double) { double :Menu, list: { :fish => 5, :chips => 3, :salad => 2 } }
+  let(:menu_double) { double :Menu, list: { :fish => "£5", :chips => "£3", :salad => "£2" } }
   it 'returns the menu on #menu' do
     order = Order.new(menu_double)
     expect(order.menu).to include(
-      :fish => 5,
-      :chips => 3,
-      :salad => 2
+      :fish => "£5",
+      :chips => "£3",
+      :salad => "£2"
     )
   end
 
   it 'allows the user to #add to their order' do
     order = Order.new(menu_double)
     expect(order.update("1 fish, 3 salad, 2 chips")).to include(:fish => 1, :salad => 3, :chips => 2)
+  end
+
+  it '#cost returns total order cost' do
+    order = Order.new(menu_double)
+    order.update("1 fish, 3 salad, 2 chips")
+    expect(order.cost).to eq 17
   end
 
 end
