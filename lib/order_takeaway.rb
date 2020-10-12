144b6b66789c5a22dcdf241d14e3311cb9ee2590
Cameron Logie <cameronlogie24@gmail.com>
2020-10-12 10:23:15 +0100

    commented out order_takeaway

--- lib/order_takeaway.rb
+++ lib/order_takeaway.rb
@@ -1,20 +1,20 @@
-require_relative 'basket'
-require_relative 'print'
-require_relative 'text'
-require_relative 'menu'
+# require_relative 'basket'
+# require_relative 'print'
+# require_relative 'text'
+# require_relative 'menu'
 
-class OrderTakeaway
+# class OrderTakeaway
 
-    attr_reader :place_order
+#     attr_reader :place_order
 
-    def initialize(texter = Text.new)
-        @place_order = false
-        @texter = texter
-    end
+#     def initialize(texter = Text.new)
+#         @place_order = false
+#         @texter = texter
+#     end
 
-    def order
-        @place_order = true
-        @texter.send_confirmation
-    end
+#     def order
+#         @place_order = true
+#         @texter.send_confirmation
+#     end
 
-end
+# end
--- spec/order_takeaway_spec.rb
+++ spec/order_takeaway_spec.rb
@@ -2,12 +2,12 @@ require 'order_takeaway'
 require 'basket'
 require 'menu'
 
-describe OrderTakeaway do
+# describe OrderTakeaway do
 
-    it 'should place your order if satisfied with your basket' do
-        subject.order
-        expect(subject.place_order).to eq true
-    end
+#     it 'should place your order if satisfied with your basket' do
+#         subject.order
+#         expect(subject.place_order).to eq true
+#     end
 
 
-end 
+# end 
