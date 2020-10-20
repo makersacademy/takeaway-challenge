2ef478928178bcdd7d0663a9cbcc5cda5b15ad06
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-20 11:04:22 +0100

    Minor corrections to database connector file so that when manually run it returns takeaway_orders database table contents

--- db/db_connector.rb
+++ db/db_connector.rb
@@ -2,15 +2,15 @@ require 'pg'
 
 begin
  
-  con = PG.connect( dbname: 'takeaways' )
+  con = PG.connect( dbname: 'takeaway_orders' )
   
   rs = con.exec "SELECT * FROM takeaways"
 
-  rs each do |row|
+  rs.each do |row|
     puts "%s %s" % [ row['id'], row['takeaway'] ]
   end
 
-rescue PG::Error => Error
+rescue PG::Error => e
 
   puts e.message
 
