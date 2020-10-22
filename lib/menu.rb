5b0e291fd33bb0f48d918955f90d14bd143398e6
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-22 21:52:58 +0100

    first user story complete

---
+++ lib/menu.rb
@@ -0,0 +1,12 @@
+class ListofDishes
+  @@menu = [{ dish: "Pierogi (dumplings)", price: 7.95 },
+            { dish: "Bigos", price: 5.45 },
+            { dish: "Golonka (ham hock)", price: 9.95 },
+            { dish: "Krokiety (croquettes)", price: 8.45 },
+            { dish: "Mushroom Soup", price: 2.95 },
+            { dish: "Grilled Trout with mashed potatos", price: 11.95 },
+            { dish: "Gulasz stew", price: 13.95 }]
+  def see_list
+    @@menu.each { |dish| puts "#{@@menu.index(dish) + 1}. #{dish[:dish]}, price: £#{dish[:price]}" }
+  end
+end
