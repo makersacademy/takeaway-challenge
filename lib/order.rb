50f4c5ca2c72282aa9452422cbd7c9fec6e671cc
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-23 11:34:00 +0100

    third user storu complete and the rubocop and coverege passed at 100%

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,5 @@
 require_relative 'menu'
 class Order
-  attr_reader :order_list
   def initialize(menu_class:)
     @menu = menu_class
     @order_list = []
@@ -13,5 +12,16 @@ class Order
   def select(menu_dish_no, how_many)
     list = menu.menu
     @order_list << { item_and_price: list[menu_dish_no - 1], quantity: how_many }
+  end
+
+  def see_order
+    @order_list.each do |item|
+      puts "#{item[:item_and_price][:dish]}, £#{item[:item_and_price][:price]} x#{item[:quantity]}"
+    end
+  end
+
+  def total
+    sub_totals = @order_list.map { |item| item[:item_and_price][:price] * item[:quantity] }
+    puts "total to pay: £#{sub_totals.inject(0, :+)}"
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,18 +1,36 @@
 require_relative '../lib/order'
 
 describe Order do
-  let(:menu_list_double)  {double :menu_list}
-  let(:menu_class_double) {double :menu_class, new: menu_list_double}
-  subject {described_class.new(menu_class: menu_class_double)}
+  let(:menu_list_double)  { double :menu_list }
+  let(:menu_class_double) { double :menu_class, new: menu_list_double }
+  subject { described_class.new(menu_class: menu_class_double) }
 
   it { is_expected.to respond_to(:menu) }
   it { is_expected.to respond_to(:select) }
-  it { is_expected.to respond_to(:order_list) }
+  it { is_expected.to respond_to(:see_order) }
+  it { is_expected.to respond_to(:total) }
 
   describe "#select" do
     it "records a dish hash with price and qunatity into order_list" do
-      allow(menu_list_double).to receive(:menu).and_return([{dish:"dish", price: 1}])
-      expect(subject.select(1, 3)).to eq [{:item_and_price=>{dish:"dish", price: 1}, :quantity=>3}]
+      allow(menu_list_double).to receive(:menu).and_return([{ dish: "dish", price: 1 }])
+      expect(subject.select(1, 3)).to eq [{ item_and_price: { dish: "dish", price: 1 }, quantity: 3 }]
     end
   end
+  describe "#see_order" do
+    it "puts out order list" do
+      allow(menu_list_double).to receive(:menu).and_return([{ dish: "dish1", price: 1 }, { dish: "dish2", price: 2 }])
+      subject.select(1, 3)
+      subject.select(2, 4)
+      expect { subject.see_order }.to output("dish1, £1 x3\ndish2, £2 x4\n").to_stdout
+    end
+  end
+  describe "#total" do
+    it "puts out verified total to pay" do
+      allow(menu_list_double).to receive(:menu).and_return([{ dish: "dish1", price: 1 }, { dish: "dish2", price: 2 }])
+      subject.select(1, 3)
+      subject.select(2, 4)
+      expect { subject.total }.to output("total to pay: £11\n").to_stdout
+    end
+  end
+
 end
