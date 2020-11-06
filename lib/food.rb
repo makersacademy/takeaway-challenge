058b20018d790011a09cee709d562719699d4c8e
Alex Withington-Smith <alex@awithsmith.com>
2020-11-06 21:21:19 +0000

    added second test for Food class, wrote code to make it pass

--- lib/food.rb
+++ lib/food.rb
@@ -2,8 +2,10 @@ class Food
   @@id = 1
   attr_reader :id
 
-  def initialize
+  def initialize(item_name, item_price)
     @id = @@id
     @@id += 1
+    @item_name = item_name
+    @item_price = item_price
   end
 end
--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -2,8 +2,14 @@ require 'food'
 
 describe Food do
   it "creates with a unique menu ID" do
-    expect(Food.new.id).to eq(1)
-    expect(Food.new.id).to eq(2)
-    expect(Food.new.id).to eq(3)
+    expect(Food.new('', 0).id).to eq(1)
+    expect(Food.new('', 0).id).to eq(2)
+    expect(Food.new('', 0).id).to eq(3)
+  end
+
+  it "initializes with the correct name and price" do
+    sample_item = Food.new("Chicken", 5)
+    expect(sample_item.item_name).to eq("Chicken")
+    expect(sample_item.item_price).to eq(5)
   end
 end
