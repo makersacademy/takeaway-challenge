be3f5b0efa22ab929475963c42935969bef047fa
Georgie Horth <georgiehorth@gmail.com>
2020-11-07 16:04:05 +0000

    menu class & spec files

---
+++ lib/menu.rb
@@ -0,0 +1,12 @@
+class Menu
+  attr_reader :menu
+
+  def initialize
+    @menu = { burger: 3, cheeseburger: 4, chips: 2}
+  end
+
+  def print_menu
+    puts "Our menu is:"
+    @menu.each { |x,y| puts "#{x}, £#{y}"}
+  end
+end
