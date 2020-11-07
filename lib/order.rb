9bcd7747685e123ce7e0666b2040bc70e59a692d
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-07 18:01:09 +0000

    check_order prints out basket items, quantities and total

--- lib/order.rb
+++ lib/order.rb
@@ -1,21 +1,26 @@
 class Order
 
   MENU = {
-    'curry goat': 9,
-    'brown stew chicken': 8,
-    'jerk chicken': 8,
-    'steamed fish': 7,
-    'vegetable curry': 6,
-    'brown rice': 4,
-    'white rice': 4,
-    'roti': 5,
-    'salad': 4,
-    'fried dumplings': 3,
-    'coleslaw': 2
+    'curry goat': 9.00,
+    'brown stew chicken': 8.00,
+    'jerk chicken': 8.00,
+    'steamed fish': 7.00,
+    'vegetable curry': 6.00,
+    'rice and peas': 4.50,
+    'plain rice': 4.50,
+    'roti': 5.00,
+    'salad': 4.00,
+    'fried dumpling': 1.50,
+    'coleslaw': 1.00,
+    'beef patty': 2.50,
+    'lamb patty': 2.50,
+    'chicken patty': 2.50,
+    'saltfish patty': 2.50,
+    'veg patty': 2.50
   }
 
   def initialize
-    @basket = []
+    @basket = Hash.new(0)
   end
 
   def see_menu
@@ -26,11 +31,22 @@ class Order
     raise 'Please choose valid menu option' if not_on_menu?(item)
     raise 'Quantity must be a number' if not_a_number?(qty)
 
-    @basket << { item: item.to_sym, qty: qty }
+    @basket[item.to_sym] += qty
   end
 
   def basket
     @basket.dup
+  end
+
+  def check_order
+    puts "Your order"
+
+    @basket.each do |item, qty|
+      subtotal = MENU[item] * qty
+      puts "x#{qty} #{item}, #{price_format(subtotal)}"
+    end
+
+    puts "Total: #{price_format(order_total)}"
   end
 
   private
@@ -40,6 +56,14 @@ class Order
   end
 
   def not_a_number?(qty)
-    ![Integer, Float].include?(qty.class)
+    qty.class != Integer
+  end
+
+  def price_format(num)
+    "£#{sprintf('%.2f', num)}"
+  end
+
+  def order_total
+    @basket.reduce(0) { |sum, (item, qty)| sum + MENU[item] * qty }
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,12 +2,12 @@ require 'order'
 
 describe Order do
   describe '#see_menu' do
-    it 'returns a Hash of dishes' do
+    it 'returns a Hash of the available dishes' do
       expect(subject.see_menu).to be_instance_of(Hash)
     end
 
     describe '#add' do
-      it 'adds a menu item to the €basket array' do
+      it 'adds a menu item to the basket hash' do
         expect { subject.add('curry goat', 1) }.to change { subject.basket.length }.by(1)
       end
 
@@ -15,8 +15,18 @@ describe Order do
         expect { subject.add('pizza', 1) }.to raise_error('Please choose valid menu option')
       end
 
-      it 'raises error if user-entered-quantity is not a number' do
+      it 'raises error if quantity is not a number' do
         expect { subject.add('curry goat', '1') }.to raise_error('Quantity must be a number')
+      end
+    end
+
+    describe '#check_order' do
+      it 'prints out order items, quantities and total' do
+        subject.add('beef patty', 1)
+        subject.add('fried dumpling', 2)
+        expect { subject.check_order }.to output(
+          "Your order\nx1 beef patty, £2.50\nx2 fried dumpling, £3.00\nTotal: £5.50\n"
+          ).to_stdout
       end
     end
   end
