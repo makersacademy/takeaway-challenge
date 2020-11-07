ecf99d33baa50f3c9155c2915f3860f9e72f3876
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-07 15:47:40 +0000

    Simplified MENU hash

--- lib/order.rb
+++ lib/order.rb
@@ -1,15 +1,15 @@
 class Order
 
   MENU = {
-    1 => {item: :'curry goat', price: 9},
-    2 => {item: :'brown stew chicken', price: 8},
-    3 => {item: :'jerk chicken', price: 8},
-    4 => {item: :'steamed fish', price: 7},
-    5 => {item: :'vegetable curry', price: 6},
-    6 => {item: :'brown rice', price: 4},
-    7 => {item: :'white rice', price: 4},
-    8 => {item: :'roti', price: 5},
-    8 => {item: :'salad', price: 4}
+    'curry goat': 9,
+    'brown stew chicken': 8,
+    'jerk chicken': 8,
+    'steamed fish': 7,
+    'vegetable curry': 6,
+    'brown rice': 4,
+    'white rice': 4,
+    'roti': 5,
+    'salad': 4
   }
 
   def see_menu
