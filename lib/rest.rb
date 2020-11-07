c20cc32f2be316d1bf510f0c694ea1b95eb39cdb
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-07 18:49:39 +0000

    updated restaurant class

--- lib/rest.rb
+++ lib/rest.rb
@@ -1,8 +1,15 @@
 require_relative './menu.rb'
+require_relative './order.rb'
 
 class Rest
-  attr_reader :menu
+  attr_accessor :menu, :order
+
   def initialize
-    @menu = Menu::MENU
+    @menu = Menu.new
+    @order = nil
+  end
+
+  def place_order
+    @order = Order.new
   end
 end
--- spec/rest_spec.rb
+++ spec/rest_spec.rb
@@ -5,6 +5,10 @@ describe Rest do
     expect(subject).to be_an_instance_of Rest
   end
   it 'initialises a restaurant with the menu from Menu class' do
-    expect(subject.menu).to be_an_instance_of Hash
+    expect(subject.menu).to be_an_instance_of Menu
+  end
+
+  it 'creates a new instance of a menu' do
+    expect(subject.place_order). to be_an_instance_of Order
   end
 end
