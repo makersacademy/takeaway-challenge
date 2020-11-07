2aa21377802a8384236a92d9ae9e5e6d2f4e3f38
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-07 15:48:15 +0000

    Added test for add_to_basket to add to basket array

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,8 +2,14 @@ require 'order'
 
 describe Order do
   describe '#see_menu' do
-    it 'returns an Hash of dishes' do
+    it 'returns a Hash of dishes' do
       expect(subject.see_menu).to be_instance_of(Hash)
+    end
+
+    describe '#add_to_basket' do
+      it 'adds a menu item to the cart' do
+        expect { subject.add_to_basket('curry goat', 3) }.to change { subject.basket }.by(1)
+      end
     end
   end
 end
