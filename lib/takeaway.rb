9ab526a0218e6b3125b2545249ccf1eb481ba518
Margarida Martelo <margmartelo@gmail.com>
2020-11-08 19:44:11 +0000

    Second user story: select some number of several available dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,22 @@
 class Takeaway
+  attr_reader :orders_list
+
   def initialize(menu_instance)
     @menu_instance = menu_instance
+    @orders_list = []
   end
 
   def print_menu
     output = ""
-    @menu_instance.dishes.each do |dish|
-      output += "#{dish[:name]}: #{dish[:price]}" + "\n"
+    (0..@menu_instance.dishes.length-1).each do |index|
+      dish = @menu_instance.dishes[index]
+      output += "#{index+1}. #{dish[:name]}: #{dish[:price]}" + "\n"
     end
     return output
   end
 
+  def order(number)
+    raise "The number you chose it's not associated to any dish" unless number > 0 && number <= @menu_instance.dishes.length
+    @orders_list << number
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,6 +6,31 @@ describe Takeaway do
   it "will present a menu with a list of dishes and prices" do
     takeaway = Takeaway.new(portuguese_menu)
     allow(portuguese_menu).to receive(:dishes) { [{name: "Bacalhau com natas", price: 7.5}] }
-    expect(takeaway.print_menu).to eq("Bacalhau com natas: 7.5\n")
+    expect(takeaway.print_menu).to eq("1. Bacalhau com natas: 7.5\n")
+  end
+
+  # As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+
+  it "allow the users to choose the dishes they want to order" do
+    takeaway = Takeaway.new(portuguese_menu)
+    allow(portuguese_menu).to receive(:dishes) { [{name: "Bacalhau com natas", price: 7.5}] }
+    takeaway.order(1)
+    expect(takeaway.orders_list).to include(1)
+  end
+
+  it "will raise an error in the case the user enter an unexisting number" do
+    takeaway = Takeaway.new(portuguese_menu)
+    allow(portuguese_menu).to receive(:dishes) { [{name: "Bacalhau com natas", price: 7.5}] }
+    expect { takeaway.order(2) }.to raise_error "The number you chose it's not associated to any dish"
+  end
+
+  it "allow the users to choose more than one dish" do
+    takeaway = Takeaway.new(portuguese_menu)
+    allow(portuguese_menu).to receive(:dishes) { [{name: "Bacalhau com natas", price: 7.5}, {name: "Ameijoas a Bulhao Pato", price: 8}, {name: "Polvo a Lagareiro", price: 10}] }
+    takeaway.order(1)
+    takeaway.order(3)
+    expect(takeaway.orders_list).to include(1, 3)
   end
 end
