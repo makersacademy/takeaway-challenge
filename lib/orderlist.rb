ffd965dc3c19f94cc4641b194e15622e4ee49c5a
Alex Withington-Smith <alex@awithsmith.com>
2020-11-08 10:35:44 +0000

    refactored OrderList for increased clarity

--- lib/orderlist.rb
+++ lib/orderlist.rb
@@ -7,28 +7,53 @@ class OrderList
   end
 
   def new_order
-    order = @order_class.new
+    pending_order = @order_class.new
+    
     loop do
-      puts "Respond 1 to see the menu, 2 to add to your order, confirm to place order, or quit to cancel"
+      show_choices
+
       response = @input.gets.chomp
       break if response == "quit"
 
       if response == "1"
         @menu.display_all
       elsif response == "2"
-        id = @input.gets.chomp.to_i
-        selection = @menu.select(id)
-        quantity = @input.gets.chomp.to_i
-        order.add(selection, quantity)
+        add_to_order(pending_order)
       elsif response == "confirm"
-        raise "Your order is empty!" if order.empty?
-        @confirmed_orders.push(order)
-        puts "Order received! You'll get a text shortly confirming your delivery time."
+        finalise_order(pending_order)
         break
       end
 
-      puts "Current order:"
-      puts order
+      display_order(pending_order)
     end
+  end
+
+  private
+
+  def show_choices
+    puts "Enter:"
+    puts "1 to see the menu"
+    puts "2 to add to your order"
+    puts "'confirm' to place your order"
+    puts "'quit' to cancel"
+  end
+
+  def add_to_order(order)
+    id = @input.gets.chomp.to_i
+    selection = @menu.select(id)
+    quantity = @input.gets.chomp.to_i
+    order.add(selection, quantity)
+  end
+
+  def finalise_order(order)
+    raise "Your order is empty!" if order.empty?
+
+    @confirmed_orders.push(order)
+    puts "Order received! You'll get a text shortly confirming your delivery time."
+  end
+
+  def display_order(order)
+    puts "Current order:"
+    puts order
   end
 end
