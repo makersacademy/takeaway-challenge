a1d4c7448f70fef6a9d4ac1f38302164c45c46b5
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-08 08:33:42 +0000

    Added mocked and stubbed test for send_order_confirmation

--- spec/twilio_client_spec.rb
+++ spec/twilio_client_spec.rb
@@ -1,5 +1,15 @@
 require 'twilio_client'
 
 describe TwilioClient do
-  
+  describe '#send_order_confirmation' do
+    let(:messages) { double :messages }
+    let(:object) { double :object }
+    let(:create) { double :create }
+
+    it 'sends a confirmation text to customer' do
+      allow(subject.client).to receive(:messages) { object }
+      allow(object).to receive(:create) { 'Thank you for your order' }
+      expect(subject.send_order_confirmation('Malachi', ENV['MOBILE_NUMBER'])).to eq('Thank you for your order')
+    end
+  end
 end
