b0ade6ee7d153d2d8b07ae10db962320f71d8c34
Joshua Retallick <josh@old-empire.co.uk>
2020-11-08 20:33:33 +0000

    Split everything in to classes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,40 +1,47 @@
+require_relative './menu.rb'
+require_relative './order.rb'
+
 class TakeAway
 
-  attr_reader :dishes, :order, :sms
+  attr_reader :order, :menu, #:dishes
 
   def initialize
-    @dishes = { hotdog: 5.00, fries: 3.50 }
+    #@dishes = { hotdog: 5.00, fries: 3.50 }
     @order = {}
-    @sms = sms
+    @menu = Menu.new
   end
 
-  def print_menu
-    dishes.map do |dish, price|
-      "%s £%.2f" % [dish.to_s.capitalize, price]
-    end.join(", ")
+  def place_order
+    @order = Order.new
   end
 
-  def add(dish, quantity)
-    fail "#{dish.capitalize} is not on the menu!" unless has_dish?(dish)
-    order[dish] = quantity
-  end
+  # def print_menu
+  #   dishes.map do |dish, price|
+  #     "%s £%.2f" % [dish.to_s.capitalize, price]
+  #   end.join(", ")
+  # end
 
-  def total
-    item_totals.inject(:+)
-  end
+  # def add(dish, quantity)
+  #   fail "#{dish.capitalize} is not on the menu!" unless has_dish?(dish)
+  #   order[dish] = quantity
+  # end
 
-  def price(dish)
-    dishes[dish]
-  end
+  # def total
+  #   item_totals.inject(:+)
+  # end
 
-  def item_totals
-    order.map do |dish, quantity|
-      dishes[dish] * quantity
-    end
-  end
+  # def price(dish)
+  #   dishes[dish]
+  # end
 
-  def has_dish?(dish)
-    !dishes[dish].nil?
-  end
+  # def item_totals
+  #   order.map do |dish, quantity|
+  #     dishes[dish] * quantity
+  #   end
+  # end
+
+  # def has_dish?(dish)
+  #   !dishes[dish].nil?
+  # end
 
 end
