a12f278a0efe6e2c04e6da6af2b122435735327c
Francesco <francescofacca@gmail.com>
2020-11-08 18:32:05 +0000

    Add class menu that prints a menu of dishes with prices

---
+++ spec/menu_spec.rb
@@ -0,0 +1,11 @@
+require 'menu'
+
+describe Menu do
+  it { is_expected.to respond_to(:print_menu) }
+
+  describe "print_menu" do
+    it "prints a menu" do
+      expect { subject.print_menu }.to output("1. Edamame £3.95\n2. Miso Soup £4.95\n3. Sake Sashimi £7.95\n4. Toro Sashimi £8.95\n5. Salmon and Avocado Roll £9.95\n6. Spicy Tuna Roll £11.95\n7. Toro Dragon Roll £14.95\n").to_stdout
+    end
+  end
+end
