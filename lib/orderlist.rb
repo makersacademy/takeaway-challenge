68eacdadb461ac1552bc295ebbe359bd5f3066d5
Alex Withington-Smith <alex@awithsmith.com>
2020-11-08 08:15:57 +0000

    added test for OrderList #new_order to allow user to add items properly

--- lib/orderlist.rb
+++ lib/orderlist.rb
@@ -1,16 +1,23 @@
 class OrderList
-  def initialize(restaurant, input = Kernel)
-    @restaurant = restaurant
+  def initialize(restaurant, input = Kernel, order_class = Order)
+    @menu = restaurant.menu
+    @order_class = order_class
     @input = input
   end
 
   def new_order
+    order = @order_class.new
     loop do
       puts "Respond 1 to see the menu, 2 to add to your order, or quit to cancel"
       response = @input.gets.chomp
       break if response == "quit"
       if response == "1"
-        @restaurant.menu.display_all
+        @menu.display_all
+      elsif response == "2"
+        id = @input.gets.chomp.to_i
+        selection = @menu.select(id)
+        quantity = @input.gets.chomp.to_i
+        order.add(selection, quantity)
       end
     end
   end
--- spec/orderlist_spec.rb
+++ spec/orderlist_spec.rb
@@ -7,22 +7,26 @@ describe OrderList do
   let(:kernel) { double :kernel }
   let(:menu) { double :menu }
   let(:restaurant) { double :restaurant, menu: menu }
-  let(:test_list) { OrderList.new(restaurant, kernel) }
+  let(:test_list) { OrderList.new(restaurant, kernel, order_class) }
+
   describe "#new_order" do
     it "starts the process of taking an order, lets user immediately quit" do
+      expect(order_class).to receive(:new)
       expect(kernel).to receive(:gets).and_return("quit")
       test_list.new_order
     end
 
     it "displays the menu when prompted, then lets user quit" do
-      expect(kernel).to receive(:gets).and_return("1")
+      expect(kernel).to receive(:gets).and_return("1", "quit")
       expect(menu).to receive(:display_all)
-      expect(kernel).to receive(:gets).and_return("quit")
       test_list.new_order
     end
 
     it "feeds choices through to #add in the order" do
-      skip
+      expect(kernel).to receive(:gets).and_return("2", "1", "1", "quit")
+      expect(menu).to receive(:select).with(1).and_return(item_ordered)
+      expect(order).to receive(:add).with(item_ordered, 1)
+      test_list.new_order
     end
 
 
