693de9f6d73ed5f1db7caa0a3730580c5fc2c550
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-08 16:05:32 +0000

    Refactored Menu tests

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,15 @@
 require 'menu'
 
 describe Menu do
-  describe '#see_menu' do
+  describe '#dishes' do
+    it 'returns the MENU constant' do
+      expect(subject.dishes).to eq(Menu::MENU)
+    end
+  end
+  
+  describe '#user_menu' do
     it 'prints the available dishes and their prices' do
-      expect { subject.see_menu }.to output.to_stdout
+      expect { subject.user_menu }.to output.to_stdout
     end
   end
 end
