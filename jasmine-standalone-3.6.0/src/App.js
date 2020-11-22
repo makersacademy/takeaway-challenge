468f6953b0dbe2918f23fe93b7fa047611c71260
Lorna Roberts <lornaroberts99@gmail.com>
2020-11-22 21:31:36 +0000

    done except for readme

--- jasmine-standalone-3.6.0/src/App.js
+++ jasmine-standalone-3.6.0/src/App.js
@@ -4,12 +4,18 @@ class Menu{
   constructor(){
     this.order = [];
     this.items = [
-     {"item": "fish and chips", "price": 4.99, "quantity": 0, "totalPrice": 0},
-     {"item": "deep fried pizza", "price": 3.99, "quantity": 0, "totalPrice": 0},
-     {"item": "battered sausage", "price": 2.99, "quantity": 0, "totalPrice": 0},
-     {"item": "battered white pudding", "price": 2.50, "quantity": 0, "totalPrice": 0},
-     {"item": "deep fried mars bar", "price": 1.50, "quantity": 0, "totalPrice": 0},
-     {"item": "Irn Bru 1.5l bottle", "price": 1.99, "quantity": 0, "totalPrice": 0}
+     {"item": "fish and chips", "price": 4.99,
+      "quantity": 0, "totalPrice": 0},
+     {"item": "deep fried pizza", "price":
+      3.99, "quantity": 0, "totalPrice": 0},
+     {"item": "battered sausage", "price": 2.99,
+      "quantity": 0, "totalPrice": 0},
+     {"item": "battered white pudding", "price": 2.50,
+      "quantity": 0, "totalPrice": 0},
+     {"item": "deep fried mars bar", "price": 1.50,
+      "quantity": 0, "totalPrice": 0},
+     {"item": "Irn Bru 1.5l bottle", "price": 1.99,
+      "quantity": 0, "totalPrice": 0}
    ];
   }
     increase(item){
@@ -22,9 +28,7 @@ class Menu{
       console.log("decrease function in action")
       this.items[item]["quantity"] -= 1;
       console.log(this.items[item].quantity)
-
     }
-
 
      choice(){
        for (var i = 0; i <this.items.length; i++){
@@ -44,37 +48,20 @@ class Menu{
             let quantity = this.items[i].quantity;
             let food = this.items[i].item;
             let price = this.items[i].price * quantity;
-             this.order.push({"food": food, "quantity": quantity, "price": price});
+             this.order.push({"food": food, "quantity": quantity, "price": +price.toFixed(2)});
            }
-
-         }   return this.order;
-
-
+          }   return this.order;
          }
 
-
-
-
-
-   totals() {
+     totals() {
      let array = this.order;
-   let prices =[];
-    for (var j = 0; j < array.length; j++){
-      prices.push(array[j]["price"]);
-    }
-    let total = 0;
-        for (var l = 0; l < prices.length; l++){
-                 total += prices[l];
+     let prices =[];
+     for (var j = 0; j < array.length; j++){
+       prices.push(array[j]["price"]);
       }
-       return total;
-  }
-//
-//   receipt(){
-//     let array2 = this.choice();
-//     for (var j = 0; j < array2.length; j++){
-//       return array2[j] + array2[j+1] + array2[j+2]+'\n';
-//       j +=2;
-//   }
-// }
-//}
-}
+     let total = 0;
+        for (var l = 0; l < prices.length; l++){
+           total += prices[l];
+        }return total;
+      }
+     }
