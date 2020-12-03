a79a47fcb60eebd5c5f4f66d5e13610f642b4c07
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-03 20:24:08 +0000

    Time now stubbed in confirmation spec

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -7,21 +7,21 @@ class Confirmation
     @client = client || Twilio::REST::Client.new(env["TWILIO_ACCOUNT_SID"], env["TWILIO_AUTH_TOKEN"])
   end
   
-  def send_message
+  def send_message(time_class = Time)
     @client.messages.create(
-      message_contents
+      message_contents(time_class)
     )
   end
 
-  def message_contents
+  def message_contents(time_class)
     { from: @env['TWILIO_PHONE_NO'],
     to: @env['PHONE_NO'],
-    body: "Thank you! Your order was placed and will be delivered before #{eta}." }
+    body: "Thank you! Your order was placed and will be delivered before #{eta(time_class)}." }
   end
 
-  def eta
-    time = Time.now + 60 * 60
-    time.strftime("%H:%M")
+  def eta(time_class)
+    time = time_class.now
+    "#{time.hour + 1}:#{time.min}"
   end
 
 end
--- spec/confirmation_spec.rb
+++ spec/confirmation_spec.rb
@@ -3,25 +3,25 @@ require 'confirmation'
 describe Confirmation do
   let(:messages) { spy :messages }
   let(:client) { double :client, messages: messages }
-  let(:time) { Time.new + 60 * 60 }
-  let(:estimated_time) { time.strftime("%H:%M") }
+  let(:time) { double :time, now: estimated_time }
+  let(:estimated_time) { double :time, hour: 12, min: 15 }
   let(:env) { { 
       'TWILIO_ACCOUNT_SID' => '123',
       'TWILIO_AUTH_TOKEN' => '321',
       } 
-    }
+  }
   let(:message_contents) { 
-  { body: "Thank you! Your order was placed and will be delivered before #{estimated_time}.",
-  from: nil,
-  to: nil } 
-   }
+    { body: "Thank you! Your order was placed and will be delivered before 13:15.",
+    from: nil,
+    to: nil } 
+  }
   let(:confirmation) { Confirmation.new(env, client) }
   let(:body) { "test" }
   
   it { is_expected.to respond_to(:send_message) }
 
   it 'creates a new message' do  
-    confirmation.send_message
+    confirmation.send_message(time)
     expect(messages).to have_received(:create).with(message_contents)
   end
 
@@ -29,7 +29,7 @@ describe Confirmation do
     it { is_expected.to respond_to(:eta) }
 
     it 'returns current time + 1 hour' do
-      expect(confirmation.eta).to eq(estimated_time)
+      expect(confirmation.eta(time)).to eq("13:15")
     end
   end
 
