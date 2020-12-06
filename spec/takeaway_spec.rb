a09af12880a52545a51a603890205f98983c0578
xavierloos <xavier.loos04@gmail.com>
2020-12-06 11:23:34 +0000

    updating spec

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,8 @@
 require "takeaway"
 describe TakeAway do
+  let(:neworder) { TakeAway.new }
   it { is_expected.to respond_to :menu }
-  it { is_expected.to respond_to :order }
+  # it { is_expected.to respond_to :add2_basket }
   it { is_expected.to respond_to :summary }
   context "has a menu" do
     it { is_expected.to respond_to { } }
@@ -11,19 +12,32 @@ describe TakeAway do
   end
 
   describe "#menu" do
-    it "contains meals" do
+    it "contains items" do
+      expect(subject.menu).not_to be_empty
+    end
+    it "shows items" do
       expect(subject.menu).not_to be_empty
     end
   end
-  describe "#order" do
-    it { is_expected.to respond_to(:order).with(1).argument }
+  describe "#add" do
+    it { is_expected.to respond_to(:add).with(1).argument }
+    context "empty query" do
+      it "meal not found" do
+        expect { neworder.add("") }.to raise_error described_class::Error[0]
+      end
+    end
   end
   describe "#summary" do
-    let(:order) { describe_class.new }
-    order.order("spring roll")
-    order.order("char sui bun")
+    it "shows empty basket" do
+      expect { neworder.summary(0) }.to raise_error described_class::Error[2]
+    end
+    before do
+      neworder.add("Spring roll")
+      neworder.add("Char sui bun")
+    end
+
     it "shows total" do
-      expect(processor.process("foo", validator)).to eq("foo processed")
+      expect(neworder.summary).to eq 4.98
     end
   end
 end
