4642495b52eb3d95f8445f0c1e32aedac24520fa
Sujeevan Rajagopalan <rsujee@MA307.local>
2020-12-06 23:35:31 +0000

    Testing show_menu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,5 +5,10 @@ describe Takeaway do
     expect(subject).to be_an_instance_of Takeaway
   end
 
-  
+  describe "#show_menu" do
+    it 'shows the menu' do
+      expect(subject).to respond_to(:show_menu)
+    end
+  end
+
 end
