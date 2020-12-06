1aed90c8a859f42d35e571feac7637918c87f3da
xavierloos <xavier.loos04@gmail.com>
2020-12-06 11:22:48 +0000

    splitting the class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,18 +1,40 @@
+require_relative "menu"
+require_relative "checkout"
+
 class TakeAway
+  attr_accessor :checkout
+  Error = ["Meal Not Found, try again", "Empty menu", "Empty basket"]
   # attr_reader :order
   def initialize
-    @menu = { "spring roll" => 0.99, "char sui bun" => 3.99, "pork dumpling" => 2.99, "peking duck" => 7.99, "fu-king fried rice" => 5.99 }
-    @basket = []
+    @checkout = Checkout.new #access to checkout class
+    @menu = Menu.new
   end
 
   def menu
-    @menu
+    fail Error[1] if @menu.show_menu.empty?
+    @menu.show_menu
   end
 
-  def order(meal)
-    @basket << @menu[meal]
+  #add meal to the basket
+  def add(meal)
+    item = @menu.menu
+    item = item[meal]
+    fail Error[0] if meal.empty?
+    puts "#{item} added to your basket"
+    #basket is global to added in checkout
+    $basket << item
   end
+
+  # def check_basket
+  #   checkout.added
+  # end
 
   def summary
+    fail Error[2] if $basket.size == 0
+    @checkout.total
+  end
+
+  def payment(price)
+    @checkout.payment
   end
 end
