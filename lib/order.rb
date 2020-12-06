67457263eb365cc7781006076c93096ed4f885d6
Irina <igiosanu29@gmail.com>
2020-12-06 18:53:53 +0000

    Check total matches given price

--- lib/order.rb
+++ lib/order.rb
@@ -9,4 +9,12 @@ class Order
   def add_dish(dish)
     @order << { dish => @menu.dish_list[dish] }
   end
+
+  def total_price
+    @order.map { |hash| hash.values }.flatten.sum
+  end
+
+  def check_price(price)
+    price == total_price
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -8,4 +8,24 @@ describe Order do
       expect(subject.order).to eq([ { "Fish and chips" => 10.99 }, { "Beef wellington" => 8.99 } ])
     end
   end
+
+  describe "#total_price" do
+    it "returns cost of entire order" do
+      subject.add_dish("Fish and chips")
+      subject.add_dish("Beef wellington")
+      expect(subject.total_price).to eq(19.98)
+    end
+  end
+
+  describe "#check_price" do
+    it "returns true if given price matches total price" do
+      subject.add_dish("Fish and chips")
+      subject.add_dish("Beef wellington")
+      expect(subject.check_price(19.98)).to be_truthy
+    end
+
+    it "returns false if given price does not match total price" do
+      expect(subject.check_price(20.00)).to be_falsey
+    end
+  end
 end
