d4589840a26e219674dc98afcb9ac517276a6643
Sujeevan Rajagopalan <rsujee@MA307.local>
2020-12-06 23:28:56 +0000

    Able to see dishes and prices

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,16 @@
 class Takeaway
+  attr_reader :menu
+
+  def initialize
+    @menu = {
+      Fish: 5.00,
+      Scampi: 5.50,
+      Chips: 1.50,
+      Gravy: 2.00 
+    }
+  end
+
+  def show_menu
+    @menu.each { |meal, price| puts "-- #{meal} : £#{price} --" }
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,5 +4,6 @@ describe Takeaway do
   it 'is an instance of a takeaway class' do
     expect(subject).to be_an_instance_of Takeaway
   end
+
+  
 end
- 
