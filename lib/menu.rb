ba38e0d16fde92aeb8bbf0fbd798c460f7c3ae84
xavierloos <xavier.loos04@gmail.com>
2020-12-06 13:03:15 +0000

    Menu

---
+++ lib/menu.rb
@@ -0,0 +1,31 @@
+class Menu
+  attr_accessor :menu
+
+  def initialize
+    @menu = {
+      "Spring roll" => 0.99,
+      "Char sui bun" => 3.99,
+      "Pork dumpling" => 2.99,
+      "Peking duck" => 7.99,
+      "Fu-king fried rice" => 5.99,
+    }
+  end
+
+  def show_menu
+    puts "---------------------------------------"
+    puts "*               MENU                  *"
+    puts "---------------------------------------"
+    @menu.each do |item, price|
+      puts "#{item} ........ #{price}".center(40)
+    end
+    puts "---------------------------------------"
+  end
+
+  def add2_menu
+    puts "Please add the name of the item:"
+    item = gets.chomp
+    puts "Please add the price of the item:"
+    price = gets.chomp
+    @menu[item] = price
+  end
+end
