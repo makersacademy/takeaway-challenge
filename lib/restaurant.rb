4f28bfbe83fc3e7489326d144a91848b29ec2c53
Sarah Power <sarahjonipower0@gmail.com>
2020-12-06 20:51:42 +0000

    total_basket and total_price methods added

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,14 +1,32 @@
 class Restaurant
-  attr_reader :menu
+  attr_accessor :menu, :basket, :total_price
     
   def initialize
-    @menu = { 'Margarita': 7, 'Bufala': 9, 'Neapolitan': 10 }
+    @menu = { 'Margarita': 7, 'Bufala': 9, 'Neapolitan': 10, 'Rocket salad': 5 }
+    @basket = {}
+    @total_price = 0
   end
     
   def list_menu
     @menu.each do |dish, price|
       puts "#{dish} : #{price}"
     end 
+  end
+
+  def order(dish, quantity)
+    @basket[dish] = quantity
+    @total_price += @menu[dish.to_sym] * quantity
+  end
+
+  def total_basket
+    puts "You have ordered:"
+     @basket.each do |dish, quantity|
+        puts "#{dish} : #{quantity}"
+     end
+  end
+
+  def total_price
+    puts "Total bill: £#{@total_price}" 
   end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -8,4 +8,39 @@ describe Restaurant do
   it 'responds to the method .list' do
     expect(subject).to respond_to(:list_menu)
   end
+
+  it 'responds to the method .order' do
+    expect(subject).to respond_to(:order).with(2).argument
+  end
+
+  it 'responds to the method .total_basket' do 
+    expect(subject).to respond_to(:total_basket)
+  end
+
+  it 'responds to method .total_price' do
+    expect(subject).to respond_to(:total_price)
+  end
+
+  describe '#order(dish,quantity)' do
+    it 'adds the order to the basket' do
+        subject.order('Margarita', 2)
+        expect(subject.basket).to include({'Margarita' => 2})
+    end
+  end 
+
+  describe '#total_basket' do
+    it 'can total multiple orders into one basket' do
+    subject.order('Margarita', 2)
+    subject.order('Bufala', 10)
+    expect(subject.total_basket).to include({'Margarita' => 2, 'Bufala' => 10})
+    end
+  end
+
+#   describe '#total_price' do
+#     it 'can total the price of multiple orders' do
+#     subject.order('Margarita', 2)
+#     subject.order('Bufala', 10)
+
+#   end 
+
 end 
