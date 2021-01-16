7a369db8e78a1d214b29874d0e7f0ec8d1c21ada
Anna Cavalla <anna.cavalla@gmail.com>
2021-01-16 18:39:03 +0000

    Added menu and takeaway classes and specs; empty show_menu method added

---
+++ lib/takeaway.rb
@@ -0,0 +1,8 @@
+class Takeaway
+  def initialize
+
+  end
+
+  def show_menu
+  end
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,6 @@
+require 'takeaway'
+
+describe Takeaway do
+  it {is_expected.to respond_to(:show_menu)}
+
+end
