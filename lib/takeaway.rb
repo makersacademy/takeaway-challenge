28480b548ee9cdc9d583b7bcfcb24710d7bdfa53
Hamish Arro <Hamisharro@icloud.com>
2021-01-16 15:40:39 +0000

    add dish.rb, takeaway.rb and related specs for each file

---
+++ lib/takeaway.rb
@@ -0,0 +1,20 @@
+require_relative 'dish'
+
+class Takeaway
+  attr_reader :order, :quantity
+
+  def initialize
+    @order = Array.new
+  end
+
+  def add_dish(quantity, dish)
+    @order << {dish: dish, quantity: quantity}
+  end
+
+  def order_sum
+    sum = 0
+    @order.each{ |dish_details| sum += dish_details[:quantity] * dish_details[:dish].price }
+    sum
+  end
+
+end
