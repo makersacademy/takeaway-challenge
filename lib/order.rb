24ec56a5a3bc3710de9bd305317cd60298c4702d
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-16 15:42:46 +0000

    updated order spec and rb file

--- lib/order.rb
+++ lib/order.rb
@@ -3,5 +3,22 @@ require_relative 'takeaway'
 
 class Order
 
+  attr_reader :dishes, :menu
+
+  def initialize(menu)
+    @dishes = Hash.new(0)
+    @menu = menu
+  end
+
+  def add_order(dish, quantity)
+    fail "#{dish.capitalize} is not on the menu!" unless menu.has_dish?(dish)
+    dishes[dish] += quantity
+  end
+
+  private
+
+  # def dish?(dish)
+  #   @menu[dish]
+  # end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,14 +2,25 @@ require 'order'
 
 describe Order do
 
-  subject { described_class.new }
+  subject { described_class.new(menu) }
 
   let(:dishes) { { burger: 2, hamburger: 1,} }
 
+  let(:menu) { double(:menu) }
+
+  before do
+    allow(menu).to receive(:has_dish?).with(:burger).and_return(true)
+    allow(menu).to receive(:has_dish?).with(:hamburger).and_return(true)
+  end
+
   it 'can order dishes from the menu' do
-    subject.add(burger: 2)
-    subject.add(hamburger: 1)
+    subject.add_order(:burger, 2)
+    subject.add_order(:hamburger, 1)
     expect(subject.dishes).to eq(dishes)
   end
 
+  it 'raises and error when dish not on the menu' do
+    allow(menu).to receive(:has_dish?).with(:beef).and_return(false)
+    expect { subject.add_order(:beef, 2) }.to raise_error "Beef is not on the menu!"
+  end
 end
