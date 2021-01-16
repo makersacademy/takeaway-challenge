9dc3950758e07196f06dc60e83773ed4194a04f1
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-16 10:22:12 +0000

    User Story #1 - refactored/passed/completed

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -4,15 +4,16 @@ describe 'User Stories' do
     # So that I can check if I want to order something
     # I would like to see a list of dishes with prices
 
+    it 'allows you to add a new dish to the menu' do
+        menu = Menu.new
+        expect { menu.add(:dish) }.not_to raise_error
+    end
+    
     it '#1 - so I can see what to order, show me a list of dishes with prices' do
         menu = Menu.new
+        menu.add('Ramen - £1.99')
+        menu.add('Katsu - £2.49')
         menu.list
-    end
-
-    it 'allows you to add a new dish to the menu' do
-        dish = Dish.new
-        menu = Menu.new
-        expect { menu.add(dish) }.not_to raise_error
     end
 
 end
