87a1fd75c9c28d51ff03b9d5e586fde8cd20f899
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-01-16 18:06:53 +0000

    refactored into 2 classes

--- lib/takeaway_menu.rb
+++ lib/takeaway_menu.rb
@@ -1,14 +1,19 @@
 class Menu
-  attr_reader :dishes
+  attr_accessor :dishes
   def initialize
     @dishes = { "onion bhaji" => 2.95, "vegetable samosa" => 2.95, "aloo pakora" => 2.95,
                 "chicken tikka" => 3.95, "mixed kebab" => 4.95 }
+  end
+
+  def add_item(item, price)
+    @dishes[item] = price
+    "#{item}, #{price}"
   end
 end
 
 
 class Takeaway
-  attr_reader :dishes
+  
   attr_accessor :basket
   def initialize
     @dishes = Menu.new.dishes
@@ -30,9 +35,9 @@ class Takeaway
   end
 
   def total
-    @basket.map do |_menu_item, price|
-      t@otal += price
-    end
-    total
+    @basket.map { |item, quantity|
+      @total +=  (@dishes[item] * quantity)
+    }
+    @total.round(2)
   end
 end
--- spec/takeaway_menu_spec.rb
+++ spec/takeaway_menu_spec.rb
@@ -1,12 +1,6 @@
 require 'takeaway_menu'
 
 describe Takeaway do
-  describe '#dishes' do
-    it 'holds a list of dishes and prices' do
-      allow(subject).to receive(:dishes).and_return({ "onion bhaji" => 2.95 })
-      expect(subject.dishes).to eq ({ "onion bhaji" => 2.95 })
-    end
-  end
 
   describe '#read_menu' do
     it 'reads the menu to the user' do
@@ -17,7 +11,7 @@ describe Takeaway do
 
   describe '#order' do
     it 'will find the item and price' do
-      expect(subject.order("onion bhaji")).to eq 'onion bhaji, 2.95'
+      expect(subject.order("onion bhaji")).to eq 'onion bhaji, x1'
     end
   end
 
@@ -26,9 +20,15 @@ describe Takeaway do
       expect(subject.basket).to eq ({})
     end
 
-    it 'holds the item and price of an ordered item' do
-      subject.order("onion bhaji")
-      expect(subject.basket).to eq ({ "onion bhaji" => 2.95 })
+    it 'holds the item and quantity of an ordered item' do
+      subject.order("onion bhaji", 4)
+      expect(subject.basket).to eq ({ "onion bhaji" => 4 })
+    end
+
+    it 'allows you to add duplicated items and updates the quantity' do
+      subject.order("onion bhaji", 4)
+      subject.order("onion bhaji", 4)
+      expect(subject.basket).to eq ({ "onion bhaji" => 8 })
     end
   end
 
@@ -38,5 +38,17 @@ describe Takeaway do
       subject.order("vegetable samosa")
       expect(subject.total).to eq 5.9
     end
+  end
+end
+
+describe Menu do
+
+  it 'tells you the items on the menu' do
+    expect(subject.dishes).to eq ({ "onion bhaji" => 2.95, "vegetable samosa" => 2.95,
+    "aloo pakora" => 2.95, "chicken tikka" => 3.95, "mixed kebab" => 4.95 })
+  end
+
+  it 'allows you to add items to the menu' do
+    expect(subject.add_item("aloo chat", 1.95)).to eq "aloo chat, 1.95"
   end
 end
