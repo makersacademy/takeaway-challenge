0e81853be135199b673396d1bf01c2dad5fc9226
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-16 11:59:15 +0000

    user story 2

--- lib/menu.rb
+++ lib/menu.rb
@@ -8,6 +8,9 @@ class Menu
         dishes.map do |title, price| 
           "%s £%.2f" % [title.to_s.capitalize, price]  
         end.join(", ")
-
+    end
+    
+    def has_dish?(dish)
+        !dishes[dish].nil?
     end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,8 +5,8 @@ describe Menu do
 
     let(:dishes) do 
         {
-            Chicken: 3.99,
-            Falafel: 4.50
+            chicken: 3.99,
+            falafel: 4.50
         }
     end
     it 'has a list of dishes with prices' do 
@@ -17,4 +17,12 @@ describe Menu do
         printed_menu = "Chicken £3.99, Falafel £4.50"
         expect(menu.print).to eq(printed_menu)
     end
+
+    it "tells if a dish is on the menu" do 
+        expect(menu.has_dish?(:chicken)).to be true 
+    end 
+
+    it "tells if a dish is not on the menu" do 
+        expect(menu.has_dish?(:beef)).to eq false 
+    end 
 end 
