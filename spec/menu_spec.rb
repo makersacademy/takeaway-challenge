24e2a174d7a8ab3ac9f0c4e63bf0075333edd613
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-16 13:58:38 +0000

    Testing for formatting menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,18 +6,27 @@ describe Menu do
   let (:path_to_config) { "CONFIG_PATH" }
   subject { Menu.new(path_to_config, file_reader, json_parser) }
 
+  before :each do
+    menu_json_data = "MENU_DATA"
+    @menu_hash_data = [{name: "dish1", price: 5}, {name: "cool_dish", price: 4}]
+
+    allow(file_reader).to receive(:read).with(path_to_config).and_return(menu_json_data)
+    allow(json_parser).to receive(:parse).with(menu_json_data).and_return(@menu_hash_data)
+  end
+
   describe '#initialize' do
 
     it 'should load menu from config file and store as array of hashes' do
-      menu_json_data = "MENU_DATA"
-      menu_hash_data = []
-
-      allow(file_reader).to receive(:read).with(path_to_config).and_return(menu_json_data)
-      allow(json_parser).to receive(:parse).with(menu_json_data).and_return(menu_hash_data)
-
-      expect(subject.dishes).to eq(menu_hash_data)
+      expect(subject.dishes).to eq(@menu_hash_data)
     end
+  end
 
+  describe '#pretty_dishes' do
+
+    it 'should return dishes as formatted string' do
+      expected_menu = "dish1....................£5.00\ncool_dish..............£4.00"
+      expect(subject.pretty_dishes).to eq expected_menu
+    end
   end
 
 end
