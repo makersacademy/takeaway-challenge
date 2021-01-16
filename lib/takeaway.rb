ecb213ee0c932ab6c66c7a1f76f29cc31087ea6f
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-16 15:04:01 +0000

    Takeaway can now show menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,12 @@
 class Takeaway
 
+  def initialize(menu, printer)
+    @menu = menu
+    @printer = printer
+  end
+
+  def show_menu
+    @printer.print(@menu.pretty_dishes)
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,7 +2,7 @@ require 'takeaway'
 
 describe Takeaway do
   let (:menu) { double('menu') }
-  let (:printer) { printer('printer') }
+  let (:printer) { double('printer') }
   subject { Takeaway.new(menu, printer) }
 
   describe '#show_menu' do
@@ -10,7 +10,6 @@ describe Takeaway do
     it 'should present menu to user' do
       menu_dishes = 'dish'
       allow(menu).to receive(:pretty_dishes).and_return(menu_dishes)
-      allow(printer).to receive(:print)
       expect(printer).to receive(:print).with(menu_dishes)
 
       subject.show_menu
