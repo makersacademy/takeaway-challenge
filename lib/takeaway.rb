fced75307929d081f6209e92279ab78564aa00b3
bullhornfixie <dawson.jon@hotmail.com>
2021-01-16 18:49:06 +0000

    Read menu unit test complete

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,12 +1,13 @@
 class Takeaway 
-  attr_reader :basket
+  attr_reader :basket, :menu
     
  def initialize 
    @basket = nil
+   @menu = {"chow mein": 1.99, "chicken balls": 2.00, "duck pancakes": 1.00}
  end
     
  def read_menu
-   menu = {}
+   @menu
  end
     
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,11 +4,12 @@ describe Takeaway do
   
   context 'On initializing Takeaway' do
     it { is_expected.to respond_to(:basket) }
+    it { is_expected.to respond_to(:menu) }
   end
   
   describe 'read_menu' do
     it 'Shows user a list of dishes and prices' do 
-      expect(subject.read_menu).to eq({})
+      expect(subject.read_menu).to include(:"chicken balls" => 2.0, :"chow mein" => 1.99, :"duck pancakes" => 1.0)
     end
   end
 
