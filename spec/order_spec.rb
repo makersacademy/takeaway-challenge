c1944ebc8a03a205a727a416ef0b337c3ee64189
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-16 17:39:49 +0000

    Added check for adding multiple to basket

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -12,6 +12,11 @@ describe Order do
     it 'increases the total price by the price of the item' do
       expect { subject.add_to_basket(dish_name) }.to change { subject.total_price }.by dish_price
     end
+    context 'when passed a quantity as well as a dish' do
+      it 'increases the total price by the quantity times the price' do
+        expect { subject.add_to_basket(dish_name, 2) }.to change { subject.total_price }.by 2 * dish_price
+      end
+    end
   end
 
 end
