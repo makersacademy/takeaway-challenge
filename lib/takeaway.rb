1688f7b01c2cea455c9a9a0d7dd320e58a752d61
Anna Cavalla <anna.cavalla@gmail.com>
2021-01-17 18:36:47 +0000

    Rspec test for items added to basket; passing

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,18 +1,21 @@
 require_relative 'menu'
 
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :basket
   def initialize
     @menu = Menu.new
+    @basket = []
   end
 
   def show_menu
-    x = menu.dishes
-    puts x
-    return x
+    print menu.dishes
     end
 
-  def confirm(like, count = 1)
+  def add(like, count = 1)
+    count.times do self.basket << {like => menu.dishes[like]} end
     print "#{count}x #{like}(s) added to your basket."
   end
+  
+  private
+  attr_writer :basket
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,6 +1,8 @@
 require 'takeaway'
 
 describe Takeaway do
+  #let(:menu) { double :menu, price: '£1.00', contains?: true }
+  #subject(:order) { described_class.new(menu) }
   context 'starting up with printable menu' do
     it "should initialize with a hash menu" do
       expect(subject.menu.dishes).to be_a Hash
@@ -14,7 +16,12 @@ describe Takeaway do
   context 'ordering food' do
     it 'should receive an order and output confirmation' do
       message = "3x Plantastic burger(s) added to your basket."
-      expect{subject.confirm 'Plantastic burger', 3}.to output(message).to_stdout
+      expect{ subject.add 'Plantastic burger', 3 }.to output(message).to_stdout
+    end
+
+    it 'should add ordered items to a basket' do
+      subject.add 'Plantastic burger', 3
+      expect(subject.basket.select { |x| x["Plantastic burger"]  == 8 }.length).to eq 3
     end
   end
 end
