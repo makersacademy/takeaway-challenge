77267237776851560fc8199ab2c9449e0194a938
twigz826 <thomastwigden@gmail.com>
2021-01-17 14:59:01 -0300

    Decided against a Dish class, menu will be set on initialize, created a menu spec file and ruby script with first two tests

--- lib/dish.rb
+++
@@ -1,9 +0,0 @@
-class Dish
-
-  attr_reader :description, :price
-
-  def initialize(description,price)
-    @description = description
-    @price = price
-  end
-end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,3 +1,18 @@
+require 'menu'
+
 describe Menu do
-  
+
+  let (:menu) { described_class.new }
+  let (:dish) { "Battered cod" }
+  let (:price) { 3.99 }
+
+  describe "#view_menu" do
+    it "shows a list of dishes" do
+      expect(menu.view_menu.has_key?(dish)).to be_truthy
+    end
+
+    it "shows the corresponding price" do
+      expect(menu.view_menu[dish]).to eq price
+    end
+  end
 end
