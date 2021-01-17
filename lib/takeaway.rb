52fefe4637c94cdc66a3c903611adf793d0c7911
bullhornfixie <dawson.jon@hotmail.com>
2021-01-17 12:20:33 +0000

    Updates basket with new order item

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,25 @@
 class Takeaway 
-  attr_reader :basket, :menu
+  attr_reader :basket, :menu, :grand_total
     
  def initialize 
-   @basket = nil
    @menu = {"chow mein": 1.99, "chicken balls": 2.00, "duck pancakes": 1.00}
+   @basket = []
+   @grand_total = 0.00
  end
     
  def read_menu
    @menu
  end
-    
+ 
+ def order(dish, quantity=1 )
+   @basket << "#{dish} x #{quantity}: #{total(dish, quantity)}"
+   "You have added #{dish} x #{quantity}"
+ end
+ 
+ def total(dish, quantity)
+   subtotal = @menu[dish.to_sym] * quantity
+   @grand_total += subtotal
+   subtotal
+ end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,17 +1,38 @@
 require 'takeaway'
 
 describe Takeaway do 
-  
+
   context 'On initializing Takeaway' do
-    it { is_expected.to respond_to(:basket) }
     it { is_expected.to respond_to(:menu) }
+    it { is_expected.to respond_to(:basket) }
+    it { is_expected.to respond_to(:grand_total) }
   end
   
   describe 'read_menu' do
-    it 'Shows user a list of dishes and prices' do 
+    it 'Shows customer a list of dishes and prices' do 
       expect(subject.read_menu).to include(:"chicken balls" => 2.0, :"chow mein" => 1.99, :"duck pancakes" => 1.0)
     end
   end
 
+  describe 'order' do 
+    
+    it 'Updates the basket with an order item' do
+      subject.order("chow mein", 2)
+      expect(subject.basket.count).to eq(1)
+    end
+    
+    it 'Provides a confirmation message of basket entry' do 
+      expect(subject.order("chow mein", 2)).to eq("You have added chow mein x 2")
+    end
+  end 
+  
+  describe 'total' do
+    it 'Provides the total cost of an order' do
+      dinnerFor3 = subject
+      dinnerFor3.order("chow mein", 2)
+      dinnerFor3.order("chicken balls")
+        expect(dinnerFor3.grand_total).to eq(5.98)
+    end
+  end 
   
 end 
