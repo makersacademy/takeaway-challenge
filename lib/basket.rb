72f1d23196fa5ea40a3b5ff7f349cab22a39127e
Richard Turney <r.turney93@gmail.com>
2021-01-17 13:18:17 +0000

    added contents menthod to basket + spec

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,3 +1,12 @@
 class Basket
+  attr_reader :contents
+
+  def initialize
+    @contents = []
+  end
+
+  def add_dish(dish)
+
+  end
 
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,4 +1,14 @@
 require 'basket'
 describe Basket do
 
+  describe "#contents" do
+
+    it "will be empty upon initialization" do
+      expect(subject.contents).to be_empty
+    end
+  end
+
+  describe "#add_dish" do
+    
+  end
 end
