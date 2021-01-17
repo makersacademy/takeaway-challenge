9503e0e918f14b10ab3c315f8ef9b3823087593b
Katy Day <day.katy@gmail.com>
2021-01-17 16:49:21 +0000

    add test fo menu_spec, pass the test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,14 @@
 require_relative 'takeaway'
 
 class Menu
+attr_reader :menu
+
+  def initialize
+    @menu = {hamburger: 3.99,
+                      tacos: 4.99,
+                      spaghetti: 5.99,
+                      fries: 4.59 }
+  
+  end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,10 @@ require 'menu'
 
 describe Menu do
 
+  describe "#menu" do
+    subject { Menu.new.menu }  
+    it { is_expected.not_to be_empty}
+  end
+  
+
 end
