3f204b98c10fdbd845004ceb41b0a24a473d5e3c
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-01-17 10:49:12 +0000

    added a list of dishes with prices

---
+++ spec/menu_spec.rb
@@ -0,0 +1,22 @@
+require 'menu'
+describe Menu do
+
+  subject(:menu) { described_class.new(dishes) }
+
+  let(:dishes) do
+    {
+      chicken: 3.50,
+      aubergine: 2.75
+    }
+  end
+
+  it 'has a list of dishes with prices' do
+    expect(menu.dishes).to eq(dishes)
+  end
+
+  it 'prints a list of dishes with prices' do
+    printed_menu = "Chicken £3.50, Aubergine £2.75"
+    expect(menu.print).to eq(printed_menu)
+  end 
+
+end
