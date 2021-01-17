da30495ca4f225a5cdcc0d76032081037e53bd5a
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-17 18:03:49 +0000

    refactoring (tests DRYing)

--- spec/order_manager_spec.rb
+++ spec/order_manager_spec.rb
@@ -5,33 +5,23 @@ describe OrderManager do
 
   let(:dish) { double(:dish1, name: "Naan", price: 1.95) }
   let(:dish2) { double(:dish2, name: "Rice", price: 3.50) }
-  let(:order) { double(total_price: 15.00) }
+  # let(:order) { double(total_price: 15.00) }
   let(:menu) { double(dishes_list: [dish, dish2]) }
 
   before(:each) do
     allow(subject).to receive(:send_sms).and_return("You will receive a confirmation SMS soon.")
     allow(subject).to receive(:menu).and_return(menu)
-  end
-
-  it "stores orders" do
     allow(STDIN).to receive(:gets).and_return("2", "")
-    expect { subject.create_order }.to change { subject.orders.length }.by 1
   end
 
-  it "adds up prices of selected dishes" do
+  it "adds up prices of dishes" do
     expect(subject.prices_sum(menu.dishes_list)).to eq(dish.price + dish2.price)
   end
 
   describe "#create_order" do
-
-    before(:each) do
+    it "stores orders" do
       allow(STDIN).to receive(:gets).and_return("2", "")
-      allow(TwilioManager.new).to receive(:send_sms).and_return("You will receive a confirmation SMS soon.")
-    end
-
-    it "allows user to create an actual Order" do
-      subject.create_order
-      expect(subject.orders[-1].class).to eq Order
+      expect { subject.create_order }.to change { subject.orders.length }.by 1
     end
 
     it "lists dishes" do
@@ -50,21 +40,22 @@ describe OrderManager do
       expect(subject.dish_choice_from_input(subject.user_choice)).to eq dish2
     end
 
-    it "makes an Order with user choice of dishes" do
-      subject.create_order
-      expect(subject.orders[-1].dishes).to include dish2
-    end
-
     it "prints out chosen dishes and subtotal during order creation" do
       expect { subject.create_order }.to output(/#{Regexp.quote(dish2.name)}/).to_stdout
     end
   end
 
-  describe "order confirmation" do
-
+  describe "makes and confirms orders" do
     before(:each) do
-      allow(STDIN).to receive(:gets).and_return("2", "")
       subject.create_order
+    end
+
+    it "allows user to create an actual Order" do
+      expect(subject.orders[-1].class).to eq Order
+    end
+
+    it "makes an Order with user choice of dishes" do
+      expect(subject.orders[-1].dishes).to include dish2
     end
 
     it "prints out the ordered dishes" do
@@ -78,7 +69,6 @@ describe OrderManager do
     it "calculates estimated arrival time" do
       expect(subject.estimated_arrival_time(subject.orders[-1])).to eq(subject.orders[-1].time + described_class::DELIVERY_TIME)
     end
-
   end
 
 end
