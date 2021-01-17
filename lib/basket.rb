aef940b65975c3fbc3451a4f985b79931f1fd560
Richard Turney <r.turney93@gmail.com>
2021-01-17 15:16:37 +0000

    added total method to basket class and specs

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,3 +1,5 @@
+require_relative 'menu'
+
 class Basket
   attr_reader :contents
 
@@ -7,6 +9,18 @@ class Basket
 
   def add_dish(dish, quantity = 1)
     quantity.times { contents << dish }
+  end
+
+  def total(menu)
+    sum = 0
+    calculate_total(menu, sum)
+  end
+
+  private #-----------------------------
+
+  def calculate_total(menu, sum)
+    contents.each { |selection| sum += menu.dishes[selection.to_sym] }
+    sum
   end
 
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -23,9 +23,21 @@ describe Basket do
   end
 
   describe "#total" do
+    let(:menu) { double(:menu, :dishes => { Korma: 5.00, Naan: 3.50, Rice: 2.00 }) }
 
     it 'will return 0 if nothing is in the basket' do
-      expect(subject.total).to eq 0
+      expect(subject.total(menu)).to eq 0
+    end
+
+    it 'will return the price of a dish in the basket' do
+      subject.add_dish('Korma')
+      expect(subject.total(menu)).to eq 5
+    end
+
+    it 'will return the total sum of all dishes in the basket' do
+      subject.add_dish('Korma')
+      subject.add_dish('Naan', 2)
+      expect(subject.total(menu)).to eq 12
     end
   end
 end
