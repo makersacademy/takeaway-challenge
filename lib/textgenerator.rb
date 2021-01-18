d58c929b1122d44daecf0e40efddfc28f3d7f388
twigz826 <thomastwigden@gmail.com>
2021-01-18 19:30:17 -0300

    Refactored textgenerator script

--- lib/textgenerator.rb
+++ lib/textgenerator.rb
@@ -11,15 +11,18 @@ class Text
   PHONE_NUMBER_TO = ENV["PHONE_NUMBER_TO"].freeze
   DEFAULT_TWILIO_CLIENT = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN.freeze
 
+  private_constant :ACCOUNT_SID, :AUTH_TOKEN, :PHONE_NUMBER_FROM, :PHONE_NUMBER_TO, :DEFAULT_TWILIO_CLIENT
+
   def initialize(twilio_client = DEFAULT_TWILIO_CLIENT)
     @twilio_client = twilio_client
   end
 
   def send_message
     @twilio_client.messages.create(
-      body: "Thanks for your order. It will be with you in roughly an hour",
+      body: "Thanks for your order from Tasty Tuck. It will be with you in roughly an hour.",
       to: PHONE_NUMBER_TO,
       from: PHONE_NUMBER_FROM)
+    return "Message sent."
   end
 
 end
---
+++ spec/textgenerator_spec.rb
@@ -0,0 +1,12 @@
+require 'textgenerator'
+
+describe Text do
+
+  let(:twilio_client) { double(:twilio_client, messages: 'message') }
+
+  describe '#send_message' do
+    it "sends a message when an order is placed" do
+
+    end
+  end
+end
