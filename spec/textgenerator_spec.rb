6ba1ea8685275a77188ff8f7d15b4793b45a4b7c
twigz826 <thomastwigden@gmail.com>
2021-01-18 20:19:20 -0300

    Failing test for textgenerator_spec

--- spec/textgenerator_spec.rb
+++ spec/textgenerator_spec.rb
@@ -2,11 +2,11 @@ require 'textgenerator'
 
 describe Text do
 
-  let(:twilio_client) { double(:twilio_client, messages: 'message') }
+  let(:twilio_client) { double(:textgenerator) }
 
   describe '#send_message' do
     it "sends a message when an order is placed" do
-
+      expect(twilio_client.send_message).to eq true
     end
   end
 end
