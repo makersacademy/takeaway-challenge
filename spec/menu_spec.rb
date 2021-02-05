23ac2b4b480e20795566fe54bdfa727d21f6b89f
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-05 11:19:19 +0000

    more planning, wrote first test

---
+++ spec/menu_spec.rb
@@ -0,0 +1,8 @@
+describe Menu do
+  describe '#view' do
+    it 'allows the customer to view a list of available dishes' do
+      pizzeria = Menu.new
+      expect(pizzeria.view).to eq [{'Peperoni' => 7}, {'Pollo Kiev' => 8}]
+    end
+  end
+end
