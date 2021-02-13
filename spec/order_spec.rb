6dd4f2aafd7f5a1e63e55938603ac31360041f0f
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-13 09:25:55 +0100

    add test to add to order list

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -19,9 +19,13 @@ describe Order do
     expect(subject).to respond_to(:view_menu)
   end
 
-
-
   it 'should show a menu' do
     expect(subject.view_menu).to eq({ "pizza" => 5, "spaghetti" => 6, "sushi" => 10, "nachos" => 8, "burger" => 7 })
   end
+
+it 'should add a dish to the order_list' do
+  subject.view_menu
+  expect(subject.add("dish")).to change(subject.order_list.length).by(1)
+end
+
 end
