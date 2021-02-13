e6cff3f339aa19ac71e155c6d41fcf0cce7cd526
Christine Chee <christinechee88@hotmail.com>
2021-02-13 17:29:17 +0000

    Add ordering method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,28 @@
 require_relative 'menu.rb'
 
 class Takeaway
-
   def initialize(menuclass = Menu)
     @menuclass = menuclass
+    @menu = @menuclass.new
+    @basket = Basket.new
   end
 
   def show_menu
-    menu = @menuclass.new
-    menu.display
+    @menu.display
   end
 
+  def order(itemname, quantity=1)
+    name = itemname.downcase
+    if @menu.items.include?(name)
+       price = @menu.items[name]
+       @basket.store(name, quantity, price)
+      "#{quantity} x #{name} has been added to your basket"
+    else
+      "Sorry! We don't have what you've just ordered. Check the menu for what's on offer!"
+    end
+  end
+
+  def basket_summary
+    @basket.show_basket
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,9 +5,29 @@ describe Takeaway do
   let(:menuclass) { double(:menuclass, new: menu) }
   subject { Takeaway.new(menuclass) }
 
-  it 'displays the menu' do
-    expect(menu).to receive(:display)
-    subject.show_menu
+  describe '#show_menu' do
+    it 'displays the menu' do
+      expect(menu).to receive(:display)
+      subject.show_menu
+    end
+  end
+
+  describe '#order' do
+    it 'orders items from the menu and tells user it is in basket' do
+      katsucurry = double(:katsucurry, include?: true)
+      allow(katsucurry).to receive(:downcase).and_return(katsucurry)
+      allow(katsucurry).to receive(:[]).and_return(1.00)
+      allow(menu).to receive(:items).and_return(katsucurry)
+      expect(subject.order(katsucurry)).to eq("1 x #{katsucurry} has been added to your basket")
+    end
+
+    it "tells user item ordered is not in menu if it menu doesn't have it" do
+      something = double
+      allow(something).to receive(:downcase)
+      allow(menu).to receive(:items).and_return({})
+      message = "Sorry! We don't have what you've just ordered. Check the menu for what's on offer!"
+      expect(subject.order(something)).to eq(message)
+    end
   end
 
 end
