f294a4c26c30a4c50662d3216c6deeeebae6878c
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-13 09:21:18 +0100

    add tests and variables for empty order list and zero balance on initialization'

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,12 @@
 require_relative 'menu'
 
 class Order
+  attr_reader :order_list, :total
+
+  def initialize
+    @order_list = []
+    @total = 0
+  end
 
 def view_menu
   menu = Menu.new.list
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,9 +7,19 @@ describe Order do
     expect(subject).to be_a Order
   end
 
+  it 'should have an empty order list' do
+    expect(subject.order_list).to be_empty
+  end
+
+  it 'should have a total of zero' do
+    expect(subject.total).to eq(0)
+  end
+
   it 'should give an option to view a menu' do
     expect(subject).to respond_to(:view_menu)
   end
+
+
 
   it 'should show a menu' do
     expect(subject.view_menu).to eq({ "pizza" => 5, "spaghetti" => 6, "sushi" => 10, "nachos" => 8, "burger" => 7 })
