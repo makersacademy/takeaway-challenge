437805c6e568358b56476865bef28958a222daaf
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-02-13 16:26:23 +0000

    Implement user story 2

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,13 +1,19 @@
 require 'menu'
 
 class TakeAway
-  
+  attr_reader :basket
   def initialize(menu = Menu.new)
     @menu = menu
+    @basket = Hash.new(0)
   end 
 
   def read_menu()
     @menu.dishes
   end 
+
+  def add_to_basket(dish, quantity = 1)
+    fail "#{dish} is not available" if @menu.dishes[dish] == nil
+    @basket[dish] += quantity
+  end
 
 end 
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -1,13 +1,29 @@
 require 'take_away'
 
 describe TakeAway do 
-  
-  let(:dishes) { double :Dishes, pierogi: 11.99 }
-  let(:menu) { double :Menu, dishes: dishes }  
+  # let(:dishes) { double :Dishes, pierogi: 11.99 }
+  let(:menu) { double :Menu } 
   subject { described_class.new(menu) }
 
+  before do 
+    allow(menu).to receive(:dishes).and_return({ pierogi: 11.99 })
+  end 
+
   it 'displays a list of all available dishes' do 
-    expect(subject.read_menu).to eq dishes
+    expect(subject.read_menu).to eq({ pierogi: 11.99 })
   end
-  
+
+  context 'when adding to basket' do
+
+    it 'allows to add dish' do 
+      expect { subject.add_to_basket(:pierogi) }.to change { subject.basket.count }.by(1)
+    end
+    it 'allows to add various quantity of the same dish' do 
+      subject.add_to_basket(:pierogi, 2)
+      expect(subject.basket[:pierogi]).to eq 2
+    end
+    it 'raises an error when dish is not in the menu' do 
+      expect { subject.add_to_basket(:fish) }.to raise_error "fish is not available"
+    end
+  end 
 end 
