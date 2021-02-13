0c66edd08db8b37b1058d46aaefc9fac1ef45b30
Christine Chee <christinechee88@hotmail.com>
2021-02-13 17:28:47 +0000

    Add menu items method and spec

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,20 @@
 class Menu
+
   def display
-    "something"
+    items
+  end
+
+  def items
+    {"katsu curry" => 7.50,
+     "nanban chicken" => 7.50,
+     "tonkotsu ramen" => 10.00,
+     "nasi lemak" => 8.00,
+     "mapo tofu" => 7.50,
+     "massaman curry" => 8.00,
+     "karaage chicken" => 5.00,
+     "korean fried chicken" => 5.00,
+     "soju" => 6.00,
+     "iced tea" => 3.00,
+    }
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,8 +2,18 @@ require 'menu'
 
 describe Menu do
   describe '#display' do
-    it 'displays a list of food with prices' do
-
+    it 'displays items in the menu' do
+      expect(subject.display).to eq ({"katsu curry" => 7.50,
+       "nanban chicken" => 7.50,
+       "tonkotsu ramen" => 10.00,
+       "nasi lemak" => 8.00,
+       "mapo tofu" => 7.50,
+       "massaman curry" => 8.00,
+       "karaage chicken" => 5.00,
+       "korean fried chicken" => 5.00,
+       "soju" => 6.00,
+       "iced tea" => 3.00,
+      })
     end
   end
 end
