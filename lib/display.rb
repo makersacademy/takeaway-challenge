a7a9624b518568aadf68e0847a664d54b9f57732
Adam Jonas <adamjonas25@gmail.com>
2021-02-13 12:57:09 +0000

    complete display

--- lib/display.rb
+++ lib/display.rb
@@ -1,9 +1,18 @@
 class Display
-  def view(menu)
-    menu.map { |meal, price| "#{meal.to_s.capitalize}: #{price}" }
+  def view_menu(menu)
+    menu.map { |meal, price| "#{meal.to_s.capitalize}: #{format(price)}" }
+  end
+
+  def view_order(order)
+    output = order.map { |meal| view_menu(meal).pop }
+    output << "Total: #{format(total(order))}"
   end
 
   def total(order)
     order.map(&:values).map(&:pop).sum
+  end
+
+  def format(amount)
+    "£%.02f" %amount
   end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,27 +1,40 @@
 describe Display do
-  let(:order) { [{bufalina: 13}, {diavola: 14}] }
+  let(:order) { [{ bufalina: 13 }, { diavola: 14 }] }
   let(:menu) { Menu::PIZZA }
-  let(:menu_output) do [
-    "Marinara: 8",
-    "Margherita: 10",
-    "Bufalina: 13",
-    "Diavola: 14",
-    "Napoli: 14",
-    "Calzone: 14",
-    "Fiorentina: 14",
-    "Capricciosa: 15"
-    ]
-  end
 
-  describe '#view' do
+  describe '#view_menu' do
     it 'shows the menu in nice format' do
-      expect(subject.view(menu)).to eq menu_output
+      expect(subject.view_menu(menu)).to include("Marinara: £8.00")
     end
   end
 
   describe '#total' do
     it 'returns the order total' do
       expect(subject.total(order)).to be 27
+    end
+  end
+
+  describe '#format' do
+    it 'formats 1 to £1.00' do
+      expect(subject.format(1)).to eq '£1.00'
+    end
+
+    it 'formats 13 to £13.00' do
+      expect(subject.format(13)).to eq '£13.00'
+    end
+
+    it 'formats 0.5 to £0.50' do
+      expect(subject.format(0.5)).to eq '£0.50'
+    end
+  end
+
+  describe '#view_order' do
+    it 'displays the order in nice format' do
+      expect(subject.view_order(order)).to include("Diavola: £14.00")
+    end
+
+    it 'displays the total in nice format' do
+      expect(subject.view_order(order)).to include("Total: £27.00")
     end
   end
 end
