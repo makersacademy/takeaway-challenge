f4c73ab9067410f1ce5376677fe80873074ecaa2
jshorns <joey.s.hornsby@gmail.com>
2021-02-13 15:04:10 +0000

    added initialize method to order to start it with a menu, a balance of zero, an empty list of dishes and an incomplete status

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,27 @@
 class Order
+  attr_reader :menu, :balance, :dishes, :complete
+
+  def initialize(menu)
+    @menu = menu
+    @dishes = []
+    @balance = 0
+    @complete = false
+  end
+
+  def add_item(item)
+
+  end
+
+  def balance
+    @balance
+  end
+
+  def check_balance
+
+  end
+
+  def complete_order
+
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,25 @@
 require 'order'
 
 describe Order do
+  let(:dish1) { double(:dish, :name => "Baked Potato", :price => 3.25) }
+  let(:dish2) { double(:dish, :name => "Spaghetti Bolognese", :price => 4.70) }
+  let(:test_menu) { double(:menu, :dishes => [dish1, dish2]) }
+
+  let(:test_order) { described_class.new(test_menu) }
+
+  describe '#initialize' do
+    it 'has menu stored to menu attribute on initialization' do
+      expect(test_order.menu).to eq test_menu
+    end
+    it 'has balance of zero' do
+      expect(test_order.balance).to eq 0
+    end
+    it 'has empty list of dishes' do
+      expect(test_order.dishes).to eq []
+    end
+    it 'is set to incomplete' do
+      expect(test_order.complete).to be false
+    end
+  end
 
 end
