de634f13aeb32397d6dbe7ce2f0962dd9c6d5ddc
jai-jk <jay.jk@hotmail.com>
2021-02-14 16:53:54 +0000

    Refactored tests for tidiness

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,4 @@
 require_relative 'menu'
-require_relative 'basket'
 
 class Order
 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,15 +6,21 @@ describe Order do
     @menu = Menu.new
   end
 
-  it 'should return the menu' do
-    expect(subject.read_menu).to eq(@menu.menu)
+  describe '#read_menu' do
+    it 'should return the menu' do
+      expect(subject.read_menu).to eq(@menu.menu)
+    end
   end
 
-  it { should respond_to(:add).with(2).arguments }
+  describe '#add' do
+    it 'should take 2 arguments' do
+      expect(subject).to respond_to(:add).with(2).arguments
+    end
 
-  it 'should add the selected dish to the basket, along with the selected amount' do
-    first_item = subject.add("1. Bhindi", 5)
-    expect(subject.basket).to be(first_item)
+    it 'should add the selected dish and amount to the basket' do
+      first_item = subject.add("1. Bhindi", 5)
+      expect(subject.basket).to be(first_item)
+    end
   end
 
 end
