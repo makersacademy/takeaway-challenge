871916d3bceeb91bcb970166479aa22782ecc3eb
spaek14 <>
2021-02-15 08:15:51 +0000

    tests unfinished

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,12 +2,4 @@ require 'takeaway'
 
 describe Takeaway do
 
-  it "shows a list of the dishes with prices" do
-    expect(subject.list).to eq(1)
-  end
-
-  it "some of the menu can be selected" do
-    expect(subject.select).to eq(1)
-  end
-
 end
