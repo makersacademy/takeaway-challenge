71b503ef1e2e89f52bcf5e654c440e373f350e6e
jackmcc08 <jackmcc08@gmail.com>
2021-03-13 17:24:11 +0000

    created checkout and place order functionality, added Timecop to gems for testing and mocking order times, created several private methods in order to clean up main methods. will add remove order items method and extract menu and order classes. Dummy text method added to be replaced by Twilio API eventually.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -7,17 +7,13 @@ class Takeaway
   end
 
   def display_menu
-    display = ['MENU']
-    @menu.each_with_index { |item, index| display << menu_line_format(item, index) }
-    display.join("\n")
+    display_menu_format
   end
 
   def display_current_order
     return "You have not selected any items." if @current_order.empty?
 
-    display = ["YOUR ORDER\n"]
-    @current_order.each_with_index { |item, index| display << order_line_format(item, index) }
-    display.push("\nTOTAL: £#{total_cost(@current_order)}").join("\n")
+    display_order_format
   end
 
   def add_to_order(item_number, quantity)
@@ -26,7 +22,24 @@ class Takeaway
     item_order
   end
 
-  private
+  def checkout(customer)
+    display_current_order
+    customer_address = "#{customer.address[:address]}, #{customer.address[:post_code]}"
+    customer_number = customer.number
+    checkout_message(customer_address, customer_number)
+  end
+
+  def place_order(customer)
+    text_confirmation
+    order_message
+  end
+
+  def text_confirmation
+    delivery_time = (Time.now + 60 * 60).strftime "%H:%M"
+    "Thank you! Your order was placed and will be delivered by #{delivery_time}"
+  end
+
+private
 
   def find_menu_item_by_number(item_number)
     @menu[item_number - 1].flatten[0]
@@ -36,8 +49,20 @@ class Takeaway
     @menu.each { |item| return item[item_name] if item.has_key?(item_name) }
   end
 
+  def display_menu_format
+    display = ['MENU']
+    @menu.each_with_index { |item, index| display << menu_line_format(item, index) }
+    display.join("\n")
+  end
+
   def menu_line_format(item, index)
     "#{index + 1}. #{item.flatten[0]}  Price: £#{gbp_format(item.flatten[1])}"
+  end
+
+  def display_order_format
+    display = ["YOUR ORDER\n"]
+    @current_order.each_with_index { |item, index| display << order_line_format(item, index) }
+    display.push("\nTOTAL: £#{total_cost(@current_order)}").join("\n")
   end
 
   def order_line_format(item, index)
@@ -63,6 +88,15 @@ class Takeaway
       total += quantity * price
     end
     gbp_format(total)
+  end
+
+  def checkout_message(address, number)
+    display = "Your order is £#{total_cost(@current_order)}.\nYour address is: #{address}\n"
+    display << "Your number is: #{number}\n\nPlease press PLACE ORDER to confirm."
+  end
+
+  def order_message
+    "Payment Confirmed.\nOrder placed.\nConfirmation test issued."
   end
 end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,10 +1,15 @@
 require 'takeaway'
+require 'Timecop'
 
 describe Takeaway do
   let(:test_takeaway) { Takeaway.new }
-  let(:test_menu) { "MENU\n1. Spicy Edamame  Price: £4.20\n2. Salt and Chilli Vat Chicken Strips  Price: £5.20\n3. Tempura Seaweed Fish  Price: £8.00\n4. Space Whale Sashimi  Price: £16.00\n5. Venerable Dragon Roll  Price: £9.80" }
+  let(:test_menu) { TEST_MENU_DISPLAY_STRING }
   let(:empty_order) { [] }
-  let(:test_order_1) { "YOUR ORDER\n\n1. Salt and Chilli Vat Chicken Strips  Price: £5.20  Qty: 2  Sub: £10.40\n2. Space Whale Sashimi  Price: £16.00  Qty: 2  Sub: £32.00\n3. Venerable Dragon Roll  Price: £9.80  Qty: 1  Sub: £9.80\n\nTOTAL: £52.20" }
+  let(:test_order_1) { TEST_ORDER_1_DISPLAY_STRING }
+  let(:checkout_message_1) { TEST_CHECKOUT_MESSAGE_1 }
+  let(:place_order_message_1) { TEST_PLACE_ORDER_CONFIRMATION }
+  let(:dummy_text) { DUMMY_TEXT } # will be upgraded to Twilio API
+  let(:dummy_customer) { instance_double('Customer', address: FAKE_ADDRESS, number: FAKE_NUMBER, payment: FAKE_PAYMENT) }
 
   describe '#display_menu' do
     it "display the menu with each dish number, name and price" do
@@ -34,4 +39,42 @@ describe Takeaway do
       end
     end
   end
+
+  describe '#checkout' do
+    it 'displays your order, your address, number and asks you to place your order' do
+      test_takeaway.add_to_order(2, 2)
+      test_takeaway.add_to_order(4, 2)
+      test_takeaway.add_to_order(5, 1)
+      expect(test_takeaway.checkout(dummy_customer)).to eq checkout_message_1
+    end
+  end
+
+  describe '#place_order' do
+    it 'confirms your payment and sends a text message' do
+      expect(test_takeaway.place_order(dummy_customer)).to eq place_order_message_1
+    end
+  end
+
+  describe '#text_confirmation' do
+    it 'sends you a text message confirming your order and delivery time' do
+      Timecop.freeze(Time.new(2021,nil,nil,18,15))
+      expect(test_takeaway.text_confirmation).to eq dummy_text
+    end
+  end
 end
+
+TEST_MENU_DISPLAY_STRING = "MENU\n1. Spicy Edamame  Price: £4.20\n2. Salt and Chilli Vat Chicken Strips  Price: £5.20\n3. Tempura Seaweed Fish  Price: £8.00\n4. Space Whale Sashimi  Price: £16.00\n5. Venerable Dragon Roll  Price: £9.80"
+
+TEST_ORDER_1_DISPLAY_STRING = "YOUR ORDER\n\n1. Salt and Chilli Vat Chicken Strips  Price: £5.20  Qty: 2  Sub: £10.40\n2. Space Whale Sashimi  Price: £16.00  Qty: 2  Sub: £32.00\n3. Venerable Dragon Roll  Price: £9.80  Qty: 1  Sub: £9.80\n\nTOTAL: £52.20"
+
+TEST_CHECKOUT_MESSAGE_1 = "Your order is £52.20.\nYour address is: 72 Reunification Avenue, New Tokyo, UNC, 12NE XS9\nYour number is: 08008888888\n\nPlease press PLACE ORDER to confirm."
+
+TEST_PLACE_ORDER_CONFIRMATION = "Payment Confirmed.\nOrder placed.\nConfirmation test issued."
+
+DUMMY_TEXT = "Thank you! Your order was placed and will be delivered by 19:15"
+
+FAKE_ADDRESS = { address: '72 Reunification Avenue, New Tokyo, UNC', post_code: '12NE XS9' }
+
+FAKE_NUMBER = '08008888888'
+
+FAKE_PAYMENT = { type: 'credit_card', number: 0001_1100_1010_1000, expiry_date: '12/77', ccc: 888 }
