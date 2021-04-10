24fe5e886d4f30003b27f3691477eaa6e8e545f1
Adam <adam.j.mccor@gmail.com>
2021-04-10 22:47:25 +0100

    'updated order.rb'

--- lib/order.rb
+++ lib/order.rb
@@ -4,23 +4,14 @@ class Order
     @menu
   end
 
-  def menu_list
-    @menu = [
-      {1 => "fried boogers", price: 3.0 },
-      {2 => "dinosaur eggs", price: 10.0 },
-      {3 => "golden bones", price: 8.0 },
-      {4 =>  "calamari milkshake", price: 5.0 },
-      {5 =>  "durian gazpacio", price: 2.0 },
-      {6 =>  "flakey teeth", price: 4.0 },
-      {7 =>  "flamingo beaks", price: 3.0 },
-      {8 =>  "crab toes", price: 3.0 },
-      {9 =>  "velociraptor pupils", price: 12.0 },
-      {10 =>  "seasoned leaves", price: 4.0 }
-    ]
-  end
-
   def view_menu
-    print menu_list
-  end
-
+      @menu = [
+        { :dish => "dinosaur eggs", :price: => 3.0 },
+        { :dish => "golden bones", :price => 10.0 },
+        { :dish => "calamari milkshake", :price => 8.0 },
+        { :dish =>  "durian gazpacio", :price => 8.0 },
+        { :dish => "flakey teeth", :price => 8.0 }
+      ]
+      puts @menu
+    end
 end
