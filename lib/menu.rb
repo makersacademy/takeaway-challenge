b19358a700ffebe25f9eb079dbdc31ef01b11fb2
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-10 14:46:06 +0100

    it stores selected dishes

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,16 @@
 class Menu
-  attr_reader :menu
-
+  attr_reader :menu, :basket
+  def initialize
+    @basket = []
+  end
   def list
     @menu = [
       { name: 'German Burger', price: '£12,70' },
       { name: 'Chips', price: '£2.50' }
     ]
+  end
+
+  def add_dish(select, number)
+    @basket << { name: select, number: number }
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,4 +6,11 @@ describe Menu do
       expect(subject.list).not_to be_empty
     end
   end
+
+  context '#add_dish' do
+    it 'should allow do add a dish into the basket' do
+      subject.add_dish('German Burger', 6)
+      expect(subject.basket).not_to be_empty
+    end
+  end
 end
