c08c6253a84b82090f52717800b113254309ae8c
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-10 19:00:27 +0100

    responds to show_menu method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,7 @@
 class Menu
 
+  def show_menu
+  
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,10 @@ require 'menu'
 
 describe Menu do
 
+  it { is_expected.to be_an_instance_of Menu}
+
+  it "responds to the show menu method" do
+    expect(subject).to respond_to(:show_menu)
+  end
+
 end
