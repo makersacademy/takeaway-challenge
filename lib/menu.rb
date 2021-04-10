eef6ff4296a218a65f8647cf21ed87f3f85f50e1
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-10 22:46:49 +0100

    1st user story, can see a list of dishes with prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,20 @@
 class Menu
+  attr_reader :dish
+ 
+  def initialize
+    @dish = { miso: 8,
+    iekei: 8,
+    charsiu: 8,
+    shio: 8,
+    shoyu: 8,
+    tonkotsu: 8,
+    hakata: 8,
+    tatsuya: 8
+   }
+  end
 
   def show_menu
-  
+    @dish.map { |food, price| "#{food}: £#{price}" }.join("\n")
   end
 
 end
