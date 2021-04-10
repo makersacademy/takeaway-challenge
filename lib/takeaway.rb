2660fe51538389ebf4accf808f1ff2f70987a43f
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-10 11:52:16 +0100

    User story 1 complete

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,18 @@
-class Takeaway 
+class Takeaway
 
+  def initialize 
+    @menu_items = {
+      Lasagne: 12,
+      Margherita: 10,
+      Risotto: 8,
+      Arancini: 7,
+      Carbonara: 10
+    }
+  end 
+
+  def read_menu
+    @menu_items.each do |item, price|
+      puts "#{item}  --£#{price}"
+    end 
+  end 
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,4 +1,10 @@
 require 'takeaway'
 describe Takeaway do
+  it 'displays a list of menu items' do
+    expect(subject.read_menu).to have_key(:Lasagne)
+  end 
 
+  it 'displays their price' do
+    expect(subject.read_menu).to have_value(12)
+  end 
 end 
