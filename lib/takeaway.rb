3a2220b4dab150208d8a1403500c4a764406085c
Phil Smithies <philsmithies@gmail.com>
2021-04-11 15:44:31 +0200

    tests updated, new ordering ability added

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -5,32 +5,41 @@ class Takeaway
   attr_reader :items, :shoppingcart
 
   def initialize
-    @items = {margarita: 8.99, vegetarian: 9.99, pepperoni: 10.99, cheesecake: 3.99, water: 0.99}
+    @items = { "Margarita" => 8.99, "Vegetarian" => 9.99, "Pepperoni" => 10.99, "Cheesecake" => 3.99, "Water" => 0.99 }
     @shoppingcart = []
   end
 
-  def add(value)
-    @shoppingcart << @items[value.to_sym] #stuck trying to convert it over to being a symbol
+  def read_menu
+    p "What would you like to order? We currently have...."
+    p @items
+  end
+
+  def add(item)
+    @shoppingcart << @items[item.capitalize] # stuck trying to convert it over to being a symbol, this wasn't needed actually. hash was the wrong values.
+    "#{item.capitalize} added to your basket"
   end
 
   def total
     @shoppingcart.reduce(:+)
   end
 
-  def complete?
+  def time
     time = Time.now + 3600 # seconds in an hour
-    account_sid = 'ACf213fd0e0968cad4cf1d53ef5e2fe301'
-    auth_token = 'f3a00b928bb594b9847b5253a4d5092c'
+    time.strftime("%k:%M")
+  end
+
+  def sendtext
+   
     client = Twilio::REST::Client.new(account_sid, auth_token)
     
-    from = '+447723565836' # Your Twilio number
-    to = '+447709018333' # Your mobile phone number
+    from = '' # Your Twilio number
+    to = '' # Your mobile phone number
     
     client.messages.create(
     from: from,
     to: to,
-    body: "Thank you! Your order was placed and will be delivered before #{time.strftime("%k:%M")}"
+    body: "Thank you! The total was #{total} Your order was placed 
+    and will be delivered before #{time}"
     )
   end 
-
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,41 +2,61 @@ require 'takeaway'
 
 describe Takeaway do
 
-let (:items) { double :items }
-let (:subject) { Takeaway.new }
+  let (:items) { double :items }
+  let (:subject) { Takeaway.new }
   
-  context "View items" do 
-    it 'Displays the dishes along with prices' do
-      expect(subject.items).to eq({margarita: 8.99, vegetarian: 9.99, pepperoni: 10.99, cheesecake: 3.99, water: 0.99})
-    end
-    it 'Has an item that has a price for the user to select' do
-      expect(subject.items[:margarita]).to be > 0
+    context "View items" do 
+      it 'Displays the dishes along with prices' do
+        expect(subject.items).to eq({ "Margarita" => 8.99, "Vegetarian" => 9.99, "Pepperoni" => 10.99, "Cheesecake" => 3.99, "Water" => 0.99})
+      end
+      it 'Has an item that has a price for the user to select' do
+        expect(subject.items["Margarita"]).to be > 0
+      end
+      it 'Allows the user to read the menu' do 
+        expect(subject.read_menu).to eq(subject.items)
+      end
+
+    context "Select items" do
+      it 'Allows the user to use the add command' do
+        expect(subject).to respond_to(:add)
+      end
+      it 'Has an empty shopping cart by default' do
+        expect(subject.shoppingcart).to eq([])
+      end
+      it 'Checks that an item can be added to the shopping cart' do
+        subject.add("margarita")
+        expect(subject.shoppingcart).to eq([8.99])
+      end
+      it 'Checks that the user can select several items on the list' do
+        subject.add("Margarita")
+        subject.add("Water")
+        subject.add("Pepperoni")
+        subject.add("Cheesecake")
+        expect(subject.shoppingcart.count).to eq(4)
+      end
+      it 'Checks that the total amount is equal to the price of items added' do 
+        subject.add("Margarita")
+        subject.add("Water")
+        subject.add("Pepperoni")
+        expect(subject.total).to eq(20.97)
+      end
     end
 
-  context "Select items" do
-    it 'Allows the user to use the add command' do
-      expect(subject).to respond_to(:add)
+    context "Submitting an order" do 
+      it "Can display the time on a users order" do
+        expect(subject).to respond_to(:time)
+      end
+
+      it "Displays one hour after the current time when ordering" do 
+        time = Time.now + 3600 # number of seconds
+        expect(subject.time).to eq(time.strftime("%k:%M"))
+      end
+
+      it "Can send a text upon completion of order" do 
+        subject.add("Water")
+        expect(subject).to receive(:sendtext)
+        subject.sendtext
+      end
     end
-    it 'Has an empty shopping cart by default' do
-      expect(subject.shoppingcart).to eq([])
     end
-    it 'Checks that an item can be added to the shopping cart' do
-      subject.add("margarita")
-      expect(subject.shoppingcart).to eq([8.99])
-    end
-    it 'Checks that the user can select several items on the list' do
-      subject.add("margarita")
-      subject.add("water")
-      subject.add("pepperoni")
-      subject.add("cheesecake")
-      expect(subject.shoppingcart.count).to eq(4)
-    end
-    it 'Checks that the total amount is equal to the price of items added' do 
-      subject.add("margarita")
-      subject.add("water")
-      subject.add("pepperoni")
-      expect(subject.total).to eq(20.97)
-    end
-  end
-end
 end
