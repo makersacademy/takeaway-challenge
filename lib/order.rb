6a3d16970b193eeaaeac8d2a05ed152b0fb409cb
Abdur Rehman <wabdulrehman@yahoo.com>
2021-04-11 14:03:18 +0100

    add a feature to add an item in basket

--- lib/order.rb
+++ lib/order.rb
@@ -1,13 +1,19 @@
 require_relative 'menu.rb'
 
 class Order
+  attr_reader :basket
   def initialize
-    @menu = Menu.new 
-  end  
+    @basket = []
+  end
+
   def show_menu
-    @menu.menu_list.each do |item|
+    Menu.new.menu_list.each do |item|
       puts "#{item[:dish]}:  #{item[:price]} pounds"
     end 
     return 
+  end  
+
+  def add_to_basket(item)
+    @basket << item
   end  
 end  
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,9 +2,17 @@ require 'order'
 
 describe Order do
   let(:visual_menu)     { "chiken:  5 pounds\nbeef:  5 pounds\nturkey:  10 pounds\n"}
+  let(:item)            { Menu.new.menu_list[2]}
   it { is_expected.to be_instance_of(Order) }
   it { is_expected.to respond_to(:show_menu)}
   it 'shows the menu' do
     expect{subject.show_menu.to_s}.to output(visual_menu).to_stdout
+  end
+  it { is_expected.to respond_to(:basket) }
+  it { is_expected.to respond_to(:add_to_basket).with(1).arguments }
+
+  it 'adds an item to basket' do
+    subject.add_to_basket(item)
+    expect(subject.basket).to include(item)
   end
 end
