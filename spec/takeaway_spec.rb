4c9e7b7bd12dad99ca7f17e5e976b9002b7e48e9
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-11 19:57:22 +0100

    user interface allow to add several dishes into the basket

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,11 +1,18 @@
 require 'takeaway'
 
 describe TakeAway do
-  subject(:takeaway) { described_class.new(menu) }
+  subject(:takeaway) { described_class.new(menu, order) }
   let(:menu) { double(:menu, display: menu_list) }
+  let(:order) { double(:order) }
   let(:menu_list) { "Chicken Katsu: £20, Spring rolls: £10"}
+  let(:meal) { { :"Chicken Katsu" => 2, :"Spring rolls" => 2 }}
 
   it 'should display the menu' do
-    expect(subject.show_menu).to eq(menu_list)
+    expect(takeaway.show_menu).to eq(menu_list)
+  end
+
+  it 'allows to add several items in the basket' do
+    expect(order).to receive(:add).at_least(:once)
+    takeaway.take_order(meal)
   end
 end
