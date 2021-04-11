1626540617a6e4c92c2eb6c476ea8eb367a44834
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-11 13:45:20 +0100

    Refactored menu spec into contexts

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,19 +1,24 @@
 require 'menu'
 describe Menu do
 
-  it 'displays a list of menu items' do
-    expect(subject.read_menu).to have_key(:Lasagne)
+  context '#read_menu' do
+    it 'displays a list of menu items' do
+      expect(subject.read_menu).to have_key(:Lasagne)
+    end 
+    it 'displays their price' do
+      expect(subject.read_menu).to have_value(12)
+    end 
   end 
 
-  it 'displays their price' do
-    expect(subject.read_menu).to have_value(12)
+  context '#dish?' do
+    it 'checks if a dish is on the menu' do
+      expect(subject.dish?("Pizza")).to eq nil 
+    end 
   end 
 
-  it 'checks if a dish is on the menu' do
-    expect(subject.dish?("Pizza")).to eq nil 
-  end 
-
-  it 'returns the price of the item' do
-    expect(subject.price("Lasagne")).to eq 12
+  context '#price' do
+    it 'returns the price of the item' do
+      expect(subject.price("Lasagne")).to eq 12
+    end 
   end 
 end 
