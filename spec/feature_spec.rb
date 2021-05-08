86b7b42f21ecbd7e7dd299738507e6886c231898
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-08 15:20:03 +0100

    added a failing feature test for adding dishes to order

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -9,12 +9,15 @@ describe 'takeaway features' do
   ]
   }
   let(:menu) { Menu.new(dishes) }
+  let(:order) { Order.new }
 
   it 'can list all dishes with their prices' do
     expect(menu.list_dishes).to eq("1. Vegan Mixed Grill - £5.50\n2. Sweet Potato Fries - £6.50\n3. Vegan Burger - £7.50") 
   end
 
   it 'can add dishes to order' do
+    order.add_dishes_to_order(menu, 1, 2, 3)
+    expect(order.check_order).to eq("Vegan Mixed Grill - £5.50\nSweet Potato Fries - £6.50\nVegan Burger - £7.50\nTOTAL: £19.50")
   end
 
   it 'can check the price of the order' do
