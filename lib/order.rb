4c9ac88d619b1253ed64ee6eca4303950efcd51c
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-08 15:14:42 +0100

    can add dishes to order

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,35 @@
 class Order
+  def initialize
+    @dishes = []
+  end
+
   def check_order
+    return nil if all_dishes.empty?
+    cost_summary + "\nTOTAL: £#{format("%<num>0.2f", num: total_price)}"
+  end
+
+  def add_dishes_to_order(menu, *dish_numbers)
+    dish_numbers.each { |dish_number|
+      @dishes << menu.get_dish(dish_number)
+    }
+  end
+
+  private
+  
+  def all_dishes
+    @dishes
+  end
+
+  def total_price
+    all_dishes.reduce(0) { |sum, dish|
+      sum += dish.price
+    }
+  end
+
+  def cost_summary
+    all_dishes.map { |dish|
+      dish.to_s
+    }.join("\n")
   end
 end
 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,12 +2,25 @@ require 'order'
 
 describe Order do
   let(:order) { Order.new }
+  let(:dish) { double(:dish, to_s: 'Vegan Mixed Grill - £5.50', price: 5.5) }
+  let(:menu) { double(:menu, get_dish: dish) }
 
-  context 'when no items have been added to order' do
+  context 'when no dishes have been added to order' do
     describe '#check_order' do
       it 'returns nil' do
         expect(order.check_order).to be_nil
       end
     end
   end
+  
+  context 'when a dish has been added to order' do
+    before { order.add_dishes_to_order(menu, 1, 2, 3) }
+    
+    describe '#check_order' do
+      it 'returns a string showing the dishes and total cost' do
+        expect(order.check_order).to eq("Vegan Mixed Grill - £5.50\nVegan Mixed Grill - £5.50\nVegan Mixed Grill - £5.50\nTOTAL: £16.50")
+      end
+    end
+  end
+
 end
