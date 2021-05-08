e7fdeb909fc3387fc99fca2c6c58dbeb29dff7c1
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-05-08 15:48:09 +0100

    Added select food method

--- lib/order.rb
+++ lib/order.rb
@@ -6,6 +6,8 @@ require_relative 'naan'
 require_relative 'chai'
 
 class Order  
+  attr_reader :order
+
   def initialize(bhaji = Bhaji.new, rice = Rice.new, korma = Korma.new, jalfrezi = Jalfrezi.new,
      naan = Naan.new, chai = Chai.new)
     @bhaji = bhaji
@@ -14,6 +16,7 @@ class Order
     @jalfrezi = jalfrezi
     @naan = naan
     @chai = chai 
+    @order = []
   end 
 
   def menu
@@ -21,5 +24,20 @@ class Order
     " #{@korma.name} £#{@korma.price},\ #{@jalfrezi.name} £#{@jalfrezi.price},"\
     " #{@naan.name} £#{@naan.price}, #{@chai.name} £#{@chai.price}"
   end 
+
+  def select_food
+    while true do 
+      puts "What would you like to order? When complete enter 'done'" 
+      user_input = gets.chomp 
+      @order.push(@bhaji.name) if user_input.downcase == "bhaji"
+      @order.push(@rice.name) if user_input.downcase == "rice"
+      @order.push(@korma.name) if user_input.downcase == "korma"
+      @order.push(@jalfrezi.name) if user_input.downcase == "jalfrezi"
+      @order.push(@naan.name) if user_input.downcase == "naan"
+      @order.push(@chai.name) if user_input.downcase == "chai"
+      break if user_input.downcase == "done"
+    end
+    @order
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,7 +1,9 @@
 require 'order'
 
 describe Order do
-  describe 'menu' do
+  
+  describe '#menu' do
+  
     it 'prints the list of dishes from the menu' do 
       bhaji_double = double :bhaji, name: "Bhajis", price: 4
       rice_double = double :rice, name: "Rice", price: 2
@@ -9,11 +11,32 @@ describe Order do
       jalfrezi_double = double :jalfrezi, name: "Jalfrezi", price: 10
       naan_double = double :naan, name: "Naan", price: 4
       chai_double = double :chai, name: "Chai", price: 2
-
       order = Order.new(bhaji_double, rice_double, korma_double, jalfrezi_double, naan_double, chai_double)
       expect(order.menu).to eq("Bhajis £4,\ Rice £2,\ Korma £8,\ Jalfrezi £10,\ Naan £4,\ Chai £2")
     end
   end
 
+  describe '#select_food' do 
+    it "should return Naan" do
+      order = Order.new
+      order.select_food
+      expect(@order).to eq("Naan")
+    end
+=begin
+    it 'should tell you to try again if user input is empty' do
+      let (:user_input) { "dshbfdjhsb" }
+      order = Order.new
+      order.select_food
+      expect(@order).to eq([])
+    end
+=end
+  end 
+  # in order to be able to select dishes we need the menu to be printed, then user input which loops 
+  # asking for dishes, which ocnsequently pushes the chosen dish into an array. the result
+  # is an array of dishes 
+  # it 'selects multiple dishes to create a new order' do 
+    # order = order.new
+  #  order.select_food
+   # expect(order).to eq(dummy_order)
 
-end 
+end
