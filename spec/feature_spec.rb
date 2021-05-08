1e34aab20c3b97f9fc02203082a4b6d87a9ca38e
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-08 18:27:43 +0100

    changed feature test wording

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -15,12 +15,9 @@ describe 'takeaway features' do
     expect(menu.list_dishes).to eq("1. Vegan Mixed Grill - £5.50\n2. Sweet Potato Fries - £6.50\n3. Vegan Burger - £7.50") 
   end
 
-  it 'can add dishes to order' do
+  it 'can add dishes to order and check the price of the order' do
     order.add_dishes_to_order(menu, 1, 2, 3)
     expect(order.check_order).to eq("Vegan Mixed Grill - £5.50\nSweet Potato Fries - £6.50\nVegan Burger - £7.50\nTOTAL: £19.50")
-  end
-
-  it 'can check the price of the order' do
   end
 
   it 'sends a text to confirm order submission and delivery time' do
