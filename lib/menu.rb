5f62450d15d22f9ba1420a45a068b47a6e71d00c
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-08 22:33:15 +0100

    changed #all_dishes to attr_reader #dishes

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,18 +4,16 @@ class Menu
   end
 
   def list_dishes
-    all_dishes.each_with_index.map { |dish, index|
+    dishes.each_with_index.map { |dish, index|
       "#{index + 1}. #{dish}"
     }.join("\n")
   end
 
   def get_dish(dish_number)
-    all_dishes[dish_number - 1]
+    dishes[dish_number - 1]
   end
 
   private
 
-  def all_dishes
-    @dishes
-  end
+  attr_reader :dishes
 end
