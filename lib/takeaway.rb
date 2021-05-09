a5242fa72ecb842810234ce9348959b87f5f7e3b
Charlie Slater <charliemslater1701@gmail.com>
2021-05-09 11:25:35 +0100

    add menu attribute

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,8 @@
+require 'menu'
+
 class Takeaway 
-  
+  attr_reader :menu
+  def initialize(menu = Menu.new)
+    @menu = menu
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,4 +1,11 @@
 require 'takeaway'
 describe Takeaway do
+  let (:menu) { double(:menu, items: [{name: "item_one", price: 2.50}, {name: "item_two", price: 5.00}]) }
+
+  describe '#menu' do
+    subject {described_class.new(menu)}
+    it 'displays the menu' do
+      expect(subject.menu).to eq(menu) 
+    end
   end
 end
