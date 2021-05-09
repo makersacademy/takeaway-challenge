4c71243885d88f0471a48adb83a28f7ab1f69771
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-09 16:58:46 +0100

    added feature test for text confirmation using doubles

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,5 +1,7 @@
 require 'menu'
 require 'dish'
+require 'order'
+require 'twilio-ruby'
 
 describe 'takeaway features' do
   let(:dishes) { [
@@ -9,7 +11,11 @@ describe 'takeaway features' do
   ]
   }
   let(:menu) { Menu.new(dishes) }
-  let(:order) { Order.new }
+  let(:message_response_double) { double(:response, error_code: nil) }
+  let(:message_double) { double(:message, create: message_response_double) }
+  let(:text_message_client_double) { double(:client, messages: message_double) }
+  let(:text_message_client_class_double) { double(:client, new: text_message_client_double) }
+  let(:order) { Order.new(text_message_client_class: text_message_client_class_double) }
 
   it 'can list all dishes with their prices' do
     expect(menu.list_dishes).to eq("1. Vegan Mixed Grill - £5.50\n2. Sweet Potato Fries - £6.50\n3. Vegan Burger - £7.50") 
@@ -21,5 +27,7 @@ describe 'takeaway features' do
   end
 
   it 'sends a text to confirm order submission and delivery time' do
+    order.add_dishes_to_order(menu, 1, 2)
+    expect(order.submit_order('555-555-5555')).to be_truthy
   end
 end
