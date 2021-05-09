ad0f345e6f133429827e5828ce889ff132424b1a
“Jonny <jonny.kelly192@gmail.com>
2021-05-09 14:42:42 +0100

    shows a menu of dishes and prices

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,14 @@
 class TakeAway
 
+  MENU = {
+    "Chicken" => 9.99,
+    "Pizza" => 7.50,
+    "Pasta" => 8.00,
+    "Salad" => 7.00
+  }
+
+  def menu
+    MENU
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,4 +2,11 @@ require 'takeaway'
 
 describe TakeAway do
 
+    describe "#menu" do 
+      it "shows a menu of dishes and prices" do
+      menu = TakeAway::MENU
+      expect(subject.menu).to eq menu
+    end
+  end
 end
+
