977924123146a5d2b12fb7c438d68698f495695b
“Jonny <jonny.kelly192@gmail.com>
2021-05-09 14:47:06 +0100

    can select a number of dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,5 +1,5 @@
 class TakeAway
-
+attr_reader :order
   MENU = {
     "Chicken" => 9.99,
     "Pizza" => 7.50,
@@ -7,8 +7,17 @@ class TakeAway
     "Salad" => 7.00
   }
 
+  def initialize 
+    @order = []
+  end
+
   def menu
     MENU
+  end
+
+  def select(dish, quantity = 1)
+    item = MENU.select { |key| key == dish }
+    order << item.transform_values { |v| quantity * v.to_f } 
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,11 +2,19 @@ require 'takeaway'
 
 describe TakeAway do
 
-    describe "#menu" do 
-      it "shows a menu of dishes and prices" do
+  describe "#menu" do
+    it "shows a menu of dishes and prices" do
       menu = TakeAway::MENU
       expect(subject.menu).to eq menu
     end
   end
+
+   describe "#select(dish, capacity)" do 
+    it "can select a number of dishes" do 
+      expect(subject.select("Chicken", 2)).to eq [{ "Chicken" => 19.98 }]  
+    end
+  end
+
+
 end
 
