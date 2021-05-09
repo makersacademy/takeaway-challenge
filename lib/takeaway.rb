615547640646d066a228855ca848d6dd407a108e
Charlie Slater <charliemslater1701@gmail.com>
2021-05-09 12:11:35 +0100

    add to order when avaialbe without updating quantity

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,4 +6,13 @@ class Takeaway
     @menu = menu
     @order = order
   end
+
+  def add_to_order(name, quantity)
+    selected_item = @menu.items.find { |item| item[:name] == name} 
+    # if seleceted quanity < quantity throw error and say available amount
+    @order.current << { name: selected_item[:name], price: selected_item[:price], quantity: 1 }
+    # some check to see if already in order
+    p @order.current
+    # need to update quantity
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,8 +1,12 @@
 require 'takeaway'
 
 describe Takeaway do
-  let (:menu) { double(:menu, items: [{name: "item_one", price: 2.50}, {name: "item_two", price: 5.00}]) }
-  let (:order) {double(:order, current: [{name: "item_one", price: 2.50}])}
+  let (:menu_items) {[
+    {name: "item_one", price: 2.50, available_quanity: 1}, 
+    {name: "item_two", price: 5.00, available_quanity: 1}
+    ]}
+  let (:menu) { double(:menu, items: menu_items)}
+  let (:order) {double(:order, current: [{name: "item_one", price: 2.50, quantity: 1}])}
   subject {described_class.new(menu, order)}
 
   describe '#menu' do
@@ -14,6 +18,15 @@ describe Takeaway do
   describe '#order' do
     it 'displays the current order' do
       expect(subject.order).to eq(order)
+    end
+  end
+
+  describe '#add_to_order' do
+    context 'when items are available' do
+      it 'adds item to order' do
+        subject.add_to_order("item_one", 1)
+        expect(subject.order.current.length).to eq(2)
+      end
     end
   end
 end
