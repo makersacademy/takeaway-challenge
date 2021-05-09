4dc9d8759d92a00e6a245381f069df0947267af6
Arif Ebrahim <>
2021-05-09 21:35:02 +0100

    user story 2: WIP

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,16 @@
 class Order
+  attr_reader :customer_order
+
+  def initialize
+    @customer_order
+  end
+
   def place_order
     "Thank you for your order"
   end
+
+  def select_items
+    
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,13 @@
 require 'order'
 
 RSpec.describe Order do
+  let (:test_order) { [{"Salad"=>1.00}, {"Pasta"=>5.00}] } 
+
+  describe '#select_items' do
+    it 'allows customers to select the dishes they want' do
+      subject.select_items
+      expect(subject.customer_order).to eq(test_order)
+    end
+  end
 
 end
