241d59dac51f5dfb9c675af4b987e0b4d40c56ce
Charlie Slater <charliemslater1701@gmail.com>
2021-05-09 11:35:32 +0100

    add display order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,8 +1,9 @@
 require 'menu'
-
+require 'order'
 class Takeaway 
-  attr_reader :menu
-  def initialize(menu = Menu.new)
+  attr_reader :menu, :order
+  def initialize(menu = Menu.new, order = Order.new)
     @menu = menu
+    @order = order
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,11 +1,18 @@
 require 'takeaway'
 describe Takeaway do
   let (:menu) { double(:menu, items: [{name: "item_one", price: 2.50}, {name: "item_two", price: 5.00}]) }
-  subject {described_class.new(menu)}
+  let (:order) {double(:order, current: [{name: "item_one", price: 2.50}])}
+  subject {described_class.new(menu, order)}
 
   describe '#menu' do
     it 'displays all items on menu' do
       expect(subject.menu).to eq(menu) 
+    end
+  end
+
+  describe '#order' do
+    it 'displays the current order' do
+      expect(subject.order).to eq(order)
     end
   end
 end
