507a8429909de7cb6a0efa76be47535007a8382d
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-10 07:36:48 +0100

    added class diagram

--- lib/message.rb
+++ lib/message.rb
@@ -10,14 +10,17 @@ class Message
 
   def initialize(client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']))
     @client = client
-    @phone = ENV['TWILIO_PHONE']
+    @sender_phone = ENV['TWILIO_PHONE']
   end
 
   def send
     confirmation_message = "Thank you for your order. Your order has been placed and will be delivered before #{DELIVERY_TIME}."
-    twilio_message_body = { 
+
+    twilio_message_body = {
       from: ENV['TWILIO_PHONE'], 
-      to: ENV['TWILIO_DESTINATION_PHONE'], body: confirmation_message }
+      to: ENV['TWILIO_DESTINATION_PHONE'],
+      body: confirmation_message }
+
     @client.messages.create(twilio_message_body)
   end 
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -5,24 +5,25 @@ Dotenv.load
 
 
 describe Message do
-  let(:client) { double :client }
-  subject(:message) { described_class.new(client: client) }
+  let(:client) { double :client, messages: messages }
+  let(:messages) { double :messages }
+  subject(:message) { described_class.new(client) }
+  account_sid = 'ACeff89egd532'
+  auth_token ='efd674'
 
   it 'sends an sms message with a delivery time' do
-    
+
     confirmation_message = "Thank you for your order. Your order has been placed and will be delivered before #{described_class::DELIVERY_TIME}."
 
-    twilio_message_body = { 
-      from: ENV['TWILIO_PHONE'], 
-      to: ENV['TWILIO_DESTINATION_PHONE'], 
-      body: confirmation_message  }
+    twilio_message_body = {
+    from: ENV['TWILIO_PHONE'], 
+    to: ENV['TWILIO_DESTINATION_PHONE'],
+    body: confirmation_message }
 
-      allow(client).to receive(:messages).and_return("client_messages")
-      allow("client_messages").to receive(:create).with(twilio_message_body)
-    # allow(client).to receive_message_chain(:messages, :create)
+    expect(client).to receive_message_chain(:messages, :create).with(twilio_message_body )
 
-    expect(Twilio::REST::Client).to receive(:new).with(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']).and_return(client)  
-      
+    allow(Twilio::REST::Client).to receive(:new).with(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']).and_return(client)
+
     message.send 
   end
 end
