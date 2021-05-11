3967c3fca1abdfb52b7805cd0f389acf2dbef376
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-11 12:08:11 +0100

    twilio test passing!

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,29 +1,24 @@
 require 'message'
-require 'dotenv'
 require 'twilio-ruby'
+require 'dotenv'
 Dotenv.load
 
 
 describe Message do
-  let(:client) { double :client, messages: messages }
-  let(:messages) { double :messages }
+  let(:messages_double) { double(:messages_double, :create => 'message_sent') }
+  let(:client) { double(:client, :messages => messages_double)}
   subject(:message) { described_class.new(client) }
-  account_sid = 'ACeff89egd532'
-  auth_token ='efd674'
 
   it 'sends an sms message with a delivery time' do
-
+  
     confirmation_message = "Thank you for your order. Your order has been placed and will be delivered before #{described_class::DELIVERY_TIME}."
 
-    twilio_message_body = {
-    from: ENV['TWILIO_PHONE'], 
-    to: ENV['TWILIO_DESTINATION_PHONE'],
-    body: confirmation_message }
+    twilio_message_body = { 
+      from: ENV['TWILIO_PHONE'], 
+      to: ENV['TWILIO_DESTINATION_PHONE'], 
+      body: confirmation_message  }
 
-    expect(client).to receive_message_chain(:messages, :create).with(twilio_message_body )
-
-    allow(Twilio::REST::Client).to receive(:new).with(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']).and_return(client)
-
-    message.send 
+  message.send 
+  expect(messages_double).to have_received(:create).with(twilio_message_body)
   end
 end
