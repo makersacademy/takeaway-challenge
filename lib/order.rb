b21be04133a8ac13056665cfb22aa2f41a61048c
Fabio <alessf13@gmail.com>
2021-06-05 19:40:08 +0100

    @meals added to the initializemethod and #add set to take an argument.

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,13 @@
 require_relative 'order'
 
 class Order 
+  attr_reader :meals
+  
+  def initialize
+    @meals = []
+  end
+
+  def add(dish)
+    @meals << dish
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,13 @@
 require 'order'
 
 RSpec.describe Order do
+  let(:menu) { double(:menu, select: ['Pizza ai quattro formaggi', 9])} 
   
+  it 'has 0 items in @meals' do 
+    expect(subject.meals.length).to be_zero
+  end
+
+  it 'can add an item to @meals' do 
+    expect{ subject.add(menu.select) }.to change {subject.meals.length}.by(1)
+  end
 end
