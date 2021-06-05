b9dd55239314d9b76983bb4c7e7e8d7612114190
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-05 16:51:27 +0100

    Completed user story 1, added food variable

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,22 @@
-class TakeAway
+class Takeaway
+  attr_reader :food
+  
+  def initialize
+    @food = food
+  end
 
   def food_prices
 
-    food = {
-      :chips => 5.50,
-      :wings => 7.20,
-      :burger => 8.50
+    @food = {
+      "chips" => 5.50,
+      "wings" => 7.20,
+      "burger" => 8.50
     }
 
+    @food.each do |key, value|
+      puts "#{key}: £#{value}"
+    end
+  
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,9 @@
 require 'takeaway'
 
-describe TakeAway do
+describe Takeaway do
 
   it 'prints list of food and prices' do
-    expect(subject.food_prices).to include(:chips => 5.50, :wings => 7.20, :burger => 8.50)
+    expect(subject.food_prices).to include("chips" => 5.50, "wings" => 7.20, "burger" => 8.50)
   end
 
 end
