4baf2fb19d83a3dba87baaea93da72098388e62a
Fabio <alessf13@gmail.com>
2021-06-05 19:04:50 +0100

    view method added to show the description of the dish

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,21 @@
 class Menu
+  attr_reader :dishes, :name
+
+  def initialize(name)
+    @dishes = {
+      spaghetti: ['Spaghetti deliciously cooked with tomato sauce and parmeggian cheese', 10],
+      pizza: ['Pizza ai quattro formaggi', 9]
+      }
+    @name = name
+  end
+
+  def welcome
+    "Welcome to #{@name}! Would you like to view our menu?"
+  end
+
+  def view_menu
+   "Tonight's menu is:
+      1. #{@dishes[:spaghetti][0]}
+      2. #{@dishes[:pizza][0]}"
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,21 @@
 require 'menu'
 
 RSpec.describe Menu do
-  
+let(:subject) { Menu.new('Rodriguez')}
+
+  it 'has zero dishes' do 
+    expect(subject.dishes.length).not_to be_zero
+  end
+
+  it 'has a name' do
+    expect(subject.name).to eq('Rodriguez')
+  end
+
+  it 'welcomes the user to the restaurant app' do 
+    expect(subject.welcome).to eq("Welcome to Rodriguez! Would you like to view our menu?")
+  end
+
+  it 'can view the menu' do 
+    expect(subject.view_menu).to include('Spaghetti', 'Pizza')
+  end
 end
