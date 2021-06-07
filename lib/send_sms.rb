3f9997282e4e76e40c3c8d83c402a3411c37d91d
Fabio <alessf13@gmail.com>
2021-06-07 09:27:42 +0100

    Changes made to send_sms.rb

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,5 +1,4 @@
-require("bundler")
-Bundler.require()
+require 'twilio-ruby'
 
 account_sid = ENV["TWILIO_ACCOUNT_SID"]
 auth_token = ENV["TWILIO_AUTH_TOKEN"]
@@ -7,7 +6,9 @@ auth_token = ENV["TWILIO_AUTH_TOKEN"]
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
 @client.messages.create(
-  to: ENV["MY_PHONE_NUMBER"],
+  from: ENV["MY_PHONE_NUMBER"],
+  to: ENV["OTHER_NUMBER"],
   body: "Thank you! Your order was placed and will be delivered before #{Time.now + 3600}"
 )
+
 
