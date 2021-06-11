d38e80d86e4be79a081817c593441d671fda4106
Emily <emily.fitzsimmonsx@gmail.com>
2021-06-11 10:52:34 +0100

    past test for showing list of dishes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,4 +5,8 @@ describe Menu do
     expect(subject.dishes).to be_a Hash
     expect(subject.dishes).not_to be_empty
   end
+
+  it 'check if menu is displaying price list' do
+    expect(subject.dishes).to include({'Vegan curry' => 13, 'Veggie burger' => 9})
+  end
 end
