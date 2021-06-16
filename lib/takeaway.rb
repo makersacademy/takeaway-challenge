9d87c76a4ddf538aaedca53b8e7fc6be331d8e5a
Yusuf <yakhaliq01@gmail.com>
2021-06-16 12:46:20 +0100

    1st user story

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,21 +3,26 @@ class Takeaway
 
   def initialize
     @menu_items = {
-      'Sunset Burger' => 8.25, 
-      'Grilled Chicken Wrap' => 6.75, 
-      'Double Chicken Wrap' => 9.95,
-      'Grilled Chicken Burger' => 6.75,
-      'Double Chicken Burger' => 9.95,
-      'Grilled Chicken Pitta' => 6.75,
-      'Double Chicken Pitta' => 9.95,
-      'Imitator Wrap' => 6.75,
-      'Imitator Burger' => 6.75,
-      'Imitator Pitta' => 6.75
+      '1. Sunset Burger' => 8.25, 
+      '2. Grilled Chicken Wrap' => 6.75, 
+      '3. Double Chicken Wrap' => 9.95,
+      '4. Grilled Chicken Burger' => 6.75,
+      '5. Double Chicken Burger' => 9.95,
+      '6. Grilled Chicken Pitta' => 6.75,
+      '7. Double Chicken Pitta' => 9.95,
+      '8. Imitator Wrap' => 6.75,
+      '9. Imitator Burger' => 6.75,
+      '10. Imitator Pitta' => 6.75
     }
   end
 
   def menu
-    @menu_items
+    puts "Here is our menu: "
+    @menu_items.each do |k, v|
+      puts "#{k}: £#{v}"
+    end
   end
 
 end
+nandos = Takeaway.new
+nandos.menu
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,6 +1,15 @@
 require 'takeaway'
 describe Takeaway do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
   it 'can display list of menu items and prices' do
     expect(subject.menu).to eq(subject.menu_items)
+  end
+  # As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+  it 'can take items from menu and add to order' do
+    expect(subject.add_items 1).to change(subject.order).by 1
   end
 end
