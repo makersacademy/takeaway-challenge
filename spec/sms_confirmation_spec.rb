809cdc327b1a60986b5e944e2931d39343881283
karimabuseer <karimabuseer@gmail.com>
2021-07-10 21:01:55 +0100

    Working time tests using timecop

--- spec/sms_confirmation_spec.rb
+++ spec/sms_confirmation_spec.rb
@@ -1,5 +1,6 @@
 require 'sms_confirmation'
 require 'dotenv/load'
+require 'timecop'
 
 describe Text do
   let(:messages) {double(create: 'sent')}
@@ -10,12 +11,21 @@ describe Text do
   end
 
   describe '#generate_message' do
-    let(:client) { double(messages: foo)}
+    let(:client) { double(messages: 'foo')}
     let(:subject) { described_class.new(client)}
-    let(:foo) {double(create: 'bar')}
+    let(:messages) { double(:messages)}
+    
     xit 'does a thing' do
       allow(subject).to receive(:generate_message) { foo }
-      expect(subject.send).to have_received(:create).with('bar')
+      expect(subject.messages).to have_received(:create).with('foo')
+    end
+  end
+
+  describe '#time' do
+      it 'returns an hour later' do
+        Timecop.freeze(2012, 10, 5, 12) do
+          expect(subject.time).to eq('13:00')
+        end
     end
   end
 end
