d5abd42a2929aa55f63fd2fe4e63877d78b8b802
jamescondon <jecondon66@icloud.com>
2021-07-10 20:50:59 +0100

    Can see a list of dishes and prices

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -17,4 +17,8 @@ describe Menu do
 	it "accesses dishes attribute of menu class expecting it to equal dishes hash of (dish/price, key/value) pairs" do
 		expect(menu.dishes).to eq(dishes)
 	end
+    it "calls print on menu expecting to equal printed_menu" do
+		printed_menu = "Dim_sum_platter £11.00, Shrimp_fu_yung £8.80, Mongolian_lamb £7.70, Egg_fried_rice £3.30, Prawn_crackers £2.00"
+		expect(menu.print).to eq(printed_menu)
+	end
 end
