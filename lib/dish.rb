73ead934dbc2b5003c6552e57142da4675a87126
thomas <thomas.lubon@icloud.com>
2021-07-10 11:53:01 +0100

    finished dish object

---
+++ lib/dish.rb
@@ -0,0 +1,18 @@
+class Dish
+
+attr_reader :name, :price    
+
+    def initialize(name, price)
+        @name = name
+        @price = price  
+    end
+
+    def change_price(new_price)
+        @price = new_price
+    end
+
+    def rename(new_name)
+        @name = new_name
+    end
+
+end
