5b586d22655ac15894946d010da5e374973c64e9
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-10 21:35:22 +0100

    Implemented menu return

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,14 @@
 class Takeaway
+attr_reader :menu
+    def initialize        
+        @menu = [{item: 'chicken', price: 4}, 
+            {item: 'beef', price: 6}, 
+            {item: 'pork', price: 5}, 
+            {item: 'vegetable', price: 3}]
+    end
+
+    def view_menu
+        return @menu
+    end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,6 +1,11 @@
 require 'takeaway'
 
 describe Takeaway do
-
-
+    it 'given there is a menu when asking to view then return menu' do
+        menu = subject.view_menu
+        expect(menu).to eq([{item: 'chicken', price: 4}, 
+            {item: 'beef', price: 6}, 
+            {item: 'pork', price: 5}, 
+            {item: 'vegetable', price: 3}])
+    end
 end
