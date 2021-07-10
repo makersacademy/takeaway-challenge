f57836d5b521c21d9bb6b0c52e4fbac0f60db143
jamescondon <jecondon66@icloud.com>
2021-07-10 22:21:06 +0100

    Able to select a number of dishes

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,15 +1,21 @@
 require 'takeaway'
 
-describe Takeaway do j
-    subject { described_class.new(menu: menu) }
+describe Takeaway do
+	subject { described_class.new(menu: menu, order: order) } 
 
 	let(:menu) { double(:menu, print: printed_menu) }
+	let(:order) { double(:order) }
 	let(:printed_menu) { "Egg fried rice: £3.30" }
 
-	let(:dishes) { {egg_fried_rice: 3.30} }
+	let(:dishes) { {dim_sum_platter: 2, shrimp_fu_yung: 1} }
 	
 	it "calls print_menu on itself expecting it to equal printed_menu" do
 		expect(subject.print_menu).to eq(printed_menu)
+	end
 
-    end
+	it "calls place_order on itself taking dishes as arg expecting it to equal sum value of dishes" do
+		expect(order).to receive(:add).twice
+		subject.place_order(dishes)
+	end
+
 end
