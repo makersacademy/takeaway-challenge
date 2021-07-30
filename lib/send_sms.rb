60038d1cc4a2b91f1c1d04331d4102a02753f885
tylercws <tylercws@outlook.com>
2021-07-30 13:58:28 +0100

    Added text confirmation

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -2,8 +2,8 @@ require 'twilio-ruby'
 t = Time.new
 class Messenger
   def initialize()
-    @account_sid = 'AC7eb4c60d8a372ab63e77be664aeba3ea'
-    @auth_token = '7a01139e91c8240549cfe28a0eeecfd6'
+    @account_sid = ''
+    @auth_token = ''
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
 
     @from = '+447445034137' # Your Twilio number
