30e8e8aafbe0c6a132fc9a77d2c0fcdc361f63fe
Beckie Doyle <beckiedoyle@hotmail.co.uk>
2021-07-30 10:48:37 +0100

    User story 1 - see menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,16 @@
 class Menu
+  
+  attr_reader :menu
+
+  def initialize
+		@menu = {"big mac" => 2.30, 
+	         "chicken mayo" => 1.10, 
+					"veggie wrap" => 2.10, 
+					"fries" => 0.70}
+	end
+
+  def see_menu
+		print @menu
+	end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,4 +1,12 @@
 require 'menu'
 
 describe Menu do
+
+  describe "#see_menu" do
+    it "shows me the menu options and their prices" do
+      menu = Menu.new
+      expect(menu.see_menu).to eq(@menu)
+    end
+  end
+
 end
