34b7e7d4baa84edb313fca7453eae9d8cc93c56f
tylercws <tylercws@outlook.com>
2021-07-30 11:07:45 +0100

    order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,21 @@
 class TakeAway
+  attr_accessor :basket
+  def initialize()
+    @basket = []
+  end
+
   def read_menu
-    return { "Salted Caramel Shake" => 5.99, "Red Velvet Shake" => 5.99, "Cookie Dough Sandwich" => 4.99,
-       "Choc-zilla Freak Shake" => 8.50, "Vanilla milkshake" => 3.99 }
+    { "Salted Caramel Shake" => 5.99, "Red Velvet Shake" => 5.99, "Cookie Dough Sandwich" => 4.99,
+       "Choc-zilla Freak Shake" => 8.50, "Vanilla Milkshake" => 3.99 }
+  end
+
+  def order(item, amount=1)
+    #read_menu.has_key?(item)
+    chosen_item = read_menu.select { |k,v| k == item }
+    amount.times{  @basket.push(chosen_item) }
+    p @basket
   end
 end
 
 t = TakeAway.new
-t.read_menu
+t.order("Salted Caramel Shake")
