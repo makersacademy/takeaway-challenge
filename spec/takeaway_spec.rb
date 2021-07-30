afcd91abc3219fd19561430915200037feabe6bf
tylercws <tylercws@outlook.com>
2021-07-30 11:07:08 +0100

    Tests for ordering an item and responding to new methods

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,12 +2,32 @@ require 'takeaway'
 
 describe TakeAway do
   let(:takeaway) { TakeAway.new }
+  let(:menu) {{"Vanilla Milkshake" => 3.99}}
+  ITEM = "Vanilla Milkshake"
+
   it 'should create a takeaway' do
-    expect(takeaway).to receive(:read_menu)
+    expect(takeaway).to respond_to(:read_menu)
+  end
+
+  it 'should have an empty basket' do
+    expect(takeaway.basket).to be_empty
   end
 
   it 'should display menu with list of prices' do
     expect(takeaway).to respond_to(:read_menu)
   end
-  
+
+  it 'should allow someone to add to basket' do
+    allow(menu).to receive(:read_menu).and_return({ "Vanilla Milkshake" => 3.99 })
+  end
+
+  it 'should respond to order method' do
+    expect(takeaway).to respond_to(:order)
+  end
+
+  it 'should allow someone to order more than 1 item' do
+    expect(takeaway).to respond_to(:order).with(2).arguments
+    expect(takeaway.order(ITEM,2)).to eq([{"Vanilla Milkshake" => 3.99}, {"Vanilla Milkshake" => 3.99}])
+  end
+
 end
