222125cb8704ee572efa4e2b00bbeb45af3b00e8
danyaal <danyaalibnasif@gmail.com>
2021-08-01 19:37:25 +0100

    user story 2: can select multiple dishes

--- lib/order.rb
+++ lib/order.rb
@@ -3,7 +3,7 @@ class Order
 
   attr_accessor :ordered_dishes, :menu
 
-  def initialize(menu:)
+  def initialize(menu)
     @menu = menu
     @ordered_dishes = {}
   end
@@ -12,16 +12,12 @@ class Order
   
 
   def add(dish, quantity)
-    raise "#{dish} is not on the menu." unless menu_has_dish?(dish)
+    raise "#{dish} is not on the menu." unless menu.has_dish?(dish)
     ordered_dishes[dish] = quantity
    
   end
 
-  private
 
-  def menu_has_dish?(dish)
-    menu.include?(dish)
-  end
   
 
 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,12 +2,21 @@ require 'order'
 
 describe Order do
 
-  let(:order1) {Order.new(menu: menu1)}
-  let(:menu1) {double("menu")}
+  let(:order1) {Order.new(menu)}
+  let(:menu) {double("menu")}
+
+  before do
+    allow(menu).to receive(:has_dish?).with(:chicken).and_return(true)
+    allow(menu).to receive(:has_dish?).with(:lamb).and_return(true)
+    allow(menu).to receive(:has_dish?).with(:camel).and_return(true)
+    allow(menu).to receive(:has_dish?).with(:pork).and_return(false)
+  end
+
   
 
   it "selects several dishes" do
 
+    
     order1.add(:chicken, 2)
     order1.add(:lamb, 1)
     order1.add(:camel, 3)
@@ -16,7 +25,7 @@ describe Order do
   end
 
   it "doesn't allow dishes that aren't on the menu" do #needs knowledge of the menu, need to inject inside order a menu object
-    expect(order1.add(:pork, 4)). to raise_error "pork is not on the menu"
-  
+    expect(order1.add(:pork, 4)).to raise_error("pork is not on the menu.")
+    
   end
 end
