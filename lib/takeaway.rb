4d8456c73b108ebd9ca06f13c91b3ebc085eb9fc
Jessica <Jessica_doyle@outlook.com>
2021-08-01 20:49:39 +0100

    User story 2 part complete: can just place blank order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,20 @@
 class Takeaway
 
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order
   end
 
   def display_menu
     menu.print
   end
 
+  def place_order(food)
+  end
+
+
   private
 
   attr_reader :menu
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,9 +6,15 @@ describe Takeaway do
 
   let(:menu) { double(:menu, print: menu_list) }
   let(:menu_list) { "Tofu ramen: £2.99" }
+  let(:food) { { rice: 2.50, noodles: 2.50 } }
+  let(:order) { double(:order) }
 
   it 'shows menu list with prices' do
     expect(takeaway.display_menu).to eq("Tofu ramen: £2.99")
+  end
+
+  it 'can order a number of dishes' do 
+    expect(takeaway.place_order(food))
   end
 
 end
