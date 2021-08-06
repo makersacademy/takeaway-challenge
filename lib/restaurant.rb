ea3ab0f571425573dc5fabd9626ac6f9b5603d8c
eballantine <lizzy.kayballantine@gmail.com>
2021-08-06 19:48:31 +0100

    select dishes method available. Better dishes layout

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -3,12 +3,23 @@ class Restaurant
 
   def initialize
     @name = "New Restaurant"
-    @dishes = {
-      "Sushi": 7.50, 
-      "Burger": 6.10, 
-      "Fries": 3.00,
-      "Noodle Soup": 10.00,
-      "Bahn Mi": 9.25
-    }
+    @dishes = [
+      {dish: "Sushi", price: 7.50}, 
+      {dish: "Burger", price: 6.10}, 
+      {dish: "Fries", price: 3.00}, 
+      {dish: "Noodle Soup", price: 10.00}, 
+      {dish: "Bahn Mi", price: 9.25}
+    ]
+  end
+
+  def select_dishes(*args)
+    # error if no arguments given
+
+    selection = []
+    @dishes.each_with_index do |dish, i|
+      menu_num = i + 1
+      selection << dish if args.include? menu_num
+    end
+    selection
   end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,7 +1,13 @@
 require 'restaurant'
 
 describe Restaurant do
-  dishes = { "Sushi": 7.50, "Burger": 6.10, "Fries": 3.00, "Noodle Soup": 10.00, "Bahn Mi": 9.25 }
+  dishes = [
+    {dish: "Sushi", price: 7.50}, 
+    {dish: "Burger", price: 6.10}, 
+    {dish: "Fries", price: 3.00}, 
+    {dish: "Noodle Soup", price: 10.00}, 
+    {dish: "Bahn Mi", price: 9.25}
+  ]
 
   describe '#initialize' do
     it 'has a list of dishes with prices' do
@@ -11,7 +17,7 @@ describe Restaurant do
 
   describe '#select_dishes' do
     it 'lets the user select dishes' do
-      expect(subject.select_dishes(1, 3, 4)).to eq { "Sushi": 7.50, "Fries": 3.00, "Noodle Soup": 10.00 }
+      expect(subject.select_dishes(1, 3, 4)).to eq [{dish: "Sushi", price: 7.50}, {dish: "Fries", price: 3.00}, {dish: "Noodle Soup", price: 10.00}]
     end
   end
 end
