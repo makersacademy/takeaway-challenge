4125b7d7954d84f421a60d8536e0ef12386fcffd
eballantine <lizzy.kayballantine@gmail.com>
2021-08-06 19:57:10 +0100

    add test for place order method

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -15,9 +15,18 @@ describe Restaurant do
     end
   end
 
+  # allows the user to see the dishes and prices
+
   describe '#select_dishes' do
     it 'lets the user select dishes' do
       expect(subject.select_dishes(1, 3, 4)).to eq [{dish: "Sushi", price: 7.50}, {dish: "Fries", price: 3.00}, {dish: "Noodle Soup", price: 10.00}]
     end
   end
+
+  describe '#place_order' do
+    it 'allows the user to place an order' do
+      expect(subject.place_order).to be_instance_of Order
+    end
+  end
+
 end
