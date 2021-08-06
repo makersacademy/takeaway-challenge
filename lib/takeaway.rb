64c0d2692928b64b5cbf87986fa1c49e08bddac7
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-06 16:29:01 +0100

    Adding Takeaway#show_menu and initialize methods

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,13 @@
 class Takeaway
+  def initialize(menu)
+    @menu = menu
+  end
 
+  def show_menu
+    @menu.items.each_pair do |item_number, item|
+      puts "#{item_number}. #{item[:name]}: £#{item[:price]}\n"
+    end
+    
+    puts
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,19 @@
 require 'takeaway'
 
 describe Takeaway do
-  it do
-    
+
+  let(:menu) { double 'Menu', items: {
+    1 => {name: 'Flat White', price: 2 },
+    2 => {name: 'Rainbow Bagel', price: 1}
+  }}
+
+  let(:takeaway) { Takeaway.new(menu) }
+
+  let(:test_menu_string) {
+    "1. Flat White: £2\n2. Rainbow Bagel: £1\n"
+  }
+
+  it 'can display a list of dishes with prices' do
+    expect { takeaway.show_menu }.to output("#{test_menu_string}\n").to_stdout
   end
 end
