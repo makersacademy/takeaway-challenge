d92dbf2ac5951e818696ae632f1214d186d53493
eballantine <lizzy.kayballantine@gmail.com>
2021-08-06 23:07:13 +0100

    expected delivery time available for confirmation text

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -1,4 +1,13 @@
 # Responsible for sending order confirmation to customer
 
 class Confirmation
+
+  def intitialize
+    expected_delivery_time = nil
+  end
+
+  def expected_delivery_time 
+    time = Time.now + 1
+    time.strftime("%k:%M")    
+  end
 end
