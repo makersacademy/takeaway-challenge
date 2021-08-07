2934e06280ae9ef6bbd31418f4eebe63858799d8
edpackard <edpackard81@gmail.com>
2021-08-08 00:19:58 +0100

    Slight adjustment to streamline methods in CheckTotal class

--- lib/check_total.rb
+++ lib/check_total.rb
@@ -1,17 +1,13 @@
 class CheckTotal
 
   def check_total(order) 
-    "#{sum_string(order)}= #{total(extract(order))} TOTAL".to_sym
+    "#{sum_string(order)}= #{extract_total(order)} TOTAL".to_sym
   end
   
   private
 
-  def extract(source)
-    source.map { |hash| hash.values }.flatten
-  end
-
-  def total(numbers)
-    numbers.inject(:+)
+  def extract_total(source)
+    source.map { |hash| hash.values }.flatten.inject(:+)
   end
 
   def sum_string(order)
