ed8028fade1db08cbc8efa6104d3244e1dec9226
ian k <kickthechair@gmail.com>
2021-08-07 19:06:20 +0100

    refactored code

--- spec/food_order_spec.rb
+++ spec/food_order_spec.rb
@@ -6,11 +6,11 @@ describe FoodOrder do
   let(:menu_item_exists) { { "meat feast pizza" => 7.00 } }
   let(:number_of_pizzas) { 5 }
 
-  # describe 'ininitialization of object instantiation' do
-  #   it 'has ah empty food_order array' do
-  #     expect(subject.food_order).to eq([])
-  #   end
-  # end
+  describe 'ininitialization of object instantiation' do
+    it 'has ah empty food_order array' do
+      expect(subject.food_order).to eq([])
+    end
+  end
 
   describe '#add_item' do
     it 'raises error if menu item does not exist' do
