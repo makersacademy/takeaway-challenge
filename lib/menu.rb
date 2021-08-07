d108acae77413b5b679418305cf2b5466270b06e
danroche10 <danielroche10@gmail.com>
2021-08-07 10:22:07 +0100

    #initialize tests for menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,9 @@
 class Menu
 
+  attr_reader :items
+
+  def initialize(items)
+    @items = items
+  end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,10 +2,25 @@ require 'menu'
 
 describe Menu do
 
+  items = {
+    "Chicken burger" => 10,
+    "Vegan Soup" => 5,
+    "Chang Beer" => 3,
+  }
+
+  subject(:menu) { described_class.new(items) }
+
     describe "#initialize" do
-      
+      it "returns correct length of menu" do
+        expect(menu.items.length).to eq 3
+      end
 
+      it "returns correct first item" do
+        expect(menu.items.keys).to eq ["Chicken burger", "Vegan Soup", "Chang Beer"]
+      end
 
+      it "returns correct value for given key" do
+        expect(menu.items["Chicken burger"]).to eq 10
+      end
     end 
-
 end
