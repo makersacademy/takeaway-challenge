fabea4b11684213f2b935bd207b866af5386909f
edpackard <edpackard81@gmail.com>
2021-08-08 23:31:50 +0100

    Commented out feature_test_spec.rb

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -1,17 +1,17 @@
-require 'takeaway'
+# require 'takeaway'
 
-describe "Basic feature test" do
+# describe "Basic feature test" do
 
-  puts "Creates new takeaway object: #{takeaway = Takeaway.new}"
-  puts "Shows menu: #{takeaway.view_menu}"
-  puts "Shows current order: #{takeaway.check_order}"
-  puts "Adds dish: #{takeaway.add("Hot Dog")}"
-  puts "Shows current order: #{takeaway.check_order}"
-  puts "Adds another dish: #{takeaway.add("Hot Dog")}"
-  puts "Shows current order: #{takeaway.check_order}"
-  puts "Adds another dish: #{takeaway.add("Cheeseburger")}"
-  puts "Adds another dish: #{takeaway.add("Hamburger")}"
-  puts "Adds a new dish: #{takeaway.add("Cheeseburger")}"
-  puts "Shows current order: #{takeaway.check_order}"
+#   puts "Creates new takeaway object: #{takeaway = Takeaway.new}"
+#   puts "Shows menu: #{takeaway.view_menu}"
+#   puts "Shows current order: #{takeaway.check_order}"
+#   puts "Adds dish: #{takeaway.add("Hot Dog")}"
+#   puts "Shows current order: #{takeaway.check_order}"
+#   puts "Adds another dish: #{takeaway.add("Hot Dog")}"
+#   puts "Shows current order: #{takeaway.check_order}"
+#   puts "Adds another dish: #{takeaway.add("Cheeseburger")}"
+#   puts "Adds another dish: #{takeaway.add("Hamburger")}"
+#   puts "Adds a new dish: #{takeaway.add("Cheeseburger")}"
+#   puts "Shows current order: #{takeaway.check_order}"
 
-end
+# end
