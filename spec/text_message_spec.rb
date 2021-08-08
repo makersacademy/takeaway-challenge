658856adcd42647b5807b36d25140d876ea7b13a
edpackard <edpackard81@gmail.com>
2021-08-08 14:02:18 +0100

    Set up text_message class and mock test, updated Gemfile for twilio

---
+++ spec/text_message_spec.rb
@@ -0,0 +1,17 @@
+require 'text_message'
+
+describe TextMessage do
+
+  let(:mock_client) { double :mock_client }
+
+  subject { described_class.new(mock_client) }
+
+  before(:each) do
+    allow(mock_client).to receive_message_chain(:account, :sms, :messages, :create) { "test" }
+  end
+
+  it "calls sms_send method with a message" do
+    expect(subject.sms_send("test")).to eq("test")
+  end
+
+end
