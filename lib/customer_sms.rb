b1231ade1c1229b8b834bbb61cbaf73bd8c0d345
Corinne <corinne.boesch@gmail.com>
2021-08-09 11:31:02 +0100

    update order cornfirmation

--- lib/customer_sms.rb
+++ lib/customer_sms.rb
@@ -2,36 +2,55 @@ require 'rubygems'
 require 'dotenv/load'
 require 'twilio-ruby'
 
+# module OrderConfirmation
+
+#   time = Time.new
+#   order_time = time.strftime("%H:%M")
+#   delivery_time = (time + 25 * 60).strftime("%H:%M")
+
+#   # @from = ENV["SERVICE_SID"]
+#   @from = ENV["TWILIO_NUM"]
+#   @customer = ENV["MY_NUMBER"] 
+#   @message = "Order placed at: #{order_time}\nThank you for placing an order with SandCream! Your delivery will be with you by: #{delivery_time}"
+  
+#   account_sid = ENV["TWILIO_ACCOUNT_SID"]
+#   auth_token = ENV["TWILIO_AUTH_TOKEN"]
+#   @client = Twilio::REST::Client.new(account_sid, auth_token) 
+
+#   # def self.send_sms
+#   #   @client.messages.create(
+#   #     from: @from, 
+#   #     to: @customer, 
+#   #     body: @message
+#   #   )
+#   # end
+  
+#   message = @client.messages.create( 
+#     body: "Order placed at: #{order_time}\nThank you for placing an order with SandCream! Your delivery will be with you by: #{delivery_time}",
+#     from: ENV["TWILIO_NUM"],      
+#     to: ENV["MY_NUMBER"] 
+#   ) 
+  
+# end
+
 module OrderConfirmation
 
-  time = Time.new
-  order_time = time.strftime("%H:%M")
-  delivery_time = (time + 25 * 60).strftime("%H:%M")
+  # def self.send_sms
+    @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]) 
 
-  # @from = ENV["SERVICE_SID"]
-  # @from = ENV["TWILIO_NUM"]
-  # @customer = ENV["MY_NUMBER"] 
-  # @message = "Order placed at: #{order_time}\nThank you for placing an order with SandCream! Your delivery will be with you by: #{delivery_time}"
-  
-  account_sid = ENV["TWILIO_ACCOUNT_SID"]
-  auth_token = ENV["TWILIO_AUTH_TOKEN"]
-  @client = Twilio::REST::Client.new(account_sid, auth_token) 
+    @message = "Order placed at: #{@order_time}\nThank you for placing an order with SandCream! Your delivery will be with you by: #{@delivery_time}"
 
-  # def send_sms
-  #   @client.messages.create(
-  #     from: @from, 
-  #     to: @customer, 
-  #     body: @message
-  #   )
+    time = Time.new
+    @order_time = time.strftime("%H:%M")
+    @delivery_time = (time + 25 * 60).strftime("%H:%M")
+
+    @client.messages.create(
+      from: ENV["TWILIO_NUM"], 
+      to: ENV["TO_MY_NUMBER"], 
+      body: @message 
+    )
+
   # end
-  
-  message = @client.messages.create( 
-    body: "Order placed at: #{order_time}\nThank you for placing an order with SandCream! Your delivery will be with you by: #{delivery_time}",
-    from: ENV["TWILIO_NUM"],      
-    to: ENV["MY_NUMBER"] 
-  ) 
-  
 end
-
-
 # bundle exec ruby customer_sms.rb
+# /desktop/takeaway-challenge/lib
--- spec/customer_sms_spec.rb
+++ spec/customer_sms_spec.rb
@@ -4,6 +4,6 @@ Describe OrderConfirmation do
   
   it 'sends notification with estimated delivery time' do
     expect(subject).to receive(:send_sms)
-    subject.send_sms(20.93)
+    subject.send_sms
   end
 end
