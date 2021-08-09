6efe166b2a146b7a1d62d859a3891b8e6790a6c0
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-09 07:58:56 +0100

    sms.send sends a message with delivery time

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,20 +1,20 @@
-# require 'twilio-ruby'
+require 'twilio-ruby'
 
-# class SMS
+class SMS
 
-#   def initialize
-#     @client = Twilio::REST::Client.new($ACC_SID, $ACC_TOKEN)
-#     @time_in_1_h = Time.now
-#   end
+  def initialize
+    @client = Twilio::REST::Client.new(ENV['ACC_SID'], ENV['AUTH_TOKEN'])
+    @time_in_1_h = Time.now
+  end
 
-#   def send
-#     # Had to remove the create method @client.messages.create as I kept getting and error
-#     # 'create': [HTTP 404] 20404 : Unable to create record (Twilio::REST::RestError)
-#     @client.messages(
-#     from: ENV['TWILIO_NUM'],
-#     to: ENV['PHONE_NUM'],
-#     body: "Thank you! Your order was placed and will be delivered before #{@time_in_1_h}"
-#     )
-#   end
+  def send
+    # Had to remove the create method @client.messages.create as I kept getting and error
+    # 'create': [HTTP 404] 20404 : Unable to create record (Twilio::REST::RestError)
+    @client.messages.create(
+    from: ENV['TWILIO_NUM'],
+    to: ENV['PHONE_NUM'],
+    body: "Thank you! Your order was placed and will be delivered before #{@time_in_1_h}"
+    )
+  end
 
-# end
+end
