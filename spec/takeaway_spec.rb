a660895e02be36f1ff94e1a262e2b4f344772571
Suhani Zaman <suhanibadiz9@gmail.com>
2021-08-09 10:27:52 +0400

    reads menu, adds order, give basket summary and total

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,4 +7,12 @@ describe Takeaway do
     expect(subject.read_menu).to receive(:puts).with(:menu)
     #require_relative 'menu.rb'
   end
+  it 'menu should have dishes to provide customer' do
+    expect(subject.menu).not_to be_empty
+  end
+  it "allows to order" do
+      subject.order("pizza")
+      expect(subject.items).to eq({"pizza" => 1})
+  end
+  end
 end
