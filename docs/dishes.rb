e09f59b9395239bb7ab61a6773990468a04bc667
Admin <admin@MA137.local>
2021-09-04 21:17:07 +0100

    user story 1 : see dishes and prices

---
+++ docs/dishes.rb
@@ -0,0 +1,3 @@
+class Dishes
+   puts menu = {'burger' => '£10.00', 'chips' => '£2.00', 'smoothie' => '£5.00', 'onion_rings' => '£4.00', 'mayo' => '£1.00', 'ketchup' => '£1.00'}
+end
---
+++ spec/dishes_spec.rb
@@ -0,0 +1,4 @@
+require '/Users/admin/Library/Mobile Documents/com~apple~CloudDocs/takeaway-challenge/takeaway-challenge/docs/dishes.rb'
+describe Dishes do
+    # it 'shows a list of dishes and their prices'
+end
