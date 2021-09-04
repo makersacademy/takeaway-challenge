b5d4a2f90b978d487716f4610dacb5bb0a92e120
jake <jakebs540@gmail.com>
2021-09-04 23:04:48 +0100

    menu can now be displayed

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,11 +1,15 @@
 require './lib/menu'
 
 describe Menu do
+
   describe '#initialize' do
     it 'initialize with the menu' do
       expect(subject.menu).to include(:dish => 'Beef burger', :price => 12)
     end
+
+    it '#displays menu' do
+      expect(subject.display_menu).to eq(Menu::MENU)
+    end    
   end
-
-
+  
 end
