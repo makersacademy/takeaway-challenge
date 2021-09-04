dc66e050f40df47ef283a9a5cbd0f3c8494ac257
Admin <admin@MA137.local>
2021-09-04 22:29:17 +0100

    refactored user story 1 : see dishes and prices

--- docs/dishes.rb
+++ docs/dishes.rb
@@ -1,3 +1,4 @@
 class Dishes
-   puts menu = {'burger' => '£10.00', 'chips' => '£2.00', 'smoothie' => '£5.00', 'onion_rings' => '£4.00', 'mayo' => '£1.00', 'ketchup' => '£1.00'}
+    def show_menu
+        puts menu = {'burger' => '£10.00', 'chips' => '£2.00', 'smoothie' => '£5.00', 'onion_rings' => '£4.00', 'mayo' => '£1.00', 'ketchup' => '£1.00'}
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -1,4 +1,5 @@
 require '/Users/admin/Library/Mobile Documents/com~apple~CloudDocs/takeaway-challenge/takeaway-challenge/docs/dishes.rb'
 describe Dishes do
-    # it 'shows a list of dishes and their prices'
+    it 'shows a list of dishes and their prices'
+    expect(dishes.show_menu).to eq({'burger' => '£10.00', 'chips' => '£2.00', 'smoothie' => '£5.00', 'onion_rings' => '£4.00', 'mayo' => '£1.00', 'ketchup' => '£1.00'})
 end
