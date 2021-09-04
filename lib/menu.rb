4017f9a5a90548ff94cd452c8ab05ff16c1e9ef2
jake <jakebs540@gmail.com>
2021-09-04 22:48:46 +0100

    menu created

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,19 @@
 
 class Menu
+    
+  MENU = {:dish => 'Chicken burger', :price => 10},
+    {:dish => 'Beef burger', :price => 12},
+    {:dish => 'Fish dog', :price => 11},
+    {:dish => 'Hallumi fritter burger', :price => 8},
+    {:dish => 'Sloppy Joe dog', :price => 9},
+    {:dish => 'Truffle fries', :price => 4},
+    {:dish => 'Mac & Cheese', :price => 5},
+    {:dish => 'Corn ribs', :price => 4}
+  
+  attr_reader :menu
 
+  def initialize
+    @menu = MENU
+  end  
+    
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,5 +1,11 @@
 require './lib/menu'
 
 describe Menu do
+  describe '#initialize' do
+    it 'initialize with the menu' do
+      expect(subject.menu).to include(:dish => 'Beef burger', :price => 12)
+    end
+  end
+
 
 end
