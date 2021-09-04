84ed11f9a7074e2911aecf5e7abdb772fc233c10
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-04 12:37:48 +0100

    Dish class created via TDD, intialize with a name and a price, format as a hash

---
+++ lib/dish.rb
@@ -0,0 +1,13 @@
+class Dish
+  attr_reader :name, :price, :hash
+  def initialize(name="Hash brown", price=99)
+    @name = name
+    @price = price
+    @hash = {}
+  end
+
+  def dish_hash
+    @hash[@name.to_sym] = "#{(@price/100.0)}"
+    @hash
+  end
+end
---
+++ spec/dish_spec.rb
@@ -0,0 +1,21 @@
+require 'dish'
+
+describe Dish do
+
+  context 'adding a dish' do
+    it 'has a #name' do 
+      expect(subject.name).to eq("Hash brown")
+    end
+
+    it 'has a #price' do 
+      expect(subject.price).to eq(99)
+    end
+  end
+
+  context 'menu format' do
+    it 'combines name and price in a hash' do 
+      expect(subject.dish_hash).to eq({:"Hash brown" => "0.99"})
+    end
+  end
+
+end
