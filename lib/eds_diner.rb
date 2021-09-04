4ac6d858558527da5ab3a627e32050fe91f52c60
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-04 12:54:47 +0100

    menny displayed when chosing items

--- lib/eds_diner.rb
+++ lib/eds_diner.rb
@@ -1,46 +1,23 @@
 class EdsDiner
 
   def initialize 
-    @dishes = {
-      1 => {
-        :name => 'Tomato Soup',
-        :price => 5
-      },
-      2 => {
-        :name => 'Avocado On Toast',
-        :price => 7
-      },
-      3 => {
-        :name => 'Spaghetti Bolognese',
-        :price => 12
-      },
-      4 => {
-        :name => 'Mushroom Surprise',
-        :price => 9
-      },
-      5 => {
-        :name => 'Pizza',
-        :price => 10
-      },
-      6 => {
-        :name => 'Ice Cream',
-        :price => 4
-      },
-      7 => {
-        :name => 'Cake',
-        :price => 4
-      }  
-    }
-
+    @dishes = { 1=> { :name => "Tomato Soup", :price => 5 }, 2 =>{ :name => "Avocado On Toast", :price => 7 }, 3 => { :name => "Spaghetti Bolognese", :price => 12 }, 4 => { :name => "Mushroom Surprise", :price => 9 }, 5 => { :name => "Pizza", :price => 10 }, 6 => { :name => "Ice Cream", :price => 4}, 7=>{ :name => "Cake", :price => 4 }} 
   end
 
   attr_reader :dishes
 
   def show_menu 
     title("EdsDiner Menu:")
-    @dishes.each { | number, dish | 
-     puts "#{number}. #{dish[:name]}  £#{dish[:price]}"
-    }
+    @dishes.each do |number, dish| 
+      puts "#{number}. #{dish[:name]}  £#{dish[:price]}"
+    end
+  end
+
+  def select_dishes
+    show_menu
+    # while true do
+
+    # end
   end
 
 
@@ -48,7 +25,7 @@ class EdsDiner
 
   def title(text)
     puts text
-    ((text.length) - 1).times { print "~"}
+    ((text.length) - 1).times { print "~" }
     puts "~"
   end
 end
--- spec/eds_diner_spec.rb
+++ spec/eds_diner_spec.rb
@@ -9,7 +9,15 @@ describe EdsDiner do
     end
 
     it 'prints list of items in menu' do
-      expect{subject.show_menu}.to output(include("1. Tomato Soup  £5", "2. Avocado On Toast  £7")).to_stdout
+      expect { subject.show_menu }.to output(include("1. Tomato Soup  £5", "2. Avocado On Toast  £7")).to_stdout
+    end
+
+  end
+
+  context 'I would like to be able to select some number of several available dishes' do
+    
+    it 'shows menu + selection instructions when selecting dishes' do
+      expect { subject.select_dishes }.to output(include("EdsDiner Menu:")).to_stdout
     end
 
   end
