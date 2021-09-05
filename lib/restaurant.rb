bfc7e732cb06bae9f9e8dd4a0ac67afb100acac3
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-05 14:00:47 +0100

    update select_dish test

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,12 +4,20 @@ attr_reader :list_of_dishes
 attr_reader :order
 
 def initialize
-@list_of_dishes = {:burger => 10 , :pizza => 5}
-@order = []
+@list_of_dishes = {"burger" => 10, "pizza" => 5, "pasta" => 15, "wings" => 7, "kombucha" => 3 ,"coke"=> 3}
+@order = {}
+end 
+
+def read_menu
+@list_of_dishes
 end 
 
 def select_dish(choice)
-  @order << choice.downcase
+  @list_of_dishes.each {|k,v| if k == choice.downcase
+  @order.merge!(k => v)
+  end}
+  
+  #running_total = order
 end
 
 
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,21 +2,21 @@ require 'restaurant'
 
 describe Restaurant do 
 
-  let(:list_of_dishes) {{:burger => 10, :pizza => 5}}
-  let(:dish) {:burger}
+  # let(:list_of_dishes) {{:burger => 10, :pizza => 5}}
+  let(:dish) {"burger"}
 
   it {is_expected.to respond_to(:list_of_dishes)}
   it {is_expected.to respond_to(:select_dish).with(1).argument}
 
   it "shows dishes" do 
     #expect(subject.list_of_dishes).to include (:burger) 
-    expect(list_of_dishes).to include(:burger)
+    expect(subject.read_menu).to eq subject.list_of_dishes
   end 
 
   describe "#select_dishes" do 
     it "should select dishes" do 
     subject.select_dish(dish)
-    expect(subject.order).to include(dish)
+    expect(subject.order).to have_key(dish)
     end 
   end 
 
