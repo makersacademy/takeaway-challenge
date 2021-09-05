a3aa6439dc8ca2be78a070fa4dd8c3a747fd4df2
jake <jakebs540@gmail.com>
2021-09-05 13:02:13 +0100

    Restrauant initialize with menu

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,7 +1,11 @@
-require 'order'
-require 'menu'
+require_relative 'menu'
 
 class Restaurant
 
-  
+  attr_reader :menu
+
+  def initialize
+    @menu = Menu.new(menu)
+  end
+
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,4 +2,13 @@ require './lib/restaurant'
 
 describe Restaurant do
 
+  describe '#initialize' do
+    it 'initializes with Menu' do
+      expect(subject.menu).to be_an_instance_of Menu
+    end
+  end
+
+
+
+
 end
