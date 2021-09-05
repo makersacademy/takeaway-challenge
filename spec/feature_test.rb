c4c7d6bdcfb50324e57685f042fbcb8314d44091
jake <jakebs540@gmail.com>
2021-09-05 13:03:50 +0100

    feature tests updated

---
+++ spec/feature_test.rb
@@ -0,0 +1,9 @@
+# require './lib/restaurant.rb'
+# require './lib/menu.rb'
+# require './lib/order.rb'
+
+# menu = Menu.new
+# menu.display_menu
+
+# restaurant = Restaurant.new
+# restaurant.view_menu
