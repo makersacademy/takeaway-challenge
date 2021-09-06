119190f3a234418f6d0ebbba04f935c79ad05ca9
tj <tomisinjoseph3@gmail.com>
2021-09-06 02:23:00 +0100

    refactored price test to use an attr_reader

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,2 +1,13 @@
 class Dish
+attr_reader :price
+  def initialize
+    @price
+  end
+
+  def see_list
+  end
+
+  # def price
+  #   @price
+  # end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,5 +1,13 @@
 require 'dish'
 
 describe Dish do
+  it { is_expected.to respond_to(:see_list) }
   
+  it 'has a price' do
+    expect(subject.price).to eq @price
+  end
+
+  # it 'initializes with a price' do
+  #   expect 
+  # end
 end
