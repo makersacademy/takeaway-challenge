7883d04984b35023cc185b9ad8eb2d2133ec5aeb
Tom Lee <tomalee2002@gmail.com>
2021-09-06 02:21:27 +0100

    added spec test for add_meal

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,9 +2,13 @@ require 'restaurant'
 
 describe Restaurant do
   
-  it "shows the menu" do
+  it 'shows the menu' do
     menu = "Fries - £2\nMilkshake - £4\n"
     expect { subject.display_menu }.to output(menu).to_stdout
   end
 
+  it 'can add a meal to the order' do
+    subject.add_meal("Curry")
+    expect { subject.display_order }.to eq("Curry" => 1)
+  end
 end
