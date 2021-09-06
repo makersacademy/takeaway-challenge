031a9c65b91c3037978c818502d9f39de2f3b146
Tom Lee <tomalee2002@gmail.com>
2021-09-06 02:15:26 +0100

    added display_menu method and relevant spec

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,14 @@
 class Restaurant
-
+  def initialize
+    @menu = {
+      "Fries" => 2,
+      "Milkshake" => 4
+    }
+  end
+  
+  def display_menu
+    @menu.each do |meal, price|
+      puts "#{meal} - £#{price}"
+    end
+  end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,2 +1,10 @@
 require 'restaurant'
 
+describe Restaurant do
+  
+  it "shows the menu" do
+    menu = "Fries - £2\nMilkshake - £4\n"
+    expect { subject.display_menu }.to output(menu).to_stdout
+  end
+
+end
