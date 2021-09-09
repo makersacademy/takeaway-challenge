93872736d68d9ee916d13767cc46bf6dc5c73faf
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-09 22:03:03 +0100

    Adjustments to Twilio method

--- lib/send_TwilioText.rb
+++ lib/send_TwilioText.rb
@@ -2,23 +2,19 @@ require 'twilio-ruby'
 require 'dotenv/load' #gem dotenv 
 
 class TwilioText
-
   def send_text
     twilio_account_sid = ENV['TWILIO_ACCOUNT_SID']
     twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
-    @client = Twilio::REST::Client.new(twilio_account_sid, twilio_auth_token)
-    message
-  end
-
-  def message
+    from = ENV['TWILIO_NUM']
+    to = ENV['PHONE_NUM']
+    client = Twilio::REST::Client.new(twilio_account_sid, twilio_auth_token)
+  
     time = Time.now
     arrival = time.hour + 1
-    @from = ENV['TWILIO_NUM']
-    @to = ENV['PHONE_NUM']
-
-    @client.messages.create(
-    from: @from,
-    to: @to,
+  
+    client.messages.create(
+    from: from,
+    to: to,
     body: "Thank you! Your order was placed and will be delivered before #{arrival}:#{time.min}"
     )
   end
