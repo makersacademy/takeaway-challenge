c1844891e0ac57180c6e23f8460d27c605e9e118
Arthur Fincham <arthurfincham@me.com>
2021-09-30 11:39:08 +0100

    add dish and dish_spec

---
+++ spec/dish_spec.rb
@@ -0,0 +1,15 @@
+require 'dish'
+
+describe Dish do
+
+  describe ' has ' do
+    subject{ Dish.new('Pepperoni Pizza', 11.5) }
+    it 'a name' do
+      expect(subject.name).to eq('Pepperoni Pizza')
+    end
+
+    it 'a price' do
+      expect(subject.price).to eq 11.5
+    end
+  end
+end
