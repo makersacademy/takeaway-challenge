44c40e3beac75af9789d7722787befe78768300d
FrancineBlanc <francine.blanc@gmail.com>
2021-10-01 11:07:00 +0100

    refactor menu tests so less brittle

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,21 +5,24 @@
 require "menu"
 
 describe Menu do
+
+  subject { described_class.new }
+  
+  let(:menu) { { test_dish1: 7.50, test_dish2: 6.00, test_dish3: 4.00 } }
+  
+  before(:example) do
+    allow(subject).to receive(:menu).and_return(menu)
+  end
+
   describe "#menu" do
     it "has a list of dishes with prices" do
-      expect(subject.menu).to include(curried_goat: 7.50)
+      expect(subject.menu).to include(test_dish1: 7.50)
     end
   end
+
   describe "#show_menu" do
     it "displays the menu" do
-      expect(subject.show_menu).to eq curried_goat: 7.50,
-        fried_chicken: 6.50,
-        fried_snapper: 7.00,
-        ackee_with_sweet_peppers: 6.00,
-        fried_plantain: 2.00,
-        mac_and_cheese: 4.00,
-        rice_and_peas: 3.00,
-        plain_rice: 2.50
+      expect(subject.show_menu).to eq test_dish1: 7.50, test_dish2: 6.00, test_dish3: 4.00
     end
   end
 end
