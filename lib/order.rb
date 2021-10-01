bc3f538387efecf00824c0597373b772b2a9eede
pinar <pnr_ozbakir@msn.com>
2021-10-01 21:47:09 +0100

    US2: choose dish and add to order

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,21 @@
 class Order
+  attr_reader :current_order
+
   def initialize
     @current_order = []
   end
 
   def choose_dish(dish_name)
     @dish_name = dish_name
-    @current_order << @dish_name
-    p "Do want to add another dish to your order?"
-    # confirm = gets.chomp
+    store_dish(@dish_name)
+    p "You can continue adding other dishes to your order"
+    # input = gets.chomp
+    # Order.new.choose_dish(dish_name) if input == "Yes"
+  end
+
+  private
+
+  def store_dish(new_dish)
+    @current_order << new_dish
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,12 +2,19 @@ require "order"
 
 describe Order do
   describe "#taking_order" do
+    # subject = Order.new.choose_dish("Salad")
     it "chooses dish from the dishlist" do
       expect(subject).to respond_to(:choose_dish).with(1).argument
     end
 
     it "asks if user wants to add more dishes" do
-      expect(subject.choose_dish("dish_one")).to eq "Do want to add another dish to your order?"
+      expect(subject.choose_dish("Salad")).to eq "You can continue adding other dishes to your order"
+    end
+
+    it "adds the dish to the current order list" do
+      new_order = Order.new()
+      new_order.choose_dish("Salad")
+      expect(new_order.current_order).to eq ["Salad"]
     end
   end
 end
