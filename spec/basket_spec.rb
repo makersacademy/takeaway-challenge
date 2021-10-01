5ee906c5ae30d1504f1ddec4f9144c16edd76acc
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-10-01 14:00:27 +0100

    added .view_basket and .initialize tests to basket_spec.rb

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -3,9 +3,18 @@ require "basket"
 RSpec.describe Basket do
   let(:basket) { Basket.new }
   describe ".add_item" do
+    describe "initialize" do
+      it "starts with an empty basket" do
+        expect(basket.basket).to eq []
+      end
+    end
     it "adds food to the basket" do
-      basket.add_item({ :item => "D1 - Rainbow Maki", :price => 8.8 })
-      expect(basket.view_basket).to eq [{ :item => "D1 - Rainbow Maki", :price => 8.8 }]
+      expect { basket.add_item("D8 - Spider") }.to change { basket.basket.length }.to 1
+    end
+  end
+  describe ".view_basket" do
+    it "returns an array of all items in the basket" do
+      expect { basket.add_item("D10 - White Tiger Maki") }.to change { basket.basket }.to ["D10 - White Tiger Maki"]
     end
   end
 end
