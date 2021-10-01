5558d4c0e0e6183d816f9de455e845e9fb1881d3
twilliams1988 <tom@teow.co.uk>
2021-10-01 19:43:50 +0100

    Add text confirmation and tests

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,11 +1,9 @@
 require 'order'
-require 'dish'
 
 describe Order do
   let(:order) { Order.new }
   let(:mapotofu) { double :dish, :name => "Mapo Tofu", :price => 5.0 }
   let(:gongbaotofu) { double :dish, :name => "Gongbao Tofu", :price => 4.5 }
-  # mapotofu = Dish.new('Mapo Tofu', 5.00)
   
   describe '#add' do
     it 'adds a dish to an order' do
@@ -17,7 +15,6 @@ describe Order do
   describe '#print_receipt' do
     it 'prints a receipt with total' do
       order.add(mapotofu)
-      # allow(mapotofu).to receive(:name) { "Mapo Tofu" }
       expect(order.print_receipt).to eq("Order:\nMapo Tofu - 5.00\nTotal: £5.00")
     end
 
