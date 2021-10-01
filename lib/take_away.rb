c651283a5b10a7234e9dce62e201fd2e591fd599
Joe <josephshaw1563@gmail.com>
2021-10-01 17:03:37 +0100

    API next

--- lib/take_away.rb
+++ lib/take_away.rb
@@ -1,6 +1,7 @@
 class Takeaway
-   attr_accessor :menu
+   attr_reader :menu
    attr_reader :basket
+   attr_reader:total
   def initialize
     @menu = {
       "burger" => 5.00,
@@ -10,13 +11,50 @@ class Takeaway
     @basket = []
     @total = 0
   end
+
   def list_menu 
     @menu
   end
-  def add_item(item)
-    @basket.push(item)
-    cost = @menu.fetch(item)
-    @total += cost
+
+  def add_dish(dish , price)
+    @menu[dish] = price
   end
 
+  def remove_dish(dish)
+    if @menu.has_key?(dish) == true
+      @menu.delete(dish)
+    else
+      return "this dish does not exist"
+    end
+  end
+  def basket_price
+    @total
+  end
+
+  def add_item(item)
+    if menu.key?(item)
+      @basket.push(item)
+      cost = @menu.fetch(item)
+      @total += cost
+    else 
+      return "This is not a menu item"
+    end
+  end
+
+  def remove_item(item)
+    if basket.include?(item) == false
+      return "this item is not in your basket"
+    else
+      cost = @menu.fetch(item)
+      @total -= cost
+      basket.delete_at(basket.find_index(item))
+
+    end
+  end
 end
+
+mc_donalds = Takeaway.new
+
+#classes 
+# Customer : basket, add_item, remove_item, deliver
+# Resturant : menu, add_dish, remove_dish
--- spec/take_away_spec.rb
+++ spec/take_away_spec.rb
@@ -4,8 +4,32 @@ describe Takeaway do
   before(:each) do
     take_away = Takeaway.new
   end
+  describe 'list_menu' do
   it 'returns the contents of the menu; item and price' do
-    expect(subject.list_menu).to eq({:burger=>5.0, :chips=>2.0, :coke=>1.0})
+    expect(subject.list_menu).to eq({"burger"=>5.0, "chips"=>2.0, "coke"=>1.0})
   end
-  
+end
+  describe 'basket & adding/removing items' do 
+    it 'adds an item to the basket' do
+      subject.add_item("burger")
+      expect(subject.basket).to eq ["burger"]
+    end
+    it 'handles invalid order requests (items not on menu)' do
+      expect(subject.add_item("pepsi")).to eq("This is not a menu item")
+    end
+    it 'tries to remove an item that is not in the basket' do
+      expect(subject.remove_item("coke")).to eq "this item is not in your basket"
+    end
+    it 'adds then removes an item; makes sure basket empty' do
+      subject.add_item("chips")
+      subject.remove_item("chips")
+      expect(subject.basket).to eq []
+    end 
+    it  'adds then removes an item; makes sure total is correct' do
+      subject.add_item("chips")
+      subject.remove_item("chips")
+      expect(subject.total).to eq 0
+    end
+
+  end
 end
