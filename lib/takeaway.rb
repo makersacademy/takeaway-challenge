c6fea79420e5266299a7c153877247b5fb52398b
Lisa Mac <lisa@MacBook-Air-2.local>
2021-10-03 00:36:09 +0100

    Step 3 Check order is correct

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -9,6 +9,22 @@ class Takeaway
   end 
 
   def order(food)
+    @food = food
     true 
+  end 
+
+  def total
+    #how to retrieve food in the order. Get food variable. Iterate, price x quantity
+    # map will return an array with transformed values of the result of the block
+    # each will not return anything, you have to save it
+    # extract the prices from the menu item, using the food order, match with menu to get price
+    total = 0
+    # food |element| iterate through menu
+
+    @food.each do | element |
+      price =  @menu.select {|dish| element[:item] == dish[:item]}.first[:price]
+      total += price * element[:quantity]
+    end 
+    total
   end 
 end 
--- spec/takeway_spec.rb
+++ spec/takeway_spec.rb
@@ -8,19 +8,31 @@ describe Takeaway do
   {:item => "Rice noodles", :price => 1}, 
   {:item => "prawn salad", :price => 2}, 
   {:item => "coffee", :price => 2}]}
-  let(:takeaway) {Takeaway.new(menu)} 
-
+  # let(:takeaway) {Takeaway.new(menu)} 
+  let(:food) {[{:item => "Rice noodles", :quantity => 1}, {:item =>"prawn salad", :quantity => 2}, {:item => "coffee", :quantity => 2}]}
+  before(:each) do
+    @takeaway = Takeaway.new(menu)
+    #each test should run independently, and shouldn't matter in its order. 
+    #if you use the same instance of takeaway for each of the test, but one test could 
+    #be setting the instance which could influence the test. 
+    #may be better than having instance variabes - which are the same object.
+  end 
   describe "#see menu" do 
     it "allows you to see the menu of dishes with prices" do
-    expect(takeaway.see_menu).to eq(menu)
+    expect(@takeaway.see_menu).to eq(menu)
     end
   end
   
   describe "#order method" do
     it "allows you to select a number of available dishes from the menu" do
- 
-      food = [{:item => "Rice noodles", :quantity => 1}, {:item =>"prawn salad", :quantity => 2}, {:item => "coffee", :quantity => 2}]
-      expect(takeaway.order(food)).to be_truthy
+      expect(@takeaway.order(food)).to be_truthy
     end
-  end  
+  end
+  
+  describe "#total method" do 
+    it "sums up all of the prices in the order" do
+      @takeaway.order(food)
+      expect(@takeaway.total).to eq(9)
+    end 
+  end 
 end 
