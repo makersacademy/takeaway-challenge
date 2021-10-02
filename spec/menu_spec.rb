07639565f22816e931de469be145afb2a883d542
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-02 11:08:24 +0100

    working on menu dish release

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -8,28 +8,23 @@ describe Menu do
   let(:dish1) { double(:dish, name: "Chicken Tikka", price: 5.00 ) }
   let(:dish2) { double(:dish, name: "Jalfrezi", price: 6.00 ) }
 
-  describe "#print" do
+  context "Adding then printing" do
 
     it "should return list of dishes and prices each on new line" do
       menu.add(dish1)
-      puts "SEE BELOW"
-      puts dish1.price
       menu.add(dish2) 
       expect(menu.print).to eq "Chicken Tikka: £5.0 \nJalfrezi: £6.0" # needs to show two zeros.
     end
 
   end
 
-  describe "#add" do
-
-    it "should add a dish and be viewable from print" do
-      
-    end
-  end
   
   describe "#release" do
 
     it "should return a dish from the menu " do
+      menu.add(dish1)
+      menu.add(dish2)
+      
       
     end
 
