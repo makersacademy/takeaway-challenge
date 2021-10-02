022b38d4d7355c771304158e02f24bc11ac6360a
Arthur Fincham <arthurfincham@me.com>
2021-10-02 11:20:33 +0100

    refactor dish with attr_reader

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,14 +1,9 @@
 class Dish
+
+  attr_reader :name, :price
+
   def initialize(name, price)
     @name = name
     @price = price
-  end
-
-  def name
-    @name
-  end
-
-  def price
-    @price
   end
 end
