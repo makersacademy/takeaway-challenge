4b79e69240887de15ac30385f346d80fb766db92
parsa <parsamesg@gmail.com>
2021-10-03 23:27:16 +0100

    add an add to order method unit test

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,4 +1,13 @@
 require "order"
 
 describe Order do
+  let(:order) { Order.new }
+  let(:pizza) { double :dish, :name => "Pizza", :price => 8.00 }
+
+  describe "#add" do
+    it "adds a specified dish to the order" do
+      order.add(pizza)
+      expect(order.dishes_ordered).to include(pizza)
+    end
+  end
 end
