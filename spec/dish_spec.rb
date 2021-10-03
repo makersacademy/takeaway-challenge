8c9d7a69404fc209d07cd05c611832ab90cf0bbd
parsa <parsamesg@gmail.com>
2021-10-03 22:02:54 +0100

    add unit test to check if dish has price

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,10 +2,14 @@ require "dish"
 
 describe Dish do
   before do
-    @dish = Dish.new("Pizza")
+    @dish = Dish.new("Pizza", 8.00)
   end
 
   it "has a dish name" do
     expect(@dish.name).to eq("Pizza")
+  end
+
+  it "has a dish price" do
+    expect(@dish.price).to eq(8.00)
   end
 end
