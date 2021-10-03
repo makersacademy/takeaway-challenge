f718786cbffe7e2edd0bf4c04cac164ae653228a
parsa <parsamesg@gmail.com>
2021-10-03 21:16:13 +0100

    add unit test for adding a dish to the menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,17 @@
 require "menu"
 
 describe Menu do
+  let(:menu) { Menu.new }
+
   describe "#list" do
     it "lists menu with dishes and prices" do
-      expect(subject.list).to eq({ :Pizza => 8, :Chicken => 6 })
+      expect(menu.list).to eq({ :Pizza => 8, :Chicken => 6 })
+    end
+  end
+  describe "#add" do
+    it "adds a dish to the menu" do
+      menu.add(:dish_name)
+      expect(menu.dishes).to include(:dish_name)
     end
   end
 end
