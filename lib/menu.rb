00e12a802bf4ed32beb663aa19b058bd509e7911
Leo Brown <leo_brown_1@hotmail.com>
2021-10-03 14:04:14 +0100

    "Split send text into separate method"

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,9 @@
 require 'twilio-ruby'
 
+  # account_sid = "AC56a614c80c40b2d730f624b8f395bf10" # Your Test Account SID from www.twilio.com/console/settings
+  # auth_token = "b22114508794bdb02e7d2fe587e926c4"   # Your Test Auth Token from www.twilio.com/console/settings
+  # send_phone_num = "+19384440993"
+
 class Menu
   attr_reader :list, :order, :total, :delivery_message
 
@@ -10,6 +14,7 @@ class Menu
       3 => "chips £3"
     }
     @order = []
+    @delivery_message = ""
   end
 
   def see_list
@@ -25,26 +30,21 @@ class Menu
     "Your total is #{@total} for the following #{@order}"
   end
 
-  def confirm_order(account_sid = "AC56a614c80c40b2d730f624b8f395bf10", auth_token = "b22114508794bdb02e7d2fe587e926c4", send_phone_num = "+19384440993")
+  def confirm_order
     t = Time.new
     hour_plus1 = ((t.strftime("%H").to_i) + 1)
-    p @delivery_message  = "Thank you! Your order was placed and will be delivered before #{t.strftime("at #{hour_plus1}:%M%p")}"
-    # Below is the twilio text functionality
-    # account_sid = "AC56a614c80c40b2d730f624b8f395bf10" # Your Test Account SID from www.twilio.com/console/settings
-    # auth_token = "b22114508794bdb02e7d2fe587e926c4"   # Your Test Auth Token from www.twilio.com/console/settings
-    # send_phone_num = "+19384440993"
+    @delivery_message  = "Thank you! Your order was placed and will be delivered before #{t.strftime("at #{hour_plus1}:%M%p")}"
+  end
+
+  def send_text(account_sid = "AC56a614c80c40b2d730f624b8f395bf10", auth_token = "b22114508794bdb02e7d2fe587e926c4", send_phone_num = "+19384440993")  
     @client = Twilio::REST::Client.new account_sid, auth_token
     message = @client.messages.create(
       body: @delivery_message,
       to: "+447947613279",    # Replace with your phone number
       from: send_phone_num)  # Use this Magic Number for creating SMS
   
-    # message.sid
+    message.sid
 
     # POST /2010-04-01/Accounts/{AC56a614c80c40b2d730f624b8f395bf10}/Messages
   end
 end
-
-# menu = Menu.new
-# menu.place_order(1, 2)
-#       menu.confirm_order("ACccb2dbca9aea7869f0ab7bbbdca1df82", "2bf2f033df709c2079d8ee1182a16315", "+15005550006")
