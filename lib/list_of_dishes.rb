2413089966cfd16af82d611a84c7d9114bb4698d
charlie83xt <charlie83xt@hotmail.com>
2021-10-03 11:54:24 +0100

    print_total_order method added

--- lib/list_of_dishes.rb
+++ lib/list_of_dishes.rb
@@ -9,35 +9,45 @@ class ListofDishes
   end
 
   def menu
-    @list_of_dishes = { "Lassagna" => 9, "Ravioli" => 9, "Spaghetti Bolonese" => 9, "Macarones" => 9, "Penne" => 8, "Canneloni" => 8, "Margherita" => 9, "4 Cheeses" => 10, "Tuna & Onion" => 8, "Veggie Lover" => 9, "Salami" => 10, "Chicken with Tomato" => 10 } 
+    @list_of_dishes = { "Lassagna" => 9, "Ravioli" => 9, "Spaghetti Bolonese" => 9, "Macarones" => 9, "Penne" => 8, "Canneloni" => 8, "Margherita" => 9, "Four Cheeses" => 10, "Tuna & Onion" => 8, "Veggie Lover" => 9, "Salami" => 10, "Chicken with Tomato" => 10 } 
   end
 
   def print_menu
     @list_of_dishes.each_with_index do |(dish, price),index|   
       puts "#{index + 1} #{dish} : £#{price}"
-      p  "Select dish or type finish"
     end
+    p  "** Select dish or type finish **"
   end
 
   def order
-    !order == "finish" ?
-    print_menu
-    # puts 
-    order = gets.chomp
-    
+    loop do
+      print_menu
+      order = gets.chomp.capitalize
+      self.make_order(order)
+      if order == "finish" 
+        break
+      end
+    end
   end
 
   def make_order(dish)
+    if @list_of_dishes.include?(dish) 
+      @total_order << @list_of_dishes.assoc(dish) 
+    elsif 
+      dish == "finish"
+      return @total_order
+    elsif
+      !@list_of_dishes.include?(dish) 
+        raise("Dish not found!")
+    end
+  end
 
-    @list_of_dishes.include?(order) ? @total_order << @list_of_dishes.assoc(order) : @total_order
+  def print_total_order
+    total = concat(make_order.total_order)
   end
 end
 
-# class Choice
-  
-#   def initialize(list_of = ListofDishes)
-#     @list_of = ListofDishes.new
-#   end
+
 
   
 
--- spec/list_of_dishes_spec.rb
+++ spec/list_of_dishes_spec.rb
@@ -2,14 +2,14 @@ require 'list_of_dishes'
 
 describe ListofDishes do
   it "prints the menu" do
-    expect(subject.print_menu).to eq("Lassagna" => 9,
+    expect(subject.menu).to eq("Lassagna" => 9,
     "Ravioli" => 9,
     "Spaghetti Bolonese" => 9,
     "Macarones" => 9,
     "Penne" => 8,
     "Canneloni" => 8,
     "Margherita" => 9,
-    "4 Cheeses" => 10,
+    "Four Cheeses" => 10,
     "Tuna & Onion" => 8,
     "Veggie Lover" => 9,
     "Salami" => 10,
@@ -24,5 +24,10 @@ describe ListofDishes do
     allow($stdin).to receive(:gets).and_return(order)
     # expect(order).to receive(:chomp).and_return()
     expect(subject.total_order.length).to eq(1)
+  end
+
+  it "raise error when dish ordered is not found in the menu" do
+    order = double("Fish")
+    expect { subject.make_order(order) }.to raise_error("Dish not found!")
   end
 end
