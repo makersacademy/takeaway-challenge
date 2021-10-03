4371d9892fcc028e3c6834637bca76e5f71d403f
parsa <parsamesg@gmail.com>
2021-10-03 21:16:40 +0100

    pass unit test for adding a dish to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,14 @@
 class Menu
+  attr_accessor :dishes
+
+  def initialize
+    @dishes = []
+  end
+
+  def add(dish)
+    @dishes << dish
+  end
+
   def list
     { :Pizza => 8, :Chicken => 6 }
   end
