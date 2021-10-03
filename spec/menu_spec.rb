72f6467ba7cbac948b25f33ff38ea5982dca3c4c
parsa <parsamesg@gmail.com>
2021-10-03 17:42:10 +0100

    add unit test for list method

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,4 +1,9 @@
 require "menu"
 
 describe Menu do
+  describe "#list" do
+    it "lists menu with dishes and prices" do
+      expect(subject.list).to eq({ :Pizza => 8, :Chicken => 6 })
+    end
+  end
 end
