8cc7fae9e05e9cb63678e3e16f57e7defd4abe22
parsa <parsamesg@gmail.com>
2021-10-03 19:32:42 +0100

    add unit test to list a menu

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,4 +1,15 @@
 require "takeaway"
 
 describe Takeaway do
+  subject(:takeaway) { described_class.new(menu: menu) }
+
+  let(:menu) { double(:menu, list: listed_menu) }
+
+  let(:listed_menu) { "Pizza: 8" }
+
+  describe "#list_menu" do
+    it "lists menu with dishes and prices" do
+      expect(takeaway.list_menu).to eq(listed_menu)
+    end
+  end
 end
