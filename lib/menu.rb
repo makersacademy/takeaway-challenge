615936049bd18d6b9f10b0ebf6a6d31619ceaa52
Xia <zeyaoliu@me.com>
2021-10-04 03:44:09 +0100

    Added methods for calculating totals

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,11 +3,11 @@ class Menu
 
   def initialize
     @menu = {
-      :avocade_maki => 4,
+      :avocado_maki => 4,
       :cucumber_maki => 4,
       :salmon_sashimi => 7,
       :vegetable_ramen => 6,
-      :katcu_curry => 9,
+      :katsu_curry => 9,
       :miso_soup => 3,
       :crab_handroll => 6,
     }
@@ -15,5 +15,10 @@ class Menu
 
   def check_menu
     @menu
+  end
+
+  def price(dishes)
+    dish_prices = dishes.map { |dish| @menu[dish] }
+    dish_prices.sum
   end
 end
