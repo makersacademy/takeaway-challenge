fff105cb990190daf15f84473f24f7fb5a3bce05
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-10-04 11:12:21 +0100

    debug add_item and added check_total

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,15 +1,30 @@
 class Basket
-  attr_accessor :basket
+  attr_accessor :basket, :balance
 
   def initialize
     @basket = []
+    @balance = 0
   end
 
   def add_item(item)
-    @basket << item
+    Menu::MENU.each do |menu_item|
+      menu_item.each do |key, value|
+        if value.is_a?(String)
+          if value.include?(item)
+            @basket << menu_item[:item]
+            @balance += menu_item[:price]
+            break
+          end
+        end
+      end
+    end
   end
 
   def view_basket
     @basket
+  end
+
+  def check_total
+    "Order: #{@basket} /n Total: £#{@balance}0"
   end
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -9,12 +9,20 @@ RSpec.describe Basket do
       end
     end
     it "adds food to the basket" do
+      item = double("Item")
       expect { basket.add_item("D8 - Spider") }.to change { basket.basket.length }.to 1
     end
   end
   describe ".view_basket" do
     it "returns an array of all items in the basket" do
       expect { basket.add_item("D10 - White Tiger Maki") }.to change { basket.basket }.to ["D10 - White Tiger Maki"]
+    end
+  end
+  describe "check_total" do
+    it "returns the value of menu items in the basket" do
+      basket.add_item("D8 - Spider")
+      p basket.check_total
+      expect(basket.check_total).to eq "Total: £9.80"
     end
   end
 end
