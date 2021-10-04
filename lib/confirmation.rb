3d01655dd25e765cdfb7dd86fc65d3bdfe7829ce
Maisha <maisha-1998@hotmail.co.uk>
2021-10-04 08:50:39 +0100

    confirmation.rb updated

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -4,6 +4,8 @@ class Confirmation
   #Confirmation --> dishes_selected --> selected_dish_total --> confirmed 
   #Confirmation --> confirmed --> text_sent 
   #--> "Thank you! Your order was placed and will be delivered before 18:52"
+  #Method without Twilio API 
+  #Dummy code unfinished 
   
   def selected_dish_total
     if dishes_selected == selected_dish_total.last 
