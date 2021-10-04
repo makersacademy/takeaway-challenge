7d84451c554292e215acabd0892ba2c7b9e0a490
parsa <parsamesg@gmail.com>
2021-10-04 08:42:04 +0100

    pass unit test for basket summary with private methods

--- lib/order.rb
+++ lib/order.rb
@@ -8,4 +8,22 @@ class Order
   def add(dish)
     @dishes_ordered << dish
   end
+
+  def basket_summary
+    "Basket:\n#{basket}\nTotal: £#{"%.2f" % total}"
+  end
+
+  private
+
+  def basket
+    dishes = ""
+    @dishes_ordered.each do |dish|
+      dishes += "#{dish.name} - #{"%.2f" % dish.price}\n"
+    end
+    dishes
+  end
+
+  def total
+    @dishes_ordered.inject(0) { |sum, dish| sum + dish.price }
+  end
 end
