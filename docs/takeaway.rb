5118799aee0b126d5db61d48f1d183d41f6d6b3e
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-22 11:36:10 +0100

    Adjusted dishes method so it prints an array of items and their prices

--- docs/takeaway.rb
+++ docs/takeaway.rb
@@ -2,14 +2,20 @@ class Takeaway
   attr_reader :menu
 
   def initialize 
-    @menu = {
-      beef_curry: 12, 
-    }
+    @menu = [
+      "Beef curry: £12",
+      "Fish curry: £14",
+      "Lamb curry: £10",
+    ]
   end
 
   def dishes
+    menu_list = ""
 
+    @menu.each do |item|
+      menu_list = menu_list + item + " "
+    end
 
-    return "#{:beef_curry}: £#{@menu.fetch(:beef_curry)}"
+    return menu_list.strip
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,9 @@
-require './docs/takeaway.rb'
+require './docs/takeaway'
 
 describe Takeaway do
   it 'prints the dishes and their prices' do
     takeaway = Takeaway.new
 
-    expect(takeaway.dishes).to eq "beef_curry: £12"
+    expect(takeaway.dishes).to eq "Beef curry: £12 Fish curry: £14 Lamb curry: £10"
   end
 end
