6fabacd34bd6cfdf713599ef8cebea7138011737
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-22 11:47:30 +0100

    Added test and method for adding an order to my_orders

--- docs/takeaway.rb
+++ docs/takeaway.rb
@@ -1,5 +1,5 @@
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :my_order
 
   def initialize 
     @menu = [
@@ -7,15 +7,23 @@ class Takeaway
       "Fish curry: £14",
       "Lamb curry: £10",
     ]
+    @my_order = []
   end
 
   def dishes
+    
     menu_list = ""
-
     @menu.each do |item|
       menu_list = menu_list + item + " "
     end
-
+    # .strip is used to remove last trailing white space on printed string
     return menu_list.strip
+  end
+
+  def add_to_order(item)
+    if @menu.include?(item)
+      @my_order << item
+    end
+    return my_order
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,21 @@
 require './docs/takeaway'
 
 describe Takeaway do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
   it 'prints the dishes and their prices' do
     takeaway = Takeaway.new
 
     expect(takeaway.dishes).to eq "Beef curry: £12 Fish curry: £14 Lamb curry: £10"
+  end
+
+  # As a customer
+  # So that I can order the meal I want
+  # I would like to be able to select some number of several available dishes
+  it 'selects several dishes and adds them to order' do
+    takeaway = Takeaway.new
+
+    expect(takeaway.add_to_order("Beef curry: £12")).to eq takeaway.my_order
   end
 end
