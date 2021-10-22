c66ed5aa27301c97b6ab5adbe71f229660568d8a
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-22 13:38:58 +0100

    refactoring my first test to include doubles

--- Lib/takeaway.rb
+++ Lib/takeaway.rb
@@ -2,14 +2,32 @@
 
 class Takeaway
     
-    attr_reader :dishes
-    # An attribute reader returns the value of an instance variable.
+    # attr_reader :menu 
+    # # An attribute reader returns the value of an instance variable.
 
-    def intialize(dishes = Hash.new )
-        @dishes = dishes 
+    # def intialize(menu = Hash.new )
+    #     @menu = menu 
+    # end 
+
+    # def dish
+    #     menu = { "topokki" => 10, "rice" => 5, "Kimchi" => 5,  "Bibimbap" => 15 }
+    # end 
+
+    # def selecting_meal 
+
+
+    # end 
+
+    def initialize(menu:) # depedency injection in ruby 
+        @menu = menu 
     end 
 
-    def dish
-        dishes = { "topokki" => 10, "rice" => 5, "Kimchi" => 5,  "Bibimbap" => 15 }
+    def get_menu 
+        menu.print
     end 
+
+    private 
+
+    attr_reader :menu 
+
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,10 +2,30 @@ require 'takeaway'
 
 
 describe Takeaway do 
-    describe '#dish' do 
-        it 'lists the dishes with prices' do 
-            takeaway = Takeaway.new
-            expect(takeaway.dish).to include("topokki" => 10, "rice" => 5, "Kimchi" => 5,  "Bibimbap" => 15)
-        end 
+    # describe '#dish' do 
+    #     it 'lists the dishes with prices' do 
+    #         takeaway = Takeaway.new
+    #         expect(takeaway.dish).to include("topokki" => 10, "rice" => 5, "Kimchi" => 5,  "Bibimbap" => 15)
+    #     end 
+    # end 
+
+    # describe '#selecting_meal and #ordering ' do 
+    #     it 'customer can order meal from menu' do 
+    #         takeaway = Takeaway.new 
+    #         expect(takeaway.selecting_meal).to eq ('Thank you for your order, your total is :  ')
+    #     end 
+    # end 
+
+    subject(:takeaway) { described_class.new(menu: menu) } #Injecting menu into the takeaway, so need to create a menu double. : is a symbol/ constant and it has a unquiue value
+
+    #menu double 
+    let(:menu) { double(:menu, print: double_menu) }
+    let(:double_menu) { "topokki : £10 " }
+    
+    it 'lists the dishes with prices' do 
+        expect(takeaway.get_menu).to eq(double_menu)
+
     end 
+
+
 end  
