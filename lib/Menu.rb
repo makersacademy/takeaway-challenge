6109819e222367096d36b56f911d6d9664a9d4b2
Yassaha Ali <yassahaali@gmail.com>
2021-10-22 16:15:30 +0100

    changed test one to be better long termn. passed

--- lib/Menu.rb
+++ lib/Menu.rb
@@ -3,7 +3,12 @@ class Menu
   attr_reader :foods
 
   def initialize
-    @foods = [:chicken => 7, :lamb => 10, :beef => 12]
+    @foods = { "chicken" => 7, "lamb" => 10, "beef" => 12 }
+  end
+
+  def order
+    output = @foods.find {|item| item.include?("chicken")}
+    print output
   end
 
 end
--- spec/Menu_spec.rb
+++ spec/Menu_spec.rb
@@ -1,15 +1,10 @@
 require './lib/Menu'
 
 describe Menu do
-    
-  describe 'foods' do
 
-    it 'Entering food array class should display the menu' do 
-      menu = Menu.new
+    it 'entering food array class should display the menu' do 
 
-      expect(menu.foods).to contain_exactly(:chicken => 7, :lamb => 10, :beef => 12)
-    end 
-
-  end
+      expect(subject.foods).to contain_exactly(["beef", 12], ["chicken", 7], ["lamb", 10])
+    end
 
 end
