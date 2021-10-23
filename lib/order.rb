bffa48f1ec3607aba7da6acc523d0f5e06f1f18f
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-23 21:07:48 +0100

    add_items_to_order method, adjusted select dish_method and created and initialised an order attr_reader

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,21 @@
 require 'menu'
 
 class Order
-  attr_reader :selection, :menu
+  attr_reader :selection, :menu, :order
 
   def initialize(menu = Menu.new)
     @selection = {}
     @menu = menu
+    @order = []
   end
 
   def select_dish(item, quantity)
     @selection = { item => quantity }
+    add_items_to_order
+    return "#{quantity} &#{item}(s) added to your order"
+  end
+
+  def add_items_to_order
+    @order << @selection
   end
 end
