cf1c4070f6f0031a9f6448b661d1ba5a377f1c6e
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-23 22:06:20 +0100

    get price method

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,13 @@
 require 'menu'
 
 class Order
-  attr_reader :selection, :menu, :order
+  attr_reader :selection, :menu, :order, :prices
 
   def initialize(menu = Menu.new)
     @selection = {}
     @menu = menu
     @order = []
+    @prices = []
   end
 
   def select_dish(item, quantity)
@@ -17,5 +18,15 @@ class Order
 
   def add_items_to_order
     @order << @selection
+  end
+
+  def get_price
+    @order.each do |food| 
+      food.each do |item, quantity|
+        @price = menu.dishes[item]
+        puts "#{quantity} x #{item} at £#{@price *= quantity}"
+        @prices << @price
+      end
+    end
   end
 end
