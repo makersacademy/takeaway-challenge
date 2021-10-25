244a9ee863811a0275ead53bc3fed803f03f2544
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-25 08:35:28 +0100

    added customer phone to text

--- lib/text.rb
+++ lib/text.rb
@@ -1,6 +1,7 @@
 require 'twilio_interface'
 
 class Text
+  CUSTOMER_PHONE= ENV['MY_PHONE']
   attr_reader :body, :expected_time
   EMPTY_FIELDS = "cannot send, empty fields in message".freeze
 
@@ -16,6 +17,6 @@ class Text
 
   def send_message
     raise EMPTY_FIELDS if unable_to_send?
-    @twilio.send_message("#{body} #{expected_time}")
+    @twilio.send_message("#{body} #{expected_time}",CUSTOMER_PHONE)
   end
 end
