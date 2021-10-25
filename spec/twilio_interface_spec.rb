55ad805aae5a56481f1e443394440280652522d1
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-25 19:19:13 +0100

    fixed test for twilio_interface_spec.rb

--- spec/twilio_interface_spec.rb
+++ spec/twilio_interface_spec.rb
@@ -1,10 +1,13 @@
 require 'twilio_interface'
+require 'twilio-ruby'
+
 CUSTOMER_PHONE = ENV['MY_PHONE']
 RESPONSE = "200"
 describe TwilioInterface do
   it "send twilio message should receive http ok response 200" do
-    subject = TwilioInterface.new
-    allow(subject).to receive_message_chain(:messages, :create).and_return(RESPONSE)
+    twilio_client = Twilio::REST::Client.new(TwilioInterface::ACCOUNT_SID, TwilioInterface::AUTH_TOKEN)
+    subject = TwilioInterface.new(twilio_client)
+    allow(twilio_client).to receive_message_chain(:messages, :create).and_return(RESPONSE)
     expect(subject.send_message("jjj", CUSTOMER_PHONE)).to eq(RESPONSE)
   end
 end
