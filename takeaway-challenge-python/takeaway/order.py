413c08f64f73bb7d6d6c601020d118aca4d0d2d8
bmargerison <bmargerison@outlook.com>
2021-10-28 11:39:06 +0100

    Refactor to separate checking out and displaying current total

--- takeaway-challenge-python/takeaway/order.py
+++ takeaway-challenge-python/takeaway/order.py
@@ -8,14 +8,21 @@ class Order():
         self.order_finalised = False
         
     def add_to_basket(self, item, quantity):
-        #Is this a good one line format for a guard case?
+        #Is this a good one line format for a guard clause?
         if self.order_finalised: raise Exception('Order is complete')
         self.basket[item] = quantity
         
+    def cost(self):
+        #Struggling to understand how I can test a class that purely prints
+        #something to CLI, similar to Takeaway.show_menu function
+        cost_so_far = 0
+        for k, v in self.basket.items():
+            cost_so_far += (Takeaway.menu[k] * v)
+        print("Total to pay so far: %s" % self.cost_so_far)
+
     def checkout(self):
         if not self.basket: raise Exception('Nothing in basket')
         if self.order_finalised: raise Exception('You have already checked out')
         for k, v in self.basket.items():
             self.checkout_total += (Takeaway.menu[k] * v)
-        print("Total to pay: %s" % self.checkout_total)
         self.order_finalised = True
