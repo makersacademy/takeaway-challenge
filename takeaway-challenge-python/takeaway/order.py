cb37c00d7d92d859007a497c8818f4c3f4cd870e
bmargerison <bmargerison@outlook.com>
2021-10-28 11:11:18 +0100

    Can only checkout once

--- takeaway-challenge-python/takeaway/order.py
+++ takeaway-challenge-python/takeaway/order.py
@@ -10,6 +10,8 @@ class Order():
         self.basket[item] = quantity
         
     def checkout(self):
+        #Is this a good one line format for a guard case?
+        if self.checkout_total != 0 : raise NameError('You have already checked out')
         for k, v in self.basket.items():
             self.checkout_total += (Takeaway.menu[k] * v)
         print("Total to pay: %s" % self.checkout_total)
--- takeaway-challenge-python/tests/test_shop.py
+++ takeaway-challenge-python/tests/test_shop.py
@@ -1,6 +1,7 @@
 #I'm unsure if this is the simplest way to import classes
 from takeaway.takeaway import Takeaway
 from takeaway.order import Order
+import pytest
 
 def test_menu():
     #This is testing state rather than behaviour, but I'm puzzled how I can test
@@ -19,3 +20,12 @@ def test_checkout():
     order_1.checkout()
     assert order_1.checkout_total == 13.9
 
+def test_checkout_fails_if_already_checked_out():
+    order_1 = Order()
+    order_1.add_to_basket('chicken fried rice', 1)
+    order_1.checkout()
+    #Is this a succinct way to test errors?
+    with pytest.raises(NameError, match='You have already checked out'):
+        obj = order_1.checkout() 
+
+    
