3febdf7e200966c582d08ed85645ff3fc9af7f85
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-10-29 19:05:37 +0200

    Add dishes to @dishes in Menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,7 +3,13 @@ class Menu
   attr_reader :dishes
 
   def initialize
-    @dishes = {}
+    @dishes = {
+      pizza: 10,
+      burger: 7,
+      paella: 15,
+      carrot: 1,
+      cake: 3
+    }
   end
 
 end
