12d02339a70f32c22e7c70d7b17d96aa4b18c0e1
bmargerison <bmargerison@outlook.com>
2021-10-29 15:22:34 +0100

    Final packaging

--- takeaway-challenge-python/setup.py
+++ takeaway-challenge-python/setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 setup(
   name = 'bm_takeaway_makers',
   packages = ['bm_takeaway_makers'],
-  version = '0.4',
+  version = '0.5',
   license='MIT',
   description = 'takeaway-challenge',
   author = 'Ben Margerison',
   author_email = 'bmargerison@outlook.com',
   url = 'https://github.com/bmargerison/takeaway-challenge',
-  download_url = 'https://github.com/bmargerison/takeaway-challenge/archive/refs/tags/v0.4.tar.gz',
+  download_url = 'https://github.com/bmargerison/takeaway-challenge/archive/refs/tags/v0.5.tar.gz',
   keywords = ['takeaway', 'order', 'text'],
   install_requires=[
-          'twilio',
-          'pytest'
+          'pytest',
+          'twilio'
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      
