f9e3ce17cbd77842bb45ed2e125fe6410109b2a4
Mark McLeod <markmcleod45@gmail.com>
2021-10-31 12:08:00 +0000

    Refactor dish tests and made them pass

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,16 +1,28 @@
 require './lib/dish'
 
 describe Dish do
+  let(:name) { double :name }
+  let(:price) { double :price}
 
   describe '#name' do
     it 'has a name attribute on initialization' do
-      expect(subject.name).to eq("")
+      dish = Dish.new(name, price)
+      expect(dish.name).to eq(name)
     end
   end
 
   describe '#price' do
     it 'has a price attribute on initialization' do
-      expect(subject.price).to eq(0)
+      dish = Dish.new(name, price)
+      expect(dish.price).to eq(price)
     end
   end
+
+  # describe '#add_name_and_price' do
+  #   it 'updates the name and price' do
+  #   subject.add_name_and_price("Chips", 2)
+  #   expect(subject.name).to eq("Chips")
+  #   expect(subject.price).to eq(2)
+  #   end
+  # end
 end
