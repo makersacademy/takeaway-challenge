641f8e744649ae023d501550c257c4c35f4a7b9b
Jacob <jacobchanrahan@gmail.com>
2021-10-31 16:13:56 +0000

    not finished

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -4,13 +4,13 @@ require 'twilio'
 class Twilio
   def message
     account_sid = 'AC7c1e1a059b71f3e3be5280d996f93324' 
-    auth_token = '1e382af25e2b8322115664209b2fc4d9' 
+    auth_token = '#my AUTH KEY' 
     @client = Twilio::REST::Client.new(account_sid, auth_token) 
      
     message = @client.messages.create( 
                                  body: "You're order is on it's way!",  
                                  messaging_service_sid: 'MG1fae5eda0d0314ed521604f6b5cb550f',      
-                                 to: '+447896606420' 
+                                 to: '#my_number' 
                                ) 
      
     puts message.sid
