00955c45fc6fe2c7902bef300902bde6ab845c73
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-10-31 16:14:38 +0000

    fails when ordering something not on menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,21 @@
 class Takeaway
 
-  attr_reader :menu
+  attr_reader :menu, :order_total
 
   def initialize
-    @menu = {:salad => 1, :soup => 2, :chicken => 4, :fish => 3, :water => 1}
+    @menu = { :salad => 1, :soup => 2, :chicken => 4, :fish => 3, :water => 1 }
+    @order_total = []
   end
 
   def order(*args)
-  #   *args.each do |item|
-  #     if @menu.includes?(item)
+    args.each do |item|
+      if @menu.include?(item)
+        @order_total << @menu[item]
+      else 
+        fail "That is not a menu item, you can not order it"
+      end
+    end
 
   end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -8,4 +8,9 @@ describe Takeaway do
   it 'responds to ordering with many arguments' do
     expect(subject).to respond_to(:order).with_unlimited_arguments
   end
+
+  it 'raises error when customer orders item not in the menu' do
+    expect { subject.order("house") }.to raise_error("That is not a menu item, you can not order it")
+  end
+
 end
