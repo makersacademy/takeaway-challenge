434530c55391db20950a3c667a26b90494aeb8da
Mark McLeod <markmcleod45@gmail.com>
2021-10-31 20:20:30 +0000

    Add test for #display_basket and pass tests

--- lib/order.rb
+++ lib/order.rb
@@ -11,7 +11,11 @@ class Order
 
   def add_dish(dish_name, quantity)
     dish_to_add_to_basket = menu.dishes.select { |dish| dish[:name] == dish_name }.reduce(&:merge)
-    quantity.times { self.basket << dish_to_add_to_basket }
+    quantity.times { @basket << dish_to_add_to_basket }
+  end
+
+  def display_basket
+    @basket.each { |dish| puts "#{dish[:name]} - £#{dish[:price]}" }.reduce(&:merge)
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,6 +3,8 @@ require './lib/order.rb'
 describe Order do
   let(:menu) { Menu.new }
   let(:dish) { { name: "Chips", price: 2} }
+  let(:dish2) { { name: "Cheese burger", price: 10} }
+  let(:quantity) { 2 }
 
   describe '#intialize' do
     it 'initializes an empty order' do
@@ -13,6 +15,13 @@ describe Order do
   describe '#add_dish' do
     it 'adds dish to basket' do
       expect { subject.add_dish(dish, 1) }.to change { subject.basket.length }.by(1)
+    end
+  end
+
+  describe '#display_basket' do
+    it 'displays the contents of the basket' do
+      subject.add_dish(dish[:name], quantity)
+      expect(subject.display_basket).to include dish
     end
   end
 end
