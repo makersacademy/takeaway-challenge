023932529bf1caa06b352c8377e49291dff4a031
Simon Jacobs <simonjacobs@simons-air.home>
2021-10-31 23:13:22 +0000

    recommitted as sensitive info included

--- lib/sms.rb
+++ lib/sms.rb
@@ -4,11 +4,11 @@ class SMS
   attr_reader :sms, :time
 
   def initialize
-    account_sid = "AC1d19000821ebfa41b7ffd1caafabf172"
-    auth_token = "14fcc2dae325f29f0ee7d9919cb640ac"
+    account_sid = ""
+    auth_token = ""
     @client = Twilio::REST::Client.new account_sid, auth_token
-    @recipient = "+447960919231"
-    @sender = "+447453258482"
+    @recipient = ""
+    @sender = ""
     time = Time.now + 1 * 60 * 60
     @text = "Thanks! Your order was placed and will be delivered before #{time.strftime("%H:%M")}"
   end
