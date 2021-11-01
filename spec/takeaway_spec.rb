4121c80b6ea74bdfb29d7aec0890aeaf5a7387ae
Ben Dowsett <bendowsett@gmail.com>
2021-11-01 09:09:07 +0000

    User story 2

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,14 +1,22 @@
 require 'takeaway'
 
 describe Takeaway do 
-    subject(:takeaway) {described_class.new(menu: menu)}
+    subject(:takeaway) {described_class.new(menu: menu, order: order)}
     
     let(:menu) {double(:menu, print: printed_menu)}
     let(:printed_menu) { "Fish and Chips: £1.50"}
+    let(:order) {double(:order) }
+
+    let(:dishes) { {fish: 1, chips: 1 }}
 
     it 'shows menu with dishes & prices' do 
         expect(takeaway.show_menu).to eq(printed_menu)
     end 
+
+    it "can order some number of available dishes" do
+        expect(order).to receive(:add).twice
+        takeaway.place_order(dishes)
+end 
 end 
 
 
