84a623522a225edfb95575d276d7390a576c7959
Corinne <corinne.boesch@gmail.com>
2021-11-08 10:34:29 +0000

    updating twilio body messag

--- lib/customer_sms.rb
+++ lib/customer_sms.rb
@@ -7,16 +7,15 @@ module OrderConfirmation
   def self.send_sms
     @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]) 
 
-    @message = "Order placed at: #{@order_time}\nThank you for placing an order with SandCream! Your delivery will be with you by: #{@delivery_time}"
-
     time = Time.new
-    @order_time = time.strftime("%H:%M")
-    @delivery_time = (time + 25 * 60).strftime("%H:%M")
+    order_time = time.strftime("%H:%M")
+    delivery_time = (time + 25 * 60).strftime("%H:%M")
 
     @client.messages.create(
       from: ENV["TWILIO_NUM"], 
       to: ENV["TO_MY_NUMBER"], 
-      body: @message 
+      body: "Order placed at: #{order_time}\nThank you for placing an order" +
+        " with SandCream! Your delivery will be with you by: #{delivery_time}"
     )
   end
 end
