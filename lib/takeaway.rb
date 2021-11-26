0d02d5dc7699fe79258491e1a6835bf371ac4c31
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-11-26 21:50:43 +0000

    can add items to order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,18 @@
 class Takeaway
 
+  attr_reader :order
+
   def initialize(menu = Menu.new)
     @menu = menu
+    @order = []
   end
 
   def show_menu
     @menu.show
+  end
+
+  def add_to_order(item)
+    @menu.dishes.each { |food| order.push(food) if food.name == item }
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -16,4 +16,14 @@ describe Takeaway do
     expect { actual_takeaway.show_menu }.to output("curry, £8.5\nnaan, £3\n").to_stdout
   end
 
+  it "should be able to add items to order" do
+    actual_menu = Menu.new
+    actual_menu.add(curry)
+    actual_menu.add(naan)
+    actual_takeaway = Takeaway.new(actual_menu)
+    actual_takeaway.add_to_order("curry")
+    actual_takeaway.add_to_order("naan")
+    expect(actual_takeaway.order).to match_array([curry, naan])
+  end
+
 end
