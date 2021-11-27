fdd39eebde21427b319d92054493ea1ee2ed0adf
zidehen <zoeidehen@gmail.com>
2021-11-27 12:50:34 +0000

    Initialised menu and added menu double for future menu class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,7 +1,13 @@
 
 class Takeaway
   
-  def read_menu
+  attr_reader :menu
 
+  def initialize
+    @menu = menu    
+  end
+
+  def read_menu
+    "Rice: £3.00"
   end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,11 +2,14 @@ require 'takeaway'
 
 describe Takeaway do
   subject(:takeaway) {described_class.new}
+  let(:menu) {double :menu}
   describe '#menu' do
 
     it { is_expected.to respond_to(:read_menu) }
   
-    
+    it "shows a list of dishes with prices" do
+        expect(takeaway.read_menu).to eq("Rice: £3.00")
+    end
   end
 end
 
