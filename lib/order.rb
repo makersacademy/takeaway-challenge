5df38ac4ade9f5a4b2070e171ed39b7d99122ddb
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-11-27 16:38:51 +0000

    user can now select dishes in order

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,41 @@
 require_relative "menu"
 
 class Order
+  attr_reader :selected_dishes
+
   def initialize(menu:)
     @menu_class = menu
+    @selected_dishes = {}
   end
 
   def list_dishes
     @menu_class.dishes
   end
+
+  def select_dishes
+    while true
+      puts "hit enter to continue or type 'exit' to exit."
+      input = gets.chomp.capitalize
+      break if input == "Exit"
+
+      dish, quantity = user_input
+      @selected_dishes[dish].nil? ? @selected_dishes[dish] = quantity : @selected_dishes[dish] += quantity
+    end
+  end
+
+  private
+
+  def user_input
+    puts "Dish:"
+    dish = gets.chomp.capitalize
+    raise "Dish not in menu" unless in_menu?(dish)
+    puts "Quantity:"
+    quantity = gets.to_i
+    return dish, quantity
+  end
+
+  def in_menu?(dish)
+    @menu_class.dishes.has_key?(dish)
+  end
+  
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,6 +3,7 @@ require 'order'
 describe Order do
   before do
     @menu_class = double("MenuClass")
+    allow(@menu_class).to receive(:dishes).and_return({ "Fries" => 3.0,"Coca Cola" => 1.5 })
     @order = Order.new(menu: @menu_class)
   end
   
@@ -13,8 +14,42 @@ describe Order do
     end
 
     it "#list_dishes will diplay the menu's available dishes and prices" do
-      allow(@menu_class).to receive(:dishes).and_return({ "Fries" => 3.0,"Coca Cola" => 1.5 })
       expect(@order.list_dishes).to eq({ "Fries" => 3.0,"Coca Cola" => 1.5 })
+    end
+  end
+
+  context "Selecting available dishes" do
+    # Stubbing stdout to not have tests write lines into the terminal
+    before do
+      allow($stdout).to receive(:write).and_return(nil)
+    end
+
+    it "Responds to #select_dishes" do
+      expect(@order).to respond_to(:select_dishes)
+    end
+
+    it "When user types exit loop ends" do
+      allow(@order).to receive(:gets).and_return('exit')
+      expect(@order.select_dishes).to eq nil
+    end
+
+    it "Raises an error if dish typed by user not in menu" do
+      allow(@order).to receive(:gets).and_return("", "Burger", "2'", "exit")
+      expect { @order.select_dishes }.to raise_error "Dish not in menu"
+    end
+
+    it "Adds dish and correct quantity to @selected_dishes hash" do
+      allow(@order).to receive(:gets).and_return("", "Fries", "2'", "exit")
+      @order.select_dishes
+      expect(@order.selected_dishes).to eq({ "Fries" => 2 })
+    end
+
+    it "If dish already selected previously by user it updates the value" do
+      allow(@order).to receive(:gets).and_return("", "Fries", "2", "exit")
+      @order.select_dishes 
+      allow(@order).to receive(:gets).and_return("", "Fries", "3", "exit")
+      @order.select_dishes 
+      expect(@order.selected_dishes).to eq({ "Fries" => 5 })
     end
   end
 
