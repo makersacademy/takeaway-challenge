476dcf349752cba194cdff113c9bdf8cce38f62a
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-27 14:52:20 +0000

    Write test for adding items to order

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,11 +1,13 @@
+require 'order'
 
 describe Order do
   subject { described_class.new }
-  let(:dish) { double :dish }
+  let(:item) { double :item }
 
   describe '#add' do
     it 'adds items to order' do
-      
+      subject.add(item)
+      expect(subject.items.include(item)).to be_truthy
     end
   end
 end
