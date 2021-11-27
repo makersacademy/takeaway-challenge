e91bc9d50eb9663955db8f7c8657be1249af375a
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-27 15:36:01 +0000

    Refactor price test out of show context

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,25 +3,23 @@ require 'menu'
 describe Menu do
   let(:dishes) { double :dishes }
   subject { described_class.new }
+  dish = { "Fried Chicken": 8}
   
   describe '#initialize' do
     it 'has a list of dishes' do
       expect(subject.dishes).to be_an_instance_of(Array)
     end
-  end
-
-  describe '#show' do
-    dish = { "Fried Chicken": 8}
-
-    it 'shows a formatted list of dishes' do
-      subject.dishes << dish
-      expect { subject.show }.to output("Fried Chicken - £8\n").to_stdout
-    end
 
     specify 'dishes have a price' do
       subject.dishes << dish
-      p subject.show.last
-      expect(subject.show.last["Fried Chicken"]).to be(8)
+      expect(subject.dishes.last[:"Fried Chicken"]).to eq(8)
+    end
+  end
+
+  describe '#show' do
+    it 'shows a formatted list of dishes' do
+      subject.dishes << dish
+      expect { subject.show }.to output("Fried Chicken - £8\n").to_stdout
     end
   end
 end
