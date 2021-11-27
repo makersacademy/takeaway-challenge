f644e891d4ef87a4ba13f981d986cc22545f8d5f
riky moroni <riky5.moroni@gmail.com>
2021-11-27 19:12:14 +0000

    add print_available_dishes method and test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,19 @@
 class Menu
-  attr_accessor :dishes
+  attr_reader :dishes
+
   def initialize
     @dishes = [
-      { dish_name: "mushroom risotto", price: 8 }, { dish_name: "pepperoni pizza", price: 4 }
+      { dish_name: "Mushroom Risotto", price: 8 }, 
+      { dish_name: "Pepperoni Pizza", price: 4 },
+      { dish_name: "Cheeseburger", price: 5 },
+      { dish_name: "Prawn Noodles", price: 8 },
+      { dish_name: "Chicken Tikka", price: 6 }
     ]
+  end
+
+  def print_available_dishes
+    @dishes.each do |dish|
+      puts "#{dish[:dish_name]}: £#{dish[:price]}"
+    end
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,13 +4,26 @@ describe Menu do
   let(:menu) { Menu.new }
 
   describe "#initialize" do
-    context "returns a list of dishes with prices" do
-      it "returns the dish name of the first dish" do
-      expect(menu.dishes[0][:dish_name]).to eq "mushroom risotto"
+    context "creates a list of dishes with prices" do
+      it "checks the dish name of the first dish" do
+        expect(menu.dishes[0][:dish_name]).to eq "Mushroom Risotto"
       end
-      it "returns the price of the second dish" do
-      expect(menu.dishes[1][:price]).to eq 4
+      it "checks the price of the second dish" do
+        expect(menu.dishes[1][:price]).to eq 4
       end
+    end
+  end
+
+  describe "#print_available_dishes" do
+    it "returns a list of dishes with price" do
+      expect { menu.print_available_dishes }.to output(<<-output 
+Mushroom Risotto: £8
+Pepperoni Pizza: £4
+Cheeseburger: £5
+Prawn Noodles: £8
+Chicken Tikka: £6
+      output
+      ).to_stdout
     end
   end
 end
