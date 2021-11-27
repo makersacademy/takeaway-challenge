ad2f997d091447698862386b7aa2b1366c4e0d7a
Jessica Gordon <ketchesj@gmail.com>
2021-11-27 15:04:41 +0000

    adds argument to initialize to allow user to choose which menu they would like to view

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,39 @@
 class Menu
   attr_reader :dishes
 
-  def initialize
-    @dishes = {
-      'hamburger' => 8, 
-      'fries' => 5, 
-      'milkshake' => 5
+  @@restaurants = {
+    'Five Girls' => {
+      'hamburger' => 8.00, 
+      'cheeseburger' => 8.00,
+      'seitan burger' => 8.00,
+      'fries' => 5.00, 
+      'milkshake' => 5.00,
+      'soda' => 3.00
+    },
+
+    'Grandos' => {
+      'quarter chicken' => 4.00,
+      'half chicken' => 7.00,
+      'whole chicken' => 14.00, 
+      'fries' => 2.50,
+      'garlic bread' => 2.50,
+      'corn on the cob' => 2.50, 
+      'grilled halloumi' => 2.50
+    },
+
+    'LEONA' => {
+      'halloumi wrap' => 6.50,
+      'fish finger wrap' => 6.50,
+      'black bean box' => 6.50,
+      'chilli chicken box' => 6.50,
+      'warm salad' => 6.00,
+      'iced tea' => 3.50,
+      'matcha latte' => 3.00
     }
+  }
+
+  def initialize(restaurant)
+    return unless @@restaurants.has_key?(restaurant)
+    @dishes = @@restaurants[restaurant]
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,11 +1,23 @@
 require 'menu'
 
 describe Menu do
-  let(:menu) { Menu.new }
+  let(:menu) { Menu.new('Five Girls') }
   
   describe '#initialisation' do
-    it 'it holds information on dishes and prices upon initialisation' do
-      expect(menu.dishes).to include('hamburger' => 8, 'fries' => 5, 'milkshake' => 5)
+    it 'holds information on dishes and prices' do
+      expect(menu.dishes).to include('hamburger' => 8.00, 'fries' => 5.00, 'milkshake' => 5.00)
+    end
+
+    it 'assigns the menu selected by the user to the dishes instances variable - Grandos' do
+      alternative_menu = Menu.new('Grandos')
+
+      expect(alternative_menu.dishes).to include('quarter chicken' => 4.00, 'corn on the cob' => 2.50, 'grilled halloumi' => 2.50)
+    end
+
+    it 'assigns the menu selected by the user to the dishes instances variable - LEONA' do
+      alternative_menu = Menu.new('LEONA')
+
+      expect(alternative_menu.dishes).to include('halloumi wrap' => 6.50, 'black bean box' => 6.50, 'matcha latte' => 3.00)
     end
   end
 end
