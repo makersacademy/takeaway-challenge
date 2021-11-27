313a9b30ea20c8a999354ddd308662fed652a0fa
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-27 15:14:07 +0000

    Write test for formatting menu list

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,16 +3,25 @@ require 'menu'
 describe Menu do
   let(:dishes) { double :dishes }
   subject { described_class.new }
+  
+  describe '#initialize' do
+    it 'has a list of dishes' do
+      expect(subject.dishes).to be_an_instance_of(Array)
+    end
+  end
 
   describe '#show' do
-    it 'shows a list of dishes' do
-      expect(subject.show).to eq(subject.dishes)
+    let(:dish) { double :dish, :fried_chicken => 8 }
+
+    it 'shows a formatted list of dishes' do
+      expect(subject.show).to eq("Fried Chicken - £8")
+      subject.dishes << dish
     end
 
     specify 'dishes have a price' do
-      dish = double("Fried Chicken", :price => 8)
       subject.dishes << dish
-      expect(subject.show.last.price).to be(8)
+      p subject.show.last
+      expect(subject.show.last.fried_chicken).to be(8)
     end
   end
 end
