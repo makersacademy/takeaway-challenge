fdb083047ff13b3d34fe9ff25e83a8ec3a791f20
Sabrina <sabrinalord@msn.com>
2021-11-27 17:02:17 +0000

    takeaway can show the menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,7 +1,17 @@
 class Takeaway
 
 attr_reader :menu
-    def initialize 
-      @menu = []
+    def initialize
+      @menu = {
+        "Eggs Benedict": 8.50, 
+         "Full English": 9, 
+         "Waffles": 6
+        }
     end 
+
+    def print_menu
+      @menu.each do |item, price|
+      puts "#{item}: £#{price}" 
+      end 
+    end
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,7 +1,14 @@
 require 'takeaway'
 
 describe Takeaway do
+
     it 'stores a menu' do
-     expect(subject.menu).not_to be_nil
+     expect(subject.menu).not_to be_empty
     end
+
+  describe '#show_menu' do
+    it 'shows the menu items' do
+    expect { subject.print_menu }.to output.to_stdout
+    end 
+  end
 end 
