62c6253aa6aea43d96db4e2b61ab5f777dccb152
Sarah <dousse.sarah@gmail.com>
2021-11-28 17:46:27 +0000

    adding auth token for twilio && use dotenv

--- lib/twilio_sms.rb
+++ lib/twilio_sms.rb
@@ -1,18 +1,22 @@
 # Download the helper library from https://www.twilio.com/docs/ruby/install
-require 'rubygems'
 require 'twilio-ruby'
 
+# Class Twilio_SMS
+
+# def initialize
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = ENV['TWILIO_ACCOUNT_SID']
-auth_token = ENV['TWILIO_AUTH_TOKEN']
-@client = Twilio::REST::Client.new(account_sid, auth_token)
+client = Twilio::REST::Client.new(
+  ENV['TWILIO_ACCOUNT_SID'],
+  ENV['TWILIO_AUTH_TOKEN']
+)
+# end
 
-message = @client.messages
-  .create(
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15017122661',
-     to: '+15558675310'
-   )
+client.messages.create(
+  from: "[YOUR TWILIO NUMBER]",
+  to: "[YOUR CELL PHONE NUMBER]",
+  body: "You just sent an SMS from Ruby!"
+)
 
-puts message.sid
+# puts message.sid
+# end
