bae599a2d2091e77cf8c9045af00c371995bfe03
Christopher <dagswebb@gmail.com>
2021-11-28 23:41:22 +0000

    removed number

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -6,7 +6,7 @@ auth_token = '7ee9c4f33eb81eebb4a9d5ca708d0992'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+447782338793' # Your Twilio number
-to = '+447708915623' # Your mobile phone number
+to = '+' # Your mobile phone number
 
 client.messages.create(
 from: from,
