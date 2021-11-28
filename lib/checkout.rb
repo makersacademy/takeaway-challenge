e35bd28c8479dc5a92cbd623a141fdf712ec2264
Sarah <dousse.sarah@gmail.com>
2021-11-28 13:07:34 +0000

    checkout.rb: test calculate_total

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -3,31 +3,14 @@ require 'menu'
 class Checkout
   attr_reader :total, :basket, :receipt_list
 
-  def basket(basket = @basket)
+  def view_basket(basket = @basket)
     @basket = basket
   end
-
-  def view_basket
-    @basket
+  
+  def calculate_total(receipt_list = @receipt_list)
+    @receipt_list = receipt_list
+    @total = @receipt_list.inject(0) { |sum, price| sum += price }
+    # send sms
+    #@total
   end
-#   def calculate_total(receipt_list = @receipt_list)
-#     @total = receipt_list.inject(0) { |sum, price| sum += price }
-#     # send sms
-#     @total
-#   end
-
-#   def view_basket(basket = @basket )
-#     print @basket
-#     # puts "Your order:" 
-#     # @basket.each do | dish_price |
-#     #   dish_price.each do | dish, price |
-#     #     puts "#{dish} : £#{price}"
-#     #   end
-#     # end
-#     # puts "your total is £#{@total}"
-#   end
-
-# #   def place_order_sms
-#       # "Thank you! Your order was placed and will be delivered before (order time + 1 hour))"
-# #   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,27 +4,39 @@ require 'menu'
 describe Checkout do
    
   describe "test view_basket" do
-    let!(:checkout) { Checkout.new } 
+    let!(:checkout) { Checkout.new }
+    # view basket needs a fake basket to be tested
     let!(:basket1) { double :basket1 => ["KFC", 6] }
-    let!(:basket3) { double :basket1 => [["KFC", 6], ["Bibimba_chicken", 10], ["Rice", 3]] }
+    let!(:basket3) { double :basket3 => [["KFC", 6], ["Bibimba_chicken", 10], ["Rice", 3]] }
 
     context "with 1 item in the basket" do
-      before do
-        allow(checkout).to receive(:basket1).and_return(["KFC", 6])
-      end
-
       it "can show the 1 item" do
         expect(checkout.view_basket).to be(@basket1)
       end
     end
    
     context "with 3 items in the basket" do
-      before do
-        allow(checkout).to receive(:basket3).and_return([["KFC", 6], ["Bibimba_chicken", 10], ["Rice", 3]])
-      end
-
       it "can show the 3 items" do
         expect(checkout.view_basket).to be(@basket3)
+      end
+    end
+  end
+
+  describe "test calculate_total" do
+    let!(:checkout) { Checkout.new } 
+    # calculate total needs a fake receipt to be tested
+    let!(:receipt_list1) { [6] }
+    let!(:receipt_list3) { [6, 10, 3] }
+
+    context "with 1 item in the basket" do
+      it "return the item price" do
+        expect(checkout.calculate_total(receipt_list1)).to eq 6
+      end
+    end
+   
+    context "with 3 items in the basket" do
+      it "calculated the sum of the 3 items' prices" do
+        expect(checkout.calculate_total(receipt_list3)).to eq 19
       end
     end
   end
