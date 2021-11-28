0a65d47a71875875d1cfdb4d9befc03c1fb8cc4b
Sarah <dousse.sarah@gmail.com>
2021-11-28 18:21:11 +0000

    set up SMS template to be sent with Time.now && fill in template for twilio_sms.rb

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,7 @@
-require 'menu'
+require_relative 'menu'
 
 class Checkout
-  attr_reader :total, :basket, :receipt_list
+  attr_reader :total, :basket, :receipt_list, :sms
 
   def initialize(basket = @basket, receipt_list = @receipt_list )
     @basket = basket
@@ -16,13 +16,13 @@ class Checkout
     @total = @receipt_list.inject(0) { |sum, price| sum += price }
   end
 
-  def send_sms
-  
+  def confirm_sms
+    @sms = "Thank you ! your order was place and will be delivered at #{(Time.now + 1*60*60).strftime("%k:%M %p")} "
   end
 
   # def place_order
   #   print @total
   #   print view_basket
-  #   send_sms
+  #   confirm_sms
   # end
 end
--- lib/twilio_sms.rb
+++ lib/twilio_sms.rb
@@ -1,22 +1,24 @@
 # Download the helper library from https://www.twilio.com/docs/ruby/install
+require 'checkout'
 require 'twilio-ruby'
+require 'dotenv'
+Dotenv.load
 
 # Class Twilio_SMS
 
-# def initialize
+def initialize
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-client = Twilio::REST::Client.new(
-  ENV['TWILIO_ACCOUNT_SID'],
-  ENV['TWILIO_AUTH_TOKEN']
-)
-# end
+  client = Twilio::REST::Client.new(
+    ENV['TWILIO_ACCOUNT_SID'],
+    ENV['TWILIO_AUTH_TOKEN']
+  )
+end
 
-client.messages.create(
-  from: "[YOUR TWILIO NUMBER]",
-  to: "[YOUR CELL PHONE NUMBER]",
-  body: "You just sent an SMS from Ruby!"
-)
-
-# puts message.sid
-# end
+def send_sms
+  client.messages.create(
+    from: "[MY TWILIO NUMBER]",
+    to: "[MY PHONE NUMBER]",
+    body: @sms
+  )
+end
