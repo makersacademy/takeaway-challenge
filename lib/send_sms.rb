69907b08ddf9f5fd534b68e036bd34d3cd0c6a75
Christopher <dagswebb@gmail.com>
2021-11-29 00:06:39 +0000

    Added message to body of send_sms.rb

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -2,14 +2,14 @@
 require 'twilio-ruby'
 
 account_sid = 'ACeb57b6f70b6a75bc1ac05158bdd48d9c'
-auth_token = '7ee9c4f33eb81eebb4a9d5ca708d0992'
+auth_token = '6b657378df120dc9651efafc0372c846'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+447782338793' # Your Twilio number
-to = '+' # Your mobile phone number
+to = '+44' # Your mobile phone number
 
 client.messages.create(
 from: from,
 to: to,
-body: "Hey friend!"
+body: "Thank you! Your order was placed and will be delivered before #{Time.now + 1*60*60}!"
 )
