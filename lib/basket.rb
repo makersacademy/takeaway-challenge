9dea9f5160f5f534b759c25f8fc74ce68d5a5659
Inimesh <ijmeshoulam@gmail.com>
2021-11-29 08:18:30 +0000

    add functionality: add_to_total() and deduct_from_total() to Basket

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,5 +1,5 @@
 class Basket
-  attr_reader :in_basket
+  attr_reader :in_basket, :total
   def initialize
     @in_basket = []
     @total = 0.0
@@ -7,10 +7,12 @@ class Basket
 
   def add_to_basket(dish)
     @in_basket << dish
+    add_to_total(dish.price)
   end
 
   def remove_from_basket(dish)
     @in_basket.delete(dish)
+    deduct_from_total(dish.price)
   end
 
   def view_basket()
@@ -28,10 +30,10 @@ class Basket
 
   private
   def add_to_total(amount)
-    # @total += amount
+    @total += amount
   end
 
   def deduct_from_total(amount)
-    # @total -= amount
+    @total -= amount
   end
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -2,20 +2,30 @@ require 'basket'
 require 'dish'
 
 describe Basket do
-  let(:dish2) { instance_double(Dish, :name => "chips", :price => 2.50) }
   let(:dish1) { instance_double(Dish, :name => "burger", :price => 4.00) }
+  let(:dish2) { instance_double(Dish, :name => "chips", :price => 2.50) }
   let(:dish3) { instance_double(Dish, :name => "pepsi", :price => 1.00) }
 
   describe '#add_to_basket' do
     it 'can add dish items to @in_basket' do
       expect { subject.add_to_basket(dish1) }.to change { subject.in_basket.length }.by(1)
     end
+
+    it 'adds the dish price to running total' do
+      expect {subject.add_to_basket(dish1) }.to change { subject.total }.by(4.00)
+    end
   end
 
   describe '#remove_from_basket' do
-    it 'can add remove items from @in_basket' do
+    before do
       subject.add_to_basket(dish1)
+    end
+    it 'can add remove items from @in_basket' do
       expect { subject.remove_from_basket(dish1) }.to change { subject.in_basket.length }.by(-1)
+    end
+
+    it 'deducts the price from the running total' do
+      expect {subject.remove_from_basket(dish1) }.to change { subject.total }.by(-4.00)
     end
   end
 end
