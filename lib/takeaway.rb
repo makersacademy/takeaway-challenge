ef41cadd041be1ef9dac67b68a0de388777f904b
Fadaka <master_kunle14@hotmail.co.uk>
2021-11-30 12:22:36 +0000

    pushing up SRP form

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,18 +1,26 @@
-class Takeaway
+class Customerorder
 
   def initialize
     @order_selection = []
-
+    @total_price = []
+    @sum = 0
   end
 
-  def add_order(food, price)
-    @menu[order] = price
-    @order_selection << food
+  def add_order(food)
+    if @view_menu.include?(food) == false
+      raise "the order is not in the menu"
+    end
+    @order_selection.push(food)
+  end
+
+  def order_selected
+    @order_selection
   end
 
   def total_order
-    sum = 0
-    @order_selection.each_value { |price| sum += price }
-    sum
+    @order_selection.each do |food| @total_price << @menu[food]
+    end
+    @total_price.each { |bill| @sum += bill }
+    return @sum
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,7 +5,7 @@ describe Menu do
   let(:menu) { described_class.new }
   let(:food) { double :food }
   let(:not_food) { double :not_food }
-  let(:price) { double :price }
+  let(:price) { double :price}
 
   it 'will check that menu contains a food and price' do
     menu.add_food(food, price)
@@ -22,7 +22,7 @@ describe Menu do
   it 'will check that the total price of the bill is equivalent to the food ordered' do
     menu.add_food(food, price)
     menu.add_order(food)
-    expect(menu.total_order).to eq(price)
+    expect(menu.total_order).to match_array([price])
   end
 
 end
