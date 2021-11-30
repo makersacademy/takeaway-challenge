cb82536d54934353c284e2f325d666f24b8ee381
Fadaka <master_kunle14@hotmail.co.uk>
2021-11-30 13:05:56 +0000

    updated so classes work independently

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,20 +6,24 @@ class Customerorder
     @sum = 0
   end
 
-  def add_order(food)
-    if @menu.include?(food) == false
+  def add_order(menu,food)
+    food.downcase!
+    if menu.menu.include?(food) == false
       raise "the order is not in the menu"
     end
     @order_selection.push(food)
+    @order_selection.each do |food| @total_price << menu.menu[food]
+    end
+  
   end
 
   def order_selected
     @order_selection
   end
 
+  
   def total_order
-    @order_selection.each do |food| @total_price << @menu[food]
-    end
+    @sum = 0
     @total_price.each { |bill| @sum += bill }
     return @sum
   end
