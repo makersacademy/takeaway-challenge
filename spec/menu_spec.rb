7e07fbd7960703c2ea98c162fb53820a92f5be14
kehinde alaka <kennyalaka93@gmail.com>
2021-12-22 14:56:11 +0000

    Added spec for menu class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,10 +1,11 @@
 require 'menu'
 
 describe Menu do
-  # it 'initialize with a list of dishes' do
-  #   menu = Menu.new
+  it 'initialize with a list of dishes' do
+  menu = Menu.new
+  expect(menu.print_dishes).to be_a(Hash)
   #   list_of_dishes = [ {dish: 'rice', price: 4}, {dish: 'noodles', price: 3}, {dish: 'chicken', price: 2} ]
 
   #   expect(menu.dishes).to eq list_of_ishes
-  # end
+  end
 end
