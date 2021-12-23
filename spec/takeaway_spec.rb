f0ee9849992e7b185b366fe54cddba1e321695b7
Josh <joshlim092@gmail.com>
2021-12-23 17:40:37 +0200

    added new test to ensure hash returns an integer

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,8 +2,13 @@ require 'takeaway'
 
 describe Menu do 
   describe '#lists' do
-    it 'contains a list of dishes and prices' do
+    it 'contains a list of dishes' do
       expect(subject.list).to_not be_empty
+    end
+
+    #need to replace Sushi with a mock
+    it 'contains a hash with values' do
+      expect(subject.list['Sushi']).to be > 0
     end 
   end 
 end 
