5c58314510b293b72a476b6176f6d289373c8a99
icvotria <icvotria@gmail.com>
2021-12-23 23:44:36 +0000

    fixed order_food, tidied displays

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -12,25 +12,17 @@ class Takeaway
   end
 
   def order_food(item_number)
-    index = item_number - 1
-    @order.select(index, @menu)
-    @total += @order.order_pad[index][:cost]
+    item_number -= 1
+    @order.select(item_number, @menu)
+    @total += @order.order_pad[-1][:cost]
   end
 
   def display_menu
-    output = ""
-    @menu.each_with_index do |item, index| 
-      output << "#{index + 1}. #{item[:name]} - £#{item[:cost]}\n"
-    end
-    puts output
+    @menu.each_with_index { |item, index| puts "#{index + 1}. #{item[:name]} - £#{item[:cost]}" }
   end
 
   def display_order
-    output = ""
-    @order.order_pad.each_with_index do |item, index| 
-      output << "#{index + 1}. #{item[:name]} - £#{item[:cost]}\n"
-    end
-    output << "Total cost: £#{@total}"
-    puts output
+    @order.order_pad.each_with_index { |item, index| puts "#{index + 1}. #{item[:name]} - £#{item[:cost]}" }
+    puts "Total cost: £#{@total}"
   end
 end
