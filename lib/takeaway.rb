05629013b61a60fcb5a854d68d71d55c3ba90785
Josh <joshlim092@gmail.com>
2021-12-23 23:05:57 +0200

    added order ability to store item and price

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,10 @@
 class Menu
+  attr_accessor :order_list
+  
+  def initialize
+    @order_list = []
+  end 
+
   def list 
     list = {
       'Sushi' => 5.90,
@@ -6,5 +12,9 @@ class Menu
       'Crispy Pork Belly' => 11.90,
       'Lobster and Egg Noodles' => 40.00
     }
+  end 
+
+  def add(item)
+    @order_list.append(item, list[item])
   end 
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,14 +1,28 @@
 require 'takeaway'
 
 describe Menu do 
-  describe '#lists' do
+
+
+  describe '#list' do
     it 'contains a list of dishes' do
       expect(subject.list).to_not be_empty
     end
-
     #need to replace Sushi with a mock
-    it 'contains a hash with values' do
+    it 'contains a price with positive integers' do
       expect(subject.list['Sushi']).to be > 0
     end 
+  end
+
+  describe '#add' do
+    it { is_expected.to respond_to(:add) }
+
+    it 'adds an item to the order_list' do 
+      subject.add("Sushi")
+      expect(subject.add("Kim Chi").count).to change(@order_list).by(1) 
+      
+    end 
+    #build a test which increases the 
+    #order_list by 1 after add
+
   end 
 end 
