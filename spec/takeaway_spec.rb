188ba6e44a441103b5a82fcee65035e7ea9d9195
Josh <joshlim092@gmail.com>
2021-12-23 23:13:45 +0200

    updated rspec to add new items - key and value counts as 2 values

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -16,10 +16,8 @@ describe Menu do
   describe '#add' do
     it { is_expected.to respond_to(:add) }
 
-    it 'adds an item to the order_list' do 
-      subject.add("Sushi")
-      expect(subject.add("Kim Chi").count).to change(@order_list).by(1) 
-      
+    it 'adds an item to the order_list' do
+      expect{subject.add("Kim Chi")}.to change{subject.order_list.count}.by(2) 
     end 
     #build a test which increases the 
     #order_list by 1 after add
