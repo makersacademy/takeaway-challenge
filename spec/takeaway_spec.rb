8f3bc41ec5860524d0f6e781080045be7df9a6c5
Haydn Stokes <haydnstokes92@gmail.com>
2021-12-31 12:03:13 +0000

    made takeaway spec and class, initialized menu

---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,12 @@
+require 'takeaway'
+
+describe Takeaway do
+  subject(:takeaway) { described_class.new(menu: menu) }
+
+  let(:menu) { double(:menu, print: printed_menu) }
+  let(:printed_menu) { "Beef noodles: £4.00" }
+
+  it 'shows menu with dishes and there prices' do
+    expect(takeaway.print_menu).to eq(printed_menu)
+  end
+end
