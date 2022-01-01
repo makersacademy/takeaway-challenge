01db7c01643c84861097a844ac9c57a8f0310447
mikitaro-27 <matt.diak@gmail.com>
2022-01-01 23:10:21 +0000

    create send_message.rb using twilio, create .env template

---
+++ lib/send_sms.rb
@@ -0,0 +1,17 @@
+require 'dotenv'
+require 'twilio-ruby'
+
+Dotenv.load
+
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+from = ENV['TWILIO_NUMBER'] # Your Twilio number
+to = ENV['MY_NUMBER'] # Your mobile phone number
+
+client.messages.create(
+from: from,
+to: to,
+body: "Thank you. Your order was placed and will arrive before TIME"
+)
--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -18,3 +18,9 @@ class Takeaway
     Order.new
   end
 end
+
+# As a customer
+# So that I am reassured that my order will be delivered on time
+# I would like to receive a text such as 
+# "Thank you! Your order was placed and will be delivered before 18:52"
+#  after I have ordered
