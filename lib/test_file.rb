29fb5aacf2e34bf296c931292cf5a18f4aed3004
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-01-02 16:51:17 +0000

    Refactor Twilio Tests and rubocop

--- lib/test_file.rb
+++ lib/test_file.rb
@@ -8,18 +8,19 @@ SAMPLE_MENU = {
   Tiramisu: 4.5,
   Gelato: 4,
   Coffee: 2.5
-}
+}.freeze
 
+client = Twilio::REST::Client.new(account_sid, auth_token),
 account_sid = "YOUR ACCOUNT_SID HERE"
 auth_token = "YOUR AUTH_TOKEN HERE"
 
 text_args = {
-  client: Twilio::REST::Client.new(account_sid, auth_token),
+
   from: "YOUR TWILIO NUMBER HERE",
   to: "YOUR MONILE NUMBER HERE"
 }
 
-text = SMS.new(text_args[:client], account_sid, auth_token, text_args[:from], text_args[:to])
+text = SMS.new(client, account_sid, auth_token, text_args[:from], text_args[:to])
 menu = Menu.new(SAMPLE_MENU)
 take_away = Takeaway.new(menu, text)
 
--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -3,7 +3,8 @@ require 'sms'
 describe SMS do
   subject(:sms) { described_class.new(client, account_sid, auth_token, from, to) }
 
-  let(:client) { double(:client) }
+  let(:client) { double(:client, messages: messages) }
+  let(:messages) { double(:messages, create: nil) }
   let(:account_sid) { double(:account_sid) }
   let(:auth_token) { double(:auth_token) }
   let(:from) { double(:from) }
@@ -11,9 +12,14 @@ describe SMS do
 
   describe '#send_text' do
     it 'sends a text with ETA of delivery' do
+      text_args = {
+        from: from,
+        to: to,
+        body: "Thank you! Your order was placed and will be delivered before #{SMS::DELIVERY_TIME}."
+      }
 
-      allow(sms).to receive(:send_text).and_return("Thank you! Your order was placed and will be delivered before #{SMS::DELIVERY_TIME}.")
-      expect(sms.send_text).to eq("Thank you! Your order was placed and will be delivered before #{SMS::DELIVERY_TIME}.")
+      sms.send_text
+      expect(messages).to have_received(:create).with(text_args)
     end
   end
 end
