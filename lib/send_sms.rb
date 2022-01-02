5077850d317e7b066507cbf5a52129a2ad647b38
mikitaro-27 <matt.diak@gmail.com>
2022-01-02 16:46:43 +0000

    make send_confirmation method in send_sms.rb

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,17 +1,22 @@
 require 'dotenv'
 require 'twilio-ruby'
+require './lib/hour'
 
 Dotenv.load
 
-account_sid = ENV['TWILIO_ACCOUNT_SID']
-auth_token = ENV['TWILIO_AUTH_TOKEN']
-client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = ENV['TWILIO_NUMBER'] # Your Twilio number
-to = ENV['MY_NUMBER'] # Your mobile phone number
+def send_confirmation
+  account_sid = ENV['TWILIO_ACCOUNT_SID']
+  auth_token = ENV['TWILIO_AUTH_TOKEN']
+  client = Twilio::REST::Client.new(account_sid, auth_token)
 
-client.messages.create(
-from: from,
-to: to,
-body: "Thank you. Your order was placed and will arrive before TIME"
-)
+  from = ENV['TWILIO_NUMBER'] # Your Twilio number
+  to = ENV['MY_NUMBER'] # Your mobile phone number
+  time = Hour.new.one_ahead
+
+  client.messages.create(
+  from: from,
+  to: to,
+  body: "Thank you. Your order was placed and will arrive before #{time}"
+  )
+end
