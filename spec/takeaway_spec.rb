10d5fe9a00fe3362bb5afb775030079d9d6f8acc
ElliottZaki <spudnick99@hotmail.com>
2022-01-02 17:15:21 +0000

    sStep2 - lib and spec folder

---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,19 @@
+require 'takeaway'
+
+describe Takeaway do
+    subject (:takeaway) {Takeaway.new}
+
+
+    #describe '#menu' do
+     #   it { is_expected.to respond_to(:menu) }
+            
+    #end
+
+
+
+
+
+
+
+
+end
