d64a85ed34e704e9dbe9daa55bc1a6fc72de7493
Jason Humbert <jasonhumbert@outlook.com>
2022-01-02 18:58:47 +0000

    Add menu feature to view available dishes

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,20 @@
 
 class Takeaway
+  
+  def initialize
+    @menu = [
+      { name: "Beef burger", price: 14 },
+      { name: "Chicken burger", price: 12 },
+      { name: "Hot dog", price: 11 },
+      { name: "Fries", price: 4 },
+      { name: "Soft drink", price: 3 },
+      { name: "Milkshake", price: 5 }
+    ]
+  end
+
+  def menu
+    @menu.each do |item|
+      puts "#{item[:name]}: #{item[:price]}"
+    end
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,7 +3,29 @@ require 'takeaway'
 describe Takeaway do
   subject (:takeaway) { Takeaway.new }
 
+  let (:menu) do
+    <<~MENU
+      Beef burger: 14
+      Chicken burger: 12
+      Hot dog: 11
+      Fries: 4
+      Soft drink: 3
+      Milkshake: 5
+    MENU
+  end
+
   it 'is a takeaway' do
     expect(takeaway).to be_a Takeaway
+  end
+
+  describe '#menu' do
+    it { is_expected.to respond_to(:menu) }
+
+    it 'shows a menu' do
+      allow(takeaway).to receive(:menu) { "Menu" }
+      expect(takeaway.menu).to eq "Menu"
+    end
+
+    specify { expect { takeaway.menu }.to output(menu).to_stdout }
   end
 end
