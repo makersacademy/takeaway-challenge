3d28ee4dd82d161c1fa1be276a295421eb4de628
ElliottZaki <spudnick99@hotmail.com>
2022-01-02 18:07:46 +0000

    Step 3 - List menu items

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,5 +1,20 @@
 class Takeaway
 
+    def initialize
+        @menu = {
+            :Beer => 4.00,
+            :Cola => 2.50,
+            :Tap_water => 0.50,
+            :Burger => 6.00,
+            :Veggie_burger => 5.00,
+            :Chips => 2.00,
+            :Salad => 3.50
+        }
+    end
+
+    def view_menu
+        puts @menu
+    end 
 
 
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,18 +2,20 @@ require 'takeaway'
 
 describe Takeaway do
     subject (:takeaway) {Takeaway.new}
+    #let (:menu) {double :menu}
 
+    describe '#view_menu' do
+        it { is_expected.to respond_to(:view_menu) }
 
-    #describe '#menu' do
-     #   it { is_expected.to respond_to(:menu) }
+        it 'lists all purchasable items' do
+            #arrange
+            #assert
+            expect(takeaway.view_menu).to eq(@menu)
+        end 
+
+    
             
-    #end
-
-
-
-
-
-
+    end
 
 
 end
