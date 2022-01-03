2981b66548a46890fae16fd6982b63a31b3aedb4
Hannah Desmond <desmond.hannah@googlemail.com>
2022-01-03 13:05:23 +0000

    Add #add_item method

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,2 +1,12 @@
 class Basket 
+  attr_reader :basket
+
+  def initialize
+    @basket = {}
+  end
+  
+  def add_item(item, price)
+    @basket[item] = price
+  end
+
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,4 +1,13 @@
 require 'basket'
 
 describe Basket do
+  let(:bibimbap) { double :bibimbap }
+  # allow bibimbap to 
+  it 'initializes with an empty basket to store items and prices' do
+    expect(subject.basket).to eq({})
+  end
+  it 'adds an item and price to the basket' do
+    subject.add_item(bibimbap, 10.50)
+    expect(subject.basket).to eq({bibimbap=>10.50}) 
+  end
 end
