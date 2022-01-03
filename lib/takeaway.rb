1e11c82f940bd4f2daadcc73d85d02792485fbcb
robin ucar <ucarmehmet.ch@gmail.com>
2022-01-03 21:35:36 +0000

    print_menu test and print_menu method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,13 @@
 class Takeaway
-    attr_reader :menu
+    attr_reader :menu_items
     def initialize
-        @menu
+        @menu_items = []
     end
+    def print_menu
+        menu = []
+        @menu_items.each { |dish|
+          menu << "#{dish.name} : #{dish.price}"
+        }
+        menu.join(' / ')
+      end
 end
