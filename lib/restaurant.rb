a8de5db44b824c652f185974b719c99a72004841
Hannah Desmond <desmond.hannah@googlemail.com>
2022-01-03 12:41:55 +0000

    Add menu attr_reader and #see_menu methods

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,2 +1,15 @@
 class Restaurant
+  attr_reader :menu
+
+  def initialize
+    @menu = {
+      'bibimbap'=>10.50,
+      'bulgogi'=>10.00,
+      'naengmyeon'=>8.00
+    }
+  end
+
+  def see_menu
+    @menu.each { |k, v| @menu[k] = "£%.2f" % v.to_s } 
+  end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,5 +1,18 @@
 require 'restaurant'
 
 describe Restaurant do
-  
+  context "menu" do
+    it 'has a menu with prices' do
+      expect(subject.menu).to eq({
+        'bibimbap'=>10.50,
+        'bulgogi'=>10.00,
+        'naengmyeon'=>8.00
+      })
+    end
+    it 'prints the menu for the customer' do
+      expect(subject.see_menu).to eq( 'bibimbap'=>"£10.50",
+        'bulgogi'=>"£10.00",
+        'naengmyeon'=>"£8.00")
+    end
+  end
 end
