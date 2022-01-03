992a10b514cffb1d1b48db1e9bf686db7916340f
Haydn Stokes <haydnstokes92@gmail.com>
2022-01-03 16:06:28 +0000

    updated takeaway spec and class and added order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,20 @@
 class Takeaway
-  def initialize(menu:)
+  def initialize(menu:, order: nil)
     @menu = menu
+    @order = order
   end
 
   def print_menu
     menu.print
   end
 
+  def place_order(dishes)
+    dishes.each do |dish, amount|
+      order.add(dish, amount)
+    end
+  end
+
   private
 
-  attr_reader :menu
+  attr_reader :menu, :order
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,12 +1,20 @@
 require 'takeaway'
 
 describe Takeaway do
-  subject(:takeaway) { described_class.new(menu: menu) }
+  subject(:takeaway) { described_class.new(menu: menu, order: order) }
 
   let(:menu) { double(:menu, print: printed_menu) }
+  let(:order) { double(:order) }
   let(:printed_menu) { "Noodles: £4.00" }
+
+  let(:dishes) { {noodles: 2, squid: 1} }
 
   it 'shows menu with dishes and there prices' do
     expect(takeaway.print_menu).to eq(printed_menu)
+  end
+
+  it "can order a number of several available dishes" do
+    expect(order).to receive(:add).twice #found via rspec mocks
+    takeaway.place_order(dishes)
   end
 end
