2e73e7cb87b944a0adb92a74923923f12f0ba98c
robin ucar <ucarmehmet.ch@gmail.com>
2022-01-03 20:26:07 +0000

    test dish class with name and price -red-

---
+++ spec/dish_spec.rb
@@ -0,0 +1,6 @@
+describe Dish do
+    subject(:dish) {described_class.new(dish)}
+    let(:dish) {Dish.new('Hamburger', 5)}
+    
+    it {is_expected.to have_attributes(name: 'Hamburger', price: 5)}
+end
