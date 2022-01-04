1ac56bcbc637024d50a4ee36de3f2dd719934247
chisambwe <c.m.kapihya@gmail.com>
2022-01-04 23:03:12 +0000

    SMS working with env

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,19 +1,21 @@
-require 'twilio-ruby'
-
-
 class SMS
+  require 'dotenv/load'
+  require "twilio-ruby"
+
     DELIVERY_TIME_ESTIMATE = 3600
 
+    
+
   def initialize
-    @account_sid = ENV["TWILIO_ACCOUNT_SID"]
-    @auth_token = ENV["TWILIO_AUTH_TOKEN"]
+    @account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @auth_token = ENV['TWILIO_AUTH_TOKEN']
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
   end
 
   def send_sms
     @client.messages.create(
-        from: ENV['TWILIO_NUMBER'],
-        to: ENV["MY_PHONE_NUMBER"],
+        to: ENV['DELIVERY_NUMBER'],
+        from: ENV['TWILIO_PHONE_NUMBER'],
         body: "Thank you! Your order was placed and will be delivered before #{delivery_time}"
     )
   end
