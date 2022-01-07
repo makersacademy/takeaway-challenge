ec7068f1a054592fdda87afddde25912ae5dcd84
Alice Gray <alicegray33@gmail.com>
2022-01-07 16:15:47 +0000

    Twilio credentials loaded from git-ignored file

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,33 +1,36 @@
 require 'twilio-ruby'
+require_relative 'twilio_credentials'
 
 class Restaurant
 
   WAIT_TIME = 3600 # in seconds
 
+  def initialize
+    @account_sid = TWILIO_ACCOUNT_SID
+    @auth_token = TWILIO_AUTH_TOKEN
+    @from = FROM
+  end
+
   def place(order, number = nil)
     @order = order
     deadline = wait.strftime('%H:%M')
     message = "Thank you! Your order was placed and will be delivered before #{deadline}"
-    return message if number == nil
+    return message if number == nil?
     send_sms(message, number)
   end
 
-  def send_sms(message, number)
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-    from = ENV['FROM']
-    
+  private
+
+  def send_sms(message, number)    
     # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new @account_sid, @auth_token
     @client.messages.create(
-      from: from,
+      from: @from,
       to: number,
       body: message
     )
     message
   end
-
-  private
 
   def wait
     wait_time = WAIT_TIME + Time.now.to_i
