aaf8df4b1ac1e7842b64d6b243b100e15a16eccf
Alice Gray <alicegray33@gmail.com>
2022-01-07 09:49:45 +0000

    shows a list of dishes with prices

--- lib/list.rb
+++ lib/list.rb
@@ -1,14 +1,22 @@
 class List
 
   def initialize
-    @menu = []
+    @list = []
   end
 
+  attr_reader :list
+
   def add_dish(dish)
-    @menu << dish
+    @list << dish
   end
 
   def view
+    @menu = []
+    @list.each do |dish|
+      name = dish.name
+      price = dish.price
+      @menu << { name: name, price: price }
+    end
     @menu
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -4,10 +4,22 @@ describe List do
   let(:dish_one) { double(:dish, name: "Pizza", price: 5) }
   let(:dish_two) { double(:dish, name: "Burger", price: 3) }
   let(:dish_three) { double(:dish, name: "Salad", price: 2) }
+  let(:menu) { [{ name: "Pizza", price: 5 }, { name: "Burger", price: 3 }, { name: "Salad", price: 2 }] }
 
   it 'can add a dish to the list' do
     subject.add_dish(dish_one)
-    expect(subject.view).to include(dish_one) 
+    expect(subject.list).to include(dish_one) 
+  end
+
+  context 'has three dishes available' do
+    before do
+      subject.add_dish(dish_one)
+      subject.add_dish(dish_two)
+      subject.add_dish(dish_three)
+    end
+    it 'shows a list of dishes with prices' do
+      expect(subject.view).to eq menu
+    end
   end
 
 end
