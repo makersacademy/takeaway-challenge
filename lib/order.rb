53e5152007ef3d41560f1a62673d62a39be87cee
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2022-01-07 15:07:18 +0000

    Wrote RSpec and code for Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,24 @@
 class Order
 
-  attr_reader :order
+  attr_reader :dishes, :total_cost, :total_prep_time
 
   def initialize
-    @order = []
+    @dishes = []
+    @total_cost = 0
+    @total_prep_time = 0
   end
 
   def add_dish(dish)
+    @dishes << dish.name
+    @total_cost += dish.price
+    @total_prep_time += dish.prep_time
   end
 
   def remove_dish(dish)
+    raise "There are no dishes in your order." if @dishes.length == 0
+    @dishes.delete_at(@dishes.index(dish.name))
+    @total_cost -= dish.price
+    @total_prep_time -= dish.prep_time
   end
+
 end
