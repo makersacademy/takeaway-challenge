d52a3cb29d8f023c516fca6344d247cde61d2f52
Sam Pickering <sam.p@tuta.io>
2022-01-07 11:48:13 +0000

    add new_order, order_total to restaurant

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,5 @@
+require_relative 'order'
+
 class Restaurant
   def initialize(menu)
     @menu = menu
@@ -5,5 +7,20 @@ class Restaurant
 
   def menu
     @menu.clone
+  end
+
+  def new_order
+    Order.new
+  end
+
+  def order_total(order)
+    raise "Invalid order - #{items_not_on_menu(order)} are not on the menu" unless items_not_on_menu(order).empty?
+    order.items.map { |item| @menu[item] }.sum
+  end
+
+  private
+
+  def items_not_on_menu(order)
+    order.items.uniq - @menu.keys
   end
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,8 +1,12 @@
 require 'restaurant'
+require 'order'
 
 describe Restaurant do
   before :all do
     @menu = { taco: 5, pasta: 10, pizza: 15, burger: 3, golden_taco: 999 }
+  end
+  before :each do
+    @order = double('order', items: [:taco, :pasta, :pizza, :pasta, :pasta])
   end
   subject { described_class.new(@menu) }
 
@@ -20,6 +24,26 @@ describe Restaurant do
       menu_copy = subject.menu
       menu_copy[:taco] = 0
       expect(subject.menu[:taco]).not_to eq(0)
+    end
+  end
+
+  describe '#new_order' do
+    it 'returns an instance of Order' do
+      expect(subject.new_order.class).to eq(Order)
+    end
+  end
+
+  describe '#order_total' do
+    it 'returns 0 for an empty order' do
+      empty_order = double('empty_order', items: [])
+      expect(subject.order_total(empty_order)).to eq(0)
+    end
+    it 'returns the total cost of an order' do
+      expect(subject.order_total(@order)).to eq(50)
+    end
+    it 'raises an error if an item is not on the menu' do
+      invalid_order = double('invalid_order', items: [:taco, :glue, :scissors, :boa_constrictor])
+      expect { subject.order_total(invalid_order) }.to raise_error RuntimeError
     end
   end
 end
