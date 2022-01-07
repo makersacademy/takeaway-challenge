6f700338a8d822e010f868cafa9c468b7249f6da
Alice Gray <alicegray33@gmail.com>
2022-01-07 09:23:41 +0000

    List can add a dish to the list

--- lib/list.rb
+++ lib/list.rb
@@ -1,3 +1,14 @@
 class List
 
+  def initialize
+    @menu = []
+  end
+
+  def add_dish(dish)
+    @menu << dish
+  end
+
+  def view
+    @menu
+  end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,5 +1,13 @@
 require 'list'
 
 describe List do
+  let(:dish_one) { double(:dish, name: "Pizza", price: 5) }
+  let(:dish_two) { double(:dish, name: "Burger", price: 3) }
+  let(:dish_three) { double(:dish, name: "Salad", price: 2) }
+
+  it 'can add a dish to the list' do
+    subject.add_dish(dish_one)
+    expect(subject.view).to include(dish_one) 
+  end
 
 end
