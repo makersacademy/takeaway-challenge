11abc7d92fecf32bd7e14e7d1b71a660be5119f8
Alice Gray <alicegray33@gmail.com>
2022-01-07 15:07:09 +0000

    Mocks sending a text messagee via Twilio

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,22 +1,30 @@
 require 'twilio-ruby'
-require 'twilio-credentials'
-
-# put your own credentials here
-account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-api_key_sid = 'zzzzzzzzzzzzzzzzzzzzzz'
-api_key_secret = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-
-# set up a client to talk to the Twilio REST API using an API Key
-@client = Twilio::REST::Client.new api_key_sid, api_key_secret, account_sid
 
 class Restaurant
 
   WAIT_TIME = 3600 # in seconds
 
-  def place(order)
+  def place(order, number = nil)
     @order = order
     deadline = wait.strftime('%H:%M')
     message = "Thank you! Your order was placed and will be delivered before #{deadline}"
+    return message if number == nil
+    send_sms(message, number)
+  end
+
+  def send_sms(message, number)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    from = ENV['FROM']
+    
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client.messages.create(
+      from: from,
+      to: number,
+      body: message
+    )
+    message
   end
 
   private
@@ -25,4 +33,5 @@ class Restaurant
     wait_time = WAIT_TIME + Time.now.to_i
     wait = Time.at(wait_time)
   end
+
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -7,11 +7,14 @@ describe Restaurant do
     wait_time = time_now + Restaurant::WAIT_TIME
     ready = Time.at(wait_time)
   end
+  let(:fake_twilio) { double(:twilio).as_null_object }
+  let(:mobile_number) { '07000 123 123' }
   let(:message) { "Thank you! Your order was placed and will be delivered before #{wait.strftime('%H:%M')}" }
 
   it 'receives order and responds with a message' do
     allow(Time).to receive(:now).and_return(time_now)
-    expect(subject.place(my_order)).to eq message
+    allow(Twilio::REST::Client).to receive(:new).and_return(fake_twilio)
+    expect(subject.place(my_order, mobile_number)).to eq message
   end
 
 end
