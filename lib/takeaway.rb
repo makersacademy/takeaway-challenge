6a078641170b3ab505402d05aa750aba2ca628de
Aadam Ali <ali.aadam@outlook.com>
2022-01-07 15:36:11 +0000

    added initialize method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,10 @@
 class Takeaway
+  attr_reader :menu, :basket
 
+  def initialize
+    @menu = { "Chicken Curry" => 4, "Lamb Curry" => 4, "Vegetable Curry" => 3, "Keema" => 4, 
+              "Mixed Grill" => 9, "Samosa" => 2, "Chicken Kebab" => 3, "Lamb Kebab" => 4 }
+    @basket = {}
+    @total = 0
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,14 @@
 require "takeaway"
 
 describe Takeaway do
-  
+  describe "#initialize" do
+    it "has a menu with a length of 8" do
+      expect(subject.menu).to eq({ "Chicken Curry" => 4, "Lamb Curry" => 4, "Vegetable Curry" => 3, "Keema" => 4, 
+                                   "Mixed Grill" => 9, "Samosa" => 2, "Chicken Kebab" => 3, "Lamb Kebab" => 4 })
+    end
+
+    it "initially has an empty basket" do
+      expect(subject.basket).to eq({})
+    end
+  end
 end
