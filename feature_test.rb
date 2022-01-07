ed884366981e0c0020bb88094090c79bc10d137e
Sam Pickering <sam.p@tuta.io>
2022-01-07 10:41:13 +0000

    behaviour and structure planning

---
+++ feature_test.rb
@@ -0,0 +1,12 @@
+require 'restaurant'
+
+scenario 
+restaurant = Restaurant.new({ taco: 5, pasta: 10, pizza: 15, burger: 3, golden_taco: 999 })
+order = restaurant.new_order
+order.add(:taco, :pasta, :pasta)
+order.remove(:pasta)
+total = restaurant.order_total(order)
+delivery_time = restaurant.delivery_time
+verify_total = order.items.map { |item| restaurant.menu[item.name] }.sum
+
+restaurant.place_order(order)
