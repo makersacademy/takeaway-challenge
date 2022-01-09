eff54185ccdea4a179211534546290d6bedd147d
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-09 11:29:35 +0100

    variable renaming

--- lib/menu.rb
+++ lib/menu.rb
@@ -6,11 +6,11 @@ class Menu
     @menu = {}
   end
 
-  def add_dish(name, price)
-    @menu[name] = price
+  def add_dish(dish_name, dish_price)
+    @menu[dish_name] = dish_price
   end
 
-  def dish_in_menu?(name)
-    return !@menu[name].nil?
+  def dish_in_menu?(dish_name)
+    return !@menu[dish_name].nil?
   end
 end
