cecc45ddfad7ceb5bc2e93a63268454d366562e2
rizinaa99 <rizinakhatun@icloud.com>
2022-01-10 08:40:24 +0000

    completed 3 user story and updated takeaway challenge

--- spec/resturant_spec.rb
+++ spec/resturant_spec.rb
@@ -7,4 +7,16 @@ describe Resturant do
   list = resturant.dish
   expect(resturant.dish).to eq list
   end 
+  it 'customers choice of order' do
+    subject = Resturant.new 
+    customer = Customer.new 
+    subject.customer_choice(customer.select_dish("Chips"))
+    expect(subject.choice).to eq [["Chips"]]
+  end 
+  it 'should give the right total' do 
+    subject = Resturant.new 
+    customer = Customer.new 
+    subject.customer_choice(customer.select_dish("Chips"))
+    expect(subject.total).to eq (5)
+  end 
 end 
