50c7b96220c61fe6390eb87e76c423a389e9a7a2
benfielding74 <billy.fadden@live.com>
2022-01-28 16:52:21 +0000

    Completed user story to verify order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,17 +1,29 @@
 class Takeaway
 
-  attr_reader :menu
+  attr_reader :menu, :order
 
   def initialize
     @menu = { "Sweet & Sour Chicken Balls" => 6.30, "Salt & Pepper Chicken" => 6.30, "Chicken Fried Rice" => 6.80, "Prawn Crackers" => 2.80 }
+    @order = {}
   end
 
   def view_menu
     @menu.each { |item, price| puts "#{item}: £#{price}" } 
   end
 
-  def place_order(order)
-  order 
+  def place_order(*order)
+    order.each { |item| @order[item] = Float(@menu.values_at(item).join) } 
+    @order
   end
 
+  def view_order
+    total = 0.0
+    order.each { |item, price| 
+    puts "#{item}: £#{price}"
+    total += price
+    }
+    puts "The order total is £#{total}"
+    total
+   end
+  
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,7 +3,7 @@ require 'takeaway'
 describe Takeaway do
 
   let(:menu) { double :menu }
-  let(:order) { {"Salt & Pepper Chicken" => 6.30, "Prawn Crackers" => 2.80 } }
+  # let(:order) { "Chicken Fried Rice", "Salt & Pepper Chicken" }
 
   it 'has a menu object' do
     expect(subject.menu).to eq ({ "Sweet & Sour Chicken Balls" => 6.30, "Salt & Pepper Chicken" => 6.30, "Chicken Fried Rice" => 6.80, "Prawn Crackers" => 2.80 })
@@ -14,7 +14,12 @@ describe Takeaway do
   end
 
   it 'can order some number of dishes' do
-    expect(subject.place_order(order)).to eq (order)
+    expect(subject.place_order("Chicken Fried Rice", "Salt & Pepper Chicken")).to eq ({"Chicken Fried Rice" => 6.80, "Salt & Pepper Chicken" => 6.30})
+  end
+
+  it 'returns a total value of order' do
+    subject.place_order("Chicken Fried Rice", "Salt & Pepper Chicken")
+    expect(subject.view_order).to eq (13.10)
   end
 
 end
