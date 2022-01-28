7039b1f638ebd77e9f9455db9d9f437930baf62e
KevinByrne <kevbee79@gmail.com>
2022-01-28 12:10:40 +0000

    items added to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,22 @@
 class Menu
 
-  MENU_LIST = {}
+  MENU_LIST = {
+    Pork_gyros:         7.80,
+    Chicken_gyros:      7.80,
+    Spinach_pie:        3.50,
+    Pita_bread:         1.00,
+    Greek_salad:        5.60,
+    Tzatziki:           3.50,
+    Feta_sauce:         1.00,
+}
 
   
 
-  def menu_dishes
-  MENU_LIST
+  def menu_dishes # defined method
+  MENU_LIST # calls items from CONSTANT above
   end
 
 end
+
+order = Menu.new
+p order.menu_dishes
