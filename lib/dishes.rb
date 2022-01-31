40e5d781789fed7cc9a5444ae6a68cb843268f91
Abdul Mobarak <abdulll@live.co.uk>
2022-01-31 06:56:56 +0000

    list of food items tested and implemented

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -1,6 +1,9 @@
 class Dishes
 
   def list
+    [
+      {name: 'sushi', price: 20.49}
+    ]
   end
-  
+
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -2,8 +2,18 @@ require 'dishes'
 
 describe Dishes do
   
+  let :list_of_dishes do
+    [
+      {name: 'sushi', price: 20.49}
+    ]
+  end
+
   it 'check that dishes has a hash of dishes with prices' do
       expect(subject).to respond_to(:list)
+  end
+
+  it 'has a list of dishes and prices' do
+    expect(subject.list).to eq list_of_dishes
   end
 
 end
