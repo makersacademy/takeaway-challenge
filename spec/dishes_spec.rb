0030792074886957ceaacdfe2b537d2c1878c76e
Abdul Mobarak <abdulll@live.co.uk>
2022-01-31 07:32:53 +0000

    changed list to hash convention

--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -3,21 +3,14 @@ require 'dishes'
 describe Dishes do
   
   let :list_of_dishes do
-    [
-      { name: 'Tuna', price: 7.99 }, 
-      { name: 'Spanish Mackerel', price: 6.54 }, 
-      { name: 'Sweet Shrimp', price: 12.49 }, 
-      { name: 'Saltwater Eel', price: 15.54 }, 
-      { name: 'Round Clam', price: 15.44 }, 
-      { name: 'Albacore White Tuna', price: 3.25 }, 
-      { name: 'Skipjack Tuna', price: 5.44 }, 
-      { name: 'Tiger Shrimp', price: 2.99 }, 
-      { name: 'Butterfish', price: 28.22 }, 
-      { name: 'Yellow Tail', price: 11.88 }, 
-      { name: 'Yellowtail Belly', price: 1.99 }, 
-      { name: 'Halibut', price: 6.44 }, 
-      { name: 'Surf Clam', price: 8.44 }
-    ]
+    { 'Tuna' => 7.99,
+      'Spanish Mackerel' =>  6.54,
+      'Sweet Shrimp' =>  12.49,
+      'Saltwater Eel' =>  15.54,
+      'Round Clam' =>  15.44,
+      'Albacore White Tuna' =>  3.25,
+      'Skipjack Tuna' =>  5.44,
+      'Yellow Tail' =>  11.88 }
   end
 
   it 'check that dishes has a hash of dishes with prices' do
@@ -26,6 +19,10 @@ describe Dishes do
 
   it 'has a list of dishes and prices' do
     expect(subject.list).to eq list_of_dishes
+  end
+
+  it 'can read the menu and check prices for items on menu' do
+    expect(subject.list[5][1]).to eq { 1 }
   end
 
 end
