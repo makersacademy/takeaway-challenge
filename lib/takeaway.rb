6d4e96f3631a948d5a5fc6be47fc7298b0f7a312
Peter O <harmless_goose@icloud.com>
2022-02-05 18:56:56 +0000

    Added ability to check total is correct

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -4,13 +4,35 @@ class Takeaway
   attr_reader :order
 
   def initialize
-    @menu = [:chips => 3, :burger => 4, :pastie => 5]
+    @menu = {chips: 3, burger: 4, pastie: 5}
     @order = []
+    @total = 0
   end
 
-  def add_to_order(item)
-    @order.push(item.to_sym)
+  def add_to_order(item, quantity)
+    quantity.times {
+      @order.push(item)
+      @total = @total + @menu[item.to_sym]
+    }
+
+    puts "Your order is: #{@order}"
+    puts "Your total is: £#{@total}"
   end
 
+  def is_total_correct?(total)
+    count = 0
+
+    @order.each {
+      |item|
+      count = count + @menu[item.to_sym]
+    }
+
+    if total == count 
+      true
+    else
+      false
+    end
+
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,20 +2,28 @@ require_relative '../lib/takeaway.rb'
 
 describe Takeaway do
 
-  let(:menu){[:chips => 3, :burger => 4, :pastie => 5]}
-  
+  let(:menu){Hash[chips: 3, burger: 4, pastie: 5]}
+  let(:order){["chips", "chips", "pastie", "pastie"]}
+
   it 'has a list of dishes' do
     expect(subject.menu).to eq menu
   end
 
   describe '#add_to_order' do
-    let(:order){[:chips, :chips, :pastie, :pastie]}
-
     it 'can select items from the menu' do
-      2.times {subject.add_to_order('chips')}
-      2.times {subject.add_to_order('pastie')}
+      subject.add_to_order("chips", 2)
+      subject.add_to_order("pastie", 2)
 
       expect(subject.order).to eq order
+    end
+  end
+
+  describe '#is_total_correct?' do
+    it 'can check given total is correct' do
+      subject.add_to_order("chips", 2)
+      subject.add_to_order("pastie", 2)
+
+      expect(subject.is_total_correct?(16)).to be true
     end
   end
 
