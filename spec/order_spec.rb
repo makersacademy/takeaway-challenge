78b35d0ae0fadab2cfcea0bf7c4a94c705c1c73c
Rakshak Shetty <rakshak_81@hotmail.com>
2022-02-06 21:48:10 +0000

    total

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -13,33 +13,3 @@ describe Order do
   end
 end
 
-
-# it "Lets you add a item to your choices" do
-#     subject.add_item("Chicken",7)
-#     subject.choose_dish("Chicken")
-#     expect(subject.my_choices).to include("Chicken")
-#   end
-
-#   it "Won't let you add an item not on the menu" do
-#     subject.choose_dish("Chicken")
-#     expect(subject.my_choices).not_to include("Chicken")
-#   end
-
-#   it "Can calculate the total of a number of dishes" do
-#     subject.choose_dish("Bryani")
-#   end
-
-#   it "Allows you to complete your order" do
-#     subject.choose_dish("Bryani")
-#     expect(subject.make_order).to eq("Bryani,7")
-
-#   end
-
-#   it "Sends a message to Twilio to send a text" do
-#     texter = double()
-#     allow(texter).to receive(:send_text).and_return(true)
-#     expect(subject).to respond_to(:send_text)
-#     expect(subject.send_text(texter)).to eq true
-#   end
-
-# end
