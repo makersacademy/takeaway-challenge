39ddd971ca99be13015d234f718fcc91953cb5bd
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-02-07 00:06:34 +0000

    unit test 2

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,12 +1,22 @@
 require_relative './menu'
 
 class Takeaway
+  attr_reader :order
 
   def initialize(menu = Menu.new)
     @menu = menu
+    @order = []
   end
 
   def show_menu
     @menu.dishes
   end
+
+  def add_to_order(dishes, quantity = 1)
+    quantity.times {
+      @order.push(dishes)
+    }
+    @order
+  end
+    
 end
