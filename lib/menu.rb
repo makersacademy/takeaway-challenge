6e2b3fc966dabb02d95bf26c093620e2a252ef31
Valentina <maggio.vale@gmail.com>
2022-03-05 12:02:13 +0000

    Add test for list_dishes
    - check the Menu method list_dishes shows a list of dishes and prices
    - add list_dishes method in Menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,9 @@
 class Menu
-
+  def list_dishes
+    menu = [
+      { pizza: 10 },
+      { pasta: 12 },
+      { burger: 13 },
+    ]
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,17 @@
 require 'menu'
 
 describe Menu do
+  subject(:menu) { described_class.new }
+
   it 'can create a Menu' do
-    expect(Menu.new).to be_a_kind_of(Menu)
+    expect(menu).to be_a_kind_of(Menu)
   end
 
+  it 'shows a list of dishes with prices' do
+    expect(menu.list_dishes).to eq [
+      { pizza: 10 },
+      { pasta: 12 },
+      { burger: 13 },
+    ]
+  end
 end
