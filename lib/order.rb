981caf981bef1d87da49c3ccd8690d77c4872ca9
Valentina <maggio.vale@gmail.com>
2022-03-05 12:58:25 +0000

    Create simple test to check add_list method
    - create add-list method to collect dishes
    - test for a simple scenario without using Menu class

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,14 @@
+require_relative 'menu'
+
 class Order
 
   attr_reader :order
 
   def initialize
     @order = []
+  end
+
+  def add_dish(pasta)
+    @order << pasta
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,13 +1,22 @@
 require 'order'
 
 describe Order do
-  subject(:order) { described_class.new }
+  subject(:my_order) { described_class.new }
 
   it 'can create an Order' do
-    expect(order).to be_a_kind_of(Order)
+    expect(my_order).to be_a_kind_of(Order)
   end
 
-  it 'creates an empty array called order' do
-    expect(order.order).to eq []
+  it 'has a default empty array to store the order' do
+    expect(my_order.order).to eq []
+  end
+
+  it 'adds pasta dish to order array' do
+    expect(my_order.add_dish('pasta')).to eq ['pasta']
+  end
+
+  it 'stores a pasta dish' do
+    my_order.add_dish('pasta')
+    expect(my_order.order).to include('pasta')
   end
 end
