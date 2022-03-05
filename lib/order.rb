2d91532cf9b2d22ad94fb287a746a324782b5324
Adam Woodcock <adam.woodcock98@gmail.com>
2022-03-05 11:40:48 +0000

    Created tests for attributes, implemented code to satisfy tests

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,29 @@
 class Order
 
+  attr_accessor :basket
+  
+  MENU = [
+    {:cod => 8.0},
+    {:haddock => 8.50},
+    {:rock_salmon => 11.25},
+    {:jumbo_battered_sausage => 8.0},
+    {:mini_battered_sausage => 6.50},
+    {:cod_fish_cake => 7.50},
+    {:salmon_fish_cake => 9.50},
+    {:pukka_pie => 7.75},
+    {:large_chips => 4.50},
+    {:regular_chips => 3.25},
+    {:curry_sauce => 0.95},
+    {:gravy => 0.95},
+    {:bread_roll => 1.50}
+  ]
+
+  def initialize
+    @basket = []
+  end
+
+  def view_menu
+    MENU
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,6 +2,12 @@ require 'order'
 
 describe Order do
   
-  
+  it 'should contain an empty order by default' do
+    expect(subject.basket).to eq []
+  end
+
+  it 'should contain a menu' do
+    expect(Order::MENU).to eq(subject.view_menu)
+  end
 
 end
