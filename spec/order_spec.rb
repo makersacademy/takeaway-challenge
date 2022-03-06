255ad070fae954bcbf875638825704917d0d5a83
Nicola Cortese <nicolacortese92@gmail.com>
2022-03-06 18:22:22 +0000

    mocked the tests, WIP on the confirmation text test

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,26 +1,27 @@
 require 'order'
-require 'menu'
-require 'dish'
 
 describe Order do
-  # let(:menu) {double(:menu)}
-  # let(:dish) {double(:dish)}
+  let(:menu) {double(:menu)}
   let(:subject) {Order.new(Menu.new)}
 
   it 'selects a dish and adds it to the order list' do
-    # allow(menu).to receive(:list).and_return([dish,2.5])
-    dish = Dish.new("katsu",2.5)
-    menu = Menu.new()
-    menu.add_dish(dish)
+    allow(menu).to receive(:list).and_return("katsu" => ["katsu",2.5])
     order = Order.new(menu)
     order.select("katsu",2)
     expect(order.list).to eq [["katsu",2.5],["katsu",2.5]]
   end
 
-  xit 'confirms the order and returns the delivery time' do
-    # WIP on how to double the Time class (is this even necessary?)
-    # Time = double(:time)
-    # allow(Time).to receive(:now).and_return("13:19")
-    expect(subject.confirm). to eq "Thank you! Your order was placed and will be delivered before "
+  it 'returns the total of an order' do
+    allow(menu).to receive(:list).and_return("katsu" => ["katsu",2.5])
+    order = Order.new(menu)
+    order.select("katsu",2)
+    expect(order.total).to eq 5
   end
+
+  # xit 'confirms the order and returns the delivery time' do
+  #   # WIP on how to double the Time class (is this even necessary?)
+  #   # Time = double(:time)
+  #   # allow(Time).to receive(:now).and_return("13:19")
+  #   expect(subject.confirm). to eq "Thank you! Your order was placed and will be delivered before "
+  # end
 end
