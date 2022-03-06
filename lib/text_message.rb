b431d1a317ddca1426aad8ccad1d3d2c84059669
Valentina <maggio.vale@gmail.com>
2022-03-06 14:59:49 +0000

    Update confirmatino message to send as text
    - add private method to calculate delivery time

--- lib/text_message.rb
+++ lib/text_message.rb
@@ -12,4 +12,18 @@ class TextMessage
     @client = client
   end
 
+  def send_message
+    @client.messages
+      .create(
+        body: "Thank you! Your order was placed and will be delivered before #{delivery_time}",
+        from: ENV['TWILIO_NUMBER'],
+        to: ENV['CUSTOMER_NUMBER']
+      )
+  end
+
+  private
+  
+  def delivery_time
+    time = (Time.now + 1*60*60).strftime("%R")
+  end
 end
