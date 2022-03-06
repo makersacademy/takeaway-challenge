a5686ab1d073999655b884b79ad6f7ff64ac54d3
Rob Oman <rob.oman@icloud.com>
2022-03-06 20:47:41 +0000

    Add functionality to Dish class to store name and price (as int) upon initialisation

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,2 +1,18 @@
 class Dish
+  attr_reader :name
+
+  def initialize(name, price)
+    @name = name
+    @price = price_as_integer(price)
+  end
+
+  def price
+    @price / 100.0
+  end
+
+  private
+
+  def price_as_integer(price)
+    price.round(2) * 100
+  end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,4 +1,11 @@
 require 'dish'
 
 describe Dish do
+  describe "#price" do
+    subject { described_class.new("Large Chips", 4.39) }
+    it "returns the price in pounds" do
+      expect(subject.price).to eq(4.39)
+    end
+  end
+
 end
