56a7d912264cf6f394c0b84aafa5a4895d794ab7
Rob Oman <rob.oman@icloud.com>
2022-03-06 22:36:37 +0000

    Add add/delete/total methods and tests for Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,2 +1,22 @@
 class Order
+
+  def initialize(menu)
+    @order = []
+    @menu = menu
+  end
+
+  def add(dish_number)
+    @order << @menu[dish_number - 1]
+  end
+
+  def delete(dish_number)
+    fail "Please enter the dish number" unless dish_number.is_a? Integer
+    fail "This dish does not exist" unless @menu[dish_number - 1] && dish_number -1 >= 0
+    @order.delete_at(dish_number - 1)
+  end
+
+  def total
+    @order.sum { |dish| dish.price }
+  end
+
 end
