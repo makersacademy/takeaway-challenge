1ab4c08ab4fca8996b03804c16b08c67d99b22dc
Paul <pkilgarriff@gmail.com>
2022-03-06 23:39:04 +0000

    Update Order class
    - add select_items method
    - test for select_items with a valid item on the menu

--- lib/order.rb
+++ lib/order.rb
@@ -5,30 +5,36 @@ class Order
     @current_items = []
   end
 
-  def add_to_order(item_number)
-    @current_items << item_number
+  def select_items(menu)
+    loop do
+      puts 'Choose a dish to add to your order'
+      selection = gets.chomp
+      break if selection.empty?
+
+      dish = menu.find_by_name(selection)
+      @current_items << dish unless dish.nil?
+    end
   end
-
-  def remove_from_order(item_name)
-    selection = @current_items.find { |item| item.name == item_number }
-    raise 'No such item in order' if selection.nil?
-
-    @current_items.delete(selection)
-  end
-
+  
   def list_order
     @current_items.each do |item|
       puts "#{item.name} - #{item.price}"
     end
+    puts "Total: #{order_total_price}"
   end
-
+  
   def order_total_price
-    @current_items.map { |item| item.price }.sum
+    "\£#{@current_items.map { |item| item.price }.sum}"
   end
 
-  def confirm_order
-    list_order
-    puts order_total_price
-    
+  def add_to_order(item)
+    @current_items << item
+  end
+
+  def remove_from_order(item_name)
+    selection = @current_items.find { |item| item.name == item_name }
+    raise 'No such item in order' if selection.nil?
+
+    @current_items.delete(selection)
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,6 +3,7 @@ require 'order'
 describe Order do
   subject(:order) { described_class.new }
   let(:dish) { double(:dish_double, :name => 'Pizza', :price => 16, :available? => true) }
+  let(:menu) { double(:menu_double) }
 
   describe '#add_to_order' do
     it "adds an item to the user's order" do
@@ -25,21 +26,34 @@ describe Order do
     end
   end
 
-  before do
-    order.add_to_order(dish)
-    order.add_to_order(dish)
-    order.add_to_order(dish)
-  end
-
-  describe '#order_total_price' do
-    it 'returns the sum of the item prices' do
-      expect(order.order_total_price).to eq 48
+  context 'order contains three pizzas' do
+    before do
+      order.add_to_order(dish)
+      order.add_to_order(dish)
+      order.add_to_order(dish)
     end
-  end
 
-  describe 'list_order' do
-    it 'prints all items in the order' do
-      expect { order.list_order }.to output("Pizza - 16\nPizza - 16\nPizza - 16\n").to_stdout
+    describe '#order_total_price' do
+      it 'returns the sum of the item prices' do
+        expect(order.order_total_price).to eq '£48'
+      end
+    end
+
+    describe '#list_order' do
+      it 'prints all items in the order' do
+        expect { order.list_order }.to output("Pizza - 16\nPizza - 16\nPizza - 16\nTotal: £48\n").to_stdout
+      end
+    end
+
+    describe '#select_items' do
+      context 'when given a valid item' do
+        it 'adds the dish to the order' do
+          allow(order).to receive(:gets).and_return("Pizza\n", "\n")
+          allow(menu).to receive(:find_by_name).with('Pizza').and_return(dish)
+          order.select_items(menu)
+          expect { order.list_order }.to output("Pizza - 16\nPizza - 16\nPizza - 16\nPizza - 16\nTotal: £64\n").to_stdout
+        end
+      end
     end
   end
 end
