53b6d919a81d2e72b26ee3ba4502be6358e03e7b
Anusha Nesargi <anushanesargi@gmail.com>
2022-03-06 20:47:36 +0000

    added bill to entry_point

--- lib/entry_point.rb
+++ lib/entry_point.rb
@@ -1,22 +1,26 @@
 require_relative 'menu'
 require_relative 'order'
+require_relative 'bill'
 
-def take_order(menu_instance, order_instance)
-    @menu = menu_instance
-    @order = order_instance
+def take_order
+    menu = Menu.new
+    order = Order.new(menu)
     puts "Kindly provide your ten digit mobile number:"
     @ten_digit_mobile_number = $stdin.gets.chomp
-    @order.send_the_delivery_notification(customer_phone_number)
+    order.send_the_delivery_notification(customer_phone_number)
     puts "Enter checkout to end the order input"
     loop do
-      @menu.display_menu_card
+      menu.display_menu_card
       puts "Enter dish number"
-      @dish = $stdin.gets.chomp
-      break if @dish == "checkout"
+      dish = $stdin.gets.chomp
+      break if dish == "checkout"
       puts "Enter the quantity"
-      @quantity = $stdin.gets.chomp.to_i
-      @order.add_order(@dish, @quantity)
+      quantity = $stdin.gets.chomp.to_i
+      order.add_order(dish, quantity)
     end
+
+    bill = Bill.new(order, menu)
+    bill.generate_bill
 
   end
 
--- spec/entry_point_spec.rb
+++ spec/entry_point_spec.rb
@@ -1,22 +1,30 @@
 require 'entry_point'
 
  describe ".take_order" do
+
+    def with_captured_stdout
+        original_stdout = $stdout
+        $stdout = StringIO.new
+        yield
+        $stdout.string   
+    ensure
+        $stdout = original_stdout
+    end
+
     it "should ask for the order" do
       allow($stdin).to receive(:gets).and_return("7404785572", "1", "2", "2", "1", "checkout")
-      menu = Menu.new
-      order = Order.new(menu)
-      take_order(menu, order)
-      expect(order.order_list).to eq "Ealing + Chips"=>1, "Vegan Burger"=>2
+      str = with_captured_stdout { take_order }
+      expect(str).to include "total = 45"
     end
 
   end
 
-  describe ".customer_phone_number" do
-    it "should return cusotmers phone number" do
-      allow($stdin).to receive(:gets).and_return( "7404785572", "1", "2", "2", "1", "checkout")
-      menu = Menu.new
-      order = Order.new(menu)
-      take_order(menu, order)
-      expect(customer_phone_number).to eq "+447404785572"
-    end
-  end
+#   describe ".customer_phone_number" do
+#     it "should return cusotmers phone number" do
+#       allow($stdin).to receive(:gets).and_return( "7404785572", "1", "2", "2", "1", "checkout")
+#       menu = Menu.new
+#       order = Order.new(menu)
+#       take_order(menu, order)
+#       expect(customer_phone_number).to eq "+447404785572"
+#     end
+#   end
