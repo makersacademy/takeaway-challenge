ae4740edab2c5a58ceace6562e011762494cce66
Paul <pkilgarriff@gmail.com>
2022-03-06 23:41:41 +0000

    Update Takeaway class
    Pass in TwilioManager, Order and Menu as parameters for initialize
    - Add interactive_menu
    - print_options
    - process
    - quit_program
    - current_order
    Mock stdin for testing interactive menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,18 @@
 class Takeaway
-  def initialize(twilio_manager)
+  def initialize(twilio_manager, menu, order_template)
     @twilio_manager = twilio_manager
+    @menu = menu
+    @order_template = order_template
+    @active_order = nil
+  end
+
+  def interactive_menu
+    loop do
+      print_options
+      selection = gets.chomp
+      p selection
+      process(selection)
+    end
   end
 
   def send_confirmation_text
@@ -9,6 +21,39 @@ class Takeaway
   
   private
   
+  def print_options
+    puts [
+      '1. View the Menu',
+      '2. Select Items to Order',
+      '3. View Current Order',
+      '9. Exit program'
+    ]
+  end
+
+  def process(selection)
+    case selection
+    when '1'
+      @menu.list_available_dishes
+    when '2'
+      current_order.select_items(@menu)
+    when '3'
+      current_order.list_order
+    when '9'
+      quit_program
+    else
+      puts "Invalid selection"
+    end
+  end
+
+  def quit_program
+    puts "Exiting program..."
+    exit
+  end
+
+  def current_order
+    @active_order = @active_order.nil? ? @order_template.dup : @active_order
+  end
+
   def confirmation_string
     "Thank you! Your order was placed and will be delivered before #{time_in_an_hour}"
   end
