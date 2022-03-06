306ec95cb84788482f5ff2d768a5c021ae6e4f8f
Paul <pkilgarriff@gmail.com>
2022-03-06 23:27:19 +0000

    Update Menu class
    - add find_by_name method
    - remove attr_reader for @dishes
    - remove unused indexes from list methods

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,20 +1,26 @@
 class Menu
-  attr_reader :dishes
+  # attr_reader :dishes
 
   def initialize
     @dishes = []
   end
 
   def list_dishes
-    @dishes.each_with_index do |dish, idx|
-      puts "#{idx + 1}. #{dish.name} - #{dish.price}"
+    @dishes.each do |dish|
+      puts "#{dish.name} - #{dish.price}"
     end
   end
 
   def list_available_dishes
-    @dishes.each_with_index do |dish, idx|
-      puts "#{idx + 1}. #{dish.name} - #{dish.price}" if dish.available?
+    @dishes.each do |dish|
+      puts "#{dish.name} - #{dish.price}" if dish.available?
     end
+  end
+
+  def find_by_name(search_name)
+    dish = @dishes.find { |dish| dish.name == search_name }
+    puts 'No such dish' if dish.nil?
+    dish
   end
 
   def add_dish(dish)
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,13 +2,13 @@ require 'menu'
 
 describe Menu do
   subject(:menu) { described_class.new }
-  let(:dish) { instance_double(Dish, :name => 'Pizza', :price => 16) }
+  let(:dish) { instance_double('Dish', :name => 'Pizza', :price => 16) }
   let(:dish_list) { [
-    instance_double(Dish, :name => 'Pizza', :price => 16, :available? => true),
-    instance_double(Dish, :name => 'Sushi', :price => 12, :available? => true),
-    instance_double(Dish, :name => 'Fish & Chips', :price => 9, :available? => false),
-    instance_double(Dish, :name => 'Thai Green Curry', :price => 14, :available? => true),
-    instance_double(Dish, :name => 'Chicken Wings', :price => 7, :available? => true),
+    instance_double('Dish', :name => 'Pizza', :price => 16, :available? => true),
+    instance_double('Dish', :name => 'Sushi', :price => 12, :available? => true),
+    instance_double('Dish', :name => 'Fish & Chips', :price => 9, :available? => false),
+    instance_double('Dish', :name => 'Thai Green Curry', :price => 14, :available? => true),
+    instance_double('Dish', :name => 'Chicken Wings', :price => 7, :available? => true),
     ]
   }
 
@@ -22,19 +22,28 @@ describe Menu do
     context 'returns a formatted list of dishes' do
       it 'with a single dish' do        
         menu.add_dish(dish)
-        expect { menu.list_dishes }.to output("1. Pizza - 16\n").to_stdout
+        expect { menu.list_dishes }.to output("Pizza - 16\n").to_stdout
       end
 
       it 'with multiple dishes' do
         dish_list.each do |dish|
           menu.add_dish(dish)
         end
-        expect { menu.list_dishes }.to output("1. Pizza - 16\n"\
-        "2. Sushi - 12\n"\
-        "3. Fish & Chips - 9\n"\
-        "4. Thai Green Curry - 14\n"\
-        "5. Chicken Wings - 7\n").to_stdout
+        expect { menu.list_dishes }.to output("Pizza - 16\n"\
+        "Sushi - 12\n"\
+        "Fish & Chips - 9\n"\
+        "Thai Green Curry - 14\n"\
+        "Chicken Wings - 7\n").to_stdout
       end
+    end
+  end
+
+  describe '#find_by_name' do
+    it 'returns an item that matches the name given' do
+      dish_list.each do |dish|
+        menu.add_dish(dish)
+      end
+      expect(menu.find_by_name('Pizza').price).to be 16
     end
   end
 
@@ -43,10 +52,10 @@ describe Menu do
       dish_list.each do |dish|
         menu.add_dish(dish)
       end
-      expect { menu.list_available_dishes }.to output("1. Pizza - 16\n"\
-      "2. Sushi - 12\n"\
-      "4. Thai Green Curry - 14\n"\
-      "5. Chicken Wings - 7\n").to_stdout
+      expect { menu.list_available_dishes }.to output("Pizza - 16\n"\
+      "Sushi - 12\n"\
+      "Thai Green Curry - 14\n"\
+      "Chicken Wings - 7\n").to_stdout
     end
   end
 end
