c7e7bd04dc257619574620433027d889657b1662
James Mooney <jmooney9304@gmail.com>
2022-03-06 13:55:49 +0000

    Can now add dishes to a Menu and view the items and prices once added

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,1 +1,26 @@
 require 'menu'
+
+describe Menu do
+  let(:menu_1) {Menu.new}
+
+  let(:dish_1) { dish_1 = double('dish_1', name: 'Pizza', price: 8) }
+  let(:dish_2) { dish_2 = double('dish_2', name: 'Pasta', price: 9) }
+
+  it 'creates a new empty Menu array' do
+    expect(menu_1.menu).to eq []
+  end
+
+  it 'adds a dish to the menu array' do
+    menu_1.add_dish(dish_1)
+    expect(menu_1.menu).to include(dish_1)
+  end
+
+  it 'shows items and prices in the menu' do
+    menu_1.add_dish(dish_1)
+    menu_1.add_dish(dish_2)
+    expect { menu_1.view_menu }.to output("Pizza: £8\nPasta: £9\n").to_stdout
+  end
+
+
+
+end
