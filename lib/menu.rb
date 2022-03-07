8f342369c547dc6876153a397d700a84ac7f10a3
dennis <denhouston1@icloud.com>
2022-03-07 07:08:34 +0000

    added menu

---
+++ lib/menu.rb
@@ -0,0 +1,18 @@
+class Menu
+  def initialize
+    @menu = {
+      "Pizza" => 8,
+      "Burger" => 9,
+      "Pasta" => 6,
+      "Special Fried Rice" => 5.5,
+      "Kebab" => 6.5,
+      "Ice Cream" => 3.2,
+      "Chocolate Cake" => 2,
+    }
+  end
+
+  def see_menu
+    @menu.each {|dish, price| puts "#{dish} - £#{price}"}
+  end
+end
+
