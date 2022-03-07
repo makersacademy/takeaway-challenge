9034312f38c23d1629f86790da917803ad806664
Rob Oman <rob.oman@icloud.com>
2022-03-07 00:25:32 +0000

    Add functionality to Notification Class to send text message confirmation

--- lib/notification.rb
+++ lib/notification.rb
@@ -1,2 +1,26 @@
+require 'twilio-ruby'
+require 'dotenv/load'
+
 class Notification
+  def initialize(order_time)
+    @order_time = order_time
+  end
+
+  def send
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.messages.create(
+        body: "Thank you! Your order was placed and will be delivered before #{delivery_time}",
+        to: ENV['MY_PHONE_NUMBER'],
+        from: ENV['TWILIO_PHONE_NUMBER'])
+  end
+
+  private
+
+  def delivery_time
+    delivery_time = @order_time + (60*60)
+    delivery_time.strftime("%-k:%M")
+  end
 end
--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -1,4 +1,5 @@
 require 'notification'
 
 describe Notification do
+
 end
