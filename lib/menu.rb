cc1dae936646315c09c31677c1559296fa5b1e05
Michael Su <mailmsuu@gmail.com>
2022-04-01 23:09:35 +0100

    initial Menu done with passed test

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,29 @@
 class Menu
+
+  MENU_HASH = { 
+    "Fried Rice" => 6.0,
+    "Chow Mein" => 6.1,
+    "Chop Suey" => 6.2,
+    "Satay" => 6.3,
+    "Salt & Pepper" => 6.4,
+    "Sweet & Sour" => 6.5,
+    "Chips" => 1.5,
+    "Large Chips" => 2.0,
+    "Fish" => 3.0,
+    "Sausage" => 1.2,
+    "Pie" => 1.3,
+    "Gravy" => 1.0,
+    "Peas" => 0.8,
+    "Spring Rolls" => 2.8
+  }.freeze
+
+  def initialize
+    @menu = MENU_HASH
+  end
+
   def show_menu
-    puts " "
+    @menu.each_with_index do |(item, price), index|
+      puts "#{index + 1}. #{item} £#{'%.2f' % price}"
+    end
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,7 +3,7 @@ require 'menu'
 describe Menu do
   describe "#show_menu" do
     it 'should show all items in the menu with their respective prices' do
-      expect { subject.show_menu }.to output(" \n").to_stdout
+      expect { subject.show_menu }.to output("1. Fried Rice £6.00\n2. Chow Mein £6.10\n3. Chop Suey £6.20\n4. Satay £6.30\n5. Salt & Pepper £6.40\n6. Sweet & Sour £6.50\n7. Chips £1.50\n8. Large Chips £2.00\n9. Fish £3.00\n10. Sausage £1.20\n11. Pie £1.30\n12. Gravy £1.00\n13. Peas £0.80\n14. Spring Rolls £2.80\n").to_stdout
     end
   end
 end
