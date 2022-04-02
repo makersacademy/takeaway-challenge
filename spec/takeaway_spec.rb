817cb074ff39e613c1e1b651acbc71b28cd70a87
Michael Su <mailmsuu@gmail.com>
2022-04-02 15:26:53 +0100

    added double for order class used in takeaway_spec test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,12 +6,18 @@ describe Takeaway do
 
   it 'should be able to take a order and return the price' do
     allow(STDIN).to receive(:gets).and_return("1", "1", "", "yes")
-    subject.take_order
-    expect(subject.order.total_cost).to eq(6)
+    allow(order_class).to receive(:show_menu)
+    allow(order_class).to receive(:dish_name).and_return("Fried Rice")
+    allow(order_class).to receive(:add_to_order).and_return({ "Fried Rice" => 1 })
+    allow(order_class).to receive(:item_list).and_return("1x Fried Rice £6.00")
+    allow(order_class).to receive(:finish_order)
+    allow(order_class).to receive(:total_cost).and_return(6)
+    takeaway.take_order
+    expect(takeaway.order.total_cost).to eq(6)
   end
 
   it 'should send a confirmation text when order is complete' do
-    expect(subject).to receive(:send_text).with("message")
-    subject.send_text("message")
+    expect(takeaway).to receive(:send_text).with("message")
+    takeaway.send_text("message")
   end
 end
