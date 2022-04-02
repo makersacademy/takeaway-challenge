939e5344390df05bd79e71d951585f5ec96e00d9
Rupert Pople <rupertpople@gmail.com>
2022-04-02 20:16:02 +0100

    user story 3 complete

--- lib/order.rb
+++ lib/order.rb
@@ -5,16 +5,24 @@ class Order
 
     attr_accessor :order
 
-    def initialize(*item_number)
+    def initialize(menu, *item_number)
         @order = []
         @item_numbers = [*item_number]
-        @ticketcount = @item_numbers.count
-        @ticket = []
-        
-        Menu.menu.each_with_index do |item, index|
-            if index == @item_numbers[0..@ticketcount]
-                @order.push(item)
-            end
-          end
+        @menu = menu
+        @total_price = 0
+    end
+
+    def submit_order
+        @order = @item_numbers.map { |dish| @menu.menu[dish-1]}
+    end
+
+    def check_order_price
+        calculate_order_price.each { |amount| @total_price+=amount}
+        @total_price
+    end
+
+    private 
+    def calculate_order_price
+        @order.map {|dish| dish.instance_variable_get(:@price)}
     end
 end
