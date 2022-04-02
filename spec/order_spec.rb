7fd612c911d91104937adbd757915b0480a26149
Michael Su <mailmsuu@gmail.com>
2022-04-02 11:21:02 +0100

    updated tests in order_spec

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,10 +1,26 @@
 require 'order'
 
 describe Order do
+
+  describe "#add_to_order" do
+    it 'should be able to add items to an order' do
+      subject.add_to_order(1,1)
+      expect(subject.order).to eq("Fried Rice x1")
+    end
+  end
+
   describe "#place_order" do
     it 'should be able to place a order of items from the menu' do
-      subject.place_order(order)
-      expect(subject.current_orders).to include(order)
+      subject.add_to_order(1,1)
+      expect(subject.place_order).to include(subject.order)
+    end
+  end
+
+  describe "#receipt" do
+    it 'should give a receipt after a order has been placed' do
+      subject.add_to_order(1,1)
+      subject.place_order
+      expect { subject.receipt }.to output(" ").to_stdout
     end
   end
 end
