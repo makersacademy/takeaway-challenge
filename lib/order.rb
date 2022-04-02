b75c851d80f1c7bcaaf45a8be58873a136c05c70
Michael Su <mailmsuu@gmail.com>
2022-04-02 12:21:10 +0100

    passed order tests, used method stubs and doubles in test

--- lib/order.rb
+++ lib/order.rb
@@ -8,19 +8,40 @@ class Order
     @menu = menu
   end
 
-  def menu
-    @menu.menu
-  end
-
   def add_to_order(item_index,quantity)
-    @order[menu.keys[item_index]] = quantity
+    @order[menu.keys[item_index]] += quantity
   end
 
   def place_order
-
+    total_cost
+    receipt
   end
 
   def receipt
+    puts "Thank you for ordering at Su's Takeaway"
+    puts "Your order today was:"
+    item_list
+    puts "The total amount for this order is £#{'%.2f' % total_cost}"
+  end
 
+  def total_cost
+    total = 0
+    @order.each do |item, quantity|
+      price = menu[item] 
+      total += price * quantity
+    end
+    total
+  end
+
+  private
+
+  def item_list
+    @order.each do |item, quantity| 
+      price = menu[item]
+      puts "#{quantity}x #{item} £#{'%.2f' % price} each" end
+  end
+
+  def menu
+    @menu.menu
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,26 +1,31 @@
 require 'order'
 
 describe Order do
+  let(:menu) { double "Menu" }
+  let(:current_order) { described_class.new(menu) }
 
   describe "#add_to_order" do
     it 'should be able to add items to an order' do
-      subject.add_to_order(0,1)
-      expect(subject.order).to eq({"Fried Rice" => 1})
+      allow(menu).to receive(:menu).and_return({"Fried Rice" => 6.0})
+      current_order.add_to_order(0,1)
+      expect(current_order.order).to eq({"Fried Rice" => 1})
     end
   end
 
   describe "#place_order" do
-    it 'should be able to place a order of items from the menu' do
-      subject.add_to_order(0,1)
-      expect(subject.place_order).to include(subject.order)
+    it 'should be able to place a order of items from the menu, which will then work out the total price' do
+      allow(menu).to receive(:menu).and_return({"Fried Rice" => 6.0})
+      current_order.add_to_order(0,1)
+      current_order.place_order
+      expect(current_order.total_cost).to eq(6)
     end
   end
 
   describe "#receipt" do
     it 'should give a receipt after a order has been placed' do
-      subject.add_to_order(0,1)
-      subject.place_order
-      expect { subject.receipt }.to output(" ").to_stdout
+      allow(menu).to receive(:menu).and_return({"Fried Rice" => 6.0})
+      current_order.add_to_order(0,1)
+      expect { current_order.receipt }.to output("Thank you for ordering at Su's Takeaway\nYour order today was:\n1x Fried Rice £6.00 each\nThe total amount for this order is £6.00\n").to_stdout
     end
   end
 end
