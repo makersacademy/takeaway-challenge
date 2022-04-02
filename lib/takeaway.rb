59e03f4ce70c9c5ff817ac443b8db936753b42cf
Joe McNally <joe.t.mcnally@hotmail.com>
2022-04-02 15:19:19 +0100

    Created and passed the test for the first user story when the customer wants to see a list of all the items on the menu with pricing. This was achieved by creating a menu instance variable in the Takeaway class and creating a method that lists each item with price.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,13 @@
 class Takeaway
+  def initialize
+    @menu = [
+      { name: "Korma", price: 8.50 },
+      { name: "Jalfrezi", price: 9.00 },
+      { name: "Tikka Masala", price: 8.00 }
+    ]
+  end
 
+  def list_menu
+    @menu.map { |item| "#{item[:name]}: £#{item[:price]}0" }.join("\n")
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/takeaway'
 
 describe Takeaway do
-  
+  context "#list_menu" do
+    it "shows a list of items with pricing" do
+      expect(subject.list_menu).to eq ("Korma: £8.50\n" +
+                                       "Jalfrezi: £9.00\n" +
+                                       "Tikka Masala: £8.00")
+    end
+  end
 end
