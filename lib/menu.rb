a721c9329301882176c58905017fa6b259e94339
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-04-03 23:40:48 +0100

    created a menu which lists dishes & prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,21 @@
 class Menu
- 
+  attr_reader :lists_dishes
 
+  def initialize
+    @list_hash = {}
+  end
+
+  def lists_dishes
+    @List_hash = 
+        { "Cod" => 11.0, 
+          "Rock" => 13.0, 
+          "Haddock" => 13.0, 
+          "Plaice" => 13.0,
+          "Skate" => 18.0,
+          "Code Roe" => 3.60,
+          "Fish Cake" => 3.0,
+          "Chips" => 3.80,
+          "chip Roll" => 4.50,   
+        }
+  end 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,16 @@
-require "menu"
+require_relative '../lib/menu'
 
 describe Menu do
-  it' shows a list of dishes with prices' do
+
+ describe "#lists_dishes" do
+
+  it' shows a list of all dishes with prices' do
+    menu = Menu.new
+    expect(subject).to respond_to(:lists_dishes)
     
   end
-end  
+ end  
+end
+  
+
 
