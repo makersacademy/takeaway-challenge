ed6d4a814a06d45e29609676ea1b9a1eefe17078
Anish <anishkakaiya@outlook.com>
2022-04-30 10:53:48 +0100

    add method to order dishes

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,9 @@
 require_relative "dishes"
 
 class Order
+
+  def order_dishes(dish, quantity=1)
+
+  end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,4 +4,9 @@ describe Order do
   it "can make a new instance of order" do
     expect(Order.new).to be_an_instance_of(Order)
   end
+
+  it "can respond to order_dishes method" do
+    order = Order.new
+    expect(order).to respond_to(:order_dishes).with(2).argument
+  end
 end
