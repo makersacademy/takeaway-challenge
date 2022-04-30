1eed378a41d76c417287b51bc87fb9ce67ba4658
LGretzk <LGretzk@protonmail.com>
2022-04-30 21:17:25 +0100

    Add remove dish and total

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,40 @@
+require_relative 'menu'
+
 class Order
+
+  attr_reader :current_order
+
+  def initialize
+    @current_order = []
+  end
+
+  def add_dish(id, quantity)
+    dish = {:dish => @@menu.select_dish(id), :quantity => quantity}
+    dish_already_added?(id) ? update_dish_quantity(id, quantity) : @current_order << dish
+  end
+
+  def remove_dish(id)
+    fail 'Dish has not been added' unless dish_already_added?(id)
+    index = @current_order.index { |current_dish| current_dish[:dish][:id] == id }
+    @current_order.delete_at(index)
+  end
+
+  def update_dish_quantity(id, quantity)
+    remove_dish(id) if quantity < 1
+    @current_order.each { |current_dish| current_dish[:quantity] += quantity if current_dish[:dish][:id] == id }
+  end
+
+  def total
+    @current_order.map { |current_dish| current_dish[:dish][:price] * current_dish[:quantity]}.reduce(:+)
+  end
+
+
+  private
+
+  @@menu = Menu.new
+
+  def dish_already_added?(id)
+    (@current_order.select { |current_dish| current_dish[:dish][:id] == id }) != [] ? true : false
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,9 +3,53 @@ require 'order'
 describe Order do
 
   subject(:order) {Order.new}
-
+  let(:dish) { {id: 1, name: 'Spinach & Cheddar Pancakes', price: 7, available?: true} }
+  let(:menu) {instance_double('Menu')}
+  
   it 'creates an instance of the order class' do
     expect(order).to be_instance_of(Order)
+  end
+
+  it 'initializes with the order variable' do
+    expect(order.current_order).to eq([])
+  end
+
+  it 'adds a dish to the current_order' do
+    allow(menu).to receive(:select_dish).and_return(dish)
+    order.add_dish(1, 1)
+    expect(order.current_order).to eq([{:dish => dish, :quantity => 1}])
+  end
+
+  it 'updates the quantity if the same dish is added twice' do
+    allow(menu).to receive(:select_dish).and_return(dish)
+    order.add_dish(1, 1)
+    order.add_dish(1, 1)
+    expect(order.current_order).to eq([{:dish => dish, :quantity =>2}])
+  end
+
+  it 'removes the dish from the current order' do
+    allow(menu).to receive(:select_dish).and_return(dish)
+    order.add_dish(1, 1)
+    order.remove_dish(1)
+    expect(order.current_order).to eq([])
+  end
+  
+  it 'raises an erro when removing dish that has not been added' do
+    expect {order.remove_dish(1)}.to raise_error 'Dish has not been added'
+  end
+
+  it 'removes the dish from the current order if the quantity is zero' do
+    allow(menu).to receive(:select_dish).and_return(dish)
+    order.add_dish(1, 1)
+    order.update_dish_quantity(1, 0)
+    expect(order.current_order).to eq([])
+  end
+
+  it 'calculates total for the current order' do
+    allow(menu).to receive(:select_dish).and_return(dish)
+    order.add_dish(1, 1)
+    order.add_dish(1, 1)
+    expect(order.total).to eq 14
   end
 
 end
