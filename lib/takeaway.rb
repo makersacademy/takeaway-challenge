9867be68575e6bfb39c548e9d2d145798fcc5dee
Anish <anishkakaiya@outlook.com>
2022-04-30 23:27:58 +0100

    add test and code for adding food to order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,9 +1,29 @@
 require_relative "dishes"
 
-class Order
+class Takeaway
 
-  def order_dishes(dish, quantity=1)
-
+  def initialize
+    @order = []
+    @dishes = Dishes.new
   end
 
+  def order_dishes(dish, quantity = 1)
+    quantity.times { search_menu(dish) }
+  end
+
+  def show_order
+    @order
+  end
+
+  def show_menu
+    @dishes.show_menu
+  end
+
+  private
+
+  def search_menu(dish)
+    @dishes.show_menu.each do |dish_name| 
+      @order.push(dish_name) if dish == dish_name[:name]
+    end
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,12 +1,18 @@
 require "takeaway"
 
-describe Order do
-  it "can make a new instance of order" do
-    expect(Order.new).to be_an_instance_of(Order)
+describe Takeaway do
+  it "can make a new instance of Takeaway" do
+    expect(Takeaway.new).to be_an_instance_of(Takeaway)
   end
 
-  it "can respond to order_dishes method" do
-    order = Order.new
-    expect(order).to respond_to(:order_dishes).with(2).argument
+  it "can respond to Takeaway_dishes method" do
+    takeaway = Takeaway.new
+    expect(takeaway).to respond_to(:order_dishes).with(2).arguments
+  end
+
+  it "can record an order" do
+    takeaway = Takeaway.new
+    takeaway.order_dishes("Avocado On Toast", 2)
+    expect(takeaway.show_order).to eq([{ name: "Avocado On Toast", price: 5.50 }, { name: "Avocado On Toast", price: 5.50 }])
   end
 end
