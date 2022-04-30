ffae6a3bd0c3bbff802e2c511b242f119c77c10c
Erlantz <erlantzram@gmail.com>
2022-05-01 00:44:15 +0100

    check order method added with tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,7 +6,7 @@ class Takeaway
 
     @menu = { "Apple Pie" => 4.5,
       "Banana Pie" => 4.5,
-      "Tiramisu" => 5}
+      "Tiramisu" => 5 }
 
     @order = {}
 
@@ -20,7 +20,7 @@ class Takeaway
 
   end
 
-  def add_to_order (dish, quantity)
+  def add_to_order(dish, quantity)
 
     fail 'Not part of the menu' unless dish_on_menu?(dish)
     @order[dish] = quantity
@@ -30,6 +30,18 @@ class Takeaway
   def dish_on_menu?(dish)
 
     @menu[dish]
+
+  end
+
+  def check_order
+
+    total = 0
+    @order.each do |dish, quantity|
+      puts "#{dish}: #{quantity} --> £%.2f" % (@menu[dish] * quantity)
+      total += (quantity * @menu[dish])
+    end
+    puts "--------------"
+    puts "Total --> £%.2f" % total
 
   end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -17,13 +17,25 @@ describe Takeaway do
     it 'adds to order if part of menu' do
 
       subject.add_to_order("Apple Pie", 2)
-      expect(subject.order).to eq ({"Apple Pie" => 2})
+      expect(subject.order).to eq ({ "Apple Pie" => 2 })
 
     end
 
     it 'raises an error if dish is no part of the menu' do
 
       expect { subject.add_to_order("Coffee Pie", 4) }.to raise_error 'Not part of the menu'
+
+    end
+
+  end
+
+  describe '#check_order' do
+
+    it 'prints complete order with total amount' do
+
+      subject.add_to_order("Apple Pie", 2)
+      subject.add_to_order("Tiramisu", 1)
+      expect { subject.check_order }.to output("Apple Pie: 2 --> £9.00\nTiramisu: 1 --> £5.00\n--------------\nTotal --> £14.00\n").to_stdout
 
     end
 
