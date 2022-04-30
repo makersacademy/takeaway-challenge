a40c406f44299300e982f3274bc9149f105985d2
Sam Button <sam_button_94@hotmail.co.uk>
2022-04-30 22:03:06 +0100

    User story 1: finished

--- docs/takeaway.rb
+++ docs/takeaway.rb
@@ -1,3 +1,29 @@
+
+
 class Takeaway
 
+  attr_reader :menu
+
+  def initialize
+    @menu = [{ item: "Guinness", price: 6.00, available: true },
+    { item: "Espresso Martini", price: 12.50, available: true },
+    { item: "Ruddles", price: 1.99, available: true },
+    { item: "Huel", price: 3.00, available: false }]
+  end 
+
+  def see_menu
+    menu_items
+  end
+
+  def menu_items
+    ["#{@menu[0][:item]}" + " £#{@menu[0][:price]}",
+    "#{@menu[1][:item]}" + " £#{@menu[1][:price]}",
+    "#{@menu[2][:item]}" + " £#{@menu[2][:price]}",
+    "#{@menu[3][:item]}" + " £#{@menu[3][:price]}"]
+  end
 end
+
+# @menu.each do |dish|
+#   dish.each do |key,value|
+#     (key.to_s + ': ' + value.to_s)
+#   end
