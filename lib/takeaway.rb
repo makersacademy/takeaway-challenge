7d9a8193aa93894c7f70b9a6ef81f3b1f031015a
Luke Storey <lukemstorey@gmail.com>
2022-04-30 13:31:14 +0100

    Add interaction with Menu class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,13 +1,15 @@
 class Takeaway
-  def initialize(menu)
+  attr_reader :menu, :current_order
+
+  def initialize(menu = Menu.new)
     @menu = menu
   end
 
-  def see_menu
-    menu
+  def display_menu
+    menu.display_available_dishes
   end
 
   private
 
-  attr_reader :menu
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,10 +1,17 @@
 describe Takeaway do
-  let(:menu) { double('Menu') }
+  let(:menu) { instance_double('Menu') }
+  subject(:takeaway) { Takeaway.new(menu)}
 
-  describe '#see_menu' do
-    it 'should return menu' do
-      takeaway = Takeaway.new(menu)
-      expect(takeaway.see_menu).to eq(menu)
+  describe '#display_menu' do
+    it 'should instruct Menu to return available dishes' do
+      expect(menu).to receive(:display_available_dishes)
+      takeaway.display_menu
+    end
+  end
+
+  describe '#add_to_order' do
+    it 'should create a new order' do
+      expect(takeaway.current_order).to be_instance_of Order
     end
   end
 end
