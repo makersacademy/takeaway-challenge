d28ddec7fe4c7d4a1523f24c1a22aef7bf4459f5
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-01 13:04:32 +0100

    able to load csv menus and convert to hash

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,22 @@
 require 'csv'
 
 class Menu
-  CSV.read("pizza_menu.csv")
+
+  
+  def initialize(file_name) 
+    @file = File.open("./lib/#{file_name}", "r")
+    @menu_hash = {}
+  end
+
+  def csv_to_hash
+    @file.readlines.each do |line|
+      key, value = line.chomp.split(",")
+      @menu_hash[key] = value
+    end
+    @menu_hash
+  end
+
+  def read_menu
+    @file.read
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,10 +2,22 @@ require 'menu'
 
 describe Menu do
 
-  subject(:menu) { Menu.new }
+  subject(:menu) { Menu.new("pizza_menu.csv") }
 
   it 'creates new instances of Menu class' do
-    expect(subject).to be_an_instance_of(Menu)
+    expect(menu).to be_an_instance_of(Menu)
+  end
+
+  describe '# read_menu' do
+    it 'reads the menu without raising error' do
+      expect { menu.read_menu }.not_to raise_error
+    end
+  end
+
+  describe '# csv_to_hash' do
+    it 'converts a csv file into a hash' do
+      expect(menu.csv_to_hash).to be_an_instance_of(Hash)
+    end
   end
 
 end
