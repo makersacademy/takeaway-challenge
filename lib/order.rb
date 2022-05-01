b75551fc14b2d72b984a3bd18763d981693ce699
Luke Storey <lukemstorey@gmail.com>
2022-05-01 13:14:05 +0100

    Add order already complete check

--- lib/order.rb
+++ lib/order.rb
@@ -1,19 +1,23 @@
 class Order
-  attr_reader :basket, :complete_status
+  attr_reader :basket, :complete_status, :basket_total
 
   def initialize
     @basket = [] # [{'pizza': 9.50}, {'pasta': 8.20}]
+    @basket_total = 0
     @complete_status = false
   end
 
-  def add_to_basket(*dishes)
+  def add_to_basket(dishes)
     dishes.each { |dish| basket << dish }
+    calculate_basket_total
   end
 
-  def display_basket_and_total # return basket and return basket total
+  def display_basket_total
+    {basket => basket_total}
   end
 
   def complete_order(completion_time)
+    order_complete?
     basket_empty?
     self.complete_status = true
     self.completion_time = completion_time
@@ -21,11 +25,21 @@ class Order
 
   private 
 
-  attr_writer :complete_status, :completion_time
-
-  attr_accessor :basket_dishes, :basket_prices, :basket_total
+  attr_writer :complete_status, :completion_time, :basket_total
 
   def basket_empty?
     fail "No dishes in basket!" if basket.empty?
   end
+
+  def order_complete?
+    fail "Order already completed!" if complete_status == true
+  end
+
+  def calculate_basket_total
+    prices = []
+    basket.each do |item| 
+      prices << item.values[0]
+    end
+    self.basket_total = prices.sum
+  end 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,29 +1,26 @@
 describe Order do
   subject(:order) { Order.new }
-  dish1 = {'pizza': 9.50}
-  dish2 = {'pasta': 8.20}
+  dishes = [{'pizza': 9.50}, {'pasta': 8.20}]
 
   describe '#add_to_basket' do
     it 'should change basket to include dishes' do
-      order.add_to_basket(dish1, dish2)
-      expect(order.basket).to include(dish1, dish2)
+      order.add_to_basket(dishes)
+      expect(order.basket).to eq(dishes)
     end
   end
   
-  describe '#display_basket_and_total' do
-    xit 'should return basket dishes and prices and total' do
-      order.add_to_basket(dish1, dish2)
-
-      expect(order).to receive(:basket_dishes)
-      order.display_basket_and_total
+  describe '#display_basket_total' do
+    it 'should return basket dishes and prices and total' do
+      order.add_to_basket(dishes)
+      expect(order.display_basket_total).to eq(dishes => 17.70)
     end
   end
 
   describe '#complete_order' do
     context 'when basket contains dishes' do
+      before(:each) { order.add_to_basket(dishes) }
+      
       it 'should change order status to completed' do
-        order.add_to_basket(dish1, dish2)
-
         expect { order.complete_order('time') }.to change { 
           order.complete_status }.from(false).to(true)
       end
@@ -31,6 +28,11 @@ describe Order do
       it 'should set time order completed'
 
       it 'should set estimated delivery time'
+
+      it 'should not allow a completed order to be completed again' do
+        allow(order).to receive(:complete_status).and_return(true)
+        expect { order.complete_order('time') }.to raise_error "Order already completed!"
+      end
     end
 
     context 'when basket is empty' do
