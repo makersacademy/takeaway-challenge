235b77af90140a51c96edb6000978b8f3fd36816
Luke Storey <lukemstorey@gmail.com>
2022-05-01 11:44:12 +0100

    Refactor to use represented by hash pair

--- lib/order.rb
+++ lib/order.rb
@@ -2,16 +2,19 @@ class Order
   attr_reader :basket, :complete_status
 
   def initialize
-    @basket = []
+    @basket = [] # [{'pizza': 9.50}, {'pasta': 8.20}]
     @complete_status = false
-    @completion_time = nil
   end
 
-  def add_to_basket(dishes)
+  def add_to_basket(*dishes)
     dishes.each { |dish| basket << dish }
   end
 
+  def display_basket_and_total # return basket and return basket total
+  end
+
   def complete_order(completion_time)
+    basket_empty?
     self.complete_status = true
     self.completion_time = completion_time
   end
@@ -19,5 +22,10 @@ class Order
   private 
 
   attr_writer :complete_status, :completion_time
-  
+
+  attr_accessor :basket_dishes, :basket_prices, :basket_total
+
+  def basket_empty?
+    fail "No dishes in basket!" if basket.empty?
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,17 +1,42 @@
 describe Order do
   subject(:order) { Order.new }
+  dish1 = {'pizza': 9.50}
+  dish2 = {'pasta': 8.20}
 
   describe '#add_to_basket' do
-    it 'should change basket by dishes chosen' do
-      dishes = ['pizza', 'pasta']
-      expect { order.add_to_basket(dishes) }.to change { 
-        order.basket.length }.by(dishes.length)
+    it 'should change basket to include dishes' do
+      order.add_to_basket(dish1, dish2)
+      expect(order.basket).to include(dish1, dish2)
+    end
+  end
+  
+  describe '#display_basket_and_total' do
+    xit 'should return basket dishes and prices and total' do
+      order.add_to_basket(dish1, dish2)
+
+      expect(order).to receive(:basket_dishes)
+      order.display_basket_and_total
     end
   end
 
   describe '#complete_order' do
-    it 'should change complete_status to completed' do
-      expect { order.complete_order('time') }.to change { order.complete_status}.from(false).to(true)
+    context 'when basket contains dishes' do
+      it 'should change order status to completed' do
+        order.add_to_basket(dish1, dish2)
+
+        expect { order.complete_order('time') }.to change { 
+          order.complete_status }.from(false).to(true)
+      end
+
+      it 'should set time order completed'
+
+      it 'should set estimated delivery time'
+    end
+
+    context 'when basket is empty' do
+      it 'should return error when basket is empty' do
+        expect { order.complete_order('time') }.to raise_error "No dishes in basket!"
+      end
     end
   end
 end
