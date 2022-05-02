9a34c7967d60475a046cdddd641d08bc66d3675d
Russell <russellmorbey@hotmail.co.uk>
2022-05-02 14:28:40 +0100

    Created Menu Class and spec. All tests passed.

---
+++ spec/menu_spec.rb
@@ -0,0 +1,22 @@
+require 'menu'
+
+describe Menu do
+  describe '#view_dishes' do
+    it 'shows a list of dishes with prices' do
+
+      expect(subject.view_dishes).to eq({
+        "Burger" => 5,
+        "Chips" => 3,
+        "Hot Dog" => 4,
+        "Wings" => 3
+      })
+    end
+  end
+
+  describe '#check_if_available' do
+
+    it 'returns a message if the dish is not available on the menu' do
+      expect(subject.check_if_available("Pie")).to eq('That dish is not available. Please select another dish.')
+    end
+  end
+end
