64e5a55644f40211c90a49288ad75ec185f07703
Kieran Carty <kierancarty@Kierans-MacBook-Air.local>
2022-05-03 07:38:22 +0100

    latest

--- spec/takeaway-challenge_spec.rb
+++ spec/takeaway-challenge_spec.rb
@@ -13,17 +13,17 @@ describe Takeaway do
     end
   end 
 
-  describe 'billing' do 
-    it 'Calculates the customers bill' do
-      subject.place_order(1)
-      expect(subject.bill).to eq(subject.menu[0][:price])
-    end 
+  context 'places an order' do
+    before(:each) {subject.place_order(1)}
 
-    it 'verifies the bill is correct based on the customers order' do
-    end 
-  end 
+    describe 'order confirmation' do 
+      it 'Calculates the correct customers bill' do
+        expect(subject.bill).to eq(subject.menu[0][:price])
+      end 
+    end
+  end  
 end
 
 # As a customer
-# So that I can verify that my order is correct
-# I would like to check that the total I have been given matches the sum of the various dishes in my ordergit
+# So that I am reassured that my order will be delivered on time
+# I would like to receive a text such as "Thank you! Your order was placed and will be delivered before 18:52" after I have ordered
