c72f8923c8da7c769fc8c8a5ca3cfcf17521ba1a
sjtinsley <simonjtinsley@gmail.com>
2022-05-03 09:40:59 +0100

    Saving all files

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,11 +1,12 @@
 require 'twilio-ruby'
-class SMS(client_class=Twilio::REST::Client)
+
   account_sid = "AC4652867e62a8ad85f253fcd1c96016ba" # Your Test Account SID from www.twilio.com/console/settings
   auth_token = "590379d91fe68d1e887e7dcf91354037"   # Your Test Auth Token from www.twilio.com/console/settings
   
-  @client = client_class.new account_sid, auth_token
+  @client = Twilio::REST::Client.new account_sid, auth_token
+  message = @client.messages.create(
+      body: "Hello from Ruby",
+      to: "+447447062441",    # Replace with your phone number
+      from: "+15005550006")  # Use this Magic Number for creating SMS
   
-  def message 
-    @client.messages.create(body: "Thank you! Your order was placed and will be delivered soon.", to: "+447447062441", from: "+15005550006")
-  end
-end
+  puts message.sid
