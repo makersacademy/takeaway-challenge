848b0f862e5f1b68a1f59c191e0693db7396432b
Mike Oliver <remoteunderscore@gmail.com>
2022-11-26 13:05:41 +0000

    Initial RSpec test for menu passed

---
+++ app.rb
@@ -0,0 +1,3 @@
+require_relative 'lib/menu'
+# require 'order'
+puts menu
---
+++ lib/menu.rb
@@ -0,0 +1,25 @@
+class Menu
+  def initialize
+    menu = [
+      { "Pizza" => "7.99" },
+      { "Fish and Chips" => "9.99" },
+      { "Pancakes with Bacon and Maple Syrup" => "6.99" },
+      { "Vegetable Stir Fry with Gyoza" => "8.99" },
+      { "Cheese and Pickle Sandwich" => "4.99" },
+      { "Ramen" => "6.99" },
+      { "Beef Pho" => "9.99" }
+    ]
+  end
+
+  def menu
+    menu = [
+      { "Pizza" => "7.99" },
+      { "Fish and Chips" => "9.99" },
+      { "Pancakes with Bacon and Maple Syrup" => "6.99" },
+      { "Vegetable Stir Fry with Gyoza" => "8.99" },
+      { "Cheese and Pickle Sandwich" => "4.99" },
+      { "Ramen" => "6.99" },
+      { "Beef Pho" => "9.99" }
+    ]
+  end
+end
